c5ca73fee74481df5796f33a476ba3701cf73cb4
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-01-05 15:19:28 +0000

    Bump to 0.5.3 David Cournapeau <david@ar.media.kyoto-u.ac.jp> | 2006-10-03 18:34:42 +0900 (Tue, 03 Oct 2006)
    From: cdavid <cdavid@cb17146a-f446-4be1-a4f7-bd7c5bb65646>
    
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@82 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

--- scikits/learn/pyem/pyem/__init__.py
+++ scikits/learn/pyem/pyem/__init__.py
@@ -1,12 +1,12 @@
 #! /usr/bin/env python
-# Last Change: Thu Sep 28 01:00 PM 2006 J
+# Last Change: Tue Oct 03 06:00 PM 2006 J
 
 from info import __doc__
 
 from numpy.testing import NumpyTest
 test = NumpyTest().test
 
-version = '0.5.2'
+version = '0.5.3'
 
 from gauss_mix import GmParamError, GM
 from gmm_em import GmmParamError, GMM, EM
