e570e2fd82ef2949f66deb0780f8b8e9983e78be
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-01-05 16:33:43 +0000

    Build a proper libsvm DLL on Windows.
    From: fullung <fullung@cb17146a-f446-4be1-a4f7-bd7c5bb65646>
    
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@157 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

--- scikits/learn/svm/libsvm-2.82/svm.h
+++ scikits/learn/svm/libsvm-2.82/svm.h
@@ -1,6 +1,16 @@
 #ifndef _LIBSVM_H
 #define _LIBSVM_H
 
+#ifdef LIBSVM_DLL
+#ifdef LIBSVM_EXPORTS
+#define LIBSVM_API __declspec(dllexport)
+#else
+#define LIBSVM_API __declspec(dllimport)
+#endif /* _LIBSVM_EXPORTS */
+#else
+#define LIBSVM_API
+#endif /* _WIN32 */
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -42,26 +52,26 @@ struct svm_parameter
 	int probability; /* do probability estimates */
 };
 
-struct svm_model *svm_train(const struct svm_problem *prob, const struct svm_parameter *param);
-void svm_cross_validation(const struct svm_problem *prob, const struct svm_parameter *param, int nr_fold, double *target);
+LIBSVM_API struct svm_model *svm_train(const struct svm_problem *prob, const struct svm_parameter *param);
+LIBSVM_API void svm_cross_validation(const struct svm_problem *prob, const struct svm_parameter *param, int nr_fold, double *target);
 
-int svm_save_model(const char *model_file_name, const struct svm_model *model);
-struct svm_model *svm_load_model(const char *model_file_name);
+LIBSVM_API int svm_save_model(const char *model_file_name, const struct svm_model *model);
+LIBSVM_API struct svm_model *svm_load_model(const char *model_file_name);
 
-int svm_get_svm_type(const struct svm_model *model);
-int svm_get_nr_class(const struct svm_model *model);
-void svm_get_labels(const struct svm_model *model, int *label);
-double svm_get_svr_probability(const struct svm_model *model);
+LIBSVM_API int svm_get_svm_type(const struct svm_model *model);
+LIBSVM_API int svm_get_nr_class(const struct svm_model *model);
+LIBSVM_API void svm_get_labels(const struct svm_model *model, int *label);
+LIBSVM_API double svm_get_svr_probability(const struct svm_model *model);
 
-void svm_predict_values(const struct svm_model *model, const struct svm_node *x, double* dec_values);
-double svm_predict(const struct svm_model *model, const struct svm_node *x);
-double svm_predict_probability(const struct svm_model *model, const struct svm_node *x, double* prob_estimates);
+LIBSVM_API void svm_predict_values(const struct svm_model *model, const struct svm_node *x, double* dec_values);
+LIBSVM_API double svm_predict(const struct svm_model *model, const struct svm_node *x);
+LIBSVM_API double svm_predict_probability(const struct svm_model *model, const struct svm_node *x, double* prob_estimates);
 
-void svm_destroy_model(struct svm_model *model);
-void svm_destroy_param(struct svm_parameter *param);
+LIBSVM_API void svm_destroy_model(struct svm_model *model);
+LIBSVM_API void svm_destroy_param(struct svm_parameter *param);
 
-const char *svm_check_parameter(const struct svm_problem *prob, const struct svm_parameter *param);
-int svm_check_probability_model(const struct svm_model *model);
+LIBSVM_API const char *svm_check_parameter(const struct svm_problem *prob, const struct svm_parameter *param);
+LIBSVM_API int svm_check_probability_model(const struct svm_model *model);
 
 #ifdef __cplusplus
 }
