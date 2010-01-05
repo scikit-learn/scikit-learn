6bd1582e0aa3caa56dd16e1f93575abfeb7ee4d1
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-01-05 13:53:01 +0000

    Updated __init__
    From: fred.mailhot <fred.mailhot@cb17146a-f446-4be1-a4f7-bd7c5bb65646>
    
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@34 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

--- scikits/learn/ann/__init__.py
+++ scikits/learn/ann/__init__.py
@@ -1,12 +1,6 @@
-#
 # req'd file for SciPy package (see DEVELOPERS.txt)
 # Fred Mailhot
 # 2006-06-13
-#
 
-# docstring
-from mlp import __doc__
-
-# symbols
-from mlp import *
+__all__ = ['mlp','srn','rbf']
 
