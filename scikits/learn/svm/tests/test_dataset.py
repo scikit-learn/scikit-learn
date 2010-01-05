b50628a0f5e44c69d1021b005070ae30a2eacfd2
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-01-05 16:44:20 +0000

    Basic tests for datasets.
    From: fullung <fullung@cb17146a-f446-4be1-a4f7-bd7c5bb65646>
    
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@160 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

---
+++ scikits/learn/svm/tests/test_dataset.py
@@ -0,0 +1,59 @@
+from numpy.testing import *
+import numpy as N
+
+from svm.dataset import convert_to_svm_node
+from svm.dataset import *
+from svm.libsvm import svm_node_dtype
+
+class test_dataset(NumpyTestCase):
+    def check_convert_dict(self):
+        x = N.array([(-1,0.)], dtype=svm_node_dtype)
+        assert_array_equal(convert_to_svm_node({}), x)
+
+        x = N.array([(1,2.),(-1,0.)], dtype=svm_node_dtype)
+        assert_array_equal(convert_to_svm_node({1:2.}), x)
+
+        x = N.array([(1,2.),(3,4.),(-1,0.)], dtype=svm_node_dtype)
+        assert_array_equal(convert_to_svm_node({1:2.,3:4.}), x)
+
+        # check for positive indexes
+        self.assertRaises(AssertionError, convert_to_svm_node, {0:0.})
+
+    def check_convert_list(self):
+        x = N.array([(-1,0.)], dtype=svm_node_dtype)
+        assert_array_equal(convert_to_svm_node([]), x)
+
+        x = N.array([(1,2.),(3,4.),(-1,0.)], dtype=svm_node_dtype)
+        # check that indexes are sorted
+        assert_array_equal(convert_to_svm_node([(3,4.),(1,2.)]), x)
+
+        # check for unique indexes
+        self.assertRaises(AssertionError,
+                          convert_to_svm_node, [(1,0.),(1,0.)])
+
+    def check_convert_array(self):
+        x = N.array([(-1,0.)], dtype=svm_node_dtype)
+        assert_array_equal(convert_to_svm_node(N.empty(0)), x)
+
+        x = N.array([(1,1.),(2,2.),(-1,0.)], dtype=svm_node_dtype)
+        assert_array_equal(convert_to_svm_node(N.arange(1,3)), x)
+
+    def check_regression(self):
+        data = [(1.0, N.arange(5))]
+        dataset = LibSvmRegressionDataSet(data)
+        self.assertAlmostEqual(dataset.gamma, 0.2)
+
+    def check_classification(self):
+        data = [(1, N.arange(4)), (2, N.arange(10))]
+        dataset = LibSvmClassificationDataSet(data)
+        self.assertAlmostEqual(dataset.gamma, 0.1)
+        self.assert_(1 in dataset.labels)
+        self.assert_(2 in dataset.labels)
+
+    def check_oneclass(self):
+        data = [N.arange(2)]
+        dataset = LibSvmOneClassDataSet(data)
+        self.assertAlmostEqual(dataset.gamma, 0.5)
+
+if __name__ == '__main__':
+    NumpyTest().run()
