1a85ab2ee949ac9510e8a50a89d6a707a2e7afc8
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-01-05 15:46:24 +0000

    Add data as a proper submodule
    From: cdavid <cdavid@cb17146a-f446-4be1-a4f7-bd7c5bb65646>
    
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@108 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

---
+++ scikits/learn/pyem/data/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+def configuration(parent_package='',top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration('data',parent_package,top_path)
+    config.add_subpackage('oldfaithful')
+    config.make_config_py() # installs __config__.py
+    return config
+
+if __name__ == '__main__':
+    print 'This is the wrong setup.py file to run'
