179dde0bc97870e12c4d54a7f51d75991cffcd10
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-01-05 15:39:25 +0000

    Add faithful data in data.
    From: cdavid <cdavid@cb17146a-f446-4be1-a4f7-bd7c5bb65646>
    
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@102 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

---
+++ scikits/learn/pyem/data/oldfaithful/__init__.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+# Last Change: Wed Apr 25 06:00 PM 2007 J
+import faith as _faith
+__doc__     = _faith.DESCRSHORT
+copyright   = _faith.COPYRIGHT
+source      = _faith.SOURCE
+
+load        = _faith.load
