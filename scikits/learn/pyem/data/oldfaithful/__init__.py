1409be5990d8c57aec23703fc807fc6bfa8b8dfa
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-01-05 16:17:43 +0000

    Remove datasets (include them in sckits.learn instead)
    From: cdavid <cdavid@cb17146a-f446-4be1-a4f7-bd7c5bb65646>
    
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@134 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

--- scikits/learn/pyem/data/oldfaithful/__init__.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-# Last Change: Fri Jun 08 12:00 PM 2007 J
-import data as _faith
-__doc__     = _faith.DESCRSHORT
-copyright   = _faith.COPYRIGHT
-source      = _faith.SOURCE
-
-load        = _faith.load
--- scikits/learn/pyem/data/pendigits/__init__.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-# Last Change: Thu Jun 21 06:00 PM 2007 J
-import data as _pendigit
-__doc__     = _pendigit.DESCRSHORT
-copyright   = _pendigit.COPYRIGHT
-source      = _pendigit.SOURCE
-
-load        = _pendigit.load
