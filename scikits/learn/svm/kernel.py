d8810b50a507477e2f88f683641eed1a0e681f7d
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-01-05 16:32:17 +0000

    Separated out kernels. Added facility for custom kernels.
    From: fullung <fullung@cb17146a-f446-4be1-a4f7-bd7c5bb65646>
    
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@153 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

---
+++ scikits/learn/svm/kernel.py
@@ -0,0 +1,75 @@
+__all__ = [
+    'LinearKernel',
+    'PolynomialKernel',
+    'RBFKernel',
+    'SigmoidKernel',
+    'CustomKernel'
+    ]
+
+def svm_node_dot(x, y):
+    # associate node indexes with array indexes
+    xidx = dict(zip(x['index'][:-1],range(0,len(x))))
+    yidx = dict(zip(y['index'][:-1],range(0,len(y))))
+    # indexes in either vector
+    indexes = N.unique(N.hstack([x['index'],y['index']]))
+    z = 0.
+    for j in indexes:
+        if j in xidx and j in yidx:
+            # dot if index is present in both vectors
+            z += x['value'][xidx[j]]*y['value'][yidx[j]]
+    return z
+
+class LinearKernel:
+    def __init__(self, dot=svm_node_dot):
+        self.dot = dot
+
+    def __call__(self, x, y):
+        return self.dot(x, y)
+
+class PolynomialKernel:
+    def __init__(self, degree, gamma, coef0, dot=svm_node_dot):
+        self.degree = degree
+        self.gamma = gamma
+        self.coef0 = coef0
+        self.dot = dot
+
+    def __call__(self, x, y):
+        base = self.gamma*self.dot(x, y) + self.coef0
+        tmp = base
+        ret = 1.
+        t = self.degree
+        while t > 0:
+            if t % 2 == 1: ret *= tmp
+            tmp *= tmp
+            t /= 2
+        return ret
+
+class RBFKernel:
+    def __init__(self, gamma, dot=svm_node_dot):
+        self.gamma = gamma
+        self.dot = dot
+
+    def __call__(self, x, y):
+        z = self.dot(x) + self.dot(y) - 2*self.dot(x, y)
+        return N.exp(-self.gamma*z)
+
+class SigmoidKernel:
+    def __init__(self, gamma, coef0, dot=svm_node_dot):
+        self.gamma = gamma
+        self.coef0 = coef0
+        self.dot = dot
+
+    def kernel_sigmoid(x, y, gamma, coef0):
+        return N.tanh(self.gamma*self.dot(x, y)+self.coef0)
+
+class CustomKernel:
+    """
+    XXX example CustomKernel(lambda x, y, d: d(x,y))
+    """
+
+    def __init__(self, f, dot=svm_node_dot):
+        self.f = f
+        self.dot = dot
+
+    def __call__(self, x, y):
+        return self.f(x, y, dot)
