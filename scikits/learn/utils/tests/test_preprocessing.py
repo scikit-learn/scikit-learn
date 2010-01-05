4b049f1102ff2646aa0c5de6f546d5e6e5abe82f
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-01-05 13:42:10 +0000

    Add function to handle Nan data for scaling.
    From: cdavid <cdavid@cb17146a-f446-4be1-a4f7-bd7c5bb65646>
    
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@18 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

--- scikits/learn/utils/tests/test_preprocessing.py
+++ scikits/learn/utils/tests/test_preprocessing.py
@@ -2,7 +2,7 @@ from numpy.testing import *
 import numpy as N
 
 set_package_path()
-from preprocessing import scale, Scaler
+from preprocessing import scale, Scaler, nanscale, NanScaler
 restore_path()
 
 DEPS = N.finfo(N.float).eps
@@ -15,21 +15,44 @@ class test_scale(NumpyTestCase):
     def test_simple(self):
         a = 5 * N.random.randn(10, 4)
         s, t = scale(a)
-        assert N.all(a <= 1 + DEPS) and N.all(a >= -1. - DEPS)
+        assert N.all(a <= 1. + DEPS) and N.all(a >= -1. - DEPS)
 
     def test_right(self):
         a = 5 * N.random.randn(10, 4)
         s, t = scale(a, 'right')
-        assert N.all(a <= 1 + DEPS) and N.all(a >= 0. - DEPS)
+        assert N.all(a <= 1. + DEPS) and N.all(a >= 0. - DEPS)
+
+    def test_nan_simple(self):
+        a = 5 * N.random.randn(10, 4)
+        a[9, 0] = N.nan
+        # We keep a copy around so that we can check that nanscale does not
+        # propagate Nan everywhere.
+        at = a.copy()
+        s, t = nanscale(at)
+        assert N.all(at[N.isfinite(a)] <= 1. + DEPS) and \
+               N.all(at[N.isfinite(a)] >= -1. - DEPS)
+
+    def test_nan_right(self):
+        a = 5 * N.random.randn(10, 4)
+        a[9, 0] = N.nan
+        # We keep a copy around so that we can check that nanscale does not
+        # propagate Nan everywhere.
+        at = a.copy()
+        s, t = nanscale(at, 'right')
+        assert N.all(at[N.isfinite(a)] <= 1. + DEPS) and \
+               N.all(at[N.isfinite(a)] >= 0. - DEPS)
 
 class test_Scaler(NumpyTestCase):
     def __init__(self, *args, **kw):
         NumpyTestCase.__init__(self, *args, **kw)
         N.random.seed(0)
 
-    def _common_simple(self, mode):
+    def _generate_simple(self):
         a = 5 * N.random.randn(10, 4)
         b = 5 * N.random.randn(10, 4)
+        return a, b
+
+    def _simple_test(self, a, b, mode):
         at = a.copy()
         bt = b.copy()
         sc = Scaler(a, mode)
@@ -37,11 +60,11 @@ class test_Scaler(NumpyTestCase):
         # Test whether preprocess -> unprocess gives back the data
         sc.preprocess(a)
         if mode == 'sym':
-            assert N.all(a <= 1 + DEPS) and N.all(a >= -1. - DEPS)
+            assert N.all(a <= 1. + DEPS) and N.all(a >= -1. - DEPS)
         elif mode == 'right':
-            assert N.all(a <= 1 + DEPS) and N.all(a >= -0. - DEPS)
+            assert N.all(a <= 1. + DEPS) and N.all(a >= -0. - DEPS)
         else:
-            raise ValueError("unepexted mode %s" % str(mode))
+            raise ValueError("unexpected mode %s" % str(mode))
         sc.unprocess(a)
         assert_array_almost_equal(a, at)
 
@@ -49,11 +72,58 @@ class test_Scaler(NumpyTestCase):
         sc.unprocess(b)
         assert_array_almost_equal(b, bt)
 
+    def _nan_test(self, a, b, mode):
+        a[9, 0] = N.nan
+        at = a.copy()
+        bt = b.copy()
+        sc = NanScaler(a, mode)
+
+        # Test whether preprocess -> unprocess gives back the data
+        sc.preprocess(at)
+        if mode == 'sym':
+            assert N.all(at[N.isfinite(a)] <= 1. + DEPS) and \
+                   N.all(at[N.isfinite(a)] >= -1. - DEPS)
+        elif mode == 'right':
+            assert N.all(at[N.isfinite(a)] <= 1. + DEPS) and \
+                   N.all(at[N.isfinite(a)] >= 0. - DEPS)
+        else:
+            raise ValueError("unexpected mode %s" % str(mode))
+        sc.unprocess(at)
+        assert_array_almost_equal(a[N.isfinite(a)], 
+                                  at[N.isfinite(a)])
+
+        sc.preprocess(b)
+        sc.unprocess(b)
+        assert_array_almost_equal(b, bt)
+
     def test_simple(self):
-        self._common_simple('sym')
+        a, b = self._generate_simple()
+        self._simple_test(a, b, 'sym')
 
     def test_right(self):
-        self._common_simple('right')
+        a, b = self._generate_simple()
+        self._simple_test(a, b, 'right')
+
+    def test_nan(self):
+        a, b = self._generate_simple()
+        self._nan_test(a, b, 'sym')
+
+    def test_nan_right(self):
+        a, b = self._generate_simple()
+        self._nan_test(a, b, 'right')
+
+    def test_feature_full_nan(self):
+        """Test that instancing a scaler from data whose feature is full of Nan
+        does not work."""
+        a = N.random.randn(10, 5)
+        a[:, 0] = N.nan
+        try:
+            sc = NanScaler(a)
+            assert(0 == 1, "Creation of scaler from data with full Nan "\
+                           "should not succeed")
+        except ValueError:
+            pass
+
 
 if __name__ == "__main__":
     NumpyTest('scikits.learn.utils.preprocessing').run()
