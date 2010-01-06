93735fe0278cade5daa95844256d94892d627a67
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-01-06 13:31:58 +0000

    Fix: Add std namespace in order to use std::isnan.
    std:isnan is a C99 extension that is not present on some compilers. It
    was defined for MVC but the namespace was missing for the other
    compilers.
    
    From: Fabian Pedregosa <fabian@fseoane.net>
    
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@329 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

--- scikits/learn/machine/manifold_learning/regression/cluster/ModifiedGeneralClustering.h
+++ scikits/learn/machine/manifold_learning/regression/cluster/ModifiedGeneralClustering.h
@@ -13,11 +13,15 @@
 #include <vector>
 #include <cmath>
 
+using namespace std;
+
 namespace Graph
 {
   namespace Clustering
   {
 #ifdef _MSC_VER
+    // std::isnan is a C99 extension
+    // some compileres do not implement it
     inline bool isnan(long double element)
     {
       return element != element;
