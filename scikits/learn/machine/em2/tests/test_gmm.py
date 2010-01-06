5abce80aaacc98fe591ab0035c2d248edc34d97c
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-01-06 10:59:31 +0000

    Add test for logresp.
    From: cdavid <cdavid@cb17146a-f446-4be1-a4f7-bd7c5bb65646>
    
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@306 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

--- scikits/learn/machine/em2/tests/test_gmm.py
+++ scikits/learn/machine/em2/tests/test_gmm.py
@@ -5,6 +5,8 @@ from numpy.testing import *
 from scipy.io import loadmat
 
 from scikits.learn.machine.em2.gmm import Parameters, EM
+from scikits.learn.machine.em2.gm import GM
+from scikits.learn.machine.em2.gmm import logresp as plogresp
 
 def load_dataset(filename):
     from scipy.io import loadmat
@@ -32,6 +34,36 @@ class BasicEmTest(TestCase):
     def test_2d_diag(self, level = 1):
         filename = 'diag_2d_3k.mat'
         self._test(filename)
+
+class TestLogresp(TestCase):
+    def ref(self, x, w, mu, va):
+        k, d = mu.shape
+        tmp = np.ones((x.shape[0], k))
+        for c in range(k):
+            inva = 1/va[c]
+            fac = (2 * np.pi) ** (-d/2.) * np.prod(np.sqrt(inva))
+            tmp[:, c] = np.log(fac) + np.dot((x-mu[c]) ** 2, -0.5 * inva.T)
+
+        tmp += np.log(w)
+        return tmp - np.log(np.sum(np.exp(tmp), axis=-1))[:, np.newaxis]
+
+    def _test(self, d, k, mode):
+        from scikits.learn.machine.em2.gmm import logresp
+        x = np.random.randn(100, d)
+        w, mu, va = GM.genparams(d, k, mode)
+        yr = self.ref(x, w, mu, va)
+
+        y = plogresp(x, w, mu, va)
+        assert_array_almost_equal(y, yr)
+
+    def test_1d_1k(self):
+        return self._test(1, 1, 'diag')
+
+    def test_1d_2k(self):
+        return self._test(1, 2, 'diag')
+
+    def test_1d_10k(self):
+        return self._test(1, 10, 'diag')
 
 if __name__ == '__main__':
     run_module_suite()
