9cd44a04a28d86d55d9849e766554170f5de3364
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-01-06 13:30:15 +0000

    Remove old testing framework
    From: Fabian Pedregosa <fabian@fseoane.net>
    
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@326 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

--- scikits/learn/__init__.py
+++ scikits/learn/__init__.py
@@ -1,6 +1,3 @@
 #
 #  scikits.learn - Machine Learning module
 #
-
-from numpy.testing import Tester
-test = Tester().test
--- tester.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-# Last Change: Sat Jul 21 03:00 PM 2007 J
-
-"""Mini script useful for top level testing of the learn package."""
-
-from scikits.learn import test
-
-test()
-
