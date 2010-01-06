649dead0fd7d18aa8006a249469d2bf54f953916
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-01-06 10:42:23 +0000

    Remove GMM class.
    From: cdavid <cdavid@cb17146a-f446-4be1-a4f7-bd7c5bb65646>
    
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@293 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

--- scikits/learn/machine/em2/gmm.py
+++ scikits/learn/machine/em2/gmm.py
@@ -155,81 +155,22 @@ def logresp(data, w, mu, va):
     return nresp
 
 class EM:
-    def __init__(self):
+    def __init__(self, hint=0):
         pass
 
-    def train(self, data, params, maxiter=10):
+    def train(self, data, params, maxiter=10, hint=0):
         """params is modified in-place."""
         ss = SStats(params.d, params.k, params.mode)
     
         for i in range(maxiter):
-            ss.compute(data, params.w, params.mu, params.va)
+            if hint > 0:
+                ss.reset()
+                for j in range(data.shape[0] / hint):
+                    ss.update(data[j * hint: j * hint + hint], 
+                              params.w, params.mu, params.va)
+                ss.update(data[j * hint + hint:], params.w, params.mu, params.va)
+            else:
+                ss.compute(data, params.w, params.mu, params.va)
             params.update(ss)
 
         return params
-
-class GMM:
-    """A class to model a Gaussian Mixture Model (GMM)."""
-    def __init__(self, gm):
-        """Create a trainable mixture model.
-        
-        Initialize the model from a GM instance. This class implements all the
-        necessary functionalities for EM.
-
-        Parameters
-        ----------
-        gm: GM
-            the mixture model to train.
-        """
-        self.gm = gm
-
-    def logresp(self, data):
-        """Compute log responsabilities.
-        
-        Note
-        ----
-        Computes the latent variable distribution (a posteriori probability)
-        knowing the explicit data for the Gaussian model (w, mu, var): gamma(t,
-        i) = P[state = i | observation = data(t); w, mu, va]
-        """
-        # compute the gaussian pdf
-        resp = mnormalik(data, self.gm.mu, self.gm.va, log=True)
-        # multiply by the weight
-        resp += np.log(self.gm.w)
-        # Normalize to get a (log) pdf
-        nresp = resp - logsumexp(resp)[:, np.newaxis]
-
-        return nresp
-
-    def ss(self, data):
-        if self.gm.mode == 'diag':
-            return self._diag_ss(data)
-        raise ValueError("Mode != diag not yet implemented")
-
-    def _diag_ss(self, data):
-        n = data.shape[0]
-        nresp = np.exp(self.logresp(data))
-        w = np.sum(nresp, axis=0) / n
-
-        wx = np.dot(nresp.T, data) / n
-        wxx = np.dot(nresp.T, data ** 2) / n
-
-        return w, wx, wxx
-
-    def update(self, w, wx, wxx):
-        """Update the model parameters from SS."""
-        if self.gm.mode == 'diag':
-            return self._diag_update(w, wx, wxx)
-        raise ValueError("Mode != diag not yet implemented")
-
-    def _diag_update(self, w, wx, wxx):
-        k, d = self.gm.k, self.gm.d
-
-        mu = np.zeros((k, d))
-        va = np.zeros((k, d))
-
-        for c in range(k):
-            mu[c] = wx[c] / w[c]
-            va[c] = wxx[c] / w[c] - mu[c] ** 2
-
-        return w, mu, va
