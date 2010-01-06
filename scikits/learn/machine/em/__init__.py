f00ae3046436f09e62460a8468e031a0c2027e7f
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-01-06 10:11:33 +0000

    Remove deprecated test runned for em machine.
    From: cdavid <cdavid@cb17146a-f446-4be1-a4f7-bd7c5bb65646>
    
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@271 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

--- scikits/learn/machine/em/__init__.py
+++ scikits/learn/machine/em/__init__.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-# Last Change: Sun Jul 22 01:00 PM 2007 J
+# Last Change: Sun Sep 07 04:00 PM 2008 J
 
 from info import __doc__
 
@@ -8,16 +8,3 @@ from gmm_em import GmmParamError, GMM, EM
 from online_em import OnGMM as _OnGMM
 
 __all__ = filter(lambda s:not s.startswith('_'), dir())
-
-from numpy.testing import NumpyTest
-test = NumpyTest().test
-
-def test_suite(*args):
-    # XXX: this is to avoid recursive call to itself. This is an horrible hack,
-    # I have no idea why infinite recursion happens otherwise.
-    if len(args) > 0:
-        import unittest
-        return unittest.TestSuite()
-    np = NumpyTest()
-    np.testfile_patterns.append(r'test_examples.py')
-    return np.test(level = -10, verbosity = 5)
