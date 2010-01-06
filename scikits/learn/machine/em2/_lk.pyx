8a235ac6c6e2920cd82bb53ba538671819a8494d
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-01-06 10:48:19 +0000

    Add (buggy) cython implementation for one comp likelihood function.
    From: cdavid <cdavid@cb17146a-f446-4be1-a4f7-bd7c5bb65646>
    
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@298 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

--- scikits/learn/machine/em2/_lk.pyx
+++ scikits/learn/machine/em2/_lk.pyx
@@ -6,8 +6,56 @@ cdef extern from "math.h":
     double exp(double)
     double log(double)
 
-def quadform(c_np.ndarray x, c_np.ndarray mu, c_np.ndarray inva, 
-             c_np.ndarray fac, c_np.ndarray y):
+# XXX: find out why this one does not work for d > 1
+#def quadform(c_np.ndarray x, c_np.ndarray mu, c_np.ndarray inva,
+#             double fac, c_np.ndarray y):
+#    """Compute (x-mu)'inva(x-mu) quadratic form for one set of K mean/inva
+#    vectors of d dimension (inva is assumed to be diagonal, and inva contains
+#    the diagonal items)."""
+#    cdef double *raw_x, *raw_y, *raw_mu, *raw_inva
+#    cdef int n, d, i
+#    cdef double *cx
+#
+#    if not x.dtype == np.float64:
+#        raise ValueError("Only float64 supported for now")
+#
+#    if not x.ndim == 2:
+#        raise ValueError("Rank != 2 not supported yet")
+#    n = x.shape[0]
+#    d = x.shape[1]
+#
+#    if not inva.ndim == 1:
+#        raise ValueError("inva has rank %d (expect 1)" % inva.ndim)
+#    if not inva.shape[0] == d:
+#        raise ValueError("inva has dim %d (expect %d)" % (inva.shape[0], d))
+#    #if not fac.ndim == 1:
+#    #    raise ValueError("fac has rank %d (expect 1)" % fac.ndim)
+#    #if not fac.shape[0] == d:
+#    #    raise ValueError("fac has dim %d (expect %d)" % (fac.shape[0], d))
+#    if not mu.ndim == 1:
+#        raise ValueError("mu has rank %d (expect 1)" % mu.ndim)
+#    if not mu.shape[0] == d:
+#        raise ValueError("mu has dim %d (expect %d)" % (mu.shape[0], d))
+#
+#    raw_x = <double*>x.data
+#    raw_y = <double*>y.data
+#    raw_mu = <double*>mu.data
+#    raw_inva = <double*>inva.data
+#
+#    for i in range(n):
+#        cx = raw_x + d * i
+#        acc = 0
+#        for j in range(d):
+#            acc += raw_inva[j] * (cx[j] - raw_mu[j]) * (cx[j] - raw_mu[j])
+#        y[i] = acc + fac
+#
+#    return y
+
+def mquadform(c_np.ndarray x, c_np.ndarray mu, c_np.ndarray inva,
+              c_np.ndarray fac, c_np.ndarray y):
+    """Compute (x-mu)'inva(x-mu) quadratic form for a set of K mean/inva
+    vectors of d dimension (inva is assumed to be diagonal, and inva contains
+    the diagonal items)."""
     cdef double *raw_x, *raw_y, *raw_mu, *raw_inva, *raw_fac
     cdef int n, k, d
 
