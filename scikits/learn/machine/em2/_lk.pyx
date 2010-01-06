f58c20ab0094b77462ecc0b893daf98dc0cd6b67
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-01-06 10:25:36 +0000

    Implement logsumexp in cython.
    From: cdavid <cdavid@cb17146a-f446-4be1-a4f7-bd7c5bb65646>
    
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@281 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

--- scikits/learn/machine/em2/_lk.pyx
+++ scikits/learn/machine/em2/_lk.pyx
@@ -2,6 +2,10 @@ import numpy as np
 cimport numpy as c_np
 cimport stdlib
 
+cdef extern from "math.h":
+    double exp(double)
+    double log(double)
+
 def quadform(c_np.ndarray x, c_np.ndarray mu, c_np.ndarray inva, 
              c_np.ndarray fac, c_np.ndarray y):
     cdef double *raw_x, *raw_y, *raw_mu, *raw_inva, *raw_fac
@@ -25,7 +29,6 @@ def quadform(c_np.ndarray x, c_np.ndarray mu, c_np.ndarray inva,
     quadform_double(raw_x, n, d, raw_mu, raw_inva, raw_fac, k, raw_y)
     return y
 
-# y values are assumed to be set to 0
 cdef int quadform_double(double* x, int n, int d, double *mu, double* inva, 
                          double *fac, int k, double*y):
     cdef int i, j, c
@@ -41,5 +44,51 @@ cdef int quadform_double(double* x, int n, int d, double *mu, double* inva,
             for j in range(d):
                 acc += cinva[j] * (cx[j] - cmu[j]) * (cx[j] - cmu[j])
             cy[c] = acc + fac[c]
+
+    return 0
+
+def logsumexp(c_np.ndarray x, c_np.ndarray out):
+    cdef double *raw_x, *raw_out
+    cdef int n, d, no
+
+    if not x.ndim == 2:
+        raise ValueError("Rank != 2 not supported yet")
+
+    if not x.dtype == np.float64:
+        raise ValueError("Only float64 supported for now")
+
+    n = x.shape[0]
+    no = out.shape[0]
+    d = x.shape[1]
+
+    if not out.ndim == 1:
+        raise ValueError("out should be a rank 1 array")
+    if not no == n:
+        raise ValueError(
+            "out should have as many items as x's rows " \
+            "(got %d, expected %d)" % (no, n))
+
+    raw_x = <double*>x.data
+    raw_out = <double*>out.data
+
+    logsumexp_double(raw_x, n, d, raw_out)
+
+cdef int logsumexp_double(double* x, int n, int d, double *out):
+    cdef int i, j
+    cdef double m, acc
+    cdef double *cx, *cout
+
+    for i in range(n):
+        cx = x + i * d
+        # For each row of x, compute m + log(e^{x[0]-m} + ... e^{x[d-1]-m})
+        # where m is the maximum of x[0] ... x[d-1]
+        m = cx[0]
+        for j in range(1, d):
+            if cx[j] > m:
+                m = cx[j]
+        acc = 0
+        for j in range(d):
+            acc += exp(cx[j] - m)
+        out[i] = m + log(acc)
 
     return 0
