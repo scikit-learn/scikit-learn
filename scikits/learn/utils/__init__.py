18bd1e02da0455dde30215d53bebb516de4f0b60
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-01-06 10:55:27 +0000

    Fix utils subpackage __init__; used old numpy testing framework.
    From: cdavid <cdavid@cb17146a-f446-4be1-a4f7-bd7c5bb65646>
    
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@303 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

--- scikits/learn/utils/__init__.py
+++ scikits/learn/utils/__init__.py
@@ -1,15 +1,9 @@
 #! /usr/bin/env python
-# Last Change: Sat Jul 21 02:00 PM 2007 J
+# Last Change: Fri Jan 23 08:00 PM 2009 J
 from preprocessing import scale, nanscale, Scaler, NanScaler
 
-from numpy.testing import NumpyTest
 __all__ = ['scale', 'nanscale', 'Scaler', 'NanScaler']
-test = NumpyTest().test
 
-def test_suite(*args):
-    # XXX: this is to avoid recursive call to itself. This is an horrible hack,
-    # I have no idea why infinite recursion happens otherwise.
-    if len(args) > 0:
-        import unittest
-        return unittest.TestSuite()
-    return NumpyTest().test(level = -10)
+from numpy.testing import Tester
+test = Tester().test
+bench = Tester().bench
