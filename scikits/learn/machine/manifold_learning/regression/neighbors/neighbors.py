fad33963ff3bea8f669f8b97471b2fd8e3165556
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-01-18 15:07:49 +0000

    Neighbors refactoring, predict method added.
    Some unused arguments were erased, Neighbors.kneighbors returns now
    separate arrays for distance and point, which makes some computations
    much easier, since typically you are interested only in one these
    vectors (see for example Neighbors.predict).
    
    Also renamed neighbors -> k, since neighbors is too similar to
    function kneighbors. Plus, k is the default parameter also in other
    packages (PyMVPA).
    
    Some optimizations could still be made, both technical, like creating
    self.dist and self.ind as fixed-sized arrays (changing a bit of the
    C++ might be needed for this) and algorithmical, for example using
    kd-trees[1].
    
    None of those are high-priority issues and probably won't be looked at
    before 0.2.
    
    [1] http://en.wikipedia.org/wiki/Kd-tree
    
    From: Fabian Pedregosa <fabian.pedregosa@inria.fr>
    
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@349 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

--- scikits/learn/machine/manifold_learning/regression/neighbors/neighbors.py
+++ scikits/learn/machine/manifold_learning/regression/neighbors/neighbors.py
@@ -6,8 +6,9 @@ k-Nearest Neighbor Algorithm
 # Last Change : 2008-04-15 10:55
 
 from numpy.ctypeslib import ndpointer, load_library
+from scipy.stats import mode
 import math
-import numpy
+import numpy as np
 import sys
 import ctypes
 
@@ -35,24 +36,29 @@ class Neighbors:
   Core algorithm is written in C, so this class is a wrapper with
   ctypes around the neighbors tree.
   """
-  def __init__(self, points, neighbors = 0, window_size = 1., **kwargs):
+  def __init__(self, points, labels, k = 1, window_size = 1.):
     """
-    Creates the tree, with a number of level depending on the log of the number of elements and the number of coordinates
+    Creates the tree, with a number of level depending on the log of
+    the number of elements and the number of coordinates
+    
     Parameters :
       - points is the matrix with the points populating the space
-      - neighbors is the default number of neighbors
+      - labels is an array representing labels for the data (only
+        arrays of integers are supported)
+      - k is the default number of neighbors
       - window_size is the default window size
     """
-    self.neighbors = neighbors
+    self._k = k
     self.window_size = window_size
-    self.points = numpy.ascontiguousarray(points) # needed for saving the state
+    self.points = np.ascontiguousarray(points) # needed for saving the state
+    self.labels = np.array(labels)
     self._neigh = _neighbors.allocate_neighborer(self.points.ctypes.data_as(ctypes.POINTER(ctypes.c_double)), self.points.shape[0], self.points.shape[1], int(math.log(self.points.shape[0]) / (self.points.shape[1] * math.log(2))))
 
   def __getinitargs__(self):
     """
     Returns the state of the neighboorhood
     """
-    return (self.points, self.neighbors, self.window_size)
+    return (self.points, self._k, self.window_size)
 
   def __setstate__(self, state):
     pass
@@ -68,18 +74,19 @@ class Neighbors:
       close_func(self._neigh)
       self._neigh = 0
 
-  def kneighbors(self, point, *args, **kwargs):
+  def kneighbors(self, point, k=None):
     """
     Finds the K-neighbors of a point
 
-    Parameters :
+    Parameters
+    ----------
       - point is a new point
-      - neighbors is the number of neighbors to get (default is the
-        value passed to the constructor)
+      - k is the number of neighbors to get (default is the value
+        passed to the constructor)
 
-    Outputs a list of tuples in which each tuple has two components,
-    the first one indicates the length to point, whereas the second
-    one is the index of that point in the population matrix.
+    Outputs tow lists, the first one indicates the length to point,
+    whereas the second one is the index of that point in the
+    population matrix.
 
     In the following example, we construnct a Neighbors class from an
     array representing our data set and ask who's the closest point to
@@ -87,43 +94,35 @@ class Neighbors:
 
     >>> import numpy as np
     >>> samples = np.array([[0., 0., 0.], [0., .5, 0.], [1., 1., .5]])
-    >>> n = Neighbors(samples)
-    >>> print n.kneighbors(np.array([1., 1., 1.]), 1)
-    [(0.5, 2L)]
+    >>> neig = Neighbors(samples, labels=[0,0,1], k=2)
+    >>> print neig.kneighbors([1., 1., 1.])
+    (array([ 0.5]), array([2], dtype=int64))
 
-    As you can see, it returns [(0.5, 2L)], which means that the
+    As you can see, it returns [0.5], and [2], which means that the
     element is at distance 0.5 and is the third element of samples
     (indexes start at 0)
     """
-    if len(args) > 0:
-      neighbors = args[0]
-    elif 'neighbors' in kwargs:
-      neighbors = kwargs['neighbors']
-    else:
-      neighbors = self.neighbors
+    if k is None: k = self._k
+    point = np.asarray(point)
 
-    self.results = []
-    _neighbors.find_kneighbors(self._neigh, point.ctypes.data_as(ctypes.POINTER(ctypes.c_double)), len(point), neighbors, CALLBACK(self.callback))
-    return self.results
+    self.dist, self.ind = [], []
+    _neighbors.find_kneighbors(self._neigh, point.ctypes.data_as(ctypes.POINTER(ctypes.c_double)), len(point), k, CALLBACK(self.callback))
+    return self.dist, self.ind
 
 
-  def parzen(self, point, *args, **kwargs):
+  def parzen(self, point, window_size=None):
     """
     Finds the neighbors of a point in a Parzen window
     Parameters :
       - point is a new point
       - window_size is the size of the window (default is the value passed to the constructor)
     """
-    if len(args) > 0:
-      window_size = args[0]
-    elif 'window_size' in kwargs:
-      window_size = kwargs['window_size']
-    else:
-      window_size = self.window_size
+    if window_size is None: window_size = self.window_size
 
-    self.results = []
+    self.dist, self.ind = [], []
     _neighbors.find_parzen(self._neigh, point.ctypes.data_as(ctypes.POINTER(ctypes.c_double)), len(point), window_size, CALLBACK(self.callback))
-    return self.results
+    return self.dist, self.ind
+
 
   def callback(self, distance, indice):
     """
@@ -133,8 +132,27 @@ class Neighbors:
       - distance is the distance of the point to another point
       - indice is the indice of the other point
     """
-    self.results.append((distance, indice))
-    return len(self.results)
+    self.dist.append(distance)
+    self.ind.append(indice)
+    return len(self.dist)
+
+
+  def predict(self, point):
+    """
+    Predict the class labels for the provided data.
+
+    Returns a list of class labels (one for each data sample).
+
+    >>> import numpy as np
+    >>> labels = np.array([0,0,1])
+    >>> samples = np.array([[0., 0., 0.], [0., .5, 0.], [1., 1., .5]])
+    >>> neig = Neighbors(samples)
+    >>> neig.predict([.2, .1, .2])
+    [0]
+    """
+    point = np.asarray(point)
+    dist, ind = self.kneighbors(point)
+    return mode(self.labels[ind])[0]
 
 class Kneighbors(Neighbors):
   """
