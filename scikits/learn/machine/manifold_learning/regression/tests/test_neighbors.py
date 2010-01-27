8ab0e8ff9d8e48bc7731978d4a299d82f5ffb154
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-01-27 14:46:57 +0000

    Refactor test_neighbors.
    Just some name updates.
    
    From: Fabian Pedregosa <fabian.pedregosa@inria.fr>
    
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@382 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

--- scikits/learn/machine/manifold_learning/regression/tests/test_neighbors.py
+++ scikits/learn/machine/manifold_learning/regression/tests/test_neighbors.py
@@ -12,19 +12,18 @@ def test_neighbors_1D():
     n_2 = n/2
     samples = [[x] for x in range(0, n)]
     labels  = [0]*n_2 + [1]*n_2
-    zeros   = np.zeros(n_2)
-    ones    = np.ones(n_2)
 
     # k = 1
-    nn = Neighbors(samples, labels=labels, k=1)
-    assert_array_equal( nn.predict([ [i +0.01] for i in range(0,n_2)]),
-                        zeros)
-    assert_array_equal( nn.predict([ [i-0.01] for i in range(n_2, 10)]),
-                        ones)
+    knn = Neighbors(samples, labels=labels, k=1)
+    assert_array_equal( knn.predict([ [i +0.01] for i in range(0,n_2)]),
+                        np.zeros(n_2))
+    assert_array_equal( knn.predict([ [i-0.01] for i in range(n_2, 10)]),
+                        np.ones(n_2))
 
     # k = 3
-    nn = Neighbors(samples, labels=labels, k=3)
-    assert_array_equal( nn.predict([ [i +0.01] for i in range(0,5)]),
-                        zeros)
-    assert_array_equal( nn.predict([ [i-0.01] for i in range(5, 10)]),
-                        ones)
+    knn = Neighbors(samples, labels=labels, k=3)
+    assert_array_equal( knn.predict([ [i +0.01] for i in range(0,5)]),
+                        np.zeros(n_2))
+    assert_array_equal( knn.predict([ [i-0.01] for i in range(5, 10)]),
+                        np.ones(n_2))    
+    
