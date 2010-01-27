f209999f048e08be75f86ed6116c963251044950
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-01-27 14:45:10 +0000

    Deleted common/myfunctools.py.
    No need for function partial, since we are using ptython2.5.
    
    From: Fabian Pedregosa <fabian.pedregosa@inria.fr>
    
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@381 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

--- scikits/learn/common/__init__.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-# Last Change: Mon Aug 20 02:00 PM 2007 J
-
-try:
-    from functools import partial
-    print "Using built-in partial"
-except ImportError:
-    from myfunctools import partial
