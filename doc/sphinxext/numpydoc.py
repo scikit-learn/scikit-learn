04595f7bcdfeb55d3135c9cff128e8fedd509a6d
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-02-03 10:45:55 +0000

    Fix typo in numpydoc (sphinx extension).
    From: Fabian Pedregosa <fabian.pedregosa@inria.fr>
    
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@400 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

--- doc/sphinxext/numpydoc.py
+++ doc/sphinxext/numpydoc.py
@@ -121,7 +121,7 @@ def setup(app, get_doc_object_=get_doc_object):
 #------------------------------------------------------------------------------
 # Input-mangling directives
 #------------------------------------------------------------------------------
-from docutils.stateimport ViewList
+from docutils.statemachine import ViewList
 
 def get_directive(name):
     from docutils.parsers.rst import directives
@@ -150,7 +150,7 @@ def wrap_mangling_directive(base_directive_name, objtype):
                 return base_func(self.name, self.arguments, self.options,
                                  self.content, self.lineno,
                                  self.content_offset, self.block_text,
-                                 self.state, self.state_
+                                 self.state, self.state_machine)
 
     class directive(base_directive):
         def run(self):
