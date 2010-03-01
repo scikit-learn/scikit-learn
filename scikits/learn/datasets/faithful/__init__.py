9e1a5b725447cde393775aa6e5bf46d14dafd68e
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-03-01 14:30:17 +0000

    New faithful dataset.
    The previous faithful dataset (oldfaithful) had some non-numerical
    values, which is cumbersome. This dataset does not have this limi-
    tation and is the same that ships with the R package.
    
    From: Fabian Pedregosa <fabian.pedregosa@inria.fr>
    
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@411 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

---
+++ scikits/learn/datasets/faithful/__init__.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+# Last Change: Fri Jun 08 12:00 PM 2007 J
+import data as _faith
+__doc__     = _faith.DESCRSHORT
+copyright   = _faith.COPYRIGHT
+source      = _faith.SOURCE
+
+load        = _faith.load
+load_data   = _faith.load_data
