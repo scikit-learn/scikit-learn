c2713a31955600e73188f1b160396c35bb1a847d
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-03-01 14:29:55 +0000

    Do not compile modules that depend on boost by default.
    To enable them, edit site.cfg and put use_boost=1 under [scikit-learn]
    
    From: Fabian Pedregosa <fabian.pedregosa@inria.fr>
    
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@410 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

--- scikits/learn/manifold/regression/cluster/setup.py
+++ scikits/learn/manifold/regression/cluster/setup.py
@@ -1,15 +1,22 @@
 import numpy
+from ConfigParser import ConfigParser
 
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
+    from numpy.distutils.system_info import get_standard_file
+
     config = Configuration('cluster',parent_package,top_path)
 
-    include_dirs=['../../src', '.', numpy.get_include()]
+    site_cfg = ConfigParser()
+    site_cfg.read(get_standard_file('site.cfg'))
+    if site_cfg.has_section('scikit-learn') and site_cfg.getboolean('scikit-learn', 'use_boost'):
+        # build this extension if enabled in site.cfg
+        include_dirs=['../../src', '.', numpy.get_include()]
+        config.add_extension('_modified_general_clustering',
+                             sources=['ModifiedGeneralClustering.i'],
+                             include_dirs=include_dirs
+                             )
 
-    config.add_extension('_modified_general_clustering',
-                         sources=["ModifiedGeneralClustering.i"],
-                         include_dirs=include_dirs
-                         )
     return config
 
 if __name__ == '__main__':
