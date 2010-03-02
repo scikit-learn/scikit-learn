80c58df61c1e3bd306f7408869f8beddb01defa8
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-03-02 15:42:08 +0000

    Add support for latex equations in sphinx.
    From: Fabian Pedregosa <fabian.pedregosa@inria.fr>
    
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@422 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ sys.path.insert(0, os.path.abspath('sphinxext'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'numpydoc']
+extensions = ['sphinx.ext.autodoc', 'numpydoc', 'sphinx.ext.pngmath']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
