0dc79e2c5fecbee0e105c799f28ef9702a974247
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-03-03 11:39:15 +0000

    Clean bayes code.
    From: Fabian Pedregosa <fabian.pedregosa@inria.fr>
    
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@435 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

--- scikits/learn/bayes/bayes.py
+++ scikits/learn/bayes/bayes.py
@@ -6,30 +6,41 @@ def bayesian_ridge( X , Y, step_th=300,th_w = 1.e-6, verbose = True) :
     Bayesian ridge regression. Optimize the regularization parameter alpha
     within a simple bayesian framework (MAP).
 
-
-    Parameters
-    ----------
-    X : numpy array of shape (length,features)
-	data
-    Y : numpy array of shape (length)
-	target
-    step_th : int (defaut is 300)
-	      Stop the algorithm after a given number of steps.
-    th_w : float (defaut is 1.e-6)
-	   Stop the algorithm if w has converged.
-    verbose  : boolean (default is True).
-	       Set the output on the console.
-
-    Returns
-    -------
-    w : numpy array of shape (dim)
-         mean of the weights distribution.
-   
-
-
     Notes
     -----
     See Bishop p 345-348 for more details.
+
+    Parameters
+    ----------
+    phi : numpy array of shape (length,dim)
+          functionnal images (gram matrix)
+    y : numpy array of shape (length)
+        target.
+    prune_th : number
+           Defaut is 1.e+12. If not None, we remove the alpha by
+           removing the ones supra-thresholded.
+    mu_th : number
+        threshold on the delta of the weights to stop the convergence.
+        Defaut is 1.e-4.
+    lambd_th : number
+           threshold on the lambda, to avoid divergence. Set the lambda
+           to lambda_th (default is 1.e+12) is lambda > lambda_th.
+    step_th : number.
+          Stop the algorithm if the number of step is > step_th.
+    mode : string
+           mode of computing for alpha : direct differenciation
+           "DirectDiff" (defaut)(see Bishop p347), or
+           expectation-maximization "EM" (see Bishop p450).
+           "DirectDiff" is normally faster.
+    verbose  : boolean.
+           Set the output on the console (default is True).
+
+    Returns
+    -------
+    mu : numpy array of shape (dim)
+         mean of the weights distribution.
+    log_evidence : number
+               the log evidence of p(y|0,Sigma)    
     """
 
     beta = 1./np.var(Y)
@@ -40,10 +51,9 @@ def bayesian_ridge( X , Y, step_th=300,th_w = 1.e-6, verbose = True) :
     ones = np.eye(gram.shape[1])
     sigma = scipy.linalg.pinv(alpha*ones + beta*gram)
     w = np.dot(beta*sigma,np.dot(X.T,Y))
+    old_w = w
     while not has_converged and step_th:
 
-	w_ = np.copy(w)
-       
 	### Update Parameters
 	# alpha
         lmbd_ = np.real(scipy.linalg.eigvals(beta * gram.T))
@@ -60,8 +70,8 @@ def bayesian_ridge( X , Y, step_th=300,th_w = 1.e-6, verbose = True) :
         step_th -= 1
 
 	# convergence : compare w
-	if np.sum(np.abs(w-w_))<th_w :
-	    break
+	has_converged =  (np.sum(np.abs(w-old_w))<th_w)
+        old_w = w
 
     return w
 
--- scikits/learn/bayes/tests/test_bayes.py
+++ scikits/learn/bayes/tests/test_bayes.py
@@ -1,10 +1,10 @@
 import numpy as np
-from scikits.learn.bayes.bayes import bayesian_regression
-from numpy.testing import assert_array_equal
+from scikits.learn.bayes.bayes import bayesian_ridge
+from numpy.testing import assert_array_almost_equal
 
 X = np.array([[1], [2]])
 Y = np.array([1, 2])
 
 def test_toy():
-    w = bayesian_regression(X, Y)
-    assert_array_equal(w, [1])
+    w = bayesian_ridge(X, Y)
+    assert_array_almost_equal(w, [1])
