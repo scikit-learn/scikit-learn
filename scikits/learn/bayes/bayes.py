abe63519856802398d8312354c23348986a2b4ba
Michel Vincent <vm.michel@gmail.com>
2010-03-03 11:30:20 +0000

    Doc and corrections for ridge_regression
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@433 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

--- scikits/learn/bayes/bayes.py
+++ scikits/learn/bayes/bayes.py
@@ -6,41 +6,30 @@ def bayesian_ridge( X , Y, step_th=300,th_w = 1.e-6, verbose = True) :
     Bayesian ridge regression. Optimize the regularization parameter alpha
     within a simple bayesian framework (MAP).
 
-    Notes
-    -----
-    See Bishop p 345-348 for more details.
 
     Parameters
     ----------
-    phi : numpy array of shape (length,dim)
-          functionnal images (gram matrix)
-    y : numpy array of shape (length)
-        target.
-    prune_th : number
-           Defaut is 1.e+12. If not None, we remove the alpha by
-           removing the ones supra-thresholded.
-    mu_th : number
-        threshold on the delta of the weights to stop the convergence.
-        Defaut is 1.e-4.
-    lambd_th : number
-           threshold on the lambda, to avoid divergence. Set the lambda
-           to lambda_th (default is 1.e+12) is lambda > lambda_th.
-    step_th : number.
-          Stop the algorithm if the number of step is > step_th.
-    mode : string
-           mode of computing for alpha : direct differenciation
-           "DirectDiff" (defaut)(see Bishop p347), or
-           expectation-maximization "EM" (see Bishop p450).
-           "DirectDiff" is normally faster.
-    verbose  : boolean.
-           Set the output on the console (default is True).
+    X : numpy array of shape (length,features)
+	data
+    Y : numpy array of shape (length)
+	target
+    step_th : int (defaut is 300)
+	      Stop the algorithm after a given number of steps.
+    th_w : float (defaut is 1.e-6)
+	   Stop the algorithm if w has converged.
+    verbose  : boolean (default is True).
+	       Set the output on the console.
 
     Returns
     -------
-    mu : numpy array of shape (dim)
+    w : numpy array of shape (dim)
          mean of the weights distribution.
-    log_evidence : number
-               the log evidence of p(y|0,Sigma)    
+   
+
+
+    Notes
+    -----
+    See Bishop p 345-348 for more details.
     """
 
     beta = 1./np.var(Y)
@@ -64,13 +53,10 @@ def bayesian_ridge( X , Y, step_th=300,th_w = 1.e-6, verbose = True) :
         # beta
         residual_ = (Y - np.dot(X, w))**2
         beta = (X.shape[0]-gamma_) / residual_.sum()
-	if np.isinf(beta)b :
-	    break
 
         ### Compute mu and sigma
 	sigma = scipy.linalg.pinv(alpha*ones + beta*gram)
 	w = np.dot(beta*sigma,np.dot(X.T,Y))
-	print w,alpha,beta
         step_th -= 1
 
 	# convergence : compare w
