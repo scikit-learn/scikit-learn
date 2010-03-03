8e2649a0b360ccf40fb599b9288bd75ffd0e5b0d
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2010-03-03 14:21:06 +0000

    adding test for ElasticNet
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@450 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

--- scikits/learn/glm/tests/test_cd.py
+++ scikits/learn/glm/tests/test_cd.py
@@ -12,6 +12,10 @@ from ..cd import lasso_coordinate_descent_slow
 from ..cd import lasso_coordinate_descent_fast
 from ..cd import Lasso
 
+from ..cd import enet_coordinate_descent_slow
+from ..cd import enet_coordinate_descent_fast
+from ..cd import ElasticNet
+
 
 def test_lasso_cd_python_cython_sanity():
     n_samples, n_features, maxit = 100, 100, 40
@@ -28,6 +32,29 @@ def test_lasso_cd_python_cython_sanity():
 
     model_fast = Lasso(alpha=1)
     model_fast.learner = lasso_coordinate_descent_fast
+    model_fast.fit(X, y, maxit=maxit)
+
+    # check t convergence using the KKT condition
+    assert_array_almost_equal(model_slow.gap, 0, 1e-6)
+
+    assert_array_almost_equal(model_slow.w, model_fast.w)
+    assert_array_almost_equal(model_slow.E, model_fast.E)
+
+def test_enet_cd_python_cython_sanity():
+    n_samples, n_features, maxit = 100, 100, 40
+    np.random.seed(0)
+    y = np.random.randn(n_samples)
+    X = np.random.randn(n_samples, n_features)
+
+    model_slow = ElasticNet(alpha=1, beta=1)
+    model_slow.learner = enet_coordinate_descent_slow
+    model_slow.fit(X, y, maxit=maxit)
+
+    # check the convergence using the KKT condition
+    assert_array_almost_equal(model_slow.gap, 0, 1e-6)
+
+    model_fast = ElasticNet(alpha=1, beta=1)
+    model_fast.learner = enet_coordinate_descent_fast
     model_fast.fit(X, y, maxit=maxit)
 
     # check t convergence using the KKT condition
