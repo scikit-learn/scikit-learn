486dc51757ad504106341613384c21a73566600e
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-03-12 15:02:21 +0000

    Move remaining code in bayes to linreg.
    From: Fabian Pedregosa <fabian.pedregosa@inria.fr>
    
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@523 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

--- scikits/learn/linreg/regression.py
+++ scikits/learn/linreg/regression.py
@@ -115,6 +115,34 @@ class BayesianRidge:
     def predict(self, T):
         return np.dot(T, self.w)
 
+from scikits.learn.utils.utils import fast_logdet
+
+
+
+class ARDRegression:
+    """
+    Encapsulate various bayesian regression algorithms
+    """
+
+    def __init__(self, ll_bool=False, step_th=300, th_w=1.e-12,\
+        alpha_th=1.e+16):
+        self.ll_bool = ll_bool
+        self.step_th = step_th
+        self.th_w = th_w
+        self.alpha_th = alpha_th
+
+    def fit(self, X, Y):
+        X = np.asanyarray(X, dtype=np.float)
+        Y = np.asanyarray(Y, dtype=np.float)
+        self.w ,self.alpha ,self.beta ,self.sigma ,self.log_likelihood = \
+            bayesian_regression_ard(X, Y, self.step_th, self.th_w,\
+            self.alpha_th, self.ll_bool)
+        return self
+
+    def predict(self, T):
+        return np.dot(T, self.w)
+
+
 
 ### helper methods
 ### we should homogeneize this
@@ -203,5 +231,103 @@ def bayesian_regression_ridge( X , Y, step_th=300, th_w = 1.e-12, ll_bool=False)
         ll -= fast_logdet(alpha*ones + beta*gram)
         ll -= X.shape[0]*np.log(2*np.pi)
         log_likelihood.append(ll)
+
+    return w, alpha, beta, sigma, log_likelihood
+
+
+
+def bayesian_regression_ard(X, Y, step_th=300, th_w=1.e-12, \
+                alpha_th=1.e+16, ll_bool=False):
+    """
+    Bayesian ard-based regression. Optimize the regularization parameters alpha
+    (vector of precisions of the weights) and beta (precision of the noise).
+
+
+    Parameters
+    ----------
+    X : numpy array of shape (length,features)
+    data
+    Y : numpy array of shape (length)
+    target
+    step_th : int (defaut is 300)
+          Stop the algorithm after a given number of steps.
+    th_w : float (defaut is 1.e-12)
+       Stop the algorithm if w has converged.
+    alpha_th : number
+           threshold on the alpha, to avoid divergence. Remove those features
+       from the weights computation if is alpha > alpha_th  (default is
+        1.e+16).
+    ll_bool  : boolean (default is False).
+           If True, compute the log-likelihood at each step of the model.
+
+    Returns
+    -------
+    w : numpy array of shape (nb_features)
+         mean of the weights distribution.
+    alpha : numpy array of shape (nb_features)
+       precision of the weights.
+    beta : float
+       precision of the noise.
+    sigma : numpy array of shape (nb_features,nb_features)
+        variance-covariance matrix of the weights
+    log_likelihood : list of float of size steps.
+             Compute (if asked) the log-likelihood of the model.
+
+    Examples
+    --------
+
+    Notes
+    -----
+    See Bishop chapter 7.2. for more details.
+    This should be resived. It is not efficient and I wonder if we
+    can't use libsvm for this.
+    """
+    gram = np.dot(X.T, X)
+    beta = 1./np.var(Y)
+    alpha = np.ones(gram.shape[1])
+
+
+    log_likelihood = None
+    if ll_bool :
+        log_likelihood = []
+    has_converged = False
+    ones = np.eye(gram.shape[1])
+    sigma = scipy.linalg.pinv(alpha*ones + beta*gram)
+    w = np.dot(beta*sigma,np.dot(X.T,Y))
+    old_w = np.copy(w)
+    keep_a  = np.ones(X.shape[1],dtype=bool)
+    while not has_converged and step_th:
+
+        # alpha
+        gamma_ = 1 - alpha[keep_a]*np.diag(sigma)
+        alpha[keep_a] = gamma_/w[keep_a]**2
+
+        # beta
+        residual_ = (Y - np.dot(X[:,keep_a], w[keep_a]))**2
+        beta = (X.shape[0]-gamma_.sum()) / residual_.sum()
+
+        ### Avoid divergence of the values by setting a maximum values of the
+        ### alpha
+        keep_a = alpha<alpha_th
+        gram = np.dot(X.T[keep_a,:], X[:,keep_a])
+
+        ### Compute mu and sigma
+        ones = np.eye(gram.shape[1])
+        sigma = scipy.linalg.pinv(alpha[keep_a]*ones+ beta*gram)
+        w[keep_a] = np.dot(beta*sigma,np.dot(X.T[keep_a,:],Y))
+        step_th -= 1
+
+        # convergence : compare w
+        has_converged =  (np.sum(np.abs(w-old_w))<th_w)
+        old_w = w
+
+
+    ### Compute the log likelihood
+    if ll_bool :
+        A_ = np.eye(X.shape[1])/alpha
+        C_ = (1./beta)*np.eye(X.shape[0]) + np.dot(X,np.dot(A_,X.T))
+        ll = X.shape[0]*np.log(2*np.pi)+fast_logdet(C_)
+        ll += np.dot(Y.T,np.dot(scipy.linalg.pinv(C_),Y))
+        log_likelihood.append(-0.5*ll)
 
     return w, alpha, beta, sigma, log_likelihood
--- scikits/learn/linreg/tests/test_ridge.py
+++ scikits/learn/linreg/tests/test_ridge.py
@@ -8,7 +8,8 @@ from numpy.testing import assert_array_equal
 from numpy.testing import assert_array_almost_equal
 from numpy.testing import assert_raises
 
-from ..regression import LinearRegression, bayesian_regression_ridge, Ridge, BayesianRidge
+from ..regression import LinearRegression, bayesian_regression_ridge, Ridge, \
+     BayesianRidge, ARDRegression, bayesian_regression_ard
 
 
 
@@ -59,3 +60,48 @@ def test_ridge():
     X = np.random.randn(nsamples, nfeatures)
     ridge = Ridge(alpha=alpha)
     ridge.fit(X, y)
+
+
+
+def test_toy_ridge_regression():
+    """
+    Test Ridge regression classifier
+    """
+    X = np.array([[1], [2]])
+    Y = np.array([1, 2])
+    w, alpha, beta, sigma, log_likelihood = bayesian_regression_ridge(X, Y)
+    assert(np.abs(1-w)<1.e-3)
+
+
+def test_toy_ard_regression():
+    """
+    Test ARD regression classifier
+    """
+    X = np.array([[1], [2]])
+    Y = np.array([1, 2])
+    w, alpha, beta, sigma, log_likelihood = bayesian_regression_ard(X, Y)
+    assert(np.abs(1-w)<1.e-3)
+
+
+def test_toy_ridge_object():
+    """
+    Test BayesianRegression ridge classifier
+    """
+    X = np.array([[1], [2]])
+    Y = np.array([1, 2])
+    clf = BayesianRidge()
+    clf.fit(X, Y)
+    Test = [[1], [2], [3], [4]]
+    assert(np.abs(clf.predict(Test)-[1, 2, 3, 4]).sum()<1.) # identity
+
+
+def test_toy_ard_object():
+    """
+    Test BayesianRegression ARD classifier
+    """
+    X = np.array([[1], [2]])
+    Y = np.array([1, 2])
+    clf = ARDRegression()
+    clf.fit(X, Y)
+    Test = [[1], [2], [3], [4]]
+    assert(np.abs(clf.predict(Test)-[1, 2, 3, 4]).sum()<1.e-3) # identity
