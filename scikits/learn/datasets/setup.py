aadac4231db4af6f8dd5c051544aa6afac303d9c
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-03-19 13:01:51 +0000

    remove iris as python package (it is now just a data directory)
    From: Fabian Pedregosa <fabian.pedregosa@inria.fr>
    
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@585 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

--- scikits/learn/datasets/setup.py
+++ scikits/learn/datasets/setup.py
@@ -5,7 +5,6 @@ def configuration(parent_package='',top_path=None):
     config = Configuration('datasets',parent_package,top_path)
     config.add_subpackage('oldfaithful')
     config.add_subpackage('pendigits')
-    config.add_subpackage('iris')
     config.add_subpackage('heart')
     config.add_subpackage('german')
     config.add_subpackage('nwis')
