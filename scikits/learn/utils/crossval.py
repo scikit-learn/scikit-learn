8ad16aa2b3b580b0aaba40d866879e95dc8b4307
Michel Vincent <vm.michel@gmail.com>
2010-03-22 11:38:21 +0000

    Add a function for crossval : leave-one-label-out
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@594 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

--- scikits/learn/utils/crossval.py
+++ scikits/learn/utils/crossval.py
@@ -83,6 +83,45 @@ def k_fold(n, k):
         yield train_index, test_index
 
 
+def leave_one_label_out(label):
+    """
+    Leave-One-Label_Out cross validation:
+    Provides train/test indexes to split data in train test sets
+
+    Parameters
+    ----------
+    labels : list
+            List of labels
+
+    Examples
+    ----------
+    >>> import numpy as np
+    >>> from scikits.learn.utils import crossval
+    >>> X = [[1, 2], [3, 4], [5, 6], [7, 8]]
+    >>> y = [1, 2, 1, 2]
+    >>> label = [1,1,2,2]
+    >>> lol = crossval.leave_one_label_out(label)
+    >>> for train_index, test_index in lol:
+    ...    print "TRAIN:", train_index, "TEST:", test_index
+    ...    X_train, X_test, y_train, y_test = crossval.split(train_index,
+           test_index, X, y)
+    ...    print X_train, X_test, y_train, y_test
+    TRAIN: [False False  True  True] TEST: [ True  True False False]
+    [[5 6]
+    [7 8]] [[1 2]
+    [3 4]] [1 2] [1 2]
+    TRAIN: [ True  True False False] TEST: [False False  True  True]
+    [[1 2]
+    [3 4]] [[5 6]
+    [7 8]] [1 2] [1 2]
+    """
+    for i in np.unique(label):
+        test_index  = np.zeros(len(label), dtype=np.bool)
+        test_index[label==i] = True
+        train_index = np.logical_not(test_index)
+        yield train_index, test_index
+
+
 def split(train_indexes, test_indexes, *args):
     """
     For each arg return a train and test subsets defined by indexes provided
