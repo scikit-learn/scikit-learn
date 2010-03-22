574f2b670f8ddae1458bed2de7325a898a995009
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2010-03-22 11:47:56 +0000

    fix doctest in crossval
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@595 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

--- scikits/learn/utils/crossval.py
+++ scikits/learn/utils/crossval.py
@@ -90,7 +90,7 @@ def leave_one_label_out(label):
 
     Parameters
     ----------
-    labels : list
+    label : list
             List of labels
 
     Examples
@@ -103,17 +103,18 @@ def leave_one_label_out(label):
     >>> lol = crossval.leave_one_label_out(label)
     >>> for train_index, test_index in lol:
     ...    print "TRAIN:", train_index, "TEST:", test_index
-    ...    X_train, X_test, y_train, y_test = crossval.split(train_index,
+    ...    X_train, X_test, y_train, y_test = crossval.split(train_index, \
            test_index, X, y)
     ...    print X_train, X_test, y_train, y_test
     TRAIN: [False False  True  True] TEST: [ True  True False False]
     [[5 6]
-    [7 8]] [[1 2]
-    [3 4]] [1 2] [1 2]
+     [7 8]] [[1 2]
+     [3 4]] [1 2] [1 2]
     TRAIN: [ True  True False False] TEST: [False False  True  True]
     [[1 2]
-    [3 4]] [[5 6]
-    [7 8]] [1 2] [1 2]
+     [3 4]] [[5 6]
+     [7 8]] [1 2] [1 2]
+
     """
     for i in np.unique(label):
         test_index  = np.zeros(len(label), dtype=np.bool)
