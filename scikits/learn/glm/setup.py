ad7a81f0997f4db10bddbfac77487ac44b63c72c
Yaroslav Halchenko <debian@onerussian.com>
2010-04-08 03:35:47 +0000

    RF: build Cython code during build time
    git-svn-id: https://scikit-learn.svn.sourceforge.net/svnroot/scikit-learn/trunk@621 22fbfee3-77ab-4535-9bad-27d1bd3bc7d8

--- scikits/learn/glm/setup.py
+++ scikits/learn/glm/setup.py
@@ -10,10 +10,10 @@ def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('glm',parent_package,top_path)
     config.add_extension('lasso_cd_fast',
-                         sources=['src/lasso_cd_fast.c'],
+                         sources=[join('src', 'lasso_cd_fast.pyx')],
                          include_dirs=[numpy.get_include()])
     config.add_extension('enet_cd_fast',
-                         sources=['src/enet_cd_fast.c'],
+                         sources=[join('src', 'enet_cd_fast.pyx')],
                          include_dirs=[numpy.get_include()])
     return config
 
