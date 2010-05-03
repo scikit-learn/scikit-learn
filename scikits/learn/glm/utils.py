4a96ecf5e1723df0cb9a47a34e27dca3aa7c9cc3
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-05-03 16:32:48 +0200

    Refactor lasso coordinate descent code.
    The code of coordinate descent was optimized algorithmically reducing
    the number of matrix operations and eliminating callbacks. The
    resulting code is thus simpler but less flexible in theory, although
    in my oppinion this feature was not really used.
    
    In my computer this gives a 150x speed improvement on the example
    plot_lasso_coordinate_descent_path.py.
    
    Further improvements should be done, notably those that exploit
    sparsity. This is planned but not done in this commit.
    
    Other changes:
    
      * squash enet_cd_fast.pyx and lasso_cd_fast.pyx into cd_fast.pyx.
      * remove python code that duplicates functionality of cython code.
      * Refactor test for this module, as they where mostly based on the
        callback code.
      * Modify plot_lasso_coordinate_descent_path.py to compute only the
        lasso path.
      * throw a warning if dual gap > tolerance.

--- scikits/learn/glm/utils.py
+++ scikits/learn/glm/utils.py
@@ -6,50 +6,6 @@
 import numpy as np
 import scipy.linalg as linalg
 
-def enet_dual_gap(X, y, w, alpha, beta=0):
-    """Compute dual gap for Elastic-Net model to check KKT optimality conditions
-
-    Returns
-    -------
-    gap : the difference  primal_objective - dual_objective (should be positive)
-        A value less that 1e-5 means convergence in practice
-    primal_objective : the value of the objective function of the primal problem
-    dual_objective : the value of the objective function of the dual problem
-
-    """
-    Xw = np.dot(X, w)
-    A = (y - Xw)
-    if beta > 0:
-        B = - np.sqrt(beta) * w
-    XtA = np.dot(X.T, A)
-    if beta > 0:
-        XtA += np.sqrt(beta) * B
-    dual_norm_XtA = np.max(XtA)
-    if (dual_norm_XtA > alpha):
-        A *= alpha / dual_norm_XtA
-        if beta > 0:
-            B *= alpha / dual_norm_XtA
-    pobj = 0.5 * linalg.norm(y - Xw)**2 + alpha * np.abs(w).sum() \
-           + 0.5 * beta * linalg.norm(w)**2
-    dobj = - 0.5 * linalg.norm(A)**2 + np.dot(A.T, y)
-    if beta > 0:
-        dobj += - 0.5 * linalg.norm(B)**2
-    gap = pobj - dobj
-    return gap, pobj, dobj
-
-def lasso_dual_gap(X, y, w, alpha):
-    """Compute dual gap for Lasso model to check KKT optimality conditions
-
-    Returns
-    -------
-    gap : the difference  primal_objective - dual_objective (should be positive)
-        A value less that 1e-5 means convergence in practice
-    primal_objective : the value of the objective function of the primal problem
-    dual_objective : the value of the objective function of the dual problem
-
-    """
-    return enet_dual_gap(X, y, w, alpha, beta=0)
-
 def lasso_objective(X, y, w, alpha, **kwargs):
     """Compute objective for Lasso model
 
