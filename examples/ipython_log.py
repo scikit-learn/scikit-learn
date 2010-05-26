f008ebee910115f393052934969db4ed7e554ae6
Alexandre Gramfort <alexandre.gramfort@gmail.com>
2010-05-26 23:13:10 +0200

    cleaning up my mess...

--- examples/ipython_log.py
+++
@@ -1,9 +0,0 @@
-#log# Automatic Logger file. *** THIS MUST BE THE FIRST LINE ***
-#log# DO NOT CHANGE THIS LINE OR THE TWO BELOW
-#log# opts = Struct({'__allownew': True, 'logfile': 'ipython_log.py', 'pylab': 1, 'wthread': 1})
-#log# args = []
-#log# It is safe to make manual edits below here.
-#log#-----------------------------------------------------------------------
-_ip.magic("run plot_precision_recall.py")
-_ip.magic("run plot_confusion_matrix.py")
-_ip.magic("run plot_roc.py")
--- scikits/learn/glm/tests/ipython_log.py
+++
@@ -1,7 +0,0 @@
-#log# Automatic Logger file. *** THIS MUST BE THE FIRST LINE ***
-#log# DO NOT CHANGE THIS LINE OR THE TWO BELOW
-#log# opts = Struct({'__allownew': True, 'logfile': 'ipython_log.py', 'pylab': 1, 'wthread': 1})
-#log# args = []
-#log# It is safe to make manual edits below here.
-#log#-----------------------------------------------------------------------
-from glmnet.elastic_net import Lasso
