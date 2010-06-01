1386a22a3b29075dfdc216d2df170c4be9abd1fc
Yaroslav Halchenko <debian@onerussian.com>
2010-06-01 11:29:40 -0400

    extending LDA unittest to catch issue fixed with previous commit + 1 more (just in case)

--- scikits/learn/tests/test_lda.py
+++ scikits/learn/tests/test_lda.py
@@ -2,11 +2,15 @@ import numpy as np
 from scikits.learn import lda
 from numpy.testing import assert_array_equal, \
                           assert_array_almost_equal, \
-                          assert_raises
+                          assert_raises, assert_
 
 # Data is just 6 separable points in the plane
 X = np.array( [[-2,-1], [-1, -1], [-1, -2], [1,1], [1,2], [2, 1]])
 y = np.array( [1, 1, 1, 2, 2, 2])
+y3 = np.array([1, 1, 2, 2, 3, 3])
+
+# Degenerate data with 1 feature (still should be separable)
+X1 = np.array( [[-2,], [-1,], [-1,], [1,], [1,], [2,]])
 
 def test_lda():
     """
@@ -20,4 +24,13 @@ def test_lda():
     y_pred = clf.fit(X, y).predict(X)
 
     assert_array_equal(y_pred, y)
+
+    # Assure that it works with 1D data
+    y_pred1 = clf.fit(X1, y).predict(X1)
+    assert_array_equal(y_pred1, y)
+
+    # Primarily test for commit 2f34950 -- "reuse" of priors
+    y_pred3 = clf.fit(X, y3).predict(X)
+    # LDA shouldn't be able to separate those
+    assert_(np.any(y_pred3 != y3))
 
