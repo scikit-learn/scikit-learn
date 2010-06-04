d76ecb886bc893f5cd8e6bf36e105ac0874f70fb
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2010-06-04 08:13:04 +0200

    ENH : imroving documentation of lasso + enet paths function

--- examples/lasso_path_with_crossvalidation.py
+++ examples/lasso_path_with_crossvalidation.py
@@ -27,7 +27,25 @@ X_test, y_test = X[n_samples/2:], y[n_samples/2:]
 
 
 ################################################################################
-# Lasso with path and cross-validation
+# Lasso with path and cross-validation using optimized_lasso
+from scikits.learn.cross_val import KFold
+from scikits.learn.glm.coordinate_descent import optimized_lasso
+
+# Instanciate cross-validation generator
+cv = KFold(n_samples/2, 5)
+
+# Estimate optimized lasso model
+lasso_opt = optimized_lasso(X_train, y_train, cv, n_alphas=100, eps=1e-3, maxit=100)
+y_ = lasso_opt.predict(X_test)
+
+print lasso_opt
+
+# Compute explained variance on test data
+print "r^2 on test data : %f" % (1 - np.linalg.norm(y_test - y_)**2
+                                      / np.linalg.norm(y_test)**2)
+
+################################################################################
+# Lasso with path and cross-validation using LassoPath path
 from scikits.learn.glm.coordinate_descent import LassoPath
 lasso_path = LassoPath()
 
@@ -36,6 +54,6 @@ y_pred = lasso_path.fit(X_train, y_train).predict(X_test)
 print lasso_path
 
 # Compute explained variance on test data
-print "r^2 on test data : %f" % (1 - np.linalg.norm(y_test - y_pred)**2
+print "r^2 on test data : %f" % (1 - np.linalg.norm(y_test - y_)**2
                                       / np.linalg.norm(y_test)**2)
 
