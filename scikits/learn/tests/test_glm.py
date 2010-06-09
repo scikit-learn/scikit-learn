229542d34c19d6d7db47f7283a2d385d8b4ebe32
ogrisel <olivier.grisel@ensta.org>
2010-06-09 17:53:47 +0200

    LARS test clean-up

--- scikits/learn/tests/test_glm.py
+++ scikits/learn/tests/test_glm.py
@@ -7,17 +7,13 @@ X = [[1, 0, -.1],
      [0, 1, .1]]
 Y = [1, 0, -1]
 
-clf = glm.LassoLARS()
-clf.fit(X, Y)
 
 def test_1():
     """
     Very simple test
     """
-    clf = glm.LassoLARS()
-    clf.fit(X, Y)
-    assert_array_almost_equal(clf.coef_,
-                [-1.4142, -1.4142, 0], decimal=4)
+    clf = glm.LassoLARS().fit(X, Y)
+    assert_array_almost_equal(clf.coef_, [-1.4142, -1.4142, 0], decimal=4)
 
 
 
