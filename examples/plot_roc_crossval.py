cbe1a9173b3d0791dd49fee747dab26ae9be66ce
Gael Varoquaux <gael.varoquaux@normalesup.org>
2010-06-16 11:12:21 +0200

    Cosmit

--- examples/plot_roc_crossval.py
+++ examples/plot_roc_crossval.py
@@ -5,16 +5,21 @@ Receiver operating characteristic (ROC)
 
 Example of Receiver operating characteristic (ROC) metric to
 evaluate the quality of the output of a classifier using
-cross-validation
+cross-validation.
 """
 
 import random
+
 import numpy as np
 from scipy import interp
 import pylab as pl
+
 from scikits.learn import svm, datasets
 from scikits.learn.metrics import roc, auc
 from scikits.learn.cross_val import KFold
+
+################################################################################
+# Data IO and generation
 
 # import some data to play with
 iris = datasets.load_iris()
@@ -28,24 +33,24 @@ random.shuffle(p)
 X, y = X[p], y[p]
 
 # Add noisy features
-np.random.seed(0)
 X = np.c_[X,np.random.randn(n_samples, 200*n_features)]
 
+################################################################################
+# Classification and ROC analysis
+
 # Run classifier with crossvalidation and plot ROC curves
-k = 6
-cv = KFold(n_samples, k)
+cv = KFold(n_samples, 6)
 classifier = svm.SVC(kernel='linear', probability=True)
 
 pl.figure(-1)
 pl.clf()
-pl.hold('on')
 
 mean_tpr = 0.0
 mean_fpr = np.linspace(0, 1, 100)
 all_tpr = []
 
 for i, (train, test) in enumerate(cv):
-    probas_ = classifier.fit(X[train],y[train]).predict_proba(X[test])
+    probas_ = classifier.fit(X[train], y[train]).predict_proba(X[test])
     # Compute ROC curve and area the curve
     fpr, tpr, thresholds = roc(y[test], probas_[:,1])
     mean_tpr += interp(mean_fpr, fpr, tpr)
@@ -58,11 +63,11 @@ pl.plot([0, 1], [0, 1], '--', color=(0.6,0.6,0.6), label='Luck')
 mean_tpr /= len(cv)
 mean_tpr[-1] = 1.0
 mean_auc = auc(mean_fpr, mean_tpr)
-pl.plot(mean_fpr, mean_tpr, 'k--', label='Mean ROC (area = %0.2f)' % mean_auc, lw=2)
+pl.plot(mean_fpr, mean_tpr, 'k--', 
+        label='Mean ROC (area = %0.2f)' % mean_auc, lw=2)
 
 pl.xlim([-0.05,1.05])
 pl.ylim([-0.05,1.05])
-
 pl.xlabel('False Positive Rate')
 pl.ylabel('True Positive Rate')
 pl.title('Receiver operating characteristic example')
