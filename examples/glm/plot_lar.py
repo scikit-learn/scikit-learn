f84bce9a55b7c2395a277e08ad9c90d8dd615a99
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-06-21 10:34:03 +0200

    Cleanup in LAR example

--- examples/glm/plot_lar.py
+++ examples/glm/plot_lar.py
@@ -10,33 +10,21 @@ Least Angle Regression
 # License: BSD Style.
 
 from datetime import datetime
-from itertools import cycle
+import itertools
 import numpy as np
 import pylab as pl
 
 from scikits.learn import glm
-
-n_samples, n_features = 500, 10
-
-np.random.seed(0)
-Y = np.random.randn(n_samples)
-X = np.random.randn(n_samples, n_features)
-
 from scikits.learn import datasets
 
 diabetes = datasets.load_diabetes()
 X = diabetes.data
 Y = diabetes.target
 
-################################################################################
-# Fit models
-################################################################################
 
 ################################################################################
 # Demo path functions
 ################################################################################
-
-eps = 1e-2 # the smaller it is the longer is the path
 
 print "Computing regularization path using the LARS ..."
 start = datetime.now()
@@ -46,7 +34,7 @@ print "This took ", datetime.now() - start
 alphas = -np.log10(clf.alphas_)
 
 # # Display results
-color_iter = cycle(['b', 'g', 'r', 'c', 'm', 'y', 'k' , 'b', 'c', 'c'])
+color_iter = itertools.cycle (['r', 'g', 'b', 'c'])
 
 for coef_, color in zip(clf.coef_path_, color_iter):
     pl.plot(alphas, coef_.T, color)
