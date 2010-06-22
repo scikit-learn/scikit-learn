74ceb896b23f52d1f869b7aab8a716f528865527
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-06-22 16:13:08 +0200

    Remove outdated doc.

--- examples/plot_gmm.py
+++ examples/plot_gmm.py
@@ -19,7 +19,7 @@ np.random.seed(0)
 C = np.array([[0., -0.7], [3.5, .7]]) # rotation and scale matrix
 
 X = np.r_[np.dot(np.random.randn(n, 2), C),
-          np.random.randn(n, 2) + np.array([10,10])]
+          np.random.randn(n, 2) + np.array([3, 3])]
 
 clf = gmm.GMM(2, cvtype='full')
 clf.fit(X)
