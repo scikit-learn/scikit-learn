c2145fbd81f96deec9b3bf707b7b58cef16190c5
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-07-01 15:42:54 -0500

    Add modules to top-level __init__.
    Mostly for autocomplete friendlyness.

--- scikits/learn/__init__.py
+++ scikits/learn/__init__.py
@@ -1,4 +1,29 @@
 """
-Machine Learning module for python.
+Machine Learning module in python
+=================================
+
+scikits.learn is a Python module integrating classique machine
+learning algorithms in the tightly-nit world of scientific Python
+packages (numpy, scipy, matplotlib).
+
+It aims to provide simple and efficient solutions to learning problems
+that are accessible to everybody and reusable in various contexts:
+machine-learning as a versatile tool for science and engineering.
+
+See http://scikit-learn.sourceforge.net for complete documentation.
 """
+
+import cross_val
+import ball_tree
+import gmm
+import glm
+import logistic
+import lda
+import metrics
+import svm
+
+__all__ = ['cross_val', 'ball_tree', 'gmm', 'glm', 'logistic', 'lda',
+           'metrics', 'svm']
+
 __version__ = '0.5-git'
+
