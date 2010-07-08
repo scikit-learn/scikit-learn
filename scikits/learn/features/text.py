41b0562e7942a1e1b9d1fb057ad62c5b3ce7cea4
Olivier Grisel <olivier.grisel@ensta.org>
2010-07-08 09:48:24 +0200

    add the ability to use stop words for text classification, but does not improve accuracy hence not enabled by default

--- scikits/learn/features/text.py
+++ scikits/learn/features/text.py
@@ -7,6 +7,48 @@ import re
 import unicodedata
 import numpy as np
 
+ENGLISH_STOP_WORDS = set([
+    "a", "about", "above", "across", "after", "afterwards", "again", "against",
+    "all", "almost", "alone", "along", "already", "also", "although", "always",
+    "am", "among", "amongst", "amoungst", "amount", "an", "and", "another",
+    "any", "anyhow", "anyone", "anything", "anyway", "anywhere", "are",
+    "around", "as", "at", "back", "be", "became", "because", "become",
+    "becomes", "becoming", "been", "before", "beforehand", "behind", "being",
+    "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom",
+    "but", "by", "call", "can", "cannot", "cant", "co", "computer", "con",
+    "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down",
+    "due", "during", "each", "eg", "eight", "either", "eleven", "else",
+    "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone",
+    "everything", "everywhere", "except", "few", "fifteen", "fify", "fill",
+    "find", "fire", "first", "five", "for", "former", "formerly", "forty",
+    "found", "four", "from", "front", "full", "further", "get", "give", "go",
+    "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter",
+    "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his",
+    "how", "however", "hundred", "i", "ie", "if", "in", "inc", "indeed",
+    "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter",
+    "latterly", "least", "less", "ltd", "made", "many", "may", "me",
+    "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly",
+    "move", "much", "must", "my", "myself", "name", "namely", "neither", "never",
+    "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor",
+    "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once",
+    "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours",
+    "ourselves", "out", "over", "own", "part", "per", "perhaps", "please",
+    "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems",
+    "serious", "several", "she", "should", "show", "side", "since", "sincere",
+    "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime",
+    "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than",
+    "that", "the", "their", "them", "themselves", "then", "thence", "there",
+    "thereafter", "thereby", "therefore", "therein", "thereupon", "these",
+    "they", "thick", "thin", "third", "this", "those", "though", "three",
+    "through", "throughout", "thru", "thus", "to", "together", "too", "top",
+    "toward", "towards", "twelve", "twenty", "two", "un", "under", "until",
+    "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what",
+    "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas",
+    "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while",
+    "whither", "who", "whoever", "whole", "whom", "whose", "why", "will",
+    "with", "within", "without", "would", "yet", "you", "your", "yours",
+    "yourself", "yourselves"])
+
 
 def strip_accents(s):
     """Transform accentuated unicode symbols into their simple counterpart"""
@@ -26,16 +68,19 @@ class SimpleAnalyzer(object):
 
     token_pattern = re.compile(r"\b\w\w+\b", re.U)
 
-    # TODO: make it possible to pass stop words list here
-
-    def __init__(self, default_charset='utf-8'):
+    def __init__(self, default_charset='utf-8', stop_words=None):
         self.charset = default_charset
+        self.stop_words = stop_words
 
     def analyze(self, text_document):
         if isinstance(text_document, str):
             text_document = text_document.decode(self.charset, 'ignore')
         text_document = strip_accents(text_document.lower())
-        return self.token_pattern.findall(text_document)
+        tokens = self.token_pattern.findall(text_document)
+        if self.stop_words is not None:
+            return [w for w in tokens if w not in self.stop_words]
+        else:
+            return tokens
 
 
 class HashingVectorizer(object):
@@ -64,7 +109,7 @@ class HashingVectorizer(object):
     # and sparse alternatives from scipy.sparse once the liblinear and libsvm
     # wrappers have been updated to be able to handle it efficiently
 
-    def __init__(self, dim=5000, probes=3, analyzer=SimpleAnalyzer(),
+    def __init__(self, dim=5000, probes=1, analyzer=SimpleAnalyzer(),
                  use_idf=True):
         self.dim = dim
         self.probes = probes
