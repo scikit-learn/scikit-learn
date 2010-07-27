7db08f5f370792d1537c49e02f97fa828208d013
vincent <vincent@vincent.org>
2010-07-27 11:51:38 +0200

    Last version of univariate selection

--- scikits/learn/feature_selection/univariate_selection.py
+++ scikits/learn/feature_selection/univariate_selection.py
@@ -8,6 +8,132 @@ Univariate features selection.
 import numpy as np
 from scipy import stats
 
+
+######################################################################
+# General class for filter univariate selection
+######################################################################
+
+class UnivariateFilter(object):
+    """
+    General class for filter univariate selection
+    """
+
+    def __init__(self,ranking,score_func):
+        """
+        Initialize the univariate feature selection.
+        score_func : function taking two arrays X and y, and returning an array.
+                     Returns both scores and pvalues.
+        ranking : ranking function
+        """
+        assert callable(score_func), ValueError(
+                "The score function should be a callable, '%s' (type %s) "
+                "was passed." % (score_func, type(score_func))
+            )
+        self.score_func = score_func
+        self.ranking = ranking
+
+    def fit(self,X,y):
+        """
+        Evaluate the function
+        """
+        self._scores = self.score_func(X, y)
+        #self._scores = _scores[0]
+        #self._pvalues = _scores[1]
+        #self._rank = np.argsort(self._pvalues)
+        return self
+
+    def transform(self,X,**kwargs):
+        """
+        Transform a new matrix using the selected features
+        """
+        self.support = self.ranking.support(self._scores,**kwargs)
+        return X[:,self.support]
+
+
+
+
+
+######################################################################
+# Specific rankings
+######################################################################
+
+class SelectPercentile(object):
+    """
+    Filter : Select the best percentile of the p_values
+    """
+    def __init__(self,percentile):
+        self.percentile = percentile
+
+    def support(self,scores,percentile=None):
+        if percentile is not None:
+                self.percentile = percentile 
+        assert self.percentile<=100, ValueError('percentile should be \
+                            between 0 and 100 (%f given)' %(self.percentile))
+        alpha = stats.scoreatpercentile(scores[1], self.percentile)
+        return (scores[1] <= alpha)
+
+
+
+class SelectKBest(object):
+    """
+    Filter : Select the k lowest p-values
+    """
+    def __init__(self,k):
+        self.k = k
+
+    def support(self,scores,k=None):
+          if k is not None:
+                self.k=k
+          assert self.k<=len(scores[1]), ValueError('cannot select %d features'
+                                  ' among %d ' % (self.k, len(scores[1])))
+          alpha = np.sort(scores[1])[self.k-1]
+          return (scores[1] <= alpha)
+
+
+
+class SelectFpr(object):
+    """
+    Filter : Select the pvalues below alpha
+    """
+    def __init__(self,alpha):
+        self.alpha = alpha
+
+    def support(self,alpha = None):
+          if alpha is not None:
+                self.alpha=alpha
+        return (self._pvalues < self.alpha)
+
+
+class SelectFdr(object):
+    """
+    Filter : Select the p-values corresponding to an estimated false
+    discovery rate of alpha. This uses the Benjamini-Hochberg procedure
+    """
+    def __init__(self,alpha):
+        self.alpha = alpha
+
+    def support(self,alpha = None):
+          if alpha is not None:
+                self.alpha=alpha
+        sv = np.sort(self._pvalues)
+        threshold = sv[sv < self.alpha*np.arange(len(self._pvalues))].max()
+        return (self._pvalues < threshold)
+
+
+class SelectFwe(object):
+    """
+    Filter : Select the p-values corresponding to a corrected p-value of alpha
+    """
+    def __init__(self,alpha):
+        self.alpha = alpha
+
+    def support(self,alpha = None):
+          if alpha is not None:
+                self.alpha=alpha
+        return (self._pvalues < self.alpha/len(self._pvalues))
+
+
+
 ######################################################################
 # Scoring functions
 ######################################################################
@@ -82,94 +208,7 @@ def f_regression(X, y, center=True):
     return F, pv
 
 
-######################################################################
-# General class for filter univariate selection
-######################################################################
 
-
-class UnivariateFilter(object):
-    """
-    Abstract class, not meant to be used directly
-    """
-
-    def __init__(self, score_func):
-        """
-        Initialize the univariate feature selection.
-        Func : function taking two arrays X and y, and returning an array.
-        score_func returning both scores and pvalues
-        """
-        assert callable(score_func), ValueError(
-                "The score function should be a callable, '%s' (type %s) "
-                "was passed." % (score_func, type(score_func))
-            )
-        self.score_func = score_func
-
-    def fit(self,X,y):
-        """
-        Evaluate the function
-        """
-        _scores = self.score_func(X, y)
-        self._scores = _scores[0]
-        self._pvalues = _scores[1]
-        #self._rank = np.argsort(self._pvalues)
-        return self
-
-    def transform(self,X,**kwargs):
-        """
-        Transform a new matrix using the selected features
-        """
-        return X[:,self.support(**kwargs)]
-
-
-######################################################################
-# Specific filters
-######################################################################
-
-class SelectPercentile(UnivariateFilter):
-    """
-    Filter : Select the best percentile of the p_values
-    """
-    def support(self,percentile):
-        assert percentile<=100, ValueError('percentile should be \
-                            between 0 and 100 (%f given)' %(percentile))
-        alpha = stats.scoreatpercentile(self._pvalues, percentile)
-        return (self._pvalues <= alpha)
-
-class SelectKBest(UnivariateFilter):
-    """
-    Filter : Select the k lowest p-values
-    """
-    def support(self,k):
-        assert k<=len(self._pvalues), ValueError('cannot select %d features'
-                                    ' among %d ' % (k, len(self._pvalues)))
-        alpha = np.sort(self._pvalues)[k-1]
-        return (self._pvalues <= alpha)
-
-class SelectFpr(UnivariateFilter):
-    """
-    Filter : Select the pvalues below alpha
-    """
-    def support(self,alpha):
-        return (self._pvalues < alpha)
-
-
-class SelectFdr(UnivariateFilter):
-    """
-    Filter : Select the p-values corresponding to an estimated false
-    discovery rate of alpha. This uses the Benjamini-Hochberg procedure
-    """
-    def support(self,alpha):
-        sv = np.sort(self._pvalues)
-        threshold = sv[sv < alpha*np.arange(len(self._pvalues))].max()
-        return (self._pvalues < threshold)
-
-
-class SelectFwe(UnivariateFilter):
-    """
-    Filter : Select the p-values corresponding to a corrected p-value of alpha
-    """
-    def support(self,alpha):
-        return (self._pvalues < alpha/len(self._pvalues))
 
 
 
@@ -178,16 +217,21 @@ if __name__ == "__main__":
     from scikits.learn.svm import SVR, SVC
 
     X,y = sg.sparse_uncorrelated(50,100)
-    univariate_filter = SelectKBest(f_regression)
-    X_r = univariate_filter.fit(X, y).transform(X, k=5)
-    sel = univariate_filter.support(k=5)
-    clf = SVR(kernel='linear', C=1.)
-    y_ = clf.fit(X_r, y).predict(X_r)
-    print sel
+    univ_filter = UnivariateFilter(SelectKBest(k=5),f_regression)
+    X_r_5 = univ_filter.fit(X, y).transform(X)
+    X_r_10 = univ_filter.transform(X,k=10)
+    univ_filter.ranking.k = 20
+    X_r_20 = univ_filter.fit(X, y).transform(X)
+    univ_filter.ranking = SelectPercentile(percentile = 50)
+    X_r_50 = univ_filter.fit(X, y).transform(X)
 
-    ### now change k
-    X_r = univariate_filter.transform(X, k=2)
-    y_ = clf.fit(X_r, y).predict(X)
-    print sel
+    #clf = SVR(kernel='linear', C=1.)
+    #y_ = clf.fit(X_r, y).predict(X_r)
+    #print sel
+
+    #### now change k
+    #X_r = univariate_filter.transform(X, k=2)
+    #y_ = clf.fit(X_r, y).predict(X)
+    #print sel
 
 
