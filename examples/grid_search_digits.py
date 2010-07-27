c1f222e84f09fae53f4aaa5739ebb1526a7e34dd
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2010-07-27 11:58:45 +0200

    API : cross val generator in now given in fit in grid search object

--- examples/grid_search_digits.py
+++ examples/grid_search_digits.py
@@ -4,20 +4,12 @@ in a nested cross-validation setting.
 
 import numpy as np
 from scikits.learn.svm import SVC
-from scikits.learn.cross_val import StratifiedKFold, KFold
+from scikits.learn.cross_val import StratifiedKFold
 from scikits.learn import datasets
 from scikits.learn.grid_search import GridSearchCV
 
 # The Digits dataset
 digits = datasets.load_digits()
-
-# # The data that we are interesting in is made of 8x8 images of digits,
-# # let's have a look at the first 3 images. We know which digit they
-# # represent: it is given in the 'target' of the dataset.
-# for index, (image, label) in enumerate(zip(digits.images, digits.target)[:4]):
-#     pl.subplot(2, 4, index+1)
-#     pl.imshow(image, cmap=pl.cm.gray_r)
-#     pl.title('Training: %i' % label)
 
 # To apply an classifier on this data, we need to flatten the image, to
 # turn the data in a (samples, feature) matrix:
@@ -30,11 +22,8 @@ parameters = {'kernel':('linear', 'rbf'), 'C':[0.1, 1]}
 def loss_func(y1, y2):
     return np.mean(y1 != y2)
 
-def cv(n_samples):
-    return KFold(n_samples, 2)
-
 svc = SVC()
-clf = GridSearchCV(svc, parameters, cv, loss_func, n_jobs=2)
+clf = GridSearchCV(svc, parameters, loss_func, n_jobs=2)
 
 """
 Run crossvalidation (different than the nested crossvalidation that is used
@@ -42,9 +31,14 @@ to select the best classifier). The classifier is optimized by "nested"
 crossvalidation
 """
 n_samples, n_features = X.shape
-y_pred = np.zeros_like(y)
+y_pred = []
+y_true = []
 for train, test in StratifiedKFold(y, 2):
-    y_pred[test] = clf.fit(X[train], y[train]).predict(X[test]).astype(np.int)
+    cv = StratifiedKFold(y[train], 2)
+    y_pred.append(clf.fit(X[train], y[train], cv=cv).predict(X[test]))
+    y_true.append(y[test])
 
-classif_rate = np.mean(y_pred == y) * 100
+y_pred = np.concatenate(y_pred)
+y_true = np.concatenate(y_true)
+classif_rate = np.mean(y_pred == y_true) * 100
 print "Classification rate : %f" % classif_rate
