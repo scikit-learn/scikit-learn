8c7b1bf16f81f2d533be850afe49eb112a2beaa5
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2010-07-30 15:26:57 +0200

    ENH : fix in svc.coef_ + cosmit

--- examples/feature_selection_pipeline.py
+++ examples/feature_selection_pipeline.py
@@ -7,13 +7,13 @@ Simple usages of pipeline:
 - ANOVA SVM-C
 """
 
-from scikits.learn import svm, datasets
+from scikits.learn import svm
 from scikits.learn.datasets import samples_generator
 from scikits.learn.feature_selection.univariate_selection import SelectKBest,f_regression
 from scikits.learn.pipeline import Pipeline
 
 # import some data to play with
-X,y = samples_generator.test_dataset_classif(k=5)
+X, y = samples_generator.test_dataset_classif(k=5)
 
 
 # ANOVA SVM-C
@@ -22,8 +22,7 @@ anova_filter = SelectKBest(f_regression, k=5)
 # 2) svm
 clf = svm.SVC(kernel='linear')
 
-anova_svm = Pipeline([anova_filter],clf)
-anova_svm.fit(X,y)
+anova_svm = Pipeline([anova_filter], clf)
+anova_svm.fit(X, y)
 anova_svm.predict(X)
-
 
