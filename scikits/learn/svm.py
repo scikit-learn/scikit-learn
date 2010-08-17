07bc52583b9a8a732d8e67b0cf0eaf848012d5ab
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-08-17 14:08:14 +0200

    Fix names and missing parameters in LinearSVC.

--- scikits/learn/svm.py
+++ scikits/learn/svm.py
@@ -552,12 +552,14 @@ class LinearSVC(BaseEstimator, ClassifierMixin):
         'PL1_LL2_D0' : 5, # L1 penalty, L2 Loss, primal problem
         }
 
-    def __init__(self, penalty='l2', loss='l2', dual=True, eps=1e-4, C=1.0):
+    def __init__(self, penalty='l2', loss='l2', dual=True, eps=1e-4, C=1.0,
+                 bias=True):
         self.penalty = penalty
         self.loss = loss
         self.dual = dual
         self.eps = eps
         self.C = C
+        self.bias = int(bias) - .5 # negative on False, positive on True
         # Check that the arguments given are valid:
         self._get_solver_type()
 
@@ -588,7 +590,7 @@ class LinearSVC(BaseEstimator, ClassifierMixin):
         
         X = np.asanyarray(X, dtype=np.float64, order='C')
         Y = np.asanyarray(Y, dtype=np.int32, order='C')
-        self.raw_coef, self.label_, self.bias_ = \
+        self.raw_coef_, self.label_, self.bias_ = \
                        _liblinear.train_wrap(X, Y,
                        self._get_solver_type(),
                        self.eps, 1.0, self.C, self._weight_label,
@@ -597,7 +599,7 @@ class LinearSVC(BaseEstimator, ClassifierMixin):
 
     def predict(self, T):
         T = np.atleast_2d(np.asanyarray(T, dtype=np.float64, order='C'))
-        return _liblinear.predict_wrap(T, self.raw_coef, 
+        return _liblinear.predict_wrap(T, self.raw_coef_,
                                       self._get_solver_type(),
                                       self.eps, self.C,
                                       self._weight_label,
@@ -611,12 +613,12 @@ class LinearSVC(BaseEstimator, ClassifierMixin):
     @property
     def intercept_(self):
         if self.bias_ > 0:
-            return self.raw_coef[:,-1]
+            return self.raw_coef_[:,-1]
         return 0.0
 
     @property
     def coef_(self):
         if self.bias_ > 0:
-            return self.raw_coef[:,:-1]
+            return self.raw_coef_[:,:-1]
         return self.raw_coef_
 
