81c2b0abcb5cec457e30c0b0d071212304f3da1d
Gael varoquaux <gael.varoquaux@normalesup.org>
2010-08-18 21:17:53 +0200

    BUG: Make sure that joblib does get installed.

---
+++ scikits/learn/externals/setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+def configuration(parent_package='', top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration('externals',parent_package,top_path)
+    config.add_subpackage('joblib')
+    config.add_subpackage('joblib/test')
+
+    return config
+
