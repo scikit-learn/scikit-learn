97bbe311ea03477bb5fb6471824085709258b951
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2010-08-24 14:12:11 +0200

    ENH : removing unused preprocessing routines

--- scikits/learn/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env python
-# Last Change: Fri Jan 23 08:00 PM 2009 J
-from preprocessing import scale, nanscale, Scaler, NanScaler
-
-__all__ = ['scale', 'nanscale', 'Scaler', 'NanScaler']
