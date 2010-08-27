c087f64f393962818c82b06cacd66121c911dcef
Gael Varoquaux <gael.varoquaux@normalesup.org>
2010-08-27 13:47:16 +0200

    BUG: Add a forgotten setup.py line

--- scikits/learn/setup.py
+++ scikits/learn/setup.py
@@ -18,6 +18,8 @@ def configuration(parent_package='',top_path=None):
     config.add_subpackage('datasets')
     config.add_subpackage('features')
     config.add_subpackage('features/tests')
+    config.add_subpackage('cluster')
+    config.add_subpackage('cluster/tests')
     config.add_subpackage('feature_selection')
     config.add_subpackage('feature_selection/tests')
     config.add_subpackage('sparse')
