2ad861c07b97f69142535eb9b25ab6f6b29fbd74
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2010-08-31 19:26:06 +0200

    FIX : fixing __init__ files for examples

--- scikits/learn/cluster/__init__.py
+++ scikits/learn/cluster/__init__.py
@@ -3,7 +3,7 @@ Clustering algorithms
 """
 
 from .spectral import spectral_clustering, SpectralClustering
-from .mean_shift_ import mean_shift, MeanShift
+from .mean_shift_ import mean_shift, MeanShift, estimate_bandwidth
 from .affinity_propagation_ import affinity_propagation, AffinityPropagation
 from .k_means_ import k_means, KMeans
 
--- scikits/learn/glm/__init__.py
+++ scikits/learn/glm/__init__.py
@@ -11,6 +11,7 @@ and coordinate descent.
 
 from .base import LinearRegression
 from .lars import LARS, LassoLARS, lars_path, LeastAngleRegression
-from .coordinate_descent import Lasso, ElasticNet, LassoCV, ElasticNetCV
+from .coordinate_descent import Lasso, ElasticNet, LassoCV, ElasticNetCV, \
+                                lasso_path, enet_path
 from .bayes import Ridge, BayesianRidge, ARDRegression
 
