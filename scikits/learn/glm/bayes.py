ebf568f10ccf614d7292852599cf5fb92c364961
Vincent Michel <vm.michel@gmail.com>
2010-09-03 15:32:35 +0200

    Newer (and faster !) version of Bayesian regression.

--- scikits/learn/glm/bayes.py
+++ scikits/learn/glm/bayes.py
@@ -5,8 +5,6 @@ from scipy import linalg
 from .base import LinearModel
 from ..utils.extmath import fast_logdet
 
-
-
 class Ridge(LinearModel):
     """
     Ridge regression.
@@ -82,8 +80,8 @@ class Ridge(LinearModel):
 
 class BayesianRidge(LinearModel):
     """
-    Bayesian ridge regression. Optimize the regularization parameters alpha
-    (precision of the weights) and beta (precision of the noise) within a
+    Bayesian ridge regression. Optimize the regularization parameters lambda
+    (precision of the weights) and alpha (precision of the noise) within a
     simple bayesian framework (MAP).
 
     Parameters
@@ -122,13 +120,21 @@ class BayesianRidge(LinearModel):
     -----
     See Bishop p 167-169 for more details.
     """
-    def __init__(self, n_iter=300, th_w=1.e-12, compute_ll=False, 
+    def __init__(self, n_iter=300, th_w=1.e-12, compute_ll=False,
         fit_intercept=True):
         self.n_iter = n_iter
         self.th_w = th_w
         self.compute_ll = compute_ll
         self.fit_intercept = fit_intercept
 
+    def compute_log_likelihood(self,X,Y):
+        ll =  0.5 * X.shape[1] * np.log(self.lambda_)\
+            + 0.5 * X.shape[0] * np.log(self.alpha_)\
+            - 0.5 * self.alpha_ *  np.sum((Y - np.dot(X, self.coef_))**2)\
+            - self.lambda_ * np.dot(self.coef_.T,self.coef_)\
+            - 0.5 * fast_logdet(self.sigma_)\
+            - 0.5 * X.shape[0] * np.log(2*np.pi)
+        return ll
 
     def fit(self, X, Y, **params):
         """
@@ -150,43 +156,43 @@ class BayesianRidge(LinearModel):
 
         X, Y = self._center_data (X, Y)
 
+
         ### "Dummy" initialization of the values of the parameters
         self.alpha_ = 1./np.var(Y)
         self.lambda_ = 1.0
-        ### Create a list for keeping the values of the log likelihood
         self.log_likelihood_ = []
-        
+        U, S, V = linalg.svd(X, full_matrices=False)
+        self.eigen_vals_ = S**2
+        self.X_XT = np.dot(X,X.T)
+        self.XT_Y = np.dot(X.T,Y)
+
         ### Convergence loop of the bayesian ridge regression
         for iter_ in range(self.n_iter):
-            print iter_
-            ### Compute mu and sigma
-            self.sigma_ = linalg.pinv(np.dot(X.T, X) + self.lambda_ *
-                        np.eye(n_features))
-            self.coef_ = self.alpha_ *np.dot(self.sigma_,np.dot(X.T,Y))
+
+            ### Compute mu and sigma (using Woodbury matrix identity)
+            self.sigma_ =  np.dot(linalg.pinv(np.eye(n_samples)/self.alpha_ +
+                                  self.X_XT/self.lambda_), X/self.lambda_)
+            self.sigma_ = - np.dot(X.T/self.lambda_, self.sigma_)
+            self.sigma_.flat[::(self.sigma_.shape[1]+1)] += 1./self.lambda_
+            self.coef_ = self.alpha_ *np.dot(self.sigma_,self.XT_Y)
 
             ### Update alpha and lambda
-            self.lambda_ = (n_features - self.lambda_*np.trace(self.sigma_))\
-                  /np.dot(self.coef_.T,self.coef_)
-            self.alpha_ = (n_samples - n_features +
-                  self.lambda_*np.trace(self.sigma_))\
-                  /np.sum((Y - np.dot(X, self.coef_))**2)
-            
+            self.gamma_ =  np.sum((self.alpha_ * self.eigen_vals_)\
+                            /(self.lambda_ + self.alpha_ * self.eigen_vals_))
+            self.lambda_ = self.gamma_ / np.dot(self.coef_.T,self.coef_)
+            self.alpha_ = (n_samples - self.gamma_)\
+                          /np.sum((Y - np.dot(X, self.coef_))**2)
 
             #### Compute the log likelihood
-            #if ll_bool:
-            #residual_ = (Y - np.dot(X, w))**2
-            #ll = 0.5*X.shape[1]*np.log(alpha) + 0.5*X.shape[0]*np.log(beta)
-            #ll -= (0.5*beta*residual_.sum()+ 0.5*alpha*np.dot(w.T,w))
-            #ll -= fast_logdet(alpha*ones + beta*gram)
-            #ll -= X.shape[0]*np.log(2*np.pi)
-            #log_likelihood.append(ll)
-            
-            ## convergence : compare w
-            #if (np.sum(np.abs(w_ -old_w_))<th_w):
-            #break
+            if self.compute_ll:
+                self.log_likelihood_.append(self.compute_log_likelihood(X,Y))
+
+            ### Check for convergence
+            if iter_ != 0 and np.sum(self.coef_old_ - self.coef_) < self.th_w:
+                    break
+            self.coef_old_ = np.copy(self.coef_)
 
         self.intercept_ = self._ymean - np.dot(self._xmean, self.coef_)
-
         # Store explained variance for __str__
         self.explained_variance_ = self._explained_variance(X, Y)
         return self
@@ -239,10 +245,11 @@ class ARDRegression(LinearModel):
     """
     # TODO: add intercept
 
-    def __init__(self, n_iter=300, th_w=1.e-12, compute_ll=False, 
-                fit_intercept=True):
+    def __init__(self, n_iter=300, th_w=1.e-12, th_lb=1.e-12, compute_ll=False,
+        fit_intercept=True):
         self.n_iter = n_iter
         self.th_w = th_w
+        self.th_lb = th_lb
         self.compute_ll = compute_ll
         self.fit_intercept = fit_intercept
 
@@ -251,50 +258,76 @@ class ARDRegression(LinearModel):
         self._set_params(**params)
         X = np.asanyarray(X, dtype=np.float)
         Y = np.asanyarray(Y, dtype=np.float)
-        log_likelihood_ = []
-        
-        ### Convergence loop of the bayesian ARD regression
-        for iter_ in range(n_iter):
+        n_samples, n_features = X.shape
 
-            # alpha
-            gamma_ = 1 - alpha[keep_a]*np.diag(sigma)
-            alpha[keep_a] = gamma_/w[keep_a]**2
-
-            # beta
-            residual_ = (Y - np.dot(X[:,keep_a], w[keep_a]))**2
-            beta = (X.shape[0]-gamma_.sum()) / residual_.sum()
-
-            ### Avoid divergence of the values by setting a maximum values of
-            ### the alpha
-            keep_a = alpha<alpha_th
-            gram = np.dot(X.T[keep_a,:], X[:,keep_a])
-
-            ### Compute mu and sigma
-            ones = np.eye(gram.shape[1])
-            sigma = linalg.pinv(alpha[keep_a]*ones+ beta*gram)
-            w[keep_a] = np.dot(beta*sigma,np.dot(X.T[keep_a,:],Y))
-            step_th -= 1
-
-            # convergence : compare w
-            has_converged =  (np.sum(np.abs(w-old_w))<th_w)
-            old_w = w
+        if self.fit_intercept:
+            self._xmean = X.mean(axis=0)
+            self._ymean = Y.mean(axis=0)
+            X = X - self._xmean
+            Y = Y - self._ymean
+        else:
+            self._xmean = 0.
+            self._ymean = 0.
 
 
-            ### Compute the log likelihood
-            if ll_bool :
-                A_ = np.eye(X.shape[1])/alpha
-                C_ = (1./beta)*np.eye(X.shape[0]) + np.dot(X,np.dot(A_,X.T))
-                ll = X.shape[0]*np.log(2*np.pi)+fast_logdet(C_)
-                ll += np.dot(Y.T,np.dot(linalg.pinv(C_),Y))
-                log_likelihood.append(-0.5*ll)
+        ### "Dummy" initialization of the values of the parameters
+        self.alpha_ = 1./np.var(Y)
+        self.lambda_ = np.ones(n_features)
+        self.log_likelihood_ = []
+        self.X_XT = np.dot(X,X.T)
+        self.XT_Y = np.dot(X.T,Y)
 
-        
+        ### Convergence loop of the bayesian ridge regression
+        for iter_ in range(self.n_iter):
+
+            ### Compute mu and sigma (using Woodbury matrix identity)
+            self.sigma_ =  np.dot(
+                    linalg.pinv(np.eye(n_samples)/self.alpha_ +
+                    np.dot(X * np.reshape(1./self.lambda_,[1,-1]),X.T)),
+                    X * np.reshape(1./self.lambda_,[1,-1]))
+            self.sigma_ = - np.dot(np.reshape(1./self.lambda_,[-1,1]) * X.T ,
+                                                                    self.sigma_)
+            self.sigma_.flat[::(self.sigma_.shape[1]+1)] += 1./self.lambda_
+            self.coef_ = self.alpha_ *np.dot(self.sigma_,self.XT_Y)
+
+            ### Update alpha and lambda
+            self.gamma_ =  1. - self.lambda_*np.diag(self.sigma_)
+            coef_2 = (self.coef_)**2
+            self.lambda_[coef_2>self.th_lb] = self.gamma_[coef_2>self.th_lb]\
+                                                   /coef_2[coef_2>self.th_lb]
+            self.lambda_[coef_2 <= self.th_lb] = 1./self.th_lb
+            self.alpha_ = (n_samples - self.gamma_.sum())\
+                          /np.sum((Y - np.dot(X, self.coef_))**2)
+
+            #### Compute the log likelihood
+            if self.compute_ll:
+                self.log_likelihood_.append(self.compute_log_likelihood(X,Y))
+
+            ### Check for convergence
+            if iter_ != 0 and np.sum(self.coef_old_ - self.coef_) < self.th_w:
+                    break
+            self.coef_old_ = np.copy(self.coef_)
+
+        self.intercept_ = self._ymean - np.dot(self._xmean, self.coef_)
         # Store explained variance for __str__
         self.explained_variance_ = self._explained_variance(X, Y)
         return self
 
-        def predict(self, T):
-            return np.dot(T, self.w)
+
+
+
+ #### Compute the log likelihood
+            #if ll_bool :
+                #A_ = np.eye(X.shape[1])/alpha
+                #C_ = (1./beta)*np.eye(X.shape[0]) + np.dot(X,np.dot(A_,X.T))
+                #ll = X.shape[0]*np.log(2*np.pi)+fast_logdet(C_)
+                #ll += np.dot(Y.T,np.dot(linalg.pinv(C_),Y))
+                #log_likelihood.append(-0.5*ll)
+
+
+
+
+
 
 
 
