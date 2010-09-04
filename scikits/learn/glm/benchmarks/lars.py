9cb4cabe997590977b9002f731aa07734130d2d6
Gael varoquaux <gael.varoquaux@normalesup.org>
2010-09-04 18:21:50 +0200

    BUG: Make sure computations do not get executed at import time, so that the tests still run.

--- scikits/learn/glm/benchmarks/lars.py
+++ scikits/learn/glm/benchmarks/lars.py
@@ -13,9 +13,10 @@ n, m = 100, 50000
 X = np.random.randn(n, m)
 y = np.random.randn(n)
 
-print "Computing regularization path using the LARS ..."
-start = datetime.now()
-alphas, active, path = glm.lars_path(X, y, method='lasso')
-print "This took ", datetime.now() - start
+if __name__ == '__main__':
+    print "Computing regularization path using the LARS ..."
+    start = datetime.now()
+    alphas, active, path = glm.lars_path(X, y, method='lasso')
+    print "This took ", datetime.now() - start
 
 
