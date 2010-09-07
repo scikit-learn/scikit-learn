530bef9e1fd39089b279e79f1b56ba039ce3c4ad
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-09-07 08:53:57 +0200

    Test for function utils.fixes._copysign.

--- scikits/learn/utils/tests/test_fixes.py
+++ scikits/learn/utils/tests/test_fixes.py
@@ -4,14 +4,22 @@
 
 import numpy as np
 
-import nose
+from nose.tools import assert_equal
+from numpy.testing import assert_array_equal
 
-from ..fixes import _in1d
+from ..fixes import _in1d, _copysign
 
 def test_in1d():
     a = np.arange(10)
     b = a[a%2 == 0]
-    nose.tools.assert_equal(_in1d(a, b).sum(), 5)
+    assert_equal(_in1d(a, b).sum(), 5)
 
+
+def test_copysign():
+    a = np.array([-1, 1, -1])
+    b = np.array([ 1, -1, 1])
+
+    assert_array_equal (np.copysign (a, b), b)
+    assert_array_equal (np.copysign (b, a), a)
 
 
