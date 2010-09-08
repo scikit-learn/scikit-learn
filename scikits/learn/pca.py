20bb33521ffb1b1e4ae2bbff9dee9b93f833f62c
bthirion <bertrand.thirion@inria.fr>
2010-09-08 18:08:28 +0200

    solved conflict in test_fastica

--- scikits/learn/pca.py
+++ scikits/learn/pca.py
@@ -8,6 +8,72 @@ from scipy import linalg
 
 from .base import BaseEstimator
 
+def _assess_dimension_(spect, rk, n, dim):
+    """
+    Compute the likelihood of a rank rk dataset 
+    embedded in gaussian noise of shape(n, dimf) having spectrum spect
+    
+    Parameters
+    ----------
+    spect: array of shape (n)
+           data spectrum
+    rk: int,  tested rank value
+    n: int, number of samples
+    dim: int, embedding/emprical dimension
+    
+    Returns
+    -------
+    ll, float, The log-likelihood
+    
+    Note
+    ---- 
+    This implements the method of Thomas P. Minka:
+    Automatic Choice of Dimensionality for PCA. NIPS 2000: 598-604
+    """
+    if rk>dim:
+        raise ValueError, "the dimension cannot exceed dim"
+    from scipy.special import gammaln
+    
+    pu = -rk*np.log(2)
+    for i in range(rk):
+        pu += gammaln((dim - i)/2) - np.log(np.pi) * (dim - i)/2
+        
+    pl = np.sum(np.log(spect[:rk]))
+    pl = -pl * n/2
+
+    if rk==dim:
+        pv = 0
+        v = 1
+    else:
+        v = np.sum(spect[rk:dim]) / (dim - rk)
+        pv = -np.log(v) * n * (dim - rk)/2
+    
+    m = dim * rk - rk * (rk + 1) / 2
+    pp = np.log(2 * np.pi) * (m + rk + 1)/2
+
+    pa = 0
+    spectrum_ = spect.copy()
+    spectrum_[rk:dim] = v
+    for i in range(rk):
+        for j in range (i + 1, dim):
+            pa += np.log((spect[i] - spect[j])*\
+                         (1./spectrum_[j] - 1./spectrum_[i])) + np.log(n)
+
+    ll = pu + pl + pv + pp -pa/2 - rk*np.log(n)/2
+
+    return ll
+
+def _infer_dimension_(spect, n, p):
+    """
+    This method infers the dimension of a dataset of shape (n,p)
+    with spectrum spect
+    """
+    ll = []
+    for rk in range(min(n, p, len(spect))):
+        ll.append(_assess_dimension_(spect, rk, n, p))
+    ll = np.array(ll)
+    return ll.argmax()
+        
 class PCA(BaseEstimator):
     """
     Principal component analysis (PCA)
@@ -20,9 +86,10 @@ class PCA(BaseEstimator):
 
     Attributes
     ----------
-    k : int
-        Number of components
-        If k is not set all components are kept
+    n_comp : int, none or string,
+             Number of components
+             if k is not set all components are kept
+             if k=='mle', Minka's mle is used to guess the dimension
 
     copy : bool
         If False, data passed to fit are overwritten
@@ -47,7 +114,7 @@ class PCA(BaseEstimator):
     Examples
     --------
     >>> X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
-    >>> pca = PCA(k=2)
+    >>> pca = PCA(n_comp=2)
     >>> pca.fit(X)
     PCA(k=2, copy=True)
     >>> print pca.explained_variance_
@@ -57,8 +124,8 @@ class PCA(BaseEstimator):
     --------
 
     """
-    def __init__(self, k=None, copy=True):
-        self.k = k
+    def __init__(self, n_comp=None, copy=True):
+        self.n_comp = n_comp
         self.copy = copy
 
     def fit(self, X, **params):
@@ -73,11 +140,17 @@ class PCA(BaseEstimator):
         U, S, V = linalg.svd(X, full_matrices=False)
         self.explained_variance_ = S**2
         self.explained_variance_ /= self.explained_variance_.sum()
-        self.components_ = V
-        if self.k is not None:
-            self.components_ = self.components_[:,:self.k]
-            self.explained_variance_ = self.explained_variance_[:self.k]
-
+        self.components_ = V.T
+        if self.n_comp=='mle':
+            self.n_comp = _infer_dimension_(self.explained_variance_,
+                                            n_samples, X.shape[1])
+            self.components_ = self.components_[:, :self.n_comp]
+            self.explained_variance_ = self.explained_variance_[:self.n_comp]
+        
+        elif self.n_comp is not None:
+            self.components_ = self.components_[:, :self.n_comp]
+            self.explained_variance_ = self.explained_variance_[:self.n_comp]
+        
         return self
 
     def transform(self, X):
@@ -85,3 +158,4 @@ class PCA(BaseEstimator):
         Xr = np.dot(Xr, self.components_)
         return Xr
 
+    
--- scikits/learn/tests/test_pca.py
+++ scikits/learn/tests/test_pca.py
@@ -1,7 +1,9 @@
 import numpy as np
+from nose.tools import assert_true
+
 
 from .. import datasets
-from ..pca import PCA
+from ..pca import PCA,  _assess_dimension_, _infer_dimension_
 
 iris = datasets.load_iris()
 
@@ -11,11 +13,75 @@ def test_pca():
     """
     PCA
     """
-
-    pca = PCA(k=2)
+    pca = PCA(n_comp=2)
     X_r = pca.fit(X).transform(X)
     np.testing.assert_equal(X_r.shape[1], 2)
 
     pca = PCA()
     pca.fit(X)
     np.testing.assert_almost_equal(pca.explained_variance_.sum(),  1.0, 3)
+
+def test_pca_check_project():
+    """test that the projection of data is correct
+    """
+    n, p = 100, 3
+    X = np.random.randn(n, p)*.1
+    X[:10] += np.array([3, 4, 5])
+    pca = PCA(n_comp=2)
+    X_r = pca.fit(X)
+    Xt = 0.1* np.random.randn(1, p) + np.array([3, 4, 5])
+    Yt = pca.transform(Xt)
+    Yt /= np.sqrt((Yt**2).sum())
+    np.testing.assert_almost_equal(Yt[0][0], 1., 1)
+
+def test_pca_dim():
+    """
+    """
+    n, p = 100, 5
+    X = np.random.randn(n, p)*.1
+    X[:10] += np.array([3, 4, 5, 1, 2])
+    pca = PCA(n_comp='mle')
+    pca.fit(X)
+    assert_true(pca.n_comp==1)
+
+def test_infer_dim_1():
+    """
+    """
+    n, p = 100, 5
+    X = np.random.randn(n, p)*.1
+    X[:10] += np.array([3, 4, 5, 1, 2])
+    pca = PCA(n_comp=p)
+    pca.fit(X)
+    spect = pca.explained_variance_
+    ll = []
+    for k in range(p):
+         ll.append(_assess_dimension_(spect, k, n, p))
+    ll = np.array(ll)
+    assert_true(ll.argmax()==1)
+
+def test_infer_dim_2():
+    """
+    """
+    n, p = 100, 5
+    X = np.random.randn(n, p)*.1
+    X[:10] += np.array([3, 4, 5, 1, 2])
+    X[10:20] += np.array([6, 0, 7, 2, -1])
+    pca = PCA(n_comp=p)
+    pca.fit(X)
+    spect = pca.explained_variance_
+    print _infer_dimension_(spect, n, p)
+    assert_true(_infer_dimension_(spect, n, p)==2)
+
+def test_infer_dim_3():
+    """
+    """
+    n, p = 100, 5
+    X = np.random.randn(n, p)*.1
+    X[:10] += np.array([3, 4, 5, 1, 2])
+    X[10:20] += np.array([6, 0, 7, 2, -1])
+    X[30:40] += 2*np.array([-1, 1, -1, 1, -1])
+    pca = PCA(n_comp=p)
+    pca.fit(X)
+    spect = pca.explained_variance_
+    assert_true(_infer_dimension_(spect, n, p)==3)
+
