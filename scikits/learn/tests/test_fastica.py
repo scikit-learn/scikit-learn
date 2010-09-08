4315aee064c9af75813fac38a3e011205b21e1db
bthirion <bertrand.thirion@inria.fr>
2010-09-08 14:23:17 +0200

    completed and cleaned the tests

--- scikits/learn/tests/test_fastica.py
+++ scikits/learn/tests/test_fastica.py
@@ -41,14 +41,13 @@ def test_gs():
 
 def test_fastica(add_noise=False):
     """ Test the FastICA algorithm on very simple data.
-
     """
-
+    import scipy.stats as st
     n_samples = 1000
     # Generate two sources:
-    t  = np.linspace(0, 100, n_samples)
-    s1 = np.sin(t)
-    s2 = np.ceil(np.sin(np.pi*t))
+    s1 = (2*np.sin(np.linspace(0, 100, n_samples))>0)-1
+    #s1 = 2*(np.random.rand(n_samples)>.5) - 1
+    s2 = st.t.rvs(1, size=n_samples)
     s = np.c_[s1, s2].T
     center_and_norm(s)
     s1, s2 = s
@@ -60,7 +59,7 @@ def test_fastica(add_noise=False):
     m  = np.dot(mixing, s)
 
     if add_noise:
-        m = np.vstack((m, np.random.randn( 1, n_samples)))
+        m += 0.1*np.random.randn(2, 1000) 
     
     center_and_norm(m)
 
@@ -69,7 +68,7 @@ def test_fastica(add_noise=False):
     for nl in non_linearity:
         for algo in algorithm:
             k_, mixing_, s_ = fastica.fastica(
-                m, n_comp=2, fun=nl, algorithm=algo)
+                m, fun=nl, algorithm=algo)
 
             # Check that the mixing model described in the docstring holds:
             np.testing.assert_almost_equal(s_, np.dot(np.dot(mixing_, k_), m))
@@ -86,9 +85,58 @@ def test_fastica(add_noise=False):
             # Check that we have estimated the original sources
             if add_noise==False:
                 np.testing.assert_almost_equal(
-                    np.dot(s1_, s1)/n_samples, 1, decimal=3)
+                    np.dot(s1_, s1)/n_samples, 1, decimal=2)
                 np.testing.assert_almost_equal(
-                    np.dot(s2_, s2)/n_samples, 1, decimal=3)
+                    np.dot(s2_, s2)/n_samples, 1, decimal=2)
+            else:
+                np.testing.assert_almost_equal(
+                    np.dot(s1_, s1)/n_samples, 1, decimal=1)
+                np.testing.assert_almost_equal(
+                    np.dot(s2_, s2)/n_samples, 1, decimal=1)
+            
+
+def test_non_square_fastica(add_noise=False):
+    """ Test the FastICA algorithm on very simple data.
+    """
+
+    n_samples = 1000
+    # Generate two sources:
+    t  = np.linspace(0, 100, n_samples)
+    s1 = np.sin(t)
+    s2 = np.ceil(np.sin(np.pi*t))
+    s = np.c_[s1, s2].T
+    center_and_norm(s)
+    s1, s2 = s
+
+    # Mixing matrix
+    mixing = np.random.randn(6, 2)
+    m  = np.dot(mixing, s)
+
+    if add_noise:
+        m += 0.1*np.random.randn(6, n_samples)
+    
+    center_and_norm(m)
+
+    k_, mixing_, s_ = fastica.fastica(m, n_comp=2)
+
+    # Check that the mixing model described in the docstring holds:
+    np.testing.assert_almost_equal(s_, np.dot(np.dot(mixing_, k_), m))
+    
+    center_and_norm(s_)
+    s1_, s2_ = s_
+    # Check to see if the sources have been estimated
+    # in the wrong order
+    if abs(np.dot(s1_, s2)) > abs(np.dot(s1_, s1)):
+        s2_, s1_ = s_
+    s1_ *= np.sign(np.dot(s1_, s1))
+    s2_ *= np.sign(np.dot(s2_, s2))
+
+    # Check that we have estimated the original sources
+    if add_noise==False:
+        np.testing.assert_almost_equal(np.dot(s1_, s1)/n_samples, 1, decimal=3)
+        np.testing.assert_almost_equal(np.dot(s2_, s2)/n_samples, 1, decimal=3)
+
+
 
 
 
@@ -96,4 +144,6 @@ if __name__ == '__main__':
     test_gs()
     test_fastica()
     test_fastica(True)
+    test_non_square_fastica()
+    test_non_square_fastica(True)
 
