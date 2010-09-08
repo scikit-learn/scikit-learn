5bc337eb123c722dcc0ba4d1e74d51a6b2c9fd8a
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2010-09-08 17:38:51 +0200

    FIX : fix ica vs pca example

--- examples/plot_ica_vs_pca.py
+++ examples/plot_ica_vs_pca.py
@@ -3,36 +3,25 @@
 FastICA on 2D point clouds
 ==========================
 
-XXX: Still buggy !!!
+Illustrate visually the results of ICA vs PCA
 
 """
 
 import numpy as np
 import pylab as pl
-from scipy import stats
 
 from scikits.learn.pca import PCA
 from scikits.learn.fastica import FastICA
 
 ###############################################################################
 # Generate sample data
-S = np.c_[np.random.normal(scale=0.5, size=(2, 10000)),
-          np.random.normal(scale=4, size=(2, 10000))]
-np.random.shuffle(S)
-
-def pdf(x):
-    e = np.exp(1.)
-    return 0.5*(stats.norm(scale=0.5/e).pdf(x) + stats.norm(scale=4/e).pdf(x))
-
-# Standardize data
-S /= S.std(axis=1)[:,np.newaxis]
-density = pdf(S[0]) * pdf(S[1])
+S = np.random.standard_t(1.5, size=(2, 10000))
+S[0] *= 2.
 
 # Mix data
 A = [[1, 1], [0, 2]] # Mixing matrix
 
 X = np.dot(A, S) # Generate observations
-X /= np.std(X)
 
 pca = PCA()
 S_pca_ = pca.fit(X.T).transform(X.T).T
@@ -45,9 +34,8 @@ S_ica_ /= S_ica_.std(axis=1)[:,np.newaxis]
 ###############################################################################
 # Plot results
 
-def plot_samples(S, density, axis_list=None):
-    pl.scatter(S[0], S[1], s=1, marker='o', c=np.sqrt(density), linewidths=0,
-                                            zorder=10)
+def plot_samples(S, axis_list=None):
+    pl.scatter(S[0], S[1], s=2, marker='o', linewidths=0, zorder=10)
     if axis_list is not None:
         colors = [(0, 0.6, 0), (0.6, 0, 0)]
         for color, axis in zip(colors, axis_list):
@@ -68,23 +56,22 @@ def plot_samples(S, density, axis_list=None):
 
 pl.close('all')
 pl.subplot(2, 2, 1)
-plot_samples(S, density)
+plot_samples(S / S.std())
 pl.title('True Independant Sources')
 
 axis_list = [pca.components_, ica.get_mixing_matrix()]
-# axis_list = [pca.components_, np.array(A)]
 pl.subplot(2, 2, 2)
-plot_samples(X, density, axis_list=axis_list)
+plot_samples(X / np.std(X), axis_list=axis_list)
 pl.legend(['PCA', 'ICA'], loc='upper left')
 pl.title('Observations')
 
 pl.subplot(2, 2, 3)
-plot_samples(S_pca_, density)
+plot_samples(S_pca_ / np.std(S_pca_))
 pl.title('PCA scores')
 
 pl.subplot(2, 2, 4)
-plot_samples(S_ica_, density)
-pl.title('ICA sources')
+plot_samples(S_ica_ / np.std(S_ica_))
+pl.title('ICA estimated sources')
 
 pl.subplots_adjust(0.09, 0.04, 0.94, 0.94, 0.26, 0.26)
 
