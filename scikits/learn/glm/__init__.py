b568f5d2e6f15cb52540ebe2170a55eba3624a6b
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-09-09 17:54:28 +0200

    Fix imports.

--- scikits/learn/glm/__init__.py
+++ scikits/learn/glm/__init__.py
@@ -13,7 +13,5 @@ from .base import LinearRegression
 from .lars import LARS, LassoLARS, lars_path, LeastAngleRegression
 from .coordinate_descent import Lasso, ElasticNet, LassoCV, ElasticNetCV, \
                                 lasso_path, enet_path
-from .bayes import BayesianRidge, ARDRegression
-from .bayes_classification import ARDClassification
 from .ridge import Ridge
 
