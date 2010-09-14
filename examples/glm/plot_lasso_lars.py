7c24dd7626e9c273624a1fec8e494e0077492ffd
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-09-14 22:49:53 +0200

    A more challenging example.

--- examples/glm/plot_lasso_lars.py
+++ examples/glm/plot_lasso_lars.py
@@ -25,19 +25,19 @@ y = diabetes.target
 X[:, 6] = -X[:, 6]
 
 
-# m, n = 200, 200
-# np.random.seed(0)
-# X = np.random.randn(m, n)
-# y = np.random.randn(m)
+m, n = 200, 200
+np.random.seed(0)
+X = np.random.randn(m, n)
+y = np.random.randn(m)
 
 
-# _xmean = X.mean(0)
-# _ymean = y.mean(0)
-# X = X - _xmean
-# y = y - _ymean
-# _norms = np.apply_along_axis (np.linalg.norm, 0, X)
-# nonzeros = np.flatnonzero(_norms)
-# X[:, nonzeros] /= _norms[nonzeros]
+_xmean = X.mean(0)
+_ymean = y.mean(0)
+X = X - _xmean
+y = y - _ymean
+_norms = np.apply_along_axis (np.linalg.norm, 0, X)
+nonzeros = np.flatnonzero(_norms)
+X[:, nonzeros] /= _norms[nonzeros]
 
 ################################################################################
 # Demo path functions
@@ -46,7 +46,7 @@ X[:, 6] = -X[:, 6]
 G = np.dot(X.T, X)
 print "Computing regularization path using the LARS ..."
 start = datetime.now()
-alphas, active, path = glm.lars_path(X, y, Gram=G, method='lasso', max_iter=12)
+alphas, active, path = glm.lars_path(X, y, Gram=G, method='lasso')
 print "This took ", datetime.now() - start
 
 alphas = np.sum(np.abs(path.T), axis=1)
