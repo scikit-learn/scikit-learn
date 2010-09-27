a22d25e9c07d314a32a5336419d89c455d2ff0fd
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-09-27 16:16:52 +0200

    Remove Minilearn C sources.

--- scikits/learn/setup.py
+++ scikits/learn/setup.py
@@ -84,7 +84,7 @@ def configuration(parent_package='', top_path=None):
 
     ## end liblinear module
 
-    # minilear needs cblas, fortran-compiled BLAS will not be sufficient
+    # some libs needs cblas, fortran-compiled BLAS will not be sufficient
     blas_info = get_info('blas_opt', 0)
     if (not blas_info) or (
         ('NO_ATLAS_INFO', 1) in blas_info.get('define_macros', [])) :
@@ -97,22 +97,6 @@ def configuration(parent_package='', top_path=None):
         blas_info.pop('libraries', None)
     else:
         cblas_libs = blas_info.pop('libraries', [])
-
-    minilearn_sources = [
-        join('src', 'minilearn', 'lars.c'),
-        join('src', 'minilearn', '_minilearn.c')]
-
-    config.add_extension('_minilearn',
-                         sources = minilearn_sources,
-                         libraries = cblas_libs,
-                         include_dirs=[join('src', 'minilearn'),
-                                       join('src', 'cblas'),
-                                       numpy.get_include(),
-                                       blas_info.pop('include_dirs', [])],
-                         extra_compile_args=['-std=c99'] + \
-                                             blas_info.pop('extra_compile_args', []),
-                         **blas_info
-                         )
 
     config.add_extension('ball_tree',
                          sources=[join('src', 'BallTree.cpp')],
