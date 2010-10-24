0a6f6d141fbdb90df7c8ac545f5b81c56b1aa785
Peter Prettenhofer <peter.prettenhofer@gmail.com>
2010-10-24 21:30:12 +0200

    added doctests to SGD and sgd.LinearModel

--- scikits/learn/sgd/base.py
+++ scikits/learn/sgd/base.py
@@ -46,6 +46,24 @@ class LinearModel(BaseEstimator):
     `intercept_` : float
         Constants in decision function.
 
+    Examples
+    --------
+    >>> import numpy as np
+    >>> X = np.array([[-1, -1], [-2, -1], [1, 1], [2, 1]])
+    >>> Y = np.array([1, 1, 2, 2])
+    >>> from scikits.learn.sgd.sparse import SGD
+    >>> clf = SGD()
+    >>> clf.fit(X, Y)
+    SGD(loss='hinge', shuffle=False, fit_intercept=True, n_iter=5,
+    penalty='l2', coef_=array([-9.80373, -9.80373]), rho=1.0,
+    alpha=0.0001, intercept_=0.1)
+    >>> print clf.predict([[-0.8, -1]])
+    [ 1.]
+
+    See also
+    --------
+    LinearSVC
+
     """
 
     def __init__(self, loss="hinge", penalty='l2', alpha=0.0001,
