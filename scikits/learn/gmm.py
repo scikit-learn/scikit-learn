e0ef8e0d53338818a57fe42d035528ab90e6e429
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2010-10-28 16:31:03 +0200

    Cosmetic changes in gmm.

--- scikits/learn/gmm.py
+++ scikits/learn/gmm.py
@@ -562,9 +562,9 @@ def _lmvnpdffull(obs, means, covars):
     log_prob = np.empty((nobs,nmix))
     for c, (mu, cv) in enumerate(itertools.izip(means, covars)):
         cv_chol = linalg.cholesky(cv, lower=True)
-        cv_det = np.prod(np.diagonal(cv_chol)) ** 2
-        cv_sol = solve_triangular(cv_chol, (obs - mu).T, lower=True)
-        log_prob[:, c] = -.5 * (np.sum(cv_sol ** 2, axis=0) + \
+        cv_det  = np.prod(np.diagonal(cv_chol))**2
+        cv_sol  = solve_triangular(cv_chol, (obs - mu).T, lower=True).T
+        log_prob[:, c]  = -.5 * (np.sum(cv_sol**2, axis=1) + \
                            ndim * np.log(2 * np.pi) + np.log(cv_det))
 
     return log_prob
