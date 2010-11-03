cf0c08ca8d67f1c00c7b962baf40ac6dc458abf3
Mathieu Blondel <mathieu@mblondel.org>
2010-11-03 17:58:18 +0900

    Fix SGD-based binary classification example.

--- examples/sgd/mlcomp_sparse_document_classification_sgd.py
+++ examples/sgd/mlcomp_sparse_document_classification_sgd.py
@@ -36,75 +36,82 @@ print __doc__
 
 # Author: Peter Prettenhofer <peter.prettenhofer@gmail.com>
 #         Olivier Grisel <olivier.grisel@ensta.org>
+#         Mathieu Blondel <mathieu@mblondel.org>
 # License: Simplified BSD
 
 from time import time
 import sys
 import os
 import numpy as np
+import scipy.sparse as sp
+import pylab as pl
 
 from scikits.learn.datasets import load_mlcomp
+from scikits.learn.feature_extraction.text.sparse import Vectorizer
+from scikits.learn.sgd.sparse import SGD
 from scikits.learn.metrics import confusion_matrix
 from scikits.learn.metrics import classification_report
-
-from scikits.learn.sgd.sparse import SGD
 
 if 'MLCOMP_DATASETS_HOME' not in os.environ:
     print "Please follow those instructions to get started:"
     print __doc__
     sys.exit(0)
 
-# Load the training set
+# Load two categories from the training set (binary classification)
 print "Loading 20 newsgroups training set... "
+news_train = load_mlcomp('20news-18828', 'train',
+                         categories=['alt.atheism', 'comp.graphics'])
+
+print news_train.DESCR
+print "%d documents" % len(news_train.filenames)
+print "%d categories" % len(news_train.target_names)
+
+print "Extracting features from the dataset using a sparse vectorizer"
 t0 = time()
-news_train = load_mlcomp('20news-18828', 'train', sparse=True)
+vectorizer = Vectorizer()
+X_train = vectorizer.fit_transform((open(f).read() for f in news_train.filenames))
 print "done in %fs" % (time() - t0)
+print "n_samples: %d, n_features: %d" % X_train.shape
+assert sp.issparse(X_train)
+y_train = news_train.target
 
-print "Creating binary classification task\n"\
-      "alt.atheism vs. comp.graphics"
-target = news_train.target
-pos = 0 # alt.atheism
-neg = 1 # comp.graphics
-pos_idx = np.where(target == pos)[0]
-neg_idx = np.where(target == neg)[0]
-idx = np.concatenate((pos_idx, neg_idx))
-np.random.seed(13)
-np.random.shuffle(idx)
-data = news_train.data[idx]
-target = news_train.target[idx]
-
-print "num train docs: ", data.shape[0]
-print ""
 print "Training a linear SVM (hinge loss and L2 regularizer) using SGD.\n"\
       "SGD(n_iter=50, alpha=0.00001, fit_intercept=True)"
 t0 = time()
 clf = SGD(n_iter=50, alpha=0.00001, fit_intercept=True)
-clf.fit(data, target)
+clf.fit(X_train, y_train)
 print "done in %fs" % (time() - t0)
 print "Percentage of non zeros coef: %f" % (np.mean(clf.coef_ != 0) * 100)
 
-print "Loading 20 newsgroups test set... "
-t0 = time()
-news_test = load_mlcomp('20news-18828', 'test', sparse=True)
-print "done in %fs" % (time() - t0)
 
-target = news_test.target
-pos_idx = np.where(target == pos)[0]
-neg_idx = np.where(target == neg)[0]
-idx = np.concatenate((pos_idx, neg_idx))
-data = news_test.data[idx]
-target = news_test.target[idx]
-target_names = news_test.target_names[:2]
+print "Loading 20 newsgroups test set... "
+news_test = load_mlcomp('20news-18828', 'test',
+                         categories=['alt.atheism', 'comp.graphics'])
+t0 = time()
+print "done in %fs" % (time() - t0)
 
 print "Predicting the labels of the test set..."
+print "%d documents" % len(news_test.filenames)
+print "%d categories" % len(news_test.target_names)
+
+print "Extracting features from the dataset using the same vectorizer"
 t0 = time()
-pred = clf.predict(data)
+X_test = vectorizer.transform((open(f).read() for f in news_test.filenames))
+y_test = news_test.target
 print "done in %fs" % (time() - t0)
-print "Classification report on test set:"
-print classification_report(target, pred, class_names=target_names)
+print "n_samples: %d, n_features: %d" % X_test.shape
 
+print "Predicting the outcomes of the testing set"
+t0 = time()
+pred = clf.predict(X_test)
+print "done in %fs" % (time() - t0)
 
-cm = confusion_matrix(target, pred)
+print "Classification report on test set for classifier:"
+print clf
+print
+print classification_report(y_test, pred, class_names=news_test.target_names)
+
+cm = confusion_matrix(y_test, pred)
 print "Confusion matrix:"
 print cm
 
