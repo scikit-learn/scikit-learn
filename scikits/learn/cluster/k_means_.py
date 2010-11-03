2ceca5c4f534732bd6c67ee607d5ecfe21e73e76
Olivier Grisel <olivier.grisel@ensta.org>
2010-11-04 00:40:38 +0100

    cosmit in k_means module

--- scikits/learn/cluster/k_means_.py
+++ scikits/learn/cluster/k_means_.py
@@ -17,29 +17,31 @@ from ..base import BaseEstimator
 # kinit originaly from pybrain:
 # http://github.com/pybrain/pybrain/raw/master/pybrain/auxiliary/kmeans.py
 def k_init(X, k, n_samples_max=500):
-    """ Init k seeds according to kmeans++
+    """Init k seeds according to kmeans++
 
-        Parameters
-        -----------
-        X: array, shape (n_samples, n_features)
-            The data
-        k: integer
-            The number of seeds to choose
-        n_samples_max: integer, optional
-            The maximum number of samples to use: the complexity of the 
-            algorithm is n_samples**2, if n_samples > n_samples_max,
-            we use the Niquist strategy, and choose our centers in the 
-            n_samples_max samples randomly choosen.
+    Parameters
+    -----------
+    X: array, shape (n_samples, n_features)
+        The data
 
-        Notes
-        ------
-        Selects initial cluster centers for k-mean clustering in a smart way 
-        to speed up convergence. see: Arthur, D. and Vassilvitskii, S. 
-        "k-means++: the advantages of careful seeding". ACM-SIAM symposium 
-        on Discrete algorithms. 2007
+    k: integer
+        The number of seeds to choose
 
-        Implementation from Yong Sun's website
-        http://blogs.sun.com/yongsun/entry/k_means_and_k_means
+    n_samples_max: integer, optional
+        The maximum number of samples to use: the complexity of the
+        algorithm is n_samples**2, if n_samples > n_samples_max,
+        we use the Niquist strategy, and choose our centers in the
+        n_samples_max samples randomly choosen.
+
+    Notes
+    ------
+    Selects initial cluster centers for k-mean clustering in a smart way
+    to speed up convergence. see: Arthur, D. and Vassilvitskii, S.
+    "k-means++: the advantages of careful seeding". ACM-SIAM symposium
+    on Discrete algorithms. 2007
+
+    Implementation from Yong Sun's website
+    http://blogs.sun.com/yongsun/entry/k_means_and_k_means
     """
     n_samples = X.shape[0]
     if n_samples >= n_samples_max:
@@ -48,21 +50,21 @@ def k_init(X, k, n_samples_max=500):
 
     'choose the 1st seed randomly, and store D(x)^2 in D[]'
     centers = [X[np.random.randint(n_samples)]]
-    D = ((X - centers[0])**2).sum(axis=-1)
+    D = ((X - centers[0]) ** 2).sum(axis=-1)
 
     for _ in range(k - 1):
         bestDsum = bestIdx = -1
 
         for i in range(n_samples):
             'Dsum = sum_{x in X} min(D(x)^2,||x-xi||^2)'
-            Dsum = np.minimum(D, ((X - X[i])**2).sum(axis=-1)
+            Dsum = np.minimum(D, ((X - X[i]) ** 2).sum(axis=-1)
                               ).sum()
 
             if bestDsum < 0 or Dsum < bestDsum:
                 bestDsum, bestIdx = Dsum, i
 
         centers.append(X[bestIdx])
-        D = np.minimum(D, ((X - X[bestIdx])**2).sum(axis=-1))
+        D = np.minimum(D, ((X - X[bestIdx]) ** 2).sum(axis=-1))
 
     return np.array(centers)
 
@@ -86,10 +88,13 @@ def k_means(X, k, init='k-means++', n_init=10, max_iter=300, verbose=0,
         'matrix', or if a ndarray is given instead, it is
         interpreted as initial cluster to use instead.
 
-    max_iter: int, optional
-        Number of iterations of the k-means algrithm to run. Note
-        that this differs in meaning from the iters parameter to
-        the kmeans function.
+    max_iter: int, optional, default 300
+        Maximum number of iterations of the k-means algorithm to run.
+
+    n_init: int, optional, default: 10
+        Number of time the k-means algorithm will be run with different centroid
+        seeds. The final results will be the best output of n_init consecutive
+        runs in terms of inertia.
 
     init: {'k-means++', 'random', or an ndarray}, optional
         Method for initialization, default to 'k-means++':
@@ -115,9 +120,11 @@ def k_means(X, k, init='k-means++', n_init=10, max_iter=300, verbose=0,
     centroid: ndarray
         A k by N array of centroids found at the last iteration of
         k-means.
+
     label: ndarray
         label[i] is the code or index of the centroid the
         i'th observation is closest to.
+
     inertia: float
         The final value of the inertia criterion
 
@@ -145,21 +152,20 @@ def k_means(X, k, init='k-means++', n_init=10, max_iter=300, verbose=0,
             raise ValueError("the init parameter for the k-means should "
                 "be 'k-mean++' or 'random' or an ndarray, "
                 "'%s' (type '%s') was passed.")
-        centers_old = centers.copy()
-    
+
         # iterations
         for i in range(max_iter):
+            centers_old = centers.copy()
             labels, inertia = _e_step(X, centers)
             centers = _m_step(X, labels, k)
-            if verbose: 
+            if verbose:
                 print 'Iteration %i, intertia %s' % (i, inertia)
-            if np.sum((centers_old - centers)**2)<delta*vdata:
-                if verbose: 
+            if np.sum((centers_old - centers) ** 2) < delta * vdata:
+                if verbose:
                     print 'Converged to similar centers at iteration', i
                 break
-            centers_old = centers.copy()
 
-            if inertia<best_inertia:
+            if inertia < best_inertia:
                 best_centers = centers.copy()
                 best_labels  = labels.copy()
                 best_inertia = inertia
@@ -167,31 +173,30 @@ def k_means(X, k, init='k-means++', n_init=10, max_iter=300, verbose=0,
         best_centers = centers
         best_labels  = labels
         best_inertia = inertia
-
     return best_centers, best_labels, best_inertia
 
 
 def _m_step(x, z ,k):
     """ M step of the K-means EM algorithm
 
-    Computation of cluster centers/means    
+    Computation of cluster centers/means
 
     Parameters
     ----------
     x array of shape (n,p)
       n = number of samples, p = number of features
-    z, array of shape (x.shape[0]) 
-        Current assignment  
+    z, array of shape (x.shape[0])
+        Current assignment
     k, int
-        Number of desired clusters       
-    
+        Number of desired clusters
+
     Returns
     -------
     centers, array of shape (k, p)
         The resulting centers
     """
     dim = x.shape[1]
-    centers = np.repeat(np.reshape(x.mean(0) ,(1, dim)), k, 0)
+    centers = np.repeat(np.reshape(x.mean(0), (1, dim)), k, 0)
     for q in range(k):
         if np.sum(z==q)==0:
             pass
@@ -201,21 +206,23 @@ def _m_step(x, z ,k):
 
 
 def _e_step(x, centers):
-    """ E step of the K-means EM algorithm
+    """E step of the K-means EM algorithm
 
     Computation of the input-to-cluster assignment
-    
+
     Parameters
     ----------
-    x: array of shape (n,p)
+    x: array of shape (n, p)
       n = number of samples, p = number of features
-    centers: array of shape (k, p) 
+
+    centers: array of shape (k, p)
         The cluster centers
-    
+
     Returns
     -------
     z: array of shape(n)
         The resulting assignment
+
     inertia: float
         The value of the inertia criterion with the assignment
     """
@@ -224,10 +231,10 @@ def _e_step(x, centers):
     mindist = np.infty * np.ones(n_samples)
     k = centers.shape[0]
     for q in range(k):
-        dist = np.sum((x-centers[q])**2,1)
+        dist = np.sum((x - centers[q]) ** 2, 1)
         z[dist<mindist] = q
         mindist = np.minimum(dist, mindist)
-    inertia = mindist.sum()    
+    inertia = mindist.sum()
     return z, inertia
 
 
@@ -250,10 +257,13 @@ class KMeans(BaseEstimator):
         'matrix', or if a ndarray is given instead, it is
         interpreted as initial cluster to use instead.
 
-    n_iter : int
-        Number of iterations of the k-means algrithm to run. Note
-        that this differs in meaning from the iters parameter to
-        the kmeans function.
+    max_iter : int
+        Maximum number of iterations of the k-means algorithm for a single run.
+
+    n_init: int, optional, default: 10
+        Number of time the k-means algorithm will be run with different centroid
+        seeds. The final results will be the best output of n_init consecutive
+        runs in terms of inertia.
 
     init : {'k-means++', 'random', 'points', 'matrix'}
         Method for initialization, defaults to 'k-means++':
@@ -295,15 +305,15 @@ class KMeans(BaseEstimator):
 
     The k-means problem is solved using the Lloyd algorithm.
 
-    The average complexity is given by O(k n T), were n is the number of 
+    The average complexity is given by O(k n T), were n is the number of
     samples and T is the number of iteration.
 
-    The worst case complexity is given by O(n^(k+2/p)) with 
-    n = n_samples, p = n_features. (D. Arthur and S. Vassilvitskii, 
+    The worst case complexity is given by O(n^(k+2/p)) with
+    n = n_samples, p = n_features. (D. Arthur and S. Vassilvitskii,
     'How slow is the k-means method?' SoCG2006)
 
     In practice, the K-means algorithm is very fast (on of the fastest
-    clustering algorithms available), but it falls in local minimas, and 
+    clustering algorithms available), but it falls in local minimas, and
     it can be useful to restarts it several times.
     """
 
@@ -318,8 +328,8 @@ class KMeans(BaseEstimator):
         """ Compute k-means"""
         X = np.asanyarray(X)
         self._set_params(**params)
-        self.cluster_centers_, self.labels_, self.inertia_ = k_means(X, 
+        self.cluster_centers_, self.labels_, self.inertia_ = k_means(X,
                     k=self.k, init=self.init, n_init=self.n_init,
                     max_iter=self.max_iter)
         return self
- 
+
