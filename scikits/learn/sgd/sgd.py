2e980bca8ac6983f152b26ec7da9aeb2e8926ec9
Olivier Grisel <olivier.grisel@ensta.org>
2010-11-07 19:20:10 +0100

    make the dense SGD code & docstring more readable

--- scikits/learn/sgd/sgd.py
+++ scikits/learn/sgd/sgd.py
@@ -11,35 +11,54 @@ from .sgd_fast import plain_sgd
 
 
 class SGD(BaseSGD):
-    """Linear Model trained by minimizing a regularized training
-    error using SGD.
+    """Linear model fitted by minimizing a regularized empirical loss with SGD
+
+    SGD stands for Stochastic Gradient Descent: the gradient of the loss is
+    estimated each sample at a time and the model is update along the way with a
+    decreasing strength schedule (aka learning rate).
+
+    The regularizer is a penalty added to the loss function that shrinks model
+    parameters towards the zero vector using either the squared euclidean
+
+    This implementation works with data represented as dense numpy arrays of
+    floating point values for the features.
 
     Parameters
     ----------
     loss : str, 'hinge' or 'log' or 'modifiedhuber'
         The loss function to be used. Defaults to 'hinge'.
+
     penalty : str, 'l2' or 'l1' or 'elasticnet'
         The penalty (aka regularization term) to be used. Defaults to 'l2'.
+
     alpha : float
         Constant that multiplies the regularization term. Defaults to 0.0001
+
     rho : float
         The Elastic Net mixing parameter, with 0 < rho <= 1.
         Defaults to 0.85.
+
     coef_ : array, shape = [n_features] if n_classes == 2 else [n_classes, n_features]
         The initial coeffients to warm-start the optimization.
+
     intercept_ : array, shape = [1] if n_classes == 2 else [n_classes]
         The initial intercept to warm-start the optimization.
+
     fit_intercept: bool
         Whether the intercept should be estimated or not. If False, the
         data is assumed to be already centered. Defaults to True.
+
     n_iter: int
         The number of passes over the training data (aka epochs).
         Defaults to 5.
+
     shuffle: bool
         Whether or not the training data should be shuffled after each epoch.
         Defaults to False.
+
     verbose: integer, optional
         The verbosity level
+
     n_jobs: integer, optional
         The number of CPUs to use to do the OVA (One Versus All, for
         multi-class problems) computation. -1 means 'all CPUs'.
@@ -73,10 +92,11 @@ class SGD(BaseSGD):
     """
 
     def fit(self, X, y, **params):
-        """Fit current model with SGD. """
+        """Fit current model with regularized Stochastic Gradient Descent"""
         self._set_params(**params)
         X = np.asanyarray(X, dtype=np.float64)
         y = np.asanyarray(y, dtype=np.float64)
+
         # largest class id is positive class
         classes = np.unique(y)
         self.classes = classes
@@ -85,14 +105,13 @@ class SGD(BaseSGD):
         elif classes.shape[0] == 2:
             self._fit_binary(X, y)
         else:
-            raise ValueError("The number of class labels must be " \
-                             "greater than one. ")
+            raise ValueError("The number of class labels must be "
+                             "greater than one.")
         # return self for chaining fit and predict calls
         return self
 
     def _fit_binary(self, X, y):
-        """Fit a binary classifier.
-        """
+        """Fit a single binary classifier"""
         # encode original class labels as 1 (classes[1]) or -1 (classes[0]).
         y_new = np.ones(y.shape, dtype=np.float64) * -1.0
         y_new[y == self.classes[1]] = 1.0
@@ -125,9 +144,10 @@ class SGD(BaseSGD):
         self.intercept_ = np.asarray(intercept_)
 
     def _fit_multiclass(self, X, y):
-        """Fit a multi-class classifier with a combination
-        of binary classifiers, each predicts one class versus
-        all others (OVA: One Versus All).
+        """Fit a multi-class classifier by combining binary classifiers
+
+        Each binary classifier predicts one class versus all others. This
+        strategy is called OVA: One Versus All.
         """
         n_classes = self.classes.shape[0]
         n_samples, n_features = X.shape[0], X.shape[1]
@@ -139,10 +159,8 @@ class SGD(BaseSGD):
                 raise ValueError("Provided coef_ does not match dataset. ")
             coef_ = self.coef_
 
-        if self.intercept_ is None \
-               or isinstance(self.intercept_, float):
-            intercept_ = np.zeros(n_classes, dtype=np.float64,
-                                  order="C")
+        if self.intercept_ is None or isinstance(self.intercept_, float):
+            intercept_ = np.zeros(n_classes, dtype=np.float64, order="C")
         else:
             if self.intercept_.shape != (n_classes, ):
                 raise ValueError("Provided intercept_ does not match dataset. ")
@@ -162,9 +180,8 @@ class SGD(BaseSGD):
         self.coef_ = coef_
         self.intercept_ = intercept_
 
-
     def predict_margin(self, X):
-        """Predict signed 'distance' to the hyperplane (aka confidence score).
+        """Predict signed 'distance' to the hyperplane (aka confidence score)
 
         Parameters
         ----------
@@ -182,17 +199,15 @@ class SGD(BaseSGD):
         else:
             return scores
 
-
     def __reduce__(self):
-        """Handler which is called at pickeling time.
+        """Handler which is called at pickeling time
 
-        This is important
-        for joblib because otherwise it will crash trying to pickle
-        the external loss function object.
+        This is important for joblib because otherwise it will crash trying to
+        pickle the external loss function object.
         """
-        return SGD,(self.loss, self.penalty, self.alpha, self.rho, self.coef_,
-                    self.intercept_, self.fit_intercept, self.n_iter,
-                    self.shuffle, self.verbose, self.n_jobs)
+        return SGD, (self.loss, self.penalty, self.alpha, self.rho, self.coef_,
+                     self.intercept_, self.fit_intercept, self.n_iter,
+                     self.shuffle, self.verbose, self.n_jobs)
 
 
 def _train_ova_classifier(i, c, X, y, clf):
