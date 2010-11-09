aa6cffaea2a61d64bcebd5cda1c13d658d0e9ad1
Peter Prettenhofer <peter.prettenhofer@gmail.com>
2010-11-09 10:47:31 +0100

    cosmit: color of data points matches color of decision regions and OVA hyperplanes.

--- examples/sgd/plot_iris.py
+++ examples/sgd/plot_iris.py
@@ -3,9 +3,9 @@
 Plot multi-class SGD on the iris dataset
 ========================================
 
-Plot decision surface of SGD one-against-all (OVA) classifier.
-The hyperplanes corresponding to the three OVA classifiers
-are represented by the dashed lines. 
+Plot decision surface of multi-class SGD on iris dataset.
+The hyperplanes corresponding to the three one-versus-all (OVA) classifiers
+are represented by the dashed lines.
 
 """
 print __doc__
@@ -16,9 +16,10 @@ from scikits.learn import sgd, datasets
 
 # import some data to play with
 iris = datasets.load_iris()
-X = iris.data[:, :2] # we only take the first two features. We could
-                     # avoid this ugly slicing by using a two-dim dataset
+X = iris.data[:, :2]  # we only take the first two features. We could
+                      # avoid this ugly slicing by using a two-dim dataset
 y = iris.target
+colors = "bry"
 
 # shuffle
 idx = np.arange(X.shape[0])
@@ -30,15 +31,15 @@ y = y[idx]
 # standardize
 mean = X.mean(axis=0)
 std = X.std(axis=0)
-X = (X-mean) / std
+X = (X - mean) / std
 
-h=.02 # step size in the mesh
+h = .02  # step size in the mesh
 
-clf = sgd.SGD(alpha=0.01, n_iter=100).fit(X, y)
+clf = sgd.SGD(alpha=0.001, n_iter=100).fit(X, y)
 
 # create a mesh to plot in
-x_min, x_max = X[:,0].min()-1, X[:,0].max()+1
-y_min, y_max = X[:,1].min()-1, X[:,1].max()+1
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
                      np.arange(y_min, y_max, h))
 
@@ -54,7 +55,9 @@ cs = pl.contourf(xx, yy, Z)
 pl.axis('tight')
 
 # Plot also the training points
-pl.scatter(X[:,0], X[:,1], c=y)
+for i, color in zip(clf.classes, colors):
+    idx = np.where(y == i)
+    pl.scatter(X[idx, 0], X[idx, 1], c=color, label=iris.target_names[i])
 pl.title("Decision surface of multi-class SGD")
 pl.axis('tight')
 
@@ -64,13 +67,15 @@ ymin, ymax = pl.ylim()
 coef = clf.coef_
 intercept = clf.intercept_
 
-def plot_hyperplane(i):
-    def line(x1):
-        return (-(x1 * coef[i, 1]) - intercept[i]) / coef[i, 0]
+
+def plot_hyperplane(c, color):
+    def line(x0):
+        return (-(x0 * coef[c, 0]) - intercept[c]) / coef[c, 1]
+
     pl.plot([xmin, xmax], [line(xmin), line(xmax)],
-            ls="--", color=pl.cm.Paired(clf.classes[i]))
+            ls="--", color=color)
 
-for i in range(3):
-    plot_hyperplane(i)
-
+for i, color in zip(clf.classes, colors):
+    plot_hyperplane(i, color)
+pl.legend()
 pl.show()
