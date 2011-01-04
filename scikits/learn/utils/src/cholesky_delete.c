7befcf0bc38085c03108e091bf6fe75ddcae73ce
Kamel Ibn Hassen Derouiche <kamel.derouiche@gmail.com>
2011-01-04 12:50:08 +0100

    FIX: compilation issues under NetBSD
    Signed-off-by: Fabian Pedregosa <fabian.pedregosa@inria.fr>

--- scikits/learn/utils/src/cholesky_delete.c
+++ scikits/learn/utils/src/cholesky_delete.c
@@ -18,27 +18,27 @@ int double_cholesky_delete (int m, int n, double *L, int go_out) {
     double c, s;
 
     /* delete row go_out */
-    double * _L = L + (go_out * m);
+    double  *L1 = L + (go_out * m);
     int i;
     for (i = go_out; i < n - 1; ++i) {
-        cblas_dcopy (i + 2, _L + m , 1, _L,  1);
-        _L += m;
+        cblas_dcopy (i + 2, L1 + m , 1, L1,  1);
+        L1 += m;
     }
 
-    _L = L + (go_out * m);
+    L1 = L + (go_out * m);
     for (i=go_out; i < n - 1; ++i) {
 
-        cblas_drotg (_L + i, _L + i + 1, &c, &s);
-        if (_L[i] < 0) {
+        cblas_drotg (L1 + i, L1 + i + 1, &c, &s);
+        if (L1[i] < 0) {
             /* Diagonals cannot be negative */
-            _L[i] = copysign(_L[i], 1.0);
+            L1[i] = copysign(L1[i], 1.0);
             c = -c;
             s = -s;
         }
-        _L[i+1] = 0.; /* just for cleanup */
-        _L += m;
+        L1[i+1] = 0.; /* just for cleanup */
+        L1 += m;
 
-        cblas_drot (n - (i + 2), _L + i, m, _L + i + 1,
+        cblas_drot (n - (i + 2), L1 + i, m, L1 + i + 1,
                     m, c, s);
 
     }
