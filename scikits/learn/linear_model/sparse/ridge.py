776d0b791bdfa87c1822fcbd45af0eebadb72748
Mathieu Blondel <mathieu@mblondel.org>
2011-01-18 23:43:03 +0900

    Support 2-d Y.

--- scikits/learn/linear_model/sparse/ridge.py
+++ scikits/learn/linear_model/sparse/ridge.py
@@ -1,40 +1,49 @@
 
 import numpy as np
 import scipy.sparse as sp
-from scipy.sparse import linalg
+from scipy import linalg
+from scipy.sparse import linalg as sp_linalg
 
 from ..base import LinearModel
 
 class Ridge(LinearModel):
 
-    def __init__(self, alpha=1.0, fit_intercept=False):
+    def __init__(self, alpha=1.0, fit_intercept=False, solver="default"):
         self.alpha = alpha
         self.intercept_ = 0
+        self.solver = solver
 
-    def _check_error(self, error):
-        if error:
-            raise ValueError, "Failed with error code %d" % error
+    def _solve(self, A, b):
+        if self.solver == "cg":
+            # this solver cannot handle a 2-d b.
+            sol, error = sp_linalg.cg(A, b)
+            if error:
+                raise ValueError, "Failed with error code %d" % error
+            return sol
+        else:
+            return linalg.solve(A.todense(), b, sym_pos=True)
 
     def fit(self, X, y):
+        X = sp.csr_matrix(X)
         n_samples, n_features = X.shape
 
-        y = np.array(y, dtype=np.int32)
+        y = np.array(y)
 
         if n_samples > n_features:
             I = sp.lil_matrix((n_features, n_features))
             I.setdiag(np.ones(n_features) * self.alpha)
-            self.coef_, error = linalg.cg(X.T * X + I, X.T * y)
-            self._check_error(error)
+            self.coef_ = self._solve(X.T * X + I, X.T * y)
         else:
             I = sp.lil_matrix((n_samples, n_samples))
             I.setdiag(np.ones(n_samples) * self.alpha)
-            sol, error = linalg.cg(X * X.T + I, y)
-            self._check_error(error)
-            self.coef_ = X.T * sol
+            c = self._solve(X * X.T + I, y)
+            self.coef_ = X.T * c
 
         # FIXME: handle fit_intercept
 
         return self
 
     def predict(self, X):
+        X = sp.csr_matrix(X)
         return X * self.coef_ + self.intercept_
+
--- scikits/learn/linear_model/sparse/tests/test_ridge.py
+++ scikits/learn/linear_model/sparse/tests/test_ridge.py
@@ -1,23 +1,37 @@
 
 import numpy as np
 import scipy.sparse as sp
-from numpy.testing import assert_almost_equal
+from numpy.testing import assert_almost_equal, assert_array_almost_equal
 
 from scikits.learn import datasets
 from scikits.learn.linear_model.ridge import Ridge
-from scikits.learn.linear_model.sparse.ridge import Ridge as SparseRidge
+from scikits.learn.linear_model.sparse.ridge import Ridge as SpRidge
 
 diabetes = datasets.load_diabetes()
 
-def test_diabetes():
+def test_ridge_diabetes():
     X, y = diabetes.data, diabetes.target
-    X_sp = sp.csr_matrix(diabetes.data)
 
     #for fi in (True, False):
     for fi in (False,):
         ridge = Ridge(fit_intercept=fi)
-        ridge_sp = SparseRidge(fit_intercept=fi)
         ridge.fit(X, y)
-        ridge_sp.fit(X_sp, y)
-        assert_almost_equal(ridge.score(X, y), ridge_sp.score(X_sp, y))
+        for solver in ("cg", "default"):
+            ridge_sp = SpRidge(fit_intercept=fi, solver=solver)
+            ridge_sp.fit(X, y)
+            assert_almost_equal(ridge.score(X, y), ridge_sp.score(X, y))
+
+def test_multi_ridge_diabetes():
+    X, y = diabetes.data, diabetes.target
+
+    # simulate several responses
+    Y = np.vstack((y,y)).T
+
+    ridge = SpRidge(fit_intercept=False)
+    ridge.fit(X, Y)
+    Y_pred = ridge.predict(X)
+    ridge.fit(X, y)
+    y_pred = ridge.predict(X)
+    assert_array_almost_equal(np.vstack((y_pred,y_pred)).T,
+                              Y_pred)
 
