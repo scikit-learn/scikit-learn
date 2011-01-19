5ec3d6f14e4e8e35ff7fab9511121aec78a844cc
Mathieu Blondel <mathieu@mblondel.org>
2011-01-19 17:12:21 +0900

    Add efficient LOO cross-val for Ridge.

--- scikits/learn/linear_model/ridge.py
+++ scikits/learn/linear_model/ridge.py
@@ -78,3 +78,88 @@ class Ridge(LinearModel):
 
         self._set_intercept(Xmean, ymean)
         return self
+
+class RidgeLOO(LinearModel):
+    """Ridge regression with built-in efficient
+       Leave-One-Out cross-validation
+
+    We want to solve (K + alpha*Id)c = y,
+    where K = X X^T is the kernel matrix.
+
+    Let G = (K + alpha*Id)^-1.
+
+    Dual solution: c = Gy
+    Primal solution: w = X^T c
+
+    Compute eigendecomposition K = Q V Q^T.
+    Then G = Q (V + alpha*Id)^-1 Q^T,
+    where (V + alpha*Id) is diagonal.
+    It is thus inexpensive to inverse for many alphas.
+
+    Let loov be the vector of prediction values for each example
+    when the model was fitted with all examples but this example.
+
+    loov = (KGY - diag(KG)Y) / diag(I-KG)
+
+    Let looe be the vector of prediction errors for each example
+    when the model was fitted with all examples but this example.
+
+    looe = c / diag(G)
+    """
+
+    def __init__(self, alphas=np.array([0.1, 1.0, 10.0]), fit_intercept=False):
+        self.alphas = alphas
+        self.fit_intercept = fit_intercept
+        self.intercept_ = 0
+
+    def _pre_compute(self, X, y):
+        K = np.dot(X, X.T)
+        v, Q = linalg.eigh(K)
+        return K, v, Q
+
+    def _errors(self, v, Q, y, alpha):
+        G = np.dot(np.dot(Q, np.diag(1.0 / (v + alpha))), Q.T)
+        c = np.dot(G, y)
+        G_diag = np.diag(G)
+        # handle case when y is 2-d
+        G_diag = G_diag if len(y.shape) == 1 else G_diag[:,np.newaxis]
+        return c / G_diag, c
+
+    def _values(self, K, v, Q, y, alpha):
+        n_samples = y.shape[0]
+
+        G = np.dot(np.dot(Q, np.diag(1.0 / (v + alpha))), Q.T)
+        c = np.dot(G, y)
+        KG = np.dot(K, G)
+        #KG = np.dot(np.dot(Q, np.diag(v / (v + alpha))), Q.T)
+        KG_diag = np.diag(KG)
+
+        denom = np.ones(n_samples) - KG_diag
+        if len(y.shape) == 2:
+            # handle case when y is 2-d
+            KG_diag = KG_diag[:,np.newaxis]
+            denom = denom[:,np.newaxis]
+
+        num = np.dot(KG, y) - KG_diag * y
+
+        return num / denom, c
+
+    def fit(self, X, y):
+        n_samples = X.shape[0]
+        K, v, Q = self._pre_compute(X, y)
+        M = np.zeros((n_samples*len(y.shape), len(self.alphas)))
+        C = []
+
+        for i, alpha in enumerate(self.alphas):
+            out, c = self._errors(v, Q, y, alpha)
+            M[:,i] = out.ravel()
+            C.append(c)
+
+        best = M.mean(axis=0).argmin()
+        self.best_alpha = self.alphas[best]
+        self.dual_coef_ = C[best]
+        self.coef_ = np.dot(X.T, self.dual_coef_)
+
+        return self
+
+
--- scikits/learn/linear_model/tests/test_ridge.py
+++ scikits/learn/linear_model/tests/test_ridge.py
@@ -1,10 +1,16 @@
 import numpy as np
 
-from numpy.testing import assert_almost_equal
+from numpy.testing import assert_almost_equal, assert_array_almost_equal, \
+                          assert_equal
 
 from ..ridge import Ridge
 from ..base import LinearRegression
 
+from scikits.learn import datasets
+from scikits.learn.linear_model.ridge import RidgeLOO
+from scikits.learn.metrics import mean_square_error
+
+diabetes = datasets.load_diabetes()
 
 def test_ridge():
     """Ridge regression convergence test using score
@@ -67,4 +73,43 @@ def test_ridge_vs_lstsq():
     ols.fit (X, y, fit_intercept=False)
     assert_almost_equal(ridge.coef_, ols.coef_)
 
+def test_ridge_loo():
+    X, y = diabetes.data, diabetes.target
+    n_samples = X.shape[0]
+    ridge_loo = RidgeLOO(fit_intercept=False)
+    ridge = Ridge(fit_intercept=False)
+
+    # efficient LOO
+    K, v, Q = ridge_loo._pre_compute(X, y)
+    errors, c = ridge_loo._errors(v, Q, y, 1.0)
+    values, c = ridge_loo._values(K, v, Q, y, 1.0)
+
+    # brute-force LOO
+    errors2 = []
+    values2 = []
+    for i in range(n_samples):
+        sel = np.arange(n_samples) != i
+        X_new = X[sel]
+        y_new = y[sel]
+        ridge.fit(X_new, y_new)
+        value = ridge.predict([X[i]])[0]
+        error = y[i] - value
+        errors2.append(error)
+        values2.append(value)
+
+    assert_almost_equal(errors, errors2)
+    assert_almost_equal(values, values2)
+
+    ridge_loo.fit(X, y)
+    assert_equal(ridge_loo.best_alpha, 10.0)
+
+    # simulate several responses
+    Y = np.vstack((y,y)).T
+
+    ridge_loo.fit(X, Y)
+    Y_pred = ridge_loo.predict(X)
+    ridge_loo.fit(X, y)
+    y_pred = ridge_loo.predict(X)
+    assert_array_almost_equal(np.vstack((y_pred,y_pred)).T,
+                              Y_pred)
 
