951b5f126ff77e44fc2165290fa1a94beff0acb4
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2011-01-25 11:40:42 -0500

    removing plot_dendogram function

--- scikits/learn/cluster/__init__.py
+++ scikits/learn/cluster/__init__.py
@@ -5,5 +5,5 @@ from .spectral import spectral_clustering, SpectralClustering
 from .mean_shift_ import mean_shift, MeanShift, estimate_bandwidth
 from .affinity_propagation_ import affinity_propagation, AffinityPropagation
 from .k_means_ import k_means, KMeans
-from .hierarchical import ward_tree, Ward, WardAgglomeration, plot_dendrogram
+from .hierarchical import ward_tree, Ward, WardAgglomeration
 
