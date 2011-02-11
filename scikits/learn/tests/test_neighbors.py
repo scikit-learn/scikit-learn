94aeef7c835f7d521889d0c9ee78a24e7e2d429f
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2011-02-11 08:39:26 +0100

    remove unused imports.

--- scikits/learn/tests/test_neighbors.py
+++ scikits/learn/tests/test_neighbors.py
@@ -1,6 +1,5 @@
 import numpy as np
-from numpy.testing import assert_array_equal, assert_array_almost_equal, \
-                          assert_equal
+from numpy.testing import assert_array_almost_equal, assert_array_equal
 
 from scikits.learn import neighbors
 
