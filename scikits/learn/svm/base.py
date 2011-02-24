62bcf5b6ee76951bbee1dcfbe0e2acef72fa29c9
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2011-02-24 19:57:19 +0100

    Cosmetic changes in decision_function.

--- scikits/learn/svm/base.py
+++ scikits/learn/svm/base.py
@@ -425,14 +425,10 @@ class BaseLibLinear(BaseEstimator):
         X = np.atleast_2d(np.asanyarray(X, dtype=np.float64, order='C'))
         self._check_n_features(X)
 
-        coef = self.raw_coef_
-
-        dec_func = _liblinear.decision_function_wrap(X, coef,
-                                      self._get_solver_type(),
-                                      self.eps, self.C,
-                                      self.class_weight_label,
-                                      self.class_weight, self.label_,
-                                      self._get_bias())
+        dec_func = _liblinear.decision_function_wrap(
+            X, self.raw_coef_, self._get_solver_type(), self.eps,
+            self.C, self.class_weight_label, self.class_weight,
+            self.label_, self._get_bias())
 
         if len(self.label_) <= 2:
             # in the two-class case, the decision sign needs be flipped
