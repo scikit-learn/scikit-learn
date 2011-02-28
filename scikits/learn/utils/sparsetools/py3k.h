edfec4c39611e7aaebff9eab03846ee0ee1e912d
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2011-02-28 09:34:28 +0100

    Python 3 compatibility.
    Simplified version of scipy strategy: copy files into build/py3k, and
    patch them using lib2to3.

---
+++ scikits/learn/utils/sparsetools/py3k.h
@@ -0,0 +1,23 @@
+/*
+ * Undefine macros defined by SWIG
+ */
+#if PY_VERSION_HEX >= 0x03000000
+
+#ifdef PyInt_Check
+#undef PyInt_Check
+#endif
+
+static int __pyfile_check_guard(PyObject *x)
+{
+    return 0;
+}
+#define PyFile_Check(x) __pyfile_check_guard((x))
+static int __pyinstance_check_guard(PyObject *x)
+{
+    return 0;
+}
+#define PyInstance_Check(x) __pyinstance_check_guard((x))
+
+#endif
+
+#include "numpy/npy_3kcompat.h"
