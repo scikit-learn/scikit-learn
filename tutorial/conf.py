d3047a19b6fa7a6863ef17a60f017a0692e728f2
Olivier Grisel <olivier.grisel@ensta.org>
2011-03-08 02:09:55 +0100

    useless conf

--- tutorial/conf.py
+++ tutorial/conf.py
@@ -13,13 +13,6 @@
 
 import sys, os
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('sphinxext'))
-
-# -- General configuration -----------------------------------------------------
-
 # The suffix of source filenames.
 source_suffix = '.rst'
 
