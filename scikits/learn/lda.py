d47d236624880635018fd605f977ba94f0429d2a
Mathieu Blondel <mathieu@mblondel.org>
2011-04-04 15:04:37 +0900

    Fix doctest.

--- scikits/learn/lda.py
+++ scikits/learn/lda.py
@@ -44,7 +44,7 @@ class LDA(BaseEstimator, ClassifierMixin):
     >>> y = np.array([1, 1, 1, 2, 2, 2])
     >>> clf = LDA()
     >>> clf.fit(X, y)
-    LDA(priors=None)
+    LDA(priors=None, n_components=None)
     >>> print clf.predict([[-0.8, -1]])
     [1]
 
