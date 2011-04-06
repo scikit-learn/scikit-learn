a480d43011536f167b0277ef3a1e1cce6e726c8c
Noel Dawe <Noel.Dawe@cern.ch>
2011-04-06 11:33:44 -0700

    sanity check in adaboost

--- scikits/learn/ensemble/boosting/adaboost.py
+++ scikits/learn/ensemble/boosting/adaboost.py
@@ -14,22 +14,32 @@ class AdaBoost(BaseEnsemble):
         I am making the assumption that one class label is positive and the other is negative
         """
         if len(sample_weight) == 0:
-            sample_weight = np.ones(X.shape[0], dtype = np.float64)
+            # initialize weights to 1/N
+            sample_weight = np.ones(X.shape[0], dtype = np.float64) / X.shape[0]
         else:
             sample_weight = np.copy(sample_weight)
         # remove any previous ensemble
         self[:] = []
-        for boost in xrange(boosts+1):
+        for i, boost in enumerate(xrange(boosts+1)):
             estimator = self.estimator(**self.params)
             estimator.fit(X,Y,sample_weight,**params)
             # TODO request that classifiers return classification of training sets when fitting
             # which would make the following line unnecessary 
             T = estimator.predict(X)
+            # instances incorrectly classified
             incorrect = (T*Y)<0
+            # error fraction
             err = np.sum(sample_weight * incorrect) / np.sum(sample_weight)
+            # boost weight
             alpha = math.log((1 - err) / err)
-            sample_weight *= np.exp(alpha * incorrect)
+            # sanity check
+            if err >= 0.5:
+                if i == 0:
+                    self.append((1., estimator))
+                break
             self.append((alpha, estimator))
+            if i < boosts:
+                sample_weight *= np.exp(alpha * incorrect)
         return self
 
     def predict(self, X):
--- testbdt.py
+++ testbdt.py
@@ -8,10 +8,10 @@ a = AdaBoost(DecisionTree, minleafsize=100, maxdepth = 1)
 
 cov = [[1,0],[0,1]]
 X_bkg = np.reshape(np.random.multivariate_normal([-1,-1], cov, 100000), (100000,-1))
-X_sig = np.reshape(np.random.multivariate_normal([1,1], cov, 100000), (100000, -1))
+X_sig = np.reshape(np.random.multivariate_normal([100,100], cov, 100000), (100000, -1))
 
 x = np.concatenate([X_sig, X_bkg])
-y = np.append(np.ones(100000),-2*np.ones(100000))
+y = np.append(np.ones(100000),-np.ones(100000))
 
 a.fit(x, y, boosts = 100)
 
