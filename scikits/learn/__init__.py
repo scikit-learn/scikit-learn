212c37b00cb9843653894056686894ff3a072eb0
Satrajit Ghosh <satra@mit.edu>
2011-04-07 21:27:06 -0400

    resolved init

--- scikits/learn/__init__.py
+++ scikits/learn/__init__.py
@@ -39,6 +39,6 @@ __all__ = ['cross_val', 'ball_tree', 'cluster', 'covariance', 'datasets',
            'decomposition', 'feature_extraction', 'feature_selection',
            'gaussian_process', 'grid_search', 'hmm', 'lda', 'linear_model',
            'metrics', 'mixture', 'naive_bayes', 'neighbors', 'pipeline', 
-           'preprocessing', 'qda', 'svm', 'test', 'clone', 'pls']
+           'preprocessing', 'qda', 'svm', 'test', 'clone', 'pls', 'tree_model']
 
 __version__ = '0.8.git'
