b6c89eec4fd1ca8ebede7147f6a71fe24e6d8d06
Noel Dawe <Noel.Dawe@cern.ch>
2011-04-08 00:20:22 -0700

    better demonstration in test module

--- scikits/learn/decisiontree/tests/test_decisiontree.py
+++ scikits/learn/decisiontree/tests/test_decisiontree.py
@@ -9,10 +9,9 @@ from scikits.learn.ensemble.bagging import Bagged
 import numpy as np
 import matplotlib.pyplot as plt
 
-#a = AdaBoost(DecisionTree, minleafsize=100, maxdepth = 0, nbins=100)
-a = Bagged(DecisionTree, minleafsize=100, maxdepth = 0, nbins=100)
-
-#a = DecisionTree(minleafsize=100, maxdepth=1, nbins=100)
+boosted = AdaBoost(DecisionTree, minleafsize=100, nbins = 100)
+bagged = Bagged(DecisionTree, minleafsize=100, nbins = 100)
+# use maxdepth=1 for decision stumps
 
 nsample = 10000
 #cov = [[.5,0,0],[0,.5,0],[0,0,0.5]]
@@ -24,11 +23,14 @@ X_sig = np.reshape(np.random.multivariate_normal([-1.5,0], cov, nsample), (nsamp
 x = np.concatenate([X_sig, X_bkg])
 y = np.append(np.ones(nsample),-np.ones(nsample))
 
-a.fit(x, y, baggs=10, sample_fraction=.8)
+boosted.fit(x, y, boosts = 20, beta = .2)
+bagged.fit(x, y, baggs = 20, sample_fraction = .6)
 
 plt.figure()
-plt.hist(a.predict(X_bkg), bins=20, range=(-1,1), label="Background")
-plt.hist(a.predict(X_sig), bins=20, range=(-1,1), label="Signal", alpha=.5)
+plt.hist(boosted.predict(X_bkg), bins=20, range=(-1,1), label="Boosted Background")
+plt.hist(boosted.predict(X_sig), bins=20, range=(-1,1), label="Boosted Signal", alpha=.5)
+plt.hist(bagged.predict(X_bkg), bins=20, range=(-1,1), label="Bagged Background", alpha=.5)
+plt.hist(bagged.predict(X_sig), bins=20, range=(-1,1), label="Bagged Signal", alpha=.5)
 l = plt.legend()
 l.legendPatch.set_alpha(0.5)
 
