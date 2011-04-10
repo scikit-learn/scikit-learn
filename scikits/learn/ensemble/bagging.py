f11ee125e30787e39e2c9928fb7cd2d40b4c51c2
Noel Dawe <Noel.Dawe@cern.ch>
2011-04-09 20:04:30 -0700

    pep8

--- scikits/learn/ensemble/bagging.py
+++ scikits/learn/ensemble/bagging.py
@@ -1,9 +1,11 @@
 from .base import BaseEnsemble
 import numpy as np
 
+
 class Bagged(BaseEnsemble):
 
-    def fit(self, X, Y, sample_weight = [], sample_fraction = .5, baggs = 10, **params):
+    def fit(self, X, Y, sample_weight=[],
+                        sample_fraction=.5, baggs=10, **params):
         """
         X: list of instance vectors
         Y: target values/classes
@@ -11,13 +13,14 @@ class Bagged(BaseEnsemble):
         baggs: number of sampling/training iterations
         """
         if not 0 < sample_fraction < 1:
-            raise ValueError("You must specify sample_fraction between 0 and 1 (exclusive)")
+            raise ValueError(
+                "You must specify sample_fraction between 0 and 1 (exclusive)")
         if baggs < 2:
             raise ValueError("baggs must be greater than 1")
-        
+
         if len(sample_weight) == 0:
             # initialize weights to 1/N
-            sample_weight = np.ones(X.shape[0], dtype = np.float64)
+            sample_weight = np.ones(X.shape[0], dtype=np.float64)
         else:
             sample_weight = np.copy(sample_weight)
         # remove any previous ensemble
@@ -25,19 +28,19 @@ class Bagged(BaseEnsemble):
         for bagg in xrange(baggs):
             # weight a random subsample with 0
             random_sample_weight = sample_weight * \
-                (np.random.random_sample(sample_weight.shape[0])<sample_fraction)
+                (np.random.random_sample(sample_weight.shape[0]) \
+                    < sample_fraction)
             estimator = self.estimator(**self.params)
-            estimator.fit(X,Y,random_sample_weight,**params)
+            estimator.fit(X, Y, random_sample_weight, **params)
             self.append(estimator)
         return self
 
     def predict(self, X):
-        
+
         if len(self) == 0:
             return None
-        prediction = np.zeros(X.shape[0], dtype = np.float64)
+        prediction = np.zeros(X.shape[0], dtype=np.float64)
         for estimator in self:
             prediction += estimator.predict(X)
         prediction /= len(self)
         return prediction
-
