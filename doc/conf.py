9a0eceaa9e7b60d46697601930045925b6bae614
Gael varoquaux <gael.varoquaux@normalesup.org>
2011-04-10 17:19:30 +0200

    DOC: Fix more warnings

--- doc/conf.py
+++ doc/conf.py
@@ -136,7 +136,7 @@ html_favicon = 'logos/favicon.ico'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static', 'images']
+html_static_path = ['images']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
