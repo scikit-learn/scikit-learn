2183c4e207e5ca72fb180de6991d9ab5d95ef2b0
Paolo Losi <paolo.losi@gmail.com>
2011-04-17 22:51:59 +0200

    cleanup tests

--- scikits/learn/svm/tests/test_bounds.py
+++ scikits/learn/svm/tests/test_bounds.py
@@ -1,15 +1,12 @@
 import numpy as np
 from scipy import sparse as sp
 
-from numpy.testing import assert_array_equal, \
-     assert_array_almost_equal, assert_almost_equal
-
 from scikits.learn.svm.bounds import l1_min_c
 from scikits.learn.svm import LinearSVC
 from scikits.learn.linear_model.logistic import LogisticRegression
-from scikits.learn.svm.sparse import LinearSVC as SparseLinearSVC
+from scikits.learn.svm.sparse import LinearSVC as SparseSVC
 from scikits.learn.linear_model.sparse.logistic import LogisticRegression as \
-                                                       SparseLogisticRegression
+                                                       SparseLogRegression
 
 
 dense_X = [[-1, 0], [0, 1], [1, 1], [1, 1]]
@@ -31,29 +28,27 @@ def test_l1_min_c():
         for X_label, X in Xs.items():
             for Y_label, Y in Ys.items():
                 for intercept_label, intercept_params in intercepts.items():
-                    desc = 'l1_min_c loss=%r %s %s %s' % \
-                                      (loss, X_label, Y_label, intercept_label)
-                    check = lambda X, Y, loss, intercept_params: \
-                                   check_l1_min_c(X, Y, loss, intercept_params)
+                    desc = 'Test l1_min_c loss=%r %s %s %s' % \
+                           (loss, X_label, Y_label, intercept_label)
+                    check = lambda X, Y, loss, intercept_ps: \
+                                   check_l1_min_c(X, Y, loss, **intercept_ps)
                     check.description = desc
                     yield check, X, Y, loss, intercept_params
 
 
-def check_l1_min_c(X, y, loss, intercept_params):
-    min_c = l1_min_c(X, y, loss, **intercept_params)
+def check_l1_min_c(X, y, loss, fit_intercept=True, intercept_scaling=None):
+    min_c = l1_min_c(X, y, loss, fit_intercept, intercept_scaling)
 
-    if loss == 'l2':
-        if sp.issparse(X):
-            algo = SparseLinearSVC
-        else:
-            algo = LinearSVC
-        clf = algo(loss='l2', penalty='l1', dual=False, **intercept_params)
-    else:
-        if sp.issparse(X):
-            algo = SparseLogisticRegression
-        else:
-            algo = LogisticRegression
-        clf = algo(penalty='l1', **intercept_params)
+    clf = {
+        ('log', False): LogisticRegression(penalty='l1'),
+        ('log', True):  SparseLogRegression(penalty='l1'),
+        ('l2', False):  LinearSVC(loss='l2', penalty='l1', dual=False),
+        ('l2', True):   SparseSVC(loss='l2', penalty='l1', dual=False),
+    }[loss, sp.issparse(X)]
+
+    clf.fit_intercept = fit_intercept
+    if clf.intercept_scaling is not None:
+        clf.intercept_scaling = intercept_scaling
 
     clf.C = min_c
     clf.fit(X, y)
