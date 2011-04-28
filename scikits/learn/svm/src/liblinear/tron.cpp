86a6c291888992a1e3b2a6f0ada7bbb757d2deb0
Lars Buitinck <L.J.Buitinck@uva.nl>
2011-04-28 16:05:00 +0200

    Minor cleanup liblinear interface code
    Now uses some more C++ standard library facilities.

--- scikits/learn/svm/src/liblinear/tron.cpp
+++ scikits/learn/svm/src/liblinear/tron.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
@@ -5,26 +6,14 @@
 #include <vector>
 #include "tron.h"
 
-#ifndef min
-template <class T> static inline T min(T x,T y) { return (x<y)?x:y; }
-#endif
-
-#ifndef max
-template <class T> static inline T max(T x,T y) { return (x>y)?x:y; }
-#endif
-
-#ifdef __cplusplus
 extern "C" {
-#endif
 
 extern double dnrm2_(int *, double *, int *);
 extern double ddot_(int *, double *, int *, double *, int *);
 extern int daxpy_(int *, double *, double *, int *, double *, int *);
 extern int dscal_(int *, double *, double *, int *);
 
-#ifdef __cplusplus
 }
-#endif
 
 static void default_print(const char *buf)
 {
@@ -95,7 +84,7 @@ void TRON::tron(double *w)
 	{
 		cg_iter = trcg(delta, g, s, r);
 
-		memcpy(w_new, w, sizeof(double)*n);
+		std::copy(w, w+n, w_new);
 		daxpy_(&n, &one, s, &inc, w_new, &inc);
 
 		gs = ddot_(&n, g, &inc, s, &inc);
@@ -108,23 +97,23 @@ void TRON::tron(double *w)
 		// On the first iteration, adjust the initial step bound.
 		snorm = dnrm2_(&n, s, &inc);
 		if (iter == 1)
-			delta = min(delta, snorm);
+			delta = std::min(delta, snorm);
 
 		// Compute prediction alpha*snorm of the step.
 		if (fnew - f - gs <= 0)
 			alpha = sigma3;
 		else
-			alpha = max(sigma1, -0.5*(gs/(fnew - f - gs)));
+			alpha = std::max(sigma1, -0.5*(gs/(fnew - f - gs)));
 
 		// Update the trust region bound according to the ratio of actual to predicted reduction.
 		if (actred < eta0*prered)
-			delta = min(max(alpha, sigma1)*snorm, sigma2*delta);
+			delta = std::min(std::max(alpha, sigma1)*snorm, sigma2*delta);
 		else if (actred < eta1*prered)
-			delta = max(sigma1*delta, min(alpha*snorm, sigma2*delta));
+			delta = std::max(sigma1*delta, std::min(alpha*snorm, sigma2*delta));
 		else if (actred < eta2*prered)
-			delta = max(sigma1*delta, min(alpha*snorm, sigma3*delta));
+			delta = std::max(sigma1*delta, std::min(alpha*snorm, sigma3*delta));
 		else
-			delta = max(delta, min(alpha*snorm, sigma3*delta));
+			delta = std::max(delta, std::min(alpha*snorm, sigma3*delta));
 
 		info("iter %2d act %5.3e pre %5.3e delta %5.3e f %5.3e |g| %5.3e CG %3d\n", iter, actred, prered, delta, f, gnorm, cg_iter);
 
