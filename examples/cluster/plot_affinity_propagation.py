1d66d0b8b91592a0bc527baf8e2ef2ed34ed11e0
Olivier Grisel <olivier.grisel@ensta.org>
2011-05-08 10:43:14 +0200

    illustrate clustering metrics on affinity propagation example

--- examples/cluster/plot_affinity_propagation.py
+++ examples/cluster/plot_affinity_propagation.py
@@ -12,45 +12,59 @@ print __doc__
 
 import numpy as np
 from scikits.learn.cluster import AffinityPropagation
+from scikits.learn.metrics import v_measure_score
+from scikits.learn.metrics import homogeneity_score
+from scikits.learn.metrics import completeness_score
 
-################################################################################
+##############################################################################
 # Generate sample data
-################################################################################
+##############################################################################
 np.random.seed(0)
 
-n_points_per_cluster = 100
-n_clusters = 3
-n_points = n_points_per_cluster*n_clusters
-means = np.array([[1,1],[-1,-1],[1,-1]])
+means = (
+    [1, 1],
+    [-1, -1],
+    [1, -1]
+)
 std = .5
+n_clusters = len(means)
+n_samples_per_cluster = 100
 
-X = np.empty((0, 2))
-for i in range(n_clusters):
-    X = np.r_[X, means[i] + std * np.random.randn(n_points_per_cluster, 2)]
+clusters = []
+labels_true = []
+for i, mean in enumerate(means):
+    clusters.append(np.random.normal(
+        loc=mean, scale=std, size=(n_samples_per_cluster, 2)))
+    labels_true += [i] * n_samples_per_cluster
 
-################################################################################
+X = np.concatenate(clusters)
+
+##############################################################################
 # Compute similarities
-################################################################################
-X_norms = np.sum(X*X, axis=1)
-S = - X_norms[:,np.newaxis] - X_norms[np.newaxis,:] + 2 * np.dot(X, X.T)
-p = 10*np.median(S)
+##############################################################################
+X_norms = np.sum(X * X, axis=1)
+S = - X_norms[:, np.newaxis] - X_norms[np.newaxis, :] + 2 * np.dot(X, X.T)
+p = 10 * np.median(S)
 
-################################################################################
+##############################################################################
 # Compute Affinity Propagation
-################################################################################
+##############################################################################
 
-af = AffinityPropagation()
-af.fit(S, p)
+af = AffinityPropagation().fit(S, p)
 cluster_centers_indices = af.cluster_centers_indices_
 labels = af.labels_
 
 n_clusters_ = len(cluster_centers_indices)
 
 print 'Estimated number of clusters: %d' % n_clusters_
+print "Homogeneity: %0.3f" % homogeneity_score(labels_true, labels)
+print "Completeness: %0.3f" % completeness_score(labels_true, labels)
+print "V-measure: %0.3f" % v_measure_score(labels_true, labels)
 
-################################################################################
+
+##############################################################################
 # Plot result
-################################################################################
+##############################################################################
 
 import pylab as pl
 from itertools import cycle
@@ -63,12 +77,11 @@ colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
 for k, col in zip(range(n_clusters_), colors):
     class_members = labels == k
     cluster_center = X[cluster_centers_indices[k]]
-    pl.plot(X[class_members,0], X[class_members,1], col+'.')
+    pl.plot(X[class_members, 0], X[class_members, 1], col + '.')
     pl.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
-                                    markeredgecolor='k', markersize=14)
+            markeredgecolor='k', markersize=14)
     for x in X[class_members]:
         pl.plot([cluster_center[0], x[0]], [cluster_center[1], x[1]], col)
 
 pl.title('Estimated number of clusters: %d' % n_clusters_)
 pl.show()
-
