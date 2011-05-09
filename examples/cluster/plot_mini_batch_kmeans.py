a3521a5b843b675e29c38ac87371f02064f4075b
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2011-05-09 23:00:11 +0100

    [MiniBatchKMeans] Added an example

--- examples/cluster/plot_mini_batch_kmeans.py
+++ examples/cluster/plot_mini_batch_kmeans.py
@@ -1,13 +1,13 @@
 """
 =====================================================
-A demo of the MiniBatch K Means clustering algorithm
+A demo of the K Means clustering algorithm
 =====================================================
 
 """
 print __doc__
 
 import numpy as np
-from scikits.learn.cluster import MiniBatchKMeans
+from scikits.learn.cluster import MiniBatchKMeans, KMeans
 
 ##############################################################################
 # Generate sample data
@@ -18,7 +18,6 @@ n_clusters = 3
 n_points = n_points_per_cluster * n_clusters
 means = np.array([[1, 1], [-1, -1], [1, -1]])
 std = .6
-iterations = 100
 batch_size = 50
 
 X = np.empty((0, 2))
@@ -26,31 +25,63 @@ for i in range(n_clusters):
     X = np.r_[X, means[i] + std * np.random.randn(n_points_per_cluster, 2)]
 
 ##############################################################################
+# Compute clustering with Means
+k_means = KMeans(init='k-means++',
+                 k=3)
+k_means.fit(X)
+k_means_labels = k_means.labels_
+k_means_cluster_centers = k_means.cluster_centers_
+k_means_labels_unique = np.unique(k_means_labels)
+
+
+##############################################################################
 # Compute clustering with MiniBatchKMeans
 mbk = MiniBatchKMeans(init='k-means++',
                       k=3,
                       chunk_size=batch_size)
 mbk.fit(X)
-labels = mbk.labels_
-cluster_centers = mbk.cluster_centers_
-
-labels_unique = np.unique(labels)
-n_clusters_ = len(labels_unique)
+mbk_means_labels = mbk.labels_
+mbk_means_cluster_centers = mbk.cluster_centers_
+mbk_means_labels_unique = np.unique(mbk_means_labels)
 
 ##############################################################################
 # Plot result
-import pylab as pl
+import matplotlib.pyplot as plt
 from itertools import cycle
 
-pl.figure(1)
-pl.clf()
+fig = plt.figure()
+colors = cycle('bgr')
 
-colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
-for k, col in zip(range(n_clusters_), colors):
-    my_members = labels == k
-    cluster_center = cluster_centers[k]
-    pl.plot(X[my_members, 0], X[my_members, 1], col + '.')
-    pl.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
+# KMeans
+ax = fig.add_subplot(1, 3, 1)
+for k, col in zip(range(n_clusters), colors):
+    my_members = k_means_labels == k
+    cluster_center = k_means_cluster_centers[k]
+    ax.plot(X[my_members, 0], X[my_members, 1], col + '.')
+    ax.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
                                     markeredgecolor='k', markersize=14)
-pl.title('Clustering with MiniBatchKMeans')
-pl.show()
+ax.set_title('Clustering with BatchKMeans')
+
+# MiniBatchKMeans
+ax = fig.add_subplot(1, 3, 2)
+for k, col in zip(range(n_clusters), colors):
+    my_members = mbk_means_labels == k
+    cluster_center = mbk_means_cluster_centers[k]
+    ax.plot(X[my_members, 0], X[my_members, 1], col + '.')
+    ax.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
+                                    markeredgecolor='k', markersize=14)
+ax.set_title('Clustering with MiniBatchKMeans')
+
+
+identic = (mbk_means_labels == k_means_labels)
+different = (mbk_means_labels != k_means_labels)
+
+
+ax = fig.add_subplot(1, 3, 3)
+for k, col in zip([identic, different], cycle('km')):
+    my_members = k
+    ax.plot(X[identic, 0], X[identic, 1], col + '.')
+
+ax.set_title('')
+
+plt.show()
