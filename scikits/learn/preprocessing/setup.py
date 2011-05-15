29578c7cabc1d4a611c910b588025966f4d60f49
Olivier Grisel <olivier.grisel@ensta.org>
2011-05-15 13:22:24 +0200

    work in progress on package structure

--- scikits/learn/preprocessing/setup.py
+++ scikits/learn/preprocessing/setup.py
@@ -1,16 +1,19 @@
 from os.path import join
-import warnings
 import numpy
-
 
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
-    from numpy.distutils.system_info import get_info, get_standard_file, BlasNotFoundError
+    from numpy.distutils.system_info import get_info
+    from numpy.distutils.system_info import get_standard_file
+    from numpy.distutils.system_info import BlasNotFoundError
 
     config = Configuration('preprocessing', parent_package, top_path)
 
-    config.add_subpackage('tests')
-    config.add_subpackage('sparse')
+    config.add_extension(
+        '_preprocessing',
+        sources=[join('src', '_preprocessing.c')],
+        include_dirs=[numpy.get_include()]
+    )
 
     return config
 
