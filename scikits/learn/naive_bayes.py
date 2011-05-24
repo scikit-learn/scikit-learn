b94c8fcaaceaeaf388400050419b8215ef076118
Lars Buitinck <L.J.Buitinck@uva.nl>
2011-05-24 12:33:27 +0200

    NB cosmit: *feature* independence

--- scikits/learn/naive_bayes.py
+++ scikits/learn/naive_bayes.py
@@ -3,7 +3,7 @@ Naive Bayes models
 ==================
 
 Naive Bayes algorithms are a set of supervised learning methods based on
-applying Bayes' theorem with strong (naive) independence assumptions.
+applying Bayes' theorem with strong (naive) feature independence assumptions.
 
 See http://scikit-learn.sourceforge.net/modules/naive_bayes.html for
 complete documentation.
