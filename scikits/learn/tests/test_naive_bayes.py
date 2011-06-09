ca22e3e4a675600b584aa88d477122ee2ccd047a
Lars Buitinck <L.J.Buitinck@uva.nl>
2011-06-09 12:03:04 +0200

    BUG: binary classification failed in MultinomialNB, +regression test

--- scikits/learn/tests/test_naive_bayes.py
+++ scikits/learn/tests/test_naive_bayes.py
@@ -102,11 +102,11 @@ def test_mnnb_pickle():
 def test_mnnb_predict_proba():
     '''Test multinomial NB's probability scores'''
 
-    clf = naive_bayes.MultinomialNB().fit([[0, 1], [0, 1], [1, 0]], [0, 0, 1])
-    assert clf.predict([0, 1]) == 0
+    clf = naive_bayes.MultinomialNB().fit([[0, 1], [1, 3], [4, 0]], [0, 0, 2])
+    assert clf.predict([4, 1]) == 2
     assert clf.predict_proba([0, 1]).shape == (1, 2)
-    assert np.sum(clf.predict_proba([0, 1])) == 1
-    assert np.sum(clf.predict_proba([1, 0])) == 1
+    assert np.sum(clf.predict_proba([1, 5])) == 1
+    assert np.sum(clf.predict_proba([3, 0])) == 1
     assert np.sum(np.exp(clf.class_log_prior_)) == 1
     assert np.sum(np.exp(clf.intercept_)) == 1
 
