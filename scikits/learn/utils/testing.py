cf623ab970e78fad5b7a59fc596c74c75456c2d9
Pietro Berkes <pietro.berkes@googlemail.com>
2011-06-10 13:28:26 -0400

    FIX: update mldata tests to match recent updates; mock_urllib2 now accepts ordering parameter

--- scikits/learn/utils/testing.py
+++ scikits/learn/utils/testing.py
@@ -3,7 +3,7 @@
 # Copyright (c) 2011 Pietro Berkes
 # License: Simplified BSD
 
-import os
+import urllib2
 from StringIO import StringIO
 import scipy as sp
 from scipy.io import savemat
@@ -57,8 +57,11 @@ class mock_urllib2(object):
     def __init__(self, mock_datasets):
         """Object that mocks the urllib2 module to fake requests to mldata.
 
-        `mock_datasets` is a dictionary of {dataset_name: data_dict}, and
-        `data_dict` itself is a dictionary of {column_name: data_array}
+        `mock_datasets` is a dictionary of {dataset_name: data_dict}, or
+        {dataset_name: (data_dict, ordering).
+        `data_dict` itself is a dictionary of {column_name: data_array},
+        and `ordering` is a list of column_names to determine the ordering
+        in the data set (see `fake_mldata_cache` for details).
 
         When requesting a dataset with a name that is in mock_datasets,
         this object creates a fake dataset in a StringIO object and
@@ -66,17 +69,25 @@ class mock_urllib2(object):
         """
         self.mock_datasets = mock_datasets
 
-    class URLError(Exception):
-        pass
+    class HTTPError(urllib2.URLError):
+        code = 404
 
     def urlopen(self, urlname):
         dataset_name = urlname.split('/')[-1]
         if dataset_name in self.mock_datasets:
             resource_name = '_' + dataset_name
             matfile = StringIO()
-            fake_mldata_cache(self.mock_datasets[dataset_name],
-                              resource_name, matfile)
+
+            dataset = self.mock_datasets[dataset_name]
+            ordering = None
+            if isinstance(dataset, tuple):
+                dataset, ordering = dataset
+            fake_mldata_cache(dataset, resource_name, matfile, ordering)
+
             matfile.seek(0)
             return matfile
         else:
-            raise mock_urllib2.URLError('%s not found.', urlname)
+            raise mock_urllib2.HTTPError('%s not found.' % urlname)
+
+    def quote(self, string, safe='/'):
+        return urllib2.quote(string, safe)
