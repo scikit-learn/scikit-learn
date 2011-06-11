6495201aae0467b100596a2de1786b56077319cd
Lars Buitinck <larsmans@gmail.com>
2011-06-11 19:44:22 +0200

    Extend MultinomialNB tests to BernoulliNB
    All tests pass.

--- scikits/learn/tests/test_naive_bayes.py
+++ scikits/learn/tests/test_naive_bayes.py
@@ -4,7 +4,7 @@ import numpy as np
 import scipy.sparse
 from numpy.testing import assert_array_equal, assert_array_almost_equal
 
-from .. import naive_bayes
+from ..naive_bayes import GaussianNB, BernoulliNB, MultinomialNB
 
 # Data is just 6 separable points in the plane
 X = np.array([[-2, -1], [-1, -1], [-1, -2], [1, 1], [1, 2], [2, 1]])
@@ -19,7 +19,7 @@ def test_gnb():
     correct values for a simple toy dataset.
     """
 
-    clf = naive_bayes.GaussianNB()
+    clf = GaussianNB()
     y_pred = clf.fit(X, y).predict(X)
     assert_array_equal(y_pred, y)
 
@@ -45,7 +45,7 @@ def test_mnnb():
     #
     # Check the ability to predict the learning set.
     #
-    clf = naive_bayes.MultinomialNB()
+    clf = MultinomialNB()
     y_pred = clf.fit(X2, y2).predict(X2)
 
     assert_array_equal(y_pred, y2)
@@ -72,7 +72,7 @@ def test_sparse_mnnb():
     #
     # Check the ability to predict the learning set.
     #
-    clf = naive_bayes.MultinomialNB()
+    clf = MultinomialNB()
     y_pred = clf.fit(X2S, y2).predict(X2S)
 
     assert_array_equal(y_pred, y2)
@@ -86,10 +86,10 @@ def test_sparse_mnnb():
     assert_array_almost_equal(np.log(y_pred_proba), y_pred_log_proba, 8)
 
 
-def test_mnnb_pickle():
-    '''Test picklability of multinomial NB'''
+def test_discretenb_pickle():
+    """Test picklability of discreate naive Bayes classifiers"""
 
-    clf = naive_bayes.MultinomialNB(alpha=2, fit_prior=False).fit(X, y)
+    clf = MultinomialNB(alpha=2, fit_prior=False).fit(X, y)
     y_pred = clf.predict(X)
 
     store = StringIO()
@@ -99,21 +99,32 @@ def test_mnnb_pickle():
     assert_array_equal(y_pred, clf.predict(X))
 
 
-def test_mnnb_predict_proba():
-    '''Test multinomial NB's probability scores'''
+def test_discretenb_predict_proba():
+    """Test discrete NB classes' probability scores"""
 
-    clf = naive_bayes.MultinomialNB().fit([[0, 1], [1, 3], [4, 0]], [0, 0, 2])
-    assert clf.predict([4, 1]) == 2
-    assert clf.predict_proba([0, 1]).shape == (1, 2)
-    assert np.sum(clf.predict_proba([1, 5])) == 1
-    assert np.sum(clf.predict_proba([3, 0])) == 1
-    assert np.sum(np.exp(clf.class_log_prior_)) == 1
-    assert np.sum(np.exp(clf.intercept_)) == 1
+    # The 100s below distinguish Bernoulli from multinomial.
+    # FIXME: write a test to show this.
+    X_bernoulli = [[1, 100, 0], [0, 1, 0], [0, 100, 1]]
+    X_multinomial = [[0, 1], [1, 3], [4, 0]]
+    y = [0, 0, 2]   # 2 is regression test for binary case, 02e673
+
+    for cls, X in ([BernoulliNB, MultinomialNB], [X_bernoulli, X_multinomial]):
+        clf = MultinomialNB().fit([[0, 1], [1, 3], [4, 0]], [0, 0, 2])
+        assert clf.predict([4, 1]) == 2
+        assert clf.predict_proba([0, 1]).shape == (1, 2)
+        assert np.sum(clf.predict_proba([1, 5])) == 1
+        assert np.sum(clf.predict_proba([3, 0])) == 1
+        assert np.sum(np.exp(clf.class_log_prior_)) == 1
+        assert np.sum(np.exp(clf.intercept_)) == 1
 
 
-def test_mnnb_uniform_prior():
-    clf = naive_bayes.MultinomialNB(fit_prior=False)
-    clf.fit([[0], [0], [1]], [0, 0, 1])
-    prior = np.exp(clf.class_log_prior_)
-    assert prior[0] == prior[1]
-    assert prior[0] == .5
+def test_discretenb_uniform_prior():
+    """Test whether discrete NB classes fit a uniform prior
+       when fit_prior=False and class_prior=None"""
+
+    for cls in [BernoulliNB, MultinomialNB]:
+        clf = cls(fit_prior=False)
+        clf.fit([[0], [0], [1]], [0, 0, 1])
+        prior = np.exp(clf.class_log_prior_)
+        assert prior[0] == prior[1]
+        assert prior[0] == .5
