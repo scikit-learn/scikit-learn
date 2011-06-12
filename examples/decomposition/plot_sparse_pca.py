bd6ac1ad1de0b4b9e85a4dc3efc64b55d166da0b
vene <vlad@vene.ro>
2011-06-12 17:55:26 +0300

    one more test and a quick example

--- examples/decomposition/plot_sparse_pca.py
+++ examples/decomposition/plot_sparse_pca.py
@@ -1,48 +1,23 @@
-def generate_toy_data(n_atoms, n_samples, image_size):
-    n_features = image_size[0] * image_size[1]
+import numpy as np
+import matplotlib.pyplot as plt
 
-    np.random.seed(0)
-    U = np.random.randn(n_samples, n_atoms)
-    V = np.random.randn(n_atoms, n_features)
+from scikits.learn.decomposition import SparsePCA
+from scikits.learn.datasets import load_digits
 
-    centers = [(3, 3), (6, 7), (8, 1)]
-    sz = [1, 2, 1]
-    for k in range(n_atoms):
-        img = np.zeros(image_size)
-        xmin, xmax = centers[k][0] - sz[k], centers[k][0] + sz[k]
-        ymin, ymax = centers[k][1] - sz[k], centers[k][1] + sz[k]
-        img[xmin:xmax][:, ymin:ymax] = 1.0
-        V[k, :] = img.ravel()
+rows, cols = 4, 3
+digits = load_digits()
+threes = digits.data[digits.target == 3]
+threes -= threes.mean(axis=0)  # todo: use preprocessors
+model = SparsePCA(n_components=rows * cols, tol=1e-3)  # low tolerance for high speed
+model.fit(threes)
+span = np.max(np.abs(model.components_))
 
-    # Y is defined by : Y = UV + noise
-    Y = np.dot(U, V)
-    Y += 0.1 * np.random.randn(Y.shape[0], Y.shape[1])  # Add noise
-    return Y, U, V
+plt.figure()
+for i, comp in enumerate(model.components_):
+    plt.subplot(rows, cols, i + 1)
+    plt.imshow(np.reshape(comp, (8, 8)), interpolation='nearest', 
+               vmin=-span, vmax=span, cmap=plt.cm.PuOr)
+    plt.xticks(())
+    plt.yticks(())
 
-
-# Generate toy data
-n_atoms = 3
-n_samples = 100
-img_sz = (10, 10)
-Y, U, V = generate_toy_data(n_atoms, n_samples, img_sz)
-
-# Estimate U,V
-alpha = 0.5
-SPCA = SparsePCA(n_atoms, alpha, max_iter=100, method='lasso', n_jobs=1)
-SPCA.fit(Y)
-V_estimated = SPCA.components_
-
-# View results
-import pylab as pl
-pl.close('all')
-
-for k in range(n_atoms):
-    pl.matshow(np.reshape(V_estimated[k, :], img_sz))
-    pl.title('Atom %d' % k)
-    pl.colorbar()
-
-pl.figure()
-pl.plot(SPCA.error_)
-pl.xlabel('Iteration')
-pl.ylabel('Cost function')
-pl.show()
+plt.show()
