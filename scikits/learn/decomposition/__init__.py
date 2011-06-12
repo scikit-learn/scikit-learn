8459e3028e8ac7fbd36beefb280516cc7694daff
vene <vlad@vene.ro>
2011-06-12 16:40:29 +0300

    Updated init.py

--- scikits/learn/decomposition/__init__.py
+++ scikits/learn/decomposition/__init__.py
@@ -4,4 +4,5 @@ Matrix decomposition algorithms
 
 from .nmf import NMF, ProjectedGradientNMF
 from .pca import PCA, RandomizedPCA, ProbabilisticPCA, KernelPCA
+from .sparse_pca import SparsePCA, sparse_pca
 from .fastica_ import FastICA, fastica
