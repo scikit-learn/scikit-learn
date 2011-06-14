f9ed2e392208f380326c11b224524432f8ebdf2c
vene <vlad@vene.ro>
2011-06-15 02:00:31 +0300

    Added ridge in transform (factored here for now)

--- scikits/learn/decomposition/tests/test_sparse_pca.py
+++ scikits/learn/decomposition/tests/test_sparse_pca.py
@@ -35,9 +35,17 @@ def test_correct_shapes():
 
 
 def test_fit_transform():
-    Y, _, _ = generate_toy_data(3, 10, (8, 8))
+    Y, _, _ = generate_toy_data(3, 10, (8, 8))  # wide array
+    print Y.shape
     U1 = SparsePCA(n_components=3).fit_transform(Y)
-    U2 = SparsePCA(n_components=3).fit(Y).transform(Y, alpha=0)
+    U2 = SparsePCA(n_components=3).fit(Y).transform(Y, alpha=0.01)
+    assert_array_almost_equal(U1, U2)
+
+
+def test_fit_transform_tall():
+    Y, _, _ = generate_toy_data(3, 65, (8, 8))  # tall array
+    U1 = SparsePCA(n_components=3).fit_transform(Y)
+    U2 = SparsePCA(n_components=3).fit(Y).transform(Y, alpha=0.01)
     assert_array_almost_equal(U1, U2)
 
 
