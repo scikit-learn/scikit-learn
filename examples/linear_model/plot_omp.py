26fad8d485db699d195bc2c54af438967e7c96ec
vene <vlad@vene.ro>
2011-06-16 07:40:56 +0300

    Added the example

---
+++ examples/linear_model/plot_omp.py
@@ -0,0 +1,54 @@
+"""Using Orthogonal Matching Pursuit for recovering a sparse signal from
+a noisy measurement encoded with a dictionary"""
+
+import pylab as pl
+import numpy as np
+from scikits.learn.linear_model import orthogonal_mp
+
+n_samples, n_features = 512, 100
+n_atoms = 17
+
+# generate the dictionary
+#########################
+X = np.random.randn(n_samples, n_features)
+X /= np.sqrt(np.sum((X ** 2), axis=0))
+
+# generate the sparse signal
+############################
+gamma = np.zeros(n_features)
+idx = np.arange(n_features)
+np.random.shuffle(idx)
+idx = idx[:n_atoms]
+gamma[idx] = np.random.normal(0, 5, n_atoms)
+
+# encode the signal
+###################
+y = np.dot(X, gamma)
+y_noisy = y + np.random.normal(0, 0.1, y.shape)
+
+# plot the sparse signal
+########################
+pl.subplot(3, 1, 1)
+pl.title("Sparse signal")
+pl.stem(idx, gamma[idx])
+
+# plot the noise-free reconstruction
+####################################
+x_r = orthogonal_mp(X, y, n_atoms)
+idx_r = np.where(x_r != 0)[0]
+print idx_r
+print x_r[idx_r]
+pl.subplot(3, 1, 2)
+pl.title("Recovered signal from noise-free measurements")
+pl.stem(idx_r, x_r[idx_r])
+
+# plot the noisy reconstruction
+###############################
+x_r = orthogonal_mp(X, y_noisy, n_atoms)
+idx_r = np.where(x_r != 0)[0]
+pl.subplot(3, 1, 3)
+pl.title("Recovered signal from noisy measurements")
+pl.stem(idx_r, x_r[idx_r])
+
+pl.subplots_adjust(0.06, 0.04, 0.98, 0.94, 0.2, 0.38)
+pl.show()
