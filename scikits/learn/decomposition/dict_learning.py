7af69d76b7f155939a3671629cea451f302db940
vene <vlad@vene.ro>
2011-06-24 20:18:47 +0300

    Initial shape of dictionary learning object

---
+++ scikits/learn/decomposition/dict_learning.py
@@ -0,0 +1,138 @@
+""" Dictionary learning
+"""
+# Author: Vlad Niculae, Gael Varoquaux, Alexandre Gramfort
+# License: BSD
+
+
+import numpy as np
+
+from .sparse_pca import dict_learning, sparse_pca
+from ..base import BaseEstimator, TransformerMixin
+
+
+class DictionaryLearning(BaseEstimator, TransformerMixin):
+    """ Dictionary learning
+
+    Finds a dictionary (a set of atoms) that can best be used to represent data
+    using a sparse code.
+
+    Solves the optimization problem:
+    (U^*,V^*) = argmin 0.5 || Y - U V ||_2^2 + alpha * || U ||_1
+                 (U,V)
+                with || V_k ||_2 = 1 for all  0<= k < n_atoms
+
+    Parameters
+    ----------
+    n_atoms: int,
+        number of dictionary elements to extract
+
+    alpha: int,
+        sparsity controlling parameter
+
+    max_iter: int,
+        maximum number of iterations to perform
+
+    tol: float,
+        tolerance for numerical error
+
+    method: 'batch'
+        algorithm to use
+
+    coding_method: 'lars' | 'cd',
+        method to use for solving the lasso problem
+
+    n_jobs: int,
+        number of parallel jobs to run
+
+    U_init: array of shape (n_samples, n_atoms),
+    V_init: array of shape (n_atoms, n_features),
+        initial values for the decomposition for warm restart scenarios
+
+    verbose:
+        degree of verbosity of the printed output
+
+    Attributes
+    ----------
+    components_: array, [n_components, n_features]
+        sparse components extracted from the data
+
+    error_: array
+        vector of errors at each iteration
+
+    References
+    ----------
+        XXX: Todo
+
+    See also
+    --------
+    `scikits.learn.decomposition.SparsePCA`
+
+    """
+    def __init__(self, n_atoms, alpha=1, max_iter=1000, tol=1e-8,
+                 method='batch', coding_method='lars', n_jobs='1', U_init=None,
+                 V_init=None, verbose=False):
+        self.n_atoms = n_atoms
+        self.alpha = alpha
+        self.max_iter = max_iter
+        self.tol = tol
+        self.method = method
+        self.coding_method = coding_method
+        self.n_jobs = n_jobs
+        self.U_init = U_init
+        self.V_init = V_init
+        self.verbose = verbose
+
+    def fit(X, y=None, **params):
+        """Fit the model from data in X.
+
+        Parameters
+        ----------
+        X: array-like, shape (n_samples, n_features)
+            Training vector, where n_samples in the number of samples
+            and n_features is the number of features.
+
+        Returns
+        -------
+        self : object
+            Returns the instance itself.
+        """
+        self._set_params(**params)
+        X = np.asanyarray(X)
+        if method='batch':
+            U, _, E = sparse_pca(X.T, self.n_components, self.alpha,
+                                 tol=self.tol, max_iter=self.max_iter, 
+                                 method=self.coding_method, n_jobs=self.n_jobs,
+                                 U_init=self.V_init.T, V_init=self.U_init.T,
+                                 verbose=self.verbose)
+            self.components_ = U.T
+            self.error_ = E
+        else:
+            raise NotImplemented('Online version will be integrated soon')
+
+        return self
+
+    def transform(X, y=None, method='omp', **kwargs):
+        """Apply the projection onto the learned sparse components
+        to new data.
+
+        Parameters
+        ----------
+        X: array of shape (n_samples, n_features)
+            Test data to be transformed, must have the same number of
+            features as the data used to train the model.
+
+        method: 'omp' | 'lars' | 'cd'
+            Sparse coding method to use. Additional parameters are passed
+            to the corresponding solver.
+
+        Returns
+        -------
+        X_new array, shape (n_samples, n_components)
+            Transformed data
+        """
+
+        # XXX: parameters should be made explicit so we can have defaults
+        if method='omp':
+            return orthogonal_mp(self.components_, X.T, **kwargs)
+        else:
+            return _update_V_parallel(self.components_.T, X.T, **kwargs)
