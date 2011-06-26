New example about robust covariance estimation + fix bugs.

examples/covariance/plot_robust_vs_empirical_covariance.py
@@ -3,12 +3,26 @@
 Robust vs Empirical covariance estimate
 =======================================
 
-The Minimum Covariance Determinant estimator (MCD) is a robust
-estimator of a data set's covariance introduced by P.J.Rousseuw in
-[1]. The idea is to find a given proportion of "good" observations
-which are not outliers and compute their empirical covariance matrix.
-From these observations, it is necessary to compute a robust location
-estimate before computing the Minimal Covariance Determinant estimator.
+The usual covariance maximum likelihood estimate is very sensitive to
+the presence of outliers in the data set. In such a case, one would
+have better to use a robust estimator of covariance to garanty that
+the estimation is resistant to "errorneous" observations in the data
+set.
+
+The Minimum Covariance Determinant estimator is a robust,
+high-breakdown point (i.e. it can be used to estimate the covariance
+matrix of highly contaminated datasets, up to
+$\frac{n_samples-n_features-1}{2}$ outliers) estimator of
+covariance. The idea is to find $\frac{n_samples+n_features+1}{2}$
+observations whose empirical covariance has the smallest determinant,
+yielding a "pure" subset of observations from which to compute
+standards estimates of location and covariance. After a correction
+step aiming at compensating the fact the the estimates were learnt
+from only a portion of the initial data, we end up with robust
+estimates of the data set location and covariance.
+
+The Minimum Covariance Determinant estimator (MCD) has been introduced
+by P.J.Rousseuw in [1].
 
 In this example, we compare the estimation errors that are made when
 using four types of location and covariance estimates on contaminated
@@ -19,12 +33,17 @@ gaussian distributed data sets:
  - The robust MCD reweighted according to Rousseeuw's recommandations:
    observations are given a 0 weight if they are found to be outlying
    according to their MCD-based Mahalanobis distance. Doing so improve the
-   efficiency of the estimators at gaussian models.
+   efficiency of the estimators at gaussian models but it assumes that
+   we perfectly know the distribution of the mahalanobis distances computed
+   from the MCD estimate (see [2] for further details).
  - The mean and the empirical covariance of the observations that are known
-   to be good ones. This can be considered as a "perfect" MCD estimation.
+   to be good ones. This can be considered as a "perfect" MCD estimation,
+   so one can trust our implementation by comparing to this case.
 
 [1] P. J. Rousseeuw. Least median of squares regression. J. Am
     Stat Ass, 79:871, 1984.
+[2] Johanna Hardin, David M Rocke. Journal of Computational and
+    Graphical Statistics. December 1, 2005, 14(4): 928-946.
 
 """
 print __doc__
@@ -35,70 +54,71 @@ import matplotlib.font_manager
 
 from scikits.learn.covariance import EmpiricalCovariance, MCD
 
-n_samples = 100
+# example settings
+n_samples = 80
 n_features = 5
 repeat = 10
-
 range_n_outliers = np.arange(0, n_samples / 2, 5)
-error_location_mcd = np.zeros((range_n_outliers.size, repeat))
-error_covariance_mcd = np.zeros((range_n_outliers.size, repeat))
-error_location_mcdr = np.zeros((range_n_outliers.size, repeat))
-error_covariance_mcdr = np.zeros((range_n_outliers.size, repeat))
-error_location_emp_full = np.zeros((range_n_outliers.size, repeat))
-error_covariance_emp_full = np.zeros((range_n_outliers.size, repeat))
-error_location_emp_pure = np.zeros((range_n_outliers.size, repeat))
-error_covariance_emp_pure = np.zeros((range_n_outliers.size, repeat))
+
+# definition of arrays to store results
+err_loc_mcd = np.zeros((range_n_outliers.size, repeat))
+err_cov_mcd = np.zeros((range_n_outliers.size, repeat))
+err_loc_mcd_reweighted = np.zeros((range_n_outliers.size, repeat))
+err_cov_mcd_reweighted = np.zeros((range_n_outliers.size, repeat))
+err_loc_emp_full = np.zeros((range_n_outliers.size, repeat))
+err_cov_emp_full = np.zeros((range_n_outliers.size, repeat))
+err_loc_emp_pure = np.zeros((range_n_outliers.size, repeat))
+err_cov_emp_pure = np.zeros((range_n_outliers.size, repeat))
+
+# computation
 for i, n_outliers in enumerate(range_n_outliers):
     for j in range(repeat):
         # generate data
-        data = np.random.randn(n_samples, n_features)
+        X = np.random.randn(n_samples, n_features)
         # add some outliers
         outliers_index = np.random.permutation(n_samples)[:n_outliers]
         outliers_offset = 10. * \
             (np.random.randint(2, size=(n_outliers,n_features)) - 0.5)
-        data[outliers_index] += outliers_offset
+        X[outliers_index] += outliers_offset
         inliers_mask = np.ones(n_samples).astype(bool)
         inliers_mask[outliers_index] = False
         
         # fit a Minimum Covariance Determinant (MCD) robust estimator to data
-        S = MCD().fit(data, reweight=None)
+        S = MCD().fit(X, reweight=None)
         # compare robust estimates with the true location and covariance
-        error_location_mcd[i,j] = np.sum(S.location_ ** 2)
-        error_covariance_mcd[i,j] = S.error(
-            np.eye(n_features), error_type='rmse')
+        err_loc_mcd[i,j] = np.sum(S.location_ ** 2)
+        err_cov_mcd[i,j] = S.error_norm(np.eye(n_features))
         # fit a reweighted MCD robust estimator to data
-        S = MCD().fit(data)
+        S = MCD().fit(X)
         # compare robust estimates with the true location and covariance
-        error_location_mcdr[i,j] = np.sum(S.location_ ** 2)
-        error_covariance_mcdr[i,j] = S.error(
-            np.eye(n_features), error_type='rmse')
+        err_loc_mcd_reweighted[i,j] = np.sum(S.location_ ** 2)
+        err_cov_mcd_reweighted[i,j] = S.error_norm(np.eye(n_features))
         # compare estimators learnt from the full data set with true parameters
-        error_location_emp_full[i,j] = np.sum(data.mean(0) ** 2)
-        error_covariance_emp_full[i,j] = EmpiricalCovariance().fit(data).error(
-            np.eye(n_features), error_type='rmse')
+        err_loc_emp_full[i,j] = np.sum(X.mean(0) ** 2)
+        err_cov_emp_full[i,j] = EmpiricalCovariance().fit(X).error_norm(
+            np.eye(n_features))
         # compare with an empirical covariance learnt from a pure data set
         # (i.e. "perfect" MCD)
-        pure_data = data[inliers_mask]
-        pure_location = pure_data.mean(0)
-        pure_emp_cov = EmpiricalCovariance().fit(pure_data)
-        error_location_emp_pure[i,j] = np.sum(pure_location ** 2)
-        error_covariance_emp_pure[i,j] = pure_emp_cov.error(
-            np.eye(n_features), error_type='rmse')    
+        pure_X = X[inliers_mask]
+        pure_location = pure_X.mean(0)
+        pure_emp_cov = EmpiricalCovariance().fit(pure_X)
+        err_loc_emp_pure[i,j] = np.sum(pure_location ** 2)
+        err_cov_emp_pure[i,j] = pure_emp_cov.error_norm(np.eye(n_features))    
 
 # Display results
-font_prop = matplotlib.font_manager.FontProperties(size=12)
+font_prop = matplotlib.font_manager.FontProperties(size=11)
 pl.subplot(2,1,1)
-pl.errorbar(range_n_outliers, error_location_mcd.mean(1),
-            yerr=error_location_mcd.std(1)/np.sqrt(repeat),
+pl.errorbar(range_n_outliers, err_loc_mcd.mean(1),
+            yerr=err_loc_mcd.std(1)/np.sqrt(repeat),
             label="Robust location", color='cyan')
-pl.errorbar(range_n_outliers, error_location_mcdr.mean(1),
-            yerr=error_location_mcdr.std(1)/np.sqrt(repeat),
+pl.errorbar(range_n_outliers, err_loc_mcd_reweighted.mean(1),
+            yerr=err_loc_mcd_reweighted.std(1)/np.sqrt(repeat),
             label="Robust location (reweighted)", color='blue')
-pl.errorbar(range_n_outliers, error_location_emp_full.mean(1),
-            yerr=error_location_emp_full.std(1)/np.sqrt(repeat),
+pl.errorbar(range_n_outliers, err_loc_emp_full.mean(1),
+            yerr=err_loc_emp_full.std(1)/np.sqrt(repeat),
             label="Full data set mean", color='green')
-pl.errorbar(range_n_outliers, error_location_emp_pure.mean(1),
-            yerr=error_location_emp_pure.std(1)/np.sqrt(repeat),
+pl.errorbar(range_n_outliers, err_loc_emp_pure.mean(1),
+            yerr=err_loc_emp_pure.std(1)/np.sqrt(repeat),
             label="Pure data set mean", color='black')
 pl.title("Influence of outliers on the location estimation")
 pl.ylabel(r"Error ($||\mu - \hat{\mu}||_2^2$)")
@@ -106,21 +126,21 @@ pl.legend(loc="upper left", prop=font_prop)
 
 pl.subplot(2,1,2)
 x_size = range_n_outliers.size
-pl.errorbar(range_n_outliers, error_covariance_mcd.mean(1),
-            yerr=error_covariance_mcd.std(1),
+pl.errorbar(range_n_outliers, err_cov_mcd.mean(1),
+            yerr=err_cov_mcd.std(1),
             label="Robust covariance (MCD)", color='cyan')
-pl.errorbar(range_n_outliers, error_covariance_mcdr.mean(1),
-            yerr=error_covariance_mcdr.std(1),
+pl.errorbar(range_n_outliers, err_cov_mcd_reweighted.mean(1),
+            yerr=err_cov_mcd_reweighted.std(1),
             label="Robust covariance (reweighted MCD)", color='blue')
-pl.errorbar(range_n_outliers[:(x_size/4 + 1)],
-            error_covariance_emp_full.mean(1)[:(x_size/4 + 1)],
-            yerr=error_covariance_emp_full.std(1)[:(x_size/4 + 1)],
+pl.errorbar(range_n_outliers[:(x_size/5 + 1)],
+            err_cov_emp_full.mean(1)[:(x_size/5 + 1)],
+            yerr=err_cov_emp_full.std(1)[:(x_size/5 + 1)],
             label="Full data set empirical covariance", color='green')
-pl.plot(range_n_outliers[(x_size/4):(x_size/2 - 1)],
-         error_covariance_emp_full.mean(1)[(x_size/4):(x_size/2 - 1)],
+pl.plot(range_n_outliers[(x_size/5):(x_size/2 - 1)],
+         err_cov_emp_full.mean(1)[(x_size/5):(x_size/2 - 1)],
          color='green', ls='--')
-pl.errorbar(range_n_outliers, error_covariance_emp_pure.mean(1),
-            yerr=error_covariance_emp_pure.std(1),
+pl.errorbar(range_n_outliers, err_cov_emp_pure.mean(1),
+            yerr=err_cov_emp_pure.std(1),
             label="Pure data set empirical covariance", color='black')
 pl.title("Influence of outliers on the covariance estimation")
 pl.xlabel("Amount of contamination (%)")

scikits/learn/covariance/robust_covariance.py
@@ -5,10 +5,6 @@ Here are implemented estimators that are resistant to outliers.
 
 """
 # Author: Virgile Fritsch <virgile.fritsch@inria.fr>
-#         implementing a method by Rousseeuw & Van Driessen described in
-#         (A Fast Algorithm for the Minimum Covariance Determinant Estimator,
-#         1999, American Statistical Association and the American Society
-#         for Quality, TECHNOMETRICS)
 #
 # License: BSD Style.
 import warnings
@@ -18,6 +14,13 @@ from scipy.stats import chi2
 from scikits.learn.covariance import empirical_covariance, EmpiricalCovariance
 from ..utils.extmath import fast_logdet as exact_logdet
 
+###############################################################################
+### Minimum Covariance Determinant
+#   Implementing of an algorithm by Rousseeuw & Van Driessen described in
+#   (A Fast Algorithm for the Minimum Covariance Determinant Estimator,
+#   1999, American Statistical Association and the American Society
+#   for Quality, TECHNOMETRICS)
+###############################################################################
 
 def c_step(X, h, remaining_iterations=30, initial_estimates=None,
            verbose=False):
@@ -63,23 +66,21 @@ def c_step(X, h, remaining_iterations=30, initial_estimates=None,
     # Initialisation
     if initial_estimates is None:
         # compute initial robust estimates from a random subset
-        support_aux = np.zeros(n_samples).astype(bool)
-        support_aux[np.random.permutation(n_samples)[:h]] = True
-        support = support_aux
+        support = np.zeros(n_samples).astype(bool)
+        support[np.random.permutation(n_samples)[:h]] = True
         T = X[support].mean(0)
         S = empirical_covariance(X[support])
     else:
         # get initial robust estimates from the function parameters
         T = initial_estimates[0]
         S = initial_estimates[1]
-        # run a special iteration for that case (to get a initial support)
+        # run a special iteration for that case (to get an initial support)
         inv_S = linalg.pinv(S)
         X_centered = X - T
         dist = (np.dot(X_centered, inv_S) * X_centered).sum(1)
         # compute new estimates
-        support_aux = np.zeros(n_samples).astype(bool)
-        support_aux[np.argsort(dist)[:h]] = True
-        support = support_aux
+        support = np.zeros(n_samples).astype(bool)
+        support[np.argsort(dist)[:h]] = True
         T = X[support].mean(0)
         S = empirical_covariance(X[support])
         detS = exact_logdet(S)
@@ -98,9 +99,8 @@ def c_step(X, h, remaining_iterations=30, initial_estimates=None,
         previous_detS = detS
         previous_support = support
         # compute new estimates
-        support_aux = np.zeros(n_samples).astype(bool)
-        support_aux[np.argsort(dist)[:h]] = True
-        support = support_aux
+        support = np.zeros(n_samples).astype(bool)
+        support[np.argsort(dist)[:h]] = True
         T = X[support].mean(0)
         S = empirical_covariance(X[support])
         detS = np.log(linalg.det(S))
@@ -379,9 +379,8 @@ def fast_mcd(X, correction="empirical", reweight="rousseeuw"):
         mask = dist < chi2(n_features).isf(0.025)
         T_reweighted = X[mask].mean(0)
         S_reweighted = empirical_covariance(X[mask])
-        support_aux = np.zeros(n_samples).astype(bool)
-        support_aux[mask] = True
-        support = support_aux
+        support = np.zeros(n_samples).astype(bool)
+        support[mask] = True
     else:
         T_reweighted = T
         S_reweighted = S_corrected

scikits/learn/covariance/tests/test_covariance.py
@@ -27,6 +27,16 @@ def test_covariance():
     assert_almost_equal(cov.error_norm(empirical_covariance(X)), 0)
     assert_almost_equal(
         cov.error_norm(empirical_covariance(X), norm='spectral'), 0)
+    assert_almost_equal(
+        cov.error_norm(empirical_covariance(X), norm='frobenius'), 0)
+    assert_almost_equal(
+        cov.error_norm(empirical_covariance(X), scaling=False), 0)
+    assert_almost_equal(
+        cov.error_norm(empirical_covariance(X), squared=False), 0)
+    # Mahalanobis distances computation test
+    mahal_dist = cov.mahalanobis(X)
+    assert(np.amax(mahal_dist) < 250)
+    assert(np.amin(mahal_dist) > 50)
 
     # test with n_features = 1
     X_1d = X[:,0]
@@ -257,14 +267,15 @@ def launch_mcd_on_dataset(n_samples, n_features, n_outliers,
     error_location = np.sum((pure_data.mean(0) - T)**2)
     assert(error_location < tol_loc)
     emp_cov = EmpiricalCovariance().fit(pure_data)
-    assert(emp_cov.error(S) < tol_cov)
+    #print emp_cov.error_norm(S)
+    assert(emp_cov.error_norm(S) < tol_cov)
     assert(np.sum(H) > tol_support)
     # check improvement
     if (n_outliers/float(n_samples) > 0.1) and (n_features > 1):
         error_bad_location = np.sum((data.mean(0) - T)**2)
         assert(error_bad_location > error_location)
         bad_emp_cov = EmpiricalCovariance().fit(data)
-        assert(emp_cov.error(S) < bad_emp_cov.error(S))
+        assert(emp_cov.error_norm(S) < bad_emp_cov.error_norm(S))
     
     # compute MCD by fitting an object
     mcd_fit = MCD().fit(data)
@@ -274,11 +285,11 @@ def launch_mcd_on_dataset(n_samples, n_features, n_outliers,
     # compare with the estimates learnt from the inliers
     error_location = np.sum((pure_data.mean(0) - T)**2)
     assert(error_location < tol_loc)
-    assert(emp_cov.error(S) < tol_cov)
+    assert(emp_cov.error_norm(S) < tol_cov)
     assert(np.sum(H) > tol_support)
     # check improvement
     if (n_outliers/float(n_samples) > 0.1) and (n_features > 1):
         error_bad_location = np.sum((data.mean(0) - T)**2)
         assert(error_bad_location > error_location)
         bad_emp_cov = EmpiricalCovariance().fit(data)
-        assert(emp_cov.error(S) < bad_emp_cov.error(S))
+        assert(emp_cov.error_norm(S) < bad_emp_cov.error_norm(S))