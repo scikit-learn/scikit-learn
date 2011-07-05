cdf7df988b0a4806b578421a49879f51ab32d111
Lars Buitinck <L.J.Buitinck@uva.nl>
2011-07-05 14:26:38 +0200

    pep8 feature_extraction.text + rm content word "computer" from stop list

--- scikits/learn/feature_extraction/text.py
+++ scikits/learn/feature_extraction/text.py
@@ -21,10 +21,10 @@ ENGLISH_STOP_WORDS = set([
     "any", "anyhow", "anyone", "anything", "anyway", "anywhere", "are",
     "around", "as", "at", "back", "be", "became", "because", "become",
     "becomes", "becoming", "been", "before", "beforehand", "behind", "being",
-    "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom",
-    "but", "by", "call", "can", "cannot", "cant", "co", "computer", "con",
-    "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down",
-    "due", "during", "each", "eg", "eight", "either", "eleven", "else",
+    "below", "beside", "besides", "between", "beyond", "bill", "both",
+    "bottom", "but", "by", "call", "can", "cannot", "cant", "co", "con",
+    "could", "couldnt", "cry", "de", "describe", "detail", "do", "done",
+    "down", "due", "during", "each", "eg", "eight", "either", "eleven", "else",
     "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone",
     "everything", "everywhere", "except", "few", "fifteen", "fify", "fill",
     "find", "fire", "first", "five", "for", "former", "formerly", "forty",
@@ -35,26 +35,27 @@ ENGLISH_STOP_WORDS = set([
     "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter",
     "latterly", "least", "less", "ltd", "made", "many", "may", "me",
     "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly",
-    "move", "much", "must", "my", "myself", "name", "namely", "neither", "never",
-    "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor",
-    "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once",
-    "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours",
-    "ourselves", "out", "over", "own", "part", "per", "perhaps", "please",
-    "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems",
-    "serious", "several", "she", "should", "show", "side", "since", "sincere",
-    "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime",
-    "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than",
-    "that", "the", "their", "them", "themselves", "then", "thence", "there",
-    "thereafter", "thereby", "therefore", "therein", "thereupon", "these",
-    "they", "thick", "thin", "third", "this", "those", "though", "three",
-    "through", "throughout", "thru", "thus", "to", "together", "too", "top",
-    "toward", "towards", "twelve", "twenty", "two", "un", "under", "until",
-    "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what",
-    "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas",
-    "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while",
-    "whither", "who", "whoever", "whole", "whom", "whose", "why", "will",
-    "with", "within", "without", "would", "yet", "you", "your", "yours",
-    "yourself", "yourselves"])
+    "move", "much", "must", "my", "myself", "name", "namely", "neither",
+    "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone",
+    "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on",
+    "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our",
+    "ours", "ourselves", "out", "over", "own", "part", "per", "perhaps",
+    "please", "put", "rather", "re", "same", "see", "seem", "seemed",
+    "seeming", "seems", "serious", "several", "she", "should", "show", "side",
+    "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone",
+    "something", "sometime", "sometimes", "somewhere", "still", "such",
+    "system", "take", "ten", "than", "that", "the", "their", "them",
+    "themselves", "then", "thence", "there", "thereafter", "thereby",
+    "therefore", "therein", "thereupon", "these", "they", "thick", "thin",
+    "third", "this", "those", "though", "three", "through", "throughout",
+    "thru", "thus", "to", "together", "too", "top", "toward", "towards",
+    "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us",
+    "very", "via", "was", "we", "well", "were", "what", "whatever", "when",
+    "whence", "whenever", "where", "whereafter", "whereas", "whereby",
+    "wherein", "whereupon", "wherever", "whether", "which", "while", "whither",
+    "who", "whoever", "whole", "whom", "whose", "why", "will", "with",
+    "within", "without", "would", "yet", "you", "your", "yours", "yourself",
+    "yourselves"])
 
 
 def strip_accents(s):
@@ -105,7 +106,7 @@ DEFAULT_TOKEN_PATTERN = r"\b\w\w+\b"
 
 
 class WordNGramAnalyzer(BaseEstimator):
-    """Simple analyzer: transform a text document into a sequence of word tokens
+    """Simple analyzer: transform text document into a sequence of word tokens
 
     This simple implementation does:
       - lower case conversion
@@ -272,7 +273,7 @@ class CountVectorizer(BaseEstimator):
 
     def _build_vectors_and_vocab(self, raw_documents):
         """Analyze documents, build vocabulary and vectorize.
-        
+
         Main part of the fit_transform implementation.
         """
 
@@ -331,7 +332,8 @@ class CountVectorizer(BaseEstimator):
         # we really sure want we want to drop them? They take some memory but
         # can be useful for corpus introspection
 
-        matrix = self._term_count_dicts_to_matrix(term_counts_per_doc, vocabulary)
+        matrix = self._term_count_dicts_to_matrix(term_counts_per_doc,
+                                                  vocabulary)
         return matrix, vocabulary
 
     def _build_vectors(self, raw_documents):
