689b339c6e6fb3dd68af7b948c2915f498fa42a4
vene <vlad@vene.ro>
2011-07-15 23:50:11 +0300

    ENH: verbose and title in sparse pca example

--- examples/decomposition/plot_sparse_pca.py
+++ examples/decomposition/plot_sparse_pca.py
@@ -24,13 +24,13 @@ rows, cols = 4, 3
 digits = load_digits()
 threes = digits.data[digits.target == 3]
 threes -= threes.mean(axis=0)  # XXX: use preprocessors
-model = SparsePCA(n_components=rows * cols, alpha=5)
+model = SparsePCA(n_components=rows * cols, alpha=5, verbose=True)
 model.fit(threes)
 span = np.max(np.abs(model.components_))
 
 ###############################################################################
 # Plot sparse components
-fig = pl.figure(figsize=(1.2 * cols, 1.4 * rows))
+fig = pl.figure(figsize=(1.2 * cols, 1.6 * rows))
 for i, comp in enumerate(model.components_):
     pl.subplot(rows, cols, i + 1)
     pl.imshow(np.reshape(comp, (8, 8)), interpolation='nearest',
@@ -38,7 +38,9 @@ for i, comp in enumerate(model.components_):
     pl.xticks(())
     pl.yticks(())
 
-pl.subplots_adjust(0.01, 0.15, 0.99, 0.99, 0.04, 0.)
+pl.subplots_adjust(0.01, 0.14, 0.99, 0.88, 0.04, 0.)
 cax = fig.add_axes([0.1, 0.06, 0.8, 0.04])
 pl.colorbar(cax=cax, orientation='horizontal')
+pl.suptitle('Sparse components extracted\nfrom images of the digit 3',
+            size=16)
 pl.show()
