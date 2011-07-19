b37929f602889f1ac8750d4558596a5331dcae23
Gael varoquaux <gael.varoquaux@normalesup.org>
2011-07-19 16:23:22 +0200

    Add forgotten 'install' for mixture.

--- scikits/learn/setup.py
+++ scikits/learn/setup.py
@@ -25,6 +25,8 @@ def configuration(parent_package='', top_path=None):
     config.add_subpackage('utils')
     config.add_subpackage('utils/tests')
     config.add_subpackage('externals')
+    config.add_subpackage('mixture')
+    config.add_subpackage('mixture/tests')
     config.add_subpackage('gaussian_process')
     config.add_subpackage('gaussian_process/tests')
     config.add_subpackage('manifold')
