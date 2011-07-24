5ab0f1d3036fdaba1105a5426cb8c600ebd1627a
Lars Buitinck <larsmans@gmail.com>
2011-07-24 19:22:41 +0200

    New-style deprecation of datasets.load_files

--- scikits/learn/datasets/__init__.py
+++ scikits/learn/datasets/__init__.py
@@ -16,10 +16,9 @@ from .mldata import fetch_mldata, mldata_filename
 from .samples_generator import swiss_roll
 from .svmlight_format import load_svmlight_file
 from .olivetti_faces import fetch_olivetti_faces
+from ..utils import deprecated
 
-# backward compatibility
+@deprecated("to be removed in 0.9;"
+            " use scikits.learn.datasets.load_filenames instead")
 def load_files(*args, **kwargs):
-    """Deprecated, use ``scikits.learn.datasets.load_filenames`` instead"""
-    import warnings
-    warnings.warn("Deprecated in 0.8 to be removed in 0.9: use load_filenames")
     return load_filenames(*args, **kwargs)
