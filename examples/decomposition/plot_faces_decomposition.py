a6b21f9d3bdd2cdc952e2a41cbf44e2563b22194
Olivier Grisel <olivier.grisel@ensta.org>
2011-07-26 18:59:31 +0200

    ENH: remove references to digits + format

--- examples/decomposition/plot_faces_decomposition.py
+++ examples/decomposition/plot_faces_decomposition.py
@@ -17,11 +17,12 @@ from time import time
 
 import pylab as pl
 
-from scikits.learn.decomposition import RandomizedPCA, NMF, \
-                                        MiniBatchSparsePCA, FastICA
 from scikits.learn.cluster import MiniBatchKMeans
 from scikits.learn.datasets import fetch_olivetti_faces
-
+from scikits.learn.decomposition import FastICA
+from scikits.learn.decomposition import MiniBatchSparsePCA
+from scikits.learn.decomposition import NMF
+from scikits.learn.decomposition import RandomizedPCA
 
 # Display progress logs on stdout
 logging.basicConfig(level=logging.INFO,
@@ -31,7 +32,7 @@ n_components = n_row * n_col
 image_shape = (64, 64)
 
 ###############################################################################
-# Load digits data
+# Load faces data
 dataset = fetch_olivetti_faces()
 faces = dataset.data
 faces_centered = faces - faces.mean(axis=0)
@@ -39,7 +40,7 @@ print "Dataset consists of %d images" % len(faces)
 
 
 ###############################################################################
-def plot_digit_gallery(title, images):
+def plot_gallery(title, images):
     pl.figure(figsize=(1. * n_col, 1.13 * n_row))
     pl.suptitle(title, size=16)
     vmax = max(images.max(), -images.min())
@@ -56,25 +57,28 @@ def plot_digit_gallery(title, images):
 # List of the different estimators, whether to center and transpose the
 # problem, and whether the transformer uses the clustering API.
 estimators = [
-    ('Eigenfaces (PCA)', RandomizedPCA(n_components=n_components,
-                                        whiten=True),
-                          True, False, False),
-    ('Non-negative components (NMF)', NMF(n_components=n_components,
-                                          init='nndsvda', beta=1, tol=1e-3,
-                                          sparseness='components'),
-                                      False, False, False),
-    ('Independent components (FastICA)', FastICA(n_components=n_components,
-                                             whiten=True, max_iter=10),
-                                     True, True, False),
-    ('Sparse components (SparsePCA)', MiniBatchSparsePCA(
-                                                n_components=n_components,
-                                                alpha=1e-3, n_iter=100,
-                                                verbose=True, chunk_size=3),
-                                      True, False, False),
-    ('Cluster centers (KMeans)', MiniBatchKMeans(k=n_components, tol=1e-2,
-                                                 chunk_size=20, max_iter=10),
-                                 True, False, True)
-    ]
+    ('Eigenfaces (PCA)',
+     RandomizedPCA(n_components=n_components, whiten=True),
+     True, False, False),
+
+    ('Non-negative components (NMF)',
+     NMF(n_components=n_components, init='nndsvda', beta=1, tol=1e-3,
+         sparseness='components'),
+     False, False, False),
+
+    ('Independent components (FastICA)',
+     FastICA(n_components=n_components, whiten=True, max_iter=10),
+     True, True, False),
+
+    ('Sparse components (SparsePCA)',
+     MiniBatchSparsePCA(n_components=n_components, alpha=1e-3, n_iter=100,
+                        verbose=True, chunk_size=3),
+     True, False, False),
+
+    ('Cluster centers (KMeans)',
+     MiniBatchKMeans(k=n_components, tol=1e-2, chunk_size=20, max_iter=10),
+     True, False, True)
+]
 
 ###############################################################################
 # Do the estimation and plot it
@@ -94,6 +98,6 @@ for name, estimator, center, transpose, cluster in estimators:
         components_ = estimator.components_
     if transpose:
         components_ = components_.T
-    plot_digit_gallery(name, components_)
+    plot_gallery(name, components_)
 
 pl.show()
