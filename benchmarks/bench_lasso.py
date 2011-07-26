6134371d54dbaf33d94042af3db490e49a9f2789
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2011-07-26 09:52:32 -0400

    MISC: s/larslasso_results/lars_lasso_results

--- benchmarks/bench_lasso.py
+++ benchmarks/bench_lasso.py
@@ -21,7 +21,7 @@ from scikits.learn.datasets.samples_generator import make_regression_dataset
 def compute_bench(alpha, n_samples, n_features, precompute):
 
     lasso_results = []
-    larslasso_results = []
+    lars_lasso_results = []
 
     n_test_samples = 0
     it = 0
@@ -52,9 +52,9 @@ def compute_bench(alpha, n_samples, n_features, precompute):
             clf = LassoLars(alpha=alpha, fit_intercept=False)
             tstart = time()
             clf.fit(X, Y, normalize=False, precompute=precompute)
-            larslasso_results.append(time() - tstart)
+            lars_lasso_results.append(time() - tstart)
 
-    return lasso_results, larslasso_results
+    return lasso_results, lars_lasso_results
 
 
 if __name__ == '__main__':
@@ -65,14 +65,14 @@ if __name__ == '__main__':
 
     n_features = 10
     list_n_samples = np.linspace(100, 1000000, 5).astype(np.int)
-    lasso_results, larslasso_results = compute_bench(alpha, list_n_samples,
+    lasso_results, lars_lasso_results = compute_bench(alpha, list_n_samples,
                                             [n_features], precompute=True)
 
     pl.clf()
     pl.subplot(211)
     pl.plot(list_n_samples, lasso_results, 'b-',
                             label='Lasso (with precomputed Gram matrix)')
-    pl.plot(list_n_samples, larslasso_results, 'r-',
+    pl.plot(list_n_samples, lars_lasso_results, 'r-',
                             label='LassoLars (with precomputed Gram matrix)')
     pl.title('Lasso benchmark (%d features - alpha=%s)' % (n_features, alpha))
     pl.legend(loc='upper left')
@@ -82,11 +82,11 @@ if __name__ == '__main__':
 
     n_samples = 2000
     list_n_features = np.linspace(500, 3000, 5).astype(np.int)
-    lasso_results, larslasso_results = compute_bench(alpha, [n_samples],
+    lasso_results, lars_lasso_results = compute_bench(alpha, [n_samples],
                                            list_n_features, precompute=False)
     pl.subplot(212)
     pl.plot(list_n_features, lasso_results, 'b-', label='Lasso')
-    pl.plot(list_n_features, larslasso_results, 'r-', label='LassoLars')
+    pl.plot(list_n_features, lars_lasso_results, 'r-', label='LassoLars')
     pl.title('Lasso benchmark (%d samples - alpha=%s)' % (n_samples, alpha))
     pl.legend(loc='upper left')
     pl.xlabel('number of features')
