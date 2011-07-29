4309819304fcc46a837ebcf0a8702a72bb05b358
Robert Layton <robertlayton@gmail.com>
2011-07-29 21:43:52 +1000

    Test using a callable function as the metric

--- scikits/learn/cluster/tests/test_dbscan.py
+++ scikits/learn/cluster/tests/test_dbscan.py
@@ -65,3 +65,31 @@ def test_dbscan_feature():
 
     n_clusters_2 = len(set(labels)) - int(-1 in labels)
     assert_equal(n_clusters_2, n_clusters)
+
+
+def test_dbscan_callable():
+    """
+    Tests the DBSCAN algorithm with a callable metric
+
+    """
+    # Parameters chosen specifically for this task.
+    # Different eps to other test, because distance is not normalised.
+    eps = 0.8
+    min_points = 10
+    # metric is the function reference, not the string key.
+    metric = distance.euclidean
+    # Compute DBSCAN
+    # parameters chosen for task
+    core_points, labels = dbscan(X, metric=metric,
+                                 eps=eps, min_points=min_points)
+
+    # number of clusters, ignoring noise if present
+    n_clusters_1 = len(set(labels)) - int(-1 in labels)
+    assert_equal(n_clusters_1, n_clusters)
+
+    db = DBSCAN()
+    labels = db.fit(X, metric=metric,
+                    eps=eps, min_points=min_points).labels_
+
+    n_clusters_2 = len(set(labels)) - int(-1 in labels)
+    assert_equal(n_clusters_2, n_clusters)
