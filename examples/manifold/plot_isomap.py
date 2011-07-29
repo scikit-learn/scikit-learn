5e852ea306f8e2c1ff0aff180d1ec161fe41d8b7
Jake Vanderplas <vanderplas@astro.washington.edu>
2011-07-28 19:38:18 -0700

    Revert "combined manifold examples"
    This reverts commit 89a81ff222cfc3b7f6de198a5b8786f76db7cdc1.

---
+++ examples/manifold/plot_isomap.py
@@ -0,0 +1,55 @@
+"""
+===========================================
+ Isomap example with s-curve dataset
+===========================================
+
+An illustration of dimensionality reduction
+with Isomap.
+"""
+
+# Author: Jake Vanderplas == <vanderplas@astro.washington.edu>
+
+print __doc__
+
+from time import time
+
+import numpy as np
+import pylab
+
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.ticker import NullFormatter
+
+from scikits.learn import manifold, datasets
+
+N = 1000
+n_neighbors = 15
+out_dim = 2
+
+#X, color = datasets.samples_generator.swiss_roll(N)
+X, color = datasets.samples_generator.s_curve(N)
+
+print "Computing Isomap embedding on %i points" % N
+
+t0 = time()
+Y = manifold.isomap(X, n_neighbors, out_dim, path_method='best')
+t1 = time()
+print " - completed in %.2g sec" % (t1 - t0)
+
+fig = pylab.figure(figsize=(6, 10))
+
+try:
+    # compatibility matplotlib < 1.0
+    ax = fig.add_subplot(211, projection='3d')
+    ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=color, cmap=pylab.cm.Spectral)
+    ax.view_init(4, -72)
+except:
+    ax = fig.add_subplot(211)
+    ax.scatter(X[:, 0], X[:, 2], c=color, cmap=pylab.cm.Spectral)
+
+ax.set_title('Original Data')
+
+pylab.subplot(212)
+pylab.scatter(Y[:, 0], Y[:, 1], c=color, cmap=pylab.cm.Spectral)
+pylab.title("Isomap Embedding")
+
+pylab.show()
