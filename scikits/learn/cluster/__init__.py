43ec924fb8c49601b91cf1f2fc701524e323ac37
Robert Layton <robertlayton@gmail.com>
2011-07-30 13:40:06 +1000

    Deleted line I returned that I really didn't delete.

--- scikits/learn/cluster/__init__.py
+++ scikits/learn/cluster/__init__.py
@@ -8,4 +8,3 @@ from .hierarchical import ward_tree, Ward, WardAgglomeration
 from .k_means_ import k_means, KMeans, MiniBatchKMeans
 from .dbscan_ import dbscan, DBSCAN
 
-from ._k_means import randindex
