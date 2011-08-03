FIX: potential division by zero when normalizing non-pruned CSR matrices

scikits/learn/preprocessing/tests/test_preprocessing.py
@@ -120,13 +120,22 @@ def test_scaler_without_centering():
 
 def test_normalizer_l1():
     rng = np.random.RandomState(0)
-    X_orig = rng.randn(4, 5)
-    X_orig[3, :] = 0.0
+    X_dense = rng.randn(4, 5)
+    X_sparse_unpruned = sp.csr_matrix(X_dense)
+
+    # set the row number 3 to zero
+    X_dense[3, :] = 0.0
+
+    # set the row number 3 to zero without pruning (can happen in real life)
+    indptr_3 = X_sparse_unpruned.indptr[3]
+    indptr_4 = X_sparse_unpruned.indptr[4]
+    X_sparse_unpruned.data[indptr_3:indptr_4] = 0.0
+
+    # build the pruned variant using the regular constructor
+    X_sparse_pruned = sp.csr_matrix(X_dense)
 
     # check inputs that support the no-copy optim
-    for init in (np.array, sp.csr_matrix):
-
-        X = init(X_orig.copy())
+    for X in (X_dense, X_sparse_pruned, X_sparse_unpruned):
 
         normalizer = Normalizer(norm='l1', copy=True)
         X_norm = normalizer.transform(X)
@@ -146,7 +155,7 @@ def test_normalizer_l1():
 
     # check input for which copy=False won't prevent a copy
     for init in (sp.coo_matrix, sp.csc_matrix, sp.lil_matrix):
-        X = init(X_orig.copy())
+        X = init(X_dense)
         X_norm = normalizer = Normalizer(norm='l2', copy=False).transform(X)
 
         assert X_norm is not X
@@ -160,13 +169,22 @@ def test_normalizer_l1():
 
 def test_normalizer_l2():
     rng = np.random.RandomState(0)
-    X_orig = rng.randn(4, 5)
-    X_orig[3, :] = 0.0
+    X_dense = rng.randn(4, 5)
+    X_sparse_unpruned = sp.csr_matrix(X_dense)
+
+    # set the row number 3 to zero
+    X_dense[3, :] = 0.0
+
+    # set the row number 3 to zero without pruning (can happen in real life)
+    indptr_3 = X_sparse_unpruned.indptr[3]
+    indptr_4 = X_sparse_unpruned.indptr[4]
+    X_sparse_unpruned.data[indptr_3:indptr_4] = 0.0
+
+    # build the pruned variant using the regular constructor
+    X_sparse_pruned = sp.csr_matrix(X_dense)
 
     # check inputs that support the no-copy optim
-    for init in (np.array, sp.csr_matrix):
-
-        X = init(X_orig.copy())
+    for X in (X_dense, X_sparse_pruned, X_sparse_unpruned):
 
         normalizer = Normalizer(norm='l2', copy=True)
         X_norm1 = normalizer.transform(X)
@@ -185,7 +203,7 @@ def test_normalizer_l2():
 
     # check input for which copy=False won't prevent a copy
     for init in (sp.coo_matrix, sp.csc_matrix, sp.lil_matrix):
-        X = init(X_orig.copy())
+        X = init(X_dense)
         X_norm = normalizer = Normalizer(norm='l2', copy=False).transform(X)
 
         assert X_norm is not X