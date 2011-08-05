8310b0814e0d052f78dda952a5c00a7e99e7190e
Conrad Lee <conradlee@gmail.com>
2011-08-05 18:39:03 +0200

    Changed scikits.learn.cluster.__init__.py and examples/cluster/plot_mean_shift.py: modified so that complies with pep8.

--- examples/cluster/plot_mean_shift.py
+++ examples/cluster/plot_mean_shift.py
@@ -14,12 +14,12 @@ import numpy as np
 from scikits.learn.cluster import MeanShift, estimate_bandwidth
 from scikits.learn.datasets.samples_generator import make_blobs
 
-################################################################################
+###############################################################################
 # Generate sample data
 centers = [[1, 1], [-1, -1], [1, -1]]
 X, _ = make_blobs(n_samples=5000, centers=centers, cluster_std=0.6)
 
-################################################################################
+###############################################################################
 # Compute clustering with MeanShift
 print "Estimating bandwidth"
 bandwidth = estimate_bandwidth(X, quantile=0.3)
@@ -34,7 +34,7 @@ n_clusters_ = len(labels_unique)
 
 print "number of estimated clusters : %d" % n_clusters_
 
-################################################################################
+###############################################################################
 # Plot result
 import pylab as pl
 from itertools import cycle
@@ -46,7 +46,7 @@ colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
 for k, col in zip(range(n_clusters_), colors):
     my_members = labels == k
     cluster_center = cluster_centers[k]
-    pl.plot(X[my_members,0], X[my_members,1], col+'.')
+    pl.plot(X[my_members, 0], X[my_members, 1], col + '.')
     pl.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
                                     markeredgecolor='k', markersize=14)
 pl.title('Estimated number of clusters: %d' % n_clusters_)
