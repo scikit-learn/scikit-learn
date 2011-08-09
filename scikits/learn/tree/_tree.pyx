361cfef22a0535e6b992a4ae4178315d671bcb11
Brian Holt <bdholt1@gmail.com>
2011-08-09 14:53:19 +0100

    FIX: corrected computation error and typed incoming arrays

--- scikits/learn/tree/_tree.pyx
+++ scikits/learn/tree/_tree.pyx
@@ -31,8 +31,7 @@ cdef extern from "math.h":
     be the proportion of class k observations in node m   
 """  
 
-#@cython.profile(False)
-cpdef double eval_gini(np.ndarray labels, 
+cpdef double eval_gini(np.ndarray[np.int_t, ndim=1] labels, 
                        np.ndarray[np.float_t, ndim=1] pm) except * :
     """
         
@@ -40,49 +39,43 @@ cpdef double eval_gini(np.ndarray labels,
                    = 1 - \sum_{k=0}^{K-1} pmk ** 2
             
     """       
-    cdef int *_labels = <int *>labels.data
     cdef int n_labels = labels.shape[0]
-    
-    cdef double *_pm = <double *>pm.data
     cdef int K = pm.shape[0]    
     
     cdef int i = 0
     for i in range(K):
-        _pm[i] = 0.
+        pm[i] = 0.
     
     cdef int value
-    for i in range(n_labels):
-        value = int(_labels[i])
-        _pm[value] += 1. / n_labels
+    for i in range(n_labels):       
+        value = labels[i]
+        pm[value] += 1. / n_labels
     
     cdef double H = 1.
     cdef Py_ssize_t k
-    for k in range(K):    
-        H -=  pow(_pm[k],2) 
+    for k in range(K): 
+        H -=  pow(pm[k],2) 
          
     return H   
 
-cpdef double eval_entropy(np.ndarray labels,
+cpdef double eval_entropy(np.ndarray[np.int_t, ndim=1] labels,
                           np.ndarray[np.float_t, ndim=1] pm) except * :
     """
         
         Cross Entropy = - \sum_{k=0}^{K-1} pmk log(pmk)
             
     """
-    cdef int *_labels = <int *>labels.data
     cdef int n_labels = labels.shape[0]
-    
-    cdef double *_pm = <double *>pm.data
     cdef int K = pm.shape[0]    
     
     cdef int i = 0
     for i in range(K):
-        _pm[i] = 0.
+        pm[i] = 0.
     
     cdef int value
-    for i in range(n_labels):
-        value = int(_labels[i])
-        _pm[value] += 1. / n_labels
+    for i in range(n_labels):       
+        value = labels[i]
+        pm[value] += 1. / n_labels
         
     cdef double H = 0.
     cdef Py_ssize_t k
@@ -92,27 +85,24 @@ cpdef double eval_entropy(np.ndarray labels,
          
     return H   
 
-cpdef double eval_miss(np.ndarray labels,
+cpdef double eval_miss(np.ndarray[np.int_t, ndim=1] labels,
                        np.ndarray[np.float_t, ndim=1] pm) except * :
     """
         
         Misclassification error = (1 - pmk)
             
     """
-    cdef int *_labels = <int *>labels.data
     cdef int n_labels = labels.shape[0]
-    
-    cdef double *_pm = <double *>pm.data
     cdef int K = pm.shape[0]    
     
     cdef int i = 0
     for i in range(K):
-        _pm[i] = 0.
+        pm[i] = 0.
     
     cdef int value
-    for i in range(n_labels):
-        value = int(_labels[i])
-        _pm[value] += 1. / n_labels
+    for i in range(n_labels):       
+        value = labels[i]
+        pm[value] += 1. / n_labels
         
     cdef double H = 1. - pm.max()
     
@@ -120,25 +110,24 @@ cpdef double eval_miss(np.ndarray labels,
  Regression entropy measures
  
 """      
-cpdef double eval_mse(np.ndarray labels,
+cpdef double eval_mse(np.ndarray[np.float_t, ndim=1] labels,
                       np.ndarray[np.float_t, ndim=1] pm) except * :
     """             
         MSE =  \sum_i (y_i - c0)^2  / N
         
-        pm is a redundant argument.    
+        pm is a redundant argument (intentional).    
     """     
-    cdef double *_labels = <double *>labels.data
     cdef int n_labels = labels.shape[0]
 
     cdef float c0
     cdef Py_ssize_t i = 0
     for i in range(n_labels):
-        c0 += _labels[i]
+        c0 += labels[i]
     c0 /= n_labels
 
     cdef double H = 0.
     for i in range(n_labels):
-        H += pow(_labels[i] - c0, 2) 
+        H += pow(labels[i] - c0, 2) 
     H /= n_labels
         
     return H
