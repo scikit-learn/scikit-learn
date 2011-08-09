12fc1dc830cc1938813b0bcf537fa95775951a45
Clay Woolam <clay@woolam.org>
2011-08-09 16:02:28 -0700

    example conforms to pep8

--- examples/label_propagation/plot_label_propagation_structure.py
+++ examples/label_propagation/plot_label_propagation_structure.py
@@ -4,9 +4,9 @@ Plot Label Propagation learning a complex structure
 ===================================================
 
 Example of LabelPropagation learning a complex internal structure
-to demonstrate "manifold learning". The outer circle should be 
+to demonstrate "manifold learning". The outer circle should be
 labeled "red" and the inner circle "blue". Because both label groups
-lie inside their own distinct shape, we can see that the labels 
+lie inside their own distinct shape, we can see that the labels
 propagate correctly around the circle.
 """
 print __doc__
@@ -32,26 +32,33 @@ inner_circ_ys = np.sin(np.linspace(0, 2 * np.pi, n_samples_per_circle)) * 0.45
 all_xs = np.append(outer_circ_xs, inner_circ_xs)
 all_ys = np.append(outer_circ_ys, inner_circ_ys)
 data = zip(outer_circ_xs, outer_circ_ys) + zip(inner_circ_xs, inner_circ_ys)
-labels = ['outer'] + ['unlabeled' for x in range(0, n_samples_per_circle-1)] + ['inner'] + ['unlabeled' for x in range(0, n_samples_per_circle-1)]
+labels = ['outer'] +\
+         ['unlabeled' for x in range(0, n_samples_per_circle - 1)] +\
+         ['inner'] +\
+         ['unlabeled' for x in range(0, n_samples_per_circle - 1)]
 
-label_spread.fit(data, labels, unlabeled_identifier='unlabeled') 
+label_spread.fit(data, labels, unlabeled_identifier='unlabeled')
 
 output_labels = label_spread.transduction
 
-pl.subplot(2,1,1)
+pl.subplot(2, 1, 1)
 plot_outer_labeled, = pl.plot(outer_circ_xs[0], outer_circ_ys[0], 'rs-')
-plot_unlabeled, = pl.plot(outer_circ_xs[1:] + inner_circ_xs[1:], outer_circ_ys[1:] + inner_circ_ys[1:], 'g.')
+plot_unlabeled, = pl.plot(outer_circ_xs[1:] + inner_circ_xs[1:], \
+        outer_circ_ys[1:] + inner_circ_ys[1:], 'g.')
 plot_inner_labeled, = pl.plot(inner_circ_xs[0], inner_circ_ys[0], 'bs-')
-pl.legend((plot_outer_labeled, plot_inner_labeled, plot_unlabeled), ('Outer Labeled', 'Inner Labeled', 'Unlabeled'), 'upper left', numpoints=1, shadow=False)
+pl.legend((plot_outer_labeled, plot_inner_labeled, plot_unlabeled), \
+        ('Outer Labeled', 'Inner Labeled', 'Unlabeled'), 'upper left', \
+        numpoints=1, shadow=False)
 pl.title("Raw data (2 classes=red and blue)")
 
-pl.subplot(2,1,2)
-output_label_array = np.asarray(output_labels) 
+pl.subplot(2, 1, 2)
+output_label_array = np.asarray(output_labels)
 outer_numbers = np.where(output_label_array == 'outer')
 inner_numbers = np.where(output_label_array == 'inner')
 plot_outer, = pl.plot(all_xs[outer_numbers], all_ys[outer_numbers], 'rs-')
 plot_inner, = pl.plot(all_xs[inner_numbers], all_ys[inner_numbers], 'bs-')
-pl.legend((plot_outer, plot_inner), ('Outer Learned', 'Inner Learned'), 'upper left', numpoints=1, shadow=False)
+pl.legend((plot_outer, plot_inner), ('Outer Learned', 'Inner Learned'), \
+        'upper left', numpoints=1, shadow=False)
 pl.title("Labels learned with Label Spreading")
 
 pl.show()
