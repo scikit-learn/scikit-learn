6d4231c29a86f641f6b6bd593733eed4d5341fc1
Clay Woolam <clay@woolam.org>
2011-08-09 16:07:51 -0700

    other example conforms to pep8

--- examples/label_propagation/label_propagation_versus_svm_iris.py
+++ examples/label_propagation/label_propagation_versus_svm_iris.py
@@ -4,16 +4,17 @@ Plot Label Propagation versus SVM with iris dataset
 ==================================================
 
 Performance comparison between Label Propagation in the semi-supervised setting
-to SVM in the supervised setting in the iris dataset. 
+to SVM in the supervised setting in the iris dataset.
 
-The first 9 experiments: SVM (SVM), Label Propagation (LP), Label Spreading (LS)
+First 9 experiments: SVM (SVM), Label Propagation (LP), Label Spreading (LS)
 operate in the "inductive setting". That is, the system is trained with some
-percentage of data and then queried against unseen datapoints to infer a label. 
+percentage of data and then queried against unseen datapoints to infer a label.
 
-The final 10th experiment is in the transductive setting. Using a label spreading 
-algorithm, the system is trained with approximately 24 percent of the data labeled
-and during training, unlabeled points are transductively assigned values. The test 
-precision, recall, and F1 scores are based on these transductively assigned labels.
+The final 10th experiment is in the transductive setting. Using a label
+spreading algorithm, the system is trained with approximately 24 percent of the
+data labeled and during training, unlabeled points are transductively assigned
+values. The test precision, recall, and F1 scores are based on these
+transductively assigned labels.
 """
 print __doc__
 
@@ -46,10 +47,12 @@ Y_all = Y[train]
 
 svc = svm.SVC(kernel='rbf')
 svc.fit(X_all, Y_all)
-print "Limited Label data example" 
+print "Limited Label data example"
 print "Test name\tprecision\trecall   \tf1"
-print "SVM 80.0pct\t%0.6f\t%0.6f\t%0.6f" % (precision_score(svc.predict(X[test]), Y[test]),
-       recall_score(svc.predict(X[test]), Y[test]), f1_score(svc.predict(X[test]), Y[test]))
+print "SVM 80.0pct\t%0.6f\t%0.6f\t%0.6f" %\
+        (precision_score(svc.predict(X[test]), Y[test]),\
+         recall_score(svc.predict(X[test]), Y[test]),\
+         f1_score(svc.predict(X[test]), Y[test]))
 print "-------"
 
 for num in [0.2, 0.3, 0.4]:
@@ -59,16 +62,20 @@ for num in [0.2, 0.3, 0.4]:
     Y_dup = np.copy(Y_all)
     Y_dup[train_new] = -1
     lp.fit(X_all, Y_dup)
-    print "LP %0.1fpct\t%0.6f\t%0.6f\t%0.6f" % (80 * num, precision_score(lp.predict(X[test]), Y[test]),
-       recall_score(lp.predict(X[test]), Y[test]), f1_score(lp.predict(X[test]), Y[test]))
+    print "LP %0.1fpct\t%0.6f\t%0.6f\t%0.6f" % \
+            (80 * num, precision_score(lp.predict(X[test]), Y[test]),\
+             recall_score(lp.predict(X[test]), Y[test]),\
+             f1_score(lp.predict(X[test]), Y[test]))
 
 lp = label_propagation.LabelPropagation()
 lp.fit(X_all, Y_all, suppress_warning=True)
-print "LP 80.0pct\t%0.6f\t%0.6f\t%0.6f" % (precision_score(lp.predict(X[test]), Y[test]),
-   recall_score(lp.predict(X[test]), Y[test]), f1_score(lp.predict(X[test]), Y[test]))
+print "LP 80.0pct\t%0.6f\t%0.6f\t%0.6f" % \
+        (precision_score(lp.predict(X[test]), Y[test]),\
+         recall_score(lp.predict(X[test]), Y[test]),\
+         f1_score(lp.predict(X[test]), Y[test]))
 
 
-# label spreading 
+# label spreading
 for num in [0.2, 0.3, 0.4]:
     lspread = label_propagation.LabelSpreading()
     hold_new = np.random.rand(len(train)) > num
@@ -76,12 +83,16 @@ for num in [0.2, 0.3, 0.4]:
     Y_dup = np.copy(Y_all)
     Y_dup[train_new] = -1
     lspread.fit(X_all, Y_dup)
-    print "LS %0.1fpct\t%0.6f\t%0.6f\t%0.6f" % (80 * num, precision_score(lspread.predict(X[test]), Y[test]),
-       recall_score(lspread.predict(X[test]), Y[test]), f1_score(lspread.predict(X[test]), Y[test]))
+    print "LS %0.1fpct\t%0.6f\t%0.6f\t%0.6f" % \
+            (80 * num, precision_score(lspread.predict(X[test]), Y[test]),\
+             recall_score(lspread.predict(X[test]), Y[test]), \
+             f1_score(lspread.predict(X[test]), Y[test]))
 lspread = label_propagation.LabelSpreading(alpha=1)
 lspread.fit(X_all, Y_all, suppress_warning=True)
-print "LS 80.0pct\t%0.6f\t%0.6f\t%0.6f" % (precision_score(lspread.predict(X[test]), Y[test]),
-   recall_score(lspread.predict(X[test]), Y[test]), f1_score(lspread.predict(X[test]), Y[test]))
+print "LS 80.0pct\t%0.6f\t%0.6f\t%0.6f" % \
+        (precision_score(lspread.predict(X[test]), Y[test]),\
+         recall_score(lspread.predict(X[test]), Y[test]), \
+         f1_score(lspread.predict(X[test]), Y[test]))
 
 print "-------"
 lspread = label_propagation.LabelSpreading(alpha=0.8)
@@ -92,5 +103,7 @@ Y_dup = np.copy(Y)
 Y_dup[train_new] = -1
 lspread.fit(X, Y, suppress_warning=True)
 trans_result = np.asarray(lspread.transduction)
-print "LS 20tran\t%0.6f\t%0.6f\t%0.6f" % (precision_score(trans_result[test], Y[test]),
-   recall_score(trans_result[test], Y[test]), f1_score(trans_result[test], Y[test]))
+print "LS 20tran\t%0.6f\t%0.6f\t%0.6f" % \
+        (precision_score(trans_result[test], Y[test]),
+         recall_score(trans_result[test], Y[test]), \
+         f1_score(trans_result[test], Y[test]))
