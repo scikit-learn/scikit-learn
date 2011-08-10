7af246b94dfc5e84a736a644e795a123ca5fc8fa
Gael varoquaux <gael.varoquaux@normalesup.org>
2011-08-11 00:28:16 +0200

    COSMIT: tweak plot_compare_methods example layout

--- examples/manifold/plot_compare_methods.py
+++ examples/manifold/plot_compare_methods.py
@@ -27,8 +27,7 @@ print __doc__
 
 from time import time
 
-import numpy
-import pylab
+import pylab as pl
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.ticker import NullFormatter
 
@@ -41,18 +40,18 @@ out_dim = 2
 methods = ['standard', 'ltsa', 'hessian', 'modified']
 labels = ['LLE', 'LTSA', 'Hessian LLE', 'Modified LLE']
 
-fig = pylab.figure(figsize=(8, 12))
-pylab.suptitle("Manifold Learning with %i points, %i neighbors"
+fig = pl.figure(figsize=(12, 8))
+pl.suptitle("Manifold Learning with %i points, %i neighbors"
                % (1000, n_neighbors), fontsize=14)
 
 try:
     # compatibility matplotlib < 1.0
-    ax = fig.add_subplot(321, projection='3d')
-    ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=color, cmap=pylab.cm.Spectral)
+    ax = fig.add_subplot(231, projection='3d')
+    ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=color, cmap=pl.cm.Spectral)
     ax.view_init(4, -72)
 except:
-    ax = fig.add_subplot(321, projection='3d')
-    ax.scatter(X[:, 0], X[:, 2], c=color, cmap=pylab.cm.Spectral)
+    ax = fig.add_subplot(231, projection='3d')
+    pl.scatter(X[:, 0], X[:, 2], c=color, cmap=pl.cm.Spectral)
 
 ax.set_title('Original Data')
 
@@ -64,20 +63,21 @@ for i, method in enumerate(methods):
     t1 = time()
     print "%s: %.2g sec" % (methods[i], t1 - t0)
 
-    ax = fig.add_subplot(322 + i)
-    ax.scatter(Y[:, 0], Y[:, 1], c=color, cmap=pylab.cm.Spectral)
-    ax.set_title("%s (%.2g sec)" % (labels[i], t1 - t0))
+    ax = fig.add_subplot(232 + i)
+    pl.scatter(Y[:, 0], Y[:, 1], c=color, cmap=pl.cm.Spectral)
+    pl.title("%s (%.2g sec)" % (labels[i], t1 - t0))
     ax.xaxis.set_major_formatter(NullFormatter())
     ax.yaxis.set_major_formatter(NullFormatter())
+    pl.axis('tight')
 
 t0 = time()
 Y = manifold.Isomap(n_neighbors, out_dim).fit_transform(X)
 t1 = time()
 print "Isomap: %.2g sec" % (t1 - t0)
-ax = fig.add_subplot(326)
-ax.scatter(Y[:, 0], Y[:, 1], c=color, cmap=pylab.cm.Spectral)
-ax.set_title("Isomap (%.2g sec)" % (t1 - t0))
+ax = fig.add_subplot(236)
+pl.scatter(Y[:, 0], Y[:, 1], c=color, cmap=pl.cm.Spectral)
+pl.title("Isomap (%.2g sec)" % (t1 - t0))
 ax.xaxis.set_major_formatter(NullFormatter())
 ax.yaxis.set_major_formatter(NullFormatter())
 
-pylab.show()
+pl.show()
