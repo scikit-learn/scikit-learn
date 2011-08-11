3493ac509cd1022f1d0bd28c430a6fb6afdf3e2b
Brian Holt <bdholt1@gmail.com>
2011-08-11 10:17:28 +0100

    removed everything except the plain and simple decision tree to make reviewing easier

--- scikits/learn/tree/__init__.py
+++ scikits/learn/tree/__init__.py
@@ -5,6 +5,5 @@ Tree-based models
 scikits.learn.tree is a module to fit data to decision trees. 
 
 """
-from .decision_tree import DecisionTree
 from .tree import DecisionTreeClassifier
 from .tree import DecisionTreeRegressor
