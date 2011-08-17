468c5536a18e0bd21c3dbb0ea182622a9a1d2f86
Jean Kossaifi <jean.kossaifi@gmail.com>
2011-08-17 19:00:57 +0200

    PEP8 : replaced tabulations by spaces

--- scikits/learn/utils/tests/test___init__.py
+++ scikits/learn/utils/tests/test___init__.py
@@ -1,21 +1,22 @@
 import numpy as np
 from .. import as_float_array
 
+
 def test_as_float_array():
-	"""
-	Test function for as_float_array
-	"""
-	# Checks that the return type is ok
-	X = np.ones((3, 10), dtype=np.int32)
-	X = X + np.arange(10, dtype=np.int32)
-	X2 = as_float_array(X, overwrite_X=True)
-	np.testing.assert_equal(X2.dtype, np.float32)
-	# Checking that the array was actually overwritten
-	np.testing.assert_equal(X.dtype, np.float32)
-	# Another test
-	X = X.astype(np.int64)
-	X2 = as_float_array(X, overwrite_X=False)
-	# Checking that the array wasn't overwritten
-	np.testing.assert_equal(X.dtype, np.int64)
-	# Checking that the new type is ok
-	np.testing.assert_equal(X2.dtype, np.float64)
+    """
+    Test function for as_float_array
+    """
+    # Checks that the return type is ok
+    X = np.ones((3, 10), dtype=np.int32)
+    X = X + np.arange(10, dtype=np.int32)
+    X2 = as_float_array(X, overwrite_X=True)
+    np.testing.assert_equal(X2.dtype, np.float32)
+    # Checking that the array was actually overwritten
+    np.testing.assert_equal(X.dtype, np.float32)
+    # Another test
+    X = X.astype(np.int64)
+    X2 = as_float_array(X, overwrite_X=False)
+    # Checking that the array wasn't overwritten
+    np.testing.assert_equal(X.dtype, np.int64)
+    # Checking that the new type is ok
+    np.testing.assert_equal(X2.dtype, np.float64)
