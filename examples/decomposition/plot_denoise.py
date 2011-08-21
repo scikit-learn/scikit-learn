6c7711ca3f1fb0f8e073292987ef2132a8d8dac6
vene <vlad@vene.ro>
2011-08-21 23:07:17 +0200

    redesign the denoising example

--- examples/decomposition/plot_denoise.py
+++ examples/decomposition/plot_denoise.py
@@ -20,6 +20,8 @@ from scikits.learn.feature_extraction.image import extract_patches_2d, \
 ###############################################################################
 # Load Lena image and extract patches
 lena = sp.lena()
+# downsample
+lena = lena[::2, ::2] + lena[1::2, ::2] + lena[::2, 1::2] + lena[1::2, 1::2]
 lenb = lena.copy()
 
 print "Extracting clean patches..."
@@ -37,60 +39,57 @@ V = dico.fit(data).components_
 ###############################################################################
 # Generate noisy data and reconstruct using various methods
 print ""  # a line break
-print "Distorting image fragments..."
-fragments = [(slice(200, 300), slice(200, 300)),
-             (slice(200, 300), slice(300, 400)),
-             (slice(300, 400), slice(200, 300)),
-             (slice(300, 400), slice(300, 400))]
+print "Distorting image..."
 
-for i, fragment in enumerate(fragments):
-    lena[fragment] += 50 * np.random.randn(100, 100)
-    img = lena[fragment]
-    data = extract_patches_2d(img, (6, 6))
-    data = data.reshape(len(data), -1)
-    data -= intercept
-    print "Reconstructing image fragment %d..." % (i + 1),
-    t0 = time()
-    if i == 0:
-        code = dico.transform(data, n_nonzero_coefs=1, precompute_gram=True)
-    elif i == 1:
-        code = dico.transform(data, n_nonzero_coefs=2, precompute_gram=True)
-    elif i == 2:
-        dico.transform_algorithm = "lars"
-        code = dico.transform(data, max_iter=5)
-    elif i == 3:
-        dico.transform_algorithm = "threshold"
-        code = dico.transform(data, alpha=1.0)
-    dt = time() - t0
-    print " done in %.2fs" % dt
-    data = np.dot(code, V) + intercept
-    data = data.reshape(len(data), 6, 6)
+lena += 50 * np.random.randn(*lena.shape)
 
-    lenb[fragment] = reconstruct_from_patches_2d(data, (100, 100))
-    if i == 3:  # tresholding breaks the range
-        lenb[fragment] -= lenb[fragment].min()
-        lenb[fragment] = lenb[fragment] / float(lenb.max()) * 256.0
+print "Extracting patches..."
+data = extract_patches_2d(lena, (6, 6))
+data = data.reshape(len(data), -1)
 
-###############################################################################
-# Display the results
+transform_algorithms = [
+    ('1-Orthogonal Matching Pursuit', 'omp',
+     {'n_nonzero_coefs': 1, 'precompute_gram': True}),
+
+    ('2-Orthogonal Matching Pursuit', 'omp',
+     {'n_nonzero_coefs': 2, 'precompute_gram': True}),
+
+    ('Least-angle regression', 'lars',
+     {'max_iter': 5}),
+
+    ('Threshold at 1', 'threshold',
+     {'alpha': 1.0})]
+
+######################
+# Display the original
+i = 1
 vmin, vmax = 0, 256
-pl.figure(figsize=(9, 5))
-pl.subplot(1, 2, 1)
+pl.figure(figsize=(10, 4))
+pl.subplot(1, 5, i)
 pl.title("Noisy image")
 pl.imshow(lena, vmin=vmin, vmax=vmax, cmap=pl.cm.gray, interpolation='nearest')
 pl.xticks(())
 pl.yticks(())
-pl.subplot(1, 2, 2)
-pl.title("Reconstructed image")
-pl.imshow(lenb, vmin=vmin, vmax=vmax, cmap=pl.cm.gray, interpolation='nearest')
-pl.xticks(())
-pl.yticks(())
-bbox_props = dict(boxstyle="round", fc="w", ec="0.5", alpha=0.9)
-params = dict(ha="center", va="center", size=12, bbox=bbox_props)
-pl.text(150, 100, "OMP-1", **params)
-pl.text(400, 100, "OMP-2", **params)
-pl.text(150, 450, "LARS", **params)
-pl.text(400, 450, "Threshold", **params)
-pl.suptitle("Image denoising with dictionary learning", fontsize=16)
-pl.subplots_adjust(0.02, 0.1, 0.98, 0.84, 0.02, 0.2)
+
+for title, transform_algorithm, fit_params in transform_algorithms:
+    print title,
+    t0 = time()
+    dico.transform_algorithm = transform_algorithm
+    code = dico.transform(data, **fit_params)
+    patches = np.dot(code, V) + intercept
+    patches = data.reshape(len(data), 6, 6)
+    if transform_algorithm == 'treshold':
+        patches -= data.min()
+        patches = data / float(data.max() * 256.0)
+    lena = reconstruct_from_patches_2d(patches, lena.shape)
+    print time() - t0
+
+    i += 1
+    pl.subplot(1, 5, i)
+    pl.title(title)
+    pl.imshow(lena, vmin=vmin, vmax=vmax, cmap=pl.cm.gray,
+    interpolation='nearest')
+    pl.xticks(())
+    pl.yticks(())
+
 pl.show()
