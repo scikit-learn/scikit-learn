5a9f68a5bc7882da99701c29006b332a3a03d5e4
vene <vlad@vene.ro>
2011-08-23 13:05:24 +0200

    Data centering in denoising example

--- examples/decomposition/plot_denoise.py
+++ examples/decomposition/plot_denoise.py
@@ -3,7 +3,10 @@
 Image denoising using dictionary learning
 =========================================
 An example comparing the effect of reconstructing noisy fragments
-of Lena using a dictionary learned from the clear image.
+of Lena using online :ref:`DictionaryLearning` and various transform methods.
+
+The dictionary is fitted on the non-distorted left half of the image, and
+subsequently used to reconstruct the right half.
 """
 print __doc__
 
@@ -20,45 +23,34 @@ from scikits.learn.feature_extraction.image import extract_patches_2d, \
 ###############################################################################
 # Load Lena image and extract patches
 lena = sp.lena() / 256.0
-# downsample
+
+# downsample for higher speed
 lena = lena[::2, ::2] + lena[1::2, ::2] + lena[::2, 1::2] + lena[1::2, 1::2]
 lena = lena[::2, ::2] + lena[1::2, ::2] + lena[::2, 1::2] + lena[1::2, 1::2]
 lena /= 16.0
-
 height, width = lena.shape
-print "Distorting image..."
 
+# Distort the right half of the image
+print "Distorting image..."
 lena[:, height/2:] += 0.075 * np.random.randn(width, height/2)
 
+# Extract all clean patches from the left half of the image
 print "Extracting clean patches..."
 patch_size = (4, 4)
-data = extract_patches_2d(lena[:, :height/2], patch_size, random_state=0)
+data = extract_patches_2d(lena[:, :height/2], patch_size)
 data = data.reshape(data.shape[0], -1)
-#intercept = np.mean(data, 0)
-#data -= intercept
+intercept = np.mean(data, 0)
+data -= intercept
 
 ###############################################################################
 # Learn the dictionary from clean patches
 dico = DictionaryLearningOnline(n_atoms=100, alpha=1e-5, n_iter=300,
                                 verbose=True, transform_algorithm='omp')
 V = dico.fit(data).components_
+print ""
 
 ###############################################################################
-# Generate noisy data and reconstruct using various methods
-print ""  # a line break
-
-transform_algorithms = [
-    ('1-Orthogonal Matching Pursuit', 'omp',
-     {'n_nonzero_coefs': 1, 'precompute_gram': True}),
-
-    ('2-Orthogonal Matching Pursuit', 'omp',
-     {'n_nonzero_coefs': 2, 'precompute_gram': True}),
-
-    ('Least-angle regression', 'lars',
-     {'max_iter': 2})]
-
-######################
-# Display the original
+# Display the distorted image
 i = 1
 vmin, vmax = 0, 1
 pl.figure(figsize=(6, 5))
@@ -68,9 +60,22 @@ pl.imshow(lena, vmin=vmin, vmax=vmax, cmap=pl.cm.gray, interpolation='nearest')
 pl.xticks(())
 pl.yticks(())
 
+###############################################################################
+# Extract noisy patches and reconstruct them using the dictionary
 print "Extracting noisy patches..."
 data = extract_patches_2d(lena[:, height/2:], patch_size, random_state=0)
-data = data.reshape(data.shape[0], -1)
+data = data.reshape(data.shape[0], -1) - intercept
+
+transform_algorithms = [
+    ('1-Orthogonal Matching Pursuit', 'omp',
+     {'n_nonzero_coefs': 1, 'precompute_gram': True}),
+
+    ('2-Orthogonal Matching Pursuit', 'omp',
+     {'n_nonzero_coefs': 2, 'precompute_gram': True}),
+
+    ('5-Least-angle regression', 'lars',
+     {'max_iter': 5})]
+
 reconstructions = {}
 for title, transform_algorithm, fit_params in transform_algorithms:
     print title,
@@ -78,7 +83,7 @@ for title, transform_algorithm, fit_params in transform_algorithms:
     t0 = time()
     dico.transform_algorithm = transform_algorithm
     code = dico.transform(data, **fit_params)
-    patches = np.dot(code, V)  # + intercept
+    patches = np.dot(code, V) + intercept
     patches = patches.reshape(len(data), *patch_size)
     reconstructions[title][:, height/2:] = reconstruct_from_patches_2d(patches,
                                                            (width, height / 2))
@@ -93,5 +98,5 @@ for title, transform_algorithm, fit_params in transform_algorithms:
     pl.yticks(())
 
 pl.subplots_adjust(0.09, 0.04, 0.92, 0.88, 0.18, 0.19)
-pl.suptitle('Reconstruction methods for image denoising')
+pl.suptitle('Transform methods for image denoising')
 pl.show()
