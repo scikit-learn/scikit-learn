d1f38c1ab099f920d2abd5e37e855256568d00a5
clay woolam <clay@woolam.org>
2011-08-23 09:37:04 -0700

    fixed structure example

--- examples/semi_supervised/plot_label_propagation_structure.py
+++ examples/semi_supervised/plot_label_propagation_structure.py
@@ -17,14 +17,14 @@ import pylab as pl
 
 from scikits.learn import svm, datasets
 
-label_spread = label_propagation.LabelSpreading(gamma=20, alpha=1.0)
+label_spread = label_propagation.LabelSpreading(gamma=15, alpha=1.0)
 
 # generate ring with inner box
 n_samples_per_circle = 100
 outer_circ_xs = np.cos(np.linspace(0, 2 * np.pi, n_samples_per_circle))
 outer_circ_ys = np.sin(np.linspace(0, 2 * np.pi, n_samples_per_circle))
-inner_circ_xs = np.cos(np.linspace(0, 2 * np.pi, n_samples_per_circle)) * 0.45
-inner_circ_ys = np.sin(np.linspace(0, 2 * np.pi, n_samples_per_circle)) * 0.45
+inner_circ_xs = np.cos(np.linspace(0, 2 * np.pi, n_samples_per_circle)) * 0.4
+inner_circ_ys = np.sin(np.linspace(0, 2 * np.pi, n_samples_per_circle)) * 0.4
 
 all_xs = np.append(outer_circ_xs, inner_circ_xs)
 all_ys = np.append(outer_circ_ys, inner_circ_ys)
