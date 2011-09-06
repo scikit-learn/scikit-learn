93f598e766e7fab4347e2af5afb72fdc38e7924c
Olivier Grisel <olivier.grisel@ensta.org>
2011-09-06 15:33:28 +0200

    cosmit

--- sklearn/cross_val.py
+++ sklearn/cross_val.py
@@ -1,4 +1,5 @@
 import warnings
-warnings.warn('sklearn.cross_val namespace is deprecated, please use'
-              ' sklearn.cross_validation instead')
+warnings.warn('sklearn.cross_val namespace is deprecated in version 0.9'
+              ' and will be removed in version 0.11.'
+              ' Please use sklearn.cross_validation instead.')
 from .cross_validation import *
