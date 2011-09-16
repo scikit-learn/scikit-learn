0a1df774a1211decca54391701e177fb69bc3c69
Peter Prettenhofer <peter.prettenhofer@gmail.com>
2011-09-16 17:34:52 +0200

    cosmit + n_samples fix

--- sklearn/tree/_tree.pyx
+++ sklearn/tree/_tree.pyx
@@ -48,11 +48,11 @@ cdef class Criterion:
         """Reset the criterion for a new feature index."""
         pass
 
-    cdef int update(self, int a, int b, DTYPE_t *y, int *sorted_X_i,
+    cdef int update(self, int a, int b, DTYPE_t *y, int *X_argsorted_i,
                     BOOL_t *sample_mask):
         """Update the criteria for each value in interval [a,b)
 
-        a and b are indices in `sorted_X_i`.
+        a and b are indices in `X_argsorted_i`.
         """
         pass
 
@@ -154,16 +154,16 @@ cdef class ClassificationCriterion(Criterion):
             self.label_count_left[c] = 0
             self.label_count_right[c] = self.label_count_init[c]
 
-    cdef int update(self, int a, int b, DTYPE_t *y, int *sorted_X_i,
+    cdef int update(self, int a, int b, DTYPE_t *y, int *X_argsorted_i,
                     BOOL_t *sample_mask):
         """Update the criteria for each value in interval [a,b)
 
-        a and b are indices in `sorted_X_i`.
+        a and b are indices in `X_argsorted_i`.
         """
         cdef int c
         # post condition: all samples from [0:b) are on the left side
         for idx from a <= idx < b:
-            s = sorted_X_i[idx]
+            s = X_argsorted_i[idx]
             if sample_mask[s] == 0:
                 continue
             c = <int>(y[s])
@@ -175,11 +175,11 @@ cdef class ClassificationCriterion(Criterion):
         # print "ClassificationCriterion.update: a = ", a, " b = ", b
         # print "    sorted [",
         # for c from 0 <= c < self.n_samples:
-        #     print sorted_X_i[c],
+        #     print X_argsorted_i[c],
         # print "] "
         # print "    y [",
         # for c from 0 <= c < self.n_samples:
-        #     print <int>y[sorted_X_i[c]],
+        #     print <int>y[X_argsorted_i[c]],
         # print "] "
         # print "    label_count_left [",
         # for c from 0 <= c < self.n_classes:
@@ -355,17 +355,17 @@ cdef class RegressionCriterion(Criterion):
         self.var_right = self.sq_sum_right - \
             self.n_samples * (self.mean_right * self.mean_right)
 
-    cdef int update(self, int a, int b, DTYPE_t *y, int *sorted_X_i,
+    cdef int update(self, int a, int b, DTYPE_t *y, int *X_argsorted_i,
                     BOOL_t *sample_mask):
         """Update the criteria for each value in interval [a,b)
 
-        a and b are indices in `sorted_X_i`.
+        a and b are indices in `X_argsorted_i`.
         """
         cdef double y_idx = 0.0
         cdef int idx, j
         # post condition: all samples from [0:b) are on the left side
         for idx from a <= idx < b:
-            j = sorted_X_i[idx]
+            j = X_argsorted_i[idx]
             if sample_mask[j] == 0:
                 continue
             y_idx = y[j]
@@ -405,28 +405,28 @@ cdef class MSE(RegressionCriterion):
 
 
 cdef int smallest_sample_larger_than(int sample_idx, DTYPE_t *X_i,
-                                     int *sorted_X_i, BOOL_t *sample_mask,
+                                     int *X_argsorted_i, BOOL_t *sample_mask,
                                      int n_total_samples):
     """Find the largest next sample.
 
     Find the index in the `X_i` array for sample who's feature
     `i` value is just about greater than those of the sample
-    `sorted_X_i[sample_idx]`.
+    `X_argsorted_i[sample_idx]`.
 
     Returns
     -------
     next_sample_idx : int
-        The index of the next smallest sample in `sorted_X`
+        The index of the next smallest sample in `X_argsorted`
         with different feature value than `sample_idx` .
-        I.e. `sorted_X_i[sample_idx] < sorted_X_i[next_sample_idx]`
+        I.e. `X_argsorted_i[sample_idx] < X_argsorted_i[next_sample_idx]`
         -1 if no such element exists.
     """
     cdef int idx = 0, j
     cdef DTYPE_t threshold = -DBL_MAX
     if sample_idx > -1:
-        threshold = X_i[sorted_X_i[sample_idx]]
+        threshold = X_i[X_argsorted_i[sample_idx]]
     for idx from sample_idx < idx < n_total_samples:
-        j = sorted_X_i[idx]
+        j = X_argsorted_i[idx]
         if sample_mask[j] == 0:
             continue
         if X_i[j] > threshold:
@@ -436,7 +436,7 @@ cdef int smallest_sample_larger_than(int sample_idx, DTYPE_t *X_i,
 
 def _find_best_split(np.ndarray[DTYPE_t, ndim=2, mode="fortran"] X,
                      np.ndarray[DTYPE_t, ndim=1, mode="c"] y,
-                     np.ndarray[np.int32_t, ndim=2, mode="fortran"] sorted_X,
+                     np.ndarray[np.int32_t, ndim=2, mode="fortran"] X_argsorted,
                      np.ndarray sample_mask,
                      np.ndarray[np.int32_t, ndim=1, mode="c"] feature_mask,
                      Criterion criterion,
@@ -449,8 +449,8 @@ def _find_best_split(np.ndarray[DTYPE_t, ndim=2, mode="fortran"] X,
         The feature values.
     y : ndarray, shape (n_total_samples,), dtype=float
         The label to predict for each sample.
-    sorted_X : ndarray, shape (n_samples, n_features)
-        Argsort of cols of `X`. `sorted_X[0,j]` gives the example
+    X_argsorted : ndarray, shape (n_samples, n_features)
+        Argsort of cols of `X`. `X_argsorted[0,j]` gives the example
         index of the smallest value of feature `j`.
     sample_mask : ndarray, shape (n_samples,), dtype=np.bool
         A mask for the samples to be considered. Only samples `j` for which
@@ -483,22 +483,22 @@ def _find_best_split(np.ndarray[DTYPE_t, ndim=2, mode="fortran"] X,
     cdef DTYPE_t *y_ptr = <DTYPE_t *>y.data
     cdef DTYPE_t *X_i = NULL
     
-    cdef int *sorted_X_i = NULL
+    cdef int *X_argsorted_i = NULL
 
     # sample mask data pointer
     cdef BOOL_t *sample_mask_ptr = <BOOL_t *>sample_mask.data
 
     # Compute the column strides (increment in pointer elements to get
-    # from column i to i + 1) for `X` and `sorted_X`
+    # from column i to i + 1) for `X` and `X_argsorted`
     cdef int X_elem_stride = X.strides[0]
     cdef int X_col_stride = X.strides[1]
     cdef int X_stride = X_col_stride / X_elem_stride
-    cdef int sorted_X_elem_stride = sorted_X.strides[0]
-    cdef int sorted_X_col_stride = sorted_X.strides[1]
-    cdef int sorted_X_stride = sorted_X_col_stride / sorted_X_elem_stride
+    cdef int X_argsorted_elem_stride = X_argsorted.strides[0]
+    cdef int X_argsorted_col_stride = X_argsorted.strides[1]
+    cdef int X_argsorted_stride = X_argsorted_col_stride / X_argsorted_elem_stride
 
     # Compute the initial entropy in the node
-    sorted_X_i = <int *>sorted_X.data
+    X_argsorted_i = <int *>X_argsorted.data
     criterion.init(y_ptr, sample_mask_ptr, n_samples, n_total_samples)
     initial_error = criterion.eval()
     if initial_error == 0: # break early if the node is pure
@@ -512,23 +512,23 @@ def _find_best_split(np.ndarray[DTYPE_t, ndim=2, mode="fortran"] X,
 
         # get i-th col of X and X_sorted
         X_i = (<DTYPE_t *>X.data) + X_stride * i
-        sorted_X_i = (<int *>sorted_X.data) + sorted_X_stride * i
+        X_argsorted_i = (<int *>X_argsorted.data) + X_argsorted_stride * i
 
         # reset the criterion for this feature
         criterion.reset()
 
-        # index of smallest sample in sorted_X_i
+        # index of smallest sample in X_argsorted_i
         a = 0
 
         while True:
-            b = smallest_sample_larger_than(a, X_i, sorted_X_i, sample_mask_ptr,
+            b = smallest_sample_larger_than(a, X_i, X_argsorted_i, sample_mask_ptr,
                                             n_total_samples)
 
             # if -1 there's none and we are finished
             if b == -1:
                 break
 
-            criterion.update(a, b, y_ptr, sorted_X_i, sample_mask_ptr)
+            criterion.update(a, b, y_ptr, X_argsorted_i, sample_mask_ptr)
 
             # get criterion value
             error = criterion.eval()
@@ -537,7 +537,7 @@ def _find_best_split(np.ndarray[DTYPE_t, ndim=2, mode="fortran"] X,
             # check if current error is smaller than previous best
             # if this is never true best_i is -1.
             if error < best_error:
-                t = (X_i[sorted_X_i[a]] + X_i[sorted_X_i[b]]) / 2.0
+                t = (X_i[X_argsorted_i[a]] + X_i[X_argsorted_i[b]]) / 2.0
                 best_i = i
                 best_t = t
                 best_error = error
