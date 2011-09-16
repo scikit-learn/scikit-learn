eb8a2c94ece2f3a7ba527174ca00eaaafd807be8
Peter Prettenhofer <peter.prettenhofer@gmail.com>
2011-09-16 17:15:52 +0200

    added GBRT regressor + classifier classes; added shrinkage

--- benchmarks/bench_gbrt.py
+++ benchmarks/bench_gbrt.py
@@ -1,7 +1,20 @@
 import numpy as np
 
-from sklearn.tree import DecisionTreeClassifier
+from sklearn import datasets
+from sklearn.utils import shuffle
+
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from sklearn.ensemble.gradient_boosting import GradientBoostingClassifier
+from sklearn.ensemble.gradient_boosting import GradientBoostingRegressor
+
+
+def repeat(f):
+    def wrapper(*args, **kargs):
+        scores = []
+        for i in range(10):
+            scores.append(f(*args, random_state=i, **kargs))
+        return np.mean(scores)
+    return wrapper
 
 
 def bench_random_gaussian():
@@ -26,10 +39,79 @@ def bench_random_gaussian():
 
     print "Tree: %.2f" % error_rate
 
-    gbrt = GradientBoostingClassifier(n_iter=100, max_depth=1)
+    tree = DecisionTreeRegressor(max_depth=20, min_split=5)
+    tree.fit(X_train, y_train)
+    y_pred = tree.predict(X_test)
+    y_pred[y_pred > 0.0] = 1.0
+    y_pred[y_pred <= 0.0] = -1.0
+    error_rate = (1.0 - np.mean(y_pred == y_test)) * 100.0
+
+    print "Tree: %.2f" % error_rate
+
+    gbrt = GradientBoostingClassifier(n_iter=100, max_depth=2, min_split=10)
     gbrt.fit(X_train, y_train)
     error_rate = (1.0 - gbrt.score(X_test, y_test)) * 100.0
     print "GBRT: %.2f" % error_rate
 
+
+regression_params = {'n_iter': 100, 'max_depth': 4,
+                     'min_split': 1, 'init': 'median',
+                     'learn_rate': 0.1}
+
+
+@repeat
+def bench_boston(random_state=None):
+    boston = datasets.load_boston()
+    X, y = shuffle(boston.data, boston.target, random_state=random_state)
+    offset = int(X.shape[0] * 0.9)
+    X_train = X[:offset]
+    y_train = y[:offset]
+    X_test = X[offset:]
+    y_test = y[offset:]
+    clf = GradientBoostingRegressor(**regression_params)
+    clf.fit(X_train, y_train)
+    mse = np.mean((clf.predict(X_test) - y_test) ** 2.0)
+    return mse
+
+
+@repeat
+def bench_friedman1(random_state=None):
+    X, y = datasets.make_friedman1(n_samples=1200,
+                                   random_state=random_state, noise=1.0)
+    X_train, y_train = X[:200], y[:200]
+    X_test, y_test = X[200:], y[200:]
+    clf = GradientBoostingRegressor(**regression_params)
+    clf.fit(X_train, y_train)
+    mse = np.mean((clf.predict(X_test) - y_test) ** 2.0)
+    return mse
+
+
+@repeat
+def bench_friedman2(random_state=None):
+    X, y = datasets.make_friedman2(n_samples=1200, random_state=random_state)
+    X_train, y_train = X[:200], y[:200]
+    X_test, y_test = X[200:], y[200:]
+    clf = GradientBoostingRegressor(**regression_params)
+    clf.fit(X_train, y_train)
+    mse = np.mean((clf.predict(X_test) - y_test) ** 2.0)
+    return mse
+
+
+@repeat
+def bench_friedman3(random_state=None):
+    X, y = datasets.make_friedman3(n_samples=1200, random_state=random_state)
+    X_train, y_train = X[:200], y[:200]
+    X_test, y_test = X[200:], y[200:]
+    clf = GradientBoostingRegressor(**regression_params)
+    clf.fit(X_train, y_train)
+    mse = np.mean((clf.predict(X_test) - y_test) ** 2.0)
+    return mse
+
+
 if __name__ == "__main__":
-    bench_random_gaussian()
+    #bench_random_gaussian()
+
+    print "Boston", bench_boston()
+    print "Friedman#1", bench_friedman1()
+    print "Friedman#2", bench_friedman2()
+    print "Friedman#3", bench_friedman3()
--- sklearn/ensemble/gradient_boosting.py
+++ sklearn/ensemble/gradient_boosting.py
@@ -1,4 +1,4 @@
-# Authors: Brian Holt, Peter Prettenhofer, Satrajit Ghosh
+# Authors: Peter Prettenhofer
 #
 # License: BSD Style.
 
@@ -12,6 +12,9 @@ from ..tree import DecisionTreeRegressor
 
 
 class MedianPredictor(object):
+    """A simple initial estimator that predicts the median
+    of the training targets.
+    """
 
     median = None
 
@@ -25,10 +28,29 @@ class MedianPredictor(object):
         return y
 
 
-class GradientBoostingClassifier(BaseEstimator, ClassifierMixin):
+class MeanPredictor(object):
+    """A simple initial estimator that predicts the mean
+    of the training targets.
+    """
 
-    def __init__(self, learn_rate=0.1, n_iter=100, min_split=5,
-                 max_depth=4, init=None, random_state=None):
+    mean = None
+
+    def fit(self, X, y):
+        y = np.asanyarray(y)
+        self.mean = np.mean(y)
+
+    def predict(self, X):
+        y = np.empty((X.shape[0],), dtype=np.float64)
+        y.fill(self.mean)
+        return y
+
+
+class GradientBoostingBase(BaseEstimator):
+
+    trees = []
+
+    def __init__(self, learn_rate, n_iter, min_split, max_depth, init,
+                 random_state):
         if n_iter <= 0:
             raise ValueError("n_iter must be greater than 0")
         self.n_iter = n_iter
@@ -40,13 +62,17 @@ class GradientBoostingClassifier(BaseEstimator, ClassifierMixin):
         self.min_split = min_split
         self.max_depth = max_depth
 
-        if init == None:
+        if init == 'median':
             self.init = MedianPredictor()
+        elif init == 'mean':
+            self.init = MeanPredictor()
+        elif init == None:
+            raise ValueError("init must not be None")
         else:
             if not hasattr(init, 'fit') or not hasattr(init, 'predict'):
                 raise ValueError("init must be valid estimator")
             self.init = init
-        
+
         self.random_state = check_random_state(random_state)
 
     def fit(self, X, y):
@@ -54,24 +80,64 @@ class GradientBoostingClassifier(BaseEstimator, ClassifierMixin):
         y = np.asanyarray(y, order='C')
         n_samples, n_features = X.shape
         if y.shape[0] != n_samples:
-            raise ValueError("Number of labels does not match number of samples.")
+            raise ValueError("Number of labels does not match " \
+                             "number of samples.")
 
         self.init.fit(X, y)
         self.trees = [self.init]
-        
+        y_pred = self._predict(X)
         for i in xrange(self.n_iter):
-            print "Boosting iteration %d" % i
-            residual = y - self.predict(X)
+            #print "Boosting iteration %d" % i
+            residual = y - y_pred
             tree = DecisionTreeRegressor(min_split=self.min_split,
                                          max_depth=self.max_depth)
             tree.fit(X, residual)
             self.trees.append(tree)
+            y_pred = self._predict(X, old_pred=y_pred,
+                                   learn_rate=self.learn_rate)
+
+    def _predict(self, X, old_pred=None, learn_rate=1.0):
+        if old_pred is not None:
+            return old_pred + learn_rate * self.trees[-1].predict(X)
+        else:
+            y = np.zeros((X.shape[0],), dtype=np.float64)
+            for i, tree in enumerate(self.trees):
+                if i == 0:
+                    y += tree.predict(X)
+                else:
+                    y += learn_rate * tree.predict(X)
+            return y
+
+
+class GradientBoostingClassifier(GradientBoostingBase, ClassifierMixin):
+
+    def __init__(self, learn_rate=0.1, n_iter=100, min_split=5,
+                 max_depth=4, init='median', random_state=None):
+
+        super(GradientBoostingClassifier, self).__init__(
+            learn_rate, n_iter, min_split, max_depth, init, random_state)
 
     def predict(self, X):
-        y = np.zeros((X.shape[0],), dtype=np.float64)
-        for tree in self.trees:
-            y += tree.predict(X)
-        y[y > 0.0] = 1
-        y[y <= 0.0] = -1
+        if len(self.trees) == 0:
+            raise ValueError("Estimator not fitted, " \
+                             "call `fit` before `predict`.")
+        y = self._predict(X)
+        y[y > 0.0] = 1.0
+        y[y <= 0.0] = -1.0
         return y
-    
+
+
+class GradientBoostingRegressor(GradientBoostingBase, RegressorMixin):
+
+    def __init__(self, learn_rate=0.1, n_iter=100, min_split=5,
+                 max_depth=4, init='median', random_state=None):
+
+        super(GradientBoostingRegressor, self).__init__(
+            learn_rate, n_iter, min_split, max_depth, init, random_state)
+
+    def predict(self, X):
+        if len(self.trees) == 0:
+            raise ValueError("Estimator not fitted, " \
+                             "call `fit` before `predict`.")
+        y = self._predict(X, learn_rate=self.learn_rate)
+        return y
