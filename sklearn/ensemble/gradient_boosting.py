18bdc1c568d6405c3b6816eabc4559e8fc98a00a
Peter Prettenhofer <peter.prettenhofer@gmail.com>
2011-09-18 19:23:17 +0200

    first work on various loss functions for gradient boosting.

--- sklearn/ensemble/gradient_boosting.py
+++ sklearn/ensemble/gradient_boosting.py
@@ -8,7 +8,16 @@ import numpy as np
 from ..base import BaseEstimator, ClassifierMixin, RegressorMixin
 from ..utils import check_random_state
 
-from ..tree import DecisionTreeRegressor
+from ..tree.tree import _build_tree
+from ..tree.tree import _apply_tree
+from ..tree._tree import MSE
+
+
+def _predict_tree(tree, X):
+    predictions = np.zeros(X.shape[0], dtype=np.float64)
+    for idx, sample in enumerate(X):
+        predictions[idx] = _apply_tree(tree, sample)
+    return predictions
 
 
 class MedianPredictor(object):
@@ -45,12 +54,85 @@ class MeanPredictor(object):
         return y
 
 
-class GradientBoostingBase(BaseEstimator):
+class LossFunction(object):
+    """Abstract base class for various loss functions."""
+
+    def loss(self, y, pred):
+        pass
+
+    def negative_gradient(self, y, pred):
+        """Compute the negative gradient."""
+        pass
+
+    def update_terminal_regions(self, tree):
+        """Update the terminal regions (=leafs) of the given
+        tree. Traverses tree and invoces template method
+        `_update_terminal_region`. """
+        if tree.is_leaf:
+            self._update_terminal_region(tree)
+        else:
+            self.update_terminal_regions(tree.left)
+            self.update_terminal_regions(tree.right)
+
+    def _update_terminal_region(self, node):
+        """Template method for updating terminal regions (=leafs). """
+        pass
+
+
+class LeastSquaresError(LossFunction):
+    """Loss function for least squares (LS) estimation. """
+
+    def loss(self, y, pred):
+        return 0.5 * np.sum((y - pred) ** 2.0)
+
+    def negative_gradient(self, y, pred):
+        return y - pred
+
+    def update_terminal_regions(self, tree):
+        """Terminal regions need not to be updated for least squares. """
+        pass
+
+
+class LeastAbsoluteError(LossFunction):
+    """Loss function for least absolute deviation (LAD) regression. """
+
+    def loss(self, y, pred):
+        return np.abs(y - pred)
+
+    def negative_gradient(self, y, pred):
+        return np.sign(y - pred)
+
+    def _update_terminal_region(self, node):
+        """LAD updates terminal regions to median estimates. """
+        ## FIXME cannot use targets -> use sample_mask instead.
+        node.value = node.targets.median()
+
+
+class BinomialDeviance(LossFunction):
+
+    def loss(self, y, pred):
+        return np.log(1 + np.exp(-2.0 * y * pred))
+
+    def negative_gradient(self, y, pred):
+        return (2.0 * y) / (1.0 + np.exp(2.0 * y * pred))
+
+    def _update_terminal_region(self, node):
+        """LAD updates terminal regions to median estimates. """
+        ## FIXME cannot use targets -> use sample_mask instead.
+        abs_targets = np.abs(node.targets)
+        node.value = node.targets.sum() / np.sum(abs_targets * \
+                                                 (2.0 - abs_targets))
+
+
+LOSS_FUNCTIONS = {'ls': SquaredError}
+
+
+class BaseGradientBoosting(BaseEstimator):
 
     trees = []
 
-    def __init__(self, learn_rate, n_iter, min_split, max_depth, init,
-                 random_state):
+    def __init__(self, loss, learn_rate, n_iter, min_split, max_depth, init,
+                 subsample, random_state):
         if n_iter <= 0:
             raise ValueError("n_iter must be greater than 0")
         self.n_iter = n_iter
@@ -59,7 +141,20 @@ class GradientBoostingBase(BaseEstimator):
             raise ValueError("learn_rate must be greater than 0")
         self.learn_rate = learn_rate
 
+        if loss not in LOSS_FUNCTIONS:
+            raise ValueError("loss not supported")
+        self.loss = LOSS_FUNCTIONS[loss]()
+
+        if min_split <= 0:
+            raise ValueError("min_split must be larger than 0.")
         self.min_split = min_split
+
+        if subsample <= 0.0 or subsample > 1:
+            raise ValueError("subsample must be in (0,1]")
+        self.subsample = subsample
+
+        if max_depth <= 0:
+            raise ValueError("max_depth must be larger than 0.")
         self.max_depth = max_depth
 
         if init == 'median':
@@ -76,6 +171,25 @@ class GradientBoostingBase(BaseEstimator):
         self.random_state = check_random_state(random_state)
 
     def fit(self, X, y):
+        """Fit the gradient boosting model.
+
+        Parameters
+        ----------
+        X : array-like, shape = [n_samples, n_features]
+            Training vectors, where n_samples is the number of samples
+            and n_features is the number of features.
+
+        y : array-like, shape = [n_samples]
+            Target values (integers in classification, real numbers in
+            regression)
+            For classification, labels must correspond to classes
+            0, 1, ..., n_classes-1
+
+        Returns
+        -------
+        self : object
+            Returns self.
+        """
         X = np.asanyarray(X, dtype=np.float32, order='F')
         y = np.asanyarray(y, order='C')
         n_samples, n_features = X.shape
@@ -83,41 +197,65 @@ class GradientBoostingBase(BaseEstimator):
             raise ValueError("Number of labels does not match " \
                              "number of samples.")
 
+        # create argsorted X for fast tree induction
+        X_argsorted = np.asfortranarray(
+            np.argsort(X.T, axis=1).astype(np.int32).T)
+
+        # fit initial model
         self.init.fit(X, y)
-        self.trees = [self.init]
-        y_pred = self._predict(X)
+
+        # init predictions
+        y_pred = self.init.predict(X)
+
+        self.trees = []
+        loss = self.loss
+
+        # perform boosting iterations
         for i in xrange(self.n_iter):
-            #print "Boosting iteration %d" % i
-            residual = y - y_pred
-            tree = DecisionTreeRegressor(min_split=self.min_split,
-                                         max_depth=self.max_depth)
-            tree.fit(X, residual)
+
+            # subsampling
+            sample_mask = np.random.rand(n_samples) > (1.0 - self.subsample)
+
+            residual = loss.negative_gradient(y, y_pred)
+
+            # induce regression tree on residuals
+            tree = _build_tree(False, X, residual, MSE(), self.max_depth,
+                               self.min_split, None, 1, self.random_state,
+                               0.0, sample_mask, X_argsorted)
+
+            loss.update_terminal_regions(tree)
             self.trees.append(tree)
+
             y_pred = self._predict(X, old_pred=y_pred,
                                    learn_rate=self.learn_rate)
 
+        return self
+
     def _predict(self, X, old_pred=None, learn_rate=1.0):
+        """Predict targets with current model. Re-uses predictions
+        from previous iteration if available.
+        """
         if old_pred is not None:
-            return old_pred + learn_rate * self.trees[-1].predict(X)
+            return old_pred + learn_rate * _predict_tree(self.trees[-1], X)
         else:
-            y = np.zeros((X.shape[0],), dtype=np.float64)
-            for i, tree in enumerate(self.trees):
-                if i == 0:
-                    y += tree.predict(X)
-                else:
-                    y += learn_rate * tree.predict(X)
+            y = self.init.predict(X)
+            for tree in self.trees:
+                y += learn_rate * _predict_tree(tree, X)
             return y
 
 
-class GradientBoostingClassifier(GradientBoostingBase, ClassifierMixin):
+class GradientBoostingClassifier(BaseGradientBoosting, ClassifierMixin):
 
-    def __init__(self, learn_rate=0.1, n_iter=100, min_split=5,
-                 max_depth=4, init='median', random_state=None):
+    def __init__(self, loss='deviance', learn_rate=0.1, n_iter=100,
+                 subsample=1.0, min_split=5, max_depth=4,
+                 init='median', random_state=None):
 
         super(GradientBoostingClassifier, self).__init__(
-            learn_rate, n_iter, min_split, max_depth, init, random_state)
+            loss, learn_rate, n_iter, min_split, max_depth, init, subsample,
+            random_state)
 
     def predict(self, X):
+        X = np.atleast_2d(X)
         if len(self.trees) == 0:
             raise ValueError("Estimator not fitted, " \
                              "call `fit` before `predict`.")
@@ -127,15 +265,17 @@ class GradientBoostingClassifier(GradientBoostingBase, ClassifierMixin):
         return y
 
 
-class GradientBoostingRegressor(GradientBoostingBase, RegressorMixin):
+class GradientBoostingRegressor(BaseGradientBoosting, RegressorMixin):
 
-    def __init__(self, learn_rate=0.1, n_iter=100, min_split=5,
-                 max_depth=4, init='median', random_state=None):
+    def __init__(self, loss='ls', learn_rate=0.1, n_iter=100, subsample=1.0,
+                 min_split=5, max_depth=4, init='median', random_state=None):
 
         super(GradientBoostingRegressor, self).__init__(
-            learn_rate, n_iter, min_split, max_depth, init, random_state)
+            loss, learn_rate, n_iter, min_split, max_depth, init, subsample,
+            random_state)
 
     def predict(self, X):
+        X = np.atleast_2d(X)
         if len(self.trees) == 0:
             raise ValueError("Estimator not fitted, " \
                              "call `fit` before `predict`.")
