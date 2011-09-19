f7ca7b3a69befa3936f4a3e98737c240b93f65fe
Gael varoquaux <gael.varoquaux@normalesup.org>
2011-09-19 16:58:05 +0200

    Better randomized_lasso

--- examples/linear_model/plot_randomized_lasso.py
+++ examples/linear_model/plot_randomized_lasso.py
@@ -7,8 +7,9 @@ Randomized Lasso: feature selection with Lasso
 print __doc__
 
 import pylab as pl
+import numpy as np
 
-from sklearn.linear_model import RandomizedLasso
+from sklearn.linear_model import RandomizedLasso, LassoLarsIC, LassoLarsCV
 from sklearn.datasets import make_regression
 
 
@@ -18,21 +19,32 @@ X, y, coef = make_regression(n_samples=40, n_features=100,
                              n_informative=10, coef=True, random_state=42)
 
 ################################################################################
-# Fit the RandomizedLasso
-
-################################################################################
 # Plot the true weights, the estimated weights and the histogram of the
 # weights
 pl.figure(figsize=(6, 5))
 pl.title("Weights of the model")
-for index, alpha in enumerate((1, 5, 10, 15, 20, 'aic')):
+scores = list()
+scores.append(coef/coef.max())
+n_alphas = 12
+alphas = np.logspace(-1, 1, n_alphas)[::-1]
+for alpha in alphas:
     clf = RandomizedLasso(verbose=True, alpha=alpha, random_state=42)
     clf.fit(X, y)
-    pl.plot(clf.scores_, ':', color=pl.cm.bone(index/6.),
-                    label="alpha = %s" % alpha)
-pl.plot(coef/coef.max(), 'g-', label="Ground truth coefficients")
-pl.xlabel("Features")
-#pl.ylabel("Values of the weights")
-pl.legend(loc=1)
+    scores.append(clf.scores_)
+scores = np.array(scores)
+pl.imshow(np.rot90(scores), cmap=pl.cm.gray,
+          interpolation='nearest', aspect='auto')
+#pl.plot(coef/coef.max(), 'g-', label="Ground truth coefficients")
+alpha_aic = LassoLarsIC(criterion='aic').fit(X, y).alpha_
+alpha_bic = LassoLarsIC(criterion='bic').fit(X, y).alpha_
+alpha_cv = LassoLarsCV().fit(X, y).alpha
+pl.xlabel("alpha (AIC=%.2e, BIC=%.2e, CV=%.2e)" %
+          (alpha_aic, alpha_bic, alpha_cv))
+pl.ylabel("Features")
+
+pl.xticks(np.arange(0, n_alphas+1),
+          ['Truth', ] + ['%.2e' % a for a in alphas])
+#pl.legend(loc=1)
 
 pl.show()
+
