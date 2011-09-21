e7f8212d012bdeddf9b511a48a2ec1e4cef316ef
Fabian Pedregosa <fabian.pedregosa@inria.fr>
2011-09-21 10:36:44 +0200

    Some Python 2.5 fixes.

--- sklearn/cross_val.py
+++ sklearn/cross_val.py
@@ -2,4 +2,4 @@ import warnings
 warnings.warn('sklearn.cross_val namespace is deprecated in version 0.9'
               ' and will be removed in version 0.11.'
               ' Please use sklearn.cross_validation instead.')
-from .cross_validation import *
+from cross_validation import *
