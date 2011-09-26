3841f439b32b0cf451c38a429871045fb9aa7ef9
Robert Layton <robertlayton@gmail.com>
2011-09-26 18:23:54 +1000

    Sparse matrix testing

--- sklearn/metrics/tests/test_unsupervised.py
+++ sklearn/metrics/tests/test_unsupervised.py
@@ -1,3 +1,5 @@
+from scipy.sparse import csr_matrix
+
 from ... import datasets
 from ..unsupervised import silhouette_coefficient
 from .. import pairwise_distances
@@ -11,5 +13,10 @@ def test_silhouette():
     D = pairwise_distances(X, metric='euclidean')
     # Given that the actual labels are used, we can assume that S would be
     # positive.
+    silhouette = silhouette_coefficient(y, D)
+    assert(silhouette > 0)
+    # Test with sparse X
+    X_sparse = csr_matrix(X)
+    D = pairwise_distances(X, metric='euclidean')
     silhouette = silhouette_coefficient(y, D)
     assert(silhouette > 0)
