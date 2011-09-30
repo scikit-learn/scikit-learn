1e9402cbc63443446a8dfa2822d37dd21af9b9ae
Robert Layton <robertlayton@gmail.com>
2011-09-30 22:48:01 +1000

    Restructured metrics/cluster into a folder with supervised and unsupervised modules

---
+++ sklearn/metrics/cluster/__init__.py
@@ -0,0 +1,11 @@
+"""
+:mod:`sklearn.metrics.cluster` is a module containing evaluation metrics for
+cluster analysis results. There are two forms of evaluation:
+
+- supervised, which uses a ground truth class values for each sample.
+- unsupervised, which does not and measures the 'quality' of the model itself.
+"""
+from supervised import (homogeneity_completeness_v_measure,
+                        homogeneity_score, completeness_score,
+                        v_measure_score)
+from unsupervised import silhouette_score, silhouette_samples
--- sklearn/metrics/cluster/tests/test_unsupervised.py
+++ sklearn/metrics/cluster/tests/test_unsupervised.py
@@ -1,8 +1,8 @@
 from scipy.sparse import csr_matrix
 
-from ... import datasets
-from ..unsupervised import silhouette_coefficient
-from .. import pairwise_distances
+from .... import datasets
+from ..unsupervised import silhouette_score
+from ... import pairwise_distances
 
 
 def test_silhouette():
@@ -13,10 +13,10 @@ def test_silhouette():
     D = pairwise_distances(X, metric='euclidean')
     # Given that the actual labels are used, we can assume that S would be
     # positive.
-    silhouette = silhouette_coefficient(D, y)
+    silhouette = silhouette_score(D, y)
     assert(silhouette > 0)
     # Test with sparse X
     X_sparse = csr_matrix(X)
     D = pairwise_distances(X, metric='euclidean')
-    silhouette = silhouette_coefficient(D, y)
+    silhouette = silhouette_score(D, y)
     assert(silhouette > 0)
