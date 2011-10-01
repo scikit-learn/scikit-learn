712b12f167c65833cdaa31b2f9c86ee71310d5e5
Olivier Grisel <olivier.grisel@ensta.org>
2011-10-01 11:51:06 +0200

    ENH: 30% speed improvements in load_svmlight_file

--- sklearn/datasets/svmlight_format.py
+++ sklearn/datasets/svmlight_format.py
@@ -12,11 +12,11 @@ libsvm command line programs.
 
 # Authors: Mathieu Blondel <mathieu@mblondel.org>
 #          Lars Buitinck <L.J.Buitinck@uva.nl>
+#          Olivier Grisel <olivier.grisel@ensta.org>
 # License: Simple BSD.
 
 import numpy as np
 import scipy.sparse as sp
-import re
 
 def _load_svmlight_file(f, buffer_mb, n_features):
     data = []
@@ -24,25 +24,22 @@ def _load_svmlight_file(f, buffer_mb, n_features):
     indices = []
     labels = []
 
-    pattern = re.compile(r'\s+')
-
     for line in f:
         line = line.strip()
 
-        if line.startswith("#"):
+        hash_position = line.find('#')
+        if hash_position == 0:
             continue
+        elif hash_position > 0:
+            line = line[:hash_position].strip()
 
-        # Remove inline comments.
-        line = line.split("#")
-        line = line[0].strip()
-
-        line = re.sub(pattern, ' ', line)
-        y, features = line.split(" ", 1)
+        line_parts = line.split()
+        y, features = line_parts[0], line_parts[1:]
 
         labels.append(float(y))
         indptr.append(len(data))
 
-        for feat in features.split(" "):
+        for feat in features:
             idx, value = feat.split(":")
             indices.append(int(idx))
             data.append(float(value))
@@ -57,8 +54,7 @@ def _load_svmlight_file(f, buffer_mb, n_features):
 
     X = sp.csr_matrix((np.array(data, dtype=np.double),
                        np.array(indices, dtype=np.int),
-                       indptr),
-                      shape)
+                       indptr), shape)
 
     return X, np.array(labels, dtype=np.double)
 
@@ -131,12 +127,14 @@ def load_svmlight_file(f, other_file=None, n_features=None, buffer_mb=40):
 
     Note
     ----
-    When fitting a model to a matrix X_train and evaluating it against a matrix
-    X_test, it is essential that X_train and X_test have the same number of
-    features (X_train.shape[1] == X_test.shape[1]). This may not be the case if
-    you load them with load_svmlight_format separately. To address this
-    problem, we recommend to use load_svmlight_format(train_file, test_file)
-    or load_svmlight_format(test_file, n_features=X_train.shape[1]).
+    When fitting a model to a matrix X_train and evaluating it against a
+    matrix X_test, it is essential that X_train and X_test have the same
+    number of features (X_train.shape[1] == X_test.shape[1]). This may
+    not be the case if you load them with load_svmlight_file separately.
+
+    To address this problem, we recommend to use
+    load_svmlight_file(train_file, test_file) or
+    load_svmlight_file(test_file, n_features=X_train.shape[1]).
     """
     ret = _load_svmlight(f, buffer_mb, n_features)
 
