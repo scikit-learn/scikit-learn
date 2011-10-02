dbc26ca97c445ccbb0b8047efb705fed1683e18d
Olivier Grisel <olivier.grisel@ensta.org>
2011-10-02 12:46:51 +0200

    DOC: better docstrings

--- sklearn/random_projection.py
+++ sklearn/random_projection.py
@@ -1,8 +1,26 @@
 """Random Projection transformers
 
-Random Projections are an efficient way to reduce the dimensionality of
-the data by trading a controlled amout of accuracy (as aditional variance)
-for faster processing times and smaller model sizes.
+Random Projections are an efficient way to reduce the dimensionality
+of the data by trading a controlled amout of accuracy (as additional
+variance) for faster processing times and smaller model sizes.
+
+The dimensions and distribution of Random Projections matrices are
+controlled so as to preserve the pairwise distances between any two
+samples of the dataset.
+
+The main theoretical result behind their efficiency is the
+Johnson-Lindenstrauss lemma (quoting Wikipedia):
+
+  In mathematics, the Johnson–Lindenstrauss lemma is a result
+  concerning low-distortion embeddings of points from high-dimensional
+  into low-dimensional Euclidean space. The lemma states that a small set
+  of points in a high-dimensional space can be embedded into a space of
+  much lower dimension in such a way that distances between the points are
+  nearly preserved. The map used for the embedding is at least Lipschitz,
+  and can even be taken to be an orthogonal projection.
+
+  http://en.wikipedia.org/wiki/Johnson%E2%80%93Lindenstrauss_lemma
+
 """
 # Authors: Olivier Grisel <olivier.grisel@ensta.org>
 # License: Simple BSD
@@ -27,15 +45,17 @@ def johnson_lindenstrauss_bound(n_samples, eps=0.1):
       (1 - eps) ||u - v||^2 < ||p(u) - p(v)||^2 < (1 + eps) ||u - v||^2
 
     Where u and v are any rows taken from a dataset of shape [n_samples,
-    n_features] and p is a projection by a random gaussian matrix with
-    shape [n_components, n_features] is given by.
+    n_features] and p is a projection by a random gaussian N(0, 1) matrix
+    with shape [n_components, n_features] (or a sparse Achlioptas matrix).
 
     The minimum number of components to garantee the embedding is given by:
 
       n_components >= 4 log(n_samples) / (eps^2 / 2 - eps^3 / 3)
 
     Note that the number of dimensions is independent of the original
-    number of features but instead depends on the size of the dataset.
+    number of features but instead depends on the size of the dataset:
+    the larger the dataset, the higher is the minimal dimensionality of
+    random projection embedding.
 
     Examples
     --------
@@ -64,9 +84,10 @@ def johnson_lindenstrauss_bound(n_samples, eps=0.1):
 
 def sparse_random_matrix(n_components, n_features, density='auto',
                          random_state=None):
-    """Generate a sparse random matrix suitable for random projection
+    """Generalized Achlioptas random sparse matrix for random projection
 
-    TODO: explain the parameters
+    Setting density to 1/3 will yield the original Achlioptas matrix
+    while setting a lower value will yield Li's generalization.
 
     Examples
     --------
@@ -97,6 +118,16 @@ def sparse_random_matrix(n_components, n_features, density='auto',
 
       >>> np.abs(r.mean())                                # doctest: +ELLIPSIS
       0.00...
+
+    References
+    ----------
+
+    - Very Sparse Random Projections. Ping Li, Trevor Hastie
+      and Kenneth Church, 2006
+      http://www.stanford.edu/~hastie/Papers/Ping/KDD06_rp.pdf
+
+    - Database-friendly random projections, Dimitris Achlioptas, 2001
+      http://www.cs.ucsc.edu/~optas/papers/jl.pdf
 
     """
     random_state = check_random_state(random_state)
@@ -163,6 +194,17 @@ class SparseRandomProjection(BaseEstimator, TransformerMixin):
 
         Use density = 1 / 3.0 if you want to reproduce the results from
         Achlioptas, 2001.
+
+    eps: strictly positive float, optional, default 0.1
+        Parameter to control the quality of the embedding according to the
+        Johnson-Lindenstrauss lemma when n_components is set to auto.
+
+        Smaller values lead to better embedding and higher number of
+        dimensions (n_components) in the target projection space.
+
+    random_state: integer, RandomState instance or None (default)
+        Control the pseudo random number generator used to generate the
+        matrix at fit time.
 
     Attributes
     ----------
@@ -234,7 +276,7 @@ class SparseRandomProjection(BaseEstimator, TransformerMixin):
         Parameters
         ----------
         X : numpy array or scipy.sparse of shape [n_samples, n_features]
-            The input data to project into a 
+            The input data to project into a smaller dimensional space.
 
         y : is not used: placehold to allow for usage in a Pipeline.
 
