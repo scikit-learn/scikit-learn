6fad3d54d8c7ffd4ba328ab2241a146710317d3a
Olivier Grisel <olivier.grisel@ensta.org>
2011-10-07 00:00:31 +0200

    FIX: remove non-ASCII char from silhouette docstrigs

--- sklearn/metrics/cluster/unsupervised.py
+++ sklearn/metrics/cluster/unsupervised.py
@@ -63,7 +63,7 @@ def silhouette_score(X, labels, metric='euclidean',
     ----------
     Peter J. Rousseeuw (1987). "Silhouettes: a Graphical Aid to the
         Interpretation and Validation of Cluster Analysis". Computational
-        and Applied Mathematics 20: 53–65. doi:10.1016/0377-0427(87)90125-7.
+        and Applied Mathematics 20: 53-65. doi:10.1016/0377-0427(87)90125-7.
 
     http://en.wikipedia.org/wiki/Silhouette_(clustering)
 
@@ -125,7 +125,7 @@ def silhouette_samples(X, labels, metric='euclidean', **kwds):
     ----------
     Peter J. Rousseeuw (1987). "Silhouettes: a Graphical Aid to the
         Interpretation and Validation of Cluster Analysis". Computational
-        and Applied Mathematics 20: 53–65. doi:10.1016/0377-0427(87)90125-7.
+        and Applied Mathematics 20: 53-65. doi:10.1016/0377-0427(87)90125-7.
 
     http://en.wikipedia.org/wiki/Silhouette_(clustering)
 
