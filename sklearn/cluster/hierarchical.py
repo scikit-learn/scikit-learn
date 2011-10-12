18f6e62bf0cb9b3e54e5f7b319dd3e1e0c76df1c
Jan Hendrik Metzen <jhm@informatik.uni-bremen.de>
2011-10-12 12:00:40 +0200

    Performance tweaking in ward_tree.
    Using a set instead of a list for lookup of already considered tree nodes during update of structure matrix.
    
    Using a set instead of a list in

--- sklearn/cluster/hierarchical.py
+++ sklearn/cluster/hierarchical.py
@@ -148,12 +148,12 @@ def ward_tree(X, connectivity=None, n_components=None, copy=True):
 
         # update the structure matrix A and the inertia matrix
         coord_col = []
-        visited = [k]        
+        visited = set([k])        
         for l in set(A[i]).union(A[j]):
             while parent[l] != l:
                l = parent[l]
             if l not in visited:
-               visited.append(l)
+               visited.add(l)
                coord_col.append(l)
                A[l].append(k)
         A.append(coord_col)
