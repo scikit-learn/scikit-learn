e0c217d6dfeb37111ee6603cac34079fbe2d728a
Olivier Grisel <olivier.grisel@ensta.org>
2011-10-17 18:04:13 +0300

    FIX: missing relative import marker

--- sklearn/metrics/cluster/__init__.py
+++ sklearn/metrics/cluster/__init__.py
@@ -5,7 +5,7 @@ cluster analysis results. There are two forms of evaluation:
 - supervised, which uses a ground truth class values for each sample.
 - unsupervised, which does not and measures the 'quality' of the model itself.
 """
-from supervised import (homogeneity_completeness_v_measure,
-                        homogeneity_score, completeness_score,
-                        v_measure_score, adjusted_rand_score)
-from unsupervised import silhouette_score, silhouette_samples
+from .supervised import (homogeneity_completeness_v_measure,
+                         homogeneity_score, completeness_score,
+                         v_measure_score, adjusted_rand_score)
+from .unsupervised import silhouette_score, silhouette_samples
