867ec2fc53b2ebb1cc7d06b9046ed72fd1a12b46
Robert Layton <robertlayton@gmail.com>
2011-10-21 10:56:12 +1100

    Higher level import for ami_score

--- sklearn/metrics/__init__.py
+++ sklearn/metrics/__init__.py
@@ -17,4 +17,5 @@ from .cluster import completeness_score
 from .cluster import v_measure_score
 from .cluster import silhouette_score
 from .cluster import mutual_information_score
+from .cluster import ami_score
 from .pairwise import euclidean_distances, pairwise_distances, pairwise_kernels
