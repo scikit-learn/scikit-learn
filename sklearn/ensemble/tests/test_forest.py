8ac39e09be7fb41f86108c3597004395ef1d32d3
Gilles Louppe <g.louppe@gmail.com>
2011-11-07 12:55:37 +0100

    Added some more tests

--- sklearn/ensemble/tests/test_forest.py
+++ sklearn/ensemble/tests/test_forest.py
@@ -6,6 +6,7 @@ import numpy as np
 from numpy.testing import assert_array_equal
 from numpy.testing import assert_array_almost_equal
 from numpy.testing import assert_almost_equal
+from numpy.testing import assert_equal
 from nose.tools import assert_raises
 
 from sklearn.ensemble import RandomForestClassifier
@@ -35,8 +36,9 @@ perm = np.random.permutation(boston.target.size)
 boston.data = boston.data[perm]
 boston.target = boston.target[perm]
 
-def test_classification_toy_rf():
-    """Check classification on a toy dataset (random forest)."""
+def test_classification_toy():
+    """Check classification on a toy dataset."""
+    # Random forest
     clf = RandomForestClassifier(n_trees=10, random_state=1)
     clf.fit(X, y)
     assert_array_equal(clf.predict(T), true_result)
@@ -45,8 +47,7 @@ def test_classification_toy_rf():
     clf.fit(X, y)
     assert_array_equal(clf.predict(T), true_result)
 
-def test_classification_toy_et():
-    """Check classification on a toy dataset (extra-trees)."""
+    # Extra-trees
     clf = ExtraTreesClassifier(n_trees=10, random_state=1)
     clf.fit(X, y)
     assert_array_equal(clf.predict(T), true_result)
@@ -55,9 +56,10 @@ def test_classification_toy_et():
     clf.fit(X, y)
     assert_array_equal(clf.predict(T), true_result)
 
-def test_iris_rf():
-    """Check consistency on dataset iris (random forest)."""
+def test_iris():
+    """Check consistency on dataset iris."""
     for c in ("gini", "entropy"):
+        # Random forest
         clf = RandomForestClassifier(n_trees=10, criterion=c, random_state=1)
         clf.fit(iris.data, iris.target)
         score = clf.score(iris.data, iris.target)
@@ -68,9 +70,7 @@ def test_iris_rf():
         score = clf.score(iris.data, iris.target)
         assert score > 0.5, "Failed with criterion %s and score = %f" % (c, score)
 
-def test_iris_et():
-    """Check consistency on dataset iris (extra-trees)."""
-    for c in ("gini", "entropy"):
+        # Extra-trees
         clf = ExtraTreesClassifier(n_trees=10, criterion=c, random_state=1)
         clf.fit(iris.data, iris.target)
         score = clf.score(iris.data, iris.target)
@@ -81,9 +81,10 @@ def test_iris_et():
         score = clf.score(iris.data, iris.target)
         assert score > 0.9, "Failed with criterion %s and score = %f" % (c, score)
 
-def test_boston_rf():
-    """Check consistency on dataset boston house prices (random forest)."""
+def test_boston():
+    """Check consistency on dataset boston house prices."""
     for c in ("mse",):
+        # Random forest
         clf = RandomForestRegressor(n_trees=10, criterion=c, random_state=1)
         clf.fit(boston.data, boston.target)
         score = clf.score(boston.data, boston.target)
@@ -94,9 +95,7 @@ def test_boston_rf():
         score = clf.score(boston.data, boston.target)
         assert score < 3, "Failed with max_features=None, criterion %s and score = %f" % (c, score)
 
-def test_boston_et():
-    """Check consistency on dataset boston house prices (extra-trees)."""
-    for c in ("mse",):
+        # Extra-trees
         clf = ExtraTreesRegressor(n_trees=10, criterion=c, random_state=1)
         clf.fit(boston.data, boston.target)
         score = clf.score(boston.data, boston.target)
@@ -107,24 +106,72 @@ def test_boston_et():
         score = clf.score(boston.data, boston.target)
         assert score < 3, "Failed with max_features=None, criterion %s and score = %f" % (c, score)
 
-def test_probability_rf():
-    """Predict probabilities (random forest)."""
+def test_probability():
+    """Predict probabilities."""
+    # Random forest
     clf = RandomForestClassifier(n_trees=10, random_state=1)
     clf.fit(iris.data, iris.target)
     assert_array_almost_equal(np.sum(clf.predict_proba(iris.data), axis=1), np.ones(iris.data.shape[0]))
+    assert_array_almost_equal(clf.predict_proba(iris.data), np.exp(clf.predict_log_proba(iris.data)))
 
-def test_probability_et():
-    """Predict probabilities (extra-trees)."""
+    # Extra-trees
     clf = ExtraTreesClassifier(n_trees=10, random_state=1)
     clf.fit(iris.data, iris.target)
     assert_array_almost_equal(np.sum(clf.predict_proba(iris.data), axis=1), np.ones(iris.data.shape[0]))
+    assert_array_almost_equal(clf.predict_proba(iris.data), np.exp(clf.predict_log_proba(iris.data)))
 
-def test_error_rf():
-    """Check that proper errors are triggered (random forest)."""
+def test_error():
+    """Check that proper errors are triggered."""
+    # Random forest
     assert_raises(ValueError, RandomForestClassifier(n_trees=-1).fit, X, y)
     assert_raises(ValueError, RandomForestRegressor(n_trees=-1).fit, X, y)
 
-def test_error_et():
-    """Check that proper errors are triggered (extra-trees)."""
+    # Extra-trees
     assert_raises(ValueError, ExtraTreesClassifier(n_trees=-1).fit, X, y)
     assert_raises(ValueError, ExtraTreesRegressor(n_trees=-1).fit, X, y)
+
+def test_pickle():
+    """Check pickability."""
+    import pickle
+
+    # Random forest
+    obj = RandomForestClassifier()
+    obj.fit(iris.data, iris.target)
+    score = obj.score(iris.data, iris.target)
+    s = pickle.dumps(obj)
+
+    obj2 = pickle.loads(s)
+    assert_equal(type(obj2), obj.__class__)
+    score2 = obj2.score(iris.data, iris.target)
+    assert score == score2
+
+    obj = RandomForestRegressor()
+    obj.fit(boston.data, boston.target)
+    score = obj.score(boston.data, boston.target)
+    s = pickle.dumps(obj)
+
+    obj2 = pickle.loads(s)
+    assert_equal(type(obj2), obj.__class__)
+    score2 = obj2.score(boston.data, boston.target)
+    assert score == score2
+
+    # Extra-trees
+    obj = ExtraTreesClassifier()
+    obj.fit(iris.data, iris.target)
+    score = obj.score(iris.data, iris.target)
+    s = pickle.dumps(obj)
+
+    obj2 = pickle.loads(s)
+    assert_equal(type(obj2), obj.__class__)
+    score2 = obj2.score(iris.data, iris.target)
+    assert score == score2
+
+    obj = ExtraTreesRegressor()
+    obj.fit(boston.data, boston.target)
+    score = obj.score(boston.data, boston.target)
+    s = pickle.dumps(obj)
+
+    obj2 = pickle.loads(s)
+    assert_equal(type(obj2), obj.__class__)
+    score2 = obj2.score(boston.data, boston.target)
+    assert score == score2
