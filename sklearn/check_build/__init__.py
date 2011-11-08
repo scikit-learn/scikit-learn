f20c0d00b1a7becccd5994737d0305d1664da2f8
Gael varoquaux <gael.varoquaux@normalesup.org>
2011-11-08 23:02:16 +0100

    ENH check_build: better error msg for local imports

--- sklearn/check_build/__init__.py
+++ sklearn/check_build/__init__.py
@@ -1,4 +1,16 @@
+""" Module to give helpful messages to the user that did not
+compile the scikit properly.
+"""
 import os
+
+INPLACE_MSG = """
+It appears that you are importing a local tree of the scikit-learn. For
+this, you need to have an inplace install. Maybe you are in the source
+directory and you need to try from another location."""
+
+STANDARD_MSG = """
+If you have used an installer, please check that it is suited for your
+Python version, your operating system and your platform."""
 
 try:
     from ._check_build import check_build
@@ -6,6 +18,11 @@ except ImportError, e:
     # Raise a comprehensible error and list the contents of the
     # directory to help debugging on the mailing list.
     local_dir = os.path.split(__file__)[0]
+    msg = STANDARD_MSG
+    if local_dir == "sklearn/check_build":
+        # Picking up the local install: this will work only if the
+        # install is an 'inplace build'
+        msg = INPLACE_MSG
     dir_content = list()
     for i, filename in enumerate(os.listdir(local_dir)):
         if ((i + 1) % 3):
@@ -23,9 +40,6 @@ It seems that the scikit-learn has not been built correctly.
 If you have installed the scikit-learn from source, please do not forget
 to build the package before using it: run `python setup.py install` or
 `make` in the source directory.
-
-If you have used an installer, please check that it is suited for your
-Python version, your operating system and your platform.
-""" % (e, local_dir, ''.join(dir_content).strip()))
+%s""" % (e, local_dir, ''.join(dir_content).strip(), msg))
 
 
