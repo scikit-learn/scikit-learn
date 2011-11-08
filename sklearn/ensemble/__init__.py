f4b80faf43829ab10755aec7b92c3a83088e4cf1
Gilles Louppe <g.louppe@gmail.com>
2011-11-08 11:00:16 +0100

    Factored out changes on the ensemble module

--- sklearn/ensemble/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Ensemble algorithms.
-"""
-
-from .forest import RandomForestClassifier
-from .forest import RandomForestRegressor
-from .forest import ExtraTreesClassifier
-from .forest import ExtraTreesRegressor
