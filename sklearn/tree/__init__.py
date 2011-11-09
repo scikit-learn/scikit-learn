5975f4ce3846b9e3f2b61be78a5045e45d514df3
Gilles Louppe <g.louppe@gmail.com>
2011-11-09 21:04:25 +0100

    Removed extra-trees from __init__

--- sklearn/tree/__init__.py
+++ sklearn/tree/__init__.py
@@ -4,6 +4,4 @@ Tree-based models.
 
 from .tree import DecisionTreeClassifier
 from .tree import DecisionTreeRegressor
-from .tree import ExtraTreeClassifier
-from .tree import ExtraTreeRegressor
 from .tree import export_graphviz
