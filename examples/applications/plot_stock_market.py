2e49b0715ecd84da907ebdee2356f8f61e240412
Olivier Grisel <olivier.grisel@ensta.org>
2011-11-09 05:25:58 -0800

    PEP8 + various cosmits

--- examples/applications/plot_stock_market.py
+++ examples/applications/plot_stock_market.py
@@ -9,6 +9,7 @@ the stock market structure from variations in historical quotes.
 The quantity that we use is the daily variation in quote price: quotes
 that are linked tend to cofluctuate during a day.
 
+
 Clustering
 ----------
 
@@ -17,6 +18,7 @@ amongst the :ref:`various clustering techniques <clustering>` available
 in the scikit-learn, we use :ref:`affinity_propagation` as it does
 not enforce equal-size clusters, and it can choose automatically the
 number of clusters from the data.
+
 
 Learning a graph structure
 --------------------------
@@ -32,6 +34,7 @@ could apply graph clustering techniques (such as
 :ref:`spectral_clustering`) on the corresponding graph, to retrieve a
 clustering consistent with the partial-independence structure.
 
+
 Embedding in 2D space
 ---------------------
 
@@ -39,7 +42,18 @@ For visualization purposes, we need to lay out the different symbols on a
 2D canvas. For this we use :ref:`manifold` techniques to retrieve 2D
 embedding.
 
-Finally, this example has a fair amount of visualization-related code, as
+
+Visualization
+-------------
+
+The output of the 3 models are combined in a 2D graph where nodes
+represents the stocks and edges the:
+
+- cluster labels are used to define the color of the nodes
+- the sparse covariance model is used to display the strength of the edges
+- the 2D embedding is used to position the nodes in the plan
+
+This example has a fair amount of visualization-related code, as
 visualization is crucial here to display the graph. One of the challenge
 is to position the labels minimizing overlap. For this we use an
 heuristic based on the direction of the nearest neighbor along each
@@ -68,76 +82,77 @@ d1 = datetime.datetime(2003, 01, 01)
 d2 = datetime.datetime(2008, 01, 01)
 
 symbol_dict = {
-        'TOT'  : 'Total',
-        'XOM'  : 'Exxon',
-        'CVX'  : 'Chevron',
-        'COP'  : 'ConocoPhillips',
-        'VLO'  : 'Valero Energy',
-        'MSFT' : 'Microsoft',
-        'IBM'  : 'IBM',
-        'TWX'  : 'Time Warner',
+        'TOT': 'Total',
+        'XOM': 'Exxon',
+        'CVX': 'Chevron',
+        'COP': 'ConocoPhillips',
+        'VLO': 'Valero Energy',
+        'MSFT': 'Microsoft',
+        'IBM': 'IBM',
+        'TWX': 'Time Warner',
         'CMCSA': 'Comcast',
-        'CVC'  : 'Cablevision',
-        'YHOO' : 'Yahoo',
-        'DELL' : 'Dell',
-        'HPQ'  : 'HP',
-        'AMZN' : 'Amazon',
-        'TM'   : 'Toyota',
-        'CAJ'  : 'Canon',
-        'MTU'  : 'Mitsubishi',
-        'SNE'  : 'Sony',
-        'F'    : 'Ford',
-        'HMC'  : 'Honda',
-        'NAV'  : 'Navistar',
-        'NOC'  : 'Northrop Grumman',
-        'BA'   : 'Boeing',
-        'KO'   : 'Coca Cola',
-        'MMM'  : '3M',
-        'MCD'  : 'Mc Donalds',
-        'PEP'  : 'Pepsi',
-        'KFT'  : 'Kraft Foods',
-        'K'    : 'Kellogg',
-        'UN'   : 'Unilever',
-        'MAR'  : 'Marriott',
-        'PG'   : 'Procter Gamble',
-        'CL'   : 'Colgate-Palmolive',
-        'NWS'  : 'News Corp',
-        'GE'   : 'General Electrics',
-        'WFC'  : 'Wells Fargo',
-        'JPM'  : 'JPMorgan Chase',
-        'AIG'  : 'AIG',
-        'AXP'  : 'American express',
-        'BAC'  : 'Bank of America',
-        'GS'   : 'Goldman Sachs',
-        'AAPL' : 'Apple',
-        'SAP'  : 'SAP',
-        'CSCO' : 'Cisco',
-        'TXN'  : 'Texas instruments',
-        'XRX'  : 'Xerox',
-        'LMT'  : 'Lookheed Martin',
-        'WMT'  : 'Wal-Mart',
-        'WAG'  : 'Walgreen',
-        'HD'   : 'Home Depot',
-        'GSK'  : 'GlaxoSmithKline',
-        'PFE'  : 'Pfizer',
-        'SNY'  : 'Sanofi-Aventis',
-        'NVS'  : 'Novartis',
-        'KMB'  : 'Kimberly-Clark',
-        'R'    : 'Ryder',
-        'GD'   : 'General Dynamics',
-        'RTN'  : 'Raytheon',
-        'CVS'  : 'CVS',
-        'CAT'  : 'Caterpillar',
-        'DD'   : 'DuPont de Nemours',
+        'CVC': 'Cablevision',
+        'YHOO': 'Yahoo',
+        'DELL': 'Dell',
+        'HPQ': 'HP',
+        'AMZN': 'Amazon',
+        'TM': 'Toyota',
+        'CAJ': 'Canon',
+        'MTU': 'Mitsubishi',
+        'SNE': 'Sony',
+        'F': 'Ford',
+        'HMC': 'Honda',
+        'NAV': 'Navistar',
+        'NOC': 'Northrop Grumman',
+        'BA': 'Boeing',
+        'KO': 'Coca Cola',
+        'MMM': '3M',
+        'MCD': 'Mc Donalds',
+        'PEP': 'Pepsi',
+        'KFT': 'Kraft Foods',
+        'K': 'Kellogg',
+        'UN': 'Unilever',
+        'MAR': 'Marriott',
+        'PG': 'Procter Gamble',
+        'CL': 'Colgate-Palmolive',
+        'NWS': 'News Corp',
+        'GE': 'General Electrics',
+        'WFC': 'Wells Fargo',
+        'JPM': 'JPMorgan Chase',
+        'AIG': 'AIG',
+        'AXP': 'American express',
+        'BAC': 'Bank of America',
+        'GS': 'Goldman Sachs',
+        'AAPL': 'Apple',
+        'SAP': 'SAP',
+        'CSCO': 'Cisco',
+        'TXN': 'Texas instruments',
+        'XRX': 'Xerox',
+        'LMT': 'Lookheed Martin',
+        'WMT': 'Wal-Mart',
+        'WAG': 'Walgreen',
+        'HD': 'Home Depot',
+        'GSK': 'GlaxoSmithKline',
+        'PFE': 'Pfizer',
+        'SNY': 'Sanofi-Aventis',
+        'NVS': 'Novartis',
+        'KMB': 'Kimberly-Clark',
+        'R': 'Ryder',
+        'GD': 'General Dynamics',
+        'RTN': 'Raytheon',
+        'CVS': 'CVS',
+        'CAT': 'Caterpillar',
+        'DD': 'DuPont de Nemours',
     }
 
 symbols, names = np.array(symbol_dict.items()).T
 
 quotes = [finance.quotes_historical_yahoo(symbol, d1, d2, asobject=True)
-                for symbol in symbols]
+          for symbol in symbols]
 
-open    = np.array([q.open   for q in quotes]).astype(np.float)
-close   = np.array([q.close  for q in quotes]).astype(np.float)
+open = np.array([q.open for q in quotes]).astype(np.float)
+close = np.array([q.close for q in quotes]).astype(np.float)
+
 # The daily variations of the quotes are what carry most information
 variation = close - open
 
@@ -148,28 +163,30 @@ correlations = np.corrcoef(variation)
 _, labels = cluster.affinity_propagation(correlations)
 n_labels = labels.max()
 
-for i in range(n_labels+1):
-    print 'Cluster %i: %s' % ((i+1),
-                              ', '.join(names[labels==i]))
+for i in range(n_labels + 1):
+    print 'Cluster %i: %s' % ((i + 1), ', '.join(names[labels == i]))
 
 ###############################################################################
 # Learn a graphical structure from the correlations
-model = covariance.GraphLassoCV()
+edge_model = covariance.GraphLassoCV()
+
 # standardize the time series: using correlations rather than covariance
 # is more efficient for structure recovery
 X = variation.copy().T
 X /= X.std(axis=0)
-model.fit(X)
+edge_model.fit(X)
 
 ###############################################################################
-# Find a low-dimension embedding for visualization
+# Find a low-dimension embedding for visualization: find the best position of
+# the nodes (the stocks) on a 2D plane
 
 # We use a dense eigen_solver to achieve reproducibility (arpack is
 # initiated with random vectors that we don't control). In addition, we
 # use a large number of neighbors to capture the large-scale structure.
-lle = manifold.LocallyLinearEmbedding(out_dim=2, eigen_solver='dense',
-            n_neighbors=6)
-embedding = lle.fit_transform(X.T).T
+node_position_model = manifold.LocallyLinearEmbedding(
+    out_dim=2, eigen_solver='dense', n_neighbors=6)
+
+embedding = node_position_model.fit_transform(X.T).T
 
 ###############################################################################
 # Visualization
@@ -179,14 +196,14 @@ ax = pl.axes([0., 0., 1., 1.])
 pl.axis('off')
 
 # Display a graph of the partial correlations
-partial_correlations = model.precision_.copy()
-d = 1/np.sqrt(np.diag(partial_correlations))
+partial_correlations = edge_model.precision_.copy()
+d = 1 / np.sqrt(np.diag(partial_correlations))
 partial_correlations *= d
 partial_correlations *= d[:, np.newaxis]
 non_zero = (np.abs(np.triu(partial_correlations, k=1)) > 0.02)
 
 # Plot the nodes using the coordinates of our embedding
-pl.scatter(embedding[0], embedding[1], s=100*d**2, c=labels,
+pl.scatter(embedding[0], embedding[1], s=100 * d ** 2, c=labels,
            cmap=pl.cm.spectral)
 
 # Plot the edges
@@ -197,17 +214,17 @@ segments = [[embedding[:, start], embedding[:, stop]]
             for start, stop in zip(start_idx, end_idx)]
 values = np.abs(partial_correlations[non_zero])
 lc = LineCollection(segments,
-                     zorder=0, cmap=pl.cm.hot_r,
-                     norm=pl.Normalize(0, .7*values.max()),
-                    )
+                    zorder=0, cmap=pl.cm.hot_r,
+                    norm=pl.Normalize(0, .7 * values.max()))
 lc.set_array(values)
-lc.set_linewidths(15*values)
+lc.set_linewidths(15 * values)
 ax.add_collection(lc)
 
 # Add a label to each node. The challenge here is that we want to
 # position the labels to avoid overlap with other labels
-for index, (name, label, (x, y)) in enumerate(zip(names,
-                                              labels, embedding.T)):
+for index, (name, label, (x, y)) in enumerate(
+    zip(names, labels, embedding.T)):
+
     dx = x - embedding[0]
     dx[index] = 1
     dy = y - embedding[1]
@@ -230,11 +247,12 @@ for index, (name, label, (x, y)) in enumerate(zip(names,
             horizontalalignment=horizontalalignment,
             verticalalignment=verticalalignment,
             bbox=dict(facecolor='w',
-                      edgecolor=pl.cm.spectral(label/float(n_labels)),
+                      edgecolor=pl.cm.spectral(label / float(n_labels)),
                       alpha=.6))
 
-pl.xlim(embedding[0].min() - .15*embedding[0].ptp(),
-        embedding[0].max() + .10*embedding[0].ptp(),)
-pl.ylim(embedding[1].min() - .03*embedding[1].ptp(),
-        embedding[1].max() + .03*embedding[1].ptp())
+pl.xlim(embedding[0].min() - .15 * embedding[0].ptp(),
+        embedding[0].max() + .10 * embedding[0].ptp(),)
+pl.ylim(embedding[1].min() - .03 * embedding[1].ptp(),
+        embedding[1].max() + .03 * embedding[1].ptp())
 
+pl.show()
