63e6a6a76be505c0fd569954142de8aeeb11dec3
Andy <amueller@ais.uni-bonn.de>
2011-11-14 12:29:50 +0100

    COSMIT pep8

--- sklearn/feature_extraction/kernel_approximation.py
+++ sklearn/feature_extraction/kernel_approximation.py
@@ -2,6 +2,7 @@ import numpy as np
 #import cuv_python as cp
 from sklearn.base import BaseEstimator
 
+
 class FourierSampler(BaseEstimator):
     """ Base class for Fourier approximations to kernel functions"""
     def fit_transform(self, X, y=None):
@@ -55,8 +56,9 @@ class RBFSampler(FourierSampler):
         """
 
         n_features = X.shape[1]
-        self.omega_ = np.sqrt(self.gamma) * np.random.normal(size=(n_features, self.D))
-        self.b = np.random.uniform(0, 2*np.pi, size=self.D)
+        self.omega_ = (np.sqrt(self.gamma)
+                * np.random.normal(size=(n_features, self.D)))
+        self.b = np.random.uniform(0, 2 * np.pi, size=self.D)
         return self
 
     def transform(self, X, y=None):
@@ -73,15 +75,15 @@ class RBFSampler(FourierSampler):
         X_new array-like, shape (n_samples, D)
         """
         projection = np.dot(X, self.omega_)
-        return np.sqrt(2)/np.sqrt(self.D)*np.cos(projection + self.b)
+        return np.sqrt(2.) / np.sqrt(self.D) * np.cos(projection + self.b)
 
 
 class SkewedChi2Sampler(FourierSampler):
-    """Approximates feature map of the "skewed chi-squared" kernel by Monte Carlo approximation
-    of its Fourier transform.
-    The "skewed chi-squared" kernel applies only to non-negative data and is particularly suited
-    for histograms. It is designed to share certain beneficial properties of the chi-squared
-    kernel.
+    """Approximates feature map of the "skewed chi-squared" kernel by Monte
+    Carlo approximation of its Fourier transform.
+    The "skewed chi-squared" kernel applies only to non-negative data and is
+    particularly suited for histograms. It is designed to share certain
+    beneficial properties of the chi-squared kernel.
 
     Parameters
     ----------
@@ -91,8 +93,9 @@ class SkewedChi2Sampler(FourierSampler):
         number of Monte Carlo samples per original feature.
         Equals the dimensionality of the computed feature space.
 
-    See "Random Fourier Approximations for Skewed Multiplicative Histogram Kernels"
-    by Fuxin Li, Catalin Ionescu and Cristian Sminchisescu for details.
+    See "Random Fourier Approximations for Skewed Multiplicative
+    Histogram Kernels" by Fuxin Li, Catalin Ionescu and Cristian
+    Sminchisescu for details.
     """
 
     def __init__(self, c, D):
@@ -116,8 +119,8 @@ class SkewedChi2Sampler(FourierSampler):
         n_features = X.shape[1]
         uniform = np.random.uniform(size=(n_features, self.D))
         # transform by inverse CDF of sech
-        self.omega_ = 1./np.pi * np.log(np.tan(np.pi/2. * uniform))
-        self.b = np.random.uniform(0, 2*np.pi, size=self.D)
+        self.omega_ = 1. / np.pi * np.log(np.tan(np.pi / 2. * uniform))
+        self.b = np.random.uniform(0, 2 * np.pi, size=self.D)
         return self
 
     def transform(self, X, y=None):
@@ -134,6 +137,4 @@ class SkewedChi2Sampler(FourierSampler):
         X_new array-like, shape (n_samples, D)
         """
         projection = np.dot(np.log(X + self.c), self.omega_)
-        return np.sqrt(2)/np.sqrt(self.D)*np.cos(projection + self.b)
-
-
+        return np.sqrt(2.) / np.sqrt(self.D) * np.cos(projection + self.b)
