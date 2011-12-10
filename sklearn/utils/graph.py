75ceb43c485b0051ea3ba7e0c0ed05646365053d
Lars Buitinck <L.J.Buitinck@uva.nl>
2011-12-10 12:25:29 +0100

    COSMIT rm some SciPy pre-0.7 compat code

--- sklearn/utils/graph.py
+++ sklearn/utils/graph.py
@@ -88,11 +88,7 @@ def _graph_laplacian_sparse(graph, normed=False, return_diag=False):
         # we need to fix that
         diag_idx = lap.row[diag_mask]
 
-        try:
-            lap = lap.tolil()
-        except AttributeError:
-            # versions of scipy prior to 0.7 do not implement .tolil()
-            lap = sparse.lil_matrix(lap.tocsr())
+        lap = lap.tolil()
 
         diagonal_holes = list(set(range(n_nodes)).difference(
                                 diag_idx))
