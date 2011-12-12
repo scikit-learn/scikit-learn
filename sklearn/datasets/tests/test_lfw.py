19705d40cec836b3c8408512d42cae95ad84e212
Lars Buitinck <L.J.Buitinck@uva.nl>
2011-12-12 12:32:46 +0100

    COSMIT use urlretrieve and "with" syntax in LFW module
    Reverts c953f1c99b3df293d8810eaaacc272e04773ea40.

--- sklearn/datasets/tests/test_lfw.py
+++ sklearn/datasets/tests/test_lfw.py
@@ -76,35 +76,31 @@ def setup_module():
                 raise SkipTest
 
     # add some random file pollution to test robustness
-    f = open(os.path.join(LFW_HOME, 'lfw_funneled', '.test.swp'), 'wb')
-    f.write('Text file to be ignored by the dataset loader.')
-    f.close()
+    with open(os.path.join(LFW_HOME, 'lfw_funneled', '.test.swp'), 'wb') as f:
+        f.write('Text file to be ignored by the dataset loader.')
 
     # generate some pairing metadata files using the same format as LFW
-    f = open(os.path.join(LFW_HOME, 'pairsDevTrain.txt'), 'wb')
-    f.write("10\n")
-    more_than_two = [name for name, count in counts.iteritems()
-                     if count >= 2]
-    for i in range(5):
-        name = random_state.choice(more_than_two)
-        first, second = random_state.sample(range(counts[name]), 2)
-        f.write('%s\t%d\t%d\n' % (name, first, second))
+    with open(os.path.join(LFW_HOME, 'pairsDevTrain.txt'), 'wb') as f:
+        f.write("10\n")
+        more_than_two = [name for name, count in counts.iteritems()
+                         if count >= 2]
+        for i in range(5):
+            name = random_state.choice(more_than_two)
+            first, second = random_state.sample(range(counts[name]), 2)
+            f.write('%s\t%d\t%d\n' % (name, first, second))
 
-    for i in range(5):
-        first_name, second_name = random_state.sample(FAKE_NAMES, 2)
-        first_index = random_state.choice(range(counts[first_name]))
-        second_index = random_state.choice(range(counts[second_name]))
-        f.write('%s\t%d\t%s\t%d\n' % (first_name, first_index,
-                                      second_name, second_index))
-    f.close()
+        for i in range(5):
+            first_name, second_name = random_state.sample(FAKE_NAMES, 2)
+            first_index = random_state.choice(range(counts[first_name]))
+            second_index = random_state.choice(range(counts[second_name]))
+            f.write('%s\t%d\t%s\t%d\n' % (first_name, first_index,
+                                          second_name, second_index))
 
-    f = open(os.path.join(LFW_HOME, 'pairsDevTest.txt'), 'wb')
-    f.write("Fake place holder that won't be tested")
-    f.close()
+    with open(os.path.join(LFW_HOME, 'pairsDevTest.txt'), 'wb') as f:
+        f.write("Fake place holder that won't be tested")
 
-    f = open(os.path.join(LFW_HOME, 'pairs.txt'), 'wb')
-    f.write("Fake place holder that won't be tested")
-    f.close()
+    with open(os.path.join(LFW_HOME, 'pairs.txt'), 'wb') as f:
+        f.write("Fake place holder that won't be tested")
 
 
 def teardown_module():
