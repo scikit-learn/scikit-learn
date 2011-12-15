d2b50d841e820508ddd3a8ca7bc1efb35197b2bd
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2011-12-15 15:24:44 +0100

    STY: prettify doctest

--- sklearn/grid_search.py
+++ sklearn/grid_search.py
@@ -205,11 +205,12 @@ class GridSearchCV(BaseEstimator):
     >>> clf.fit(iris.data, iris.target)
     ...                             # doctest: +NORMALIZE_WHITESPACE +ELLIPSIS
     GridSearchCV(cv=None,
-           estimator=SVR(C=1.0, cache_size=..., coef0=..., degree=..., epsilon=..., gamma=...,
-      kernel='rbf', probability=False, scale_C=False, shrinking=True, tol=...),
-           fit_params={}, iid=True, loss_func=None, n_jobs=1,
-           param_grid=...,
-           ...)
+        estimator=SVR(C=1.0, cache_size=..., coef0=..., degree=...,
+            epsilon=..., gamma=..., kernel='rbf', probability=False,
+            scale_C=False, shrinking=True, tol=...),
+        fit_params={}, iid=True, loss_func=None, n_jobs=1,
+            param_grid=...,
+            ...)
 
     Attributes
     ----------
