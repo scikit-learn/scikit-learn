5ad5207dadca9a3a831ff3be01de77d55a949083
Andreas Mueller <amueller@ais.uni-bonn.de>
2011-12-16 22:55:42 +0100

    COSMIT pep8

--- examples/svm/plot_svm_nonlinear.py
+++ examples/svm/plot_svm_nonlinear.py
@@ -17,22 +17,21 @@ from sklearn import svm
 xx, yy = np.meshgrid(np.linspace(-5, 5, 500), np.linspace(-5, 5, 500))
 np.random.seed(0)
 X = np.random.randn(300, 2)
-Y = np.logical_xor(X[:,0]>0, X[:,1]>0)
+Y = np.logical_xor(X[:, 0] > 0, X[:, 1] > 0)
 
 # fit the model
 clf = svm.NuSVC()
 clf.fit(X, Y)
 
-# plot the decision function for each datapoint on the grid 
+# plot the decision function for each datapoint on the grid
 Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
 Z = Z.reshape(xx.shape)
 
 pl.set_cmap(pl.cm.jet)
 pl.pcolormesh(xx, yy, Z)
-pl.scatter(X[:,0], X[:,1], c=Y)
+pl.scatter(X[:, 0], X[:, 1], c=Y)
 pl.xticks(())
 pl.yticks(())
 
 pl.axis('tight')
 pl.show()
-
