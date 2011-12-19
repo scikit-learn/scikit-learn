7800b7e7f9c3269157d21d954c582348262a559e
Christoph Hermes <hermes@hausmilbe.net>
2011-12-19 14:05:57 +0100

    removed functions and simplified loop (as suggested by agramfort in the pull request)

--- examples/polynomial_classifier.py
+++ examples/polynomial_classifier.py
@@ -10,66 +10,42 @@ from sklearn.decomposition import PCA
 
 import random
 
+print __doc__
 
-def divide_dataset(X, y):
-    """Randomly divide dataset given by X,y into train (2/3) and test set (1/3)
+# list of databases the Polynomial Classifier is applied to
+DBs = [('iris', datasets.load_iris()), ('digits', datasets.load_digits())]
 
-    Returns
-    -------
-    X_test : array-like, shape = [n_samples, n_features]
-    y_test : array-like, shape = [n_samples, n_features]
-    X_train : array-like, shape = [n_samples, n_features]
-    y_train : array-like, shape = [n_samples, n_features]
-    """
+for DB_desc, DB_data in DBs:
 
+    X = DB_data.data
+    y = DB_data.target
+
+    # Randomly divide dataset into train (2/3) and test set (1/3)
     n_samples, n_features = X.shape
     p = range(n_samples)
     random.seed(0)
     random.shuffle(p)
     X, y = X[p], y[p]
     third = int(n_samples / 3)
-
     X_test, y_test = X[:third], y[:third]
     X_train, y_train = X[third:], y[third:]
 
-    return X_test, y_test, X_train, y_train
+    # PCA: reduce feature dimension by keeping specified amount of information
+    pca = PCA(n_components=0.95).fit(X_train)
 
+    # apply classifier
+    clf = PolynomialClassifier(degree=2)
+    X_train_pca = pca.transform(X_train)
+    X_test_pca = pca.transform(X_test)
+    y_pred = clf.fit(X_train_pca, y_train).predict(X_test_pca)
 
-def pc_apply_datasets():
-    """Apply Polynomial Classifier to 'iris' and 'digits' datasets and prints
-    results on console
-    """
+    # Compute confusion matrix
+    cm = confusion_matrix(y_test, y_pred)
 
-    DBs = [('iris', datasets.load_iris()), ('digits', datasets.load_digits())]
+    # print info on console
+    print "-" * 80
+    print pca, clf
+    print "Dataset size: n_samples(Train) = %d, n_samples(Test) = %d" % (
+        X_train.shape[0], X_test.shape[0])
+    print "Confusion matrix for '%s' dataset:\n" % DB_desc, cm
 
-    for DB_desc, database in DBs:
-
-        X = database.data
-        y = database.target
-
-        X_test, y_test, X_train, y_train = divide_dataset(X, y)
-
-        # PCA: reduce feature dimension by keeping specified amount of
-        # information
-        pca = PCA(n_components=0.98).fit(X_train)
-
-        # apply classifier
-        clf = PolynomialClassifier(degree=2)
-        X_train_pca = pca.transform(X_train)
-        X_test_pca = pca.transform(X_test)
-        y_pred = clf.fit(X_train_pca, y_train).predict(X_test_pca)
-
-        # Compute confusion matrix
-        cm = confusion_matrix(y_test, y_pred)
-
-        # print info on console
-        print "-" * 80
-        print pca, clf
-        print "Dataset size: n_samples(Train) = %d, n_samples(Test) = %d" % (
-            X_train.shape[0], X_test.shape[0])
-        print "Confusion matrix for '%s' dataset:\n" % DB_desc, cm
-
-
-if __name__ == '__main__':
-    print __doc__
-    pc_apply_datasets()
