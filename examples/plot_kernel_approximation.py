44e9baee1c8427fdab5f2f2e218b5c380fcd8877
Andreas Mueller <amueller@ais.uni-bonn.de>
2011-12-19 17:35:25 +0100

    ENH timing results in approx kernel example

--- examples/plot_kernel_approximation.py
+++ examples/plot_kernel_approximation.py
@@ -12,7 +12,10 @@ approximate mapping are shown.
 
 Sampling more dimensions clearly leads to better classification results, but
 comes at a greater cost. This means there is a tradeoff between runtime and
-accuracy, given by the parameter n_components.
+accuracy, given by the parameter n_components.  Note that solving the Linear
+SVM and also the approximate kernel SVM could be greatly accelerated by using
+stochastic gradient descent via SGDClassifier. This is not easily possible for
+the case of the kernelized SVM.
 """
 print __doc__
 
@@ -23,6 +26,7 @@ print __doc__
 # Standard scientific Python imports
 import pylab as pl
 import numpy as np
+from time import time
 
 # Import datasets, classifiers and performance metrics
 from sklearn import datasets, svm, pipeline
@@ -33,56 +37,83 @@ digits = datasets.load_digits()
 
 # To apply an classifier on this data, we need to flatten the image, to
 # turn the data in a (samples, feature) matrix:
-n_samples = len(digits.images)
-data = digits.images.reshape((n_samples, -1))
+n_samples = len(digits.data)
+data = digits.data / 16.
 
 # We learn the digits on the first half of the digits
 data_train, targets_train = data[:n_samples / 2], digits.target[:n_samples / 2]
 
+
 # Now predict the value of the digit on the second half:
 data_test, targets_test = data[n_samples / 2:], digits.target[n_samples / 2:]
+#data_test = scaler.transform(data_test)
 
 # Create a classifier: a support vector classifier
-kernel_svm = svm.SVC(gamma=0.001)
+kernel_svm = svm.SVC(gamma=.2)
 linear_svm = svm.LinearSVC()
 
 # create pipeline from kernel approximation
 # and linear svm
-feature_map = RBFSampler(gamma=0.001)
+feature_map = RBFSampler(gamma=.2, random_state=1)
 approx_kernel_svm = pipeline.Pipeline([("feature_map", feature_map),
     ("svm", svm.LinearSVC())])
 
 # fit and predict using linear and kernel svm:
+
+kernel_svm_time = time()
 kernel_svm.fit(data_train, targets_train)
 kernel_svm_score = kernel_svm.score(data_test, targets_test)
+kernel_svm_time = time() - kernel_svm_time
 
+linear_svm_time = time()
 linear_svm.fit(data_train, targets_train)
 linear_svm_score = linear_svm.score(data_test, targets_test)
+linear_svm_time = time() - linear_svm_time
 
-sample_sizes = 20 * np.arange(1, 15)
+sample_sizes = 50 * np.arange(1, 10)
 approx_kernel_scores = []
+approx_kernel_times = []
+
 for D in sample_sizes:
     approx_kernel_svm.set_params(feature_map__n_components=D)
+    approx_kernel_timing = time()
     approx_kernel_svm.fit(data_train, targets_train)
+    approx_kernel_times.append(time() - approx_kernel_timing)
     score = approx_kernel_svm.score(data_test, targets_test)
     approx_kernel_scores.append(score)
 
 # plot the results:
-pl.plot(sample_sizes, approx_kernel_scores, label="approximate kernel")
+accuracy = pl.gca()
+print(kernel_svm_time)
+print(linear_svm_time)
+print(approx_kernel_times)
+print(kernel_svm_score)
+# second y axis for timeings
+timescale = accuracy.twinx()
+
+accuracy.plot(sample_sizes, approx_kernel_scores, label="approximate kernel")
+timescale.plot(sample_sizes, approx_kernel_times, '--',
+        label='runtime approx. kernel')
 
 # horizontal lines for exact rbf and linear kernels:
-pl.plot([sample_sizes[0], sample_sizes[-1]], [linear_svm_score,
+accuracy.plot([sample_sizes[0], sample_sizes[-1]], [linear_svm_score,
     linear_svm_score], label="linear svm")
-pl.plot([sample_sizes[0], sample_sizes[-1]], [kernel_svm_score,
+timescale.plot([sample_sizes[0], sample_sizes[-1]], [linear_svm_time,
+        linear_svm_time], '--', label='runtime linear')
+
+accuracy.plot([sample_sizes[0], sample_sizes[-1]], [kernel_svm_score,
     kernel_svm_score], label="rbf svm")
+timescale.plot([sample_sizes[0], sample_sizes[-1]], [kernel_svm_time,
+        kernel_svm_time], '--', label='runtime exact kernel')
 
 # vertical line for dataset dimensionality = 64
-pl.plot([64, 64], [0.7, 1], label="original dimensionality")
+accuracy.plot([64, 64], [0.7, 1], label="original dimensionality")
 
 # legends and labels
-pl.xlim(sample_sizes[0], sample_sizes[-1])
-pl.ylim(np.min(approx_kernel_scores), 1)
-pl.xlabel("Sampling steps = transformed feature dimension")
-pl.ylabel("Classification accuracy")
-pl.legend(loc='best')
+accuracy.set_xlim(sample_sizes[0], sample_sizes[-1])
+accuracy.set_ylim(np.min(approx_kernel_scores), 1)
+accuracy.set_xlabel("Sampling steps = transformed feature dimension")
+accuracy.set_ylabel("Classification accuracy")
+timescale.set_ylabel("Training time in seconds (dashed lines)")
+accuracy.legend(loc='best')
 pl.show()
