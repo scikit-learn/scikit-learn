8330697e301164b595eb63f9d1f21b8f4695c24e
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2011-12-19 16:10:11 +0100

    ENH : better plot_randomized_lasso.py with score path

--- examples/linear_model/plot_randomized_lasso.py
+++ examples/linear_model/plot_randomized_lasso.py
@@ -1,51 +1,83 @@
 """
-==================================================
+==============================================
 Randomized Lasso: feature selection with Lasso
-==================================================
+==============================================
 
 """
 print __doc__
 
 import pylab as pl
 import numpy as np
+from scipy.interpolate import interp1d
 
-from sklearn.linear_model import RandomizedLasso, LassoLarsIC, LassoLarsCV
-from sklearn.datasets import make_regression
+from sklearn.linear_model import RandomizedLasso, lars_path, LassoLarsCV
+from sklearn.cross_validation import ShuffleSplit
+from sklearn.datasets import load_diabetes
+from sklearn.preprocessing import Scaler
 
-
-################################################################################
+###############################################################################
 # Generating simulated data with Gaussian weigthts
-X, y, coef = make_regression(n_samples=40, n_features=100,
-                             n_informative=10, coef=True, random_state=42)
 
-################################################################################
-# Plot the true weights, the estimated weights and the histogram of the
-# weights
-pl.figure(figsize=(6, 5))
-pl.title("Weights of the model")
-scores = list()
-scores.append(coef/coef.max())
-n_alphas = 12
-alphas = np.logspace(-1, 1.5, n_alphas)[::-1]
-for alpha in alphas:
-    clf = RandomizedLasso(verbose=True, alpha=alpha, random_state=42)
-    clf.fit(X, y)
-    scores.append(clf.scores_)
-scores = np.array(scores)
-pl.imshow(np.rot90(scores), cmap=pl.cm.spectral,
-          interpolation='nearest', aspect='auto')
-#pl.plot(coef/coef.max(), 'g-', label="Ground truth coefficients")
-alpha_aic = LassoLarsIC(criterion='aic').fit(X, y).alpha_
-alpha_bic = LassoLarsIC(criterion='bic').fit(X, y).alpha_
-alpha_cv = LassoLarsCV().fit(X, y).alpha
-pl.xlabel("alpha (AIC=%.2e, BIC=%.2e, CV=%.2e)" %
-          (alpha_aic, alpha_bic, alpha_cv))
-pl.ylabel("Features")
+diabetes = load_diabetes()
+X = diabetes.data
+y = diabetes.target
 
-pl.xticks(np.arange(0, n_alphas+1),
-          ['Truth', ] + ['%.1e' % a for a in alphas])
-pl.colorbar()
-#pl.legend(loc=1)
+n_bad_features = 30
+n_samples, n_good_features = X.shape
 
+# Some noisy data not correlated
+E = np.random.normal(size=(n_samples, n_bad_features))
+# Add the noisy data to the informative features
+X = np.hstack((X, E))
+X = Scaler().fit_transform(X)
+
+n_features = n_good_features + n_bad_features
+
+###############################################################################
+# Plot stability selection path
+
+print "Computing regularization path using the LARS ..."
+
+a = 0.2
+n_resampling = 200
+rng = np.random.RandomState(42)
+xx_grid = np.linspace(0, 1, 100)
+scores_path = np.zeros((n_features, 100))
+
+for k in xrange(n_resampling):
+    weights = 1. - a * rng.random_integers(0, 1, size=(n_features,))
+    X_r = X * weights[np.newaxis, :]
+    alphas, _, coefs = lars_path(X_r, y, method='lasso', verbose=False)
+
+    xx = np.sum(np.abs(coefs.T), axis=1)
+    xx /= xx[-1]
+
+    interpolator = interp1d(xx, coefs)
+    coefs_grid = interpolator(xx_grid)
+    scores_path += (coefs_grid != 0.0)
+
+scores_path /= n_resampling
+
+pl.figure()
+pl.plot(xx_grid, scores_path[:n_good_features].T, 'r')
+pl.plot(xx_grid, scores_path[n_good_features:].T, 'k')
+ymin, ymax = pl.ylim()
+pl.xlabel('|coef| / max|coef|')
+pl.ylabel('Proportion of times selected')
+pl.title('Stability Scores Path')
+pl.axis('tight')
+pl.ylim([0, 1.05])
+
+###############################################################################
+# Plot the estimated stability scores for best cross-validated alpha
+
+cv = ShuffleSplit(n_samples, n_iterations=50, test_fraction=0.2)
+alpha_cv = LassoLarsCV(cv=cv).fit(X, y).alpha
+clf = RandomizedLasso(verbose=True, alpha=alpha_cv, random_state=42, a=a)
+clf.fit(X, y)
+
+pl.figure()
+pl.plot(clf.scores_)
+pl.xlabel("Features")
+pl.ylabel("Selection score")
 pl.show()
-
