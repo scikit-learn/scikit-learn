791608c2003ff69d88986b419fabea537df73a18
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2011-12-19 12:01:11 +0100

    mv randomized_lasso.py randomized_l1.py

--- sklearn/linear_model/__init__.py
+++ sklearn/linear_model/__init__.py
@@ -25,8 +25,6 @@ from .ridge import Ridge, RidgeCV, RidgeClassifier, RidgeClassifierCV, \
                    ridge_regression
 from .logistic import LogisticRegression
 from .omp import orthogonal_mp, orthogonal_mp_gram, OrthogonalMatchingPursuit
-from .randomized_lasso import RandomizedLasso, RandomizedLogistic
+from .randomized_l1 import RandomizedLasso, RandomizedLogistic
 
 from . import sparse
-
-
