0d927f00ea73a06ad3bf11c283bf52c72691f827
Mathieu Blondel <mathieu@mblondel.org>
2011-12-19 17:18:19 +0100

    Cosmit.

--- sklearn/utils/tests/test_sparsefuncs.py
+++ sklearn/utils/tests/test_sparsefuncs.py
@@ -8,11 +8,11 @@ from sklearn.utils.sparsefuncs import mean_variance_axis0
 def test_mean_variance_axis0():
     X, _ = make_classification(5, 4, random_state=0)
     # Sparsify the array a little bit
-    X[0, 0] = 0; X[2, 1] = 0; X[4, 3] = 0
+    X[0, 0] = 0
+    X[2, 1] = 0
+    X[4, 3] = 0
     X_csr = sp.csr_matrix(X)
     X_means, X_vars = mean_variance_axis0(X_csr)
 
-    assert_array_almost_equal(X_means,
-                              np.mean(X, axis=0))
-    assert_array_almost_equal(X_vars,
-                              np.var(X, axis=0))
+    assert_array_almost_equal(X_means, np.mean(X, axis=0))
+    assert_array_almost_equal(X_vars, np.var(X, axis=0))
