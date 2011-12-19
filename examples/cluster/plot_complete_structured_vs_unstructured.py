fe9447e6c52422ee7167f96b624c554bb7c68df9
Jan Hendrik Metzen <jhm@informatik.uni-bremen.de>
2011-12-19 14:16:47 +0100

    EXAMPLES: Comparing structured and unstructured hierarchical clustering with complete linkage.

---
+++ examples/cluster/plot_complete_structured_vs_unstructured.py
@@ -0,0 +1,86 @@
+"""
+=====================================================================
+Hierarchical clustering: structured vs unstructured complete linkage
+=====================================================================
+
+Example builds a swiss roll dataset and runs
+:ref:`hierarchical_clustering` with complete linkage on their position.
+
+In a first step, the hierarchical clustering without connectivity
+constraints on structure, solely based on distance, whereas in a second
+step clustering restricted to the k-Nearest Neighbors graph: it's a
+hierarchical clustering with structure prior.
+
+Some of the clusters learned without connectivity constraints do not
+respect the structure of the swiss roll and extend across different folds of
+the manifolds. On the opposite, when opposing connectivity constraints,
+the clusters form a nice parcellation of the swiss roll.
+"""
+
+# Authors : Jan Hendrik Metzen, 2011
+# License: BSD
+# Reference: based on plot_ward_structured_vs_unstructured
+
+print __doc__
+
+import time as time
+import numpy as np
+import pylab as pl
+import mpl_toolkits.mplot3d.axes3d as p3
+from sklearn.cluster import HierarchicalClustering
+from sklearn.datasets.samples_generator import make_swiss_roll
+
+###############################################################################
+# Generate data (swiss roll dataset)
+n_samples = 1000
+noise = 0.05
+X, _ = make_swiss_roll(n_samples, noise)
+# Make it thinner
+X[:, 1] *= .5
+
+###############################################################################
+# Compute clustering
+print "Compute unstructured hierarchical clustering..."
+st = time.time()
+hc = HierarchicalClustering(n_clusters=6, linkage_criterion="complete").fit(X)
+label = hc.labels_
+print "Elapsed time: ", time.time() - st
+print "Number of points: ", label.size
+
+###############################################################################
+# Plot result
+fig = pl.figure()
+ax = p3.Axes3D(fig)
+ax.view_init(7, -80)
+for l in np.unique(label):
+    ax.plot3D(X[label == l, 0], X[label == l, 1], X[label == l, 2],
+              'o', color=pl.cm.jet(np.float(l) / np.max(label + 1)))
+pl.title('Without connectivity constraints')
+
+
+###############################################################################
+# Define the structure A of the data. Here a 10 nearest neighbors
+from sklearn.neighbors import kneighbors_graph
+connectivity = kneighbors_graph(X, n_neighbors=10)
+
+###############################################################################
+# Compute clustering
+print "Compute structured hierarchical clustering..."
+st = time.time()
+hc = HierarchicalClustering(n_clusters=6, connectivity=connectivity,
+                            linkage_criterion="complete").fit(X)
+label = hc.labels_
+print "Elapsed time: ", time.time() - st
+print "Number of points: ", label.size
+
+###############################################################################
+# Plot result
+fig = pl.figure()
+ax = p3.Axes3D(fig)
+ax.view_init(7, -80)
+for l in np.unique(label):
+    ax.plot3D(X[label == l, 0], X[label == l, 1], X[label == l, 2],
+              'o', color=pl.cm.jet(float(l) / np.max(label + 1)))
+pl.title('With connectivity constraints')
+
+pl.show()
