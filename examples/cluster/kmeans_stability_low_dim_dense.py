d32a22002422222932c3cd159f6fbafd31e18c94
Olivier Grisel <olivier.grisel@ensta.org>
2011-12-20 16:10:37 +0100

    FIX compat for errorbar legend for old matplotlib versions

--- examples/cluster/kmeans_stability_low_dim_dense.py
+++ examples/cluster/kmeans_stability_low_dim_dense.py
@@ -85,16 +85,14 @@ for factory, init, params in cases:
             inertia[i, run_id] = km.inertia_
             print "Inertia for n_init=%02d, run_id=%d: %0.3f" % (
                 n_init, run_id, km.inertia_)
-
-    plots.append(
-        pl.errorbar(n_init_range, inertia.mean(axis=1), inertia.std(axis=1)))
+    p = pl.errorbar(n_init_range, inertia.mean(axis=1), inertia.std(axis=1))
+    plots.append(p[0])
     n_reinit = params.get('n_reinit')
     if n_reinit is not None:
         legends.append("%s with %s init and %d reinit" % (
             factory.__name__, init, n_reinit))
     else:
         legends.append("%s with %s init" % (factory.__name__, init))
-
 
 pl.xlabel('n_init')
 pl.ylabel('inertia')
