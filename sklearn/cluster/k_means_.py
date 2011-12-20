FIX: numerical instability caused by collapsed allocation of bad clusters to the center of mass

sklearn/cluster/k_means_.py
@@ -237,14 +237,19 @@ def k_means(X, k, init='k-means++', n_init=10, max_iter=300, verbose=0,
         if verbose:
             print 'Initialization complete'
 
+        # Allocate memory to store the distances for each sample to it's
+        # closer center for reallocation in case of ties
+        distances = np.zeros(shape=(X.shape[0],), dtype=np.float64)
+
         # iterations
         for i in range(max_iter):
             centers_old = centers.copy()
             # labels assignement is also called the E-step of EM
-            labels, inertia = _labels_inertia(X, x_squared_norms, centers)
+            labels, inertia = _labels_inertia(X, x_squared_norms, centers,
+                                              distances=distances)
 
             # computation of the means is also called the M-step of EM
-            centers = _centers(X, labels, k)
+            centers = _centers(X, labels, k, distances)
 
             if verbose:
                 print 'Iteration %i, inertia %s' % (i, inertia)
@@ -291,7 +296,7 @@ def _labels_inertia(X, x_squared_norms, centers, distances=None):
     centers: float64 array, shape (k, n_features)
         The cluster centers.
 
-    distances: float64 array, shape (k, n_samples)
+    distances: float64 array, shape (n_samples,)
         Distances for each sample to its closest center.
 
     Returns
@@ -306,7 +311,7 @@ def _labels_inertia(X, x_squared_norms, centers, distances=None):
     # set the default value of centers to -1 to be able to detect any anomaly
     # easily
     labels = - np.ones(n_samples, np.int32)
-    if distances == None:
+    if distances is None:
         distances = np.zeros(shape=(0,), dtype=np.float64)
     if sp.issparse(X):
         inertia = _k_means._assign_labels_csr(
@@ -317,7 +322,7 @@ def _labels_inertia(X, x_squared_norms, centers, distances=None):
     return labels, inertia
 
 
-def _centers(X, labels, n_clusters):
+def _centers(X, labels, n_clusters, distances):
     """M step of the K-means EM algorithm
 
     Computation of cluster centers / means.
@@ -342,15 +347,17 @@ def _centers(X, labels, n_clusters):
 
     # TODO: explicit dtype handling
     centers = np.empty((n_clusters, n_features))
-    X_center = None
+    far_from_centers = None
+    reallocated_idx = 0
+
     for center_id in range(n_clusters):
         center_mask = labels == center_id
         if not np.any(center_mask):
-            # The centroid of empty clusters is set to the center of
-            # everything
-            if X_center is None:
-                X_center = X.mean(axis=0)
-            centers[center_id] = X_center
+            # Reassign empty cluster center to sample far from any cluster
+            if far_from_centers is None:
+                far_from_centers = distances.argsort()[::-1]
+            centers[center_id] = X[far_from_centers[reallocated_idx]]
+            reallocated_idx += 1
         else:
             centers[center_id] = np.mean(X[center_mask], axis=0)
     return centers

sklearn/cluster/tests/test_k_means.py
@@ -5,9 +5,10 @@ from scipy import sparse as sp
 from numpy.testing import assert_equal
 from numpy.testing import assert_array_equal
 from numpy.testing import assert_array_almost_equal
+from nose.tools import assert_almost_equal
+from nose.tools import assert_greater
 from nose.tools import assert_raises
 from nose.tools import assert_true
-from nose.tools import assert_almost_equal
 
 from sklearn.metrics.cluster import v_measure_score
 from sklearn.cluster import KMeans
@@ -242,15 +243,26 @@ def test_k_means_copyx():
     assert_array_almost_equal(my_X, X)
 
 
-def test_k_means_singleton():
-    """Check k_means with bad initialization and singleton clustering."""
-    my_X = np.array([[1.1, 1.1], [0.9, 1.1], [1.1, 0.9], [0.9, 0.9]])
-    array_init = np.array([[1.0, 1.0], [5.0, 5.0]])
-    k_means = KMeans(init=array_init, k=2, random_state=42, n_init=1)
+def test_k_means_non_collapsed():
+    """Check k_means with a bad initialization does not yield a singleton
+
+    Starting with bad centers that are quickly ignored should not
+    result in a repositioning of the centers to the center of mass that
+    would lead to collapsed centers which in turns make the clustering
+    dependent of the numerical unstabilities.
+    """
+    my_X = np.array([[1.1, 1.1], [0.9, 1.1], [1.1, 0.9], [0.9, 1.1]])
+    array_init = np.array([[1.0, 1.0], [5.0, 5.0], [-5.0, -5.0]])
+    k_means = KMeans(init=array_init, k=3, random_state=42, n_init=1)
     k_means.fit(my_X)
 
-    # must be singleton clustering
-    assert_equal(np.unique(k_means.labels_).size, 1)
+    # centers must not been collapsed
+    assert_equal(len(np.unique(k_means.labels_)), 3)
+
+    centers = k_means.cluster_centers_
+    assert_greater(np.linalg.norm(centers[0] - centers[1]), 0.1)
+    assert_greater(np.linalg.norm(centers[0] - centers[2]), 0.1)
+    assert_greater(np.linalg.norm(centers[1] - centers[2]), 0.1)
 
 
 def test_predict():