b791792fea1837c608c4577dd02b51f9c33c3bf2
Vlad Niculae <vlad@vene.ro>
2011-12-21 10:59:39 +0100

    pyflakes test_multiclass

--- sklearn/tests/test_multiclass.py
+++ sklearn/tests/test_multiclass.py
@@ -1,8 +1,6 @@
 
 import numpy as np
-import scipy.sparse as sp
 
-from numpy.testing import assert_array_almost_equal
 from numpy.testing import assert_array_equal
 from nose.tools import assert_equal
 from nose.tools import assert_almost_equal
@@ -122,12 +120,12 @@ def test_ovo_exceptions():
 def test_ovo_fit_predict():
     # A classifier which implements decision_function.
     ovo = OneVsOneClassifier(LinearSVC())
-    pred = ovo.fit(iris.data, iris.target).predict(iris.data)
+    ovo.fit(iris.data, iris.target).predict(iris.data)
     assert_equal(len(ovo.estimators_), n_classes * (n_classes - 1) / 2)
 
     # A classifier which implements predict_proba.
     ovo = OneVsOneClassifier(MultinomialNB())
-    pred = ovo.fit(iris.data, iris.target).predict(iris.data)
+    ovo.fit(iris.data, iris.target).predict(iris.data)
     assert_equal(len(ovo.estimators_), n_classes * (n_classes - 1) / 2)
 
 
@@ -148,12 +146,12 @@ def test_ecoc_exceptions():
 def test_ecoc_fit_predict():
     # A classifier which implements decision_function.
     ecoc = OutputCodeClassifier(LinearSVC(), code_size=2)
-    pred = ecoc.fit(iris.data, iris.target).predict(iris.data)
+    ecoc.fit(iris.data, iris.target).predict(iris.data)
     assert_equal(len(ecoc.estimators_), n_classes * 2)
 
     # A classifier which implements predict_proba.
     ecoc = OutputCodeClassifier(MultinomialNB(), code_size=2)
-    pred = ecoc.fit(iris.data, iris.target).predict(iris.data)
+    ecoc.fit(iris.data, iris.target).predict(iris.data)
     assert_equal(len(ecoc.estimators_), n_classes * 2)
 
 
