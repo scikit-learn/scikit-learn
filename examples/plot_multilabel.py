955fdf8e1655c18aa16b7949226909c656503d71
Vlad Niculae <vlad@vene.ro>
2011-12-22 23:08:06 +0100

    Change seed to make example behave better

--- examples/plot_multilabel.py
+++ examples/plot_multilabel.py
@@ -91,14 +91,14 @@ pl.figure(figsize=(13, 6))
 
 X, Y = make_multilabel_classification(n_classes=2, n_labels=1,
                                       allow_unlabeled=True,
-                                      random_state=42)
+                                      random_state=0)
 
 plot_subfigure(X, Y, 1, "With unlabeled samples + CCA", "cca")
 plot_subfigure(X, Y, 2, "With unlabeled samples + PCA", "pca")
 
 X, Y = make_multilabel_classification(n_classes=2, n_labels=1,
                                       allow_unlabeled=False,
-                                      random_state=42)
+                                      random_state=0)
 
 plot_subfigure(X, Y, 3, "Without unlabeled samples + CCA", "cca")
 plot_subfigure(X, Y, 4, "Without unlabeled samples + PCA", "pca")
