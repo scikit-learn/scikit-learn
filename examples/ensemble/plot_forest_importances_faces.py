1d1348eb2126a0a8ee1a18b37a5254b59c3a4c76
Olivier Grisel <olivier.grisel@ensta.org>
2011-12-30 10:00:39 +0100

    Update random forest face example to use several cores

--- examples/ensemble/plot_forest_importances_faces.py
+++ examples/ensemble/plot_forest_importances_faces.py
@@ -9,10 +9,14 @@ the more important.
 """
 print __doc__
 
+from time import time
 import pylab as pl
 
 from sklearn.datasets import fetch_olivetti_faces
 from sklearn.ensemble import ExtraTreesClassifier
+
+# Number of cores to use to perform parallel fitting of the forest model
+n_jobs=2
 
 # Loading the digits dataset
 data = fetch_olivetti_faces()
@@ -24,12 +28,15 @@ X = X[mask]
 y = y[mask]
 
 # Build a forest and compute the pixel importances
+print "Fitting ExtraTreesClassifier on faces data with %d cores..." % n_jobs
+t0 = time()
 forest = ExtraTreesClassifier(n_estimators=1000,
                               max_features=128,
                               compute_importances=True,
-                              random_state=0)
+                              random_state=0, n_jobs=n_jobs)
 
 forest.fit(X, y)
+print "done in %0.3fs" % (time() - t0)
 importances = forest.feature_importances_
 importances = importances.reshape(data.images[0].shape)
 
