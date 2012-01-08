2f64f0d53a2f30cc21495b8b12b8b8edaead93c1
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2012-01-08 15:53:30 +0100

    ENH : reformatting hmm_stock_analysis.py examples

--- examples/hmm_stock_analysis.py
+++ examples/hmm_stock_analysis.py
@@ -1,103 +1,83 @@
-# -*- coding: utf-8 -*-
 """
-Created on Sat Jan  7 15:47:17 2012
+==========================
+Gaussian HMM of stock data
+==========================
 
-@author: du
-"""
-
-"""
 This script shows how to use Gaussian HMM.
 It uses stock price data, which can be obtained from yahoo finance.
-For more information on how to get stock price by matplitlib, please refere
-to date_demo1.py of matplotlib
+For more information on how to get stock price by matplitlib, please refer
+to date_demo1.py of matplotlib.
 """
+print __doc__
 
-import sys
+import datetime
 import numpy as np
-from pylab import figure, show
+import pylab as pl
 from matplotlib.finance import quotes_historical_yahoo
 from matplotlib.dates import YearLocator, MonthLocator, DateFormatter
-import datetime
-import sklearn.hmm
+from sklearn.hmm import GaussianHMM
 
-def getQuotes(tick):
-    
-    # start date
-    date1 = datetime.date( 1995, 1, 1 )
-    # end date    
-    date2 = datetime.date( 2012, 1, 6 )
+###############################################################################
+# Downloading the data
+date1 = datetime.date(1995, 1, 1)  # start date
+date2 = datetime.date(2012, 1, 6)  # end date
+# get quotes from yahoo finance
+quotes = quotes_historical_yahoo("INTC", date1, date2)
+if len(quotes) == 0:
+    raise SystemExit
 
-    # get quotes from yahoo finance    
-    quotes = quotes_historical_yahoo(tick, date1, date2)
-    if len(quotes) == 0:
-        raise SystemExit
-    
-    # quotes[t] = (date, open_v, close_v, highest_v, lowest_v, volume)
-    return quotes
-    
-def testHMM(n_components, quotes):
-    # unpack quotes 
-    dates = np.array([q[0] for q in quotes], dtype=int)
-    close_v = np.array([q[2] for q in quotes])
-    volume = np.array([q[2] for q in quotes])[1:] 
+# unpack quotes
+dates = np.array([q[0] for q in quotes], dtype=int)
+close_v = np.array([q[2] for q in quotes])
+volume = np.array([q[2] for q in quotes])[1:]
 
-    # take diff of close value
-    # this makes len(diff) = len(close_t) - 1
-    # therefore, others quontity also need to be shifted
-    diff = close_v[1:] - close_v[:-1]
-    dates = dates[1:]
-    close_v = close_v[1:]
-    
-    # pack diff and volume for training
-    X = np.column_stack([diff, volume])
-    
-    # make an HMM instance and execute fit
-    model = sklearn.hmm.GaussianHMM(n_components, "diag")
-    model.fit([X], n_iter=1000)
+# take diff of close value
+# this makes len(diff) = len(close_t) - 1
+# therefore, others quontity also need to be shifted
+diff = close_v[1:] - close_v[:-1]
+dates = dates[1:]
+close_v = close_v[1:]
 
-    # predict the optimal sequence of internal hidden state
-    hidden_states = model.decode(X)[1]
-    
-    return dates, close_v, volume, hidden_states, model
+# pack diff and volume for training
+X = np.column_stack([diff, volume])
 
-
-tick = "INTC"
+###############################################################################
+# Run Gaussian HMM
+print "fittiing to HMM and decoding ...",
 n_components = 5
 
-print "downloading quotes data"
-sys.stdout.flush()
-quotes = getQuotes(tick)
+# make an HMM instance and execute fit
+model = GaussianHMM(n_components, "diag")
+model.fit([X], n_iter=1000)
 
-print "fittiing to HMM and decoding ...",
-sys.stdout.flush()
-dates, close_v, volume, hidden_states, model = testHMM(n_components, quotes)
+# predict the optimal sequence of internal hidden state
+_, hidden_states = model.decode(X)
 
 print "done\n"
-sys.stdout.flush()
 
-# print trained parameters
-
-print "transition matrix"
+###############################################################################
+# print trained parameters and plot
+print "Transition matrix"
 print model.transmat
 print ""
 
 print "means and vars of each hidden state"
 for i in xrange(n_components):
-    print "%dth hidden state"%i
+    print "%dth hidden state" % i
     print "mean = ", model.means[i]
     print "var = ", np.diag(model.covars[i])
     print ""
 
-years    = YearLocator()   # every year
-months   = MonthLocator()  # every month
+years = YearLocator()   # every year
+months = MonthLocator()  # every month
 yearsFmt = DateFormatter('%Y')
-fig = figure()
+fig = pl.figure()
 ax = fig.add_subplot(111)
 
 for i in xrange(n_components):
     # use fancy indexing to plot data in each state
     idx = (hidden_states == i)
-    ax.plot_date(dates[idx], close_v[idx], 'o', label="%dth hidden state"%i)
+    ax.plot_date(dates[idx], close_v[idx], 'o', label="%dth hidden state" % i)
 ax.legend()
 
 # format the ticks
@@ -106,11 +86,10 @@ ax.xaxis.set_major_formatter(yearsFmt)
 ax.xaxis.set_minor_locator(months)
 ax.autoscale_view()
 
-#format the coords message box
-def price(x): return '$%1.2f'%x
+# format the coords message box
 ax.fmt_xdata = DateFormatter('%Y-%m-%d')
-ax.fmt_ydata = price
+ax.fmt_ydata = lambda x: '$%1.2f' % x
 ax.grid(True)
 
 fig.autofmt_xdate()
-show()
+pl.show()
