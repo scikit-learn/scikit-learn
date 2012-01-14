f43236bdbec68aaabf17572325fa679f54422c9e
emmanuelle <emmanuelle.gouillart@nsup.org>
2012-01-14 22:52:33 +0100

    Tomography example: PEP08, typos...

--- examples/applications/plot_tomography_l1_reconstruction.py
+++ examples/applications/plot_tomography_l1_reconstruction.py
@@ -3,12 +3,12 @@
 Tomography reconstruction with L1 prior (Lasso)
 =====================================================
 
-This example shows the reconstruction of an image from a set of parallel 
-projections, acquired along different angles. Such a dataset is acquired in 
-**computed tomography** (CT), where the image to reconstruct from the 
+This example shows the reconstruction of an image from a set of parallel
+projections, acquired along different angles. Such a dataset is acquired in
+**computed tomography** (CT), where the image to reconstruct from the
 projections is the absorption of the sample.
 
-WIthout any prior information on the sample, the number of projections required
+Without any prior information on the sample, the number of projections required
 to reconstruct the image is of the order of the linear size ``l`` of the image
 (in pixels). We consider here a sparse image, where only pixels on the boundary
 of objects have a non-zero value. Such synthetic data could correspond for
@@ -20,7 +20,7 @@ projections are acquired, therefore it is necessary to use prior
 information available on the sample (its sparsity): this is an example of
 **compressive sensing**.
 
-The tomography projecton operation is a linear transformation. In addition to
+The tomography projection operation is a linear transformation. In addition to
 the data-fidelity term corresponding to a linear regression, we penalize the L1
 norm of the image to account for its sparsity. The resulting optimization
 problem is called the :ref:`lasso`. We use the class :class:`Lasso` in
@@ -30,12 +30,17 @@ matrix is used to store the projection operator.
 
 The reconstruction with L1 penalization gives a result with zero error (all
 pixels are successfully labeled with 0 or 1), even if noise was added to the
-projections. In comparison, we have shown the result of an L2 penalization 
-(:class:`Ridge` class), that produces a large number of labeling errors for 
-the pixels. Important artifacts are observed on the reconstructed image, 
-contrary to the L1 penalization. 
+projections. In comparison, we have shown the result of an L2 penalization
+(:class:`Ridge` class), that produces a large number of labeling errors for
+the pixels. Important artifacts are observed on the reconstructed image,
+contrary to the L1 penalization. Note in particular the circular artifact
+separating the pixels in the corners, that have contributed to fewer
+projections than the central disk.
 """
+print __doc__
 
+# Author: Emmanuelle Gouillart <emmanuelle.gouillart@nsup.org>
+# License: Simplified BSD
 
 import numpy as np
 from scipy import sparse
@@ -51,6 +56,7 @@ def _weights(x, dx=1, orig=0):
     alpha = (x - orig - floor_x * dx) / dx
     return np.hstack((floor_x, floor_x + 1)), np.hstack((1 - alpha, alpha))
 
+
 def _generate_center_coordinates(l_x):
     l_x = float(l_x)
     X, Y = np.mgrid[:l_x, :l_x]
@@ -59,9 +65,10 @@ def _generate_center_coordinates(l_x):
     Y += 0.5 - center
     return X, Y
 
+
 def build_projection_operator(l_x, n_dir):
     """
-    Compute the tomography design matrix. 
+    Compute the tomography design matrix.
 
     Parameters
     ----------
@@ -78,41 +85,42 @@ def build_projection_operator(l_x, n_dir):
     """
     X, Y = _generate_center_coordinates(l_x)
     angles = np.linspace(0, np.pi, n_dir, endpoint=False)
-    data_inds, weights, camera_inds = [], [], [] 
-    data_unravel_indices = np.arange(l_x**2)
-    data_unravel_indices = np.hstack((data_unravel_indices, 
+    data_inds, weights, camera_inds = [], [], []
+    data_unravel_indices = np.arange(l_x ** 2)
+    data_unravel_indices = np.hstack((data_unravel_indices,
                                       data_unravel_indices))
     for i, angle in enumerate(angles):
         Xrot = np.cos(angle) * X - np.sin(angle) * Y
-        Yrot = np.sin(angle) * X + np.cos(angle) * Y
         inds, w = _weights(Xrot, dx=1, orig=X.min())
-        mask = np.logical_and(inds >= 0, inds < l_x) 
+        mask = np.logical_and(inds >= 0, inds < l_x)
         weights += list(w[mask])
         camera_inds += list(inds[mask] + i * l_x)
         data_inds += list(data_unravel_indices[mask])
     proj_operator = sparse.coo_matrix((weights, (camera_inds, data_inds)))
     return proj_operator
 
+
 def generate_synthetic_data():
     """
     Synthetic binary data
-    """
+     """
     rs = np.random.RandomState(0)
-    n_pts = 36 
+    n_pts = 36.
     x, y = np.ogrid[0:l, 0:l]
-    mask_outer = (x - l/2)**2 + (y - l/2)**2 < (l/2)**2
+    mask_outer = (x - l / 2) ** 2 + (y - l / 2) ** 2 < (l / 2) ** 2
     mask = np.zeros((l, l))
     points = l * rs.rand(2, n_pts)
     mask[(points[0]).astype(np.int), (points[1]).astype(np.int)] = 1
-    mask = ndimage.gaussian_filter(mask, sigma=l/(n_pts))
-    res = np.logical_and(mask>mask.mean(), mask_outer)
+    mask = ndimage.gaussian_filter(mask, sigma=l / n_pts)
+    res = np.logical_and(mask > mask.mean(), mask_outer)
     return res - ndimage.binary_erosion(res)
+
 
 # Generate synthetic images, and projections
 l = 128
-proj_operator = build_projection_operator(l, l/7.)
+proj_operator = build_projection_operator(l, l / 7.)
 data = generate_synthetic_data()
-proj = (proj_operator * data.ravel()[:, np.newaxis])
+proj = proj_operator * data.ravel()[:, np.newaxis]
 proj += 0.15 * np.random.randn(*proj.shape)
 
 # Reconstruction with L2 (Ridge) penalization
@@ -123,7 +131,7 @@ rec_l2.shape = (l, l)
 binary_rec_l2 = rec_l2 > 0.5
 
 # Reconstruction with L1 (Lasso) penalization
-# the value of alpha was determined using cross validation
+# the best value of alpha was determined using cross validation
 # with LassoCV
 rgr_lasso = Lasso(alpha=0.001)
 rgr_lasso.fit(proj_operator, proj.ravel())
