152d225789aba09bfbc386ca9b30313e3540d6d8
Olivier Grisel <olivier.grisel@ensta.org>
2012-01-17 02:30:59 +0100

    implementation & test for the murmurhash wrapper module

--- sklearn/utils/murmurhash.pyx
+++ sklearn/utils/murmurhash.pyx
@@ -1,26 +1,88 @@
+"""Cython wrapper for MurmurHash3 non-cryptographic hash function
+
+MurmurHash is an extensively tested and very fast hash function that has
+good distribution properties suitable for machine learning use cases
+such as feature hashing and random projections.
+
+The original C++ code by Austin Appleby is released the public domain
+and can be found here:
+
+  https://code.google.com/p/smhasher/
+
+"""
+# Author: Olivier Grisel <olivier.grisel@ensta.org>
+#
+# License: BSD Style.
+
+
 cdef extern from "MurmurHash3.h":
     void MurmurHash3_x86_32(void* key, int len, unsigned int seed,
                             void* out)
 
 
-cpdef unsigned int murmurhash3_int(int key, unsigned int seed):
+cpdef unsigned int murmurhash3_32_int_uint(int key, unsigned int seed):
+    cdef unsigned int out
+    MurmurHash3_x86_32(&key, sizeof(int), seed, &out)
+    return out
+
+
+cpdef int murmurhash3_32_int_int(int key, unsigned int seed):
     cdef int out
-    MurmurHash3_x86_32(&key, 1, seed, &out)
-    return 0
+    MurmurHash3_x86_32(&key, sizeof(int), seed, &out)
+    return out
 
 
-cpdef unsigned int murmurhash3_bytes(bytes key, unsigned int seed):
-    # TODO
-    return 0
+cpdef unsigned int murmurhash3_32_bytes_uint(bytes key, unsigned int seed):
+    cdef unsigned int out
+    cdef char* key_c = key
+    MurmurHash3_x86_32(key_c, len(key), seed, &out)
+    return out
 
 
-def murmurhash3(key, seed):
+cpdef int murmurhash3_32_bytes_int(bytes key, unsigned int seed):
+    cdef int out
+    cdef char* key_c = key
+    MurmurHash3_x86_32(key_c, len(key), seed, &out)
+    return out
+
+
+def murmurhash3_32(key, seed=0, positive=False):
+    """Compute the 32bit murmurhash3_32 of key at seed.
+
+    The underlying implementation is MurmurHash3_x86_32 generating low
+    latency 32bits hash suitable for implementing lookup tables, Bloom
+    filters, count min sketch or feature hashing.
+
+    Parameters
+    ----------
+    key: int, bytes or unicode
+        the physical object to hash
+
+    seed: int, optional default is 0
+        integer seed for the hashing algorithm.
+
+    positive: boolean, optional default is False
+        True: the results is casted to an unsigned int
+          from 0 to 2 ** 32 - 1
+        False: the results is casted to a signed int
+          from -(2 ** 31) to 2 ** 31 - 1
+
+    """
     if isinstance(key, bytes):
-        return murmurhash3_bytes(key, seed)
+        if positive:
+            return murmurhash3_32_bytes_uint(key, seed)
+        else:
+            return murmurhash3_32_bytes_int(key, seed)
     elif isinstance(key, unicode):
-        return murmurhash3_bytes(key.encode('utf-8'), seed)
+        if positive:
+            return murmurhash3_32_bytes_uint(key.encode('utf-8'), seed)
+        else:
+            return murmurhash3_32_bytes_int(key.encode('utf-8'), seed)
     elif isinstance(key, int):
-        return murmurhash3_int(key, seed)
+        if positive:
+            return murmurhash3_32_int_uint(key, seed)
+        else:
+            return murmurhash3_32_int_int(key, seed)
     else:
         raise ValueError(
             "key %r with type %s is not supported. "
--- sklearn/utils/tests/test_murmurhash.py
+++ sklearn/utils/tests/test_murmurhash.py
@@ -1,10 +1,59 @@
-from sklearn.utils.murmurhash import murmurhash3
+# Author: Olivier Grisel <olivier.grisel@ensta.org>
+#
+# License: BSD Style.
+
+import numpy as np
+from sklearn.utils.murmurhash import murmurhash3_32
+from numpy.testing import assert_array_almost_equal
 from nose.tools import assert_equal
 
 
-def test_mmhash3():
-    assert_equal(murmurhash3('foo', 0), 3)
-    assert_equal(murmurhash3('foo', 42), 3)
-    assert_equal(murmurhash3(u'foo', 0), 3)
-    assert_equal(murmurhash3(u'foo', 42), 3)
-    assert_equal(murmurhash3(3, 0), 3)
+def test_mmhash3_int():
+    assert_equal(murmurhash3_32(3), 847579505)
+    assert_equal(murmurhash3_32(3, seed=0), 847579505)
+    assert_equal(murmurhash3_32(3, seed=42), -1823081949)
+
+    assert_equal(murmurhash3_32(3, positive=False), 847579505)
+    assert_equal(murmurhash3_32(3, seed=0, positive=False), 847579505)
+    assert_equal(murmurhash3_32(3, seed=42, positive=False), -1823081949)
+
+    assert_equal(murmurhash3_32(3, positive=True), 847579505L)
+    assert_equal(murmurhash3_32(3, seed=0, positive=True), 847579505L)
+    assert_equal(murmurhash3_32(3, seed=42, positive=True), 2471885347L)
+
+
+def test_mmhash3_bytes():
+    assert_equal(murmurhash3_32('foo', 0), -156908512)
+    assert_equal(murmurhash3_32('foo', 42), -1322301282)
+
+    assert_equal(murmurhash3_32('foo', 0, positive=True), 4138058784L)
+    assert_equal(murmurhash3_32('foo', 42, positive=True), 2972666014L)
+
+
+def test_mmhash3_unicode():
+    assert_equal(murmurhash3_32(u'foo', 0), -156908512)
+    assert_equal(murmurhash3_32(u'foo', 42), -1322301282)
+
+    assert_equal(murmurhash3_32(u'foo', 0, positive=True), 4138058784L)
+    assert_equal(murmurhash3_32(u'foo', 42, positive=True), 2972666014L)
+
+
+def test_no_collision_on_byte_range():
+    previous_hashes = set()
+    for i in range(100):
+        h = murmurhash3_32(' ' * i, 0)
+        assert (h not in previous_hashes,
+                "Found collision on growing empty string")
+
+
+def test_uniform_distribution():
+    n_bins, n_samples = 10, 100000
+    bins = np.zeros(n_bins, dtype=np.float)
+
+    for i in range(n_samples):
+        bins[murmurhash3_32(i, positive=True) % n_bins] += 1
+
+    means = bins / n_samples
+    expected = np.ones(n_bins) / n_bins
+
+    assert_array_almost_equal(means / expected, np.ones(n_bins), 2)
