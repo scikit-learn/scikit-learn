c9d27f3dc03592508c16aa0a6408e71e1c6670f8
Mathieu Blondel <mathieu@mblondel.org>
2012-01-21 05:39:34 +0900

    Move preprocessing.py to sklearn/.

--- sklearn/preprocessing/__init__.py
+++
@@ -1,5 +0,0 @@
-from .preprocessing import scale, Scaler
-from .preprocessing import normalize, Normalizer
-from .preprocessing import binarize, Binarizer
-from .preprocessing import LabelBinarizer
-from .preprocessing import KernelCenterer
