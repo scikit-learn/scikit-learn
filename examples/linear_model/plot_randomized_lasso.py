6273024fa7893af4472858dca5f400cc1ba5cf6e
GaelVaroquaux <gael.varoquaux@normalesup.org>
2012-01-24 01:10:22 +0100

    ENH: grid in terms of alpha/alpha_max
    And rework example

--- examples/linear_model/plot_randomized_lasso.py
+++ examples/linear_model/plot_randomized_lasso.py
@@ -15,45 +15,46 @@ import numpy as np
 from sklearn.linear_model import RandomizedLasso, lasso_stability_path, \
                                  LassoLarsCV
 from sklearn.cross_validation import ShuffleSplit
+
 from sklearn import datasets
 from sklearn.feature_selection import f_regression
 from sklearn.preprocessing import Scaler
 
 ###############################################################################
 # Load diabetes data and add noisy features
-n_samples = 30
+n_samples = 40
 n_features = 100
-n_good_features = 10
+n_good_features = 5
 X, y, coef = datasets.make_regression(n_samples=n_samples,
                     n_features=n_features, n_informative=n_good_features,
-                    coef=True)
+                    coef=True, random_state=42)
 X = Scaler().fit_transform(X)
 
 ###############################################################################
 # Plot stability selection path
 scaling = .3
-coef_grid, scores_path = lasso_stability_path(X, y, scaling=scaling,
+alpha_grid, scores_path = lasso_stability_path(X, y, scaling=scaling,
                                       random_state=42)
 
 pl.figure()
-hg = pl.plot(coef_grid[:-1], scores_path[coef != 0].T[:-1], 'r')
-hb = pl.plot(coef_grid[:-1], scores_path[coef == 0].T[:-1], 'k')
+hg = pl.plot(-np.log10(alpha_grid[1:]), scores_path[coef != 0].T[1:], 'r')
+hb = pl.plot(-np.log10(alpha_grid[1:]), scores_path[coef == 0].T[1:], 'k')
 ymin, ymax = pl.ylim()
-pl.xlabel('|coef| / max|coef|')
+pl.xlabel(r'$-log_{10}(\alpha / \alpha_{max})$')
 pl.ylabel('Proportion of times selected')
 pl.title('Stability Scores Path')
 pl.axis('tight')
 pl.legend((hg[0], hb[0]), ('good features', 'bad features'),
           loc='lower right')
 pl.ylim([0, 1.05])
+#pl.xlim(0, 5)
 
 ###############################################################################
 # Plot the estimated stability scores for best cross-validated alpha
 
 # First find the best alpha:
-cv = ShuffleSplit(n_samples, n_iterations=50, test_fraction=0.2)
-alpha_cv = LassoLarsCV(cv=cv).fit(X, y).alpha
-pl.axvline(alpha_cv, color='.3')
+cv = ShuffleSplit(n_samples, n_iterations=10, test_fraction=0.25)
+alpha_cv = LassoLarsCV().fit(X, y).alpha
 
 # Then run the RandomizedLasso
 clf = RandomizedLasso(verbose=False, alpha=alpha_cv, random_state=42,
@@ -69,7 +70,7 @@ f_plot = pl.plot(F / np.max(F))
 ground_truth = pl.plot(np.where(coef != 0), .5, 'ro')
 pl.xlabel("Features")
 pl.ylabel("Score")
-pl.legend((score_plot, f_plot, ground_truth[0]), 
-            ("Stability-selection score", "(scaled) F-score", 
+pl.legend((score_plot, f_plot, ground_truth[0]),
+            ("Stability-selection score", "(scaled) F-score",
              "Ground truth"))
 pl.show()
