2b08a0ddaed6695ad4bf1736aeaade9cfbf92c98
GaelVaroquaux <gael.varoquaux@normalesup.org>
2012-01-29 00:06:51 +0100

    DOC: enrich example docstring.

--- examples/linear_model/plot_randomized_lasso.py
+++ examples/linear_model/plot_randomized_lasso.py
@@ -3,9 +3,25 @@
 Randomized Lasso: feature selection with Lasso
 ==============================================
 
-Performs feature scoring and selection using a randomized linear
-model (Lasso)
+Performs feature scoring and selection using a :ref:`randomized sparse
+linear model <randomized_l1>` (Lasso).
 
+The problem here is, given a small number of observations, to recover
+which features of X are relevant to explain y. For this we use the
+randomized lasso, which can outperform standard statistical tests, if the
+true model is sparse, i.e. if a small fraction of the features are
+relevant. The output of this procedure is a stability score for each
+feature: the higher, the more likely the feature is to be relevant.
+
+The use of the randomized lasso requires the choice of an alpha
+parameter. In the first figure, we vary this parameter and look at the
+stability score as a function of it. While this analysis, knowing the
+ground truth shows an optimal regime in which relevant features stand
+out from the irrelevant ones. However, without knowing the ground truth,
+we cannot select this value of alpha. We therefore set it by
+cross-validation. This leads to a suboptimal choice, but the stability
+score of the randomized lasso still outperforms standard statistics, as
+shown on the second figure.
 """
 print __doc__
 
@@ -24,9 +40,9 @@ from sklearn.preprocessing import Scaler
 # Load diabetes data and add noisy features
 n_samples = 40
 n_features = 100
-n_good_features = 5
+n_relevant_features = 5
 X, y, coef = datasets.make_regression(n_samples=n_samples,
-                    n_features=n_features, n_informative=n_good_features,
+                    n_features=n_features, n_informative=n_relevant_features,
                     coef=True, random_state=42)
 X = Scaler().fit_transform(X)
 
@@ -44,10 +60,9 @@ pl.xlabel(r'$(\alpha / \alpha_{max})^{1/3}$')
 pl.ylabel('Proportion of times selected')
 pl.title('Stability Scores Path')
 pl.axis('tight')
-pl.legend((hg[0], hb[0]), ('good features', 'bad features'),
+pl.legend((hg[0], hb[0]), ('relevant features', 'irrelevant features'),
           loc='lower right')
 pl.ylim([0, 1.05])
-#pl.xlim(0, 5)
 
 ###############################################################################
 # Plot the estimated stability scores for best cross-validated alpha
@@ -67,7 +82,7 @@ F, _ = f_regression(X, y)  # compare with F-score
 pl.figure()
 pl.plot(clf.scores_ / np.max(clf.scores_), label="Stability-selection score")
 pl.plot(F / np.max(F), label="(scaled) F-score")
-pl.plot(np.where(coef != 0)[0], [.5] * n_good_features, 'ro',
+pl.plot(np.where(coef != 0)[0], [.5] * n_relevant_features, 'ro',
         label="Ground truth")
 pl.xlabel("Features")
 pl.ylabel("Score")
