c5aed1d4d15ca8191c5526df500d052553f62dfa
Clay Woolam <clay@woolam.org>
2012-01-28 12:07:48 -0800

    added __init__.py file to semi_supervised folder

---
+++ sklearn/semi_supervised/__init__.py
@@ -0,0 +1,8 @@
+"""
+The :mod:`sklearn.semi_supervised` module implements semi-supervised learning
+algorithms. These algorithms utilized small amounts of labeled data and large
+amounts of unlabeled data for classification tasks. This module includes Label
+Propagation.
+"""
+
+from .label_propagation import LabelPropagation, LabelSpreading
