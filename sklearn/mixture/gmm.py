ea5f0f3177eb6e9cf0a36b4738c09e70e71e21e2
Andreas Mueller <amueller@ais.uni-bonn.de>
2012-02-06 22:40:44 +0100

    COSMIT pep8

--- sklearn/mixture/gmm.py
+++ sklearn/mixture/gmm.py
@@ -178,9 +178,9 @@ class GMM(BaseEstimator):
     >>> # and 10 to use for training.
     >>> obs = np.concatenate((np.random.randn(100, 1),
     ...                       10 + np.random.randn(300, 1)))
-    >>> g.fit(obs)
-    GMM(covariance_type=None, min_covar=0.001, n_components=2, random_state=None,
-      thresh=0.01)
+    >>> g.fit(obs) # doctest: +NORMALIZE_WHITESPACE
+    GMM(covariance_type=None, min_covar=0.001, n_components=2,
+            random_state=None, thresh=0.01)
     >>> np.round(g.weights_, 2)
     array([ 0.75,  0.25])
     >>> np.round(g.means_, 2)
@@ -195,9 +195,9 @@ class GMM(BaseEstimator):
     array([-2.19, -4.58, -1.75, -1.21])
     >>> # Refit the model on new data (initial parameters remain the
     >>> # same), this time with an even split between the two modes.
-    >>> g.fit(20 * [[0]] +  20 * [[10]])
-    GMM(covariance_type=None, min_covar=0.001, n_components=2, random_state=None,
-      thresh=0.01)
+    >>> g.fit(20 * [[0]] +  20 * [[10]]) # doctest: +NORMALIZE_WHITESPACE
+    GMM(covariance_type=None, min_covar=0.001, n_components=2,
+            random_state=None, thresh=0.01)
     >>> np.round(g.weights_, 2)
     array([ 0.5,  0.5])
 
