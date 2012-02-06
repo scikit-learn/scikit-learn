48021c61bd46c920c95397764d23366900bcf6b4
GaelVaroquaux <gael.varoquaux@normalesup.org>
2012-02-06 11:55:08 +0100

    MISC: assert -> raise ValueError
    as per @ogrisel's comment

--- sklearn/cluster/hierarchical.py
+++ sklearn/cluster/hierarchical.py
@@ -199,9 +199,11 @@ def _hc_cut(n_clusters, children, n_leaves):
         cluster labels for each point
 
     """
-    assert n_clusters <= n_leaves, \
-                'Cannot extract more clusters than samples'
-    # In this function, we stores nodes as a heap to avoid recomputing
+    if not n_clusters > n_leaves:
+        raise ValueError('Cannot extract more clusters than samples: '
+            '%s clusters where given for a tree with %s leaves.'
+            % (n_clusters, n_leaves))
+    # In this function, we store nodes as a heap to avoid recomputing
     # the max of the nodes: the first element is always the smallest
     # We use negated indices as heaps work on smallest elements, and we
     # are interested in largest elements
