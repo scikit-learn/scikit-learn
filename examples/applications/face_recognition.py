70fa9d5557c4886514237329791b0c64247d1ae2
Olivier Grisel <olivier.grisel@ensta.org>
2012-02-28 23:29:00 +0100

    manually rescale C in face recognition example

--- examples/applications/face_recognition.py
+++ examples/applications/face_recognition.py
@@ -106,7 +106,7 @@ print "done in %0.3fs" % (time() - t0)
 print "Fitting the classifier to the training set"
 t0 = time()
 param_grid = {
- 'C': [1, 5, 10, 50, 100],
+ 'C': [1e3, 5e3, 1e4, 5e4, 1e5],
  'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1],
 }
 clf = GridSearchCV(SVC(kernel='rbf', class_weight='auto'), param_grid)
