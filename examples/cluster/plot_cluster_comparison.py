9bcd00af23ef28c19be13a333eb7e04960467a54
Andreas Mueller <amueller@ais.uni-bonn.de>
2012-03-04 17:46:38 +0100

    MISC use Gaels pretty plot, add dbscan, normalize data...

--- examples/cluster/plot_cluster_comparison.py
+++ examples/cluster/plot_cluster_comparison.py
@@ -19,51 +19,56 @@ from sklearn.cluster import MeanShift, estimate_bandwidth
 from sklearn.cluster import KMeans
 from sklearn.cluster import Ward
 from sklearn.cluster import SpectralClustering
+from sklearn.cluster import DBSCAN
 from sklearn.neighbors import kneighbors_graph
 from sklearn.datasets import make_circles, make_moons, make_blobs
+from sklearn.preprocessing import Scaler
 
 # Generate datasets
 n_samples = 300
-circles = make_circles(n_samples=n_samples, factor=.5)
 noisy_circles = make_circles(n_samples=n_samples, factor=.5, noise=.05)
-moons = make_moons(n_samples=n_samples)
 noisy_moons = make_moons(n_samples=n_samples, noise=.05)
 blobs = make_blobs(n_samples=n_samples)
 
-i = 1
 colors = np.array([x for x in 'bgrcmykbgrcmykbgrcmykbgrcmyk'])
 colors = np.hstack([colors] * 5)
 
-for dataset in [circles, noisy_circles, moons, noisy_moons, blobs]:
+pl.figure(figsize=(16,10))
+pl.subplots_adjust(left=.001, right=.999, bottom=.01, top=.95, wspace=.05, hspace=.01)
+plot_num = 1
+for i_dataset, dataset in enumerate([noisy_circles,noisy_moons, blobs]):
     X, y = dataset
+    # normalize dataset for easier parameter selection
+    X = Scaler().fit_transform(X)
+
     # estimate bandwidth for mean shift
-    bandwidth = estimate_bandwidth(X, quantile=0.2, n_samples=100)
+    bandwidth = estimate_bandwidth(X, quantile=0.2, n_samples=n_samples)
 
-    connectivity = kneighbors_graph(X, n_neighbors=10)
+    # connectivity matrix for structured Ward
+    connectivity = kneighbors_graph(X, n_neighbors=20)
+    connectivity = 0.5 * (connectivity + connectivity.T) # make connectivity symmetric
 
-    connectivity = 0.5 * (connectivity + connectivity.T) # make affinity symmetric
-    dists = euclidean_distances(X)
     # create clustering estimators
     ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
     two_means = KMeans(k=2)
-    ten_means = KMeans(k=10)
     ward_five = Ward(n_clusters=2, connectivity=connectivity)
     spectral = SpectralClustering(k=2, mode='arpack')
+    dbscan = DBSCAN(eps=.3)
 
-    for algorithm in [two_means, ten_means, spectral, ms, ward_five]:
+    for algorithm in [two_means, dbscan, spectral, ms, ward_five]:
         # predict cluster memberships
         if algorithm == spectral:
             algorithm.fit(connectivity)
         else:
             algorithm.fit(X)
-        y_pred = algorithm.labels_
-        pl.subplot(5, 5, i)
-        pl.title(str(algorithm).split('(')[0])
-        pl.scatter(X[:, 0], X[:, 1], color=colors[y_pred])
+        y_pred = algorithm.labels_.astype(np.int)
+        pl.subplot(3, 5, plot_num)
+        if i_dataset == 0:
+            pl.title(str(algorithm).split('(')[0])
+        pl.scatter(X[:, 0], X[:, 1], color=colors[y_pred].tolist())
         pl.xticks(())
         pl.yticks(())
-
-        i += 1
+        plot_num += 1
 
 pl.show()
 
