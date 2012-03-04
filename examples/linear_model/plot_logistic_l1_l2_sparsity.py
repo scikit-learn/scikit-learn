13ea995372ce8760753a89476d117b3926d6fc59
Andreas Mueller <amueller@ais.uni-bonn.de>
2012-03-04 22:57:26 +0100

    ENH 2d plot for l1l2 digits example

--- examples/linear_model/plot_logistic_l1_l2_sparsity.py
+++ examples/linear_model/plot_logistic_l1_l2_sparsity.py
@@ -34,8 +34,9 @@ X = Scaler().fit_transform(X)
 # classify small against large digits
 y = (y > 4).astype(np.int)
 
+
 # Set regularization parameter
-for C in 10. ** np.arange(1, 4):
+for i, C in enumerate(10. ** np.arange(1, 4)):
     # turn down tolerance for short training time
     clf_l1_LR = LogisticRegression(C=C, penalty='l1', tol=0.01)
     clf_l2_LR = LogisticRegression(C=C, penalty='l2', tol=0.01)
@@ -57,9 +58,21 @@ for C in 10. ** np.arange(1, 4):
     print "Sparsity with L2 penalty: %f" % sparsity_l2_LR
     print "score with L2 penalty: %f" % clf_l2_LR.score(X, y)
 
-    pl.plot(coef_l1_LR, label='L1 C=%d' % C)
-    pl.plot(coef_l2_LR, '--', label='L2 C=%d' % C)
+    l1_plot = pl.subplot(3, 2, 2 * i + 1)
+    l2_plot = pl.subplot(3, 2, 2 * (i + 1))
+    if i == 0:
+        l1_plot.set_title("L1 penalty")
+        l2_plot.set_title("L2 penalty")
 
-pl.xlim(0, 63)
-pl.legend(loc='best')
+    l1_plot.imshow(coef_l1_LR.reshape(8, 8), interpolation='nearest',
+            cmap='gray', vmax=1, vmin=-1)
+    l2_plot.imshow(coef_l2_LR.reshape(8, 8), interpolation='nearest',
+            cmap='gray', vmax=1, vmin=-1)
+    pl.text(-8, 3,"C = %d" % C)
+
+    l1_plot.set_xticks(())
+    l1_plot.set_yticks(())
+    l2_plot.set_xticks(())
+    l2_plot.set_yticks(())
+
 pl.show()
