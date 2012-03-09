43f18191711dc39e57ae4b2a6ae747878af5d136
Lars Buitinck <L.J.Buitinck@uva.nl>
2012-03-09 18:41:43 +0100

    ENH add test for DictVectorizer.restrict

--- sklearn/feature_extraction/tests/test_dictvectorizer.py
+++ sklearn/feature_extraction/tests/test_dictvectorizer.py
@@ -8,6 +8,7 @@ from nose.tools import assert_equal
 from numpy.testing import assert_array_equal
 
 from sklearn.feature_extraction import DictVectorizer
+from sklearn.feature_selection import SelectKBest, chi2
 
 
 def test_dictvectorizer():
@@ -24,6 +25,23 @@ def test_dictvectorizer():
             assert_equal(X.shape, (3, 5))
             assert_equal(X.sum(), 14)
             assert_equal(v.inverse_transform(X), D)
+
+
+def test_feature_selection():
+    # make two feature dicts with two useful features and a bunch of useless
+    # ones, in terms of chi2
+    d1 = dict([("useless%d" % i, 10) for i in xrange(20)],
+              useful1=1, useful2=20)
+    d2 = dict([("useless%d" % i, 10) for i in xrange(20)],
+              useful1=20, useful2=1)
+
+    for indices in (True, False):
+        v = DictVectorizer().fit([d1, d2])
+        X = v.transform([d1, d2])
+        sel = SelectKBest(chi2, k=2).fit(X, [0, 1])
+
+        v.restrict(sel.get_support(indices=indices), indices=indices)
+        assert_equal(v.get_feature_names(), ["useful1", "useful2"])
 
 
 def test_unseen_features():
