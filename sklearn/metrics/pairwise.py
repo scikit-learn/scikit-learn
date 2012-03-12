7def8df0fb2f0d4ec307c47c1ccdf00a1f9121ee
Subhodeep Moitra <subhodeep.moitra@gmail.com>
2012-03-12 16:45:41 -0700

    P3K: Fixed dtype doctests for Python3

--- sklearn/metrics/pairwise.py
+++ sklearn/metrics/pairwise.py
@@ -219,21 +219,22 @@ def manhattan_distances(X, Y=None, sum_over_features=True):
     Examples
     --------
     >>> from sklearn.metrics.pairwise import manhattan_distances
-    >>> manhattan_distances(3, 3)
-    array([[0]])
-    >>> manhattan_distances(3, 2)
-    array([[1]])
-    >>> manhattan_distances(2, 3)
-    array([[1]])
-    >>> manhattan_distances([[1, 2], [3, 4]], [[1, 2], [0, 3]])
+    >>> manhattan_distances(3, 3)#doctest:+ELLIPSIS
+    array([[0]]...)
+    >>> manhattan_distances(3, 2)#doctest:+ELLIPSIS
+    array([[1]]...)
+    >>> manhattan_distances(2, 3)#doctest:+ELLIPSIS
+    array([[1]]...)
+    >>> manhattan_distances([[1, 2], [3, 4]],\
+		 [[1, 2], [0, 3]])#doctest:+ELLIPSIS
     array([[0, 2],
-           [4, 4]])
+           [4, 4]]...)
     >>> import numpy as np
     >>> X = np.ones((1, 2))
     >>> y = 2 * np.ones((2, 2))
-    >>> manhattan_distances(X, y, sum_over_features=False)
+    >>> manhattan_distances(X, y, sum_over_features=False)#doctest:+ELLIPSIS
     array([[ 1.,  1.],
-           [ 1.,  1.]])
+           [ 1.,  1.]]...)
     """
     X, Y = check_pairwise_arrays(X, Y)
     n_samples_X, n_features_X = X.shape
