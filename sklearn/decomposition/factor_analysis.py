ac96f90d8df54119134cc3d71b8ae1b602d8d77c
Andreas Mueller <amueller@ais.uni-bonn.de>
2012-03-31 18:14:27 +0200

    ENH real stopping criterion

--- sklearn/decomposition/factor_analysis.py
+++ sklearn/decomposition/factor_analysis.py
@@ -1,5 +1,5 @@
 """Factor Analysis.
-A latent variabel model that assumes diagonal, but in contrast to PCA not
+A latent variable model that assumes diagonal, but in contrast to PCA not
 isotropic, Gaussian latent variables. 
 
 This implementation is based on Bishop's book chapter 12.2.4.
@@ -13,13 +13,14 @@ import numpy as np
 from scipy import linalg
 
 from ..base import BaseEstimator, TransformerMixin
-from ..utils import array2d, as_float_array, check_random_state
+from ..utils import as_float_array, check_random_state
+from ..utils.extmath import fast_logdet
 
 
 class FactorAnalysis(BaseEstimator, TransformerMixin):
     """Factor Analysis (FA)
 
-    Linear dimensionality reduction using a latent variabel model. The
+    Linear dimensionality reduction using a latent variable model. The
     Observations are assumed to be caused by a linear transformation of some
     lower dimensional `latent` factors.
     Factor analysis assumes these factors have a diagonal Gaussian
@@ -64,12 +65,13 @@ class FactorAnalysis(BaseEstimator, TransformerMixin):
     ICA: Independent component analysis, a latent variable model with
         non-Gaussian latent variables.
     """
-    def __init__(self, n_components, tol=.001, copy=True, random_state=None, max_iter=1000):
+    def __init__(self, n_components, tol=.01, copy=True, random_state=None, max_iter=1000, verbose=0):
         self.n_components = n_components
         self.copy = copy
         self.tol = tol
         self.random_state = random_state
         self.max_iter = max_iter
+        self.verbose = verbose
 
     def fit(self, X, y=None):
         """Fit the FA model to X using EM.
@@ -85,9 +87,8 @@ class FactorAnalysis(BaseEstimator, TransformerMixin):
         """
         self.random_state = check_random_state(self.random_state)
 
-        X = array2d(X)
+        np.atleast_2d(as_float_array(X, copy=self.copy))
         n_samples, n_features = X.shape
-        X = as_float_array(X, copy=self.copy)
         self.mean_ = np.mean(X, axis=0)
         X -= self.mean_
         self.cov_ = np.cov(X.T)
@@ -101,6 +102,10 @@ class FactorAnalysis(BaseEstimator, TransformerMixin):
         uniqueness = 1. / np.diag(self.cov_)
         latent = np.zeros((n_samples, self.n_components))
 
+        # constant factor of likelihood
+        loglikelihood_constant = n_features / 2. * np.log(2. * np.pi)
+        p = []
+        loglik_old = -np.inf
         for i in xrange(self.max_iter):
             # expectation step, find latent representation
             inv = linalg.inv(np.eye(self.n_components) + np.dot(loadings.T /
@@ -113,13 +118,22 @@ class FactorAnalysis(BaseEstimator, TransformerMixin):
             loadings = linalg.solve(latent_cov.T, latent_times_X, sym_pos=True).T
             uniqueness = np.diag(self.cov_
                    - np.dot(loadings, latent_times_X / n_samples))
-            #print(linalg.norm(latent_old - latent))
-            #if linalg.norm(latent_old - latent) < self.tol:
-                #break
+            precision = linalg.inv(np.dot(loadings, loadings.T) + np.diag(uniqueness))
+            loglikelihood = -(loglikelihood_constant + fast_logdet(precision) + (np.dot(X, precision) * X).sum()) / 2
+            p.append(loglikelihood)
+            if self.verbose > 0:
+                print(loglikelihood)
+            if np.abs(loglikelihood - loglik_old) < self.tol:
+                break
+            loglik_old = loglikelihood
 
+        import matplotlib.pyplot as plt
+        plt.plot(p)
+        plt.show()
         self.loadings_ = loadings
         self.uniqueness_ = np.diag(uniqueness)
         self.components_ = np.dot(inv, loadings.T) / uniqueness
+        self.precision_ = precision
         return self
 
     def transform(self, X):
@@ -138,19 +152,3 @@ class FactorAnalysis(BaseEstimator, TransformerMixin):
         X_transformed = X - self.mean_
         X_transformed = np.dot(X_transformed, self.components_.T)
         return X_transformed
-
-    def fit_transform(self, X, y=None):
-        """Fit the FA model to X and then apply dimensionality reduction to X
-        using the learned model.
-
-        Parameters
-        ----------
-        X : array-like, shape (n_samples, n_features)
-            Training data.
-
-        Returns
-        -------
-        X_new : array-like, shape (n_samples, n_components)
-            The latent factors of X.
-        """
-        return self.fit(X).transform(X)
