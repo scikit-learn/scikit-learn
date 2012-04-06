b9ec6ca31b2807e3d3e2c7929bc49ce20c8152a9
Jaques Grobler <jaquesgrobler@gmail.com>
2012-04-06 17:06:39 +0200

    updated documentation for gmm,dpgmm and vbgmm

--- sklearn/mixture/dpgmm.py
+++ sklearn/mixture/dpgmm.py
@@ -148,6 +148,16 @@ class DPGMM(GMM):
 
     thresh : float, optional
         Convergence threshold.
+    n_iter : int, optional
+        Maximum number of iterations to perform before convergence.
+    params : string, optional
+        Controls which parameters are updated in the training
+        process.  Can contain any combination of 'w' for weights,
+        'm' for means, and 'c' for covars.  Defaults to 'wmc'.
+    init_params : string, optional
+        Controls which parameters are updated in the initialization
+        process.  Can contain any combination of 'w' for weights,
+        'm' for means, and 'c' for covars.  Defaults to 'wmc'.
 
     Attributes
     ----------
@@ -469,25 +479,15 @@ class DPGMM(GMM):
 
         A initialization step is performed before entering the em
         algorithm. If you want to avoid this step, set the keyword
-        argument init_params to the empty string ''. Likewise, if you
-        would like just to do an initialization, call this method with
-        n_iter=0.
+        argument init_params to the empty string '' when when creating
+        the object. Likewise, if you would like just to do an 
+        initialization, set n_iter=0.
 
         Parameters
         ----------
         X : array_like, shape (n, n_features)
             List of n_features-dimensional data points.  Each row
             corresponds to a single data point.
-        n_iter : int, optional
-             Maximum number of iterations to perform before convergence.
-        params : string, optional
-            Controls which parameters are updated in the training
-            process.  Can contain any combination of 'w' for weights,
-            'm' for means, and 'c' for covars.  Defaults to 'wmc'.
-        init_params : string, optional
-            Controls which parameters are updated in the initialization
-            process.  Can contain any combination of 'w' for weights,
-            'm' for means, and 'c' for covars.  Defaults to 'wmc'.
         """
         self.random_state = check_random_state(self.random_state)
         if kwargs:
