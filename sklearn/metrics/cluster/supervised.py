189425211690c59f0b10959525146123495e9eba
Andreas Mueller <amueller@ais.uni-bonn.de>
2012-04-16 18:10:50 +0200

    ENH normalized_mutual_information

--- sklearn/metrics/cluster/supervised.py
+++ sklearn/metrics/cluster/supervised.py
@@ -609,6 +609,16 @@ def adjusted_mutual_info_score(labels_true, labels_pred):
       >>> adjusted_mutual_info_score([0, 0, 0, 0], [0, 1, 2, 3])
       0.0
 
+    References
+    ----------
+    .. [1] Vinh, Epps, and Bailey, (2010). Information Theoretic Measures for
+       Clusterings Comparison: Variants, Properties, Normalization and
+       Correction for Chance}, JMLR
+       http://jmlr.csail.mit.edu/papers/volume11/vinh10a/vinh10a.pdf
+
+    .. [3] `Wikipedia entry for the Adjusted Mutual Information
+       <http://en.wikipedia.org/wiki/Adjusted_Mutual_Information>`_
+
     """
     labels_true, labels_pred = check_clusterings(labels_true, labels_pred)
     n_samples = labels_true.shape[0]
@@ -630,6 +640,79 @@ def adjusted_mutual_info_score(labels_true, labels_pred):
     h_true, h_pred = entropy(labels_true), entropy(labels_pred)
     ami = (mi - emi) / (max(h_true, h_pred) - emi)
     return ami
+
+
+def normalized_mutual_info_score(labels_true, labels_pred):
+    """Normalied Mutual Information between two clusterings
+
+    Normalized Mutual Information (AMI) is an adjustement of the Mutual
+    Information (MI) score to account for chance.
+
+    This metric is independent of the absolute values of the labels:
+    a permutation of the class or cluster label values won't change the
+    score value in any way.
+
+    This metric is furthermore symmetric: switching `label_true` with
+    `label_pred` will return the same score value. This can be useful to
+    measure the agreement of two independent label assignments strategies
+    on the same dataset when the real ground truth is not known.
+
+    Parameters
+    ----------
+    labels_true : int array, shape = [n_samples]
+        A clustering of the data into disjoint subsets.
+
+    labels_pred : array, shape = [n_samples]
+        A clustering of the data into disjoint subsets.
+
+    Returns
+    -------
+    nmi: float
+       score between 0.0 and 1.0. 1.0 stands for perfectly complete labeling
+
+    See also
+    --------
+    adjusted_rand_score: Adjusted Rand Index
+    adjusted_mutual_information_score: Adjusted Mutual Information (recent
+       modification of NMI)
+
+    Examples
+    --------
+
+    Perfect labelings are both homogeneous and complete, hence have
+    score 1.0::
+
+      >>> from sklearn.metrics.cluster import normalized_mutual_info_score
+      >>> normalized_mutual_info_score([0, 0, 1, 1], [0, 0, 1, 1])
+      1.0
+      >>> normalized_mutual_info_score([0, 0, 1, 1], [1, 1, 0, 0])
+      1.0
+
+    If classes members are completly splitted across different clusters,
+    the assignment is totally in-complete, hence the NMI is null::
+
+      >>> normalized_mutual_info_score([0, 0, 0, 0], [0, 1, 2, 3])
+      0.0
+
+    """
+    labels_true, labels_pred = check_clusterings(labels_true, labels_pred)
+    classes = np.unique(labels_true)
+    clusters = np.unique(labels_pred)
+    # Special limit cases: no clustering since the data is not split.
+    # This is a perfect match hence return 1.0.
+    if (classes.shape[0] == clusters.shape[0] == 1
+        or classes.shape[0] == clusters.shape[0] == 0):
+        return 1.0
+    contingency = contingency_matrix(labels_true, labels_pred)
+    contingency = np.array(contingency, dtype='float')
+    # Calculate the MI for the two clusterings
+    mi = mutual_info_score(labels_true, labels_pred,
+                           contingency=contingency)
+    # Calculate the expected value for the mutual information
+    # Calculate entropy for each labeling
+    h_true, h_pred = entropy(labels_true), entropy(labels_pred)
+    nmi = mi / np.sqrt(h_true * h_pred)
+    return nmi
 
 
 def expected_mutual_information(contingency, n_samples):
