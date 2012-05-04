786d3eb26252fa9ac9f9f4bd1d092c82f5ba68a4
Alexis Mignon <alexis.mignon@gmail.com>
2012-05-04 13:38:39 +0200

    Corrected missing import of csc_mean_variance_axis0

--- sklearn/utils/tests/test_sparsefuncs.py
+++ sklearn/utils/tests/test_sparsefuncs.py
@@ -3,7 +3,7 @@ import scipy.sparse as sp
 from numpy.testing import assert_array_almost_equal
 
 from sklearn.datasets import make_classification
-from sklearn.utils.sparsefuncs import mean_variance_axis0
+from sklearn.utils.sparsefuncs import mean_variance_axis0, csc_mean_variance_axis0
 
 
 def test_mean_variance_axis0():
@@ -16,6 +16,21 @@ def test_mean_variance_axis0():
     X_csr[1, 0] = 0
     X[1, 0] = 0
     X_means, X_vars = mean_variance_axis0(X_csr)
+
+    assert_array_almost_equal(X_means, np.mean(X, axis=0))
+    assert_array_almost_equal(X_vars, np.var(X, axis=0))
+
+
+def test_csc_mean_variance_axis0():
+    X, _ = make_classification(5, 4, random_state=0)
+    # Sparsify the array a little bit
+    X[0, 0] = 0
+    X[2, 1] = 0
+    X[4, 3] = 0
+    X_csc = sp.csc_matrix(X)
+    X_csc[1, 0] = 0
+    X[1, 0] = 0
+    X_means, X_vars = csc_mean_variance_axis0(X_csc)
 
     assert_array_almost_equal(X_means, np.mean(X, axis=0))
     assert_array_almost_equal(X_vars, np.var(X, axis=0))
