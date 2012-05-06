ed580831b709f361f8de2c3c8e5f704dd17eb63f
Gael Varoquaux <gael.varoquaux@normalesup.org>
2012-05-07 00:16:30 +0200

    ENH: prevent multiprocessing in tests under Windows

---
+++ sklearn/test_setup.py
@@ -0,0 +1,11 @@
+"""
+Fixtures for the tests.
+
+This module gets loaded by test discovery scanners (such as nose) in
+their collection scan.
+"""
+
+import os
+if os.name.startswith('win'):
+    # During the tests, under Windows, we don't want any multiprocessing
+    os.environ['JOBLIB_MULTIPROCESSING'] = 0
