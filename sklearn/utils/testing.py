1cf2faae9e2a39487726efdaab3d63f015274b6c
Gael Varoquaux <gael.varoquaux@normalesup.org>
2012-05-06 17:10:55 +0200

    BUG: fix assert_greater
    And add a test: when doing a backport, it is always wise to check ones
    implementation against the original

--- sklearn/utils/testing.py
+++ sklearn/utils/testing.py
@@ -22,23 +22,28 @@ except ImportError:
         assert_false(x in container, msg="%r in %r" % (x, container))
 
 
+def _assert_less(a, b, msg=None):
+    message = "%r is not lower than %r" % (a, b)
+    if msg is not None:
+        message += ": " + msg
+    assert a < b, message
+
+def _assert_greater(a, b, msg=None):
+    message = "%r is not lower than %r" % (a, b)
+    if msg is not None:
+        message += ": " + msg
+    assert a > b, message
+
+
 try:
     from nose.tools import assert_less
 except ImportError:
-    def assert_less(a, b, msg=None):
-        message = "%r is not lower than %r" % (a, b)
-        if msg is not None:
-            message += ": " + msg
-        assert a < b, message
+    assert_less = _assert_less
 
 try:
     from nose.tools import assert_greater
 except ImportError:
-    def assert_greater(a, b, msg=None):
-        message = "%r is not lower than %r" % (a, b)
-        if msg is not None:
-            message += ": " + msg
-        assert a < b, message
+    assert_greater = _assert_greater
 
 
 def fake_mldata_cache(columns_dict, dataname, matfile, ordering=None):
