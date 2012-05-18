1e0e330d997956e73260fae653fded14e2f2567f
Lars Buitinck <L.J.Buitinck@uva.nl>
2012-05-18 09:57:06 +0200

    BUG remove debugging statements from multiclass

--- sklearn/multiclass.py
+++ sklearn/multiclass.py
@@ -37,8 +37,6 @@ def _fit_binary(estimator, X, y):
 def _predict_binary(estimator, X):
     """Make predictions using a single binary estimator."""
     if hasattr(estimator, "decision_function"):
-        print X.shape
-        print estimator.decision_function(X).shape
         return np.ravel(estimator.decision_function(X))
     else:
         # probabilities of the positive class
