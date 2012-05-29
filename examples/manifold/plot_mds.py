b402868b9542b0b11cbd361e52daecf0d817266c
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2012-05-29 15:51:10 +0200

    a += a.T is different from a = a + a.T

--- examples/manifold/plot_mds.py
+++ examples/manifold/plot_mds.py
@@ -28,7 +28,7 @@ similarities = euclidean_distances(X_true)
 
 # Add noise to the similarities
 noise = np.random.rand(n_samples, n_samples)
-noise += noise.T
+noise = noise + noise.T
 noise[np.arange(noise.shape[0]), np.arange(noise.shape[0])] = 0
 similarities += noise
 
