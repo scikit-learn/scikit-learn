7a585e46ea96624b633fc8613eff996352e83b1f
Andreas Mueller <amueller@ais.uni-bonn.de>
2012-06-07 22:14:45 +0200

    ENH more tests

--- sklearn/tests/test_common.py
+++ sklearn/tests/test_common.py
@@ -2,6 +2,9 @@
 General tests for all estimators in sklearn.
 """
 import warnings
+import numpy as np
+from nose.tools import assert_raises
+from numpy.testing import assert_array_equal
 
 from sklearn.utils.testing import all_estimators
 from sklearn.utils.testing import assert_greater
@@ -28,8 +31,8 @@ def test_all_estimators():
     estimators = all_estimators()
     clf = LDA()
 
-    # some can just not be sensibly default constructed
     for name, E in estimators:
+        # some can just not be sensibly default constructed
         if E in dont_test:
             continue
         # test default-constructibility
@@ -52,16 +55,32 @@ def test_classifiers():
         ClassifierMixin)]
     iris = load_iris()
     X, y = iris.data, iris.target
-    print(classifiers)
+    #X = Scaler().fit_transform(X)
     for name, Clf in classifiers:
         if Clf in dont_test or Clf in meta_estimators:
             continue
         clf = Clf()
-        try:
-            clf.fit(X, y)
-            y_pred = clf.predict(X)
-            assert_greater(zero_one_score(y, y_pred), 0.9)
-        except Exception as ex:
-            print("++++++++++++++++++++++++")
-            print(clf)
-            print(ex)
+        # fit
+        clf.fit(X, y)
+        y_pred = clf.predict(X)
+        # training set performance
+        assert_greater(zero_one_score(y, y_pred), 0.9)
+        # raises error on malformed input for predict
+        assert_raises(ValueError, clf.predict, X.T)
+        if hasattr(clf, "decision_function"):
+            try:
+                # raises error on malformed input for decision_function
+                #assert_raises(ValueError, clf.decision_function, X.T)
+                # decision_function agrees with predict:
+                decision = clf.decision_function(X)
+                assert_array_equal(np.argmax(decision, axis=1), y_pred)
+            except NotImplementedError:
+                pass
+        if hasattr(clf, "predict_proba"):
+            try:
+                #assert_raises(ValueError, clf.predict_proba, X.T)
+                # decision_function agrees with predict:
+                y_prob = clf.predict_proba(X)
+                assert_array_equal(np.argmax(y_prob, axis=1), y_pred)
+            except NotImplementedError:
+                pass
