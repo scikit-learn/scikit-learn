636e9d3983f414f6960243ca8c097eba2d3bf3b4
Vlad Niculae <vlad@vene.ro>
2012-06-26 12:23:08 +0200

    ENH: multidimensional y in ElasticNet (WIP)

--- sklearn/linear_model/tests/test_coordinate_descent.py
+++ sklearn/linear_model/tests/test_coordinate_descent.py
@@ -268,6 +268,26 @@ def test_enet_positive_constraint():
     enet.fit(X, y)
     assert_true(min(enet.coef_) >= 0)
 
+
+def test_enet_multitarget():
+    rng = np.random.RandomState(0)
+    X, y = rng.randn(10, 8), rng.randn(10, 3)
+    n_targets = y.shape[1]
+
+    estimator = ElasticNet(alpha=0.01, fit_intercept=True, precompute=None)
+    # XXX: There is a bug when precompute is not None!
+    estimator.fit(X, y)
+    coef, intercept, dual_gap, eps = (estimator.coef_, estimator.intercept_,
+                                      estimator.dual_gap_, estimator.eps_)
+
+    for k in xrange(n_targets):
+        estimator.fit(X, y[:, k])
+        assert_array_almost_equal(coef[k, :], estimator.coef_)
+        assert_array_almost_equal(intercept[k], estimator.intercept_)
+        assert_array_almost_equal(dual_gap[k], estimator.dual_gap_)
+        assert_array_almost_equal(eps[k], estimator.eps_)
+
+
 if __name__ == '__main__':
     import nose
     nose.runmodule()
