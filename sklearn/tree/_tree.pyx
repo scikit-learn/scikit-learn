923e471d34d6e3a52f16943ea42d5cfb883d7ebf
Gilles Louppe <g.louppe@gmail.com>
2012-07-16 14:41:24 +0200

    FIX: PyArray_ZEROS -> np.zeros?

--- sklearn/tree/_tree.pyx
+++ sklearn/tree/_tree.pyx
@@ -466,8 +466,10 @@ cdef class Tree:
 
             # Split
             X_ptr = X_ptr + feature * X_stride
-            sample_mask_left = np.PyArray_ZEROS(1, &n_total_samples, np.NPY_BOOL, np.NPY_DEFAULT)
-            sample_mask_right = np.PyArray_ZEROS(1, &n_total_samples, np.NPY_BOOL, np.NPY_DEFAULT)
+            sample_mask_left = np.zeros((n_total_samples, ), dtype=np.bool)
+            sample_mask_right = np.zeros((n_total_samples, ), dtype=np.bool)
+            #sample_mask_left = np.PyArray_ZEROS(1, &n_total_samples, np.NPY_BOOL, 0)
+            #sample_mask_right = np.PyArray_ZEROS(1, &n_total_samples, np.NPY_BOOL, 0)
             n_node_samples_left = 0
             n_node_samples_right = 0
 
