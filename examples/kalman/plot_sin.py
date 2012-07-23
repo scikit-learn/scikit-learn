909842063d1f93fcdd4ebd74d575ce743a90af18
Gael Varoquaux <gael.varoquaux@normalesup.org>
2012-07-23 22:27:52 +0200

    Cosmit: simplify example

--- examples/kalman/plot_sin.py
+++ examples/kalman/plot_sin.py
@@ -18,7 +18,6 @@ import pylab as pl
 
 from sklearn.kalman import KalmanFilter
 
-
 rnd = np.random.RandomState(0)
 
 # generate a noisy sine wave to act as our fake measurements
@@ -29,12 +28,8 @@ observations = 20 * (np.sin(x) + 0.5 * rnd.randn(n_timesteps))
 # create a Kalman Filter by hinting at the size of the state and observation
 # space.  If you already have good guesses for the initial parameters, put them
 # in here.  The Kalman Filter will try to learn the values of all variables.
-kf = KalmanFilter(
-    transition_matrices=np.array([[1, 1],
-                                  [0, 1]]),
-    transition_covariance=np.eye(2) * 0.01,
-    observation_covariance=10.0,
-)
+kf = KalmanFilter(transition_matrices=np.array([[1, 1], [0, 1]]),
+                  transition_covariance=0.01 * np.eye(2))
 
 # You can use the Kalman Filter immediately without fitting, but its estimates
 # may not be as good as if you fit first.
@@ -44,18 +39,15 @@ print 'fitted model: %s' % (kf,)
 # Plot lines for the measurements without noise, the estimated position of the
 # target before fitting, and the estimated position after fitting.
 pl.figure(figsize=(16, 6))
-obs_scatter = pl.scatter(x, observations, marker='x', color='b')
-position_line = pl.plot(
-    x, states_pred[:, 0], linestyle='-', marker='o', color='r'
-)
-velocity_line = pl.plot(
-    x, states_pred[:, 1], linestyle='-', marker='o', color='g'
-)
-pl.legend(
-    (obs_scatter, position_line[0], velocity_line[0]),
-    ('measurements', 'position est.', 'velocity est.'),
-    loc='lower right'
-)
+obs_scatter = pl.scatter(x, observations, marker='x', color='b',
+                         label='measurements')
+position_line = pl.plot(x, states_pred[:, 0],
+                        linestyle='-', marker='o', color='r',
+                        label='position est.')
+velocity_line = pl.plot(x, states_pred[:, 1],
+                        linestyle='-', marker='o', color='g',
+                        label='velocity est.')
+pl.legend(loc='lower right')
 pl.xlim(xmin=0, xmax=x.max())
 pl.xlabel('time')
 pl.show()
