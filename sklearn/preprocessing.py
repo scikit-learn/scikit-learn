8d08721b2a5affe83d3df1671c44b38dc10b8364
Andreas Mueller <amueller@ais.uni-bonn.de>
2012-07-25 08:32:50 +0100

    FIX doctests for changed dtype

--- sklearn/preprocessing.py
+++ sklearn/preprocessing.py
@@ -660,12 +660,12 @@ class LabelBinarizer(BaseEstimator, TransformerMixin):
     >>> lb.classes_
     array([1, 2, 4, 6])
     >>> lb.transform([1, 6])
-    array([[ 1.,  0.,  0.,  0.],
-           [ 0.,  0.,  0.,  1.]])
+    array([[1, 0, 0, 0],
+           [0, 0, 0, 1]])
 
     >>> lb.fit_transform([(1, 2), (3,)])
-    array([[ 1.,  1.,  0.],
-           [ 0.,  0.,  1.]])
+    array([[1, 1, 0],
+           [0, 0, 1]])
     >>> lb.classes_
     array([1, 2, 3])
     """
