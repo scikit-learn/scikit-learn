c5e23a75da741cb866150fb8e43b337a6498da12
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2012-07-26 12:09:58 +0200

    ENH: Isotonic regression

---
+++ sklearn/linear_model/isotonic_regression_.py
@@ -0,0 +1,120 @@
+# Authors: Fabian Pedregosa <fabian@fseoane.net>
+#          Alexandre Gramfort <alexandre.gramfort@inria.fr>
+#          Nelle Varoquaux <nelle.varoquaux@gmail.com>
+# License: BSD Style.
+
+import numpy as np
+from ..base import BaseEstimator
+
+
+def isotonic_regression(y, w=None, x_min=None, x_max=None):
+    """
+    Solve the isotonic regression model:
+
+        min Sum w_i (y_i - x_i) ** 2
+
+        subject to x_min = x_1 <= x_2 ... <= x_n = x_max
+
+    where each w_i is strictly positive and each y_i is an arbitrary
+    real number.
+
+    Parameters
+    ----------
+    y: iterable of floating-point values
+
+    w: iterable of floating-point values
+        If None w is set to 1 (no weights)
+
+    x_min: optional, default: None
+        if not None, set the lowest value of the fit to x_min
+
+    x_max: optional, default: None
+        if not None, set the highest value of the fit to x_max
+
+    Returns
+    -------
+    x: list of floating-point values
+    """
+    if w is None:
+        w = np.ones(len(y), dtype=y.dtype)
+
+    if x_min is not None or x_max is not None:
+        y = np.copy(y)
+        w = np.copy(w)
+        if x_min is not None:
+            y[0] = x_min
+            w[0] = 1e32
+        if x_max is not None:
+            y[-1] = x_max
+            w[-1] = 1e32
+
+    J = [[i, ] for i in range(len(y))]
+    cur = 0
+
+    while cur < len(J) - 1:
+        av0, av1, av2 = 0, 0, np.inf
+        while av0 <= av1 and cur < len(J) - 1:
+            idx0 = J[cur]
+            idx1 = J[cur + 1]
+            av0 = np.dot(w[idx0], y[idx0]) / np.sum(w[idx0])
+            av1 = np.dot(w[idx1], y[idx1]) / np.sum(w[idx1])
+            cur += 1 if av0 <= av1 else 0
+
+        if cur == len(J) - 1:
+            break
+
+        a = J.pop(cur)
+        b = J.pop(cur)
+        J.insert(cur, a + b)
+        while av2 > av0 and cur > 0:
+            idx0 = J[cur]
+            idx2 = J[cur - 1]
+            av0 = np.dot(w[idx0], y[idx0]) / np.sum(w[idx0])
+            av2 = np.dot(w[idx2], y[idx2]) / np.sum(w[idx2])
+            if av2 >= av0:
+                a = J.pop(cur - 1)
+                b = J.pop(cur - 1)
+                J.insert(cur - 1, a + b)
+                cur -= 1
+
+    sol = []
+    for idx in J:
+        sol += [np.dot(w[idx], y[idx]) / np.sum(w[idx])] * len(idx)
+    return np.asarray(sol)
+
+
+class IsotonicRegression(BaseEstimator):
+    """
+    Solve the isotonic regression model:
+        min Sum w_i (y_i - x_i) ** 2
+
+        subject to x_min = x_1 <= x_2 ... <= x_n = x_max
+
+    where each w_i is strictly positive and each y_i is an arbitrary
+    real number.
+
+    Attributes
+    ----------
+    `X_fit`: array
+        Estimated fit
+
+    Notes
+    -----
+    Isotonic Median Regression: A Linear Programming Approach
+    Nilotpal Chakravarti
+    Mathematics of Operations Research
+    Vol. 14, No. 2 (May, 1989), pp. 303-308
+    """
+
+    def _check_fit_data(self, X, w=None):
+        if w is not None:
+            if len(X) != len(w):
+                raise ValueError("Shapes of X and w do not match")
+
+    def fit(self, X, w=None, x_min=None, x_max=None):
+        """
+        """
+        # FIXME X should be a 1D array
+        self._check_fit_data(X, w)
+        self.X_fit = isotonic_regression(X, w, x_min, x_max)
+        return self
