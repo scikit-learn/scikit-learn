480f46283831644a2515233d277f78812a291a48
Vlad Niculae <vlad@vene.ro>
2012-08-11 21:58:47 +0200

    Add dense sparse support and precomputation

--- sklearn/metrics/euclidean_fast.pyx
+++ sklearn/metrics/euclidean_fast.pyx
@@ -62,28 +62,44 @@ cdef inline sparse_sum_sq(int X_rows, np.ndarray[DTYPE_t, ndim=1] X_data,
 def dense_euclidean_distances(int X_rows, int Y_rows, int X_cols,
                               np.ndarray[DTYPE_t, ndim=2, mode="c"] X,
                               np.ndarray[DTYPE_t, ndim=2, mode="c"] Y,
+                              np.ndarray[DTYPE_t, ndim=1] XX,
+                              np.ndarray[DTYPE_t, ndim=1] YY,
+                              bint precomputed_XX, bint precomputed_YY,
                               np.ndarray[DTYPE_t, ndim=2, mode="c"] out,
                               bint squared=False):
-    cdef np.ndarray[DTYPE_t, ndim=1] XX = np.empty(X_rows, dtype=np.float64)
-    cdef np.ndarray[DTYPE_t, ndim=1] YY = np.empty(Y_rows, dtype=np.float64)
+    if XX is None:
+        XX = np.empty(X_rows, dtype=np.float64)
+    if YY is None:
+        YY = np.empty(Y_rows, dtype=np.float64)
+
     gemm(101, 111, 112, X_rows, Y_rows, X_cols, 1.0,
          <DTYPE_t *>X.data, X.strides[0] / sizeof(DTYPE_t),
          <DTYPE_t *>Y.data, Y.strides[0] / sizeof(DTYPE_t), 0.0,
          <DTYPE_t *>out.data, out.strides[0] / sizeof(DTYPE_t))
-    dense_sum_sq(X_rows, X_cols, X, XX)
-    dense_sum_sq(Y_rows, X_cols, Y, YY)
+
+    if not precomputed_XX:
+        dense_sum_sq(X_rows, X_cols, X, XX)
+    if not precomputed_YY:
+        dense_sum_sq(Y_rows, X_cols, Y, YY)
     _euclidean_distances(X_rows, Y_rows, XX, YY, out, squared)
 
 
 def dense_euclidean_distances_sym(int X_rows, int X_cols,
                                   np.ndarray[DTYPE_t, ndim=2, mode="c"] X,
+                                  np.ndarray[DTYPE_t, ndim=1] XX,
+                                  bint precomputed_XX,
                                   np.ndarray[DTYPE_t, ndim=2, mode="c"] out,
                                   bint squared=False):
-    cdef np.ndarray[DTYPE_t, ndim=1] XX = np.empty(X_rows, dtype=np.float64)
+    if XX is None:
+        XX = np.empty(X_rows, dtype=np.float64)
+
     syrk(101, 122, 111, X_rows, X_cols, 1.0, <DTYPE_t *>X.data,
          X.strides[0] / sizeof(DTYPE_t), 0.0, <DTYPE_t *>out.data,
          out.strides[0] / sizeof(DTYPE_t))
-    dense_sum_sq(X_rows, X_cols, X, XX)
+
+    if not precomputed_XX:
+        dense_sum_sq(X_rows, X_cols, X, XX)
+
     _euclidean_distances(X_rows, X_rows, XX, XX, out, squared, True)
 
 
@@ -94,12 +110,21 @@ def sparse_euclidean_distances(int X_rows, int Y_rows,
                                np.ndarray[DTYPE_t, ndim=1] Y_data,
                                np.ndarray[int, ndim=1] Y_indices,
                                np.ndarray[int, ndim=1] Y_indptr,
+                               np.ndarray[DTYPE_t, ndim=1] XX,
+                               np.ndarray[DTYPE_t, ndim=1] YY,
+                               bint precomputed_XX, bint precomputed_YY,
                                np.ndarray[DTYPE_t, ndim=2] XYt,
                                bint squared=False):
-    cdef np.ndarray[DTYPE_t, ndim=1] XX = np.empty(X_rows, dtype=np.float64)
-    cdef np.ndarray[DTYPE_t, ndim=1] YY = np.empty(Y_rows, dtype=np.float64)
-    sparse_sum_sq(X_rows, X_data, X_indices, X_indptr, XX)
-    sparse_sum_sq(Y_rows, Y_data, Y_indices, Y_indptr, YY)
+    if XX is None:
+        XX = np.empty(X_rows, dtype=np.float64)
+    if YY is None:
+        YY = np.empty(Y_rows, dtype=np.float64)
+
+    if not precomputed_XX:
+        sparse_sum_sq(X_rows, X_data, X_indices, X_indptr, XX)
+    if not precomputed_YY:
+        sparse_sum_sq(Y_rows, Y_data, Y_indices, Y_indptr, YY)
+
     _euclidean_distances(X_rows, Y_rows, XX, YY, XYt, squared)
 
 
@@ -107,8 +132,37 @@ def sparse_euclidean_distances_sym(int X_rows,
                                    np.ndarray[DTYPE_t, ndim=1] X_data,
                                    np.ndarray[int, ndim=1] X_indices,
                                    np.ndarray[int, ndim=1] X_indptr,
+                                   np.ndarray[DTYPE_t, ndim=1] XX,
+                                   bint precomputed_XX,
                                    np.ndarray[DTYPE_t, ndim=2] XXt,
                                    bint squared=False):
-    cdef np.ndarray[DTYPE_t, ndim=1] XX = np.empty(X_rows, dtype=np.float64)
-    sparse_sum_sq(X_rows, X_data, X_indices, X_indptr, XX)
+    if XX is None:
+        XX = np.empty(X_rows, dtype=np.float64)
+
+    if not precomputed_XX:
+        sparse_sum_sq(X_rows, X_data, X_indices, X_indptr, XX)
+
     _euclidean_distances(X_rows, X_rows, XX, XX, XXt, squared, True)
+
+
+def sparse_dense_euclidean_distances(int X_rows, int X_cols, int Y_rows,
+                                     np.ndarray[DTYPE_t, ndim=1] X_data,
+                                     np.ndarray[int, ndim=1] X_indices,
+                                     np.ndarray[int, ndim=1] X_indptr,
+                                     np.ndarray[DTYPE_t, ndim=2, mode="c"] Y,
+                                     np.ndarray[DTYPE_t, ndim=1] XX,
+                                     np.ndarray[DTYPE_t, ndim=1] YY,
+                                     bint precomputed_XX, bint precomputed_YY,
+                                     np.ndarray[DTYPE_t, ndim=2] XYt,
+                                     bint squared=False):
+    if XX is None:
+        XX = np.empty(X_rows, dtype=np.float64)
+    if YY is None:
+        YY = np.empty(Y_rows, dtype=np.float64)
+
+    if not precomputed_XX:
+        sparse_sum_sq(X_rows, X_data, X_indices, X_indptr, XX)
+    if not precomputed_YY:
+        dense_sum_sq(Y_rows, X_cols, Y, YY)
+
+    _euclidean_distances(X_rows, Y_rows, XX, YY, XYt, squared)
