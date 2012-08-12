22531a3ea70ac0d4b22fc6457fb6b683d4318833
Vlad Niculae <vlad@vene.ro>
2012-08-12 20:03:58 +0200

    More lapack inverting

--- sklearn/covariance/empirical_covariance_.py
+++ sklearn/covariance/empirical_covariance_.py
@@ -13,11 +13,16 @@ Maximum likelihood covariance estimator.
 from __future__ import division
 import warnings
 import numpy as np
+from scipy.linalg.lapack import get_lapack_funcs
 from scipy import linalg
 
 from ..base import BaseEstimator
 from ..utils import array2d
 from ..utils.extmath import fast_logdet
+
+getri, getrf = get_lapack_funcs(('getri', 'getrf'),
+                                (np.empty((), dtype=np.float64),
+                                 np.empty((), dtype=np.float64)))
 
 
 def log_likelihood(emp_cov, precision):
@@ -113,7 +118,9 @@ class EmpiricalCovariance(BaseEstimator):
         self.covariance_ = covariance
         # set precision
         if self.store_precision:
-            self.precision_ = linalg.pinv(covariance)
+            lu, piv, _ = getrf(np.dot(covariance.T, covariance), True)
+            self.precision_, _ = getri(lu, piv, overwrite_lu=True)
+            self.precision_ = np.dot(covariance, self.precision_)
         else:
             self.precision_ = None
 
@@ -129,7 +136,9 @@ class EmpiricalCovariance(BaseEstimator):
         if self.store_precision:
             precision = self.precision_
         else:
-            precision = linalg.pinv(self.covariance_)
+            lu, piv, _ = getrf(np.dot(self.covariance_.T, self.covariance_))
+            precision, _ = getri(lu, piv, overwrite_lu=True)
+            precision = np.dot(self.covariance_, precision)
         return precision
 
     def fit(self, X):
