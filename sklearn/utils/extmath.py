12f4eb9047a1ec4ceb65552f1fa704d49dbf54a5
Jake Vanderplas <vanderplas@astro.washington.edu>
2012-08-15 21:45:37 -0700

    @jakevdp's version of pinvh speed up symmetric_pinv

--- sklearn/utils/extmath.py
+++ sklearn/utils/extmath.py
@@ -299,38 +299,41 @@ def weighted_mode(a, w, axis=0):
     return mostfrequent, oldcounts
 
 
-def symmetric_pinv(a, cond=None, rcond=None):
-    """Compute the (Moore-Penrose) pseudo-inverse of a matrix.
+def pinvh(a, cond=None, rcond=None, lower=True):
+    """Compute the (Moore-Penrose) pseudo-inverse of a hermetian matrix.
 
     Calculate a generalized inverse of a symmetric matrix using its
     eigenvalue decomposition and including all 'large' eigenvalues.
 
-    Inspired by ``scipy.linalg.pinv2``, credited to Pearu Peterson and Travis
-    Oliphant.
-
     Parameters
     ----------
     a : array, shape (N, N)
-        Symmetric matrix to be pseudo-inverted
+        Real symmetric or complex hermetian matrix to be pseudo-inverted
     cond, rcond : float or None
         Cutoff for 'small' eigenvalues.
         Singular values smaller than rcond * largest_eigenvalue are considered
         zero.
 
         If None or -1, suitable machine precision is used.
+    lower : boolean
+        Whether the pertinent array data is taken from the lower or upper
+        triangle of a. (Default: lower)
 
     Returns
     -------
     B : array, shape (N, N)
 
-    Raises LinAlgError if eigenvalue does not converge
+    Raises
+    ------
+    LinAlgError
+        If eigenvalue does not converge
 
     Examples
     --------
     >>> from numpy import *
     >>> a = random.randn(9, 6)
     >>> a = np.dot(a, a.T)
-    >>> B = symmetric_pinv(a)
+    >>> B = pinvh(a)
     >>> allclose(a, dot(a, dot(B, a)))
     True
     >>> allclose(B, dot(B, dot(a, B)))
@@ -338,22 +341,18 @@ def symmetric_pinv(a, cond=None, rcond=None):
 
     """
     a = np.asarray_chkfinite(a)
-    s, u = linalg.eigh(a)
-    # eigh returns eigvals in reverse order, but this doesn't affect anything.
+    s, u = linalg.eigh(a, lower=lower)
 
-    t = u.dtype.char
     if rcond is not None:
         cond = rcond
     if cond in [None, -1]:
-        eps = np.finfo(np.float).eps
-        feps = np.finfo(np.single).eps
-        _array_precision = {'f': 0, 'd': 1, 'F': 0, 'D': 1}
-        cond = {0: feps * 1e3, 1: eps * 1e6}[_array_precision[t]]
-    n = a.shape[0]
-    cutoff = cond * np.maximum.reduce(s)
-    psigma = np.zeros(n, t)
-    above_cutoff = np.where(s > cutoff)
-    psigma[above_cutoff] = 1.0 / np.conjugate(s[above_cutoff])
-    #XXX: use lapack/blas routines for dot
-    #XXX: above comment is from scipy, but I (@vene)'ll take a look
-    return np.transpose(np.conjugate(np.dot(u * psigma, u.T.conjugate())))
+        t = u.dtype.char.lower()
+        factor = {'f': 1E3, 'd': 1E6}
+        cond = factor[t] * np.finfo(t).eps
+
+    # unlike svd case, eigh can lead to negative eigenvalues
+    above_cutoff = (abs(s) > cond * np.max(abs(s)))
+    psigma_diag = np.zeros_like(s)
+    psigma_diag[above_cutoff] = 1.0 / s[above_cutoff]
+
+    return np.dot(u * psigma_diag, np.conjugate(u).T)
