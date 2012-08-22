7fb4fa359d5168e34f33b38cf3dc53caa435fb65
Francois Savard <francois@fsavard.com>
2012-08-22 16:04:01 -0400

    Fixed docstring for C param in BaseLibLinear/SVM subclasses.
    And added deprecation warning.

--- sklearn/svm/base.py
+++ sklearn/svm/base.py
@@ -84,6 +84,12 @@ class BaseLibSVM(BaseEstimator):
             raise ValueError("impl should be one of %s, %s was given" % (
                 LIBSVM_IMPL, impl))
 
+        if C is None:
+            warnings.warn("Using 'None' for C of BaseLibSVM"
+                    "is deprecated. Setting C=1.0.",
+                    DeprecationWarning)
+            C = 1.0
+
         self.impl = impl
         self.kernel = kernel
         self.degree = degree
@@ -546,6 +552,13 @@ class BaseLibLinear(BaseEstimator):
     def __init__(self, penalty='l2', loss='l2', dual=True, tol=1e-4, C=1.0,
             multi_class='ovr', fit_intercept=True, intercept_scaling=1,
             class_weight=None, verbose=0):
+
+        if C is None:
+            warnings.warn("Using 'None' for C of BaseLibLinear"
+                    "is deprecated. Setting C=1.0.",
+                    DeprecationWarning)
+            C = 1.0
+
         self.penalty = penalty
         self.loss = loss
         self.dual = dual
