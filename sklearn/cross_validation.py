4de6d3f87162eea4fb327acaf4ba2353898a0c50
Christian Jauvin <cjauvin@gmail.com>
2012-08-31 10:28:53 -0400

    changed **fit_kwargs to explicit fit_params dict

--- sklearn/cross_validation.py
+++ sklearn/cross_validation.py
@@ -1027,18 +1027,18 @@ class StratifiedShuffleSplit(object):
 
 ##############################################################################
 
-def _cross_val_score(estimator, X, y, score_func, train, test, verbose, **fit_kwargs):
+def _cross_val_score(estimator, X, y, score_func, train, test, verbose, fit_params):
     """Inner loop for cross validation"""
     n_samples = X.shape[0] if sp.issparse(X) else len(X)
-    fit_kwargs = dict([(k, np.array(v)[train] if len(v) == n_samples else v) for k, v in fit_kwargs.items()])
+    fit_params = dict([(k, np.array(v)[train] if len(v) == n_samples else v) for k, v in fit_params.items()])
     if y is None:
-        estimator.fit(X[train], **fit_kwargs)
+        estimator.fit(X[train], **fit_params)
         if score_func is None:
             score = estimator.score(X[test])
         else:
             score = score_func(X[test])
     else:
-        estimator.fit(X[train], y[train], **fit_kwargs)
+        estimator.fit(X[train], y[train], **fit_params)
         if score_func is None:
             score = estimator.score(X[test], y[test])
         else:
@@ -1049,7 +1049,7 @@ def _cross_val_score(estimator, X, y, score_func, train, test, verbose, **fit_kw
 
 
 def cross_val_score(estimator, X, y=None, score_func=None, cv=None, n_jobs=1,
-                    verbose=0, **fit_kwargs):
+                    verbose=0, fit_params=None):
     """Evaluate a score by cross-validation
 
     Parameters
@@ -1082,8 +1082,8 @@ def cross_val_score(estimator, X, y=None, score_func=None, cv=None, n_jobs=1,
     verbose: integer, optional
         The verbosity level
 
-    fit_kwargs: the optional keyword arguments to pass to the estimator's fit
-        method
+    fit_params : dict, optional
+        parameters to pass to the fit method
     """
     X, y = check_arrays(X, y, sparse_format='csr')
     cv = check_cv(cv, X, y, classifier=is_classifier(estimator))
@@ -1095,9 +1095,10 @@ def cross_val_score(estimator, X, y=None, score_func=None, cv=None, n_jobs=1,
                 "does not." % estimator)
     # We clone the estimator to make sure that all the folds are
     # independent, and that it is pickle-able.
+    fit_params = fit_params if fit_params is not None else {}
     scores = Parallel(n_jobs=n_jobs, verbose=verbose)(
                 delayed(_cross_val_score)(clone(estimator), X, y, score_func,
-                                          train, test, verbose, **fit_kwargs)
+                                          train, test, verbose, fit_params)
                 for train, test in cv)
     return np.array(scores)
 
