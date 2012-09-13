b0bdd3f1b5ef070b11251c7939916b5e1d5657f9
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2012-09-13 15:31:27 +0200

    adding my name in IR example

--- examples/linear_model/plot_isotonic_regression.py
+++ examples/linear_model/plot_isotonic_regression.py
@@ -7,6 +7,7 @@ An illustration of the isotonic regression on generated data.
 """
 
 # Author: Nelle Varoquaux <nelle.varoquaux@gmail.com>
+#         Alexandre Gramfort <alexandre.gramfort@inria.fr>
 # Licence: BSD
 
 import numpy as np
@@ -15,10 +16,9 @@ from matplotlib.collections import LineCollection
 
 from sklearn.linear_model import IsotonicRegression
 
-a = 50.
 n = 100
 x = np.arange(n)
-y = np.random.randint(-50, 50, size=(n,)) + a * np.log(1 + np.arange(n))
+y = np.random.randint(-50, 50, size=(n,)) + 50. * np.log(1 + np.arange(n))
 
 ###############################################################################
 # Fit IsotonicRegression model
