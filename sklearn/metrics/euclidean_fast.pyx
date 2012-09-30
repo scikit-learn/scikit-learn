4a13d47747569e958a5a841ce570ffb4b57f47c5
Vlad Niculae <vlad@vene.ro>
2012-09-30 19:30:03 +0100

    API change: mutually exclusive preallocation and precomputation

--- sklearn/metrics/euclidean_fast.pyx
+++ sklearn/metrics/euclidean_fast.pyx
@@ -3,7 +3,7 @@
 # Author: Vlad Niculae <vlad@vene.ro>
 # License: Simple BSD.
 #
-#cython: boundscheck=False
+#cython: boundscheck=True
 #cython: wraparound=False
 #cython: cdivision=True
 
@@ -71,16 +71,29 @@ def dense_euclidean_distances(int X_rows,
                               int X_cols,
                               np.ndarray[DTYPE_t, ndim=2, mode="c"] X,
                               np.ndarray[DTYPE_t, ndim=2, mode="c"] Y,
-                              np.ndarray[DTYPE_t, ndim=1] X_norm_squared,
-                              np.ndarray[DTYPE_t, ndim=1] Y_norm_squared,
-                              bint X_norm_precomputed,
-                              bint Y_norm_precomputed,
+                              np.ndarray[DTYPE_t, ndim=1] X_norm_preallocated,
+                              np.ndarray[DTYPE_t, ndim=1] Y_norm_preallocated,
+                              np.ndarray[DTYPE_t, ndim=1] X_norm_precomputed,
+                              np.ndarray[DTYPE_t, ndim=1] Y_norm_precomputed,
                               np.ndarray[DTYPE_t, ndim=2, mode="c"] out,
                               bint squared=False):
-    if X_norm_squared is None:
-        X_norm_squared = np.empty(X_rows, dtype=np.float64)
-    if Y_norm_squared is None:
-        Y_norm_squared = np.empty(Y_rows, dtype=np.float64)
+    if X_norm_preallocated is not None and X_norm_precomputed is not None:
+        raise ValueError
+    elif X_norm_precomputed is None:
+        if X_norm_preallocated is None:
+            X_norm_preallocated = np.empty(X_rows, dtype=np.float64)
+        dense_sum_sq(X_rows, X_cols, X, X_norm_preallocated)
+    else:
+        X_norm_preallocated = X_norm_precomputed
+
+    if Y_norm_preallocated is not None and Y_norm_precomputed is not None:
+        raise ValueError
+    elif Y_norm_precomputed is None:
+        if Y_norm_preallocated is None:
+            Y_norm_preallocated = np.empty(Y_rows, dtype=np.float64)
+        dense_sum_sq(Y_rows, X_cols, Y, Y_norm_preallocated)
+    else:
+        Y_norm_preallocated = Y_norm_precomputed
 
     # 101 = c-order, 111 = no transpose, 112 = transpose (computes XY')
     dgemm(101, 111, 112, X_rows, Y_rows, X_cols, 1.0,
@@ -88,23 +101,26 @@ def dense_euclidean_distances(int X_rows,
           <DTYPE_t *>Y.data, Y.strides[0] / sizeof(DTYPE_t), 0.0,
           <DTYPE_t *>out.data, out.strides[0] / sizeof(DTYPE_t))
 
-    if not X_norm_precomputed:
-        dense_sum_sq(X_rows, X_cols, X, X_norm_squared)
-    if not Y_norm_precomputed:
-        dense_sum_sq(Y_rows, X_cols, Y, Y_norm_squared)
-    _euclidean_distances(X_rows, Y_rows, X_norm_squared, Y_norm_squared, out,
-                         squared)
+    _euclidean_distances(X_rows, Y_rows, X_norm_preallocated,
+                         Y_norm_preallocated, out, squared)
 
 
 def dense_euclidean_distances_sym(int X_rows,
                                   int X_cols,
                                   np.ndarray[DTYPE_t, ndim=2, mode="c"] X,
-                                  np.ndarray[DTYPE_t, ndim=1] X_norm_squared,
-                                  bint X_norm_precomputed,
+                                  np.ndarray[DTYPE_t, ndim=1] X_norm_preallocated,
+                                  np.ndarray[DTYPE_t, ndim=1] X_norm_precomputed,
                                   np.ndarray[DTYPE_t, ndim=2, mode="c"] out,
                                   bint squared=False):
-    if X_norm_squared is None:
-        X_norm_squared = np.empty(X_rows, dtype=np.float64)
+
+    if X_norm_preallocated is not None and X_norm_precomputed is not None:
+        raise ValueError
+    elif X_norm_precomputed is None:
+        if X_norm_preallocated is None:
+            X_norm_preallocated = np.empty(X_rows, dtype=np.float64)
+        dense_sum_sq(X_rows, X_cols, X, X_norm_preallocated)
+    else:
+        X_norm_preallocated = X_norm_precomputed
 
     # 101 = c-order, 122 = lower triangular, 111 = no transpose (computes XX')
     # Attention, the upper triangle contains garbage.
@@ -112,11 +128,8 @@ def dense_euclidean_distances_sym(int X_rows,
           X.strides[0] / sizeof(DTYPE_t), 0.0, <DTYPE_t *>out.data,
           out.strides[0] / sizeof(DTYPE_t))
 
-    if not X_norm_precomputed:
-        dense_sum_sq(X_rows, X_cols, X, X_norm_squared)
-
-    _euclidean_distances(X_rows, X_rows, X_norm_squared, X_norm_squared, out,
-                         squared, True)
+    _euclidean_distances(X_rows, X_rows, X_norm_preallocated,
+                         X_norm_preallocated, out, squared, True)
 
 
 # This function assumes the XYt parameter contains the precomputed outer dot
@@ -128,70 +141,87 @@ def sparse_euclidean_distances(int X_rows,
                                np.ndarray[DTYPE_t, ndim=1] Y_data,
                                np.ndarray[int, ndim=1] Y_indices,
                                np.ndarray[int, ndim=1] Y_indptr,
-                               np.ndarray[DTYPE_t, ndim=1] X_norm_squared,
-                               np.ndarray[DTYPE_t, ndim=1] Y_norm_squared,
-                               bint X_norm_precomputed,
-                               bint Y_norm_precomputed,
+                               np.ndarray[DTYPE_t, ndim=1] X_norm_preallocated,
+                               np.ndarray[DTYPE_t, ndim=1] Y_norm_preallocated,
+                               np.ndarray[DTYPE_t, ndim=1] X_norm_precomputed,
+                               np.ndarray[DTYPE_t, ndim=1] Y_norm_precomputed,
                                np.ndarray[DTYPE_t, ndim=2] XYt,
                                bint squared=False):
-    if X_norm_squared is None:
-        X_norm_squared = np.empty(X_rows, dtype=np.float64)
-    if Y_norm_squared is None:
-        Y_norm_squared = np.empty(Y_rows, dtype=np.float64)
+    if X_norm_preallocated is not None and X_norm_precomputed is not None:
+        raise ValueError
+    elif X_norm_precomputed is None:
+        if X_norm_preallocated is None:
+            X_norm_preallocated = np.empty(X_rows, dtype=np.float64)
+        sparse_sum_sq(X_rows, X_data, X_indices, X_indptr, X_norm_preallocated)
+    else:
+        X_norm_preallocated = X_norm_precomputed
 
-    if not X_norm_precomputed:
-        sparse_sum_sq(X_rows, X_data, X_indices, X_indptr, X_norm_squared)
-    if not Y_norm_precomputed:
-        sparse_sum_sq(Y_rows, Y_data, Y_indices, Y_indptr, Y_norm_squared)
+    if Y_norm_preallocated is not None and Y_norm_precomputed is not None:
+        raise ValueError
+    elif Y_norm_precomputed is None:
+        if Y_norm_preallocated is None:
+            Y_norm_preallocated = np.empty(Y_rows, dtype=np.float64)
+        sparse_sum_sq(Y_rows, Y_data, Y_indices, Y_indptr, Y_norm_preallocated)
+    else:
+        Y_norm_preallocated = Y_norm_precomputed
 
-    _euclidean_distances(X_rows, Y_rows, X_norm_squared, Y_norm_squared, XYt,
-                         squared)
+    _euclidean_distances(X_rows, Y_rows, X_norm_preallocated,
+                         Y_norm_preallocated, XYt, squared)
 
 
 # This function assumes the XXt parameter contains the precomputed outer dot
-def sparse_euclidean_distances_sym(int X_rows,
-                                   np.ndarray[DTYPE_t, ndim=1] X_data,
-                                   np.ndarray[int, ndim=1] X_indices,
-                                   np.ndarray[int, ndim=1] X_indptr,
-                                   np.ndarray[DTYPE_t, ndim=1] X_norm_squared,
-                                   bint X_norm_precomputed,
-                                   np.ndarray[DTYPE_t, ndim=2] XXt,
-                                   bint squared=False):
-    if X_norm_squared is None:
-        X_norm_squared = np.empty(X_rows, dtype=np.float64)
-
-    if not X_norm_precomputed:
-        sparse_sum_sq(X_rows, X_data, X_indices, X_indptr, X_norm_squared)
-
-    _euclidean_distances(X_rows, X_rows, X_norm_squared, X_norm_squared, XXt,
-                         squared, True)
+def sparse_euclidean_distances_sym(
+                              int X_rows,
+                              np.ndarray[DTYPE_t, ndim=1] X_data,
+                              np.ndarray[int, ndim=1] X_indices,
+                              np.ndarray[int, ndim=1] X_indptr,
+                              np.ndarray[DTYPE_t, ndim=1] X_norm_preallocated,
+                              np.ndarray[DTYPE_t, ndim=1] X_norm_precomputed,
+                              np.ndarray[DTYPE_t, ndim=2] XXt,
+                              bint squared=False):
+    if X_norm_preallocated is not None and X_norm_precomputed is not None:
+        raise ValueError
+    elif X_norm_precomputed is None:
+        if X_norm_preallocated is None:
+            X_norm_preallocated = np.empty(X_rows, dtype=np.float64)
+        sparse_sum_sq(X_rows, X_data, X_indices, X_indptr, X_norm_preallocated)
+    else:
+        X_norm_preallocated = X_norm_precomputed
+    _euclidean_distances(X_rows, X_rows, X_norm_preallocated,
+                         X_norm_preallocated, XXt, squared, True)
 
 
 # This function assumes the XYt parameter contains the precomputed outer dot
-def sparse_dense_euclidean_distances(int X_rows,
-                                     int X_cols,
-                                     int Y_rows,
-                                     np.ndarray[DTYPE_t, ndim=1] X_data,
-                                     np.ndarray[int, ndim=1] X_indices,
-                                     np.ndarray[int, ndim=1] X_indptr,
-                                     np.ndarray[DTYPE_t, ndim=2, mode="c"] Y,
-                                     np.ndarray[DTYPE_t, ndim=1]
-                                         X_norm_squared,
-                                     np.ndarray[DTYPE_t, ndim=1]
-                                         Y_norm_squared,
-                                     bint X_norm_precomputed,
-                                     bint Y_norm_precomputed,
-                                     np.ndarray[DTYPE_t, ndim=2] XYt,
-                                     bint squared=False):
-    if X_norm_squared is None:
-        X_norm_squared = np.empty(X_rows, dtype=np.float64)
-    if Y_norm_squared is None:
-        Y_norm_squared = np.empty(Y_rows, dtype=np.float64)
+def sparse_dense_euclidean_distances(
+                            int X_rows,
+                            int X_cols,
+                            int Y_rows,
+                            np.ndarray[DTYPE_t, ndim=1] X_data,
+                            np.ndarray[int, ndim=1] X_indices,
+                            np.ndarray[int, ndim=1] X_indptr,
+                            np.ndarray[DTYPE_t, ndim=2, mode="c"] Y,
+                            np.ndarray[DTYPE_t, ndim=1] X_norm_preallocated,
+                            np.ndarray[DTYPE_t, ndim=1] Y_norm_preallocated,
+                            np.ndarray[DTYPE_t, ndim=1] X_norm_precomputed,
+                            np.ndarray[DTYPE_t, ndim=1] Y_norm_precomputed,
+                            np.ndarray[DTYPE_t, ndim=2] XYt,
+                            bint squared=False):
+    if X_norm_preallocated is not None and X_norm_precomputed is not None:
+        raise ValueError
+    elif X_norm_precomputed is None:
+        if X_norm_preallocated is None:
+            X_norm_preallocated = np.empty(X_rows, dtype=np.float64)
+        sparse_sum_sq(X_rows, X_data, X_indices, X_indptr, X_norm_preallocated)
+    else:
+        X_norm_preallocated = X_norm_precomputed
 
-    if not X_norm_precomputed:
-        sparse_sum_sq(X_rows, X_data, X_indices, X_indptr, X_norm_squared)
-    if not Y_norm_precomputed:
-        dense_sum_sq(Y_rows, X_cols, Y, Y_norm_squared)
-
-    _euclidean_distances(X_rows, Y_rows, X_norm_squared, Y_norm_squared, XYt,
-                         squared)
+    if Y_norm_preallocated is not None and Y_norm_precomputed is not None:
+        raise ValueError
+    elif Y_norm_precomputed is None:
+        if Y_norm_preallocated is None:
+            Y_norm_preallocated = np.empty(Y_rows, dtype=np.float64)
+        dense_sum_sq(Y_rows, X_cols, Y, Y_norm_preallocated)
+    else:
+        Y_norm_preallocated = Y_norm_precomputed
+    _euclidean_distances(X_rows, Y_rows, X_norm_preallocated,
+                         Y_norm_preallocated, XYt, squared)
