b7c43855391e1c938020131276c2ef7003d2c6e5
kuantkid <kuantkid@gmail.com>
2012-09-30 22:39:30 +0800

    ENH docs partially updated, happy mooncake festival

--- sklearn/manifold/spectral_embedding.py
+++ sklearn/manifold/spectral_embedding.py
@@ -10,134 +10,14 @@ import numpy as np
 from ..base import BaseEstimator, TransformerMixin
 from ..utils import check_random_state
 from ..utils.graph import graph_laplacian
-from ..utils.arpack import eigsh
 from ..metrics.pairwise import rbf_kernel
 from ..neighbors import kneighbors_graph
 from ..metrics.pairwise import pairwise_kernels
 from ..metrics.pairwise import rbf_kernel
-from scipy import sparse
-from scipy.sparse.linalg import lobpcg
-
-
-def spectral_embedding(adjacency, n_components=2,
-                       eigen_solver=None, random_state=None,
-                       norm_laplacian=True):
-    """Project the sample on the first eigen vectors of the graph Laplacian
-
-    The adjacency matrix is used to compute a normalized graph Laplacian
-    whose spectrum (especially the eigen vectors associated to the
-    smallest eigen values) has an interpretation in terms of minimal
-    number of cuts necessary to split the graph into comparably sized
-    components.
-
-    This embedding can also 'work' even if the ``adjacency`` variable is
-    not strictly the adjacency matrix of a graph but more generally
-    an affinity or similarity matrix between samples (for instance the
-    heat kernel of a euclidean distance matrix or a k-NN matrix).
-
-    However care must taken to always make the affinity matrix symmetric
-    so that the eigen vector decomposition works as expected.
-
-    Parameters
-    -----------
-    adjacency : array-like or sparse matrix, shape: (n_samples, n_samples)
-        The adjacency matrix of the graph to embed.
-
-    n_components : integer, default: 2
-        The dimension of the projected subspace.
-
-    norm_laplacian : bool, default: False
-        Use normalized graph laplacian (True) or un-normalized graph
-        laplacian (False)
-
-    Returns
-    --------
-    embedding : array, shape: (n_samples, n_components)
-        The reduced samples.
-
-    Notes
-    ------
-    The graph should contain only one connected component, elsewhere the
-    results make little sense.
-    """
-
-    try:
-        from pyamg import smoothed_aggregation_solver
-    except ImportError:
-        if eigen_solver == "amg":
-            raise ValueError(
-                "The eigen_solver was set to 'amg', but pyamg is "
-                "not available.")
-    n_nodes = adjacency.shape[0]
-    # XXX: Should we check that the matrices given is symmetric
-    if eigen_solver is None:
-        eigen_solver = 'arpack'
-    laplacian, dd = graph_laplacian(adjacency,
-                                    normed=True, return_diag=True)
-    if (eigen_solver == 'arpack'
-        or not sparse.isspmatrix(laplacian)
-        or n_nodes < 5 * n_components):
-        # lobpcg used with mode='amg' has bugs for low number of nodes
-
-        # We need to put the diagonal at zero
-        if not sparse.isspmatrix(laplacian):
-            laplacian.flat[::n_nodes + 1] = 0
-        else:
-            laplacian = laplacian.tocoo()
-            diag_idx = (laplacian.row == laplacian.col)
-            laplacian.data[diag_idx] = 0
-            # If the matrix has a small number of diagonals (as in the
-            # case of structured matrices comming from images), the
-            # dia format might be best suited for matvec products:
-            n_diags = np.unique(laplacian.row - laplacian.col).size
-            if n_diags <= 7:
-                # 3 or less outer diagonals on each side
-                laplacian = laplacian.todia()
-            else:
-                # csr has the fastest matvec and is thus best suited to
-                # arpack
-                laplacian = laplacian.tocsr()
-
-        # Here we'll use shift-invert mode for fast eigenvalues (see
-        # http://docs.scipy.org/doc/scipy/reference/tutorial/arpack.html
-        # for a short explanation of what this means) Because the
-        # normalized Laplacian has eigenvalues between 0 and 2, I - L has
-        # eigenvalues between -1 and 1.  ARPACK is most efficient when
-        # finding eigenvalues of largest magnitude (keyword which='LM') and
-        # when these eigenvalues are very large compared to the rest. For
-        # very large, very sparse graphs, I - L can have many, many
-        # eigenvalues very near 1.0.  This leads to slow convergence.  So
-        # instead, we'll use ARPACK's shift-invert mode, asking for the
-        # eigenvalues near 1.0.  This effectively spreads-out the spectrum
-        # near 1.0 and leads to much faster convergence: potentially an
-        # orders-of-magnitude speedup over simply using keyword which='LA'
-        # in standard mode.
-        lambdas, diffusion_map = eigsh(-laplacian, k=n_components + 1,
-                                        sigma=1.0, which='LM')
-        embedding = diffusion_map.T[n_components - 1::-1] * dd
-    elif eigen_solver == 'amg':
-        # Use AMG to get a preconditioner and speed up the eigenvalue
-        # problem.
-        laplacian = laplacian.astype(np.float)  # lobpcg needs native float
-        ml = smoothed_aggregation_solver(laplacian.tocsr())
-        random_state = check_random_state(random_state)
-        X = random_state.rand(laplacian.shape[0],
-                                   n_components)
-        X[:, 0] = 1. / dd.ravel()
-        M = ml.aspreconditioner()
-        lambdas, diffusion_map = lobpcg(laplacian, X, M=M, tol=1.e-12,
-                                        largest=False)
-        embedding = diffusion_map.T * dd
-        if embedding.shape[0] == 1:
-            raise ValueError
-    else:
-        raise ValueError("Unknown value for mode: '%s'."
-                         "Should be 'amg' or 'arpack'" % eigen_solver)
-    return embedding.T
 
 
 class SpectralEmbedding(BaseEstimator, TransformerMixin):
-    """Spectral Embedding for Non-linear Dimensionality Reduction.
+    """Spectral Embedding for Non-linear Dimensionality Reduction
 
     Forms an affinity matrix given by the specified function and
     applies spectral decomposition to the corresponding graph laplacian.
@@ -162,13 +42,13 @@ class SpectralEmbedding(BaseEstimator, TransformerMixin):
         How to construct the affinity matrix.
          - 'nearest_neighbors' : construct affinity matrix by knn graph
          - 'rbf' : construct affinity matrix by rbf kernel
-         - 'precomputed' : interpret X as precomputed affinity matrix
-         - callable : use passed in function as affinity
-           the function takes in data matrix (n_samples, n_features)
-           and return affinity matrix (n_samples, n_samples).
+         - 'precomputed' : precomputed affinity matrix
+         - callable : a specific function that takes in
+           data matrix (n_samples, n_features) and return
+           affinity matrix (n_samples, n_samples).
 
     gamma : float, optional, default : 1/n_features
-        Kernel coefficient for rbf kernel.
+        Affinity coefficient for rbf graph.
 
     n_neighbors : int, default : max(n_samples/10 , 1)
         Number of nearest neighbors for nearest_neighbors graph building.
@@ -180,10 +60,10 @@ class SpectralEmbedding(BaseEstimator, TransformerMixin):
     ----------
 
     `embedding_` : array, shape = (n_samples, n_components)
-        Spectral embedding of the training matrix.
+        Spectral embedding of the training matrix
 
     `affinity_matrix_` : array, shape = (n_samples, n_samples)
-        Affinity_matrix constructed from samples or precomputed.
+        affinity_matrix constructed from samples or precomputed
 
     References
     ----------
@@ -195,13 +75,13 @@ class SpectralEmbedding(BaseEstimator, TransformerMixin):
     - On Spectral Clustering: Analysis and an algorithm, 2011
       Andrew Y. Ng, Michael I. Jordan, Yair Weiss
       http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.19.8100
-
+      
     - Normalized cuts and image segmentation, 2000
       Jianbo Shi, Jitendra Malik
       http://citeseer.ist.psu.edu/viewdoc/summary?doi=10.1.1.160.2324
     """
 
-    def __init__(self, n_components=2, affinity="nearest_neighbors",
+    def __init__(self, n_components=None, affinity="nearest_neighbors",
                  gamma=None, fit_inverse_transform=False,
                  random_state=None, eigen_solver=None, n_neighbors=None):
         self.n_components = n_components
@@ -220,14 +100,13 @@ class SpectralEmbedding(BaseEstimator, TransformerMixin):
         """Caclulate the affinity matrix from data
         Parameters
         ----------
-        X : array-like, shape (n_samples, n_features)
+        X: array-like, shape (n_samples, n_features)
             Training vector, where n_samples in the number of samples
             and n_features is the number of features.
 
-            If affinity is "precomputed"
-            X : array-like, shape (n_samples, n_samples),
-            Interpret X as precomputed adjacency graph computed from
-            samples.
+           array-like, shape (n_samples, n_samples)
+            If self.precomputed == true
+            Precomputed adjacency graph computed from samples
 
         Returns
         -------
@@ -242,9 +121,6 @@ class SpectralEmbedding(BaseEstimator, TransformerMixin):
             if self.n_neighbors is None:
                 self.n_neighbors = max(int(X.shape[0] / 10), 1)
             self.affinity_matrix_ = kneighbors_graph(X, self.n_neighbors)
-            # currently only symmetric affinity_matrix supported
-            self.affinity_matrix_ = 0.5 * (self.affinity_matrix_ +
-                                           self.affinity_matrix_.T)
             return self.affinity_matrix_
         if self.affinity == 'rbf':
             if self.gamma is None:
@@ -269,30 +145,25 @@ class SpectralEmbedding(BaseEstimator, TransformerMixin):
             Training vector, where n_samples in the number of samples
             and n_features is the number of features.
 
-            If affinity is "precomputed"
-            X : array-like, shape (n_samples, n_samples),
-            Interpret X as precomputed adjacency graph computed from
-            samples.
+            If self.precomputed is True
+            Precomputed affinity matrix computed from samples is used
 
         Returns
         -------
         self : object
             Returns the instance itself.
         """
-        if isinstance(self.affinity, str):
-            if self.affinity not in \
+        if isinstance(affinity, str):
+            if self.affinity not in
                 {'precomputed', 'rbf', 'nearest_neighbors'}:
                 raise ValueError(
                     "Only precomputed, rbf,"
                     "nearest_neighbors graph supported.")
-        if self.fit_inverse_transform and self.affinity == 'precomputed':
+        if fit_inverse_transform and graph == 'precomputed':
             raise ValueError(
                 "Cannot fit_inverse_transform with a precomputed kernel.")
         affinity_matrix = self._get_affinity_matrix(X)
-        self.embedding_ = spectral_embedding(affinity_matrix,
-                                             n_components=self.n_components,
-                                             eigen_solver=self.eigen_solver,
-                                             random_state=self.random_state)
+        self.embedding_ = self._spectra_embedding(affinity_matrix)
         return self
 
     def fit_transform(self, X, y=None):
@@ -304,10 +175,8 @@ class SpectralEmbedding(BaseEstimator, TransformerMixin):
             Training vector, where n_samples in the number of samples
             and n_features is the number of features.
 
-            If affinity is "precomputed"
-            X : array-like, shape (n_samples, n_samples),
-            Interpret X as precomputed adjacency graph computed from
-            samples.
+            If self.precomputed is True
+            Precomputed affinity matrix computed from samples is used
 
         Returns
         -------
@@ -315,3 +184,112 @@ class SpectralEmbedding(BaseEstimator, TransformerMixin):
         """
         self.fit(X)
         return self.embedding_
+
+    def _spectra_embedding(self, adjacency):
+        """Project the sample on the first eigen vectors of the graph Laplacian
+
+        The adjacency matrix is used to compute a normalized graph Laplacian
+        whose spectrum (especially the eigen vectors associated to the
+        smallest eigen values) has an interpretation in terms of minimal
+        number of cuts necessary to split the graph into comparably sized
+        components.
+
+        This embedding can also 'work' even if the ``adjacency`` variable is
+        not strictly the adjacency matrix of a graph but more generally
+        an affinity or similarity matrix between samples (for instance the
+        heat kernel of a euclidean distance matrix or a k-NN matrix).
+
+        However care must taken to always make the affinity matrix symmetric
+        so that the eigen vector decomposition works as expected.
+
+        Parameters
+        -----------
+        adjacency: array-like or sparse matrix, shape: (n_samples, n_samples)
+            The adjacency matrix of the graph to embed.
+
+        Returns
+        --------
+        embedding: array, shape: (n_samples, n_components)
+            The reduced samples
+
+        Notes
+        ------
+        The graph should contain only one connected component, elsewhere the
+        results make little sense.
+        """
+
+        from scipy import sparse
+        from ..utils.arpack import eigsh
+        from scipy.sparse.linalg import lobpcg
+        try:
+            from pyamg import smoothed_aggregation_solver
+        except ImportError:
+            if self.eigen_solver == "amg":
+                raise ValueError(
+                    "The eigen_solver was set to 'amg', but pyamg is "
+                    "not available.")
+        n_nodes = adjacency.shape[0]
+        # XXX: Should we check that the matrices given is symmetric
+        if self.eigen_solver is None:
+            self.eigen_solver = 'arpack'
+        laplacian, dd = graph_laplacian(adjacency,
+                                        normed=True, return_diag=True)
+        if (self.eigen_solver == 'arpack'
+            or not sparse.isspmatrix(laplacian)
+            or n_nodes < 5 * self.n_components):
+            # lobpcg used with mode='amg' has bugs for low number of nodes
+
+            # We need to put the diagonal at zero
+            if not sparse.isspmatrix(laplacian):
+                laplacian.flat[::n_nodes + 1] = 0
+            else:
+                laplacian = laplacian.tocoo()
+                diag_idx = (laplacian.row == laplacian.col)
+                laplacian.data[diag_idx] = 0
+                # If the matrix has a small number of diagonals (as in the
+                # case of structured matrices comming from images), the
+                # dia format might be best suited for matvec products:
+                n_diags = np.unique(laplacian.row - laplacian.col).size
+                if n_diags <= 7:
+                    # 3 or less outer diagonals on each side
+                    laplacian = laplacian.todia()
+                else:
+                    # csr has the fastest matvec and is thus best suited to
+                    # arpack
+                    laplacian = laplacian.tocsr()
+
+            # Here we'll use shift-invert mode for fast eigenvalues (see
+            # http://docs.scipy.org/doc/scipy/reference/tutorial/arpack.html
+            # for a short explanation of what this means) Because the
+            # normalized Laplacian has eigenvalues between 0 and 2, I - L has
+            # eigenvalues between -1 and 1.  ARPACK is most efficient when
+            # finding eigenvalues of largest magnitude (keyword which='LM') and
+            # when these eigenvalues are very large compared to the rest. For
+            # very large, very sparse graphs, I - L can have many, many
+            # eigenvalues very near 1.0.  This leads to slow convergence.  So
+            # instead, we'll use ARPACK's shift-invert mode, asking for the
+            # eigenvalues near 1.0.  This effectively spreads-out the spectrum
+            # near 1.0 and leads to much faster convergence: potentially an
+            # orders-of-magnitude speedup over simply using keyword which='LA'
+            # in standard mode.
+            lambdas, diffusion_map = eigsh(-laplacian, k=self.n_components,
+                                           sigma=1.0, which='LM')
+            embedding = diffusion_map.T[::-1] * dd
+        elif self.eigen_solver == 'amg':
+            # Use AMG to get a preconditioner and speed up the eigenvalue
+            # problem.
+            laplacian = laplacian.astype(np.float)  # lobpcg needs native float
+            ml = smoothed_aggregation_solver(laplacian.tocsr())
+            X = self.random_state.rand(laplacian.shape[0],
+                                       self.n_components)
+            X[:, 0] = 1. / dd.ravel()
+            M = ml.aspreconditioner()
+            lambdas, diffusion_map = lobpcg(laplacian, X, M=M, tol=1.e-12,
+                                            largest=False)
+            embedding = diffusion_map.T * dd
+            if embedding.shape[0] == 1:
+                raise ValueError
+        else:
+            raise ValueError("Unknown value for mode: '%s'."
+                             "Should be 'amg' or 'arpack'" % self.eigen_solver)
+        return embedding.T
--- sklearn/manifold/tests/test_spectral_embedding.py
+++ sklearn/manifold/tests/test_spectral_embedding.py
@@ -1,63 +1,27 @@
 from nose.tools import assert_true
 from nose.tools import assert_equal
 
-from scipy import sparse
 from scipy.sparse import csr_matrix
 import numpy as np
 from numpy.testing import assert_almost_equal, assert_array_almost_equal
 
-from nose.tools import assert_raises
-
-from sklearn.manifold.spectral_embedding import SpectralEmbedding
+from sklearn.manifold.spectra_embedding import SpectralEmbedding
 from sklearn.metrics.pairwise import rbf_kernel
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import normalized_mutual_info_score
 from sklearn.cluster import KMeans, SpectralClustering
-from sklearn.datasets.samples_generator import make_blobs
+
+S = np.array([[1, 5, 2, 1, 0, 0, 0],
+              [5, 1, 3, 1, 0, 0, 0],
+              [2, 3, 1, 1, 0, 0, 0],
+              [1, 1, 1, 1, 2, 1, 1],
+              [0, 0, 0, 2, 2, 3, 2],
+              [0, 0, 0, 1, 3, 1, 4],
+              [0, 0, 0, 1, 2, 4, 1],
+              ])
 
 
-# non centered, sparse centers to check the
-centers = np.array([
-    [0.0, 5.0, 0.0, 0.0, 0.0],
-    [0.0, 0.0, 4.0, 0.0, 0.0],
-    [1.0, 0.0, 0.0, 5.0, 1.0],
-])
-n_samples = 1000
-n_clusters, n_features = centers.shape
-S, true_labels = make_blobs(n_samples=n_samples, centers=centers,
-                            cluster_std=1., random_state=42)
-
-
-def test_spectral_embedding_two_components(seed=36):
-    """Test spectral embedding with two components"""
-    random_state = np.random.RandomState(seed)
-    n_sample = 10
-    affinity = np.zeros(shape=[n_sample * 2,
-                               n_sample * 2])
-    # first component
-    affinity[0:n_sample,
-             0:n_sample] = np.abs(random_state.randn(n_sample, n_sample)) + 2
-    # second component
-    affinity[n_sample::,
-             n_sample::] = np.abs(random_state.randn(n_sample, n_sample)) + 2
-    affinity = 0.5 * (affinity + affinity.T)
-    # connection
-    affinity[0, n_sample + 1] = 1
-    affinity[n_sample + 1, 0] = 1
-    affinity.flat[::2 * n_sample + 1] = 0
-
-    true_label = np.zeros(shape=2 * n_sample)
-    true_label[0:n_sample] = 1
-
-    se_precomp = SpectralEmbedding(n_components=1, affinity="precomputed",
-                                   random_state=np.random.RandomState(seed))
-    embedded_corrdinate = np.squeeze(se_precomp.fit_transform(affinity))
-    # thresholding on the first components using 0.
-    label_ = np.array(embedded_corrdinate < 0, dtype="float")
-    assert_equal(normalized_mutual_info_score(true_label, label_), 1.0)
-
-
-def test_spectral_embedding_precomputed_affinity(seed=36):
+def test_spectra_embedding_precomputed_graph(seed=36):
     """Test spectral embedding with precomputed kernel"""
     gamma = 1.0
     se_precomp = SpectralEmbedding(n_components=3, affinity="precomputed",
@@ -65,15 +29,23 @@ def test_spectral_embedding_precomputed_affinity(seed=36):
     se_rbf = SpectralEmbedding(n_components=3, affinity="rbf",
                                gamma=gamma,
                                random_state=np.random.RandomState(seed))
+    se_knn = SpectralEmbedding(n_components=3, affinity="nearest_neighbors",
+                               gamma=gamma,
+                               random_state=np.random.RandomState(seed))
     embed_precomp = se_precomp.fit_transform(rbf_kernel(S, gamma=gamma))
     embed_rbf = se_rbf.fit_transform(S)
+    embed_knn = se_knn.fit_transform(S)
     assert_array_almost_equal(
         se_precomp.affinity_matrix_, se_rbf.affinity_matrix_)
     assert_array_almost_equal(np.abs(embed_precomp), np.abs(embed_rbf), 0)
 
 
-def test_spectral_embedding_callable_affinity(seed=36):
-    """Test spectral embedding with callable affinity"""
+def test_spectra_embedding_knn_graph(seed=36):
+    """Test spectral embedding with knn graph"""
+
+
+def test_spectra_embedding_callable_graph(seed=36):
+    """Test spectral embedding with knn graph"""
     gamma = 0.9
     kern = rbf_kernel(S, gamma=gamma)
     se_callable = SpectralEmbedding(n_components=3,
@@ -86,76 +58,24 @@ def test_spectral_embedding_callable_affinity(seed=36):
                                random_state=np.random.RandomState(seed))
     embed_rbf = se_rbf.fit_transform(S)
     embed_callable = se_callable.fit_transform(S)
-    embed_rbf = se_rbf.fit_transform(S)
-    embed_callable = se_callable.fit_transform(S)
-    assert_array_almost_equal(
-        se_callable.affinity_matrix_, se_rbf.affinity_matrix_)
-    assert_array_almost_equal(np.abs(embed_rbf), np.abs(embed_callable), 2)
 
 
-def test_spectral_embedding_amg_solver(seed=36):
-    """Test spectral embedding with amg solver"""
-    gamma = 0.9
-    se_amg = SpectralEmbedding(n_components=3, affinity="rbf",
-                               gamma=gamma, eigen_solver="amg",
-                               random_state=np.random.RandomState(seed))
-    se_arpack = SpectralEmbedding(n_components=3, affinity="rbf",
-                                  gamma=gamma, eigen_solver="arpack",
-                                  random_state=np.random.RandomState(seed))
-    embed_amg = se_amg.fit_transform(S)
-    embed_arpack = se_arpack.fit_transform(S)
-    assert_array_almost_equal(
-        se_amg.affinity_matrix_, se_arpack.affinity_matrix_)
-    assert_array_almost_equal(np.abs(embed_amg), np.abs(embed_arpack), 2)
-
-
-def test_pipline_spectral_clustering(seed=36):
+def test_pipline_spectra_clustering():
     """Test using pipline to do spectral clustering"""
-    random_state = np.random.RandomState(seed)
-    se_rbf = SpectralEmbedding(n_components=n_clusters,
-                               affinity="rbf",
-                               random_state=random_state)
-    se_knn = SpectralEmbedding(n_components=n_clusters,
-                               affinity="nearest_neighbors",
-                               n_neighbors=5,
-                               random_state=random_state)
-    for se in [se_rbf, se_knn]:
-        km = KMeans(n_clusters=n_clusters, random_state=random_state)
-        km.fit(se.fit_transform(S))
+    spectral_clustering = Pipeline([
+        ('se', SpectralEmbedding()),
+        ('km', KMeans()),
+    ])
+
+    for n_cluster in range(1, 5):
+        n_cluster = 3
+        spectral_clustering.set_params(km__n_clusters=n_cluster)
+        spectral_clustering.set_params(se__n_components=n_cluster)
+        spectral_clustering.set_params(se__gamma=1.0)
+        spectral_clustering.fit(S)
+        SC = SpectralClustering(n_clusters=n_cluster)
+        SC.fit(S)
         assert_array_almost_equal(
             normalized_mutual_info_score(
-                km.labels_,
-                true_labels), 1.0, 2)
-
-
-def test_spectral_embedding_unknown_eigensolver(seed=36):
-    """Test that SpectralClustering fails with an unknown eigensolver"""
-    centers = np.array([
-        [0., 0., 0.],
-        [10., 10., 10.],
-        [20., 20., 20.],
-    ])
-    X, true_labels = make_blobs(n_samples=100, centers=centers,
-                                cluster_std=1., random_state=42)
-    D = rbf_kernel(X)  # Distance matrix
-
-    se_precomp = SpectralEmbedding(n_components=1, affinity="precomputed",
-                                   random_state=np.random.RandomState(seed),
-                                   eigen_solver="<unknown>")
-    assert_raises(ValueError, se_precomp.fit, S)
-
-
-def test_spectral_embedding_unknown_affinity(seed=36):
-    """Test that SpectralClustering fails with an unknown affinity type"""
-    centers = np.array([
-        [0., 0., 0.],
-        [10., 10., 10.],
-        [20., 20., 20.],
-    ])
-    X, true_labels = make_blobs(n_samples=100, centers=centers,
-                                cluster_std=1., random_state=42)
-    D = rbf_kernel(X)  # Distance matrix
-
-    se_precomp = SpectralEmbedding(n_components=1, affinity="<unknown>",
-                                   random_state=np.random.RandomState(seed))
-    assert_raises(ValueError, se_precomp.fit, S)
+                spectral_clustering.steps[1][1].labels_,
+                SC.labels_), 0.0, 0)
