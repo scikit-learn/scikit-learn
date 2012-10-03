dc6212b063ab93cccfcfb4375612b51c4f9fd0ba
kuantkid <kuantkid@gmail.com>
2012-10-03 19:49:12 +0800

    FIX: eliminate \ for continuation from doctests

--- sklearn/metrics/cluster/supervised.py
+++ sklearn/metrics/cluster/supervised.py
@@ -309,21 +309,21 @@ def homogeneity_score(labels_true, labels_pred):
     Non-pefect labelings that futher split classes into more clusters can be
     perfectly homogeneous::
 
-      >>> print ("%.6f" % homogeneity_score([0, 0, 1, 1],\
-                                            [0, 0, 1, 2])) # doctest: +ELLIPSIS
+      >>> print ("%.6f" % homogeneity_score([0, 0, 1, 1], [0, 0, 1, 2]))
+      ...                                                  # doctest: +ELLIPSIS
       1.0...
-      >>> print ("%.6f" % homogeneity_score([0, 0, 1, 1],\
-                                            [0, 1, 2, 3])) # doctest: +ELLIPSIS
+      >>> print ("%.6f" % homogeneity_score([0, 0, 1, 1], [0, 1, 2, 3]))
+      ...                                                  # doctest: +ELLIPSIS
       1.0...
 
     Clusters that include samples from different classes do not make for an
     homogeneous labeling::
 
-      >>> print ("%.6f" % homogeneity_score([0, 0, 1, 1],\
-                                            [0, 1, 0, 1])) # doctest: +ELLIPSIS
+      >>> print ("%.6f" % homogeneity_score([0, 0, 1, 1], [0, 1, 0, 1]))
+      ...                                                  # doctest: +ELLIPSIS
       0.0...
-      >>> print ("%.6f" % homogeneity_score([0, 0, 1, 1],\
-                                            [0, 0, 0, 0])) # doctest: +ELLIPSIS
+      >>> print ("%.6f" % homogeneity_score([0, 0, 1, 1], [0, 0, 0, 0]))
+      ...                                                  # doctest: +ELLIPSIS
       0.0...
 
     """
@@ -454,36 +454,36 @@ def v_measure_score(labels_true, labels_pred):
     Labelings that assign all classes members to the same clusters
     are complete be not homogeneous, hence penalized::
 
-      >>> print("%.6f" % v_measure_score([0, 0, 1, 2],\
-                                         [0, 0, 1, 1])) # doctest: +ELLIPSIS
+      >>> print("%.6f" % v_measure_score([0, 0, 1, 2], [0, 0, 1, 1]))
+      ...                                                  # doctest: +ELLIPSIS
       0.8...
-      >>> print("%.6f" % v_measure_score([0, 1, 2, 3],\
-                                         [0, 0, 1, 1])) # doctest: +ELLIPSIS
+      >>> print("%.6f" % v_measure_score([0, 1, 2, 3], [0, 0, 1, 1]))
+      ...                                                  # doctest: +ELLIPSIS
       0.66...
 
     Labelings that have pure clusters with members coming from the same
     classes are homogeneous but un-necessary splits harms completeness
     and thus penalize V-measure as well::
 
-      >>> print("%.6f" % v_measure_score([0, 0, 1, 1],\
-                                         [0, 0, 1, 2]))  # doctest: +ELLIPSIS
+      >>> print("%.6f" % v_measure_score([0, 0, 1, 1], [0, 0, 1, 2]))
+      ...                                                  # doctest: +ELLIPSIS
       0.8...
-      >>> print("%.6f" % v_measure_score([0, 0, 1, 1],\
-                                         [0, 1, 2, 3]))  # doctest: +ELLIPSIS
+      >>> print("%.6f" % v_measure_score([0, 0, 1, 1], [0, 1, 2, 3]))
+      ...                                                  # doctest: +ELLIPSIS
       0.66...
 
     If classes members are completly splitted across different clusters,
     the assignment is totally in-complete, hence the v-measure is null::
 
-      >>> print("%.6f" % v_measure_score([0, 0, 0, 0],\
-                                         [0, 1, 2, 3]))  # doctest: +ELLIPSIS
+      >>> print("%.6f" % v_measure_score([0, 0, 0, 0], [0, 1, 2, 3]))
+      ...                                                  # doctest: +ELLIPSIS
       0.0...
 
     Clusters that include samples from totally different classes totally
     destroy the homogeneity of the labeling, hence::
 
-      >>> print("%.6f" % v_measure_score([0, 0, 1, 1],\
-                                         [0, 0, 0, 0])) # doctest: +ELLIPSIS
+      >>> print("%.6f" % v_measure_score([0, 0, 1, 1], [0, 0, 0, 0]))
+      ...                                                  # doctest: +ELLIPSIS
       0.0...
 
     """
