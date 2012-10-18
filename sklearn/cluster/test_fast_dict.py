9149b1cc125dd868bb871c5a6e9016b2bf7c93be
GaelVaroquaux <gael.varoquaux@normalesup.org>
2012-10-18 14:35:08 +0200

    ENH: speed up single linkage

--- sklearn/cluster/test_fast_dict.py
+++ sklearn/cluster/test_fast_dict.py
@@ -2,16 +2,16 @@
 """
 import numpy as np
 
-from fast_dict import IntFloatDict, MinMergeDict
+from fast_dict import IntFloatDict, min_merge
 
 keys = np.random.randint(100, size=10).astype(np.int32)
 values = np.random.random(size=10)
 
-other_keys = np.arange(50).astype(np.int32)
-other_values = .5*np.ones(50)
+#other_keys = np.arange(50).astype(np.int32)
+#other_values = .5*np.ones(50)
 
 d = IntFloatDict(keys, values)
-other = MinMergeDict(other_keys, other_values)
-
-other.update_min(d)
-
+#other = IntFloatDict(other_keys, other_values)
+#c = min_merge(d, other, mask=np.ones(100, dtype=np.int32))
+#d.append(120, 3.)
+[d.append(i+1000, 3.0) for i in xrange(2000)]
