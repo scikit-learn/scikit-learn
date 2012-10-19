bd7badb6480c9737d39cabc016827faa6d73ee57
Vlad Niculae <vlad@vene.ro>
2012-10-19 16:51:09 +0100

    Clean setup.py in metrics

--- sklearn/metrics/setup.py
+++ sklearn/metrics/setup.py
@@ -3,24 +3,19 @@ from os.path import join
 
 import numpy
 
+from sklearn._build_utils import get_blas_info
+
 
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
-    from numpy.distutils.system_info import get_info
+
     config = Configuration('metrics', parent_package, top_path)
 
     # euclidean_fast needs CBLAS
-    blas_info = get_info('blas_opt', 0)
-    if (not blas_info) or (
-        ('NO_ATLAS_INFO', 1) in blas_info.get('define_macros', [])):
-        cblas_libs = ['cblas']
-        blas_info.pop('libraries', None)
-    else:
-        cblas_libs = blas_info.pop('libraries', [])
-    libraries = []
+    cblas_libs, blas_info = get_blas_info()
+
     if os.name == 'posix':
         cblas_libs.append('m')
-        libraries.append('m')
 
     config.add_extension('_euclidean_fast',
          sources=['_euclidean_fast.c'],
@@ -31,7 +26,6 @@ def configuration(parent_package='', top_path=None):
          extra_compile_args=blas_info.pop('extra_compile_args', []),
          **blas_info
          )
-
 
     # add other directories
     config.add_subpackage('tests')
