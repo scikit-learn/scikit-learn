efb134469e986ae456ef9e08f45697ee4b256d16
cow <briancheung>
2012-10-26 04:00:33 -0400

    Modified lena segmenation example to include different embedding solvers

--- examples/cluster/plot_lena_segmentation.py
+++ examples/cluster/plot_lena_segmentation.py
@@ -9,11 +9,17 @@ partly-homogenous regions.
 
 This procedure (spectral clustering on an image) is an efficient
 approximate solution for finding normalized graph cuts.
+
+Spectral clustering with kmeans will cluster samples in the embedding space 
+with kmeans whereas discrete will iteratively search for the closest partition 
+space to the embedding space.
 """
 print __doc__
 
-# Author: Gael Varoquaux <gael.varoquaux@normalesup.org>
+# Author: Gael Varoquaux <gael.varoquaux@normalesup.org>, Brian Cheung
 # License: BSD
+
+import time
 
 import numpy as np
 import scipy as sp
@@ -41,16 +47,27 @@ graph.data = np.exp(-beta * graph.data / lena.std()) + eps
 # Apply spectral clustering (this step goes much faster if you have pyamg
 # installed)
 N_REGIONS = 11
-labels = spectral_clustering(graph, n_clusters=N_REGIONS)
-labels = labels.reshape(lena.shape)
-
+embedding_solvers = ['kmeans', 'discrete']
 ###############################################################################
 # Visualize the resulting regions
-pl.figure(figsize=(5, 5))
-pl.imshow(lena,   cmap=pl.cm.gray)
-for l in range(N_REGIONS):
-    pl.contour(labels == l, contours=1,
-            colors=[pl.cm.spectral(l / float(N_REGIONS)), ])
-pl.xticks(())
-pl.yticks(())
+pl.figure(figsize=(5, 10))
+plot_num = 1
+for embed_solve in embedding_solvers:
+    t0 = time.time()
+    labels = spectral_clustering(graph, n_clusters=N_REGIONS, embed_solve=embed_solve)
+    t1 = time.time()
+    labels = labels.reshape(lena.shape)
+
+    pl.subplot(2,1,plot_num)
+    pl.imshow(lena,   cmap=pl.cm.gray)
+    for l in range(N_REGIONS):
+        pl.contour(labels == l, contours=1,
+                colors=[pl.cm.spectral(l / float(N_REGIONS)), ])
+    pl.xticks(())
+    pl.yticks(())
+    pl.title('%s, %.2fs' % (embed_solve, (t1 - t0)))
+        
+    plot_num += 1
+        
 pl.show()
+pl.savefig('seg_compare.png')
