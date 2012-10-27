6f3ccc18555175a9920b1b1dc4839506d79f051d
Andreas Mueller <amueller@ais.uni-bonn.de>
2012-10-27 15:10:31 +0200

    DOC more extensive classifier comparision on synthetic datasets

---
+++ examples/plot_classifier_comparison.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+======================
+Classifiers Comparison
+======================
+A comparison of a several classifiers in scikit-learn on synthetic datasets.
+The point of this example is to illustrate the nature of decision boundaries
+of different classifiers.
+This should be taken with a grain of salt, as the intuition conveyed by
+these examples does not necessarily carry over to real datasets.
+
+In high dimensional spaces, data can more easily be separated linearly and
+the simplicity of classifiers such as naive Bayes and linear SVMs might lead
+to better generalization.
+
+"""
+print __doc__
+
+
+# Code source: Gael Varoqueux
+#              Andreas Mueller
+# Modified for Documentation merge by Jaques Grobler
+# License: BSD
+
+import numpy as np
+import pylab as pl
+from sklearn.datasets import make_moons, make_circles, make_blobs
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.lda import LDA
+from sklearn.qda import QDA
+
+h = .02  # step size in the mesh
+
+names = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Decision Tree",
+        "Random Forest", "Naive Bayes", "LDA", "QDA"]
+classifiers = [KNeighborsClassifier(),
+    SVC(kernel="linear", C=0.001),
+    SVC(gamma=1),
+    DecisionTreeClassifier(),
+    RandomForestClassifier(),
+    GaussianNB(),
+    LDA(),
+    QDA()
+    ]
+
+datasets = [make_moons(noise=0.1, random_state=0),
+            make_circles(noise=0.1, factor=0.5, random_state=0),
+            make_blobs(centers=2, random_state=0)]
+
+figure, all_axes = pl.subplots(len(datasets), len(classifiers))
+for ds, axes in zip(datasets, all_axes):
+    X, y = ds
+    # we create an instance of Neighbours Classifier and fit the data.
+    for name, clf, ax in zip(names, classifiers, axes):
+        clf.fit(X, y)
+
+        # Plot the decision boundary. For that, we will asign a color to each
+        # point in the mesh [x_min, m_max]x[y_min, y_max].
+        x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+        y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                np.arange(y_min, y_max, h))
+        if hasattr(clf, "decision_function"):
+            Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+        else:
+            Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+
+        # Put the result into a color plot
+        Z = Z.reshape(xx.shape)
+        ax.contourf(xx, yy, Z, cmap=pl.cm.Spectral)
+
+        # Plot also the training points
+        ax.scatter(X[:, 0], X[:, 1], c=y, cmap=pl.cm.Spectral)
+
+        ax.set_xlim(xx.min(), xx.max())
+        ax.set_ylim(yy.min(), yy.max())
+        ax.set_xticks(())
+        ax.set_yticks(())
+        ax.set_title(name)
+
+pl.show()
