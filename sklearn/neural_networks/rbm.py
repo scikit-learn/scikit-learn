4212444ba615d9fb16791a88fb5d8d3f162b1446
Lars Buitinck <larsmans@gmail.com>
2012-10-28 15:27:32 +0100

    BUG + COSMIT + ENH RBMs
    * fit must return self
    * use self.random_state instead of np.random
    * PEP8 + pyflakes cleanup
    * several micro-optimizations: np.clip, in-place ops, do less work in fit
      loop when not self.verbose

--- sklearn/neural_networks/rbm.py
+++ sklearn/neural_networks/rbm.py
@@ -1,4 +1,4 @@
-""" Restricted Boltzmann Machine
+"""Restricted Boltzmann Machine
 """
 
 # Author: Yann N. Dauphin <dauphiya@iro.umontreal.ca>
@@ -16,7 +16,7 @@ from ..utils.extmath import safe_sparse_dot
 def logistic_sigmoid(x):
     """
     Implements the logistic function.
-    
+
     Parameters
     ----------
     x: array-like, shape (M, N)
@@ -25,20 +25,20 @@ def logistic_sigmoid(x):
     -------
     x_new: array-like, shape (M, N)
     """
-    return 1. / (1. + np.exp(-np.maximum(np.minimum(x, 30), -30)))
+    return 1. / (1. + np.exp(-np.clip(x, -30, 30)))
 
 
 class RestrictedBolzmannMachine(BaseEstimator, TransformerMixin):
     """
     Restricted Boltzmann Machine (RBM)
-    
+
     A Restricted Boltzmann Machine with binary visible units and
     binary hiddens. Parameters are estimated using Stochastic Maximum
     Likelihood (SML).
-    
+
     The time complexity of this implementation is ``O(d ** 2)`` assuming
     d ~ n_features ~ n_components.
-    
+
     Parameters
     ----------
     n_components : int, optional
@@ -57,7 +57,7 @@ class RestrictedBolzmannMachine(BaseEstimator, TransformerMixin):
     random_state : RandomState or an int seed (0 by default)
         A random number generator instance to define the state of the
         random permutations generator.
-    
+
     Attributes
     ----------
     components_ : array-like, shape (n_components, n_features), optional
@@ -67,40 +67,36 @@ class RestrictedBolzmannMachine(BaseEstimator, TransformerMixin):
         Biases of the hidden units
     intercept_visible_ : array-like, shape (n_features,), optional
         Biases of the visible units
-    
+
     Examples
     --------
-    
+
     >>> import numpy as np
     >>> from sklearn.neural_networks import RestrictedBolzmannMachine
     >>> X = np.array([[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 1]])
     >>> model = RestrictedBolzmannMachine(n_components=2)
     >>> model.fit(X)
-    
+
     References
     ----------
-    
+
     [1] Hinton, G. E., Osindero, S. and Teh, Y. A fast learning algorithm for
         deep belief nets. Neural Computation 18, pp 1527-1554.
         http://www.cs.toronto.edu/~hinton/absps/fastnc.pdf
     """
-    def __init__(self, n_components=256,
-                       learning_rate=0.1,
-                       n_particles=10,
-                       n_iter=10,
-                       verbose=False,
-                       random_state=0):
+    def __init__(self, n_components=256, learning_rate=0.1, n_particles=10,
+                       n_iter=10, verbose=False, random_state=None):
         self.n_components = n_components
         self.learning_rate = learning_rate
         self.n_particles = n_particles
         self.n_iter = n_iter
         self.verbose = verbose
         self.random_state = check_random_state(random_state)
-    
+
     def _sample_binomial(self, p):
         """
         Compute the element-wise binomial using the probabilities p.
-        
+
         Parameters
         ----------
         x: array-like, shape (M, N)
@@ -115,13 +111,13 @@ class RestrictedBolzmannMachine(BaseEstimator, TransformerMixin):
         x_new: array-like, shape (M, N)
         """
         p[self.random_state.uniform(size=p.shape) < p] = 1.
-        
+
         return np.floor(p, p)
-    
+
     def transform(self, X):
         """
         Computes the probabilities ``P({\bf h}_j=1|{\bf v}={\bf X})``.
-        
+
         Parameters
         ----------
         X: array-like, shape (n_samples, n_features)
@@ -131,11 +127,11 @@ class RestrictedBolzmannMachine(BaseEstimator, TransformerMixin):
         h: array-like, shape (n_samples, n_components)
         """
         return self.mean_hiddens(X)
-    
+
     def mean_hiddens(self, v):
         """
         Computes the probabilities ``P({\bf h}_j=1|{\bf v})``.
-        
+
         Parameters
         ----------
         v: array-like, shape (n_samples, n_features)
@@ -146,98 +142,98 @@ class RestrictedBolzmannMachine(BaseEstimator, TransformerMixin):
         """
         return logistic_sigmoid(safe_sparse_dot(v, self.components_.T)
             + self.intercept_hidden_)
-    
+
     def sample_hiddens(self, v):
         """
         Sample from the distribution ``P({\bf h}|{\bf v})``.
-        
+
         Parameters
         ----------
         v: array-like, shape (n_samples, n_features)
-        
+
         Returns
         -------
         h: array-like, shape (n_samples, n_components)
         """
         return self._sample_binomial(self.mean_hiddens(v))
-    
+
     def mean_visibles(self, h):
         """
         Computes the probabilities ``P({\bf v}_i=1|{\bf h})``.
-        
+
         Parameters
         ----------
         h: array-like, shape (n_samples, n_components)
-        
+
         Returns
         -------
         v: array-like, shape (n_samples, n_features)
         """
         return logistic_sigmoid(np.dot(h, self.components_)
             + self.intercept_visible_)
-    
+
     def sample_visibles(self, h):
         """
         Sample from the distribution ``P({\bf v}|{\bf h})``.
-        
+
         Parameters
         ----------
         h: array-like, shape (n_samples, n_components)
-        
+
         Returns
         -------
         v: array-like, shape (n_samples, n_features)
         """
         return self._sample_binomial(self.mean_visibles(h))
-    
+
     def free_energy(self, v):
         """
         Computes the free energy
         ``\mathcal{F}({\bf v}) = - \log \sum_{\bf h} e^{-E({\bf v},{\bf h})}``.
-        
+
         Parameters
         ----------
         v: array-like, shape (n_samples, n_features)
-        
+
         Returns
         -------
         free_energy: array-like, shape (n_samples,)
         """
         return - np.dot(v, self.intercept_visible_) - np.log(1. + np.exp(
             safe_sparse_dot(v, self.components_.T) + self.intercept_hidden_)) \
-            .sum(1)
-    
+            .sum(axis=1)
+
     def gibbs(self, v):
         """
         Perform one Gibbs sampling step.
-        
+
         Parameters
         ----------
         v: array-like, shape (n_samples, n_features)
-        
+
         Returns
         -------
         v_new: array-like, shape (n_samples, n_features)
         """
         h_ = self.sample_hiddens(v)
         v_ = self.sample_visibles(h_)
-        
+
         return v_
-    
+
     def _fit(self, v_pos):
         """
         Adjust the parameters to maximize the likelihood of ``{\bf v}``
         using Stochastic Maximum Likelihood (SML) [1].
-        
+
         Parameters
         ----------
         v_pos: array-like, shape (n_samples, n_features)
-        
+
         Returns
         -------
         pseudo_likelihood: array-like, shape (n_samples,)
             If verbose=True, Pseudo Likelihood estimate for this batch.
-        
+
         References
         ----------
         [1] Tieleman, T. Training Restricted Boltzmann Machines using
@@ -247,43 +243,92 @@ class RestrictedBolzmannMachine(BaseEstimator, TransformerMixin):
         h_pos = self.mean_hiddens(v_pos)
         v_neg = self.sample_visibles(self.h_samples_)
         h_neg = self.mean_hiddens(v_neg)
-        
+
         lr = self.learning_rate / self.n_particles
-        self.components_ += safe_sparse_dot(lr * v_pos.T, h_pos).T
-        self.components_ -= np.dot(lr * v_neg.T, h_neg).T
+        v_pos *= lr
+        v_neg *= lr
+        self.components_ += safe_sparse_dot(v_pos.T, h_pos).T
+        self.components_ -= np.dot(v_neg.T, h_neg).T
         self.intercept_hidden_ += lr * (h_pos.sum(0) - h_neg.sum(0))
-        self.intercept_visible_ += lr * (v_pos.sum(0) - v_neg.sum(0))
-        
+        self.intercept_visible_ += (v_pos.sum(0) - v_neg.sum(0))
+
         self.h_samples_ = self._sample_binomial(h_neg)
-        
+
         if self.verbose:
             return self.pseudo_likelihood(v_pos)
-    
+
     def pseudo_likelihood(self, v):
         """
         Compute the pseudo-likelihood of ``{\bf v}``.
-        
+
         Parameters
         ----------
         v: array-like, shape (n_samples, n_features)
-        
+
         Returns
         -------
         pseudo_likelihood: array-like, shape (n_samples,)
         """
         fe = self.free_energy(v)
-        
+
         v_ = v.copy()
         i_ = self.random_state.randint(0, v.shape[1], v.shape[0])
-        v_[range(v.shape[0]), i_] = 1 - v_[range(v.shape[0]), i_]
+        v_[np.arange(v.shape[0]), i_] = 1 - v_[np.arange(v.shape[0]), i_]
         fe_ = self.free_energy(v_)
-        
+
         return v.shape[1] * np.log(logistic_sigmoid(fe_ - fe))
-    
+
     def fit(self, X, y=None):
         """
         Fit the model to the data X.
-        
+
+        Parameters
+        ----------
+        X: array-like, shape (n_samples, n_features)
+            Training data, where n_samples in the number of samples
+            and n_features is the number of features.
+
+        Returns
+        -------
+        self
+        """
+        X = array2d(X)
+
+        self.components_ = np.asarray(self.random_state.normal(0, 0.01,
+            (self.n_components, X.shape[1])), dtype=X.dtype, order='fortran')
+        self.intercept_hidden_ = np.zeros(self.n_components, dtype=X.dtype)
+        self.intercept_visible_ = np.zeros(X.shape[1], dtype=X.dtype)
+        self.h_samples_ = np.zeros((self.n_particles, self.n_components),
+            dtype=X.dtype)
+
+        inds = np.arange(X.shape[0])
+        self.random_state.shuffle(inds)
+
+        n_batches = int(np.ceil(len(inds) / float(self.n_particles)))
+
+        verbose = self.verbose
+        for iteration in xrange(self.n_iter):
+            pl = 0.
+            if verbose:
+                begin = time.time()
+            for minibatch in xrange(n_batches):
+                pl_batch = self._fit(X[inds[minibatch::n_batches]])
+
+                if verbose:
+                    pl += pl_batch.sum()
+
+            if verbose:
+                pl /= X.shape[0]
+                end = time.time()
+                print("Iteration %d, pseudo-likelihood = %.2f, time = %.2fs"
+                      % (iteration, pl, end - begin))
+
+        return self
+
+    def fit_transform(self, X, y=None):
+        """
+        Fit the model to the data X and transform it.
+
         Parameters
         ----------
         X: array-like, shape (n_samples, n_features)
@@ -291,45 +336,5 @@ class RestrictedBolzmannMachine(BaseEstimator, TransformerMixin):
             and n_features is the number of features.
         """
         X = array2d(X)
-        
-        self.components_ = np.asarray(self.random_state.normal(0, 0.01,
-            (self.n_components, X.shape[1])), dtype=X.dtype, order='fortran')
-        self.intercept_hidden_ = np.zeros(self.n_components, dtype=X.dtype)
-        self.intercept_visible_ = np.zeros(X.shape[1], dtype=X.dtype)
-        self.h_samples_ = np.zeros((self.n_particles, self.n_components),
-            dtype=X.dtype)
-        
-        inds = range(X.shape[0])
-        
-        np.random.shuffle(inds)
-        
-        n_batches = int(np.ceil(len(inds) / float(self.n_particles)))
-        
-        for iteration in range(self.n_iter):
-            pl = 0.
-            begin = time.time()
-            for minibatch in range(n_batches):
-                pl_batch = self._fit(X[inds[minibatch::n_batches]])
-                
-                if self.verbose:
-                    pl += pl_batch.sum()
-            end = time.time()
-            pl /= X.shape[0]
-            
-            if self.verbose:
-                print "Iteration %d, Pseudo-Likelihood = %.2f, time = %.2fs" \
-                    % (iteration, pl, end-begin)
-    
-    def fit_transform(self, X, y=None):
-        """
-        Fit the model to the data X and transform it.
-        
-        Parameters
-        ----------
-        X: array-like, shape (n_samples, n_features)
-            Training data, where n_samples in the number of samples
-            and n_features is the number of features.
-        """
         self.fit(X, y)
-        
         return self.transform(X)
--- sklearn/neural_networks/tests/test_rbm.py
+++ sklearn/neural_networks/tests/test_rbm.py
@@ -2,20 +2,38 @@ import numpy as np
 
 from numpy.testing import assert_almost_equal, assert_array_equal
 
+from sklearn.base import clone
 from sklearn.datasets import load_digits
 from sklearn.neural_networks import RestrictedBolzmannMachine
 
 np.seterr(all='warn')
 
+Xdigits = load_digits().data
+Xdigits -= Xdigits.min()
+Xdigits /= Xdigits.max()
+
 
 def test_fit():
-    X = load_digits().data
-
-    X -= X.min()
-    X /= X.max()
+    X = Xdigits.copy()
 
     rbm = RestrictedBolzmannMachine(n_components=64, learning_rate=0.1,
                                     n_particles=10, n_iter=7, random_state=9)
     rbm.fit(X)
 
     assert_almost_equal(rbm.pseudo_likelihood(X).mean(), -20., decimal=0)
+
+    # in-place tricks shouldn't have modified X
+    assert_array_equal(X, Xdigits)
+
+
+def test_fit_transform():
+    """Check proper implementation of fit_transform"""
+    X = Xdigits[:100]
+    rbm1 = RestrictedBolzmannMachine(n_components=16, n_particles=5,
+                                     n_iter=5, random_state=42)
+    rbm2 = clone(rbm1)
+
+    Xt1 = rbm1.fit(X).transform(X)
+    Xt2 = rbm2.fit_transform(X)
+
+    assert_array_equal(Xt1, Xt2)
