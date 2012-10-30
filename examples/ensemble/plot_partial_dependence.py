87d3246e9ead499fced9771cbf3fa28bb14ced41
Peter Prettenhofer <peter.prettenhofer@gmail.com>
2012-10-30 08:24:59 +0100

    renamed dependency -> dependence; docstring and cosmit

--- examples/ensemble/plot_partial_dependence.py
+++ examples/ensemble/plot_partial_dependence.py
@@ -1,3 +1,45 @@
+"""
+========================
+Partial Dependence Plots
+========================
+
+Partial dependence plots show the dependence between the target function [1]_
+and a set of 'target' features, marginalizing over the
+values of all other features (the complement features). Due to the limits
+of human perception the size of the target feature set must be small (usually,
+one or two) thus the target features are usually choosen among the most
+important features (see :meth:`ensemble.GradientBoostingRegressor.variable_importance_`).
+
+This example shows how to obtain partial dependence plots from a
+:class:`ensemble.GradientBoostingRegressor` trained on the California
+housing dataset. The example is taken from [HTF2009]_.
+
+The first plot shows four one-dimensional partial dependence plots. The target
+variables are: median income (`MedInc`), avg. occupants per household (`AvgOccup`),
+median house age (`HouseAge`), and avg. rooms per household (`AveRooms`).
+We can clearly see that the median house price shows a linear relationship with the
+median income (top left) and that the house price drops when the avg. occupants
+per household increases (top right).
+The lower left plot shows that the house age in a district does not have
+a strong influence on the (median) house price.
+The red tick marks on the x-axis represent the deciles of the feature values
+in the trainig data.
+
+Partial dependence plots with two target features enable us to visualize
+interactions among them. The two-variable partial dependence plot shows the
+dependence of median house price on joint values of house age and avg.
+occupants per household. We can clearly see an interaction between the
+two features:
+For an avg. occupancy greather than two, the house price is nearly independent
+of the house age, whereas for values less than two there is a strong dependence
+on age.
+
+.. [HTF2009] T. Hastie, R. Tibshirani and J. Friedman, "Elements of Statistical Learning Ed. 2", Springer, 2009.
+
+.. [1] For classification you can think of it as the regression score before the link function.
+"""
+print __doc__
+
 import numpy as np
 import pylab as pl
 import matplotlib
@@ -20,45 +62,17 @@ X_train, X_test, y_train, y_test = train_test_split(cal_housing.data,
                                                     random_state=1)
 names = cal_housing.feature_names
 
+print('_' * 80)
+print("Training GBRT...")
 clf = GradientBoostingRegressor(n_estimators=800, max_depth=4,
                                 learn_rate=0.1, loss='huber',
                                 random_state=1)
-print('_' * 80)
-print("Training GBRT...")
 clf.fit(X_train, y_train)
 print("fin.")
 
 print('_' * 80)
-print('Plot train-test deviance')
-print
-
-
-def aae(y_true, y_pred):
-    return np.mean(np.abs(y_true.ravel() - y_pred.ravel()))
-
-
-test_score = np.zeros((clf.n_estimators,), dtype=np.float64)
-for i, y_pred in enumerate(clf.staged_decision_function(X_test)):
-    test_score[i] = aae(y_test, y_pred)
-
-train_score = np.zeros((clf.n_estimators,), dtype=np.float64)
-for i, y_pred in enumerate(clf.staged_decision_function(X_train)):
-    train_score[i] = aae(y_train, y_pred)
-
-pl.figure()
-pl.title('Training and Test Absolute Error')
-pl.plot(np.arange(clf.n_estimators) + 1, train_score, 'b-',
-        label='Train error')
-pl.plot(np.arange(clf.n_estimators) + 1, test_score, 'r-',
-        label='Test error')
-pl.legend(loc='upper right')
-pl.xlabel('Boosting Iterations')
-pl.ylabel('Deviance')
-
-print('_' * 80)
 print('One-way partial dependence plots')
 print
-
 pl.figure()
 sub_plots = []
 
@@ -68,7 +82,7 @@ for i, fx in enumerate([0, 5, 1, 2]):
 
     ax = pl.subplot(2, 2, i + 1)
 
-    # plot partial dependency
+    # plot partial dependence
     pdp, (axis,) = gradient_boosting.partial_dependency(clf, target_feature,
                                                         X=X_train)
     ax.plot(axis, pdp.ravel(), 'g-')
@@ -80,7 +94,7 @@ for i, fx in enumerate([0, 5, 1, 2]):
     ax.vlines(deciles, 0.0, 0.05, transform=trans, color='red')
 
     pl.xlabel(name)
-    pl.ylabel('Partial Dependency')
+    pl.ylabel('Partial Dependence')
 
     sub_plots.append(ax)
 
@@ -89,31 +103,24 @@ y_min = min((ax.get_ylim()[0] for ax in sub_plots))
 y_max = max((ax.get_ylim()[1] for ax in sub_plots))
 for ax in sub_plots:
     ax.set_ylim((y_min, y_max))
-
 pl.tight_layout()
 
 print('_' * 80)
 print('Two-way partial dependence plot')
 print
-
 fig = pl.figure()
-
 target_feature = np.array([1, 5], dtype=np.int32)
 pdp, (x_axis, y_axis) = gradient_boosting.partial_dependency(clf,
                                                              target_feature,
                                                              X=X_train,
                                                              grid_resolution=50)
 XX, YY = np.meshgrid(x_axis, y_axis)
-
 Z = pdp.T.reshape(XX.shape).T
 ax = Axes3D(fig)
 surf = ax.plot_surface(XX, YY, Z, rstride=1, cstride=1, cmap=pl.cm.BuPu)
-
 ax.set_xlabel(names[target_feature[0]])
 ax.set_ylabel(names[target_feature[1]])
-ax.set_zlabel('Partial dependency')
-
+ax.set_zlabel('Partial dependence')
 #  pretty init view
 ax.view_init(elev=22, azim=122)
-
 pl.colorbar(surf)
