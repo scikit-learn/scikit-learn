0530c106c8250605b5874ee6cf5648e25e3bfc20
Peter Prettenhofer <peter.prettenhofer@gmail.com>
2012-11-10 12:59:46 +0100

    pep8

--- sklearn/utils/extmath.py
+++ sklearn/utils/extmath.py
@@ -414,9 +414,9 @@ def cartesian(arrays, out=None):
         out = np.zeros([n, len(arrays)], dtype=dtype)
 
     m = n / arrays[0].size
-    out[:,0] = np.repeat(arrays[0], m)
+    out[:, 0] = np.repeat(arrays[0], m)
     if arrays[1:]:
         cartesian(arrays[1:], out=out[0:m, 1:])
         for j in xrange(1, arrays[0].size):
-            out[j*m:(j+1)*m,1:] = out[0:m, 1:]
+            out[j * m:(j + 1) * m, 1:] = out[0:m, 1:]
     return out
