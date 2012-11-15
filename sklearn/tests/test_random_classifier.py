7358fd4c304a9dbf61e07c2dd78ba3fecc30ac7a
Mathieu Blondel <mathieu@mblondel.org>
2012-11-16 00:08:36 +0900

    PEP8.

--- sklearn/tests/test_random_classifier.py
+++ sklearn/tests/test_random_classifier.py
@@ -6,15 +6,16 @@ from sklearn.random_classifier import RandomClassifier
 
 
 def test_most_frequent_sampling():
-    X = [[0], [0], [0], [0]] # ignored
+    X = [[0], [0], [0], [0]]  # ignored
     y = [1, 2, 1, 1]
 
     clf = RandomClassifier(sampling="most_frequent", random_state=0)
     clf.fit(X, y)
     assert_array_equal(clf.predict(X), np.ones(len(y)))
 
+
 def test_stratified_sampling():
-    X = [[0], [0], [0], [0], [0]] # ignored
+    X = [[0], [0], [0], [0], [0]]  # ignored
     y = [1, 2, 1, 1, 2]
 
     clf = RandomClassifier(sampling="stratified", random_state=0)
@@ -23,10 +24,9 @@ def test_stratified_sampling():
 
 
 def test_uniform_sampling():
-    X = [[0], [0], [0], [0]] # ignored
+    X = [[0], [0], [0], [0]]  # ignored
     y = [1, 2, 1, 1]
 
     clf = RandomClassifier(sampling="uniform", random_state=0)
     clf.fit(X, y)
     assert_array_equal(clf.predict(X), [1, 2, 2, 1])
-
