72c62f837b80bf4b4f21fa4e71a5a27ed3e3563d
Lars Buitinck <L.J.Buitinck@uva.nl>
2012-11-23 17:47:20 +0100

    move MLPClassifier to sklearn.neural

--- sklearn/setup.py
+++ sklearn/setup.py
@@ -36,7 +36,7 @@ def configuration(parent_package='', top_path=None):
     config.add_subpackage('gaussian_process')
     config.add_subpackage('gaussian_process/tests')
     config.add_subpackage('neighbors')
-    config.add_subpackage('neural_networks')
+    config.add_subpackage('neural')
     config.add_subpackage('manifold')
     config.add_subpackage('metrics')
     config.add_subpackage('mixture')
