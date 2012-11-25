f47ff8579e01e1c7ba0365ad6593e87f71131b54
Peter Prettenhofer <peter.prettenhofer@gmail.com>
2012-11-25 23:20:40 +0100

    show how to use ``partial_dependence`` to generate custom plots.

--- examples/ensemble/plot_partial_dependence.py
+++ examples/ensemble/plot_partial_dependence.py
@@ -14,19 +14,22 @@ This example shows how to obtain partial dependence plots from a
 :class:`~sklearn.ensemble.GradientBoostingRegressor` trained on the California
 housing dataset. The example is taken from [HTF2009]_.
 
-The first plot shows four one-dimensional partial dependence plots. The target
-variables are: median income (`MedInc`), avg. occupants per household (`AvgOccup`),
+The plot shows four one-way and one two-way partial dependence plots.
+The target variables for the one-way PDP are:
+median income (`MedInc`), avg. occupants per household (`AvgOccup`),
 median house age (`HouseAge`), and avg. rooms per household (`AveRooms`).
+
 We can clearly see that the median house price shows a linear relationship
 with the median income (top left) and that the house price drops when the
-avg. occupants per household increases (top right).
-The lower left plot shows that the house age in a district does not have
-a strong influence on the (median) house price.
-The red tick marks on the x-axis represent the deciles of the feature values
+avg. occupants per household increases (top middle).
+The top right plot shows that the house age in a district does not have
+a strong influence on the (median) house price; so does the average rooms
+per household.
+The tick marks on the x-axis represent the deciles of the feature values
 in the trainig data.
 
 Partial dependence plots with two target features enable us to visualize
-interactions among them. The two-variable partial dependence plot shows the
+interactions among them. The two-way partial dependence plot shows the
 dependence of median house price on joint values of house age and avg.
 occupants per household. We can clearly see an interaction between the
 two features:
@@ -42,14 +45,13 @@ print __doc__
 
 import numpy as np
 import pylab as pl
-import matplotlib
 
 from mpl_toolkits.mplot3d import Axes3D
-from scipy.stats.mstats import mquantiles
 
 from sklearn.cross_validation import train_test_split
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.ensemble.partial_dependence import partial_dependence_plots
+from sklearn.ensemble.partial_dependence import partial_dependence
 from sklearn.datasets.california_housing import fetch_california_housing
 
 # fetch California housing dataset
@@ -64,76 +66,43 @@ names = cal_housing.feature_names
 
 print('_' * 80)
 print("Training GBRT...")
-clf = GradientBoostingRegressor(n_estimators=80, max_depth=4,
+clf = GradientBoostingRegressor(n_estimators=800, max_depth=4,
                                 learning_rate=0.1, loss='huber',
                                 random_state=1)
 clf.fit(X_train, y_train)
 print("done.")
 
 print('_' * 80)
-print('One-way partial dependence plots')
+print('Convenience plot with ``partial_dependence_plots``')
 print
 
 features = [0, 5, 1, 2, (5, 1)]
+fig, axs = partial_dependence_plots(clf, X_train, features, feature_names=names,
+                                    n_jobs=3, grid_resolution=50)
+fig.suptitle('Partial dependence of house value on nonlocation features\n' \
+             'for the California housing dataset')
+pl.subplots_adjust(top=0.9)  # tight_layout causes overlap with suptitle
 
-partial_dependence_plots(clf, X_train, features, feature_names=names,
-                         n_jobs=3, verbose=3)
+print('_' * 80)
+print('Custom 3d plot via ``partial_dependence``')
+print
+fig = pl.figure()
+
+target_feature = (1, 5)
+pdp, (x_axis, y_axis) = partial_dependence(clf, target_feature,
+                                           X=X_train, grid_resolution=50)
+XX, YY = np.meshgrid(x_axis, y_axis)
+Z = pdp.T.reshape(XX.shape).T
+ax = Axes3D(fig)
+surf = ax.plot_surface(XX, YY, Z, rstride=1, cstride=1, cmap=pl.cm.BuPu)
+ax.set_xlabel(names[target_feature[0]])
+ax.set_ylabel(names[target_feature[1]])
+ax.set_zlabel('Partial dependence')
+#  pretty init view
+ax.view_init(elev=22, azim=122)
+pl.colorbar(surf)
+pl.suptitle('Partial dependence of house value on median age and ' \
+            'average occupancy')
+pl.subplots_adjust(top=0.9)
+
 pl.show()
-
-## sub_plots = []
-## fig = pl.figure()
-## for i, fx in enumerate([0, 5, 1, 2]):
-##     name = names[fx]
-##     target_feature = np.array([fx], dtype=np.int32)
-
-##     ax = pl.subplot(2, 2, i + 1)
-
-##     # plot partial dependence
-##     pdp, (axis,) = gradient_boosting.partial_dependence(clf, target_feature,
-##                                                         X=X_train)
-##     ax.plot(axis, pdp.ravel(), 'g-')
-
-##     # plot data deciles
-##     deciles = mquantiles(X_train[:, fx], prob=np.arange(0.1, 1.0, 0.1))
-##     trans = matplotlib.transforms.blended_transform_factory(ax.transData,
-##                                                             ax.transAxes)
-##     ax.vlines(deciles, 0.0, 0.05, transform=trans, color='red')
-
-##     pl.xlabel(name)
-##     pl.ylabel('Partial Dependence')
-
-##     sub_plots.append(ax)
-
-## # set common ylim
-## y_min = min((ax.get_ylim()[0] for ax in sub_plots))
-## y_max = max((ax.get_ylim()[1] for ax in sub_plots))
-## for ax in sub_plots:
-##     ax.set_ylim((y_min, y_max))
-
-## fig.suptitle('Partial dependence of house value on nonlocation features ' \
-##              'for the California housing dataset')
-
-## print('_' * 80)
-## print('Two-way partial dependence plot')
-## print
-## fig = pl.figure()
-
-
-## target_feature = np.array([1, 5], dtype=np.int32)
-## pdp, (x_axis, y_axis) = gradient_boosting.partial_dependence(clf,
-##                                                              target_feature,
-##                                                              X=X_train,
-##                                                              grid_resolution=50)
-## XX, YY = np.meshgrid(x_axis, y_axis)
-## Z = pdp.T.reshape(XX.shape).T
-## ax = Axes3D(fig)
-## surf = ax.plot_surface(XX, YY, Z, rstride=1, cstride=1, cmap=pl.cm.BuPu)
-## ax.set_xlabel(names[target_feature[0]])
-## ax.set_ylabel(names[target_feature[1]])
-## ax.set_zlabel('Partial dependence')
-## #  pretty init view
-## ax.view_init(elev=22, azim=122)
-## pl.colorbar(surf)
-## pl.suptitle('Partial dependence of house value on median age and ' \
-##             'average occupancy')
-## pl.show()
