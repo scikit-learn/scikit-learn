b3906dbdf9d72350450345d62cea2d04a158ea34
carbon <carbon@trusty.(none)>
2012-12-02 15:11:01 -0500

    Sync with upstream

--- examples/cluster/plot_lena_segmentation.py
+++ examples/cluster/plot_lena_segmentation.py
@@ -10,18 +10,12 @@ partly-homogenous regions.
 This procedure (spectral clustering on an image) is an efficient
 approximate solution for finding normalized graph cuts.
 
-<<<<<<< HEAD
-Spectral clustering with kmeans will cluster samples in the embedding space 
-with kmeans whereas discrete will iteratively search for the closest partition 
-space to the embedding space.
-=======
 There are two options to assign labels:
 
 * with 'kmeans' spectral clustering will cluster samples in the embedding space
   using a kmeans algorithm
 * whereas 'discrete' will iteratively search for the closest partition
   space to the embedding space.
->>>>>>> upstream/master
 """
 print __doc__
 
@@ -56,20 +50,6 @@ graph.data = np.exp(-beta * graph.data / lena.std()) + eps
 # Apply spectral clustering (this step goes much faster if you have pyamg
 # installed)
 N_REGIONS = 11
-<<<<<<< HEAD
-embedding_solvers = ['kmeans', 'discrete']
-###############################################################################
-# Visualize the resulting regions
-pl.figure(figsize=(5, 10))
-plot_num = 1
-for embed_solve in embedding_solvers:
-    t0 = time.time()
-    labels = spectral_clustering(graph, n_clusters=N_REGIONS, embed_solve=embed_solve)
-    t1 = time.time()
-    labels = labels.reshape(lena.shape)
-
-    pl.subplot(2,1,plot_num)
-=======
 
 ###############################################################################
 # Visualize the resulting regions
@@ -83,21 +63,12 @@ for assign_labels in ('kmeans', 'discretize'):
     labels = labels.reshape(lena.shape)
 
     pl.figure(figsize=(5, 5))
->>>>>>> upstream/master
     pl.imshow(lena,   cmap=pl.cm.gray)
     for l in range(N_REGIONS):
         pl.contour(labels == l, contours=1,
                 colors=[pl.cm.spectral(l / float(N_REGIONS)), ])
     pl.xticks(())
     pl.yticks(())
-<<<<<<< HEAD
-    pl.title('%s, %.2fs' % (embed_solve, (t1 - t0)))
-        
-    plot_num += 1
-        
-pl.show()
-=======
     pl.title('Spectral clustering: %s, %.2fs' % (assign_labels, (t1 - t0)))
 
 pl.show()
->>>>>>> upstream/master
