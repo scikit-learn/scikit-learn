add00fa8c9986664637eb5bee235ce9877699c35
Arnaud Joly <arnaud.joly@yahoo.com>
2012-12-03 11:39:06 +0100

    Clean random_dot features

--- sklearn/random_projection.py
+++ sklearn/random_projection.py
@@ -214,145 +214,6 @@ def sparse_random_matrix(n_components, n_features, density='auto',
     return math.sqrt(1 / density) / math.sqrt(n_components) * r
 
 
-def random_dot(A, n_components, density='auto', random_state=None,
-               dense_output=False, out=None):
-    """Implicit dot product by a random sparse matrix
-
-    Calling this function is equivalent (up to a random seed shift) to::
-
-        safe_sparse_dot(A, sparse_random_matrix(n_features, n_components)
-
-    The difference is that random matrix is never fully allocated in
-    memory but instead generated on the fly using a hash function.
-
-    Parameters
-    ==========
-    A: array or sparse matrix with shape (n_samples, n_features)
-        The input data to randomly project.
-
-    n_components: int
-        The dimension of the target space.
-
-    density: "auto" or float in range (0, 1/3)
-        The density (ratio of non-zeros) of the simulated sparse random matrix.
-
-    random_state: RandomState instance, int or None
-        The PRNG used to seed the hash functions.
-
-    dense_ouput: boolean, False by default
-        Force the output to be a dense numpy array even if the input is sparse.
-
-    out: array or None
-        Preallocated numpy ndarray to store the result.
-
-    Returns
-    =======
-    out: array or scipy sparse COO matrix
-        Result of the dot product of A by a sparse random matrix.
-    """
-    if n_components <= 0:
-        raise ValueError("n_components must be strictly positive, got %d" %
-                         n_components)
-    half_uint32_max = np.iinfo(np.uint32).max / 2
-
-    # TODO: avoid conversion of sparse matrices to CSR by adding support for
-    # other formats? Especially CSC would be interesting as faster to hash
-    # feature-wise.
-    A = atleast2d_or_csr(A)
-    n_samples, n_features = A.shape
-    density = _check_density(density, n_features)
-
-    if out is not None:
-        dense_output = True
-        if out.shape != (n_samples, n_components):
-            raise ValueError("out should have shape (%d, %d), got %r" %
-                             (n_samples, n_components, out.shape))
-        out.fill(0)
-
-    # find the number of non-zero component in the random matrix
-    # to simulate for each feature
-    random_state = check_random_state(random_state)
-    seed = random_state.randint(np.iinfo(np.int32).max)
-    nonzeros = random_state.binomial(
-        n_components, density, size=n_features)
-
-    # pre-allocate an array of indices to be sliced and hashed to find the
-    # actual non zero component indices for each feature
-    base = np.arange(n_components, dtype=np.int32)
-
-    if not sp.issparse(A):
-        if out is None:
-            out = np.zeros((n_samples, n_components), A.dtype)
-        for feature_idx in xrange(n_features):
-            # use the hash function to retrieve the indices of the
-            # components in the random matrix that have a non-zero value
-            # for the current feature
-            h = murmurhash3_32(base[:nonzeros[feature_idx]],
-                               seed=feature_idx ^ seed, positive=True)
-            nnz_components = h % n_components
-
-            # TODO: find a vectorized implementation that is faster than the
-            # for loop or refactor as cython code?
-            for component_idx in nnz_components[h < half_uint32_max]:
-                out[:, component_idx] += A[:, feature_idx]
-
-            for component_idx in nnz_components[h >= half_uint32_max]:
-                out[:, component_idx] -= A[:, feature_idx]
-
-    elif sp.isspmatrix_csr(A) and dense_output:
-        if out is None:
-            out = np.zeros((n_samples, n_components), A.dtype)
-        for sample_idx in xrange(n_samples):
-            for k in xrange(A.indptr[sample_idx], A.indptr[sample_idx + 1]):
-                feature_idx = A.indices[k]
-                h = murmurhash3_32(base[:nonzeros[feature_idx]],
-                                   seed=feature_idx ^ seed, positive=True)
-                nnz_components = h % n_components
-                for component_idx in nnz_components[h < half_uint32_max]:
-                    out[sample_idx, component_idx] += A.data[k]
-
-                for component_idx in nnz_components[h >= half_uint32_max]:
-                    out[sample_idx, component_idx] -= A.data[k]
-
-    elif sp.isspmatrix_csr(A) and not dense_output:
-        # TODO: rewrite the following nested for loops in cython
-        # TODO: using python (unboxed) lists can have large memory overhead
-        # w.r.t. extendable numpy arrays with a fixed dtype
-        values = []
-        rows = []
-        cols = []
-        for sample_idx in xrange(n_samples):
-            for k in xrange(A.indptr[sample_idx], A.indptr[sample_idx + 1]):
-                feature_idx = A.indices[k]
-                h = murmurhash3_32(base[:nonzeros[feature_idx]],
-                                   seed=feature_idx ^ seed, positive=True)
-                nnz_components = h % n_components
-
-                rows.extend([sample_idx] * nnz_components.shape[0])
-                for component_idx in nnz_components[h < half_uint32_max]:
-                    values.append(A.data[k])
-                    cols.append(component_idx)
-
-                for component_idx in nnz_components[h >= half_uint32_max]:
-                    values.append(-A.data[k])
-                    cols.append(component_idx)
-
-        out = sp.coo_matrix((values, (rows, cols)),
-                            shape=(n_samples, n_components))
-
-    # TODO: implement support for CSC here
-
-    else:
-        raise TypeError(
-            "random_dot is not supported for input with type %s "
-            "and dense_output=%r" % (type(A), dense_output))
-
-    weight = math.sqrt(1 / density) / math.sqrt(n_components)
-    # inplace modification of out necessary if out was passed as an argument
-    out *= weight
-    return out
-
-
 class SparseRandomProjection(BaseEstimator, TransformerMixin):
     """Transformer to reduce the dimensionality with sparse random projection
 
@@ -393,21 +254,6 @@ class SparseRandomProjection(BaseEstimator, TransformerMixin):
 
         Use density = 1 / 3.0 if you want to reproduce the results from
         Achlioptas, 2001.
-
-    materialize : boolean, optional, True by default
-        If materialize is True a CSR sparse matrix is allocated in
-        memory. If false, the dot product by the sparse matrix is
-        implicitly simulated using a family of hash functions.
-
-        ``materialize == True`` will incur a slower fit time and allocate
-        memory proportionally to ``n_components * n_features * density``
-        but on the other hand ``predict`` will be computed faster.
-
-        ``materialize == False`` will incur slower ``predict`` calls
-        but will not allocate any additional memory during fit.
-
-        In practice, ``materialize == False`` is interesting for high-dim
-        sparse data with ``n_components >> 10000``.
 
     eps : strictly positive float, optional, default 0.1
         Parameter to control the quality of the embedding according to
@@ -505,15 +351,10 @@ class SparseRandomProjection(BaseEstimator, TransformerMixin):
 
         self.density_ = _check_density(self.density, n_features)
 
-        if self.materialize:
-            self.components_ = sparse_random_matrix(
-                self.n_components_, n_features, density=self.density,
-                random_state=self.random_state)
-        else:
-            # fix the seed to be reused at each call to transform (must be
-            # frozen once and for all to simulate the same random matrix
-            # across repeated calls)
-            self.seed_ = self.random_state.randint(np.iinfo(np.int32).max)
+        self.components_ = sparse_random_matrix(
+            self.n_components_, n_features, density=self.density,
+            random_state=self.random_state)
+
         return self
 
     def transform(self, X, y=None):
@@ -534,10 +375,6 @@ class SparseRandomProjection(BaseEstimator, TransformerMixin):
         """
         if not sp.issparse(X):
             X = np.atleast_2d(X)
-        if self.materialize:
-            return safe_sparse_dot(X, self.components_.T,
-                                   dense_output=self.dense_output)
-        else:
-            return random_dot(X, self.n_components_, self.density_,
-                              random_state=self.seed_,
-                              dense_output=self.dense_output)
+
+        return safe_sparse_dot(X, self.components_.T,
+                               dense_output=self.dense_output)
