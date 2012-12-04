662869d8a7519826533430f438e6a9bed414139e
Arnaud Joly <arnaud.joly@yahoo.com>
2012-12-04 17:30:14 +0100

    ENH Add gaussian projeciton + refactor sparse random matrix to reuse code

--- sklearn/random_projection.py
+++ sklearn/random_projection.py
@@ -31,15 +31,18 @@ import math
 import random
 
 import numpy as np
+from numpy.testing import assert_equal
 import scipy.sparse as sp
 
 from sklearn.utils import check_random_state
-from sklearn.utils import murmurhash3_32
-from sklearn.utils import atleast2d_or_csr
 from sklearn.utils.extmath import safe_sparse_dot
 from sklearn.base import BaseEstimator
 from sklearn.base import TransformerMixin
-from sklearn.utils.testing import assert_equal
+
+__all__ = ["BernouilliRandomProjection",
+           "GaussianRandomProjection",
+           "johnson_lindenstrauss_min_dim"]
+
 
 def johnson_lindenstrauss_min_dim(n_samples, eps=0.1):
     """Find a 'safe' number of components to randomly project to
@@ -103,8 +106,57 @@ def _check_density(density, n_features):
     return density
 
 
-def sparse_random_matrix(n_components, n_features, density='auto',
-                         random_state=None):
+def _check_input_size(n_components, n_features):
+    """Factorize argument checking for random matrix generation"""
+    if n_components <= 0:
+        raise ValueError("n_components must be strictly positive, got %d" %
+                         n_components)
+    if n_features <= 0:
+        raise ValueError("n_features must be strictly positive, got %d" %
+                         n_components)
+
+
+def gaussian_random_matrix(n_components, n_features, random_state=None):
+    """ Generate a dense gaussian random matrix.
+
+    The component of the random matrix is drawn from N(0, 1.0 / n_components).
+
+    Parameters
+    ----------
+    n_components: int,
+        Dimensionality of the target projection space.
+
+    n_features: int,
+        Dimensionality of the original source space.
+
+    random_state : int, RandomState instance or None (default)
+        Control the pseudo random number generator used to generate the
+        matrix at fit time.
+
+    Examples
+    --------
+      >>> import numpy as np
+      >>> from sklearn.random_projection import gaussian_random_matrix
+
+      >>> n_components, n_features = 10, 10000
+
+      >>> r = gaussian_random_matrix(n_components, n_features, random_state=0)
+
+
+    See Also
+    --------
+    bernouilli_random_matrix
+    """
+    _check_input_size(n_components, n_features)
+    rng = check_random_state(random_state)
+    r = rng.normal(loc=0.0,
+                   scale=1.0 / np.sqrt(n_components),
+                   size=(n_components, n_features))
+    return r
+
+
+def bernouilli_random_matrix(n_components, n_features, density='auto',
+                             random_state=None):
     """Generalized Achlioptas random sparse matrix for random projection
 
     Setting density to 1/3 will yield the original matrix by Dimitris
@@ -119,10 +171,10 @@ def sparse_random_matrix(n_components, n_features, density='auto',
 
     Parameters
     ----------
-    n_components: int
+    n_components: int,
         Dimensionality of the target projection space.
 
-    n_features: int
+    n_features: int,
         Dimensionality of the original source space.
 
     density: float in range (0, 1/3], optional
@@ -133,6 +185,16 @@ def sparse_random_matrix(n_components, n_features, density='auto',
 
         Use density = 1 / 3.0 if you want to reproduce the results from
         Achlioptas, 2001.
+
+        Use density = 1 if you want to get a dense Bernouilli random matrix.
+
+    random_state : integer, RandomState instance or None (default)
+        Control the pseudo random number generator used to generate the
+        matrix at fit time.
+
+    See Also
+    --------
+    gaussian_random_matrix
 
     Examples
     --------
@@ -168,24 +230,23 @@ def sparse_random_matrix(n_components, n_features, density='auto',
       http://www.cs.ucsc.edu/~optas/papers/jl.pdf
 
     """
-    if n_components <= 0:
-        raise ValueError("n_components must be strictly positive, got %d" %
-                         n_components)
+    _check_input_size(n_components, n_features)
+    density = _check_density(density, n_features)
+
     # seed numpy and python pseudo random number generators from one another
     # to ensure reproducible executions
-    random_state = check_random_state(random_state)
-    py_random_state = random.Random(random_state.rand())
-    density = _check_density(density, n_features)
+    rng = check_random_state(random_state)
 
     # placeholders for the CSR datastructure
     indices = []
     data = []
     offset = 0
     indptr = [offset]
+    py_random_state = random.Random(rng.rand())
 
     for i in xrange(n_components):
         # find the indices of the non-zero components for row i
-        n_nonzero_i = random_state.binomial(n_features, density)
+        n_nonzero_i = rng.binomial(n_features, density)
 
         # Use the python rng to perform reservoir sampling without
         # replacement and without exhausting the memory.
@@ -201,7 +262,7 @@ def sparse_random_matrix(n_components, n_features, density='auto',
 
         # among non zero components the probability of the sign is 50%/50%
         data_i = np.ones(n_nonzero_i, dtype=np.float)
-        u = random_state.uniform(size=n_nonzero_i)
+        u = rng.uniform(size=n_nonzero_i)
         data_i[u < 0.5] *= -1
         data.append(data_i)
         offset += n_nonzero_i
@@ -211,21 +272,204 @@ def sparse_random_matrix(n_components, n_features, density='auto',
     data, indices = np.concatenate(data), np.concatenate(indices)
     r = sp.csr_matrix((data, indices, indptr),
                       shape=(n_components, n_features))
+
     return math.sqrt(1 / density) / math.sqrt(n_components) * r
 
 
-class SparseRandomProjection(BaseEstimator, TransformerMixin):
-    """Transformer to reduce the dimensionality with sparse random projection
+class BaseRandomProjection(BaseEstimator, TransformerMixin):
+    """Base class for random projections.
 
-    Sparse random matrix is an alternative to the dense Gaussian
-    random matrix that guarantees similar embedding quality while being
+    Warning: This class should not be used directly.
+    Use derived classes instead.
+    """
+    def __init__(self, n_components='auto', density='auto', eps=0.5,
+                 dense_output=False, random_state=None,
+                 distribution="bernouilli"):
+        self.n_components = n_components
+        self.density = density
+        self.eps = eps
+        self.dense_output = dense_output
+        self.random_state = random_state
+        self.distribution = distribution
+
+        self.components_ = None
+        self.n_components_ = None
+
+    def fit(self, X, y=None):
+        """Generate a sparse random projection matrix
+
+        Parameters
+        ----------
+        X : numpy array or scipy.sparse of shape [n_samples, n_features]
+            Training set: only the shape is used to find optimal random
+            matrix dimensions based on the theory referenced in the
+            afore mentioned papers.
+
+        y : is not used: placeholder to allow for usage in a Pipeline.
+
+        Returns
+        -------
+        self
+
+        """
+        if not sp.issparse(X):
+            X = np.atleast_2d(X)
+        self.random_state = check_random_state(self.random_state)
+        n_samples, n_features = X.shape
+
+        if self.n_components == 'auto':
+            self.n_components_ = johnson_lindenstrauss_min_dim(
+                n_samples, eps=self.eps)
+
+            if self.n_components_ <= 0:
+                raise ValueError(
+                    'eps=%f and n_samples=%d lead to a target dimension of '
+                    '%d which is invalid' % (
+                        self.eps, n_samples, self.n_components_))
+            elif self.n_components_ > n_features:
+                raise ValueError(
+                    'eps=%f and n_samples=%d lead to a target dimension of '
+                    '%d which is larger than the original space with '
+                    'n_features=%d' % (self.eps, n_samples, self.n_components_,
+                                       n_features))
+        else:
+            if self.n_components <= 0:
+                raise ValueError("n_components must be greater than 0, got %s"
+                                 % self.n_components_)
+            self.n_components_ = self.n_components
+
+        if self.distribution == "bernouilli":
+            self.density_ = _check_density(self.density, n_features)
+            self.components_ = bernouilli_random_matrix(
+                self.n_components_, n_features, density=self.density,
+                random_state=self.random_state)
+
+        elif self.distribution == "gaussian":
+            if self.density != "auto" and self.density != 1.0:
+                raise ValueError("Density should be equal to one for Gaussian"
+                                 " random projection.")
+
+            self.components_ = gaussian_random_matrix(self.n_components_,
+                n_features, random_state=self.random_state)
+
+        else:
+            raise ValueError('Unknown specified distribution.')
+
+        assert_equal(
+            self.components_.shape,
+            (self.n_components_, n_features),
+            err_msg=('An error has occured the projection matrix has not '
+                 'the proper shape.'))
+
+        return self
+
+    def transform(self, X, y=None):
+        """Project the data by using matrix product with the random matrix
+
+        Parameters
+        ----------
+        X : numpy array or scipy.sparse of shape [n_samples, n_features]
+            The input data to project into a smaller dimensional space.
+
+        y : is not used: placeholder to allow for usage in a Pipeline.
+
+        Returns
+        -------
+        X_new : numpy array or scipy sparse of shape [n_samples, n_components]
+            Projected array.
+
+        """
+        if self.components_ is None:
+            raise ValueError('No random projection matrix had been fit.')
+
+        if X.shape[1] != self.components_.shape[1]:
+            raise ValueError('Impossible to perform projection:'
+            'X at fit stage had a different number of features.'
+            '(%s != %s)' % (X.shape[1], self.components_.shape[1]))
+
+        if not sp.issparse(X):
+            X = np.atleast_2d(X)
+
+        return safe_sparse_dot(X, self.components_.T,
+                               dense_output=self.dense_output)
+
+
+class GaussianRandomProjection(BaseRandomProjection):
+    """Transformer to reduce the dimensionality with Gaussian random
+    projection.
+
+    The components of the random matrix are drawn from
+        N(0, 1.0 / n_components).
+
+    Parameters
+    ----------
+    n_components : int or 'auto', optional
+        Dimensionality of the target projection space.
+
+        n_components can be automatically adjusted according to the
+        number of samples in the dataset and the bound given by the
+        Johnson Lindenstrauss lemma. In that case the quality of the
+        embedding is controlled by the ``eps`` parameter.
+
+        It should be noted that Johnson Lindenstrauss lemma can yield
+        very conservative estimated of the required number of components
+        as it makes no assumption on the structure of the dataset.
+
+    density : float in range (0, 1], optional
+        Ratio of non-zero component in the random projection matrix.
+
+        By default the value is set to the minimum density as recommended
+        by Ping Li et al.: 1 / sqrt(n_features)
+
+        Use density = 1 / 3.0 if you want to reproduce the results from
+        Achlioptas, 2001.
+
+        Use density = 1 if you want to get a dense Bernouilli random matrix.
+
+    eps : strictly positive float, optional, default 0.1
+        Parameter to control the quality of the embedding according to
+        the Johnson-Lindenstrauss lemma when n_components is set to
+        'auto'.
+
+        Smaller values lead to better embedding and higher number of
+        dimensions (n_components) in the target projection space.
+
+    random_state : integer, RandomState instance or None (default)
+        Control the pseudo random number generator used to generate the
+        matrix at fit time.
+
+    Attributes
+    ----------
+    n_component_: int
+        Concrete number of components computed when n_components="auto".
+
+    components_: CSR matrix with shape [n_components, n_features]
+        Random matrix used for the projection.
+
+    """
+    def __init__(self, n_components='auto', eps=0.5, random_state=None):
+        super(GaussianRandomProjection, self).__init__(
+            n_components=n_components,
+            density=1.0,
+            eps=eps,
+            dense_output=True,
+            random_state=random_state,
+            distribution="gaussian")
+
+
+class BernouilliRandomProjection(BaseRandomProjection):
+    """Transformer to reduce the dimensionality with bernouilli random
+    projection.
+
+    Bernouilli random matrix is an alternative to Gaussian projection.
+
+    Sparse bernouilli random matrix is an alternative to dense random
+    projection matrix that guarantees similar embedding quality while being
     much more memory efficient and allowing faster computation of the
     projected data.
 
-    The implementation uses either a random CSR matrix defined by a
-    hashing function and a seed.
-
-    If we note `s = 1 / density` the components of the random matrix are:
+    If we note `s = 1 / density` the components of the random matrix are
+    drawn from:
 
       - -sqrt(s) / sqrt(n_components)   with probability 1 / 2s
       -  0                              with probability 1 - 1 / s
@@ -253,6 +497,8 @@ class SparseRandomProjection(BaseEstimator, TransformerMixin):
 
         Use density = 1 / 3.0 if you want to reproduce the results from
         Achlioptas, 2001.
+
+        Use density = 1 if you want to get a dense Bernouilli random matrix.
 
     eps : strictly positive float, optional, default 0.1
         Parameter to control the quality of the embedding according to
@@ -298,96 +544,12 @@ class SparseRandomProjection(BaseEstimator, TransformerMixin):
       http://www.cs.ucsc.edu/~optas/papers/jl.pdf
 
     """
-
     def __init__(self, n_components='auto', density='auto', eps=0.5,
                  dense_output=False, random_state=None):
-        self.n_components = n_components
-        self.density = density
-        self.eps = eps
-        self.dense_output = dense_output
-        self.random_state = random_state
-
-    def fit(self, X, y=None):
-        """Generate a sparse random projection matrix
-
-        Parameters
-        ----------
-        X : numpy array or scipy.sparse of shape [n_samples, n_features]
-            Training set: only the shape is used to find optimal random
-            matrix dimensions based on the theory referenced in the
-            afore mentioned papers.
-
-        y : is not used: placeholder to allow for usage in a Pipeline.
-
-        Returns
-        -------
-        self
-
-        """
-        if not sp.issparse(X):
-            X = np.atleast_2d(X)
-        self.random_state = check_random_state(self.random_state)
-        n_samples, n_features = X.shape
-
-        if self.n_components == 'auto':
-            self.n_components_ = johnson_lindenstrauss_min_dim(
-                n_samples, eps=self.eps)
-
-            if self.n_components_ <= 0:
-                raise ValueError(
-                    'eps=%f and n_samples=%d lead to a target dimension of '
-                    '%d which is invalid' % (
-                        self.eps, n_samples, self.n_components_))
-            elif self.n_components_ > n_features:
-                raise ValueError(
-                    'eps=%f and n_samples=%d lead to a target dimension of '
-                    '%d which is larger than the original space with '
-                    'n_features=%d' % (self.eps, n_samples, self.n_components_,
-                                       n_features))
-        else:
-            self.n_components_ = self.n_components
-
-        self.density_ = _check_density(self.density, n_features)
-
-        self.components_ = sparse_random_matrix(
-            self.n_components_, n_features, density=self.density,
-            random_state=self.random_state)
-
-        assert_equal(
-            self.components_.shape,
-            (self.n_components_, n_features),
-            msg='An error has occured the produced projection matrix has'
-                    'not the proper shape.')
-
-        return self
-
-    def transform(self, X, y=None):
-        """Project the data by using matrix product with the random matrix
-
-        Parameters
-        ----------
-        X : numpy array or scipy.sparse of shape [n_samples, n_features]
-            The input data to project into a smaller dimensional space.
-
-        y : is not used: placeholder to allow for usage in a Pipeline.
-
-        Returns
-        -------
-        X_new : numpy array or scipy sparse of shape [n_samples, n_components]
-            Projected array.
-
-        """
-        if not hasattr(self, 'components_'):
-            raise ValueError('No random projection matrix has been fit.')
-
-        if X.shape[1] != self.components_.shape[1]:
-            raise ValueError('''Impossible to perform projection:
-            basis doesn't have the same number of dimension as sample
-            n_components != n_features (%s != %s)''' %
-            (X.shape[1], self.components_.shape[1]))
-
-        if not sp.issparse(X):
-            X = np.atleast_2d(X)
-
-        return safe_sparse_dot(X, self.components_.T,
-                               dense_output=self.dense_output)
+        super(BernouilliRandomProjection, self).__init__(
+            n_components=n_components,
+            density=density,
+            eps=eps,
+            dense_output=dense_output,
+            random_state=random_state,
+            distribution="bernouilli")
