cb19c7725d37ca32f47e2ed91bd887b22730be03
Arnaud Joly <arnaud.joly@yahoo.com>
2012-12-10 16:56:50 +0100

    FIX: argument passing selection of sparse/dense matrix

--- benchmarks/bench_random_projections.py
+++ benchmarks/bench_random_projections.py
@@ -138,7 +138,7 @@ if __name__ == "__main__":
 
     op.add_option("--sparse",
                   dest="sparse",
-                  default='Gaussian,Bernouilli',
+                  default=False,
                   action="store_true",
                   help="Set input space as a sparse matrix.")
 
