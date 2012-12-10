f014ed52db1bded33fdda39ef6d2b5612d132aae
Arnaud Joly <arnaud.joly@yahoo.com>
2012-12-10 16:31:22 +0100

    ENH optim sparse bernouilli matrix

---
+++ sklearn/random_projection/__init__.py
@@ -0,0 +1,4 @@
+from random_projection import (BernouilliRandomProjection,
+                               GaussianRandomProjection,
+                               johnson_lindenstrauss_min_dim,
+                               )
