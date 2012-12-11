007ff923ed2412ed342b62db68607cae4e316a80
Arnaud Joly <arnaud.joly@yahoo.com>
2012-12-11 13:41:15 +0100

    COSMIT + ENH better handle dense bernouilli random matrix

--- sklearn/random_projection/__init__.py
+++ sklearn/random_projection/__init__.py
@@ -1,4 +1,15 @@
-from random_projection import (BernouilliRandomProjection,
-                               GaussianRandomProjection,
-                               johnson_lindenstrauss_min_dim,
-                               )
+"""
+The :mod:`sklearn.random_projection` module implements several random
+projection matrix.
+"""
+
+from sklearn.random_projection.random_projection import (
+    BernouilliRandomProjection,
+    GaussianRandomProjection,
+    johnson_lindenstrauss_min_dim)
+
+__all__ = [
+    "BernouilliRandomProjection",
+    "GaussianRandomProjection"
+    "johnson_lindenstrauss_min_dim",
+]
