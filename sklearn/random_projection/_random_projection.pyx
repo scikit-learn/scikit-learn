3fd684b9e69fa57d1502e318fa638b08bd16bf07
Arnaud Joly <arnaud.joly@yahoo.com>
2012-12-11 17:13:22 +0100

    ENH add implementation choice for sample_int

--- sklearn/random_projection/_random_projection.pyx
+++ sklearn/random_projection/_random_projection.pyx
@@ -15,24 +15,36 @@ np.import_array()
 
 from sklearn.utils import check_random_state
 
+cpdef _sample_int_check_input(np.int_t n_population, np.int_t n_samples):
+    """ Check that input are consistent for sample_int function"""
+    if n_population < 0:
+        raise ValueError('n_population should be greater than 0, got %s.'
+                         % n_population)
+
+    if n_samples > n_population:
+        raise ValueError('n_population should be greater or equal than '
+                         'n_samples, got n_samples > n_population (%s > %s)'
+                         % (n_samples, n_population))
+
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cpdef sample_int(np.int_t n_population, np.int_t n_samples, random_state=None):
+cpdef sample_int_with_set_based_method(np.int_t n_population,
+                                       np.int_t n_samples,
+                                       random_state=None):
     """Sample integers without replacement.
 
     Select n_samples integers from the set [0, n_population) without
     replacement.
 
-
     Parameters
     ----------
-    n_population : int,
+    n_population: int,
         The size of the set to sample from.
 
-    n_samples : int,
+    n_samples: int,
         The number of integer to sample.
 
-    random_state : int, RandomState instance or None, optional (default=None)
+    random_state: int, RandomState instance or None, optional (default=None)
         If int, random_state is the seed used by the random number generator;
         If RandomState instance, random_state is the random number generator;
         If None, the random number generator is the RandomState instance used
@@ -43,12 +55,7 @@ cpdef sample_int(np.int_t n_population, np.int_t n_samples, random_state=None):
     result : array of size (n_samples, )
         The sampled subsets of integer.
     """
-    if n_population < 0:
-        raise ValueError('n_population should be greater than 0')
-
-    if n_samples > n_population:
-        raise ValueError('n_population should be greater or equal than '
-                         'n_samples')
+    _sample_int_check_input(n_population, n_samples)
 
     cdef np.int_t i
     cdef np.int_t j
@@ -70,26 +77,117 @@ cpdef sample_int(np.int_t n_population, np.int_t n_samples, random_state=None):
         selected_add(j)
         result[i] = j
 
-    # If memory is an issue it might be interesting to integrate this
-    # cython implementation by Robert Kern:
+    return result
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cpdef sample_int_with_reservoir_sampling(np.int_t n_population,
+                                         np.int_t n_samples,
+                                         random_state=None):
+    """Sample integers without replacement.
+
+    Select n_samples integers from the set [0, n_population) without
+    replacement.
+
+    Time complexity of O(n_population)
+    Space complexity of O(n_samples)
+
+
+    Parameters
+    ----------
+    n_population: int,
+        The size of the set to sample from.
+
+    n_samples: int,
+         The number of integer to sample.
+
+    random_state: int, RandomState instance or None, optional (default=None)
+        If int, random_state is the seed used by the random number generator;
+        If RandomState instance, random_state is the random number generator;
+        If None, the random number generator is the RandomState instance used
+        by `np.random`.
+
+    Returns
+    -------
+    result : array of size (n_samples, )
+        The sampled subsets of integer. The order of the items is not
+        necessarily random. Use a random permutation of the array if the order
+        of the items to be randomized.
+    """
+    _sample_int_check_input(n_population, n_samples)
+
+    cdef np.int_t i
+    cdef np.int_t j
+    cdef np.ndarray[np.int_t, ndim=1] result = np.empty((n_samples, ),
+                                                        dtype=np.int)
+
+    rng = check_random_state(random_state)
+    rng_randint = rng.randint
+
+    # This cython implementation  is based on the one of Robert Kern:
     # http://mail.scipy.org/pipermail/numpy-discussion/2010-December/
     # 054289.html
     #
-    # Note on the reservoir sampling implementation:
-    #  + The order of the items is not necessarily random. Use a random
-    # permutation of the array if you need the order of the items to be
-    # randomized.
-    #  + Time complexity of O(n_population)
-    #  + Space complexity of O(n_samples)
-    #
-    # for i in range(n_samples):
-    #     result[i] = i
-    #
-    # for i from n_samples <= i < n_population:
-    #     j = rng_randint(0, i + 1)
-    #     if j < n_samples:
-    #         result[j] = i
+    for i in range(n_samples):
+        result[i] = i
+
+    for i from n_samples <= i < n_population:
+        j = rng_randint(0, i + 1)
+        if j < n_samples:
+            result[j] = i
 
     return result
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cpdef sample_int(np.int_t n_population, np.int_t n_samples, method="auto",
+                 random_state=None):
+    """Sample integers without replacement.
+
+    Select n_samples integers from the set [0, n_population) without
+    replacement.
+
+
+    Parameters
+    ----------
+    n_population: int,
+        The size of the set to sample from.
+
+    n_samples: int,
+        The number of integer to sample.
+
+    random_state: int, RandomState instance or None, optional (default=None)
+        If int, random_state is the seed used by the random number generator;
+        If RandomState instance, random_state is the random number generator;
+        If None, the random number generator is the RandomState instance used
+        by `np.random`.
+
+    method: "auto", "set_based" or "reservoir_sampling"
+        If method == "auto", an algorithm is automatically selected.
+        If method =="set_based", a set based implementation is used which is
+        suitable for `n_samples` <<< `n_population`.
+        If method == "reservoir_sampling", a reservoir samplingn algorithm
+        which is suitable for high memory constraint or when
+        `n_samples` ~ `n_population`
+
+    Returns
+    -------
+    result : array of size (n_samples, )
+        The sampled subsets of integer.
+    """
+    _sample_int_check_input(n_population, n_samples)
+
+    all_methods = ("auto", "set_based", "reservoir_sampling")
+
+    if method == "auto" or method == "set_based":
+        return sample_int_with_set_based_method(n_population, n_samples,
+                                                random_state)
+
+    elif method == "reservoir_sampling":
+        return sample_int_with_reservoir_sampling(n_population, n_samples,
+                                                  random_state)
+    else:
+        raise ValueError('Expected a method name in %s, got %s. '
+                         % (all_methods, method))
 
 
