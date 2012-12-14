ad44b9e2c83e1f5605921826acd563f3e596d9b8
Olivier Grisel <olivier.grisel@ensta.org>
2012-12-14 18:53:20 +0100

    Improved the clustering example with HashingVectorizer

--- examples/document_clustering.py
+++ examples/document_clustering.py
@@ -7,8 +7,42 @@ This is an example showing how the scikit-learn can be used to cluster
 documents by topics using a bag-of-words approach. This example uses
 a scipy.sparse matrix to store the features instead of standard numpy arrays.
 
-Two algorithms are demoed: ordinary k-means and its faster cousin minibatch
-k-means.
+Two feature extraction methods can be used in this example:
+
+  - TfidfVectorizer uses a in-memory vocabulary (a python dict) to map the most
+    frequent words to features indices and hence compute a word occurrence
+    frequency (sparse) matrix. The word frequencies are then reweighted using
+    the Inverse Document Frequency (IDF) vector collected feature-wise over
+    the corpus.
+
+  - HashingVectorizer hashes word occurrences to a fixed dimensional space,
+    possibly with collisions. The word count vectors are then projected to the
+    euclidean unit-ball which seems to be important for k-means to work in high
+    dimensional space.
+
+    HashingVectorizer does not provide IDF weighting as this is a stateless
+    model (the fit method does nothing). When IDF is need it can be added by
+    pipelining its output to a TfidfTransformer instance.
+
+Two algorithms are demoed: ordinary k-means and its more scalable cousin
+minibatch k-means.
+
+It can be noted that k-means and (minibatch k-means) are very sensitive to
+feature scaling and that in this case the IDF weighting helps improve the
+quality of the clustering by quite a lot as measured against the "ground truth"
+provided by the class label assignments of the 20 newsgroups dataset.
+
+This improvement is not visible in the Silhouette Coefficient which is small
+for both as this measure seem to suffer from the phenomenom called
+"Concentration of Measure" or "Curse of Dimensionality" for high dimensional
+datasets such as text data. Other measures such as V-measure and Adjusted Rand
+Index are information theoretic based evaluation scores: as they are only based
+on cluster assignements rather than distances, hence not affected by the curse
+of dimensionality.
+
+Note: as k-means is optimizing a non convex objective function, it will likely
+end up in a local optimum. Several runs with independent random init might be
+necessary to get a good convergence.
 
 """
 
@@ -18,6 +52,9 @@ k-means.
 
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.pipeline import Pipeline
 from sklearn import metrics
 
 from sklearn.cluster import KMeans, MiniBatchKMeans
@@ -38,7 +75,15 @@ logging.basicConfig(level=logging.INFO,
 op = OptionParser()
 op.add_option("--no-minibatch",
               action="store_false", dest="minibatch", default=True,
-              help="Use ordinary k-means algorithm.")
+              help="Use ordinary k-means algorithm (in batch mode).")
+op.add_option("--no-idf",
+              action="store_false", dest="use_idf", default=True,
+              help="Disable Inverse Document Frequency feature weighting.")
+op.add_option("--use-hashing",
+              action="store_true", default=False,
+              help="Use a hashing feature vectorizer")
+op.add_option("--n-features", type=int, default=10000,
+              help="Maximum number of features (dimensions) to extract from text.")
 
 print __doc__
 op.print_help()
@@ -75,8 +120,23 @@ true_k = np.unique(labels).shape[0]
 
 print "Extracting features from the training dataset using a sparse vectorizer"
 t0 = time()
-vectorizer = TfidfVectorizer(max_df=0.5, max_features=10000,
-                             stop_words='english')
+if opts.use_hashing:
+    if opts.use_idf:
+        # Perform an IDF normalization on the output of HashingVectorizer
+        hasher = HashingVectorizer(n_features=opts.n_features,
+                                   stop_words='english', non_negative=True,
+                                   norm=None, binary=False)
+        vectorizer = Pipeline((
+            ('hasher', hasher),
+            ('tf_idf', TfidfTransformer())
+        ))
+    else:
+        vectorizer = HashingVectorizer(n_features=opts.n_features,
+                                   stop_words='english', non_negative=False,
+                                   norm='l2', binary=False)
+else:
+    vectorizer = TfidfVectorizer(max_df=0.5, max_features=opts.n_features,
+                                 stop_words='english', use_idf=opts.use_idf)
 X = vectorizer.fit_transform(dataset.data)
 
 print "done in %fs" % (time() - t0)
@@ -92,7 +152,7 @@ if opts.minibatch:
                          init_size=1000,
                          batch_size=1000, verbose=1)
 else:
-    km = KMeans(n_clusters=true_k, init='random', max_iter=100, n_init=1,
+    km = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=1,
                 verbose=1)
 
 print "Clustering sparse data with %s" % km
