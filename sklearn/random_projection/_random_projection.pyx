3676a3ecc3cef30676aedaaf89e34e92de96f0d9
Arnaud Joly <arnaud.joly@yahoo.com>
2012-12-18 17:04:22 +0100

    ENH Rename sample_int to sample_without_replacement

--- sklearn/random_projection/_random_projection.pyx
+++ sklearn/random_projection/_random_projection.pyx
@@ -19,8 +19,9 @@ from sklearn.utils import check_random_state
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cpdef _sample_int_check_input(np.int_t n_population, np.int_t n_samples):
-    """ Check that input are consistent for sample_int function"""
+cpdef _sample_without_replacement_check_input(np.int_t n_population,
+                                              np.int_t n_samples):
+    """ Check that input are consistent for sample_without_replacement function"""
     if n_population < 0:
         raise ValueError('n_population should be greater than 0, got %s.'
                          % n_population)
@@ -33,9 +34,9 @@ cpdef _sample_int_check_input(np.int_t n_population, np.int_t n_samples):
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cpdef sample_int_with_tracking_selection(np.int_t n_population,
-                                         np.int_t n_samples,
-                                         random_state=None):
+cpdef sample_without_replacement_with_tracking_selection(np.int_t n_population,
+                                                         np.int_t n_samples,
+                                                         random_state=None):
     """Sample integers without replacement.
 
     Select n_samples integers from the set [0, n_population) without
@@ -74,7 +75,7 @@ cpdef sample_int_with_tracking_selection(np.int_t n_population,
     result : array of size (n_samples, )
         The sampled subsets of integer.
     """
-    _sample_int_check_input(n_population, n_samples)
+    _sample_without_replacement_check_input(n_population, n_samples)
 
     cdef np.int_t i
     cdef np.int_t j
@@ -99,9 +100,9 @@ cpdef sample_int_with_tracking_selection(np.int_t n_population,
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cpdef sample_int_with_pool(np.int_t n_population,
-                           np.int_t n_samples,
-                           random_state=None):
+cpdef sample_without_replacement_with_pool(np.int_t n_population,
+                                           np.int_t n_samples,
+                                           random_state=None):
     """Sample integers without replacement.
 
     Select n_samples integers from the set [0, n_population) without
@@ -131,7 +132,7 @@ cpdef sample_int_with_pool(np.int_t n_population,
     result : array of size (n_samples, )
         The sampled subsets of integer.
     """
-    _sample_int_check_input(n_population, n_samples)
+    _sample_without_replacement_check_input(n_population, n_samples)
 
     cdef np.int_t i
     cdef np.int_t j
@@ -160,15 +161,16 @@ cpdef sample_int_with_pool(np.int_t n_population,
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cpdef sample_int_with_reservoir_sampling(np.int_t n_population,
-                                         np.int_t n_samples,
-                                         random_state=None):
+cpdef sample_without_replacement_with_reservoir_sampling(np.int_t n_population,
+                                                         np.int_t n_samples,
+                                                         random_state=None):
     """Sample integers without replacement.
 
     Select n_samples integers from the set [0, n_population) without
     replacement.
 
-    Time complexity of O((n_population - n_samples) * O(np.random.randint))
+    Time complexity of
+        O((n_population - n_samples) * O(np.random.randint) + n_samples)
     Space complexity of O(n_samples)
 
 
@@ -191,9 +193,9 @@ cpdef sample_int_with_reservoir_sampling(np.int_t n_population,
     result : array of size (n_samples, )
         The sampled subsets of integer. The order of the items is not
         necessarily random. Use a random permutation of the array if the order
-        of the items to be randomized.
+        of the items has to be randomized.
     """
-    _sample_int_check_input(n_population, n_samples)
+    _sample_without_replacement_check_input(n_population, n_samples)
 
     cdef np.int_t i
     cdef np.int_t j
@@ -219,8 +221,8 @@ cpdef sample_int_with_reservoir_sampling(np.int_t n_population,
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cpdef sample_int(np.int_t n_population, np.int_t n_samples, method="auto",
-                 random_state=None):
+cpdef sample_without_replacement(np.int_t n_population, np.int_t n_samples,
+                                 method="auto", random_state=None):
     """Sample integers without replacement.
 
     Select n_samples integers from the set [0, n_population) without
@@ -248,23 +250,23 @@ cpdef sample_int(np.int_t n_population, np.int_t n_samples, method="auto",
         If method =="tracking_selection", a set based implementation is used
         which is suitable for `n_samples` <<< `n_population`.
 
-        If method == "reservoir_sampling", a reservoir sampling algorithm
-        which is suitable for high memory constraint or when
+        If method == "reservoir_sampling", a reservoir sampling algorithm is
+        used which is suitable for high memory constraint or when
         O(`n_samples`) ~ O(`n_population`).
         The subset of selected integer is not randomized.
 
-        If method == "pool", a pool based algorithm is used which is suitable
-        for O(`n_samples`) ~ O(`n_population`), but not if you have high
-        memory constraint. For n_samples ~ n_population, the reservoir sampling
-        method is faster.
+        If method == "pool", a pool based algorithm is particularly fast, even
+        faster than the tracking selection method. Hovewer, a vector containing
+        the entire population has to be initialized.
+        If n_samples ~ n_population, the reservoir sampling method is faster.
 
     Returns
     -------
     result : array of size (n_samples, )
-        The sampled subsets of integer. This subset might not be randomized,
-        see method argument.
+        The sampled subsets of integer. The subset of selected integer might
+        not be randomized, see the method argument.
     """
-    _sample_int_check_input(n_population, n_samples)
+    _sample_without_replacement_check_input(n_population, n_samples)
 
     all_methods = ("auto", "tracking_selection", "reservoir_sampling", "pool")
 
@@ -276,18 +278,19 @@ cpdef sample_int(np.int_t n_population, np.int_t n_samples, method="auto",
 
         # The value 0.2 has been determined through benchmarking.
         if ratio < 0.2:
-            return sample_int_with_tracking_selection(n_population, n_samples,
-                                                      random_state)
+            return sample_without_replacement_with_tracking_selection(
+                n_population, n_samples, random_state)
         else:
-            return sample_int_with_reservoir_sampling(n_population, n_samples,
-                                                  random_state)
+            return sample_without_replacement_with_reservoir_sampling(
+                n_population, n_samples, random_state)
 
     elif method == "reservoir_sampling":
-        return sample_int_with_reservoir_sampling(n_population, n_samples,
-                                                  random_state)
+        return sample_without_replacement_with_reservoir_sampling(n_population,
+                                                                  n_samples,
+                                                                  random_state)
     elif method == "pool":
-        return sample_int_with_pool(n_population, n_samples,
-                                    random_state)
+        return sample_without_replacement_with_pool(n_population, n_samples,
+                                                    random_state)
     else:
         raise ValueError('Expected a method name in %s, got %s. '
                          % (all_methods, method))
