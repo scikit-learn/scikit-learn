9a8a8c8aa9b5178cec6df525a9861fed973851f5
Arnaud Joly <arnaud.joly@yahoo.com>
2012-12-21 17:49:47 +0100

    ENH add multioutput support for dummy estimator

--- sklearn/dummy.py
+++ sklearn/dummy.py
@@ -1,5 +1,6 @@
 
 # Author: Mathieu Blondel <mathieu@mblondel.org>
+#         Arnaud Joly <a.joly@ulg.ac.be>
 # License: BSD Style.
 
 import numpy as np
@@ -52,7 +53,7 @@ class DummyClassifier(BaseEstimator, ClassifierMixin):
             Training vectors, where n_samples is the number of samples
             and n_features is the number of features.
 
-        y : array-like, shape = [n_samples]
+        y : array-like, shape = [n_samples] or [n_samples, n_outputs]
             Target values.
 
         Returns
@@ -63,8 +64,26 @@ class DummyClassifier(BaseEstimator, ClassifierMixin):
         if self.strategy not in ("most_frequent", "stratified", "uniform"):
             raise ValueError("Unknown strategy type.")
 
-        self.classes_, y = unique(y, return_inverse=True)
-        self.class_prior_ = np.bincount(y) / float(y.shape[0])
+        y = np.atleast_1d(y)
+        if y.ndim == 1:
+            y = np.reshape(y, (-1, 1))
+
+        self.n_outputs_ = y.shape[1]
+        self.classes_ = []
+        self.n_classes_ = []
+        self.class_prior_ = []
+
+        for k in xrange(self.n_outputs_):
+            classes, y_k = unique(y[:, k], return_inverse=True)
+            self.classes_.append(classes)
+            self.n_classes_.append(classes.shape[0])
+            self.class_prior_.append(np.bincount(y_k) / float(y_k.shape[0]))
+
+        if self.n_outputs_ == 1:
+            self.n_classes_ = self.n_classes_[0]
+            self.classes_ = self.classes_[0]
+            self.class_prior_ = self.class_prior_[0]
+
         return self
 
     def predict(self, X):
@@ -79,7 +98,7 @@ class DummyClassifier(BaseEstimator, ClassifierMixin):
 
         Returns
         -------
-        y : array, shape = [n_samples]
+        y : array, shape = [n_samples] or [n_samples, n_outputs]
             Predicted target values for X.
         """
         if not hasattr(self, "classes_"):
@@ -89,14 +108,39 @@ class DummyClassifier(BaseEstimator, ClassifierMixin):
         n_samples = X.shape[0]
         rs = check_random_state(self.random_state)
 
-        if self.strategy == "most_frequent":
-            ret = np.ones(n_samples, dtype=int) * self.class_prior_.argmax()
-        elif self.strategy == "stratified":
-            ret = self.predict_proba(X).argmax(axis=1)
-        elif self.strategy == "uniform":
-            ret = rs.randint(len(self.classes_), size=n_samples)
+        n_classes_ = self.n_classes_
+        classes_ = self.classes_
+        class_prior_ = self.class_prior_
+        if self.n_outputs_ == 1:
+            # Get same type even for n_output == 1
+            n_classes_ = [n_classes_]
+            classes_ = [classes_]
+            class_prior_ = [class_prior_]
 
-        return self.classes_[ret]
+        # Compute probability only once
+        if self.strategy == "stratified":
+            proba = self.predict_proba(X)
+            if self.n_outputs_ == 1:
+                proba = [proba]
+
+        y = []
+        for k in xrange(self.n_outputs_):
+            if self.strategy == "most_frequent":
+                ret = np.ones(n_samples, dtype=int) * class_prior_[k].argmax()
+
+            elif self.strategy == "stratified":
+                ret = proba[k].argmax(axis=1)
+
+            elif self.strategy == "uniform":
+                ret = rs.randint(n_classes_[k], size=n_samples)
+
+            y.append(classes_[k][ret])
+
+        y = np.vstack(y).T
+        if self.n_outputs_ == 1:
+            y = np.ravel(y)
+
+        return y
 
     def predict_proba(self, X):
         """
@@ -110,29 +154,46 @@ class DummyClassifier(BaseEstimator, ClassifierMixin):
 
         Returns
         -------
-        P : array-like, shape = [n_samples, n_classes]
+        P : array-like or list of array-lke of shape = [n_samples, n_classes]
             Returns the probability of the sample for each class in
-            the model, where classes are ordered arithmetically.
+            the model, where classes are ordered arithmetically, for each
+            output.
         """
         if not hasattr(self, "classes_"):
             raise ValueError("DummyClassifier not fitted.")
 
         X = safe_asarray(X)
         n_samples = X.shape[0]
-        n_classes = len(self.classes_)
         rs = check_random_state(self.random_state)
 
-        if self.strategy == "most_frequent":
-            ind = np.ones(n_samples, dtype=int) * self.class_prior_.argmax()
-            out = np.zeros((n_samples, n_classes), dtype=np.float64)
-            out[:, ind] = 1.0
-        elif self.strategy == "stratified":
-            out = rs.multinomial(1, self.class_prior_, size=n_samples)
-        elif self.strategy == "uniform":
-            out = np.ones((n_samples, n_classes), dtype=np.float64)
-            out /= n_classes
+        n_classes_ = self.n_classes_
+        classes_ = self.classes_
+        class_prior_ = self.class_prior_
+        if self.n_outputs_ == 1:
+            n_classes_ = [n_classes_]
+            classes_ = [classes_]
+            class_prior_ = [class_prior_]
 
-        return out
+        P = []
+        for k in xrange(self.n_outputs_):
+            if self.strategy == "most_frequent":
+                ind = np.ones(n_samples, dtype=int) * class_prior_[k].argmax()
+                out = np.zeros((n_samples, n_classes_[k]), dtype=np.float64)
+                out[:, ind] = 1.0
+
+            elif self.strategy == "stratified":
+                out = rs.multinomial(1, class_prior_[k], size=n_samples)
+
+            elif self.strategy == "uniform":
+                out = np.ones((n_samples, n_classes_[k]), dtype=np.float64)
+                out /= n_classes_[k]
+
+            P.append(out)
+
+        if self.n_outputs_ == 1:
+            P = P[0]
+
+        return P
 
     def predict_log_proba(self, X):
         """
@@ -146,11 +207,16 @@ class DummyClassifier(BaseEstimator, ClassifierMixin):
 
         Returns
         -------
-        P : array-like, shape = [n_samples, n_classes]
+        P : array-like or list of array-like of shape = [n_samples, n_classes]
             Returns the log probability of the sample for each class in
-            the model, where classes are ordered arithmetically.
+            the model, where classes are ordered arithmetically for each
+            output.
         """
-        return np.log(self.predict_proba(X))
+        proba = self.predict_proba(X)
+        if self.n_outputs_ == 1:
+            return np.log(proba)
+        else:
+            return [np.log(p) for p in proba]
 
 
 class DummyRegressor(BaseEstimator, RegressorMixin):
@@ -163,7 +229,7 @@ class DummyRegressor(BaseEstimator, RegressorMixin):
 
     Attributes
     ----------
-    `y_mean_` : float
+    `y_mean_` : float or array of shape [n_outputs]
         Mean of the training targets.
     """
 
@@ -176,7 +242,7 @@ class DummyRegressor(BaseEstimator, RegressorMixin):
             Training vectors, where n_samples is the number of samples
             and n_features is the number of features.
 
-        y : array-like, shape = [n_samples]
+        y : array-like, shape = [n_samples] or [n_samples, n_outputs]
             Target values.
 
         Returns
@@ -184,7 +250,7 @@ class DummyRegressor(BaseEstimator, RegressorMixin):
         self : object
             Returns self.
         """
-        self.y_mean_ = np.mean(y)
+        self.y_mean_ = np.reshape(np.mean(y, axis=0), (1, -1))
         return self
 
     def predict(self, X):
@@ -199,7 +265,7 @@ class DummyRegressor(BaseEstimator, RegressorMixin):
 
         Returns
         -------
-        y : array, shape = [n_samples]
+        y : array, shape = [n_samples]  or [n_samples, n_outputs]
             Predicted target values for X.
         """
         if not hasattr(self, "y_mean_"):
@@ -207,5 +273,11 @@ class DummyRegressor(BaseEstimator, RegressorMixin):
 
         X = safe_asarray(X)
         n_samples = X.shape[0]
+        n_outputs = np.size(self.y_mean_)
 
-        return np.ones(n_samples) * self.y_mean_
+        y = np.ones((n_samples, 1)) * self.y_mean_
+
+        if n_outputs == 1:
+            y = np.ravel(y)
+
+        return y
