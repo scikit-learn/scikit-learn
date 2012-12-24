e2e67c919bf3961f96327dd24a40abe4cb8a0842
Vlad Niculae <vlad@vene.ro>
2012-12-24 13:40:27 +0000

    TEST flipped svd remains correct

--- sklearn/utils/tests/test_extmath.py
+++ sklearn/utils/tests/test_extmath.py
@@ -219,6 +219,9 @@ def test_randomized_svd_sign_flip():
         u2, s2, v2 = randomized_svd(a, 2, flip_sign=True, random_state=seed)
         assert_almost_equal(u1, u2)
         assert_almost_equal(v1, v2)
+        assert_almost_equal(np.dot(u2 * s2, v2), a)
+        assert_almost_equal(np.dot(u2.T, u2), np.eye(2))
+        assert_almost_equal(np.dot(v2.T, v2), np.eye(2))
 
 
 def test_cartesian():
