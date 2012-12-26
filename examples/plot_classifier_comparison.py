ff3ea9570d48639f3bd332b7be5a52e5ec886335
Andreas Mueller <amueller@ais.uni-bonn.de>
2012-12-26 21:13:58 +0100

    DOC classifier comparison: plot data without decision boundary first, better (imho) color scheme.

--- examples/plot_classifier_comparison.py
+++ examples/plot_classifier_comparison.py
@@ -29,6 +29,7 @@ print __doc__
 
 import numpy as np
 import pylab as pl
+from matplotlib.colors import ListedColormap
 from sklearn.cross_validation import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.datasets import make_moons, make_circles, make_classification
@@ -65,7 +66,7 @@ datasets = [make_moons(noise=0.3, random_state=0),
             linearly_separable
             ]
 
-figure = pl.figure(figsize=(20, 8))
+figure = pl.figure(figsize=(24, 8))
 i = 1
 # iterate over datasets
 for ds in datasets:
@@ -74,18 +75,33 @@ for ds in datasets:
     X = StandardScaler().fit_transform(X)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.4)
 
+    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+
+    # just plot the dataset first
+    cm = pl.cm.RdBu
+    cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+    ax = pl.subplot(len(datasets), len(classifiers) + 1, i)
+    # Plot the training points
+    ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright)
+    # and testing points
+    ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.6)
+    ax.set_xlim(xx.min(), xx.max())
+    ax.set_ylim(yy.min(), yy.max())
+    ax.set_xticks(())
+    ax.set_yticks(())
+    i += 1
+
     # iterate over classifiers
     for name, clf in zip(names, classifiers):
-        ax = pl.subplot(len(datasets), len(classifiers), i)
+        ax = pl.subplot(len(datasets), len(classifiers) + 1, i)
         clf.fit(X_train, y_train)
         score = clf.score(X_test, y_test)
 
         # Plot the decision boundary. For that, we will asign a color to each
         # point in the mesh [x_min, m_max]x[y_min, y_max].
-        x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
-        y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
-        xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-                             np.arange(y_min, y_max, h))
         if hasattr(clf, "decision_function"):
             Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
         else:
@@ -93,13 +109,13 @@ for ds in datasets:
 
         # Put the result into a color plot
         Z = Z.reshape(xx.shape)
-        cm = pl.cm.Spectral
-        ax.contourf(xx, yy, Z, cmap=cm)
+        ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
 
         # Plot also the training points
-        ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm)
+        ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright)
         # and testing points
-        ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm, alpha=0.5)
+        ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,
+                   alpha=0.6)
 
         ax.set_xlim(xx.min(), xx.max())
         ax.set_ylim(yy.min(), yy.max())
