cd79faf83aec4ad3c8cd09cad59747d2951be8c6
Vlad Niculae <vlad@vene.ro>
2013-01-04 00:02:03 +0000

    FIX: make BernoulliRBM doctest pass

--- sklearn/neural_networks/rbm.py
+++ sklearn/neural_networks/rbm.py
@@ -75,7 +75,10 @@ class BernoulliRBM(BaseEstimator, TransformerMixin):
     >>> from sklearn.neural_networks import BernoulliRBM
     >>> X = np.array([[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 1]])
     >>> model = BernoulliRBM(n_components=2)
-    >>> model.fit(X)
+    >>> model.fit(X)  # doctest: +ELLIPSIS
+    BernoulliRBM(learning_rate=0.1, n_components=2, n_iter=10, n_particles=10,
+           random_state=...,
+           verbose=False)
 
     References
     ----------
