6b136918388fc6db0bee33aebcf5fd7419a107f8
Noel Dawe <noel.dawe@gmail.com>
2013-01-14 12:32:55 +0100

    FIX: show decision boundary in plot_adaboost_classification.py

--- examples/ensemble/plot_adaboost_classification.py
+++ examples/ensemble/plot_adaboost_classification.py
@@ -1,10 +1,11 @@
 """
 ==============================
-Adaboosted Class Probabilities
+AdaBoosted Class Probabilities
 ==============================
 
-This example fits an Adaboosted decision tree on a classification dataset and
-plots the decision surfaces and class probabilities.
+This example fits an AdaBoosted decision stump on a classification dataset and
+plots the decision boundary and class probabilities.
+
 """
 print __doc__
 
@@ -12,6 +13,7 @@ import pylab as pl
 import numpy as np
 
 from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.datasets import make_classification
 
 X, y = make_classification(n_samples=1000,
@@ -22,7 +24,7 @@ X, y = make_classification(n_samples=1000,
                            n_redundant=0,
                            random_state=1)
 
-bdt = AdaBoostClassifier()
+bdt = AdaBoostClassifier(DecisionTreeClassifier(max_depth=1))
 
 bdt.fit(X, y)
 
@@ -31,17 +33,16 @@ plot_step = 0.02
 
 pl.figure(figsize=(15, 5))
 
-# plot the decision boundaries
+# Plot the decision boundaries
 pl.subplot(121)
 x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
 y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
 xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),
                      np.arange(y_min, y_max, plot_step))
-norm = sum(bdt.weights_)
-for weight, tree in zip(bdt.weights_, bdt.estimators_):
-    Z = tree.predict(np.c_[xx.ravel(), yy.ravel()])
-    Z = Z.reshape(xx.shape)
-    cs = pl.contourf(xx, yy, Z, alpha=0.2, cmap=pl.cm.Paired)
+
+Z = bdt.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+cs = pl.contourf(xx, yy, Z, cmap=pl.cm.Paired)
 pl.axis("tight")
 
 # Plot the training points
@@ -49,14 +50,14 @@ for i, c in zip(xrange(2), plot_colors):
     idx = np.where(y == i)
     pl.scatter(X[idx, 0], X[idx, 1], c=c, cmap=pl.cm.Paired)
 pl.axis("tight")
-pl.xlabel("Decision Surfaces")
+pl.xlabel("Decision Boundary")
 
 a_prob = bdt.predict_proba(X[y == 0])[:, -1]
 b_prob = bdt.predict_proba(X[y == 1])[:, -1]
 hist_range = (min(a_prob.min(), b_prob.min()),
               max(a_prob.max(), b_prob.max()))
 
-# plot the class probabilities
+# Plot the class probabilities
 pl.subplot(122)
 pl.hist(a_prob,
         bins=20,
