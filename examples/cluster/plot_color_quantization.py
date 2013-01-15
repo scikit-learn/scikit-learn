a71f616d3ea8cfb4da42009985d136b935cceeea
Jaques Grobler <jaquesgrobler@gmail.com>
2013-01-15 14:36:00 +0100

    typo fix and heading changes

--- examples/cluster/plot_color_quantization.py
+++ examples/cluster/plot_color_quantization.py
@@ -39,7 +39,7 @@ n_colors = 64
 china = load_sample_image("china.jpg")
 
 # Convert to floats instead of the default 8 bits integer coding. Dividing by
-# 255 is important so that pl.imshow behaves works well on foat data (need to
+# 255 is important so that pl.imshow behaves works well on float data (need to
 # be in the range [0-1]
 china = np.array(china, dtype=np.float64) / 255
 
