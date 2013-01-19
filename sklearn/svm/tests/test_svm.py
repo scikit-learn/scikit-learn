04bedd87b7a9040536e350989978708f1854e43d
Andreas Mueller <amueller@ais.uni-bonn.de>
2013-01-19 17:17:59 +0100

    TST catch deprecation warning when testing SVC label_ attribute, also test new classes_ attribute.

--- sklearn/svm/tests/test_svm.py
+++ sklearn/svm/tests/test_svm.py
@@ -54,7 +54,12 @@ def test_libsvm_iris():
         clf = svm.SVC(kernel=k).fit(iris.data, iris.target)
         assert_greater(np.mean(clf.predict(iris.data) == iris.target), 0.9)
 
-    assert_array_equal(clf.label_, np.sort(clf.label_))
+    # check deprecated ``label_`` attribute:
+    with warnings.catch_warnings(record=True):
+        # catch deprecation warning
+        assert_array_equal(clf.label_, np.sort(clf.label_))
+
+    assert_array_equal(clf.classes_, np.sort(clf.classes_))
 
     # check also the low-level API
     model = svm.libsvm.fit(iris.data, iris.target.astype(np.float64))
