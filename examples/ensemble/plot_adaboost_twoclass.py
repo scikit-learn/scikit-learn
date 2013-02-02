491319dd182aa8be9edc14da91fd1205cc935ea6
Noel Dawe <noel.dawe@gmail.com>
2013-02-02 22:53:08 +0100

    explain decision scores in two-class example

--- examples/ensemble/plot_adaboost_twoclass.py
+++ examples/ensemble/plot_adaboost_twoclass.py
@@ -6,8 +6,14 @@ Two-class AdaBoost
 This example fits an AdaBoosted decision stump on a non-linearly separable
 classification dataset composed of two "Gaussian quantiles" clusters
 (see :func:`sklearn.datasets.make_gaussian_quantiles`) and plots the decision
-boundary and decision scores. Samples with decision scores greater than zero
-are classified as B, and are otherwise classified as A.
+boundary and decision scores. The distributions of decision scores are shown
+separately for samples of class A and B. The predicted class label for each
+sample is determined by the sign of the decision score. Samples with decision
+scores greater than zero are classified as B, and are otherwise classified
+as A. The magnitude of a decision score determines the degree of likeness with
+the predicted class label. Additionally, a new dataset could be constructed
+containing a desired purity of class B, for example, by only selecting samples
+with a decision score above some value.
 
 """
 print __doc__
