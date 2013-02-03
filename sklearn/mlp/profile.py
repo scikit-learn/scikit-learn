1ced74166ef43574b56168c1ba3efbecd07b1ec5
Hannes <schulz@ais.uni-bonn.de>
2013-02-03 16:13:17 +0100

    remove additional params in profile

--- sklearn/mlp/profile.py
+++ sklearn/mlp/profile.py
@@ -9,7 +9,7 @@ n_samples = len(digits.images)
 data = digits.images.reshape((n_samples, -1))
 
 data = preprocessing.scale(data)
-classifier = MLPClassifier(n_hidden=10, lr=0.3, loss_function='cross-entropy', output_function='softmax', batch_size=100)
+classifier = MLPClassifier(n_hidden=10, lr=0.3, lr_moment=0.3, batch_size=100)
 cProfile.runctx("""classifier.fit(data[:n_samples / 2], digits.target[:n_samples / 2],
     max_epochs=1000)""", globals(), locals(), "Profile.prof")
 s = pstats.Stats("Profile.prof")
