db528524747ee9ffa95116f15317fa3a8feca93c
Lars Buitinck <larsmans@gmail.com>
2013-02-03 22:24:31 +0100

    COSMIT rm Python 2.5 and Jython compat from six

--- sklearn/externals/six.py
+++ sklearn/externals/six.py
@@ -45,23 +45,19 @@ else:
     text_type = unicode
     binary_type = str
 
-    if sys.platform.startswith("java"):
-        # Jython always uses 32 bits.
+    # It's possible to have sizeof(long) != sizeof(Py_ssize_t).
+    class X(object):
+        def __len__(self):
+            return 1 << 31
+    try:
+        len(X())
+    except OverflowError:
+        # 32-bit
         MAXSIZE = int((1 << 31) - 1)
     else:
-        # It's possible to have sizeof(long) != sizeof(Py_ssize_t).
-        class X(object):
-            def __len__(self):
-                return 1 << 31
-        try:
-            len(X())
-        except OverflowError:
-            # 32-bit
-            MAXSIZE = int((1 << 31) - 1)
-        else:
-            # 64-bit
-            MAXSIZE = int((1 << 63) - 1)
-            del X
+        # 64-bit
+        MAXSIZE = int((1 << 63) - 1)
+        del X
 
 
 def _add_doc(func, doc):
@@ -228,14 +224,6 @@ else:
 
 
 try:
-    advance_iterator = next
-except NameError:
-    def advance_iterator(it):
-        return it.next()
-next = advance_iterator
-
-
-try:
     callable = callable
 except NameError:
     def callable(obj):
@@ -317,7 +305,6 @@ if PY3:
         raise value
 
 
-    print_ = getattr(builtins, "print")
     del builtins
 
 else:
@@ -338,51 +325,6 @@ else:
     raise tp, value, tb
 """)
 
-
-    def print_(*args, **kwargs):
-        """The new-style print function."""
-        fp = kwargs.pop("file", sys.stdout)
-        if fp is None:
-            return
-        def write(data):
-            if not isinstance(data, basestring):
-                data = str(data)
-            fp.write(data)
-        want_unicode = False
-        sep = kwargs.pop("sep", None)
-        if sep is not None:
-            if isinstance(sep, unicode):
-                want_unicode = True
-            elif not isinstance(sep, str):
-                raise TypeError("sep must be None or a string")
-        end = kwargs.pop("end", None)
-        if end is not None:
-            if isinstance(end, unicode):
-                want_unicode = True
-            elif not isinstance(end, str):
-                raise TypeError("end must be None or a string")
-        if kwargs:
-            raise TypeError("invalid keyword arguments to print()")
-        if not want_unicode:
-            for arg in args:
-                if isinstance(arg, unicode):
-                    want_unicode = True
-                    break
-        if want_unicode:
-            newline = unicode("\n")
-            space = unicode(" ")
-        else:
-            newline = "\n"
-            space = " "
-        if sep is None:
-            sep = space
-        if end is None:
-            end = newline
-        for i, arg in enumerate(args):
-            if i:
-                write(sep)
-            write(arg)
-        write(end)
 
 _add_doc(reraise, """Reraise an exception.""")
 
