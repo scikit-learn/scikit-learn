e3511236dee0958ad901ce29305e36f5cf5e15bd
Hannes Schulz <schulz@ais.uni-bonn.de>
2013-02-06 09:38:36 +0100

    start profiling MNIST

--- sklearn/mlp/profile.py
+++ sklearn/mlp/profile.py
@@ -4,13 +4,15 @@ import cProfile
 from sklearn.mlp.classes import MLPClassifier
 from sklearn import datasets, preprocessing
 
-digits = datasets.load_digits()
-n_samples = len(digits.images)
-data = digits.images.reshape((n_samples, -1))
+digits = datasets.fetch_mldata("MNIST Original")
+n_samples = len(digits.data)
+data = digits.data.reshape((n_samples, -1))
 
-data = preprocessing.scale(data)
-classifier = MLPClassifier(n_hidden=10, lr=0.3, lr_moment=0.3, batch_size=100)
+data = data / 255.
+
+#data = preprocessing.scale(data)
+classifier = MLPClassifier(n_hidden=64, lr=0.3, lr_moment=0.3, batch_size=1)
 cProfile.runctx("""classifier.fit(data[:n_samples / 2], digits.target[:n_samples / 2],
-    max_epochs=1000)""", globals(), locals(), "Profile.prof")
+    max_epochs=10)""", globals(), locals(), "Profile.prof")
 s = pstats.Stats("Profile.prof")
 s.strip_dirs().sort_stats("time").print_stats()
