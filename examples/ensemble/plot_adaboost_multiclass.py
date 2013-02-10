70f449bcc3faa5529a46f1b174ec3b35edf3d953
Mathieu Blondel <mathieu@mblondel.org>
2013-02-10 11:20:19 +0900

    Cosmit: more explicit xlabel.

--- examples/ensemble/plot_adaboost_multiclass.py
+++ examples/ensemble/plot_adaboost_multiclass.py
@@ -89,7 +89,7 @@ pl.plot(n_trees, bdt_discrete.estimator_errors_, "b", label='SAMME', alpha=.5)
 pl.plot(n_trees, bdt_real.estimator_errors_, "r", label='SAMME.R', alpha=.5)
 pl.legend()
 pl.ylabel('Error')
-pl.xlabel('Tree')
+pl.xlabel('Number of Trees')
 pl.ylim((.2,
         max(bdt_real.estimator_errors_.max(),
             bdt_discrete.estimator_errors_.max()) * 1.2))
@@ -99,7 +99,7 @@ pl.subplot(133)
 pl.plot(n_trees, bdt_discrete.estimator_weights_, "b", label='SAMME')
 pl.legend()
 pl.ylabel('Weight')
-pl.xlabel('Tree')
+pl.xlabel('Number of Trees')
 pl.ylim((0, bdt_discrete.estimator_weights_.max() * 1.2))
 pl.xlim((-20, len(bdt_discrete) + 20))
 
