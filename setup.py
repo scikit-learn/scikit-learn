17d3b5da438f483ea264843a2631ff1c0b6e4616
Olivier Grisel <olivier.grisel@ensta.org>
2013-02-14 10:17:25 +0100

    Disable lib2to3

--- setup.py
+++ setup.py
@@ -75,39 +75,6 @@ def configuration(parent_package='', top_path=None):
 
 if __name__ == "__main__":
 
-    old_path = os.getcwd()
-    local_path = os.path.dirname(os.path.abspath(sys.argv[0]))
-    # python 3 compatibility stuff.
-    # Simplified version of scipy strategy: copy files into
-    # build/py3k, and patch them using lib2to3.
-    if sys.version_info[0] == 3:
-        try:
-            import lib2to3cache
-        except ImportError:
-            pass
-        local_path = os.path.join(local_path, 'build', 'py3k')
-        if os.path.exists(local_path):
-            shutil.rmtree(local_path)
-        print("Copying source tree into build/py3k for 2to3 transformation"
-              "...")
-        shutil.copytree(os.path.join(old_path, 'sklearn'),
-                        os.path.join(local_path, 'sklearn'))
-        import lib2to3.main
-        from io import StringIO
-        print("Converting to Python3 via 2to3...")
-        _old_stdout = sys.stdout
-        try:
-            sys.stdout = StringIO()  # supress noisy output
-            res = lib2to3.main.main("lib2to3.fixes", ['-x', 'import', '-w', local_path])
-        finally:
-            sys.stdout = _old_stdout
-
-        if res != 0:
-            raise Exception('2to3 failed, exiting ...')
-
-    os.chdir(local_path)
-    sys.path.insert(0, local_path)
-
     setup(configuration=configuration,
           name=DISTNAME,
           maintainer=MAINTAINER,
