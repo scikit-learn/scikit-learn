8075887585b0449b6e87ee54c2ca4dbd56960e1e
Andreas Mueller <amueller@ais.uni-bonn.de>
2013-02-23 17:17:56 +0100

    REL increased version number, added whatsnew

--- doc/conf.py
+++ doc/conf.py
@@ -67,7 +67,7 @@ copyright = u'2010–2013, scikit-learn developers (BSD License)'
 # built documents.
 #
 # The short X.Y version.
-version = '0.13'
+version = '0.13.1'
 # The full version, including alpha/beta/rc tags.
 import sklearn
 release = sklearn.__version__
