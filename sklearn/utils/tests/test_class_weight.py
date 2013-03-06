269cde2e3e3c91acc8f8f4396173f8610a90483c
Lars Buitinck <larsmans@gmail.com>
2013-03-06 22:58:49 +0100

    FIX + TST + DOC compute_class_weight
    Fixes #1746, #1747. Bug reported and fix proposed by @gatagat.

--- sklearn/utils/tests/test_class_weight.py
+++ sklearn/utils/tests/test_class_weight.py
@@ -1,14 +1,15 @@
 import numpy as np
 
 from sklearn.utils.class_weight import compute_class_weight
+from sklearn.utils.fixes import unique
 
 from sklearn.utils.testing import assert_almost_equal
 from sklearn.utils.testing import assert_true
 
 
-def test_non_index_class_labels():
-    """Test class_weight="auto" with unique(y) not the range [0, K)."""
-    y = np.asarray([2, 2, 2, 3, 3, 4])
-    cw = compute_class_weight("auto", np.asarray([2, 3, 4]), y)
-    assert_almost_equal(cw.sum(), np.unique(y).shape)
+def test_compute_class_weight():
+    """Test (and demo) compute_class_weight."""
+    classes, y = unique(np.asarray([2, 2, 2, 3, 3, 4]), return_inverse=True)
+    cw = compute_class_weight("auto", classes, y)
+    assert_almost_equal(cw.sum(), classes.shape)
     assert_true(cw[0] < cw[1] < cw[2])
