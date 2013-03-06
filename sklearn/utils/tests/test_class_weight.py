71cf08c25c375d5b17793587a12e49d76507e117
Andreas Mueller <amueller@ais.uni-bonn.de>
2013-03-06 23:41:24 +0100

    FIX compute_class_weight edge case

--- sklearn/utils/tests/test_class_weight.py
+++ sklearn/utils/tests/test_class_weight.py
@@ -5,6 +5,7 @@ from sklearn.utils.fixes import unique
 
 from sklearn.utils.testing import assert_almost_equal
 from sklearn.utils.testing import assert_true
+from sklearn.utils.testing import assert_equal
 
 
 def test_compute_class_weight():
@@ -13,3 +14,13 @@ def test_compute_class_weight():
     cw = compute_class_weight("auto", classes, y)
     assert_almost_equal(cw.sum(), classes.shape)
     assert_true(cw[0] < cw[1] < cw[2])
+
+
+def test_compute_class_weight_not_present():
+    """Test compute_class_weight in case y doesn't contain all classes."""
+    classes = np.arange(4)
+    y = np.asarray([0, 0, 0, 1, 1, 2])
+    cw = compute_class_weight("auto", classes, y)
+    assert_almost_equal(cw.sum(), classes.shape)
+    assert_equal(len(cw), len(classes))
+    assert_true(cw[0] < cw[1] < cw[2] <= cw[3])
