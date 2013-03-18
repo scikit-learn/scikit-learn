9e93ba5eb9c1cdce9b2ce65d845d9b4fa10cf661
Raul Garreta <rgarreta@gmail.com>
2013-03-18 15:38:51 -0700

    PY3: fix None comparison (when not in OS X) in test_k_means.py

--- sklearn/cluster/tests/test_k_means.py
+++ sklearn/cluster/tests/test_k_means.py
@@ -204,16 +204,23 @@ def test_k_means_new_centers():
         np.testing.assert_array_equal(this_labels, labels)
 
 
-def _get_mac_os_version():
+def _is_mac_os_version_ge(version):
+    """Returns True iff Mac OS X and newer than specified version."""
     import platform
     mac_version, _, _ = platform.mac_ver()
     if mac_version:
-        # turn something like '10.7.3' into '10.7'
-        return '.'.join(mac_version.split('.')[:2])
+        my_major, my_minor = version.split('.')
+        # keep only major and minor system version
+        sys_major, sys_minor = mac_version.split('.')[:2]
+        if int(sys_major) > int(my_major):
+            return True
+        else:
+            return int(sys_minor) >= int(my_minor)
+    return False
 
 
 def test_k_means_plus_plus_init_2_jobs():
-    if _get_mac_os_version() >= '10.7':
+    if _is_mac_os_version_ge('10.7'):
         raise SkipTest('Multi-process bug in Mac OS X Lion (see issue #636)')
     km = KMeans(init="k-means++", n_clusters=n_clusters, n_jobs=2,
                      random_state=42).fit(X)
