3fb9fabc16bd174200e5e05c9fd9ec6b8e08d614
Steven De Gryze <sdegryze@gmail.com>
2013-03-18 22:19:34 -0700

    fixing comparison of array of doubles and string through try/except block

--- sklearn/feature_selection/selector_mixin.py
+++ sklearn/feature_selection/selector_mixin.py
@@ -94,7 +94,11 @@ class SelectorMixin(TransformerMixin):
             threshold = float(threshold)
 
         # Selection
-        mask = importances >= threshold
+        try:
+            mask = importances >= threshold
+        except TypeError:
+            # Fails in Python 3.x when threshold is str; result is array of True
+            mask = np.ones(shape = importances.shape, dtype = bool)
 
         if np.any(mask):
             mask = safe_mask(X, mask)
