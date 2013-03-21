af39e512bb4168082213957b6f88713a635c6121
Hannes Schulz <schulz@ais.uni-bonn.de>
2013-03-21 18:33:02 +0100

    ENH add sparse matrix support to MinMaxScaler

--- sklearn/utils/sparsefuncs.pyx
+++ sklearn/utils/sparsefuncs.pyx
@@ -18,6 +18,66 @@ ctypedef np.float64_t DOUBLE
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True)
+def csr_min_max_axis0(X):
+    """Compute minimum and maximum along axis 0 on a CSR matrix
+
+    Parameters
+    ----------
+    X: CSR sparse matrix, shape (n_samples, n_features)
+        Input data.
+
+    Returns
+    -------
+
+    min: float array with shape (n_features,)
+        Feature-wise minima
+
+    max: float array with shape (n_features,)
+        Feature-wise maxima
+    """
+    cdef unsigned int n_samples = X.shape[0]
+    cdef unsigned int n_features = X.shape[1]
+
+    cdef np.ndarray[DOUBLE, ndim=1] X_data = X.data
+    cdef np.ndarray[int, ndim=1] X_indices = X.indices
+    cdef np.ndarray[int, ndim=1] X_indptr = X.indptr
+
+    cdef unsigned int i
+    cdef unsigned int j
+    cdef unsigned int ind
+    cdef double v
+
+    # mins[j] contains the minimum of feature j
+    cdef np.ndarray[DOUBLE, ndim=1] mins = np.empty((n_features))
+
+    # maxs[j] contains the maximum of feature j
+    cdef np.ndarray[DOUBLE, ndim=1] maxs = np.empty((n_features))
+
+    # nvals[j] contains the number of values for feature j
+    cdef np.ndarray[np.int64_t, ndim=1] nvals = np.zeros((n_features), dtype=np.int64)
+
+    mins[:] = np.inf
+    maxs[:] = -np.inf
+
+    for i in xrange(n_samples):
+        for j in xrange(X_indptr[i], X_indptr[i + 1]):
+            ind = X_indices[j]
+            v   = X_data[j]
+            mins[ind] = min(mins[ind], v)
+            maxs[ind] = max(maxs[ind], v)
+            nvals[ind] += 1
+
+    # cython cannot do boolean arrays (yet)
+    # cdef np.ndarray[bool, ndim=1] not_dense = nvals != n_samples
+    not_dense = nvals != n_samples
+    mins[not_dense * (mins > 0.0)] = 0.0
+    maxs[not_dense * (maxs < 0.0)] = 0.0
+    return mins, maxs
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
 def csr_mean_variance_axis0(X):
     """Compute mean and variance along axis 0 on a CSR matrix
 
@@ -55,7 +115,7 @@ def csr_mean_variance_axis0(X):
     cdef np.ndarray[DOUBLE, ndim=1] variances = np.zeros_like(means)
 
     # counts[j] contains the number of samples where feature j is non-zero
-    cdef np.ndarray[int, ndim=1] counts = np.zeros_like(means, dtype=int)
+    cdef np.ndarray[np.int64_t, ndim=1] counts = np.zeros_like(means, dtype=np.int64)
 
     for i in xrange(n_samples):
         for j in xrange(X_indptr[i], X_indptr[i + 1]):
@@ -171,6 +231,61 @@ def inplace_csr_column_scale(X, np.ndarray[DOUBLE, ndim=1] scale):
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True)
+def csc_min_max_axis0(X):
+    """Compute minimum and maximum along axis 0 on a CSC matrix
+
+    Parameters
+    ----------
+    X: CSC sparse matrix, shape (n_samples, n_features)
+        Input data.
+
+    Returns
+    -------
+
+    min: float array with shape (n_features,)
+        Feature-wise minima
+
+    max: float array with shape (n_features,)
+        Feature-wise maxima
+    """
+    cdef unsigned int n_samples = X.shape[0]
+    cdef unsigned int n_features = X.shape[1]
+
+    cdef np.ndarray[DOUBLE, ndim=1] X_data = X.data
+    cdef np.ndarray[int, ndim=1] X_indices = X.indices
+    cdef np.ndarray[int, ndim=1] X_indptr = X.indptr
+
+    cdef unsigned int i
+    cdef unsigned int j
+    cdef double v
+
+    # mins[j] contains the mean of feature j
+    cdef np.ndarray[DOUBLE, ndim=1] mins = np.empty((n_features))
+
+    # maxs[j] contains the variance of feature j
+    cdef np.ndarray[DOUBLE, ndim=1] maxs = np.empty((n_features))
+
+    # nvals[j] contains the number of values for feature j
+    cdef np.ndarray[np.int64_t, ndim=1] nvals = np.zeros((n_features), dtype=np.int64)
+
+    for i in xrange(n_features):
+        for j in xrange(X_indptr[i], X_indptr[i + 1]):
+            v = X_data[j]
+            mins[i] = min(mins[i], v)
+            maxs[i] = max(maxs[i], v)
+            nvals[i] += 1
+
+    # cython cannot do boolean arrays (yet)
+    # cdef np.ndarray[bool, ndim=1] not_dense = nvals != n_samples
+    not_dense = nvals != n_samples
+    mins[not_dense * (mins > 0.0)] = 0.0
+    maxs[not_dense * (maxs < 0.0)] = 0.0
+    return mins, maxs
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
 def csc_mean_variance_axis0(X):
     """Compute mean and variance along axis 0 on a CSC matrix
 
@@ -207,7 +322,7 @@ def csc_mean_variance_axis0(X):
     cdef np.ndarray[DOUBLE, ndim=1] variances = np.zeros_like(means)
 
     # counts[j] contains the number of samples where feature j is non-zero
-    cdef np.ndarray[int, ndim=1] counts = np.zeros_like(means, dtype=int)
+    cdef np.ndarray[np.int64_t, ndim=1] counts = np.zeros_like(means, dtype=np.int64)
 
     for i in xrange(n_features):
         for j in xrange(X_indptr[i], X_indptr[i + 1]):
@@ -250,6 +365,33 @@ def inplace_csc_column_scale(X, np.ndarray[DOUBLE, ndim=1] scale):
     for i in xrange(n_features):
         for j in xrange(X_indptr[i], X_indptr[i + 1]):
             X_data[j] *= scale[i]
+
+
+def min_max_axis0(X):
+    """Compute minimum and maximum along axis 0 on a CSR or CSC matrix
+
+    Parameters
+    ----------
+    X: CSR or CSC sparse matrix, shape (n_samples, n_features)
+        Input data.
+
+    Returns
+    -------
+
+    mins: float array with shape (n_features,)
+        Feature-wise minima
+
+    maxs: float array with shape (n_features,)
+        Feature-wise maxima
+
+    """
+    if isinstance(X, sp.csr_matrix):
+        return csr_min_max_axis0(X)
+    elif isinstance(X, sp.csc_matrix):
+        return csc_min_max_axis0(X)
+    else:
+        raise TypeError(
+                "Unsupported type; expected a CSR or CSC sparse matrix.")
 
 def mean_variance_axis0(X):
     """Compute mean and variance along axis 0 on a CSR or CSC matrix
