0696f78a8a09bbb8e879388dd9a75d77c760894e
Nicolas Trésegnie <nicolas.tresegnie@gmail.com>
2013-03-26 09:29:20 +0100

    Removing of _tree.c (the extension is now directly built from _tree.pyx)

--- sklearn/tree/setup.py
+++ sklearn/tree/setup.py
@@ -1,7 +1,9 @@
 import os
 
 import numpy
-from numpy.distutils.misc_util import Configuration
+from numpy.distutils.misc_util import Configuration, dot_join
+from numpy.distutils.core import Extension
+from Cython.Build import cythonize
 
 
 def configuration(parent_package="", top_path=None):
@@ -9,10 +11,17 @@ def configuration(parent_package="", top_path=None):
     libraries = []
     if os.name == 'posix':
         libraries.append('m')
-    config.add_extension("_tree",
-                         sources=["_tree.c"],
-                         include_dirs=[numpy.get_include()],
-                         libraries=libraries)
+
+    extensions = [
+        Extension(
+            dot_join(config.name, '_tree'),
+            sources=config.paths(['_tree.pyx']),
+            include_dirs=[numpy.get_include()],
+            libraries=libraries)
+    ]
+    extensions = cythonize(extensions)
+
+    config.ext_modules.extend(extensions)
 
     config.add_subpackage("tests")
 
