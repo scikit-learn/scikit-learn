34022b528343db1c4ccaa3dbe09ab29b1dc26b68
Felix Brockherde <fbrockherde@gmail.com>
2013-04-09 16:03:52 +0200

    FIX scores calculation in ovo multiclass
    Verify that ties can be won by all labels

--- sklearn/multiclass.py
+++ sklearn/multiclass.py
@@ -315,8 +315,8 @@ def predict_ovo(estimators, classes, X):
         for j in range(i + 1, n_classes):
             pred = estimators[k].predict(X)
             score = _predict_binary(estimators[k], X)
-            scores[:, 0] += score
-            scores[:, 1] -= score
+            scores[:, i] += score
+            scores[:, j] -= score
             votes[pred == 0, i] += 1
             votes[pred == 1, j] += 1
             k += 1
