ad4877a7b6cb4c0ccd9abca15798626d398427ba
Yann N. Dauphin <dhaemon@gmail.com>
2013-04-15 14:58:15 -0400

    bugfix test

--- sklearn/neural_network/rbm.py
+++ sklearn/neural_network/rbm.py
@@ -76,7 +76,7 @@ class BernoulliRBM(BaseEstimator, TransformerMixin):
     >>> X = np.array([[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 1]])
     >>> model = BernoulliRBM(n_components=2)
     >>> model.fit(X)  # doctest: +ELLIPSIS
-    BernoulliRBM(learning_rate=0.1, n_components=2, n_iter=10, batch_size=10,
+    BernoulliRBM(batch_size=10, learning_rate=0.1, n_components=2, n_iter=10,
            random_state=...,
            verbose=False)
 
