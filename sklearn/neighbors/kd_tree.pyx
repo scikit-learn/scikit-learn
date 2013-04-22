55f409cdf9d527a73f255ac4315c34eff5435d74
Jake Vanderplas <vanderplas@astro.washington.edu>
2013-04-21 20:40:06 -0700

    add chebyshev distance to kd tree

--- sklearn/neighbors/kd_tree.pyx
+++ sklearn/neighbors/kd_tree.pyx
@@ -11,7 +11,8 @@ __all__ = ['KDTree']
 
 DOC_DICT = {'BinaryTree':'KDTree', 'binary_tree':'kd_tree'}
 
-VALID_METRICS = ['EuclideanDistance', 'ManhattanDistance', 'MinkowskiDistance']
+VALID_METRICS = ['EuclideanDistance', 'ManhattanDistance',
+                 'ChebyshevDistance', 'MinkowskiDistance']
 
 include "binary_tree.pxi"
 
@@ -54,17 +55,27 @@ cdef DTYPE_t min_rdist(BinaryTree bt, ITYPE_t i_node, DTYPE_t* pt) except -1:
     cdef DTYPE_t d, d_lo, d_hi, rdist=0.0
     cdef ITYPE_t j
 
-    # here we'll use the fact that x + abs(x) = 2 * max(x, 0)
-    for j in range(n_features):
-        d_lo = bt.node_bounds[0, i_node, j] - pt[j]
-        d_hi = pt[j] - bt.node_bounds[1, i_node, j]
-        d = (d_lo + fabs(d_lo)) + (d_hi + fabs(d_hi))
-        rdist += pow(0.5 * d, bt.dm.p)
+    if bt.dm.p == INF:
+        for j in range(n_features):
+            d_lo = bt.node_bounds[0, i_node, j] - pt[j]
+            d_hi = pt[j] - bt.node_bounds[1, i_node, j]
+            d = (d_lo + fabs(d_lo)) + (d_hi + fabs(d_hi))
+            rdist = fmax(rdist, 0.5 * d)
+    else:
+        # here we'll use the fact that x + abs(x) = 2 * max(x, 0)
+        for j in range(n_features):
+            d_lo = bt.node_bounds[0, i_node, j] - pt[j]
+            d_hi = pt[j] - bt.node_bounds[1, i_node, j]
+            d = (d_lo + fabs(d_lo)) + (d_hi + fabs(d_hi))
+            rdist += pow(0.5 * d, bt.dm.p)
 
     return rdist
 
 cdef DTYPE_t min_dist(BinaryTree bt, ITYPE_t i_node, DTYPE_t* pt) except -1:
-    return pow(min_rdist(bt, i_node, pt), 1. / bt.dm.p)
+    if bt.dm.p == INF:
+        return min_rdist(bt, i_node, pt)
+    else:
+        return pow(min_rdist(bt, i_node, pt), 1. / bt.dm.p)
 
 cdef DTYPE_t max_rdist(BinaryTree bt, ITYPE_t i_node, DTYPE_t* pt) except -1:
     cdef ITYPE_t n_features = bt.data.shape[1]
@@ -72,15 +83,23 @@ cdef DTYPE_t max_rdist(BinaryTree bt, ITYPE_t i_node, DTYPE_t* pt) except -1:
     cdef DTYPE_t d, d_lo, d_hi, rdist=0.0
     cdef ITYPE_t j
 
-    for j in range(n_features):
-        d_lo = fabs(pt[j] - bt.node_bounds[0, i_node, j])
-        d_hi = fabs(pt[j] - bt.node_bounds[1, i_node, j])
-        rdist += pow(fmax(d_lo, d_hi), bt.dm.p)
+    if bt.dm.p == INF:
+        for j in range(n_features):
+            rdist = fmax(rdist, fabs(pt[j] - bt.node_bounds[0, i_node, j]))
+            rdist = fmax(rdist, fabs(pt[j] - bt.node_bounds[1, i_node, j]))
+    else:
+        for j in range(n_features):
+            d_lo = fabs(pt[j] - bt.node_bounds[0, i_node, j])
+            d_hi = fabs(pt[j] - bt.node_bounds[1, i_node, j])
+            rdist += pow(fmax(d_lo, d_hi), bt.dm.p)
 
     return rdist
 
 cdef DTYPE_t max_dist(BinaryTree bt, ITYPE_t i_node, DTYPE_t* pt) except -1:
-    return pow(max_rdist(bt, i_node, pt), 1. / bt.dm.p)
+    if bt.dm.p == INF:
+        return max_rdist(bt, i_node, pt)
+    else:
+        return pow(max_rdist(bt, i_node, pt), 1. / bt.dm.p)
 
 cdef inline int min_max_dist(BinaryTree bt, ITYPE_t i_node, DTYPE_t* pt,
                              DTYPE_t* min_dist, DTYPE_t* max_dist) except -1:
@@ -92,35 +111,57 @@ cdef inline int min_max_dist(BinaryTree bt, ITYPE_t i_node, DTYPE_t* pt,
     min_dist[0] = 0.0
     max_dist[0] = 0.0
 
-    # as above, use the fact that x + abs(x) = 2 * max(x, 0)
-    for j in range(n_features):
-        d_lo = bt.node_bounds[0, i_node, j] - pt[j]
-        d_hi = pt[j] - bt.node_bounds[1, i_node, j]
-        d = (d_lo + fabs(d_lo)) + (d_hi + fabs(d_hi))
-        min_dist[0] += pow(0.5 * d, bt.dm.p)
-        max_dist[0] += pow(fmax(fabs(d_lo), fabs(d_hi)), bt.dm.p)
+    if bt.dm.p == INF:
+        for j in range(n_features):
+            d_lo = bt.node_bounds[0, i_node, j] - pt[j]
+            d_hi = pt[j] - bt.node_bounds[1, i_node, j]
+            d = (d_lo + fabs(d_lo)) + (d_hi + fabs(d_hi))
+            min_dist[0] = fmax(min_dist[0], 0.5 * d)
+            max_dist[0] = fmax(max_dist[0],
+                               fabs(pt[j] - bt.node_bounds[0, i_node, j]))
+            max_dist[0] = fmax(max_dist[0],
+                               fabs(pt[j] - bt.node_bounds[1, i_node, j]))
+    else:
+        # as above, use the fact that x + abs(x) = 2 * max(x, 0)
+        for j in range(n_features):
+            d_lo = bt.node_bounds[0, i_node, j] - pt[j]
+            d_hi = pt[j] - bt.node_bounds[1, i_node, j]
+            d = (d_lo + fabs(d_lo)) + (d_hi + fabs(d_hi))
+            min_dist[0] += pow(0.5 * d, bt.dm.p)
+            max_dist[0] += pow(fmax(fabs(d_lo), fabs(d_hi)), bt.dm.p)
 
-    min_dist[0] = pow(min_dist[0], 1. / bt.dm.p)
-    max_dist[0] = pow(max_dist[0], 1. / bt.dm.p)
+        min_dist[0] = pow(min_dist[0], 1. / bt.dm.p)
+        max_dist[0] = pow(max_dist[0], 1. / bt.dm.p)
+
     return 0
 
-cdef inline DTYPE_t min_rdist_dual(BinaryTree bt, ITYPE_t i_node1,
+cdef inline DTYPE_t min_rdist_dual(BinaryTree bt1, ITYPE_t i_node1,
                                    BinaryTree bt2, ITYPE_t i_node2) except -1:
-    cdef ITYPE_t n_features = bt.data.shape[1]
+    cdef ITYPE_t n_features = bt1.data.shape[1]
 
     cdef DTYPE_t d, d1, d2, rdist=0.0
     cdef DTYPE_t zero = 0.0
     cdef ITYPE_t j
 
-    # here we'll use the fact that x + abs(x) = 2 * max(x, 0)
-    for j in range(n_features):
-        d1 = (bt.node_bounds[0, i_node1, j]
-              - bt2.node_bounds[1, i_node2, j])
-        d2 = (bt2.node_bounds[0, i_node2, j]
-              - bt.node_bounds[1, i_node1, j])
-        d = (d1 + fabs(d1)) + (d2 + fabs(d2))
+    if bt1.dm.p == INF:
+        for j in range(n_features):
+            d1 = (bt1.node_bounds[0, i_node1, j]
+                  - bt2.node_bounds[1, i_node2, j])
+            d2 = (bt2.node_bounds[0, i_node2, j]
+                  - bt1.node_bounds[1, i_node1, j])
+            d = (d1 + fabs(d1)) + (d2 + fabs(d2))
 
-        rdist += pow(0.5 * d, bt.dm.p)
+            rdist = fmax(rdist, 0.5 * d)
+    else:
+        # here we'll use the fact that x + abs(x) = 2 * max(x, 0)
+        for j in range(n_features):
+            d1 = (bt1.node_bounds[0, i_node1, j]
+                  - bt2.node_bounds[1, i_node2, j])
+            d2 = (bt2.node_bounds[0, i_node2, j]
+                  - bt1.node_bounds[1, i_node1, j])
+            d = (d1 + fabs(d1)) + (d2 + fabs(d2))
+
+            rdist += pow(0.5 * d, bt1.dm.p)
 
     return rdist
 
@@ -137,12 +178,19 @@ cdef inline DTYPE_t max_rdist_dual(BinaryTree bt1, ITYPE_t i_node1,
     cdef DTYPE_t zero = 0.0
     cdef ITYPE_t j
 
-    for j in range(n_features):
-        d1 = fabs(bt1.node_bounds[0, i_node1, j]
-                  - bt2.node_bounds[1, i_node2, j])
-        d2 = fabs(bt1.node_bounds[1, i_node1, j]
-                  - bt2.node_bounds[0, i_node2, j])
-        rdist += pow(fmax(d1, d2), bt1.dm.p)
+    if bt1.dm.p == INF:
+        for j in range(n_features):
+            rdist = fmax(rdist, fabs(bt1.node_bounds[0, i_node1, j]
+                                     - bt2.node_bounds[1, i_node2, j]))
+            rdist = fmax(rdist, fabs(bt1.node_bounds[1, i_node1, j]
+                                     - bt2.node_bounds[0, i_node2, j]))
+    else:
+        for j in range(n_features):
+            d1 = fabs(bt1.node_bounds[0, i_node1, j]
+                      - bt2.node_bounds[1, i_node2, j])
+            d2 = fabs(bt1.node_bounds[1, i_node1, j]
+                      - bt2.node_bounds[0, i_node2, j])
+            rdist += pow(fmax(d1, d2), bt1.dm.p)
 
     return rdist
 
--- sklearn/neighbors/tests/test_kd_tree.py
+++ sklearn/neighbors/tests/test_kd_tree.py
@@ -12,6 +12,7 @@ DIMENSION = 3
 
 METRICS = {'euclidean':{},
            'manhattan':{},
+           'chebyshev':{},
            'minkowski':dict(p=3)}
 
 
