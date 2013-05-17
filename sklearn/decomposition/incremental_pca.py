133a01d5d1ae376feba044e4619f290a8392796e
Kevin Hughes <kevin@kevin-M3920.(none)>
2013-05-17 11:51:10 -0400

    added another ipca method

--- sklearn/decomposition/incremental_pca.py
+++ sklearn/decomposition/incremental_pca.py
@@ -11,6 +11,8 @@ from ..base import BaseEstimator, TransformerMixin
 from ..utils import array2d, as_float_array
 from ..utils.extmath import safe_sparse_dot
 
+from pca import PCA
+
 class IncrementalPCA(BaseEstimator, TransformerMixin):
     """Incremental principal component analysis (IncrementalPCA)
     
@@ -19,16 +21,10 @@ class IncrementalPCA(BaseEstimator, TransformerMixin):
     Each new observation (u) is projected on the eigenspace spanned by
     the current components and the residual vector is used as a new 
     component. The new principal components are then rotated by a rotation 
-    matrix (R) whose columns are the eigenvectors of the transformed covariance matrix   
-    to yield p + 1 principal components. From those, only the first p are selected.
-    
-    Based on P.Hall, D. Marshall and R. Martin "Incremental Eigenalysis for 
-    Classification" which appeared in British Machine Vision Conference, volume 1,
-    pages 286-295, September 1998.
-    
-    implementation based on the python recipe (recipe-577213-1) 
-    published by Micha Kalfon
-    
+    matrix (R) whose columns are the eigenvectors of the transformed covariance 
+    matrix to yield p + 1 principal components. From those, only the first p are 
+    selected.
+     
     Parameters
     ----------
     n_components : int
@@ -99,34 +95,41 @@ class IncrementalPCA(BaseEstimator, TransformerMixin):
         X = array2d(X)
         n_samples, n_features = X.shape 
         X = as_float_array(X, copy=self.copy)
-        
-        # init
-        if self.iteration == 0:  
-            self.mean_ = np.zeros([n_features], np.float)
-            self.covariance_ = np.zeros([n_features, n_features], np.float)
-            self.components_ = np.zeros([self.n_components,n_features], np.float)
-            self.explained_variance_ratio_ = np.zeros([self.n_components], np.float)
-        else:
-            if n_features != self.components_.shape[1]:
-                raise ValueError('The dimensionality of the new data and the existing components_ does not match')   
+          
+        if self.iteration != 0 and n_features != self.components_.shape[1]:
+            raise ValueError('The dimensionality of the new data and the existing components_ does not match')   
         
         # incrementally fit the model
         for i in range(0,X.shape[0]):
-            self.partial_fit(X[i,:])
-        
-        # normalize explained_variance_ratio_
-        self.explained_variance_ratio_ = (self.explained_variance_ratio_ / self.explained_variance_ratio_.sum())
+            self.partial_fit2(X[i,:])
         
         return self
       
     def partial_fit(self, u):
         """ Updates the mean and components to account for a new vector.
 
+        Based on P.Hall, D. Marshall and R. Martin "Incremental Eigenalysis for 
+        Classification" which appeared in British Machine Vision Conference, volume 1,
+        pages 286-295, September 1998.
+        
+        implementation based on the python recipe (recipe-577213-1) 
+        published by Micha Kalfon
+
+        NOTE: Does not scale well with high dimensional data
+
         Parameters
         ----------
-        _u : array [1, n_features]
+        u : array [1, n_features]
             a single new data sample
         """   
+
+        # init
+        if self.iteration == 0:  
+            n_features = len(u)
+            self.mean_ = np.zeros([n_features], np.float)
+            self.covariance_ = np.zeros([n_features, n_features], np.float)
+            self.components_ = np.zeros([self.n_components,n_features], np.float)
+            self.explained_variance_ratio_ = np.zeros([self.n_components], np.float)
 
         n = float(self.iteration)
         C = self.covariance_
@@ -170,6 +173,87 @@ class IncrementalPCA(BaseEstimator, TransformerMixin):
         self.covariance_ = C
         self.components_ = V[0:self.n_components,:]
         self.explained_variance_ratio_ = E[0:self.n_components]
+
+        # normalize explained_variance_ratio_
+        self.explained_variance_ratio_ = (self.explained_variance_ratio_ / self.explained_variance_ratio_.sum())
+
+        return
+
+    def partial_fit2(self, x):
+        """ Updates the mean and components to account for a new vector.
+
+        An Implementation of the incremental pca technique (algorithm 1)
+        described in:
+
+        Skocaj, Danijel, and Ales Leonardis. "Weighted and robust incremental method
+        for subspace learning." Computer Vision, 2003. Proceedings. Ninth IEEE
+        International Conference on. IEEE, 2003.
+
+        Parameters
+        ----------
+        x : array [1, n_features]
+            a single new data sample
+        """  
+
+        # init
+        if self.iteration == 0:  
+            n_features = len(x)
+            self.mean_ = x
+            self.coeffs_ = np.zeros([1,1], np.float)
+            self.components_ = np.zeros([1,n_features], np.float)
+            self.explained_variance_ratio_ = np.zeros([1], np.float)
+
+        xm = self.mean_
+        U = self.components_
+        D = self.explained_variance_ratio_
+        A = self.coeffs_
+
+        # 1: Project a new image x into the current eigenspacea = np.dot((u-xm),U.T)
+        a = np.dot((x-xm),U.T)
+
+        # 2: Reconstruct the new image
+        y = np.dot(a,U) + xm
+
+        # 3: Compute the residual vector
+        r = x - y
+        normr = np.linalg.norm(r)	
+        if normr > 1e-9:
+            r = (r / normr)
+        else:
+            r = np.zeros_like(r)
+
+        # 4: Append r as a new basis vector
+        U = np.vstack([U,r])
+
+        # 5: Determine the coefficients in the new basis
+        A = np.vstack([A,a])
+        A = np.hstack([A,np.zeros([A.shape[0],1])])
+        A[-1,-1] = normr
+
+        # 6: Perform PCA on A. Obtain the mean value xm, the
+        # eigenvectors U, and the eigenvalues D
+        pca = PCA()
+        pca.fit(A)
+
+        # 7: Project the coefficient vectors to the new basis
+        A = pca.transform(A)
+
+        # 9: Update the mean
+        xm = xm + np.dot(pca.mean_,U)
+
+        # 8: Rotate the supspace
+        U = np.dot(pca.components_,U)
+
+        # 10: New eigenvalues
+        D = pca.explained_variance_ratio_
+
+        # Select only self.n_components largest eigenvectors and values 
+        # and update state
+        self.iteration += 1
+        self.mean = xm
+        self.components_ = U[0:self.n_components,:]
+        self.coeffs_ = A[:,0:self.n_components]
+        self.explained_variance_ratio_ = D[0:self.n_components]
 
         return
 
