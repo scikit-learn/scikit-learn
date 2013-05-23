d5c0460d58b8d54408109d7b14d32ff7f6d3b241
Joel Nothman <joel.nothman@gmail.com>
2013-05-23 21:58:00 +1000

    Ensure non-positive label indicators are 0 before summing

--- sklearn/metrics/metrics.py
+++ sklearn/metrics/metrics.py
@@ -1593,6 +1593,9 @@ def precision_recall_fscore_support(y_true, y_pred, beta=1.0, labels=None,
     if y_type == 'multilabel-indicator':
         ## sum (common) 1s in each column, or each row for average='samples' ##
         sum_axis = 1 if average == 'samples' else 0
+        # pos_label should always be 1, but we need to zero the negative indicators
+        y_true = y_true == pos_label
+        y_pred = y_pred == pos_label
         tp_sum = np.sum(np.logical_and(y_true, y_pred), axis=sum_axis)
         pos_sum = np.sum(y_pred, axis=sum_axis, dtype=int)
         true_sum = np.sum(y_true, axis=sum_axis, dtype=int)
