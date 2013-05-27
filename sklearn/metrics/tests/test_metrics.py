813203d5e48213cd336bc8c4c9d420a6c34995af
Arnaud Joly <arnaud.v.joly@gmail.com>
2013-05-27 08:44:19 +0200

    flake8

--- sklearn/metrics/tests/test_metrics.py
+++ sklearn/metrics/tests/test_metrics.py
@@ -1,8 +1,6 @@
 from __future__ import division, print_function
 
 from functools import partial
-from collections import namedtuple
-from itertools import product
 import warnings
 import numpy as np
 
@@ -21,8 +19,7 @@ from sklearn.utils.testing import (assert_true,
                                    assert_not_equal,
                                    assert_array_equal,
                                    assert_array_almost_equal,
-                                   assert_greater,
-                                   assert_false)
+                                   assert_greater)
 
 
 from sklearn.metrics import (accuracy_score,
