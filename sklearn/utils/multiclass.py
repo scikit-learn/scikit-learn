ecf135a49b65ac74a46459edacff8357b6b4c33c
Joel Nothman <joel.nothman@gmail.com>
2013-05-28 13:22:41 +1000

    ENH faster unique_labels for big sequences of sequences

--- sklearn/utils/multiclass.py
+++ sklearn/utils/multiclass.py
@@ -7,6 +7,7 @@ Multi-class / multi-label utility function
 
 """
 from collections import Sequence
+from itertools import chain
 
 import numpy as np
 
@@ -50,7 +51,7 @@ def unique_labels(*lists_of_labels):
             if is_label_indicator_matrix(y):
                 classes = np.arange(y.shape[1])
             else:
-                classes = np.array(sorted(set.union(*map(set, y))))
+                classes = np.array(sorted(set(chain(*y))))
 
         else:
             classes = np.unique(y)
