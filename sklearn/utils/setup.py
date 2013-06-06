c1e7034fcadf6ba70c7677e841d53695c36ee683
Robert Layton <robertlayton@gmail.com>
2013-06-06 14:34:38 +1000

    Missed a spot

--- sklearn/utils/setup.py
+++ sklearn/utils/setup.py
@@ -9,7 +9,6 @@ def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
 
     config = Configuration('utils', parent_package, top_path)
-    config.add_subpackage('sparsetools')
     config.add_subpackage('mst')
 
     cblas_libs, blas_info = get_blas_info()
