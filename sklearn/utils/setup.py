c04b575f3421ac2c41a19b559a1d2ad1a8043750
Robert Layton <robertlayton@gmail.com>
2013-06-06 14:55:57 +1000

    Now that the folder has more than just mst in it, rename to sparsetools, which should help with referencing it.

--- sklearn/utils/setup.py
+++ sklearn/utils/setup.py
@@ -9,7 +9,7 @@ def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
 
     config = Configuration('utils', parent_package, top_path)
-    config.add_subpackage('mst')
+    config.add_subpackage('sparsetools')
 
     cblas_libs, blas_info = get_blas_info()
 
