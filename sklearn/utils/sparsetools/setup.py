150c50e5250a717c88941f6d329996d08ace3403
Robert Layton <robertlayton@gmail.com>
2013-06-10 19:30:32 +1000

    Test for connected components and pep8 for tests

--- sklearn/utils/sparsetools/setup.py
+++ sklearn/utils/sparsetools/setup.py
@@ -1,26 +1,36 @@
+import os
 import numpy
 
-def configuration(parent_package='', top_path=None):
+from sklearn._build_utils import get_blas_info
+
+
+def configuration(parent_package='utils', top_path=None):
     from numpy.distutils.misc_util import Configuration
 
-    config = Configuration('mst', parent_package, top_path)
+    cblas_libs, blas_info = get_blas_info()
+    
+    libraries = []
+    if os.name == 'posix':
+        libraries.append('m')
+        cblas_libs.append('m')
+
+    config = Configuration('sparsetools', parent_package, top_path)
 
     config.add_extension('_min_spanning_tree',
                          sources=['_min_spanning_tree.c'],
                          include_dirs=[numpy.get_include()],
-                         #libraries=libraries
+                         libraries=libraries
                          )
     config.add_extension('_traversal',
                          sources=['_traversal.c'],
                          include_dirs=[numpy.get_include()],
-                         #libraries=libraries
+                         libraries=libraries
                          )
     config.add_extension('_graph_tools',
                          sources=['_graph_tools.c'],
                          include_dirs=[numpy.get_include()],
                          #libraries=libraries
                          )
-
     return config
 
 if __name__ == '__main__':
