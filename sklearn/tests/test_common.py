a73fd1a630d3d3cfb8f0a4ee8d81c37204fae347
Olivier Grisel <olivier.grisel@ensta.org>
2013-06-13 10:57:52 +0200

    ENH: removed leftover condition to get a wider application of the import all consistency check

--- sklearn/tests/test_common.py
+++ sklearn/tests/test_common.py
@@ -989,7 +989,7 @@ def test_import_all_consistency():
     # in the namespace of the module or package.
     for importer, modname, ispkg in pkgutil.walk_packages(
         path=sklearn.__path__, prefix='sklearn.', onerror=lambda x: None):
-        if ".tests." in modname or not ispkg:
+        if ".tests." in modname:
             continue
         package = __import__(modname, fromlist="dummy")
         for name in getattr(package, '__all__', ()):
