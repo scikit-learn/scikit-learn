474d3a00e5ca741547767f2e281b6226d43c6316
Rob Speer <rspeer@mit.edu>
2013-06-28 12:29:52 -0500

    Revise documentation about handling text and bytes.

--- sklearn/datasets/base.py
+++ sklearn/datasets/base.py
@@ -84,19 +84,25 @@ def load_files(container_path, description=None, categories=None,
                 file_44.txt
                 ...
 
-    The folder names are used has supervised signal label names. The individual
-    file names are not important.
+    The folder names are used has supervised signal label names. The
+    individual file names are not important.
 
     This function does not try to extract features into a numpy array or
     scipy sparse matrix. In addition, if load_content is false it
     does not try to load the files in memory.
 
-    To use utf-8 text files in a scikit-learn classification or clustering
-    algorithm you will first need to use the `sklearn.features.text`
+    To use text files in a scikit-learn classification or clustering
+    algorithm, you will need to use the `sklearn.feature_extraction.text`
     module to build a feature extraction transformer that suits your
     problem.
 
-    Similar feature extractors should be build for other kind of unstructured
+    If you set load_content=True, you should also specify the encoding of
+    the text using the 'charset' parameter. For many modern text files,
+    'utf-8' will be the correct encoding. If you leave charset equal to None,
+    then the content will be made of bytes instead of Unicode, and you will
+    not be able to use most functions in `sklearn.feature_extraction.text`.
+
+    Similar feature extractors should be built for other kind of unstructured
     data input such as images, audio, video, ...
 
     Parameters
