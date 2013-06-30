baffa3017b2baa7ec73a9ce148e7eeaf254247fa
Kyle Kastner <kastnerkyle@gmail.com>
2013-06-30 12:23:15 -0500

    Strengthened tests for OneVsRestClassifier decision_function

--- sklearn/tests/test_multiclass.py
+++ sklearn/tests/test_multiclass.py
@@ -187,8 +187,7 @@ def test_ovr_multilabel_decision_function():
     X_train, Y_train = X[:80], Y[:80]
     X_test, Y_test = X[80:], Y[80:]
     tst = OneVsRestClassifier(svm.SVC(probability=True)).fit(X_train, Y_train)
-    #This is pretty weak test, but at least verifies output shape
-    assert_true(tst.decision_function(X_test).shape == tst.predict_proba(X_test).shape)
+    assert_true(list((tst.decision_function(X_test) > 0).nonzero()[1]) == [x for t in tst.predict(X_test) for x in t])
 
 
 def test_ovr_single_label_decision_function():
@@ -198,8 +197,7 @@ def test_ovr_single_label_decision_function():
     X_train, Y_train = X[:80], Y[:80]
     X_test, Y_test = X[80:], Y[80:]
     tst = OneVsRestClassifier(svm.SVC(probability=True)).fit(X_train, Y_train)
-    #This is a pretty weak test, but at least verifies the output shape
-    assert_true(tst.decision_function(X_test).shape == tst.predict_proba(X_test).shape)
+    assert_true(np.all((tst.decision_function(X_test) > 0).ravel() == tst.predict(X_test)))
 
 
 def test_ovr_gridsearch():
