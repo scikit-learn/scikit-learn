7ede0df219d732d39d572c533d41d952620d22e9
Tzu-Ming Kuo <b99902073@patty.(none)>
2013-07-01 23:57:43 +0800

    MAINT liblinear class label logic (disabled)
    Pulled to make pulling in patches from upstream easier, but disabled with
    an #if 0 block because I didn't know how this would interact with our own
    modifications.  --LB
    
    Was: a932c3fe99fe03a69197dcd0c89df1e6fa86cc24
    
    Modify function group_classes in linear.cpp to ensure label[0] = 1 if class labels are +1 and -1
    
    Conflicts:
    	sklearn/svm/src/liblinear/linear.cpp

--- sklearn/svm/src/liblinear/linear.cpp
+++ sklearn/svm/src/liblinear/linear.cpp
@@ -2197,6 +2197,25 @@ static void group_classes(const problem *prob, int *nr_class_ret, int **label_re
 
         /* END MOD */
 
+#if 0
+	//
+	// Labels are ordered by their first occurrence in the training set.
+	// However, for two-class sets with -1/+1 labels and -1 appears first,
+	// we swap labels to ensure that internally the binary SVM has positive data corresponding to the +1 instances.
+	//
+	if (nr_class == 2 && label[0] == -1 && label[1] == 1)
+	{
+		swap(label[0],label[1]);
+		swap(count[0],count[1]);
+		for(i=0;i<l;i++)
+		{
+			if(data_label[i] == 0)
+				data_label[i] = 1;
+			else
+				data_label[i] = 0;
+		}
+	}
+#endif
 
 	int *start = Malloc(int,nr_class);
 	start[0] = 0;
