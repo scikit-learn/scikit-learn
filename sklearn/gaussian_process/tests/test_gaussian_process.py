6cbc43a0bdbbd6bf4094138db59f9d968b937133
John Novak <john.franc.novak@gmail.com>
2013-07-16 11:46:17 -0400

    created test for 2D output space

--- sklearn/gaussian_process/tests/test_gaussian_process.py
+++ sklearn/gaussian_process/tests/test_gaussian_process.py
@@ -65,6 +65,36 @@ def test_2d(regr=regression.constant, corr=correlation.squared_exponential,
     assert_true(np.allclose(y_pred, y) and np.allclose(MSE, 0.))
 
 
+def test_2d_2d(regr=regression.constant, corr=correlation.squared_exponential,
+            random_start=10, beta0=None):
+    """
+    MLE estimation of a two-dimensional Gaussian Process model accounting for
+    anisotropy. Check random start optimization.
+
+    Test the GP interpolation for 2D output
+    """
+    b, kappa, e = 5., .5, .1
+    g = lambda x: b - x[:, 1] - kappa * (x[:, 0] - e) ** 2.
+    f = lambda x: np.vstack((g(x), g(x))).T
+    X = np.array([[-4.61611719, -6.00099547],
+                  [4.10469096, 5.32782448],
+                  [0.00000000, -0.50000000],
+                  [-6.17289014, -4.6984743],
+                  [1.3109306, -6.93271427],
+                  [-5.03823144, 3.10584743],
+                  [-2.87600388, 6.74310541],
+                  [5.21301203, 4.26386883]])
+    y = f(X)
+    gp = GaussianProcess(regr=regr, corr=corr, beta0=beta0,
+                         theta0=[1e-2] * 2, thetaL=[1e-4] * 2,
+                         thetaU=[1e-1] * 2,
+                         random_start=random_start, verbose=False)
+    gp.fit(X, y)
+    y_pred, MSE = gp.predict(X, eval_MSE=True)
+
+    assert_true(np.allclose(y_pred, y) and np.allclose(MSE, 0.))
+
+
 @raises(ValueError)
 def test_wrong_number_of_outputs():
     gp = GaussianProcess()
@@ -82,6 +112,7 @@ def test_more_builtin_correlation_models(random_start=1):
     for corr in all_corr:
         test_1d(regr='constant', corr=corr, random_start=random_start)
         test_2d(regr='constant', corr=corr, random_start=random_start)
+        test_2d_2d(regr='constant', corr=corr, random_start=random_start)
 
 
 def test_ordinary_kriging():
@@ -93,6 +124,8 @@ def test_ordinary_kriging():
     test_1d(regr='quadratic', beta0=[0., 0.5, 0.5])
     test_2d(regr='linear', beta0=[0., 0.5, 0.5])
     test_2d(regr='quadratic', beta0=[0., 0.5, 0.5, 0.5, 0.5, 0.5])
+    test_2d_2d(regr='linear', beta0=[0., 0.5, 0.5])
+    test_2d_2d(regr='quadratic', beta0=[0., 0.5, 0.5, 0.5, 0.5, 0.5])
 
 
 def test_no_normalize():
