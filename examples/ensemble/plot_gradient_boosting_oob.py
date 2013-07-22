9b2008101a97cf4c59ceec51c6ad610f3a0cd4a1
Peter Prettenhofer <peter.prettenhofer@gmail.com>
2013-07-22 19:29:50 +0300

    plot cv error as well

--- examples/ensemble/plot_gradient_boosting_oob.py
+++ examples/ensemble/plot_gradient_boosting_oob.py
@@ -3,7 +3,15 @@
 Gradient Boosting OOB estimates
 ===============================
 
+Out-of-bag estimates can be used to estimate the "optimal" number of
+boosting iterations. To compute OOB estimates the ``subsample``
+argument has to be smaller than 1.
+The argument ``oob_improvement_[i]`` holds the improvement in loss for the i-th
+iteration.
 
+The generated plot shows the cumulative sum of the negative OOB improvements
+as a function of the boosting iteration. This function should track the
+loss on the test set.
 """
 print(__doc__)
 
@@ -14,12 +22,11 @@ print(__doc__)
 import numpy as np
 import pylab as plt
 
-from scipy.stats.distributions import binom
-
 from sklearn import ensemble
+from sklearn.cross_validation import KFold
 
 ###############################################################################
-# Generate data
+# Generate data (adapted from G. Ridgeway's gbm example)
 
 n = 1000
 rs = np.random.RandomState(13)
@@ -28,7 +35,8 @@ x2 = rs.uniform(size=n)
 x3 = rs.randint(0, 4, size=n)
 
 p = 1 / (1.0 + np.exp(-(np.sin(3 * x1) - 4 * x2 + x3)))
-y = binom.rvs(1, p, size=n)
+y = rs.binomial(1, p, size=n)
+
 X = np.c_[x1, x2, x3]
 
 X = X.astype(np.float32)
@@ -38,7 +46,7 @@ X_test, y_test = X[offset:], y[offset:]
 
 ###############################################################################
 # Fit regression model
-params = {'n_estimators': 3000, 'max_depth': 4, 'subsample': 0.5,
+params = {'n_estimators': 2000, 'max_depth': 4, 'subsample': 0.5,
           'learning_rate': 0.01, 'min_samples_leaf': 1, 'random_state': 3}
 clf = ensemble.GradientBoostingClassifier(**params)
 
@@ -49,25 +57,49 @@ print("ACC: %.4f" % acc)
 n_estimators = params['n_estimators']
 x = np.arange(n_estimators) + 1
 
-test_score = np.zeros((n_estimators,), dtype=np.float64)
 
-for i, y_pred in enumerate(clf.staged_decision_function(X_test)):
-    test_score[i] = clf.loss_(y_test, y_pred)
+def heldout_score(clf, X_test, y_test):
+    score = np.zeros((n_estimators,), dtype=np.float64)
+    for i, y_pred in enumerate(clf.staged_decision_function(X_test)):
+        score[i] = clf.loss_(y_test, y_pred)
+    return score
+
+
+def cv_estimate(n_folds=3):
+    cv = KFold(n=X_train.shape[0], n_folds=n_folds)
+    cv_clf = ensemble.GradientBoostingClassifier(**params)
+    val_scores = np.zeros((n_estimators,), dtype=np.float64)
+    for train, test in cv:
+        cv_clf.fit(X_train[train], y_train[train])
+        val_scores += heldout_score(cv_clf, X_train[test], y_train[test])
+    val_scores /= n_folds
+    return val_scores
+
+
+test_score = heldout_score(clf, X_test, y_test)
+cv_score = cv_estimate(3)
 
 
 #plt.plot(x, -np.cumsum(clf.oob_score_))
-cumsum = np.cumsum(clf.oob_improvement_)
-best_iter = x[np.argmax(cumsum)]
-true_best_iter = np.argmin(test_score)
-print("best_iter: %d" % best_iter)
-print("true_best_iter: %d" % true_best_iter)
+cumsum = -np.cumsum(clf.oob_improvement_)
+oob_best_iter = x[np.argmin(cumsum)]
+test_score -= test_score[0]
+test_best_iter = np.argmin(test_score)
+cv_score -= cv_score[0]
+cv_best_iter = np.argmin(cv_score)
 
-plt.plot(x, cumsum, label='oob', color='cyan')
-plt.plot(x, test_score, label='test', color='red')
+oob_color = map(lambda x: x / 256.0, (190, 174, 212))
+test_color = map(lambda x: x / 256.0, (127, 201, 127))
+cv_color = map(lambda x: x / 256.0, (253, 192, 134))
 
-plt.plot([best_iter, best_iter], [0.0, 1.0], color='cyan')
-plt.plot([true_best_iter, true_best_iter], [0.0, 1.0], color='red')
+plt.plot(x, cumsum, label='OOB loss', color=oob_color)
+plt.plot(x, test_score, label='Test loss', color=test_color)
+plt.plot(x, cv_score, label='CV loss', color=cv_color)
+plt.axvline(x=oob_best_iter, color=oob_color)
+plt.axvline(x=test_best_iter, color=test_color)
+plt.axvline(x=cv_best_iter, color=cv_color)
 
 plt.legend(loc='upper right')
+plt.ylabel('normalized loss')
 
 plt.show()
