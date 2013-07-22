5aefbebafbcb59a5b372845eeca382adb0983c29
Kemal Eren <kemal@kemaleren.com>
2013-07-22 17:03:52 -0400

    merged newsgroup example with Vlad's

--- examples/cluster/plot_bicluster_newsgroups.py
+++ examples/cluster/plot_bicluster_newsgroups.py
@@ -3,69 +3,102 @@
 Biclustering documents with the Spectral Co-clustering algorithm
 ================================================================
 
-This example demonstrates the Spectral Co-clustering algorithm on a
-few categories from the twenty newsgroups dataset.
+This example demonstrates the Spectral Co-clustering algorithm on
+the twenty newsgroups dataset.
 
 The TF-IDF vectorized posts form a word frequency matrix, which is
 then biclustered using Dhillon's Spectral Co-Clustering algorithm. The
 resulting document-word biclusters indicate subsets words used more
 often in those subsets documents.
 
-Another way to look at this result: given a document clustering, the
-words could be clustered to associate with each document cluster the
-words used more often in that cluster than in any other. Similarly,
-given a word clustering, the documents could be clustered in the same
-way. The Spectral Co-Clustering algorithm does both simultaneously.
-
 For each bicluster, the category percentage of its documents and its
 ten most common words get printed.
 
+The document clusters achieve a better V-measure score than clusters
+found by MiniBatchKMeans.
+
 """
+from __future__ import print_function
+
 print(__doc__)
 
-# Author: Kemal Eren <kemal@kemaleren.com>
-# License: BSD 3 clause
-
+from time import time
+import re
 from collections import Counter
 
 import numpy as np
 
-from sklearn.datasets import fetch_20newsgroups
+from sklearn.datasets.twenty_newsgroups import fetch_20newsgroups
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster.bicluster import SpectralCoclustering
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.metrics.cluster import v_measure_score
 
-categories = ['alt.atheism', 'comp.graphics']
-newsgroups = fetch_20newsgroups(subset='all', shuffle=False,
-                                categories=categories)
 
-vec = TfidfVectorizer(stop_words='english', min_df=0.01)
-data = vec.fit_transform(newsgroups.data)
+def number_aware_tokenizer(doc):
+    token_pattern = re.compile(u'(?u)\\b\\w\\w+\\b')
+    tokens = token_pattern.findall(doc)
+    tokens = ["#NUMBER" if token[0] in "0123456789_" else token
+              for token in tokens]
+    return tokens
 
-row_names = list(newsgroups.target_names[i] for i in newsgroups.target)
-feature_names = vec.get_feature_names()
 
-model = SpectralCoclustering(n_clusters=len(categories),
-                             svd_method='randomized', random_state=0)
-model.fit(data)
+vectorizer = TfidfVectorizer(stop_words='english', min_df=5,
+                             tokenizer=number_aware_tokenizer)
+cocluster = SpectralCoclustering(n_clusters=20, svd_method='arpack',
+                                 random_state=0)
+kmeans = MiniBatchKMeans(n_clusters=20, batch_size=5000, random_state=0)
 
-for i in range(model.rows_.shape[0]):
-    n_rows, n_cols = model.get_shape(i)
-    row_idx, col_idx = model.get_indices(i)
+twenty = fetch_20newsgroups()
+y_true = twenty.target
+
+print("Vectorizing...")
+X = vectorizer.fit_transform(twenty.data)
+
+print("Coclustering...")
+start_time = time()
+cocluster.fit(X)
+y_cocluster = cocluster.row_labels_
+print("Done in {}s. V-measure: {}".format(
+    time() - start_time,
+    v_measure_score(y_cocluster, y_true)))
+
+print("MiniBatchKMeans...")
+start_time = time()
+y_kmeans = kmeans.fit_predict(X)
+print("Done in {}s. V-measure: {}".format(
+    time() - start_time,
+    v_measure_score(y_kmeans, y_true)))
+
+feature_names = vectorizer.get_feature_names()
+document_names = list(twenty.target_names[i] for i in twenty.target)
+
+print("")
+print("Biclusters:")
+print("----------")
+for cluster in xrange(20):
+    n_rows, n_cols = cocluster.get_shape(cluster)
+    cluster_docs, cluster_words = cocluster.get_indices(cluster)
+    if not len(cluster_docs) or not len(cluster_words):
+        continue
 
     # categories
-    category_indices = list(row_names[i] for i in row_idx)
-    counter = Counter(category_indices)
-
+    categories = list(document_names[i] for i in cluster_docs)
+    counter = Counter(categories)
     cat_string = ", ".join("{:.0f}% {}".format(float(c) / n_rows * 100,
                                                name)
-                           for name, c in counter.most_common())
+                           for name, c in counter.most_common()[:3])
 
     # words
-    word_sum = data[row_idx, :][:, col_idx].sum(axis=0)
-    word_sum = np.asarray(word_sum).squeeze()
-    important_indices = np.argsort(word_sum)[::-1][:10]
-    words = list(feature_names[col_idx[i]] for i in important_indices)
+    out_of_cluster_docs = np.where(cocluster.row_labels_[cluster] != cluster)[0]
+    word_col = X[:, cluster_words]
+    word_scores = np.array(word_col[cluster_docs, :].sum(axis=0) -
+                           word_col[out_of_cluster_docs, :].sum(axis=0))
+    word_scores = word_scores.ravel()
+    important_words = list(feature_names[cluster_words[i]]
+                           for i in word_scores.argsort()[:-10:-1])
 
-    print "bicluster {} : {} documents, {} words".format(i, n_rows, n_cols)
-    print "categories  : {}".format(cat_string)
-    print "words       : {}\n".format(', '.join(words))
+    print("bicluster {} : {} documents, {} words".format(
+        cluster, n_rows, n_cols))
+    print("categories  : {}".format(cat_string))
+    print("words       : {}\n".format(', '.join(important_words)))
