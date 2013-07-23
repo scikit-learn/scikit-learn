d045be02277a726546865beac74b176f25c4cc6f
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2013-07-23 19:07:28 +0200

    FIX added the new module to the setup.py installation

--- sklearn/setup.py
+++ sklearn/setup.py
@@ -47,6 +47,8 @@ def configuration(parent_package='', top_path=None):
     config.add_subpackage('metrics/tests')
     config.add_subpackage('metrics/cluster')
     config.add_subpackage('metrics/cluster/tests')
+    config.add_subpackage('cross_decomposition')
+
 
     # add cython extension module for hmm
     config.add_extension(
