ba69ce46b8efdd7f13bb7b5d293ec8cfbbb3dbe9
Olivier Grisel <olivier.grisel@ensta.org>
2013-07-23 10:58:20 +0200

    Add missing test for deprecation warning

--- sklearn/tests/test_naive_bayes.py
+++ sklearn/tests/test_naive_bayes.py
@@ -3,6 +3,8 @@ from io import BytesIO
 import numpy as np
 import scipy.sparse
 
+import warnings
+
 from sklearn.utils.testing import assert_almost_equal
 from sklearn.utils.testing import assert_array_equal
 from sklearn.utils.testing import assert_array_almost_equal
@@ -224,6 +226,23 @@ def test_discretenb_provide_prior():
         assert_raises(ValueError, clf.fit, [[0], [1], [2]], [0, 1, 2])
         assert_raises(ValueError, clf.partial_fit, [[0], [1]], [0, 1],
                       classes=[0, 1, 1])
+
+
+def test_deprecated_fit_param():
+    for cls in [BernoulliNB, MultinomialNB]:
+        clf = cls()
+        with warnings.catch_warnings(record=True) as w:
+            clf.fit([[0], [1], [2]], [0, 1, 1], class_prior=[0.5, 0.5])
+
+        # Passing class_prior as a fit param should raise a deprecation
+        # warning
+        assert_equal(len(w), 1)
+        assert_equal(w[0].category, DeprecationWarning)
+
+        with warnings.catch_warnings(record=True):
+            # Inconsistent number of classes with prior
+            assert_raises(ValueError, clf.fit, [[0], [1], [2]], [0, 1, 2],
+                          class_prior=[0.5, 0.5])
 
 
 def test_sample_weight_multiclass():
