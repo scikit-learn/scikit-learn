c752ddea076b7410c95dc1845797884d3b76aad6
Kemal Eren <kemal@kemaleren.com>
2013-07-24 12:22:42 -0400

    removed plot_ from newsgroups example file

--- examples/bicluster/bicluster_newsgroups.py
+++ examples/bicluster/bicluster_newsgroups.py
@@ -21,6 +21,36 @@ For comparison, the documents are also clustered using
 MiniBatchKMeans. The document clusters derived from the biclusters
 achieve a better V-measure than clusters found by MiniBatchKMeans.
 
+Output::
+
+    Vectorizing...
+    Coclustering...
+    Done in 9.53s. V-measure: 0.4455
+    MiniBatchKMeans...
+    Done in 12.00s. V-measure: 0.3309
+
+    Best biclusters:
+    ----------------
+    bicluster 0 : 1951 documents, 4373 words
+    categories   : 23% talk.politics.guns, 19% talk.politics.misc, 14% sci.med
+    words        : gun, guns, geb, banks, firearms, drugs, gordon, clinton, cdt, amendment
+
+    bicluster 1 : 1165 documents, 3304 words
+    categories   : 29% talk.politics.mideast, 26% soc.religion.christian, 25% alt.atheism
+    words        : god, jesus, christians, atheists, kent, sin, morality, belief, resurrection, marriage
+
+    bicluster 2 : 2219 documents, 2830 words
+    categories   : 18% comp.sys.mac.hardware, 16% comp.sys.ibm.pc.hardware, 16% comp.graphics
+    words        : voltage, dsp, board, receiver, circuit, shipping, packages, stereo, compression, package
+
+    bicluster 3 : 1860 documents, 2745 words
+    categories   : 26% rec.motorcycles, 23% rec.autos, 13% misc.forsale
+    words        : bike, car, dod, engine, motorcycle, ride, honda, cars, bmw, bikes
+
+    bicluster 4 : 12 documents, 155 words
+    categories   : 100% rec.sport.hockey
+    words        : scorer, unassisted, reichel, semak, sweeney, kovalenko, ricci, audette, momesso, nedved
+
 """
 from __future__ import print_function
 
