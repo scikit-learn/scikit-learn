5b9e679998ea69ca23caa671d71f999b53c84411
Vlad Niculae <vlad@vene.ro>
2013-07-24 15:54:42 +0200

    Consistency for floats in consensus score

--- sklearn/metrics/cluster/bicluster/bicluster_metrics.py
+++ sklearn/metrics/cluster/bicluster/bicluster_metrics.py
@@ -74,7 +74,7 @@ def consensus_score(a, b, similarity="jaccard"):
     if similarity == "jaccard":
         similarity = _jaccard
     matrix = _pairwise_similarity(a, b, similarity)
-    indices = linear_assignment(1 - matrix)
+    indices = linear_assignment(1. - matrix)
     n_a = len(a[0])
     n_b = len(b[0])
-    return np.trace(matrix[:, indices[:, 1]]).sum() / max(n_a, n_b)
+    return np.trace(matrix[:, indices[:, 1]]) / max(n_a, n_b)
