73591a16471d0a9203a0c4e7c56f7606b827f4ba
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2013-07-24 22:35:44 +0200

    TEST test that ran with ward linkage now also run with average and complete linkage

--- sklearn/cluster/__init__.py
+++ sklearn/cluster/__init__.py
@@ -8,7 +8,7 @@ from .mean_shift_ import mean_shift, MeanShift, estimate_bandwidth, \
     get_bin_seeds
 from .affinity_propagation_ import affinity_propagation, AffinityPropagation
 from .hierarchical import ward_tree, Ward, WardAgglomeration, \
-    AgglomerativeClustering
+    AgglomerativeClustering, linkage_tree
 from .k_means_ import k_means, KMeans, MiniBatchKMeans
 from .dbscan_ import dbscan, DBSCAN
 from .bicluster import SpectralBiclustering, SpectralCoclustering
