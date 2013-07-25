732003ef0050816d7d006433ba13b96d57c72473
Gael Varoquaux <gael.varoquaux@normalesup.org>
2013-07-25 08:52:18 +0200

    MISC: move fast_dict to utils

--- sklearn/utils/tests/test_fast_dict.py
+++ sklearn/utils/tests/test_fast_dict.py
@@ -2,13 +2,16 @@
 """
 import numpy as np
 
-from fast_dict import IntFloatDict#, max_merge
+from sklearn.utils.fast_dict import IntFloatDict#, max_merge
 
 def test_int_float_dict():
-    keys = np.random.randint(100, size=10).astype(np.int32)
-    values = np.random.random(size=10)
+    rng = np.random.RandomState(0)
+    keys = np.unique(rng.randint(100, size=10).astype(np.int32))
+    values = rng.rand(len(keys))
 
     d = IntFloatDict(keys, values)
+    for key, value in zip(keys, values):
+        assert d[key] == value
 
     #other_keys = np.arange(50).astype(np.int32)[::2]
     #other_values = 0.5*np.ones(50)[::2]
@@ -16,5 +19,7 @@ def test_int_float_dict():
     #c = max_merge(d, other, mask=np.ones(100, dtype=np.int32))
 
     d.append(120, 3.)
-    [d.append(i+1000, 3.0) for i in xrange(2000)]
+    assert d[120] == 3.0
+    [d.append(i+1000, 4.0) for i in xrange(2000)]
+    assert d[1100] == 4.0
 
