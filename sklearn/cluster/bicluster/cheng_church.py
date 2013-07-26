fixed precision in cython

sklearn/cluster/bicluster/cheng_church.py
@@ -196,23 +196,17 @@ class ChengChurch(six.with_metaclass(ABCMeta, BaseEstimator,
         results = []
 
         for i in range(self.n_clusters):
-            try:
-                rows = np.arange(n_rows, dtype=np.int)[None].T
-                cols = np.arange(n_cols, dtype=np.int)
-                rows, cols = self._multiple_node_deletion(rows, cols, X)
-                rows, cols = self._single_node_deletion(rows, cols, X)
-                rows, cols = self._node_addition(rows, cols, X)
-                self._mask(X, rows, cols, generator, minval, maxval)
-                if len(rows) == 0 or len(cols) == 0:
-                    break
-                results.append((rows.ravel(), cols))
-            except EmptyBiclusterException:
+            rows = np.arange(n_rows, dtype=np.int)[None].T
+            cols = np.arange(n_cols, dtype=np.int)
+            rows, cols = self._multiple_node_deletion(rows, cols, X)
+            rows, cols = self._single_node_deletion(rows, cols, X)
+            rows, cols = self._node_addition(rows, cols, X)
+            self._mask(X, rows, cols, generator, minval, maxval)
+            if len(rows) == 0 or len(cols) == 0:
                 break
-        if results:
-            indicators = (get_indicators(r, c, X.shape) for r, c in results)
-            rows, cols = zip(*indicators)
-            self.rows_ = np.vstack(rows)
-            self.columns_ = np.vstack(cols)
-        else:
-            self.rows_ = np.array([])
-            self.columns_ = np.array([])
+            results.append((rows.ravel(), cols))
+
+        indicators = (get_indicators(r, c, X.shape) for r, c in results)
+        rows, cols = zip(*indicators)
+        self.rows_ = np.vstack(rows)
+        self.columns_ = np.vstack(cols)

sklearn/cluster/bicluster/tests/test_cheng_church.py
@@ -25,12 +25,18 @@ def test_cheng_church():
 
 
 def test_empty_biclusters():
-    generator = np.random.RandomState(0)
-    data = generator.uniform(0, 1000, (50, 50))
-    model = ChengChurch(n_clusters=1, max_msr=0)
-    model.fit(data)
-    assert_equal(len(model.rows_), 0)
-    assert_equal(len(model.columns_), 0)
+    """Cheng and Church should always find at least one bicluster.
+
+    The MSR of a bicluster with one row or one column is zero.
+
+    """
+    for i in range(10):
+        generator = np.random.RandomState(i)
+        data = generator.uniform(0, 1000, (50, 50))
+        model = ChengChurch(n_clusters=1, max_msr=0)
+        model.fit(data)
+        assert_equal(len(model.rows_), 1)
+        assert_equal(len(model.columns_), 1)
 
 
 def test_single_node_deletion():