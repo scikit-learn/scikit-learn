0db387f0810f2b117e75ce64fe4d8b08401c0653
Gael Varoquaux <gael.varoquaux@normalesup.org>
2013-07-26 17:06:52 +0200

    MAINT: update copy_joblib script

--- sklearn/externals/copy_joblib.sh
+++ sklearn/externals/copy_joblib.sh
@@ -2,10 +2,9 @@
 # Script to do a local install of joblib
 rm -rf tmp joblib
 mkdir -p tmp/lib/python2.7/site-packages
-ln -s tmp/lib/python2.7 tmp/lib/python2.5
 ln -s tmp/lib/python2.7 tmp/lib/python2.6
 mkdir -p tmp/bin
-export PYTHONPATH=$(pwd)/tmp/lib/python2.7/site-packages:$(pwd)/tmp/lib/python2.6/site-packages:$(pwd)/tmp/lib/python2.5/site-packages
+export PYTHONPATH=$(pwd)/tmp/lib/python2.7/site-packages:$(pwd)/tmp/lib/python2.6/site-packages
 easy_install -Zeab tmp joblib
 old_pwd=$(pwd)
 #cd /home/varoquau/dev/joblib/ 
