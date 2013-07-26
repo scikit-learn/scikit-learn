8c9dbc99dc83b16b87f30b497b2309f5de372704
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2013-07-26 10:01:05 +0200

    TST added tests for encoding/charset deprecation
    Checking the old interface still works.

--- sklearn/feature_extraction/tests/test_text.py
+++ sklearn/feature_extraction/tests/test_text.py
@@ -176,6 +176,11 @@ def test_unicode_decode_error():
                          encoding='ascii').build_analyzer()
     assert_raises(UnicodeDecodeError, ca, text_bytes)
 
+    # Check the old interface
+    ca = CountVectorizer(analyzer='char', ngram_range=(3, 6),
+                         charset='ascii').build_analyzer()
+    assert_raises(UnicodeDecodeError, ca, text_bytes)
+
 
 def test_char_ngram_analyzer():
     cnga = CountVectorizer(analyzer='char', strip_accents='unicode',
