31ae3cae9bf6c9f76005441a3faafe96091207fc
Nicolas Trésegnie <nicolas.tresegnie@gmail.com>
2013-07-27 13:32:56 +0200

    Add score_overrides_loss to _deprecate_loss_and_score_funcs

--- sklearn/metrics/scorer.py
+++ sklearn/metrics/scorer.py
@@ -143,7 +143,12 @@ class _ThresholdScorer(_BaseScorer):
     def _factory_args(self):
         return ", needs_threshold=True"
 
-def _deprecate_loss_and_score_funcs(loss_func, score_func, scoring):
+def _deprecate_loss_and_score_funcs(
+        loss_func, score_func, scoring,
+        score_overrides_loss=False):
+
+    scorer = None
+    if loss_func is not None or score_func is not None:
 
         if loss_func is not None:
             warnings.warn("Passing a loss function is "
@@ -151,23 +156,24 @@ def _deprecate_loss_and_score_funcs(loss_func, score_func, scoring):
                           "Either use strings or score objects."
                           "The relevant new parameter is called ''scoring''. ")
             scorer = make_scorer(loss_func, greater_is_better=False)
-        elif score_func is not None:
+        if score_func is not None:
             warnings.warn("Passing function as ``score_func`` is "
                           "deprecated and will be removed in 0.15. "
                           "Either use strings or score objects."
                           "The relevant new parameter is called ''scoring''.")
-            scorer = make_scorer(score_func)
-        elif isinstance(scoring, six.string_types):
-            try:
-                scorer = SCORERS[scoring]
-            except KeyError:
-                raise ValueError('%r is not a valid scoring value. '
-                                 'Valid options are %s' % (scoring,
-                                 sorted(SCORERS.keys())))
-        else:
-            scorer = scoring
+            if loss_func is None or score_overrides_loss:
+                scorer = make_scorer(score_func)
 
-        return scorer
+    elif isinstance(scoring, six.string_types):
+        try:
+            scorer = SCORERS[scoring]
+        except KeyError:
+            raise ValueError('%r is not a valid scoring value. '
+                             'Valid options are %s' % (scoring,
+                             sorted(SCORERS.keys())))
+    else:
+        scorer = scoring
+    return scorer
 
 def make_scorer(score_func, greater_is_better=True, needs_proba=False,
                 needs_threshold=False, **kwargs):
