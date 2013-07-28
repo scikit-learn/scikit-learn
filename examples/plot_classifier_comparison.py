04f7d4989e3cd318013cb66b76671babb8c00046
Lars Buitinck <larsmans@gmail.com>
2013-07-28 13:07:20 +0200

    DOC copyedit example docstring

--- examples/plot_classifier_comparison.py
+++ examples/plot_classifier_comparison.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 """
-======================
-Classifiers Comparison
-======================
+=====================
+Classifier comparison
+=====================
 
 A comparison of a several classifiers in scikit-learn on synthetic datasets.
 The point of this example is to illustrate the nature of decision boundaries
@@ -12,9 +12,9 @@ of different classifiers.
 This should be taken with a grain of salt, as the intuition conveyed by
 these examples does not necessarily carry over to real datasets.
 
-In particular in high dimensional spaces data can more easily be separated
+Particularly in high-dimensional spaces, data can more easily be separated
 linearly and the simplicity of classifiers such as naive Bayes and linear SVMs
-might lead to better generalization.
+might lead to better generalization than is achieved by other classifiers.
 
 The plots show training points in solid colors and testing points
 semi-transparent. The lower right shows the classification accuracy on the test
