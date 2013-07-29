89011e369a2954e0ac1585368d1dc197786bf2d3
Gael Varoquaux <gael.varoquaux@normalesup.org>
2013-07-29 02:44:50 +0200

    MISC: deprecate balance_weights (it's internal)

--- sklearn/preprocessing/tests/test_weights.py
+++ sklearn/preprocessing/tests/test_weights.py
@@ -1,13 +1,13 @@
-from sklearn.preprocessing import balance_weights
+from sklearn.preprocessing._weights import _balance_weights
 from sklearn.utils.testing import assert_array_equal
 
 
 def test_balance_weights():
-    weights = balance_weights([0, 0, 1, 1])
+    weights = _balance_weights([0, 0, 1, 1])
     assert_array_equal(weights, [1., 1., 1., 1.])
 
-    weights = balance_weights([0, 1, 1, 1, 1])
+    weights = _balance_weights([0, 1, 1, 1, 1])
     assert_array_equal(weights, [1., 0.25, 0.25, 0.25, 0.25])
 
-    weights = balance_weights([0, 0])
+    weights = _balance_weights([0, 0])
     assert_array_equal(weights, [1., 1.])
