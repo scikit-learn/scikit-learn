6d28ca532170b37f3a738b9546309ddb23a29644
Issam Laradji <issam.laradji@gmail.com>
2013-08-04 07:47:43 +0300

    rebased

--- sklearn/neural_network/__init__.py
+++ sklearn/neural_network/__init__.py
@@ -5,8 +5,12 @@ networks.
 """
 
 from .rbm import BernoulliRBM
+<<<<<<< HEAD
 
 __all__ = ['BernoulliRBM']
 
 from .mlp import MLPClassifier
 (WIP) Added Multi-layer perceptron (MLP)
+=======
+from .mlp import MultilayerPerceptronClassifier
+>>>>>>> rebased
--- sklearn/setup.py
+++ sklearn/setup.py
@@ -42,10 +42,12 @@ def configuration(parent_package='', top_path=None):
     config.add_subpackage('gaussian_process/tests')
     config.add_subpackage('neighbors')
     config.add_subpackage('neural_network')
-<<<<<<< HEAD
     config.add_subpackage('preprocessing')
+<<<<<<< HEAD
 =======
 >>>>>>> added neural_network to setup.py
+=======
+>>>>>>> rebased
     config.add_subpackage('manifold')
     config.add_subpackage('metrics')
     config.add_subpackage('semi_supervised')
