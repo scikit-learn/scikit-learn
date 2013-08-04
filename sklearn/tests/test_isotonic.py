40b42e101e886057f116b263e9dbd840b3fbffc9
Karol Pysniak <kpysniak@gmail.com>
2013-08-03 18:27:09 -0700

    Added unit tests for parameters in isotonic regression

--- sklearn/tests/test_isotonic.py
+++ sklearn/tests/test_isotonic.py
@@ -45,6 +45,47 @@ def test_assert_raises_exceptions():
     assert_raises(ValueError, ir.transform, rng.randn(3, 10))
 
 
+def test_isotonic_sample_weight_parameter_default_value():
+    # check if default value of sample_weight parameter is one
+    ir = IsotonicRegression();
+    sample_weight_default_value = 1;
+    # random test data
+    rng = np.random.RandomState(42);
+    n = 100;
+    x = np.arange(n);
+    y = rng.randint(-50, 50, size=(n,)) + 50. * np.log(1 + np.arange(n));
+    # check if value is correctly used
+    weights = np.ones(n);
+    y_set_value     = ir.fit_transform(x,y,sample_weight=weights);
+    y_default_value = ir.fit_transform(x,y);
+
+    assert_array_equal(y_set_value,y_default_value);
+
+
+def test_isotonic_min_max_boundaries():
+    # check if min value is used correctly
+    ir = IsotonicRegression();
+    ir.y_min = 2;
+    ir.y_max = 4;
+    n = 6;
+    x = np.arange(n);
+    y = np.arange(n);
+    y_test   = [2,2,2,3,4,4];
+    y_result = np.round(ir.fit_transform(x,y));
+    assert_array_equal(y_result,y_test);
+
+
+def test_isotonic_sample_weight():
+    ir = IsotonicRegression();
+    x = [1,2,3,4,5,6,7];
+    y = [1,41,51,1,2,5,24];
+    sample_weight = [1,2,3,4,5,6,7];
+    expected_y = [1,13.95,13.95,13.95,13.95,13.95,24];
+    received_y = ir.fit_transform(x,y,sample_weight=sample_weight);
+
+    assert_array_equal(expected_y,received_y);
+
+
 if __name__ == "__main__":
     import nose
     nose.run(argv=['', __file__])
