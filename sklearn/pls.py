33cf98cd5f9853b6dc15bcaa0133eaed4f6740b3
Gael Varoquaux <gael.varoquaux@normalesup.org>
2013-08-07 23:24:11 +0200

    MISC: deprection is in 2 releases

--- sklearn/pls.py
+++ sklearn/pls.py
@@ -4,4 +4,4 @@ from .cross_decomposition import CCA, PLSSVD, PLSRegression, PLSCanonical
 
 
 warnings.warn("This module has been moved to cross_decomposition and will be "
-              "removed in 0.15", DeprecationWarning)
+              "removed in 0.16", DeprecationWarning)
