5902a9e6ad726e291666d65909c376c61ad2965e
Arnaud Joly <arnaud.v.joly@gmail.com>
2013-08-07 10:42:02 +0200

    FIX search and replace misstake

--- sklearn/metrics/metrics.py
+++ sklearn/metrics/metrics.py
@@ -348,10 +348,10 @@ def auc_score(y_true, y_score):
     Examples
     --------
     >>> import numpy as np
-    >>> from sklearn.metrics import roc_auc_score
+    >>> from sklearn.metrics import auc_score
     >>> y_true = np.array([0, 0, 1, 1])
     >>> y_scores = np.array([0.1, 0.4, 0.35, 0.8])
-    >>> roc_auc_score(y_true, y_scores)
+    >>> auc_score(y_true, y_scores)
     0.75
 
     """
