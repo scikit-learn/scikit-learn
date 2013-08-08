c71685e6835209d590ca531362ffe54392f0cc00
Karol Pysniak <kpysniak@gmail.com>
2013-08-07 21:05:20 -0700

    Fixed passing y_max and y_min to IsotonicRegression in test_isotonic

--- sklearn/tests/test_isotonic.py
+++ sklearn/tests/test_isotonic.py
@@ -64,9 +64,7 @@ def test_isotonic_sample_weight_parameter_default_value():
 
 def test_isotonic_min_max_boundaries():
     # check if min value is used correctly
-    ir = IsotonicRegression()
-    ir.y_min = 2
-    ir.y_max = 4
+    ir = IsotonicRegression(y_min = 2, y_max = 4)
     n = 6
     x = np.arange(n)
     y = np.arange(n)
