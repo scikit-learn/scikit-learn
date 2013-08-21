667e8269fd13ebb3a848f9ae19c328516ae0dcd5
Gilles Louppe <g.louppe@gmail.com>
2013-08-21 22:59:14 +0200

    DOC: bias-variance example (continued)

--- examples/ensemble/plot_bias_variance.py
+++ examples/ensemble/plot_bias_variance.py
@@ -11,27 +11,42 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 from sklearn.ensemble import BaggingRegressor
+from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 
 # Settings
-n_repeat = 50       # Number of repetitions
-n_train = 100       # Size of the training set
-n_test = 5000       # Size of the test set
-eps = 0.05          # Standard deviation of the noise
-
-estimator = DecisionTreeRegressor(min_samples_split=1)
-bagging = BaggingRegressor(estimator, n_estimators=50)
-
+n_repeat = 50       # Number of iterations for computing expectations
+n_train = 50        # Size of the training set
+n_test = 1000       # Size of the test set
+noise = 0.1         # Standard deviation of the noise
 np.random.seed(0)
+
+estimators = [("Tree", DecisionTreeRegressor()),
+              ("Bagging(Tree)", BaggingRegressor(DecisionTreeRegressor(),
+                                                 n_estimators=50)),
+              ("KNN", KNeighborsRegressor()),
+              ("Bagging(KNN)", BaggingRegressor(KNeighborsRegressor(),
+                                                n_estimators=50))]
+
+n_estimators = len(estimators)
 
 # Generate data
 def f(x):
+    x = x.reshape((-1,))
     return np.exp(-x ** 2) + 1.5 * np.exp(-(x - 2) ** 2)
 
-def generate(n_samples, eps):
+def generate(n_samples, noise, n_repeat=1):
     X = np.random.rand(n_samples) * 10 - 5
     X = np.sort(X)
-    y = f(X) + np.random.normal(0.0, eps, n_samples)
+
+    if n_repeat == 1:
+        y = f(X) + np.random.normal(0.0, noise, n_samples)
+    else:
+        y = np.zeros((n_samples, n_repeat))
+
+        for i in range(n_repeat):
+            y[:, i] = f(X) + np.random.normal(0.0, noise, n_samples)
+
     X = X.reshape((n_samples, 1))
 
     return X, y
@@ -40,63 +55,66 @@ X_train = []
 y_train = []
 
 for i in range(n_repeat):
-    X, y = generate(n_samples=n_train, eps=eps)
+    X, y = generate(n_samples=n_train, noise=noise)
     X_train.append(X)
     y_train.append(y)
 
-X_test, y_test = generate(n_samples=n_test, eps=eps)
+X_test, y_test = generate(n_samples=n_test, noise=noise, n_repeat=n_repeat)
 
-# Build estimators and compute predictions
-y_predict_estimator = np.zeros((n_test, n_repeat))
-y_predict_bagging = np.zeros((n_test, n_repeat))
+# Loop over estimators to compare
+for n, (name, estimator) in enumerate(estimators):
+    # Compute predictions
+    y_predict = np.zeros((n_test, n_repeat))
 
-for i in xrange(n_repeat):
-    estimator.fit(X_train[i], y_train[i])
-    y_predict_estimator[:, i] = estimator.predict(X_test)
-    bagging.fit(X_train[i], y_train[i])
-    y_predict_bagging[:, i] = bagging.predict(X_test)
+    for i in xrange(n_repeat):
+        estimator.fit(X_train[i], y_train[i])
+        y_predict[:, i] = estimator.predict(X_test)
 
-# Bias^2 + Variance decomposition of the mean squared error
-X_test = X_test[:, 0]
+    # Bias^2 + Variance + Noise decomposition of the expected mean squared error
+    y_error = np.zeros(n_test)
 
-y_error_estimator = np.zeros(n_test)
-y_error_bagging = np.zeros(n_test)
+    for i in range(n_repeat):
+        for j in range(n_repeat):
+            y_error += (y_test[:, j] - y_predict[:, i]) ** 2
 
-for i in range(n_repeat):
-    y_error_estimator += (f(X_test) - y_predict_estimator[:, i]) ** 2
-    y_error_bagging += (f(X_test) - y_predict_bagging[:, i]) ** 2
+    y_error /= (n_repeat * n_repeat)
 
-y_error_estimator /= n_repeat
-y_error_bagging /= n_repeat
+    y_noise = np.var(y_test, axis=1)
+    y_bias = (f(X_test) - np.mean(y_predict, axis=1)) ** 2
+    y_var = np.var(y_predict, axis=1)
 
-y_bias_estimator = (f(X_test) - np.mean(y_predict_estimator, axis=1)) ** 2
-y_var_estimator = np.var(y_predict_estimator, axis=1)
-y_bias_bagging = (f(X_test) - np.mean(y_predict_bagging, axis=1)) ** 2
-y_var_bagging = np.var(y_predict_bagging, axis=1)
+    print "%s: %f (mse) = %f (bias^2) + %f (var) + %f (noise)" % (
+        name,
+        np.mean(y_error),
+        np.mean(y_bias),
+        np.mean(y_var),
+        np.mean(y_noise))
 
-print "Single estimator: %f (mse) = %f (bias^2) + %f (var)" % (np.mean(y_error_estimator), np.mean(y_bias_estimator), np.mean(y_var_estimator))
-print "Bagging: %f (mse) = %f (bias^2) + %f (var)" % (np.mean(y_error_bagging), np.mean(y_bias_bagging), np.mean(y_var_bagging))
+    # Plot
+    plt.subplot(2, n_estimators, n + 1)
+    plt.plot(X_test, f(X_test), "b", label="mean(f(x))")
 
-# Plot
-plt.subplot(2, 1, 1)
+    for i in range(n_repeat):
+        plt.plot(X_test, y_predict[:, i], "r", alpha=0.05)
 
-for i in range(n_repeat):
-    plt.plot(X_test, y_predict_estimator[:, i], "r", alpha=0.05)
+    plt.plot(X_test, np.mean(y_predict, axis=1), "r", label="mean prediction")
 
-plt.plot(X_test, f(X_test), "b", label="f(x)")
-plt.plot(X_test, np.mean(y_predict_estimator, axis=1), "r", label="mean prediction (single estimator)")
-plt.plot(X_test, np.mean(y_predict_bagging, axis=1), "-.r", label="mean prediction (bagging)")
-plt.legend(loc="best", prop={"size": 9})
+    plt.xlim([-5, 5])
+    plt.title(name)
 
-plt.subplot(2, 1, 2)
+    if n == 0:
+        plt.legend(loc="upper left", prop={"size": 9})
 
-plt.plot(X_test, y_error_estimator, "r", label="mse (single estimator)")
-plt.plot(X_test, y_bias_estimator, "b", label="bias^2 (single estimator")
-plt.plot(X_test, y_var_estimator, "g", label="var (single estimator)")
+    plt.subplot(2, n_estimators, n_estimators + n + 1)
+    plt.plot(X_test, y_error, "r", label="mean squared error")
+    plt.plot(X_test, y_bias, "b", label="bias^2"),
+    plt.plot(X_test, y_var, "g", label="variance"),
+    plt.plot(X_test, y_noise, "c", label="noise")
 
-plt.plot(X_test, y_error_bagging, "-.r", label="mse (bagging)")
-plt.plot(X_test, y_bias_bagging, "-.b", label="bias^2 (bagging)")
-plt.plot(X_test, y_var_bagging, "-.g", label="var (bagging)")
-plt.legend(loc="best", prop={"size": 9})
+    plt.xlim([-5, 5])
+    plt.ylim([0, 0.1])
+
+    if n == 0:
+        plt.legend(loc="upper left", prop={"size": 9})
 
 plt.show()
