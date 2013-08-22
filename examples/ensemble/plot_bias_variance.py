e563097c085bf6a07bb944d901e844466b863050
Gilles Louppe <g.louppe@gmail.com>
2013-08-22 11:55:35 +0200

    DOC: wording on the example

--- examples/ensemble/plot_bias_variance.py
+++ examples/ensemble/plot_bias_variance.py
@@ -6,14 +6,14 @@ Single estimator versus bagging: bias-variance decomposition
 This example illustrates and compares the bias-variance decomposition of the
 expected mean squared error of a single estimator against a bagging ensemble.
 
-In regression, the expected mean squared error of an estimator can be decomposed
-in terms of bias, variance and noise. On average over dataset instances LS of
-the regression problem, the bias term measures the difference between the
-predictions of the estimator and the predictions of the best possible estimator
-for the problem (i.e., the Bayes model). The variance term measures the
-variability of the predictions of the estimator when fit over different
-instances LS of the problem. Finally, the noise measures the irreducible part of
-the error which is due the variability in the data.
+In regression, the expected mean squared error of an estimator can be
+decomposed in terms of bias, variance and noise. On average over dataset
+instances LS of the regression problem, the bias term measures the difference
+between the predictions of the estimator and the predictions of the best
+possible estimator for the problem (i.e., the Bayes model). The variance term
+measures the variability of the predictions of the estimator when fit over
+different instances LS of the problem. Finally, the noise measures the
+irreducible part of the error which is due the variability in the data.
 
 The upper left figure illustrates the predictions (in dark red) of a single
 decision tree trained over a random instance LS (the blue dots) of a toy
@@ -29,9 +29,9 @@ close to each other) while the variance is large (the red beam is rather wide).
 
 The lower left figure plots the pointwise decomposition of the expected mean
 squared error of a single decision tree. It confirms that the bias term (in
-blue) is low while the variance is large (in green). It also illustrates
-the noise part of the error which, as expected, appears to be constant and
-around `0.01`.
+blue) is low while the variance is large (in green). It also illustrates the
+noise part of the error which, as expected, appears to be constant and around
+`0.01`.
 
 The right figures corespond to the same plots but using instead a bagging
 ensemble of decision trees. In both figures, we can observe that the bias term
@@ -40,14 +40,15 @@ between the average prediction (in cyan) and the best possible model is larger
 (e.g., notice the offset around `x=2`). In the lower right figure, the bias
 curve is also slightly higher than in the lower left figure. In terms of
 variance however, the beam of predictions is narrower, which suggests that the
-variance is lower. Indeed, as the lower right figure confirms, the variance term
-(in green) is lower than for single decision trees. Overall, the bias-variance
-decomposition is therefore no longer the same. The tradeoff is better for
-bagging: increasing slightly the bias term allows for a larger reduction of the
-variance, which results in a lower overall mean squared error (compare the red
-curves the lower figures). The script output also confirms this intuition. The
-error of the bagging ensemble is lower than the error of a single decision tree
-and this difference indeed mainly stems from a lower variance.
+variance is lower. Indeed, as the lower right figure confirms, the variance
+term (in green) is lower than for single decision trees. Overall, the bias-
+variance decomposition is therefore no longer the same. The tradeoff is better
+for bagging: averaging several decision trees fit on bootstrap copies of the
+dataset slightly increases the bias term but allows for a larger reduction of
+the variance, which results in a lower overall mean squared error (compare the
+red curves the lower figures). The script output also confirms this intuition.
+The error of the bagging ensemble is lower than the error of a single decision
+tree and this difference indeed mainly stems from a reduced variance.
 
 """
 print(__doc__)
@@ -66,6 +67,7 @@ n_test = 1000       # Size of the test set
 noise = 0.1         # Standard deviation of the noise
 np.random.seed(0)
 
+# Change this for exploring the bias-variance decomposition of other estimators
 estimators = [("Tree", DecisionTreeRegressor()),
               ("Bagging(Tree)", BaggingRegressor(DecisionTreeRegressor()))]
 
@@ -112,7 +114,7 @@ for n, (name, estimator) in enumerate(estimators):
         estimator.fit(X_train[i], y_train[i])
         y_predict[:, i] = estimator.predict(X_test)
 
-    # Bias^2 + Variance + Noise decomposition of the expected mean squared error
+    # Bias^2 + Variance + Noise decomposition of the mean squared error
     y_error = np.zeros(n_test)
 
     for i in range(n_repeat):
@@ -143,7 +145,8 @@ for n, (name, estimator) in enumerate(estimators):
         else:
             plt.plot(X_test, y_predict[:, i], "r", alpha=0.05)
 
-    plt.plot(X_test, np.mean(y_predict, axis=1), "c", label="$\mathbb{E}_{LS} \^y(x)$")
+    plt.plot(X_test, np.mean(y_predict, axis=1), "c",
+             label="$\mathbb{E}_{LS} \^y(x)$")
 
     plt.xlim([-5, 5])
     plt.title(name)
