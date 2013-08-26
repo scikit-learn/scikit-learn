e3583daa2f196b5822f784c7e53f081668ecef6d
Lars Buitinck <L.J.Buitinck@uva.nl>
2013-08-26 17:01:48 +0200

    COSMIT skip some repeated computations in k-means

--- sklearn/cluster/k_means_.py
+++ sklearn/cluster/k_means_.py
@@ -847,27 +847,26 @@ def _mini_batch_step(X, x_squared_norms, centers, counts,
     # Perform label assignment to nearest centers
     nearest_center, inertia = _labels_inertia(X, x_squared_norms, centers,
                                               distances=distances)
+
     if random_reassign and reassignment_ratio > 0:
         random_state = check_random_state(random_state)
         # Reassign clusters that have very low counts
         to_reassign = np.logical_or(
             (counts <= 1), counts <= reassignment_ratio * counts.max())
-        number_of_reassignments = to_reassign.sum()
-        if number_of_reassignments:
+        n_reassigns = min(to_reassign.sum(), X.shape[0])
+        if n_reassigns:
             # Pick new clusters amongst observations with probability
             # proportional to their closeness to their center.
             # Flip the ordering of the distances.
             distances -= distances.max()
             distances *= -1
-            rand_vals = random_state.rand(number_of_reassignments)
+            rand_vals = random_state.rand(n_reassigns)
             rand_vals *= distances.sum()
             new_centers = np.searchsorted(distances.cumsum(),
                                           rand_vals)
             if verbose:
-                n_reassigns = to_reassign.sum()
-                if n_reassigns:
-                    print("[MiniBatchKMeans] Reassigning %i cluster centers."
-                          % n_reassigns)
+                print("[MiniBatchKMeans] Reassigning %i cluster centers."
+                      % n_reassigns)
 
             if sp.issparse(X) and not sp.issparse(centers):
                 assign_rows_csr(X, new_centers, np.where(to_reassign)[0],
@@ -875,7 +874,7 @@ def _mini_batch_step(X, x_squared_norms, centers, counts,
             else:
                 centers[to_reassign] = X[new_centers]
 
-    # implementation for the sparse CSR reprensation completely written in
+    # implementation for the sparse CSR representation completely written in
     # cython
     if sp.issparse(X):
         return inertia, _k_means._mini_batch_update_csr(
