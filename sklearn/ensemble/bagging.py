441c1bd440dd2026a1c6f551de1e5b73b288750b
Maheshakya Wijewardena <maheshakya@localhost.localdomain>
2013-08-27 08:48:22 +0530

    External libraries imported

--- sklearn/ensemble/bagging.py
+++ sklearn/ensemble/bagging.py
@@ -2,3 +2,12 @@
 
 #Author: Maheshakya Wijewardena <pmaheshakya4@gmail.com>
 #License: BSD 3 clause
+
+from __future__ import division
+
+import itertools
+import numbers
+import numpy as np
+from warnings import warn
+from abc import ABCMeta, abstractmethod
+from inspect import getargspec
