5f5e96af8a18b7581f4e99106052961869bb2871
Joel Nothman <joel.nothman@gmail.com>
2013-09-11 00:21:00 +1000

    TST classification_report example that does not trigger warning

--- sklearn/metrics/metrics.py
+++ sklearn/metrics/metrics.py
@@ -1723,17 +1723,17 @@ def classification_report(y_true, y_pred, labels=None, target_names=None):
     Examples
     --------
     >>> from sklearn.metrics import classification_report
-    >>> y_true = [0, 1, 2, 2, 0]
-    >>> y_pred = [0, 0, 2, 2, 0]
+    >>> y_true = [0, 1, 2, 2, 2]
+    >>> y_pred = [0, 0, 2, 2, 1]
     >>> target_names = ['class 0', 'class 1', 'class 2']
     >>> print(classification_report(y_true, y_pred, target_names=target_names))
                  precision    recall  f1-score   support
     <BLANKLINE>
-        class 0       0.67      1.00      0.80         2
+        class 0       0.50      1.00      0.67         1
         class 1       0.00      0.00      0.00         1
-        class 2       1.00      1.00      1.00         2
+        class 2       1.00      0.67      0.80         3
     <BLANKLINE>
-    avg / total       0.67      0.80      0.72         5
+    avg / total       0.70      0.60      0.61         5
     <BLANKLINE>
 
     """
