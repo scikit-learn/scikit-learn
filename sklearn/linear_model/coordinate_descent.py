67b143bc553316917f4e6bb6cb309b57f3952297
Lars Buitinck <larsmans@gmail.com>
2013-09-16 13:35:41 +0200

    Partially revert "MAINT remove deprecated code from CD"
    This reverts commit 680234d3687c22023d99a439ee832d9d33136c9d.
    
    The dictionary learning code seems to rely on coef_init (even
    though CD docstrings say it's a no-op). I leave it to @vene and
    @agramfort to sort this out further.

--- sklearn/linear_model/coordinate_descent.py
+++ sklearn/linear_model/coordinate_descent.py
@@ -552,7 +552,7 @@ class ElasticNet(LinearModel, RegressorMixin):
         self.positive = positive
         self.intercept_ = 0.0
 
-    def fit(self, X, y):
+    def fit(self, X, y, coef_init=None):
         """Fit model with coordinate descent.
 
         Parameters
@@ -562,6 +562,10 @@ class ElasticNet(LinearModel, RegressorMixin):
 
         y : ndarray, shape = (n_samples,) or (n_samples, n_targets)
             Target
+
+        coef_init : ndarray of shape n_features or (n_targets, n_features)
+            The initial coefficients to warm-start the optimization
+            WARNING : ignored and will be deprecated in 0.15
 
         Notes
         -----
