afc82c5a933e069287c795dbe56fb5a26c4e1210
Arnaud Joly <arnaud.v.joly@gmail.com>
2013-09-20 09:53:29 +0200

    TST scorer works with multi-output decision function

--- sklearn/metrics/tests/test_score_objects.py
+++ sklearn/metrics/tests/test_score_objects.py
@@ -114,6 +114,16 @@ def test_thresholded_scorers_multilabel_indicator_data():
 
     # Multi-output multi-class decision_function
     # TODO Is there any yet?
+    clf = DecisionTreeClassifier()
+    clf.fit(X_train, y_train)
+    clf._predict_proba = clf.predict_proba
+    clf.predict_proba = None
+    clf.decision_function = clf._predict_proba
+
+    y_proba = clf.decision_function(X_test)
+    score1 = SCORERS['roc_auc'](clf, X_test, y_test)
+    score2 = roc_auc_score(y_test, np.vstack(p[:, -1] for p in y_proba).T)
+    assert_almost_equal(score1, score2)
 
     # Multilabel predict_proba
     clf = OneVsRestClassifier(DecisionTreeClassifier())
