0e0db46b01a957afd71e5eb3680a3cdcad6c18b3
Johannes Schönberger <jsch@demuc.de>
2013-09-27 10:43:32 +0200

    Change linear_model access and call explicitly with kwargs

--- sklearn/linear_model/tests/test_ransac.py
+++ sklearn/linear_model/tests/test_ransac.py
@@ -2,7 +2,7 @@ import numpy as np
 from numpy.testing import assert_equal, assert_raises
 from scipy import sparse
 
-from sklearn import linear_model
+from sklearn.linear_model import LinearRegression, RANSAC
 
 
 # Generate coordinates of line
@@ -22,9 +22,9 @@ y = data[:, 1]
 
 def test_ransac_inliers_outliers():
 
-    base_estimator = linear_model.LinearRegression()
-    ransac_estimator = linear_model.RANSAC(base_estimator, 2, 5,
-                                           random_state=0)
+    base_estimator = LinearRegression()
+    ransac_estimator = RANSAC(base_estimator, min_n_samples=2,
+                              residual_threshold=5, random_state=0)
 
     # Estimate parameters of corrupted data
     ransac_estimator.fit(X, y)
@@ -45,10 +45,10 @@ def test_ransac_is_data_valid():
     X = np.random.rand(10, 2)
     y = np.random.rand(10, 1)
 
-    base_estimator = linear_model.LinearRegression()
-    ransac_estimator = linear_model.RANSAC(base_estimator, 2, 5,
-                                           is_data_valid=is_data_valid,
-                                           random_state=0)
+    base_estimator = LinearRegression()
+    ransac_estimator = RANSAC(base_estimator, min_n_samples=2,
+                              residual_threshold=5,
+                              is_data_valid=is_data_valid, random_state=0)
 
     assert_raises(ValueError, ransac_estimator.fit, X, y)
 
@@ -58,41 +58,45 @@ def test_ransac_is_model_valid():
         assert X.shape[0] == y.shape[0] == 2
         return False
 
-    base_estimator = linear_model.LinearRegression()
-    ransac_estimator = linear_model.RANSAC(base_estimator, 2, 5,
-                                           is_model_valid=is_model_valid,
-                                           random_state=0)
+    base_estimator = LinearRegression()
+    ransac_estimator = RANSAC(base_estimator, min_n_samples=2,
+                              residual_threshold=5,
+                              is_model_valid=is_model_valid, random_state=0)
 
     assert_raises(ValueError, ransac_estimator.fit, X, y)
 
 
 def test_ransac_max_trials():
-    base_estimator = linear_model.LinearRegression()
+    base_estimator = LinearRegression()
 
-    ransac_estimator = linear_model.RANSAC(base_estimator, 2, 5, max_trials=0,
-                                           random_state=0)
+    ransac_estimator = RANSAC(base_estimator, min_n_samples=2,
+                              residual_threshold=5, max_trials=0,
+                              random_state=0)
     assert_raises(ValueError, ransac_estimator.fit, X, y)
 
-    ransac_estimator = linear_model.RANSAC(base_estimator, 2, 5, max_trials=11,
-                                           random_state=0)
+    ransac_estimator = RANSAC(base_estimator, min_n_samples=2,
+                              residual_threshold=5, max_trials=11,
+                              random_state=0)
     assert getattr(ransac_estimator, 'n_trials_', None) is None
     ransac_estimator.fit(X, y)
     assert ransac_estimator.n_trials_ == 11
 
 
 def test_ransac_stop_n_inliers():
-    base_estimator = linear_model.LinearRegression()
-    ransac_estimator = linear_model.RANSAC(base_estimator, 2, 5,
-                                           stop_n_inliers=2, random_state=0)
+    base_estimator = LinearRegression()
+    ransac_estimator = RANSAC(base_estimator, min_n_samples=2,
+                              residual_threshold=5, stop_n_inliers=2,
+                              random_state=0)
     ransac_estimator.fit(X, y)
 
     assert ransac_estimator.n_trials_ == 1
 
 
 def test_ransac_stop_score():
-    base_estimator = linear_model.LinearRegression()
-    ransac_estimator = linear_model.RANSAC(base_estimator, 2, 5,
-                                           stop_score=0, random_state=0)
+    base_estimator = LinearRegression()
+    ransac_estimator = RANSAC(base_estimator, min_n_samples=2,
+                              residual_threshold=5, stop_score=0,
+                              random_state=0)
     ransac_estimator.fit(X, y)
 
     assert ransac_estimator.n_trials_ == 1
@@ -104,9 +108,9 @@ def test_ransac_score():
     y[0] = 1
     y[1] = 100
 
-    base_estimator = linear_model.LinearRegression()
-    ransac_estimator = linear_model.RANSAC(base_estimator, 2, 0.5,
-                                           random_state=0)
+    base_estimator = LinearRegression()
+    ransac_estimator = RANSAC(base_estimator, min_n_samples=2,
+                              residual_threshold=0.5, random_state=0)
     ransac_estimator.fit(X, y)
 
     assert ransac_estimator.score(X[2:], y[2:]) == 1
@@ -119,9 +123,9 @@ def test_ransac_predict():
     y[0] = 1
     y[1] = 100
 
-    base_estimator = linear_model.LinearRegression()
-    ransac_estimator = linear_model.RANSAC(base_estimator, 2, 0.5,
-                                           random_state=0)
+    base_estimator = LinearRegression()
+    ransac_estimator = RANSAC(base_estimator, min_n_samples=2,
+                              residual_threshold=0.5, random_state=0)
     ransac_estimator.fit(X, y)
 
     assert_equal(ransac_estimator.predict(X), np.zeros((100, 1)))
@@ -130,9 +134,9 @@ def test_ransac_predict():
 def test_ransac_sparse_coo():
     X_sparse = sparse.coo_matrix(X)
 
-    base_estimator = linear_model.LinearRegression()
-    ransac_estimator = linear_model.RANSAC(base_estimator, 2, 5,
-                                           random_state=0)
+    base_estimator = LinearRegression()
+    ransac_estimator = RANSAC(base_estimator, min_n_samples=2,
+                              residual_threshold=5, random_state=0)
     ransac_estimator.fit(X_sparse, y)
 
     ref_inlier_mask = np.ones_like(ransac_estimator.inlier_mask_,
@@ -145,9 +149,9 @@ def test_ransac_sparse_coo():
 def test_ransac_sparse_csr():
     X_sparse = sparse.csr_matrix(X)
 
-    base_estimator = linear_model.LinearRegression()
-    ransac_estimator = linear_model.RANSAC(base_estimator, 2, 5,
-                                           random_state=0)
+    base_estimator = LinearRegression()
+    ransac_estimator = RANSAC(base_estimator, min_n_samples=2,
+                              residual_threshold=5, random_state=0)
     ransac_estimator.fit(X_sparse, y)
 
     ref_inlier_mask = np.ones_like(ransac_estimator.inlier_mask_,
@@ -160,9 +164,9 @@ def test_ransac_sparse_csr():
 def test_ransac_sparse_csc():
     X_sparse = sparse.csc_matrix(X)
 
-    base_estimator = linear_model.LinearRegression()
-    ransac_estimator = linear_model.RANSAC(base_estimator, 2, 5,
-                                           random_state=0)
+    base_estimator = LinearRegression()
+    ransac_estimator = RANSAC(base_estimator, min_n_samples=2,
+                              residual_threshold=5, random_state=0)
     ransac_estimator.fit(X_sparse, y)
 
     ref_inlier_mask = np.ones_like(ransac_estimator.inlier_mask_,
@@ -174,11 +178,11 @@ def test_ransac_sparse_csc():
 
 def test_ransac_none_estimator():
 
-    base_estimator = linear_model.LinearRegression()
+    base_estimator = LinearRegression()
 
-    ransac_estimator = linear_model.RANSAC(base_estimator, 2, 5,
-                                           random_state=0)
-    ransac_none_estimator = linear_model.RANSAC(None, 2, 5, random_state=0)
+    ransac_estimator = RANSAC(base_estimator, min_n_samples=2,
+                              residual_threshold=5, random_state=0)
+    ransac_none_estimator = RANSAC(None, 2, 5, random_state=0)
 
     ransac_estimator.fit(X, y)
     ransac_none_estimator.fit(X, y)
@@ -188,13 +192,13 @@ def test_ransac_none_estimator():
 
 
 def test_ransac_min_n_samples():
-    base_estimator = linear_model.LinearRegression()
-    ransac_estimator1 = linear_model.RANSAC(base_estimator, 2, 5,
-                                            random_state=0)
-    ransac_estimator2 = linear_model.RANSAC(base_estimator, 2. / X.shape[0],
-                                            5, random_state=0)
-    ransac_estimator3 = linear_model.RANSAC(base_estimator, -1,
-                                            5, random_state=0)
+    base_estimator = LinearRegression()
+    ransac_estimator1 = RANSAC(base_estimator, min_n_samples=2,
+                               residual_threshold=5,  random_state=0)
+    ransac_estimator2 = RANSAC(base_estimator, min_n_samples=2. / X.shape[0],
+                               residual_threshold=5, random_state=0)
+    ransac_estimator3 = RANSAC(base_estimator, min_n_samples=-1,
+                               residual_threshold=5, random_state=0)
 
     ransac_estimator1.fit(X, y)
     ransac_estimator2.fit(X, y)
@@ -205,9 +209,9 @@ def test_ransac_min_n_samples():
 
 def test_ransac_multi_dimensional_targets():
 
-    base_estimator = linear_model.LinearRegression()
-    ransac_estimator = linear_model.RANSAC(base_estimator, 2, 5,
-                                           random_state=0)
+    base_estimator = LinearRegression()
+    ransac_estimator = RANSAC(base_estimator, min_n_samples=2,
+                              residual_threshold=5, random_state=0)
 
     # 3-D target values
     yyy = np.column_stack([y, y, y])
