00eb9b6c75e3abf0fa2a667d8761d3d347ca65b1
Rohit Sivaprasad <rohit.sprasad@gmail.com>
2013-09-27 03:05:28 -0400

    DOC LabelBinarizer

--- sklearn/preprocessing/label.py
+++ sklearn/preprocessing/label.py
@@ -189,7 +189,7 @@ class LabelBinarizer(BaseEstimator, TransformerMixin):
     >>> from sklearn import preprocessing
     >>> lb = preprocessing.LabelBinarizer()
     >>> lb.fit([1, 2, 6, 4, 2])
-    LabelBinarizer(neg_label=0, pos_label=1)
+    LabelBinarizer(dense_output=True, neg_label=0, pos_label=1)
     >>> lb.classes_
     array([1, 2, 4, 6])
     >>> lb.multilabel_
