db75461e21442f7c1337fa47d199992c403cb15e
Johannes Schönberger <jsch@demuc.de>
2013-09-28 18:40:37 +0200

    Remove bagged regressor

--- examples/linear_model/plot_ransac.py
+++ examples/linear_model/plot_ransac.py
@@ -10,7 +10,7 @@ the RANSAC algorithm.
 import numpy as np
 from matplotlib import pyplot as plt
 
-from sklearn import linear_model, ensemble
+from sklearn import linear_model
 
 
 # Set random seed for both equal data noise and equal random sample selection
@@ -45,22 +45,16 @@ model_ransac.fit(X, y)
 inlier_mask = model_ransac.inlier_mask_
 outlier_mask = np.logical_not(inlier_mask)
 
-# Robustly fit linear model with bagged linear regressor
-model_bagged = ensemble.BaggingRegressor(linear_model.LinearRegression())
-model_bagged.fit(X, y)
-
 # Generate coordinates of estimated models
 line_X = np.arange(-250, 250)
 line_y = model.predict(line_X[:, np.newaxis])
 line_y_ransac = model_ransac.predict(line_X[:, np.newaxis])
-line_y_bagged = model_bagged.predict(line_X[:, np.newaxis])
 
 plt.plot(data[inlier_mask, 0], data[inlier_mask, 1], '.g',
-         label='RANSAC inliers')
+         label='Inliers')
 plt.plot(data[outlier_mask, 0], data[outlier_mask, 1], '.r',
-         label='RANSAC outliers')
+         label='Outliers')
 plt.plot(line_X, line_y, '-k', label='Linear regressor')
 plt.plot(line_X, line_y_ransac, '-b', label='RANSAC regressor')
-plt.plot(line_X, line_y_bagged, '-y', label='Bagging regressor')
 plt.legend(loc='lower left')
 plt.show()
