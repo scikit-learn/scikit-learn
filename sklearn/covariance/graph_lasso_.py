corrected sign error in _objective

sklearn/covariance/graph_lasso_.py
@@ -32,7 +32,7 @@ def _objective(mle, precision_, alpha):
     # the objective function is made of a shifted scaled version of the 
     # normalized log-likelihood (i.e. its empirical mean over the samples)
     p = precision_.shape[0]
-    cost = - 2. * log_likelihood(mle, precision_) + p * np.log(2 * np.pi)
+    cost = - 2. * log_likelihood(mle, precision_) - p * np.log(2 * np.pi)
     cost += alpha * (np.abs(precision_).sum()
                      - np.abs(np.diag(precision_)).sum())
     return cost