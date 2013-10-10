bug fixes in graph_lasso and moving non-custom log-likelihood definition to specific definition of cost

sklearn/covariance/empirical_covariance_.py
@@ -21,7 +21,7 @@ from ..utils.extmath import fast_logdet, pinvh
 
 
 def log_likelihood(emp_cov, precision):
-    """Computes the log_likelihood of the data
+    """Computes the sample mean of the log_likelihood under a covariance model
 
     Parameters
     ----------
@@ -31,8 +31,11 @@ def log_likelihood(emp_cov, precision):
     precision : 2D ndarray (n_features, n_features)
         The precision matrix of the covariance model to be tested
     """
-    return -np.sum(emp_cov * precision) + fast_logdet(precision)
-
+    p = precision.shape[0]
+    log_likelihood_ = - np.sum(emp_cov * precision) + fast_logdet(precision)
+    log_likelihood_ -= p * np.log(2 * np.pi)
+    log_likelihood_ /= 2.
+    return log_likelihood_
 
 def empirical_covariance(X, assume_centered=False):
     """Computes the Maximum likelihood covariance estimator

sklearn/covariance/tests/test_graph_lasso.py
@@ -26,10 +26,10 @@ def test_graph_lasso(random_state=0):
     X = random_state.multivariate_normal(np.zeros(dim), cov, size=n_samples)
     emp_cov = empirical_covariance(X)
 
-    for alpha in (.1, .01):
+    for alpha in (.01, .1):
         covs = dict()
         for method in ('cd', 'lars'):
-            cov_, _, costs = graph_lasso(emp_cov, alpha=.1, return_costs=True)
+            cov_, _, costs = graph_lasso(emp_cov, alpha=alpha, return_costs=True)
             covs[method] = cov_
             costs, dual_gap = np.array(costs).T
             # Check that the costs always decrease
@@ -39,9 +39,18 @@ def test_graph_lasso(random_state=0):
 
     # Smoke test the estimator
     model = GraphLasso(alpha=.1).fit(X)
+    model.score(X)
     assert_array_almost_equal(model.covariance_, covs['cd'])
 
-
+    # For a centered matrix, assume_centered could be chosen True or False
+    # Check that this returns indeed the same result
+    Z = X - X.mean(0)
+    precs = list()
+    for assume_centered in (False, True):
+        prec_ = GraphLasso(assume_centered=assume_centered).fit(Z).precision_
+        precs.append(prec_)
+    assert_array_almost_equal(precs[0], precs[1])
+        
 def test_graph_lasso_cv(random_state=1):
     # Sample data from a sparse multivariate normal
     dim = 5