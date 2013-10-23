a0f0a3d9611677f7500a71ff7735cdf87c5277a7
dengemann <d.engemann@fz-juelich.de>
2013-10-23 21:07:31 +0200

    FIX: PPCA related warnings

--- sklearn/decomposition/tests/test_pca.py
+++ sklearn/decomposition/tests/test_pca.py
@@ -361,12 +361,11 @@ def test_probabilistic_pca_1():
     rng = np.random.RandomState(0)
     X = rng.randn(n, p) * .1 + np.array([3, 4, 5])
 
-    with warnings.catch_warnings(record=True) as w:
-        ppca = ProbabilisticPCA(n_components=2)
-        ppca.fit(X)
-        ll1 = ppca.score(X)
-        h = -0.5 * np.log(2 * np.pi * np.exp(1) * 0.1 ** 2) * p
-        np.testing.assert_almost_equal(ll1.mean() / h, 1, 0)
+    ppca = assert_warns(DeprecationWarning, ProbabilisticPCA, n_components=2)
+    ppca.fit(X)
+    ll1 = ppca.score(X)
+    h = -0.5 * np.log(2 * np.pi * np.exp(1) * 0.1 ** 2) * p
+    np.testing.assert_almost_equal(ll1.mean() / h, 1, 0)
 
 
 def test_probabilistic_pca_2():
@@ -374,12 +373,11 @@ def test_probabilistic_pca_2():
     n, p = 100, 3
     rng = np.random.RandomState(0)
     X = rng.randn(n, p) * .1 + np.array([3, 4, 5])
-    with warnings.catch_warnings(record=True) as w:
-        ppca = ProbabilisticPCA(n_components=2)
-        ppca.fit(X)
-        ll1 = ppca.score(X)
-        ll2 = ppca.score(rng.randn(n, p) * .2 + np.array([3, 4, 5]))
-        assert_greater(ll1.mean(), ll2.mean())
+    ppca = assert_warns(DeprecationWarning, ProbabilisticPCA, n_components=2)
+    ppca.fit(X)
+    ll1 = ppca.score(X)
+    ll2 = ppca.score(rng.randn(n, p) * .2 + np.array([3, 4, 5]))
+    assert_greater(ll1.mean(), ll2.mean())
 
 
 def test_probabilistic_pca_3():
@@ -389,14 +387,11 @@ def test_probabilistic_pca_3():
     n, p = 100, 3
     rng = np.random.RandomState(0)
     X = rng.randn(n, p) * .1 + np.array([3, 4, 5])
-    with warnings.catch_warnings(record=True) as w:
-        ppca = ProbabilisticPCA(n_components=2)
-        ppca.fit(X)
-        ll1 = ppca.score(X)
-        ppca.fit(X, homoscedastic=False)
-        ll2 = ppca.score(X)
-        # XXX : Don't test as homoscedastic=False is buggy
-        # Comment to be removed with ProbabilisticPCA is removed
+    ppca = assert_warns(DeprecationWarning, ProbabilisticPCA, n_components=2)
+    ppca.fit(X).score(X)
+    ppca.fit(X, homoscedastic=False).score(X)
+    # XXX : Don't test as homoscedastic=False is buggy
+    # Comment to be removed with ProbabilisticPCA is removed
 
 def test_probabilistic_pca_4():
     """Check that ppca select the right model"""
@@ -407,11 +402,11 @@ def test_probabilistic_pca_4():
     Xt = (rng.randn(n, p) + rng.randn(n, 1) * np.array([3, 4, 5])
           + np.array([1, 0, 7]))
     ll = np.zeros(p)
-    with warnings.catch_warnings(record=True) as w:
-        for k in range(p):
-            ppca = ProbabilisticPCA(n_components=k)
-            ppca.fit(Xl)
-            ll[k] = ppca.score(Xt).mean()
+    for k in range(p):
+        ppca = assert_warns(DeprecationWarning, ProbabilisticPCA,
+                            n_components=k)
+        ppca.fit(Xl)
+        ll[k] = ppca.score(Xt).mean()
 
         assert_true(ll.argmax() == 1)
 
@@ -423,9 +418,9 @@ def test_probabilistic_pca_vs_pca():
     rng = np.random.RandomState(0)
     X = rng.randn(n, p) * .1 + np.array([3, 4, 5])
     pca = PCA(n_components=2).fit(X)
-    with warnings.catch_warnings(record=True) as w:
-        ppca = ProbabilisticPCA(n_components=2).fit(X)
-        assert_array_almost_equal(pca.score_samples(X), ppca.score(X))
+    ppca = assert_warns(DeprecationWarning, ProbabilisticPCA,
+                        n_components=2).fit(X)
+    assert_array_almost_equal(pca.score_samples(X), ppca.score(X))
 
 
 if __name__ == '__main__':
