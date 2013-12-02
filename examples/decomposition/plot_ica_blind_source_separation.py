c0259847b42fa3bbb6f7a0b1d6658add9aedff18
dengemann <d.engemann@fz-juelich.de>
2013-12-02 01:02:11 +0100

    ENH: color scheme + add PCA

--- examples/decomposition/plot_ica_blind_source_separation.py
+++ examples/decomposition/plot_ica_blind_source_separation.py
@@ -6,48 +6,65 @@ Blind source separation using FastICA
 An example of estimating sources from noisy data.
 
 :ref:`ICA` is used to estimate sources given noisy measurements.
-Imagine 2 instruments playing simultaneously and 2 microphones
+Imagine 3 instruments playing simultaneously and 2 microphones
 recording the mixed signals. ICA is used to recover the sources
-ie. what is played by each instrument.
+ie. what is played by each instrument. Importantly, PCA fails
+at recovering our `instruments` since the related signals are
+non-Gaussian .
 
 """
 print(__doc__)
 
 import numpy as np
 import pylab as pl
-from sklearn.decomposition import FastICA
+from scipy import signal
+
+from sklearn.decomposition import FastICA, RandomizedPCA
 
 ###############################################################################
 # Generate sample data
 np.random.seed(0)
 n_samples = 2000
-time = np.linspace(0, 10, n_samples)
+time = np.linspace(0, 8, n_samples)
+
 s1 = np.sin(2 * time)  # Signal 1 : sinusoidal signal
 s2 = np.sign(np.sin(3 * time))  # Signal 2 : square signal
-S = np.c_[s1, s2]
+s3 = signal.sawtooth(2 * np.pi * time)  # Signal 3: saw tooth signal
+
+S = np.c_[s1, s2, s3]
 S += 0.2 * np.random.normal(size=S.shape)  # Add noise
 
 S /= S.std(axis=0)  # Standardize data
 # Mix data
-A = np.array([[1, 1], [0.5, 2]])  # Mixing matrix
+A = np.array([[1, 1, 1], [0.5, 2, 1.0], [1.5, 1.0, 2.0]])  # Mixing matrix
 X = np.dot(S, A.T)  # Generate observations
 # Compute ICA
-ica = FastICA()
+ica = FastICA(3)
 S_ = ica.fit(X).transform(X)  # Get the estimated sources
 A_ = ica.mixing_  # Get estimated mixing matrix
 assert np.allclose(X, np.dot(S_, A_.T) + ica.mean_)
 
+# compute PCA
+pca = RandomizedPCA()
+H = pca.fit(X).transform(X)  # Get the estimated sources
+
 ###############################################################################
 # Plot results
+
 pl.figure()
-pl.subplot(3, 1, 1)
-pl.plot(S)
-pl.title('True Sources')
-pl.subplot(3, 1, 2)
-pl.plot(X)
-pl.title('Observations (mixed signal)')
-pl.subplot(3, 1, 3)
-pl.plot(S_)
-pl.title('ICA estimated sources')
-pl.subplots_adjust(0.09, 0.04, 0.94, 0.94, 0.26, 0.36)
+
+models = [X, S, S_, H]
+names = ['Observations (mixed signal)',
+         'True Sources',
+         'ICA estimated sources', 
+         'PCA estimated sources']
+colors = ['red', 'steelblue', 'orange']
+
+for ii, (model, name) in enumerate(zip(models, names), 1):
+    pl.subplot(4, 1, ii)
+    pl.title(name)
+    for sig, color in zip(model.T, colors):
+        pl.plot(sig, color=color)
+
+pl.tight_layout()
 pl.show()
