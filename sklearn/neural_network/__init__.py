b780f5ddcd64e687221067c7956ca8c523ea95f6
Issam Laradji <issam.laradji@gmail.com>
2013-12-20 14:09:32 +0300

    Created a BaseRBM with its extentions

--- sklearn/neural_network/__init__.py
+++ sklearn/neural_network/__init__.py
@@ -3,5 +3,4 @@ The :mod:`sklearn.neural_network` module includes models based on neural
 networks.
 """
 
-from .rbm import BernoulliRBM
-from .gaussian_bernoulli_rbm import GaussianBernoulliRBM
+from .rbm import BernoulliRBM, GaussianBernoulliRBM
