96fc07854d973ab475ef702e50aa39164fd98c25
Alexander Fabisch <afabisch@googlemail.com>
2014-01-01 10:25:57 +0100

    Clean up learning curve, separate example

---
+++ examples/plot_learning_curve.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.naive_bayes import GaussianNB
+from sklearn.datasets import load_digits
+from sklearn.learning_curve import learning_curve # TODO should be: from sklearn import learning_curve
+
+if __name__ == "__main__":
+    estimator = GaussianNB()
+    digits = load_digits()
+    X, y = digits.data, digits.target
+
+    n_samples_range, train_scores, test_scores = learning_curve(
+        estimator, X, y, step_size=50, n_jobs=4, verbose=False)
+
+    plt.title("Learning Curves (Naive Bayes on Digits Dataset)")
+    plt.xlabel("Training examples")
+    plt.ylabel("Score (Accuracy)")
+    plt.plot(n_samples_range, train_scores, label="Training score")
+    plt.plot(n_samples_range, test_scores, label="Cross-validation score")
+    plt.legend(loc="best")
+    plt.show()
--- sklearn/learning_curve.py
+++ sklearn/learning_curve.py
@@ -1,66 +1,80 @@
 import numpy as np
-from sklearn import clone # TODO relative import
-from sklearn.cross_validation import KFold # TODO relative import
-from sklearn.externals.joblib import Parallel, delayed # TODO relative import
-#from .metrics import _deprecate_loss_and_score_funcs # TODO relative import
+from .base import clone
+from .cross_validation import KFold
+from .externals.joblib import Parallel, delayed
+from .metrics.scorer import _deprecate_loss_and_score_funcs
 
-def learning_curve(estimator, X, y, n_samples_range,
-                   loss_func=None, scoring='accuracy', n_cv_folds=10, n_jobs=1,
-                   verbose=False, random_state=None):
-    n_max_samples = np.max(n_samples_range)
+def learning_curve(estimator, X, y, n_samples_range=None, step_size=1,
+                   n_cv_folds=10, loss_func=None, scoring=None,
+                   n_jobs=1, verbose=False, random_state=None):
+    # TODO tests, doc
+    # TODO allow y to be None for unsupervised learning
+    # TODO test different n_cv_folds / dataset sizes / etc. (there could be bugs)
+    # TODO there is a huge overlap with grid search -> refactoring
+    # TODO exploit incremental learning?! (might be a bit complicated with CV)
+
     n_samples = X.shape[0]
-    n_required_samples = int(n_max_samples * (1 + 1.0 / n_cv_folds))
+    max_fold_size = n_samples / n_cv_folds
+
+    if n_samples_range is None:
+        if step_size is None or step_size < 1:
+            raise ValueError("Define either a range of training set sizes or "
+                             "a proper step size.")
+        n_samples_range = np.arange(n_cv_folds-1, n_samples-max_fold_size+1,
+                                    step_size)
+
+    n_max_samples = np.max(n_samples_range)
+    n_required_samples = n_max_samples + max_fold_size
     if n_samples < n_required_samples:
-        # TODO test case
         raise ValueError(
                 "For %d-fold cross-validation with %d training examples, "
                 "%d samples are required (got %d)."
                 % (n_cv_folds, n_max_samples, n_required_samples, n_samples))
 
-    # TODO check if scoring is possible (see BaseGridSearch)
-
-    #scorer = _deprecate_loss_and_score_funcs(loss_func=loss_func, scoring=scoring) # TODO enable with relative import
-    from sklearn.metrics import accuracy_score
-    scorer = accuracy_score
+    # TODO copied from BaseGridSearch -> move to utils? .base? where?
+    if (not hasattr(estimator, 'fit') or
+            not (hasattr(estimator, 'predict')
+                  or hasattr(estimator, 'score'))):
+        raise TypeError("estimator should a be an estimator implementing"
+                        " 'fit' and 'predict' or 'score' methods,"
+                        " %s (type %s) was passed" %
+                        (estimator, type(estimator)))
+    if scoring is None and loss_func is None:
+        if not hasattr(estimator, 'score'):
+            raise TypeError(
+                "If no scoring is specified, the estimator passed "
+                "should have a 'score' method. The estimator %s "
+                "does not." % estimator)
+        scorer = _deprecate_loss_and_score_funcs(loss_func=loss_func,
+                                                 scoring=scoring)
 
     scores = []
     for n_train_samples in n_samples_range:
         # TODO maybe take random indices instead of the first slice_length?
-        slice_length = int(n_train_samples * (1 + 1.0 / n_cv_folds))
+        fold_size = (n_train_samples+1) / n_cv_folds
+        slice_length = n_train_samples + fold_size
         cv = KFold(n=slice_length, n_folds=n_cv_folds,
                    random_state=random_state)
 
         out = Parallel(
-            n_jobs=n_jobs, verbose=verbose)( # TODO set pre_dispatch parameter?
-                delayed(fit_estimator)(estimator, X, y, train, test, scorer, verbose)
+            # TODO set pre_dispatch parameter? what is it good for?
+            n_jobs=n_jobs, verbose=verbose)(
+                delayed(_fit_estimator)(estimator, X, y, train, test, scorer,
+                                       verbose)
                 for train, test in cv)
         scores.append(np.mean(out, axis=0))
     scores = np.array(scores)
-    print scores
-    return scores[:, 0], scores[:, 1]
 
-def fit_estimator(base_estimator, X, y, train, test, scorer, verbose):
+    return n_samples_range, scores[:, 0], scores[:, 1]
+
+def _fit_estimator(base_estimator, X, y, train, test, scorer, verbose):
+    # TODO similar to fit_grid_point from grid search, refactor
     estimator = clone(base_estimator)
     estimator.fit(X[train], y[train])
-    y_test_pred = estimator.predict(X[test])
-    y_train_pred = estimator.predict(X[train])
-    train_score = scorer(y[train], y_train_pred)
-    test_score = scorer(y[test], y_test_pred)
+    if scorer is None:
+        train_score = estimator.score(X[train], y[train])
+        test_score = estimator.score(X[test], y[test])
+    else:
+        train_score = scorer(estimator, X[train], y[train])
+        test_score = scorer(estimator, X[test], y[test])
     return train_score, test_score
-
-if __name__ == "__main__":
-    #from sklearn.linear_model import RidgeClassifier
-    #estimator = RidgeClassifier(alpha=100)
-    from sklearn.svm import SVC
-    estimator = SVC(gamma=0.001)
-
-    from sklearn.datasets import load_digits
-    digits = load_digits()
-    X, y = digits.data, digits.target
-
-    n_samples_range = np.arange(10, 1611, 100)
-    train_scores, test_scores = learning_curve(estimator, X, y, n_samples_range, n_jobs=2, verbose=False)
-    import pylab
-    pylab.plot(n_samples_range, train_scores)
-    pylab.plot(n_samples_range, test_scores)
-    pylab.show()
