c08f462477ea9f12045275df786d1c74a5de35d8
Andreas Mueller <amueller@ais.uni-bonn.de>
2014-01-06 15:26:08 +0000

    MAINT remove _label attribute from SVC.
    Fixes #1573.

--- sklearn/svm/base.py
+++ sklearn/svm/base.py
@@ -219,7 +219,7 @@ class BaseLibSVM(six.with_metaclass(ABCMeta, BaseEstimator)):
         # we don't pass **self.get_params() to allow subclasses to
         # add other parameters to __init__
         self.support_, self.support_vectors_, self.n_support_, \
-            self.dual_coef_, self.intercept_, self._label, self.probA_, \
+            self.dual_coef_, self.intercept_, self.probA_, \
             self.probB_, self.fit_status_ = libsvm.fit(
                 X, y,
                 svm_type=solver_type, sample_weight=sample_weight,
@@ -242,7 +242,7 @@ class BaseLibSVM(six.with_metaclass(ABCMeta, BaseEstimator)):
         libsvm_sparse.set_verbosity_wrap(self.verbose)
 
         self.support_, self.support_vectors_, dual_coef_data, \
-            self.intercept_, self._label, self.n_support_, \
+            self.intercept_, self.n_support_, \
             self.probA_, self.probB_, self.fit_status_ = \
             libsvm_sparse.libsvm_sparse_train(
                 X.shape[1], X.data, X.indices, X.indptr, y, solver_type,
@@ -254,7 +254,10 @@ class BaseLibSVM(six.with_metaclass(ABCMeta, BaseEstimator)):
 
         self._warn_from_fit_status()
 
-        n_class = len(self._label) - 1
+        if hasattr(self, "classes_"):
+            n_class = len(self.classes_) - 1
+        else:   # regression
+            n_class = 1
         n_SV = self.support_vectors_.shape[0]
 
         dual_coef_indices = np.tile(np.arange(n_SV), n_class)
@@ -299,7 +302,7 @@ class BaseLibSVM(six.with_metaclass(ABCMeta, BaseEstimator)):
 
         return libsvm.predict(
             X, self.support_, self.support_vectors_, self.n_support_,
-            self.dual_coef_, self._intercept_, self._label,
+            self.dual_coef_, self._intercept_,
             self.probA_, self.probB_, svm_type=svm_type, kernel=kernel,
             degree=self.degree, coef0=self.coef0, gamma=self._gamma,
             cache_size=self.cache_size)
@@ -325,7 +328,7 @@ class BaseLibSVM(six.with_metaclass(ABCMeta, BaseEstimator)):
             self.degree, self._gamma, self.coef0, self.tol,
             C, self.class_weight_,
             self.nu, self.epsilon, self.shrinking,
-            self.probability, self.n_support_, self._label,
+            self.probability, self.n_support_,
             self.probA_, self.probB_)
 
     def _compute_kernel(self, X):
@@ -365,7 +368,7 @@ class BaseLibSVM(six.with_metaclass(ABCMeta, BaseEstimator)):
 
         dec_func = libsvm.decision_function(
             X, self.support_, self.support_vectors_, self.n_support_,
-            self.dual_coef_, self._intercept_, self._label,
+            self.dual_coef_, self._intercept_,
             self.probA_, self.probB_,
             svm_type=LIBSVM_IMPL.index(self._impl),
             kernel=kernel, degree=self.degree, cache_size=self.cache_size,
@@ -551,7 +554,7 @@ class BaseSVC(BaseLibSVM, ClassifierMixin):
         svm_type = LIBSVM_IMPL.index(self._impl)
         pprob = libsvm.predict_proba(
             X, self.support_, self.support_vectors_, self.n_support_,
-            self.dual_coef_, self._intercept_, self._label,
+            self.dual_coef_, self._intercept_,
             self.probA_, self.probB_,
             svm_type=svm_type, kernel=kernel, degree=self.degree,
             cache_size=self.cache_size, coef0=self.coef0, gamma=self._gamma)
@@ -577,7 +580,7 @@ class BaseSVC(BaseLibSVM, ClassifierMixin):
             self.degree, self._gamma, self.coef0, self.tol,
             self.C, self.class_weight_,
             self.nu, self.epsilon, self.shrinking,
-            self.probability, self.n_support_, self._label,
+            self.probability, self.n_support_,
             self.probA_, self.probB_)
 
 
