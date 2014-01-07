974812725111ff23479689007d6df9ddba594e30
Alexander Fabisch <afabisch@googlemail.com>
2014-01-07 22:37:58 +0100

    Add documentation of '_translate_train_sizes'

--- sklearn/learning_curve.py
+++ sklearn/learning_curve.py
@@ -39,11 +39,12 @@ def learning_curve(estimator, X, y, train_sizes=np.linspace(0.1, 1.0, 10),
         Target relative to X for classification or regression;
         None for unsupervised learning.
 
-    train_sizes : array-like, shape = [n_ticks,], dtype float or int
-        Numbers of training examples that will be used to generate the
-        learning curve. If the dtype is float, it is regarded as a
+    train_sizes : array-like, shape = (n_ticks,), dtype float or int
+        Relative or absolute numbers of training examples that will be used to
+        generate the learning curve. If the dtype is float, it is regarded as a
         fraction of the maximum size of the training set (that is determined
         by the selected validation method), i.e. it has to be within (0, 1].
+        Otherwise it is interpreted as absolute sizes of the training sets.
         Note that for classification the number of samples usually have to
         be big enough to contain at least one sample from each class.
         (default: np.linspace(0.1, 1.0, 10))
@@ -103,11 +104,12 @@ def learning_curve(estimator, X, y, train_sizes=np.linspace(0.1, 1.0, 10),
         cv = new_cv
 
     n_max_training_samples = len(cv[0][0])
-    train_sizes_abs, n_unique_ticks = _translate_train_sizes(
-        train_sizes, n_max_training_samples)
     # Because the lengths of folds can be significantly different, it is
     # not guaranteed that we use all of the available training data when we
     # use the first 'n_max_training_samples' samples.
+    train_sizes_abs = _translate_train_sizes(train_sizes,
+                                             n_max_training_samples)
+    n_unique_ticks = train_sizes_abs.shape[0]
     if verbose > 0:
         print("[learning_curve] Training set sizes: " + str(train_sizes_abs))
 
@@ -138,11 +140,28 @@ def learning_curve(estimator, X, y, train_sizes=np.linspace(0.1, 1.0, 10),
 
 
 def _translate_train_sizes(train_sizes, n_max_training_samples):
-    """Determine range of number of training samples.
+    """Determine absolute sizes of training subsets and validate 'train_sizes'.
 
-    If the dtype of train_sizes is float, the numbers will be interpreted as
-    fractions of n_max_training_samples. Otherwise they will be interpreted
-    as absolute values with n_max_training_samples as maximum value.
+    Examples:
+        _translate_train_sizes([0.5, 1.0], 10) -> ([5, 10], 2)
+        _translate_train_sizes([5, 10], 10) -> ([5, 10], 2)
+
+    Parameters
+    ----------
+    train_sizes : array-like, shape = (n_ticks,), dtype float or int
+        Numbers of training examples that will be used to generate the
+        learning curve. If the dtype is float, it is regarded as a
+        fraction of 'n_max_training_samples', i.e. it has to be within (0, 1].
+
+    n_max_training_samples : int
+        Maximum number of training samples (upper bound of 'train_sizes').
+
+    Returns
+    -------
+    train_sizes_abs : array, shape = [n_unique_ticks,], dtype int
+        Numbers of training examples that will be used to generate the
+        learning curve. Note that the number of ticks might be less
+        than n_ticks because duplicate entries will be removed.
     """
     train_sizes_abs = np.asarray(train_sizes)
     n_ticks = train_sizes_abs.shape[0]
@@ -150,31 +169,33 @@ def _translate_train_sizes(train_sizes, n_max_training_samples):
     n_max_required_samples = np.max(train_sizes_abs)
     if np.issubdtype(train_sizes_abs.dtype, np.float):
         if n_min_required_samples <= 0.0 or n_max_required_samples > 1.0:
-            raise ValueError("train_sizes must be within (0, 1], "
-                             "but is within [%f, %f]."
+            raise ValueError("train_sizes has been interpreted as fractions of "
+                             "the maximum number of training samples and must "
+                             "be within (0, 1], but is within [%f, %f]."
                              % (n_min_required_samples,
                                 n_max_required_samples))
         train_sizes_abs = (train_sizes_abs
-                             * n_max_training_samples).astype(np.int)
+                           * n_max_training_samples).astype(np.int)
         train_sizes_abs = np.clip(train_sizes_abs, 1,
-                                    n_max_training_samples)
+                                  n_max_training_samples)
     else:
         if (n_min_required_samples <= 0 or
                 n_max_required_samples > n_max_training_samples):
-            raise ValueError("train_sizes must be within (0, %d], "
-                             "but is within [%d, %d]."
+            raise ValueError("train_sizes has been interpreted as absolute "
+                             "numbers of training samples and must be within "
+                             "(0, %d], but is within [%d, %d]."
                              % (n_max_training_samples,
                                 n_min_required_samples,
                                 n_max_required_samples))
 
     train_sizes_abs = np.unique(train_sizes_abs)
-    n_unique_ticks = train_sizes_abs.shape[0]
-    if n_ticks > n_unique_ticks:
-        warnings.warn("Number of ticks will be less than than the size of "
-                      "'train_sizes' (%d instead of %d)."
-                      % (n_unique_ticks, n_ticks), RuntimeWarning)
+    if n_ticks > train_sizes_abs.shape[0]:
+        warnings.warn("Removed duplicate entries from 'train_sizes'. Number of "
+                      "ticks will be less than than the size of 'train_sizes' "
+                      "(%d instead of %d)."
+                      % (train_sizes_abs.shape[0], n_ticks), RuntimeWarning)
 
-    return train_sizes_abs, n_unique_ticks
+    return train_sizes_abs
 
 
 def _fit_estimator(base_estimator, X, y, train, test,
