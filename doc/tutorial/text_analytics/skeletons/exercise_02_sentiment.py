67d3bb23400c67df4544cf7e69dd39d3578cb365
Olivier Grisel <olivier.grisel@ensta.org>
2014-01-10 14:56:10 +0100

    Python 3 fixes for the second exercise

--- doc/tutorial/text_analytics/skeletons/exercise_02_sentiment.py
+++ doc/tutorial/text_analytics/skeletons/exercise_02_sentiment.py
@@ -12,8 +12,7 @@ In this examples we will use a movie review dataset.
 # License: Simplified BSD
 
 import sys
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
 from sklearn.pipeline import Pipeline
 from sklearn.grid_search import GridSearchCV
@@ -21,9 +20,6 @@ from sklearn.datasets import load_files
 from sklearn.cross_validation import train_test_split
 from sklearn import metrics
 
-#
-# The real code starts here
-#
 
 if __name__ == "__main__":
     # NOTE: we put the following in a 'if __name__ == "__main__"' protected
@@ -35,36 +31,33 @@ if __name__ == "__main__":
     # the training data folder must be passed as first argument
     movie_reviews_data_folder = sys.argv[1]
     dataset = load_files(movie_reviews_data_folder, shuffle=False)
-    print "n_samples: %d" % len(dataset.data)
+    print("n_samples: %d" % len(dataset.data))
 
     # split the dataset in training and test set:
     docs_train, docs_test, y_train, y_test = train_test_split(
-        dataset.data, dataset.target, test_fraction=0.25, random_state=None)
+        dataset.data, dataset.target, test_size=0.25, random_state=None)
 
-    # TASK: Build a vectorizer / classifier pipeline using the previous
-    # analyzer
-
-    parameters = {
-        'vect__analyzer__max_n': (1, 2),
-        'vect__max_df': (.95,),
-    }
+    # TASK: Build a vectorizer / classifier pipeline that filters out tokens
+    # that are too rare or too frequent
 
     # TASK: Build a grid search to find out whether unigrams or bigrams are
     # more useful.
     # Fit the pipeline on the training set using grid search for the parameters
-    # Refit the best parameter set on the complete training set
 
-    # Predict the outcome on the testing set
-    y_predicted = clf.predict(docs_test)
+    # TASK: print the cross-validated scores for the each parameters set
+    # explored by the grid search
+
+    # TASK: Predict the outcome on the testing set and store it in a variable
+    # named y_predicted
 
     # Print the classification report
-    print metrics.classification_report(y_test, y_predicted,
-                                        target_names=dataset.target_names)
+    print(metrics.classification_report(y_test, y_predicted,
+                                        target_names=dataset.target_names))
 
-    # Plot the confusion matrix
+    # Print and plot the confusion matrix
     cm = metrics.confusion_matrix(y_test, y_predicted)
-    print cm
+    print(cm)
 
-    #import pylab as pl
-    #pl.matshow(cm)
-    #pl.show()
+    # import matplotlib.pyplot as plt
+    # plt.matshow(cm)
+    # plt.show()
--- doc/tutorial/text_analytics/solutions/exercise_02_sentiment.py
+++ doc/tutorial/text_analytics/solutions/exercise_02_sentiment.py
@@ -12,8 +12,7 @@ In this examples we will use a movie review dataset.
 # License: Simplified BSD
 
 import sys
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
 from sklearn.pipeline import Pipeline
 from sklearn.grid_search import GridSearchCV
@@ -21,9 +20,6 @@ from sklearn.datasets import load_files
 from sklearn.cross_validation import train_test_split
 from sklearn import metrics
 
-#
-# The real code starts here
-#
 
 if __name__ == "__main__":
     # NOTE: we put the following in a 'if __name__ == "__main__"' protected
@@ -35,44 +31,44 @@ if __name__ == "__main__":
     # the training data folder must be passed as first argument
     movie_reviews_data_folder = sys.argv[1]
     dataset = load_files(movie_reviews_data_folder, shuffle=False)
-    print "n_samples: %d" % len(dataset.data)
+    print("n_samples: %d" % len(dataset.data))
 
     # split the dataset in training and test set:
     docs_train, docs_test, y_train, y_test = train_test_split(
-        dataset.data, dataset.target, test_fraction=0.25, random_state=None)
+        dataset.data, dataset.target, test_size=0.25, random_state=None)
 
-    # TASK: Build a vectorizer / classifier pipeline using the previous
-    # analyzer
+    # TASK: Build a vectorizer / classifier pipeline that filters out tokens
+    # that are too rare or too frequent
     pipeline = Pipeline([
-        ('vect', CountVectorizer(max_features=100000)),
-        ('tfidf', TfidfTransformer()),
+        ('vect', TfidfVectorizer(min_df=3, max_df=0.95)),
         ('clf', LinearSVC(C=1000)),
     ])
-
-    parameters = {
-        'vect__analyzer__max_n': (1, 2),
-        'vect__max_df': (.95,),
-    }
 
     # TASK: Build a grid search to find out whether unigrams or bigrams are
     # more useful.
     # Fit the pipeline on the training set using grid search for the parameters
+    parameters = {
+        'vect__ngram_range': [(1, 1), (1, 2)],
+    }
     grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1)
-    grid_search.fit(docs_train[:200], y_train[:200])
-    # Refit the best parameter set on the complete training set
-    clf = grid_search.best_estimator.fit(docs_train, y_train)
+    grid_search.fit(docs_train, y_train)
 
-    # Predict the outcome on the testing set
-    y_predicted = clf.predict(docs_test)
+    # TASK: print the cross-validated scores for the each parameters set
+    # explored by the grid search
+    print(grid_search.grid_scores_)
+
+    # TASK: Predict the outcome on the testing set and store it in a variable
+    # named y_predicted
+    y_predicted = grid_search.predict(docs_test)
 
     # Print the classification report
-    print metrics.classification_report(y_test, y_predicted,
-                                        target_names=dataset.target_names)
+    print(metrics.classification_report(y_test, y_predicted,
+                                        target_names=dataset.target_names))
 
-    # Plot the confusion matrix
+    # Print and plot the confusion matrix
     cm = metrics.confusion_matrix(y_test, y_predicted)
-    print cm
+    print(cm)
 
-    #import pylab as pl
-    #pl.matshow(cm)
-    #pl.show()
+    # import matplotlib.pyplot as plt
+    # plt.matshow(cm)
+    # plt.show()
