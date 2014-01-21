8b7175cc54835d35e8374482ecf06284e594bfdd
ugurthemaster <ugurthemaster@gmail.com>
2014-01-21 09:19:04 +0200

    Update plot_digits_pipe.py
    Import statements have been moved to the top of the file.

--- examples/plot_digits_pipe.py
+++ examples/plot_digits_pipe.py
@@ -24,11 +24,12 @@ import numpy as np
 import pylab as pl
 
 from sklearn import linear_model, decomposition, datasets
+from sklearn.pipeline import Pipeline
+from sklearn.grid_search import GridSearchCV
 
 logistic = linear_model.LogisticRegression()
 
 pca = decomposition.PCA()
-from sklearn.pipeline import Pipeline
 pipe = Pipeline(steps=[('pca', pca), ('logistic', logistic)])
 
 digits = datasets.load_digits()
@@ -49,8 +50,6 @@ pl.ylabel('explained_variance_')
 
 ###############################################################################
 # Prediction
-
-from sklearn.grid_search import GridSearchCV
 
 n_components = [20, 40, 64]
 Cs = np.logspace(-4, 4, 3)
