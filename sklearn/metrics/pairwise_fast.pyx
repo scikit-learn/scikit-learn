506a3383809d7c031ad820920c1d32cee0aa55a3
Matti Lyra <m.lyra@sussex.ac.uk>
2014-02-08 14:34:24 +0000

    Added pyx file for computing the pairwise L1 distances for a sparse coo matrix.

--- sklearn/metrics/pairwise_fast.pyx
+++ sklearn/metrics/pairwise_fast.pyx
@@ -8,6 +8,8 @@ import cython
 
 np.import_array()
 
+ctypedef np.float64_t double_t
+ctypedef unsigned int uint_t
 ctypedef float [:, :] float_array_2d_t 
 ctypedef double [:, :] double_array_2d_t 
 
@@ -36,3 +38,36 @@ def _chi2_kernel_fast(floating_array_2d_t X,
                 if nom != 0:
                     res  += denom * denom / nom
             result[i, j] = -res
+
+
+def _manhattan_distances_sparse(X, Y,
+        np.ndarray[double_t, ndim=2] result):
+    cdef:
+        int i, r
+        int n_x = len(X.data)
+        int n_y = len(Y.data)
+        int n_clusters = Y.shape[0]
+        int n_documents = X.shape[0]
+        
+        np.ndarray[double_t, ndim=1] xdata = X.data
+        np.ndarray[uint_t, ndim=1] xrow = X.row
+        np.ndarray[uint_t, ndim=1] xcol = X.col
+        np.ndarray[double_t, ndim=1] ydata = Y.data
+        np.ndarray[uint_t, ndim=1] yrow = Y.row
+        np.ndarray[uint_t, ndim=1] ycol = Y.col
+
+    # for x, i, j in zip(X.data, X.row, X.col):
+    for i in xrange(n_x):
+        r = xrow[i] * n_clusters
+        for y in xrange(n_clusters):
+            r += y
+            result[r, xcol[i]] = xdata[i]  # result[r, j] = x
+
+    # for y, i, j in zip(Y.data, Y.row, Y.col):
+    for i in xrange(n_y):
+        for x in xrange(n_documents):
+            r = x * n_clusters + yrow[i]
+            result[r, ycol[i]] -= ydata[i]  # result[r, j] -= y
+
+    result = np.abs(result)
+
