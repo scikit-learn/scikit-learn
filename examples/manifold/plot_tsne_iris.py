53e015c9613e68e2c38c7b8e8c647836c76231e1
Alexander Fabisch <afabisch@tzi.de>
2014-02-09 01:32:03 +0100

    Simplify optimization
    * remove L-BFGS, gradient descent finds better visualizations (although the cost is often higher)
    * learning_rate is a parameter
    * learning schedule:
        * initialize embedding with standard deviation 1
        * 100 episodes with early exaggeration
        * 20 iterations with momentum 0.5
        * then switch to momentum 0.8
        * default learning rate is 100, reasonable range is 100-500

--- examples/manifold/plot_tsne_iris.py
+++ examples/manifold/plot_tsne_iris.py
@@ -13,37 +13,35 @@ from sklearn.datasets import load_iris
 from sklearn.cross_validation import train_test_split
 
 
-if __name__ == "__main__":
-    iris = load_iris()
-    X, y = iris.data, iris.target
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5,
-                                                        random_state=0)
-    tsne = TSNE(n_components=2, optimizer="lbfgs", perplexity=20, n_iter=1000,
-                verbose=1, random_state=0)
-    tsne.fit(X_train)
-    X_embedded_train = tsne.transform(X_train)
-    X_embedded_test = tsne.transform(X_test)
+iris = load_iris()
+X, y = iris.data, iris.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5,
+                                                    random_state=0)
+tsne = TSNE(n_components=2, perplexity=20, random_state=0)
+tsne.fit(X_train)
+X_embedded_train = tsne.transform(X_train)
+X_embedded_test = tsne.transform(X_test)
 
-    plt.figure(figsize=(12, 5))
-    plt.suptitle("Two-dimensional embedding of Iris dataset with t-SNE")
-    ax = plt.subplot(121)
-    ax.set_title("Training data, trustworthiness = %.3f" % tsne.score(X_train))
-    p1 = ax.scatter(X_embedded_train[y_train == 0, 0],
-                    X_embedded_train[y_train == 0, 1], c="r")
-    p2 = ax.scatter(X_embedded_train[y_train == 1, 0],
-                    X_embedded_train[y_train == 1, 1], c="g")
-    p3 = ax.scatter(X_embedded_train[y_train == 2, 0],
-                    X_embedded_train[y_train == 2, 1], c="b")
-    ax.legend((p1, p2, p3), iris.target_names)
-    plt.setp(ax, xticks=(), yticks=())
-    ax = plt.subplot(122)
-    ax.set_title("Test data, trustworthiness = %.3f" % tsne.score(X_test))
-    p1 = ax.scatter(X_embedded_test[y_test == 0, 0],
-                    X_embedded_test[y_test == 0, 1], c="r")
-    p2 = ax.scatter(X_embedded_test[y_test == 1, 0],
-                    X_embedded_test[y_test == 1, 1], c="g")
-    p3 = ax.scatter(X_embedded_test[y_test == 2, 0],
-                    X_embedded_test[y_test == 2, 1], c="b")
-    ax.legend((p1, p2, p3), iris.target_names)
-    plt.setp(ax, xticks=(), yticks=())
-    plt.show()
+plt.figure(figsize=(12, 5))
+plt.suptitle("Two-dimensional embedding of Iris dataset with t-SNE")
+ax = plt.subplot(121)
+ax.set_title("Training data, trustworthiness = %.3f" % tsne.score(X_train))
+p1 = ax.scatter(X_embedded_train[y_train == 0, 0],
+                X_embedded_train[y_train == 0, 1], c="r")
+p2 = ax.scatter(X_embedded_train[y_train == 1, 0],
+                X_embedded_train[y_train == 1, 1], c="g")
+p3 = ax.scatter(X_embedded_train[y_train == 2, 0],
+                X_embedded_train[y_train == 2, 1], c="b")
+ax.legend((p1, p2, p3), iris.target_names, loc="center right")
+plt.setp(ax, xticks=(), yticks=())
+ax = plt.subplot(122)
+ax.set_title("Test data, trustworthiness = %.3f" % tsne.score(X_test))
+p1 = ax.scatter(X_embedded_test[y_test == 0, 0],
+                X_embedded_test[y_test == 0, 1], c="r")
+p2 = ax.scatter(X_embedded_test[y_test == 1, 0],
+                X_embedded_test[y_test == 1, 1], c="g")
+p3 = ax.scatter(X_embedded_test[y_test == 2, 0],
+                X_embedded_test[y_test == 2, 1], c="b")
+ax.legend((p1, p2, p3), iris.target_names, loc="center right")
+plt.setp(ax, xticks=(), yticks=())
+plt.show()
