5b4a3c8d41f9d556c8b9d3a7ac6756ad82bf859d
Joel Nothman <joel.nothman@gmail.com>
2014-02-25 23:44:54 +1100

    TST Validate input MultiLabelBinarizer.inverse_transform

--- sklearn/preprocessing/label.py
+++ sklearn/preprocessing/label.py
@@ -509,7 +509,7 @@ class MultiLabelBinarizer(BaseEstimator, TransformerMixin):
 
         Returns
         -------
-        y_indicator : matrix of shape (n_samples, n_classes)
+        y_indicator : array, shape (n_samples, n_classes)
             A matrix such that `y_indicator[i, j] = 1` iff `classes_[j]` is in
             `y[i]`, and 0 otherwise.
         """
@@ -542,7 +542,7 @@ class MultiLabelBinarizer(BaseEstimator, TransformerMixin):
 
         Returns
         -------
-        y_indicator : matrix of shape (n_samples, n_classes)
+        y_indicator : array, shape (n_samples, n_classes)
             A matrix such that `y_indicator[i, j] = 1` iff `classes_[j]` is in
             `y[i]`, and 0 otherwise.
         """
@@ -579,7 +579,7 @@ class MultiLabelBinarizer(BaseEstimator, TransformerMixin):
 
         Parameters
         ----------
-        yt : matrix of shape (n_samples, n_classes)
+        yt : array of shape (n_samples, n_classes)
             A matrix containing only 1s ands 0s.
 
         Returns
@@ -588,4 +588,13 @@ class MultiLabelBinarizer(BaseEstimator, TransformerMixin):
             The set of labels for each sample such that `y[i]` consists of
             `classes_[j]` for each `yt[i, j] == 1`.
         """
+        yt = np.asarray(yt)
+        if yt.shape[1] != len(self.classes_):
+            raise ValueError('Expected indicator for {0} classes, but got {1}'
+                             .format(len(self.classes_), yt.shape[1]))
+        unexpected = np.setdiff1d(yt, [0, 1])
+        if len(unexpected) > 0:
+            raise ValueError('Expected only 0s and 1s in label indicator. '
+                             'Also got {0}'.format(unexpected))
+
         return [tuple(self.classes_.compress(indicators)) for indicators in yt]
