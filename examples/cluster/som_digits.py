920a2adff0b847b5526472ed5733e92306dfb207
naught101 <naught101@naught101.org>
2014-03-25 14:58:20 +1100

    Make print statements python3 friendly

--- examples/cluster/som_digits.py
+++ examples/cluster/som_digits.py
@@ -10,7 +10,8 @@ with the Caliński-Harabasz criterion (Caliński & Harabasz 1974).
 """
 
 from __future__ import division
-print __doc__
+
+print(__doc__)
 
 from time import time
 import numpy as np
@@ -52,36 +53,36 @@ data = scale(digits.data)
 n_samples, n_features = data.shape
 n_digits = len(np.unique(digits.target))
 
-print "Digits dataset"
-print "n_digits   : %d" % n_digits
-print "n_features : %d" % n_features
-print "n_samples  : %d" % n_samples
-print
+print("Digits dataset")
+print("n_digits   : %d" % n_digits)
+print("n_features : %d" % n_features)
+print("n_samples  : %d" % n_samples)
+print()
 
 # Digits dataset clustering using Self-Organizing Map
 
-print "Self-Organizing Map "
+print("Self-Organizing Map ")
 t0 = time()
 grid_width = 4
 som = SelfOrganizingMap(affinity=(grid_width, grid_width),
                         n_iterations=n_samples*5, learning_rate=1)
 som.fit(data)
-print "done in %0.3fs" % (time() - t0)
-print
+print("done in %0.3fs" % (time() - t0))
+print()
 
 F = Calinski_Harabasz_criterion(data, som.labels_, som.centers_)
 print('Caliński-Harabasz criterion (pseudo F): %0.2f | %0.2f%%' %
       (F, 100 * (F / (1 + F))))
-print
+print()
 
 # Digits dataset clustering using Kmeans
 
-print "KMeans "
+print("KMeans ")
 t0 = time()
 km = KMeans(init='k-means++', n_clusters=grid_width**2, n_init=10)
 km.fit(data)
-print "done in %0.3fs" % (time() - t0)
-print
+print("done in %0.3fs" % (time() - t0))
+print()
 
 F = Calinski_Harabasz_criterion(data, km.labels_, km.cluster_centers_)
 print('Caliński-Harabasz criterion (pseudo F): %0.2f | %0.2f%%' %
