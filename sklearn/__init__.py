6136990d4d0130dee8d6f1436e4acc763e9d706c
Olivier Grisel <olivier.grisel@ensta.org>
2014-03-29 01:11:29 +0100

    Do not import sklearn package from setup.py and generate better dev version
    The execution of setup.py never imports the sklearn package to avoid cyclid
    dependencies.
    
    The sklearn/version.py file is now generated each time setup.py is executed.
    If the version is not a release version, a '.dev_aaaaaaa' suffix is appended
    where 'aaaaaaa' is the 7 first digits of the hexdigest of the last git
    commit in the local repo.

--- sklearn/__init__.py
+++ sklearn/__init__.py
@@ -15,33 +15,20 @@ See http://scikit-learn.org for complete documentation.
 import sys
 import re
 import warnings
-__version__ = '0.15-git'
+from . import __check_build
+from .base import clone
+from .version import full_version as __version__
 
 # Make sure that DeprecationWarning within this package always gets printed
 warnings.filterwarnings('always', category=DeprecationWarning,
                         module='^{0}\.'.format(re.escape(__name__)))
 
-try:
-    # This variable is injected in the __builtins__ by the build
-    # process. It used to enable importing subpackages of sklearn when
-    # the binaries are not built
-    __SKLEARN_SETUP__
-except NameError:
-    __SKLEARN_SETUP__ = False
 
-if __SKLEARN_SETUP__:
-    sys.stderr.write('Partial import of sklearn during the build process.\n')
-    # We are not importing the rest of the scikit during the build
-    # process, as it may not be compiled yet
-else:
-    from . import __check_build
-    from .base import clone
-
-    def test(*args, **kwargs):
-        import warnings
-        # Not using a DeprecationWarning, as they are turned off by
-        # default
-        warnings.warn("""sklearn.test() is no longer supported to run the
+def test(*args, **kwargs):
+    import warnings
+    # Not using a DeprecationWarning, as they are turned off by
+    # default
+    warnings.warn("""sklearn.test() is no longer supported to run the
 scikit-learn test suite.
 
 After installation, you can launch the test suite from outside the
@@ -56,18 +43,18 @@ This function, `sklearn.test()` does not do anything. It does not run
 the tests and will be removed in release 0.16.
 """, stacklevel=2)
 
-    # The following line is useful so that nosetests doesn't consider
-    # "test" as a test function
-    test.__test__ = False
+# The following line is useful so that nosetests doesn't consider
+# "test" as a test function
+test.__test__ = False
 
-    __all__ = ['cross_validation', 'cluster', 'covariance',
-               'datasets', 'decomposition', 'feature_extraction',
-               'feature_selection', 'semi_supervised',
-               'gaussian_process', 'grid_search', 'hmm', 'lda', 'linear_model',
-               'metrics', 'mixture', 'naive_bayes', 'neighbors', 'pipeline',
-               'preprocessing', 'qda', 'svm', 'clone',
-               'cross_decomposition',
-               'isotonic', 'pls']
+__all__ = ['cross_validation', 'cluster', 'covariance',
+           'datasets', 'decomposition', 'feature_extraction',
+           'feature_selection', 'semi_supervised',
+           'gaussian_process', 'grid_search', 'hmm', 'lda', 'linear_model',
+           'metrics', 'mixture', 'naive_bayes', 'neighbors', 'pipeline',
+           'preprocessing', 'qda', 'svm', 'clone',
+           'cross_decomposition',
+           'isotonic', 'pls']
 
 
 def setup_module(module):
