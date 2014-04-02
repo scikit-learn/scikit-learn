62401aa3bdf17d33fd09e0b21d55a47e3042d079
Peter Gijsbert Jacob-Jan Smit <pjottrschmitz@gmail.com>
2014-04-02 18:23:28 +0200

    Added text to the example.

--- examples/cluster/plot_mean_shift_kernels.py
+++ examples/cluster/plot_mean_shift_kernels.py
@@ -1,7 +1,22 @@
 """
-=============================================
-A demo of the mean-shift clustering algorithm
-=============================================
+============================================
+Mean-Shift clustering with different kernels
+============================================
+
+This example shows the different outcomes with different kernels.
+The flat kernel simply wants to have as many points as possible located
+in it, while the rbf kernel wants as many points possible near its center.
+This means that the flat kernel will be more likely to try to cover up two
+or more distinct clusters because it doesn't care if the dense cluster
+centers are located at its periphery.
+
+With real world data the rbf kernel is probably preferred. The cluster center
+is more likely to end up directly above an area with high density. It should
+also be more robust on datasets with few points and noise.
+
+The code borrows heavily from the other Mean-Shift example, and a
+blog post by the original author:
+http://sociograph.blogspot.nl/2011/11/accessible-introduction-to-mean-shift.html
 
 Reference:
 
@@ -9,18 +24,20 @@ Dorin Comaniciu and Peter Meer, "Mean Shift: A robust approach toward
 feature space analysis". IEEE Transactions on Pattern Analysis and
 Machine Intelligence. 2002. pp. 603-619.
 
+Yizong Cheng, "Mean Shift, Mode Seeking, and Clustering". IEEE
+Transactions on Pattern Analysis and Machine Intelligence. 1995. pp. 790-799.
+
 """
-#print(__doc__)
+print(__doc__)
 
 import numpy as np
-from sklearn.cluster import MeanShift, estimate_bandwidth
+from sklearn.cluster import MeanShift
 from sklearn.datasets.samples_generator import make_blobs
-import sklearn
 
 ###############################################################################
 # Generate sample data
 centers = [[2, 0], [-2, 0]]
-X, _ = make_blobs(n_samples=10000, centers=centers, cluster_std=(1.0,1.0))
+X, _ = make_blobs(n_samples=10000, centers=centers, cluster_std=(1.0, 1.0))
 
 ###############################################################################
 # Compute clustering with MeanShift
@@ -50,7 +67,7 @@ cluster_centers_flat = ms_flat.cluster_centers_
 labels_unique_flat = np.unique(labels_flat)
 n_clusters_flat = len(labels_unique_flat)
 
-print("number of estimated clusters using the flat kernel: %d" % 
+print("number of estimated clusters using the flat kernel: %d" %
       n_clusters_flat)
 
 ###############################################################################
@@ -62,7 +79,7 @@ pl.figure(1)
 pl.clf()
 colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
 
-pl.subplot(2,1,1)
+pl.subplot(2, 1, 1)
 for k, col in zip(range(n_clusters_rbf), colors):
     my_members = labels_rbf == k
     cluster_center = cluster_centers_rbf[k]
@@ -71,7 +88,7 @@ for k, col in zip(range(n_clusters_rbf), colors):
             markeredgecolor='k', markersize=14)
 pl.title('Clustering using the rbf kernel')
 
-pl.subplot(2,1,2)
+pl.subplot(2, 1, 2)
 for k, col in zip(range(n_clusters_flat), colors):
     my_members = labels_flat == k
     cluster_center = cluster_centers_flat[k]
