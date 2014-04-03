80ef9ebb10a6e447b2386ddf2ba6a87e30d0784b
Olivier Grisel <olivier.grisel@ensta.org>
2014-04-03 14:59:26 +0200

    DOC: add some header doc to the travis scripts

--- continuous_integration/test_script.sh
+++ continuous_integration/test_script.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# This script is meant to be called by the "script" step defined in
+# .travis.yml. See http://docs.travis-ci.com/ for more details.
+# The behavior of the script is controlled by environment variabled defined
+# in the .travis.yml in the top level folder of the project.
 
 set -e
 
