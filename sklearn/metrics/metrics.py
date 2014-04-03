6f00678a7097d70e11255955295b0be3de792d4f
Arnaud Joly <arnaud.v.joly@gmail.com>
2014-04-03 08:04:23 +0200

    FIX use np.bincount

--- sklearn/metrics/metrics.py
+++ sklearn/metrics/metrics.py
@@ -2210,13 +2210,13 @@ def label_ranking_average_precision_score(y_true, y_score):
         # Compute denominators
         unique_all, inverse = np.unique(y_score[i], return_inverse=True)
         count = np.bincount(inverse, minlength=unique_all.size)
-        cum_count = count[::-1].cumsum()[::-1] # reverse cumsum
+        cum_count = count[::-1].cumsum()[::-1]  # reverse cumsum
 
         # Compute numerators
         unique_relevant, relevant_inverse = np.unique(y_score[i, relevant],
                                                       return_inverse=True)
         count_relevant = np.bincount(relevant_inverse,
-                                  minlength=unique_relevant.size)
+                                     minlength=unique_relevant.size)
         cum_count_relevant = count_relevant[::-1].cumsum()[::-1]
 
         score += (cum_count_relevant[relevant_inverse] /
