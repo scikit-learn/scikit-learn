e4f9b5b4b07960b153059e7ea55f584e11386a6b
sdenton4 <sdenton4@gmail.com>
2014-04-17 15:59:15 -0400

    Improved the learning curve example.
    Scaled y-axis to make it easier to compare the two methods.
    Wrapped duplicate code into a utility function that users can copy, paste
    and adapt in their own project.
    Added more iterations to cross-validate the GaussianNB model to get a smoother
    curve.

--- examples/plot_learning_curve.py
+++ examples/plot_learning_curve.py
@@ -17,51 +17,90 @@ print(__doc__)
 
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import cross_validation
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn.datasets import load_digits
 from sklearn.learning_curve import learning_curve
 
 
+def plot_learning_curve(estimator, title, X, y, ylim=(0.7,1.01), cv=None, 
+    n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
+    """
+    Generate a simple plot of the test and traning learning curve.
+
+    Parameters
+    ----------
+    estimator : object type that implements the "fit" and "predict" methods
+        An object of that type which is cloned for each validation.
+
+    title : string
+        Title for the chart.
+
+    X : array-like, shape (n_samples, n_features)
+        Training vector, where n_samples is the number of samples and
+        n_features is the number of features.
+
+    y : array-like, shape (n_samples) or (n_samples, n_features), optional
+        Target relative to X for classification or regression;
+        None for unsupervised learning.
+
+    ylim : tuple, shape (ymin, ymax), optional
+        Defines minimum and maximum yvalues plotted.  Defaults to (0.7, 1.01)
+        for easy comparison of plots.
+
+    cv : integer, cross-validation generator, optional
+        If an integer is passed, it is the number of folds (defaults to 3).
+        Specific cross-validation objects can be passed, see
+        sklearn.cross_validation module for the list of possible objects
+
+    n_jobs : integer, optional
+        Number of jobs to run in parallel (default 1).
+    """
+    plt.figure()
+    plt.title(title)
+    plt.ylim( *ylim )
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1, color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+             label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
+
+    plt.legend(loc="best")
+    return plt
+
+
 digits = load_digits()
 X, y = digits.data, digits.target
 
-plt.figure()
-plt.title("Learning Curve (Naive Bayes)")
-plt.xlabel("Training examples")
-plt.ylabel("Score")
-train_sizes, train_scores, test_scores = learning_curve(
-    GaussianNB(), X, y, cv=10, n_jobs=1)
-train_scores_mean = np.mean(train_scores, axis=1)
-train_scores_std = np.std(train_scores, axis=1)
-test_scores_mean = np.mean(test_scores, axis=1)
-test_scores_std = np.std(test_scores, axis=1)
-plt.plot(train_sizes, train_scores_mean, label="Training score", color="r")
-plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
-                 train_scores_mean + train_scores_std, alpha=0.2, color="r")
-plt.plot(train_sizes, test_scores_mean, label="Cross-validation score",
-         color="g")
-plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
-                 test_scores_mean + test_scores_std, alpha=0.2, color="g")
-plt.legend(loc="best")
 
-plt.figure()
-plt.title("Learning Curve (SVM, RBF kernel, $\gamma=0.001$)")
-plt.xlabel("Training examples")
-plt.ylabel("Score")
-train_sizes, train_scores, test_scores = learning_curve(
-    SVC(gamma=0.001), X, y, cv=10, n_jobs=4)
-train_scores_mean = np.mean(train_scores, axis=1)
-train_scores_std = np.std(train_scores, axis=1)
-test_scores_mean = np.mean(test_scores, axis=1)
-test_scores_std = np.std(test_scores, axis=1)
-plt.plot(train_sizes, train_scores_mean, label="Training score", color="r")
-plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
-                 train_scores_mean + train_scores_std, alpha=0.2, color="r")
-plt.plot(train_sizes, test_scores_mean, label="Cross-validation score",
-         color="g")
-plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
-                 test_scores_mean + test_scores_std, alpha=0.2, color="g")
-plt.legend(loc="best")
+title="Learning Curve (Naive Bayes)"
+# Cross validation with 100 iterations to get smoother mean test and train 
+# score curves, each time with 20% data randomly selected as the validation set.
+cv = cross_validation.ShuffleSplit(digits.data.shape[0], n_iter=100,
+                                   test_size=0.2, random_state=0)
+
+estimator=GaussianNB()
+plot_learning_curve(estimator, title, X, y, cv=cv, n_jobs=4)
+
+title="Learning Curve (SVM, RBF kernel, $\gamma=0.001$)"
+# SVC is more expensive so we do a lower number of CV iterations:
+cv = cross_validation.ShuffleSplit(digits.data.shape[0], n_iter=10,
+                                   test_size=0.2, random_state=0)
+estimator=SVC(gamma=0.001)
+plot_learning_curve(estimator, title, X, y, cv=cv, n_jobs=4)
 
 plt.show()
