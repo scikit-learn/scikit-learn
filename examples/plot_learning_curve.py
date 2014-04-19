979262448309770524981578e64e2160a076eb29
Olivier Grisel <olivier.grisel@ensta.org>
2014-04-19 12:38:16 -0400

    COSMIT: use plural, there are 2 learning curves

--- examples/plot_learning_curve.py
+++ examples/plot_learning_curve.py
@@ -88,7 +88,7 @@ digits = load_digits()
 X, y = digits.data, digits.target
 
 
-title = "Learning Curve (Naive Bayes)"
+title = "Learning Curves (Naive Bayes)"
 # Cross validation with 100 iterations to get smoother mean test and train
 # score curves, each time with 20% data randomly selected as a validation set.
 cv = cross_validation.ShuffleSplit(digits.data.shape[0], n_iter=100,
@@ -97,7 +97,7 @@ cv = cross_validation.ShuffleSplit(digits.data.shape[0], n_iter=100,
 estimator = GaussianNB()
 plot_learning_curve(estimator, title, X, y, ylim=(0.7, 1.01), cv=cv, n_jobs=4)
 
-title = "Learning Curve (SVM, RBF kernel, $\gamma=0.001$)"
+title = "Learning Curves (SVM, RBF kernel, $\gamma=0.001$)"
 # SVC is more expensive so we do a lower number of CV iterations:
 cv = cross_validation.ShuffleSplit(digits.data.shape[0], n_iter=10,
                                    test_size=0.2, random_state=0)
