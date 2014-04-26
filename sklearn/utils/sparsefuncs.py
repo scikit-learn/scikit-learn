31d438eb3df7e75f53d4cf895c7a0d91269be399
Manoj-Kumar-S <manojkumarsivaraj334@gmail.com>
2014-04-26 23:38:58 +0530

    COSMIT: Replaced ptr1/2 with start/stop

--- sklearn/utils/sparsefuncs.py
+++ sklearn/utils/sparsefuncs.py
@@ -97,27 +97,30 @@ def swap_row_csr(X, m, n):
         m, n = n, m
 
     indptr = X.indptr
-    m_ptr1 = indptr[m]
-    m_ptr2 = indptr[m + 1]
-    n_ptr1 = indptr[n]
-    n_ptr2 = indptr[n + 1]
-    nz_m = m_ptr2 - m_ptr1
-    nz_n = n_ptr2 - n_ptr1
+    m_start = indptr[m]
+    m_stop = indptr[m + 1]
+    n_start = indptr[n]
+    n_stop = indptr[n + 1]
+    nz_m = m_stop - m_start
+    nz_n = n_stop - n_start
 
 
     if nz_m != nz_n:
         # Modify indptr first
         X.indptr[m + 2:n] += nz_n - nz_m
-        X.indptr[m + 1] = X.indptr[m] + nz_n
-        X.indptr[n] = X.indptr[n + 1] - nz_m
+        X.indptr[m + 1] = m_start + nz_n
+        X.indptr[n] = n_stop - nz_m
 
-    X.indices = np.concatenate([X.indices[:m_ptr1], X.indices[n_ptr1:n_ptr2],
-                                X.indices[m_ptr2:n_ptr1],
-                                X.indices[m_ptr1:m_ptr2],
-                                X.indices[n_ptr2:]])
-    X.data = np.concatenate([X.data[:m_ptr1], X.data[n_ptr1:n_ptr2],
-                             X.data[m_ptr2:n_ptr1], X.data[m_ptr1:m_ptr2],
-                             X.data[n_ptr2:]])
+    X.indices = np.concatenate([X.indices[:m_start],
+                                X.indices[n_start:n_stop],
+                                X.indices[m_stop:n_start],
+                                X.indices[m_start:m_stop],
+                                X.indices[n_stop:]])
+    X.data = np.concatenate([X.data[:m_start],
+                             X.data[n_start:n_stop],
+                             X.data[m_stop:n_start],
+                             X.data[m_start:m_stop],
+                             X.data[n_stop:]])
 
 
 def swap_row(X, m, n):
