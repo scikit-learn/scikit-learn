da79a67575486aa45deeea6fb30139647dcda463
Alexis Mignon <alexis.mignon@gmail.com>
2014-04-29 17:58:15 +0200

    Remove useless import

--- sklearn/mixture/gmm.py
+++ sklearn/mixture/gmm.py
@@ -97,7 +97,6 @@ def sample_gaussian(mean, covar, covariance_type='diag', n_samples=1,
     elif covariance_type == 'diag':
         rand = np.dot(np.diag(np.sqrt(covar)), rand)
     else:
-        from scipy import linalg
         U, s, V = linalg.svd(covar)
         sqrtS = np.diag(np.sqrt(s))
         sqrt_covar = np.dot(U, np.dot(sqrtS, V))
