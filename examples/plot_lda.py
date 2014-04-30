dd2244d95b91713344a66de4e081236d3613b08b
Clemens Brunner <clemens.brunner@tugraz.at>
2014-04-30 13:08:00 +0200

    Renamed SLDA to ShrinkageLDA and moved it to lda.py.

---
+++ examples/plot_lda.py
@@ -0,0 +1,66 @@
+"""
+====================================================================
+Normal and Shrinkage Linear Discriminant Analysis for classification
+====================================================================
+
+Shows how shrinkage improves classification.
+"""
+
+from __future__ import division
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.lda import LDA, ShrinkageLDA
+
+
+n_train = 10     # samples per class for training
+n_test = 100    # samples per class for training
+n_averages = 50
+
+
+def generate_data(n_samples, n_features):
+    """ generate `n_samples` samples of data with 1 discriminative features
+    and n_`features` non-discriminative features."""
+    X = np.vstack([np.random.randn(n_samples, 1) - 2,
+                   np.random.randn(n_samples, 1) + 2])
+    y = np.hstack([[-1] * n_samples, [1] * n_samples])
+    # add non discriminative features
+    X = np.hstack([X, np.random.randn(2 * n_samples, n_features)])
+    return X, y
+
+acc_lda, acc_slda, acc_nlda = [], [], []
+m_range = range(50)
+for m in m_range:
+    print(m)
+    tmp_lda, tmp_slda, tmp_nlda = 0, 0, 0
+    for i in range(n_averages):
+        X, y = generate_data(n_train, m)
+
+        lda = LDA().fit(X, y)
+        slda = ShrinkageLDA().fit(X, y)
+        nlda = ShrinkageLDA(shrinkage=None).fit(X, y)
+
+        X, y = generate_data(n_test, m)
+        tmp_lda += lda.score(X, y) / n_averages
+        tmp_slda += slda.score(X, y) / n_averages
+        tmp_nlda += nlda.score(X, y) / n_averages
+
+    acc_lda.append(tmp_lda)
+    acc_slda.append(tmp_slda)
+    acc_nlda.append(tmp_nlda)
+
+m_range = (1 + np.array(m_range)) / (2 * n_train)
+
+plt.plot(m_range, acc_slda, linewidth=2, label='ShrinkageLDA()', color='r')
+plt.plot(m_range, acc_nlda, linewidth=2, label='ShrinkageLDA(shrinkage=None)', color='g')
+plt.plot(m_range, acc_lda, linewidth=2, label='LDA()', color='b')
+
+plt.xlabel('n_features / n_samples')
+plt.ylabel('Classification accuracy')
+
+plt.legend(loc=4, prop={'size': 8})
+
+plt.suptitle('LDA vs sLDA (1 discriminative feature)')
+
+plt.show()
--- sklearn/tests/test_lda.py
+++ sklearn/tests/test_lda.py
@@ -26,30 +26,14 @@ def test_lda_predict():
     correct values for a simple toy dataset.
     """
 
-    # Test invalid skrinkage
-    clf = slda.SLDA(shrinkage='000')
+    clf = lda.LDA()
     y_pred = clf.fit(X, y2).predict(X)
-    assert_array_equal(y_pred, y2)
 
-    # Test without shrinkage
-    clf = slda.SLDA(shrinkage=None)
-    y_pred = clf.fit(X, y2).predict(X)
-    assert_array_equal(y_pred, y2)
-
-    # Test with shrinkage
-    clf = slda.SLDA(shrinkage='auto')
-    y_pred = clf.fit(X, y2).predict(X)
     assert_array_equal(y_pred, y2)
 
     # Assure that it works with 1D data
     y_pred1 = clf.fit(X1, y2).predict(X1)
     assert_array_equal(y_pred1, y2)
-
-    # Test decision function (two-class case)
-    clf = slda.SLDA().fit(X, y2)
-    y_decf = clf.decision_function(X)
-    y_logp = clf.predict_log_proba(X)
-    assert_array_almost_equal(y_decf, y_logp[:, 1] - y_logp[:, 0], 8)
 
     # Test probas estimates
     y_proba_pred1 = clf.predict_proba(X1)
@@ -62,20 +46,78 @@ def test_lda_predict():
     # LDA shouldn't be able to separate those
     assert_true(np.any(y_pred3 != y3))
 
+<<<<<<< HEAD
 <<<<<<< HEAD:sklearn/tests/test_lda.py
 
+=======
+>>>>>>> Renamed SLDA to ShrinkageLDA and moved it to lda.py.
 def test_lda_transform():
     clf = lda.LDA()
-    X_transformed = clf.fit(X, y).transform(X)
+    X_transformed = clf.fit(X, y2).transform(X)
     assert_equal(X_transformed.shape[1], 1)
+<<<<<<< HEAD
 =======
     # Test priors
     clf = slda.SLDA(priors=[-2, 4])
     assert_raises(ValueError, clf.fit, X, y1)
     clf = slda.SLDA(priors=[2, 1])
+=======
+
+def test_slda_predict():
+    """
+    Shrinkage LDA classification.
+
+    This checks that ShrinkageLDA implements fit and predict and returns
+    correct values for a simple toy dataset.
+    """
+
+    # Test invalid shrinkage
+    clf = lda.ShrinkageLDA(shrinkage='000')
+    y_pred = clf.fit(X, y2).predict(X)
+    assert_array_equal(y_pred, y2)
+
+    # Test without shrinkage
+    clf = lda.ShrinkageLDA(shrinkage=None)
+    y_pred = clf.fit(X, y2).predict(X)
+    assert_array_equal(y_pred, y2)
+
+    # Test with shrinkage
+    clf = lda.ShrinkageLDA(shrinkage='auto')
+    y_pred = clf.fit(X, y2).predict(X)
+    assert_array_equal(y_pred, y2)
+
+    # Assure that it works with 1D data
+    y_pred1 = clf.fit(X1, y2).predict(X1)
+    assert_array_equal(y_pred1, y2)
+
+    # Test decision function (two-class case)
+    clf = lda.ShrinkageLDA().fit(X, y2)
+    y_decf = clf.decision_function(X)
+    y_logp = clf.predict_log_proba(X)
+    assert_array_almost_equal(y_decf, y_logp[:, 1] - y_logp[:, 0], 8)
+
+    # Test probability estimates
+    y_proba_pred1 = clf.predict_proba(X1)
+    assert_array_equal((y_proba_pred1[:, 1] > 0.5) + 1, y2)
+    y_log_proba_pred1 = clf.predict_log_proba(X1)
+    assert_array_almost_equal(np.exp(y_log_proba_pred1), y_proba_pred1, 8)
+
+    # Primarily test for commit 2f34950 -- "reuse" of priors
+    y_pred3 = clf.fit(X, y3).predict(X)
+    # LDA shouldn't be able to separate those
+    assert_true(np.any(y_pred3 != y3))
+
+    # Test priors
+    clf = lda.ShrinkageLDA(priors=[-2, 4])
+    assert_raises(ValueError, clf.fit, X, y1)
+    clf = lda.ShrinkageLDA(priors=[2, 1])
+>>>>>>> Renamed SLDA to ShrinkageLDA and moved it to lda.py.
     y_pred = clf.fit(X, y2).predict(X)
     assert_array_equal(y_pred, y2)
 
     # Test one class
     assert_raises(ValueError, clf.fit, X, y1)
+<<<<<<< HEAD
 >>>>>>> Added more tests for improved coverage:sklearn/tests/test_slda.py
+=======
+>>>>>>> Renamed SLDA to ShrinkageLDA and moved it to lda.py.
