FIX numerical stability in GMM with eigh sampling

sklearn/mixture/gmm.py
@@ -98,8 +98,10 @@ def sample_gaussian(mean, covar, covariance_type='diag', n_samples=1,
         rand = np.dot(np.diag(np.sqrt(covar)), rand)
     else:
         s, U = linalg.eigh(covar)
-        sqrt_covar = U * np.sqrt(s)
-        rand = np.dot(sqrt_covar, rand)
+        np.abs(s, out=s)        # get rid of tiny negatives
+        np.sqrt(s, out=s)
+        U *= s
+        rand = np.dot(U, rand)
 
     return (rand.T + mean).T
 