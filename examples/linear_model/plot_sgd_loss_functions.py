6f87dc26a32054bf3a07b0becec06b3f9caaa538
Fares Hedayati <fares.hedayati@gmail.com>
2014-05-20 09:03:39 -0700

    fixed merge conflicts

--- examples/linear_model/plot_sgd_loss_functions.py
+++ examples/linear_model/plot_sgd_loss_functions.py
@@ -9,11 +9,7 @@ A plot that compares the various convex loss functions supported by
 print(__doc__)
 
 import numpy as np
-<<<<<<< HEAD
-import matplotlib.pyplot as plt
-=======
 import pylab as pl
->>>>>>> 00a5d7ee3aa701b3a6d0d523ae0a9887911e651a
 
 
 def modified_huber_loss(y_true, y_pred):
@@ -26,25 +22,6 @@ def modified_huber_loss(y_true, y_pred):
 
 xmin, xmax = -4, 4
 xx = np.linspace(xmin, xmax, 100)
-<<<<<<< HEAD
-plt.plot([xmin, 0, 0, xmax], [1, 1, 0, 0], 'k-',
-         label="Zero-one loss")
-plt.plot(xx, np.where(xx < 1, 1 - xx, 0), 'g-',
-         label="Hinge loss")
-plt.plot(xx, -np.minimum(xx, 0), 'm-',
-         label="Perceptron loss")
-plt.plot(xx, np.log2(1 + np.exp(-xx)), 'r-',
-         label="Log loss")
-plt.plot(xx, np.where(xx < 1, 1 - xx, 0) ** 2, 'b-',
-         label="Squared hinge loss")
-plt.plot(xx, modified_huber_loss(xx, 1), 'y--',
-         label="Modified Huber loss")
-plt.ylim((0, 8))
-plt.legend(loc="upper right")
-plt.xlabel(r"Decision function $f(x)$")
-plt.ylabel("$L(y, f(x))$")
-plt.show()
-=======
 pl.plot([xmin, 0, 0, xmax], [1, 1, 0, 0], 'k-',
         label="Zero-one loss")
 pl.plot(xx, np.where(xx < 1, 1 - xx, 0), 'g-',
@@ -62,4 +39,3 @@ pl.legend(loc="upper right")
 pl.xlabel(r"Decision function $f(x)$")
 pl.ylabel("$L(y, f(x))$")
 pl.show()
->>>>>>> 00a5d7ee3aa701b3a6d0d523ae0a9887911e651a
