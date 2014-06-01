9bee5b4d2f8f06b483e2924ac7b784c1930ae313
bwignall <brianwignall@gmail.com>
2014-06-01 17:43:21 -0400

    CLN: Capitalize "Dirichlet" and "Mexican" in example docstrings

--- examples/mixture/plot_gmm_sin.py
+++ examples/mixture/plot_gmm_sin.py
@@ -8,7 +8,7 @@ complexity control and dealing with sparse data. The dataset is formed
 by 100 points loosely spaced following a noisy sine curve. The fit by
 the GMM class, using the expectation-maximization algorithm to fit a
 mixture of 10 Gaussian components, finds too-small components and very
-little structure. The fits by the dirichlet process, however, show
+little structure. The fits by the Dirichlet process, however, show
 that the model can either learn a global structure for the data (small
 alpha) or easily interpolate to finding relevant local structure
 (large alpha), never falling into the problems shown by the GMM class.
