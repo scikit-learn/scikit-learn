2df32d0e6cb8fd963649c766aea0db86f815f683
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2014-06-08 11:26:12 +0200

    DOC : clarify error message in _binary_roc_auc_score

--- sklearn/metrics/metrics.py
+++ sklearn/metrics/metrics.py
@@ -579,7 +579,8 @@ def roc_auc_score(y_true, y_score, average="macro", sample_weight=None):
     """
     def _binary_roc_auc_score(y_true, y_score, sample_weight=None):
         if len(np.unique(y_true)) != 2:
-            raise ValueError("ROC AUC score is not defined")
+            raise ValueError("Only one class present in y_true. ROC AUC score "
+                             "is not defined in that case.")
 
         fpr, tpr, tresholds = roc_curve(y_true, y_score,
                                         sample_weight=sample_weight)
