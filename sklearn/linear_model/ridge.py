2931627bdc44c1bfc568193b9f5b3d74407053e9
Lars Buitinck <larsmans@gmail.com>
2014-06-18 23:52:02 +0200

    FIX RidgeClassifierCV didn't have scoring parameter
    ... but is advertised as having one

--- sklearn/linear_model/ridge.py
+++ sklearn/linear_model/ridge.py
@@ -1027,11 +1027,11 @@ class RidgeClassifierCV(LinearClassifierMixin, _BaseRidgeCV):
     advantage of the multi-variate response support in Ridge.
     """
     def __init__(self, alphas=np.array([0.1, 1.0, 10.0]), fit_intercept=True,
-                 normalize=False, score_func=None, loss_func=None, cv=None,
-                 class_weight=None):
+                 normalize=False, scoring=None, score_func=None,
+                 loss_func=None, cv=None, class_weight=None):
         super(RidgeClassifierCV, self).__init__(
             alphas=alphas, fit_intercept=fit_intercept, normalize=normalize,
-            score_func=score_func, loss_func=loss_func, cv=cv)
+            scoring=scoring, score_func=score_func, loss_func=loss_func, cv=cv)
         self.class_weight = class_weight
 
     def fit(self, X, y, sample_weight=None, class_weight=None):
