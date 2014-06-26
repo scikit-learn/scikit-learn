e6da2c19c491d0af3ad2cb2da4181b52ab53ee5d
abhishek thakur <abhishekkrthakur@users.noreply.github.com>
2014-06-26 11:36:36 +0200

    small spelling error in doc

--- sklearn/linear_model/stochastic_gradient.py
+++ sklearn/linear_model/stochastic_gradient.py
@@ -559,7 +559,7 @@ class SGDClassifier(BaseSGDClassifier, _LearntSelectorMixin):
     penalty : str, 'l2' or 'l1' or 'elasticnet'
         The penalty (aka regularization term) to be used. Defaults to 'l2'
         which is the standard regularizer for linear SVM models. 'l1' and
-        'elasticnet' migh bring sparsity to the model (feature selection)
+        'elasticnet' might bring sparsity to the model (feature selection)
         not achievable with 'l2'.
 
     alpha : float
@@ -969,7 +969,7 @@ class SGDRegressor(BaseSGDRegressor, _LearntSelectorMixin):
     penalty : str, 'l2' or 'l1' or 'elasticnet'
         The penalty (aka regularization term) to be used. Defaults to 'l2'
         which is the standard regularizer for linear SVM models. 'l1' and
-        'elasticnet' migh bring sparsity to the model (feature selection)
+        'elasticnet' might bring sparsity to the model (feature selection)
         not achievable with 'l2'.
 
     alpha : float
