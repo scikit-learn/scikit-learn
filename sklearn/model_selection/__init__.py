f8c4b044d96cbe4254571d02817635c3ce907105
Ignacio Rossi <rossi.ignacio@gmail.com>
2014-07-01 17:19:53 -0300

    Move learning_curve module into model_selection.validate

--- sklearn/model_selection/__init__.py
+++ sklearn/model_selection/__init__.py
@@ -1,7 +1,8 @@
 from .partition import LeaveOneOut, LeavePOut, KFold, \
         StratifiedKFold, LeaveOneLabelOut, LeavePLabelOut, \
         ShuffleSplit, StratifiedShuffleSplit, train_test_split, check_cv
-from .validate import cross_val_score, permutation_test_score
+from .validate import cross_val_score, permutation_test_score, \
+        learning_curve, validation_curve
 
 __all__ = ['KFold',
            'LeaveOneLabelOut',
@@ -14,4 +15,7 @@ __all__ = ['KFold',
            'check_cv',
            'cross_val_score',
            'permutation_test_score',
-           'train_test_split']
+           'train_test_split',
+           'learning_curve',
+           'validation_curve',
+          ]
