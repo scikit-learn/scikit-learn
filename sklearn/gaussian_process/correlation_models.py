bd293b64ca28a9b79ab2465301c33bbc8c5e8edb
Jan Hendrik Metzen <jhm@informatik.uni-bremen.de>
2014-07-04 10:23:24 +0200

    REFACTOR Factored out common code of all correlation models into baseclass

--- sklearn/gaussian_process/correlation_models.py
+++ sklearn/gaussian_process/correlation_models.py
@@ -2,6 +2,8 @@
 
 # Author: Vincent Dubourg <vincent.dubourg@gmail.com>
 #         (mostly translation, see implementation details)
+#         Jan Hendrik Metzen <jhm@informatik.uni-bremen.de>
+#         (converting to a object-oriented, more modular design)
 # Licence: BSD 3 clause
 
 """
@@ -69,30 +71,7 @@ class StationaryCorrelation(object):
 #                raise Exception("Multiple input features cannot have the same"
 #                                " value.")
 
-
-class AbsoluteExponential(StationaryCorrelation):
-
     def __call__(self, theta, X=None):
-        """
-        Absolute exponential autocorrelation model.
-        (Ornstein-Uhlenbeck stochastic process)::
-
-                                                n
-            theta, dx --> r(theta, dx) = exp(  sum  - theta_i * |dx_i| )
-                                              i = 1
-
-        Parameters
-        ----------
-        theta : array_like
-            An array with shape 1 (isotropic) or n (anisotropic) giving the
-            autocorrelation parameter(s).
-
-        Returns
-        -------
-        r : array_like
-            An array with shape (n_eval, ) containing the values of the
-            autocorrelation model.
-        """
         theta = np.asarray(theta, dtype=np.float)
         if X is not None:
             # Get pairwise componentwise L1-distances to the input training set
@@ -105,12 +84,7 @@ class AbsoluteExponential(StationaryCorrelation):
         else:
             n_features = 1
 
-        if theta.size == 1:
-            r = np.exp(- theta[0] * np.sum(d, axis=1))
-        elif theta.size != n_features:
-            raise ValueError("Length of theta must be 1 or %s" % n_features)
-        else:
-            r = np.exp(- np.sum(theta.reshape(1, n_features) * d, axis=1))
+        r = self._compute_corr(theta, d, n_features)
 
         if X is not None:
             return r.reshape(-1, self.n_samples)
@@ -119,95 +93,113 @@ class AbsoluteExponential(StationaryCorrelation):
             R[self.ij[:, 0], self.ij[:, 1]] = r
             R[self.ij[:, 1], self.ij[:, 0]] = r
             return R
+
+
+class AbsoluteExponential(StationaryCorrelation):
+    """ Absolute exponential autocorrelation model.
+
+    Absolute exponential autocorrelation model (Ornstein-Uhlenbeck stochastic
+    process)::
+
+                                              n
+            theta, d --> r(theta, d) = exp(  sum  - theta_i * d_i )
+                                             i = 1
+    """
+
+    def _compute_corr(self, theta, d, n_features):
+        """ Correlation for given pairwise, component-wise L1-differences.
+
+        Parameters
+        ----------
+        theta : array_like, shape=(1,) or (n_features,)
+            An array with shape 1 (isotropic) or n_features (anisotropic)
+            giving the autocorrelation parameter(s).
+
+        d : array_like, shape=(n_eval, n_features)
+            An array with the pairwise, component-wise L1-differences of x
+            and x' at which the correlation model should be evaluated.
+
+        Returns
+        -------
+        r : array_like, shape=(n_eval, )
+            An array containing the values of the autocorrelation model.
+        """
+        if theta.size == 1:
+            return np.exp(- theta[0] * np.sum(d, axis=1))
+        elif theta.size != n_features:
+            raise ValueError("Length of theta must be 1 or %s" % n_features)
+        else:
+            return np.exp(- np.sum(theta.reshape(1, n_features) * d, axis=1))
 
 
 class SquaredExponential(StationaryCorrelation):
+    """ Squared exponential correlation model.
 
-    def __call__(self, theta, X=None):
-        """
-        Squared exponential correlation model (Radial Basis Function).
-        (Infinitely differentiable stochastic process, very smooth)::
+    Squared exponential correlation model (Radial Basis Function).
+    (Infinitely differentiable stochastic process, very smooth)::
 
-                                                n
-            theta, dx --> r(theta, dx) = exp(  sum  - theta_i * (dx_i)^2 )
-                                              i = 1
+                                              n
+            theta, d --> r(theta, d) = exp(  sum  - theta_i * (d_i)^2 )
+                                            i = 1
+    """
+
+    def _compute_corr(self, theta, d, n_features):
+        """ Correlation for given pairwise, component-wise L1-differences.
 
         Parameters
         ----------
-        theta : array_like
-            An array with shape 1 (isotropic) or n (anisotropic) giving the
-            autocorrelation parameter(s).
+        theta : array_like, shape=(1,) or (n_features,)
+            An array with shape 1 (isotropic) or n_features (anisotropic)
+            giving the autocorrelation parameter(s).
+
+        d : array_like, shape=(n_eval, n_features)
+            An array with the pairwise, component-wise L1-differences of x
+            and x' at which the correlation model should be evaluated.
 
         Returns
         -------
-        r : array_like
-            An array with shape (n_eval, ) containing the values of the
-            autocorrelation model.
+        r : array_like, shape=(n_eval, )
+            An array containing the values of the autocorrelation model.
         """
-        theta = np.asarray(theta, dtype=np.float)
-        if X is not None:
-            # Get pairwise componentwise L1-distances to the input training set
-            d = manhattan_distances(X, Y=self.X, sum_over_features=False)
-        else:
-            d = self.D
-
-        if d.ndim > 1:
-            n_features = d.shape[1]
-        else:
-            n_features = 1
-
         if theta.size == 1:
-            r = np.exp(-theta[0] * np.sum(d ** 2, axis=1))
+            return np.exp(-theta[0] * np.sum(d ** 2, axis=1))
         elif theta.size != n_features:
             raise ValueError("Length of theta must be 1 or %s" % n_features)
         else:
-            r = np.exp(-np.sum(theta.reshape(1, n_features) * d ** 2, axis=1))
-
-        if X is not None:
-            return r.reshape(-1, self.n_samples)
-        else:
-            R = np.eye(self.n_samples) * (1. + self.nugget)
-            R[self.ij[:, 0], self.ij[:, 1]] = r
-            R[self.ij[:, 1], self.ij[:, 0]] = r
-            return R
+            return np.exp(-np.sum(theta.reshape(1, n_features) * d ** 2,
+                          axis=1))
 
 
 class GeneralizedExponential(StationaryCorrelation):
+    """ Generalized exponential correlation model.
 
-    def __call__(self, theta, X=None):
-        """
-        Generalized exponential correlation model.
-        (Useful when one does not know the smoothness of the function to be
-        predicted.)::
+    Generalized exponential correlation model.
+    (Useful when one does not know the smoothness of the function to be
+    predicted.)::
 
-                                                n
-            theta, dx --> r(theta, dx) = exp(  sum  - theta_i * |dx_i|^p )
-                                              i = 1
+                                          n
+        theta, d --> r(theta, d) = exp(  sum  - theta_i * |d_i|^p )
+                                        i = 1
+    """
+
+    def _compute_corr(self, theta, d, n_features):
+        """ Correlation for given pairwise, component-wise L1-differences.
 
         Parameters
         ----------
-        theta : array_like
-            An array with shape 1+1 (isotropic) or n+1 (anisotropic) giving the
-            autocorrelation parameter(s) (theta, p).
+        theta : array_like, shape=(1+1,) or (n_features+1,)
+            An array with shape 1+1 (isotropic) or n_features+1 (anisotropic)
+            giving the autocorrelation parameter(s) (theta, p).
+
+        d : array_like, shape=(n_eval, n_features)
+            An array with the pairwise, component-wise L1-differences of x
+            and x' at which the correlation model should be evaluated.
 
         Returns
         -------
-        r : array_like
-            An array with shape (n_eval, ) with the values of the
-            autocorrelation model.
+        r : array_like, shape=(n_eval, )
+            An array containing the values of the autocorrelation model.
         """
-        theta = np.asarray(theta, dtype=np.float)
-        if X is not None:
-            # Get pairwise componentwise L1-distances to the input training set
-            d = manhattan_distances(X, Y=self.X, sum_over_features=False)
-        else:
-            d = self.D
-
-        if d.ndim > 1:
-            n_features = d.shape[1]
-        else:
-            n_features = 1
-
         lth = theta.size
         if n_features > 1 and lth == 2:
             theta = np.hstack([np.repeat(theta[0], n_features), theta[1]])
@@ -219,34 +211,32 @@ class GeneralizedExponential(StationaryCorrelation):
 
         td = theta[:, 0:-1].reshape(1, n_features) \
             * np.abs(d) ** theta[:, -1]
-        r = np.exp(- np.sum(td, 1))
-
-        if X is not None:
-            return r.reshape(-1, self.n_samples)
-        else:
-            R = np.eye(self.n_samples) * (1. + self.nugget)
-            R[self.ij[:, 0], self.ij[:, 1]] = r
-            R[self.ij[:, 1], self.ij[:, 0]] = r
-            return R
+        return np.exp(- np.sum(td, 1))
 
 
 class PureNugget(StationaryCorrelation):
+    """ Spatial independence correlation model (pure nugget).
 
-    def __call__(self, theta, X=None):
-        """
-        Spatial independence correlation model (pure nugget).
-        (Useful when one wants to solve an ordinary least squares problem!)::
+    Useful when one wants to solve an ordinary least squares problem!::
 
-                                                 n
-            theta, dx --> r(theta, dx) = 1 if   sum |dx_i| == 0
-                                               i = 1
-                                         0 otherwise
+                                                n
+            theta, d --> r(theta, dx) = 1 if   sum |d_i| == 0
+                                              i = 1
+                                        0 otherwise
+    """
+
+    def _compute_corr(self, theta, d, n_features):
+        """ Correlation for given pairwise, component-wise L1-differences.
 
         Parameters
         ----------
         theta : array_like
             None.
 
+        d : array_like, shape=(n_eval, n_features)
+            An array with the pairwise, component-wise L1-differences of x
+            and x' at which the correlation model should be evaluated.
+
 
         Returns
         -------
@@ -254,62 +244,42 @@ class PureNugget(StationaryCorrelation):
             An array with shape (n_eval, ) with the values of the
             autocorrelation model.
         """
-
-        theta = np.asarray(theta, dtype=np.float)
-        if X is not None:
-            # Get pairwise componentwise L1-distances to the input training set
-            d = manhattan_distances(X, Y=self.X, sum_over_features=False)
-        else:
-            d = self.D
-
         n_eval = d.shape[0]
         r = np.zeros(n_eval)
         r[np.all(d == 0., axis=1)] = 1.
 
-        if X is not None:
-            return r.reshape(-1, self.n_samples)
-        else:
-            R = np.eye(self.n_samples) * (1. + self.nugget)
-            R[self.ij[:, 0], self.ij[:, 1]] = r
-            R[self.ij[:, 1], self.ij[:, 0]] = r
-            return R
+        return r
 
 
 class Cubic(StationaryCorrelation):
+    """ Cubic correlation model.
 
-    def __call__(self, theta, X=None):
-        """
-        Cubic correlation model::
+    Cubic correlation model::
 
-            theta, dx --> r(theta, dx) =
-              n
-            prod max(0, 1 - 3(theta_j*d_ij)^2 + 2(theta_j*d_ij)^3) ,  i = 1,...,m
-            j = 1
+        theta, d --> r(theta, d) =
+          n
+        prod max(0, 1 - 3(theta_j*d_ij)^2 + 2(theta_j*d_ij)^3) ,  i = 1,...,m
+        j = 1
+    """
+
+    def _compute_corr(self, theta, d, n_features):
+        """ Correlation for given pairwise, component-wise L1-differences.
 
         Parameters
         ----------
-        theta : array_like
-            An array with shape 1 (isotropic) or n (anisotropic) giving the
-            autocorrelation parameter(s).
+        theta : array_like, shape=(1,) or (n_features,)
+            An array with shape 1 (isotropic) or n_features (anisotropic)
+            giving the autocorrelation parameter(s).
+
+        d : array_like, shape=(n_eval, n_features)
+            An array with the pairwise, component-wise L1-differences of x
+            and x' at which the correlation model should be evaluated.
 
         Returns
         -------
-        r : array_like
-            An array with shape (n_eval, ) with the values of the
-            autocorrelation model.
+        r : array_like, shape=(n_eval, )
+            An array containing the values of the autocorrelation model.
         """
-        theta = np.asarray(theta, dtype=np.float)
-        if X is not None:
-            # Get pairwise componentwise L1-distances to the input training set
-            d = manhattan_distances(X, Y=self.X, sum_over_features=False)
-        else:
-            d = self.D
-
-        if d.ndim > 1:
-            n_features = self.D.shape[1]
-        else:
-            n_features = 1
-
         lth = theta.size
         if lth == 1:
             td = np.abs(d) * theta
@@ -320,52 +290,38 @@ class Cubic(StationaryCorrelation):
 
         td[td > 1.] = 1.
         ss = 1. - td ** 2. * (3. - 2. * td)
-        r = np.prod(ss, 1)
-
-        if X is not None:
-            return r.reshape(-1, self.n_samples)
-        else:
-            R = np.eye(self.n_samples) * (1. + self.nugget)
-            R[self.ij[:, 0], self.ij[:, 1]] = r
-            R[self.ij[:, 1], self.ij[:, 0]] = r
-            return R
+        return np.prod(ss, 1)
 
 
 class Linear(StationaryCorrelation):
+    """ Linear correlation model.
 
-    def __call__(self, theta, X=None):
-        """
-        Linear correlation model::
+    Linear correlation model::
 
-            theta, dx --> r(theta, dx) =
-                  n
-                prod max(0, 1 - theta_j*d_ij) ,  i = 1,...,m
-                j = 1
+        theta, d --> r(theta, d) =
+              n
+            prod max(0, 1 - theta_j*d_ij) ,  i = 1,...,m
+            j = 1
+    """
+
+    def _compute_corr(self, theta, d, n_features):
+        """ Correlation for given pairwise, component-wise L1-differences.
 
         Parameters
         ----------
-        theta : array_like
-            An array with shape 1 (isotropic) or n (anisotropic) giving the
-            autocorrelation parameter(s).
+        theta : array_like, shape=(1,) or (n_features,)
+            An array with shape 1 (isotropic) or n_features (anisotropic)
+            giving the autocorrelation parameter(s).
+
+        d : array_like, shape=(n_eval, n_features)
+            An array with the pairwise, component-wise L1-differences of x
+            and x' at which the correlation model should be evaluated.
 
         Returns
         -------
-        r : array_like
-            An array with shape (n_eval, ) with the values of the
-            autocorrelation model.
+        r : array_like, shape=(n_eval, )
+            An array containing the values of the autocorrelation model.
         """
-        theta = np.asarray(theta, dtype=np.float)
-        if X is not None:
-            # Get pairwise componentwise L1-distances to the input training set
-            d = manhattan_distances(X, Y=self.X, sum_over_features=False)
-        else:
-            d = self.D
-
-        if d.ndim > 1:
-            n_features = d.shape[1]
-        else:
-            n_features = 1
-
         lth = theta.size
         if lth == 1:
             td = np.abs(d) * theta
@@ -376,12 +332,4 @@ class Linear(StationaryCorrelation):
 
         td[td > 1.] = 1.
         ss = 1. - td
-        r = np.prod(ss, 1)
-
-        if X is not None:
-            return r.reshape(-1, self.n_samples)
-        else:
-            R = np.eye(self.n_samples) * (1. + self.nugget)
-            R[self.ij[:, 0], self.ij[:, 1]] = r
-            R[self.ij[:, 1], self.ij[:, 0]] = r
-            return R
+        return np.prod(ss, 1)
