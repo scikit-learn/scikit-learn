3729143bd07737cf0d872cc087b0d1593b543f18
Olivier Grisel <olivier.grisel@ensta.org>
2014-07-04 15:11:56 +0200

    Typo

--- doc/tutorial/text_analytics/working_with_text_data_fixture.py
+++ doc/tutorial/text_analytics/working_with_text_data_fixture.py
@@ -4,7 +4,7 @@ The 20 newsgroups data is rather large and some CI workers such as travis are
 stateless hence will not cache the dataset as regular sklearn users would do.
 
 The following will skip the execution of the working_with_text_data.rst doctests
-is the proper environment variable is configured (see the source code of
+if the proper environment variable is configured (see the source code of
 check_skip_network for more details).
 
 """
