7ffd392c68bdcb98910951c178508895224ecd56
Hamzeh Alsalhi <ha258@cornell.edu>
2014-07-08 21:20:17 -0400

    Implmented some basic outline for predict with sparse support

--- sklearn/neighbors/classification.py
+++ sklearn/neighbors/classification.py
@@ -9,6 +9,7 @@
 # License: BSD 3 clause (C) INRIA, University of Amsterdam
 
 import numpy as np
+import scipy.sparse as sp
 from scipy import stats
 from ..utils.extmath import weighted_mode
 
@@ -134,11 +135,45 @@ class KNeighborsClassifier(NeighborsBase, KNeighborsMixin,
         """
         X = check_array(X, accept_sparse='csr')
 
+        # XXX Current
+        # X = atleast2d_or_csr(X)
+
+        # neigh_dist, neigh_ind = self.kneighbors(X)
+
+        # classes_ = self.classes_
+        # _y = self._y
+        # if not self.outputs_2d_:
+        #     _y = self._y.reshape((-1, 1))
+        #     classes_ = [self.classes_]
+
+        # n_outputs = len(classes_)
+        # n_samples = X.shape[0]
+        # weights = _get_weights(neigh_dist, self.weights)
+
+        # y_pred = np.empty((n_samples, n_outputs), dtype=classes_[0].dtype)
+        # for k, classes_k in enumerate(classes_):
+        #     if weights is None:
+        #         mode, _ = stats.mode(_y[neigh_ind, k], axis=1)
+        #     else:
+        #         mode, _ = weighted_mode(_y[neigh_ind, k], weights, axis=1)
+
+        #     mode = np.asarray(mode.ravel(), dtype=np.intp)
+        #     y_pred[:, k] = classes_k.take(mode)
+
+        # if not self.outputs_2d_:
+        #     y_pred = y_pred.ravel()
+
+        # return y_pred
+
+        # XXX sparse
+        X = atleast2d_or_csr(X)
+
         neigh_dist, neigh_ind = self.kneighbors(X)
 
         classes_ = self.classes_
         _y = self._y
         if not self.outputs_2d_:
+            # XXX sparsiify this
             _y = self._y.reshape((-1, 1))
             classes_ = [self.classes_]
 
@@ -146,15 +181,36 @@ class KNeighborsClassifier(NeighborsBase, KNeighborsMixin,
         n_samples = X.shape[0]
         weights = _get_weights(neigh_dist, self.weights)
 
-        y_pred = np.empty((n_samples, n_outputs), dtype=classes_[0].dtype)
-        for k, classes_k in enumerate(classes_):
-            if weights is None:
-                mode, _ = stats.mode(_y[neigh_ind, k], axis=1)
-            else:
-                mode, _ = weighted_mode(_y[neigh_ind, k], weights, axis=1)
+        _y = sp.csc_matrix(_y) # XXX Remove when sparse matrix comes from fit in one continous pass
 
-            mode = np.asarray(mode.ravel(), dtype=np.intp)
-            y_pred[:, k] = classes_k.take(mode)
+        data = np.array([], dtype=classes_[0].dtype)
+        indices = np.array([])
+        indptr = np.array([0])
+        for k, classes_k in enumerate(classes_):
+            neigh_lbls_k = _y[neigh_ind,k]
+            print "neigh_lbls_k", neigh_lbls_k.toarray().T
+            print "current", _y.toarray()[neigh_ind,k]
+            if weights is None:
+                # XXX make a sparse mode function, do not densify neigh_lbls_k
+                mode, _ = stats.mode(neigh_lbls_k.toarray().T, axis=1)
+                mode = sp.csc_matrix(mode ,dtype=np.intp)
+            else:
+                # XXX make a sparse weighted_mode function, do not densify neigh_lbls_k
+                mode, _ = weighted_mode(neigh_lbls_k.toarray().T, weights, axis=1)
+                mode = sp.csc_matrix(mode, dtype=np.intp)
+
+            # XXX Changes appends to concats
+            data = np.append(data, mode.data)
+            #print "data", data
+            print "mode", mode.data
+            indices = np.append(indices, mode.indices)
+            indptr = np.append(indptr, len(indices))
+
+        y_pred = sp.csc_matrix((data, indices, indptr), (n_samples, n_outputs), dtype=np.intp)
+        print classes_[0].dtype
+
+        y_pred = y_pred.toarray() # XXX remove this
+        y_pred = classes_k[y_pred] # XXX remove this for sparse cases, sparse matricies cant have str data
 
         if not self.outputs_2d_:
             y_pred = y_pred.ravel()
