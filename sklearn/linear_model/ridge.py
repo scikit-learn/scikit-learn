f433f661ef73e3212a47a089dcc8eddd9eb2efae
Olivier Grisel <olivier.grisel@ensta.org>
2014-07-13 18:23:30 +0200

    FIX multilabel deprecation warning in RidgeClassifierCV

--- sklearn/linear_model/ridge.py
+++ sklearn/linear_model/ridge.py
@@ -1072,7 +1072,7 @@ class RidgeClassifierCV(LinearClassifierMixin, _BaseRidgeCV):
 
         self._label_binarizer = LabelBinarizer(pos_label=1, neg_label=-1)
         Y = self._label_binarizer.fit_transform(y)
-        if not self._label_binarizer.multilabel_:
+        if not self._label_binarizer.y_type_.startswith('multilabel'):
             y = column_or_1d(y, warn=True)
         cw = compute_class_weight(class_weight,
                                   self.classes_, Y)
