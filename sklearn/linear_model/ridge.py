e107e1f5f675d9eb2c0e96a131b2ff332d845e61
lesteve <loic.esteve@ymail.com>
2014-07-16 17:53:36 +0100

    Remove deprecated 'class_weight' parameter from RidgeClassifierCV.fit

--- sklearn/linear_model/ridge.py
+++ sklearn/linear_model/ridge.py
@@ -1034,7 +1034,7 @@ class RidgeClassifierCV(LinearClassifierMixin, _BaseRidgeCV):
             scoring=scoring, score_func=score_func, loss_func=loss_func, cv=cv)
         self.class_weight = class_weight
 
-    def fit(self, X, y, sample_weight=None, class_weight=None):
+    def fit(self, X, y, sample_weight=None):
         """Fit the ridge classifier.
 
         Parameters
@@ -1049,24 +1049,11 @@ class RidgeClassifierCV(LinearClassifierMixin, _BaseRidgeCV):
         sample_weight : float or numpy array of shape (n_samples,)
             Sample weight.
 
-        class_weight : dict, optional
-            Weights associated with classes in the form
-            ``{class_label : weight}``. If not given, all classes are
-            supposed to have weight one. This is parameter is
-            deprecated.
-
         Returns
         -------
         self : object
             Returns self.
         """
-        if class_weight is None:
-            class_weight = self.class_weight
-        else:
-            warnings.warn("'class_weight' is now an initialization parameter."
-                          " Using it in the 'fit' method is deprecated and "
-                          "will be removed in 0.15.", DeprecationWarning,
-                          stacklevel=2)
         if sample_weight is None:
             sample_weight = 1.
 
@@ -1074,7 +1061,7 @@ class RidgeClassifierCV(LinearClassifierMixin, _BaseRidgeCV):
         Y = self._label_binarizer.fit_transform(y)
         if not self._label_binarizer.y_type_.startswith('multilabel'):
             y = column_or_1d(y, warn=True)
-        cw = compute_class_weight(class_weight,
+        cw = compute_class_weight(self.class_weight,
                                   self.classes_, Y)
         # modify the sample weights with the corresponding class weight
         sample_weight *= cw[np.searchsorted(self.classes_, y)]
