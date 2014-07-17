b3aaf1eb8f5806426a6f7787559fe510e14a184f
Arnaud Joly <arnaud.v.joly@gmail.com>
2014-07-17 11:22:15 +0200

    pep8

--- sklearn/utils/tests/test_stats.py
+++ sklearn/utils/tests/test_stats.py
@@ -5,23 +5,13 @@ from sklearn.utils.testing import assert_array_equal
 from sklearn.utils.stats import rankdata
 
 
-
-
 _cases = (
     # values, method, expected
-    # ([], 'max', []),
-
     ([100], 'max', [1.0]),
-
     ([100, 100, 100], 'max', [3.0, 3.0, 3.0]),
-
-    #
     ([100, 300, 200], 'max', [1.0, 3.0, 2.0]),
-    #
     ([100, 200, 300, 200], 'max', [1.0, 3.0, 4.0, 3.0]),
-    #
     ([100, 200, 300, 200, 100], 'max', [2.0, 4.0, 5.0, 4.0, 2.0]),
-    #
 )
 
 
@@ -33,4 +23,3 @@ def test_cases():
 
     for values, method, expected in _cases:
         yield check_case, values, method, expected
-
