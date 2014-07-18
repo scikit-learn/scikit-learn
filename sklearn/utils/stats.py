56bba96e8d20e8f3e01b8dfa9f8dcda2ab506d15
Arnaud Joly <arnaud.v.joly@gmail.com>
2014-07-18 17:07:14 +0200

    DOC add documentation to backported function

--- sklearn/utils/stats.py
+++ sklearn/utils/stats.py
@@ -5,8 +5,35 @@ from scipy.stats import rankdata as _sp_rankdata
 try:
     _sp_rankdata([1.], 'max')
     rankdata = _sp_rankdata
+
 except TypeError as e:
     def rankdata(a, method="average"):
+        """Assign ranks to data, dealing with ties appropriately.
+
+        Ranks begin at 1. The method argument controls how ranks are assigned
+        to equal values.
+
+        Parameters
+        ----------
+        a : array_like
+            The array of values to be ranked. The array is first flattened.
+
+        method : str, optional
+            The method used to assign ranks to tied elements.
+            The options are 'max'.
+
+                ‘max’: The maximum of the ranks that would have been assigned
+                      to all the tied values is assigned to each value.
+        Returns
+        -------
+        ranks : ndarray
+            An array of length equal to the size of a, containing rank scores.
+
+        Note
+        ----
+        We only backport the 'max' method
+
+        """
         if method != "max":
             raise NotImplementedError()
 
