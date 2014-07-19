58feda099464a5d9e393d84aaaeb0e5a87ef7a44
Arnaud Joly <arnaud.v.joly@gmail.com>
2014-07-19 16:56:57 +0200

    FIX import

--- sklearn/metrics/cluster/tests/test_bicluster.py
+++ sklearn/metrics/cluster/tests/test_bicluster.py
@@ -4,8 +4,8 @@ import numpy as np
 
 from sklearn.utils.testing import assert_equal
 
-from sklearn.metrics.bicluster import _jaccard
-from sklearn.metrics.bicluster import consensus_score
+from sklearn.metrics.cluster.bicluster import _jaccard
+from sklearn.metrics import consensus_score
 
 
 def test_jaccard():
