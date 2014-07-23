22f7569418e83c8d01711535fc517ce4a4b69677
Olivier Grisel <olivier.grisel@ensta.org>
2014-07-23 09:05:26 +0200

    ENH warn against non-embedded joblib

--- sklearn/externals/copy_joblib.sh
+++ sklearn/externals/copy_joblib.sh
@@ -25,9 +25,14 @@ rm -r joblib/test
 mv joblib/__init__.py joblib/embedded_joblib_init.py
 NEW_INIT="
 import os
+import warnings
 if int(os.environ.get('SKLEARN_SYSTEM_JOBLIB', '0')):
     # Try to import joblib from the system for development purpose
     from joblib import *
+    from joblib import __version__
+    from .embedded_joblib_init import __version__ as orig_version
+    warnings.warn('Using system joblib %s instead of embedded joblib %s'
+                  % (__version__, orig_version))
 else:
     from .embedded_joblib_init import *
 "
--- sklearn/externals/joblib/__init__.py
+++ sklearn/externals/joblib/__init__.py
@@ -1,8 +1,13 @@
 
 import os
+import warnings
 if int(os.environ.get('SKLEARN_SYSTEM_JOBLIB', '0')):
     # Try to import joblib from the system for development purpose
     from joblib import *
+    from joblib import __version__
+    from .embedded_joblib_init import __version__ as orig_version
+    warnings.warn('Using system joblib %s instead of embedded joblib %s'
+                  % (__version__, orig_version))
 else:
     from .embedded_joblib_init import *
 
