1db3e03ee71fd26d7f49eccfb2c801f07c197e2e
Al.Bahnsen <al.bahnsen@gmail.com>
2014-07-31 15:54:09 +0200

    PolFeatures using sparse, log and exp

--- examples/polynomial_features_memory.py
+++ examples/polynomial_features_memory.py
@@ -16,16 +16,16 @@ from 3455 MiB to 374 MiB, without increasing the execution time.
 Examples
 --------
 >>> %timeit XP1 = p.transform_OLD(X)
-1 loops, best of 3: 6.95 s per loop
+1 loops, best of 3: 7.3 s per loop
 
 >>> %memit XP1 = p.transform_OLD(X)
-peak memory: 3455.32 MiB, increment: 3405.33 MiB
+peak memory: 4325.37 MiB, increment: 3701.51 MiB
 
 >>> %timeit XP2 = p.transform(X)
-1 loops, best of 3: 6.47 s per loop
+1 loops, best of 3: 1.75 s per loop
 
 >>> %memit XP2 = p.transform(X)
-peak memory: 374.59 MiB, increment: 161.95 MiB
+peak memory: 1346.08 MiB, increment: 751.89 MiB
 
 """
 # Author: Alejandro Correa Bahnsen <al.bahnsen@gmail.com>
@@ -35,14 +35,11 @@ peak memory: 374.59 MiB, increment: 161.95 MiB
 import numpy as np
 from sklearn.preprocessing import PolynomialFeatures
 
-n_samples, n_features = 2000, 20
-X = np.arange(n_samples * n_features).reshape((n_samples, n_features))
+n_samples, n_features = 100000, 20
+X = np.arange(-n_samples, n_samples * n_features - n_samples).reshape((n_samples, n_features))
 
-p = PolynomialFeatures(degree=4)
+p = PolynomialFeatures(degree=2)
 p.fit(X)
 
 XP1 = p.transform_OLD(X)
 XP2 = p.transform(X)
-
-print np.all(XP1 == XP2)
-
--- sklearn/preprocessing/data.py
+++ sklearn/preprocessing/data.py
@@ -497,6 +497,31 @@ class PolynomialFeatures(BaseEstimator, TransformerMixin):
         Parameters
         ----------
         X : array with shape [n_samples, n_features]
+        The data to transform, row by row.
+
+        Returns
+        -------
+        XP : np.ndarray shape [n_samples, NP]
+        The matrix of features, where NP is the number of polynomial
+        features generated from the combination of inputs.
+        """
+        X = check_array(X)
+        n_samples, n_features = X.shape
+
+        if n_features != self.powers_.shape[1]:
+            raise ValueError("X shape does not match training shape")
+
+        powers = sparse.csc_matrix(self.powers_.T)
+        logX = np.log(X if X.min() >= 0 else X.astype(complex))
+
+        return np.exp(logX * powers).real
+
+    def transform_OLD2(self, X, y=None):
+        """Transform data to polynomial features
+
+        Parameters
+        ----------
+        X : array with shape [n_samples, n_features]
             The data to transform, row by row.
 
         Returns
