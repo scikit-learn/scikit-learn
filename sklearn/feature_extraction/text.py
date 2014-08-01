64929ad9bad42c1a537ed320d9c5ee0bc5645d95
Jonathan Ronen <yablee@gmail.com>
2014-08-01 14:44:23 -0400

    Add UNICODE flag to re.compile() in text tokenization

--- sklearn/feature_extraction/text.py
+++ sklearn/feature_extraction/text.py
@@ -204,7 +204,7 @@ class VectorizerMixin(object):
         """Return a function that splits a string into a sequence of tokens"""
         if self.tokenizer is not None:
             return self.tokenizer
-        token_pattern = re.compile(self.token_pattern)
+        token_pattern = re.compile(self.token_pattern, flags=re.UNICODE)
         return lambda doc: token_pattern.findall(doc)
 
     def get_stop_words(self):
