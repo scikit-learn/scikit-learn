17e2777abe0ed9692770a948f9318b777d74b64f
Valentin Haenel <valentin.haenel@gmx.de>
2014-08-03 23:31:42 +0200

    smoke test the cyfht too

--- sklearn/utils/tests/test_fht.py
+++ sklearn/utils/tests/test_fht.py
@@ -2,10 +2,15 @@ import numpy as np
 import numpy.testing as npt
 from scipy.linalg import hadamard
 from sklearn.utils.fht import fht
+from sklearn.utils.cyfht import fht as cyfht
 
-def test():
-    input_ = np.array([1, 0, 1, 0, 0, 1, 1, 0])
+def single(fht_type):
+    input_ = np.array([1, 0, 1, 0, 0, 1, 1, 0], dtype=np.float64)
     copy = input_.copy()
     H = hadamard(8)
-    fht(input_)
+    fht_type(input_)
     npt.assert_array_equal(np.dot(copy, H), input_)
+
+def test_all():
+    single(fht)
+    single(cyfht)
