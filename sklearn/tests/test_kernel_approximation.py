b17abbb70d28a0d6147f5b6a5b54ceacb7dddc63
felixmaximilian <felixmaximilian@gmail.com>
2014-08-04 08:54:32 +0200

    test digit dataset almost finished. Only assertion is missing.

--- sklearn/tests/test_kernel_approximation.py
+++ sklearn/tests/test_kernel_approximation.py
@@ -581,10 +581,9 @@ def test_digit_recognition():
     import pylab as pl
     
     # Import datasets, classifiers and performance metrics
-    from sklearn import datasets, svm, metrics, LinearSVC
+    from sklearn import datasets, svm, metrics
     from sklearn.linear_model.stochastic_gradient import SGDClassifier
-    from sklearn.multiclass import
-    
+
     # The digits dataset
     digits = datasets.load_digits()
     
@@ -613,33 +612,42 @@ def test_digit_recognition():
     rbf_transform = Fastfood(sigma=sigma, n_components=number_of_features_to_generate, random_state=42)
     data_transformed_train = rbf_transform.fit(data[train__idx]).transform_fast(data[train__idx])
     data_transformed_test = rbf_transform.transform_fast(data[test__idx])
+    data_transformed_train = rbf_transform.fit(data[train__idx]).transform_fast(data[train__idx])
+    data_transformed_test = rbf_transform.transform_fast(data[test__idx])
 
     # Create a classifier: a support vector classifier
     classifier = svm.SVC(gamma=gamma)
     linear_classifier = svm.LinearSVC()
     linear_classifier_transformation = svm.LinearSVC()
+    linear_classifier_twice_transformation = svm.LinearSVC()
 
     # We learn the digits on the first half of the digits
     classifier.fit(data[train__idx], digits.target[train__idx])
     linear_classifier.fit(data[train__idx], digits.target[train__idx])
     linear_classifier_transformation.fit(data_transformed_train, digits.target[train__idx])
+    linear_classifier_twice_transformation.fit(data_transformed_train, digits.target[train__idx])
 
     # Now predict the value of the digit on the second half:
     expected = digits.target[test__idx]
     predicted = classifier.predict(data[test__idx])
     predicted_linear = linear_classifier.predict(data[test__idx])
     predicted_linear_transformed = linear_classifier_transformation.predict(data_transformed_test)
-    
+    predicted_linear_twice_transformed = linear_classifier_transformation.predict(data_transformed_test)
+
     print "Classification report for dual classifier %s:\n%s\n" % (
         classifier, metrics.classification_report(expected, predicted))
     print "Classification report for primal linear classifier %s:\n%s\n" % (
         linear_classifier, metrics.classification_report(expected, predicted_linear))
     print "Classification report for primal transformation classifier %s:\n%s\n" % (
         linear_classifier_transformation, metrics.classification_report(expected, predicted_linear_transformed))
+    print "Classification report for primal twice transformation classifier %s:\n%s\n" % (
+        linear_classifier_twice_transformation, metrics.classification_report(expected, predicted_linear_twice_transformed))
 
-    print "Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted)
-    print "Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted)
-    
+    print "Confusion matrix for dual classifier:\n%s" % metrics.confusion_matrix(expected, predicted)
+    print "Confusion matrix for primal linear classifier:\n%s" % metrics.confusion_matrix(expected, predicted_linear)
+    print "Confusion matrix for for primal transformation classifier:\n%s" % metrics.confusion_matrix(expected, predicted_linear_transformed)
+    print "Confusion matrix for for primal twice transformation classifier:\n%s" % metrics.confusion_matrix(expected, predicted_linear_twice_transformed)
+
     for index, (image, prediction) in enumerate(zip(digits.images[test__idx], predicted)[:4]):
         pl.subplot(2, 4, index + 5)
         pl.axis('off')
