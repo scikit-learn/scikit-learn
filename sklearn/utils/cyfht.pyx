873ad801f1faf06561c1f2dcfe55e6df64dc802a
Felix Maximilian Möller <felixmaximilian@gmail.com>
2014-08-05 20:14:53 +0200

    added 2 dimensional cyfht

--- sklearn/utils/cyfht.pyx
+++ sklearn/utils/cyfht.pyx
@@ -9,7 +9,7 @@ ctypedef np.double_t DTYPE_t
 
 @cython.boundscheck(False)
 def fht(np.ndarray[DTYPE_t] array_):
-    cdef int bit, length, _, i, j
+    cdef unsigned int bit, length, _, i, j
     cdef double temp
     bit = length = len(array_)
     for _ in xrange(int(log2(length))):
@@ -20,4 +20,20 @@ def fht(np.ndarray[DTYPE_t] array_):
                 temp = array_[i]
                 array_[i] += array_[j]
                 array_[j] = temp - array_[j]
+
+@cython.boundscheck(False)
+def fht2(np.ndarray[DTYPE_t, ndim=2] array_):
+    cdef unsigned int bit, length, _, i, j, n
+    cdef double temp
+    n = array_.shape[0]
+    for x in xrange(n):
+        bit = length = array_.shape[1]
+        for _ in xrange(int(log2(length))):
+            bit >>= 1
+            for i in xrange(length):
+                if i & bit == 0:
+                    j = i | bit
+                    temp = array_[x, i]
+                    array_[x, i] += array_[x, j]
+                    array_[x, j] = temp - array_[x, j]
 
