0f89415424dcc53675416892fe0221e757d6044b
GaelVaroquaux <gael.varoquaux@normalesup.org>
2014-08-19 10:49:59 +0200

    MAINT: add license info
    Fixes #3577

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -4,6 +4,8 @@
 # The behavior of the script is controlled by environment variabled defined
 # in the .travis.yml in the top level folder of the project.
 
+# License: 3-clause BSD
+
 set -e
 
 sudo apt-get update -qq
