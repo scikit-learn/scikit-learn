8be4d5e1b275c74a85cbee87875f2b40f79483d4
Lars Buitinck <larsmans@gmail.com>
2014-09-03 12:11:13 +0200

    MAINT remove unused MST code

--- sklearn/utils/sparsetools/__init__.py
+++ sklearn/utils/sparsetools/__init__.py
@@ -1,6 +1,5 @@
 """sparsetools - a collection of routines for sparse matrix operations"""
 
-from ._min_spanning_tree import minimum_spanning_tree
 from ._traversal import connected_components
 
-__all__ = ["minimum_spanning_tree", "connected_components"]
+__all__ = ["connected_components"]
