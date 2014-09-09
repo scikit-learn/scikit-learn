28f1da41b304dac8fcfc88527c9a5b752d8f53fd
Danny Sullivan <dsullivan7@hotmail.com>
2014-09-09 09:19:35 +0200

    more changes to learning rates

--- sklearn/linear_model/learning_rates.pyx
+++ sklearn/linear_model/learning_rates.pyx
@@ -1,12 +1,10 @@
-cdef char* c = "constant"
-cdef LearningRate constant = LearningRate()
 
 cdef extern from "stdlib.h":
     int strcmp(const char*, const char*)
 
 cdef LearningRate get_learning_rate(char* learning_rate):
-    if(strcmp(learning_rate, c)):
-        return constant
+    if(strcmp(learning_rate, "constant")):
+        return Constant()
 
 cdef class LearningRate:
     cdef double step(self, double eta0):
