b2174b2c62a934d4ab27d65c03cacef08d3c8d2a
Valentin Haenel <valentin@haenel.co>
2014-09-14 14:47:47 +0200

    fuzz testing fht2

--- sklearn/utils/tests/test_fht.py
+++ sklearn/utils/tests/test_fht.py
@@ -22,5 +22,15 @@ def test_numerical_fuzzing_fht():
         npt.assert_array_almost_equal(np.dot(copy, H), input_)
 
 
+def test_numerical_fuzzing_fht2():
+    for length in [2, 4, 8, 16, 32, 64]:
+        for rows in [1, 2, 3, 4, 5]:
+            input_ = np.random.normal(size=(rows, length))
+            copy = input_.copy()
+            H = hadamard(length)
+            cyfht2(input_)
+            npt.assert_array_almost_equal(np.dot(copy, H), input_)
+
+
 def test_exception_when_input_not_power_two():
     nt.assert_raises(ValueError, cyfht, np.zeros(9, dtype=np.float64))
