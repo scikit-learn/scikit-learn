72688469757b0165d4fb706228c65510429a947b
Valentin Haenel <valentin@haenel.co>
2014-09-14 14:01:28 +0200

    test to check that fht/cyfht aborts on non-power-two input

--- sklearn/utils/tests/test_fht.py
+++ sklearn/utils/tests/test_fht.py
@@ -1,5 +1,6 @@
 import numpy as np
 import numpy.testing as npt
+import nose.tools as nt
 from scipy.linalg import hadamard
 from sklearn.utils.fht import fht
 from sklearn.utils.cyfht import fht as cyfht
@@ -10,6 +11,10 @@ def single(fht_type):
     H = hadamard(8)
     fht_type(input_)
     npt.assert_array_equal(np.dot(copy, H), input_)
+
+def test_exception_when_input_not_power_two():
+    for fht_type in  [fht, cyfht]:
+        yield nt.assert_raises, ValueError, fht_type, np.zeros(9, dtype=np.float64)
 
 def test_all():
     single(fht)
