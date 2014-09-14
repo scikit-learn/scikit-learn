561c83271ca0f654a5356db3a84fe0b17413c113
Valentin Haenel <valentin@haenel.co>
2014-09-14 14:18:11 +0200

    remove test of toy fht.py and rename test

--- sklearn/utils/tests/test_fht.py
+++ sklearn/utils/tests/test_fht.py
@@ -5,16 +5,12 @@ from scipy.linalg import hadamard
 from sklearn.utils.fht import fht
 from sklearn.utils.cyfht import fht as cyfht
 
-def single(fht_type):
+def test_wikipedia_example():
     input_ = np.array([1, 0, 1, 0, 0, 1, 1, 0], dtype=np.float64)
     copy = input_.copy()
     H = hadamard(8)
-    fht_type(input_)
+    cyfht(input_)
     npt.assert_array_equal(np.dot(copy, H), input_)
 
 def test_exception_when_input_not_power_two():
     nt.assert_raises(ValueError, cyfht, np.zeros(9, dtype=np.float64))
-
-def test_all():
-    single(fht)
-    single(cyfht)
