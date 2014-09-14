b7f4b5b468913b4c9f9040a3dee60d5020dddf47
Valentin Haenel <valentin@haenel.co>
2014-09-14 14:22:37 +0200

    pep8 the testfile

--- sklearn/utils/tests/test_fht.py
+++ sklearn/utils/tests/test_fht.py
@@ -2,8 +2,8 @@ import numpy as np
 import numpy.testing as npt
 import nose.tools as nt
 from scipy.linalg import hadamard
-from sklearn.utils.fht import fht
 from sklearn.utils.cyfht import fht as cyfht
+
 
 def test_wikipedia_example():
     input_ = np.array([1, 0, 1, 0, 0, 1, 1, 0], dtype=np.float64)
@@ -11,6 +11,7 @@ def test_wikipedia_example():
     H = hadamard(8)
     cyfht(input_)
     npt.assert_array_equal(np.dot(copy, H), input_)
+
 
 def test_exception_when_input_not_power_two():
     nt.assert_raises(ValueError, cyfht, np.zeros(9, dtype=np.float64))
