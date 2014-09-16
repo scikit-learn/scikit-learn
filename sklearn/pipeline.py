25a327c563df0ca48e2ffad0d1f428a2851661da
Aaron Schumacher <ajschumacher@gmail.com>
2014-09-16 15:52:58 -0400

    tweak to more natural version

--- sklearn/pipeline.py
+++ sklearn/pipeline.py
@@ -32,7 +32,7 @@ class Pipeline(BaseEstimator):
     Sequentially apply a list of transforms and a final estimator.
     Intermediate steps of the pipeline must be 'transforms', that is, they
     must implement fit and transform methods.
-    The final estimator need only implement fit.
+    The final estimator only needs to implement fit.
 
     The purpose of the pipeline is to assemble several steps that can be
     cross-validated together while setting different parameters.
