75f6e015c98f6fb58275617e2ed69169129bbcfe
Peter Prettenhofer <peter.prettenhofer@gmail.com>
2014-09-17 10:07:53 +0200

    more tests for weighted percentile

--- sklearn/ensemble/tests/test_gradient_boosting_loss_functions.py
+++ sklearn/ensemble/tests/test_gradient_boosting_loss_functions.py
@@ -15,6 +15,7 @@ from sklearn.ensemble.gradient_boosting import LogOddsEstimator
 from sklearn.ensemble.gradient_boosting import LeastSquaresError
 from sklearn.ensemble.gradient_boosting import RegressionLossFunction
 from sklearn.ensemble.gradient_boosting import LOSS_FUNCTIONS
+from sklearn.ensemble.gradient_boosting import _weighted_percentile
 
 
 def test_binomial_deviance():
@@ -111,3 +112,33 @@ def test_sample_weight_init_estimators():
 
         # check if predictions match
         assert_array_equal(out, sw_out)
+
+
+def test_weighted_percentile():
+    y = np.empty(102, dtype=np.float)
+    y[:50] = 0
+    y[-51:] = 2
+    y[-1] = 100000
+    y[50] = 1
+    sw = np.ones(102, dtype=np.float)
+    sw[-1] = 0.0
+    score = _weighted_percentile(y, sw, 50)
+    assert score == 1
+
+
+def test_weighted_percentile_equal():
+    y = np.empty(102, dtype=np.float)
+    y.fill(0.0)
+    sw = np.ones(102, dtype=np.float)
+    sw[-1] = 0.0
+    score = _weighted_percentile(y, sw, 50)
+    assert score == 0
+
+
+def test_weighted_percentile_zero_weight():
+    y = np.empty(102, dtype=np.float)
+    y.fill(1.0)
+    sw = np.ones(102, dtype=np.float)
+    sw.fill(0.0)
+    score = _weighted_percentile(y, sw, 50)
+    assert score == 1.0
