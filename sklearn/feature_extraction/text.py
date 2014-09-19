a57a3065096a4bd534fd6ee200f47be21392465d
danfrankj <danfrankj@gmail.com>
2014-09-18 18:21:43 -0700

    Correct documentation for TfidfVectorizer

--- sklearn/feature_extraction/text.py
+++ sklearn/feature_extraction/text.py
@@ -1091,14 +1091,14 @@ class TfidfVectorizer(CountVectorizer):
         and always treated as a token separator).
 
     max_df : float in range [0.0, 1.0] or int, optional, 1.0 by default
-        When building the vocabulary ignore terms that have a term frequency
+        When building the vocabulary ignore terms that have a document frequency
         strictly higher than the given threshold (corpus specific stop words).
         If float, the parameter represents a proportion of documents, integer
         absolute counts.
         This parameter is ignored if vocabulary is not None.
 
     min_df : float in range [0.0, 1.0] or int, optional, 1 by default
-        When building the vocabulary ignore terms that have a term frequency
+        When building the vocabulary ignore terms that have a document frequency
         strictly lower than the given threshold.
         This value is also called cut-off in the literature.
         If float, the parameter represents a proportion of documents, integer
