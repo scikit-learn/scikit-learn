324e3c9419a84ca245bfc9e69719674a2d9935cb
Dan Blanchard <dan.blanchard@gmail.com>
2014-09-20 21:58:42 -0400

    ENH sort option for memory-efficient DictVectorizer
    Added memory efficient UnsortedDictVectorizer
    
    When loading really large files for SKLL, I found that temporarily
    storing a list of dictionaries to pass to DictVectorizer was frequently
    using up huge amounts of memory. You can now call `fit_transform` on an
    iterable, and not have to waste the temporary space.
    
    Sorting is done in-place, so it doesn't waste memory.

--- sklearn/feature_extraction/dict_vectorizer.py
+++ sklearn/feature_extraction/dict_vectorizer.py
@@ -1,4 +1,5 @@
-# Author: Lars Buitinck <L.J.Buitinck@uva.nl>
+# Authors: Lars Buitinck <L.J.Buitinck@uva.nl>
+#          Dan Blanchard <dblanchard@ets.org>
 # License: BSD 3 clause
 
 from array import array
@@ -49,6 +50,9 @@ class DictVectorizer(BaseEstimator, TransformerMixin):
     sparse: boolean, optional.
         Whether transform should produce scipy.sparse matrices.
         True by default.
+    sort: boolean, optional.
+        Whether feature_names_ and vocabulary_ should be sorted when fitting.
+        True by default.
 
     Attributes
     ----------
@@ -81,10 +85,12 @@ class DictVectorizer(BaseEstimator, TransformerMixin):
       encoded as columns of integers.
     """
 
-    def __init__(self, dtype=np.float64, separator="=", sparse=True):
+    def __init__(self, dtype=np.float64, separator="=", sparse=True,
+                 sort=True):
         self.dtype = dtype
         self.separator = separator
         self.sparse = sparse
+        self.sort = sort
 
     def fit(self, X, y=None):
         """Learn a list of feature name -> indices mappings.
@@ -101,24 +107,31 @@ class DictVectorizer(BaseEstimator, TransformerMixin):
         self
         """
         # collect all the possible feature names
-        feature_names = set()
+        self.feature_names_ = []
+        self.vocabulary_ = {}
+
+        vocab = self.vocabulary_
+
         for x in X:
             for f, v in six.iteritems(x):
                 if isinstance(v, six.string_types):
                     f = "%s%s%s" % (f, self.separator, v)
-                feature_names.add(f)
+                if f not in vocab:
+                    self.feature_names_.append(f)
+                    vocab[f] = len(vocab)
 
-        # sort the feature names to define the mapping
-        feature_names = sorted(feature_names)
-        self.vocabulary_ = dict((f, i) for i, f in enumerate(feature_names))
-        self.feature_names_ = feature_names
+        if self.sort:
+            self.feature_names_.sort()
+            self.vocabulary_ = dict((f, i) for i, f in
+                                    enumerate(self.feature_names_))
 
         return self
 
     def fit_transform(self, X, y=None):
         """Learn a list of feature name -> indices mappings and transform X.
 
-        Like fit(X) followed by transform(X).
+        Like fit(X) followed by transform(X), but does not require
+        materializing X in memory.
 
         Parameters
         ----------
@@ -131,15 +144,73 @@ class DictVectorizer(BaseEstimator, TransformerMixin):
         -------
         Xa : {array, sparse matrix}
             Feature vectors; always 2-d.
-
-        Notes
-        -----
-        Because this method requires two passes over X, it materializes X in
-        memory.
         """
-        X = _tosequence(X)
-        self.fit(X)
-        return self.transform(X)
+        # Sanity check: Python's array has no way of explicitly requesting the
+        # signed 32-bit integers that scipy.sparse needs, so we use the next
+        # best thing: typecode "i" (int). However, if that gives larger or
+        # smaller integers than 32-bit ones, np.frombuffer screws up.
+        assert array("i").itemsize == 4, (
+            "sizeof(int) != 4 on your platform; please report this at"
+            " https://github.com/scikit-learn/scikit-learn/issues and"
+            " include the output from platform.platform() in your bug report")
+
+        self.vocabulary_ = {}
+        self.feature_names_ = []
+
+        dtype = self.dtype
+        vocab = self.vocabulary_
+
+        # Process everything as sparse regardless of setting
+        X = [X] if isinstance(X, Mapping) else X
+
+        indices = array("i")
+        indptr = array("i", [0])
+        # XXX we could change values to an array.array as well, but it
+        # would require (heuristic) conversion of dtype to typecode...
+        values = []
+
+        # collect all the possible feature names and build sparse matrix at
+        # same time
+        for x in X:
+            for f, v in six.iteritems(x):
+                if isinstance(v, six.string_types):
+                    f = "%s%s%s" % (f, self.separator, v)
+                    v = 1
+                if f not in vocab:
+                    self.feature_names_.append(f)
+                    vocab[f] = len(vocab)
+                indices.append(vocab[f])
+                values.append(dtype(v))
+
+            indptr.append(len(indices))
+
+        if len(indptr) == 1:
+            raise ValueError("Sample sequence X is empty.")
+
+        if len(indices) > 0:
+            # workaround for bug in older NumPy:
+            # http://projects.scipy.org/numpy/ticket/1943
+            indices = np.frombuffer(indices, dtype=np.intc)
+        indptr = np.frombuffer(indptr, dtype=np.intc)
+        shape = (len(indptr) - 1, len(vocab))
+
+        result_matrix = sp.csr_matrix((values, indices, indptr),
+                                      shape=shape, dtype=dtype)
+
+        # Sort everything if asked
+        if self.sort:
+            self.feature_names_.sort()
+            map_index = np.empty(len(self.feature_names_), dtype=np.int32)
+            for new_val, f in enumerate(self.feature_names_):
+                map_index[new_val] = self.vocabulary_[f]
+                self.vocabulary_[f] = new_val
+            result_matrix = result_matrix[:, map_index]
+
+        # Convert to dense if asked
+        if not self.sparse:
+            result_matrix = result_matrix.toarray()
+
+        return result_matrix
 
     def inverse_transform(self, X, dict_type=dict):
         """Transform array or sparse matrix X back to feature mappings.
