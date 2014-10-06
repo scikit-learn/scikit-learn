d239fc4e4f5ff9969f6dc88b79c212a64e422734
Clemens Brunner <clemens.brunner@tugraz.at>
2014-10-06 15:18:57 +0200

    Simplified LDA vs. shrinkage LDA example

--- examples/classification/plot_lda.py
+++ examples/classification/plot_lda.py
@@ -11,58 +11,50 @@ from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 
+from sklearn.datasets import make_blobs
 from sklearn.lda import LDA
 
 
-n_train = 10     # samples per class for training
-n_test = 100     # samples per class for testing
+n_train = 20  # samples for training
+n_test = 200  # samples for testing
 n_averages = 50  # how often to repeat classification
 
 
 def generate_data(n_samples, n_features):
-    """ generate `n_samples` samples of data with 1 discriminative features
+    """Generate `n_samples` samples of data with 1 discriminative features
     and n_`features` non-discriminative features."""
-    X = np.vstack([np.random.randn(n_samples, 1) - 2,
-                   np.random.randn(n_samples, 1) + 2])
-    y = np.hstack([[-1] * n_samples, [1] * n_samples])
-    # add non discriminative features
-    X = np.hstack([X, np.random.randn(2 * n_samples, n_features)])
+    X, y = make_blobs(n_samples=n_samples, n_features=1, centers=[[-2], [2]])
+
+    # add non-discriminative features
+    if n_features > 1:
+        X = np.hstack([X, np.random.randn(n_samples, n_features - 1)])
     return X, y
 
-acc_lda, acc_slda, acc_nlda = [], [], []
-m_range = range(0, 50)
+acc_clf1, acc_clf2 = [], []
+m_range = range(1, 100)
 for m in m_range:
-    score_lda, score_slda, score_nlda = 0, 0, 0
+    score_clf1, score_clf2 = 0, 0
     for i in range(n_averages):
         X, y = generate_data(n_train, m)
 
-        lda = LDA().fit(X, y)
-        slda = LDA(use_covariance='ledoit_wolf').fit(X, y)
-        nlda = LDA(use_covariance='empirical').fit(X, y)
+        clf1 = LDA(solver='lsqr', alpha='ledoit_wolf').fit(X, y)
+        clf2 = LDA(solver='lsqr', alpha=None).fit(X, y)
 
         X, y = generate_data(n_test, m)
-        score_lda += lda.score(X, y) / n_averages
-        score_slda += slda.score(X, y) / n_averages
-        score_nlda += nlda.score(X, y) / n_averages
+        score_clf1 += clf1.score(X, y)
+        score_clf2 += clf2.score(X, y)
 
-    acc_lda.append(score_lda)
-    acc_slda.append(score_slda)
-    acc_nlda.append(score_nlda)
+    acc_clf1.append(score_clf1 / n_averages)
+    acc_clf2.append(score_clf2 / n_averages)
 
-m_range = (1 + np.array(m_range)) / (2 * n_train)
+m_range = np.array(m_range) / n_train
 
-plt.plot(m_range, acc_slda, linewidth=2,
-         label="LDA(use_covariance='ledoit_wolf')", color='r')
-plt.plot(m_range, acc_nlda, linewidth=2,
-         label="LDA(use_covariance='empirical')", color='g')
-plt.plot(m_range, acc_lda, linewidth=2,
-         label="LDA()", color='b')
+plt.plot(m_range, acc_clf1, linewidth=2, label="LDA with shrinkage", color='r')
+plt.plot(m_range, acc_clf2, linewidth=2, label="LDA", color='g')
 
 plt.xlabel('n_features / n_samples')
 plt.ylabel('Classification accuracy')
 
-plt.legend(loc=4, prop={'size': 8})
-
-plt.suptitle('LDA vs sLDA (1 discriminative feature)')
-
+plt.legend(loc=1, prop={'size': 8})
+plt.suptitle('LDA vs shrinkage LDA (1 discriminative feature)')
 plt.show()
