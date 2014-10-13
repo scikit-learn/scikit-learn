fb5b48176429dee9a35aa8178399a8330411750f
x0l <x0l@github.com>
2014-10-13 18:17:42 +0200

    tests

--- sklearn/tests/test_qda.py
+++ sklearn/tests/test_qda.py
@@ -4,6 +4,7 @@ from sklearn.utils.testing import assert_array_equal
 from sklearn.utils.testing import assert_array_almost_equal
 from sklearn.utils.testing import assert_true
 from sklearn.utils.testing import assert_greater
+from sklearn.utils.testing import assert_raises
 
 from sklearn import qda
 
@@ -20,6 +21,13 @@ X1 = np.array([[-3, ], [-2, ], [-1, ], [-1, ], [0, ], [1, ], [1, ],
 # Data that has zero variance in one dimension and needs regularization
 X2 = np.array([[-3, 0], [-2, 0], [-1, 0], [-1, 0], [0, 0], [1, 0], [1, 0],
                [2, 0], [3, 0]])
+
+# One element class
+y4 = np.array([1, 1, 1, 1, 1, 1, 1, 1, 2])
+
+# Data with less samples in a class than n_features
+X5 = np.c_[np.arange(8), np.zeros((8,3))]
+y5 = np.array([0, 0, 0, 0, 0, 1, 1, 1])
 
 
 def test_qda():
@@ -46,6 +54,9 @@ def test_qda():
     y_pred3 = clf.fit(X, y3).predict(X)
     # QDA shouldn't be able to separate those
     assert_true(np.any(y_pred3 != y3))
+
+    # Classes should have at least 2 elements
+    assert_raises(ValueError, clf.fit, X, y4)
 
 
 def test_qda_priors():
@@ -92,3 +103,9 @@ def test_qda_regularization():
     clf = qda.QDA(reg_param=0.01)
     y_pred = clf.fit(X2, y).predict(X2)
     assert_array_equal(y_pred, y)
+
+    # Case n_samples_in_a_class < n_features
+    # (needs some stronger regularization, test is very singular)
+    clf = qda.QDA(reg_param=1e-1)
+    y_pred5 = clf.fit(X5, y5).predict(X5)
+    assert_array_equal(y_pred5, y5)
