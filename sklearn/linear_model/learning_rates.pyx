99e77d41fb772f501ac218b734b53516a39c92f3
Danny Sullivan <dsullivan7@hotmail.com>
2014-10-24 17:20:32 +0200

    fixing pickling problem

--- sklearn/linear_model/learning_rates.pyx
+++ sklearn/linear_model/learning_rates.pyx
@@ -17,20 +17,29 @@ cdef class Constant(LearningRate):
     cdef double eta(self, double eta0, double alpha, double t, double power_t):
         return eta0
 
+    def __reduce__(self):
+        return Constant, ()
+
 cdef class Optimal(LearningRate):
     @cython.cdivision(True)
     cdef double eta(self, double eta0, double alpha, double t, double power_t):
         return 1.0 / (alpha * (t - 1))
+
+    def __reduce__(self):
+        return Optimal, ()
 
 cdef class InvScaling(LearningRate):
     @cython.cdivision(True)
     cdef double eta(self, double eta0, double alpha, double t, double power_t):
         return eta0 / pow(t, power_t)
 
+    def __reduce__(self):
+        return InvScaling, ()
+
 cdef class AdaGrad(LearningRate):
 
-    def __cinit__(self, double eps0, double rho0):
-        self.sum_squared_grad = 0.0
+    def __cinit__(self, double sum_squared_grad, double eps0, double rho0):
+        self.sum_squared_grad = sum_squared_grad
         self.eps0 = eps0
 
     cdef double eta(self, double eta0, double alpha, double t, double power_t):
@@ -43,9 +52,13 @@ cdef class AdaGrad(LearningRate):
         self.sum_squared_grad += gradient ** 2.0 + self.eps0
         return -(eta / sqrt(self.sum_squared_grad)) * gradient
 
+    def __reduce__(self):
+        return AdaGrad, (self.sum_squared_grad, self.eps0, self.rho0)
+
+
 cdef class AdaDelta(LearningRate):
-    def __cinit__(self, double eps0, double rho0):
-        self.sum_squared_grad = 0
+    def __cinit__(self, double sum_squared_grad, double eps0, double rho0):
+        self.sum_squared_grad = sum_squared_grad
         self.rho0 = rho0
         self.eps0 = eps0
         self.accugrad = 0
@@ -66,6 +79,9 @@ cdef class AdaDelta(LearningRate):
             (1. - self.rho0) * dx * dx
         self.accugrad = agrad
         return dx
+
+    def __reduce__(self):
+        return AdaDelta, (self.sum_squared_grad, self.eps0, self.rho0)
 
 cdef class PA(LearningRate):
     cdef double _get_multiplier(self, int is_hinge, double p, double y):
@@ -91,6 +107,9 @@ cdef class PA1(PA):
         update *= self._get_multiplier(is_hinge, p, y)
         return update
 
+    def __reduce__(self):
+        return PA1, ()
+
 cdef class PA2(PA):
     cdef double eta(self, double eta0, double alpha, double t, double power_t):
         return eta0
@@ -102,3 +121,6 @@ cdef class PA2(PA):
         update = loss / (norm + 0.5 / C)
         update *= self._get_multiplier(is_hinge, p, y)
         return update
+
+    def __reduce__(self):
+        return PA2, ()
