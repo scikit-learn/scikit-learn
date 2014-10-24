125fa22165f8335a34427878724b35855aa80164
Clemens Brunner <clemens.brunner@gmail.com>
2014-10-24 11:18:50 +0200

    Updated LDA example and removed decision plane comparison

--- examples/classification/plot_lda.py
+++ examples/classification/plot_lda.py
@@ -22,8 +22,9 @@ n_features_max = 75  # maximum number of features
 
 
 def generate_data(n_samples, n_features):
-    """Generate `n_samples` samples of data with 1 discriminative features
-    and n_`features` non-discriminative features."""
+    """Generate `n_samples` samples of data with `n_features` features. Of
+    these, only one feature contains discriminative information, the other
+    features contain only noise."""
     X, y = make_blobs(n_samples=n_samples, n_features=1, centers=[[-2], [2]])
 
     # add non-discriminative features
@@ -32,26 +33,28 @@ def generate_data(n_samples, n_features):
     return X, y
 
 acc_clf1, acc_clf2 = [], []
-m_range = range(1, n_features_max + 1)
-for m in m_range:
+n_features_range = range(1, n_features_max + 1)
+for n_features in n_features_range:
     score_clf1, score_clf2 = 0, 0
-    for i in range(n_averages):
-        X, y = generate_data(n_train, m)
+    for _ in range(n_averages):
+        X, y = generate_data(n_train, n_features)
 
         clf1 = LDA(solver='lsqr', alpha='ledoit_wolf').fit(X, y)
         clf2 = LDA(solver='lsqr', alpha=None).fit(X, y)
 
-        X, y = generate_data(n_test, m)
+        X, y = generate_data(n_test, n_features)
         score_clf1 += clf1.score(X, y)
         score_clf2 += clf2.score(X, y)
 
     acc_clf1.append(score_clf1 / n_averages)
     acc_clf2.append(score_clf2 / n_averages)
 
-m_range = np.array(m_range) / n_train
+features_samples_ratio = np.array(n_features_range) / n_train
 
-plt.plot(m_range, acc_clf1, linewidth=2, label="LDA with shrinkage", color='r')
-plt.plot(m_range, acc_clf2, linewidth=2, label="LDA", color='g')
+plt.plot(features_samples_ratio, acc_clf1, linewidth=2,
+         label="LDA with shrinkage", color='r')
+plt.plot(features_samples_ratio, acc_clf2, linewidth=2,
+         label="LDA", color='g')
 
 plt.xlabel('n_features / n_samples')
 plt.ylabel('Classification accuracy')
