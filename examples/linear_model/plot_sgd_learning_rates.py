165a6650ee1ba941a1d66a4b9060c6ea66ff4d78
Danny Sullivan <dsullivan7@hotmail.com>
2014-10-29 21:07:53 +0100

    adding comparison for all learning rates TODO: if this is a good comparison for all learning rates, remove other datasets

--- examples/linear_model/plot_sgd_learning_rates.py
+++ examples/linear_model/plot_sgd_learning_rates.py
@@ -8,8 +8,10 @@ schedules affect accuracy convergence
 
 """
 # Author: Danny Sullivan <dsullivan7 at hotmail dot com>
+#         Alex Gramfort
 
 import numpy as np
+import time
 import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.linear_model import SGDClassifier
@@ -21,87 +23,105 @@ train_proportion = .8
 
 def train(X_train, y_train, X_test, y_test, classes, classifiers):
     plt.figure()
-    for name, clf, scores in classifiers:
-        print("training", name, "...")
+    colors = ['b', 'r', 'g', 'k', 'm']
+    xx = 1 + np.arange(epochs)
+    for col, (name, clf) in zip(colors, classifiers):
+        scores_train = []
+        scores_test = []
+        print("Training %s ..." % name)
+        t0 = time.time()
         for r in range(epochs):
             clf.partial_fit(X_train, y_train, classes=classes)
-            score = clf.score(X_test, y_test)
-            scores.append(1.0 - score)
+            scores_train.append(1.0 - clf.score(X_train, y_train))
+            scores_test.append(1.0 - clf.score(X_test, y_test))
 
-        plt.plot(1 + np.arange(epochs), scores, label=name)
-        print(name, scores)
+        print('Time elapsed: %s' % (time.time() - t0))
+        plt.plot(xx, scores_train, col, label=name + ' (train)')
+        plt.plot(xx, scores_test, col + '--', label=name + ' (test)')
 
     plt.legend(loc="upper right")
     plt.xlabel("epoch")
     plt.ylabel("error")
+    plt.show()
 
 
 if __name__ == "__main__":
-    alpha = 1.e-5
+    train_proportion = .8
+    # shuffle = True
+    shuffle = False
+    loss = 'log'
+    # loss = 'hinge'
+
+    alpha = 1e-8
     news_clfs = [
         ("invscaling", SGDClassifier(learning_rate="invscaling",
-                                     alpha=alpha,
-                                     n_iter=20,
-                                     eta0=50.0), []),
+                                     alpha=alpha, loss=loss,
+                                     n_iter=20, shuffle=shuffle,
+                                     eta0=60.0)),
         ("optimal", SGDClassifier(learning_rate="optimal",
-                                  alpha=alpha,
-                                  n_iter=20), []),
-        # ("adadelta", SGDClassifier(learning_rate="adadelta",
-        #                            alpha=alpha,
-        #                            n_iter=20,
-        #                            eps0=0.2,
-        #                            rho0=.9), []),
+                                  alpha=alpha, loss=loss,
+                                  n_iter=20)),
         ("adadelta", SGDClassifier(learning_rate="adadelta",
-                                   alpha=alpha,
-                                   n_iter=20,
-                                   eps0=100.0,
-                                   rho0=.8), []),
+                                   alpha=alpha, loss=loss,
+                                   n_iter=20, shuffle=shuffle,
+                                   eps0=100.,
+                                   rho0=.8)),
+        ("adagrad", SGDClassifier(learning_rate="adagrad",
+                                  alpha=alpha, loss=loss,
+                                  n_iter=20, shuffle=shuffle,
+                                  eta0=60.,
+                                  eps0=0.0)),
     ]
 
-    alpha = 1.e-8
+    alpha = 1e-1
     digits_clfs = [
         ("invscaling", SGDClassifier(learning_rate="invscaling",
-                                     alpha=alpha,
-                                     n_iter=20,
-                                     eta0=.01), []),
+                                     alpha=alpha, loss=loss,
+                                     n_iter=20, shuffle=shuffle,
+                                     eta0=.01)),
         ("optimal", SGDClassifier(learning_rate="optimal",
-                                  alpha=alpha,
-                                  n_iter=20), []),
+                                  alpha=alpha, loss=loss,
+                                  n_iter=20)),
+        ("adadelta", SGDClassifier(learning_rate="adadelta",
+                                   alpha=alpha, loss=loss,
+                                   n_iter=20, shuffle=shuffle,
+                                   eps0=10.,
+                                   rho0=.9)),
         ("adagrad", SGDClassifier(learning_rate="adagrad",
-                                  alpha=alpha,
-                                  n_iter=20,
-                                  eta0=.01,
-                                  eps0=.0), []),
+                                  alpha=alpha, loss=loss,
+                                  n_iter=20, shuffle=shuffle,
+                                  eta0=.00001,
+                                  eps0=1e-2)),
     ]
 
-    alpha = 1.e-5
+    alpha = 1e-1
     faces_clfs = [
-        # ("invscaling", SGDClassifier(learning_rate="invscaling",
-        #                              alpha=alpha,
-        #                              n_iter=20,
-        #                              eta0=40.), []),
-        # ("optimal", SGDClassifier(learning_rate="optimal",
-        #                           alpha=alpha,
-        #                           n_iter=20), []),
-        # ("adagrad", SGDClassifier(learning_rate="adagrad",
-        #                           alpha=alpha,
-        #                           n_iter=20,
-        #                           eta0=50.0,
-        #                           eps0=0.1), []),
-        # ("adadelta", SGDClassifier(learning_rate="adadelta",
-        #                            alpha=alpha,
-        #                            n_iter=20,
-        #                            eps0=1.0,
-        #                            rho0=.95), []),
+        ("invscaling", SGDClassifier(learning_rate="invscaling",
+                                     alpha=alpha, loss=loss,
+                                     n_iter=20, shuffle=shuffle,
+                                     eta0=1.)),
+        ("optimal", SGDClassifier(learning_rate="optimal",
+                                  alpha=alpha, loss=loss,
+                                  n_iter=20)),
+        ("adagrad", SGDClassifier(learning_rate="adagrad",
+                                  alpha=alpha, loss=loss,
+                                  n_iter=20, shuffle=shuffle,
+                                  eta0=1.0,
+                                  eps0=0.1)),
+        ("adadelta", SGDClassifier(learning_rate="adadelta",
+                                   alpha=alpha, loss=loss,
+                                   n_iter=20, shuffle=shuffle,
+                                   eps0=1.0,
+                                   rho0=.95)),
     ]
 
-    tests = [
-        (datasets.load_digits(), digits_clfs),
+    all_datasets = [
+        # (datasets.load_digits(), digits_clfs),
         (datasets.fetch_20newsgroups_vectorized(), news_clfs),
         # (datasets.fetch_lfw_pairs(), faces_clfs),
     ]
 
-    for data_set, classifiers in tests:
+    for data_set, classifiers in all_datasets:
         X, y = data_set.data, data_set.target
         classes = np.unique(y)
 
