14763217b7f31916e51d9e287a00e228656e21d6
maheshakya <pmaheshakya4@gmail.com>
2014-11-11 21:45:22 +0530

    ENH Modified hyperparameters example to calculate stadard deviations of accuracies.

--- examples/neighbors/plot_approximate_nearest_neighbors_hyperparameters.py
+++ examples/neighbors/plot_approximate_nearest_neighbors_hyperparameters.py
@@ -48,36 +48,51 @@ X, _ = make_blobs(n_samples=n_samples + n_queries,
                   random_state=0)
 X_index = X[:n_samples]
 X_query = X[n_samples:]
-# Set `n_candidate` values
-n_candidates_values = np.linspace(10, 500, 5).astype(np.int)
-n_estimators_for_candidate_value = [1, 5, 10]
-accuracies_c = np.zeros((len(n_estimators_for_candidate_value),
-                         n_candidates_values.shape[0]), dtype=float)
-
+# Get exact neighbors
 nbrs = NearestNeighbors(n_neighbors=1, algorithm='brute',
                         metric='cosine').fit(X_index)
 neighbors_exact = nbrs.kneighbors(X_query, return_distance=False)
 
-# Calculate average accuracy for each value of `n_candidates`
+# Set `n_candidate` values
+n_candidates_values = np.linspace(10, 500, 5).astype(np.int)
+n_estimators_for_candidate_value = [1, 5, 10]
+n_iter = 10
+stds_accuracies = np.zeros((len(n_estimators_for_candidate_value),
+                            n_candidates_values.shape[0]),
+                           dtype=float)
+accuracies_c = np.zeros((len(n_estimators_for_candidate_value),
+                         n_candidates_values.shape[0]), dtype=float)
+
+# LSH Forest is a stochastic index: perform several iteration to estimate
+# expected accuracy and standard deviation displayed as error bars in
+# the plots
 for j, value in enumerate(n_estimators_for_candidate_value):
     for i, n_candidates in enumerate(n_candidates_values):
-        lshf = LSHForest(n_estimators=value,
-                         n_candidates=n_candidates, n_neighbors=1)
-        # Fit LSHForest
-        lshf.fit(X_index)
-        # Get neighbors
-        neighbors_approx = lshf.kneighbors(X_query, return_distance=False)
-        accuracies_c[j, i] = np.sum(np.equal(neighbors_approx,
-                                             neighbors_exact))/n_queries
+        accuracy_c = []
+        for seed in range(n_iter):
+            lshf = LSHForest(n_estimators=value,
+                             n_candidates=n_candidates, n_neighbors=1,
+                             random_state=seed)
+            # Build the LSH Forest index
+            lshf.fit(X_index)
+            # Get neighbors
+            neighbors_approx = lshf.kneighbors(X_query,
+                                               return_distance=False)
+            accuracy_c.append(np.sum(np.equal(neighbors_approx,
+                                              neighbors_exact)) /
+                              n_queries)
+
+        stds_accuracies[j, i] = np.std(accuracy_c)
+        accuracies_c[j, i] = np.mean(accuracy_c)
 
 # Set `n_estimators` values
-n_estimators_values = np.arange(1, 11, 1).astype(np.int)
-accuracies_trees = np.zeros(n_estimators_values.shape[0], dtype=float)
+n_estimators_values = [1, 5, 10, 20, 30, 40, 50]
+accuracies_trees = np.zeros(len(n_estimators_values), dtype=float)
 
 # Calculate average accuracy for each value of `n_estimators`
 for i, n_estimators in enumerate(n_estimators_values):
     lshf = LSHForest(n_estimators=n_estimators, n_neighbors=1)
-    # Fit the Nearest neighbor models
+    # Build the LSH Forest index
     lshf.fit(X_index)
     # Get neighbors
     neighbors_approx = lshf.kneighbors(X_query, return_distance=False)
@@ -86,23 +101,17 @@ for i, n_estimators in enumerate(n_estimators_values):
 
 ###############################################################################
 # Plot the accuracy variation with `n_candidates`
-colors = ['c', 'm', 'y']
-p1 = plt.Rectangle((0, 0), 0.1, 0.1, fc=colors[0])
-p2 = plt.Rectangle((0, 0), 0.1, 0.1, fc=colors[1])
-p3 = plt.Rectangle((0, 0), 0.1, 0.1, fc=colors[2])
-
-labels = ['n_estimators = ' + str(n_estimators_for_candidate_value[0]),
-          'n_estimators = ' + str(n_estimators_for_candidate_value[1]),
-          'n_estimators = ' + str(n_estimators_for_candidate_value[2])]
-
 plt.figure()
-plt.legend((p1, p2, p3), (labels[0], labels[1], labels[2]),
-           loc='upper left', fontsize='small')
+colors = ['c', 'm', 'y']
+for i, n_estimators in enumerate(n_estimators_for_candidate_value):
+    label = 'n_estimators = %d ' % n_estimators
+    plt.plot(n_candidates_values, accuracies_c[i, :],
+             'o-', c=colors[i], label=label)
+    plt.errorbar(n_candidates_values, accuracies_c[i, :],
+                 stds_accuracies[i, :], c=colors[i])
 
-for i in range(len(n_estimators_for_candidate_value)):
-    plt.scatter(n_candidates_values, accuracies_c[i, :], c=colors[i])
-    plt.plot(n_candidates_values, accuracies_c[i, :], c=colors[i])
-plt.ylim([0, 1])
+plt.legend(loc='upper left', fontsize='small')
+plt.ylim([0, 1.2])
 plt.xlim(min(n_candidates_values), max(n_candidates_values))
 plt.ylabel("Accuracy")
 plt.xlabel("n_candidates")
@@ -113,7 +122,7 @@ plt.title("Accuracy variation with n_candidates")
 plt.figure()
 plt.scatter(n_estimators_values, accuracies_trees, c='k')
 plt.plot(n_estimators_values, accuracies_trees, c='g')
-plt.ylim([0, 1])
+plt.ylim([0, 1.2])
 plt.xlim(min(n_estimators_values), max(n_estimators_values))
 plt.ylabel("Accuracy")
 plt.xlabel("n_estimators")
