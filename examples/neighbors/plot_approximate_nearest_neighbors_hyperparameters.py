0ebfff2b5cd5326e8fa71f411bee423041b96f63
maheshakya <pmaheshakya4@gmail.com>
2014-11-25 11:18:18 +0530

    ENH Changed description of hyper-parameters example.

--- examples/neighbors/plot_approximate_nearest_neighbors_hyperparameters.py
+++ examples/neighbors/plot_approximate_nearest_neighbors_hyperparameters.py
@@ -1,10 +1,9 @@
 """
-=============================
-Approximate Nearest Neighbors
-=============================
+=================================================
+Hyper-parameters of Approximate Nearest Neighbors
+=================================================
 
-Locality Sensitive Hashing is used as the approximate nearest neighbor
-search method in this example. This demonstrates the behaviour of the
+This example demonstrates the behaviour of the
 accuracy of the nearest neighbor queries of Locality Sensitive Hashing
 Forest as the number of candidates and the number of estimators (trees)
 vary.
@@ -12,9 +11,10 @@ vary.
 In the first plot, accuracy is measured with the number of candidates. Here,
 the term "number of candidates" refers to maximum bound for the number of
 distinct points retrieved from each tree to calculate the distances. Nearest
-neighbors are selected from this pool of candidates.
+neighbors are selected from this pool of candidates. Number of estimators is
+maintained at three fixed levels (1, 5, 10).
 
-In the second plot, the number of candidates is fixed at 500. Number of trees
+In the second plot, the number of candidates is fixed at 50. Number of trees
 is varied and the accuracy is plotted against those values. To measure the
 accuracy, the true nearest neighbors are required, therefore
 :class:`sklearn.neighbors.NearestNeighbors` is used to compute the exact
