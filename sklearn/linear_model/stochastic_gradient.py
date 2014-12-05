27ba52d36eaa25709f4b7ca5625b77a18eee5cc2
dsullivan7 <dbsullivan23@gmail.com>
2014-12-05 11:42:45 +0100

    getting rid of unused methods

--- sklearn/linear_model/stochastic_gradient.py
+++ sklearn/linear_model/stochastic_gradient.py
@@ -234,7 +234,7 @@ class BaseSGD(six.with_metaclass(ABCMeta, BaseEstimator, SparseCoefMixin)):
                 average_intercept_init = 0.0
 
         assert y.shape[0] == sample_weight.shape[0]
-        dataset, intercept_decay = _make_dataset(X, y, sample_weight)
+        dataset, intercept_decay = self._make_dataset(X, y, sample_weight)
 
         penalty_type = self._get_penalty_type(self.penalty)
         learning_rate_type = self._get_learning_rate_type(learning_rate)
@@ -282,6 +282,21 @@ class BaseSGD(six.with_metaclass(ABCMeta, BaseEstimator, SparseCoefMixin)):
 
         return coefs, intercepts
 
+    def _make_dataset(self, X, y_i, sample_weight):
+        """Create ``Dataset`` abstraction for sparse and dense inputs.
+
+        This also returns the ``intercept_decay`` which is different
+        for sparse datasets.
+        """
+        if sp.issparse(X):
+            dataset = CSRDataset(X.data, X.indptr,
+                                 X.indices, y_i, sample_weight)
+            intercept_decay = SPARSE_INTERCEPT_DECAY
+        else:
+            dataset = ArrayDataset(X, y_i, sample_weight)
+            intercept_decay = 1.0
+        return dataset, intercept_decay
+
 
 def _make_dataset(X, y_i, sample_weight):
     """Create ``Dataset`` abstraction for sparse and dense inputs.
@@ -296,91 +311,6 @@ def _make_dataset(X, y_i, sample_weight):
         dataset = ArrayDataset(X, y_i, sample_weight)
         intercept_decay = 1.0
     return dataset, intercept_decay
-
-
-def _prepare_fit_binary(est, y, i):
-    """Initialization for fit_binary.
-
-    Returns y, coef, intercept.
-    """
-    y_i = np.ones(y.shape, dtype=np.float64, order="C")
-    y_i[y != est.classes_[i]] = -1.0
-    average_intercept = 0
-    average_coef = None
-
-    if len(est.classes_) == 2:
-        if not est.average:
-            coef = est.coef_.ravel()
-            intercept = est.intercept_[0]
-        else:
-            coef = est.standard_coef_.ravel()
-            intercept = est.standard_intercept_[0]
-            average_coef = est.average_coef_.ravel()
-            average_intercept = est.average_intercept_[0]
-    else:
-        if not est.average:
-            coef = est.coef_[i]
-            intercept = est.intercept_[i]
-        else:
-            coef = est.standard_coef_[i]
-            intercept = est.standard_intercept_[i]
-            average_coef = est.average_coef_[i]
-            average_intercept = est.average_intercept_[i]
-
-    return y_i, coef, intercept, average_coef, average_intercept
-
-
-def fit_binary(est, i, X, y, alpha, C, learning_rate, n_iter,
-               pos_weight, neg_weight, sample_weight):
-    """Fit a single binary classifier.
-
-    The i'th class is considered the "positive" class.
-    """
-    # if average is not true, average_coef, and average_intercept will be
-    # unused
-    y_i, coef, intercept, average_coef, average_intercept = \
-        _prepare_fit_binary(est, y, i)
-    assert y_i.shape[0] == y.shape[0] == sample_weight.shape[0]
-    dataset, intercept_decay = _make_dataset(X, y_i, sample_weight)
-
-    penalty_type = est._get_penalty_type(est.penalty)
-    learning_rate_type = est._get_learning_rate_type(learning_rate)
-
-    # XXX should have random_state_!
-    random_state = check_random_state(est.random_state)
-    # numpy mtrand expects a C long which is a signed 32 bit integer under
-    # Windows
-    seed = random_state.randint(0, np.iinfo(np.int32).max)
-
-    if not est.average:
-        return plain_sgd(coef, intercept, est.loss_function,
-                         penalty_type, alpha, C, est.l1_ratio,
-                         dataset, n_iter, int(est.fit_intercept),
-                         int(est.verbose), int(est.shuffle), seed,
-                         pos_weight, neg_weight,
-                         learning_rate_type, est.eta0,
-                         est.power_t, est.t_, intercept_decay)
-
-    else:
-        standard_coef, standard_intercept, average_coef, \
-            average_intercept = average_sgd(coef, intercept, average_coef,
-                                            average_intercept,
-                                            est.loss_function, penalty_type,
-                                            alpha, C, est.l1_ratio, dataset,
-                                            n_iter, int(est.fit_intercept),
-                                            int(est.verbose), int(est.shuffle),
-                                            seed, pos_weight, neg_weight,
-                                            learning_rate_type, est.eta0,
-                                            est.power_t, est.t_,
-                                            intercept_decay,
-                                            est.average)
-
-        if len(est.classes_) == 2:
-            est.average_intercept_[0] = average_intercept
-        else:
-            est.average_intercept_[i] = average_intercept
-
-        return standard_coef, standard_intercept
 
 
 class BaseSGDClassifier(six.with_metaclass(ABCMeta, BaseSGD,
