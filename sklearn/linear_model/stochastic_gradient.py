104d0d02d5ef2f97a5dec7dc5f278e56510a68a2
dsullivan7 <dbsullivan23@gmail.com>
2014-12-05 16:04:37 +0100

    removing more unused code

--- sklearn/linear_model/stochastic_gradient.py
+++ sklearn/linear_model/stochastic_gradient.py
@@ -302,21 +302,6 @@ class BaseSGD(six.with_metaclass(ABCMeta, BaseEstimator, SparseCoefMixin)):
         return dataset, intercept_decay
 
 
-def _make_dataset(X, y_i, sample_weight):
-    """Create ``Dataset`` abstraction for sparse and dense inputs.
-
-    This also returns the ``intercept_decay`` which is different
-    for sparse datasets.
-    """
-    if sp.issparse(X):
-        dataset = CSRDataset(X.data, X.indptr, X.indices, y_i, sample_weight)
-        intercept_decay = SPARSE_INTERCEPT_DECAY
-    else:
-        dataset = ArrayDataset(X, y_i, sample_weight)
-        intercept_decay = 1.0
-    return dataset, intercept_decay
-
-
 class BaseSGDClassifier(six.with_metaclass(ABCMeta, BaseSGD,
                                            LinearClassifierMixin)):
 
@@ -1066,77 +1051,6 @@ class BaseSGDRegressor(BaseSGD, RegressorMixin):
            Predicted target values per element in X.
         """
         return self.decision_function(X)
-
-    def _fit_regressor(self, X, y, alpha, C, loss, learning_rate,
-                       sample_weight, n_iter):
-        dataset, intercept_decay = _make_dataset(X, y, sample_weight)
-
-        loss_function = self._get_loss_function(loss)
-        penalty_type = self._get_penalty_type(self.penalty)
-        learning_rate_type = self._get_learning_rate_type(learning_rate)
-
-        if self.t_ is None:
-            self.t_ = 1.0
-
-        random_state = check_random_state(self.random_state)
-        # numpy mtrand expects a C long which is a signed 32 bit integer under
-        # Windows
-        seed = random_state.randint(0, np.iinfo(np.int32).max)
-
-        if self.average > 0:
-            self.standard_coef_, self.standard_intercept_, \
-                self.average_coef_, self.average_intercept_ =\
-                average_sgd(self.standard_coef_,
-                            self.standard_intercept_[0],
-                            self.average_coef_,
-                            self.average_intercept_[0],
-                            loss_function,
-                            penalty_type,
-                            alpha, C,
-                            self.l1_ratio,
-                            dataset,
-                            n_iter,
-                            int(self.fit_intercept),
-                            int(self.verbose),
-                            int(self.shuffle),
-                            seed,
-                            1.0, 1.0,
-                            learning_rate_type,
-                            self.eta0, self.power_t, self.t_,
-                            intercept_decay, self.average)
-
-            self.average_intercept_ = np.atleast_1d(self.average_intercept_)
-            self.standard_intercept_ = np.atleast_1d(self.standard_intercept_)
-            self.t_ += n_iter * X.shape[0]
-
-            if self.average <= self.t_ - 1.0:
-                self.coef_ = self.average_coef_
-                self.intercept_ = self.average_intercept_
-            else:
-                self.coef_ = self.standard_coef_
-                self.intercept_ = self.standard_intercept_
-
-        else:
-            self.coef_, self.intercept_ = \
-                plain_sgd(self.coef_,
-                          self.intercept_[0],
-                          loss_function,
-                          penalty_type,
-                          alpha, C,
-                          self.l1_ratio,
-                          dataset,
-                          n_iter,
-                          int(self.fit_intercept),
-                          int(self.verbose),
-                          int(self.shuffle),
-                          seed,
-                          1.0, 1.0,
-                          learning_rate_type,
-                          self.eta0, self.power_t, self.t_,
-                          intercept_decay)
-
-            self.t_ += n_iter * X.shape[0]
-            self.intercept_ = np.atleast_1d(self.intercept_)
 
 
 class SGDRegressor(BaseSGDRegressor, _LearntSelectorMixin):
