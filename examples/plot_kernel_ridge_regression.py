47a59aa333e3a10e2d41afe8f5595340b95bc160
Jan Hendrik Metzen <jhm@informatik.uni-bremen.de>
2014-12-07 11:05:32 +0100

    ENH Example comparing KRR and SVR documented and slightly modified

--- examples/plot_kernel_ridge_regression.py
+++ examples/plot_kernel_ridge_regression.py
@@ -1,3 +1,40 @@
+"""
+================================================
+Comapriation of kernel ridge regression and SVR
+================================================
+
+Both kernel ridge regression (KRR) and SVR learn a non-linear function by 
+employing the kernel trick, i.e., they learn a
+linear function in the Reproducing kernel Hilbert space induced by the
+respective kernel. They differ in the loss functions (ridge versus epsilon-
+insensitive loss). In contrast to SVR, fitting a KRR can be done in closed-
+form and is typically faster for medium-sized datasets. On the other hand,
+the learned model is non-sparse and thus slower than SVR at prediction-time.
+
+This example illustrates both methods on an artificial dataset, which
+consists of a sinusoidal target function and strong noise added to every fifth
+datapoint. The first figure compares the learned model of KRR and SVR when both
+complexity/regularization and bandwidth of the RBF kernel are optimized using
+grid-search. The learned functions are very similar; however, fitting KRR is
+approx. seven times faster than fitting SVR (both with grid-search). However,
+prediction of 100000 target values is more than tree times faster with SVR since
+it has learned a sparse model using only approx. 1/3 of the 100 training
+datapoints as support vectors. 
+
+The next figure compares the time for fitting and prediction of KRR and SVR for
+different sizes of the training set. Fitting KRR is faster than SVR for medium-
+sized training sets (less than 1000 samples); however, for larger training sets
+SVR scales better. With regard to prediction time, SVR is faster than
+KRR for all sizes of the training set because of the learned sparse
+solution. Note that the degree of sparsity and thus the prediction time depends
+on the parameters epsilon and C of the SVR.  
+"""
+
+# Authors: Jan Hendrik Metzen <jhm@informatik.uni-bremen.de>
+# License: BSD 3 clause
+
+
+from __future__ import division
 import time
 
 import numpy as np
@@ -10,7 +47,7 @@ import matplotlib.pyplot as plt
 
 np.random.seed(0)
 
-###############################################################################
+#############################################################################
 # Generate sample data
 X = 5 * np.random.rand(10000, 1)
 y = np.sin(X).ravel()
@@ -20,8 +57,9 @@ y[::5] += 3 * (0.5 - np.random.rand(X.shape[0]/5))
 
 X_plot = np.linspace(0, 5, 100000)[:, None]
 
-###############################################################################
+#############################################################################
 # Fit regression model
+train_size = 100
 svr = GridSearchCV(SVR(kernel='rbf', gamma=1e-1), cv=5,
 				   param_grid={"C": [1e0, 1e1, 1e2, 1e3],
 							   "gamma": np.logspace(-2, 2, 5)
@@ -33,35 +71,46 @@ kr = GridSearchCV(KernelRidge(kernel='rbf', gamma=1e-1), cv=5,
 				  			  })
 
 t0 = time.time()
-svr.fit(X[:100], y[:100])
-print "SVR complexity and bandwidth selected and model fitted in %.3f s" \
-	% (time.time() - t0)
+svr.fit(X[:train_size], y[:train_size])
+svr_fit = time.time() - t0
+print("SVR complexity and bandwidth selected and model fitted in %.3f s"
+	  % svr_fit)
 
 t0 = time.time()
-kr.fit(X[:100], y[:100])
-print "KR complexity and bandwidth selected and model fitted in %.3f s" \
-	% (time.time() - t0)
+kr.fit(X[:train_size], y[:train_size])
+kr_fit = time.time() - t0
+print("KRR complexity and bandwidth selected and model fitted in %.3f s"
+	  % kr_fit)
+
+sv_ratio = svr.best_estimator_.support_.shape[0] / train_size
+print("Support vector ratio: %.3f" % sv_ratio)
 
 t0 = time.time()
 y_svr = svr.predict(X_plot)
-print "SVR prediction for %d inputs in %.3f s" \
-	% (X_plot.shape[0], time.time() - t0)
+svr_predict = time.time() - t0
+print("SVR prediction for %d inputs in %.3f s"
+	  % (X_plot.shape[0], svr_predict))
 
 t0 = time.time()
 y_kr = kr.predict(X_plot)
-print "KR prediction for %d inputs in %.3f s" \
-	% (X_plot.shape[0], time.time() - t0)
+kr_predict = time.time() - t0
+print("KRR prediction for %d inputs in %.3f s"
+	  % (X_plot.shape[0], kr_predict))
 
 
-###############################################################################
+#############################################################################
 # look at the results
+sv_ind = svr.best_estimator_.support_
+plt.scatter(X[sv_ind], y[sv_ind], c='r', s=50, label='SVR support vectors')
 plt.scatter(X[:100], y[:100], c='k', label='data')
 plt.hold('on')
-plt.plot(X_plot, y_svr, c='r', label='SVR')
-plt.plot(X_plot, y_kr, c='g', label='KR')
+plt.plot(X_plot, y_svr, c='r', 
+	     label='SVR (fit: %.3fs, predict: %.3fs)' % (svr_fit, svr_predict))
+plt.plot(X_plot, y_kr, c='g', 
+	     label='KRR (fit: %.3fs, predict: %.3fs)' % (kr_fit, kr_predict))
 plt.xlabel('data')
 plt.ylabel('target')
-plt.title('Support Vector Regression')
+plt.title('SVR versus Kernel Ridge')
 plt.legend()
 
 # Visualize training and prediction time
@@ -72,8 +121,9 @@ X = 5 * np.random.rand(10000, 1)
 y = np.sin(X).ravel()
 y[::5] += 3 * (0.5 - np.random.rand(X.shape[0]/5))
 sizes = np.logspace(1, 4, 7)
-for name, estimator in {"kr": KernelRidge(kernel='rbf', alpha=1e-1, gamma=1e+1),
-             		    "svr": SVR(kernel='rbf', C=1e1, gamma=1e+1)}.items():
+for name, estimator in {"KRR": KernelRidge(kernel='rbf', alpha=1e-1, 
+	                                      gamma=1e+1),
+             		    "SVR": SVR(kernel='rbf', C=1e1, gamma=1e+1)}.items():
 	train_time = []
 	test_time = []
 	for train_test_size in sizes:
@@ -82,7 +132,7 @@ for name, estimator in {"kr": KernelRidge(kernel='rbf', alpha=1e-1, gamma=1e+1),
 		train_time.append(time.time() - t0)
 
 		t0 = time.time()
-		estimator.predict(X_plot[:train_test_size])
+		estimator.predict(X_plot[:1000])
 		test_time.append(time.time() - t0)
 
 	plt.plot(sizes, train_time, 'o-', color="r" if name == "svr" else "g",
@@ -92,7 +142,7 @@ for name, estimator in {"kr": KernelRidge(kernel='rbf', alpha=1e-1, gamma=1e+1),
 
 plt.xscale("log")
 plt.yscale("log")
-plt.xlabel("Train/Test size")
+plt.xlabel("Train size")
 plt.ylabel("Time (seconds)")
 plt.title('Execution Time')
 plt.legend(loc="best")
@@ -112,7 +162,7 @@ train_sizes_abs, train_scores_kr, test_scores_kr = \
 plt.plot(train_sizes, test_scores_svr.mean(1), 'o-', color="r",
  		 label="SVR")
 plt.plot(train_sizes, test_scores_kr.mean(1), 'o-', color="g",
-		 label="Kernel Regression")
+		 label="KRR")
 plt.xlabel("Train size")
 plt.ylabel("Mean Squared Error")
 plt.title('Learning curves')
