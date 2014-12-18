5db4a34841fae28c8bf1cefbb8cbd2da582c23a1
Matteo Visconti dOC <matteo.visconti.gr@dartmouth.edu>
2014-12-18 13:17:45 +0100

    Clean code

--- sklearn/cluster/hierarchical.py
+++ sklearn/cluster/hierarchical.py
@@ -286,7 +286,7 @@ def ward_tree(X, connectivity=None, n_components=None, n_clusters=None,
 
     if return_distance:
         # 2 is scaling factor to compare w/ unstructured version
-        distances = np.sqrt(np.array(distances) * 2)
+        distances = np.sqrt(2. * np.array(distances))
         return children, n_components, n_leaves, parent, distances
     else:
         return children, n_components, n_leaves, parent
