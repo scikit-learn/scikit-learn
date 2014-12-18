3c8210f752f256e098f4017955a1e2232f425618
Olivier Grisel <olivier.grisel@ensta.org>
2014-12-18 16:36:10 +0100

    ENH better 'constant' learning rate schedule

--- benchmarks/bench_mnist.py
+++ benchmarks/bench_mnist.py
@@ -92,8 +92,9 @@ ESTIMATORS = {
     make_pipeline(RBFSampler(gamma=0.015, n_components=1000), LinearSVC(C=100)),
     'LinearRegression-SAG': LogisticRegression(solver='sag', tol=1e-1, C=1e4)
     'MultilayerPerceptron': MultilayerPerceptronClassifier(
-        hidden_layer_sizes=(200,), max_iter=400, alpha=0.5, algorithm='l-bfgs',
-        random_state=1)
+        hidden_layer_sizes=(100, 100), max_iter=400, alpha=1e-4,
+        algorithm='sgd', learning_rate_init=0.5, momentum=0.9, verbose=1,
+        tol=1e-4, random_state=1)
 }
 
 
