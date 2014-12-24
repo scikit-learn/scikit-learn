d255866c667b74801b68e828b6fb5a769c531662
Jan Hendrik Metzen <jhm@informatik.uni-bremen.de>
2014-12-24 13:06:25 +0100

    DOC Add inline comment with reference to scipy issue

--- sklearn/isotonic.py
+++ sklearn/isotonic.py
@@ -316,6 +316,8 @@ class IsotonicRegression(BaseEstimator, TransformerMixin, RegressorMixin):
                              .format(self.out_of_bounds))
 
         if self.out_of_bounds == "clip":
+            # XXX: Address scipy issue 4304 by adding a tiny value to 
+            # self.X_min_ during clipping
             T = np.clip(T, self.X_min_ + np.finfo(float).resolution,
                         self.X_max_)
         return self.f_(T)
