1cf3e06648f11dc18b5f594140297b0332f59d67
Andreas Mueller <amueller@nyu.edu>
2015-01-20 15:22:07 -0500

    special case if dist is object in _get_weights

--- sklearn/neighbors/base.py
+++ sklearn/neighbors/base.py
@@ -83,14 +83,20 @@ def _get_weights(dist, weights):
         # if user attempts to classify a point that was zero distance from one
         # or more training points, those training points are weighted as 1.0
         # and the other points as 0.0
-        for point_dist_i, point_dist in enumerate(dist):
-            # check if point_dist is iterable
-            # (ex: RadiusNeighborClassifier.predict may set an element of dist
-            # to 1e-6 to represent an 'outlier')
-            if hasattr(point_dist, '__contains__') and 0. in point_dist:
-                dist[point_dist_i] = (point_dist == 0.)
-            else:
-                dist[point_dist_i] = 1. / point_dist
+        if dist.dtype is np.dtype(object):
+            for point_dist_i, point_dist in enumerate(dist):
+                # check if point_dist is iterable
+                # (ex: RadiusNeighborClassifier.predict may set an element of dist
+                # to 1e-6 to represent an 'outlier')
+                if hasattr(point_dist, '__contains__') and 0. in point_dist:
+                    dist[point_dist_i] = point_dist == 0.
+                else:
+                    dist[point_dist_i] = 1. / point_dist
+        else:
+            dist = 1. / dist
+            inf_mask = np.isinf(dist)
+            inf_row = np.any(inf_mask, axis=1)
+            dist[inf_row] = inf_mask[inf_row]
         return dist
     elif callable(weights):
         return weights(dist)
