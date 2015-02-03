92ed5eee423bf2986e6610a552a20cc4fc038d55
Arnaud Joly <arnaud.v.joly@gmail.com>
2015-02-03 13:39:18 +0100

    FIX ensure that negative float max_features will lead to an error

--- sklearn/ensemble/tests/test_gradient_boosting.py
+++ sklearn/ensemble/tests/test_gradient_boosting.py
@@ -340,6 +340,9 @@ def test_check_max_features():
                                     max_features=(len(X[0]) + 1))
     assert_raises(ValueError, clf.fit, X, y)
 
+    clf = GradientBoostingRegressor(n_estimators=100, random_state=1,
+                                    max_features=-0.1)
+    assert_raises(ValueError, clf.fit, X, y)
 
 def test_max_feature_regression():
     """Test to make sure random state is set properly. """
