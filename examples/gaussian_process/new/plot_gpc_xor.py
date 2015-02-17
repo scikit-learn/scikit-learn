4ac2f5b601be96ef7757509b580022a3f006e115
Jan Hendrik Metzen <jhm@informatik.uni-bremen.de>
2015-02-17 07:21:06 +0100

    ADD Examples for GP classification

---
+++ examples/gaussian_process/new/plot_gpc_xor.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.gaussian_process import GaussianProcessClassification
+from sklearn.gaussian_process.kernels import RBF
+
+xx, yy = np.meshgrid(np.linspace(-3, 3, 100),
+                     np.linspace(-3, 3, 100))
+np.random.seed(0)
+X = np.random.randn(200, 2)
+Y = np.logical_xor(X[:, 0] > 0, X[:, 1] > 0)
+
+# fit the model
+kernel = [1e-10, 1.0, 100] * RBF(param_space=(1e-10, 1.0, 10))
+clf = GaussianProcessClassification(kernel=kernel).fit(X, Y)
+
+# plot the decision function for each datapoint on the grid
+Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+
+image = plt.imshow(Z, interpolation='nearest',
+           extent=(xx.min(), xx.max(), yy.min(), yy.max()), aspect='auto',
+           origin='lower', cmap=plt.cm.PuOr_r)
+contours = plt.contour(xx, yy, Z, levels=[0], linewidths=2,
+                       linetypes='--')
+plt.scatter(X[:, 0], X[:, 1], s=30, c=Y, cmap=plt.cm.Paired)
+plt.xticks(())
+plt.yticks(())
+plt.axis([-3, 3, -3, 3])
+plt.colorbar(image)
+plt.show()
