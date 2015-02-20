226bb3a8390e571b9b9d26c3473a94bf1f958881
Jan Hendrik Metzen <jhm@informatik.uni-bremen.de>
2015-02-20 19:59:37 +0100

    DOC Revised examples based on @eickenberg's comments

--- examples/gaussian_process/new/plot_gpc_xor.py
+++ examples/gaussian_process/new/plot_gpc_xor.py
@@ -14,10 +14,11 @@ import matplotlib.pyplot as plt
 from sklearn.gaussian_process import GaussianProcessClassification
 from sklearn.gaussian_process.kernels import RBF
 
+
 xx, yy = np.meshgrid(np.linspace(-3, 3, 50),
                      np.linspace(-3, 3, 50))
-np.random.seed(0)
-X = np.random.randn(200, 2)
+rng = np.random.RandomState(0)
+X = rng.randn(200, 2)
 Y = np.logical_xor(X[:, 0] > 0, X[:, 1] > 0)
 
 # fit the model
@@ -25,12 +26,12 @@ kernel = [1e-10, 1.0, 100] * RBF(param_space=(1e-10, 1.0, 10))
 clf = GaussianProcessClassification(kernel=kernel).fit(X, Y)
 
 # plot the decision function for each datapoint on the grid
-Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])
+Z = clf.predict_proba(np.vstack((xx.ravel(), yy.ravel())).T)
 Z = Z.reshape(xx.shape)
 
 image = plt.imshow(Z, interpolation='nearest',
-           extent=(xx.min(), xx.max(), yy.min(), yy.max()), aspect='auto',
-           origin='lower', cmap=plt.cm.PuOr_r)
+                   extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+                   aspect='auto', origin='lower', cmap=plt.cm.PuOr_r)
 contours = plt.contour(xx, yy, Z, levels=[0], linewidths=2,
                        linetypes='--')
 plt.scatter(X[:, 0], X[:, 1], s=30, c=Y, cmap=plt.cm.Paired)
