98f9774cd160498feffa4ef9a8584ee469ffe16e
Jan Hendrik Metzen <jhm@informatik.uni-bremen.de>
2015-02-28 18:53:55 +0100

    DOC Revising CO2 example

--- examples/gaussian_process/new/plot_gpr_co2.py
+++ examples/gaussian_process/new/plot_gpr_co2.py
@@ -19,23 +19,37 @@ X = np.array(data.time)[:, np.newaxis]
 y = np.array(data.co2)
 y_mean = y.mean()
 
+# Kernel with parameters given in GPML book
+k1 = 66.0**2 * RBF(67.0)  # long term smooth rising trend
+k2 = 2.4**2 * RBF(90) * ExpSineSquared(((1.3,), (1.0,)))  # seasonal component
+k3 = 0.66**2 * RationalQuadratic(((0.78,), (1.2,)))  # medium term irregularit.
+k4 = 0.18**2 * RBF(0.134) + WhiteKernel(0.19**2) # noise terms
+kernel_gpml = k1 + k2 + k3 + k4
 
-k1 = 66.0**2 * RBF(67.0)
-k2 = 2.4**2 * RBF(90) * ExpSineSquared(((1.3,), (1.0,)))
-k3 = 0.66**2 * RationalQuadratic(((0.78,), (1.2,)))
-k4 = 0.18**2 * RBF(0.134) + WhiteKernel((0, 0.19**2, None))
-kernel = k1 + k2 + k3 + k4
-
-gp = GaussianProcessRegressor(kernel=kernel, y_err=1e-2)
+gp = GaussianProcessRegressor(kernel=kernel_gpml, y_err=0, optimizer=None)
 gp.fit(X, y - y_mean)
 
-print "Learned kernel: %s" % kernel
+print "GPML kernel: %s" % kernel_gpml
+print "Log-marginal-likelihood: %.3f" % gp.log_marginal_likelihood(gp.theta_)
+
+# Kernel with optimized parameters
+k1 = 50.0**2 * RBF(50.0) # long term smooth rising trend
+k2 = 2.0**2 * RBF(100) * ExpSineSquared(((1.0,), (1.0,))) # seasonal component
+k3 = 0.5**2 * RationalQuadratic(((1.0,), (1.0,))) # medium term irregularities
+k4 = 0.1**2 * RBF(0.1) + WhiteKernel((1e-3, 0.1**2, np.inf))  # noise terms
+kernel = k1 + k2 + k3 + k4
+
+gp = GaussianProcessRegressor(kernel=kernel, y_err=0)
+gp.fit(X, y - y_mean)
+
+print "\nLearned kernel: %s" % kernel
 print "Log-marginal-likelihood: %.3f" % gp.log_marginal_likelihood(gp.theta_)
 
 X_ = np.linspace(X.min(), X.max() + 30, 1000)[:, np.newaxis]
 y_pred, y_std = gp.predict(X_, return_std=True)
 y_pred += y_mean
 
+# Illustration
 plt.scatter(X, y, c='k')
 plt.plot(X_, y_pred)
 plt.fill_between(X_[:, 0], y_pred - y_std, y_pred + y_std,
