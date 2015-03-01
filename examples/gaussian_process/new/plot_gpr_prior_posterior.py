ac5f635cee600863b780670b7ac2d096612f025b
Jan Hendrik Metzen <jhm@informatik.uni-bremen.de>
2015-03-01 14:44:41 +0100

    DOC Illustrating different kernels in plot_gpr_prior_posterior.py

--- examples/gaussian_process/new/plot_gpr_prior_posterior.py
+++ examples/gaussian_process/new/plot_gpr_prior_posterior.py
@@ -1,7 +1,8 @@
 """Gaussian process regression (GPR) prior and posterior
 
-This example illustrates the prior and posterior of a GPR. Mean, standard
-deviation, and 10 samples are shown for both prior and posterior.
+This example illustrates the prior and posterior of a GPR with different
+kernels. Mean, standard deviation, and 10 samples are shown for both prior
+and posterior.
 """
 print __doc__
 
@@ -14,48 +15,57 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF
+from sklearn.gaussian_process.kernels \
+    import RBF, RationalQuadratic, ExpSineSquared, DotProduct
 
 
-# Specify Gaussian Process
-kernel = 1.0 * RBF((1e-10, 1.0, None))
-gp = GaussianProcessRegressor(kernel=kernel)
+kernels = [1.0 * RBF((1e-10, 1.0, None)),
+           1.0 * RationalQuadratic(((0.1,), (1.0,))),
+           1.0 * ExpSineSquared(((0.1, 1.0, 10.0), (1.0, 3.0, 10.0))),
+           (0.01, 0.1, 10.0) * DotProduct(((0.0, 1.0, 10.0)), degree=2)]
 
-# Plot prior
-plt.figure(0, figsize=(8, 8))
-plt.subplot(2, 1, 1)
-X_ = np.linspace(0, 5, 100)
-y_mean, y_cov = gp.predict(X_[:, np.newaxis], return_cov=True)
-plt.plot(X_, y_mean, 'k', lw=3, zorder=9)
-plt.fill_between(X_, y_mean - np.sqrt(np.diag(y_cov)),
-                 y_mean + np.sqrt(np.diag(y_cov)),
-                 alpha=0.5, color='k')
-y_samples = gp.sample(X_[:, np.newaxis], 10)
-plt.plot(X_, y_samples, color='b', lw=1)
-plt.xlim(0, 5)
-plt.ylim(-3, 3)
-plt.title("Prior, kernel:  %s" % kernel)
+for fig_index, kernel in enumerate(kernels):
+    if fig_index > 3: continue
 
-# Generate data and fit GP
-rng = np.random.RandomState(4)
-X = rng.uniform(0, 5, 10)[:, np.newaxis]
-y = np.sin((X[:, 0] - 2.5) ** 2)
-gp.fit(X, y)
+    # Specify Gaussian Process
+    gp = GaussianProcessRegressor(kernel=kernel)
 
-# Plot posterior
-plt.subplot(2, 1, 2)
-X_ = np.linspace(0, 5, 100)
-y_mean, y_cov = gp.predict(X_[:, np.newaxis], return_cov=True)
-plt.plot(X_, y_mean, 'k', lw=3, zorder=9)
-plt.fill_between(X_, y_mean - np.sqrt(np.diag(y_cov)),
-                 y_mean + np.sqrt(np.diag(y_cov)),
-                 alpha=0.5, color='k')
+    # Plot prior
+    plt.figure(fig_index, figsize=(8, 8))
+    plt.subplot(2, 1, 1)
+    X_ = np.linspace(0, 5, 100)
+    y_mean, y_cov = gp.predict(X_[:, np.newaxis], return_cov=True)
+    plt.plot(X_, y_mean, 'k', lw=3, zorder=9)
+    plt.fill_between(X_, y_mean - np.sqrt(np.diag(y_cov)),
+                     y_mean + np.sqrt(np.diag(y_cov)),
+                     alpha=0.5, color='k')
+    y_samples = gp.sample(X_[:, np.newaxis], 10)
+    plt.plot(X_, y_samples, color='b', lw=1)
+    plt.xlim(0, 5)
+    plt.ylim(-3, 3)
+    plt.title("Prior (kernel:  %s)" % kernel)
 
-y_samples = gp.sample(X_[:, np.newaxis], 10)
-plt.plot(X_, y_samples, color='b', lw=1)
-plt.scatter(X[:, 0], y, c='r', s=50, zorder=10)
-plt.xlim(0, 5)
-plt.ylim(-3, 3)
-plt.title("Posterior, kernel: %s" % kernel)
-plt.tight_layout()
+    # Generate data and fit GP
+    rng = np.random.RandomState(4)
+    X = rng.uniform(0, 5, 10)[:, np.newaxis]
+    y = np.sin((X[:, 0] - 2.5) ** 2)
+    gp.fit(X, y)
+
+    # Plot posterior
+    plt.subplot(2, 1, 2)
+    X_ = np.linspace(0, 5, 100)
+    y_mean, y_cov = gp.predict(X_[:, np.newaxis], return_cov=True)
+    plt.plot(X_, y_mean, 'k', lw=3, zorder=9)
+    plt.fill_between(X_, y_mean - np.sqrt(np.diag(y_cov)),
+                     y_mean + np.sqrt(np.diag(y_cov)),
+                     alpha=0.5, color='k')
+
+    y_samples = gp.sample(X_[:, np.newaxis], 10)
+    plt.plot(X_, y_samples, color='b', lw=1)
+    plt.scatter(X[:, 0], y, c='r', s=50, zorder=10)
+    plt.xlim(0, 5)
+    plt.ylim(-3, 3)
+    plt.title("Posterior (kernel: %s)" % kernel)
+    plt.tight_layout()
+
 plt.show()
