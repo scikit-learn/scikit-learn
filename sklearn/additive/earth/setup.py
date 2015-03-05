b193076f7a510f61a7a70dba29360a82cb902af0
Mehdi Cherti <mehdi@cherti.name>
2015-03-05 02:48:51 +0100

    change package name in setup.py; update C files from pyx files

--- sklearn/additive/earth/setup.py
+++ sklearn/additive/earth/setup.py
@@ -3,7 +3,7 @@ from numpy.distutils.misc_util import Configuration
 
 
 def configuration(parent_package="", top_path=None):
-    config = Configuration("additive", parent_package, top_path)
+    config = Configuration("earth", parent_package, top_path)
     config.add_extension("_basis",
                          sources=["_basis.pyx"],
                          include_dirs=[numpy.get_include()])
