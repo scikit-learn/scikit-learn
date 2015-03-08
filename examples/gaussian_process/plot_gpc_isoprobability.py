6a0b32ee6a3915a9d0ce7a2b8976c1da0f544f5f
Jan Hendrik Metzen <jhm@informatik.uni-bremen.de>
2015-03-08 18:29:45 +0100

    DOC Reorganizing gaussian_process examples

--- examples/gaussian_process/plot_gpc_isoprobability.py
+++ examples/gaussian_process/plot_gpc_isoprobability.py
@@ -2,37 +2,30 @@
 # -*- coding: utf-8 -*-
 
 """
-==============================================================================
-Gaussian Processes classification example: exploiting the probabilistic output
-==============================================================================
+===================================================================
+Gaussian Processes classification example with probabilistic output
+===================================================================
 
-A two-dimensional regression exercise with a post-processing allowing for
-probabilistic classification thanks to the Gaussian property of the prediction.
-
-The figure illustrates the probability that the prediction is negative with
-respect to the remaining uncertainty in the prediction. The red and blue lines
-corresponds to the 95% confidence interval on the prediction of the zero level
-set.
+A two-dimensional classification exampe showing iso-probability lines for
+the predicted probabilities.
 """
 print(__doc__)
 
 # Author: Vincent Dubourg <vincent.dubourg@gmail.com>
+# Adapted to GaussianProcessClassifier:
+#         Jan Hendrik Metzen <jhm@informatik.uni-bremen.de>
 # Licence: BSD 3 clause
 
 import numpy as np
-from scipy import stats
-from sklearn.gaussian_process import GaussianProcess
+
 from matplotlib import pyplot as pl
 from matplotlib import cm
 
-# Standard normal distribution functions
-phi = stats.distributions.norm().pdf
-PHI = stats.distributions.norm().cdf
-PHIinv = stats.distributions.norm().ppf
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import DotProduct
 
 # A few constants
 lim = 8
-
 
 def g(x):
     """The function to predict (classification will then consist in predicting
@@ -50,32 +43,28 @@ X = np.array([[-4.61611719, -6.00099547],
               [5.21301203, 4.26386883]])
 
 # Observations
-y = g(X)
+y = np.array(g(X) > 0, dtype=int)
 
 # Instanciate and fit Gaussian Process Model
-gp = GaussianProcess(theta0=5e-1)
-
-# Don't perform MLE or you'll get a perfect prediction for this simple example!
+kernel = 0.1 * DotProduct(0.1) ** 2
+gp = GaussianProcessClassifier(kernel=kernel)
 gp.fit(X, y)
+print "Learned kernel: %s " % gp.kernel_
 
-# Evaluate real function, the prediction and its MSE on a grid
+# Evaluate real function and the predicted probability
 res = 50
 x1, x2 = np.meshgrid(np.linspace(- lim, lim, res),
                      np.linspace(- lim, lim, res))
 xx = np.vstack([x1.reshape(x1.size), x2.reshape(x2.size)]).T
 
 y_true = g(xx)
-y_pred, MSE = gp.predict(xx, eval_MSE=True)
-sigma = np.sqrt(MSE)
+y_prob = gp.predict_proba(xx)
 y_true = y_true.reshape((res, res))
-y_pred = y_pred.reshape((res, res))
-sigma = sigma.reshape((res, res))
-k = PHIinv(.975)
+y_prob = y_prob.reshape((res, res))
 
-# Plot the probabilistic classification iso-values using the Gaussian property
-# of the prediction
+# Plot the probabilistic classification iso-values
 fig = pl.figure(1)
-ax = fig.add_subplot(111)
+ax = fig.gca()
 ax.axes.set_aspect('equal')
 pl.xticks([])
 pl.yticks([])
@@ -84,11 +73,12 @@ ax.set_yticklabels([])
 pl.xlabel('$x_1$')
 pl.ylabel('$x_2$')
 
-cax = pl.imshow(np.flipud(PHI(- y_pred / sigma)), cmap=cm.gray_r, alpha=0.8,
-                extent=(- lim, lim, - lim, lim))
+cax = pl.imshow(y_prob, cmap=cm.gray_r, alpha=0.8,
+                extent=(-lim, lim, -lim, lim))
 norm = pl.matplotlib.colors.Normalize(vmin=0., vmax=0.9)
 cb = pl.colorbar(cax, ticks=[0., 0.2, 0.4, 0.6, 0.8, 1.], norm=norm)
 cb.set_label('${\\rm \mathbb{P}}\left[\widehat{G}(\mathbf{x}) \leq 0\\right]$')
+pl.clim(0, 1)
 
 pl.plot(X[y <= 0, 0], X[y <= 0, 1], 'r.', markersize=12)
 
@@ -96,15 +86,15 @@ pl.plot(X[y > 0, 0], X[y > 0, 1], 'b.', markersize=12)
 
 cs = pl.contour(x1, x2, y_true, [0.], colors='k', linestyles='dashdot')
 
-cs = pl.contour(x1, x2, PHI(- y_pred / sigma), [0.025], colors='b',
+cs = pl.contour(x1, x2, y_prob, [0.666], colors='b',
                 linestyles='solid')
 pl.clabel(cs, fontsize=11)
 
-cs = pl.contour(x1, x2, PHI(- y_pred / sigma), [0.5], colors='k',
+cs = pl.contour(x1, x2, y_prob, [0.5], colors='k',
                 linestyles='dashed')
 pl.clabel(cs, fontsize=11)
 
-cs = pl.contour(x1, x2, PHI(- y_pred / sigma), [0.975], colors='r',
+cs = pl.contour(x1, x2, y_prob, [0.334], colors='r',
                 linestyles='solid')
 pl.clabel(cs, fontsize=11)
 
