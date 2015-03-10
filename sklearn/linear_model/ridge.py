49b0e04f296c407168be14c38ae17ac608124b52
donbeo <lucapuggio@gmail.com>
2015-03-10 21:18:22 +0000

    redefinition of alphas is removed from init and added to fit method

--- sklearn/linear_model/ridge.py
+++ sklearn/linear_model/ridge.py
@@ -847,10 +847,10 @@ class _BaseRidgeCV(LinearModel):
         -------
         self : Returns self.
         """
-        self.alphas = np.asarray(self.alphas)
+        alphas = np.asarray(self.alphas)
 
         if self.cv is None:
-            estimator = _RidgeGCV(self.alphas,
+            estimator = _RidgeGCV(alphas,
                                   fit_intercept=self.fit_intercept,
                                   normalize=self.normalize,
                                   scoring=self.scoring,
@@ -866,7 +866,7 @@ class _BaseRidgeCV(LinearModel):
             if self.store_cv_values:
                 raise ValueError("cv!=None and store_cv_values=True "
                                  " are incompatible")
-            parameters = {'alpha': self.alphas}
+            parameters = {'alpha': alphas}
             # FIXME: sample_weight must be split into training/validation data
             #        too!
             #fit_params = {'sample_weight' : sample_weight}
