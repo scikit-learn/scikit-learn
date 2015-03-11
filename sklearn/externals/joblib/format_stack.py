a135f7b960995e6daa2ec7fe287e16d2e1ab771c
donbeo <lucapuggio@gmail.com>
2015-03-11 12:30:41 +0000

    use tuples instead of mutable  default values

--- sklearn/externals/joblib/format_stack.py
+++ sklearn/externals/joblib/format_stack.py
@@ -18,7 +18,7 @@ Adapted from IPython's VerboseTB.
 #            2001 Nathaniel Gray
 # License: BSD 3 clause
 
-
+import collections
 import inspect
 import keyword
 import linecache
@@ -264,7 +264,13 @@ def format_records(records):   # , print_globals=False):
         # dotted names
         tokeneater.name_cont = False
 
-        def linereader(file=file, lnum=[lnum], getline=linecache.getline):
+        def linereader(file=file, lnum_loc=lnum, getline=linecache.getline):
+            
+            if isinstance(lnum_loc, collections.Iterable):
+                lnum = lnum_loc
+            else:
+                lnum = [lnum_loc]
+
             line = getline(file, lnum[0])
             lnum[0] += 1
             return line
