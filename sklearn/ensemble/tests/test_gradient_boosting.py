5018a44ccf7c2889698c3f19136b4920ffce21d7
Peter Prettenhofer <peter.prettenhofer@gmail.com>
2015-03-25 14:22:31 +0100

    add regression test case for issue 4447  - fix another test

--- sklearn/ensemble/tests/test_gradient_boosting.py
+++ sklearn/ensemble/tests/test_gradient_boosting.py
@@ -976,10 +976,25 @@ def test_non_uniform_weights_toy_edge_case_reg():
     y = [0, 0, 1, 0]
     # ignore the first 2 training samples by setting their weight to 0
     sample_weight = [0, 0, 1, 1]
-    for loss in ('ls', 'huber', 'lad', 'quantile'):
-        gb = GradientBoostingRegressor(n_estimators=5)
+    for loss in ('ls', 'lad', 'quantile'):  # not for huber
+        gb = GradientBoostingRegressor(learning_rate=1.0, n_estimators=1, loss=loss)
         gb.fit(X, y, sample_weight=sample_weight)
-        assert_true(gb.predict([[1, 0]])[0] > 0.5)
+        assert_true(gb.predict([[1, 0]])[0] > 0.5, loss)
+
+def test_non_uniform_weights_toy_min_weight_leaf():
+    """Regression test for https://github.com/scikit-learn/scikit-learn/issues/4447 """
+    X = [[1, 0],
+         [1, 0],
+         [1, 0],
+         [0, 1],
+        ]
+    y = [0, 0, 1, 0]
+    # ignore the first 2 training samples by setting their weight to 0
+    sample_weight = [0, 0, 1, 1]
+    gb = GradientBoostingRegressor(n_estimators=5, min_weight_fraction_leaf=0.1)
+    gb.fit(X, y, sample_weight=sample_weight)
+    assert_true(gb.predict([[1, 0]])[0] > 0.5)
+    assert_almost_equal(gb.estimators_[0,0].splitter.min_weight_leaf, 0.2)
 
 
 def test_non_uniform_weights_toy_edge_case_clf():
