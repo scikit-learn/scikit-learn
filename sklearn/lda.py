440cb9d073a10ad95bc1c3b3a54713593613ab6d
Vinayak Mehta <vmehta94@gmail.com>
2015-04-06 06:19:13 +0530

    Removed class definition

--- sklearn/lda.py
+++ sklearn/lda.py
@@ -4,12 +4,4 @@ warnings.warn("lda.LDA has been moved to "
               "discriminant_analysis.LinearDiscriminantAnalysis "
               "in 0.17 and will be removed in 0.19", DeprecationWarning)
 
-from .discriminant_analysis import LinearDiscriminantAnalysis
-
-
-class LDA(LinearDiscriminantAnalysis):
-    def __init__(self, solver='svd', shrinkage=None, priors=None,
-                 n_components=None, store_covariance=False, tol=1e-4):
-        super(LDA, self).__init__(solver=solver, shrinkage=shrinkage, priors=priors,
-                                  n_components=n_components, store_covariance=store_covariance,
-                                  tol=tol)
+from .discriminant_analysis import LinearDiscriminantAnalysis as LDA
--- sklearn/qda.py
+++ sklearn/qda.py
@@ -3,8 +3,4 @@ warnings.warn("qda.QDA has been moved to "
               "discriminant_analysis.QuadraticDiscriminantAnalysis "
               "in 0.17 and will be removed in 0.19.", DeprecationWarning)
 
-from .discriminant_analysis import QuadraticDiscriminantAnalysis
-
-class QDA(QuadraticDiscriminantAnalysis):
-    def __init__(self, priors=None, reg_param=0.):
-        super(QDA, self).__init__(priors=priors, reg_param=reg_param)
+from .discriminant_analysis import QuadraticDiscriminantAnalysis as QDA
