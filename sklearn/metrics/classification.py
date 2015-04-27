c4bbb58ff595040f2d668b3362d55b3edd46a6a7
scls19fr <scls19fr@users.noreply.github.com>
2015-04-27 20:51:16 +0200

    Update confusion_matrix docstring
    Improve `confusion_matrix` docstring with labels parameters usage

--- sklearn/metrics/classification.py
+++ sklearn/metrics/classification.py
@@ -229,7 +229,7 @@ def confusion_matrix(y_true, y_pred, labels=None):
 
     >>> y_true = ["cat", "ant", "cat", "cat", "ant", "bird"]
     >>> y_pred = ["ant", "ant", "cat", "cat", "ant", "cat"]
-    >>> confusion_matrix(y_true, y_pred, labels=["ant","bird","cat"])
+    >>> confusion_matrix(y_true, y_pred, labels=["ant", "bird", "cat"])
     array([[2, 0, 0],
            [0, 0, 1],
            [1, 0, 2]])
