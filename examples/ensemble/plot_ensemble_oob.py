b6ce3226f75d5a6aeaff81b5453838276257f40d
Kian Ho <hui.kian.ho@googlemail.com>
2015-05-05 14:32:33 +1000

    Amended plot_ensemble_oob.py according to #4665.
    @amueller provided feedback on improving my original PR (#4665) of the
    plot_ensemble_oob.py script.
    
    A number of major changes were made accordingly:
    
    - Used `matplotlib.pyplot` instead of `pylab`.
    
    - To improve the run-time to <10secs, I reduced the dimensionality of
      the sample dataset and set the max. number of estimators to 150.
    
    - To avoid OOB warnings, the min. number of estimators was set to 15.
      Values <15 would still raise the warnings.
    
    - The script is PEP8-compliant via the `pep8` command-line script. I
      needed to move `print(__doc__)` and author list comments.
    
    - Re-added @amueller to the author list (had mistakenly been removed).
    
    - Added a link to this example to the user-guide under the `Ensemble
      Methods` section.

--- examples/ensemble/plot_ensemble_oob.py
+++ examples/ensemble/plot_ensemble_oob.py
@@ -1,5 +1,4 @@
 """
-
 =========================================================
 OOB Errors for Random Forests and Extra Trees Classifiers
 =========================================================
@@ -21,69 +20,72 @@ can be halted.
        Learning Ed. 2", Springer, 2009.
 
 """
-print(__doc__)
-
-# Author: Kian Ho <hui.kian.ho@gmail.com>
-#         Gilles Louppe <g.louppe@gmail.com>
-#         
-# License: BSD 3 Clause
-
-import pylab
+import matplotlib.pyplot as plt
 
 from collections import OrderedDict
 from sklearn.datasets import make_classification
 from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
 
-RANDOM_STATE = 12345
+# Author: Kian Ho <hui.kian.ho@gmail.com>
+#         Gilles Louppe <g.louppe@gmail.com>
+#         Andreas Mueller <amueller@ais.uni-bonn.de>
+#
+# License: BSD 3 Clause
 
-# Generate a binary classification dataset. 
-N_SAMPLES = 300
-N_FEATURES = 50
+print(__doc__)
 
-X, y = make_classification(n_samples=N_SAMPLES, n_features=N_FEATURES, 
+RANDOM_STATE = 123
+
+# Generate a binary classification dataset.
+X, y = make_classification(n_samples=500, n_features=30,
+                           n_clusters_per_class=1,
                            random_state=RANDOM_STATE)
 
 # NOTE: Setting the `warm_start` construction parameter to `True` disables
 # support for paralellised ensembles but is necessary for tracking the OOB
 # error trajectory during training.
-clfs = [
-    ("RandomForestClassifier (max_features='auto')",
+ensemble_clfs = [
+    ("RandomForestClassifier, max_features='auto'",
         RandomForestClassifier(warm_start=True, oob_score=True,
                                max_features="auto",
                                random_state=RANDOM_STATE)),
-    ("RandomForestClassifier (max_features=2)",
-        RandomForestClassifier(warm_start=True, max_features=2, 
-                               oob_score=True, random_state=RANDOM_STATE)),
-    ("ExtraTreesClassifier (max_features='auto')",
+    ("RandomForestClassifier, max_features=2",
+        RandomForestClassifier(warm_start=True, max_features=2,
+                               oob_score=True,
+                               random_state=RANDOM_STATE)),
+    ("ExtraTreesClassifier, max_features='auto'",
         ExtraTreesClassifier(warm_start=True, max_features="auto",
                              oob_score=True, bootstrap=True,
                              random_state=RANDOM_STATE)),
-    ("ExtraTreesClassifier (max_features=2)",
+    ("ExtraTreesClassifier, max_features=2",
         ExtraTreesClassifier(warm_start=True, max_features=2,
                              oob_score=True, bootstrap=True,
                              random_state=RANDOM_STATE))
 ]
 
 # Map a classifier name to a list of (<n_estimators>, <error rate>) pairs.
-error_rate = OrderedDict((label, []) for label, _ in clfs)
+error_rate = OrderedDict((label, []) for label, _ in ensemble_clfs)
 
-# Max no. of estimators to use in each ensemble.
-n_estimators = 400
+# Range of `n_estimators` values to explore.
+min_estimators = 15
+max_estimators = 150
 
-for label, clf in clfs:
-    for i in range(1, n_estimators + 1):
+for label, clf in ensemble_clfs:
+    for i in range(min_estimators, max_estimators + 1):
         clf.set_params(n_estimators=i)
         clf.fit(X, y)
-        
-        # Record the <error rate> for each <n_estimators> setting. 
-        error_rate[label].append((i, 1 - clf.oob_score_))
-    
-# Generate the "OOB error rate" vs. "no. of estimators" plot
+
+        # Record the OOB error for each `n_estimators=i` setting.
+        oob_error = 1 - clf.oob_score_
+        error_rate[label].append((i, oob_error))
+
+# Generate the "OOB error rate" vs. "n_estimators" plot.
 for label, clf_err in error_rate.items():
     xs, ys = zip(*clf_err)
-    pylab.plot(xs, ys, label=label)
-    
-pylab.xlabel("n_estimators")
-pylab.ylabel("OOB error rate")
-pylab.legend(loc="upper right")
-pylab.show()
+    plt.plot(xs, ys, label=label)
+
+plt.xlim(min_estimators, max_estimators)
+plt.xlabel("n_estimators")
+plt.ylabel("OOB error rate")
+plt.legend(loc="upper right")
+plt.show()
