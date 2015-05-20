20c622f81973c9a8393024d7ec264fe71e587d1f
Thomas Unterthiner <thomas.unterthiner@gmx.net>
2015-05-20 22:05:22 +0200

    Improved example

--- examples/preprocessing/plot_robust_scaling.py
+++ examples/preprocessing/plot_robust_scaling.py
@@ -46,24 +46,28 @@ X_train[0, 0] = -1000  # a fairly large outlier
 
 
 # Scale data
-s = StandardScaler()
-Xtr_s = s.fit_transform(X_train)
-Xte_s = s.transform(X_test)
+standard_scaler = StandardScaler()
+Xtr_s = standard_scaler.fit_transform(X_train)
+Xte_s = standard_scaler.transform(X_test)
 
-r = RobustScaler()
-Xtr_r = r.fit_transform(X_train)
-Xte_r = r.fit_transform(X_test)
+robust_scaler = RobustScaler()
+Xtr_r = robust_scaler.fit_transform(X_train)
+Xte_r = robust_scaler.fit_transform(X_test)
 
 
-# Plot data (note we zoom in to the data center, so the outlier
-# data point can't be seen in this plot).
-fig, ax = plt.subplots(1, 2, figsize=(8, 4), sharex=True, sharey=True)
-ax[0].scatter(Xtr_s[:, 0], Xtr_s[:, 1], color=np.where(Y_train > 0, 'r', 'b'))
-ax[1].scatter(Xtr_r[:, 0], Xtr_r[:, 1], color=np.where(Y_train > 0, 'r', 'b'))
-ax[0].set_title("Training set after standard scaling")
-ax[1].set_title("Training set after robust scaling")
-ax[0].set_xlim(-3, 3)
-ax[0].set_ylim(-3, 3)
+# Plot data
+fig, ax = plt.subplots(1, 3, figsize=(12, 4))
+ax[0].scatter(X_train[:, 0], X_train[:, 1],
+              color=np.where(Y_train > 0, 'r', 'b'))
+ax[1].scatter(Xtr_s[:, 0], Xtr_s[:, 1], color=np.where(Y_train > 0, 'r', 'b'))
+ax[2].scatter(Xtr_r[:, 0], Xtr_r[:, 1], color=np.where(Y_train > 0, 'r', 'b'))
+ax[0].set_title("Unscaled data")
+ax[1].set_title("After standard scaling (zoomed in)")
+ax[2].set_title("After robust scaling (zoomed in)")
+# for the scaled data, we zoom in to the data center (outlier can't be seen!)
+for a in ax[1:]:
+    a.set_xlim(-3, 3)
+    a.set_ylim(-3, 3)
 plt.tight_layout()
 plt.show()
 
