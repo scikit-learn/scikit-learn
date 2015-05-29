baf96e1862b3bc2a863b14929d31004a9d6e011b
B@rmaley.exe <blog@barmaley.exe.name>
2015-05-30 00:07:38 +0300

    Adds loopy NCA optimizer (without memory overhead)

--- sklearn/metric_learning/nca.py
+++ sklearn/metric_learning/nca.py
@@ -5,7 +5,7 @@ import scipy as sp
 import scipy.misc
 import scipy.optimize
 
-def vectorized_nca(X, y, params):
+def nca_vectorized_oracle(X, y, params):
     dX = X[:, None, :] - X[None, :, :]  # n_samples x n_samples x n_comp
     outer = dX[:, :, None, :] * dX[:, :, :, None]  # n_samples x n_samples x n_comp x n_comp
 
@@ -46,11 +46,72 @@ def vectorized_nca(X, y, params):
 
         return [-fnc, -grad.flatten()]
 
+    return oracle
+
+
+def nca_loopy_oracle(X, y, params, threshold=None):
+    n_samples, n_features = X.shape
+    n_components = params.n_components
+
+    if threshold is None:
+        threshold = 1e-3 / np.abs(X).max() ** 2
+
+    def oracle(L):
+        L = L.reshape((n_components, n_features))
+
+        Lx = np.dot(X, L.T)  # n_samples x n_comp
+        assert Lx.shape == (n_samples, n_components)
+
+        grad = np.zeros((n_features, n_features))
+        fnc = 0
+
+        for i in range(n_samples):
+            A = Lx[i, :] - Lx  # n_samples x n_comp
+            assert A.shape == (n_samples, n_components)
+
+            logp = -(A*A).sum(axis=2)  # n_samples
+            logp[i] = -np.inf
+            logp -= sp.misc.logsumexp(logp)
+            assert logp.shape == (n_samples, )
+
+            p = np.exp(logp)  # n_samples
+            assert p.shape == (n_samples, )
+
+            class_neighbours = y == y[i]
+            p_i = (p * class_neighbours).sum()
+
+            for j in range(n_samples):
+                a = p[j] * (p_i if params.loss == 'l1' else 1)
+                if a > threshold:
+                    Xij = X[i] - X[j]
+                    grad += a * np.outer(Xij, Xij)
+
+                b = p[j] / (1 if params.loss == 'l1' else p_i)
+                if b > threshold and y[j] == y[i]:
+                    Xij = X[i] - X[j]
+                    grad -= b * np.outer(Xij, Xij)
+
+            fnc += p_i if params.loss == 'l1' else np.log(p_i)
+
+        grad = 2 * np.dot(L, grad)
+
+        return [-fnc, -grad.flatten()]
+
+    return oracle
+
+def optimize_nca(X, y, params, cache_size=200*1024*1024):
     n_samples, n_features = X.shape
 
     rng = np.random.RandomState(params.random_state)
     best_fnc = None
     best_L = None
+
+    outer_products_size = (n_samples * n_features) ** 2
+    memory_permits = outer_products_size < cache_size
+    if memory_permits:
+        oracle = nca_vectorized_oracle(X, y, params)
+    else:
+        oracle = nca_loopy_oracle(X, y, params)
 
     for _ in range(params.n_init):
         L = rng.uniform(0, 1, (params.n_components, n_features))
@@ -94,7 +155,6 @@ def vectorized_nca(X, y, params):
     return best_L
 
 
-
 class BaseNCA(BaseEstimator):
     def __init__(self, n_components, method, tol, loss, learning_rate, max_iter, n_init, random_state, verbose):
         self.n_components = n_components
@@ -108,7 +168,7 @@ class BaseNCA(BaseEstimator):
         self.tol = tol
 
     def fit(self, X, y):
-        self.L = vectorized_nca(X, y, self)
+        self.L = optimize_nca(X, y, self)
         return self
 
     def transform(self, X):
