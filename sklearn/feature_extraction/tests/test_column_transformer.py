372b883261196592bab01f7074746c54eb030ef3
Andreas Mueller <amueller@nyu.edu>
2015-06-05 16:04:12 -0400

    more / better tests

--- sklearn/feature_extraction/tests/test_column_transformer.py
+++ sklearn/feature_extraction/tests/test_column_transformer.py
@@ -4,6 +4,7 @@ from sklearn.base import BaseEstimator
 from sklearn.feature_extraction import ColumnTransformer
 
 from sklearn.utils.testing import assert_array_equal
+from sklearn.utils.validation import check_array
 
 
 class Trans(BaseEstimator):
@@ -11,28 +12,52 @@ class Trans(BaseEstimator):
         return self
 
     def transform(self, X, y=None):
-        return X
+        return check_array(X).reshape(-1, 1)
 
 
-def test_fields():
+def test_column_selection():
     # dictionary
-    X_dict = {'first': [[0], [1], [2]],
-              'second': [[2], [4], [6]]}
+    X_dict = {'first': [0, 1, 2],
+              'second': [2, 4, 6]}
     # recarray
-    X_recarray = np.recarray((3, 1),
+    X_recarray = np.recarray((3,),
                              dtype=[('first', np.int), ('second', np.int)])
     X_recarray['first'] = X_dict['first']
     X_recarray['second'] = X_dict['second']
+    Xs = [X_dict, X_recarray]
 
-    for X in [X_dict, X_recarray]:
+    try:
+        import pandas as pd
+        X_df = pd.DataFrame(X_dict)
+        Xs.append(X_df)
+    except:
+        print("Pandas not found, not testing ColumnTransformer with"
+              " DataFrame.")
+    X_res_first = np.array(X_dict['first']).reshape(-1, 1)
+    X_res_second = np.array(X_dict['second']).reshape(-1, 1)
+    X_res_both = np.vstack([X_dict['first'], X_dict['second']]).T
+
+    for X in Xs:
         first_feat = ColumnTransformer({'trans': (Trans(), 'first')})
         second_feat = ColumnTransformer({'trans': (Trans(), 'second')})
         both = ColumnTransformer({'trans1': (Trans(), 'first'),
                                   'trans2': (Trans(), 'second')})
-        assert_array_equal(first_feat.fit_transform(X), X['first'])
-        assert_array_equal(second_feat.fit_transform(X), X['second'])
-        assert_array_equal(both.fit_transform(X), np.hstack([X['first'], X['second']]))
+        assert_array_equal(first_feat.fit_transform(X), X_res_first)
+        assert_array_equal(second_feat.fit_transform(X), X_res_second)
+        assert_array_equal(both.fit_transform(X), X_res_both)
         # fit then transform
-        assert_array_equal(first_feat.fit(X).transform(X), X['first'])
-        assert_array_equal(second_feat.fit(X).transform(X), X['second'])
-        assert_array_equal(both.fit(X).transform(X), np.hstack([X['first'], X['second']]))
+        assert_array_equal(first_feat.fit(X).transform(X), X_res_first)
+        assert_array_equal(second_feat.fit(X).transform(X), X_res_second)
+        assert_array_equal(both.fit(X).transform(X), X_res_both)
+
+    # test with transformer_weights
+    transformer_weights = {'trans1': .1, 'trans2': 10}
+    for X in Xs:
+        both = ColumnTransformer({'trans1': (Trans(), 'first'),
+                                  'trans2': (Trans(), 'second')},
+                                 transformer_weights=transformer_weights)
+        res = np.vstack([transformer_weights['trans1'] * np.array(X['first']),
+                         transformer_weights['trans2'] * np.array(X['second'])]).T
+        assert_array_equal(both.fit_transform(X), res)
+        # fit then transform
+        assert_array_equal(both.fit(X).transform(X), res)
