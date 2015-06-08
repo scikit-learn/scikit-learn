fix wrong negative sign in previous commit

sklearn/ensemble/tests/test_gradient_boosting.py
@@ -964,7 +964,7 @@ def test_probability_exponential():
     assert np.all(y_proba <= 1.0)
     score = clf.decision_function(T).ravel()
     assert_array_equal(y_proba[:, 1],
-                       expit(-2.0 * score))
+                       expit(2.0 * score))
 
     # derive predictions from probabilities
     y_pred = clf.classes_.take(y_proba.argmax(axis=1), axis=0)