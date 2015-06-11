add examples for knn imputation, fix error messages, add possible euclidean calculation method and add some tests

examples/missing_values.py
@@ -8,23 +8,24 @@ than discarding the samples containing any missing value.
 Imputing does not always improve the predictions, so please check via cross-validation.
 Sometimes dropping rows or using marker values is more effective.
 
-Missing values can be replaced by the mean, the median or the most frequent
-value using the ``strategy`` hyper-parameter.
+Missing values can be replaced by the mean, the median, the most frequent
+value or the mean of values of k-nearest neighbors using the ``strategy`` hyper-parameter.
 The median is a more robust estimator for data with high magnitude variables
 which could dominate results (otherwise known as a 'long tail').
 
 Script output::
 
-  Score with the entire dataset = 0.56
-  Score without the samples containing missing values = 0.48
-  Score after imputation of the missing values = 0.55
+  Score with the entire dataset = 0.43
+  Score without the samples containing missing values = 0.36
+  Score after mean imputation of the missing values = 0.42
+  Score after knn imputation with 10 neighbors of the missing values = 0.43
 
 In this case, imputing helps the classifier get close to the original score.
-  
+
 """
 import numpy as np
 
-from sklearn.datasets import load_boston
+from sklearn.datasets import load_diabetes
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import Imputer
@@ -32,7 +33,7 @@ from sklearn.cross_validation import cross_val_score
 
 rng = np.random.RandomState(0)
 
-dataset = load_boston()
+dataset = load_diabetes()
 X_full, y_full = dataset.data, dataset.target
 n_samples = X_full.shape[0]
 n_features = X_full.shape[1]
@@ -42,15 +43,18 @@ estimator = RandomForestRegressor(random_state=0, n_estimators=100)
 score = cross_val_score(estimator, X_full, y_full).mean()
 print("Score with the entire dataset = %.2f" % score)
 
-# Add missing values in 75% of the lines
-missing_rate = 0.75
+# Add missing values in 60% of the lines
+missing_rate = 0.60 # 60% of samples have missing value
+missing2_rate = 0.80 # 80% of samples with missing value have 2 missing features
 n_missing_samples = np.floor(n_samples * missing_rate)
+n_missing2_samples = np.floor(n_samples * missing_rate * missing2_rate)
 missing_samples = np.hstack((np.zeros(n_samples - n_missing_samples,
                                       dtype=np.bool),
                              np.ones(n_missing_samples,
                                      dtype=np.bool)))
 rng.shuffle(missing_samples)
 missing_features = rng.randint(0, n_features, n_missing_samples)
+missing2_features = rng.randint(0, n_features, n_missing2_samples)
 
 # Estimate the score without the lines containing missing values
 X_filtered = X_full[~missing_samples, :]
@@ -59,14 +63,25 @@ estimator = RandomForestRegressor(random_state=0, n_estimators=100)
 score = cross_val_score(estimator, X_filtered, y_filtered).mean()
 print("Score without the samples containing missing values = %.2f" % score)
 
-# Estimate the score after imputation of the missing values
+# Estimate the score after mean imputation of the missing values
+missing_index = np.where(missing_samples)[0]
+missing2_index = np.random.choice(missing_index, n_missing2_samples)
 X_missing = X_full.copy()
-X_missing[np.where(missing_samples)[0], missing_features] = 0
+X_missing[np.where(missing_samples)[0], missing_features] = np.nan
+X_missing[missing2_index, missing2_features] = np.nan
 y_missing = y_full.copy()
-estimator = Pipeline([("imputer", Imputer(missing_values=0,
-                                          strategy="mean",
+estimator = Pipeline([("imputer", Imputer(strategy="mean",
                                           axis=0)),
                       ("forest", RandomForestRegressor(random_state=0,
                                                        n_estimators=100))])
 score = cross_val_score(estimator, X_missing, y_missing).mean()
-print("Score after imputation of the missing values = %.2f" % score)
+print("Score after mean imputation of the missing values = %.2f" % score)
+
+# Estimate the score after knn imputation of the missing values
+neigh = 7
+estimator2 = Pipeline([("imputer", Imputer(strategy="knn",
+                                           axis=0, n_neighbors=neigh)),
+                      ("forest", RandomForestRegressor(random_state=0,
+                                                       n_estimators=100))])
+score = cross_val_score(estimator2, X_missing, y_missing).mean()
+print("Score after knn imputation with %d neighbors of the missing values = %.2f" % (neigh, score))

sklearn/preprocessing/imputation.py
@@ -106,7 +106,7 @@ class Imputer(BaseEstimator, TransformerMixin):
         - If `axis=0` and X is encoded as a CSR matrix;
         - If `axis=1` and X is encoded as a CSC matrix.
 
-    kneighbor : int, optional (default=1)
+    n_neighbors : int, optional (default=1)
         It only has effect if the strategy is "knn". It controls the number of nearest
         neighbors used to compute the mean along the axis.
 
@@ -124,13 +124,13 @@ class Imputer(BaseEstimator, TransformerMixin):
       contain missing values).
     """
     def __init__(self, missing_values="NaN", strategy="mean",
-                 axis=0, verbose=0, copy=True, kneighbor=1):
+                 axis=0, verbose=0, copy=True, n_neighbors=1):
         self.missing_values = missing_values
         self.strategy = strategy
         self.axis = axis
         self.verbose = verbose
         self.copy = copy
-        self.kneighbor = kneighbor
+        self.n_neighbors = n_neighbors
 
     def fit(self, X, y=None):
         """Fit the imputer on X.
@@ -258,7 +258,7 @@ class Imputer(BaseEstimator, TransformerMixin):
                 return most_frequent
 
             elif strategy == "knn":
-                raise ValueError("Sparse matrix not supported!")
+                raise ValueError("strategy='knn' does not support sparse matrix input")
 
 
     def _dense_fit(self, X, strategy, missing_values, axis):
@@ -320,9 +320,9 @@ class Imputer(BaseEstimator, TransformerMixin):
 
             full_data = X[np.logical_not(mask.any(1))]
             if full_data.size == 0:
-                raise ValueError("There is no row with complete data!")
-            if full_data.shape[0] < self.kneighbor:
-                raise ValueError("There are at most %d neighbors!" %(full_data.shape[0]))
+                raise ValueError("There is no sample with complete data.")
+            if full_data.shape[0] < self.n_neighbors:
+                raise ValueError("There are only %d complete samples, but n_neighbors=%d." %(full_data.shape[0], self.n_neighbors))
             if axis == 1:
                 full_data = full_data.transpose()
 
@@ -404,18 +404,29 @@ class Imputer(BaseEstimator, TransformerMixin):
                     mask = mask.transpose()
                     statistics = statistics.transpose()
                 missing_index = np.where(mask.any(1))[0]
-                for i, row in zip(missing_index, X[missing_index]):
-                    col_index = np.where(np.logical_not(np.isnan(row)))[0]
-                    impute_index = np.where(np.isnan(row))[0]
-                    neigh = NearestNeighbors(self.kneighbor)
-                    neigh = neigh.fit(statistics[:, col_index])
-                    _dist, ind = neigh.kneighbors(row[np.logical_not(np.isnan(row))],
-                                           self.kneighbor)
-                    #tree = KDTree(statistics[:, col_index])
-                    #dist, ind = tree.query(row[np.logical_not(np.isnan(row))],
-                    #                       k=self.kneighbor)
-                    nn_index = ind[0]
-                    X[i][impute_index] = statistics[nn_index][:, impute_index].mean(0)
+                if True:
+                    for i, row in zip(missing_index, X[missing_index]):
+                        col_na_mask = np.isnan(row)
+                        col_full_mask = np.logical_not(col_na_mask)
+                        col_index = np.where(col_full_mask)[0]
+                        impute_index = np.where(col_na_mask)[0]
+                        neigh = NearestNeighbors(self.n_neighbors)
+                        neigh = neigh.fit(statistics[:, col_index])
+                        _dist, ind = neigh.kneighbors(row[col_full_mask],
+                                                       self.n_neighbors)
+                        nn_index = ind[0]
+                        X[i][impute_index] = statistics[nn_index][:, impute_index].mean(0)
+                else:
+
+                    #@jnothman 's method
+
+                    D2 = (X[missing_index, np.newaxis] - statistics) ** 2
+                    D2[np.isnan(D2)] = 0
+                    missing_row, missing_col = np.where(np.isnan(X))
+                    sqdist = D2.sum(axis=2)
+                    ind = np.argsort(sqdist, axis=1)[:, :self.n_neighbors]
+                    means = np.mean(statistics[ind], axis=1)
+                    X[missing_row, missing_col] = means[np.where(np.isnan(X[missing_index]))[0], missing_col]
 
                 if self.axis == 1:
                     X = X.transpose()

sklearn/preprocessing/tests/test_imputation.py
@@ -16,7 +16,7 @@ from sklearn.random_projection import sparse_random_matrix
 
 
 def _check_statistics(X, X_true,
-                      strategy, statistics, missing_values, kneighbor=1):
+                      strategy, statistics, missing_values):
     """Utility function for testing imputation for a given strategy.
 
     Test:
@@ -370,6 +370,13 @@ def test_imputation_knn():
         [-1,  2,  3,  np.nan],
     ])
 
+    X4 = np.array([
+        [np.nan, -1,  0,  5],
+        [np.nan,  2, -1,  3],
+        [-1,  -1,  0, 5],
+        [0,  2,  -1,  6],
+    ])
+
     X_true_1 = np.array([
         [-1, -1,  0,  5],
         [0,  2, -1,  3],
@@ -384,28 +391,39 @@ def test_imputation_knn():
         [-1,  2,  3,  7],
     ])
 
+    X_true_4 = np.array([
+        [-1, -1,  0,  5],
+        [0,  2, -1,  3],
+        [-1,  -1,  0, 5],
+        [0,  2, -1,  6],
+    ])
 
-    imputer = Imputer(missing_values='NaN', strategy="knn", axis=0, kneighbor=1)
+    imputer = Imputer(missing_values='NaN', strategy="knn", axis=0, n_neighbors=1)
     X_impute = imputer.fit(X).transform(X)
     assert_array_equal(X_true_1, X_impute)
 
-    imputer = Imputer(missing_values='NaN', strategy="knn", axis=1, kneighbor=1)
+    imputer = Imputer(missing_values='NaN', strategy="knn", axis=1, n_neighbors=1)
     X_impute = imputer.fit(X.transpose()).transform(X.transpose())
     assert_array_equal(X_true_1.transpose(), X_impute)
 
-    imputer = Imputer(missing_values='NaN', strategy="knn", axis=0, kneighbor=2)
+    imputer = Imputer(missing_values='NaN', strategy="knn", axis=0, n_neighbors=2)
     X_impute = imputer.fit(X).transform(X)
     assert_array_equal(X_true_2, X_impute)
 
-    imputer = Imputer(missing_values='NaN', strategy="knn", axis=0, kneighbor=1)
+    imputer = Imputer(missing_values='NaN', strategy="knn", axis=0, n_neighbors=1)
     X_impute = imputer.fit(X2).transform(X2)
     assert_array_equal(X_true_1, X_impute)
 
+    imputer = Imputer(missing_values='NaN', strategy="knn", axis=0, n_neighbors=1)
+    X_impute = imputer.fit(X4).transform(X4)
+    assert_array_equal(X_true_4, X_impute)
+
     imputer = Imputer(missing_values='NaN', strategy="knn", axis=0)
-    msg = "There is no row with complete data!"
+    msg = "There is no sample with complete data."
     assert_raise_message(ValueError, msg, imputer.fit, X3)
 
-    imputer = Imputer(missing_values='NaN', strategy="knn", axis=0, kneighbor=4)
-    msg = "There are at most 3 neighbors!"
+    imputer = Imputer(missing_values='NaN', strategy="knn", axis=0, n_neighbors=4)
+    msg = "There are only 3 complete samples, but n_neighbors=4."
     assert_raise_message(ValueError, msg, imputer.fit, X)
+
 