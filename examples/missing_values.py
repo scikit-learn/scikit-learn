85039ac769fd39bd351cd1e7344dcf3282a7188c
Tian Wang <tw991@nyu.edu>
2015-06-16 17:32:08 -0400

    change example/missing_value; using diabetes dataset, and use random matrix to create na. add comparision between knn and mean imputation

--- examples/missing_values.py
+++ examples/missing_values.py
@@ -18,7 +18,7 @@ Script output::
   Score with the entire dataset = 0.43
   Score without the samples containing missing values = 0.36
   Score after mean imputation of the missing values = 0.42
-  Score after knn imputation with 10 neighbors of the missing values = 0.43
+  Score after knn imputation with 7 neighbors of the missing values = 0.43
 
 In this case, imputing helps the classifier get close to the original score.
 
@@ -38,23 +38,19 @@ X_full, y_full = dataset.data, dataset.target
 n_samples = X_full.shape[0]
 n_features = X_full.shape[1]
 
+#Create a random matrix to randomly make missing values
+missing_matrix = np.random.rand(n_samples, n_features)
+th = 0.15  # each sample has (1-th)^n_features of probability to have full features
+mask = missing_matrix < th
+missing_samples = mask.any(1)
+full_percentage = (n_samples - missing_samples.sum())/float(n_samples)
+print("Percentage of samples with full features: %f" %full_percentage )
+
 # Estimate the score on the entire dataset, with no missing values
 estimator = RandomForestRegressor(random_state=0, n_estimators=100)
 score = cross_val_score(estimator, X_full, y_full).mean()
 print("Score with the entire dataset = %.2f" % score)
 
-# Add missing values in 60% of the lines
-missing_rate = 0.60 # 60% of samples have missing value
-missing2_rate = 0.80 # 80% of samples with missing value have 2 missing features
-n_missing_samples = np.floor(n_samples * missing_rate)
-n_missing2_samples = np.floor(n_samples * missing_rate * missing2_rate)
-missing_samples = np.hstack((np.zeros(n_samples - n_missing_samples,
-                                      dtype=np.bool),
-                             np.ones(n_missing_samples,
-                                     dtype=np.bool)))
-rng.shuffle(missing_samples)
-missing_features = rng.randint(0, n_features, n_missing_samples)
-missing2_features = rng.randint(0, n_features, n_missing2_samples)
 
 # Estimate the score without the lines containing missing values
 X_filtered = X_full[~missing_samples, :]
@@ -64,12 +60,11 @@ score = cross_val_score(estimator, X_filtered, y_filtered).mean()
 print("Score without the samples containing missing values = %.2f" % score)
 
 # Estimate the score after mean imputation of the missing values
-missing_index = np.where(missing_samples)[0]
-missing2_index = np.random.choice(missing_index, n_missing2_samples)
+
 X_missing = X_full.copy()
-X_missing[np.where(missing_samples)[0], missing_features] = np.nan
-X_missing[missing2_index, missing2_features] = np.nan
+X_missing[mask] = np.nan
 y_missing = y_full.copy()
+
 estimator = Pipeline([("imputer", Imputer(strategy="mean",
                                           axis=0)),
                       ("forest", RandomForestRegressor(random_state=0,
