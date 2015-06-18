247d0df2f0936e8a09c0674f3ed06b7c6abe2f64
hlin117 <hlin117@gmail.com>
2015-06-17 20:31:29 -0700

    changed documentation, can now discretize 1-d arrays

--- sklearn/preprocessing/discretization.py
+++ sklearn/preprocessing/discretization.py
@@ -12,28 +12,30 @@ from ..utils import check_array, check_consistent_length, column_or_1d
 
 class MDLP(BaseEstimator):
     """Implements the MDLP discretization criterion from Usama Fayyad's
-    paper "Multi-Interval Discretization of Continuous-Valued Attributes
-    for Classification Learning". Given the class labels for each sample,
-    this transformer attempts to discretize a continuous attribute by
-    minimizing the entropy at each interval.
+    paper "Multi-Interval Discretization of Continuous-Valued
+    Attributes for Classification Learning". Given the class labels
+    for each sample, this transformer attempts to discretize a
+    continuous attribute by minimizing the entropy at each interval.
 
     Attributes
     ----------
-    min_depth : The minimum depth of the interval splitting. Overrides the
-        MDLP stopping criterion. If the entropy at a given interval is
-        found to be zero before `min_depth`, the algorithm will stop.
+    min_depth : The minimum depth of the interval splitting. Overrides
+        the MDLP stopping criterion. If the entropy at a given interval
+        is found to be zero before `min_depth`, the algorithm will stop.
 
         Defaults to 0.
 
-    num_classes_ : The number of classes the discretizer was fit to in `y`.
-
     cut_points_ : A dictionary mapping indices to a numpy array. Each
-    numpy array is a sorted list of cut points found from discretization.
+        numpy array is a sorted list of cut points found from
+        discretization.
 
-    continuous_features_ : A list of indices indicating which columns should
-        be discretized.
+    dimensions_ : Describes whether `X` is a 2-D or 1-D array.
 
-        Defaults to `range(X.shape[1])` if not specified.
+    continuous_features_ : A list of indices indicating which columns
+        should be discretized.
+
+        If `X` is a 1-D array, then should be None. Otherwise, for a
+        2-D array, defaults to `range(X.shape[1])`.
 
     Examples
     --------
@@ -47,23 +49,25 @@ class MDLP(BaseEstimator):
     >>> conv_X = mdlp.fit_transform(X, y)
 
     `conv_X` will be the same shape as `X`, except it will contain
-    integers instead of continuous attributes representing the results of
-    the discretization process.
+    integers instead of continuous attributes representing the results
+    of the discretization process.
 
-    To retrieve the explicit intervals of the discretization of, say, the
-    third column (index 2), one can do
+    To retrieve the explicit intervals of the discretization of, say,
+    the third column (index 2), one can do
 
     >>> mdlp.cat2intervals(conv_X, 2)
 
-    which would return a list of tuples `(a, b)`. Each tuple represents the
-    contnuous interval (a, b], where `a` can be `float("-inf")`, and `b` can
-    be `float("inf")`.
+    which would return a list of tuples `(a, b)`. Each tuple represents
+    the contnuous interval (a, b], where `a` can be `float("-inf")`,
+    and `b` can be `float("inf")`.
     """
 
     def __init__(self, continuous_features=None, min_depth=0, shuffle=True):
         self.continuous_features_ = continuous_features
         self.min_depth = min_depth
         self.shuffle = shuffle
+        self.cut_points_ = None
+        self.dimensions_ = None
 
     def fit(self, X, y):
         """Finds the intervals of interest from the input data.
@@ -72,24 +76,38 @@ class MDLP(BaseEstimator):
         ----------
         X : The array containing features to be discretized. Continuous
             features should be specified by the `continuous_features`
-            attribute.
+            attribute if `X` is a 2-D array.
 
         y : A list or array of class labels corresponding to `X`.
         """
-        X = check_array(X, force_all_finite=True, estimator="MDLP discretizer")
+        self.dimensions_ = len(X.shape)
+
+        if self.dimensions_ > 2:
+            raise ValueError("Invalid input dimension for `X`. Input shape is"
+                             "{0}".format(X.shape))
+
+        X = check_array(X, force_all_finite=True, estimator="MDLP discretizer",
+                        ensure_2d=False)
         y = column_or_1d(y)
         check_consistent_length(X, y)
-        if self.continuous_features_ is None:
-            self.continuous_features_ = range(X.shape[1])
 
-        self.num_classes_ = set(y)
-        self.cut_points_ = dict()
+        if self.dimensions_ == 2:
+            if self.continuous_features_ is None:
+                self.continuous_features_ = range(X.shape[1])
 
-        for index, col in enumerate(X.T):
-            if index not in self.continuous_features_:
-                continue
-            cut_points = self._mdlp(col, y)
-            self.cut_points_[index] = np.array(cut_points)
+            self.cut_points_ = dict()
+
+            for index, col in enumerate(X.T):
+                if index not in self.continuous_features_:
+                    continue
+                cut_points = self._mdlp(col, y)
+                self.cut_points_[index] = np.array(cut_points)
+        else:
+            # TODO: Throw warning if self.continuous_features_ is not None
+            self.continuous_features_ = None
+            cut_points = self._mdlp(X, y)
+            self.cut_points_ = np.array(cut_points)
+
         return self
 
     def transform(self, X, y=None):
@@ -97,13 +115,15 @@ class MDLP(BaseEstimator):
         0... k-1 (`k` is the number of intervals the discretizer created
         from a given continuous feature.)
         """
-        if self.continuous_features_ is None:
+        if self.cut_points_ is None:
             raise ValueError("You must fit the MDLP discretizer before "
                              "transforming data.")
-
-        output = X.copy()
-        for i in self.continuous_features_:
-            output[:, i] = np.searchsorted(self.cut_points_[i], X[:, i])
+        if self.dimensions_ == 1:
+            output = np.searchsorted(self.cut_points_, X)
+        else:
+            output = X.copy()
+            for i in self.continuous_features_:
+                output[:, i] = np.searchsorted(self.cut_points_[i], X[:, i])
         return output
 
     def fit_transform(self, X, Y):
@@ -112,18 +132,29 @@ class MDLP(BaseEstimator):
         self.fit(X, Y)
         return self.transform(X)
 
-    def cat2intervals(self, X, index):
+    def cat2intervals(self, X, index=None):
         """Converts a categorical feature into a list of intervals.
         """
-        cp_indices = X.T[index]
-        return self._assign_intervals(cp_indices, index)
+        # TODO: Throw warning if `self.dimensions_` == 1 and index is not None
+        if self.dimensions_ == 1:
+            return self._assign_intervals(X, index)
+        elif self.dimensions_ == 2 and index is None:
+            raise ValueError("Index of `X` to be discretized needs to be "
+                             "specified.")
+        else:
+            cp_indices = X.T[index]
+            return self._assign_intervals(cp_indices, index)
 
     def _assign_intervals(self, cp_indices, index):
         """Assigns the cut point indices `cp_indices` (representing
         categorical features) into a list of intervals.
         """
 
-        cut_points = self.cut_points_[index]
+        # Case for a 1-D array
+        if self.dimensions_ == 1:
+            cut_points = self.cut_points_
+        else:
+            cut_points = self.cut_points_[index]
 
         non_zero_mask = cp_indices[cp_indices - 1 != -1].astype(int) - 1
         fronts = np.zeros(cp_indices.shape)
@@ -138,23 +169,27 @@ class MDLP(BaseEstimator):
 
         return [(front, back) for front, back in zip(fronts, backs)]
 
-
-    def cts2intervals(self, col, index):
-        """Bins a continuous feature into its appropriate interval.
-
-        TODO: There's probably a more elegant way of going about this
-        when `col` is a scalar.
-        """
-        cp_indices = self.cts2cat(col, index)
-        if cp_indices is not np.array:
-            cp_indices = np.array([cp_indices])
-        return self._assign_intervals(cp_indices, index)
-
-    def cts2cat(self, col, index):
+    def cts2cat(self, col, index=None):
         """Converts each continuous feature from index `index` into
         a categorical feature from the input column `col`.
         """
+        if self.dimensions_ == 1:
+            return np.searchsorted(self.cut_points_, col)
+        if self.dimensions_ == 2 and index is None:
+            raise ValueError("Index of `X` to be discretized needs to be "
+                             "specified.")
         return np.searchsorted(self.cut_points_[index], col)
+
+    def cts2intervals(self, col, index=None):
+        """Bins a continuous feature into its appropriate interval.
+        """
+        cp_indices = self.cts2cat(col, index)
+
+        # TODO: Below occurs if `col` is a number. There's probably a more
+        # elegant way of handling this case.
+        if cp_indices is not np.array:
+            cp_indices = np.array([cp_indices])
+        return self._assign_intervals(cp_indices, index)
 
     def _mdlp(self, col, y):
 
