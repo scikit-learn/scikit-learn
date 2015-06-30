3700253eb431fe114d4713e73457bf82be3c1780
Henri Dwyer <henri.dwyer@gmail.com>
2015-06-30 18:08:33 -0400

    Update title and introduction to include adaptive lasso

--- examples/linear_model/plot_lasso_adaptive_lasso_and_elasticnet.py
+++ examples/linear_model/plot_lasso_adaptive_lasso_and_elasticnet.py
@@ -1,11 +1,11 @@
 """
-========================================
-Lasso and Elastic Net for Sparse Signals
-========================================
+========================================================
+Lasso, Adaptive Lasso and Elastic Net for Sparse Signals
+========================================================
 
-Estimates Lasso and Elastic-Net regression models on a manually generated
-sparse signal corrupted with an additive noise. Estimated coefficients are
-compared with the ground-truth.
+Estimates Lasso, Adaptive Lasso and Elastic-Net regression models on a 
+manually generated sparse signal corrupted with an additive noise. 
+Estimated coefficients are compared with the ground-truth.
 
 """
 print(__doc__)
