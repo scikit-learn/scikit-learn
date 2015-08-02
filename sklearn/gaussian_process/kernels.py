2396737a24a10b095f89ebada1b0af2733221e36
Jan Hendrik Metzen <jhm@informatik.uni-bremen.de>
2015-08-02 10:25:58 +0200

    ADD CompoundKernel for multi-class GP classification

--- sklearn/gaussian_process/kernels.py
+++ sklearn/gaussian_process/kernels.py
@@ -244,6 +244,133 @@ class Kernel(six.with_metaclass(ABCMeta)):
         return True
 
 
+class CompoundKernel(Kernel):
+    """Kernel which is composed of a set of other kernels."""
+
+    def __init__(self, kernels):
+        self.kernels = kernels
+
+    def get_params(self, deep=True):
+        """Get parameters of this kernel.
+
+        Parameters
+        ----------
+        deep: boolean, optional
+            If True, will return the parameters for this estimator and
+            contained subobjects that are estimators.
+
+        Returns
+        -------
+        params : mapping of string to any
+            Parameter names mapped to their values.
+        """
+        return dict(kernels=kernels)
+
+    @property
+    def theta(self):
+        """Returns the (flattened, log-transformed) non-fixed hyperparameters.
+
+        Note that theta are typically the log-transformed values of the
+        kernel's hyperparameters as this representation of the search space
+        is more amenable for hyperparameter search, as hyperparameters like
+        length-scales naturally live on a log-scale.
+
+        Returns
+        -------
+        theta : array, shape (n_dims,)
+            The non-fixed, log-transformed hyperparameters of the kernel
+        """
+        return np.hstack([kernel.theta for kernel in self.kernels])
+
+    @theta.setter
+    def theta(self, theta):
+        """Sets the (flattened, log-transformed) non-fixed hyperparameters.
+
+        Parameters
+        ----------
+        theta : array, shape (n_dims,)
+            The non-fixed, log-transformed hyperparameters of the kernel
+        """
+        k_dims = self.k1.n_dims
+        for i, kernel in enumerate(self.kernels):
+            kernel.theta = theta[i*k_dims:(i+1)*k_dims]
+
+    @property
+    def bounds(self):
+        """Returns the bounds on the kernel's hyperparameters theta.
+
+        Returns
+        -------
+        bounds : array, shape (n_dims, 2)
+            The bounds on the kernel's hyperparameters theta
+        """
+        return np.vstack([kernel.bounds for kernel in self.kernels])
+
+    @bounds.setter
+    def bounds(self, bounds):
+        """Sets the bounds on the kernel's hyperparameters theta.
+
+        Parameters
+        ----------
+        bounds : array, shape (n_dims, 2)
+            The bounds on the kernel's hyperparameters theta
+        """
+        k1_dims = self.k1.n_dims
+        for i, kernel in enumerate(self.kernels):
+            kernel.bounds = bounds[i*k_dims:(i+1)*k_dims]
+
+    def __call__(self, X, Y=None, eval_gradient=False):
+        """Return the kernel k(X, Y) and optionally its gradient.
+
+        Note that this compound kernel returns the results of all simple kernel
+        stacked along an additional axis.
+
+        Parameters
+        ----------
+        X : array, shape (n_samples_X, n_features)
+            Left argument of the returned kernel k(X, Y)
+
+        Y : array, shape (n_samples_Y, n_features), (optional, default=None)
+            Right argument of the returned kernel k(X, Y). If None, k(X, X)
+            if evaluated instead.
+
+        eval_gradient : bool (optional, default=False)
+            Determines whether the gradient with respect to the kernel
+            hyperparameter is determined.
+
+        Returns
+        -------
+        K : array, shape (n_samples_X, n_samples_Y, n_kernels)
+            Kernel k(X, Y)
+
+        K_gradient : array, shape (n_samples_X, n_samples_X, n_dims, n_kernels)
+            The gradient of the kernel k(X, X) with respect to the
+            hyperparameter of the kernel. Only returned when eval_gradient
+            is True.
+        """
+        if eval_gradient:
+            K = []
+            K_grad = []
+            for kernel in self.kernels:
+                K_single, K_grad_single = kernel(X, Y, eval_gradient)
+                K.append(K_single)
+                K_grad.append(K_grad_single[..., np.newaxis])
+            return np.dstack(K), np.concatenate(K_grad, 3)
+        else:
+            return np.dstack([kernel(X, Y, eval_gradient)
+                              for kernel in self.kernels])
+
+    def __eq__(self, b):
+        if type(self) != type(b) or len(self.kernels) != len(b.kernels):
+            return False
+        return np.all([self.kernels[i] == b.kernels[i]
+                       for i in range(len(self.kernels))])
+
+    def is_stationary(self):
+        """Returns whether the kernel is stationary. """
+        return np.all([kernel.is_stationary() for kernel in self.kernels])
+
+
 class KernelOperator(Kernel):
     """Base class for all kernel operators. """
 
