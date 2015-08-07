bb9fb488c29e25fabe4032a486a5653c25c1cb9a
Sebastien <sdubois.sebastien@gmail.com>
2015-08-07 17:55:05 +0200

    example/update and fixed typo

--- examples/model_selection/gp_search.py
+++ examples/model_selection/gp_search.py
@@ -27,31 +27,23 @@ def test1():
 	clf = RandomForestClassifier(n_estimators=20)
 
 	# specify parameters and distributions to sample from
-	parameters = {"max_depth": [3, 3],
-					"max_features": [1,11],
-					"min_samples_split": [1,11],
-					"min_samples_leaf": [1,11],
-					"bootstrap": [True, False],
-					"criterion": ["gini", "entropy"]}
+	parameters = {"max_depth": ['int',[3, 3]],
+					"max_features": ['int',[1,11]],
+					"min_samples_split": ['int',[1,11]],
+					"min_samples_leaf": ['int',[1,11]],
+					"bootstrap": ['cat',[True, False]],
+					"criterion": ['cat',["gini", "entropy"]]}
 
-	parameters_details = {"max_depth": 'int',
-					"max_features": 'int',
-					"min_samples_split": 'int',
-					"min_samples_leaf": 'int',
-					"bootstrap": 'cat',
-					"criterion": 'cat'}
-
-	search = GPSearchCV(parameters,parameters_details,estimator=clf,X=X,y=y,n_iter=20)
+	search = GPSearchCV(parameters,estimator=clf,X=X,y=y,n_iter=20)
 	search._fit()
 
 
 def test2():
-	parameters = {'kernel' : ['rbf','poly'],'d' : [1,3],'C' : [1,10] }
-	parameters_details = {'kernel' : 'cat','d' : 'int','C' : 'float'}
+	parameters = {'kernel' : ['cat',['rbf','poly']],'d' : ['int',[1,3]],'C' : ['float',[1,10]] }
 	def scoring_function(x):
 		return 0.5
 
-	search = GPSearchCV(parameters,parameters_details,estimator=scoring_function,n_iter=20)
+	search = GPSearchCV(parameters,estimator=scoring_function,n_iter=20)
 	search._fit()
 
 
@@ -87,32 +79,21 @@ def test3():
 	# uncommenting more parameters will give better exploring power but will
 	# increase processing time in a combinatorial way
 	parameters = {
-	    'vect__max_df': [0.5,1.],
+	    'vect__max_df': ['float',[0.5,1.]],
 	    #'vect__max_features': (None, 5000, 10000, 50000),
-	    'vect__ngram_range': [(1, 1), (1, 2)],  # unigrams or bigrams
+	    'vect__ngram_range': ['cat',[(1, 1), (1, 2)]],  # unigrams or bigrams
 	    #'tfidf__use_idf': (True, False),
 	    #'tfidf__norm': ('l1', 'l2'),
-	    'clf__alpha': [0.000001, 0.00001],
-	    'clf__penalty': ['l2', 'elasticnet'],
+	    'clf__alpha': ['float',[0.000001, 0.00001]],
+	    'clf__penalty': ['cat',['l2', 'elasticnet']]
 	    #'clf__n_iter': (10, 50, 80),
 	}
 
-	parameters_details = {
-	    'vect__max_df': 'float',
-	    #'vect__max_features': (None, 5000, 10000, 50000),
-	    'vect__ngram_range': 'cat' ,  # unigrams or bigrams
-	    #'tfidf__use_idf': (True, False),
-	    #'tfidf__norm': ('l1', 'l2'),
-	    'clf__alpha': 'float',
-	    'clf__penalty': 'cat',
-	    #'clf__n_iter': (10, 50, 80),
-	}
-
-	search = GPSearchCV(parameters,parameters_details,estimator=pipeline,X=data.data, y=data.target,n_iter=20)
+	search = GPSearchCV(parameters,estimator=pipeline,X=data.data, y=data.target,n_iter=20)
 	search._fit()
 
 
-def gp_vs_random_search(test_name,n_tests,search_lenght):
+def gp_vs_random_search(test_name,n_tests,search_lenght,save_data=False):
 	"""
 	Compare GP-based search vs a simple random one
 	Choose test_name in {'iris','text'}
@@ -123,22 +104,15 @@ def gp_vs_random_search(test_name,n_tests,search_lenght):
 	if(test_name == 'iris'):
 		iris = load_digits()
 		X, y = iris.data, iris.target
-		pipeline = RandomForestClassifier(n_estimators=20)
+		pipeline = RandomForestClassifier()
 
 		# specify parameters and distributions to sample from
-		parameters = {"max_depth": [2, 15],
-						"max_features": [1,20],
-						"min_samples_split": [1,20],
-						"min_samples_leaf": [1,20],
-						"bootstrap": [True, False],
-						"criterion": ["gini", "entropy"]}
-
-		parameters_details = {"max_depth": 'int',
-						"max_features": 'int',
-						"min_samples_split": 'int',
-						"min_samples_leaf": 'int',
-						"bootstrap": 'cat',
-						"criterion": 'cat'}
+		parameters = {"max_depth": ['int',[3, 3]],
+						"max_features": ['int',[1,11]],
+						"min_samples_split": ['int',[1,11]],
+						"min_samples_leaf": ['int',[1,11]],
+						"bootstrap": ['cat',[True, False]],
+						"criterion": ['cat',["gini", "entropy"]]}
 
 	elif(test_name == 'text'):
 		# Display progress logs on stdout
@@ -173,54 +147,66 @@ def gp_vs_random_search(test_name,n_tests,search_lenght):
 		# uncommenting more parameters will give better exploring power but will
 		# increase processing time in a combinatorial way
 		parameters = {
-		    'vect__max_df': [0.5,1.],
-		    #'vect__max_features': (None, 5000, 10000, 50000),
-		    'vect__ngram_range': [(1, 1), (1, 2)],  # unigrams or bigrams
-		    #'tfidf__use_idf': (True, False),
-		    #'tfidf__norm': ('l1', 'l2'),
-		    'clf__alpha': [0.000001, 0.00001],
-		    'clf__penalty': ['l2', 'elasticnet'],
-		    #'clf__n_iter': (10, 50, 80),
-		}
-
-		parameters_details = {
-		    'vect__max_df': 'float',
-		    #'vect__max_features': (None, 5000, 10000, 50000),
-		    'vect__ngram_range': 'cat' ,  # unigrams or bigrams
-		    #'tfidf__use_idf': (True, False),
-		    #'tfidf__norm': ('l1', 'l2'),
-		    'clf__alpha': 'float',
-		    'clf__penalty': 'cat',
-		    #'clf__n_iter': (10, 50, 80),
+			'vect__max_df': ['float',[0.5,1.]],
+			#'vect__max_features': (None, 5000, 10000, 50000),
+			'vect__ngram_range': ['cat',[(1, 1), (1, 2)]],  # unigrams or bigrams
+			#'tfidf__use_idf': (True, False),
+			#'tfidf__norm': ('l1', 'l2'),
+			'clf__alpha': ['float',[0.000001, 0.00001]],
+			'clf__penalty': ['cat',['l2', 'elasticnet']]
+			#'clf__n_iter': (10, 50, 80),
 		}
 
 	else:
 		print('Dataset not available for test')
 
-	# GP search
-	all_gp_results = []
-	print 'GP search'
+	# GP UCB search
+	all_gp_ucb_results = []
+	print 'GP_ucb search'
 	for i in range(n_tests):
-		search = GPSearchCV(parameters,parameters_details,estimator=pipeline,X=X,y=y,
+		ucb_search = GPSearchCV(parameters,estimator=pipeline,X=X,y=y,
+							acquisition_function='UCB',
 							n_iter=n_iter_search, n_init=20, verbose=False)
-		_,scores = search._fit()
+		_,scores = ucb_search._fit()
 
 		max_scores = [scores[0]]
 		print 'Test',i,'-',len(scores),'parameters tested'
 
 		for j in range(1,len(scores)):
 			max_scores.append(max(max_scores[j-1],scores[j]))
-		all_gp_results.append(extend_result(n_iter_search,max_scores))
-	all_gp_results = np.asarray(all_gp_results)
-	print all_gp_results.shape
+		all_gp_ucb_results.append(extend_result(n_iter_search,max_scores))
+	all_gp_ucb_results = np.asarray(all_gp_ucb_results)
+	print all_gp_ucb_results.shape
+	if(save_data):
+		np.savetxt('gp_ucb_scores.csv',all_gp_ucb_results,delimiter=',')
+
+	# # GP EI search
+	# all_gp_ei_results = []
+	# print 'GP_ei search'
+	# for i in range(n_tests):
+	# 	ei_search = GPSearchCV(parameters,estimator=pipeline,X=X,y=y,
+	# 						acquisition_function='EI',
+	# 						n_iter=n_iter_search, n_init=20, verbose=False)
+	# 	_,scores = ei_search._fit()
+
+	# 	max_scores = [scores[0]]
+	# 	print 'Test',i,'-',len(scores),'parameters tested'
+
+	# 	for j in range(1,len(scores)):
+	# 		max_scores.append(max(max_scores[j-1],scores[j]))
+	# 	all_gp_ei_results.append(extend_result(n_iter_search,max_scores))
+	# all_gp_ei_results = np.asarray(all_gp_ei_results)
+	# print all_gp_ei_results.shape
+	# if(save_data):
+	# 	np.savetxt('gp_ei_scores.csv',all_gp_ei_results,delimiter=',')
 
 	# Randomized search
 	print 'Random search'
 	all_random_results = []
 	for i in range(n_tests):
-		random_search = GPSearchCV(parameters,parameters_details,estimator=pipeline,X=X,y=y,
+		random_search = GPSearchCV(parameters,estimator=pipeline,X=X,y=y,
                                   	n_iter=n_iter_search, n_init=n_iter_search, verbose=False)
-		_,scores = search._fit()
+		_,scores = random_search._fit()
 
 		max_scores = [scores[0]]
 		print 'Test',i,'-',len(scores),'parameters tested'
@@ -229,11 +215,14 @@ def gp_vs_random_search(test_name,n_tests,search_lenght):
 			max_scores.append(max(max_scores[j-1],scores[j]))
 		all_random_results.append(extend_result(n_iter_search,max_scores))
 	all_random_results = np.asarray(all_random_results)
+	if(save_data):
+		np.savetxt('rand_scores.csv',all_random_results,delimiter=',')
 
 	plt.figure()
-	plt.plot(range(n_iter_search),np.mean(all_gp_results,axis=0),'r',label='GP')
+	# plt.plot(range(n_iter_search),np.mean(all_gp_ei_results,axis=0),'r',label='GP-EI')
+	plt.plot(range(n_iter_search),np.mean(all_gp_ucb_results,axis=0),'b',label='GP-UCB')
 	plt.plot(range(n_iter_search),np.mean(all_random_results,axis=0),'g',label='Random')
-	plt.legend()
+	plt.legend(loc=4)
 	plt.title('Test GP vs Random on ' + test_name +' dataset - Average on ' + str(n_tests) + ' trials')
 	plt.xlabel('Iterations')
 	plt.ylabel('Max CV performance')
@@ -245,8 +234,8 @@ if __name__ == "__main__":
 	
 	# print 'Routine Test'
 	# test2()
-	test_name = 'iris'
+	test_name = 'text'
 	n_tests = 20
-	search_lenght = 50
+	search_lenght = 60
 	print '\nTest GP vs Random on',test_name,'dataset - Average on',n_tests,'trials'
-	gp_vs_random_search(test_name,n_tests,search_lenght)
+	gp_vs_random_search(test_name,n_tests,search_lenght,save_data=True)
