d0ce6322b749cd126da86c95cce0d76fb0702b28
Frank Zalkow <frank_zalkow@web.de>
2015-08-14 20:27:01 +0200

    added newline (for pep8) and reverted to regex solution due to 0.0/-0.0 problem on windows

--- sklearn/tree/tests/test_export.py
+++ sklearn/tree/tests/test_export.py
@@ -225,6 +225,7 @@ def test_graphviz_errors():
     out = StringIO()
     assert_raises(IndexError, export_graphviz, clf, out, class_names=[])
 
+
 def test_friedman_mse_in_graphviz():
 
     clf = DecisionTreeRegressor(criterion="friedman_mse", random_state=0)
@@ -237,43 +238,7 @@ def test_friedman_mse_in_graphviz():
     for estimator in clf.estimators_:
         export_graphviz(estimator[0], out_file=dot_data)
 
-    contents1 = dot_data.getvalue()
-    contents2 = 'digraph Tree {\n' \
-                'node [shape=box] ;\n' \
-                '0 [label="X[1] <= 0.0\\nfriedman_mse = 1.0\\nsamples = 6\\n' \
-                'value = 0.0"] ;\n' \
-                '1 [label="friedman_mse = 0.0\\nsamples = 3\\n' \
-                'value = -1.0"] ;\n' \
-                '0 -> 1 [labeldistance=2.5, labelangle=45, ' \
-                'headlabel="True"] ;\n' \
-                '2 [label="friedman_mse = 0.0\\nsamples = 3\\n' \
-                'value = 1.0"] ;\n' \
-                '0 -> 2 [labeldistance=2.5, labelangle=-45, ' \
-                'headlabel="False"] ;\n' \
-                '}digraph Tree {\n' \
-                'node [shape=box] ;\n' \
-                '0 [label="X[1] <= 0.0\\nfriedman_mse = 0.25\\nsamples = 6' \
-                '\\nvalue = 0.0"] ;\n' \
-                '1 [label="friedman_mse = 0.0\\nsamples = 3\\n' \
-                'value = -2.0"] ;\n' \
-                '0 -> 1 [labeldistance=2.5, labelangle=45, ' \
-                'headlabel="True"] ;\n' \
-                '2 [label="friedman_mse = 0.0\\nsamples = 3\\n' \
-                'value = 2.0"] ;\n' \
-                '0 -> 2 [labeldistance=2.5, labelangle=-45, ' \
-                'headlabel="False"] ;\n' \
-                '}digraph Tree {\n' \
-                'node [shape=box] ;\n' \
-                '0 [label="X[1] <= 0.0\\nfriedman_mse = 0.2026\\n' \
-                'samples = 6\\nvalue = -0.0"] ;\n' \
-                '1 [label="friedman_mse = 0.0\\nsamples = 3\\n' \
-                'value = -1.8187"] ;\n' \
-                '0 -> 1 [labeldistance=2.5, labelangle=45, ' \
-                'headlabel="True"] ;\n' \
-                '2 [label="friedman_mse = -0.0\\nsamples = 3\\n' \
-                'value = 1.8187"] ;\n' \
-                '0 -> 2 [labeldistance=2.5, labelangle=-45, ' \
-                'headlabel="False"] ;\n' \
-                '}'
+    for finding in re.finditer("\[.*?samples.*?\]", dot_data.getvalue()):
+        assert_in("friedman_mse", finding.group())
 
     assert_equal(contents1, contents2)
