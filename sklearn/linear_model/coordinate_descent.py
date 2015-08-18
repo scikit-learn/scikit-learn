66702ec8b5c0730b7fbd523983088291838d5a2a
Arthur Mensch <arthur.mensch@m4x.org>
2015-08-18 08:59:10 +0200

    FIX

--- sklearn/linear_model/coordinate_descent.py
+++ sklearn/linear_model/coordinate_descent.py
@@ -866,9 +866,9 @@ class Lasso(ElasticNet):
     >>> from sklearn import linear_model
     >>> clf = linear_model.Lasso(alpha=0.1)
     >>> clf.fit([[0,0], [1, 1], [2, 2]], [0, 1, 2])
-    Lasso(alpha=0.1, copy_X=True, fit_intercept=True, max_iter=1000,
-       normalize=False, positive=False, precompute=False, random_state=None,
-       selection='cyclic', tol=0.0001, warm_start=False)
+    Lasso(alpha=0.1, bypass_checks=False, copy_X=True, fit_intercept=True,
+       max_iter=1000, normalize=False, positive=False, precompute=False,
+       random_state=None, selection='cyclic', tol=0.0001, warm_start=False)
     >>> print(clf.coef_)
     [ 0.85  0.  ]
     >>> print(clf.intercept_)
