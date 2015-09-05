5a1f18312b1da30e0f1e1bf3f8d91312def82235
djipey <you@example.com>
2015-09-05 17:44:05 +0200

    Creation of the attribute LDA.explained_variance_ratio_, for the eigen solver. It is an analogy to PCA.explained_variance_ratio, and works in the same way.

--- sklearn/lda.py
+++ sklearn/lda.py
@@ -316,6 +316,7 @@ class LDA(BaseEstimator, LinearClassifierMixin, TransformerMixin):
         Sb = St - Sw  # between scatter
 
         evals, evecs = linalg.eigh(Sb, Sw)
+        self.explained_variance_ratio_ = np.sort(evals / np.sum(evals))[::-1]
         evecs = evecs[:, np.argsort(evals)[::-1]]  # sort eigenvectors
         # evecs /= np.linalg.norm(evecs, axis=0)  # doesn't work with numpy 1.6
         evecs /= np.apply_along_axis(np.linalg.norm, 0, evecs)
