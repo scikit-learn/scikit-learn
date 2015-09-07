c3a41a310f689da2ea54c8f710b43480f1120041
Jan Hendrik Metzen <jhm@informatik.uni-bremen.de>
2015-09-07 12:15:57 +0200

    FIX String comparison via equality and not identity in Hyperparameter.__init__

--- sklearn/gaussian_process/kernels.py
+++ sklearn/gaussian_process/kernels.py
@@ -77,7 +77,7 @@ class Hyperparameter(namedtuple('Hyperparameter',
     __slots__ = ()
 
     def __new__(cls, name, value_type, bounds, n_elements=1, fixed=None):
-        if bounds is not "fixed":
+        if bounds != "fixed":
             bounds = np.atleast_2d(bounds)
             if n_elements > 1:  # vector-valued parameter
                 if bounds.shape[0] == 1:
@@ -88,7 +88,7 @@ class Hyperparameter(namedtuple('Hyperparameter',
                                      % (name, n_elements, bounds.shape[0]))
 
         if fixed is None:
-            fixed = bounds is "fixed"
+            fixed = (bounds == "fixed")
         return super(Hyperparameter, cls).__new__(
             cls, name, value_type, bounds, n_elements, fixed)
 
