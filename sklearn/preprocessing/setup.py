e05aabbc1c8cb744e66cea14e9df5d23e3e4f6fe
hlin117 <hlin117@gmail.com>
2015-09-21 15:44:03 -0500

    MDLP: Edited setup.py file

--- sklearn/preprocessing/setup.py
+++ sklearn/preprocessing/setup.py
@@ -1,13 +1,3 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy
-
-setup(
-    ext_modules=cythonize("_mdlp.pyx"),
-    include_dirs=[numpy.get_include()],
-    extra_compiler_args=["-03"]
-)
-
 import os
 
 import numpy
