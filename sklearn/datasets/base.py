FIX precision to float64 across the codebase

sklearn/datasets/base.py
@@ -279,7 +279,7 @@ def load_iris():
         target = np.empty((n_samples,), dtype=np.int)
 
         for i, ir in enumerate(data_file):
-            data[i] = np.asarray(ir[:-1], dtype=np.float)
+            data[i] = np.asarray(ir[:-1], dtype=np.float64)
             target[i] = np.asarray(ir[-1], dtype=np.int)
 
     with open(join(module_path, 'descr', 'iris.rst')) as rst_file:
@@ -342,7 +342,7 @@ def load_breast_cancer():
         target = np.empty((n_samples,), dtype=np.int)
 
         for count, value in enumerate(data_file):
-            data[count] = np.asarray(value[:-1], dtype=np.float)
+            data[count] = np.asarray(value[:-1], dtype=np.float64)
             target[count] = np.asarray(value[-1], dtype=np.int)
 
     with open(join(module_path, 'descr', 'breast_cancer.rst')) as rst_file:
@@ -536,8 +536,8 @@ def load_boston():
         feature_names = np.array(temp)
 
         for i, d in enumerate(data_file):
-            data[i] = np.asarray(d[:-1], dtype=np.float)
-            target[i] = np.asarray(d[-1], dtype=np.float)
+            data[i] = np.asarray(d[:-1], dtype=np.float64)
+            target[i] = np.asarray(d[-1], dtype=np.float64)
 
     return Bunch(data=data,
                  target=target,

sklearn/feature_extraction/tests/test_image.py
@@ -51,8 +51,8 @@ def test_grid_to_graph():
     assert_true(A.dtype == np.bool)
     A = grid_to_graph(n_x=size, n_y=size, n_z=size, mask=mask, dtype=np.int)
     assert_true(A.dtype == np.int)
-    A = grid_to_graph(n_x=size, n_y=size, n_z=size, mask=mask, dtype=np.float)
-    assert_true(A.dtype == np.float)
+    A = grid_to_graph(n_x=size, n_y=size, n_z=size, mask=mask, dtype=np.float64)
+    assert_true(A.dtype == np.float64)
 
 
 def test_connect_regions():
@@ -80,7 +80,7 @@ def _downsampled_lena():
             + lena[1::2, 1::2])
     lena = (lena[::2, ::2] + lena[1::2, ::2] + lena[::2, 1::2]
             + lena[1::2, 1::2])
-    lena = lena.astype(np.float)
+    lena = lena.astype(np.float32)
     lena /= 16.0
     return lena
 

sklearn/isotonic.py
@@ -119,11 +119,11 @@ def isotonic_regression(y, sample_weight=None, y_min=None, y_max=None,
     "Active set algorithms for isotonic regression; A unifying framework"
     by Michael J. Best and Nilotpal Chakravarti, section 3.
     """
-    y = np.asarray(y, dtype=np.float)
+    y = np.asarray(y, dtype=np.float64)
     if sample_weight is None:
         sample_weight = np.ones(len(y), dtype=y.dtype)
     else:
-        sample_weight = np.asarray(sample_weight, dtype=np.float)
+        sample_weight = np.asarray(sample_weight, dtype=np.float64)
     if not increasing:
         y = y[::-1]
         sample_weight = sample_weight[::-1]

sklearn/preprocessing/data.py
@@ -1651,7 +1651,7 @@ class OneHotEncoder(BaseEstimator, TransformerMixin):
     >>> enc = OneHotEncoder()
     >>> enc.fit([[0, 0, 3], [1, 1, 0], [0, 2, 1], \
 [1, 0, 2]])  # doctest: +ELLIPSIS
-    OneHotEncoder(categorical_features='all', dtype=<... 'float'>,
+    OneHotEncoder(categorical_features='all', dtype=<... 'numpy.float64'>,
            handle_unknown='error', n_values='auto', sparse=True)
     >>> enc.n_values_
     array([2, 3, 4])
@@ -1668,7 +1668,7 @@ class OneHotEncoder(BaseEstimator, TransformerMixin):
       encoding of dictionary items or strings.
     """
     def __init__(self, n_values="auto", categorical_features="all",
-                 dtype=np.float, sparse=True, handle_unknown='error'):
+                 dtype=np.float64, sparse=True, handle_unknown='error'):
         self.n_values = n_values
         self.categorical_features = categorical_features
         self.dtype = dtype

sklearn/svm/base.py
@@ -886,7 +886,7 @@ def _fit_liblinear(X, y, C, fit_intercept, intercept_scaling, class_weight,
 
         class_weight_ = compute_class_weight(class_weight, classes_, y)
     else:
-        class_weight_ = np.empty(0, dtype=np.float)
+        class_weight_ = np.empty(0, dtype=np.float64)
         y_ind = y
     liblinear.set_verbosity_wrap(verbose)
     rnd = check_random_state(random_state)