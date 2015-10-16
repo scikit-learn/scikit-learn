dbb418b14e3b34ecd6ec929b0b2aed2ff9320548
Lilian Besson <naereen@crans.org>
2015-10-16 14:19:24 +0200

    Typo on doc for default value of solver for lda.LDA
    Fix a style issue on the documentation for the default value of the `solver` parameter for lda.LDA.
    
    [sklearn.lda.LDA.html](http://scikit-learn.org/stable/modules/generated/sklearn.lda.LDA.html#sklearn.lda.LDA) displayed badly (cf screenshot).

--- sklearn/discriminant_analysis.py
+++ sklearn/discriminant_analysis.py
@@ -143,9 +143,9 @@ class LinearDiscriminantAnalysis(BaseEstimator, LinearClassifierMixin,
     ----------
     solver : string, optional
         Solver to use, possible values:
-          - 'svd': Singular value decomposition (default). Does not compute the
-                covariance matrix, therefore this solver is recommended for
-                data with a large number of features.
+          - 'svd': Singular value decomposition (default).
+                Does not compute the covariance matrix, therefore this solver is
+                recommended for data with a large number of features.
           - 'lsqr': Least squares solution, can be combined with shrinkage.
           - 'eigen': Eigenvalue decomposition, can be combined with shrinkage.
 
