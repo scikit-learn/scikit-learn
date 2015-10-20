31ab442d697e49d81253a0ee9e4643c6616b2f0b
MartinBpr <mbompr@gmail.com>
2015-10-20 13:33:01 +0200

    fixed typo in LDA

--- sklearn/discriminant_analysis.py
+++ sklearn/discriminant_analysis.py
@@ -144,8 +144,8 @@ class LinearDiscriminantAnalysis(BaseEstimator, LinearClassifierMixin,
     solver : string, optional
         Solver to use, possible values:
           - 'svd': Singular value decomposition (default).
-                Does not compute the covariance matrix, therefore this solver is
-                recommended for data with a large number of features.
+            Does not compute the covariance matrix, therefore this solver is
+            recommended for data with a large number of features.
           - 'lsqr': Least squares solution, can be combined with shrinkage.
           - 'eigen': Eigenvalue decomposition, can be combined with shrinkage.
 
