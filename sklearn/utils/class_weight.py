cd5688a00394c7d85fbd185756a30b31b4a29cde
Arnaud Rachez <arnaud.rachez@gmail.com>
2015-10-20 10:22:10 +0200

    MAINT Removed deprecated stuff.

--- sklearn/utils/class_weight.py
+++ sklearn/utils/class_weight.py
@@ -54,11 +54,7 @@ def compute_class_weight(class_weight, classes, y):
 
         # inversely proportional to the number of samples in the class
         if class_weight == 'auto':
-            recip_freq = 1. / bincount(y_ind)
-            weight = recip_freq[le.transform(classes)] / np.mean(recip_freq)
-            warnings.warn("The class_weight='auto' heuristic is deprecated in"
-                          " favor of a new heuristic class_weight='balanced'."
-                          " 'auto' will be removed in 0.18", DeprecationWarning)
+            raise ValueError('auto keyword is not supported anymore.')
         else:
             recip_freq = len(y) / (len(le.classes_) *
                                    bincount(y_ind).astype(np.float64))
