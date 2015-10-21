4d9a955933918a398b47f0655fd097f99b385351
Arnaud Joly <arnaud.v.joly@gmail.com>
2015-10-21 10:33:34 +0200

    DOC make visualizatoin aware of the tree depth

--- examples/tree/plot_structure.py
+++ examples/tree/plot_structure.py
@@ -7,6 +7,7 @@ The decision tree structure can be analysed to gain further insight on the
 relation between the features and the target to predict. In this example, we
 show how to retrieve:
     - the binary tree structure;
+    - the depth of each node and whether or not it's a leaf.
     - the nodes that were reached by a sample using the decision_paths method;
     - the leaf that was reached by a sample using the apply method;
     - the rules that were used to predict a sample;
@@ -17,15 +18,14 @@ import numpy as np
 
 from sklearn.cross_validation import train_test_split
 from sklearn.datasets import load_iris
-from sklearn.tree import DecisionTreeRegressor
-
+from sklearn.tree import DecisionTreeClassifier
 
 iris = load_iris()
 X = iris.data
 y = iris.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
-estimator = DecisionTreeRegressor(max_leaf_nodes=3, random_state=0)
+estimator = DecisionTreeClassifier(max_leaf_nodes=3, random_state=0)
 estimator.fit(X_train, y_train)
 
 # The decision estimator has an attribute called tree_  which stores the entire
@@ -44,21 +44,44 @@ estimator.fit(X_train, y_train)
 
 # Using those arrays, we can parse the tree structure:
 
+n_nodes = estimator.tree_.node_count
+children_left = estimator.tree_.children_left
+children_right = estimator.tree_.children_right
+feature = estimator.tree_.feature
+threshold = estimator.tree_.threshold
+
+
+# The tree structure can be traversed to compute various properties such
+# as the depth of each node and whether or not it is a leaf.
+node_depth = np.zeros(shape=n_nodes)
+is_leaves = np.zeros(shape=n_nodes, dtype=bool)
+stack = [(0, -1)]  # seed is the root node id and its parent depth
+while len(stack) > 0:
+    node_id, parent_depth = stack.pop()
+    node_depth[node_id] = parent_depth + 1
+
+    # If we have a test node
+    if (children_left[node_id] != children_right[node_id]):
+        stack.append((children_left[node_id], parent_depth + 1))
+        stack.append((children_right[node_id], parent_depth + 1))
+    else:
+        is_leaves[node_id] = True
+
 print("The binary tree structure has %s nodes and has "
       "the following tree structure:"
-      % estimator.tree_.node_count)
-
-for i in range(estimator.tree_.node_count):
-    if estimator.tree_.children_left[i] == estimator.tree_.children_right[i]:
-        print("node=%s leaf node." % i)
+      % n_nodes)
+for i in range(n_nodes):
+    if is_leaves[i]:
+        print("%snode=%s leaf node." % (node_depth[i] * "\t", i))
     else:
-        print("node=%s test node: go to node %s if X[:, %s] <= %ss else to "
+        print("%snode=%s test node: go to node %s if X[:, %s] <= %ss else to "
               "node %s."
-              % (i,
-                 estimator.tree_.children_left[i],
-                 estimator.tree_.feature[i],
-                 estimator.tree_.threshold[i],
-                 estimator.tree_.children_right[i],
+              % (node_depth[i] * "\t",
+                 i,
+                 children_left[i],
+                 feature[i],
+                 threshold[i],
+                 children_right[i],
                  ))
 print()
 
@@ -70,7 +93,7 @@ node_indicator = estimator.decision_paths(X_test)
 
 # Similarly, we can also have the leaves ids reach by each sample.
 
-leave_id = set(estimator.apply(X_test))
+leave_id = estimator.apply(X_test)
 
 # Now, it's possible to get the tests that were used to predict a sample or
 # a group of samples. First, let's make it for the  sample.
@@ -81,11 +104,10 @@ node_index = node_indicator.indices[node_indicator.indptr[sample_id]:
 
 print('Rules used to predict sample %s: ' % sample_id)
 for i, node_id in enumerate(node_index):
-    if node_id in leave_id:
+    if is_leaves[node_id]:
         continue
 
-    if (X_test[i, estimator.tree_.feature[node_id]] <=
-            estimator.tree_.threshold[node_id]):
+    if (X_test[i, feature[node_id]] <= threshold[node_id]):
         threshold_sign = "<="
     else:
         threshold_sign = ">"
@@ -94,19 +116,19 @@ for i, node_id in enumerate(node_index):
           % (i,
              node_id,
              sample_id,
-             estimator.tree_.feature[node_id],
-             X_test[i, estimator.tree_.feature[node_id]],
+             feature[node_id],
+             X_test[i, feature[node_id]],
              threshold_sign,
-             estimator.tree_.threshold[node_id]))
+             threshold[node_id]))
 
 # For a group of samples, we have the following common node.
 sample_ids = [0, 1]
 common_nodes = (node_indicator.toarray()[sample_ids].sum(axis=0) ==
                 len(sample_ids))
 
-common_node_id = np.arange(estimator.tree_.node_count)[common_nodes]
+common_node_id = np.arange(n_nodes)[common_nodes]
 
-print("\nThe following sample %s shares the following path %s  in the tree"
+print("\nThe following sample %s shares the following node %s  in the tree"
       % (sample_ids, common_node_id))
 print("It is %s %% of all nodes."
-      % (len(common_node_id) / estimator.tree_.node_count * 100,))
+      % (len(common_node_id) / n_nodes * 100,))
