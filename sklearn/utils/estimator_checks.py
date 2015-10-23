484f30011eb062e93ad1ba93e289ba408ed3aa7c
sotlampr <sot.lampr@gmail.com>
2015-10-23 12:34:07 +0300

    test error message in check_fit_2d1sample and check_fit_2d1feature

--- sklearn/utils/estimator_checks.py
+++ sklearn/utils/estimator_checks.py
@@ -434,8 +434,19 @@ def check_fit2d_1sample(name, Estimator):
     set_random_state(estimator, 1)
     try:
         estimator.fit(X, y)
-    except ValueError:
-        pass
+    except ValueError as e:
+        if 'sample' not in repr(e) and 'class' not in repr(e):
+            print("Estimator %s doesn't seem to fail gracefully when fitting "
+                  "an array containing only one sample: error message state "
+                  "explicitly that fitting with only one sample is not "
+                  "supported." % name)
+            raise
+    except Exception:
+        print("Estimator %s doesn't seem to fail gracefully when fitting an "
+              "array containing only one feature: it should raise a "
+              "ValueError if fitting with only one sample is explicitly "
+              "not supported" % name)
+        raise
 
 
 @ignore_warnings
@@ -456,8 +467,19 @@ def check_fit2d_1feature(name, Estimator):
     set_random_state(estimator, 1)
     try:
         estimator.fit(X, y)
-    except ValueError:
-        pass
+    except ValueError as e:
+        if 'feature' not in repr(e):
+            print("Estimator %s doesn't seem to fail gracefully when fitting "
+                  "an array with samples consisting of only one feature: "
+                  "error message state explicitly that fitting samples with "
+                  "only one feature is not supported." % name)
+            raise
+    except Exception:
+        print("Estimator %s doesn't seem to fail gracefully when fitting an "
+              "array with samples consisting of only one feature: it should "
+              "raise a ValueError if fitting samples with only one feature is "
+              "explicitly not supported" % name)
+        raise
 
 
 @ignore_warnings
