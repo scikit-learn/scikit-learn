0061ae7725c610f490ce0b9492c4a503e1823739
Gilles Louppe <g.louppe@gmail.com>
2015-10-26 09:02:04 +0100

    DOC: cleanup example

--- examples/plot_predictive_standard_deviation.py
+++ examples/plot_predictive_standard_deviation.py
@@ -1,124 +1,116 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-r"""
+"""
 ==============================================================
 Comparison of predictive distributions of different regressors
 ==============================================================
 
-A simple one-dimensional, noisy regression problem adressed by three different
+A simple one-dimensional, noisy regression problem addressed by two different
 regressors:
 
-1. A Gaussian Process
-2. A Random Forest
-3. A Bagging-based Regressor
+1. A Random Forest
+2. A Gaussian Process
 
 The regressors are fitted based on noisy observations where the magnitude of
 the noise at the different training point is constant and known. Plotted are
 both the mean and the pointwise 95% confidence interval of the predictions.
-The mean predictions are evaluated on noise-less test data using the mean-
-squared-error. The mean log probabilities of the noise-less test data are used
+The mean predictions are evaluated on noise-less test data using the mean
+squared error. The mean log probabilities of the noise-less test data are used
 to evaluate the predictive distributions (a normal distribution with the
 predicted mean and standard deviation) of the three regressors.
 
-This example is based on the example gaussian_process/plot_gp_regression.py.
+The mean predictions of the Gaussian Process are slightly better than those of
+Random Forest. The predictive distribution (taking into account
+also the predictive variance) of the Random Forest is however slightly
+more likely for this example.
 """
 print(__doc__)
 
-# Author: Jan Hendrik Metzen <jhm@informatik.uni-bremen.de>
+# Authors: Jan Hendrik Metzen <jhm@informatik.uni-bremen.de>
+#          Gilles Louppe <g.louppe@gmail.com>
 # Licence: BSD 3 clause
 
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import norm
+
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF, WhiteKernel
-from sklearn.ensemble import RandomForestRegressor, BaggingRegressor
 from sklearn.metrics import mean_squared_error
-from matplotlib import pyplot as pl
+from sklearn.utils import check_random_state
 
-np.random.seed(1)
+rng = check_random_state(1)
 
-
+# Observations and noise
 def f(x):
     """The function to predict."""
     return x * np.sin(x)
 
-X = np.linspace(0.1, 9.9, 20)
-X = np.atleast_2d(X).T
+X_train = rng.rand(20) * 10.0
+X_train = np.atleast_2d(X_train).T
 
-# Observations and noise
-y = f(X).ravel()
-dy = np.ones_like(y)
-noise = np.random.normal(0, dy)
-y += noise
+y_train = f(X_train).ravel()
+dy = np.ones_like(y_train)
+noise = rng.normal(0, dy)
+y_train += noise
 
 # Mesh the input space for evaluations of the real function, the prediction and
 # its standard deviation
-x = np.atleast_2d(np.linspace(0, 10, 1000)).T
+X_test = np.atleast_2d(np.linspace(0, 10, 1000)).T
 
-regrs = {
+regressors = {
     "Gaussian Process": GaussianProcessRegressor(
-        alpha=(dy / y) ** 2, kernel=1.0 * RBF() + WhiteKernel(),
-        n_restarts_optimizer=100),
-    "Random Forest": RandomForestRegressor(n_estimators=250),
-    "Bagging": BaggingRegressor(n_estimators=250)
+        alpha=(dy / y_train) ** 2,
+        kernel=1.0 * RBF() + WhiteKernel(),
+        n_restarts_optimizer=100, random_state=rng),
+    "Random Forest": RandomForestRegressor(n_estimators=500, random_state=rng),
 }
 
-# Plot predictive distributions of different regressors
-fig = pl.figure()
 # Plot the function and the observations
-pl.plot(x, f(x), 'r', label=u'$f(x) = x\,\sin(x)$')
-pl.fill(np.concatenate([x, x[::-1]]),
-        np.concatenate([f(x) - 1.9600, (f(x) + 1.9600)[::-1]]),
-        alpha=.3, fc='r', ec='None')
-pl.plot(X.ravel(), y, 'ko', zorder=5, label=u'Observations')
+fig = plt.figure()
+plt.plot(X_test, f(X_test), 'r', label=u'$f(x) = x\,\sin(x)$')
+plt.fill(np.concatenate([X_test, X_test[::-1]]),
+         np.concatenate([f(X_test) - 1.9600, (f(X_test) + 1.9600)[::-1]]),
+         alpha=.3, fc='r', ec='None')
+plt.plot(X_train.ravel(), y_train, 'ko', zorder=5, label=u'Observations')
+
 # Plot predictive distibutions of GP and Bagging
-colors = {"Gaussian Process": 'b', "Bagging": 'g'}
+colors = {"Gaussian Process": 'b', "Random Forest": 'g'}
 mse = {}
 log_pdf_loss = {}
-for name, regr in regrs.items():
-    regr.fit(X, y)
+
+for name, regr in regressors.items():
+    regr.fit(X_train, y_train)
 
     # Make the prediction on the meshed x-axis (ask for standard deviation
     # as well)
-    y_pred, sigma = regr.predict(x, return_std=True)
+    y_pred, sigma = regr.predict(X_test, return_std=True)
 
     # Compute mean-squared error and log predictive loss
-    mse[name] = mean_squared_error(f(x), y_pred)
+    mse[name] = mean_squared_error(f(X_test), y_pred)
     log_pdf_loss[name] = \
-        norm(y_pred, sigma).logpdf(f(x)).mean()
-
-    if name == "Random Forest":  # Skip because RF is very similar to Bagging
-        continue
+        norm(y_pred, sigma).logpdf(f(X_test)).mean()
 
     # Plot 95% confidence interval based on the predictive standard deviation
-    pl.plot(x, y_pred, colors[name], label=name)
-    pl.fill(np.concatenate([x, x[::-1]]),
-            np.concatenate([y_pred - 1.9600 * sigma,
-                            (y_pred + 1.9600 * sigma)[::-1]]),
-            alpha=.3, fc=colors[name], ec='None')
+    plt.plot(X_test, y_pred, colors[name], label=name)
+    plt.fill(np.concatenate([X_test, X_test[::-1]]),
+             np.concatenate([y_pred - 1.9600 * sigma,
+                             (y_pred + 1.9600 * sigma)[::-1]]),
+             alpha=.3, fc=colors[name], ec='None')
 
-
-pl.xlabel('$x$')
-pl.ylabel('$f(x)$')
-pl.ylim(-10, 20)
-pl.legend(loc='upper left')
+plt.xlabel('$x$')
+plt.ylabel('$f(x)$')
+plt.ylim(-10, 20)
+plt.legend(loc='upper left')
 
 print("Mean-squared error of predictors on 1000 equidistant noise-less test "
-      "datapoints:\n\tRandom Forest: %.2f\n\tBagging: %.2f"
-      "\n\tGaussian Process: %.2f"
-      % (mse["Random Forest"], mse["Bagging"], mse["Gaussian Process"]))
+      "datapoints:\n\tRandom Forest: %.2f\n\tGaussian Process: %.2f\n"
+      % (mse["Random Forest"], mse["Gaussian Process"]))
 
-print("Mean log-probability of 1000 equidistant noise-less test datapoints "
-      "under the (normal) predictive distribution of the predictors, i.e., "
+print("Mean log-probability of 1000 equidistant noise-less test datapoints\n"
+      "under the (normal) predictive distribution of the predictors, i.e.,\n"
       "log N(y_true| y_pred_mean, y_pred_std) [less is better]:"
-      "\n\tRandom Forest: %.2f\n\tBagging: %.2f\n\tGaussian Process: %.2f"
-      % (log_pdf_loss["Random Forest"], log_pdf_loss["Bagging"],
+      "\n\tRandom Forest: %.2f\n\tGaussian Process: %.2f\n"
+      % (log_pdf_loss["Random Forest"],
          log_pdf_loss["Gaussian Process"]))
 
-print("In summary, the mean predictions of the Gaussian Process are slightly "
-      "better than those of Random Forest and Bagging. The predictive "
-      "distributions (taking into account also the predictive variance) "
-      "of the Gaussian Process are considerably better.")
-
-pl.show()
+plt.show()
