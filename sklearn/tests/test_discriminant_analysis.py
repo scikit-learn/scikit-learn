FIX port LDA covariance fix to decomposition module

sklearn/tests/test_discriminant_analysis.py
@@ -16,6 +16,7 @@ from sklearn.utils.testing import ignore_warnings
 from sklearn.datasets import make_blobs
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.discriminant_analysis import _cov
 
 
 # import reload
@@ -116,7 +117,7 @@ def test_lda_priors():
     priors = np.array([0.5, 0.6])
     prior_norm = np.array([0.45, 0.55])
     clf = LinearDiscriminantAnalysis(priors=priors)
-    clf.fit(X, y)
+    assert_warns(UserWarning, clf.fit, X, y)
     assert_array_almost_equal(clf.priors_, prior_norm, 2)
 
 
@@ -325,3 +326,17 @@ def test_deprecated_lda_qda_deprecation():
 
     qda = assert_warns(DeprecationWarning, import_qda_module)
     assert qda.QDA is QuadraticDiscriminantAnalysis
+
+
+def test_covariance():
+    x, y = make_blobs(n_samples=100, n_features=5,
+                      centers=1, random_state=42)
+
+    # make features correlated
+    x = np.dot(x, np.arange(x.shape[1] ** 2).reshape(x.shape[1], x.shape[1]))
+
+    c_e = _cov(x, 'empirical')
+    assert_almost_equal(c_e, c_e.T)
+
+    c_s = _cov(x, 'auto')
+    assert_almost_equal(c_s, c_s.T)