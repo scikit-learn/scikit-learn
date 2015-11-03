49192ee5d14187abe0f8e9edff6694cc0d4cadb7
Ramana.S <vxrram95@gmail.com>
2015-11-04 03:06:06 +0530

    Fixed VisibleDeprecationWarning

--- sklearn/naive_bayes.py
+++ sklearn/naive_bayes.py
@@ -351,9 +351,10 @@ class GaussianNB(BaseNB):
         unique_y_in_classes = in1d(unique_y, classes)
 
         if not np.all(unique_y_in_classes):
+            not_unique_y_in_classes = ~unique_y_in_classes
             raise ValueError("The target label(s) %s in y do not exist in the "
                              "initial classes %s" %
-                             (y[~unique_y_in_classes], classes))
+                             (y[np.where(not_unique_y_in_classes)], classes))
 
         for y_i in unique_y:
             i = classes.searchsorted(y_i)
