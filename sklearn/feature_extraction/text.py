015c50518447e8ef3586c38d8d13bc41c104eeca
Mikhail Korobov <kmike84@gmail.com>
2015-11-14 16:50:07 +0500

    micro-optimize VectorizerMixin tokenizer

--- sklearn/feature_extraction/text.py
+++ sklearn/feature_extraction/text.py
@@ -210,8 +210,7 @@ class VectorizerMixin(object):
         """Return a function that splits a string into a sequence of tokens"""
         if self.tokenizer is not None:
             return self.tokenizer
-        token_pattern = re.compile(self.token_pattern)
-        return lambda doc: token_pattern.findall(doc)
+        return re.compile(self.token_pattern).findall
 
     def get_stop_words(self):
         """Build or fetch the effective stop words list"""
