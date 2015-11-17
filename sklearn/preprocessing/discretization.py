3e7d47585f8d6bc553d5bdc7f94f740940cf3bd9
hlin117 <hlin117@gmail.com>
2015-11-17 16:30:49 -0600

    #5778: Allowing Discretizer to accept 2D arrays

--- sklearn/preprocessing/discretization.py
+++ sklearn/preprocessing/discretization.py
@@ -1,16 +1,11 @@
-# Title          : discretization.py
-# Author         : Henry Lin <hlin117@gmail.com>
-# License        : BSD 3 clause
-#==============================================================================
-
 from __future__ import division
-import numpy as np
-from ..externals import six
-from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.utils import column_or_1d
-from sklearn.utils.validation import check_is_fitted
 
-xrange = six.moves.xrange
+__author__ = "Henry Lin <hlin117@gmail.com>"
+
+import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils import column_or_1d, check_array
+from sklearn.utils.validation import check_is_fitted
 
 __all__ = [
     "Discretizer"
@@ -25,6 +20,18 @@ class Discretizer(BaseEstimator, TransformerMixin):
     n_bins : int (default=2)
         The number of bins to produce. The intervals for the bins are
         determined by the minimum and maximum of the input data.
+        Raises ValueError if n_bins < 2.
+
+    categorical_features : array-like or None (default=None)
+        Specifies the indices of categorical columns which are not to
+        be discretized. If None, assumes that all columns are continuous
+        features.
+
+    strategy : str (default="uniform")
+        Specifies what discretization strategy should be used.
+
+        - Uniform: Intervals are spaced uniformly between the minimum
+        and maximum of a feature.
 
     Attributes
     ----------
@@ -38,69 +45,135 @@ class Discretizer(BaseEstimator, TransformerMixin):
         Contains the boundaries for which the data lies. Each interval
         has an open left boundary, and a closed right boundary.
 
-    spacing_ : float
-        The spacing between each one of the cut points. Calculated as
+    n_features_ : int
+        The number of features from the original dataset.
 
-            (max - min) / n_bins.
+    continuous_mask_ : array, shape [n_continuous_features_]
+        An array of booleans, representing columns for which there are
+        continuous features.
 
     Example
     -------
+    >>> # X has two examples, with X[:, 2] as categorical
+    >>> X = [[0, 1, 0, 4], \
+             [6, 7, 1, 5]]
     >>> from sklearn.preprocessing import Discretizer
-    >>> X = [0, 1, 2, 3, 4, 5, 6]
-    >>> discretizer = Discretizer(n_bins=3)
-    >>> discretizer.fit(X)
-    Discretizer(n_bins=3)
-    >>> discretizer.cut_points_
-    array([2., 4.])
+    >>> discretizer = Discretizer(n_bins=3, strategy="uniform", \
+                                  categorical_features=[2])
+    >>> discretizer.fit(X) # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
+    Discretizer(...)
+    >>> discretizer.cut_points_ # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
+    array([[ 2. , 3. , 4.3333...],
+           [ 4. , 5. , 4.6666...]])
+    >>> # Transforming X will move the categorical features to the last indices
     >>> discretizer.transform(X)
-    array([0, 0, 0, 1, 1, 2, 2])
+    array([[0, 0, 0, 0],
+           [2, 2, 2, 1]])
     """
 
-    def __init__(self, n_bins=2):
+    def __init__(self, n_bins=2, categorical_features="all", strategy="uniform"):
         if n_bins < 2:
             raise ValueError("Discretizer received an invalid number "
                              "of bins")
         self.n_bins = n_bins
+        self.categorical_features = categorical_features
+        self.strategy = strategy  # TODO: Add more strategies!
 
         # Attributes
         self.min_ = None
         self.max_ = None
         self.cut_points_ = None
-        self.spacing_ = None
+        self.n_features_ = None
+        self.continuous_mask_ = None
 
-    def fit(self, y):
+    def _set_continuous_mask(self):
+        """Sets a boolean array that determines which columns are
+        continuous features.
+        """
+        if self.categorical_features == "all":
+            self.n_continuous_features_ = self.n_features_
+            self.continuous_mask_ = np.ones(self.n_features_, dtype=bool)
+            return
+
+        if len(self.categorical_features) > self.n_features_:
+            raise ValueError("The number of categorical indices is more than "
+                             "the number of features in the dataset.")
+
+        self.continuous_mask_ = np.ones(self.n_features_, dtype=bool)
+        self.continuous_mask_[self.categorical_features] = False
+
+        # Checks if there are duplicate columns from self.categorical_features,
+        # such as [-1, 0, 0, self.n_features - 1]
+        if self.n_features_ - len(self.categorical_features) \
+                != self.continuous_mask_.sum():
+            raise ValueError("Duplicate indices detected from input "
+                             "categorical indices.")
+
+    def fit(self, X, y=None):
         """Finds the intervals of interest from the input data.
 
         Parameters
         ----------
-        y : array-like, shape [n_samples]
-            The array containing continuous features to be discretized.
-            Input must be 1d arrays.
+        X : array-like, shape [n_samples, n_features]
+            The input data containing continuous features to be
+            discretized. Categorical columns are indicated by
+            categorical_columns from the constructor.
 
+        Returns
+        -------
+        self
         """
 
-        y = column_or_1d(y)
+        X = check_array(X)
+        self.n_features_ = X.shape[1]
 
-        self.min_ = y.min()
-        self.max_ = y.max()
-        self.spacing_ = (self.max_ - self.min_) / self.n_bins
-        cut_points = (self.min_ + self.spacing_ * i
-                      for i in xrange(1, self.n_bins))
-        self.cut_points_ = np.array(list(cut_points))
+        # Set the mask for the continuous features in the array
+        self._set_continuous_mask()
+
+        continuous = X[:, self.continuous_mask_]
+
+        self.min_ = continuous.min(axis=0)
+        self.max_ = continuous.max(axis=0)
+        cut_points = list()
+        for min_, max_ in zip(self.min_, self.max_):
+            points = np.linspace(min_, max_, num=self.n_bins, endpoint=False)[1:]
+            cut_points.append(points.reshape(-1, 1))
+        self.cut_points_ = np.hstack(cut_points)
         return self
 
-    def transform(self, y):
+    def transform(self, X, y=None):
         """Discretizes the input data.
 
         Parameters
         ----------
-        y : array-like, shape [n_samples]
-            The array containing continuous features to be discretized.
-            Input must be 1d arrays.
+        X : array-like, shape [n_samples, n_features]
+            The input data containing continuous features to be
+            discretized. Categorical columns are indicated by
+            categorical_columns from the constructor.
+
+        Returns
+        -------
+        T : array-like, shape [n_samples, n_features]
+            Array containing discretized features alongside the
+            categorical features. Categorical features are represented
+            as the last features of the output.
 
         """
-        check_is_fitted(self, ["cut_points_"])
-        y = column_or_1d(y)
-        output = np.searchsorted(self.cut_points_, y)
-        return output
+        check_is_fitted(self, ["cut_points_", "continuous_mask_"])
+        X = check_array(X)
+        if X.shape[1] != self.n_features_:
+            raise ValueError("Transformed array does not have currect number "
+                             "of features. Expecting {0}, received {1}"
+                             .format(self.n_features_, X.shape[1]))
+
+        continuous = X[:, self.continuous_mask_]
+        discretized = list()
+        for cut_points, cont in zip(self.cut_points_.T, continuous.T):
+            dis_features = np.searchsorted(cut_points, cont)
+            discretized.append(dis_features.reshape(-1, 1))
+
+        discretized = np.hstack(discretized)
+        categorical = X[:, ~self.continuous_mask_]
+
+        return np.hstack((discretized, categorical))
 
---
+++ sklearn/preprocessing/tests/test_discretizer.py
@@ -0,0 +1,64 @@
+__author__ = "Henry Lin <hlin117@gmail.com>"
+
+from sklearn.preprocessing.discretization import Discretizer
+from sklearn.utils.testing import assert_array_almost_equal, assert_array_equal
+
+X = [[0, 0, 1],
+     [1, 1, 2],
+     [2, 0, 3]]
+
+def test_discretizer_bad_n_bins():
+    try:
+        dis = Discretizer(n_bins=1)
+    except:
+        return
+    else:
+        raise ValueError("Discretizer needs to raise error when number of "
+                         "bins is too low.")
+
+def test_discretizer_fit_transform():
+    discretized = Discretizer(n_bins=2).fit_transform(X)
+    expected = [[0, 0, 0],
+                [0, 1, 0],
+                [1, 0, 1]]
+    assert_array_equal(expected, discretized)
+
+def test_discretizer_fit_transform_cat():
+    dis = Discretizer(n_bins=3, categorical_features=[0])
+    discretized = dis.fit_transform(X)
+
+    expected = [[0, 0, 0],
+                [2, 1, 1],
+                [0, 2, 2]]
+
+    assert_array_equal(expected, discretized)
+
+def test_discretizer_fit():
+    dis = Discretizer(n_bins=3, categorical_features=[1]).fit(X)
+
+    expected = [True, False, True]
+    assert_array_almost_equal(expected, dis.continuous_mask_)
+
+    expected = [[0.666666, 1.666666],
+                [1.333333, 2.333333]]
+
+    assert_array_almost_equal(expected, dis.cut_points_)
+
+    expected = [0.0, 1.0]
+    assert_array_almost_equal(expected, dis.min_)
+
+    expected = [2.0, 3.0]
+    assert_array_almost_equal(expected, dis.max_)
+
+def test_discretizer_bad_cat_features():
+    bad_cats = [[-1, 2], [0, 0], [0, 1, 2, 3]]
+    for cats in bad_cats:
+        try:
+            dis = Discretizer(categorical_features=cats).fit(X)
+        except:
+            continue
+        else:
+            raise ValueError("Discretizer should have failed to fit with bad "
+                             "input categories. Failed to fail on {0}" \
+                             .format(cats))
+
