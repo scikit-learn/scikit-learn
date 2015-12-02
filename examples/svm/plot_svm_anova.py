7602a3f0862513dc25c3906e77e25f6ae650bcfb
Ganiev Ibraim <olologin@gmail.com>
2015-12-02 21:09:56 +0300

    It's better to fix a comment

--- examples/svm/plot_svm_anova.py
+++ examples/svm/plot_svm_anova.py
@@ -42,7 +42,7 @@ percentiles = (1, 3, 6, 10, 15, 20, 30, 40, 60, 80, 100)
 
 for percentile in percentiles:
     clf.set_params(anova__percentile=percentile)
-    # Compute cross-validation score using all CPUs
+    # Compute cross-validation score using 1 CPU
     this_scores = cross_val_score(clf, X, y, n_jobs=1)
     score_means.append(this_scores.mean())
     score_stds.append(this_scores.std())
