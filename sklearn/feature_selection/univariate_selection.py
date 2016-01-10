8331f7524d8c63807b03799ace5e93f9a5ee906f
dsquareindia <ashu.9412@gmail.com>
2016-01-10 22:34:54 +0530

    DOC: Fix redundant doc for f_regression.

--- sklearn/feature_selection/univariate_selection.py
+++ sklearn/feature_selection/univariate_selection.py
@@ -232,12 +232,12 @@ def f_regression(X, y, center=True):
     Quick linear model for testing the effect of a single regressor,
     sequentially for many regressors.
 
-    This is done in 3 steps:
+    This is done in 2 steps:
 
-    1. The regressor of interest and the data are orthogonalized
-       wrt constant regressors.
-    2. The cross correlation between data and regressors is computed.
-    3. It is converted to an F score then to a p-value.
+    1. The cross correlation between each regressor and the target is computed,
+       that is, ((X[:, i] - mean(X[:, i])) * (y - mean_y)) / (std(X[:, i]) *
+       std(y)).
+    2. It is converted to an F score then to a p-value.
 
     Read more in the :ref:`User Guide <univariate_feature_selection>`.
 
