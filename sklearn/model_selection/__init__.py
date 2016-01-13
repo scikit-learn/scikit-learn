4a872343a1e03b2654949e269ed6b1bb055958b5
Jamie Bull <jamie.bull@oco-carbon.com>
2016-01-13 17:23:27 +0100

    Refactor model_selection._search to include unsupervised estimators

--- sklearn/model_selection/__init__.py
+++ sklearn/model_selection/__init__.py
@@ -1,32 +1,34 @@
+from ._search import GridSearchCV
+from ._search import GridSearchCluster
+from ._search import ParameterGrid
+from ._search import ParameterSampler
+from ._search import RandomizedSearchCV
+from ._search import RandomizedSearchCluster
+from ._search import fit_grid_point
 from ._split import BaseCrossValidator
 from ._split import KFold
 from ._split import LabelKFold
-from ._split import StratifiedKFold
+from ._split import LabelShuffleSplit
 from ._split import LeaveOneLabelOut
 from ._split import LeaveOneOut
 from ._split import LeavePLabelOut
 from ._split import LeavePOut
-from ._split import ShuffleSplit
-from ._split import LabelShuffleSplit
-from ._split import StratifiedShuffleSplit
 from ._split import PredefinedSplit
-from ._split import train_test_split
+from ._split import ShuffleSplit
+from ._split import StratifiedKFold
+from ._split import StratifiedShuffleSplit
 from ._split import check_cv
-
-from ._validation import cross_val_score
+from ._split import train_test_split
 from ._validation import cross_val_predict
+from ._validation import cross_val_score
 from ._validation import learning_curve
 from ._validation import permutation_test_score
 from ._validation import validation_curve
 
-from ._search import GridSearchCV
-from ._search import RandomizedSearchCV
-from ._search import ParameterGrid
-from ._search import ParameterSampler
-from ._search import fit_grid_point
 
 __all__ = ('BaseCrossValidator',
            'GridSearchCV',
+           'GridSearchCluster',
            'KFold',
            'LabelKFold',
            'LabelShuffleSplit',
@@ -38,6 +40,7 @@ __all__ = ('BaseCrossValidator',
            'ParameterSampler',
            'PredefinedSplit',
            'RandomizedSearchCV',
+           'RandomizedSearchCluster',
            'ShuffleSplit',
            'StratifiedKFold',
            'StratifiedShuffleSplit',
