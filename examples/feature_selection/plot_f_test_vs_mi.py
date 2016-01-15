824dda37671fe32fd76e5790c4857c1c28f21da3
Nikolay Mayorov <nikolay.mayorov@zoho.com>
2016-01-15 22:24:41 +0500

    MAINT: Small changes to plot_f_test_vs_mi.py

--- examples/feature_selection/plot_f_test_vs_mi.py
+++ examples/feature_selection/plot_f_test_vs_mi.py
@@ -26,6 +26,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.feature_selection import f_regression, mutual_info_regression
 
+np.random.seed(0)
 X = np.random.rand(1000, 3)
 y = X[:, 0] + np.sin(6 * np.pi * X[:, 1]) + 0.1 * np.random.randn(1000)
 
@@ -45,3 +46,4 @@ for i in range(3):
     plt.title("F-test={:.2f}, MI={:.2f}".format(f_test[i], mi[i]),
               fontsize=16)
 plt.show()
+
