8394c1bfccba77f91947c38e41cfa8987f73df25
Nikolay Mayorov <nikolay.mayorov@zoho.com>
2016-01-15 21:28:56 +0500

    MAINT: Add blank lines between parameters in mutual_info_.py

--- sklearn/feature_selection/mutual_info_.py
+++ sklearn/feature_selection/mutual_info_.py
@@ -20,7 +20,8 @@ def _compute_mi_cc(x, y, n_neighbors):
     Parameters
     ----------
     x, y : ndarray
-        Samples from random variables, 1-d arrays of identical shape.
+        Samples of random variables, 1-d arrays of identical shape.
+
     n_neighbors : int
         Number of nearest neighbors to search for each point, see [1]_.
 
@@ -77,8 +78,10 @@ def _compute_mi_cd(c, d, n_neighbors):
     ----------
     c : ndarray
         Samples from a continuous random variable.
+
     d : ndarray
         Samples from a discrete random variable.
+
     n_neighbors : int
         Number of nearest neighbors to search for each point, see [1]_.
 
@@ -154,6 +157,7 @@ def _get_column(X, i):
     ----------
     X : ndarray or csc_matrix, shape (n_samples, n_features)
         Matrix from which to get a column.
+
     i : int
         Column index.
 
@@ -179,6 +183,7 @@ def _iterate_columns(X, columns=None):
     ----------
     X : ndarray or csc_matrix, shape (n_samples, n_features)
         Matrix over which to iterate.
+
     columns : iterable or None, default None
         Indices of columns to iterate over. If None, iterate over all columns.
 
@@ -202,23 +207,29 @@ def _estimate_mi(X, y, discrete_features='auto', discrete_target=False,
     ----------
     X : array_like or sparse matrix, shape (n_samples, n_features)
         Feature matrix.
+
     y : array_like, shape (n_samples,)
         Target vector.
+
     discrete_features : {'auto', bool, array_like}, default 'auto'
         If bool, then determines whether to consider all features discrete
         or continuous. If array, then it should be either a boolean mask
         with shape (n_features,) or array with indices of discrete features.
         If 'auto', it is assigned to False for dense `X` and to True for
         sparse `X`.
+
     discrete_target : bool, default False
         Whether to consider `y` as a discrete variable.
+
     n_neighbors : int, default 3
         Number of neighbors to use for MI estimation for continuous variables,
         see [1]_ and [2]_. Higher values reduce variance of the estimation, but
         could introduce a bias.
+
     copy : bool, default True
         Whether to make a copy of the given data. If set to False, the initial
         data will be overwritten.
+
     random_state : int seed, RandomState instance or None, default None
         The seed of the pseudo random number generator for adding small noise
         to continuous variables in order to remove repeated values.
@@ -301,21 +312,26 @@ def mutual_info_regression(X, y, discrete_features='auto', n_neighbors=3,
     ----------
     X : array_like or sparse matrix, shape (n_samples, n_features)
         Feature matrix.
+
     y : array_like, shape (n_samples,)
         Target vector.
+
     discrete_features : {'auto', bool, array_like}, default 'auto'
         If bool, then determines whether to consider all features discrete
         or continuous. If array, then it should be either a boolean mask
         with shape (n_features,) or array with indices of discrete features.
         If 'auto', it is assigned to False for dense `X` and to True for
         sparse `X`.
+
     n_neighbors : int, default 3
         Number of neighbors to use for MI estimation for continuous variables,
         see [2]_ and [3]_. Higher values reduce variance of the estimation, but
         could introduce a bias.
+
     copy : bool, default True
         Whether to make a copy of the given data. If set to False, the initial
         data will be overwritten.
+
     random_state : int seed, RandomState instance or None, default None
         The seed of the pseudo random number generator for adding small noise
         to continuous variables in order to remove repeated values.
@@ -368,21 +384,26 @@ def mutual_info_classif(X, y, discrete_features='auto', n_neighbors=3,
     ----------
     X : array_like or sparse matrix, shape (n_samples, n_features)
         Feature matrix.
+
     y : array_like, shape (n_samples,)
         Target vector.
+
     discrete_features : {'auto', bool, array_like}, default 'auto'
         If bool, then determines whether to consider all features discrete
         or continuous. If array, then it should be either a boolean mask
         with shape (n_features,) or array with indices of discrete features.
         If 'auto', it is assigned to False for dense `X` and to True for
         sparse `X`.
+
     n_neighbors : int, default 3
         Number of neighbors to use for MI estimation for continuous variables,
         see [2]_ and [3]_. Higher values reduce variance of the estimation, but
         could introduce a bias.
+
     copy : bool, default True
         Whether to make a copy of the given data. If set to False, the initial
         data will be overwritten.
+
     random_state : int seed, RandomState instance or None, default None
         The seed of the pseudo random number generator for adding small noise
         to continuous variables in order to remove repeated values.
