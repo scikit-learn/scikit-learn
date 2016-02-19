17c619db4b34220dcebfa0769d49b0075e411f23
hlin117 <hlin117@gmail.com>
2016-02-19 10:54:14 -0600

    Specifying that X_fit_ will not be None. @jakevdp

--- sklearn/decomposition/kernel_pca.py
+++ sklearn/decomposition/kernel_pca.py
@@ -86,9 +86,9 @@ class KernelPCA(BaseEstimator, TransformerMixin):
         If `-1`, then the number of jobs is set to the number of CPU cores.
 
     copy_X : boolean, default=True
-        If True, input X is copied and stored by the model. If no further
-        changes will be done to X, setting `copy_X=False` saves memory by
-        storing a reference.
+        If True, input X is copied and stored by the model in the `X_fit_`
+        attribute. If no further changes will be done to X, setting
+        `copy_X=False` saves memory by storing a reference.
 
     Attributes
     ----------
@@ -109,7 +109,7 @@ class KernelPCA(BaseEstimator, TransformerMixin):
         Projection of the fitted data on the kernel principal components.
 
     X_fit_ : (n_samples, n_features)
-        A copy of the original fitted data. If `copy_X=False`, X_fit_=None.
+        The data used to fit the model.
 
 
     References
