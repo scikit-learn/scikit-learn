823876a07f896e9637bdf2dc531e9fcf7fe29c54
Antony Lee <anntzer.lee@gmail.com>
2016-04-20 13:31:12 -0700

    Faster isotonic regression.

--- benchmarks/bench_isotonic.py
+++ benchmarks/bench_isotonic.py
@@ -22,8 +22,8 @@ import argparse
 
 
 def generate_perturbed_logarithm_dataset(size):
-    return np.random.randint(-50, 50, size=n) \
-        + 50. * np.log(1 + np.arange(n))
+    return (np.random.randint(-50, 50, size=size) +
+            50. * np.log(1 + np.arange(size)))
 
 
 def generate_logistic_dataset(size):
@@ -33,7 +33,9 @@ def generate_logistic_dataset(size):
 
 def generate_pathological_dataset(size):
     # Triggers O(n^2) complexity on the original implementation.
-    return np.r_[np.arange(n), np.arange(-(n - 1), n), np.arange(-(n - 1), 1)]
+    return np.r_[np.arange(size),
+                 np.arange(-(size - 1), size),
+                 np.arange(-(size - 1), 1)]
 
 
 DATASET_GENERATORS = {
@@ -59,6 +61,8 @@ def bench_isotonic_regression(Y):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="Isotonic Regression benchmark tool")
+    parser.add_argument('--seed', type=int,
+                        help="RNG seed")
     parser.add_argument('--iterations', type=int, required=True,
                         help="Number of iterations to average timings over "
                         "for each problem size")
@@ -72,6 +76,8 @@ if __name__ == '__main__':
                         required=True)
 
     args = parser.parse_args()
+
+    np.random.seed(args.seed)
 
     timings = []
     for exponent in range(args.log_min_problem_size,
