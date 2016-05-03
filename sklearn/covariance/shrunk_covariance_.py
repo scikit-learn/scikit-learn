7e8dac5fc95bc22f7e762dca9a28d66791b8a00a
okbalefthanded <okba.bekhelifi@gmail.com>
2016-05-03 22:49:59 +0200

    Update shrunk_covariance_.py

--- sklearn/covariance/shrunk_covariance_.py
+++ sklearn/covariance/shrunk_covariance_.py
@@ -487,7 +487,7 @@ class OAS(EmpiricalCovariance):
     article. It has been taken from the Matlab program available from the
     authors' webpage (http://tbayes.eecs.umich.edu/yilun/covestimation).
     In the original article, formula (23) states that 2/p is multiplied by 
-    Tr(S*S) in both the numerator and denominator, this operation is omitted
+    Trace(cov*cov) in both the numerator and denominator, this operation is omitted
     in the author's MATLAB program because for a large p, the value of 2/p is so 
     small that it doesn't affect the value of the estimator. 
 
