979591643397d78fcf27c51e2fbe6493d527a42f
Samuel St-Jean <stjeansam@gmail.com>
2016-06-02 15:43:47 +0200

    Doc: Fix typo (#6854)

--- sklearn/linear_model/least_angle.py
+++ sklearn/linear_model/least_angle.py
@@ -63,7 +63,7 @@ def lars_path(X, y, Xy=None, Gram=None, max_iter=500,
         When using this option together with method 'lasso' the model
         coefficients will not converge to the ordinary-least-squares solution
         for small values of alpha (neither will they when using method 'lar'
-        ..). Only coeffiencts up to the smallest alpha value (``alphas_[alphas_ >
+        ..). Only coefficients up to the smallest alpha value (``alphas_[alphas_ >
         0.].min()`` when fit_path=True) reached by the stepwise Lars-Lasso
         algorithm are typically in congruence with the solution of the
         coordinate descent lasso_path function.
