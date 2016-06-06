ae821aba7603295e26d320823b2d74beac7fefcc
Sagar Kar <sagarkar10@gmail.com>
2016-06-06 14:28:09 +0530

    Updated __init__.py with requred comment
    The question why we need the __init__.py is answered as comment. Issue #6274  and #6849 motivated me to write it, after having a hard time finding the possible break in testing.

---
+++ sklearn/tests/__init__.py
@@ -0,0 +1,4 @@
+'''
+The __init__.py file makes Python treat directories containing it as modules.
+Here it is required for nose tests to discover the directory, when run from outside the source directory.
+'''
