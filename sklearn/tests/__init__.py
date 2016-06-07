a8cdab89d82a06faf0b81c8a03ae285b22abe797
Sagar Kar <sagarkar10@gmail.com>
2016-06-07 10:57:17 +0530

    Updated with rectification

--- sklearn/tests/__init__.py
+++ sklearn/tests/__init__.py
@@ -1,4 +1,6 @@
 '''
-The __init__.py file makes Python treat directories containing it as modules.
-Here it is required for nose tests to discover the directory, when run from outside the source directory.
+The __init__.py file makes Python treat directories containing it as module/package.
+If test folders have a __init__.py then it is installed in site-packages so when you run nosetests sklearn 
+outside of the source tree after doing python setup.py install you will find the tests that have been installed in site-packages.
+Omiting the __init__.py before setup.py installation don't install the test!
 '''
