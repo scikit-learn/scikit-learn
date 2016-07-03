c0fa4141f8e1471eb95e529359c6b6fda2ab69d2
Umnov Alexey <alexeyum@gmail.com>
2016-07-03 13:18:33 +0300

    Added usage of dict_learning interface in K-SVD benchmark

--- benchmarks/bench_ksvd.py
+++ benchmarks/bench_ksvd.py
@@ -6,7 +6,7 @@ from time import time
 import numpy as np
 import scipy.misc
 
-from sklearn.decomposition import learn_dictionary_ksvd
+from sklearn.decomposition import dict_learning
 from sklearn.feature_extraction.image import extract_patches_2d
 from sklearn.utils.random import choice
 
@@ -15,7 +15,7 @@ RANDOM_STATE = np.random.RandomState(0)
 
 
 def compute_ksvd(patch_size, samples_count, n_components, n_nonzero_coefs,
-                 n_iters, approximate_svd):
+                 max_iter, approximate_svd):
     image = scipy.misc.lena() / 256.0
     image_distorted = image + 0.1 * RANDOM_STATE.randn(*image.shape)
 
@@ -29,21 +29,26 @@ def compute_ksvd(patch_size, samples_count, n_components, n_nonzero_coefs,
     data = data_full[samples_indexes, :]
 
     t0 = time()
-    learn_dictionary_ksvd(data, n_nonzero_coefs, n_components, n_iters,
-                          approximate_svd=approximate_svd)
+    if approximate_svd:
+        method = 'ksvd'
+    else:
+        method = 'exact_ksvd'
+
+    dict_learning(data, n_components, n_nonzero_coefs=n_nonzero_coefs,
+                  max_iter=max_iter, method=method)
     dt = time() - t0
     return dt
 
 
 BENCHMARKS_PARAMETERS = [
     dict(patch_size=4, samples_count=5000, n_components=50,
-         n_nonzero_coefs=2, n_iters=25, approximate_svd=True),
+         n_nonzero_coefs=2, max_iter=25, approximate_svd=True),
     dict(patch_size=4, samples_count=5000, n_components=50,
-         n_nonzero_coefs=2, n_iters=25, approximate_svd=False),
+         n_nonzero_coefs=2, max_iter=25, approximate_svd=False),
     dict(patch_size=4, samples_count=10000, n_components=100,
-         n_nonzero_coefs=2, n_iters=25, approximate_svd=True),
+         n_nonzero_coefs=2, max_iter=25, approximate_svd=True),
     dict(patch_size=6, samples_count=10000, n_components=100,
-         n_nonzero_coefs=2, n_iters=25, approximate_svd=True),
+         n_nonzero_coefs=2, max_iter=25, approximate_svd=True),
 ]
 
 
