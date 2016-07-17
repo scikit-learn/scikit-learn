57d1d87d8bd0c668121e6dbfe91ca57ed1b2d237
Julia Galstad <julia.galstad@gmail.com>
2016-07-17 10:14:35 -0500

    temporary commit to check nosetests on travis

--- sklearn/mixture/tests/test_dpgmm.py
+++ sklearn/mixture/tests/test_dpgmm.py
@@ -35,186 +35,185 @@ def test_class_weights():
         assert_array_less(dpgmm.weights_[~active], .05)
 
 
-@ignore_warnings(category=DeprecationWarning)
-def test_verbose_boolean():
-    # checks that the output for the verbose output is the same
-    # for the flag values '1' and 'True'
-    # simple 3 cluster dataset
-    X, y = make_blobs(random_state=1)
-    for Model in [DPGMM, VBGMM]:
-        dpgmm_bool = Model(n_components=10, random_state=1, alpha=20,
-                           n_iter=50, verbose=True)
-        dpgmm_int = Model(n_components=10, random_state=1, alpha=20,
-                          n_iter=50, verbose=1)
+# @ignore_warnings(category=DeprecationWarning)
+# def test_verbose_boolean():
+#     # checks that the output for the verbose output is the same
+#     # for the flag values '1' and 'True'
+#     # simple 3 cluster dataset
+#     X, y = make_blobs(random_state=1)
+#     for Model in [DPGMM, VBGMM]:
+#         dpgmm_bool = Model(n_components=10, random_state=1, alpha=20,
+#                            n_iter=50, verbose=True)
+#         dpgmm_int = Model(n_components=10, random_state=1, alpha=20,
+#                           n_iter=50, verbose=1)
 
-        old_stdout = sys.stdout
-        sys.stdout = StringIO()
-        try:
-            # generate output with the boolean flag
-            dpgmm_bool.fit(X)
-            verbose_output = sys.stdout
-            verbose_output.seek(0)
-            bool_output = verbose_output.readline()
-            # generate output with the int flag
-            dpgmm_int.fit(X)
-            verbose_output = sys.stdout
-            verbose_output.seek(0)
-            int_output = verbose_output.readline()
-            assert_equal(bool_output, int_output)
-        finally:
-            sys.stdout = old_stdout
-
-
-@ignore_warnings(category=DeprecationWarning)
-def test_verbose_first_level():
-    # simple 3 cluster dataset
-    X, y = make_blobs(random_state=1)
-    for Model in [DPGMM, VBGMM]:
-        dpgmm = Model(n_components=10, random_state=1, alpha=20, n_iter=50,
-                      verbose=1)
-
-        old_stdout = sys.stdout
-        sys.stdout = StringIO()
-        try:
-            dpgmm.fit(X)
-        finally:
-            sys.stdout = old_stdout
+#         old_stdout = sys.stdout
+#         sys.stdout = StringIO()
+#         try:
+#             # generate output with the boolean flag
+#             dpgmm_bool.fit(X)
+#             verbose_output = sys.stdout
+#             verbose_output.seek(0)
+#             bool_output = verbose_output.readline()
+#             # generate output with the int flag
+#             dpgmm_int.fit(X)
+#             verbose_output = sys.stdout
+#             verbose_output.seek(0)
+#             int_output = verbose_output.readline()
+#             assert_equal(bool_output, int_output)
+#         finally:
+#             sys.stdout = old_stdout
 
 
-@ignore_warnings(category=DeprecationWarning)
-def test_verbose_second_level():
-    # simple 3 cluster dataset
-    X, y = make_blobs(random_state=1)
-    for Model in [DPGMM, VBGMM]:
-        dpgmm = Model(n_components=10, random_state=1, alpha=20, n_iter=50,
-                      verbose=2)
+# @ignore_warnings(category=DeprecationWarning)
+# def test_verbose_first_level():
+#     # simple 3 cluster dataset
+#     X, y = make_blobs(random_state=1)
+#     for Model in [DPGMM, VBGMM]:
+#         dpgmm = Model(n_components=10, random_state=1, alpha=20, n_iter=50,
+#                       verbose=1)
 
-        old_stdout = sys.stdout
-        sys.stdout = StringIO()
-        try:
-            dpgmm.fit(X)
-        finally:
-            sys.stdout = old_stdout
-
-
-@ignore_warnings(category=DeprecationWarning)
-def test_digamma():
-    assert_warns_message(DeprecationWarning, "The function digamma is"
-                         " deprecated in 0.18 and will be removed in 0.20. "
-                         "Use scipy.special.digamma instead.", digamma, 3)
+#         old_stdout = sys.stdout
+#         sys.stdout = StringIO()
+#         try:
+#             dpgmm.fit(X)
+#         finally:
+#             sys.stdout = old_stdout
 
 
-@ignore_warnings(category=DeprecationWarning)
-def test_gammaln():
-    assert_warns_message(DeprecationWarning, "The function gammaln"
-                         " is deprecated in 0.18 and will be removed"
-                         " in 0.20. Use scipy.special.gammaln instead.",
-                         gammaln, 3)
+# @ignore_warnings(category=DeprecationWarning)
+# def test_verbose_second_level():
+#     # simple 3 cluster dataset
+#     X, y = make_blobs(random_state=1)
+#     for Model in [DPGMM, VBGMM]:
+#         dpgmm = Model(n_components=10, random_state=1, alpha=20, n_iter=50,
+#                       verbose=2)
+
+#         old_stdout = sys.stdout
+#         sys.stdout = StringIO()
+#         try:
+#             dpgmm.fit(X)
+#         finally:
+#             sys.stdout = old_stdout
 
 
-@ignore_warnings(category=DeprecationWarning)
-def test_log_normalize():
-    v = np.array([0.1, 0.8, 0.01, 0.09])
-    a = np.log(2 * v)
-    result = assert_warns_message(DeprecationWarning, "The function "
-                                  "log_normalize is deprecated in 0.18 and"
-                                  " will be removed in 0.20.",
-                                  log_normalize, a)
-    assert np.allclose(v, result, rtol=0.01)
+# @ignore_warnings(category=DeprecationWarning)
+# def test_digamma():
+#     assert_warns_message(DeprecationWarning, "The function digamma is"
+#                          " deprecated in 0.18 and will be removed in 0.20. "
+#                          "Use scipy.special.digamma instead.", digamma, 3)
 
 
-@ignore_warnings(category=DeprecationWarning)
-def test_wishart_log_det():
-    a = np.array([0.1, 0.8, 0.01, 0.09])
-    b = np.array([0.2, 0.7, 0.05, 0.1])
-    assert_warns_message(DeprecationWarning, "The function "
-                         "wishart_log_det is deprecated in 0.18 and"
-                         " will be removed in 0.20.",
-                         wishart_log_det, a, b, 2, 4)
+# @ignore_warnings(category=DeprecationWarning)
+# def test_gammaln():
+#     assert_warns_message(DeprecationWarning, "The function gammaln"
+#                          " is deprecated in 0.18 and will be removed"
+#                          " in 0.20. Use scipy.special.gammaln instead.",
+#                          gammaln, 3)
 
 
-@ignore_warnings(category=DeprecationWarning)
-def test_wishart_logz():
-    assert_warns_message(DeprecationWarning, "The function "
-                         "wishart_logz is deprecated in 0.18 and "
-                         "will be removed in 0.20.", wishart_logz,
-                         3, np.identity(3), 1, 3)
+# @ignore_warnings(category=DeprecationWarning)
+# def test_log_normalize():
+#     v = np.array([0.1, 0.8, 0.01, 0.09])
+#     a = np.log(2 * v)
+#     result = assert_warns_message(DeprecationWarning, "The function "
+#                                   "log_normalize is deprecated in 0.18 and"
+#                                   " will be removed in 0.20.",
+#                                   log_normalize, a)
+#     assert np.allclose(v, result, rtol=0.01)
 
 
-@ignore_warnings(category=DeprecationWarning)
-def test_DPGMM_deprecation():
-    assert_warns_message(DeprecationWarning, "The DPGMM class is"
-                         " not working correctly and it's better "
-                         "to not use it. DPGMM is deprecated in 0.18 "
-                         "and will be removed in 0.20.", DPGMM)
+# @ignore_warnings(category=DeprecationWarning)
+# def test_wishart_log_det():
+#     a = np.array([0.1, 0.8, 0.01, 0.09])
+#     b = np.array([0.2, 0.7, 0.05, 0.1])
+#     assert_warns_message(DeprecationWarning, "The function "
+#                          "wishart_log_det is deprecated in 0.18 and"
+#                          " will be removed in 0.20.",
+#                          wishart_log_det, a, b, 2, 4)
 
 
-@ignore_warnings(category=DeprecationWarning)
+# @ignore_warnings(category=DeprecationWarning)
+# def test_wishart_logz():
+#     assert_warns_message(DeprecationWarning, "The function "
+#                          "wishart_logz is deprecated in 0.18 and "
+#                          "will be removed in 0.20.", wishart_logz,
+#                          3, np.identity(3), 1, 3)
+
+
+# @ignore_warnings(category=DeprecationWarning)
+# def test_DPGMM_deprecation():
+#     assert_warns_message(DeprecationWarning, "The DPGMM class is"
+#                          " not working correctly and it's better "
+#                          "to not use it. DPGMM is deprecated in 0.18 "
+#                          "and will be removed in 0.20.", DPGMM)
+
+# 
 def do_model(self, **kwds):
     return VBGMM(verbose=False, **kwds)
 
 
-class DPGMMTester(GMMTester):
-    model = DPGMM
-    do_test_eval = False
+# class DPGMMTester(GMMTester):
+#     model = DPGMM
+#     do_test_eval = False
 
-    def score(self, g, train_obs):
-        _, z = g.score_samples(train_obs)
-        return g.lower_bound(train_obs, z)
-
-
-class TestDPGMMWithSphericalCovars(unittest.TestCase, DPGMMTester):
-    covariance_type = 'spherical'
-    setUp = GMMTester._setUp
+#     def score(self, g, train_obs):
+#         _, z = g.score_samples(train_obs)
+#         return g.lower_bound(train_obs, z)
 
 
-class TestDPGMMWithDiagCovars(unittest.TestCase, DPGMMTester):
-    covariance_type = 'diag'
-    setUp = GMMTester._setUp
+# class TestDPGMMWithSphericalCovars(unittest.TestCase, DPGMMTester):
+#     covariance_type = 'spherical'
+#     setUp = GMMTester._setUp
 
 
-class TestDPGMMWithTiedCovars(unittest.TestCase, DPGMMTester):
-    covariance_type = 'tied'
-    setUp = GMMTester._setUp
+# class TestDPGMMWithDiagCovars(unittest.TestCase, DPGMMTester):
+#     covariance_type = 'diag'
+#     setUp = GMMTester._setUp
 
 
-class TestDPGMMWithFullCovars(unittest.TestCase, DPGMMTester):
-    covariance_type = 'full'
-    setUp = GMMTester._setUp
+# class TestDPGMMWithTiedCovars(unittest.TestCase, DPGMMTester):
+#     covariance_type = 'tied'
+#     setUp = GMMTester._setUp
 
 
-@ignore_warnings(category=DeprecationWarning)
-def test_VBGMM_deprecation():
-    assert_warns_message(DeprecationWarning, "The VBGMM class is"
-                         " not working correctly and it's better"
-                         " to not use it. VBGMM is deprecated in 0.18"
-                         " and will be removed in 0.20.", VBGMM)
+# class TestDPGMMWithFullCovars(unittest.TestCase, DPGMMTester):
+#     covariance_type = 'full'
+#     setUp = GMMTester._setUp
 
 
-class VBGMMTester(GMMTester):
-    model = do_model
-    do_test_eval = False
-
-    def score(self, g, train_obs):
-        _, z = g.score_samples(train_obs)
-        return g.lower_bound(train_obs, z)
-
-
-class TestVBGMMWithSphericalCovars(unittest.TestCase, VBGMMTester):
-    covariance_type = 'spherical'
-    setUp = GMMTester._setUp
+# @ignore_warnings(category=DeprecationWarning)
+# def test_VBGMM_deprecation():
+#     assert_warns_message(DeprecationWarning, "The VBGMM class is"
+#                          " not working correctly and it's better"
+#                          " to not use it. VBGMM is deprecated in 0.18"
+#                          " and will be removed in 0.20.", VBGMM)
 
 
-class TestVBGMMWithDiagCovars(unittest.TestCase, VBGMMTester):
-    covariance_type = 'diag'
-    setUp = GMMTester._setUp
+# class VBGMMTester(GMMTester):
+#     model = do_model
+#     do_test_eval = False
+
+#     def score(self, g, train_obs):
+#         _, z = g.score_samples(train_obs)
+#         return g.lower_bound(train_obs, z)
 
 
-class TestVBGMMWithTiedCovars(unittest.TestCase, VBGMMTester):
-    covariance_type = 'tied'
-    setUp = GMMTester._setUp
+# class TestVBGMMWithSphericalCovars(unittest.TestCase, VBGMMTester):
+#     covariance_type = 'spherical'
+#     setUp = GMMTester._setUp
 
 
-class TestVBGMMWithFullCovars(unittest.TestCase, VBGMMTester):
-    covariance_type = 'full'
-    setUp = GMMTester._setUp
+# class TestVBGMMWithDiagCovars(unittest.TestCase, VBGMMTester):
+#     covariance_type = 'diag'
+#     setUp = GMMTester._setUp
+
+
+# class TestVBGMMWithTiedCovars(unittest.TestCase, VBGMMTester):
+#     covariance_type = 'tied'
+#     setUp = GMMTester._setUp
+
+
+# class TestVBGMMWithFullCovars(unittest.TestCase, VBGMMTester):
+#     covariance_type = 'full'
+#     setUp = GMMTester._setUp
