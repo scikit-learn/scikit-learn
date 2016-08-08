ab4d8467ccd1586701c7bd698cfeeff460987b34
Boxiang Sun <daetalusun@gmail.com>
2016-08-08 16:40:45 +0800

    Get right type of _WrapperDescriptor both in CPython and Pyston
    The type of type.__call__ is instancemethod in Pyston. And the function
    of `signature` rely on it to avoid recursion and segfault(mentioned in
    the comment of `signature` function. Get the type of wrapper_descriptor
    by bytearray.__add__, and get method_wrapper by `u'str'.__add__`. These
    could both work for Pyston, CPython 2 and CPython 3.

--- sklearn/externals/funcsigs.py
+++ sklearn/externals/funcsigs.py
@@ -20,8 +20,8 @@ __version__ = "0.4"
 __all__ = ['BoundArguments', 'Parameter', 'Signature', 'signature']
 
 
-_WrapperDescriptor = type(type.__call__)
-_MethodWrapper = type(all.__call__)
+_WrapperDescriptor = type(bytearray.__add__)
+_MethodWrapper = type(u'str'.__add__)
 
 _NonUserDefinedCallables = (_WrapperDescriptor,
                             _MethodWrapper,
