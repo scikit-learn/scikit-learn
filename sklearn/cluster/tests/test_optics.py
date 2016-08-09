517a72e826fea8af6a51d9e187f2a3834e721d65
Shane <shane@geog.ucsb.edu>
2016-08-09 15:20:31 -0600

    fixing test error
    setting minPts to < data size returns None (with error message)

--- sklearn/cluster/tests/test_optics.py
+++ sklearn/cluster/tests/test_optics.py
@@ -22,6 +22,23 @@ def test_optics():
     n_clusters_1 = len(set(clust.labels_)) - int(-1 in clust.labels_)
     assert_equal(n_clusters_1, n_clusters)
 
+def test_filter():
+    '''
+    Tests the filter function.
+    '''
+
+    n_clusters = 3
+    X = generate_clustered_data(n_clusters=n_clusters)
+    # Parameters chosen specifically for this task.
+    # Compute OPTICS
+    clust = OPTICS(eps=6.0, min_samples=4, metric='euclidean')
+    clust.fit(X)
+    # number of clusters, ignoring noise if present
+    n_clusters_1 = len(set(clust.labels_)) - int(-1 in clust.labels_)
+    assert_equal(n_clusters_1, n_clusters)
+
+
+
 def test_optics2():
     '''
     Tests the optics clustering method and all functions inside it
@@ -42,10 +59,11 @@ def test_optics2():
 
     clust2 = clust.fit(X)
 
-    samples, labels = clust2.extract(0.4, 'dbscan')
+    assert clust2 == None
+    # samples, labels = clust2.extract(0.4, 'dbscan')
 
-    assert samples.size == 0
-    assert labels[0] == -1
+    # assert samples.size == 0
+    # assert labels[0] == -1
 
 
 def test_empty_extract():
