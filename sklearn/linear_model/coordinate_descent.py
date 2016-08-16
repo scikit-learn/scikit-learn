dd4a42ea6a1aa219d24f79cd1886e7e259cd2969
YenChenLin <yenchenlin1994@gmail.com>
2016-08-16 13:49:00 +0800

    Add concrete value

--- sklearn/linear_model/coordinate_descent.py
+++ sklearn/linear_model/coordinate_descent.py
@@ -477,8 +477,9 @@ def enet_path(X, y, l1_ratio=0.5, eps=1e-3, n_alphas=100, alphas=None,
                           ConvergenceWarning)
             if X.dtype == np.float32:
                 warnings.warn('It may cause by precision issues' +
-                              ' when fitting float32 data with small alpha.'
-                              ' Try to increase alpha of your model.')
+                              ' when fitting float32 data with small alpha,' +
+                              ' e.g., 1e-8.' +
+                              'Try to increase alpha of your model.')
 
         if verbose:
             if verbose > 2:
