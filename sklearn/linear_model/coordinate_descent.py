82fdf0962e3c7b0965b54ca137a56ab6d01fc226
YenChenLin <yenchenlin1994@gmail.com>
2016-08-23 19:16:43 +0800

    Add error msg

--- sklearn/linear_model/coordinate_descent.py
+++ sklearn/linear_model/coordinate_descent.py
@@ -473,7 +473,9 @@ def enet_path(X, y, l1_ratio=0.5, eps=1e-3, n_alphas=100, alphas=None,
         if dual_gap_ > eps_:
             warnings.warn('Objective did not converge.' +
                           ' You might want' +
-                          ' to increase the number of iterations.',
+                          ' to increase the number of iterations.' +
+                          ' Fitting data with alpha near zero, e.g., 1e-8,' +
+                          ' may cause precision problems.',
                           ConvergenceWarning)
 
         if verbose:
