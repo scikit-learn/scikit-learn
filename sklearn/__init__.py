85899419ac2b2e4f2a3c63802527ba4cb5a502c8
Olivier Grisel <olivier.grisel@ensta.org>
2016-09-13 19:25:31 +0200

    MAINT master is now 0.19.dev0

--- sklearn/__init__.py
+++ sklearn/__init__.py
@@ -37,7 +37,7 @@ warnings.filterwarnings('always', category=DeprecationWarning,
 # Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
 # 'X.Y.dev0' is the canonical version of 'X.Y.dev'
 #
-__version__ = '0.18.dev0'
+__version__ = '0.19.dev0'
 
 
 try:
