cef2b62701f80ff50a37528b5337dd9a96f0069e
Olivier Grisel <olivier.grisel@ensta.org>
2016-09-14 21:42:34 +0200

    Release 0.18rc2

--- sklearn/__init__.py
+++ sklearn/__init__.py
@@ -37,7 +37,7 @@ warnings.filterwarnings('always', category=DeprecationWarning,
 # Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
 # 'X.Y.dev0' is the canonical version of 'X.Y.dev'
 #
-__version__ = '0.18rc1'
+__version__ = '0.18rc2'
 
 
 try:
