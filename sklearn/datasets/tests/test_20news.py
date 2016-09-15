1ec701d7d44c1d09b865d5d2f4e49e5c776727c8
Nelson Liu <nelson.liu.2009@gmail.com>
2016-09-14 20:34:54 -0700

    fix test_20news_vectorized

--- sklearn/datasets/tests/test_20news.py
+++ sklearn/datasets/tests/test_20news.py
@@ -62,18 +62,18 @@ def test_20news_vectorized():
 
     bunch = datasets.fetch_20newsgroups_vectorized(subset="train")
     assert_true(sp.isspmatrix_csr(bunch.data))
-    assert_equal(bunch.data.shape, (11314, 107428))
+    assert_equal(bunch.data.shape, (11314, 130107))
     assert_equal(bunch.target.shape[0], 11314)
     assert_equal(bunch.data.dtype, np.float64)
 
     bunch = datasets.fetch_20newsgroups_vectorized(subset="test")
     assert_true(sp.isspmatrix_csr(bunch.data))
-    assert_equal(bunch.data.shape, (7532, 107428))
+    assert_equal(bunch.data.shape, (7532, 130107))
     assert_equal(bunch.target.shape[0], 7532)
     assert_equal(bunch.data.dtype, np.float64)
 
     bunch = datasets.fetch_20newsgroups_vectorized(subset="all")
     assert_true(sp.isspmatrix_csr(bunch.data))
-    assert_equal(bunch.data.shape, (11314 + 7532, 107428))
+    assert_equal(bunch.data.shape, (11314 + 7532, 130107))
     assert_equal(bunch.target.shape[0], 11314 + 7532)
     assert_equal(bunch.data.dtype, np.float64)
