c2371dd9d8a4eb31a9d22b67848487af48689644
ndingwall <ndingwall@gmail.com>
2016-09-16 14:47:43 +0100

    Change interpolation options to None and 'eleven_point'

--- sklearn/metrics/ranking.py
+++ sklearn/metrics/ranking.py
@@ -158,11 +158,12 @@ def auc(x, y, reorder=False, interpolation='linear',
 
 
 def average_precision_score(y_true, y_score, average="macro",
-                            sample_weight=None, interpolation="linear"):
+                            sample_weight=None, interpolation=None):
     """Compute average precision (AP) from prediction scores
 
-    This score corresponds to the area under the precision-recall curve, where
-    points are joined using either linear or step-wise interpolation.
+    Optionally, this will compute an eleven-point interpolated average
+    precision score, which fixes recall values and averages precisions
+    corresponding to those recall values.
 
     Note: this implementation is restricted to the binary classification task
     or multilabel classification task.
@@ -197,15 +198,20 @@ def average_precision_score(y_true, y_score, average="macro",
     sample_weight : array-like of shape = [n_samples], optional
         Sample weights.
 
-    interpolation : string ['linear' (default), 'step']
-        Determines the kind of interpolation used when computed AUC. If there
-        are many repeated scores, 'step' is recommended to avoid under- or
-        over-estimating the AUC.
+    interpolation : None (default), or string ['eleven_point']
 
-        ``'linear'``:
-            Linearly interpolates between operating points.
-        ``'step'``:
-            Uses a step function to interpolate between operating points.
+        ``None``:
+            Do not interpolate the average precision. Instead, compute
+            sum(p[i] * (r[i] - r[i-1])) for all precision, recall pairs
+            p[i], r[i] for i ≥ 1. This is the primary definition used in
+            the `Wikipedia entry for the Average precision
+            <http://en.wikipedia.org/wiki/Average_precision>`
+        ``'eleven_point'``:
+            For each of the recall values, r in {0, 0.1, 0.2, ..., 1.0},
+            compute the arithmetic mean of the first precision value with a
+            corresponding recall ≥ r. See the `Stanford Information Retrieval
+            book <http://nlp.stanford.edu/IR-book/html/htmledition/
+            evaluation-of-ranked-retrieval-results-1.html>`
 
     Returns
     -------
@@ -215,9 +221,10 @@ def average_precision_score(y_true, y_score, average="macro",
     ----------
     .. [1] `Wikipedia entry for the Average precision
            <http://en.wikipedia.org/wiki/Average_precision>`_
-    .. [2] `Roam Analytics blog post
-           <https://github.com/roaminsight/roamresearch/blob/master/BlogPosts/
-           Average_precision/Average_precision_post.ipynb>`
+    .. [2] `Stanford Information Retrieval book
+            <http://nlp.stanford.edu/IR-book/html/htmledition/
+            evaluation-of-ranked-retrieval-results-1.html>`_
+
     See also
     --------
     roc_auc_score : Area under the ROC curve
@@ -232,28 +239,57 @@ def average_precision_score(y_true, y_score, average="macro",
     >>> y_true = np.array([0, 0, 1, 1])
     >>> y_scores = np.array([0.1, 0.4, 0.35, 0.8])
     >>> average_precision_score(y_true, y_scores)  # doctest: +ELLIPSIS
-    0.79...
+    0.83...
+    >>> import numpy as np
+    >>> from sklearn.metrics import average_precision_score
+    >>> yt = np.array([0, 0, 1, 1])
+    >>> ys = np.array([0.1, 0.4, 0.35, 0.8])
+    >>> ap = average_precision_score(yt, ys, interpolation='eleven_point')
+    >>> ap # doctest: +ELLIPSIS
+    0.84...
 
     """
-    def _binary_average_precision(y_true, y_score, sample_weight=None):
+    def _binary_uninterpolated_average_precision(
+            y_true, y_score, sample_weight=None):
         precision, recall, thresholds = precision_recall_curve(
             y_true, y_score, sample_weight=sample_weight)
-        return auc(recall, precision, interpolation=interpolation,
-                   interpolation_direction='right')
+        return -sum(np.diff(recall) * np.array(precision)[:-1])
 
-    if interpolation == "linear":
-        # Check for number of unique predictions. If this is substantially less
-        # than the number of predictions, linear interpolation is likely to be
-        # biased.
-        n_discrete_predictions = len(np.unique(y_score))
-        if n_discrete_predictions < 0.75 * len(y_score):
-            warnings.warn("Number of unique scores is less than 75% of the "
-                          "number of scores provided. Linear interpolation "
-                          "is likely to be biased in this case. You may wish "
-                          "to use step interpolation instead. See docstring "
-                          "for details.")
-    return _average_binary_score(_binary_average_precision, y_true, y_score,
-                                 average, sample_weight=sample_weight)
+    def _binary_eleven_point_average_precision(
+            y_true, y_score, sample_weight=None):
+        precision, recall, thresholds = precision_recall_curve(
+            y_true, y_score)
+
+        # We need the recall values in ascending order
+        precision = list(reversed(precision))
+        recall = list(reversed(recall))
+
+        eleven_precisions = list()
+        # Don't use the first (precision, recall) pair with precision = 1.
+        start = 1
+        for target_recall in np.arange(0, 1.1, 0.1):
+            for i in range(start, len(precision) + 1):
+                if recall[i] >= target_recall:
+                    eleven_precisions.append(precision[i])
+                    start = i
+                    break  # Go to next target_recall
+        assert len(eleven_precisions) == 11, \
+            "Ended up with the wrong number of precision values. Should be 11."
+        return np.mean(eleven_precisions)
+
+    if interpolation is None:
+        return _average_binary_score(_binary_uninterpolated_average_precision,
+                                     y_true, y_score, average,
+                                     sample_weight=sample_weight)
+
+    elif interpolation == 'eleven_point':
+        return _average_binary_score(_binary_eleven_point_average_precision,
+                                     y_true, y_score, average,
+                                     sample_weight=sample_weight)
+
+    else:
+        raise ValueError("interpolation has to be one of "
+                         "(None, 'eleven_points').")
 
 
 def roc_auc_score(y_true, y_score, average="macro", sample_weight=None):
