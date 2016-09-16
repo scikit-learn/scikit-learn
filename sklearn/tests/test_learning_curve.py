e257d4c8825dfcbe0407f497b42b69dad944f864
Aleksandr Sandrovskii <aleksandr.sandrovskii@lazada.com>
2016-09-16 19:41:19 +0800

    Add test to verify validation_curve clones estimator

--- sklearn/tests/test_learning_curve.py
+++ sklearn/tests/test_learning_curve.py
@@ -6,6 +6,7 @@ import sys
 from sklearn.externals.six.moves import cStringIO as StringIO
 import numpy as np
 import warnings
+import math
 from sklearn.base import BaseEstimator
 from sklearn.utils.testing import assert_raises
 from sklearn.utils.testing import assert_warns
@@ -79,6 +80,30 @@ class MockEstimatorWithParameter(BaseEstimator):
 
     def score(self, X=None, y=None):
         return self.param if self._is_training_data(X) else 1 - self.param
+
+    def _is_training_data(self, X):
+        return X is self.X_subset
+
+
+class MockImprovingEstimatorWithParameter(BaseEstimator):
+    """Dummy classifier that improves with the size of a training set"""
+    def __init__(self, param=0.5):
+        self.X_subset = None
+        self.param = param
+        self.error_decay = 0.9
+        self.error = 0.5
+
+    def fit(self, X_subset, y_subset):
+        self.error *= math.pow(self.error_decay, len(X_subset)) * self.param
+        self.X_subset = X_subset
+        self.train_sizes = X_subset.shape[0]
+        return self
+
+    def predict(self, X):
+        raise NotImplementedError
+
+    def score(self, X=None, y=None):
+        return self.error if self._is_training_data(X) else 1 - self.error
 
     def _is_training_data(self, X):
         return X is self.X_subset
@@ -257,3 +282,22 @@ def test_validation_curve():
 
     assert_array_almost_equal(train_scores.mean(axis=1), param_range)
     assert_array_almost_equal(test_scores.mean(axis=1), 1 - param_range)
+
+
+def test_validation_curve_clone_estimator():
+    X, y = make_classification(n_samples=2, n_features=1, n_informative=1,
+                               n_redundant=0, n_classes=2,
+                               n_clusters_per_class=1, random_state=0)
+
+
+    param_range = np.linspace(1, 0, 10)
+    with warnings.catch_warnings(record=True) as w:
+        train_scores, test_scores = validation_curve(
+                MockImprovingEstimatorWithParameter(), X, y, param_name="param",
+                param_range=param_range, cv=2
+        )
+    if len(w) > 0:
+        raise RuntimeError("Unexpected warning: %r" % w[0].message)
+
+    assert_array_equal(test_scores[:, 0], test_scores[:, 1])
+    assert_array_equal(train_scores[:, 0], train_scores[:, 1])
