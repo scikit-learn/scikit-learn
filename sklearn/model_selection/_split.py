54b0e4bf62152ac9abb5b93780a0af3e6f5d1c44
Russell Smith <rsmith54@users.noreply.github.com>
2016-09-20 07:46:45 -0400

    Add OneVs{One,All}Classifier._pairwise: fix for #7306 (#7350)

--- sklearn/model_selection/_split.py
+++ sklearn/model_selection/_split.py
@@ -1677,40 +1677,6 @@ def train_test_split(*arrays, **options):
 
 train_test_split.__test__ = False  # to avoid a pb with nosetests
 
-
-def _safe_split(estimator, X, y, indices, train_indices=None):
-    """Create subset of dataset and properly handle kernels."""
-    if (hasattr(estimator, 'kernel') and callable(estimator.kernel) and
-            not isinstance(estimator.kernel, GPKernel)):
-        # cannot compute the kernel values with custom function
-        raise ValueError("Cannot use a custom kernel function. "
-                         "Precompute the kernel matrix instead.")
-
-    if not hasattr(X, "shape"):
-        if getattr(estimator, "_pairwise", False):
-            raise ValueError("Precomputed kernels or affinity matrices have "
-                             "to be passed as arrays or sparse matrices.")
-        X_subset = [X[index] for index in indices]
-    else:
-        if getattr(estimator, "_pairwise", False):
-            # X is a precomputed square kernel matrix
-            if X.shape[0] != X.shape[1]:
-                raise ValueError("X should be a square kernel matrix")
-            if train_indices is None:
-                X_subset = X[np.ix_(indices, indices)]
-            else:
-                X_subset = X[np.ix_(indices, train_indices)]
-        else:
-            X_subset = safe_indexing(X, indices)
-
-    if y is not None:
-        y_subset = safe_indexing(y, indices)
-    else:
-        y_subset = None
-
-    return X_subset, y_subset
-
-
 def _build_repr(self):
     # XXX This is copied from BaseEstimator's get_params
     cls = self.__class__
