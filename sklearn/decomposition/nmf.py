4277ae66a0c9cb2370cda91e793eaf94020eb893
Tom Dupré la Tour <tom.dupre-la-tour@m4x.org>
2016-09-21 12:07:24 +0200

    fix doctest

--- sklearn/decomposition/nmf.py
+++ sklearn/decomposition/nmf.py
@@ -1152,8 +1152,8 @@ def non_negative_factorization(X, W=None, H=None, n_components=None,
     >>> import numpy as np
     >>> X = np.array([[1,1], [2, 1], [3, 1.2], [4, 1], [5, 0.8], [6, 1]])
     >>> from sklearn.decomposition import non_negative_factorization
-    >>> W, H, n_iter = non_negative_factorization(
-        X, n_components=2, init='random', random_state=0)
+    >>> W, H, n_iter = non_negative_factorization(X, n_components=2, \
+        init='random', random_state=0)
 
     References
     ----------
