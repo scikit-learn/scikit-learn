965c6e5ca51df53ee524f41bfbe84e62ce0d6be3
Mehul Ahuja <blackdragon0909@gmail.com>
2016-09-30 09:06:09 +0530

    [MRG + 1] Raised value error in HashingVectorizer and CountVectorizer (#7507)
    * Raised value error in HashingVectorizer and CountVectorizer
    
    * error statements fixed
    
    * added unit tests
    
    * added unit tests and value error in tfidvect
    
    * fixed pep8 errors
    
    * changed assertion type
    
    * removed fit assert from hashingvectorizer
    
    * changed test names
    
    * unit tests changes and redundancies removed

--- sklearn/feature_extraction/text.py
+++ sklearn/feature_extraction/text.py
@@ -452,6 +452,11 @@ class HashingVectorizer(BaseEstimator, VectorizerMixin):
     def fit(self, X, y=None):
         """Does nothing: this transformer is stateless."""
         # triggers a parameter validation
+        if isinstance(X, six.string_types):
+            raise ValueError(
+                "Iterable over raw text documents expected, "
+                "string object received.")
+
         self._get_hasher().fit(X, y=y)
         return self
 
@@ -473,6 +478,11 @@ class HashingVectorizer(BaseEstimator, VectorizerMixin):
             Document-term matrix.
 
         """
+        if isinstance(X, six.string_types):
+            raise ValueError(
+                "Iterable over raw text documents expected, "
+                "string object received.")
+
         analyzer = self.build_analyzer()
         X = self._get_hasher().transform(analyzer(doc) for doc in X)
         if self.binary:
@@ -815,6 +825,11 @@ class CountVectorizer(BaseEstimator, VectorizerMixin):
         # We intentionally don't call the transform method to make
         # fit_transform overridable without unwanted side effects in
         # TfidfVectorizer.
+        if isinstance(raw_documents, six.string_types):
+            raise ValueError(
+                "Iterable over raw text documents expected, "
+                "string object received.")
+
         self._validate_vocabulary()
         max_df = self.max_df
         min_df = self.min_df
@@ -864,6 +879,11 @@ class CountVectorizer(BaseEstimator, VectorizerMixin):
         X : sparse matrix, [n_samples, n_features]
             Document-term matrix.
         """
+        if isinstance(raw_documents, six.string_types):
+            raise ValueError(
+                "Iterable over raw text documents expected, "
+                "string object received.")
+
         if not hasattr(self, 'vocabulary_'):
             self._validate_vocabulary()
 
