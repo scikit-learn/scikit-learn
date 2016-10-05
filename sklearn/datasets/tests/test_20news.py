4da9802468db9dce1381abcf205c244445a21551
Loïc Estève <loic.esteve@ymail.com>
2016-10-05 19:05:44 +0200

    Fix test_20news_vectorized

--- sklearn/datasets/tests/test_20news.py
+++ sklearn/datasets/tests/test_20news.py
@@ -58,8 +58,8 @@ def test_20news_length_consistency():
 
 def test_20news_vectorized():
     try:
-        data = datasets.fetch_20newsgroups(subset='all',
-                                           download_if_missing=False)
+        datasets.fetch_20newsgroups(subset='all',
+                                    download_if_missing=False)
     except IOError:
         raise SkipTest("Download 20 newsgroups to run this test")
 
@@ -78,8 +78,9 @@ def test_20news_vectorized():
     assert_equal(bunch.data.dtype, np.float64)
 
     # test subset = all
-    bunch = data
+    bunch = datasets.fetch_20newsgroups_vectorized(subset='all')
     assert_true(sp.isspmatrix_csr(bunch.data))
     assert_equal(bunch.data.shape, (11314 + 7532, 130107))
     assert_equal(bunch.target.shape[0], 11314 + 7532)
     assert_equal(bunch.data.dtype, np.float64)
+-
