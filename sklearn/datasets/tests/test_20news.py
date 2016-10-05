3e54bf767f0da2d749d6c0d010a2bba17f0002b6
Nelson Liu <nelson.liu.2009@gmail.com>
2016-10-05 07:43:36 -0700

    fix pep8 error by using data

--- sklearn/datasets/tests/test_20news.py
+++ sklearn/datasets/tests/test_20news.py
@@ -59,24 +59,27 @@ def test_20news_length_consistency():
 def test_20news_vectorized():
     # This test is slow.
     try:
-        data = datasets.fetch_20newsgroups(
-            subset='all', download_if_missing=False, shuffle=False)
+        data = datasets.fetch_20newsgroups(subset='all',
+                                           download_if_missing=False)
     except IOError:
         raise SkipTest("Download 20 newsgroups to run this test")
 
+    # test subset = train
     bunch = datasets.fetch_20newsgroups_vectorized(subset="train")
     assert_true(sp.isspmatrix_csr(bunch.data))
     assert_equal(bunch.data.shape, (11314, 130107))
     assert_equal(bunch.target.shape[0], 11314)
     assert_equal(bunch.data.dtype, np.float64)
 
+    # test subset = test
     bunch = datasets.fetch_20newsgroups_vectorized(subset="test")
     assert_true(sp.isspmatrix_csr(bunch.data))
     assert_equal(bunch.data.shape, (7532, 130107))
     assert_equal(bunch.target.shape[0], 7532)
     assert_equal(bunch.data.dtype, np.float64)
 
-    bunch = datasets.fetch_20newsgroups_vectorized(subset="all")
+    # test subset = all
+    bunch = data
     assert_true(sp.isspmatrix_csr(bunch.data))
     assert_equal(bunch.data.shape, (11314 + 7532, 130107))
     assert_equal(bunch.target.shape[0], 11314 + 7532)
