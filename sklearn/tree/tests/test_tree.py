729eb5f356559bdd47a29624df27b3635cde582a
Nelson Liu <nelson.liu.2009@gmail.com>
2016-10-09 06:39:17 -0700

    [MRG] FIX min_samples_split and min_samples_leaf validation for float vs int (#7604)

--- sklearn/tree/tests/test_tree.py
+++ sklearn/tree/tests/test_tree.py
@@ -509,6 +509,7 @@ def test_error():
         assert_raises(ValueError, TreeEstimator(min_samples_leaf=-1).fit, X, y)
         assert_raises(ValueError, TreeEstimator(min_samples_leaf=.6).fit, X, y)
         assert_raises(ValueError, TreeEstimator(min_samples_leaf=0.).fit, X, y)
+        assert_raises(ValueError, TreeEstimator(min_samples_leaf=3.).fit, X, y)
         assert_raises(ValueError,
                       TreeEstimator(min_weight_fraction_leaf=-1).fit,
                       X, y)
@@ -520,6 +521,8 @@ def test_error():
         assert_raises(ValueError, TreeEstimator(min_samples_split=0.0).fit,
                       X, y)
         assert_raises(ValueError, TreeEstimator(min_samples_split=1.1).fit,
+                      X, y)
+        assert_raises(ValueError, TreeEstimator(min_samples_split=2.5).fit,
                       X, y)
         assert_raises(ValueError, TreeEstimator(max_depth=-1).fit, X, y)
         assert_raises(ValueError, TreeEstimator(max_features=42).fit, X, y)
