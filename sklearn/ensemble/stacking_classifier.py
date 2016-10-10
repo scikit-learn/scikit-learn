5a028b1d967ba87f2b4ed9058ba12379fd0c3217
Yichuan Liu <yichuanliu2004@gmail.com>
2016-10-09 22:42:26 -0400

    Use `auto` as the default `method`

--- sklearn/ensemble/stacking_classifier.py
+++ sklearn/ensemble/stacking_classifier.py
@@ -6,19 +6,19 @@ from ..preprocessing import LabelEncoder
 from ..externals.joblib import Parallel, delayed
 from ..utils.metaestimators import if_delegate_has_method
 from ..utils.validation import has_fit_parameter, check_is_fitted
+from ..utils.multiclass import type_of_target
 from ..externals import six
 
 
-def _parallel_predict(estimator, method, X, y, fit_params):
-    """Private function used to fit an estimator within a job."""
-    estimator.fit(X, y, **fit_params)
-    return estimator, getattr(estimator, method)(X)
+def _parallel_fit(clf, X, y, fit_params):
+    clf.fit(X, y, **fit_params)
+    return clf
 
 
 class StackingClassifier(BaseEstimator, ClassifierMixin):
-    """ Stacking classifier for combining unfitted estimators
+    """ Stacking classifier for combining estimators
 
-    The cross-validated predictions of unfitted estimators will be used as
+    The cross-validated predictions of estimators will be used as
     inputs to a meta-estimator for making a final prediction
 
     Parameters
@@ -32,9 +32,10 @@ class StackingClassifier(BaseEstimator, ClassifierMixin):
         The meta-estimator to combine the predictions of each individual
         estimator
 
-    method : string, optional, default='predict_proba'
+    method : string, optional, default='auto'
         Specifies which method of the estimators will be called to generate
-        inputs to the meta_estimator
+        inputs to the meta_estimator. The default is 'auto' which is
+        `predict_proba` if it exists and `decision_function` otherwise
 
     cv : int, cross-validation generator or an iterable, optional
         Determines the cross-validation splitting strategy to generate inputs
@@ -47,8 +48,8 @@ class StackingClassifier(BaseEstimator, ClassifierMixin):
           - An iterable yielding train, test splits.
 
         For integer/None inputs, if the estimator is a classifier and ``y`` is
-        either binary or multiclass, :class:`StratifiedKFold` is used. In all
-        other cases, :class:`KFold` is used.
+        either binary or multiclass, :class:`StratifiedKFold` is used. If y is
+        a different format, :class:`KFold` is used.
 
         Refer :ref:`User Guide <cross_validation>` for the various
         cross-validation strategies that can be used here.
@@ -90,7 +91,7 @@ class StackingClassifier(BaseEstimator, ClassifierMixin):
     >>>
     """
 
-    def __init__(self, estimators, meta_estimator, method='predict_proba',
+    def __init__(self, estimators, meta_estimator, method='auto',
                  cv=None, n_jobs=1, verbose=0):
         self.estimators = estimators
         self.named_estimators = dict(estimators)
@@ -99,6 +100,21 @@ class StackingClassifier(BaseEstimator, ClassifierMixin):
         self.method = method
         self.n_jobs = n_jobs
         self.verbose = verbose
+
+    def _method(self, estimator):
+        if self.method == 'auto':
+            if hasattr(estimator, 'predict_proba'):
+                method = 'predict_proba'
+            elif hasattr(estimator, 'decision_function'):
+                method = 'decision_function'
+            else:
+                raise AttributeError("Estimator %s has no method "
+                                     "`predict_proba` or `decision_function`. "
+                                     "Try specify a method instead of using "
+                                     "the default `auto`" % estimator)
+        else:
+            method = self.method
+        return method
 
     def fit(self, X, y, **kwargs):
         """ Fit the estimators.
@@ -119,81 +135,89 @@ class StackingClassifier(BaseEstimator, ClassifierMixin):
         -------
         self : object
         """
-        if isinstance(y, np.ndarray) and len(y.shape) > 1 and y.shape[1] > 1:
+        if any(s in type_of_target(y) for s in ['multilabel', 'multioutput']):
             raise NotImplementedError('Multilabel and multi-output'
                                       ' classification is not supported.')
 
-        if self.estimators is None or len(self.estimators) == 0:
-            raise AttributeError('Invalid `estimators` attribute, `estimators`'
-                                 ' should be a list of (string, estimator)'
-                                 ' tuples')
+        if not self.estimators:
+            raise AttributeError('Invalid `estimators` attribute %s, '
+                                 '`estimators` should be a list of (string, '
+                                 'estimator) tuples.' % type(self.estimators))
 
         if not is_classifier(self.meta_estimator):
-            raise AttributeError('Invalid `meta_estimator` attribute, '
-                                 '`meta_estimator` should be a classifier')
-
+            raise AttributeError('Invalid `meta_estimator` attribute '
+                                 '%s, `meta_estimator` should be a classifier.'
+                                 % type(self.meta_estimator))
         for name, step in self.estimators:
-            if not hasattr(step, self.method):
-                raise ValueError('Underlying estimator {0} does not support '
-                                 '{1}.'.format(name, self.method))
+            if not hasattr(step, self._method(step)):
+                raise ValueError('Underlying estimator %s %s does '
+                                 'not support %s.' % (name,
+                                                      type(step),
+                                                      self._method(step)))
 
         for param in kwargs:
             if not has_fit_parameter(self.meta_estimator, param):
-                raise ValueError('Underlying meta estimator {0} '
-                                 'does not support `{1}`.'.format(
-                                     type(self.meta_estimator), param))
+                raise ValueError('Underlying meta estimator %s '
+                                 'does not support `%s`.' % (
+                                     self.meta_estimator, param))
             for name, step in self.estimators:
                 if not has_fit_parameter(step, param):
-                    raise ValueError('Underlying estimator `{0}` does not '
-                                     'support `{1}`.'.format(name, param))
+                    raise ValueError('Underlying estimator %s of type %s does '
+                                     'not support `%s`.' % (name,
+                                                            type(step),
+                                                            param))
 
-        self.le_ = LabelEncoder()
-        self.le_.fit(y)
+        self.le_ = LabelEncoder().fit(y)
         self.classes_ = self.le_.classes_
 
         transformed_y = self.le_.transform(y)
         if self.cv == 1:  # Do not cross-validation
             # Parallel fit each estimator
-            prediction_blocks = Parallel(n_jobs=self.n_jobs)(
-                    delayed(_parallel_predict)(clone(clf), self.method,
-                                               X, transformed_y, kwargs)
+
+            self.estimators_ = Parallel(n_jobs=self.n_jobs)(
+                    delayed(_parallel_fit)(clone(clf),
+                                           X, transformed_y, kwargs)
                     for _, clf in self.estimators)
+            scores = self._est_predict(X)
         else:
             # Use the n_jobs of cross_val_predict
-            prediction_blocks = [(clf.fit(X, y, **kwargs),
-                                  cross_val_predict(clf, X, y, cv=self.cv,
-                                                    method=self.method,
-                                                    fit_params=kwargs,
-                                                    verbose=self.verbose,
-                                                    n_jobs=self.n_jobs))
-                                 for _, clf in self.estimators]
-
-        scores = self._form_meta_inputs([p for _, p in prediction_blocks])
-        self.estimators_ = [est for est, _ in prediction_blocks]
+            self.estimators_ = []
+            scores = []
+            for _, clf in self.estimators:
+                s1 = cross_val_predict(clf, X, y, cv=self.cv,
+                                       method=self._method(clf),
+                                       fit_params=kwargs,
+                                       verbose=self.verbose,
+                                       n_jobs=self.n_jobs)
+                s1 = self._form_meta_inputs(clf, s1)
+                scores.append(s1)
+                self.estimators_.append(clf.fit(X, y, **kwargs))
+            scores = np.concatenate([s.reshape(-1, 1) if s.ndim == 1 else s
+                                     for s in scores], axis=1)
         self.meta_estimator_ = clone(self.meta_estimator)
         self.meta_estimator_.fit(scores, transformed_y, **kwargs)
         return self
 
-    def _form_meta_inputs(self, predicted):
-        """ Concatenate estimator predictions to form input to meta-estimator
-        """
-        if self.method in ['predict_proba', 'predict_log_proba']:
+    def _form_meta_inputs(self, clf, predicted):
+        if self._method(clf) in ['predict_proba', 'predict_log_proba']:
             # Remove first column to avoid multicollinearity since sum of
             # probabilities equals 1
-            predicted = [s[:, 1:] for s in predicted]
-
-        if self.method == 'predict_log_proba':
+            predicted = predicted[:, 1:]
+        if self._method(clf) == 'predict_log_proba':
             # Replace inf
-            predicted = [np.clip(s, -1e30, 1e30) for s in predicted]
-
-        return np.concatenate([s.reshape(-1, 1) if s.ndim == 1 else s
-                               for s in predicted], axis=1)
+            predicted = np.clip(predicted, -1e30, 1e30)
+        return predicted
 
     def _est_predict(self, X):
         """ Generate input to meta_estimator from predictions of estimators
         """
-        return self._form_meta_inputs([getattr(clf, self.method)(X)
-                                       for clf in self.estimators_])
+        predicted = []
+        for clf in self.estimators_:
+            s1 = getattr(clf, self._method(clf))(X)
+            s1 = self._form_meta_inputs(clf, s1)
+            predicted.append(s1)
+        return np.concatenate([s.reshape(-1, 1) if s.ndim == 1 else s
+                               for s in predicted], axis=1)
 
     @if_delegate_has_method(delegate=('meta_estimator_', 'meta_estimator'))
     def predict(self, X):
