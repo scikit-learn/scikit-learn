540f96e375e7b9d875608b95f25b2652b893f84e
sshleifer <sam@kensho.com>
2016-10-09 21:08:20 -0400

    use fit_predict from 17.x

--- sklearn/pipeline.py
+++ sklearn/pipeline.py
@@ -353,10 +353,7 @@ class Pipeline(_BasePipeline):
         -------
         y_pred : array-like
         """
-        Xt = X
-        for name, transform in self.steps[:-1]:
-            if transform is not None:
-                Xt = transform.fit_transform(Xt)
+        Xt, fit_params = self._pre_transform(X, y, **fit_params)
         return self.steps[-1][-1].fit_predict(Xt, y, **fit_params)
 
     @if_delegate_has_method(delegate='_final_estimator')
