2b5bb7b79fca95464d4ea9ea8f9b0905133edf3f
Kyle L. Gilliam <kgilliam125@gmail.com>
2016-10-14 14:57:23 -0600

    Changed fit_transform for code consistency

--- sklearn/preprocessing/label.py
+++ sklearn/preprocessing/label.py
@@ -304,6 +304,26 @@ class LabelBinarizer(BaseEstimator, TransformerMixin):
         self.classes_ = unique_labels(y)
         return self
 
+    def fit_transform(self, y):
+        """Fit label binarizer and transform multi-class labels to binary labels.
+        
+        The output of transform is sometimes referred to by some authors as the
+        1-of-K coding scheme.
+
+        Parameters
+        ----------
+        y : numpy array or sparse matrix of shape (n_samples,) or
+            (n_samples, n_classes) Target values. The 2-d matrix should only
+            contain 0 and 1, represents multilabel classification. Sparse
+            matrix can be CSR, CSC, COO, DOK, or LIL.
+
+        Returns
+        -------
+        Y : numpy array or CSR matrix of shape [n_samples, n_classes]
+            Shape will be [n_samples, 1] for binary problems.
+        """
+        return self.fit(y).transform(y)
+
     def transform(self, y):
         """Transform multi-class labels to binary labels
 
@@ -333,27 +353,6 @@ class LabelBinarizer(BaseEstimator, TransformerMixin):
                               pos_label=self.pos_label,
                               neg_label=self.neg_label,
                               sparse_output=self.sparse_output)
-    
-    def fit_transform(self, y):
-        """Fit label binarizer and transform multi-class labels to binary labels.
-        
-        The output of transform is sometimes referred to by some authors as the
-        1-of-K coding scheme.
-
-        Parameters
-        ----------
-        y : numpy array or sparse matrix of shape (n_samples,) or
-            (n_samples, n_classes) Target values. The 2-d matrix should only
-            contain 0 and 1, represents multilabel classification. Sparse
-            matrix can be CSR, CSC, COO, DOK, or LIL.
-
-        Returns
-        -------
-        Y : numpy array or CSR matrix of shape [n_samples, n_classes]
-            Shape will be [n_samples, 1] for binary problems.
-        """
-        return super(fit_transform, self).fit_transform(y)
-    
     
     def inverse_transform(self, Y, threshold=None):
         """Transform binary labels back to multi-class labels
