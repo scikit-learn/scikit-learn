9c7001d0b97844d3d07b0c4137435b0154e6cafd
Kyle Gilliam <kgilliam125@gmail.com>
2016-10-14 10:29:41 -0600

    Added override of fit_transform to LabelBinarizer

--- sklearn/preprocessing/label.py
+++ sklearn/preprocessing/label.py
@@ -333,7 +333,29 @@ class LabelBinarizer(BaseEstimator, TransformerMixin):
                               pos_label=self.pos_label,
                               neg_label=self.neg_label,
                               sparse_output=self.sparse_output)
+    
+    def fit_transform(self, y):
+        """Fit label binarizer and transform multi-class labels to binary labels.
+        
+        The output of transform is sometimes referred to by some authors as the
+        1-of-K coding scheme.
 
+        Parameters
+        ----------
+        y : numpy array or sparse matrix of shape (n_samples,) or
+            (n_samples, n_classes) Target values. The 2-d matrix should only
+            contain 0 and 1, represents multilabel classification. Sparse
+            matrix can be CSR, CSC, COO, DOK, or LIL.
+
+        Returns
+        -------
+        Y : numpy array or CSR matrix of shape [n_samples, n_classes]
+            Shape will be [n_samples, 1] for binary problems.
+        """
+        self.fit(y)
+        return self.transform(y)
+    
+    
     def inverse_transform(self, Y, threshold=None):
         """Transform binary labels back to multi-class labels
 
