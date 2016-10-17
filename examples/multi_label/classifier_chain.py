be960a7954aed1ee89ba07d3947ef0d86bd9c4d1
Adam Kleczewski <adam.kleczewski@human.x.ai>
2016-10-17 11:35:47 -0400

    now using numpy RandomState example now uses the yeast dataset instead of reuters formatting fixes

--- examples/multi_label/classifier_chain.py
+++ examples/multi_label/classifier_chain.py
@@ -2,11 +2,8 @@
 ============================
 Classifier Chain
 ============================
-
-Demonstrate that a single classifier chain
-out performs 10 independent classifiers
-and that an ensemble of 10 randomly ordered
-classifier chains out performs a single classifier chain
+On a multi-label dataset an ensemble of classifier chains achieves a higher
+Jaccard similarity score than a set of independently trained models.
 
 """
 
@@ -16,71 +13,29 @@ from sklearn.model_selection import train_test_split
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.metrics import jaccard_similarity_score
 from sklearn.linear_model import LogisticRegression
-from sklearn.datasets import fetch_rcv1
+from sklearn.datasets import fetch_mldata
 
-# Get the Reuters Corpus Volume I dataset
-rcv1 = fetch_rcv1()
+# Fetch a multi-label dataset
+yeast = fetch_mldata('yeast')
+X = yeast['data']
+Y = yeast['target'].transpose().toarray()
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=.5)
 
-# RCV1 contains 804414 rows. By convention the first 23149 rows
-# are designated as training data
-split = 23149
-X = rcv1.data[:split, :].toarray()
-
-# RCV1 is a multi-label with 103 labels. To keep this example simple
-# we will only try to predict 10 of those labels.
-selected_labels = np.random.choice(list(range(rcv1.target.shape[1])), 10)
-Y = rcv1.target[:split, selected_labels].toarray()
-
-# But since we are only using a subset of the labels many
-# of the rows in our training data will not contain any of
-# the 10 labels we are trying to predict.
-# So let's only use the rows that contain at least one of the
-# labels we are trying to predict.
-mask = Y.sum(axis=1) != 0
-
-# Now split the data into training and test datasets.
-# There needs to be at least one positive example of
-# each label in the training dataset or the train method
-# will throw an error. So let's ensure that we get
-# a good train/test split.
-valid_split_found = False
-count = 0
-while not valid_split_found:
-    X_train, X_test, Y_train, Y_test = \
-        train_test_split(X[mask, :], Y[mask, :])
-    valid_split_found = all(Y_train.sum(axis=0) > 0)
-    if valid_split_found:
-        print("valid split found")
-    if count >= 10:
-        print("valid split not found")
-        break
-    count += 1
-
-
-# 1. Fit an independent LogisticRegression model for each class
-# using the OneVsRestClassifier wrapper
+# Fit an independent logistic regression model for each class using the
+# OneVsRestClassifier wrapper
 ovr = OneVsRestClassifier(LogisticRegression())
 ovr.fit(X_train, Y_train)
 Y_pred_ovr = ovr.predict(X_test)
-print("one vs rest jaccard similarity",
-      jaccard_similarity_score(Y_test, Y_pred_ovr))
+print("Independent models Jaccard similarity score:", jaccard_similarity_score(
+    Y_test, Y_pred_ovr))
 
-# 2. fit a single chain of LogisticRegression models
-classifier_chain = ClassifierChain(LogisticRegression())
-classifier_chain.fit(X_train, Y_train)
-Y_chain = classifier_chain.predict(X_test)
-print("classifier chain jaccard similarity",
-      jaccard_similarity_score(Y_test, Y_chain))
-
-# 3. fit an ensemble of classifier chains
-chains = [ClassifierChain(LogisticRegression())
-          for i in range(10)]
+# Fit an ensemble of logistic regression classifier chains and take the
+# take the average prediction of all the chains
+chains = [ClassifierChain(LogisticRegression()) for i in range(10)]
 for chain in chains:
     chain.fit(X_train, Y_train)
-Y_pred_ensemble = \
-    np.array([chain.predict(X_test) for chain in chains]).mean(axis=0)
-print("classifier chain ensemble jaccard similarity",
+Y_pred_ensemble = np.array([chain.predict(X_test) for chain in
+                            chains]).mean(axis=0)
+print("Classifier chain ensemble Jaccard similarity score:",
       jaccard_similarity_score(Y_test, Y_pred_ensemble >= .5))
 
-
-# The three cases should have increasing Jaccard similarity scores.
--- sklearn/multi_label/classifier_chain.py
+++ sklearn/multi_label/classifier_chain.py
@@ -1,13 +1,14 @@
 import numpy as np
 from ..base import BaseEstimator, clone
+from ..utils import check_random_state
 import random
 
 
 class ClassifierChain(BaseEstimator):
     """Classifier Chain
-    A multi-label model that arranges classifiers into a chain.
-    Predictions are made in the order specified by the chain.
-    The predictions of earlier models are used as features by later models.
+    A multi-label model that arranges classifiers into a chain. Predictions
+    are made in the order specified by the chain. The predictions of
+    earlier models are used as features by later models.
 
     By default the order of the chain is random although it can be specified
     at the time of fitting with the chain_order parameter. Since the optimal
@@ -18,9 +19,10 @@ class ClassifierChain(BaseEstimator):
     ----------
     base_estimator : object
         The base estimator used for fitting the model for each label.
-        Defaults to LogisticRegression
-    random_state : int
-        An integer used to seed the random number generator
+    random_state : integer or numpy.RandomState, optional
+        The generator used to generate random chain orders. If an
+        integer is given, it fixes the seed. Defaults to the global numpy
+        random number generator.
 
     Attributes
     ----------
@@ -31,28 +33,25 @@ class ClassifierChain(BaseEstimator):
         For example, for a chain of length 5
             chain_order = [1, 3, 2, 4, 0]
         means that the first model in the chain will make predictions for
-        column 1 in the Y matrix, the second model will make predictions for
-        column 3, etc.
-        If chain_order is not None it must have a length equal to the
-        number of columns in Y.
+        column 1 in the Y matrix, the second model will make predictions
+        for column 3, etc. If chain_order is not None it must have a length
+        equal to the number of columns in Y.
         If chain_order is None an ordered list of integers will be used
             chain_order = [0, 1, 2, ..., Y.shape[1]]
         where Y is the label matrix passed to the fit method.
 
     References
     ----------
-    Jesse Read, Bernhard Pfahringer, Geoff Holmes, Eibe Frank,
-        "Classifier Chains for Multi-label Classification", 2009.
+    Jesse Read, Bernhard Pfahringer, Geoff Holmes, Eibe Frank, "Classifier
+    Chains for Multi-label Classification", 2009.
 
     """
 
-    def __init__(self,
-                 base_estimator,
-                 random_state=None):
+    def __init__(self, base_estimator, random_state=None):
 
         self.base_estimator = base_estimator
-        random.seed(random_state)
-        self.classifiers = []
+        self.random_state = check_random_state(random_state)
+        self.classifiers_ = []
         self.chain_order = None
 
     def fit(self, X, Y, chain_order=None, shuffle=True):
@@ -62,37 +61,37 @@ class ClassifierChain(BaseEstimator):
         shuffle : bool
             If true chain_order is shuffled
         chain_order: list
-            A list of integers specifying the order of the classes
-            in the chain.
+            A list of integers specifying the order of the classes in the
+            chain.
         """
-        self.classifiers = [clone(self.base_estimator)
-                            for _ in range(Y.shape[1])]
+        self.classifiers_ = [clone(self.base_estimator) for _ in range(
+            Y.shape[1])]
 
         if chain_order is not None:
-            assert len(chain_order) == Y.shape[1],\
-                "chain_order length must equal n_labels"
-            assert all([isinstance(i, int) for i in chain_order]),\
+            assert len(chain_order) == Y.shape[1], "chain_order length must " \
+                                                   "equal n_labels"
+            assert all([isinstance(i, int) for i in chain_order]), \
                 "chain_order must be a list of integers"
             self.chain_order = chain_order
         else:
             self.chain_order = list(range(Y.shape[1]))
         if shuffle:
-            random.shuffle(self.chain_order)
+            self.random_state.shuffle(self.chain_order)
 
-        for chain_idx, classifier in enumerate(self.classifiers):
+        for chain_idx, classifier in enumerate(self.classifiers_):
             previous_labels = Y[:, self.chain_order[:chain_idx]]
             X_aug = np.hstack((X, previous_labels))
             y = np.squeeze(Y[:, self.chain_order[chain_idx]])
             classifier.fit(X_aug, y)
 
     def predict(self, X):
-        Y_pred_chain = np.zeros((X.shape[0], len(self.classifiers)))
-        for chain_idx, classifier in enumerate(self.classifiers):
+        Y_pred_chain = np.zeros((X.shape[0], len(self.classifiers_)))
+        for chain_idx, classifier in enumerate(self.classifiers_):
             previous_predictions = Y_pred_chain[:, :chain_idx]
             X_aug = np.hstack((X, previous_predictions))
             Y_pred_chain[:, chain_idx] = classifier.predict(X_aug)
-        chain_key = [self.chain_order.index(i)
-                     for i in range(len(self.chain_order))]
+        chain_key = [self.chain_order.index(i) for i in range(len(
+            self.chain_order))]
         Y_pred = Y_pred_chain[:, chain_key]
 
         return Y_pred
