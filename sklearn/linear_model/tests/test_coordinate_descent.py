6de9d1a6b139b7c2816a6a51ca424087502b1d02
nuffe <erik.cfr@gmail.com>
2016-10-24 18:20:01 +0200

    Add random state to test

--- sklearn/linear_model/tests/test_coordinate_descent.py
+++ sklearn/linear_model/tests/test_coordinate_descent.py
@@ -724,22 +724,23 @@ def test_enet_l1_ratio():
     X = np.array([[1, 2, 4, 5, 8], [3, 5, 7, 7, 8]]).T
     y = np.array([12, 10, 11, 21, 5])
 
-    assert_raise_message(ValueError, msg,
-                         ElasticNetCV(l1_ratio=0).fit, X, y)
-    assert_raise_message(ValueError, msg,
-                         MultiTaskElasticNetCV(l1_ratio=0).fit, X, y[:, None])
+    assert_raise_message(ValueError, msg, ElasticNetCV(
+        l1_ratio=0, random_state=42).fit, X, y)
+    assert_raise_message(ValueError, msg, MultiTaskElasticNetCV(
+        l1_ratio=0, random_state=42).fit, X, y[:, None])
 
     # Test that l1_ratio=0 is allowed if we supply a grid manually
     alphas = [0.1, 10]
-    est_desired = ElasticNetCV(l1_ratio=0.00001, alphas=alphas)
-    est = ElasticNetCV(l1_ratio=0, alphas=alphas)
+    estkwds = {'alphas': alphas, 'random_state': 42}
+    est_desired = ElasticNetCV(l1_ratio=0.00001, **estkwds)
+    est = ElasticNetCV(l1_ratio=0, **estkwds)
     with ignore_warnings():
         est_desired.fit(X, y)
         est.fit(X, y)
     assert_array_almost_equal(est.coef_, est_desired.coef_, decimal=5)
 
-    est_desired = MultiTaskElasticNetCV(l1_ratio=0.00001, alphas=alphas)
-    est = MultiTaskElasticNetCV(l1_ratio=0, alphas=alphas)
+    est_desired = MultiTaskElasticNetCV(l1_ratio=0.00001, **estkwds)
+    est = MultiTaskElasticNetCV(l1_ratio=0, **estkwds)
     with ignore_warnings():
         est.fit(X, y[:, None])
         est_desired.fit(X, y[:, None])
