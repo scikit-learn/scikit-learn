6f3bb792fe465ab08a1e92825c267804e62a09dd
CJ Carey <perimosocordiae@gmail.com>
2016-10-25 13:47:14 -0500

    Fixing for old numpy
    Older versions don't support kwargs for `astype`

--- sklearn/preprocessing/label.py
+++ sklearn/preprocessing/label.py
@@ -732,7 +732,7 @@ class MultiLabelBinarizer(BaseEstimator, TransformerMixin):
         class_mapping = np.empty(len(tmp), dtype=dtype)
         class_mapping[:] = tmp
         self.classes_, inverse = np.unique(class_mapping, return_inverse=True)
-        yt.indices = inverse[yt.indices].astype(yt.indices.dtype, copy=False)
+        yt.indices = np.array(inverse[yt.indices], dtype=yt.indices.dtype, copy=False)
 
         if not self.sparse_output:
             yt = yt.toarray()
