2670f0d3aa32934d90557b80c8efedcba5e7e742
Raghav RV <rvraghav93@gmail.com>
2016-10-26 18:01:25 +0200

    TST Check if shuffle=True produces different splits

--- sklearn/model_selection/tests/test_split.py
+++ sklearn/model_selection/tests/test_split.py
@@ -848,9 +848,23 @@ def test_cv_iterable_wrapper():
     kf_iter_wrapped = check_cv(kf_iter)
     # Since the wrapped iterable is enlisted and stored,
     # split can be called any number of times to produce
-    # consistent results
+    # consistent results.
     assert_array_equal(list(kf_iter_wrapped.split(X, y)),
                        list(kf_iter_wrapped.split(X, y)))
+    # If the splits are randomized, successive calls to split yields different
+    # results
+    kf_randomized_iter = KFold(n_splits=5, shuffle=True).split(X, y)
+    kf_randomized_iter_wrapped = check_cv(kf_iter)
+    assert_array_equal(list(kf_randomized_iter_wrapped.split(X, y)),
+                       list(kf_randomized_iter_wrapped.split(X, y)))
+    try:
+        assert_array_equal(list(kf_iter_wrapped.split(X, y)),
+                           list(kf_randomized_iter_wrapped.split(X, y)))
+    except AssertionError:
+        pass
+    else:
+        raise AssertionError("After randomization, wrapped kfold (with "
+                             "shuffle) did not produce differing results.")
 
 
 def test_group_kfold():
