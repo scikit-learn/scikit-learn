620dc2b53184c3387f2e8a743be843fb9522d547
waterponey <prosper.burq@gmail.com>
2016-10-27 17:37:22 +0200

    fixup test alias lda.LDA is instance of LinearDiscriminantAnalysis

--- sklearn/tests/test_discriminant_analysis.py
+++ sklearn/tests/test_discriminant_analysis.py
@@ -329,7 +329,7 @@ def test_deprecated_lda_qda_deprecation():
         return sklearn.lda
 
     lda = assert_warns(DeprecationWarning, import_lda_module)
-    assert lda.LDA.__class__ is LinearDiscriminantAnalysis.__class__
+    assert isinstance(lda.LDA(), LinearDiscriminantAnalysis)
 
     def import_qda_module():
         import sklearn.qda
@@ -339,7 +339,7 @@ def test_deprecated_lda_qda_deprecation():
         return sklearn.qda
 
     qda = assert_warns(DeprecationWarning, import_qda_module)
-    assert qda.QDA.__class__ is QuadraticDiscriminantAnalysis.__class__
+    assert isinstance(qda.QDA(), QuadraticDiscriminantAnalysis)
 
 
 def test_covariance():
