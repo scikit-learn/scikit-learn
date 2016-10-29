0a7c83f1b57bfb66f74727cbd86c562d6882d710
Josh Karnofsky <jkarno@seas.upenn.edu>
2016-10-29 07:08:53 -0400

    [MRG] ENH faster sample_without_replacement for recent numpy (#7703)

--- sklearn/pipeline.py
+++ sklearn/pipeline.py
@@ -136,13 +136,13 @@ class Pipeline(_BasePipeline):
     Pipeline(steps=[...])
     >>> prediction = anova_svm.predict(X)
     >>> anova_svm.score(X, y)                        # doctest: +ELLIPSIS
-    0.77...
+    0.829...
     >>> # getting the selected features chosen by anova_filter
     >>> anova_svm.named_steps['anova'].get_support()
     ... # doctest: +NORMALIZE_WHITESPACE
-    array([ True,  True,  True, False, False,  True, False,  True,  True, True,
-           False, False,  True, False,  True, False, False, False, False,
-           True], dtype=bool)
+    array([False, False,  True,  True, False, False, True,  True, False,
+           True,  False,  True,  True, False, True,  False, True, True,
+           False, False], dtype=bool)
     """
 
     # BaseEstimator interface
