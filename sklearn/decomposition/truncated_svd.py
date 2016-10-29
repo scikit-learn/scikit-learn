ae86e2f53d5f7c4411b5ec1d0c6679b1d4e87f0b
Tommy Löfstedt <tommy.lofstedt@umu.se>
2016-10-29 13:57:36 +0200

    TEST: Fixed doctests for truncated PCA.

--- sklearn/decomposition/truncated_svd.py
+++ sklearn/decomposition/truncated_svd.py
@@ -93,11 +93,11 @@ class TruncatedSVD(BaseEstimator, TransformerMixin):
     TruncatedSVD(algorithm='randomized', n_components=5, n_iter=7,
             random_state=42, tol=0.0)
     >>> print(svd.explained_variance_ratio_)  # doctest: +ELLIPSIS
-    [ 0.0782... 0.0552... 0.0544... 0.0499... 0.0413...]
+    [ 0.0606... 0.0584... 0.0497... 0.0434... 0.0372...]
     >>> print(svd.explained_variance_ratio_.sum())  # doctest: +ELLIPSIS
-    0.279...
+    0.249...
     >>> print(svd.singular_values_)  # doctest: +ELLIPSIS
-    [ 2.6318... 2.2215... 2.1939... 2.1010... 1.9317...]
+    [ 2.5841... 2.5245... 2.3201... 2.1753... 2.0443...]
 
     See also
     --------
