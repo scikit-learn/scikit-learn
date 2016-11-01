063b06475c079431bef4adbe08f6f88993dd4391
Vishesh Mistry <mistry.1@iitj.ac.in>
2016-11-01 15:17:15 +0530

    This fixes bug #7751. In scikit-learn/sklearn/metrics/classification.py under the function classification_report , a check is made to make sure that if the user enters the elements of target_names the size of that is checked with the size of labels. If the size is unequal, a warning is raised which will inform the user about the issue

--- test1.py
+++
@@ -1,5 +0,0 @@
-from sklearn.metrics import classification_report
-y_true = [0, 0, 2, 0, 0]
-y_pred = [0, 2, 2, 0, 0]
-target_names = ['class 0', 'class 1', 'class 2']
-print(classification_report(y_true, y_pred, target_names=target_names))
