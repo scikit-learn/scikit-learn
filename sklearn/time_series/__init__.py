4b1a9a038a65918636fc95a7ba63d79959f43a26
marhamil723 <mhamilton723@gmail.com>
2016-11-01 12:03:40 -0400

    -fixed naming

--- sklearn/time_series/__init__.py
+++ sklearn/time_series/__init__.py
@@ -2,7 +2,7 @@
 The :mod:`sklearn.time_series` module includes the time series regressor object
 """
 
-from .time_series import TimeSeriesRegressor
+from .time_series_estimator import TimeSeriesEstimator
 
 
-__all__ = ["TimeSeriesRegressor"]
+__all__ = ["TimeSeriesEstimator"]
