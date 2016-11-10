e9fd54ad6f0edb1d4f6b014d4cf2cddb04bae229
Leland McInnes <leland.mcinnes@gmail.com>
2016-11-10 03:34:30 -0500

    Make Arccos an allowed metric for ball-trees. (Use in kd-trees still needs to be verified).

--- sklearn/neighbors/ball_tree.pyx
+++ sklearn/neighbors/ball_tree.pyx
@@ -19,7 +19,8 @@ VALID_METRICS = ['EuclideanDistance', 'SEuclideanDistance',
                  'DiceDistance', 'KulsinskiDistance',
                  'RogersTanimotoDistance', 'RussellRaoDistance',
                  'SokalMichenerDistance', 'SokalSneathDistance',
-                 'PyFuncDistance', 'HaversineDistance']
+                 'ArccosDistance', 'HaversineDistance'
+                 'PyFuncDistance']
 
 
 include "binary_tree.pxi"
