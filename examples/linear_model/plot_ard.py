8eaa4c79d49ac6503211b399bbb7664684770ea9
Sergey Feldman <sergeyfeldman@gmail.com>
2016-11-15 19:48:19 -0800

    Updating "polynomia" for ARD

--- examples/linear_model/plot_ard.py
+++ examples/linear_model/plot_ard.py
@@ -16,7 +16,8 @@ prior is implied on the weights.
 The estimation of the model is done by iteratively maximizing the
 marginal log-likelihood of the observations.
 
-We also plot predictions and uncertainties for polynomial ARD.
+We also plot predictions and uncertainties for ARD
+for one dimensional regression using polynomial feature expansion.
 Note the uncertainty starts going up on the right side of the plot.
 This is because these test samples are outside of the range of the training
 samples.
