e0142c277d7b6d1a78aae3259fdcaa658235f639
Tyler Lanigan <tylerlanigan@gmail.com>
2016-11-19 14:19:39 -0800

    fix base.py doc string error

--- sklearn/datasets/base.py
+++ sklearn/datasets/base.py
@@ -290,8 +290,8 @@ def load_wine(return_X_y=False):
 
     >>> from sklearn.datasets import load_wine
     >>> data = load_wine()
-    >>> data.target[[10, 25, 50]]
-    array([0, 0, 1])
+    >>> data.target[[10, 80, 140]]
+    array([0, 1, 2])
     >>> list(data.target_names)
     ['class_0', 'class_1', 'class_2']
     """
