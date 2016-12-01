c22c17dd30e1550efadfe20aac8f496b01c805ba
Sergey Feldman <sergeyfeldman@gmail.com>
2016-12-01 07:52:18 -0800

    [MRG+2] Adding return_std options for models in linear_model/bayes.py (#7838)
    * initial commit for return_std
    
    * initial commit for return_std
    
    * adding tests, examples, ARD predict_std
    
    * adding tests, examples, ARD predict_std
    
    * a smidge more documentation
    
    * a smidge more documentation
    
    * Missed a few PEP8 issues
    
    * Changing predict_std to return_std #1
    
    * Changing predict_std to return_std #2
    
    * Changing predict_std to return_std #3
    
    * Changing predict_std to return_std final
    
    * adding better plots via polynomial regression
    
    * trying to fix flake error
    
    * fix to ARD plotting issue
    
    * fixing some flakes
    
    * Two blank lines part 1
    
    * Two blank lines part 2
    
    * More newlines!
    
    * Even more newlines
    
    * adding info to the doc string for the two plot files
    
    * Rephrasing "polynomial" for Bayesian Ridge Regression
    
    * Updating "polynomia" for ARD
    
    * Adding more formal references
    
    * Another asked-for improvement to doc string.
    
    * Fixing flake8 errors
    
    * Cleaning up the tests a smidge.
    
    * A few more flakes
    
    * requested fixes from Andy
    
    * Mini bug fix
    
    * Final pep8 fix
    
    * pep8 fix round 2
    
    * Fix beta_ to alpha_ in the comments

--- sklearn/linear_model/tests/test_bayes.py
+++ sklearn/linear_model/tests/test_bayes.py
@@ -56,3 +56,35 @@ def test_toy_ard_object():
     # Check that the model could approximately learn the identity function
     test = [[1], [3], [4]]
     assert_array_almost_equal(clf.predict(test), [1, 3, 4], 2)
+
+
+def test_return_std():
+    # Test return_std option for both Bayesian regressors
+    def f(X):
+        return np.dot(X, w) + b
+
+    def f_noise(X, noise_mult):
+        return f(X) + np.random.randn(X.shape[0])*noise_mult
+
+    d = 5
+    n_train = 50
+    n_test = 10
+
+    w = np.array([1.0, 0.0, 1.0, -1.0, 0.0])
+    b = 1.0
+
+    X = np.random.random((n_train, d))
+    X_test = np.random.random((n_test, d))
+
+    for decimal, noise_mult in enumerate([1, 0.1, 0.01]):
+        y = f_noise(X, noise_mult)
+
+        m1 = BayesianRidge()
+        m1.fit(X, y)
+        y_mean1, y_std1 = m1.predict(X_test, return_std=True)
+        assert_array_almost_equal(y_std1, noise_mult, decimal=decimal)
+
+        m2 = ARDRegression()
+        m2.fit(X, y)
+        y_mean2, y_std2 = m2.predict(X_test, return_std=True)
+        assert_array_almost_equal(y_std2, noise_mult, decimal=decimal)
