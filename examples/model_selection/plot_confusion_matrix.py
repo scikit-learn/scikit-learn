e7c805a0c9efd887aa59922cc4fd2fa3dbdcb82e
Roman Shchedrin <rashchedrin@gmail.com>
2016-12-03 17:32:56 +0300

    deduplicated code

--- examples/model_selection/plot_confusion_matrix.py
+++ examples/model_selection/plot_confusion_matrix.py
@@ -72,18 +72,12 @@ def plot_confusion_matrix(cm, classes,
     plt.xticks(tick_marks, classes, rotation=45)
     plt.yticks(tick_marks, classes)
 
-
+    fmt = '.2f' if normalize else 'd'
     thresh = cm.max() / 2.
-    if normalize:
-        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-            plt.text(j, i, format(cm[i, j], '.2f'),
-                     horizontalalignment="center",
-                     color="white" if cm[i, j] > thresh else "black")
-    else:
-        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-            plt.text(j, i, cm[i, j],
-                     horizontalalignment="center",
-                     color="white" if cm[i, j] > thresh else "black")
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
 
     plt.tight_layout()
     plt.ylabel('True label')
