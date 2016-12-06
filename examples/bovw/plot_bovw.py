b5a5d7fc56eb99cf636416b25cd8a40721213c31
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-12-06 23:20:03 +0100

    Remove PCA and make it more pythonic

--- examples/bovw/plot_bovw.py
+++ examples/bovw/plot_bovw.py
@@ -38,19 +38,8 @@ from tudarmstadt import fetch_tu_darmstadt
 print(__doc__)
 
 
-def proc_image(path_image, rng, patch_size=(9, 9), max_patches=10000):
-    """ Function to extract couple of patches from an image """
-
-    # Read the current image
-    im = imread(path_image)
-    # Extract patches
-    patch = extract_patches_2d(im, patch_size=patch_size,
-                               max_patches=max_patches, random_state=rng)
-    return patch.reshape((max_patches, np.prod(patch_size) * len(im.shape)))
-
-
-def image_extraction_projection(path_image, dict_PCA, rng,
-                                patch_size=(9, 9), max_patches=100000):
+def image_extraction(path_image, rng, patch_size=(9, 9),
+                                max_patches=100000):
     """ Function to extract a couple of patches from an image and apply PCA """
 
     # Read the current image
@@ -59,14 +48,12 @@ def image_extraction_projection(path_image, dict_PCA, rng,
     patch = extract_patches_2d(im, patch_size=patch_size,
                                max_patches=max_patches, random_state=rng)
 
-    return dict_PCA.transform(patch.reshape((patch.shape[0],
-                                             np.prod(patch_size) *
-                                             len(im.shape))))
+    return patch.reshape((patch.shape[0], np.prod(patch_size) * len(im.shape)))
 
 # Define the parameters in use afterwards
 patch_size = (9, 9)
 max_patches = 100
-n_jobs = -1
+n_jobs = 1
 n_components = 9
 max_patches_classify = 2000
 nb_words = 50
@@ -75,38 +62,11 @@ rng = 42
 # Load the data
 png_files, labels = fetch_tu_darmstadt()
 
-# We will extract some patches from the different images to construct
-# a PCA codebook that will be used afterwards
-# Extract the patches
-patch_arr = Parallel(n_jobs=n_jobs)(delayed(proc_image)(path_im,
-                                                        rng,
-                                                        patch_size,
-                                                        max_patches)
-                                    for path_im in png_files)
+# Extract the data and project them
+patch_arr = [image_extraction(path_im, rng, patch_size, max_patches_classify)
+             for path_im in png_files]
 
-print('Extracted patch to build dictionary')
-
-# Create a plain matrix to apply the PCA decomposition
-patch_arr = np.array(patch_arr, copy=False)
-patch_arr = patch_arr.reshape((patch_arr.shape[0] * patch_arr.shape[1],
-                               patch_arr.shape[2]))
-# Build a PCA dictionary
-dict_PCA = PCA(n_components=n_components, random_state=rng)
-dict_PCA.fit(patch_arr)
-
-print('Built the PCA dictionary')
-
-# Extract the data and project them using the PCA codebook
-# Extract and project all the image feature
-patch_arr = Parallel(n_jobs=n_jobs)(delayed(image_extraction_projection)
-                                    (path_im,
-                                     dict_PCA,
-                                     rng,
-                                     patch_size,
-                                     max_patches_classify)
-                                    for path_im in png_files)
-
-print('Extracted and projected patches for image classification')
+print('Extracted patches for image classification')
 
 # Apply a stratified K-fold classification in which we will learn
 # a dictionary
@@ -128,26 +88,22 @@ vq.fit(stack_training)
 print('Codebook learnt')
 
 # Build the training and testing data
-train_data = []
-for tr_im in train_idx:
-    train_data.append(np.histogram(vq.predict(patch_arr[tr_im]),
-                                   bins=range(nb_words),
-                                   density=True))
-train_data = np.array(train_data, copy=False)
+train_data = np.array([np.histogram(vq.predict(patch_arr[tr_im]),
+                                    bins=range(nb_words),
+                                    density=True)
+                       for tr_im in train_idx])
 train_data = np.vstack(train_data[:, 0])
 train_label = labels[train_idx]
 
-test_data = []
-for te_im in test_idx:
-    test_data.append(np.histogram(vq.predict(patch_arr[te_im]),
-                                  bins=range(nb_words),
-                                  density=True))
-test_data = np.array(test_data, copy=False)
+test_data = np.array([np.histogram(vq.predict(patch_arr[te_im]),
+                                   bins=range(nb_words),
+                                   density=True)
+                      for te_im in test_idx])
 test_data = np.vstack(test_data[:, 0])
 test_label = labels[test_idx]
 
 # Classification using Random Forest
-rf = RandomForestClassifier(n_estimators=10, random_state=rng)
+rf = RandomForestClassifier(n_estimators=10, random_state=rng, n_jobs=n_jobs)
 pred = rf.fit(train_data, train_label).predict(test_data)
 
 print('Classification performed - the confusion matrix obtained is:')
