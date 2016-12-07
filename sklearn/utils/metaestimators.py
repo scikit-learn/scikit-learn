da0d0a599805d44f579f3c7235eed007396750ce
Andreas Mueller <t3kcit@gmail.com>
2016-12-07 18:03:30 -0500

    simplify logic in _safe_split

--- sklearn/utils/metaestimators.py
+++ sklearn/utils/metaestimators.py
@@ -124,22 +124,19 @@ def _safe_split(estimator, X, y, indices, train_indices=None):
         Sliced targets.
 
     """
-    if not hasattr(X, "shape"):
-        if getattr(estimator, "_pairwise", False):
+    if getattr(estimator, "_pairwise", False):
+        if not hasattr(X, "shape"):
             raise ValueError("Precomputed kernels or affinity matrices have "
                              "to be passed as arrays or sparse matrices.")
-        X_subset = [X[index] for index in indices]
-    else:
-        if getattr(estimator, "_pairwise", False):
-            # X is a precomputed square kernel matrix
-            if X.shape[0] != X.shape[1]:
-                raise ValueError("X should be a square kernel matrix")
-            if train_indices is None:
-                X_subset = X[np.ix_(indices, indices)]
-            else:
-                X_subset = X[np.ix_(indices, train_indices)]
+        # X is a precomputed square kernel matrix
+        if X.shape[0] != X.shape[1]:
+            raise ValueError("X should be a square kernel matrix")
+        if train_indices is None:
+            X_subset = X[np.ix_(indices, indices)]
         else:
-            X_subset = safe_indexing(X, indices)
+            X_subset = X[np.ix_(indices, train_indices)]
+    else:
+        X_subset = safe_indexing(X, indices)
 
     if y is not None:
         y_subset = safe_indexing(y, indices)
