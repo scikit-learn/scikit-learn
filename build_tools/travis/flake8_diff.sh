0e91abbf473a7f051b9394d784aa309a8fa27787
Joel Nothman <joel.nothman@gmail.com>
2016-12-12 23:04:39 +1100

    Remove debug statements; clean up comments

--- build_tools/travis/flake8_diff.sh
+++ build_tools/travis/flake8_diff.sh
@@ -126,19 +126,16 @@ MODIFIED_FILES="$(git diff --name-only $COMMIT_RANGE | grep -v 'sklearn/external
 check_files() {
 	files="$1"
 	options="$2"
-	echo "$files"
-	echo $options
+	# Conservative approach: diff without context (--unified=0) so that code
+    # that was not changed does not create failures
 	git diff --unified=0 $COMMIT -- $files | flake8 --diff --show-source $options
 }
 
 if [[ "$MODIFIED_FILES" == "no_match" ]]; then
     echo "No file outside sklearn/externals and doc/sphinxext/sphinx_gallery has been modified"
 else
-    # Conservative approach: diff without context so that code that
-    # was not changed does not create failures
 	check_files "$(echo "$MODIFIED_FILES" | grep -v ^examples)"
-    # Examples are allowed to not have imports at top of file due to print(__doc__)
+    # Examples are allowed to not have imports at top of file
     check_files "$(echo "$MODIFIED_FILES" | grep ^examples)" --ignore=E402
-    check_files "$MODIFIED_FILES"
 fi
 echo -e "No problem detected by flake8\n"
