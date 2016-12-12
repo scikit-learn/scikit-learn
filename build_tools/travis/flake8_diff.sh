655c1d959c63d08e0ae3f335dd51560b53e932b9
Loïc Estève <loic.esteve@ymail.com>
2016-12-12 13:51:43 +0100

    COSMIT use spaces instead of tabs

--- build_tools/travis/flake8_diff.sh
+++ build_tools/travis/flake8_diff.sh
@@ -124,17 +124,17 @@ MODIFIED_FILES="$(git diff --name-only $COMMIT_RANGE | grep -v 'sklearn/external
                      grep -v 'doc/sphinxext/sphinx_gallery' || echo "no_match")"
 
 check_files() {
-	files="$1"
-	options="$2"
-	# Conservative approach: diff without context (--unified=0) so that code
+    files="$1"
+    options="$2"
+    # Conservative approach: diff without context (--unified=0) so that code
     # that was not changed does not create failures
-	git diff --unified=0 $COMMIT -- $files | flake8 --diff --show-source $options
+    git diff --unified=0 $COMMIT -- $files | flake8 --diff --show-source $options
 }
 
 if [[ "$MODIFIED_FILES" == "no_match" ]]; then
     echo "No file outside sklearn/externals and doc/sphinxext/sphinx_gallery has been modified"
 else
-	check_files "$(echo "$MODIFIED_FILES" | grep -v ^examples)"
+    check_files "$(echo "$MODIFIED_FILES" | grep -v ^examples)"
     # Examples are allowed to not have imports at top of file
     check_files "$(echo "$MODIFIED_FILES" | grep ^examples)" --ignore=E402
 fi
