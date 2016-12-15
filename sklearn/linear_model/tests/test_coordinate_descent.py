74d1e7e5c360b8fcb58426a86c99f1bc6af95275
Thierry Guillemot <thierry.guillemot.work@gmail.com>
2016-12-15 12:23:56 +0100

    Add tests to check floatting issue for MultiTaskElasticNet.

--- sklearn/linear_model/tests/test_coordinate_descent.py
+++ sklearn/linear_model/tests/test_coordinate_descent.py
@@ -681,9 +681,7 @@ def test_enet_float_precision():
     for normalize in [True, False]:
         for fit_intercept in [True, False]:
             coef = {}
-            coef_multi = {}
             intercept = {}
-            intercept_multi = {}
             for dtype in [np.float64, np.float32]:
                 clf = ElasticNet(alpha=0.5, max_iter=100, precompute=False,
                                  fit_intercept=fit_intercept,
@@ -693,8 +691,8 @@ def test_enet_float_precision():
                 y = dtype(y)
                 ignore_warnings(clf.fit)(X, y)
 
-                coef[dtype] = clf.coef_
-                intercept[dtype] = clf.intercept_
+                coef[('simple', dtype)] = clf.coef_
+                intercept[('simple', dtype)] = clf.intercept_
 
                 assert_equal(clf.coef_.dtype, dtype)
 
@@ -709,33 +707,23 @@ def test_enet_float_precision():
                 assert_array_almost_equal(clf.intercept_,
                                           clf_precompute.intercept_)
 
-                # test multioutput case
-                new_y = np.hstack((y[:, np.newaxis], y[:, np.newaxis]))
-                clf_multioutput = ElasticNet(alpha=0.5, max_iter=100,
-                                             fit_intercept=fit_intercept,
-                                             normalize=normalize)
-
-                ignore_warnings(clf_multioutput.fit)(X, new_y)
-
-                coef_multi[dtype] = clf_multioutput.coef_
-
-                intercept_multi[dtype] = clf_multioutput.intercept_
-
+                # test multi task enet
+                multi_y = np.hstack((y[:, np.newaxis], y[:, np.newaxis]))
+                clf_multioutput = MultiTaskElasticNet(
+                    alpha=0.5, max_iter=100, fit_intercept=fit_intercept,
+                    normalize=normalize)
+                clf_multioutput.fit(X, multi_y)
+                coef[('multi', dtype)] = clf_multioutput.coef_
+                intercept[('multi', dtype)] = clf_multioutput.intercept_
                 assert_equal(clf.coef_.dtype, dtype)
 
-
-            assert_array_almost_equal(coef[np.float32], coef[np.float64],
-                                      decimal=4)
-            assert_array_almost_equal(intercept[np.float32],
-                                      intercept[np.float64],
-                                      decimal=4)
-
-            assert_array_almost_equal(coef_multi[np.float32][0],
-                                      coef_multi[np.float64][0],
-                                      decimal=4)
-            assert_array_almost_equal(intercept_multi[np.float32],
-                                      intercept_multi[np.float64],
-                                      decimal=4)
+            for v in ['simple', 'multi']:
+                assert_array_almost_equal(coef[(v, np.float32)],
+                                          coef[(v, np.float64)],
+                                          decimal=4)
+                assert_array_almost_equal(intercept[(v, np.float32)],
+                                          intercept[(v, np.float64)],
+                                          decimal=4)
 
 
 def test_enet_l1_ratio():
