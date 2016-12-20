9345bd154bc5a679493af661da0b2ba2ecbc4628
Joel Nothman <joel.nothman@gmail.com>
2016-12-20 21:12:07 +1100

    CI fix bug in getting changed docs when no sklearn/ files modified

--- build_tools/circle/build_doc.sh
+++ build_tools/circle/build_doc.sh
@@ -135,7 +135,10 @@ affected_doc_paths() {
 	echo "$files" | grep ^doc/.*\.rst | sed 's/^doc\/\(.*\)\.rst$/\1.html/'
 	echo "$files" | grep ^examples/.*.py | sed 's/^\(.*\)\.py$/auto_\1.html/'
 	sklearn_files=$(echo "$files" | grep '^sklearn/')
-	grep -hlR -f<(echo "$sklearn_files" | sed 's/^/scikit-learn\/blob\/[a-z0-9]*\//') doc/_build/html/stable/modules/generated | cut -d/ -f5-
+	if [ -n "$sklearn_files" ]
+	then
+		grep -hlR -f<(echo "$sklearn_files" | sed 's/^/scikit-learn\/blob\/[a-z0-9]*\//') doc/_build/html/stable/modules/generated | cut -d/ -f5-
+	fi
 }
 
 if [ -n "$CI_PULL_REQUEST" ]
