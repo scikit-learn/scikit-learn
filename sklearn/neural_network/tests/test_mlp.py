7dd880967a69dba7ba45c73aa7c444ff3cb73977
Pham, Vincent (CONT) <Vincent.Pham@capitalone.com>
2016-12-28 15:38:53 -0800

    updated ignore warning

--- sklearn/neural_network/tests/test_mlp.py
+++ sklearn/neural_network/tests/test_mlp.py
@@ -564,6 +564,7 @@ def test_adaptive_learning_rate():
     assert_greater(1e-6, clf._optimizer.learning_rate)
 
 
+@ignore_warnings(RuntimeError)
 def test_warm_start():
     X = X_iris
     y = y_iris
@@ -574,17 +575,16 @@ def test_warm_start():
     y_4classes = np.array([0] * 37 + [1] * 37 + [2] * 38 + [3] * 38)
     y_5classes = np.array([0] * 30 + [1] * 30 + [2] * 30 + [3] * 30 + [4] * 30)
 
-    with ignore_warnings(category=Warning):
-        # No error raised
+    # No error raised
+    clf = MLPClassifier(hidden_layer_sizes=2, solver='lbfgs',
+                        warm_start=True).fit(X, y)
+    clf.fit(X, y)
+    clf.fit(X, y_3classes)
+
+    for y_i in (y_2classes, y_3classes_alt, y_4classes, y_5classes):
         clf = MLPClassifier(hidden_layer_sizes=2, solver='lbfgs',
                             warm_start=True).fit(X, y)
-        clf.fit(X, y)
-        clf.fit(X, y_3classes)
-
-        for y_i in (y_2classes, y_3classes_alt, y_4classes, y_5classes):
-            clf = MLPClassifier(hidden_layer_sizes=2, solver='lbfgs',
-                                warm_start=True).fit(X, y)
-            message = ('warm_start can only be used where `y` has the same '
-                       'classes as in the previous call to fit.'
-                       ' Previously got [0 1 2], `y` has %s' % np.unique(y_i))
-            assert_raise_message(ValueError, message, clf.fit, X, y_i)
+        message = ('warm_start can only be used where `y` has the same '
+                   'classes as in the previous call to fit.'
+                   ' Previously got [0 1 2], `y` has %s' % np.unique(y_i))
+        assert_raise_message(ValueError, message, clf.fit, X, y_i)
