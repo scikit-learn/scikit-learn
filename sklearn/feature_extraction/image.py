f1f93b09093aafdc4c26bb2a04b8cf4b39656b4d
Kevin Mader <kmader@users.noreply.github.com>
2017-01-08 11:16:15 +0100

    Update image.py
    Adding a ```reconstruct_from_patches_3d``` method for reconstructing 3d images from patches made with ```extract_patches```

--- sklearn/feature_extraction/image.py
+++ sklearn/feature_extraction/image.py
@@ -435,6 +435,73 @@ def reconstruct_from_patches_2d(patches, image_size):
     return img
 
 
+def reconstruct_from_patches_3d(patches, image_size):
+    """Reconstruct a 3D image from all of its patches.
+    Patches are assumed to overlap and the image is constructed by filling in
+    the patches from left to right, top to bottom, averaging the overlapping
+    regions.
+    Read more in the :ref:`User Guide <image_feature_extraction>`.
+    Parameters
+    ----------
+    patches : array, shape = (n_patches, patch_height, patch_width, patch_depth) or
+        (n_patches, patch_height, patch_width, patch_depth, n_channels)
+        The complete set of patches. If the patches contain colour information,
+        channels are indexed along the last dimension: RGB patches would
+        have `n_channels=3`.
+    image_size : tuple of ints (image_height, image_width, image_depth) or
+        (image_height, image_width, image_depth, n_channels)
+        the size of the image that will be reconstructed
+    Returns
+    -------
+    image : array, shape = image_size
+        the reconstructed image
+
+    Examples
+    --------
+
+    >>> import numpy as np
+    >>> raw_img = np.arange(64).reshape((4,4,4))
+    >>> patches = extract_patches(raw_img, patch_shape=(2, 2, 2), extraction_step=1).reshape((-1, 2, 2, 2))
+    >>> out_img = reconstruct_from_patches_3d(patches, (4,4,4))
+    >>> out_img.shape
+    (4, 4, 4)
+    >>> int(np.sum(np.abs(out_img - raw_img))*1000)
+    0
+    >>> raw_img_color = np.arange(192).reshape((4,4,4,3))
+    >>> patches = extract_patches(raw_img_color, patch_shape=(2, 2, 2, 3), extraction_step=1).reshape((-1, 2, 2, 2, 3))
+    >>> out_img = reconstruct_from_patches_3d(patches, (4, 4, 4, 3))
+    >>> out_img.shape
+    (4, 4, 4, 3)
+    >>> int(np.sum(np.abs(out_img - raw_img_color))*1000)
+    0
+    >>> reconstruct_from_patches_3d(np.zeros((32, 4, 4)), (8, 8))
+    """
+    assert len(image_size) == 3 or len(
+        image_size) == 4, "Image size must be a 3- or 4-dimensional: Image Size:{}".format(image_size)
+    assert len(patches.shape) == len(
+        image_size) + 1, "Patch dimensions should be one larger than image, Patch:{} and Image:{}".format(patches.shape,
+                                                                                                          image_size)
+    i_h, i_w, i_d = image_size[:3]
+    p_h, p_w, p_d = patches.shape[1:4]
+    img = np.zeros(image_size)
+    # compute the dimensions of the patches array
+    n_h = i_h - p_h + 1
+    n_w = i_w - p_w + 1
+    n_d = i_d - p_d + 1
+    for p, (i, j, k) in zip(patches, product(range(n_h), range(n_w), range(n_d))):
+        img[i:i + p_h, j:j + p_w, k:k + p_d] += p
+
+    for i in range(i_h):
+        for j in range(i_w):
+            for k in range(i_d):
+                # divide by the amount of overlap
+                # XXX: is this the most efficient way? memory-wise yes, cpu wise?
+                img[i, j, k] /= float(min(i + 1, p_h, i_h - i) *
+                                      min(j + 1, p_w, i_w - j) *
+                                      min(k + 1, p_d, i_d - k))
+    return img
+
+
 class PatchExtractor(BaseEstimator):
     """Extracts patches from a collection of images
 
