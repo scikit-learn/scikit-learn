15616751b37f605aec3dc2a84b3e1116be59b607
Joshua Levy <jlevy@ojolabs.com>
2017-01-14 13:29:03 -0600

    fixing whitespace in test_make_moons

--- sklearn/datasets/tests/test_samples_generator.py
+++ sklearn/datasets/tests/test_samples_generator.py
@@ -367,5 +367,5 @@ def test_make_moons():
     for x, label in zip(X, y):
         center = [0.0, 0.0] if label == 0 else [1.0, 0.5]
         dist_sqr = ((x - center) ** 2).sum()
-        assert_almost_equal(dist_sqr, 1.0, 
-		    err_msg="Point is not on expected unit circle")
+        assert_almost_equal(dist_sqr, 1.0,
+            err_msg="Point is not on expected unit circle")
