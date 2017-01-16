2a5d6d77ab27cfe8005c14a590814a1872a7d478
Adam Kleczewski <adam.kleczewski@human.x.ai>
2017-01-16 14:15:55 -0500

    requested changes

--- sklearn/multilabel/classifier_chain.py
+++ sklearn/multilabel/classifier_chain.py
@@ -11,10 +11,37 @@ class ClassifierChain(BaseEstimator):
     of the available features provided to the model plus the predictions of
     models that are earlier in the chain.
 
-    By default the order of the chain is random although it can be specified
-    at the time of fitting with the chain_order parameter. Since the optimal
-    chain_order is not known a priori it is common to use the mean prediction
-    of an ensemble randomly ordered classifier chains.
+    Parameters
+    ----------
+    base_estimator : estimator
+        The base estimator from which the classifier chain is built.
+
+    random_state : int or RandomState, optional, default None
+        State or seed for random number generator.
+
+    order : list of integers, None, or 'random'
+        The order of the chain can be explicitly set by providing a list of
+        integers. For example, for a chain of length 5
+            order = [1, 3, 2, 4, 0]
+        means that the first model in the chain will make predictions for
+        column 1 in the Y matrix, the second model will make predictions
+        for column 3, etc.
+
+        If order is None the order will be determined by the
+        order of columns in the label matrix Y.
+            order = [0, 1, 2, ..., Y.shape[1] - 1]
+
+        If order is 'random' a random ordering will be used.
+
+    Attributes
+    ----------
+    estimators_ : list
+        A list of copies of base_estimator. Once fit the estimators in
+        this list will be ordered as specified by the order attribute.
+        The original order of labels (as specified by Y) is recovered in
+        the predict method by indexing into the predictions with the
+        list of indices in the order attribute.
+
 
     References
     ----------
@@ -23,63 +50,20 @@ class ClassifierChain(BaseEstimator):
 
     """
 
+
     def __init__(self, base_estimator, random_state=None,
                  order=None, shuffle=True):
-        """
-        Parameters
-        ----------
-        base_estimator : estimator
-            The base estimator from which the classifier chain is built.
-
-        random_state : int or RandomState, optional, default None
-            State or seed for random number generator.
-
-        order : list
-            A list of integers specifying the order of the classes in the
-            chain (0 indexed). For example, for a chain of length 5
-                order = [1, 3, 2, 4, 0]
-            means that the first model in the chain will make predictions for
-            column 1 in the Y matrix, the second model will make predictions
-            for column 3, etc. If chain_order is not None it must have a length
-            equal to the number of columns in Y.
-            If chain_order is None an ordered list of integers will be used
-                chain_order = [0, 1, 2, ..., Y.shape[1]]
-            where Y is the label matrix passed to the fit method.
-
-        shuffle : bool
-            If true order is shuffled
-        """
         self.base_estimator = base_estimator
         self.random_state = random_state
-        if order is not None:
-            if not isinstance(order, list):
-                order = list(order)
-            if not all([isinstance(i, int) for i in order]):
-                raise ValueError("all elements of order parameter must be "
-                                 "integers")
-
         self.order = order
 
-        self.shuffle = shuffle
-
     def fit(self, X, Y):
-        """Fit the ClassifierChain model according to the given training (X)
-        and the true labels (Y) of classes that appear earlier in the chain as
-        defined by the order parameter.
+        """Fit the model to data matrix X and targets Y.
 
         Parameters
         ----------
         X : {array-like, sparse matrix}, shape (n_samples, n_features)
-        Y : array, shape (n_samples, n_classes)
-
-        Attributes
-        ----------
-        classifiers_ : list
-            A list of copies of base_estimator. Once fit the classifiers in
-            this list will be ordered as specified by the order attribute.
-            The original order of labels (as specified by Y) is recovered in
-            the predict method by indexing into the predictions with the
-            list of indices in the order attribute.
+        Y : {array-like, sparse matrix}, shape (n_samples, n_classes)
 
         Returns
         -------
@@ -90,29 +74,30 @@ class ClassifierChain(BaseEstimator):
         if issparse(Y):
             Y = Y.toarray()
 
+        if issparse(X):
+            X = X.toarray()
+
         random_state = check_random_state(self.random_state)
-        self.classifiers_ = [clone(self.base_estimator)
-                             for _ in range(Y.shape[1])]
 
-        if self.order is not None:
-            if not len(self.order) == Y.shape[1]:
-                raise ValueError("chain_order length must equal n_labels")
-        else:
+        if self.order is None:
             self.order = list(range(Y.shape[1]))
-
-        if self.shuffle:
+        elif self.order == 'random':
+            self.order = list(range(Y.shape[1]))
             random_state.shuffle(self.order)
+        elif sorted(self.order) != list(range(Y.shape[1])):
+                raise ValueError("invalid order")
 
-        for chain_idx, classifier in enumerate(self.classifiers_):
+        self.estimators_ = [clone(self.base_estimator)
+                            for _ in range(Y.shape[1])]
+
+        for chain_idx, estimator in enumerate(self.estimators_):
             previous_labels = Y[:, self.order[:chain_idx]]
             y = Y[:, self.order[chain_idx]]
             X_aug = np.hstack((X, previous_labels))
-            classifier.fit(X_aug, y)
+            estimator.fit(X_aug, y)
 
     def predict(self, X):
-        """For each row in X make a prediction for each of the models in
-        the chain using the features available in X plus the predictions of
-        previous models in the chain as defined by the order parameter.
+        """Predict on the data matrix X using the ClassifierChain model.
 
         Parameters
         ----------
@@ -120,14 +105,17 @@ class ClassifierChain(BaseEstimator):
 
         Returns
         -------
-        Y_pred : array, shape (n_samples, n_classes)
+        Y_pred-like : array-like, shape (n_samples, n_classes)
         """
 
-        Y_pred_chain = np.zeros((X.shape[0], len(self.classifiers_)))
-        for chain_idx, classifier in enumerate(self.classifiers_):
+        if issparse(X):
+            X = X.toarray()
+
+        Y_pred_chain = np.zeros((X.shape[0], len(self.estimators_)))
+        for chain_idx, estimator in enumerate(self.estimators_):
             previous_predictions = Y_pred_chain[:, :chain_idx]
             X_aug = np.hstack((X, previous_predictions))
-            Y_pred_chain[:, chain_idx] = classifier.predict(X_aug)
+            Y_pred_chain[:, chain_idx] = estimator.predict(X_aug)
         chain_key = [self.order.index(i) for i in range(len(self.order))]
         Y_pred = Y_pred_chain[:, chain_key]
 
--- sklearn/multilabel/tests/test_classifier_chain.py
+++ sklearn/multilabel/tests/test_classifier_chain.py
@@ -1,9 +1,8 @@
 from sklearn.multilabel import ClassifierChain
 from sklearn.datasets import make_multilabel_classification
 from sklearn.linear_model import LogisticRegression
-from sklearn.utils.testing import assert_equal, assert_greater
+from sklearn.utils.testing import assert_equal, assert_greater, assert_not_equal
 from sklearn.metrics import jaccard_similarity_score
-
 from scipy import sparse
 
 
@@ -19,17 +18,17 @@ def test_fit_and_predict():
     assert_equal(Y_pred.shape, Y.shape)
     assert_greater(jaccard_similarity_score(Y, Y_pred_binary), 0.5)
 
-
-def test_fit_and_predict_with_sparse_labels():
-    """Fit classifier chain with sparse labels"""
+def test_fit_and_predict_with_data_and_labels():
+    """Fit classifier chain with sparse data and labels"""
     X, Y = make_multilabel_classification(n_samples=10000,
                                           n_features=100,
                                           n_classes=10)
 
     Y_sparse = sparse.csr_matrix(Y)
+    X_sparse = sparse.csr_matrix(X)
     classifier_chain = ClassifierChain(LogisticRegression())
-    classifier_chain.fit(X, Y_sparse)
-    Y_pred = classifier_chain.predict(X)
+    classifier_chain.fit(X_sparse, Y_sparse)
+    Y_pred = classifier_chain.predict(X_sparse)
     assert_equal(Y_pred.shape, Y.shape)
 
 
@@ -39,10 +38,9 @@ def test_order_shuffle():
                                           n_features=100,
                                           n_classes=10)
     classifier_chain = ClassifierChain(LogisticRegression(),
-                                       order=range(10),
-                                       shuffle=True)
+                                       order='random')
     classifier_chain.fit(X, Y)
-    assert (classifier_chain.order != list(range(10)))
+    assert_not_equal(classifier_chain.order, list(range(10)))
     assert (len(classifier_chain.order) == 10)
     assert (len(set(classifier_chain.order)) == 10)
 
@@ -55,5 +53,11 @@ def test_classifiers_coef_size():
     classifier_chain = ClassifierChain(LogisticRegression(), shuffle=True)
     classifier_chain.fit(X, Y)
 
-    assert_equal([c.coef_.size for c in classifier_chain.classifiers_],
+    assert_equal([c.coef_.size for c in classifier_chain.estimators_],
                  list(range(X.shape[1], X.shape[1] + Y.shape[1])))
+
+if __name__=='__main__':
+    test_classifiers_coef_size()
+    test_order_shuffle()
+    test_fit_and_predict()
+    test_fit_and_predict_with_data_and_labels()
