411b5c30ab8c09ff248abcc205353e51682503dd
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2017-01-25 01:04:02 +0100

    Fix esthetic example roc crossval

--- examples/model_selection/plot_roc_crossval.py
+++ examples/model_selection/plot_roc_crossval.py
@@ -62,32 +62,34 @@ cv = StratifiedKFold(n_splits=6)
 classifier = svm.SVC(kernel='linear', probability=True,
                      random_state=random_state)
 
-mean_tpr = 0.0
+tprs = []
 mean_fpr = np.linspace(0, 1, 100)
 
-colors = cycle(['cyan', 'indigo', 'seagreen', 'yellow', 'blue', 'darkorange'])
-lw = 2
-
 i = 0
-for (train, test), color in zip(cv.split(X, y), colors):
+for train, test in cv.split(X, y):
     probas_ = classifier.fit(X[train], y[train]).predict_proba(X[test])
     # Compute ROC curve and area the curve
     fpr, tpr, thresholds = roc_curve(y[test], probas_[:, 1])
-    mean_tpr += interp(mean_fpr, fpr, tpr)
-    mean_tpr[0] = 0.0
+    tprs.append(interp(mean_fpr, fpr, tpr))
+    tprs[-1][0] = 0.0
     roc_auc = auc(fpr, tpr)
-    plt.plot(fpr, tpr, lw=lw, color=color,
-             label='ROC fold %d (area = %0.2f)' % (i, roc_auc))
+    plt.plot(fpr, tpr, lw=1, color='b', alpha=0.15,
+             label='ROC fold %d (AUC = %0.2f)' % (i, roc_auc))
 
     i += 1
-plt.plot([0, 1], [0, 1], linestyle='--', lw=lw, color='k',
-         label='Luck')
+plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
+         label='Luck', alpha=.8)
 
-mean_tpr /= cv.get_n_splits(X, y)
+mean_tpr = np.mean(tprs, axis=0)
 mean_tpr[-1] = 1.0
 mean_auc = auc(mean_fpr, mean_tpr)
-plt.plot(mean_fpr, mean_tpr, color='g', linestyle='--',
-         label='Mean ROC (area = %0.2f)' % mean_auc, lw=lw)
+plt.plot(mean_fpr, mean_tpr, color='b',
+         label='Mean ROC (AUC = %0.2f)' % mean_auc, lw=2, alpha=.8)
+
+std_tpr = np.std(tprs, axis=0)
+tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+plt.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2)
 
 plt.xlim([-0.05, 1.05])
 plt.ylim([-0.05, 1.05])
