69b0877574a7a5af25ee3f06f7163615c0f3de00
Wazeer Zulfikar <wazeer.zulfikar@gmail.com>
2017-01-28 16:56:57 +0530

    Added tests for dictionary output

--- sklearn/metrics/tests/test_classification.py
+++ sklearn/metrics/tests/test_classification.py
@@ -20,6 +20,7 @@ from sklearn.utils.testing import assert_equal
 from sklearn.utils.testing import assert_almost_equal
 from sklearn.utils.testing import assert_array_equal
 from sklearn.utils.testing import assert_array_almost_equal
+from sklearn.utils.testing import assert_dict_equal
 from sklearn.utils.testing import assert_warns
 from sklearn.utils.testing import assert_no_warnings
 from sklearn.utils.testing import assert_warns_message
@@ -608,6 +609,25 @@ avg / total       0.51      0.53      0.47        75
 """
     report = classification_report(y_true, y_pred)
     assert_equal(report, expected_report)
+
+
+def test_classification_report_dictionary_output():
+
+    # Test performance report with dictionary output
+    iris = datasets.load_iris()
+    y_true, y_pred, _ = make_prediction(dataset=iris, binary=False)
+
+    # print classification report with class names
+    expected_report = {'avg / total': {'recall': '0.53', 'f1-score': '0.47', 'support': '75', 'precision': '0.51'},
+    'setosa': {'recall': '0.79', 'f1-score': '0.81', 'support': '24', 'precision': '0.83'},
+    'versicolor': {'recall': '0.10', 'f1-score': '0.15', 'support': '31', 'precision': '0.33'},
+    'virginica': {'recall': '0.90', 'f1-score': '0.57', 'support': '20', 'precision': '0.42'}}
+
+    report = classification_report(
+        y_true, y_pred, labels=np.arange(len(iris.target_names)),
+        target_names=iris.target_names,output_dict=True)
+
+    assert_dict_equal(report, expected_report)
 
 
 def test_classification_report_multiclass_with_digits():
