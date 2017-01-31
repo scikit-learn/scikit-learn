0e6a568bffb728f315e16884373e7f4dd319806e
bm424 <bm424@cam.ac.uk>
2017-01-31 13:43:47 +0000

    Introduce initialization, and basis c-means algorithm boilerplate

--- sklearn/cluster/c_means.py
+++ sklearn/cluster/c_means.py
@@ -4,18 +4,78 @@ import numpy as np
 
 from ..base import BaseEstimator, ClusterMixin, TransformerMixin
 from ..utils import check_array
+from ..utils import check_random_state
+from ..utils import as_float_array
+from ..externals.six import string_types
+
+
+def _validate_center_shape(X, n_centers, centers):
+    """Check if centers is compatible with X and n_centers"""
+    if len(centers) != n_centers:
+        raise ValueError('The shape of the initial centers (%s) '
+                         'does not match the number of clusters %i'
+                         % (centers.shape, n_centers))
+    if centers.shape[1] != X.shape[1]:
+        raise ValueError(
+            "The number of features of the initial centers %s "
+            "does not match the number of features of the data %s."
+            % (centers.shape[1], X.shape[1]))
+
 
 def c_means(X, n_clusters, n_init=10, max_iter=300, verbose=False, tol=1e-4,
-            random_state=None, algorithm="auto", return_n_iter=False):
+            random_state=None, algorithm="auto", return_n_iter=False,
+            copy_x=True):
     if n_init <= 0:
-        raise ValueError("Invalid number of initializations."
-                         " n_init={:d}")
+        raise ValueError('Number of initializations should be a positive'
+                         ' number, got {:d} instead.'.format(n_init))
+
+    random_state = check_random_state(random_state)
+
+    if max_iter <= 0:
+        raise ValueError('Number of iterations should be a positive number,'
+                         ' got {:d} instead.'.format(max_iter))
+
+    X = as_float_array(X, copy=copy_x)
+
+    X_mean = X.mean(axis=0)
+    X -= X_mean
+
+    if not copy_x:
+        X += X_mean
+
+def _cmeans_single_probabilistic(X, n_clusters, max_iter=300, random_state=None,
+                                 tol=1e-4):
+    random_state = check_random_state(random_state)
+
+    memberships_best, intertia_best, centers_best = None, None, None
+
+
+def _init_centroids(X, k, init, random_state=None):
+    random_state = check_random_state(random_state)
+    n_samples = X.shape[0]
+
+    if n_samples < k:
+        raise ValueError(
+            "`n_samples={:d} should be larger than n_clusters={:d}".format(
+                n_samples, k
+            )
+        )
+
+    if isinstance(init, string_types) and init == 'random':
+        seeds = random_state.permutation(n_samples)[:k]
+        centers = X[seeds]
+    else:
+        raise ValueError("`init` should be 'random'. '{}' (type '{}') "
+                         "was passed.".format(init, type(init)))
+
+    _validate_center_shape(X, k, centers)
+    return centers
 
 
 class CMeans(BaseEstimator, ClusterMixin, TransformerMixin):
 
     def __init__(self, n_clusters=8, n_init=10, max_iter=300, tol=1e-4,
-                 random_state=None, algorithm='auto'):
+                 random_state=None, algorithm='auto', copy_x=True):
 
         self.n_clusters = n_clusters
         self.max_iter = max_iter
@@ -23,10 +83,12 @@ class CMeans(BaseEstimator, ClusterMixin, TransformerMixin):
         self.n_init = n_init
         self.random_state = random_state
         self.algorithm = algorithm
+        self.copy_x = copy_x
 
     def fit(self, X, y=None):
         c_means(
             X, n_clusters=self.n_clusters, n_init=self.n_init,
             max_iter=self.max_iter, tol=self.tol,
-            random_state=self.random_state, algorithm=self.algorithm)
+            random_state=self.random_state, algorithm=self.algorithm,
+            copy_x=self.copy_x)
         return self
--- sklearn/cluster/tests/test_c_means.py
+++ sklearn/cluster/tests/test_c_means.py
@@ -2,16 +2,51 @@
 
 import numpy as np
 
-from sklearn.utils.testing import assert_raises_regex
+from sklearn.utils.testing import assert_raise_message
+from sklearn.utils.testing import assert_array_almost_equal
 
 from sklearn.cluster import CMeans, c_means
+from sklearn.datasets.samples_generator import make_blobs
 
 
-def test_c_means_n_init():
-    rnd = np.random.RandomState(0)
-    X = rnd.normal(size=(40, 2))
+centers = np.array([
+    [0.0, 5.0, 0.0, 0.0, 0.0],
+    [1.0, 1.0, 4.0, 0.0, 0.0],
+    [1.0, 0.0, 0.0, 5.0, 1.0],
+])
+n_samples = 100
+n_clusters, n_features = centers.shape
+X, true_labels = make_blobs(n_samples=n_samples, centers=centers,
+                            cluster_std=1., random_state=42)
 
-    # two regression tests on bad n_init argument
-    # previous bug: n_init <= 0 threw non-informative TypeError (#3858)
-    assert_raises_regex(ValueError, "n_init", CMeans(n_init=0).fit, X)
-    assert_raises_regex(ValueError, "n_init", CMeans(n_init=-1).fit, X)
+
+def test_n_init_error():
+    cm = CMeans(n_init=0)
+    assert_raise_message(ValueError,
+                         'Number of initializations should be a positive number,'
+                         ' got 0 instead.',
+                         cm.fit, X)
+    cm = CMeans(n_init=-1)
+    assert_raise_message(ValueError,
+                         'Number of initializations should be a positive number, got -1 instead.',
+                         cm.fit, X)
+
+def test_max_iter_error():
+    cm = CMeans(max_iter=0)
+    assert_raise_message(ValueError,
+                         'Number of iterations should be a positive number,'
+                         ' got 0 instead.',
+                         cm.fit, X)
+    cm = CMeans(max_iter=-1)
+    assert_raise_message(ValueError,
+                         'Number of iterations should be a positive number, got -1 instead.',
+                         cm.fit, X)
+
+
+def test_copyx():
+    # Check if copy_x=False returns nearly equal X after de-centering.
+    my_X = X.copy()
+    cm = CMeans(copy_x=False, n_clusters=n_clusters, random_state=42).fit(my_X)
+
+    # check if my_X is centered
+    assert_array_almost_equal(my_X, X)
