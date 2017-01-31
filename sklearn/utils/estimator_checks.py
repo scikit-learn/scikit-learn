70a74c6bf935828acc0581b1f97240d299f52f63
shubham0704 <shubham.bhardwaj2015@vit.ac.in>
2017-02-01 01:54:13 +0530

    [WEP] added check_rank_corr in estimator_checks

--- sklearn/utils/estimator_checks.py
+++ sklearn/utils/estimator_checks.py
@@ -9,6 +9,7 @@ from copy import deepcopy
 
 import numpy as np
 from scipy import sparse
+from scipy.stats import stats
 import struct
 
 from sklearn.externals.six.moves import zip
@@ -52,6 +53,7 @@ from sklearn.utils.fixes import signature
 from sklearn.utils.validation import has_fit_parameter
 from sklearn.preprocessing import StandardScaler
 from sklearn.datasets import load_iris, load_boston, make_blobs
+from sklearn.datasets import make_multilabel_classification
 
 
 BOSTON = None
@@ -114,7 +116,7 @@ def _yield_classifier_checks(name, Classifier):
     yield check_classifiers_regression_target
     if (name not in ["MultinomialNB", "LabelPropagation", "LabelSpreading"]
         # TODO some complication with -1 label
-            and name not in ["DecisionTreeClassifier",
+        and name not in ["DecisionTreeClassifier",
                              "ExtraTreeClassifier"]):
             # We don't raise a warning in these classifiers, as
             # the column y interface is used by the forests.
@@ -161,6 +163,7 @@ def _yield_regressor_checks(name, Regressor):
     yield check_regressors_no_decision_function
     yield check_supervised_y_2d
     yield check_supervised_y_no_nan
+    yield check_rank_corr
     if name != 'CCA':
         # check that the regressor handles int input
         yield check_regressors_int
@@ -268,8 +271,7 @@ def set_testing_parameters(estimator):
     # set parameters to speed up some estimators and
     # avoid deprecated behaviour
     params = estimator.get_params()
-    if ("n_iter" in params
-            and estimator.__class__.__name__ != "TSNE"):
+    if ("n_iter" in params and estimator.__class__.__name__ != "TSNE"):
         estimator.set_params(n_iter=5)
     if "max_iter" in params:
         warnings.simplefilter("ignore", ConvergenceWarning)
@@ -1096,8 +1098,7 @@ def check_classifiers_train(name, Classifier):
                     assert_equal(decision.shape, (n_samples,))
                     dec_pred = (decision.ravel() > 0).astype(np.int)
                     assert_array_equal(dec_pred, y_pred)
-                if (n_classes is 3
-                        and not isinstance(classifier, BaseLibSVM)):
+                if (n_classes is 3 and not isinstance(classifier, BaseLibSVM)):
                     # 1on1 of LibSVM works differently
                     assert_equal(decision.shape, (n_samples, n_classes))
                     assert_array_equal(np.argmax(decision, axis=1), y_pred)
@@ -1705,3 +1706,35 @@ def check_classifiers_regression_target(name, Estimator):
     e = Estimator()
     msg = 'Unknown label type: '
     assert_raises_regex(ValueError, msg, e.fit, X, y)
+
+
+@ignore_warnings(category=DeprecationWarning)
+def check_rank_corr(name, Estimator):
+    """
+    Check whether an estimator having both decision_function and
+    predict_proba methods has outputs with perfect rank correlation.
+    """
+
+    X, Y = make_multilabel_classification(n_classes=2, n_labels=1,
+                                          allow_unlabeled=True,
+                                          random_state=1)
+
+    estimator = Estimator()
+
+    set_testing_parameters(estimator)
+
+    if hasattr(estimator, "decision_function"):
+
+        if hasattr(estimator, "predict_proba"):
+            try:
+                estimator.fit(X, Y)
+                a = estimator.predict_proba([i for i in range(20)])
+                b = estimator.decision_function([i for i in range(20)])
+                assert_equal(a.shape, b.shape)
+                assert_equal(stats.spearmanr(a[0], b[0]).correlation, 1)
+
+            except ValueError:
+                pass
+
+    else:
+        pass
