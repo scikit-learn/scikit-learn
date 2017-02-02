7a7d41eea3821068697e0ec497098e5bc75b9d50
Isaac Laughlin <isaac.laughlin@galvanize.com>
2017-02-02 15:51:44 -0800

    Refactoring plot_iris example.

--- examples/svm/plot_iris.py
+++ examples/svm/plot_iris.py
@@ -39,55 +39,69 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import svm, datasets
 
+
+def make_meshgrid(x, y, h=.02):
+    """Create a mesh of points to plot in
+    """
+    x_min, x_max = x.min() - 1, x.max() + 1
+    y_min, y_max = y.min() - 1, y.max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    return xx, yy
+
+def plot_contours(ax, clf, xx, yy, **params):
+    """Plot the decision boundaries for a classifier.
+    """
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    out = ax.contourf(xx, yy, Z, **params)
+    ax.set_xlim(xx.min(), xx.max())
+    ax.set_ylim(yy.min(), yy.max())
+    return out
+
+def plot_points_boundary(ax, clf, x, y, xx, yy,
+                               points_params, contour_params):
+    """Plot the decision boundaries and points.
+    """
+    contours = plot_contours(ax, clf, xx, yy, **contour_params)
+    points = ax.scatter(x, y, **points_params)
+    return [contours, points]
+
 # import some data to play with
 iris = datasets.load_iris()
 X = iris.data[:, :2]  # we only take the first two features. We could
                       # avoid this ugly slicing by using a two-dim dataset
 y = iris.target
 
-h = .02  # step size in the mesh
-
 # we create an instance of SVM and fit out data. We do not scale our
 # data since we want to plot the support vectors
 C = 1.0  # SVM regularization parameter
-svc = svm.SVC(kernel='linear', C=C).fit(X, y)
-rbf_svc = svm.SVC(kernel='rbf', gamma=0.7, C=C).fit(X, y)
-poly_svc = svm.SVC(kernel='poly', degree=3, C=C).fit(X, y)
-lin_svc = svm.LinearSVC(C=C).fit(X, y)
-
-# create a mesh to plot in
-x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-                     np.arange(y_min, y_max, h))
+models = (svm.SVC(kernel='linear', C=C),
+          svm.LinearSVC(C=C),
+          svm.SVC(kernel='rbf', gamma=0.7, C=C),
+          svm.SVC(kernel='poly', degree=3, C=C))
+models = (clf.fit(X, y) for clf in models)
 
 # title for the plots
-titles = ['SVC with linear kernel',
+titles = ('SVC with linear kernel',
           'LinearSVC (linear kernel)',
           'SVC with RBF kernel',
-          'SVC with polynomial (degree 3) kernel']
+          'SVC with polynomial (degree 3) kernel')
 
+#Set-up 2x2 grid for plotting.
+fig, sub = plt.subplots(2,2)
+plt.subplots_adjust(wspace=0.4, hspace=0.4)
 
-for i, clf in enumerate((svc, lin_svc, rbf_svc, poly_svc)):
-    # Plot the decision boundary. For that, we will assign a color to each
-    # point in the mesh [x_min, x_max]x[y_min, y_max].
-    plt.subplot(2, 2, i + 1)
-    plt.subplots_adjust(wspace=0.4, hspace=0.4)
+xx, yy = make_meshgrid(X[:, 0], X[:, 1])
 
-    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-
-    # Put the result into a color plot
-    Z = Z.reshape(xx.shape)
-    plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
-
-    # Plot also the training points
-    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.coolwarm)
-    plt.xlabel('Sepal length')
-    plt.ylabel('Sepal width')
-    plt.xlim(xx.min(), xx.max())
-    plt.ylim(yy.min(), yy.max())
-    plt.xticks(())
-    plt.yticks(())
-    plt.title(titles[i])
+for clf, title, ax in zip(models, titles, sub.flatten()):
+    plot_points_boundary(ax, clf, X[:, 0], X[:, 1], xx, yy,
+                               dict(c=y, cmap=plt.cm.coolwarm),
+                               dict(cmap=plt.cm.coolwarm, alpha=0.8))
+    ax.set_xlabel('Sepal length')
+    ax.set_ylabel('Sepal width')
+    ax.set_xticks(())
+    ax.set_yticks(())
+    ax.set_title(title)
 
 plt.show()
