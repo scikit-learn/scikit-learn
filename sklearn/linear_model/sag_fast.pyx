9a9ebec050b493bc90624133fe6c6559c1d07920
shubham0704 <shubham.bhardwaj2015@vit.ac.in>
2017-02-04 16:08:51 +0530

    [WIP] Fixes #8289 minor changes

--- sklearn/linear_model/sag_fast.pyx
+++ sklearn/linear_model/sag_fast.pyx
@@ -562,12 +562,12 @@ cdef void predict_sample(double* x_data_ptr, int* x_ind_ptr, int xnnz,
 
         prediction[class_ind] = wscale * innerprod + intercept[class_ind]
 
-cdef get_max_squared_sum(X):
-     """
-     Computes the summation of square of individual elements along each row
-     finds the maximum and return it
-     Parameters:
-     -----------
-     X : np.ndarray
-     """
-     return np.sum(X ** 2, axis=1).max()
+def get_max_squared_sum(X):
+    """
+    Computes the summation of square of individual elements along each row
+    finds the maximum and return it
+    Parameters:
+    -----------
+    X : np.ndarray
+    """
+    return np.sum(X ** 2, axis=1).max()
