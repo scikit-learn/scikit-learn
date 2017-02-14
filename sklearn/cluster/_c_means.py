b59331265ad4f22ffffa0c2c72621fea531462b1
bm424 <bm424@cam.ac.uk>
2017-02-14 15:21:18 +0000

    Refactor for prediction

--- sklearn/cluster/_c_means.py
+++ sklearn/cluster/_c_means.py
@@ -1,91 +1,99 @@
 import numpy as np
 
 
-def _memberships_probabilistic(distances, m=2.):
-    """Calculate probabilistic memberships based on distances.
+class Probabilistic:
 
-    Parameters
-    ----------
-    distances : array-like, shape (n_samples, n_samples)
-        The distances from each point to every other.
-    m : float, optional, default 2.0
-        The fuzziness factor. Larger values dilute membership.
+    @staticmethod
+    def memberships(distances, m=2.):
+        """Calculate probabilistic memberships based on distances.
 
-    Returns
-    -------
-    memberships : float ndarray, shape (n_samples, n_clusters)
-        Updated memberships
+        Parameters
+        ----------
+        distances : array-like, shape (n_samples, n_samples)
+            The distances from each point to every other.
+        m : float, optional, default 2.0
+            The fuzziness factor. Larger values dilute membership.
 
-    """
-    distance_ratio = np.divide(distances[:, :, np.newaxis],
-                               distances[:, np.newaxis, :])
-    distance_ratio[np.isnan(distance_ratio)] = 1.
-    memberships = np.sum(np.power(distance_ratio, 2 / (m - 1)), axis=2) ** -1
-    return memberships
+        Returns
+        -------
+        memberships : float ndarray, shape (n_samples, n_clusters)
+            Updated memberships
 
+        """
+        distance_ratio = np.divide(distances[:, :, np.newaxis],
+                                   distances[:, np.newaxis, :])
+        distance_ratio[np.isnan(distance_ratio)] = 1.
+        memberships = np.sum(np.power(distance_ratio, 2 / (m - 1)), axis=2) ** -1
+        return memberships
 
-def _centers_probabilistic(X, memberships, m=2.):
-    """Calculate probabilistic centers based on memberships.
+    @staticmethod
+    def centers(X, memberships, m=2.):
+        """Calculate probabilistic centers based on memberships.
 
-    Parameters
-    ----------
-    X : array-like, shape (n_samples, n_features)
-        The data points to cluster.
-    memberships : float ndarray, shape (n_samples, n_clusters)
-        Membership of each data point to the cluster centers.
-    m : float, optional, default 2.0
-        The fuzziness factor. Larger values dilute membership.
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            The data points to cluster.
+        memberships : float ndarray, shape (n_samples, n_clusters)
+            Membership of each data point to the cluster centers.
+        m : float, optional, default 2.0
+            The fuzziness factor. Larger values dilute membership.
 
-    Returns
-    -------
-    centers : float ndarray, shape (n_clusters, n_features)
-        The positions of the cluster centers.
+        Returns
+        -------
+        centers : float ndarray, shape (n_clusters, n_features)
+            The positions of the cluster centers.
 
-    """
-    centers = np.dot(memberships.T ** m, X) / \
-              np.sum(memberships.T ** m, axis=1)[..., np.newaxis]
-    return centers
-
-
-def _memberships_possibilistic(distances, weights, m=2.):
-    """Calculate possibilistic memberships based on distances.
-
-    Parameters
-    ----------
-    distances : array-like, shape (n_samples, n_samples)
-        The distances from each point to every other.
-    weights : array-like, shape (n_clusters, )
-        The relative weighting of the clusters.
-    m : float, optional, default 2.0
-        The fuzziness factor. Larger values dilute membership.
-
-    Returns
-    -------
-    memberships : float ndarray, shape (n_samples, n_clusters)
-        Updated memberships
-
-    """
-    memberships = (1. + (distances / weights) ** (1. / (m - 1))) ** -1.
-    return memberships
+        """
+        centers = np.dot(memberships.T ** m, X) / \
+                  np.sum(memberships.T ** m, axis=1)[..., np.newaxis]
+        return centers
 
 
-def _centers_possibilistic(X, memberships, m=2.):
-    """Calculate possibilistic centers based on memberships.
+class Possibilistic:
 
-    Parameters
-    ----------
-    X : array-like, shape (n_samples, n_features)
-        The data points to cluster.
-    memberships : float ndarray, shape (n_samples, n_clusters)
-        Membership of each data point to the cluster centers.
-    m : float, optional, default 2.0
-        The fuzziness factor. Larger values dilute membership.
+    @staticmethod
+    def memberships(distances, weights, m=2.):
+        """Calculate possibilistic memberships based on distances.
 
-    Returns
-    -------
-    centers : float ndarray, shape (n_clusters, n_features)
-        The positions of the cluster centers.
+        Parameters
+        ----------
+        distances : array-like, shape (n_samples, n_samples)
+            The distances from each point to every other.
+        weights : array-like, shape (n_clusters, )
+            The relative weighting of the clusters.
+        m : float, optional, default 2.0
+            The fuzziness factor. Larger values dilute membership.
 
-    """
-    return np.divide(np.dot((memberships ** m).T, X),
-                     np.sum(memberships ** m, axis=0)[..., np.newaxis])
+        Returns
+        -------
+        memberships : float ndarray, shape (n_samples, n_clusters)
+            Updated memberships
+
+        """
+        memberships = (1. + (distances / weights) ** (1. / (m - 1))) ** -1.
+        return memberships
+
+    @staticmethod
+    def centers(X, memberships, m=2.):
+        """Calculate possibilistic centers based on memberships.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            The data points to cluster.
+        memberships : float ndarray, shape (n_samples, n_clusters)
+            Membership of each data point to the cluster centers.
+        m : float, optional, default 2.0
+            The fuzziness factor. Larger values dilute membership.
+
+        Returns
+        -------
+        centers : float ndarray, shape (n_clusters, n_features)
+            The positions of the cluster centers.
+
+        """
+        return np.divide(np.dot((memberships ** m).T, X),
+                         np.sum(memberships ** m, axis=0)[..., np.newaxis])
+
+
--- sklearn/cluster/tests/test_c_means.py
+++ sklearn/cluster/tests/test_c_means.py
@@ -9,14 +9,12 @@ from sklearn.utils.testing import assert_raise_message
 from sklearn.utils.testing import (assert_array_almost_equal,
                                    assert_array_less)
 
-from sklearn.cluster import CMeans
-from sklearn.cluster.c_means_ import (_cmeans_single_probabilistic,
-                                      _cmeans_single_possibilistic,
+from sklearn.cluster.c_means_ import (probabilistic_single,
+                                      possibilistic_single,
+                                      ProbabilisticCMeans,
+                                      PossibilisticCMeans,
                                       _init_centroids)
-from sklearn.cluster._c_means import (_memberships_probabilistic,
-                                      _centers_probabilistic,
-                                      _memberships_possibilistic,
-                                      _centers_possibilistic,)
+from sklearn.cluster._c_means import Probabilistic, Possibilistic
 
 from sklearn.datasets.samples_generator import make_blobs
 from sklearn.metrics.pairwise import euclidean_distances
@@ -36,12 +34,12 @@ X, true_labels = make_blobs(n_samples=n_samples, centers=centers,
 
 
 def test_n_init_error():
-    cm = CMeans(n_init=0)
+    cm = ProbabilisticCMeans(n_init=0)
     assert_raise_message(ValueError,
                          'Number of initializations should be a positive '
                          'number, got 0 instead.',
                          cm.fit, X)
-    cm = CMeans(n_init=-1)
+    cm = ProbabilisticCMeans(n_init=-1)
     assert_raise_message(ValueError,
                          'Number of initializations should be a positive '
                          'number, got -1 instead.',
@@ -49,12 +47,12 @@ def test_n_init_error():
 
 
 def test_max_iter_error():
-    cm = CMeans(max_iter=0)
+    cm = ProbabilisticCMeans(max_iter=0)
     assert_raise_message(ValueError,
                          'Number of iterations should be a positive number,'
                          ' got 0 instead.',
                          cm.fit, X)
-    cm = CMeans(max_iter=-1)
+    cm = ProbabilisticCMeans(max_iter=-1)
     assert_raise_message(ValueError,
                          'Number of iterations should be a positive number, '
                          'got -1 instead.',
@@ -64,7 +62,7 @@ def test_max_iter_error():
 def test_copyx():
     # Check if copy_x=False returns nearly equal X after de-centering.
     my_X = X.copy()
-    CMeans(copy_x=False, n_clusters=n_clusters, random_state=42).fit(my_X)
+    ProbabilisticCMeans(copy_x=False, n_clusters=n_clusters, random_state=42).fit(my_X)
 
     # check if my_X is centered
     assert_array_almost_equal(my_X, X)
@@ -78,69 +76,69 @@ def test_init_centroids_bounds():
 def test_probabilistic_update_memberships_shape():
     c = _init_centroids(X, n_clusters, 'random', random_state=42)
     distances = euclidean_distances(X, c)
-    m = _memberships_probabilistic(distances, 2)
+    m = Probabilistic.memberships(distances, 2)
     assert_equal(m.shape, (n_samples, n_clusters))
 
 
 def test_probabilistic_update_memberships_bounds():
     c = _init_centroids(X, n_clusters, 'random', random_state=42)
     distances = euclidean_distances(X, c)
-    m = _memberships_probabilistic(distances, 2)
+    m = Probabilistic.memberships(distances, 2)
     assert_array_less(m, 1e-6 + np.ones_like(m))
     assert_array_less(np.zeros_like(m) - 1e-6, m)
 
 
 def test_probabilistic_update_centers_shape():
     distances = euclidean_distances(X, centers)
-    m = _memberships_probabilistic(distances, 2)
-    c = _centers_probabilistic(X, m, 2)
+    m = Probabilistic.memberships(distances, 2)
+    c = Probabilistic.centers(X, m, 2)
     assert_equal(c.shape, (n_clusters, n_features))
 
 
 def test_probabilistic_membership_shape():
-    m, i, c, _ = _cmeans_single_probabilistic(X, n_clusters)
+    m = probabilistic_single(X, n_clusters)['memberships']
     assert_equal(m.shape, (n_samples, n_clusters))
 
 
 def test_probabilistic_center_shape():
-    m, i, c, _ = _cmeans_single_probabilistic(X, n_clusters)
+    c = probabilistic_single(X, n_clusters)['centers']
     assert_equal(c.shape, (n_clusters, n_features))
 
 
 def test_possibilistic_update_memberships_shape():
     c = _init_centroids(X, n_clusters, 'random', random_state=42)
     distances = euclidean_distances(X, c)
-    m = _memberships_possibilistic(distances, 2)
+    m = Possibilistic.memberships(distances, 2)
     assert_equal(m.shape, (n_samples, n_clusters))
 
 
 def test_possibilistic_update_memberships_bounds():
     c = _init_centroids(X, n_clusters, 'random', random_state=42)
     distances = euclidean_distances(X, c)
-    m = _memberships_possibilistic(distances, 2)
+    m = Possibilistic.memberships(distances, 2)
     assert_array_less(m, 1e-6 + np.ones_like(m))
     assert_array_less(np.zeros_like(m) - 1e-6, m)
 
 
 def test_possibilistic_update_centers_shape():
     distances = euclidean_distances(X, centers)
-    m = _memberships_possibilistic(distances, 2)
-    c = _centers_possibilistic(X, m, 2)
+    m = Possibilistic.memberships(distances, 2)
+    c = Possibilistic.centers(X, m, 2)
     assert_equal(c.shape, (n_clusters, n_features))
 
 
 def test_possibilistic_membership_shape():
-    m, i, c, _ = _cmeans_single_possibilistic(X, n_clusters)
+    m = possibilistic_single(X, n_clusters)['memberships']
     assert_equal(m.shape, (n_samples, n_clusters))
 
 
 def test_possibilistic_center_shape():
-    m, i, c, _ = _cmeans_single_possibilistic(X, n_clusters)
+    c = possibilistic_single(X, n_clusters)['centers']
     assert_equal(c.shape, (n_clusters, n_features))
     
 
 def test_labels():
-    cm = CMeans()
+    cm = ProbabilisticCMeans()
     cm.memberships_ = np.array([
         [1.0, 0.0],
         [0.7, 0.3],
@@ -158,13 +156,30 @@ def _check_fitted_model(cm):
 
 
 def test_probabilistic_results():
-    cm = CMeans(n_clusters=3, algorithm="probabilistic", random_state=4)
+    cm = ProbabilisticCMeans(n_clusters=3, algorithm="probabilistic", random_state=4)
     cm.fit(X)
     _check_fitted_model(cm)
 
 
 def test_possibilistic_results():
-    cm = CMeans(n_clusters=3, algorithm="possibilistic", random_state=4)
+    cm = PossibilisticCMeans(n_clusters=3, algorithm="possibilistic", random_state=4)
     cm.fit(X)
     _check_fitted_model(cm)
+
+
+def test_probabilistic_predict():
+    cm = ProbabilisticCMeans(n_clusters=3, algorithm="probabilistic", random_state=4)
+    cm.fit(X)
+    pred = cm.predict(centers)
+    assert_equal(pred.shape, (n_clusters, n_clusters))
+    assert_equal(v_measure_score(np.arange(n_clusters), np.argmax(pred, axis=1)), 1.0)
+
+
+def test_possibilistic_predict():
+    cm = PossibilisticCMeans(n_clusters=3, algorithm="possibilistic", random_state=4)
+    cm.fit(X)
+    pred = cm.predict(centers)
+    assert_equal(pred.shape, (n_clusters, n_clusters))
+    assert_equal(v_measure_score(np.arange(n_clusters), np.argmax(pred, axis=1)), 1.0)
+
 
