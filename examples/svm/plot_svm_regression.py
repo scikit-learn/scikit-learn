944be8c685c0bfdb4b4d08842e8a1b8f5cc132ff
Rishikesh <rishikksh20@gmail.com>
2017-02-16 03:46:46 +0530

    Modify SVR example for better plot
    Change coef0 value for better plot of poly kernal. Also
    improve the support vectors marking in SVR plot.
    
    Fixes: #8365

--- examples/svm/plot_svm_regression.py
+++ examples/svm/plot_svm_regression.py
@@ -23,9 +23,9 @@ y[::5] += 3 * (0.5 - np.random.rand(8))
 
 ###############################################################################
 # Fit regression model
-svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
-svr_lin = SVR(kernel='linear', C=1e3)
-svr_poly = SVR(kernel='poly', C=1e3, degree=2)
+svr_rbf = SVR(kernel='rbf', C=100, gamma=0.1, epsilon=.1)
+svr_lin = SVR(kernel='linear', C=100)
+svr_poly = SVR(kernel='poly', C=100, degree=3, epsilon=.1, coef0=1)
 y_rbf = svr_rbf.fit(X, y).predict(X)
 y_lin = svr_lin.fit(X, y).predict(X)
 y_poly = svr_poly.fit(X, y).predict(X)
@@ -33,7 +33,12 @@ y_poly = svr_poly.fit(X, y).predict(X)
 ###############################################################################
 # look at the results
 lw = 2
-plt.scatter(X, y, color='darkorange', label='data')
+plt.scatter(X[svr_rbf.support_], y[svr_rbf.support_], facecolor="none", edgecolor="k", marker='^',
+            label='rbf support vectors', s=40)
+plt.scatter(X[svr_lin.support_], y[svr_lin.support_], facecolor="none", edgecolor="k", marker='o',
+            label='lin support vectors', s=40)
+plt.scatter(X[svr_poly.support_], y[svr_poly.support_], facecolor="none", edgecolor="k", marker='s',
+            label='poly support vectors', s=40)
 plt.hold('on')
 plt.plot(X, y_rbf, color='navy', lw=lw, label='RBF model')
 plt.plot(X, y_lin, color='c', lw=lw, label='Linear model')
