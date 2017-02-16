7be0c9e4d71bffebba59c3f8870c008e66dc7e9c
Loïc Estève <loic.esteve@ymail.com>
2017-02-16 07:07:54 +0100

    TRAVIS revert flake8 version to 2.5.1

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -118,5 +118,8 @@ except ImportError:
 fi
 
 if [[ "$RUN_FLAKE8" == "true" ]]; then
-    conda install --yes flake8
+    # flake8 version is temporarily set to 2.5.1 because the next
+    # version available on conda (3.3.0) has a bug that checks non
+    # python files and cause non meaningful flake8 errors
+    conda install --yes flake8=2.5.1
 fi
