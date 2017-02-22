11709a3ec10f58df57a16d1ed57184da37d64ddb
Raghav RV <rvraghav93@gmail.com>
2017-02-22 18:42:49 +0100

    EXA Use OrderedDict; Make it easier to add more transformations

--- examples/preprocessing/plot_all_scaling.py
+++ examples/preprocessing/plot_all_scaling.py
@@ -23,7 +23,10 @@ print(__doc__)
 #          Thomas Unterthiner
 # License: BSD 3 clause
 
+from collections import OrderedDict
+
 import numpy as np
+import scipy
 
 import matplotlib as mpl
 from matplotlib import pyplot as plt
@@ -48,14 +51,26 @@ X_full, y_full = dataset.data, dataset.target
 
 X = X_full[:, [0, 5]]
 
-X_min_max_scaled = MinMaxScaler().fit_transform(X)
-X_max_abs_scaled = MaxAbsScaler().fit_transform(X)
-X_standard_scaled = StandardScaler().fit_transform(X)
-X_robust_scaled = RobustScaler(quantile_range=(25, 75)).fit_transform(X)
-X_l2_normalized = Normalizer().fit_transform(X)
-X_quantile_normalized = QuantileNormalizer().fit_transform(X)
+distributions = OrderedDict((
+    ('Unscaled data', X),
+    ('Data after min-max scaling',
+        MinMaxScaler().fit_transform(X)),
+    ('Data after robust scaling',
+        RobustScaler(quantile_range=(25, 75)).fit_transform(X)),
+    ('Data after max-abs scaling',
+        MaxAbsScaler().fit_transform(X)),
+    ('Data after standard scaling',
+        StandardScaler().fit_transform(X)),
+    ('Data after sample-wise L2 normalizing',
+        Normalizer().fit_transform(X)),
+    ('Data after quantile normalizing (uniform pdf)',
+        QuantileNormalizer(output_pdf=scipy.stats.uniform)
+        .fit_transform(X)),
+    ('Data after quantile normalizing (gaussian pdf)',
+        QuantileNormalizer(output_pdf=scipy.stats.norm)
+        .fit_transform(X))))
 
-y = minmax_scale(y_full)  # To make colors corresponding to the target
+y = minmax_scale(y_full)  # To make colors corresponding to the targe),
 
 
 def plot_distribution(axes, X, y, hist_nbins=50, plot_title="", size=(15, 10),
@@ -90,25 +105,19 @@ def plot_distribution(axes, X, y, hist_nbins=50, plot_title="", size=(15, 10),
                  color='grey', ec='grey')
     hist_X0.axis('off')
 
-fig = plt.figure(figsize=(15, 50))
-gs = gridspec.GridSpec(15, 5,
+n_dist = len(distributions)
+fig = plt.figure(figsize=(15, n_dist * 8 + 1))
+gs = gridspec.GridSpec(n_dist * 2 + 1, 5,
                        width_ratios=[5, 1, 0.1, 5, 1], wspace=0.3,
-                       height_ratios=[5, 1] * 7 + [0.4], hspace=0.4)
+                       height_ratios=[5, 1] * n_dist + [0.4],
+                       hspace=0.4)
 subplots = list(plt.subplot(g) for g in gs)
 
-for i, (X, title) in enumerate((
-        (X, "Unscaled data"),
-        (X_min_max_scaled, "Data after min-max scaling"),
-        (X_robust_scaled, "Data after robust scaling"),
-        (X_max_abs_scaled, "Data after max-abs scaling"),
-        (X_standard_scaled, "Data after standard scaling"),
-        (X_l2_normalized, "Data after sample-wise L2 normalizing"),
-        (X_quantile_normalized, "Data after quantile normalizing"))):
+for i, (title, X) in enumerate(distributions.items()):
     offset = 10 * i
-
     # Distribution with all outliers
     axes = subplots[offset:offset + 2] + subplots[offset + 5:offset + 7]
-    plot_distribution(axes, X, y, hist_nbins=50,
+    plot_distribution(axes, X, y, hist_nbins=200,
                       plot_title=title + " including outliers\n",
                       X_label="Median Income", y_label="Number of households")
 
@@ -116,15 +125,19 @@ for i, (X, title) in enumerate((
     subplots[offset + 2].axis('off')
     subplots[offset + 7].axis('off')
 
+    zoom_in_percentile_range = (0, 99)
     # Distribution with extreme outliers removed
-    X0_min, X0_99th_pc = np.percentile(X[:, 0], [0, 99])
-    X1_min, X1_99th_pc = np.percentile(X[:, 1], [0, 99])
+    cutoffs_X0 = np.percentile(X[:, 0], zoom_in_percentile_range)
+    cutoffs_X1 = np.percentile(X[:, 1], zoom_in_percentile_range)
 
-    non_outliers = np.all(X < [X0_99th_pc, X1_99th_pc], axis=1)
+    non_outliers_mask = (
+        np.all(X > [cutoffs_X0[0], cutoffs_X1[0]], axis=1) &
+        np.all(X < [cutoffs_X0[1], cutoffs_X1[1]], axis=1))
     axes = subplots[offset + 3:offset + 5] + subplots[offset + 8:offset + 10]
-    plot_distribution(axes, X[non_outliers], y[non_outliers], hist_nbins=50,
+    plot_distribution(axes, X[non_outliers_mask], y[non_outliers_mask], hist_nbins=50,
                       plot_title=(title +
-                                  "\nZoomed-in at percentile range [0, 99)"),
+                                  "\nZoomed-in at percentile range %s"
+                                  % str(zoom_in_percentile_range)),
                       X_label="Median Income", y_label="Number of households")
 
 # Plot a heatmap legend for the y, combining a row of 4 cols
