d76945273a1031497cc8664a681dd74ddc2140e9
Tahar <zanouda@gmail.com>
2017-02-22 15:58:26 +0300

    revert changes

--- sklearn/linear_model/ransac.py
+++ sklearn/linear_model/ransac.py
@@ -68,8 +68,8 @@ class RANSACRegressor(BaseEstimator, MetaEstimatorMixin, RegressorMixin):
     base_estimator : object, optional
         Base estimator object which implements the following methods:
 
-         * fit(X, y) : Fit model to given training data and target values.
-         * score(X, y) : Returns the mean accuracy on the given test data,
+         * `fit(X, y)`: Fit model to given training data and target values.
+         * `score(X, y)`: Returns the mean accuracy on the given test data,
            which is used for the stop criterion defined by `stop_score`.
            Additionally, the score is used to decide which of two equally
            large consensus sets is chosen as the better one.
