93bc9900d31a834e875ffdc9cf552217d4b6b9bc
Isaac Laughlin <isaac.laughlin@gmail.com>
2017-02-23 02:22:26 -0800

    [MRG+1] Refactoring plot_iris svm example. (#8279)

--- examples/svm/plot_iris.py
+++ examples/svm/plot_iris.py
@@ -74,29 +74,8 @@ def plot_contours(ax, clf, xx, yy, **params):
     Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
     Z = Z.reshape(xx.shape)
     out = ax.contourf(xx, yy, Z, **params)
-    ax.set_xlim(xx.min(), xx.max())
-    ax.set_ylim(yy.min(), yy.max())
     return out
 
-
-def plot_points_boundary(ax, clf, x, y, xx, yy,
-                         points_params, contour_params):
-    """Plot the decision boundaries and points.
-
-    Parameters
-    ----------
-    ax: matplotlib axes object
-    clf: a classifier
-    x: x-axis of points to plot
-    y: y-axis of points to plot
-    xx: meshgrid ndarray
-    yy: meshgrid ndarray
-    point_params: dictionary of params to pass to `ax.scatter`, optional
-    countour_params: dictionary of params to pass to `plot_contours`, optional
-    """
-    contours = plot_contours(ax, clf, xx, yy, **contour_params)
-    points = ax.scatter(x, y, **points_params)
-    return [contours, points]
 
 # import some data to play with
 iris = datasets.load_iris()
@@ -123,12 +102,15 @@ titles = ('SVC with linear kernel',
 fig, sub = plt.subplots(2, 2)
 plt.subplots_adjust(wspace=0.4, hspace=0.4)
 
-xx, yy = make_meshgrid(X[:, 0], X[:, 1])
+X0, X1 = X[:, 0], X[:, 1]
+xx, yy = make_meshgrid(X0, X1)
 
 for clf, title, ax in zip(models, titles, sub.flatten()):
-    plot_points_boundary(ax, clf, X[:, 0], X[:, 1], xx, yy,
-                         dict(c=y, cmap=plt.cm.coolwarm),
-                         dict(cmap=plt.cm.coolwarm, alpha=0.8))
+    plot_contours(ax, clf, xx, yy,
+                  cmap=plt.cm.coolwarm, alpha=0.8)
+    ax.scatter(X0, X1, c=y, cmap=plt.cm.coolwarm, s=20, edgecolors='k')
+    ax.set_xlim(xx.min(), xx.max())
+    ax.set_ylim(yy.min(), yy.max())
     ax.set_xlabel('Sepal length')
     ax.set_ylabel('Sepal width')
     ax.set_xticks(())
