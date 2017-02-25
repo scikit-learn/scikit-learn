3a9d940711af651139bfab2c6ee0dba7ae28d2be
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2017-02-25 21:28:10 +0100

    reverse __init__ in tree module

--- sklearn/tree/__init__.py
+++ sklearn/tree/__init__.py
@@ -4,6 +4,10 @@ classification and regression.
 """
 
 from .tree import DecisionTreeClassifier
+from .tree import DecisionTreeRegressor
+from .tree import ExtraTreeClassifier
+from .tree import ExtraTreeRegressor
 from .export import export_graphviz
 
-__all__ = ["DecisionTreeClassifier", "export_graphviz"]
+__all__ = ["DecisionTreeClassifier", "DecisionTreeRegressor",
+           "ExtraTreeClassifier", "ExtraTreeRegressor", "export_graphviz"]
