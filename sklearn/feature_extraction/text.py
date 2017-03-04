919564899f8f456947fd9cbc67b90e93f7432d6e
ltcguthrie <leah.guthrie@phd.einstein.yu.edu>
2017-03-04 12:56:50 -0500

    Added an example of how to implement class TfidfVectorizer

--- sklearn/feature_extraction/text.py
+++ sklearn/feature_extraction/text.py
@@ -1239,6 +1239,23 @@ class TfidfVectorizer(CountVectorizer):
 
         This is only available if no vocabulary was given.
 
+    Examples
+    --------
+    >>> from sklearn.feature_extraction.text import TfidfVectorizer
+    >>> import numpy as np
+    >>> import pandas as pd
+    >>> #create document
+    >>> doc_test = ['Every summer our family gathers on the beach.',
+                    'Everytime our family has a gathering someone is late.',
+                    'Every winter our family goes to a ski resort.',
+                    'This fall everyone will run the Pumpkin 10K run.']
+    >>> vectorizer = TfidfVectorizer()
+    >>> X = vectorizer.fit_transform(doc_test)
+    >>> print(type(X))
+    >>> vectorizer.get_feature_names()
+
+
+
     See also
     --------
     CountVectorizer
@@ -1254,35 +1271,10 @@ class TfidfVectorizer(CountVectorizer):
     The ``stop_words_`` attribute can get large and increase the model size
     when pickling. This attribute is provided only for introspection and can
     be safely removed using delattr or set to None before pickling.
+    
+
+
     """
-
-// add code here //
-##Reference Issue
-##Example: Fixes Issue 3846 for class TfidfVectorizer -->
-
-
-from sklearn.feature_extraction.text import TfidfVectorizer
-import numpy as np
-import pandas as pd
-
-#create test document
-doc_test = ['Every summer our family gathers on the beach.',
-            'Everytime our family has a gathering someone is late.',
-            'Every winter our family goes to a ski resort in Colorado.',
-            'This fall everyone in the family will run the Pumpkin 10K run.']
-#call class TfidfVectorizer
-vectorizer = TfidfVectorizer()
-X = vectorizer.fit_transform(doc_test)
-print(type(X))
-vectorizer.get_feature_names()
-
-
-#### This code provides as an example of how to implement class TfidfVectorizer.
-
-//end of added code //
-
-
-
     def __init__(self, input='content', encoding='utf-8',
                  decode_error='strict', strip_accents=None, lowercase=True,
                  preprocessor=None, tokenizer=None, analyzer='word',
