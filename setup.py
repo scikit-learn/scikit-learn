8d3db7e0bfbfe7864f2aa3d3258e6a2a9dad378a
Shubham Bhardwaj <shubham.bhardwaj2015@vit.ac.in>
2017-03-06 13:29:39 +0530

    [MRG] removed download_url from setup.py (#8513)

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ MAINTAINER = 'Andreas Mueller'
 MAINTAINER_EMAIL = 'amueller@ais.uni-bonn.de'
 URL = 'http://scikit-learn.org'
 LICENSE = 'new BSD'
-DOWNLOAD_URL = 'http://sourceforge.net/projects/scikit-learn/files/'
 
 # We can actually import a restricted version of sklearn that
 # does not need the compiled code
@@ -185,7 +184,6 @@ def setup_package():
                     license=LICENSE,
                     url=URL,
                     version=VERSION,
-                    download_url=DOWNLOAD_URL,
                     long_description=LONG_DESCRIPTION,
                     classifiers=['Intended Audience :: Science/Research',
                                  'Intended Audience :: Developers',
