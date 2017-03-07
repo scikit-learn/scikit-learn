5e5318ff5d729270d71a7f78cafdc502c4d9ccf2
Loïc Estève <loic.esteve@ymail.com>
2017-03-07 11:20:08 +0100

    [MRG] Update joblib to 0.11 (#8492)
    Use pip rather than easy_install in copy_joblib.sh. Also need to remove joblib/testing.py to avoid pytest dependency.

--- sklearn/externals/copy_joblib.sh
+++ sklearn/externals/copy_joblib.sh
@@ -1,20 +1,12 @@
 #!/bin/sh
 # Script to do a local install of joblib
 export LC_ALL=C
-rm -rf tmp joblib
-PYTHON_VERSION=$(python -c 'import sys; print("{0[0]}.{0[1]}".format(sys.version_info))')
-SITE_PACKAGES="$PWD/tmp/lib/python$PYTHON_VERSION/site-packages"
+INSTALL_FOLDER=tmp/joblib_install
+rm -rf joblib $INSTALL_FOLDER
+pip install joblib --target $INSTALL_FOLDER
+cp -r $INSTALL_FOLDER/joblib .
+rm -rf $INSTALL_FOLDER
 
-mkdir -p $SITE_PACKAGES
-mkdir -p tmp/bin
-export PYTHONPATH="$SITE_PACKAGES"
-easy_install -Zeab tmp joblib
-
-cd tmp/joblib/
-python setup.py install --prefix $OLDPWD/tmp
-cd $OLDPWD
-cp -r $SITE_PACKAGES/joblib-*.egg/joblib .
-rm -rf tmp
 # Needed to rewrite the doctests
 # Note: BSD sed -i needs an argument unders OSX
 # so first renaming to .bak and then deleting backup files
@@ -25,4 +17,6 @@ find joblib -name "*.bak" | xargs rm
 # joblib is already tested on its own CI infrastructure upstream.
 rm -r joblib/test
 
-chmod -x joblib/*.py
+# Remove joblib/testing.py which is only used in tests and has a
+# pytest dependency (needed until we drop nose)
+rm joblib/testing.py
