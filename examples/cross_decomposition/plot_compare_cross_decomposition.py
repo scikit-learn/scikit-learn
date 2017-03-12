1b9bcb041458e34edc7c2c1e6838fe6a7e84f7c2
Janine Harper <ja9harper@users.noreply.github.com>
2017-03-12 11:13:40 -0400

    [MRG + 1] Correct typo in cross decomposition example (Fixes #8307) (#8578)
    * changed plsca to cca
    
    * corrected variable plsca in line 56-57

--- examples/cross_decomposition/plot_compare_cross_decomposition.py
+++ examples/cross_decomposition/plot_compare_cross_decomposition.py
@@ -144,5 +144,5 @@ print(np.round(pls1.coef_, 1))
 
 cca = CCA(n_components=2)
 cca.fit(X_train, Y_train)
-X_train_r, Y_train_r = plsca.transform(X_train, Y_train)
-X_test_r, Y_test_r = plsca.transform(X_test, Y_test)
+X_train_r, Y_train_r = cca.transform(X_train, Y_train)
+X_test_r, Y_test_r = cca.transform(X_test, Y_test)
