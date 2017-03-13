Corrected IsolationForest average path formula and added integer array equiv test

sklearn/ensemble/iforest.py
@@ -314,7 +314,7 @@ def _average_path_length(n_samples_leaf):
 
         average_path_length[mask] = 1.
         average_path_length[not_mask] = 2. * (
-            np.log(n_samples_leaf[not_mask]) + np.euler_gamma) - 2. * (
+            np.log(n_samples_leaf[not_mask] - 1.) + np.euler_gamma) - 2. * (
                 n_samples_leaf[not_mask] - 1.) / n_samples_leaf[not_mask]
 
         return average_path_length.reshape(n_samples_leaf_shape)

sklearn/ensemble/tests/test_iforest.py
@@ -217,10 +217,16 @@ def test_iforest_subsampled_features():
 
 def test_iforest_average_path_length():
     # It tests non-regression for #8549 which used the wrong formula
-    # for average path length
+    # for average path length, strictly for the integer case
 
     result_one = 2. * (np.log(4.) + np.euler_gamma) - 2. * (4. / 5.)
     result_two = 2. * (np.log(998.) + np.euler_gamma) - 2. * ( 998./999.)
     assert_almost_equal(_average_path_length(1), 1., decimal=10, )
     assert_almost_equal(_average_path_length(5), result_one, decimal=10)
     assert_almost_equal(_average_path_length(999), result_two, decimal=10)
+
+
+def test_average_path_length_arr_int():
+    # It tests non-regression for #8549 for integer array equivalence
+
+    assert_almost_equal(_average_path_length(5), _average_path_length(np.array([5])), decimal=10)