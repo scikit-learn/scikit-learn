06225a956130a21d7590c0a5924a5dcf9cdd06ed
Peter Wang <PeterWang@Peters-MacBook-Pro.local>
2017-03-12 22:34:19 -0400

    added constants to iforest average_path_length and the according non regression test

--- sklearn/ensemble/tests/test_iforest.py
+++ sklearn/ensemble/tests/test_iforest.py
@@ -219,6 +219,8 @@ def test_iforest_average_path_length():
     # It tests non-regression for #8549 which used the wrong formula
     # for average path length
 
-    assert_almost_equal(_average_path_length(1), 1., decimal=10)
-    assert_almost_equal(_average_path_length(5), 2.327020052, decimal=10)
-    assert_almost_equal(_average_path_length(999), 12.9679398844, decimal=10)
+    result_one = 2. * (np.log(4.) + np.euler_gamma) - 2. * (4. / 5.)
+    result_two = 2. * (np.log(998.) + np.euler_gamma) - 2. * ( 998./999.)
+    assert_almost_equal(_average_path_length(1), 1., decimal=10, )
+    assert_almost_equal(_average_path_length(5), result_one, decimal=10)
+    assert_almost_equal(_average_path_length(999), result_two, decimal=10)
