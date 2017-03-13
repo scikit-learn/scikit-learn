7e3a0c5d85c3b686f1975067dc9046f45f127d8c
Peter Wang <PeterWang@Peters-MacBook-Pro.local>
2017-03-12 23:49:46 -0400

    changed line to under 80 char

--- sklearn/ensemble/tests/test_iforest.py
+++ sklearn/ensemble/tests/test_iforest.py
@@ -229,4 +229,6 @@ def test_iforest_average_path_length():
 def test_average_path_length_arr_int():
     # It tests non-regression for #8549 for integer array equivalence
 
-    assert_almost_equal(_average_path_length(5), _average_path_length(np.array([5])), decimal=10)
+    assert_almost_equal(_average_path_length(5),
+                        _average_path_length(np.array([5])),
+                        decimal=10)
