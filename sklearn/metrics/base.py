fixed the normalization equation for ovo

sklearn/metrics/base.py
@@ -171,30 +171,30 @@ def _average_multiclass_ovo_score(binary_metric, y_true, y_score, average):
     score : float
         Average the sum of pairwise binary metric scores
     """
-    n_labels = len(np.unique(y_true))
-    pos_and_neg_prevalence = []
-    label_scores = []
-    for pos, neg in itertools.combinations(range(n_labels), 2):
-        pos_ix = y_true == pos
-        ix = np.logical_or(pos_ix, y_true == neg)
+    n_classes = len(np.unique(y_true))
+    n_pairs = n_classes * (n_classes - 1) // 2
+    prevalence = np.empty(n_pairs)
+    pair_scores = np.empty(n_pairs)
 
-        pos_and_neg_prevalence.append(float(np.sum(ix)) / len(y_true))
+    ix = 0
+    for a, b in itertools.combinations(range(n_classes), 2):
+        a_mask = y_true == a
+        ab_mask = np.logical_or(a_mask, y_true == b)
 
-        y_score_filtered = y_score[ix]
+        prevalence[ix] = np.sum(ab_mask) / len(y_true)
 
-        class_a = pos_ix[ix]
-        class_b = np.logical_not(class_a)
+        y_score_filtered = y_score[ab_mask]
 
-        score_class_a = binary_metric(
-                class_a, y_score_filtered[:, pos])
-        score_class_b = binary_metric(
-                class_b, y_score_filtered[:, neg])
-        binary_avg_score = (score_class_a + score_class_b) / 2.
-        label_scores.append(binary_avg_score)
+        a_true = a_mask[ab_mask]
+        b_true = np.logical_not(a_true)
 
-    if average == "weighted":
-        label_scores = np.multiply(np.array(pos_and_neg_prevalence),
-                                   np.array(label_scores))
-        return np.sum(label_scores) / (n_labels * (n_labels - 1))
-    else:
-        return 2 * np.sum(label_scores) / (n_labels * (n_labels - 1))
+        a_true_score = binary_metric(
+                a_true, y_score_filtered[:, a])
+        b_true_score = binary_metric(
+                b_true, y_score_filtered[:, b])
+        binary_avg_score = (a_true_score + b_true_score) / 2
+        pair_scores[ix] = binary_avg_score
+
+        ix += 1
+    return (np.average(pair_scores, weights=prevalence)
+            if average == "weighted" else np.average(pair_scores))

sklearn/metrics/tests/test_ranking.py
@@ -427,11 +427,9 @@ def test_multi_ovo_auc_toydata():
 
     # Weighted, one-vs-one multiclass ROC AUC algorithm
     # Each term is weighted by the posterior for the positive label.
-    weighted_sum_avg_scores = (0.75 * average_score_01 +
-                               0.75 * average_score_02 +
-                               0.50 * average_score_12)
-    ovo_weighted_coefficient = 1. / (n_labels * (n_labels - 1))
-    ovo_weighted_score = ovo_weighted_coefficient * weighted_sum_avg_scores
+    pair_scores = [average_score_01, average_score_02, average_score_12]
+    prevalence = [0.75, 0.75, 0.50]
+    ovo_weighted_score = np.average(pair_scores, weights=prevalence)
     assert_almost_equal(
         roc_auc_score(y_true, y_scores, multiclass="ovo", average="weighted"),
         ovo_weighted_score)
@@ -480,7 +478,7 @@ def test_multi_auc_score_under_permutation():
                 y_true_perm = np.take(perm, y_true)
                 score = roc_auc_score(y_true_perm, y_score_perm,
                                       multiclass=multiclass, average=average)
-                if not same_score_under_permutation:
+                if same_score_under_permutation is None:
                     same_score_under_permutation = score
                 else:
                     assert_almost_equal(score, same_score_under_permutation)
@@ -712,8 +710,6 @@ def test_score_scale_invariance():
     # issue #3864 (and others), where overly aggressive rounding was causing
     # problems for users with very small y_score values
     y_true, _, probas_pred = make_prediction(binary=True)
-    print(y_true.shape)
-    print(probas_pred.shape)
     roc_auc = roc_auc_score(y_true, probas_pred)
     roc_auc_scaled_up = roc_auc_score(y_true, 100 * probas_pred)
     roc_auc_scaled_down = roc_auc_score(y_true, 1e-6 * probas_pred)