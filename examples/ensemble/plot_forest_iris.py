e6411ca0211ccac994d389b30020018953d65446
qinhanmin2014 <qinhanmin2005@sina.com>
2017-03-27 09:35:41 +0800

    fix flake8 again

--- examples/ensemble/plot_forest_iris.py
+++ examples/ensemble/plot_forest_iris.py
@@ -139,7 +139,8 @@ for pair in ([0, 1], [0, 2], [2, 3]):
 
         # Plot the training points, these are clustered together and have a
         # black outline
-        plt.scatter(X[:, 0], X[:, 1], c=y, cmap=ListedColormap(['r', 'y', 'b']))
+        plt.scatter(X[:, 0], X[:, 1], c=y,
+                    cmap=ListedColormap(['r', 'y', 'b']))
         plot_idx += 1  # move on to the next plot in sequence
 
 plt.suptitle("Classifiers on feature subsets of the Iris dataset")
