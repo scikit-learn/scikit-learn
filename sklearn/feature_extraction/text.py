087f7d07ab0361f9f82c9a5a9387a37d23bdeaa0
Neyanbhbin <kanng.yongjie@gmail.com>
2017-04-04 16:31:45 -0500

    add "ngram_range" check in vectorizer initialization

--- sklearn/feature_extraction/text.py
+++ sklearn/feature_extraction/text.py
@@ -102,18 +102,6 @@ class VectorizerMixin(object):
 
     _white_spaces = re.compile(r"\s\s+")
 
-    @property
-    def ngram_range(self):
-        return self._ngram_range
-
-    @ngram_range.setter
-    def ngram_range(self, value):
-        if isinstance(value, tuple):
-            # raise 'ValueError' if min_n > max_n.
-            if value[0] > value[1]:
-                raise ValueError("min_n must not be greater than max_n.")
-        self._ngram_range = value
-
     def decode(self, doc):
         """Decode the input into a string of unicode symbols
 
@@ -447,6 +435,8 @@ class HashingVectorizer(BaseEstimator, VectorizerMixin):
         self.stop_words = stop_words
         self.n_features = n_features
         self.ngram_range = ngram_range
+        if ngram_range[0] > ngram_range[1]:
+            raise ValueError("min_n must not be greater than max_n.")
         self.binary = binary
         self.norm = norm
         self.non_negative = non_negative
@@ -695,6 +685,8 @@ class CountVectorizer(BaseEstimator, VectorizerMixin):
                     "max_features=%r, neither a positive integer nor None"
                     % max_features)
         self.ngram_range = ngram_range
+        if ngram_range[0] > ngram_range[1]:
+            raise ValueError("min_n must not be greater than max_n.")
         self.vocabulary = vocabulary
         self.binary = binary
         self.dtype = dtype
