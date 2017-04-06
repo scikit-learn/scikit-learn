1cdaf3ccb7ebd93d4877742aa8bde8878ed4bcf6
Vathsala Achar <vathsala.sachar@gmail.com>
2017-04-06 23:42:34 +0100

    [MRG+1] OneClassSVM predict now returns int (#8711)
    * Added predict method to class OneClassSVM
    
    - This method overrides the default behaviour to only return integer class values.
    
    * Test for the change in OneClassSVM predict method
    
    - small addition to test that the predicted result is integer
    
    * Changed test to check for intp datatype
    
    * Updated whats new

--- sklearn/svm/classes.py
+++ sklearn/svm/classes.py
@@ -1064,3 +1064,24 @@ class OneClassSVM(BaseLibSVM):
         """
         dec = self._decision_function(X)
         return dec
+
+    def predict(self, X):
+        """
+        Perform classification on samples in X.
+
+        For an one-class model, +1 or -1 is returned.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix}, shape (n_samples, n_features)
+            For kernel="precomputed", the expected shape of X is
+            [n_samples_test, n_samples_train]
+
+        Returns
+        -------
+        y_pred : array, shape (n_samples,)
+            Class labels for samples in X.
+        """
+        y = super(OneClassSVM, self).predict(X)
+        return np.asarray(y, dtype=np.intp)
+
