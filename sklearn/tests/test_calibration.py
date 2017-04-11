25699e3c320ccae4a33b80fe6f4da0f5cdc2921f
Emmanuel Contreras-Campana <emmanuelc82@gmail.com>
2017-04-11 16:38:39 -0400

    Updated test_calibration.py in order to test for the new sample_weight parameter of the calibration_curve method

--- sklearn/tests/test_calibration.py
+++ sklearn/tests/test_calibration.py
@@ -243,15 +243,19 @@ def test_calibration_curve():
     """Check calibration_curve function"""
     y_true = np.array([0, 0, 0, 1, 1, 1])
     y_pred = np.array([0., 0.1, 0.2, 0.8, 0.9, 1.])
+    sample_weight = np.array([0.7, 0.2, 0.4, 0.8, 1.1, 1.3])
     prob_true, prob_pred = calibration_curve(y_true, y_pred, n_bins=2)
     prob_true_unnormalized, prob_pred_unnormalized = \
-        calibration_curve(y_true, y_pred * 2, n_bins=2, normalize=True)
-    assert_equal(len(prob_true), len(prob_pred))
+        calibration_curve(y_true, y_pred * 2, normalize=True, n_bins=2)
+    prob_true_weighted, prob_pred_weighted = \
+        calibration_curve(y_true, y_pred, n_bins=2, sample_weight=sample_weight)
     assert_equal(len(prob_true), 2)
+    assert_equal(len(prob_true), len(prob_pred))
     assert_almost_equal(prob_true, [0, 1])
     assert_almost_equal(prob_pred, [0.1, 0.9])
     assert_almost_equal(prob_true, prob_true_unnormalized)
     assert_almost_equal(prob_pred, prob_pred_unnormalized)
+    assert_almost_equal(prob_true, prob_true_weighted)
 
     # probabilities outside [0, 1] should not be accepted when normalize
     # is set to False
