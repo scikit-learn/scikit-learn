3d538f9cd8e1ed2cf5457b5fc8dca7681a65d428
Toshihiro Kamishima <mail@kamishima.net>
2017-04-26 06:31:20 +0900

    remove needless argument checking

--- sklearn/model_selection/_split.py
+++ sklearn/model_selection/_split.py
@@ -902,11 +902,9 @@ class LeavePGroupsOut(BaseCrossValidator):
         ----------
         X : object
             Always ignored, exists for compatibility.
-            ``np.zeros(n_samples)`` may be used as a placeholder.
 
         y : object
             Always ignored, exists for compatibility.
-            ``np.zeros(n_samples)`` may be used as a placeholder.
 
         groups : array-like, with shape (n_samples,), optional
             Group labels for the samples used while splitting the dataset into
@@ -920,7 +918,6 @@ class LeavePGroupsOut(BaseCrossValidator):
         if groups is None:
             raise ValueError("The groups parameter should not be None")
         groups = check_array(groups, ensure_2d=False, dtype=None)
-        X, y, groups = indexable(X, y, groups)
         return int(comb(len(np.unique(groups)), self.n_groups, exact=True))
 
 
