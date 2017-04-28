7c837b1ac07bdc3baf7733eaf33a55f7c2099e21
Ed Rogers <f5d2h4eeyw@snkmail.com>
2017-04-27 20:11:29 -0500

    Small fix: change of wording.

--- sklearn/feature_extraction/text.py
+++ sklearn/feature_extraction/text.py
@@ -159,8 +159,8 @@ class VectorizerMixin(object):
         """Whitespace sensitive char-n-gram tokenization.
 
         Tokenize text_document into a sequence of character n-grams
-        operating only inside word boundaries (padded with space on each
-        side)"""
+        operating only inside word boundaries. n-grams at the edges
+        of words are passed with space."""
         # normalize white spaces
         text_document = self._white_spaces.sub(" ", text_document)
 
@@ -355,7 +355,7 @@ class HashingVectorizer(BaseEstimator, VectorizerMixin):
     analyzer : string, {'word', 'char', 'char_wb'} or callable
         Whether the feature should be made of word or character n-grams.
         Option 'char_wb' creates character n-grams only from text inside
-        word boundaries (padded with space on each side).
+        word boundaries; n-grams at the edges of words are passed with space.
 
         If a callable is passed it is used to extract the sequence of features
         out of the raw, unprocessed input.
@@ -554,7 +554,7 @@ class CountVectorizer(BaseEstimator, VectorizerMixin):
     analyzer : string, {'word', 'char', 'char_wb'} or callable
         Whether the feature should be made of word or character n-grams.
         Option 'char_wb' creates character n-grams only from text inside
-        word boundaries (padded with space on each side).
+        word boundaries; n-grams at the edges of words are passed with space.
 
         If a callable is passed it is used to extract the sequence of features
         out of the raw, unprocessed input.
