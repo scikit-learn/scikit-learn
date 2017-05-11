adc6d3b6ed34257e4c596694f40dc8c5281c17c9
Naoya Kanai <naopon@gmail.com>
2017-05-11 03:31:24 -0700

    wget Miniconda over https

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -37,7 +37,7 @@ if [[ "$DISTRIB" == "conda" ]]; then
     echo "Cached in $HOME/download :"
     ls -l
     echo
-    wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
         -O miniconda.sh
     chmod +x miniconda.sh && ./miniconda.sh -b -p /home/travis/miniconda
     cd ..
