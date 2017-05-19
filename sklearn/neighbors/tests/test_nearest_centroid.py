2627f7dbe9be23c14afd4dbbb85a381eafb97e0d
Sergul Aydore <sergulaydore@users.noreply.github.com>
2017-05-18 23:22:10 -0400

    [MRG+1] removed precomputed support in nearestcentroid (#8515)
    * removed test_precomputed
    
    * Revert "removed test_precomputed"
    
    This reverts commit 71ace3a39ce7d5ad72ecaa1ae2b762d6d7cb6c3e.
    
    * raise an error if metric is precomputed
    
    * raise error earlier

--- sklearn/neighbors/tests/test_nearest_centroid.py
+++ sklearn/neighbors/tests/test_nearest_centroid.py
@@ -6,10 +6,10 @@ import numpy as np
 from scipy import sparse as sp
 from numpy.testing import assert_array_equal
 from numpy.testing import assert_equal
+from numpy.testing import assert_raises
 
 from sklearn.neighbors import NearestCentroid
 from sklearn import datasets
-from sklearn.metrics.pairwise import pairwise_distances
 
 # toy sample
 X = [[-2, -1], [-1, -1], [-1, -2], [1, 1], [1, 2], [2, 1]]
@@ -56,11 +56,10 @@ def test_classification_toy():
 
 
 def test_precomputed():
-    clf = NearestCentroid(metric="precomputed")
-    clf.fit(X, y)
-    S = pairwise_distances(T, clf.centroids_)
-    assert_array_equal(clf.predict(S), true_result)
-
+    clf = NearestCentroid(metric='precomputed')
+    with assert_raises(ValueError) as context:
+        clf.fit(X, y)
+    assert_equal(ValueError, type(context.exception))
 
 def test_iris():
     # Check consistency on dataset iris.
