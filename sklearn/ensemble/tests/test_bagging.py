d92e38b42ed3f046410e8fb18552d932de921984
Michael Lewis <miclewis@n0002.scs00>
2017-05-25 18:46:57 -0700

    fixed labels to allow for strings

--- sklearn/ensemble/tests/test_bagging.py
+++ sklearn/ensemble/tests/test_bagging.py
@@ -730,35 +730,27 @@ def test_set_oob_score():
     # See: https://github.com/scikit-learn/scikit-learn/issues/8933
     N = 50
     randState = 5
-    label = 3
-    features = range(3)
     x = []
     y = []
-    dfs = []
     # Needs to maintain order of labels in case there are
     # cases of equal predictions when running argmax
     dict1 = {-1: 0, 0: 1, 1: 2}
     dict2 = {-1: 'a', 0: 'b', 1: 'c'}
 
-    labels = np.random.randint(3, size=N) - 1
-    df1 = np.tile(labels, (4, 1)).T
-    for col in features:
-        df1[:, col] = df1[:, col] + 2 * np.random.rand(N)
+    label1 = np.random.randint(3, size=N) - 1
+    label2 = [dict1[l] for l in label1]
+    label3 = [dict2[l] for l in label1]
 
-    dfs.append(df1)
-    df2 = df1.copy()
-    df2[:, label] = [dict1[l] for l in df2[:, label]]
-    dfs.append(df2)
-    df3 = df1.copy()
-    df3[:, label] = [dict2[l] for l in df3[:, label]]
-    dfs.append(df3)
+    df = np.tile(label1, (3, 1)).T
+    for col in range(3):
+        df[:, col] = df[:, col] + 2 * np.random.rand(N)
 
     # Try 3 possibilities
-    for df in dfs:
+    for label in [ label1, label2, label3]:
         clf = BaggingClassifier(base_estimator=KNeighborsClassifier(),
                                 n_estimators=10, oob_score=True,
                                 random_state=randState)
-        clf.fit(df[:, features], df[:, label])
+        clf.fit(df, label)
         x.append(clf.oob_score_)
 
     for i in range(len(x)):
