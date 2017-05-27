80153337e90b6f9fc73714f32d088050ac0fe09d
Rameshwar Bhaskaran <rameshwar.zorro@gmail.com>
2017-05-28 00:46:34 +0530

    Added test for graph

--- sklearn/utils/tests/test_graph.py
+++ sklearn/utils/tests/test_graph.py
@@ -22,3 +22,14 @@ def test_graph_laplacian():
             np.testing.assert_array_almost_equal(laplacian.T, laplacian)
             np.testing.assert_array_almost_equal(
                 laplacian, graph_laplacian(sp_mat, normed=normed).toarray())
+
+def test_graph_laplacian_with_copy():
+    for mat in (np.arange(10) * np.arange(10)[:, np.newaxis],
+                np.ones((7, 7)),
+                np.eye(19),
+                np.vander(np.arange(4)) + np.vander(np.arange(4)).T,):
+        sp_mat = sparse.csr_matrix(mat)
+        for normed in (True, False):
+            laplacian = graph_laplacian(mat, normed=normed)
+            laplacian_no_copy = graph_laplacian(mat, normed=normed, copy=False)
+            np.testing.assert_array_equal(laplacian, laplacian_no_copy)
