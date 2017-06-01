7bd579269a75c2f1dd92ac529bd8a08f7fd762fe
qinhanmin2014 <qinhanmin2005@sina.com>
2017-06-01 20:15:05 +0800

    add space

--- sklearn/datasets/base.py
+++ sklearn/datasets/base.py
@@ -588,12 +588,12 @@ def load_diabetes(return_X_y=False):
 def load_linnerud(return_X_y=False):
     """Load and return the linnerud dataset (multivariate regression).
 
-    ===============   ============================
+    ==============    ============================
     Samples total     20
-    Dimensionality    3(for both data and target)
+    Dimensionality    3 (for both data and target)
     Features          integer
     Targets           integer
-    ===============   ============================
+    ==============    ============================
 
     Parameters
     ----------
