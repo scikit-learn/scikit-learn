7a1c47dc47ccbb4b1b94d264b043050d50ab8fa9
aarshayj <aarshay.jain@gmail.com>
2017-06-03 21:59:15 -0400

    examples/cluster/plot_kmeans_assumptions.py - matplotlib2.0 update

--- examples/cluster/plot_kmeans_assumptions.py
+++ examples/cluster/plot_kmeans_assumptions.py
@@ -30,16 +30,16 @@ X, y = make_blobs(n_samples=n_samples, random_state=random_state)
 y_pred = KMeans(n_clusters=2, random_state=random_state).fit_predict(X)
 
 plt.subplot(221)
-plt.scatter(X[:, 0], X[:, 1], c=y_pred)
+plt.scatter(X[:, 0], X[:, 1], c=y_pred, s=20, edgecolor='k')
 plt.title("Incorrect Number of Blobs")
 
 # Anisotropicly distributed data
-transformation = [[ 0.60834549, -0.63667341], [-0.40887718, 0.85253229]]
+transformation = [[0.60834549, -0.63667341], [-0.40887718, 0.85253229]]
 X_aniso = np.dot(X, transformation)
 y_pred = KMeans(n_clusters=3, random_state=random_state).fit_predict(X_aniso)
 
 plt.subplot(222)
-plt.scatter(X_aniso[:, 0], X_aniso[:, 1], c=y_pred)
+plt.scatter(X_aniso[:, 0], X_aniso[:, 1], c=y_pred, s=20, edgecolor='k')
 plt.title("Anisotropicly Distributed Blobs")
 
 # Different variance
@@ -49,15 +49,16 @@ X_varied, y_varied = make_blobs(n_samples=n_samples,
 y_pred = KMeans(n_clusters=3, random_state=random_state).fit_predict(X_varied)
 
 plt.subplot(223)
-plt.scatter(X_varied[:, 0], X_varied[:, 1], c=y_pred)
+plt.scatter(X_varied[:, 0], X_varied[:, 1], c=y_pred, s=20, edgecolor='k')
 plt.title("Unequal Variance")
 
 # Unevenly sized blobs
 X_filtered = np.vstack((X[y == 0][:500], X[y == 1][:100], X[y == 2][:10]))
-y_pred = KMeans(n_clusters=3, random_state=random_state).fit_predict(X_filtered)
+y_pred = KMeans(n_clusters=3,
+                random_state=random_state).fit_predict(X_filtered)
 
 plt.subplot(224)
-plt.scatter(X_filtered[:, 0], X_filtered[:, 1], c=y_pred)
+plt.scatter(X_filtered[:, 0], X_filtered[:, 1], c=y_pred, s=20, edgecolor='k')
 plt.title("Unevenly Sized Blobs")
 
 plt.show()
