c18761a35096132933428671d366a229df2810cc
aarshayj <aarshay.jain@gmail.com>
2017-06-03 21:55:32 -0400

    examples/cluster/plot_ward_structured_vs_unstructured.py - matplotlib2.0 update

--- examples/cluster/plot_ward_structured_vs_unstructured.py
+++ examples/cluster/plot_ward_structured_vs_unstructured.py
@@ -57,8 +57,9 @@ fig = plt.figure()
 ax = p3.Axes3D(fig)
 ax.view_init(7, -80)
 for l in np.unique(label):
-    ax.plot3D(X[label == l, 0], X[label == l, 1], X[label == l, 2],
-              'o', color=plt.cm.jet(np.float(l) / np.max(label + 1)))
+    ax.scatter(X[label == l, 0], X[label == l, 1], X[label == l, 2],
+               color=plt.cm.jet(np.float(l) / np.max(label + 1)),
+               s=20, edgecolor='k')
 plt.title('Without connectivity constraints (time %.2fs)' % elapsed_time)
 
 
@@ -84,8 +85,9 @@ fig = plt.figure()
 ax = p3.Axes3D(fig)
 ax.view_init(7, -80)
 for l in np.unique(label):
-    ax.plot3D(X[label == l, 0], X[label == l, 1], X[label == l, 2],
-              'o', color=plt.cm.jet(float(l) / np.max(label + 1)))
+    ax.scatter(X[label == l, 0], X[label == l, 1], X[label == l, 2],
+               color=plt.cm.jet(float(l) / np.max(label + 1)),
+               s=20, edgecolor='k')
 plt.title('With connectivity constraints (time %.2fs)' % elapsed_time)
 
 plt.show()
