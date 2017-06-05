779074a60946dcb332d7be7c0549fa1f7253c212
Andreas Mueller <t3kcit@gmail.com>
2017-06-05 13:22:06 +0200

    fix test lol

--- sklearn/decomposition/tests/test_dict_learning.py
+++ sklearn/decomposition/tests/test_dict_learning.py
@@ -30,9 +30,10 @@ def test_sparse_encode_shapes_omp():
     rng = np.random.RandomState(0)
     algorithms = ['omp', 'lasso_lars', 'lasso_cd', 'lars', 'threshold']
     for n_components, n_samples in itertools.product([1, 5], [1, 9]):
+        X_ = rng.randn(n_samples, n_features)
         dictionary = rng.randn(n_components, n_features)
         for algorithm, n_jobs in itertools.product(algorithms, [1, 3]):
-            code = sparse_encode(X, dictionary, algorithm=algorithm,
+            code = sparse_encode(X_, dictionary, algorithm=algorithm,
                                  n_jobs=n_jobs)
             assert_equal(code.shape, (n_samples, n_components))
 
