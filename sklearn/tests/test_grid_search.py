8ef3cb6710f4853ea1f717c23b46b1c83f6cd91c
sentient07 <vxrram95@gmail.com>
2017-06-06 18:35:00 +0530

    Removed warning from BaseSearchCV
    Removed all warning tests
    
    Restored a test

--- sklearn/tests/test_grid_search.py
+++ sklearn/tests/test_grid_search.py
@@ -220,12 +220,11 @@ def test_grid_search_score_method():
     # Check warning only occurs in situation where behavior changed:
     # estimator requires score method to compete with scoring parameter
     score_no_scoring = assert_no_warnings(search_no_scoring.score, X, y)
-    score_accuracy = assert_warns(ChangedBehaviorWarning,
-                                  search_accuracy.score, X, y)
+    score_accuracy = assert_no_warnings(search_accuracy.score, X, y)
     score_no_score_auc = assert_no_warnings(search_no_score_method_auc.score,
                                             X, y)
-    score_auc = assert_warns(ChangedBehaviorWarning,
-                             search_auc.score, X, y)
+    score_auc = assert_no_warnings(search_auc.score, X, y)
+
     # ensure the test is sane
     assert_true(score_auc < 1.0)
     assert_true(score_accuracy < 1.0)
