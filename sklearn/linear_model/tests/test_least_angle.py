82092007cb1245009ecd1cd0f14da0d35cb259c6
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2017-06-06 14:41:08 +0200

    FIX : remove n_nonzero_coefs from attr of LassoLarsCV + clean up call to Lars._fit

--- sklearn/linear_model/tests/test_least_angle.py
+++ sklearn/linear_model/tests/test_least_angle.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.utils.testing import assert_equal
 from sklearn.utils.testing import assert_array_almost_equal
 from sklearn.utils.testing import assert_true
+from sklearn.utils.testing import assert_false
 from sklearn.utils.testing import assert_less
 from sklearn.utils.testing import assert_greater
 from sklearn.utils.testing import assert_raises
@@ -402,6 +403,7 @@ def test_lars_cv():
         lars_cv.fit(X, y)
         np.testing.assert_array_less(old_alpha, lars_cv.alpha_)
         old_alpha = lars_cv.alpha_
+    assert_false(hasattr(lars_cv, 'n_nonzero_coefs'))
 
 
 def test_lasso_lars_ic():
