0e5d9ed0ee03b632dde00060df72a9389d2bca04
Narine Kokhlikyan <narine@slice.com>
2017-06-07 12:29:22 +0200

    Addressed Joel's comments

--- examples/manifold/plot_t_sne_perplexity.py
+++ examples/manifold/plot_t_sne_perplexity.py
@@ -6,16 +6,15 @@
 An illustration of t-SNE on the two concentric circles and the S-curve
 datasets for different perplexity values.
 
-We observe the tendency of higher variations for small perplexity values and
-clearer shapes as the value increases.
+We observe a tendency towards clearer shapes as the preplexity value increases.
 
 The size, the distance and the shape of clusters may vary upon initialization,
 perplexity values and does not always convey a meaning.
 
 As shown below, t-SNE for higher perplexities finds meaningful topology of
 two concentric circles, however the size and the distance of the circles varies
-slightly from the original. In contrary to two circles dataset, the shapes
-visually diverge from S-curve topology on S-curve dateset even for larger
+slightly from the original. Contrary to the two circles dataset, the shapes
+visually diverge from S-curve topology on the S-curve dateset even for larger
 perplexity values.
 
 For further details, "How to Use t-SNE Effectively"
@@ -36,11 +35,11 @@ from sklearn import manifold, datasets
 from time import time
 
 # Next line to silence pyflakes. This import is needed.
-Axes3D
+from mpl_toolkits.mplot3d import Axes3D  # noqa
 
 n_samples = 500
 n_components = 2
-subplot_index = 251
+(fig, subplots) = plt.subplots(2, 5, figsize=(15, 8))
 perplexities = [5, 50, 100, 150]
 
 X, y = datasets.make_circles(n_samples=n_samples, factor=.5, noise=.05)
@@ -48,8 +47,7 @@ X, y = datasets.make_circles(n_samples=n_samples, factor=.5, noise=.05)
 red = y == 0
 green = y == 1
 
-fig = plt.figure(figsize=(15, 8))
-ax = fig.add_subplot(subplot_index)
+ax = subplots[0][0]
 ax.scatter(X[red, 0], X[red, 1], c="r")
 ax.scatter(X[green, 0], X[green, 1], c="g")
 ax.xaxis.set_major_formatter(NullFormatter())
@@ -57,8 +55,7 @@ ax.yaxis.set_major_formatter(NullFormatter())
 plt.axis('tight')
 
 for i, perplexity in enumerate(perplexities):
-    subplot_index += 1
-    ax = fig.add_subplot(subplot_index)
+    ax = subplots[0][i + 1]
 
     t0 = time()
     tsne = manifold.TSNE(n_components=n_components, init='random',
@@ -66,28 +63,23 @@ for i, perplexity in enumerate(perplexities):
     Y = tsne.fit_transform(X)
     t1 = time()
 
-    plt.title("%s %d (%.2g sec)" % ("Perplexity ", perplexity, t1 - t0))
+    ax.set_title("%s %d (%.2g sec)" % ("Perplexity=", perplexity, t1 - t0))
     ax.scatter(Y[red, 0], Y[red, 1], c="r")
     ax.scatter(Y[green, 0], Y[green, 1], c="g")
     ax.xaxis.set_major_formatter(NullFormatter())
     ax.yaxis.set_major_formatter(NullFormatter())
-    plt.axis('tight')
+    ax.axis('tight')
 
+# Another example using s-curve
 X, color = datasets.samples_generator.make_s_curve(n_samples, random_state=0)
 
-subplot_index += 1
-ax = fig.add_subplot(subplot_index, projection='3d')
-ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=color, cmap=plt.cm.Spectral)
+ax = subplots[1][0]
+ax.scatter(X[:, 0], X[:, 2], c=color, cmap=plt.cm.Spectral)
 ax.xaxis.set_major_formatter(NullFormatter())
 ax.yaxis.set_major_formatter(NullFormatter())
-ax.view_init(4, -72)
 
 for i, perplexity in enumerate(perplexities):
-    subplot_index += 1
-    if subplot_index < 260:
-        ax = fig.add_subplot(subplot_index)
-    else:
-        ax = fig.add_subplot(2, 5, 10)
+    ax = subplots[1][i + 1]
 
     t0 = time()
     tsne = manifold.TSNE(n_components=n_components, init='random',
@@ -95,10 +87,10 @@ for i, perplexity in enumerate(perplexities):
     Y = tsne.fit_transform(X)
     t1 = time()
 
-    plt.title("%s %d (%.2g sec)" % ("Perplexity ", perplexity, t1 - t0))
-    plt.scatter(Y[:, 0], Y[:, 1], c=color, cmap=plt.cm.Spectral)
+    ax.set_title("%s %d (%.2g sec)" % ("Perplexity=", perplexity, t1 - t0))
+    ax.scatter(Y[:, 0], Y[:, 1], c=color, cmap=plt.cm.Spectral)
     ax.xaxis.set_major_formatter(NullFormatter())
     ax.yaxis.set_major_formatter(NullFormatter())
-    plt.axis('tight')
+    ax.axis('tight')
 
 plt.show()
