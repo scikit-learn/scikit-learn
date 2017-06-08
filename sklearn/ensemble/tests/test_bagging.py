5a1aa24c73c56de533bfa4386ea391063411ba5a
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2017-06-08 11:39:16 +0200

    pep8 + update what's new

--- sklearn/ensemble/tests/test_bagging.py
+++ sklearn/ensemble/tests/test_bagging.py
@@ -728,15 +728,15 @@ def test_max_samples_consistency():
 def test_set_oob_score_label_encoding():
     # Make sure the oob_score doesn't change when the labels change
     # See: https://github.com/scikit-learn/scikit-learn/issues/8933
-    randState = 5
+    random_state = 5
     X = [[-1], [0], [1]] * 5
     Y1 = ['A', 'B', 'C'] * 5
     Y2 = [-1, 0, 1] * 5
     Y3 = [0, 1, 2] * 5
     x1 = BaggingClassifier(oob_score=True,
-                           random_state=randState).fit(X, Y1).oob_score_
+                           random_state=random_state).fit(X, Y1).oob_score_
     x2 = BaggingClassifier(oob_score=True,
-                           random_state=randState).fit(X, Y2).oob_score_
+                           random_state=random_state).fit(X, Y2).oob_score_
     x3 = BaggingClassifier(oob_score=True,
-                           random_state=randState).fit(X, Y3).oob_score_
+                           random_state=random_state).fit(X, Y3).oob_score_
     assert_equal([x1, x2], [x3, x3])
