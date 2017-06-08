c9935c5419253baf34264954753abd8268b351f9
aarshayj <aarshay.jain@gmail.com>
2017-06-07 22:34:47 -0400

    flake8 fix

--- examples/ensemble/plot_forest_iris.py
+++ examples/ensemble/plot_forest_iris.py
@@ -102,7 +102,7 @@ for pair in ([0, 1], [0, 2], [2, 3]):
         model_details = model_title
         if hasattr(model, "estimators_"):
             model_details += " with {} estimators".format(
-                                            len(model.estimators_))
+                len(model.estimators_))
         print(model_details + " with features", pair,
               "has a score of", scores)
 
