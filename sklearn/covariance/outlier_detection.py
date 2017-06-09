3116c1710f8bad07c911a172d1f5d23c0f7f5683
Nicolas Goix <goix.nicolas@gmail.com>
2017-06-09 17:34:30 +0200

    [MRG+2] clean outlier_detection.py (#9018)
    Remove OutlierDetectionMixin, which was only used by by EllipticEnvelope

--- sklearn/covariance/outlier_detection.py
+++ sklearn/covariance/outlier_detection.py
@@ -19,14 +19,53 @@ from ..utils.validation import check_is_fitted, check_array
 from ..metrics import accuracy_score
 
 
-class OutlierDetectionMixin(object):
-    """Set of methods for outliers detection with covariance estimators.
+class EllipticEnvelope(MinCovDet):
+    """An object for detecting outliers in a Gaussian distributed dataset.
+
+    Read more in the :ref:`User Guide <outlier_detection>`.
 
     Parameters
     ----------
-    contamination : float, 0. < contamination < 0.5
+    store_precision : boolean, optional (default=True)
+        Specify if the estimated precision is stored.
+
+    assume_centered : boolean, optional (default=False)
+        If True, the support of robust location and covariance estimates
+        is computed, and a covariance estimate is recomputed from it,
+        without centering the data.
+        Useful to work with data whose mean is significantly equal to
+        zero but is not exactly zero.
+        If False, the robust location and covariance are directly computed
+        with the FastMCD algorithm without additional treatment.
+
+    support_fraction : float in (0., 1.), optional (default=None)
+        The proportion of points to be included in the support of the raw
+        MCD estimate. If None, the minimum value of support_fraction will
+        be used within the algorithm: `[n_sample + n_features + 1] / 2`.
+
+    contamination : float in (0., 0.5), optional (default=0.1)
         The amount of contamination of the data set, i.e. the proportion
         of outliers in the data set.
+
+    Attributes
+    ----------
+    location_ : array-like, shape (n_features,)
+        Estimated robust location
+
+    covariance_ : array-like, shape (n_features, n_features)
+        Estimated robust covariance matrix
+
+    precision_ : array-like, shape (n_features, n_features)
+        Estimated pseudo inverse matrix.
+        (stored only if store_precision is True)
+
+    support_ : array-like, shape (n_samples,)
+        A mask of the observations that have been used to compute the
+        robust estimates of location and shape.
+
+    See Also
+    --------
+    EmpiricalCovariance, MinCovDet
 
     Notes
     -----
@@ -34,9 +73,27 @@ class OutlierDetectionMixin(object):
     perform well in high-dimensional settings. In particular, one will
     always take care to work with ``n_samples > n_features ** 2``.
 
+    References
+    ----------
+    ..  [1] Rousseeuw, P.J., Van Driessen, K. "A fast algorithm for the minimum
+        covariance determinant estimator" Technometrics 41(3), 212 (1999)
+
     """
-    def __init__(self, contamination=0.1):
+    def __init__(self, store_precision=True, assume_centered=False,
+                 support_fraction=None, contamination=0.1,
+                 random_state=None):
+        super(EllipticEnvelope, self).__init__(
+            store_precision=store_precision,
+            assume_centered=assume_centered,
+            support_fraction=support_fraction,
+            random_state=random_state)
         self.contamination = contamination
+
+    def fit(self, X, y=None):
+        super(EllipticEnvelope, self).fit(X)
+        self.threshold_ = sp.stats.scoreatpercentile(
+            self.dist_, 100. * (1. - self.contamination))
+        return self
 
     def decision_function(self, X, raw_values=False):
         """Compute the decision function of the given observations.
@@ -53,9 +110,9 @@ class OutlierDetectionMixin(object):
         Returns
         -------
         decision : array-like, shape (n_samples, )
-            The values of the decision function for each observations.
+            Decision function of the samples.
             It is equal to the Mahalanobis distances if `raw_values`
-            is True. By default (``raw_values=True``), it is equal
+            is True. By default (``raw_values=False``), it is equal
             to the cubic root of the shifted Mahalanobis distances.
             In that case, the threshold for being an outlier is 0, which
             ensures a compatibility with other outlier detection tools
@@ -83,7 +140,7 @@ class OutlierDetectionMixin(object):
         Returns
         -------
         is_outliers : array, shape = (n_samples, ), dtype = bool
-            For each observations, tells whether or not it should be considered
+            For each observation, tells whether or not it should be considered
             as an outlier according to the fitted model.
 
         threshold : float,
@@ -126,80 +183,3 @@ class OutlierDetectionMixin(object):
 
         """
         return accuracy_score(y, self.predict(X), sample_weight=sample_weight)
-
-
-class EllipticEnvelope(OutlierDetectionMixin, MinCovDet):
-    """An object for detecting outliers in a Gaussian distributed dataset.
-
-    Read more in the :ref:`User Guide <outlier_detection>`.
-
-    Parameters
-    ----------
-    store_precision : bool
-        Specify if the estimated precision is stored.
-
-    assume_centered : Boolean
-        If True, the support of robust location and covariance estimates
-        is computed, and a covariance estimate is recomputed from it,
-        without centering the data.
-        Useful to work with data whose mean is significantly equal to
-        zero but is not exactly zero.
-        If False, the robust location and covariance are directly computed
-        with the FastMCD algorithm without additional treatment.
-
-    support_fraction : float, 0 < support_fraction < 1
-        The proportion of points to be included in the support of the raw
-        MCD estimate. Default is ``None``, which implies that the minimum
-        value of support_fraction will be used within the algorithm:
-        `[n_sample + n_features + 1] / 2`.
-
-    contamination : float, 0. < contamination < 0.5
-        The amount of contamination of the data set, i.e. the proportion
-        of outliers in the data set.
-
-    Attributes
-    ----------
-    location_ : array-like, shape (n_features,)
-        Estimated robust location
-
-    covariance_ : array-like, shape (n_features, n_features)
-        Estimated robust covariance matrix
-
-    precision_ : array-like, shape (n_features, n_features)
-        Estimated pseudo inverse matrix.
-        (stored only if store_precision is True)
-
-    support_ : array-like, shape (n_samples,)
-        A mask of the observations that have been used to compute the
-        robust estimates of location and shape.
-
-    See Also
-    --------
-    EmpiricalCovariance, MinCovDet
-
-    Notes
-    -----
-    Outlier detection from covariance estimation may break or not
-    perform well in high-dimensional settings. In particular, one will
-    always take care to work with ``n_samples > n_features ** 2``.
-
-    References
-    ----------
-    ..  [1] Rousseeuw, P.J., Van Driessen, K. "A fast algorithm for the minimum
-        covariance determinant estimator" Technometrics 41(3), 212 (1999)
-
-    """
-    def __init__(self, store_precision=True, assume_centered=False,
-                 support_fraction=None, contamination=0.1,
-                 random_state=None):
-        MinCovDet.__init__(self, store_precision=store_precision,
-                           assume_centered=assume_centered,
-                           support_fraction=support_fraction,
-                           random_state=random_state)
-        OutlierDetectionMixin.__init__(self, contamination=contamination)
-
-    def fit(self, X, y=None):
-        MinCovDet.fit(self, X)
-        self.threshold_ = sp.stats.scoreatpercentile(
-            self.dist_, 100. * (1. - self.contamination))
-        return self
