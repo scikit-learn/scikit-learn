ab0342a5ef7ab07db538de962a565e275f0ecf5f
Loïc Estève <loic.esteve@ymail.com>
2017-06-09 15:59:43 +0200

    COSMIT add missing spaces in deprecated messages

--- sklearn/utils/graph.py
+++ sklearn/utils/graph.py
@@ -71,8 +71,8 @@ def single_source_shortest_path_length(graph, source, cutoff=None):
     return seen  # return all path lengths as dictionary
 
 
-@deprecated("sklearn.utils.graph.connected_components was deprecated in"
-            "version 0.19 and will be removed in 0.21. Use"
+@deprecated("sklearn.utils.graph.connected_components was deprecated in "
+            "version 0.19 and will be removed in 0.21. Use "
             "scipy.sparse.csgraph.connected_components instead.")
 def connected_components(*args, **kwargs):
     return sparse.csgraph.connected_components(*args, **kwargs)
