26bcd3c6bf0e56c14bb059889220f6dc0cc9892e
Attractadore <attractadore02@gmail.com>
2017-06-09 18:11:18 +0300

    Fixed sklearn-related invalid escape sequence DesprecationWarnings (#8951)

--- sklearn/__init__.py
+++ sklearn/__init__.py
@@ -91,7 +91,7 @@ def config_context(**new_config):
 
 # Make sure that DeprecationWarning within this package always gets printed
 warnings.filterwarnings('always', category=DeprecationWarning,
-                        module='^{0}\.'.format(re.escape(__name__)))
+                        module=r'^{0}\.'.format(re.escape(__name__)))
 
 # PEP0440 compatible formatted version, see:
 # https://www.python.org/dev/peps/pep-0440/
