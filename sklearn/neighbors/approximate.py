ce93d594e532df2805ebea593dd178d8ed3ee652
ldirer <laurent.direr@gmail.com>
2017-06-10 16:29:04 +0200

    [MRG] Deprecate lsh forest (#9078)
    * Add deprecation message and test.
    
    * Adding performance warning and ignore_warnings in test
    
    * Add deprecation to whatsnew and remove LSHForest references from docs.
    
    Removing benchmark for lsh

--- sklearn/neighbors/approximate.py
+++ sklearn/neighbors/approximate.py
@@ -217,6 +217,10 @@ class LSHForest(BaseEstimator, KNeighborsMixin, RadiusNeighborsMixin):
         self.min_hash_match = min_hash_match
         self.radius_cutoff_ratio = radius_cutoff_ratio
 
+        warnings.warn("LSHForest has poor performance and has been deprecated "
+                      "in 0.19. It will be removed in version 0.21.",
+                      DeprecationWarning)
+
     def _compute_distances(self, query, candidates):
         """Computes the cosine distance.
 
