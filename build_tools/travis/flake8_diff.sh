3cd70ebd2c1cceb8927a211f874f770dca6b8ace
Roman Yurchak <rth.yurchak@gmail.com>
2017-06-14 14:31:57 +0100

    [MRG] MNT Use the default flake8 --ignore options (#9123)

--- build_tools/travis/flake8_diff.sh
+++ build_tools/travis/flake8_diff.sh
@@ -137,8 +137,12 @@ check_files() {
 if [[ "$MODIFIED_FILES" == "no_match" ]]; then
     echo "No file outside sklearn/externals and doc/sphinxext/sphinx_gallery has been modified"
 else
-    check_files "$(echo "$MODIFIED_FILES" | grep -v ^examples)" --ignore=W503
+    # Default ignore PEP8 violations are from flake8 3.3.0
+    DEFAULT_IGNORED_PEP8=E121,E123,E126,E226,E24,E704,W503,W504
+    check_files "$(echo "$MODIFIED_FILES" | grep -v ^examples)" \
+           --ignore $DEFAULT_IGNORED_PEP8
     # Examples are allowed to not have imports at top of file
-    check_files "$(echo "$MODIFIED_FILES" | grep ^examples)" --ignore=E402,W503
+    check_files "$(echo "$MODIFIED_FILES" | grep ^examples)" \
+           --ignore $DEFAULT_IGNORED_PEP8 --ignore E402
 fi
 echo -e "No problem detected by flake8\n"
