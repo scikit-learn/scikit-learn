3805b8f3a89a7305c7070d51ddd76a8c7cb0d754
Joan Massich <mailsik@gmail.com>
2017-06-15 10:41:10 +0200

    TST Better tests in ridge float64 upcasting (#9125)
    Also invert the solvers check to highlight the fact that new solver should support both 32 bit and 64 bit float by default from now on.

--- sklearn/linear_model/tests/test_ridge.py
+++ sklearn/linear_model/tests/test_ridge.py
@@ -813,12 +813,12 @@ def test_dtype_match():
         ridge_64.fit(X_64, y_64)
         coef_64 = ridge_64.coef_
 
-        # Do all the checks at once, like this is easier to debug
-        assert_almost_equal(ridge_32.coef_, ridge_64.coef_, decimal=5)
-
         # Do the actual checks at once for easier debug
-        assert_equal(coef_32.dtype, X_32.dtype)
-        assert_equal(coef_64.dtype, X_64.dtype)
+        assert coef_32.dtype == X_32.dtype
+        assert coef_64.dtype == X_64.dtype
+        assert ridge_32.predict(X_32).dtype == X_32.dtype
+        assert ridge_64.predict(X_64).dtype == X_64.dtype
+        assert_almost_equal(ridge_32.coef_, ridge_64.coef_, decimal=5)
 
 
 def test_dtype_match_cholesky():
@@ -844,6 +844,8 @@ def test_dtype_match_cholesky():
     coef_64 = ridge_64.coef_
 
     # Do all the checks at once, like this is easier to debug
-    assert_equal(coef_32.dtype, X_32.dtype)
-    assert_equal(coef_64.dtype, X_64.dtype)
+    assert coef_32.dtype == X_32.dtype
+    assert coef_64.dtype == X_64.dtype
+    assert ridge_32.predict(X_32).dtype == X_32.dtype
+    assert ridge_64.predict(X_64).dtype == X_64.dtype
     assert_almost_equal(ridge_32.coef_, ridge_64.coef_, decimal=5)
