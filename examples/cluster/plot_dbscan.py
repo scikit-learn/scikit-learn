e3246e0356813a7990fdca36ccc3c6e5703e134e
Nate Guerin <gusennan@users.noreply.github.com>
2017-06-17 18:53:38 -0400

    Use tuples for color specification
    In the Matplotlib 'Specifying Colors' section, a list is not a valid color specification type.  When this example is run using Python 3.5, and matplotlib 2.0.2 (latest), it errors with the message: 
    
    "ValueError: The truth value of an array with more than one element is ambiguous. Use a.any() or a.all()"
    
    This change preserves the semantics of the example while allowing it to successfully run by converting the lists to tuples for the matplotlib color specification.

--- examples/cluster/plot_dbscan.py
+++ examples/cluster/plot_dbscan.py
@@ -57,16 +57,16 @@ colors = [plt.cm.Spectral(each)
 for k, col in zip(unique_labels, colors):
     if k == -1:
         # Black used for noise.
-        col = 'k'
+        col = [0, 0, 0, 1]
 
     class_member_mask = (labels == k)
 
     xy = X[class_member_mask & core_samples_mask]
-    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
+    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
              markeredgecolor='k', markersize=14)
 
     xy = X[class_member_mask & ~core_samples_mask]
-    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
+    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
              markeredgecolor='k', markersize=6)
 
 plt.title('Estimated number of clusters: %d' % n_clusters_)
