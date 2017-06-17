6f686b258bf198bb1258a9832c13a508e1354bf6
herilalaina <rkt.herilalaina@gmail.com>
2017-06-17 02:38:08 +0200

    Remove unused import

--- sklearn/neighbors/tests/test_neighbors.py
+++ sklearn/neighbors/tests/test_neighbors.py
@@ -1,25 +1,25 @@
 from itertools import product
+
 import numpy as np
 from scipy.sparse import (bsr_matrix, coo_matrix, csc_matrix, csr_matrix,
                           dok_matrix, lil_matrix)
 
 from sklearn import metrics
-from sklearn.model_selection import train_test_split
+from sklearn import neighbors, datasets
+from sklearn.exceptions import DataConversionWarning
+from sklearn.metrics.pairwise import pairwise_distances
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors.base import VALID_METRICS_SPARSE, VALID_METRICS
 from sklearn.utils.testing import assert_array_almost_equal
 from sklearn.utils.testing import assert_array_equal
-from sklearn.utils.testing import assert_raises
 from sklearn.utils.testing import assert_equal
+from sklearn.utils.testing import assert_greater
+from sklearn.utils.testing import assert_raises
 from sklearn.utils.testing import assert_true
 from sklearn.utils.testing import assert_warns
 from sklearn.utils.testing import ignore_warnings
-from sklearn.utils.testing import assert_greater
 from sklearn.utils.validation import check_random_state
-from sklearn.metrics.pairwise import (pairwise_distances,
-                                      PAIRWISE_DISTANCE_FUNCTIONS)
-from sklearn import neighbors, datasets
-from sklearn.exceptions import DataConversionWarning
-from sklearn.neighbors.base import VALID_METRICS_SPARSE, VALID_METRICS
 
 rng = np.random.RandomState(0)
 # load and shuffle iris dataset
