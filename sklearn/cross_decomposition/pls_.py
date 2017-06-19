4e00dff4e15835f649e2e5e7fa1cc9c10ffc9c0b
Andreas Mueller <t3kcit@gmail.com>
2017-06-19 16:50:30 -0400

    fix test_accuracy -> test_predictions rename in PLS

--- sklearn/cross_decomposition/pls_.py
+++ sklearn/cross_decomposition/pls_.py
@@ -456,7 +456,7 @@ class _PLS(six.with_metaclass(ABCMeta), BaseEstimator, TransformerMixin,
         return self.fit(X, y, **fit_params).transform(X, y)
 
     def _get_tags(self):
-        return _update_tags(super(_PLS, self), test_accuracy=False)
+        return _update_tags(super(_PLS, self), test_predictions=False)
 
 
 class PLSRegression(_PLS):
