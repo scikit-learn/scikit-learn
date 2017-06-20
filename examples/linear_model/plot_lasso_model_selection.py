7bbf4ae47c8610fa5e88d70f345c52eefd5cc42d
plagree <paul.lagree@gmail.com>
2017-06-20 10:07:45 +0200

    Remove last notebook style example

--- examples/linear_model/plot_lasso_model_selection.py
+++ examples/linear_model/plot_lasso_model_selection.py
@@ -64,9 +64,8 @@ X = np.c_[X, rng.randn(X.shape[0], 14)]  # add some bad features
 # normalize data as done by Lars to allow for comparison
 X /= np.sqrt(np.sum(X ** 2, axis=0))
 
-##############################################################################
+# #############################################################################
 # LassoLarsIC: least angle regression with BIC/AIC criterion
-# ----------------------------------------------------------
 
 model_bic = LassoLarsIC(criterion='bic')
 t1 = time.time()
@@ -97,9 +96,8 @@ plt.legend()
 plt.title('Information-criterion for model selection (training time %.3fs)'
           % t_bic)
 
-##############################################################################
+# #############################################################################
 # LassoCV: coordinate descent
-# ---------------------------
 
 # Compute paths
 print("Computing regularization path using the coordinate descent lasso...")
@@ -127,9 +125,8 @@ plt.title('Mean square error on each fold: coordinate descent '
 plt.axis('tight')
 plt.ylim(ymin, ymax)
 
-##############################################################################
+# #############################################################################
 # LassoLarsCV: least angle regression
-# -----------------------------------
 
 # Compute paths
 print("Computing regularization path using the Lars lasso...")
