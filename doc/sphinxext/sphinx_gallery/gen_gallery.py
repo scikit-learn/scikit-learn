7ed5410bdee3197ae0b078f36cb5a35d41e2d802
Andreas Mueller <t3kcit@gmail.com>
2017-06-20 15:27:33 -0400

    clearly not my day

--- doc/sphinxext/sphinx_gallery/gen_gallery.py
+++ doc/sphinxext/sphinx_gallery/gen_gallery.py
@@ -136,7 +136,7 @@ def setup(app):
     app.add_stylesheet('gallery.css')
 
     extensions_attr = '_extensions' if hasattr(app, '_extensions') else 'extensions'
-    if 'sphinx.ext.autodoc' in getattr(app, extensions_attr)::
+    if 'sphinx.ext.autodoc' in getattr(app, extensions_attr):
         app.connect('autodoc-process-docstring', touch_empty_backreferences)
 
     app.connect('builder-inited', generate_gallery_rst)
