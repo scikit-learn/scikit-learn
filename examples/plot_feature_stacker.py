4dd33c9bc809816480715aecc655bfbf16332396
Dror Atariah <d.atariah@rebuy.com>
2017-06-21 16:18:58 +0200

    Fixed typo in an example

--- examples/plot_feature_stacker.py
+++ examples/plot_feature_stacker.py
@@ -54,5 +54,5 @@ param_grid = dict(features__pca__n_components=[1, 2, 3],
                   svm__C=[0.1, 1, 10])
 
 grid_search = GridSearchCV(pipeline, param_grid=param_grid, verbose=10)
-grid_search.fit(X, y)
+grid_search.fit(X_features, y)
 print(grid_search.best_estimator_)
