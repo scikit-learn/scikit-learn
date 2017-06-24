287eb1e8385259bafea68c9c5fa3ab12f61d0d1b
harke <ashimb9@gmail.com>
2017-06-24 03:25:08 -0500

    Added k-Nearest Neighbor imputation of missing data

--- sklearn/preprocessing/imputation.py
+++ sklearn/preprocessing/imputation.py
@@ -59,6 +59,95 @@ def _most_frequent(array, extra_value, n_repeat):
         else:
             return extra_value
 
+def _get_knn(X, value_to_mask="NaN", n_neighbors=10, Y=None):
+    """Returns the k(=n_neighbors) nearest neighbors of vectors in a
+     given matrix in euclidean space. If two matrices are passed,
+     then k-Nearest Neighbors of vectors in X in the matrix Y is returned."""
+
+    # Setup missing mask
+    mask_X = _get_mask(X, value_to_mask)
+
+    if Y is None:
+        # Setup the anti-mask and change missing to zero
+        mask_X = _get_mask(X, value_to_mask)
+        XT = np.transpose(X)
+        N = (~mask_X) * 1
+        NT = np.transpose(N)
+        X[mask_X] = 0
+
+        # Matrix formula to calculate pair-wise distance between all vectors in a
+        # matrix with missing values. It zero-weights coordinates with missing value
+        # in either vector in the pair and up-weights the remaining coordinates.
+        # Matrix formula derived by: Shreya Bhattarai <shreya.bhattarai@gmail.com>
+
+        """
+        Store np.dot(N, (XT * XT)) and add its transpose rather than 
+        redoing a matrix product
+        dist = np.sqrt((X.shape[1] * 1 / ((np.dot(N, NT)))) * (
+            np.dot(N, (XT * XT)) - 2 * (np.dot(X, XT)) +
+            np.dot((X * X), NT)))        
+
+        N_dot_XT2 = np.dot(N, (XT * XT))
+        N_dot_XT2_T = np.transpose(N_dot_XT2)
+        """
+
+        N_dot_XT2 = np.dot(N, (XT * XT))
+        N_dot_XT2_T = np.transpose(N_dot_XT2)
+
+        dist = np.sqrt((X.shape[1] * 1 / ((np.dot(N, NT)))) * (
+            N_dot_XT2 - 2 * (np.dot(X, XT)) +
+            N_dot_XT2_T))
+
+        # Set distance with self to np.inf
+        np.fill_diagonal(dist, np.inf)
+
+    else:
+        # ValueError if X and Y have incompatible dimensions
+        if X.shape[1] != Y.shape[1]:
+            raise ValueError("The search dimension of the matrices "
+                             "are not equal: [{0}] versus [{1}]".
+                             format(X.shape[1], Y.shape[1]))
+
+        mask_Y = _get_mask(Y, value_to_mask)
+        NY = (~mask_Y) * 1
+        YT = np.transpose(Y)
+        mask_YT = _get_mask(YT, value_to_mask)
+        NYT = np.transpose(NY)
+        YT[mask_YT] = 0
+
+        NX = (~mask_X) * 1
+        X[mask_X] = 0
+
+        # Matrix formula to calculate pair-wise distance between all vectors in a
+        # matrix X to vectors in matrix Y. It handles missing values the same way
+        # as for a single matrix.
+        # Matrix formula derived by: Shreya Bhattarai <shreya.bhattarai@gmail.com>
+
+        dist = np.sqrt((X.shape[1] * 1 / ((np.dot(NX, NYT)))) *
+                       (np.dot((X * X), NYT) - 2 * (np.dot(X, YT)) + np.dot(NX, (YT * YT))))
+
+    # Ensure enough candidate neighbors are available
+    n_candidates = dist.shape[1] if Y is not None else dist.shape[1] - 1
+    if n_candidates < n_neighbors:
+        raise ValueError("There are only %d candidate neighbors, "
+                         "but n_neighbors=%d."
+                         % (dist.shape[1] - 1, n_neighbors))
+
+    # Missing locations and counts
+    row_missing_sum_X = mask_X.sum(axis=1)
+    # is_row_missing_X = np.any(mask_X, axis=1)
+    # is_col_missing_X = np.any(mask_X, axis=0)
+    col_missing_index_X = np.where(mask_X)[1]
+
+    # Arg-partition (quasi-argsort) of n_neighbors and retrieve them
+    nbors_index = np.argpartition(dist, n_neighbors - 1, axis=1)
+    knn_row_index = nbors_index[:, :n_neighbors]
+    knn_row_index = np.vsplit(knn_row_index, knn_row_index.shape[0])
+    knn_row_index = np.repeat(knn_row_index, row_missing_sum_X, axis=0)
+    knn_row_index = knn_row_index.ravel()
+    # This assumes columns in X and Y are in the same order; maybe change this?
+    knn_col_index = np.repeat(col_missing_index_X, n_neighbors)
+    return knn_row_index, knn_col_index
 
 class Imputer(BaseEstimator, TransformerMixin):
     """Imputation transformer for completing missing values.
@@ -115,12 +204,16 @@ class Imputer(BaseEstimator, TransformerMixin):
       contain missing values).
     """
     def __init__(self, missing_values="NaN", strategy="mean",
-                 axis=0, verbose=0, copy=True):
+                 axis=0, verbose=0, copy=True, n_neighbors=10,
+                 row_max_missing=0.5, col_max_missing=0.8):
         self.missing_values = missing_values
         self.strategy = strategy
         self.axis = axis
         self.verbose = verbose
         self.copy = copy
+        self.n_neighbors = n_neighbors
+        self.row_max_missing = row_max_missing
+        self.col_max_missing = col_max_missing
 
     def fit(self, X, y=None):
         """Fit the imputer on X.
@@ -137,7 +230,7 @@ class Imputer(BaseEstimator, TransformerMixin):
             Returns self.
         """
         # Check parameters
-        allowed_strategies = ["mean", "median", "most_frequent"]
+        allowed_strategies = ["mean", "median", "most_frequent", "knn"]
         if self.strategy not in allowed_strategies:
             raise ValueError("Can only use these strategies: {0} "
                              " got strategy={1}".format(allowed_strategies,
@@ -247,7 +340,12 @@ class Imputer(BaseEstimator, TransformerMixin):
 
                 return most_frequent
 
-    def _dense_fit(self, X, strategy, missing_values, axis):
+            # KNN
+            elif strategy == "knn":
+                raise ValueError("strategy='knn' does not support sparse "
+                                 "matrix input yet.")
+
+    def _dense_fit(self, X, strategy, missing_values, axis, Y=None):
         """Fit the transformer on dense data."""
         X = check_array(X, force_all_finite=False)
         mask = _get_mask(X, missing_values)
@@ -298,6 +396,79 @@ class Imputer(BaseEstimator, TransformerMixin):
 
             return most_frequent
 
+        # KNN
+        elif strategy == "knn":
+
+            if axis == 1:
+                X = X.transpose()
+                mask = mask.transpose()
+
+            #Get dimensions and missing count
+            n_rows, n_cols = X.shape
+            row_missing_sum = mask.sum(axis=1)
+
+            #ValueError if % missing in any column > self.col_max_missing
+            if np.any(mask.sum(axis=0) > (X.shape[0] * self.col_max_missing)):
+                raise ValueError("The following axis position(s) have, "
+                                 "more than {0}% missing values: {1}"
+                                 .format(self.col_max_missing*100,np.where(mask.sum(axis=0) >
+                                                  (X.shape[0] * self.col_max_missing))))
+
+            if X.shape[0] < self.n_neighbors:
+                raise ValueError("There are only %d samples, "
+                                 "but n_neighbors=%d."
+                                 % (X.shape[0], self.n_neighbors))
+
+            #Fit to data
+
+            # Check for excessive missingness in rows
+            bad_rows = row_missing_sum > (mask.shape[1] * self.row_max_missing)
+            Xbad = X[bad_rows, :]
+
+            if np.any(bad_rows):
+                X = X[~bad_rows, :]
+                mask = _get_mask(X, missing_values)
+
+            #Get the k nearest neighbors
+            if Y is None:
+                knnrows_index, knncols_index = _get_knn(X,
+                                                        n_neighbors=self.n_neighbors)
+
+            else:
+                knnrows_index, knncols_index = _get_knn(X,
+                                                        n_neighbors=self.n_neighbors, Y=Y)
+            X[mask] = np.nan
+            if Y is None:
+                imputed = np.nanmean((X[(knnrows_index, knncols_index)]).
+                                     reshape((-1, self.n_neighbors)), axis=1)
+            else:
+                imputed = np.nanmean((Y[(knnrows_index, knncols_index)]).
+                                     reshape((-1, self.n_neighbors)), axis=1)
+            X[mask] = imputed
+
+            #Merge bad rows to X and mean impute any leftover missing
+            if np.any(bad_rows):
+                X_merged = np.empty((n_rows, n_cols))
+                X_merged[bad_rows, :] = Xbad
+                X_merged[~bad_rows, :] = X
+                X = X_merged
+
+            #Impute bad_rows and leftover missing with column means
+            mask_after_knn = _get_mask(X, self.missing_values)
+            if np.any(mask_after_knn):
+                missing_index = np.where(mask_after_knn)
+                X_col_means = masked_X.mean(axis=0).data
+                X[missing_index] = np.take(X_col_means, missing_index[1])
+
+            # Transpose back
+            if axis == 1:
+                X = X.transpose()
+
+            #The mask is used to compare this imputed matrix with
+            #input matrix in transform(), so return X as a masked array.
+            X = np.ma.array(X,mask=masked_X.mask)
+            return X
+
     def transform(self, X):
         """Impute all missing values in X.
 
@@ -311,7 +482,8 @@ class Imputer(BaseEstimator, TransformerMixin):
             X = check_array(X, accept_sparse='csc', dtype=FLOAT_DTYPES,
                             force_all_finite=False, copy=self.copy)
             statistics = self.statistics_
-            if X.shape[1] != statistics.shape[0]:
+            #> Added knn exception below
+            if self.strategy != "knn" and X.shape[1] != statistics.shape[0]:
                 raise ValueError("X has %d features per sample, expected %d"
                                  % (X.shape[1], self.statistics_.shape[0]))
 
@@ -334,21 +506,22 @@ class Imputer(BaseEstimator, TransformerMixin):
                                              self.missing_values,
                                              self.axis)
 
-        # Delete the invalid rows/columns
-        invalid_mask = np.isnan(statistics)
-        valid_mask = np.logical_not(invalid_mask)
-        valid_statistics = statistics[valid_mask]
-        valid_statistics_indexes = np.where(valid_mask)[0]
-        missing = np.arange(X.shape[not self.axis])[invalid_mask]
+        if self.strategy != "knn":
+            # Delete the invalid rows/columns
+            invalid_mask = np.isnan(statistics)
+            valid_mask = np.logical_not(invalid_mask)
+            valid_statistics = statistics[valid_mask]
+            valid_statistics_indexes = np.where(valid_mask)[0]
+            missing = np.arange(X.shape[not self.axis])[invalid_mask]
 
-        if self.axis == 0 and invalid_mask.any():
-            if self.verbose:
-                warnings.warn("Deleting features without "
-                              "observed values: %s" % missing)
-            X = X[:, valid_statistics_indexes]
-        elif self.axis == 1 and invalid_mask.any():
-            raise ValueError("Some rows only contain "
-                             "missing values: %s" % missing)
+            if self.axis == 0 and invalid_mask.any():
+                if self.verbose:
+                    warnings.warn("Deleting features without "
+                                  "observed values: %s" % missing)
+                X = X[:, valid_statistics_indexes]
+            elif self.axis == 1 and invalid_mask.any():
+                raise ValueError("Some rows only contain "
+                                 "missing values: %s" % missing)
 
         # Do actual imputation
         if sparse.issparse(X) and self.missing_values != 0:
@@ -364,13 +537,38 @@ class Imputer(BaseEstimator, TransformerMixin):
 
             mask = _get_mask(X, self.missing_values)
             n_missing = np.sum(mask, axis=self.axis)
-            values = np.repeat(valid_statistics, n_missing)
 
-            if self.axis == 0:
-                coordinates = np.where(mask.transpose())[::-1]
+            if self.strategy == 'knn':
+                if self.axis == 1:
+                    X = X.transpose()
+                    mask = mask.transpose()
+                    statistics = statistics.transpose()
+
+                #Check if unmasked values of X and statistics are equal
+                mask_fitted = statistics.mask
+                if mask_fitted.shape == X.shape:
+                    masked_X = np.ma.array(X, mask=mask_fitted)
+                    if np.ma.allequal(masked_X,statistics):
+                        X = statistics.data
+                else:
+                    X_imputed_masked = self._dense_fit(X,
+                                                       self.strategy,
+                                                       self.missing_values,
+                                                       self.axis,
+                                                       statistics.data)
+                    X = X_imputed_masked.data
+
+                if self.axis == 1:
+                    X = X.transpose()
+
             else:
-                coordinates = mask
+                values = np.repeat(valid_statistics, n_missing)
 
-            X[coordinates] = values
+                if self.axis == 0:
+                    coordinates = np.where(mask.transpose())[::-1]
+                else:
+                    coordinates = mask
+
+                X[coordinates] = values
 
         return X
