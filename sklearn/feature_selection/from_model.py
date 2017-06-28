3b384709773bcc349f87157c44ab14064a747aa6
James Bourbeau <jrbourbeau@gmail.com>
2017-06-27 22:38:03 -0500

    Updates docstring

--- sklearn/feature_selection/from_model.py
+++ sklearn/feature_selection/from_model.py
@@ -88,7 +88,7 @@ class SelectFromModel(BaseEstimator, SelectorMixin, MetaEstimatorMixin):
         The base estimator from which the transformer is built.
         This can be both a fitted (if ``prefit`` is set to True)
         or a non-fitted estimator. The estimator must have either a
-        ``feature_importances_`` or ``coef_`` attribute.
+        ``feature_importances_`` or ``coef_`` attribute after fitting.
 
     threshold : string, float, optional default None
         The threshold value to use for feature selection. Features whose
