3bda05ae8fceb5308de6a288ab7e22bde1f04ece
aarshayj <aarshay.jain@gmail.com>
2017-07-04 16:36:36 -0400

    modified travis files

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -42,12 +42,14 @@ if [[ "$DISTRIB" == "conda" ]]; then
     if [[ "$INSTALL_MKL" == "true" ]]; then
         conda create -n testenv --yes python=$PYTHON_VERSION pip nose pytest \
             numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION \
+            matplotlib=$MATPLOTLIB_VERSION \
             mkl cython=$CYTHON_VERSION \
             ${PANDAS_VERSION+pandas=$PANDAS_VERSION}
             
     else
         conda create -n testenv --yes python=$PYTHON_VERSION pip nose pytest \
             numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION \
+            matplotlib=$MATPLOTLIB_VERSION \
             nomkl cython=$CYTHON_VERSION \
             ${PANDAS_VERSION+pandas=$PANDAS_VERSION}
     fi
