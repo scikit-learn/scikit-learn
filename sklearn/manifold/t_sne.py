0509d1215b0437608924951063b7b85ede635d5d
Olivier Grisel <olivier.grisel@ensta.org>
2017-07-07 15:35:11 +0200

    ENH simplify example usage in TSNE docstring

--- sklearn/manifold/t_sne.py
+++ sklearn/manifold/t_sne.py
@@ -588,9 +588,7 @@ class TSNE(BaseEstimator):
     >>> import numpy as np
     >>> from sklearn.manifold import TSNE
     >>> X = np.array([[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 1]])
-    >>> model = TSNE(n_components=2, random_state=0)
-    >>> np.set_printoptions(suppress=True)
-    >>> X_embedded = model.fit_transform(X)
+    >>> X_embedded = TSNE(n_components=2).fit_transform(X)
     >>> X_embedded.shape
     (4, 2)
 
