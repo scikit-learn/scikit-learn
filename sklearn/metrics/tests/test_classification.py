c0b00e52628b4bb8421265b28433c366e4dbd47b
Joel Nothman <joel.nothman@gmail.com>
2017-07-11 01:31:10 +1000

    More informative error message for classification metrics given regression output (#9275)

--- sklearn/metrics/tests/test_classification.py
+++ sklearn/metrics/tests/test_classification.py
@@ -1360,7 +1360,8 @@ def test__check_targets():
             if type1 != type2:
                 assert_raise_message(
                     ValueError,
-                    "Can't handle mix of {0} and {1}".format(type1, type2),
+                    "Classification metrics can't handle a mix of {0} and {1} "
+                    "targets".format(type1, type2),
                     _check_targets, y1, y2)
 
             else:
