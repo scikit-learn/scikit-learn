69ad9393297662f2f9278de2a27241aeff795ada
Sharan Yalburgi <sharanyalburgi@gmail.com>
2017-07-10 12:54:33 +0530

    Test for deprecation

--- sklearn/cluster/tests/test_hierarchical.py
+++ sklearn/cluster/tests/test_hierarchical.py
@@ -35,6 +35,20 @@ from sklearn.utils.fast_dict import IntFloatDict
 from sklearn.utils.testing import assert_array_equal
 from sklearn.utils.testing import assert_warns
 
+def test_deprecation_of_linkage_tree():
+
+    for tree_builder in _TREE_BUILDERS.values():
+        for this_X in (X, X[0]):
+            with ignore_warnings():
+                    # Test for warning of deprecation of n_components in linkage_tree
+                    children, n_nodes, n_leaves, parent = assert_warns(DeprecationWarning, linkage_tree,this_X.T, n_components=10)
+                    children1, n_nodes1, n_leaves1, parent1 = linkage_tree(this_X.T, n_components=10)
+                    children2, n_nodes2, n_leaves2, parent2 = linkage_tree(this_X.T)
+                    # Test is to see if n_components is making any difference
+                    assert_array_equal(children1,children2)
+                    assert_equal(n_nodes1,n_nodes2)
+                    assert_equal(n_leaves1,n_leaves2)
+                    assert_equal(parent1,parent2)
 
 def test_linkage_misc():
     # Misc tests on linkage
@@ -44,8 +58,6 @@ def test_linkage_misc():
     assert_raises(ValueError, linkage_tree, X, linkage='foo')
     assert_raises(ValueError, linkage_tree, X, connectivity=np.ones((4, 4)))
 
-    # Test for warning of deprecation of n_components in linkage_tree
-    children, n_nodes, n_leaves, parent = assert_warns(UserWarning, linkage_tree, n_components=10)
 
     # Smoke test FeatureAgglomeration
     FeatureAgglomeration().fit(X)
