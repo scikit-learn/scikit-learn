c751c6362cb8635113bba2e7c3ed0bff9b765cd9
Sharan Yalburgi <sharanyalburgi@gmail.com>
2017-07-10 17:28:24 +0530

    modifying test

--- sklearn/cluster/tests/test_hierarchical.py
+++ sklearn/cluster/tests/test_hierarchical.py
@@ -39,14 +39,12 @@ from sklearn.utils.testing import assert_warns
 def test_deprecation_of_n_components_in_linkage_tree():
     rng = np.random.RandomState(0)
     X = rng.randn(50, 100)
-    for tree_builder in _TREE_BUILDERS.values():
-        for this_X in (X, X[0]):
-            # Test for warning of deprecation of n_components in linkage_tree
-            children, n_nodes, n_leaves, parent = assert_warns(
-                                                DeprecationWarning,
-                                                linkage_tree,
-                                                this_X.T,
-                                                n_components=10)
+    # Test for warning of deprecation of n_components in linkage_tree
+    children, n_nodes, n_leaves, parent = assert_warns(
+                                        DeprecationWarning,
+                                        linkage_tree,
+                                        X.T,
+                                        n_components=10)
 
 def test_linkage_misc():
     # Misc tests on linkage
