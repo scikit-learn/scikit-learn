93cc6d6fbe9767a67cb1f97fc0ffc7fd59a464ca
Raghav RV <rvraghav93@gmail.com>
2017-07-10 12:08:03 -0500

    Add plot for fit times too

--- examples/ensemble/plot_gradient_boosting_early_stopping.py
+++ examples/ensemble/plot_gradient_boosting_early_stopping.py
@@ -34,6 +34,8 @@ using much less estimators. This can save memory and prediction time.
 #          Raghav RV <rvraghav93@gmail.com>
 # License: BSD 3 clause
 
+import time
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -50,8 +52,10 @@ names = ['Iris Data', 'Digits Data', 'Hastie Data']
 
 n_gb = []
 score_gb = []
+time_gb = []
 n_gbes = []
 score_gbes = []
+time_gbes = []
 
 n_estimators = 500
 
@@ -67,9 +71,14 @@ for X, y in data_list:
                                                random_state=0)
     gb = ensemble.GradientBoostingClassifier(n_estimators=n_estimators,
                                              random_state=0)
-
+    
+    start = time.time()
     gb.fit(X_train, y_train)
+    time_gb.append(time.time() - start)
+
+    start = time.time()
     gbes.fit(X_train, y_train)
+    time_gbes.append(time.time() - start)
 
     score_gb.append(gb.score(X_test, y_test))
     score_gbes.append(gbes.score(X_test, y_test))
@@ -77,11 +86,16 @@ for X, y in data_list:
     n_gb.append(gb.n_estimators_)
     n_gbes.append(gbes.n_estimators_)
 
-plt.figure(figsize=(9, 5))
 bar_width = 0.2
 n = len(data_list)
 index = np.arange(0, n*bar_width, bar_width)*2.5
 index = index[0:n]
+
+#######################################################################
+# Compare scores with and without early stopping
+# ----------------------------------------------
+
+plt.figure(figsize=(9, 5))
 
 bar1 = plt.bar(index, score_gb, bar_width, label='Without early stopping',
                color='crimson')
@@ -102,6 +116,7 @@ def autolabel(rects, n_estimators):
         plt.text(rect.get_x() + rect.get_width()/2., 1.05*rect.get_height(),
                  'n_est=%d' % n_estimators[i], ha='center', va='bottom')
 
+
 autolabel(bar1, n_gb)
 autolabel(bar2, n_gbes)
 
@@ -111,5 +126,34 @@ plt.grid(True)
 
 plt.xlabel('Datasets')
 plt.ylabel('Score')
+
+plt.show()
+
+
+#######################################################################
+# Compare fit times with and without early stopping
+# ----------------------------------------------
+
+plt.figure(figsize=(9, 5))
+
+bar1 = plt.bar(index, time_gb, bar_width, label='Without early stopping',
+               color='crimson')
+bar2 = plt.bar(index + bar_width, time_gbes, bar_width,
+               label='With early stopping', color='coral')
+
+max_y = np.amax(np.maximum(time_gb, time_gbes))
+
+plt.xticks(index + bar_width, names)
+plt.yticks(np.linspace(0, 1.3 * max_y, 13))
+
+autolabel(bar1, n_gb)
+autolabel(bar2, n_gbes)
+
+plt.ylim([0, 1.3 * max_y])
+plt.legend(loc='best')
+plt.grid(True)
+
+plt.xlabel('Datasets')
+plt.ylabel('Fit Time')
 
 plt.show()
