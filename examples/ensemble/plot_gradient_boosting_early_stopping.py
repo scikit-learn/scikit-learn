521b3be68d95fc1499872423ff4951e8a0939b1a
Raghav RV <rvraghav93@gmail.com>
2017-07-10 12:08:50 -0500

    Flake8 fix

--- examples/ensemble/plot_gradient_boosting_early_stopping.py
+++ examples/ensemble/plot_gradient_boosting_early_stopping.py
@@ -71,7 +71,6 @@ for X, y in data_list:
                                                random_state=0)
     gb = ensemble.GradientBoostingClassifier(n_estimators=n_estimators,
                                              random_state=0)
-    
     start = time.time()
     gb.fit(X_train, y_train)
     time_gb.append(time.time() - start)
