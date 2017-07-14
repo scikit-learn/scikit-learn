29cabba9adb96f37bb0f458624495a1b5f4b1e24
Minghui Liu <minghui.liu@trincoll.edu>
2017-07-14 02:26:03 -0700

    more non_negative option deprecation warnings

--- sklearn/feature_extraction/tests/test_feature_hasher.py
+++ sklearn/feature_extraction/tests/test_feature_hasher.py
@@ -19,7 +19,7 @@ def test_feature_hasher_dicts():
     X2 = FeatureHasher(n_features=16, input_type="pair").transform(gen)
     assert_array_equal(X1.toarray(), X2.toarray())
 
-
+@ignore_warnings(category=DeprecationWarning)
 def test_feature_hasher_strings():
     # mix byte and Unicode strings; note that "foo" is a duplicate in row 0
     raw_X = [["foo", "bar", "baz", "foo".encode("ascii")],
