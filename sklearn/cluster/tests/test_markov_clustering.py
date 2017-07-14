92edacc7af1e11cedf999f263d9dab11d40ec8d3
Uri Goren <uri@goren4u.com>
2017-07-14 21:56:34 +0300

    Fixing text

--- sklearn/cluster/tests/test_markov_clustering.py
+++ sklearn/cluster/tests/test_markov_clustering.py
@@ -7,5 +7,6 @@ from sklearn.utils.testing import assert_array_equal
 
 def test_cosine_dissimilarity():
     A = [[1, 2, 0], [2, 1, 0], [0, 0, 4], [0, 0, 9]]
-    predicted = MarkovClustering().fit(A, verbose=True).labels_
-    assert_array_equal([1, 1, 0, 0], predicted)
+    predicted = MarkovClustering().fit(A).labels_
+    #Making sure that element 0 & 1 are in the same cluster, and 2 & 3 are in an other
+    assert_array_equal([predicted[0], predicted[0], predicted[3], predicted[3]], predicted)
