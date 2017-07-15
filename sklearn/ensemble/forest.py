d2311e4ed386defb53769bee3b37a276b861d492
Dmitry Petrov <to.dmitry.petrov@gmail.com>
2017-07-15 16:14:20 -0500

    changed example for RandomForestRegressor using make_regression

--- sklearn/ensemble/forest.py
+++ sklearn/ensemble/forest.py
@@ -1165,18 +1165,21 @@ class RandomForestRegressor(ForestRegressor):
     Examples
     --------
     >>> from sklearn.ensemble import RandomForestRegressor
-    >>> regr = RandomForestRegressor(max_depth=2, random_state=0)
-    >>> regr.fit([[0,0], [1, 1], [0, 1], [1,0]], [1, 1, 2, 2])
-    RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=2,
+    >>> from sklearn.datasets import make_regression
+    >>>
+    >>> X, y = make_regression(n_features=4, n_informative=2, random_state=0)
+    >>> regr = RandomForestRegressor(max_depth=5, random_state=0)
+    >>> regr.fit(X, y)
+    RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=5,
                max_features='auto', max_leaf_nodes=None,
                min_impurity_decrease=0.0, min_impurity_split=None,
                min_samples_leaf=1, min_samples_split=2,
                min_weight_fraction_leaf=0.0, n_estimators=10, n_jobs=1,
                oob_score=False, random_state=0, verbose=0, warm_start=False)
     >>> print(regr.feature_importances_)
-    [ 0.46666667  0.33333333]
-    >>> print(regr.predict([[0.5, 1]]))
-    [ 1.6]
+    [ 0.24051502  0.74652696  0.00636629  0.00659173]
+    >>> print(regr.predict([[0, 0, 0, 0]]))
+    [ 5.67871752]
 
     Notes
     -----
