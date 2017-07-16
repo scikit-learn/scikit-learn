f5b9077ec2f383127607e6f36d50a96e774e1adb
Leland McInnes <leland.mcinnes@gmail.com>
2017-07-15 22:52:08 -0500

    More Flake8 fixes.

--- examples/cluster/plot_linkage_comparison.py
+++ examples/cluster/plot_linkage_comparison.py
@@ -29,8 +29,7 @@ import warnings
 import numpy as np
 import matplotlib.pyplot as plt
 
-from sklearn import cluster, datasets, mixture
-from sklearn.neighbors import kneighbors_graph
+from sklearn import cluster, datasets
 from sklearn.preprocessing import StandardScaler
 from itertools import cycle, islice
 
