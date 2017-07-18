8911fee3c81ef014f2914e26143616180b16c61b
Andreas Mueller <t3kcit@gmail.com>
2017-07-18 12:34:54 -0500

    [MRG] update numpydoc to upstream, include fix for line numbers (#7355)

--- build_tools/circle/build_doc.sh
+++ build_tools/circle/build_doc.sh
@@ -109,6 +109,7 @@ conda update --yes --quiet conda
 conda create -n $CONDA_ENV_NAME --yes --quiet python numpy scipy \
   cython nose coverage matplotlib sphinx=1.6.2 pillow
 source activate testenv
+pip install numpydoc
 
 # Build and install scikit-learn in dev mode
 python setup.py develop
