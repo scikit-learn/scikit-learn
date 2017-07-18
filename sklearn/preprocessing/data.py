77cb44b1135a925e62f7ea728f18462ef1034784
Dmitry Petrov <to.dmitry.petrov@gmail.com>
2017-07-18 11:08:10 -0700

    made MinMaxScaler example more illustrative

--- sklearn/preprocessing/data.py
+++ sklearn/preprocessing/data.py
@@ -248,19 +248,19 @@ class MinMaxScaler(BaseEstimator, TransformerMixin):
     --------
     >>> from sklearn.preprocessing import MinMaxScaler
     >>>
-    >>> data = [[-1, -1], [-1, -1], [1, 1], [1, 1]]
+    >>> data = [[-1, 2], [-0.5, 6], [0, 10], [1, 18]]
     >>> scaler = MinMaxScaler()
     >>> print(scaler.fit(data))
     MinMaxScaler(copy=True, feature_range=(0, 1))
     >>> print(scaler.data_max_)
-    [ 1.  1.]
+    [  1.  18.]
     >>> print(scaler.transform(data))
-    [[ 0.  0.]
-     [ 0.  0.]
-     [ 1.  1.]
-     [ 1.  1.]]
+    [[ 0.    0.  ]
+     [ 0.25  0.25]
+     [ 0.5   0.5 ]
+     [ 1.    1.  ]]
     >>> print(scaler.transform([[2, 2]]))
-    [[ 1.5  1.5]]
+    [[ 1.5  0. ]]
 
     See also
     --------
