76b00ea368d505960644438b853b949a47daad91
RAKOTOARISON Herilalaina <rkt.herilalaina@gmail.com>
2017-07-20 01:36:47 +0200

    Change index

--- sklearn/feature_extraction/tests/test_text.py
+++ sklearn/feature_extraction/tests/test_text.py
@@ -238,7 +238,7 @@ def test_word_ngram_analyzer():
     cnga_file = CountVectorizer(input='file', analyzer='word',
                                 ngram_range=(3, 6)).build_analyzer()
     file = StringIO(text)
-    assert_equal(cnga_file(file)[:4], cnga(text)[:4])
+    assert_equal(cnga_file(file)[:3], cnga(text)[:3])
 
 
 def test_countvectorizer_custom_vocabulary():
