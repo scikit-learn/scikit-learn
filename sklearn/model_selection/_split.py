2f4ce3afbe56f80dea0a158cee99a0bc2aefa494
Andreas van Cranenburgh <andreas@unstable.nl>
2017-07-22 14:53:45 +0200

    fix doctest failure

--- sklearn/model_selection/_split.py
+++ sklearn/model_selection/_split.py
@@ -458,11 +458,11 @@ class GroupKFold(_BaseKFold):
     >>> X = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
     >>> y = np.array([1, 2, 3, 4])
     >>> groups = np.array([0, 0, 2, 2])
-    >>> group_kfold = GroupKFold(n_splits=2)
+    >>> group_kfold = GroupKFold(method='balance', n_splits=2)
     >>> group_kfold.get_n_splits(X, y, groups)
     2
     >>> print(group_kfold)
-    GroupKFold(n_splits=2)
+    GroupKFold(method='balance', n_splits=2)
     >>> for train_index, test_index in group_kfold.split(X, y, groups):
     ...     print("TRAIN:", train_index, "TEST:", test_index)
     ...     X_train, X_test = X[train_index], X[test_index]
