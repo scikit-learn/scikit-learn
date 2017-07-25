076efb1c4f21d79665012992a730c8157cf4fe96
Loïc Estève <loic.esteve@ymail.com>
2017-07-25 14:42:56 +0200

    Rename variable

--- sklearn/datasets/olivetti_faces.py
+++ sklearn/datasets/olivetti_faces.py
@@ -37,13 +37,11 @@ from ..externals import joblib
 
 # The original data can be found at:
 # http://cs.nyu.edu/~roweis/data/olivettifaces.mat
-ARCHIVE = RemoteFileMetadata(
+FACES = RemoteFileMetadata(
     filename='olivettifaces.mat',
     url='https://ndownloader.figshare.com/files/5976027',
     checksum=('b612fb967f2dc77c9c62d3e1266e0c73'
               'd5fca46a4b8906c18e454d41af987794'))
-
-TARGET_FILENAME = "olivetti.pkz"
 
 # Grab the module-level docstring to use as a description of the
 # dataset
@@ -112,14 +110,14 @@ def fetch_olivetti_faces(data_home=None, shuffle=False, random_state=0,
     data_home = get_data_home(data_home=data_home)
     if not exists(data_home):
         makedirs(data_home)
-    filepath = _pkl_filepath(data_home, TARGET_FILENAME)
+    filepath = _pkl_filepath(data_home, 'olivetti.pkz')
     if not exists(filepath):
         if not download_if_missing:
             raise IOError("Data not found and `download_if_missing` is False")
 
         print('downloading Olivetti faces from %s to %s'
-              % (ARCHIVE.url, data_home))
-        mat_path = _fetch_remote(ARCHIVE, dirname=data_home)
+              % (FACES.url, data_home))
+        mat_path = _fetch_remote(FACES, dirname=data_home)
         mfile = loadmat(file_name=mat_path)
         # delete raw .mat data
         remove(mat_path)
