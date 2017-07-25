c187197d57574d78479f6ed9ebe63c602e5bd2dc
mrbean <liujiacheng0810@163.com>
2017-07-25 11:11:43 +0800

    fix style

--- sklearn/discriminant_analysis.py
+++ sklearn/discriminant_analysis.py
@@ -170,7 +170,7 @@ class LinearDiscriminantAnalysis(BaseEstimator, LinearClassifierMixin,
         Number of components (< n_classes - 1) for dimensionality reduction.
 
     store_covariance : bool, optional
-        Additionally compute class covariance matrix (default False), used 
+        Additionally compute class covariance matrix (default False), used
         only in 'svd' solver.
 
         .. versionadded:: 0.17
