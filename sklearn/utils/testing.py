24edba53577d91561cf82559cb2e6e9a0172367f
kumarashutosh.ee@gmail.com <kumarashutosh.ee@gmail.com>
2017-08-13 21:46:39 +0530

    _formatMessage added

--- sklearn/utils/testing.py
+++ sklearn/utils/testing.py
@@ -167,6 +167,27 @@ class _AssertRaisesContext(_AssertRaisesBaseContext):
 
 
 class TestCase_new(object):
+    def _formatMessage(self, msg, standardMsg):
+        """Honour the longMessage attribute when generating failure messages.
+        If longMessage is False this means:
+        * Use only an explicit message if it is provided
+        * Otherwise use the standard message for the assert
+
+        If longMessage is True:
+        * Use the standard message
+        * If an explicit message is provided, plus ' : ' and the explicit message
+        """
+        if not self.longMessage:
+            return msg or standardMsg
+        if msg is None:
+            return standardMsg
+        try:
+            # don't switch to '{}' formatting in Python 2.X
+            # it changes the way unicode input is handled
+            return '%s : %s' % (standardMsg, msg)
+        except UnicodeDecodeError:
+            return  '%s : %s' % (safe_repr(standardMsg), safe_repr(msg))
+
     def assertRaises(self, expected_exception, *args, **kwargs):
         """Fail unless an exception of class expected_exception is raised
            by the callable when invoked with specified positional and
