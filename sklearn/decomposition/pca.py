d44986827fb9e2049e5882dcd84d6fb26c2aa08b
Wally <wallygauze@gmail.com>
2017-08-15 22:42:50 +0100

    fixed flake8

--- sklearn/decomposition/pca.py
+++ sklearn/decomposition/pca.py
@@ -136,9 +136,9 @@ class PCA(_BasePCA):
         of components such that the amount of variance that needs to be
         explained is greater than the percentage specified by n_components.
         If svd_solver == 'arpack', the number of components must be strictly
-        less than the minimum of n_features and n_samples. 
+        less than the minimum of n_features and n_samples.
         Hence, the None case results in:
-        
+
             n_components == min(n_samples, n_features) - 1
 
     copy : bool (default True)
