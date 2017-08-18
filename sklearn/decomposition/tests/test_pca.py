2251ae5046495523d385a5f294da846bdeb36ffc
Wally <wallygauze@gmail.com>
2017-08-18 20:01:55 +0100

    arpack case was missing from test; improved overall test

--- sklearn/decomposition/tests/test_pca.py
+++ sklearn/decomposition/tests/test_pca.py
@@ -354,17 +354,30 @@ def test_pca_validation():
     # (less than 0 or more than the lesser dimension of the input
     # matrix X) raise errors.
     X = np.array([[0, 1, 0], [1, 0, 0]])
+    minimum = 2 # The smallest dimension
+    lower_limit = {'randomized':1,'full':0,'auto':0}
     # We conduct the same test on X.T so that it is invariant to axis.
     for data in [X, X.T]:
         for solver in solver_list:
             for n_components in [-1, 3]:
-                assert_raises_regex(ValueError,
-                                    "n_components\=.* must be between .* and "
-                                    "min\(n_samples, n_features\)\=.* with "
-                                    "svd_solver\="
-                                    "\'(?:full|arpack|randomized|auto)\'$",
-                                    PCA(n_components, svd_solver=solver).fit,
-                                    data)
+                if solver == 'arpack':
+                    assert_raises_regex(ValueError,
+                                        "n_components={} must be stricly less "
+                                        "than min(n_samples, n_features)={} "
+                                        "with svd_solver='arpack'"
+                                        .format(n_components, minimum),
+                                        PCA(n_components, svd_solver=solver)
+                                        .fit, data)
+                else:
+                    assert_raises_regex(ValueError,
+                                        "n_components={} must be between {} "
+                                        "and min(n_samples, n_features)={} with "
+                                        "svd_solver='{}'"
+                                        .format(n_components,
+                                                lower_limit[solver], minimum,
+                                                solver),
+                                        PCA(n_components,
+                                            svd_solver=solver).fit, data)
 
 
 def test_n_components_none():
