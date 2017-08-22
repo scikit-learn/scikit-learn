Fix rank and masking cases

sklearn/model_selection/_search.py
@@ -565,7 +565,8 @@ class BaseSearchCV(six.with_metaclass(ABCMeta, BaseEstimator,
         To be overridden by implementors.
 
         It can iteratively generate a list of candidate parameter dicts, and is
-        returned the results corresponding to those candidates::
+        returned the results (without ranks) corresponding to those
+        candidates::
 
             def _generate_candidates(self):
                 results = yield self.initial_candidates()
@@ -677,18 +678,42 @@ class BaseSearchCV(six.with_metaclass(ABCMeta, BaseEstimator,
                 except StopIteration:
                     break
 
-        # FIXME: handle keys present in some resutls and not in others
-        # FIXME: only construct ranks after this
-        results = {k: (np.ma if hasattr(all_results[0][k], 'mask')
-                       else np).concatenate([r[k] for r in all_results])
-                   for k in all_results[0]}
+        # Merge all_results into a single dict
+        if len(all_results) == 1:
+            results = all_results[0]
+        else:
+            results = {}
+            keys = set()
+            for some_results in all_results:
+                keys.update(some_results)
+            for k in keys:
+                to_concat = []
+                for some_results in all_results:
+                    try:
+                        v = some_results[k]
+                    except KeyError:
+                        assert k.startswith('param_')
+                        v = np.ma.masked_all(len(some_results['params']),
+                                             dtype='O')
+                    to_concat.append(v)
+
+                if hasattr(v, 'mask'):
+                    results[k] = np.ma.concatenate(to_concat)
+                else:
+                    results[k] = np.concatenate(to_concat)
+
+        results.update({"rank_%s" % k[5:]:
+                        np.asarray(rankdata(-v, method='min'),
+                                   dtype=np.int32)
+                        for k, v in results.items()
+                        if k.startswith('mean_test_')})
 
         # For multi-metric evaluation, store the best_index_, best_params_ and
         # best_score_ iff refit is one of the scorer names
         # In single metric evaluation, refit_metric is "score"
         if self.refit or not self.multimetric_:
             self.best_index_ = results["rank_test_%s" % refit_metric].argmin()
-            self.best_params_ = candidate_params[self.best_index_]
+            self.best_params_ = results["params"][self.best_index_]
             self.best_score_ = results["mean_test_%s" % refit_metric][
                 self.best_index_]
 
@@ -727,7 +752,7 @@ class BaseSearchCV(six.with_metaclass(ABCMeta, BaseEstimator,
 
         results = dict()
 
-        def _store(key_name, array, weights=None, splits=False, rank=False):
+        def _store(key_name, array, weights=None, splits=False):
             """A small helper to store the scores/times to the cv_results_"""
             # When iterated first by splits, then by parameters
             # We want `array` to have `n_candidates` rows and `n_splits` cols.
@@ -746,10 +771,6 @@ class BaseSearchCV(six.with_metaclass(ABCMeta, BaseEstimator,
                                              array_means[:, np.newaxis]) ** 2,
                                             axis=1, weights=weights))
             results['std_%s' % key_name] = array_stds
-
-            if rank:
-                results["rank_%s" % key_name] = np.asarray(
-                    rankdata(-array_means, method='min'), dtype=np.int32)
 
         _store('fit_time', fit_time)
         _store('score_time', score_time)
@@ -777,7 +798,7 @@ class BaseSearchCV(six.with_metaclass(ABCMeta, BaseEstimator,
         for scorer_name in scorers.keys():
             # Computed the (weighted) mean and std for test scores alone
             _store('test_%s' % scorer_name, test_scores[scorer_name],
-                   splits=True, rank=True,
+                   splits=True,
                    weights=test_sample_counts if self.iid else None)
             if self.return_train_score:
                 _store('train_%s' % scorer_name, train_scores[scorer_name],

sklearn/model_selection/tests/test_search.py
@@ -1490,15 +1490,19 @@ def test_transform_inverse_transform_round_trip():
 def test_generate_candidates():
     def check_results(results, gscv):
         exp_results = gscv.cv_results_
-        assert sorted(results.keys()) == sorted(exp_results.keys())
+        assert_equal(sorted(results.keys()),
+                     sorted(k for k in exp_results
+                            if not k.startswith('rank_')))
         for k in results:
             if not k.endswith('_time'):
-                # XXX: results['param'] is a list :(
+                # XXX: results['params'] is a list :|
                 results[k] = np.asanyarray(results[k])
                 if results[k].dtype.kind == 'O':
-                    assert_array_equal(exp_results[k], results[k])
+                    assert_array_equal(exp_results[k], results[k],
+                                       err_msg='Checking ' + k)
                 else:
-                    assert_almost_equal(exp_results[k], results[k])
+                    assert_almost_equal(exp_results[k], results[k],
+                                        err_msg='Checking ' + k)
 
     def fit_grid(param_grid):
         return GridSearchCV(clf, param_grid).fit(X, y)
@@ -1508,17 +1512,27 @@ def test_generate_candidates():
             super(MySearchCV, self).__init__(*args, **kwargs)
 
         def _generate_candidates(self):
-            results = yield [{'C': 0.1}, {'C': 1}]
-            check_results(results, fit_grid({'C': [0.1, 1]}))
-            results = yield [{'C': 5}, {'C': 10}]
-            check_results(results, fit_grid({'C': [5, 10]}))
+            results = yield [{'max_depth': 1}, {'max_depth': 2}]
+            check_results(results, fit_grid({'max_depth': [1, 2]}))
+            results = yield [{'min_samples_split': 5},
+                             {'min_samples_split': 10}]
+            check_results(results, fit_grid({'min_samples_split': [5, 10]}))
 
-    clf = LinearSVC(random_state=0)
-    X, y = make_classification(n_samples=50)
+    # Using regressor to make sure each score differs
+    clf = DecisionTreeRegressor(random_state=0)
+    X, y = make_classification(n_samples=100, n_informative=4,
+                               random_state=0)
     mycv = MySearchCV(clf).fit(X, y)
-    gscv = fit_grid({'C': [0.1, 1, 5, 10]})
+    gscv = fit_grid([{'max_depth': [1, 2]},
+                     {'min_samples_split': [5, 10]}])
 
-    check_results(mycv.cv_results_, gscv)
+    results = mycv.cv_results_
+    rank = results.pop('rank_test_score')
+    check_results(results, gscv)
+    assert_array_equal(rank, gscv.cv_results_['rank_test_score'])
     for attr in dir(gscv):
-        if attr[0].islower() and attr[-1:] == '_' and attr != 'cv_results_':
-            assert_equal(getattr(gscv, attr), getattr(mycv, attr))
+        if attr[0].islower() and attr[-1:] == '_' and \
+           attr not in {'cv_results_', 'best_estimator_',
+                        'grid_scores_'}:
+            assert_equal(getattr(gscv, attr), getattr(mycv, attr),
+                         msg='Attribute %s not equal' % attr)