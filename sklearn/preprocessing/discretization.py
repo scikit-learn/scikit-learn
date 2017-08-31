5a062b3877014349521a18e2e1af05df856db5fa
Hanmin Qin <qinhanmin2005@sina.com>
2017-08-31 17:51:44 +0800

    minor fix

--- sklearn/preprocessing/discretization.py
+++ sklearn/preprocessing/discretization.py
@@ -68,7 +68,7 @@ class KBinsDiscretizer(BaseEstimator, TransformerMixin):
     ...      [-1, 2, -3, -0.5],
     ...      [ 0, 3, -2,  0.5],
     ...      [ 1, 4, -1,    2]]
-    >>> est = KBinsDiscretizer(n_bins=3, encode='ordinal')
+    >>> est = KBinsDiscretizer(n_bins=3)
     >>> est.fit(X)  # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
     KBinsDiscretizer(...)
     >>> Xt = est.transform(X)
