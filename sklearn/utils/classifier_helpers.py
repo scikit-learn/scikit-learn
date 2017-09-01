bc8814d823ee25c6df4888aec9685c42a77e1992
aarshayj <aarshay.jain@gmail.com>
2017-09-01 17:30:50 -0400

    added multioutput and present_classes parameters

--- sklearn/utils/classifier_helpers.py
+++ sklearn/utils/classifier_helpers.py
@@ -1,9 +1,9 @@
 import numpy as np
 from . import _check_classes, _check_y_classes
-from .. preprocessing import label_binarize
+from .. preprocessing import LabelEncoder
 
 
-def _set_classes(classes, y, label_binarize_y=False):
+def _set_classes(classes, y, multioutput=False, return_present_classes=False):
         """Returns the value to be set in classes_ attribute and transformed y
 
         The classes are selected based on the classes present in y and
@@ -17,26 +17,60 @@ def _set_classes(classes, y, label_binarize_y=False):
         y : array-like, shape = (n_samples, n_outputs)
             The true labels
 
-        Returns
-        -------
-        classes : array-like, shape = (n_classes)
-            A sorted array of unique classes to be set as class_ attriute
+        multioutput : boolean
+            If True, it will return: (classes, n_classes, y_encoded)
+            If False, it will return: (classes, y_encoded)
 
-        y_transformed : array-like or None
-            The transformed version of y based on function inputs or None if
-            no transformations passed as True.
+        return_present_classes : boolean
+            This will only work if multioutput is False. Otherwise, this will
+            be ignored.
+            If True, returns: (classes, present_classes, y_encoded)
+            If Falase, returns: (classes, y_encoded)
         """
-        y_output = None
+        if multioutput:
+            # multioutput case:
+            n_outputs_ = y.shape[1]
+            classes_ = []
+            n_classes_ = []
+            y_encoded = np.zeros(y.shape, dtype=np.int)
 
+            if classes is None:
+                for k in range(n_outputs_):
+                    classes_k, y_encoded[:, k] = np.unique(
+                                                    y[:, k],
+                                                    return_inverse=True)
+                    classes_.append(classes_k)
+                    n_classes_.append(classes_k.shape[0])
+            else:
+                _check_classes(classes, n_outputs_)
+                if n_outputs_ == 1:
+                    classes_ = np.atleast_2d(classes)
+                else:
+                    classes_ = np.asarray(classes)
+                # encode y:
+                for k in range(n_outputs_):
+                    classes_k = np.asarray(classes_[k])
+                    # check y has subset of classes:
+                    _check_y_classes(np.unique(y[:, k]), classes_k)
+                    le = LabelEncoder().fit(classes_k)
+                    y_encoded[:, k] = le.transform(y[:, k])
+                    n_classes_.append(classes_k.shape[0])
+
+            return classes_, n_classes_, y_encoded
+
+        # non multi-output case:
         # check if classes was passed in init
+        present_classes = np.unique(y)
         if classes is None:
-            classes_ = np.unique(y)
+            classes_ = present_classes
         else:
             _check_classes(classes)
             classes_ = np.asarray(classes)
-            _check_y_classes(np.unique(y), classes_)
+            _check_y_classes(present_classes, classes_)
 
-        if label_binarize_y:
-            y_output = label_binarize(y, classes=classes_)
+        y_encoded = LabelEncoder().fit(classes_).transform(y)
 
-        return classes_, y_output
+        if return_present_classes:
+            return classes_, present_classes, y_encoded
+        else:
+            return classes_, y_encoded
