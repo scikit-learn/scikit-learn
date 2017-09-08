a2e40d78eb088f3deff4ce099d0f45f781ce9665
Andreas Mueller <t3kcit@gmail.com>
2017-09-08 12:17:25 -0400

    remove references to old GP, GMM and sparse_center_data
    Remove mixture/gmm

--- sklearn/mixture/__init__.py
+++ sklearn/mixture/__init__.py
@@ -2,21 +2,9 @@
 The :mod:`sklearn.mixture` module implements mixture modeling algorithms.
 """
 
-from .gmm import sample_gaussian, log_multivariate_normal_density
-from .gmm import GMM, distribute_covar_matrix_to_match_covariance_type
-from .gmm import _validate_covars
-from .dpgmm import DPGMM, VBGMM
-
 from .gaussian_mixture import GaussianMixture
 from .bayesian_mixture import BayesianGaussianMixture
 
 
-__all__ = ['DPGMM',
-           'GMM',
-           'VBGMM',
-           '_validate_covars',
-           'distribute_covar_matrix_to_match_covariance_type',
-           'log_multivariate_normal_density',
-           'sample_gaussian',
-           'GaussianMixture',
+__all__ = ['GaussianMixture',
            'BayesianGaussianMixture']
