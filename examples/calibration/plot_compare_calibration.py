38d68d8ac3c9bd319f7b5508213f4e51e0273001
Vrishank Bhardwaj <vrishank1997@gmail.com>
2017-09-14 14:47:56 +0530

    Added max_iter=100

--- examples/calibration/plot_compare_calibration.py
+++ examples/calibration/plot_compare_calibration.py
@@ -75,7 +75,7 @@ y_train = y[:train_samples]
 y_test = y[train_samples:]
 
 # Create classifiers
-lr = LogisticRegression()
+lr = LogisticRegression(max_iter=100)
 gnb = GaussianNB()
 svc = LinearSVC(C=1.0)
 rfc = RandomForestClassifier(n_estimators=100)
