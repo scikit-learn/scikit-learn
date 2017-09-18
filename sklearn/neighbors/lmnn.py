399088844a2655ce3033aa089e4b8d16e0478f5c
John Chiotellis <chiotell@in.tum.de>
2017-09-18 12:00:42 +0200

    fix multiline code in docstring

--- sklearn/neighbors/lmnn.py
+++ sklearn/neighbors/lmnn.py
@@ -149,7 +149,7 @@ class LargeMarginNearestNeighbor(BaseEstimator, TransformerMixin):
     >>> from sklearn.model_selection import train_test_split
     >>> X, y = load_iris(return_X_y=True)
     >>> X_train, X_test, y_train, y_test = train_test_split(X, y,
-    stratify=y, test_size=0.7, random_state=42)
+    ... stratify=y, test_size=0.7, random_state=42)
     >>> lmnn = LargeMarginNearestNeighbor(n_neighbors=3, random_state=42)
     >>> lmnn.fit(X_train, y_train) # doctest: +ELLIPSIS
     LargeMarginNearestNeighbor(...)
