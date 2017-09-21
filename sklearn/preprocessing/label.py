effbd45198c3300018403a218f1ad85858ac82dc
Charles Newey <charlie@newey.me>
2017-09-21 14:55:08 +0100

    Wrap error message to <79 chars

--- sklearn/preprocessing/label.py
+++ sklearn/preprocessing/label.py
@@ -130,7 +130,8 @@ class LabelEncoder(BaseEstimator, TransformerMixin):
         classes = np.unique(y)
         if len(np.intersect1d(classes, self.classes_)) < len(classes):
             diff = np.setdiff1d(classes, self.classes_)
-            raise ValueError("y contains previously unseen labels: %s" % str(diff))
+            raise ValueError(
+                    "y contains previously unseen labels: %s" % str(diff))
         return np.searchsorted(self.classes_, y)
 
     def inverse_transform(self, y):
@@ -149,7 +150,8 @@ class LabelEncoder(BaseEstimator, TransformerMixin):
 
         diff = np.setdiff1d(y, np.arange(len(self.classes_)))
         if len(diff):
-            raise ValueError("y contains previously unseen labels: %s" % str(diff))
+            raise ValueError(
+                    "y contains previously unseen labels: %s" % str(diff))
         y = np.asarray(y)
         return self.classes_[y]
 
