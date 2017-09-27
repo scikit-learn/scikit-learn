c89654a3332de815de5dd5c32f96cadf42487e31
Loïc Estève <loic.esteve@ymail.com>
2017-09-27 23:38:29 +0200

    FIX do not update conda as a temporary work-around for conda issue
    https://github.com/conda/conda/issues/6030

--- build_tools/circle/build_doc.sh
+++ build_tools/circle/build_doc.sh
@@ -102,7 +102,8 @@ wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
    -O miniconda.sh
 chmod +x miniconda.sh && ./miniconda.sh -b -p $MINICONDA_PATH
 export PATH="$MINICONDA_PATH/bin:$PATH"
-conda update --yes --quiet conda
+# Temporary work-around (2017-09-27)
+# conda update --yes --quiet conda
 
 # Configure the conda environment and put it in the path using the
 # provided versions
