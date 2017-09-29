0aed10864cb429984388bd6ec0cf9d5a9ef72fb4
kumarashutosh.ee@gmail.com <kumarashutosh.ee@gmail.com>
2017-09-29 18:32:43 +0530

    doc added

--- sklearn/model_selection/_search.py
+++ sklearn/model_selection/_search.py
@@ -895,6 +895,11 @@ class GridSearchCV(BaseSearchCV):
         in 0.21.
         If ``False``, the ``cv_results_`` attribute will not include training
         scores.
+        Computing training scores is used to get insights on how different
+        parameter settings impact the overfitting/underfitting trade-off.
+        However computing the scores on the training set can be computationally
+        expensive and is not strictly required to select the parameters that
+        yield the best generalization performance.
 
 
     Examples
