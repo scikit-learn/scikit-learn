9811c25e69e8a5798147426d1f81443b2ce9f428
Hanmin Qin <qinhanmin2005@sina.com>
2017-10-03 23:39:56 +0800

    use int in the test to avoid strange failure

--- sklearn/metrics/tests/test_ranking.py
+++ sklearn/metrics/tests/test_ranking.py
@@ -427,13 +427,13 @@ def test_auc_errors():
     assert_raises(ValueError, auc, [0.0], [0.1])
 
     # x is not in order
-    error_message = ("x is neither increasing nor decreasing : "
-                     "[ 2.5  1.6  3.7  4.8]. np.diff(x) contains 2 positive "
-                     "values and 1 negative values. The most positive value "
-                     "in np.diff(x) : x[2] - x[1] = 2.1. The most negative "
-                     "value in np.diff(x) : x[1] - x[0] = -0.9.")
+    error_message = ("x is neither increasing nor decreasing : [2 1 3 4]. "
+                     "np.diff(x) contains 2 positive values and 1 negative "
+                     "values. The most positive value in np.diff(x) : x[2] "
+                     "- x[1] = 2. The most negative value in np.diff(x) : "
+                     "x[1] - x[0] = -1.")
     assert_raise_message(ValueError, error_message, auc,
-                         [2.5, 1.6, 3.7, 4.8], [5, 6, 7, 8])
+                         [2, 1, 3, 4], [5, 6, 7, 8])
 
 
 def test_deprecated_auc_reorder():
