c7d1db1417b506f32dce395c8795bdd031cf87ae
nielsenmarkus11 <nielsenmarkus11@users.noreply.github.com>
2017-10-06 09:00:39 -0600

    [MRG+1] Raise error when SparseSeries is passed into classification metrics (#7373)
    * Raise error when SparseSeries is passed into roc_curve
    
    * Changed "y_true" in second if block to "y_score"
    
    * Remove code to import pandas and add sparseseries check to 'type_of_target' function. Finally, add 'type_of_target' call to _binary_clf_curve
    
    * Remove pandas import and old comparison in roc_curve.
    
    * Add test for 'type_of_target' function
    
    * Add white space after commas
    
    * Correct other white space issues
    
    * Move type_of_target test into try clause, remove test_precision_recall_curve_pos_label since as multiclass it doesn't make sense
    
    * Add test_precision_recall_curve_pos_label back in and also add test_binary_clf_curve to test new logic in _binary_clf_curve function
    
    * Correct syntax and formatting.
    
    * Remove trailing white space
    
    * Correct validation logic
    
    * Update test_multiclass.py per @jnothman 's request.
    
    * Import SkipTest function.
    
    * Remove extra white space from line 303

--- sklearn/metrics/tests/test_ranking.py
+++ sklearn/metrics/tests/test_ranking.py
@@ -580,6 +580,14 @@ def test_auc_score_non_binary_class():
                              roc_auc_score, y_true, y_pred)
 
 
+def test_binary_clf_curve():
+    rng = check_random_state(404)
+    y_true = rng.randint(0, 3, size=10)
+    y_pred = rng.rand(10)
+    msg = "multiclass format is not supported"
+    assert_raise_message(ValueError, msg, precision_recall_curve,
+                         y_true, y_pred)
+
 def test_precision_recall_curve():
     y_true, _, probas_pred = make_prediction(binary=True)
     _test_precision_recall_curve(y_true, probas_pred)
