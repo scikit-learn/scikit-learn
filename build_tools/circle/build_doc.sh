cf174afdd3c0d50163afba190e2fbd0774043b27
Joel Nothman <joel.nothman@gmail.com>
2017-10-10 10:56:07 +1100

    CI avoid matplotlib 2.1.0
    Fixes #9896

--- build_tools/circle/build_doc.sh
+++ build_tools/circle/build_doc.sh
@@ -107,7 +107,7 @@ conda update --yes --quiet conda
 # Configure the conda environment and put it in the path using the
 # provided versions
 conda create -n $CONDA_ENV_NAME --yes --quiet python numpy scipy \
-  cython nose coverage matplotlib sphinx=1.6.2 pillow
+  cython nose coverage 'matplotlib=2.0.*|>2.1.0' sphinx=1.6.2 pillow
 source activate testenv
 pip install sphinx-gallery numpydoc
 
