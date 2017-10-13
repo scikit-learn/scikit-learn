75a0c59f8d41e376fd31cd6fd30ac762554ae241
reiinakano <reiichiro.s.nakano@gmail.com>
2017-10-14 02:58:21 +0800

    change warning to runtimewarning

--- sklearn/model_selection/_validation.py
+++ sklearn/model_selection/_validation.py
@@ -735,8 +735,8 @@ def _fit_and_predict(estimator, X, y, train, test, verbose, fit_params,
                           "not match total number of classes ({}).  "
                           "Results may not be appropriate for your use case."
                           " {}".format(len(estimator.classes_),
-                                       n_classes, err_mess)
-                          )
+                                       n_classes, err_mess),
+                          RuntimeWarning)
             if method == 'decision_function':
                 if (predictions.ndim == 2 and
                         predictions.shape[1] != len(estimator.classes_)):
@@ -749,15 +749,13 @@ def _fit_and_predict(estimator, X, y, train, test, verbose, fit_params,
                                      ' reconcile different number of classes'
                                      ' in different folds. {}'.format(
                                         predictions.shape, method,
-                                        len(estimator.classes_), err_mess
-                                     ))
+                                        len(estimator.classes_), err_mess))
                 if len(estimator.classes_) <= 2:
                     # In this special case, `predictions` contains a 1D array.
                     raise ValueError('Only {} class/es in training fold, this'
                                      ' is not supported for decision_function'
                                      ' with imbalanced folds. {}'.format(
-                                        len(estimator.classes_), err_mess
-                                     ))
+                                        len(estimator.classes_), err_mess))
 
             float_min = np.finfo(predictions.dtype).min
             default_values = {'decision_function': float_min,
