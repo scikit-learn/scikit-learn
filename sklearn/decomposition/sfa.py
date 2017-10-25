422771104f3746e4ba9bbc4c12875b112d8629b8
Pietro Berkes <pberkes@enthought.com>
2017-10-25 19:34:16 +0200

    Straightforward implementation of the SFA algorithm

--- sklearn/decomposition/sfa.py
+++ sklearn/decomposition/sfa.py
@@ -1,3 +1,7 @@
+import numpy as np
+import scipy as sp
+
+from sklearn.covariance import empirical_covariance
 from ..base import BaseEstimator, TransformerMixin
 from ..utils.validation import check_array, check_is_fitted
 
@@ -16,13 +20,18 @@ class SFA(BaseEstimator, TransformerMixin):
     Parameters
     ----------
 
+    copy : bool (default True)
+        If False, data passed to fit are overwritten and running
+        fit(X).transform(X) will not yield the expected results,
+        use fit_transform(X) instead.
+
     Attributes
     ----------
     n_input_dim_ : int
         The number of input dimensions of the data passed to :meth:`fit`
     """
-    def __init__(self):
-        pass
+    def __init__(self, copy=True):
+        self.copy = copy
 
     def fit(self, X, y=None):
         """Fit the SFA parameters with the `X`.
@@ -40,9 +49,19 @@ class SFA(BaseEstimator, TransformerMixin):
         self : SFA
             Returns self
         """
-        X = check_array(X)
-
+        X = check_array(X, dtype=[np.float64, np.float32], copy=self.copy,
+                        ensure_min_samples=2)
+        dX_dt = X[1:, :] - X[:-1, :]
         self.n_input_dim_ = X.shape[1]
+
+        self.mean_ = np.mean(X, axis=0)
+        X -= self.mean_
+
+        cov_mtx = empirical_covariance(X, assume_centered=True)
+        dcov_mtx = empirical_covariance(dX_dt, assume_centered=True)
+
+        d, self.components_ = sp.linalg.eigh(dcov_mtx, cov_mtx)
+        self.bias_ = np.dot(self.mean_, self.components_)
 
         # Return the transformer
         return self
@@ -64,15 +83,18 @@ class SFA(BaseEstimator, TransformerMixin):
         X_new : array-like, shape (n_samples, n_components)
         """
         # Check that fit has been called
-        check_is_fitted(self, ['n_input_dim_'])
+        check_is_fitted(self, ['n_input_dim_', 'components_', 'bias_'])
 
         # Input validation
-        X = check_array(X)
+        X = check_array(X, dtype=[np.float64, np.float32],
+                        ensure_min_samples=2)
 
         # Check that the input dimenstionality is the same as the one passed
         # during fit
         if X.shape[1] != self.n_input_dim_:
             raise ValueError(
-                'Dimenstionality of input is different from that seen '
+                'Dimensionality of input is different from that seen '
                 'in `fit`')
-        return X
+
+        X_transformed = np.dot(X, self.components_) - self.bias_
+        return X_transformed
--- sklearn/decomposition/tests/test_sfa.py
+++ sklearn/decomposition/tests/test_sfa.py
@@ -1,7 +1,38 @@
-from sklearn.utils.estimator_checks import check_estimator
+import numpy as np
+from numpy.testing import assert_array_almost_equal
 
+from sklearn.utils.estimator_checks import check_estimator
 from ..sfa import SFA
 
 
-def test_sfa_is_estimator():
+def test_sfa_is_valid_estimator():
     return check_estimator(SFA)
+
+
+def test_sfa():
+    # Create two sin waves with zero mean and unit variance, and mix them
+    # together with a arbitrary linear transform. SFA should be able to
+    # unmix them.
+    n_samples = 10000
+    t = np.linspace(0.0, 1.0, num=n_samples)
+    X = np.array([np.sin(2 * np.pi * t),
+                  np.sin(2 * np.pi * 5 * t)]).T
+    X = X - X[:-1, :].mean(axis=0)
+    X = X / X[:-1, :].std(axis=0)
+
+    transformation_mix = np.array([[0.3, 0.9], [0.4, 0.1]])
+    transformation_mean = np.array([10.3, -29.99])
+    X_mixed = np.dot(X, transformation_mix) + transformation_mean
+
+    sfa = SFA()
+    sfa.fit(X_mixed)
+    X_output = sfa.transform(X_mixed)
+
+    # Output should have zero mean, unit variance
+    assert_array_almost_equal(X_output.mean(axis=0), 0)
+    assert_array_almost_equal(X_output.std(axis=0), 1)
+
+    # The output signals are perfectly correlated with the original,
+    # unmixed signals.
+    correlation = np.dot(X.T, X_output) / n_samples
+    assert_array_almost_equal(abs(correlation), np.eye(2), 4)
