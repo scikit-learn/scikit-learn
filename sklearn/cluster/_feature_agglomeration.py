7b5de282c9afdd4d94b9b028ff432399229c203c
sergul <sergulaydore@gmail.com>
2017-10-29 14:42:28 -0400

    special case was added back

--- sklearn/cluster/_feature_agglomeration.py
+++ sklearn/cluster/_feature_agglomeration.py
@@ -41,15 +41,20 @@ class AgglomerationTransform(TransformerMixin):
 
         pooling_func = self.pooling_func
         X = check_array(X)
-        size = np.bincount(self.labels_)
-        n_samples = X.shape[0]
-        nX = []
         if len(self.labels_) != X.shape[1]:
             raise ValueError("X has a different number of features than "
                              "during fitting.")
-        # a fast way to compute the mean of grouped features
-        nX = np.array([np.bincount(self.labels_, X[i, :])/size
-                      for i in range(n_samples)])
+        if pooling_func == np.mean:
+            size = np.bincount(self.labels_)
+            n_samples = X.shape[0]
+            # a fast way to compute the mean of grouped features
+            nX = np.array([np.bincount(self.labels_, X[i, :])/size
+                          for i in range(n_samples)])
+        else:
+            nX = []
+            for l in np.unique(self.labels_):
+                nX.append(pooling_func(X[:, self.labels_ == l], axis=1))
+            nX = np.array(nX).T
         return nX
 
     def inverse_transform(self, Xred):
