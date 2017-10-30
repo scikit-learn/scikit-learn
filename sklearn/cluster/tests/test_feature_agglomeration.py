5818abfcd96684f45003af0f364b1cdc00a048a1
sergul <sergulaydore@gmail.com>
2017-10-30 13:08:32 -0400

    after jnothman's review

--- sklearn/cluster/tests/test_feature_agglomeration.py
+++ sklearn/cluster/tests/test_feature_agglomeration.py
@@ -10,9 +10,9 @@ from sklearn.utils.testing import assert_array_almost_equal
 
 
 def test_feature_agglomeration():
-    rng = np.random.RandomState(0)
-    n_samples, n_features, n_clusters = 10000, 1000, 200
-    X = rng.randn(n_samples, n_features)
+    n_clusters = 1
+    X = np.array([0, 0, 1], ndmin=2)  # (n_samples, n_features)
+
     agglo_mean = FeatureAgglomeration(n_clusters=n_clusters,
                                       pooling_func=np.mean)
     agglo_median = FeatureAgglomeration(n_clusters=n_clusters,
@@ -23,22 +23,18 @@ def test_feature_agglomeration():
     assert_true(np.size(np.unique(agglo_median.labels_)) == n_clusters)
 
     # Test transform
-    X_red_mean = agglo_mean.transform(X)
-    X_red_median = agglo_median.transform(X)
-    assert_true(X_red_mean.shape[1] == n_clusters)
-    assert_true(X_red_median.shape[1] == n_clusters)
-
-    # Check that fitting with no samples raises a ValueError
-    assert_raises(ValueError, agglo_mean.fit, X[:0])
-    assert_raises(ValueError, agglo_median.fit, X[:0])
+    Xt_mean = agglo_mean.transform(X)
+    Xt_median = agglo_median.transform(X)
+    assert_true(Xt_mean.shape[1] == n_clusters)
+    assert_true(Xt_median.shape[1] == n_clusters)
 
     # Test inverse transform
-    X_full_mean = agglo_mean.inverse_transform(X_red_mean)
-    X_full_median = agglo_mean.inverse_transform(X_red_median)
+    X_full_mean = agglo_mean.inverse_transform(Xt_mean)
+    X_full_median = agglo_median.inverse_transform(Xt_median)
     assert_true(np.unique(X_full_mean[0]).size == n_clusters)
     assert_true(np.unique(X_full_median[0]).size == n_clusters)
 
     assert_array_almost_equal(agglo_mean.transform(X_full_mean),
-                              X_red_mean)
-    assert_array_almost_equal(agglo_mean.transform(X_full_median),
-                              X_red_median)
+                              Xt_mean)
+    assert_array_almost_equal(agglo_median.transform(X_full_median),
+                              Xt_median)
