44416461a3d4c139a45c823d2963be31ed5741dc
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2017-11-02 20:21:22 +0100

    address comments from @glemaitre

--- examples/plot_anomaly_comparison.py
+++ examples/plot_anomaly_comparison.py
@@ -3,29 +3,29 @@
 Comparing anomaly detection algorithms for outlier detection on toy datasets
 ============================================================================
 
-This example shows characteristics of different
-anomaly detection algorithms on 2D datasets. Datasets contain
-one or two modes (regions of high density) to illustrate the ability
-of algorithms to cope with multimodal data.
+This example shows characteristics of different anomaly detection algorithms
+on 2D datasets. Datasets contain one or two modes (regions of high density)
+to illustrate the ability of algorithms to cope with multimodal data.
 
 For each dataset, 15% of samples are generated as random uniform noise. This
 proportion is the value given to the nu parameter of the OneClassSVM and the
 contamination parameter of the other outlier detection algorithms.
 Decision boundaries between inliers and outliers are displayed in black.
 
-Local Outlier Factor (LOF) does not show a decision boundary in black
-as it has no predict method to be applied on new data.
+Local Outlier Factor (LOF) does not show a decision boundary in black as it
+has no predict method to be applied on new data.
 
-While these examples give some intuition about the
-algorithms, this intuition might not apply to very high
-dimensional data.
+While these examples give some intuition about the algorithms, this
+intuition might not apply to very high dimensional data.
 
-Finally, note that parameters of the models have been here handpicked
-but that in practice they need to be adjusted. In the absence of labelled
-data, the problem is completely unsupervised so model selection can be
-a challenge.
+Finally, note that parameters of the models have been here handpicked but
+that in practice they need to be adjusted. In the absence of labelled data,
+the problem is completely unsupervised so model selection can be a challenge.
 """
-print(__doc__)  # noqa
+
+# Author: Alexandre Gramfort <alexandre.gramfort@inria.fr>
+#         Albert Thomas <albert.thomas@telecom-paristech.fr>
+# License: BSD 3 clause
 
 import time
 
@@ -38,6 +38,8 @@ from sklearn.datasets import make_moons, make_blobs
 from sklearn.covariance import EllipticEnvelope
 from sklearn.ensemble import IsolationForest
 from sklearn.neighbors import LocalOutlierFactor
+
+print(__doc__)
 
 matplotlib.rcParams['contour.negative_linestyle'] = 'solid'
 
@@ -77,10 +79,10 @@ plt.subplots_adjust(left=.02, right=.98, bottom=.001, top=.96, wspace=.05,
                     hspace=.01)
 
 plot_num = 1
+rng = np.random.RandomState(42)
 
 for i_dataset, X in enumerate(datasets):
     # Add outliers
-    rng = np.random.RandomState(42)
     X = np.concatenate([X, rng.uniform(low=-6, high=6,
                        size=(n_outliers, 2))], axis=0)
 
