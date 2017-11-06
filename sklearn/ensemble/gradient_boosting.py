[MRG] Fix MultinomialDeviance not using average logloss (#10055)

sklearn/ensemble/gradient_boosting.py
@@ -550,11 +550,12 @@ class MultinomialDeviance(ClassificationLossFunction):
             Y[:, k] = y == k
 
         if sample_weight is None:
-            return np.sum(-1 * (Y * pred).sum(axis=1) +
-                          logsumexp(pred, axis=1))
+            return np.mean(-1 * (Y * pred).sum(axis=1) +
+                           logsumexp(pred, axis=1))
         else:
-            return np.sum(-1 * sample_weight * (Y * pred).sum(axis=1) +
-                          logsumexp(pred, axis=1))
+            return (-1.0 / sample_weight.sum() *
+                    np.sum(sample_weight * (Y * pred).sum(axis=1) +
+                           logsumexp(pred, axis=1)))
 
     def negative_gradient(self, y, pred, k=0, **kwargs):
         """Compute negative gradient for the ``k``-th class. """