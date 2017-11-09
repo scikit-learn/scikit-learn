776bba1248ebfa98edcf5eebbb35b5e6fa79ecd1
Joan Massich <sik@visor.udg.edu>
2017-11-09 17:30:14 +0100

    Remove gaussian_process

--- sklearn/gaussian_process/__init__.py
+++ sklearn/gaussian_process/__init__.py
@@ -14,10 +14,9 @@ from .gpr import GaussianProcessRegressor
 from .gpc import GaussianProcessClassifier
 from . import kernels
 
-from .gaussian_process import GaussianProcess
 from . import correlation_models
 from . import regression_models
 
-__all__ = ['GaussianProcess', 'correlation_models', 'regression_models',
+__all__ = ['correlation_models', 'regression_models',
            'GaussianProcessRegressor', 'GaussianProcessClassifier',
            'kernels']
