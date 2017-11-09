2362011efcbf6651ed6ce4c3cea2cafab67857e2
Joan Massich <sik@visor.udg.edu>
2017-11-09 16:47:37 +0100

    Remove grid_search and learning_curve also deprecated

--- sklearn/__init__.py
+++ sklearn/__init__.py
@@ -137,13 +137,12 @@ else:
     __all__ = ['calibration', 'cluster', 'covariance', 'cross_decomposition',
                'datasets', 'decomposition', 'dummy', 'ensemble', 'exceptions',
                'externals', 'feature_extraction', 'feature_selection',
-               'gaussian_process', 'grid_search', 'isotonic',
-               'kernel_approximation', 'kernel_ridge', 'learning_curve',
-               'linear_model', 'manifold', 'metrics', 'mixture',
-               'model_selection', 'multiclass', 'multioutput', 'naive_bayes',
-               'neighbors', 'neural_network', 'pipeline', 'preprocessing',
-               'random_projection', 'semi_supervised', 'svm', 'tree',
-               'discriminant_analysis',
+               'gaussian_process', 'isotonic', 'kernel_approximation',
+               'kernel_ridge', 'linear_model', 'manifold', 'metrics',
+               'mixture', 'model_selection', 'multiclass', 'multioutput',
+               'naive_bayes', 'neighbors', 'neural_network', 'pipeline',
+               'preprocessing', 'random_projection', 'semi_supervised', 'svm',
+               'tree', 'discriminant_analysis',
                # Non-modules:
                'clone']
 
