2ffa7bdad5b18ea5d516e305fcef57738c215e4a
Joan Massich <sik@visor.udg.edu>
2017-11-10 16:09:28 +0100

    More in scoring

--- sklearn/metrics/tests/test_score_objects.py
+++ sklearn/metrics/tests/test_score_objects.py
@@ -499,25 +499,6 @@ def test_scorer_memmap_input():
         yield check_scorer_memmap, name
 
 
-def test_deprecated_names():
-    X, y = make_blobs(random_state=0, centers=2)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-    clf = LogisticRegression(random_state=0)
-    clf.fit(X_train, y_train)
-
-    for name in ('mean_absolute_error', 'mean_squared_error',
-                 'median_absolute_error', 'log_loss'):
-        warning_msg = "Scoring method %s was renamed to" % name
-        for scorer in (get_scorer(name), SCORERS[name]):
-            assert_warns_message(DeprecationWarning,
-                                 warning_msg,
-                                 scorer, clf, X, y)
-
-        assert_warns_message(DeprecationWarning,
-                             warning_msg,
-                             cross_val_score, clf, X, y, scoring=name)
-
-
 def test_scoring_is_not_metric():
     assert_raises_regexp(ValueError, 'make_scorer', check_scoring,
                          LogisticRegression(), f1_score)
