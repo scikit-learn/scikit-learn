4e4026e970a106fe3073d8a93903de083c53ce7b
William de Vazelhes <william.de-vazelhes@inria.fr>
2017-11-16 17:03:08 +0100

    add the same modif to model_selection

--- sklearn/cross_validation.py
+++ sklearn/cross_validation.py
@@ -527,7 +527,7 @@ class StratifiedKFold(_BaseKFold):
     For each class `c`, the first `n_samples_c % n_splits` folds have
     `n_samples_c // n_splits + 1` samples of class `c`, and other folds have
     `n_samples_c // n_splits` of them, where `n_samples_c` is the number of
-    samples of the dataset of class `c`. See also :class:`KFold`.
+    samples of the dataset of class `c`.
 
     See also
     --------
