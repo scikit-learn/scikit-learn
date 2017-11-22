9c352157dd0e90629bf2c0505ac38ac1e1108997
sergul <sergulaydore@gmail.com>
2017-11-22 01:03:11 -0500

    pep8 error fixed

--- sklearn/neural_network/tests/test_mlp.py
+++ sklearn/neural_network/tests/test_mlp.py
@@ -425,7 +425,7 @@ def test_params_errors():
     assert_raises(ValueError, clf(learning_rate='converge').fit, X, y)
     assert_raises(ValueError, clf(activation='cloak').fit, X, y)
 
-    assert_raises(ValueError, clf(activation=['identity','softmax'],
+    assert_raises(ValueError, clf(activation=['identity' , 'softmax'],
                                   hidden_layer_sizes=10).fit, X, y)
 
 def test_predict_proba_binary():
