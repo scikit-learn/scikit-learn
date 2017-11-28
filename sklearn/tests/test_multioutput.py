062ed0426c39a34c86066ebf2ebff5baddc6cbb9
kumarashutosh.ee@gmail.com <kumarashutosh.ee@gmail.com>
2017-11-28 22:34:23 +0530

    tests modified

--- sklearn/tests/test_multioutput.py
+++ sklearn/tests/test_multioutput.py
@@ -32,6 +32,7 @@ from sklearn.multioutput import MultiOutputClassifier
 from sklearn.multioutput import MultiOutputRegressor
 from sklearn.svm import LinearSVC
 from sklearn.base import ClassifierMixin
+from skleann.base import RegressorMixin
 from sklearn.utils import shuffle
 
 
@@ -368,25 +369,6 @@ def generate_multilabel_dataset_with_correlations():
     return X, Y_multi
 
 
-def test_classifier_chain_fit_and_predict_with_logistic_regression():
-    # Fit classifier chain and verify predict performance
-    X, Y = generate_multilabel_dataset_with_correlations()
-    classifier_chain = ClassifierChain(LogisticRegression())
-    classifier_chain.fit(X, Y)
-
-    Y_pred = classifier_chain.predict(X)
-    assert_equal(Y_pred.shape, Y.shape)
-
-    Y_prob = classifier_chain.predict_proba(X)
-    Y_binary = (Y_prob >= .5)
-    assert_array_equal(Y_binary, Y_pred)
-
-    assert_equal([c.coef_.size for c in classifier_chain.estimators_],
-                 list(range(X.shape[1], X.shape[1] + Y.shape[1])))
-
-    assert isinstance(classifier_chain, ClassifierMixin)
-
-
 def test_classifier_chain_fit_and_predict_with_linear_svc():
     # Fit classifier chain and verify predict performance using LinearSVC
     X, Y = generate_multilabel_dataset_with_correlations()
@@ -419,40 +401,6 @@ def test_classifier_chain_fit_and_predict_with_sparse_data():
     assert_array_equal(Y_pred_sparse, Y_pred_dense)
 
 
-def test_classifier_chain_fit_and_predict_with_sparse_data_and_cv():
-    # Fit classifier chain with sparse data cross_val_predict
-    X, Y = generate_multilabel_dataset_with_correlations()
-    X_sparse = sp.csr_matrix(X)
-    classifier_chain = ClassifierChain(LogisticRegression(), cv=3)
-    classifier_chain.fit(X_sparse, Y)
-    Y_pred = classifier_chain.predict(X_sparse)
-    assert_equal(Y_pred.shape, Y.shape)
-
-
-def test_classifier_chain_random_order():
-    # Fit classifier chain with random order
-    X, Y = generate_multilabel_dataset_with_correlations()
-    classifier_chain_random = ClassifierChain(LogisticRegression(),
-                                              order='random',
-                                              random_state=42)
-    classifier_chain_random.fit(X, Y)
-
-    assert_not_equal(list(classifier_chain_random.order), list(range(4)))
-    assert_equal(len(classifier_chain_random.order_), 4)
-    assert_equal(len(set(classifier_chain_random.order_)), 4)
-
-    classifier_chain_fixed = \
-        ClassifierChain(LogisticRegression(),
-                        order=classifier_chain_random.order_)
-    classifier_chain_fixed.fit(X, Y)
-
-    # Randomly ordered chain should behave identically to a fixed order chain
-    # with the same order.
-    for est1, est2 in zip(classifier_chain_random.estimators_,
-                          classifier_chain_fixed.estimators_):
-        assert_array_almost_equal(est1.coef_, est2.coef_)
-
-
 def test_classifier_chain_vs_independent_models():
     # Verify that an ensemble of classifier chains (each of length
     # N) can achieve a higher Jaccard similarity score than N independent
@@ -475,51 +423,62 @@ def test_classifier_chain_vs_independent_models():
                    jaccard_similarity_score(Y_test, Y_pred_ovr))
 
 
-def test_regressor_chain_fit_and_predict_with_linear_regression():
-    # Fit regressor chain and verify coefficients are of correct size
+def test_base_chain_fit_and_predict():
+    # Fit base chain and verify predict performance
     X, Y = generate_multilabel_dataset_with_correlations()
-    regressor_chain = RegressorChain(LinearRegression())
-    regressor_chain.fit(X, Y)
+    chains = [ClassifierChain(LogisticRegression()),
+             RegressorChain(LinearRegression())]
+    [chain.fit(X, Y) for chain in chains]
 
-    Y_pred = regressor_chain.predict(X)
-    assert_equal(Y_pred.shape, Y.shape)
+    for i in range(0,2):
+        Y_pred = chains[i].predict(X)
+        assert_equal(Y_pred.shape, Y.shape)
 
-    assert_equal([c.coef_.size for c in regressor_chain.estimators_],
+        assert_equal([c.coef_.size for c in chains[i].estimators_],
                  list(range(X.shape[1], X.shape[1] + Y.shape[1])))
 
+    Y_prob = chains[0].predict_proba(X)
+    Y_binary = (Y_prob >= .5)
+    assert_array_equal(Y_binary, Y_pred)
 
-def test_regressor_chain_fit_and_predict_with_sparse_data_and_cv():
-    # Fit regressor chain with sparse data cross_val_predict
+    assert isinstance(chains[0], ClassifierMixin)
+    assert isinstance(chains[1], RegressorMixin)
+
+
+def test_base_chain_fit_and_predict_with_sparse_data_and_cv():
+    # Fit base chain with sparse data cross_val_predict
     X, Y = generate_multilabel_dataset_with_correlations()
     X_sparse = sp.csr_matrix(X)
-    regressor_chain = RegressorChain(LinearRegression(), cv=3)
-    regressor_chain.fit(X_sparse, Y)
-    Y_pred = regressor_chain.predict(X_sparse)
-    assert_equal(Y_pred.shape, Y.shape)
+    base_chain = [ClassifierChain(LogisticRegression(), cv=3),
+                  RegressorChain(LinearRegression(), cv=3)]
+    [chain.fit(X_sparse, Y) for chain in base_chain]
+    for i in range(0,2):
+        Y_pred = base_chain[i].predict(X_sparse)
+        assert_equal(Y_pred.shape, Y.shape)
 
 
-def test_regressor_chain_random_order():
-    # Fit regressor chain with random order
+def test_base_chain_random_order():
+    # Fit base chain with random order
     X, Y = generate_multilabel_dataset_with_correlations()
-    regressor_chain_random = RegressorChain(LinearRegression(),
-                                            order='random',
-                                            random_state=42)
-    regressor_chain_random.fit(X, Y)
-
-    assert_not_equal(list(regressor_chain_random.order), list(range(4)))
-    assert_equal(len(regressor_chain_random.order_), 4)
-    assert_equal(len(set(regressor_chain_random.order_)), 4)
-
-    regressor_chain_fixed = \
-        RegressorChain(LinearRegression(),
-                       order=regressor_chain_random.order_)
-    regressor_chain_fixed.fit(X, Y)
-
-    # Randomly ordered chain should behave identically to a fixed order chain
-    # with the same order.
-    for est1, est2 in zip(regressor_chain_random.estimators_,
-                          regressor_chain_fixed.estimators_):
-        assert_array_almost_equal(est1.coef_, est2.coef_)
+    base_chain_random = [ClassifierChain(LogisticRegression(),
+                                         order='random', random_state=42),
+                         RegressorChain(LinearRegression(),
+                                        order='random', random_state=42)]
+    [chain_random.fit(X, Y) for chain_random in base_chain_random]
+    base_chain_fixed = [ClassifierChain(LogisticRegression(),
+                                        order=base_chain_random[0].order_),
+                        RegressorChain(LinearRegression(),
+                                        order=base_chain_random[1].order_)]
+    [chain_fixed.fit(X, Y) for chain_fixed in base_chain_fixed]
+    for i in range(0,2):
+        assert_not_equal(list(base_chain_random[i].order), list(range(4)))
+        assert_equal(len(base_chain_random[i].order_), 4)
+        assert_equal(len(set(base_chain_random[i].order_)), 4)
+        # Randomly ordered chain should behave identically to a fixed order chain
+        # with the same order.
+        for est1, est2 in zip(base_chain_random[i].estimators_,
+                              base_chain_fixed[i].estimators_):
+            assert_array_almost_equal(est1.coef_, est2.coef_)
 
 
 def test_base_chain_crossval_fit_and_predict():
