fb043778424bba9c0ed9b3431251b8925a0e4ef5
fabsig <fabio.sigrist@hslu.ch>
2017-11-30 14:31:45 +0100

    Tests added

--- sklearn/ensemble/tests/test_gradient_boosting.py
+++ sklearn/ensemble/tests/test_gradient_boosting.py
@@ -146,6 +146,15 @@ def test_classifier_parameter_checks():
                          GradientBoostingClassifier(presort='invalid')
                          .fit, X, y)
 
+    # Tobit model needs positive variance parameter
+    assert_raises(ValueError,
+                  GradientBoostingRegressor(loss='tobit', sigma=-1.).fit, X, y)
+
+    # Tobit: upper limit needs to be larger than lower limit
+    assert_raises(ValueError,
+                  GradientBoostingRegressor(loss='tobit', yl=1.,
+                                            yu=0.).fit, X, y)
+
 
 def test_regressor_parameter_checks():
     # Check input parameter validation for GradientBoostingRegressor
@@ -573,6 +582,25 @@ def test_quantile_loss():
     clf_lad.fit(boston.data, boston.target)
     y_lad = clf_lad.predict(boston.data)
     assert_array_almost_equal(y_quantile, y_lad, decimal=4)
+
+
+def test_tobit_loss():
+    # Check if results for 'tobit' loss with no censoring
+    # equal results for 'ls' loss.
+    # Use boston data with yl=0 and yu=100.
+    clf_tobit = GradientBoostingRegressor(n_estimators=100, loss='tobit',
+                                          max_depth=4, yl=0., yu=100.,
+                                          random_state=7)
+
+    clf_tobit.fit(boston.data, boston.target)
+    y_tobit = clf_tobit.predict(boston.data)
+
+    clf_ls = GradientBoostingRegressor(n_estimators=100, loss='ls',
+                                       max_depth=4, random_state=7)
+
+    clf_ls.fit(boston.data, boston.target)
+    y_ls = clf_ls.predict(boston.data)
+    assert_array_almost_equal(y_tobit, y_ls, decimal=4)
 
 
 def test_symbol_labels():
