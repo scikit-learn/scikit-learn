95ded120060b74e5b247d35a4c1219e67c14e3b5
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2017-12-03 23:39:34 +0530

    better error message

--- sklearn/ensemble/gradient_boosting.py
+++ sklearn/ensemble/gradient_boosting.py
@@ -997,7 +997,8 @@ class BaseGradientBoosting(six.with_metaclass(ABCMeta, BaseEnsemble)):
             sample_weight = column_or_1d(sample_weight, warn=True)
 
         if np.count_nonzero(np.bincount(y, weights=sample_weight)) < 2:
-            raise ValueError("y should contain atleast 2 classes")
+            raise ValueError("y should contain 2 classes after sample_weight "
+                             "trims samples with zero weights.")
 
         check_consistent_length(X, y, sample_weight)
 
