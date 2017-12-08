75e54ddcf1c1b0ddc8f341f09a1c3e67d36a63fa
kumarashutosh.ee@gmail.com <kumarashutosh.ee@gmail.com>
2017-12-08 19:07:26 +0530

    META_ESTIMATORS removed

--- sklearn/utils/testing.py
+++ sklearn/utils/testing.py
@@ -512,7 +512,7 @@ META_ESTIMATORS = ["OneVsOneClassifier", "MultiOutputEstimator",
                    "MultiOutputRegressor", "MultiOutputClassifier",
                    "OutputCodeClassifier", "OneVsRestClassifier",
                    "RFE", "RFECV", "BaseEnsemble", "ClassifierChain",
-                   "RegressorChain", "_BaseChain"]
+                   "RegressorChain"]
 # estimators that there is no way to default-construct sensibly
 OTHER = ["Pipeline", "FeatureUnion", "GridSearchCV", "RandomizedSearchCV",
          "SelectFromModel"]
