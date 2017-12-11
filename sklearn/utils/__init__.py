a2b2b0a144e9d61ad7e975b9fd0fc3c759d50208
Joel Nothman <joel.nothman@gmail.com>
2017-12-11 23:39:09 +1100

    Remove unused import

--- sklearn/utils/__init__.py
+++ sklearn/utils/__init__.py
@@ -17,7 +17,6 @@ from .class_weight import compute_class_weight, compute_sample_weight
 from ..externals.joblib import cpu_count
 from ..exceptions import DataConversionWarning
 from .deprecation import deprecated
-from .validation import _num_samples
 from .. import get_config
 
 __all__ = ["murmurhash3_32", "as_float_array",
