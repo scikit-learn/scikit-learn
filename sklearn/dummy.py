de1ab574e0d1a2a11e0ed1873702b315790f3e53
Vrishank Bhardwaj <vrishank1997@gmail.com>
2017-12-12 23:14:13 +0530

    Add params to check_array in DummyClassifier

--- sklearn/dummy.py
+++ sklearn/dummy.py
@@ -107,7 +107,8 @@ class DummyClassifier(BaseEstimator, ClassifierMixin):
             Returns self.
         """
         X = check_array(X, accept_sparse=['csr', 'csc', 'coo'],
-                        force_all_finite=False)
+                        force_all_finite=False, dtype=None,
+                        ensure_min_features=0)
 
         if self.strategy not in ("most_frequent", "stratified", "uniform",
                                  "constant", "prior"):
@@ -178,7 +179,8 @@ class DummyClassifier(BaseEstimator, ClassifierMixin):
         check_is_fitted(self, 'classes_')
 
         X = check_array(X, accept_sparse=['csr', 'csc', 'coo'],
-                        force_all_finite=False)
+                        force_all_finite=False, dtype=None,
+                        ensure_min_features=0)
         # numpy random_state expects Python int and not long as size argument
         # under Windows
         n_samples = int(X.shape[0])
