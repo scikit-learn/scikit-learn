814c4e6bb58e50d22920aa07d0b05f6ba3de8258
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2017-12-16 18:25:24 +0530

    revert change to change self.gamma

--- sklearn/svm/base.py
+++ sklearn/svm/base.py
@@ -169,21 +169,19 @@ class BaseLibSVM(six.with_metaclass(ABCMeta, BaseEstimator)):
                              "boolean masks (use `indices=True` in CV)."
                              % (sample_weight.shape, X.shape))
 
-        if self.gamma == 'auto_deprecated':
-           warnings.warn("The default gamma parameter value 'auto', "
-                         "calculated as 1 / n_features, is deprecated in "
-                         "version 0.20 and will be replaced by 'scale', "
-                         "calculated as 1 / (n_features * X.std()) in "
-                         "version 0.22.", DeprecationWarning)
-           self.gamma = 'auto'
-
         if self.gamma == 'scale':
             if isinstance(X, sp.spmatrix):
                 X_std = np.sqrt(X.power(2).mean() - (X.mean())**2)
             else:
                 X_std = X.std()
             self._gamma = 1.0 / (X.shape[1] * X_std)
-        elif self.gamma == 'auto':
+        elif self.gamma == 'auto' or self.gamma == 'auto_deprecated':
+            if self.gamma == 'auto_deprecated':
+                warnings.warn("The default gamma parameter value 'auto', "
+                              "calculated as 1 / n_features, is deprecated in "
+                              "version 0.20 and will be replaced by 'scale', "
+                              "calculated as 1 / (n_features * X.std()) in "
+                              "version 0.22.", DeprecationWarning)
             self._gamma = 1.0 / X.shape[1]
         else:
             self._gamma = self.gamma
