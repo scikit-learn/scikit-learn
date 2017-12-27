989d003ac1c794442f3d6f431c7c7a9c053e3848
John Silva <jomicobasi@gmail.com>
2017-12-27 18:02:54 +0000

    Fix (attempt) LDA

--- sklearn/discriminant_analysis.py
+++ sklearn/discriminant_analysis.py
@@ -444,9 +444,9 @@ class LinearDiscriminantAnalysis(BaseEstimator, LinearClassifierMixin,
 
         # Get the maximum number of components
         if self.n_components is None:
-            self._max_components = len(self.classes_) - 1
+            self._max_components = X.shape[1]
         else:
-            self._max_components = min(len(self.classes_) - 1,
+            self._max_components = min(X.shape[1],
                                        self.n_components)
 
         if self.solver == 'svd':
