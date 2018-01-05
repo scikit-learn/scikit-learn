4b48f6f9d38feeadc25fe7ac19afbafefb904e2d
William de Vazelhes <william.de-vazelhes@inria.fr>
2018-01-05 15:32:33 +0100

    correct flake8 error

--- sklearn/cross_validation.py
+++ sklearn/cross_validation.py
@@ -525,9 +525,9 @@ class StratifiedKFold(_BaseKFold):
     Notes
     -----
     For each class ``c``, the first ``n_samples_c % n_splits`` folds have
-    ``n_samples_c // n_splits + 1`` samples of class ``c``, and other folds have
-    ``n_samples_c // n_splits` of them, where ``n_samples_c`` is the number of
-    samples of the dataset of class ``c``.
+    ``n_samples_c // n_splits + 1`` samples of class ``c``, and other folds
+    have ``n_samples_c // n_splits` of them, where ``n_samples_c`` is the
+    number of samples of the dataset of class ``c``.
 
     See also
     --------
