db127bd9693068a5b187d49d08738e690c5c7d98
Loïc Estève <loic.esteve@ymail.com>
2018-01-08 21:36:28 +0100

    [MRG+1] Support minimal dependencies in examples (#10351)
    * Support minimal dependencies in examples
    * bins="auto" only supported for numpy >= 1.11
    * viridis is default cmap in matplotlib 2
    * Use signature instead of viridis cmap

--- examples/preprocessing/plot_transformed_target.py
+++ examples/preprocessing/plot_transformed_target.py
@@ -63,13 +63,13 @@ y_trans = log_transform(y)
 
 f, (ax0, ax1) = plt.subplots(1, 2)
 
-ax0.hist(y, bins='auto', normed=True)
+ax0.hist(y, bins=100, normed=True)
 ax0.set_xlim([0, 2000])
 ax0.set_ylabel('Probability')
 ax0.set_xlabel('Target')
 ax0.set_title('Target distribution')
 
-ax1.hist(y_trans, bins='auto', normed=True)
+ax1.hist(y_trans, bins=100, normed=True)
 ax1.set_ylabel('Probability')
 ax1.set_xlabel('Target')
 ax1.set_title('Transformed target distribution')
@@ -148,12 +148,12 @@ y_trans = quantile_transform(dataset.data[:, target],
 
 f, (ax0, ax1) = plt.subplots(1, 2)
 
-ax0.hist(y, bins='auto', normed=True)
+ax0.hist(y, bins=100, normed=True)
 ax0.set_ylabel('Probability')
 ax0.set_xlabel('Target')
 ax0.set_title('Target distribution')
 
-ax1.hist(y_trans, bins='auto', normed=True)
+ax1.hist(y_trans, bins=100, normed=True)
 ax1.set_ylabel('Probability')
 ax1.set_xlabel('Target')
 ax1.set_title('Transformed target distribution')
