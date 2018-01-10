1119216375745f0704d4af4353a5bc4bd3fd5332
johayon <johayon.math@gmail.com>
2018-01-11 00:30:34 +0100

    add a small function to compute on whole set and batches + comment change

--- sklearn/utils/estimator_checks.py
+++ sklearn/utils/estimator_checks.py
@@ -644,10 +644,24 @@ def check_fit2d_predict1d(name, estimator_orig):
                                  getattr(estimator, method), X[0])
 
 
+def _apply_func(func, X):
+    # apply function on the whole set and on mini batches
+    result_full = func(X)
+    n_features = X.shape[1]
+    result_by_batch = [func(batch.reshape(1, n_features))
+                       for batch in X]
+    # func can output tuple (e.g. score_samples)
+    if type(result_full) == tuple:
+        result_full = result_full[0]
+        result_by_batch = list(map(lambda x: x[0], result_by_batch))
+
+    return np.ravel(result_full), np.ravel(result_by_batch)
+
+
 @ignore_warnings(category=(DeprecationWarning, FutureWarning))
 def check_methods_subset_invariance(name, estimator_orig):
     # check that method gives invariant results if applied
-    # one by one or on all elements together.
+    # on mini bathes or the whole set
     rnd = np.random.RandomState(0)
     X = 3 * rnd.uniform(size=(20, 3))
     X = pairwise_estimator_convert_X(X, estimator_orig)
@@ -665,24 +679,20 @@ def check_methods_subset_invariance(name, estimator_orig):
 
     for method in ["predict", "transform", "decision_function",
                    "score_samples", "predict_proba"]:
+
+        msg = ("{method} of {name} is not invariant when applied "
+               "to a subset.").format(method=method, name=name)
+        # TODO remove cases when corrected
+        if (name, method) in [('SVC', 'decision_function'),
+                              ('SparsePCA', 'transform'),
+                              ('MiniBatchSparsePCA', 'transform'),
+                              ('BernoulliRBM', 'score_samples')]:
+            raise SkipTest(msg)
+
         if hasattr(estimator, method):
-            msg = ("{method} of {name} is not invariant when applied "
-                   "to a subset.").format(method=method, name=name)
-            func = getattr(estimator, method)
-            res_all = func(X)
-            res_one = [func(X[i].reshape(1, X.shape[1]))
-                       for i in range(X.shape[0])]
-            # func can output tuple (e.g. score_samples)
-            if type(res_all) == tuple:
-                res_all = res_all[0]
-                res_one = list(map(lambda x: x[0], res_one))
-            # TODO remove cases when corrected
-            if (name, method) in [('SVC', 'decision_function'),
-                                  ('SparsePCA', 'transform'),
-                                  ('MiniBatchSparsePCA', 'transform'),
-                                  ('BernoulliRBM', 'score_samples')]:
-                raise SkipTest(msg)
-            assert_allclose(np.ravel(res_all), np.ravel(res_one),
+            result_full, result_by_batch = _apply_func(getattr(estimator,
+                                                               method), X)
+            assert_allclose(result_full, result_by_batch,
                             atol=1e-7, err_msg=msg)
 
 
