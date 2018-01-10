e2c7e7a3d40434ddb9f16e9a7b4e7fbccc4ae112
Luigi De Bianchi <luigi.debianchi@gmail.com>
2018-01-10 01:32:16 +0100

    Added rectangular smoothing or Unweighted Sliding Average Smooth.

--- sklearn/preprocessing/smooth.py
+++ sklearn/preprocessing/smooth.py
@@ -19,15 +19,20 @@ class Smoother(BaseEstimator, TransformerMixin):
 
     _modes = ["rect"]
 
-    def __init__(self, mode="rect"):
+    def __init__(self, mode="rect", size=3):
         if mode not in self._modes:
-            raise ValueError("Given mode is not supported")
+            raise ValueError("Given mode is not supported.")
+
+        if size%2 == 0:
+            raise ValueError("Size must be odd.")
+
         self._mode = mode
+        self._size = size
 
     def _reset(self):
-        #TODO: add here all variables of the class
         if hasattr(self, '_mode'):
             del self._mode
+            del self._size
 
     def fit(self, X, y=None):
         self._reset()
@@ -50,6 +55,23 @@ class Smoother(BaseEstimator, TransformerMixin):
 
         X = check_array(X, copy=self.copy, dtype=FLOAT_DTYPES)
 
+        wiskersLen = int((self._size - 1) / 2)
+        sampleSize = X.shape[0]
 
+        if self._mode == "rect":
+            for row in range(wiskersLen, sampleSize-wiskersLen):
+                #print("row: " + str(row) + " \t ")
+                #print(X[row, :])
+                average = self._sizeSum(X, row - wiskersLen, row + wiskersLen + 1) / self._size
+                # print("from line " + str(row-wiskersLen) + "to line" + str(row+wiskersLen+1))
+                # print(average)
+                X[row, :] = average
 
-        return X
+                return X
+
+    def _sizeSum(self, X, start, end):
+        featureNumber = X.shape[1]
+        result = np.zeros(featureNumber)
+        for row in X[start:end, :]:
+            result += row
+        return result
