77b302b284350cac7811c362be49052d27bf6783
Hanmin Qin <qinhanmin2005@sina.com>
2018-01-11 21:26:26 +0800

    update copyright year 2018

--- doc/conf.py
+++ doc/conf.py
@@ -75,7 +75,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u('scikit-learn')
-copyright = u('2007 - 2017, scikit-learn developers (BSD License)')
+copyright = u('2007 - 2018, scikit-learn developers (BSD License)')
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
