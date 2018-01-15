8bb0ecf8a74a3d4682580ea057dcc49e23f1bed1
Webber_Windows <webber26232@gmail.com>
2018-01-14 21:52:52 -0600

    resolve regression conflict

--- sklearn/neighbors/regression.py
+++ sklearn/neighbors/regression.py
@@ -257,16 +257,14 @@ class RadiusNeighborsRegressor(NeighborsBase, RadiusNeighborsMixin,
     """
 
     def __init__(self, radius=1.0, weights='uniform',
-                 algorithm='auto', leaf_size=30, p=2, metric='minkowski',
-                 outlier_value=None, metric_params=None, **kwargs):
-        self._init_params(radius=radius,
-                          algorithm=algorithm,
-                          leaf_size=leaf_size,
-                          p=p, metric=metric, metric_params=metric_params,
-                          **kwargs)
+                 algorithm='auto', leaf_size=30,
+                 p=2, metric='minkowski', metric_params=None, **kwargs):
+        super(RadiusNeighborsRegressor, self).__init__(
+              radius=radius,
+              algorithm=algorithm,
+              leaf_size=leaf_size,
+              p=p, metric=metric, metric_params=metric_params, **kwargs)
         self.weights = _check_weights(weights)
-        self.outlier_value = _check_outlier_handler(outlier_value,
-                                                    kind='value')
 
     def predict(self, X):
         """Predict the target for the provided data
