69d2676b3b75a2c644f0df98b72000b16cccea20
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-01-16 13:28:17 +0100

    iter

--- sklearn/utils/validation.py
+++ sklearn/utils/validation.py
@@ -39,14 +39,14 @@ def _assert_all_finite(X, allow_nan=False):
     # First try an O(n) time, O(1) space solution for the common case that
     # everything is finite; fall back to O(n) space np.isfinite to prevent
     # false positives from overflow in sum method.
-    is_float = X.dtype.char in np.typecodes['AllFloat']
+    is_float = X.dtype.kind in 'fc'
     if is_float and np.isfinite(X.sum()):
         pass
     elif is_float:
         msg_err = "Input contains {} or a value too large for {!r}."
-        cond_err, type_err = ((np.isinf(X).any(), 'infinity') if allow_nan
-                              else (not np.isfinite(X).all(), 'NaN, infinity'))
-        if cond_err:
+        if (allow_nan and np.isinf(X).any() or
+                not allow_nan and not np.isfinite(X).all()):
+            type_err = 'infinity' if allow_nan else 'NaN, infinity'
             raise ValueError(msg_err.format(type_err, X.dtype))
 
 
