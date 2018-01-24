710d1d9a660f4181ff1f53fd90ba8a80efb14587
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2018-01-24 15:02:47 +0530

    add comment about test case

--- sklearn/svm/tests/test_svm.py
+++ sklearn/svm/tests/test_svm.py
@@ -997,5 +997,7 @@ def test_gamma_scale():
     assert_no_warnings(clf.fit, X, y)
     assert_equal(clf._gamma, 2.)
 
+    # X_std ~= 1 shouldn't raise warning, for when
+    # gamma is not explicitly set
     X, y = [[1, 2], [3, 2 * np.sqrt(6) / 3 + 2]], [0, 1]
     assert_no_warnings(clf.fit, X, y)
