5c825c952cd9b9710be1983ebfd7bf46e26a25d4
Tom Dupré la Tour <tom.dupre-la-tour@m4x.org>
2018-01-29 17:35:25 +0100

    just move classes to graph.py

--- sklearn/neighbors/__init__.py
+++ sklearn/neighbors/__init__.py
@@ -7,8 +7,8 @@ from .ball_tree import BallTree
 from .kd_tree import KDTree
 from .dist_metrics import DistanceMetric
 from .graph import kneighbors_graph, radius_neighbors_graph
+from .graph import KNeighborsTransformer, RadiusNeighborsTransformer
 from .unsupervised import NearestNeighbors
-from .unsupervised import KNeighborsTransformer, RadiusNeighborsTransformer
 from .classification import KNeighborsClassifier, RadiusNeighborsClassifier
 from .regression import KNeighborsRegressor, RadiusNeighborsRegressor
 from .nearest_centroid import NearestCentroid
