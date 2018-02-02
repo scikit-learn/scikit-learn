fixed colours and spurious factor of /2

examples/linear_model/plot_sgd_penalties.py
@@ -15,9 +15,9 @@ print(__doc__)
 import numpy as np
 import matplotlib.pyplot as plt
 
-l1_color = 'C0'
-l2_color = 'C1'
-elastic_net_color = 'C2'
+l1_color = "navy"
+l2_color = "c"
+elastic_net_color = "darkorange"
 
 line = np.linspace(-1.5, 1.5, 1001)
 xx, yy = np.meshgrid(line, line)
@@ -25,15 +25,15 @@ xx, yy = np.meshgrid(line, line)
 l2 = xx ** 2 + yy ** 2
 l1 = np.abs(xx) + np.abs(yy)
 rho = 0.5
-elastic_net = rho * l1 + (1-rho)/2 * l2
+elastic_net = rho * l1 + (1 - rho) * l2
 
 plt.figure(figsize=(10, 10), dpi=100)
 ax = plt.gca()
 
-elastic_net_contour = plt.contour(xx, yy, elastic_net, levels="1",
+elastic_net_contour = plt.contour(xx, yy, elastic_net, levels=[1],
                                   colors=elastic_net_color)
-l2_contour = plt.contour(xx, yy, l2, levels="1", colors=l2_color)
-l1_contour = plt.contour(xx, yy, l1, levels="1", colors=l1_color)
+l2_contour = plt.contour(xx, yy, l2, levels=[1], colors=l2_color)
+l1_contour = plt.contour(xx, yy, l1, levels=[1], colors=l1_color)
 ax.set_aspect("equal")
 ax.spines['left'].set_position('center')
 ax.spines['right'].set_color('none')