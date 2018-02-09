6ceea69b845049bf30a0be24ac8f2130f3dc7857
Maskani Filali Mohamed <maskani.mohamed@gmail.com>
2018-02-09 10:46:13 -0500

    Fix docstring

--- sklearn/datasets/samples_generator.py
+++ sklearn/datasets/samples_generator.py
@@ -705,10 +705,10 @@ def make_blobs(n_samples=100, n_features=2, centers=None, cluster_std=1.0,
 
     Parameters
     ----------
-    n_samples : int or list or array, optional (default=100)
+    n_samples : int or array-like, optional (default=100)
         If int, it is the the total number of points equally divided among
         clusters.
-        If list or array, each element of the list indicates
+        If list, tuple or array, each element of the iterable indicates
         the number of samples per cluster.
 
     n_features : int, optional (default=2)
@@ -718,8 +718,8 @@ def make_blobs(n_samples=100, n_features=2, centers=None, cluster_std=1.0,
         (default=None)
         The number of centers to generate, or the fixed center locations.
         If n_samples is an int and centers is None, 3 centers are generated.
-        If n_samples is a list, centers must be either None or an array of
-        length equal to the length of n_samples.
+        If n_samples is a list, tuple or array, centers must be
+        either None or an array of length equal to the length of n_samples.
 
     cluster_std : float or sequence of floats, optional (default=1.0)
         The standard deviation of the clusters.
