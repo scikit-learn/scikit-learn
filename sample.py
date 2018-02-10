9f70391e5493a00b3345884ba2b2e6beae236ed0
coderinuse <rkerimov@std.qu.edu.az>
2018-02-10 22:06:25 +0400

    issue

--- sample.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from sklearn import linear_model
-
-est_no_intercept = linear_model.Lasso(fit_intercept=False)
-est_no_intercept.fit([ [0] ], [1])
-#print(est_no_intercept.coef_.shape)
-assert est_no_intercept.coef_.shape  == (1,)
