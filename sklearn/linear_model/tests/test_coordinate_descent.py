f70c7a37397ee5d427296046e34706e22add33a1
coderinuse <rkerimov@std.qu.edu.az>
2018-02-10 17:29:45 +0400

    issue 10571

--- sklearn/linear_model/tests/test_coordinate_descent.py
+++ sklearn/linear_model/tests/test_coordinate_descent.py
@@ -318,6 +318,17 @@ def test_lasso_alpha_warning():
     clf = Lasso(alpha=0)
     assert_warns(UserWarning, clf.fit, X, Y)
 
+def test_lasso_fit_intercept():
+    X = [[-1], [0], [1]]
+    Y = [-1, 0, 1]
+
+    clf = Lasso(fit_intercept=False)
+    clf.fit(X, Y)
+    assert_equal(clf.coef_.shape, (1,))
+
+    clf2 = Lasso(fit_intercept=True)
+    clf2.fit(X, Y)
+    assert_equal(clf.coef_.shape, (1,))
 
 def test_lasso_positive_constraint():
     X = [[-1], [0], [1]]
