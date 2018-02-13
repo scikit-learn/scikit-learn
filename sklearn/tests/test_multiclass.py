1ae4121ec2f195394496b88d1936bc46b70cac07
EmreAtes <ates@bu.edu>
2018-02-13 12:05:59 -0500

    Style fix

--- sklearn/tests/test_multiclass.py
+++ sklearn/tests/test_multiclass.py
@@ -256,7 +256,7 @@ def test_ovr_binary():
         assert_equal(set(y_pred), set("eggs"))
         if hasattr(base_clf, 'decision_function'):
             dec = clf.decision_function(X)
-            assert_equal(dec.shape, (5,1))
+            assert_equal(dec.shape, (5, 1))
 
         if test_predict_proba:
             X_test = np.array([[0, 0, 4]])
