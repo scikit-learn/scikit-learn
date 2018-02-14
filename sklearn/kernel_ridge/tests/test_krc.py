1e83662ceb080ae6c9d9ca5f4d41f16d1f5213dd
cperales <sir.perales@gmail.com>
2018-02-14 15:49:35 +0100

    sparse removed

--- sklearn/kernel_ridge/tests/test_krc.py
+++ sklearn/kernel_ridge/tests/test_krc.py
@@ -9,7 +9,6 @@ License: BSD 3 clause
 import numpy as np
 from numpy.testing import assert_array_equal, assert_array_almost_equal
 from numpy.testing import assert_almost_equal
-from scipy import sparse
 from sklearn import datasets, base
 from sklearn.utils import check_random_state
 from sklearn.utils.testing import assert_equal, assert_true
