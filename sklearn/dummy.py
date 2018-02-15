8666598ec0b67fca18c18204f3dec03d9ebafab1
Sergey Feldman <sergeyfeldman@gmail.com>
2018-02-15 06:39:41 -0800

    update dummy.py

--- sklearn/dummy.py
+++ sklearn/dummy.py
@@ -456,6 +456,7 @@ class DummyRegressor(BaseEstimator, RegressorMixin):
 
         return_std : boolean, optional
             Whether to return the standard deviation of posterior prediction.
+            All zeros in this case.
 
         Returns
         -------
@@ -468,8 +469,8 @@ class DummyRegressor(BaseEstimator, RegressorMixin):
         check_is_fitted(self, "constant_")
         n_samples = _num_samples(X)
 
-        y = np.ones((n_samples, 1)) * self.constant_
-        y_std = np.zeros((n_samples, 1))
+        y = np.ones((n_samples, self.n_outputs_)) * self.constant_
+        y_std = np.zeros((n_samples, self.n_outputs_))
 
         if self.n_outputs_ == 1 and not self.output_2d_:
             y = np.ravel(y)
