0804952aacb9d4c4ce2689380d982871e0ed1458
Webber_Windows <webber26232@gmail.com>
2018-02-25 13:53:03 -0600

    fix inheritance

--- sklearn/neighbors/classification.py
+++ sklearn/neighbors/classification.py
@@ -347,7 +347,7 @@ class RadiusNeighborsClassifier(NeighborsBase, RadiusNeighborsMixin,
 
         """
         self.outlier_label = _check_outlier_label(self.outlier_label)
-        super(SupervisedIntegerMixin, self).fit(X, y)
+        RadiusNeighborsClassifier.fit(self, X, y)
 
 
     def predict(self, X):
