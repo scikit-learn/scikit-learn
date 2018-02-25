9bc3cae689e3851889b5b2d3968693f55ad0f75a
Mohamed Ali Jamaoui <m.ali.jamaoui@gmail.com>
2018-02-25 23:29:20 +0100

    Add whitespace to enhance readability

--- sklearn/gaussian_process/gpc.py
+++ sklearn/gaussian_process/gpc.py
@@ -100,6 +100,7 @@ class _BinaryGaussianProcessClassifierLaplace(BaseEstimator):
         initialization for the next call of _posterior_mode(). This can speed
         up convergence when _posterior_mode is called several times on similar
         problems as in hyperparameter optimization. 
+        
         See :term:`the Glossary <warm_start>`.
 
     copy_X_train : bool, optional (default: True)
