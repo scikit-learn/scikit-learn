f90ba0388a4d47294d10a51e26f96ec4af709ca2
Mohamed Ali Jamaoui <m.ali.jamaoui@gmail.com>
2018-02-26 08:55:02 +0100

    fix pep8: W291 trailing whitespace

--- sklearn/linear_model/coordinate_descent.py
+++ sklearn/linear_model/coordinate_descent.py
@@ -579,7 +579,7 @@ class ElasticNet(LinearModel, RegressorMixin):
 
     warm_start : bool, optional
         When set to ``True``, reuse the solution of the previous call to fit as
-        initialization, otherwise, just erase the previous solution. 
+        initialization, otherwise, just erase the previous solution.
         See :term:`the Glossary <warm_start>`.
 
     positive : bool, optional
