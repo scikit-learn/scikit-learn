abf481a237a89bf2cf49c03a87a1e151fea15901
Mohamed Ali Jamaoui <m.ali.jamaoui@gmail.com>
2018-02-26 10:58:06 +0100

    Add warm_start glossary reference to the definition's paragraph

--- sklearn/mixture/bayesian_mixture.py
+++ sklearn/mixture/bayesian_mixture.py
@@ -173,7 +173,6 @@ class BayesianGaussianMixture(BaseMixture):
         If 'warm_start' is True, the solution of the last fitting is used as
         initialization for the next call of fit(). This can speed up
         convergence when fit is called several times on similar problems.
-
         See :term:`the Glossary <warm_start>`.
 
     verbose : int, default to 0.
