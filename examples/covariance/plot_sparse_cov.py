751a1ecfc8464386664decafcb89de92e45505d4
Arunava <learningdroidarunava@gmail.com>
2018-03-07 10:33:40 +0530

    Updated plot_sparse_cov.py

--- examples/covariance/plot_sparse_cov.py
+++ examples/covariance/plot_sparse_cov.py
@@ -121,7 +121,8 @@ for i, (name, this_prec) in enumerate(precs):
     plt.xticks(())
     plt.yticks(())
     plt.title('%s precision' % name)
-    ax.set_facecolor('.7') if hasattr(ax, 'set_facecolor') else ax.set_axis_bgcolor('.7')
+    if hasattr(ax, 'set_facecolor'): ax.set_facecolor('.7')
+    else: ax.set_axis_bgcolor('.7')
 
 # plot the model selection metric
 plt.figure(figsize=(4, 3))
