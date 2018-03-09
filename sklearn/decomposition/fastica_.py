229a3ce0d40fdf872b02d3d0b487ab69a9bbc7cb
Pierre Ablin <pierreablin@gmail.com>
2018-03-09 14:50:36 +0100

    fix error

--- sklearn/decomposition/fastica_.py
+++ sklearn/decomposition/fastica_.py
@@ -456,7 +456,8 @@ class FastICA(BaseEstimator, TransformerMixin):
                  w_init=None, random_state=None):
         super(FastICA, self).__init__()
         if max_iter < 1:
-            raise ValueError("max_iter should be greater than 1")
+            raise ValueError("max_iter should be greater than 1, got "
+                             "(max_iter=%d)" % max_iter)
         self.n_components = n_components
         self.algorithm = algorithm
         self.whiten = whiten
