5f73583c99c773fbc204b63afe2acce793fe58fa
Chris Catalfo <foxnorth@gmail.com>
2018-03-15 16:31:12 -0400

    [MRG] Add return_X_y to more datasets - switch to straight asserts in test.

--- sklearn/datasets/tests/test_common.py
+++ sklearn/datasets/tests/test_common.py
@@ -1,10 +1,8 @@
 """Test loaders for common functionality.
 """
-from sklearn.utils.testing import assert_array_equal
-from sklearn.utils.testing import assert_true
 
 
 def check_return_X_y(bunch, X_y_tuple):
-    assert_true(isinstance(X_y_tuple, tuple))
-    assert_array_equal(X_y_tuple[0].shape, bunch.data.shape)
-    assert_array_equal(X_y_tuple[1].shape, bunch.target.shape)
+    assert(isinstance(X_y_tuple, tuple))
+    assert(X_y_tuple[0].shape == bunch.data.shape)
+    assert(X_y_tuple[1].shape == bunch.target.shape)
