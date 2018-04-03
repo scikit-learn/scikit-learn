04f8520f8ab58f7374dcda5b8f2340988eab0c25
Rodrigo Agundez <rragundez@gmail.com>
2018-04-03 20:09:50 +0200

    Update tests for unkown class in MultiLabelBinarizer
    Previously this tests will check for a KeyError. MultiLabelBinarizer
    now ignores unknown classes.

--- sklearn/preprocessing/tests/test_label.py
+++ sklearn/preprocessing/tests/test_label.py
@@ -307,10 +307,11 @@ def test_multilabel_binarizer_empty_sample():
 def test_multilabel_binarizer_unknown_class():
     mlb = MultiLabelBinarizer()
     y = [[1, 2]]
-    assert_raises(KeyError, mlb.fit(y).transform, [[0]])
+    Y = np.array([[0, 0]])
+    assert_array_equal(mlb.fit(y).transform([[0]]), Y)
 
     mlb = MultiLabelBinarizer(classes=[1, 2])
-    assert_raises(KeyError, mlb.fit_transform, [[0]])
+    assert_array_equal(mlb.fit(y).transform([[0]]), Y)
 
 
 def test_multilabel_binarizer_given_classes():
