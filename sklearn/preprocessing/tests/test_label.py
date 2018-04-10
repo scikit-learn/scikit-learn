b0afb72789f098c5b21b1ec9f683d5d9e2e3798c
Rodrigo Agundez <rragundez@gmail.com>
2018-04-10 09:34:34 +0200

    Remove unused import assert_warns

--- sklearn/preprocessing/tests/test_label.py
+++ sklearn/preprocessing/tests/test_label.py
@@ -14,7 +14,6 @@ from sklearn.utils.testing import assert_equal
 from sklearn.utils.testing import assert_true
 from sklearn.utils.testing import assert_raises
 from sklearn.utils.testing import assert_raise_message
-from sklearn.utils.testing import assert_warns
 from sklearn.utils.testing import assert_warns_message
 from sklearn.utils.testing import ignore_warnings
 
