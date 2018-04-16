2d528e0164a5ec8a719d723557fe7cb9b201266f
aishgrt1 <31906139+aishgrt1@users.noreply.github.com>
2018-04-16 11:04:20 -0400

    np.clip used

--- sklearn/calibration.py
+++ sklearn/calibration.py
@@ -388,7 +388,7 @@ class _CalibratedClassifier(object):
         # XXX : for some reason all probas can be 0
         proba[np.isnan(proba)] = 1. / n_classes
 
-        # Deal with cases where the predicted probability minimally exceeds 1.0
+        # Deal with cases where the predicted probability is out of [0,1] range 
         np.clip(proba,0.0, 1.0,proba)
 
         return proba
