25ed665fbc159a90621d58487d01bcecf46b6946
aishgrt1 <31906139+aishgrt1@users.noreply.github.com>
2018-04-15 20:46:30 -0400

    Update isotonic.py

--- sklearn/isotonic.py
+++ sklearn/isotonic.py
@@ -130,9 +130,9 @@ def isotonic_regression(y, sample_weight=None, y_min=None, y_max=None,
     if y_min is not None or y_max is not None:
         # Older versions of np.clip don't accept None as a bound, so use np.inf
         if y_min is None:
-            y_min = 0
+            y_min = - np.inf
         if y_max is None:
-            y_max = 1
+            y_max = np.inf
         np.clip(y, y_min, y_max, y)
     return y[order]
 
