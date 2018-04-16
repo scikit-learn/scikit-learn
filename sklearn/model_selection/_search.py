1a7c2c7348fb637fd8e3f5925ce413121ec521ff
Juliet Lawton <jclawton@0587367635.wireless.umich.net>
2018-04-15 22:18:00 -0400

    Fixed whitespace

--- sklearn/model_selection/_search.py
+++ sklearn/model_selection/_search.py
@@ -248,7 +248,7 @@ class ParameterSampler(object):
                 warnings.warn(
                     'The total space of parameters %d is smaller '
                     'than n_iter=%d. Running %d iterations. For exhaustive '
-                    'searches, use GridSearchCV.' 
+                    'searches, use GridSearchCV.'
                     % (grid_size, self.n_iter, grid_size), RuntimeWarning)
                 n_iter = grid_size
             for i in sample_without_replacement(grid_size, n_iter,
