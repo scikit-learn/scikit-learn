6be492420cd78bd427120b2afb5651332ec7bd1b
Nick Sorros <nsorros@gmail.com>
2018-04-17 09:57:59 +0100

    flake8 errors in tests

--- sklearn/tests/test_dummy.py
+++ sklearn/tests/test_dummy.py
@@ -82,6 +82,7 @@ def _check_equality_regressor(statistic, y_learn, y_pred_learn,
     assert_array_almost_equal(np.tile(statistic, (y_test.shape[0], 1)),
                               y_pred_test)
 
+
 def test_most_frequent_and_prior_strategy():
     X = [[0], [0], [0], [0]]  # ignored
     y = [1, 2, 1, 1]
@@ -108,10 +109,11 @@ def test_most_frequent_and_prior_strategy_with_2d_column_y():
     for strategy in ("most_frequent", "prior"):
         clf_1d = DummyClassifier(strategy=strategy, random_state=0)
         clf_2d = DummyClassifier(strategy=strategy, random_state=0)
-        
+
         clf_1d.fit(X, y_1d)
         clf_2d.fit(X, y_2d)
         assert_array_equal(clf_1d.predict(X), clf_2d.predict(X))
+
 
 def test_most_frequent_and_prior_strategy_multioutput():
     X = [[0], [0], [0], [0]]  # ignored
