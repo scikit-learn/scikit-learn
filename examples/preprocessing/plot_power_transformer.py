dd29ca6f42ac7d9b057b8d064ace7d52458acecc
Theodore Yoong <theodoreyoong@Theodores-MacBook-Pro.local>
2018-04-22 11:26:52 +0100

    EXA change example with comparison power and quantile transformer

--- examples/preprocessing/plot_power_transformer.py
+++ examples/preprocessing/plot_power_transformer.py
@@ -15,54 +15,51 @@ Weibull, Gaussian, Uniform, and Bimodal.
 Note that the transformation successfully maps the data to a normal
 distribution when applied to certain datasets, but is ineffective with others.
 This highlights the importance of visualizing the data before and after
-transformation. Also note that while the standardize option is set to False for
-the plot examples, by default, :class:`preprocessing.PowerTransformer` also
-applies zero-mean, unit-variance standardization to the transformed outputs.
+transformation. We set the standardize option is set to True for these examples,
+implying that we have performed a zero-mean, unit-variance standardization to
+the transformed outputs.
+
+We also include overlays of transformations via
+:class:`preprocessing.QuantileTransformer` for comparison purposes. The blue
+scatterplots represent the quantile transformation, while the red scatterplots
+represent the power transformation.
 """
 
-# Author: Eric Chang <ericchang2017@u.northwestern.edu>
+# Authors: Theodore Yoong <guo-zheng.yoong@univ.ox.ac.uk>, Eric Chang <ericchang2017@u.northwestern.edu>
 # License: BSD 3 clause
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-from sklearn.preprocessing import PowerTransformer, minmax_scale
+from sklearn.preprocessing import QuantileTransformer, PowerTransformer
 
 print(__doc__)
 
-
-N_SAMPLES = 3000
-FONT_SIZE = 6
-BINS = 100
-
-
-pt = PowerTransformer(method='box-cox', standardize=False)
-rng = np.random.RandomState(304)
-size = (N_SAMPLES, 1)
+qt = QuantileTransformer(output_distribution='normal')
+pt = PowerTransformer(method='box-cox', standardize=True)
 
 
 # lognormal distribution
-X_lognormal = rng.lognormal(size=size)
+X_lognormal = np.random.lognormal(0, 1, 1000)
 
 # chi-squared distribution
-df = 3
-X_chisq = rng.chisquare(df=df, size=size)
+X_chisq = np.random.chisquare(2,1000)
 
 # weibull distribution
-a = 50
-X_weibull = rng.weibull(a=a, size=size)
+X_weibull = np.random.weibull(5, 1000)
 
 # gaussian distribution
-loc = 100
-X_gaussian = rng.normal(loc=loc, size=size)
+X_gaussian = np.random.normal(0,1,1000)
+# ensure strictly positive values (box-cox does not allow non-positive values)
+X_gaussian = X_gaussian - X_gaussian.min() + 1
 
 # uniform distribution
-X_uniform = rng.uniform(low=0, high=1, size=size)
+X_uniform = np.random.uniform(0,1,1000)
 
 # bimodal distribution
-loc_a, loc_b = 100, 105
-X_a, X_b = rng.normal(loc=loc_a, size=size), rng.normal(loc=loc_b, size=size)
-X_bimodal = np.concatenate([X_a, X_b], axis=0)
+X_bimodal = 0.5*(np.random.normal(0,1,1000) + np.random.normal(0,50,1000))
+# ensure strictly positive values
+X_bimodal = X_bimodal - X_bimodal.min() + 1
 
 
 # create plots
@@ -75,35 +72,20 @@ distributions = [
     ('Bimodal', X_bimodal)
 ]
 
-colors = ['firebrick', 'darkorange', 'goldenrod',
-          'seagreen', 'royalblue', 'darkorchid']
+fig = plt.figure(figsize = (10,10))
 
-fig, axes = plt.subplots(nrows=4, ncols=3)
-axes = axes.flatten()
-axes_idxs = [(0, 3), (1, 4), (2, 5), (6, 9), (7, 10), (8, 11)]
-axes_list = [(axes[i], axes[j]) for i, j in axes_idxs]
-
-
-for distribution, color, axes in zip(distributions, colors, axes_list):
+for distribution, i in zip(distributions, range(1,7)):
     name, X = distribution
-    # scale all distributions to the range [0, 10]
-    X = minmax_scale(X, feature_range=(1e-10, 10))
-
-    # perform power transform
-    X_trans = pt.fit_transform(X)
-    lmbda = round(pt.lambdas_[0], 2)
-
-    ax_original, ax_trans = axes
-
-    ax_original.hist(X, color=color, bins=BINS)
-    ax_original.set_title(name, fontsize=FONT_SIZE)
-    ax_original.tick_params(axis='both', which='major', labelsize=FONT_SIZE)
-
-    ax_trans.hist(X_trans, color=color, bins=BINS)
-    ax_trans.set_title('{} after Box-Cox, $\lambda$ = {}'.format(name, lmbda),
-                       fontsize=FONT_SIZE)
-    ax_trans.tick_params(axis='both', which='major', labelsize=FONT_SIZE)
-
+    # transform X (currently a row vector) into a column vector
+    # before performing respective transforms
+    X_qt_trans = qt.fit_transform(X.reshape(-1,1))
+    X_pt_trans = pt.fit_transform(X.reshape(-1,1))
+    
+    ax = fig.add_subplot(3,2,i)
+    ax.set_title('{}'.format(name))
+    # we denote 's' as markersize, since scatter does not have markersize kwarg
+    plt.scatter(X, X_qt_trans, s=1)
+    plt.scatter(X, X_pt_trans, s=1, c='r')
 
 plt.tight_layout()
 plt.show()
