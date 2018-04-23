37a029430efbab5dd59e05d71e01a04601aa384f
Roman Yurchak <rth.yurchak@gmail.com>
2018-04-24 00:37:35 +0200

    chmod a+x build_tools/circle/install_pypy.sh

--- build_tools/circle/install_pypy.sh
+++ build_tools/circle/install_pypy.sh
@@ -2,15 +2,24 @@
 set -x
 set -e
 
-python --version
-which python
-which pip
-
-#if command -v pypy3; then ln -s $(command -v pypy3) /usr/local/bin/python; fi
 
 apt-get -yq update
-apt-get -y install libatlas-dev libatlas-base-dev liblapack-dev gfortran
+apt-get -yq install libatlas-dev libatlas-base-dev liblapack-dev gfortran
+
+# python executable is incorrect by default
+if command -v pypy3; then ln -s $(command -v pypy3) /usr/local/bin/python; fi
+
+python --version
+which python
+
+python -m pip install --upgrade pip
+
+which pip
+
 
 pip install --extra-index https://antocuni.github.io/pypy-wheels/ubuntu numpy Cython Tempita
 pip install scipy==1.1.0rc1
 pip install -e .
+
+
+pytest sklearn/
