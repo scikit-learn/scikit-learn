d9dde9fcd350bb97bbbc2963ef989111b29ee0e2
ryancatlin <rycatlin62@gmail.com>
2018-04-25 00:06:52 +0100

    W and H switched in NMF docs
    Issue: #10944

--- sklearn/decomposition/nmf.py
+++ sklearn/decomposition/nmf.py
@@ -1171,8 +1171,8 @@ class NMF(BaseEstimator, TransformerMixin):
     >>> X = np.array([[1, 1], [2, 1], [3, 1.2], [4, 1], [5, 0.8], [6, 1]])
     >>> from sklearn.decomposition import NMF
     >>> model = NMF(n_components=2, init='random', random_state=0)
-    >>> W = model.fit_transform(X)
-    >>> H = model.components_
+    >>> H = model.fit_transform(X)
+    >>> W = model.components_
 
     References
     ----------
