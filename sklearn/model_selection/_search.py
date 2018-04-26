002f95cd912783d2e86f36a5bb79aca79b6ab917
Viraj Mavani <viraj.mavani1996@gmail.com>
2018-04-27 01:37:02 +0530

    [MRG+1] Fix Docstring for random_state in RandomizedSearchCV inaccurate #10987 (#11003)

--- sklearn/model_selection/_search.py
+++ sklearn/model_selection/_search.py
@@ -1127,6 +1127,12 @@ class RandomizedSearchCV(BaseSearchCV):
     It is highly recommended to use continuous distributions for continuous
     parameters.
 
+    Note that before SciPy 0.16, the ``scipy.stats.distributions`` do not
+    accept a custom RNG instance and always use the singleton RNG from
+    ``numpy.random``. Hence setting ``random_state`` will not guarantee a
+    deterministic iteration whenever ``scipy.stats`` distributions are used to
+    define the parameter search space.
+
     Read more in the :ref:`User Guide <randomized_parameter_search>`.
 
     Parameters
