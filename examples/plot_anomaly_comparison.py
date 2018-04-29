ae49a51a05563f60db4848aeda61d3b1b6a8b549
Albert Thomas <albertthomas88@gmail.com>
2018-04-29 17:17:36 +0200

    add 2 components gaussian mixture with same variance in plot_anomaly_comparison to remove plot_outlier_detection

--- examples/plot_anomaly_comparison.py
+++ examples/plot_anomaly_comparison.py
@@ -64,6 +64,8 @@ blobs_params = dict(random_state=0, n_samples=n_inliers, n_features=2)
 datasets = [
     make_blobs(centers=[[0, 0], [0, 0]], cluster_std=0.5,
                **blobs_params)[0],
+    make_blobs(centers=[[2, 2], [-2, -2]], cluster_std=[0.5, 0.5],
+               **blobs_params)[0],
     make_blobs(centers=[[2, 2], [-2, -2]], cluster_std=[1.5, .3],
                **blobs_params)[0],
     4. * (make_moons(n_samples=n_samples, noise=.05, random_state=0)[0] -
