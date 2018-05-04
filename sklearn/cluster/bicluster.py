scikit-learn/scikit-learn

commit aca956b738c0a839eee473acea794662a2c65608

[MRG] Make random_state descriptions more informative and refer to Glossary for clusters module #10548 (#10614)

sklearn/cluster/bicluster.py
@@ -238,11 +238,10 @@ class SpectralCoclustering(BaseSpectral):
         (n_cpus + 1 + n_jobs) are used. Thus for n_jobs = -2, all CPUs but one
         are used.
 
-    random_state : int, RandomState instance or None, optional, default: None
-        If int, random_state is the seed used by the random number generator;
-        If RandomState instance, random_state is the random number generator;
-        If None, the random number generator is the RandomState instance used
-        by `np.random`.
+    random_state : int, RandomState instance or None (default)
+        Used for randomizing the singular value decomposition and the k-means
+        initialization. Use an int to make the randomness deterministic.
+        See :term:`random_state <Glossary>`.
 
     Attributes
     ----------
@@ -370,11 +369,10 @@ class SpectralBiclustering(BaseSpectral):
         (n_cpus + 1 + n_jobs) are used. Thus for n_jobs = -2, all CPUs but one
         are used.
 
-    random_state : int, RandomState instance or None, optional, default: None
-        If int, random_state is the seed used by the random number generator;
-        If RandomState instance, random_state is the random number generator;
-        If None, the random number generator is the RandomState instance used
-        by `np.random`.
+    random_state : int, RandomState instance or None (default)
+        Used for randomizing the singular value decomposition and the k-means
+        initialization. Use an int to make the randomness deterministic.
+        See :term:`random_state <Glossary>`.
 
     Attributes
     ----------

sklearn/cluster/k_means_.py
@@ -56,8 +56,10 @@ def _k_init(X, n_clusters, x_squared_norms, random_state, n_local_trials=None):
     x_squared_norms : array, shape (n_samples,)
         Squared Euclidean norm of each data point.
 
-    random_state : RandomState
-        The generator used to initialize the centers.
+    random_state : int, RandomState instance
+        The generator used to initialize the centers. Use an int to make the
+        randomness deterministic.
+        See :term:`random_state <Glossary>`.
 
     n_local_trials : integer, optional
         The number of seeding trials for each center (except the first),
@@ -225,11 +227,10 @@ def k_means(X, n_clusters, init='k-means++', precompute_distances='auto',
     tol : float, optional
         The relative increment in the results before declaring convergence.
 
-    random_state : int, RandomState instance or None, optional, default: None
-        If int, random_state is the seed used by the random number generator;
-        If RandomState instance, random_state is the random number generator;
-        If None, the random number generator is the RandomState instance used
-        by `np.random`.
+    random_state : int, RandomState instance or None (default)
+        Determines random number generation for centroid initialization. Use
+        an int to make the randomness deterministic.
+        See :term:`random_state <Glossary>`.
 
     copy_x : boolean, optional
         When pre-computing distances it is more numerically accurate to center
@@ -466,11 +467,10 @@ def _kmeans_single_lloyd(X, n_clusters, max_iter=300, init='k-means++',
     precompute_distances : boolean, default: True
         Precompute distances (faster but takes more memory).
 
-    random_state : int, RandomState instance or None, optional, default: None
-        If int, random_state is the seed used by the random number generator;
-        If RandomState instance, random_state is the random number generator;
-        If None, the random number generator is the RandomState instance used
-        by `np.random`.
+    random_state : int, RandomState instance or None (default)
+        Determines random number generation for centroid initialization. Use
+        an int to make the randomness deterministic.
+        See :term:`random_state <Glossary>`.
 
     Returns
     -------
@@ -656,11 +656,10 @@ def _init_centroids(X, k, init, random_state=None, x_squared_norms=None,
     init : {'k-means++', 'random' or ndarray or callable} optional
         Method for initialization
 
-    random_state : int, RandomState instance or None, optional, default: None
-        If int, random_state is the seed used by the random number generator;
-        If RandomState instance, random_state is the random number generator;
-        If None, the random number generator is the RandomState instance used
-        by `np.random`.
+    random_state : int, RandomState instance or None (default)
+        Determines random number generation for centroid initialization. Use
+        an int to make the randomness deterministic.
+        See :term:`random_state <Glossary>`.
 
     x_squared_norms :  array, shape (n_samples,), optional
         Squared euclidean norm of each data point. Pass it if you have it at
@@ -773,11 +772,10 @@ class KMeans(BaseEstimator, ClusterMixin, TransformerMixin):
     verbose : int, default 0
         Verbosity mode.
 
-    random_state : int, RandomState instance or None, optional, default: None
-        If int, random_state is the seed used by the random number generator;
-        If RandomState instance, random_state is the random number generator;
-        If None, the random number generator is the RandomState instance used
-        by `np.random`.
+    random_state : int, RandomState instance or None (default)
+        Determines random number generation for centroid initialization. Use
+        an int to make the randomness deterministic.
+        See :term:`random_state <Glossary>`.
 
     copy_x : boolean, optional
         When pre-computing distances it is more numerically accurate to center
@@ -1051,11 +1049,11 @@ def _mini_batch_step(X, x_squared_norms, centers, counts,
         the distances of each sample to its closest center.
         May not be None when random_reassign is True.
 
-    random_state : int, RandomState instance or None, optional, default: None
-        If int, random_state is the seed used by the random number generator;
-        If RandomState instance, random_state is the random number generator;
-        If None, the random number generator is the RandomState instance used
-        by `np.random`.
+    random_state : int, RandomState instance or None (default)
+        Determines random number generation for centroid initialization and to
+        pick new clusters amongst observations with uniform probability. Use
+        an int to make the randomness deterministic.
+        See :term:`random_state <Glossary>`.
 
     random_reassign : boolean, optional
         If True, centers with very low counts are randomly reassigned
@@ -1264,11 +1262,10 @@ class MiniBatchKMeans(KMeans):
         Compute label assignment and inertia for the complete dataset
         once the minibatch optimization has converged in fit.
 
-    random_state : int, RandomState instance or None, optional, default: None
-        If int, random_state is the seed used by the random number generator;
-        If RandomState instance, random_state is the random number generator;
-        If None, the random number generator is the RandomState instance used
-        by `np.random`.
+    random_state : int, RandomState instance or None (default)
+        Determines random number generation for centroid initialization and
+        random reassignment. Use an int to make the randomness deterministic.
+        See :term:`random_state <Glossary>`.
 
     tol : float, default: 0.0
         Control early stopping based on the relative center changes as

sklearn/cluster/mean_shift_.py
@@ -47,11 +47,11 @@ def estimate_bandwidth(X, quantile=0.3, n_samples=None, random_state=0,
     n_samples : int, optional
         The number of samples to use. If not given, all samples are used.
 
-    random_state : int, RandomState instance or None, optional (default=None)
-        If int, random_state is the seed used by the random number generator;
-        If RandomState instance, random_state is the random number generator;
-        If None, the random number generator is the RandomState instance used
-        by `np.random`.
+    random_state : int, RandomState instance or None (default)
+        The generator used to randomly select the samples from input points
+        for bandwidth estimation. Use an int to make the randomness
+        deterministic.
+        See :term:`random_state <Glossary>`.
 
     n_jobs : int, optional (default = 1)
         The number of parallel jobs to run for neighbors search.

sklearn/cluster/spectral.py
@@ -38,11 +38,10 @@ def discretize(vectors, copy=True, max_svd_restarts=30, n_iter_max=20,
         Maximum number of iterations to attempt in rotation and partition
         matrix search if machine precision convergence is not reached
 
-    random_state : int, RandomState instance or None, optional, default: None
-        If int, random_state is the seed used by the random number generator;
-        If RandomState instance, random_state is the random number generator;
-        If None, the random number generator is the RandomState instance used
-        by `np.random`.
+    random_state : int, RandomState instance or None (default)
+        Determines random number generation for rotation matrix initialization.
+        Use an int to make the randomness deterministic.
+        See :term:`random_state <Glossary>`.
 
     Returns
     -------
@@ -195,13 +194,12 @@ def spectral_clustering(affinity, n_clusters=8, n_components=None,
         to be installed. It can be faster on very large, sparse problems,
         but may also lead to instabilities
 
-    random_state : int, RandomState instance or None, optional, default: None
+    random_state : int, RandomState instance or None (default)
         A pseudo random number generator used for the initialization of the
         lobpcg eigen vectors decomposition when eigen_solver == 'amg' and by
-        the K-Means initialization. If int, random_state is the seed used by
-        the random number generator; If RandomState instance, random_state is
-        the random number generator; If None, the random number generator is
-        the RandomState instance used by `np.random`.
+        the K-Means initialization. Use an int to make the randomness
+        deterministic.
+        See :term:`random_state <Glossary>`.
 
     n_init : int, optional, default: 10
         Number of time the k-means algorithm will be run with different
@@ -309,13 +307,12 @@ class SpectralClustering(BaseEstimator, ClusterMixin):
         to be installed. It can be faster on very large, sparse problems,
         but may also lead to instabilities
 
-    random_state : int, RandomState instance or None, optional, default: None
+    random_state : int, RandomState instance or None (default)
         A pseudo random number generator used for the initialization of the
         lobpcg eigen vectors decomposition when eigen_solver == 'amg' and by
-        the K-Means initialization.  If int, random_state is the seed used by
-        the random number generator; If RandomState instance, random_state is
-        the random number generator; If None, the random number generator is
-        the RandomState instance used by `np.random`.
+        the K-Means initialization. Use an int to make the randomness
+        deterministic.
+        See :term:`random_state <Glossary>`.
 
     n_init : int, optional, default: 10
         Number of time the k-means algorithm will be run with different

commit 44e33d20d4fc8104fd8bff3eeaa68c0f28100308

MNT Broken links in #10614

sklearn/cluster/bicluster.py
@@ -241,7 +241,7 @@ class SpectralCoclustering(BaseSpectral):
     random_state : int, RandomState instance or None (default)
         Used for randomizing the singular value decomposition and the k-means
         initialization. Use an int to make the randomness deterministic.
-        See :term:`random_state <Glossary>`.
+        See :term:`Glossary <random_state>`.
 
     Attributes
     ----------
@@ -372,7 +372,7 @@ class SpectralBiclustering(BaseSpectral):
     random_state : int, RandomState instance or None (default)
         Used for randomizing the singular value decomposition and the k-means
         initialization. Use an int to make the randomness deterministic.
-        See :term:`random_state <Glossary>`.
+        See :term:`Glossary <random_state>`.
 
     Attributes
     ----------

sklearn/cluster/k_means_.py
@@ -59,7 +59,7 @@ def _k_init(X, n_clusters, x_squared_norms, random_state, n_local_trials=None):
     random_state : int, RandomState instance
         The generator used to initialize the centers. Use an int to make the
         randomness deterministic.
-        See :term:`random_state <Glossary>`.
+        See :term:`Glossary <random_state>`.
 
     n_local_trials : integer, optional
         The number of seeding trials for each center (except the first),
@@ -230,7 +230,7 @@ def k_means(X, n_clusters, init='k-means++', precompute_distances='auto',
     random_state : int, RandomState instance or None (default)
         Determines random number generation for centroid initialization. Use
         an int to make the randomness deterministic.
-        See :term:`random_state <Glossary>`.
+        See :term:`Glossary <random_state>`.
 
     copy_x : boolean, optional
         When pre-computing distances it is more numerically accurate to center
@@ -470,7 +470,7 @@ def _kmeans_single_lloyd(X, n_clusters, max_iter=300, init='k-means++',
     random_state : int, RandomState instance or None (default)
         Determines random number generation for centroid initialization. Use
         an int to make the randomness deterministic.
-        See :term:`random_state <Glossary>`.
+        See :term:`Glossary <random_state>`.
 
     Returns
     -------
@@ -659,7 +659,7 @@ def _init_centroids(X, k, init, random_state=None, x_squared_norms=None,
     random_state : int, RandomState instance or None (default)
         Determines random number generation for centroid initialization. Use
         an int to make the randomness deterministic.
-        See :term:`random_state <Glossary>`.
+        See :term:`Glossary <random_state>`.
 
     x_squared_norms :  array, shape (n_samples,), optional
         Squared euclidean norm of each data point. Pass it if you have it at
@@ -775,7 +775,7 @@ class KMeans(BaseEstimator, ClusterMixin, TransformerMixin):
     random_state : int, RandomState instance or None (default)
         Determines random number generation for centroid initialization. Use
         an int to make the randomness deterministic.
-        See :term:`random_state <Glossary>`.
+        See :term:`Glossary <random_state>`.
 
     copy_x : boolean, optional
         When pre-computing distances it is more numerically accurate to center
@@ -1053,7 +1053,7 @@ def _mini_batch_step(X, x_squared_norms, centers, counts,
         Determines random number generation for centroid initialization and to
         pick new clusters amongst observations with uniform probability. Use
         an int to make the randomness deterministic.
-        See :term:`random_state <Glossary>`.
+        See :term:`Glossary <random_state>`.
 
     random_reassign : boolean, optional
         If True, centers with very low counts are randomly reassigned
@@ -1265,7 +1265,7 @@ class MiniBatchKMeans(KMeans):
     random_state : int, RandomState instance or None (default)
         Determines random number generation for centroid initialization and
         random reassignment. Use an int to make the randomness deterministic.
-        See :term:`random_state <Glossary>`.
+        See :term:`Glossary <random_state>`.
 
     tol : float, default: 0.0
         Control early stopping based on the relative center changes as

sklearn/cluster/mean_shift_.py
@@ -51,7 +51,7 @@ def estimate_bandwidth(X, quantile=0.3, n_samples=None, random_state=0,
         The generator used to randomly select the samples from input points
         for bandwidth estimation. Use an int to make the randomness
         deterministic.
-        See :term:`random_state <Glossary>`.
+        See :term:`Glossary <random_state>`.
 
     n_jobs : int, optional (default = 1)
         The number of parallel jobs to run for neighbors search.

sklearn/cluster/spectral.py
@@ -41,7 +41,7 @@ def discretize(vectors, copy=True, max_svd_restarts=30, n_iter_max=20,
     random_state : int, RandomState instance or None (default)
         Determines random number generation for rotation matrix initialization.
         Use an int to make the randomness deterministic.
-        See :term:`random_state <Glossary>`.
+        See :term:`Glossary <random_state>`.
 
     Returns
     -------
@@ -199,7 +199,7 @@ def spectral_clustering(affinity, n_clusters=8, n_components=None,
         lobpcg eigen vectors decomposition when eigen_solver == 'amg' and by
         the K-Means initialization. Use an int to make the randomness
         deterministic.
-        See :term:`random_state <Glossary>`.
+        See :term:`Glossary <random_state>`.
 
     n_init : int, optional, default: 10
         Number of time the k-means algorithm will be run with different
@@ -312,7 +312,7 @@ class SpectralClustering(BaseEstimator, ClusterMixin):
         lobpcg eigen vectors decomposition when eigen_solver == 'amg' and by
         the K-Means initialization. Use an int to make the randomness
         deterministic.
-        See :term:`random_state <Glossary>`.
+        See :term:`Glossary <random_state>`.
 
     n_init : int, optional, default: 10
         Number of time the k-means algorithm will be run with different