bbf8097fc2e1560746a89fdaed53a49a3096fd7e
Hanmin Qin <qinhanmin2005@sina.com>
2018-05-11 08:57:58 +0800

    I guess two is better than three

--- sklearn/datasets/base.py
+++ sklearn/datasets/base.py
@@ -361,7 +361,7 @@ def load_iris(return_X_y=False):
         .. versionadded:: 0.18
 
         .. versionchanged:: 0.20
-            Fixed three wrong data points according to Fisher's paper.
+            Fixed two wrong data points according to Fisher's paper.
 
     Examples
     --------
