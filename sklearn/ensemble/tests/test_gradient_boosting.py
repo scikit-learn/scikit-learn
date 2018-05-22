Normalization fix for gbm ensemble feature importances
This makes a small change to the ``BaseDecisionTree`` and
``DecisionTreeRegressor``, adding a ``normalize_feature_importances`` ``kwarg``
that defaults to ``True`` (matching current behavior) but is set to ``False``
for GBMs.

Tree-level feature importances are calculated as the proportional reduction of
that tree's root node entropy (see ``_tree.pyx``) and ensemble-level importances are
averaged across trees.

Because the feature importances are (currently, by default) normalized and then
averaged, feature importances from later stages are overweighted.

GBMs now defer the normalization step until after the per-tree importances have
been calculated (for the ``DecisionTreeRegressor`` used in
``ensemble.gradient_boosting.BaseGradientBoosting``).

h/t to Joel Danke for spotting this and writing up the issue.

Signed-off-by: Gil Forsyth <Gilbert.Forsyth@capitalone.com>

sklearn/ensemble/tests/test_gradient_boosting.py
@@ -19,7 +19,7 @@ from sklearn.ensemble.gradient_boosting import ZeroEstimator
 from sklearn.ensemble._gradient_boosting import predict_stages
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
-from sklearn.utils import check_random_state, tosequence
+from sklearn.utils import check_random_state, tosequence, shuffle
 from sklearn.utils.testing import assert_almost_equal
 from sklearn.utils.testing import assert_array_almost_equal
 from sklearn.utils.testing import assert_array_equal
@@ -450,6 +450,32 @@ def test_max_feature_regression():
     gbrt.fit(X_train, y_train)
     deviance = gbrt.loss_(y_test, gbrt.decision_function(X_test))
     assert_true(deviance < 0.5, "GB failed with deviance %.4f" % deviance)
+
+
+def test_feature_importance_regression():
+    # Test to make sure GINI importance is calculated correctly
+    # Make sure that results are summed, then normalized to avoid
+    # over-weighting features from later stages
+    boston = datasets.load_boston()
+    X, y = shuffle(boston.data, boston.target, random_state=13)
+    X = X.astype(np.float32)
+    offset = int(X.shape[0] * 0.9)
+    X_train, y_train = X[:offset], y[:offset]
+
+    clf = GradientBoostingRegressor(n_estimators=500, min_samples_split=2,
+                                    max_depth=4, learning_rate=0.01, loss='ls',
+                                    random_state=1)
+    clf.fit(X_train, y_train)
+
+    feature_importance = clf.feature_importances_
+    feature_importance = 100.0 * (feature_importance /
+                                  feature_importance.max())
+    sorted_idx = np.argsort(feature_importance)
+
+    feature_order = ['CHAS', 'ZN', 'RAD', 'INDUS', 'B', 'AGE', 'TAX',
+                     'PTRATIO', 'CRIM', 'NOX', 'DIS', 'RM', 'LSTAT']
+
+    assert_equal(list(boston.feature_names[sorted_idx]), feature_order)
 
 
 def test_max_feature_auto():