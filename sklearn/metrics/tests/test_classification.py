b329c5597d1a4b090d8a59b53191c5692e72599f
Dan Barkhorn <danielbarkhorn@gmail.com>
2018-05-29 11:33:33 -0700

    fixing indentation and spacing errors on test for flake8 tests

--- sklearn/metrics/tests/test_classification.py
+++ sklearn/metrics/tests/test_classification.py
@@ -108,16 +108,26 @@ def test_classification_report_dictionary_output():
     y_true, y_pred, _ = make_prediction(dataset=iris, binary=False)
 
     # print classification report with class names
-    expected_report = {'setosa': {'precision': 0.82608695652173914, 'recall': 0.79166666666666663,
-                        'f1-score': 0.8085106382978724, 'support': 24}, 'versicolor': {'precision': 0.33333333333333331,
-                        'recall': 0.096774193548387094, 'f1-score': 0.15000000000000002, 'support': 31},
-                        'virginica': {'precision': 0.41860465116279072, 'recall': 0.90000000000000002,
-                        'f1-score': 0.57142857142857151, 'support': 20}, 'avg / total': {'precision': 0.51375351084147847,
-                        'recall': 0.53333333333333333, 'f1-score': 0.47310435663627154, 'support': 75}}
+    expected_report = {'setosa': {'precision': 0.82608695652173914,
+                                  'recall': 0.79166666666666663,
+                                  'f1-score': 0.8085106382978724,
+                                  'support': 24},
+                       'versicolor': {'precision': 0.33333333333333331,
+                                      'recall': 0.096774193548387094,
+                                      'f1-score': 0.15000000000000002,
+                                      'support': 31},
+                       'virginica': {'precision': 0.41860465116279072,
+                                     'recall': 0.90000000000000002,
+                                     'f1-score': 0.57142857142857151,
+                                     'support': 20},
+                       'avg / total': {'precision': 0.51375351084147847,
+                                       'recall': 0.53333333333333333,
+                                       'f1-score': 0.47310435663627154,
+                                       'support': 75}}
 
     report = classification_report(
         y_true, y_pred, labels=np.arange(len(iris.target_names)),
-        target_names=iris.target_names,output_dict=True)
+        target_names=iris.target_names, output_dict=True)
 
     assert_dict_equal(report, expected_report)
 
