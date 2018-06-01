7735eca0bb528ae32b424da8a1366acddad6fa2b
Peng Yu <peng.yu@shopify.com>
2018-05-31 22:11:40 -0400

    fix the rebase conflict

--- sklearn/feature_extraction/tests/test_dict_vectorizer.py
+++ sklearn/feature_extraction/tests/test_dict_vectorizer.py
@@ -99,30 +99,29 @@ def test_iterable_value_with_generator():
             {"version=3": True, "spam": -1}]
     v = DictVectorizer(sparse=False)
     X = v.fit_transform(D_in)
-    assert_equal(X.shape, (3, 5))
+    assert X.shape == (3, 5)
 
     D_out = v.inverse_transform(X)
-    assert_equal(D_out[0], {"version=1": 1, "version=2": 1, "ham": 2})
+    assert D_out[0] == {"version=1": 1, "version=2": 1, "ham": 2}
 
     actual_x0 = v.transform({"version": map(str, range(1, 3)), "ham": 2})
     assert_array_equal(actual_x0, X[0:1])
 
     names = v.get_feature_names()
-    assert_true("version=2" in names)
-    assert_true("version=1" in names)
-    assert_true("version=3" in names)
-    assert_false("version" in names)
+    assert "version=2" in names
+    assert "version=1" in names
+    assert "version=3" in names
+    assert "version" not in names
 
 
 def test_iterable_value_fail_with_mapping():
-    D_in = [{"version": dict((i, str(i)) for i in xrange(1, 3)), "ham": 2},
+    D_in = [{"version": dict((i, str(i)) for i in range(1, 3)), "ham": 2},
             {"version": "2", "spam": .3},
             {"version=3": True, "spam": -1}]
     v = DictVectorizer()
-    try:
+    with pytest.raises(ValueError) as e:
         v.fit(D_in)
-    except ValueError as e:
-        assert_in("Unsupported Value Type", str(e))
+    assert "Unsupported Value Type" in str(e)
 
 
 def test_unseen_or_no_features():
