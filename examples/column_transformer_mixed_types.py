bf900e5b98ad93aef29b533389d67d762a99920f
partmor <part.morales@gmail.com>
2018-06-04 23:48:38 +0200

    DOC change ColumnTransformer example. using pandas now

--- examples/column_transformer_mixed_types.py
+++ examples/column_transformer_mixed_types.py
@@ -4,86 +4,118 @@ Column Transformer with Mixed Types
 ===================================
 
 This example demonstrates how to use
-:class:`sklearn.compose.ColumnTransformer` on a dataset containing categorical 
-and continuous data.  We use the Credit Approval Dataset (UCI ML Repository -
-https://archive.ics.uci.edu/ml/machine-learning-databases/credit-screening).
-We take a subset of numerical and categorical features and use 
-:class:`sklearn.compose.ColumnTransformer` to standard-scale the former and 
-one-hot-encode the latter.
-The choice of features is not particularly helpful, but serves to illustrate
-the technique.
+:class:`sklearn.compose.ColumnTransformer` on a dataset containing mixed
+data types. We take a subset of numerical and categorical features
+and use :class:`sklearn.compose.ColumnTransformer` to apply two non-sequential
+preprocessing pipelines, one for each data type:
+
+* Numerical data: missing value imputation, followed by standard scaling
+* Categorical data: missing value imputation (with a custom transformer),
+and then one-hot-encode.
 """
 
 # Author: Pedro Morales <part.morales@gmail.com>
-# 
+#
 # License: BSD 3 clause
 
-import io
+from __future__ import print_function
+
 import numpy as np
-from sklearn.externals.six.moves.urllib.request import urlopen
+import pandas as pd
 
-def get_clean_data():
-    """
-    Retrieve data from UCI credit screening database and store it in a numpy array
-    """
-    url = ('https://archive.ics.uci.edu/ml/machine-learning-databases/'
-           'credit-screening/crx.data')
-
-    # Read and decode the data from the url
-    f = io.StringIO(urlopen(url).read().decode('utf-8'))
-
-    # Drop rows with missing values (in this dataset NAs are '?')
-    f = [row for row in f if not '?' in row]
-
-    # Load data as numpy array
-    data = np.loadtxt(f, dtype='object', delimiter=',')
-
-    # Parse numerical data 
-    for col in [1, 2, 7, 10, 13, 14]:
-        data[:, col] = data[:, col].astype(float)
-
-    return data
-
-
-data = get_clean_data()
-
-from sklearn.preprocessing import LabelBinarizer
+from sklearn.base import TransformerMixin
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import make_pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, CategoricalEncoder
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
 
-# Split train/test sets. The last column of the array contains the class labels
-X_train, X_test, y_train, y_test = train_test_split(
-    data[:, :-1],
-    LabelBinarizer().fit_transform(data[:, -1]),
-    test_size=0.2, 
-    shuffle=True,
-    random_state=0
+
+class CustomCategoryImputer(TransformerMixin):
+    """Impute missing values for categorical features with a desired string.
+
+    Parameters
+    ----------
+    imputer_value: string, default='missing'
+        The placeholder for the missing value.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> X = np.array(['a', 'b', np.nan, 'c'], dtype=object)
+    >>> imputer = CustomCategoryImputer(imputer_value='missing')
+    >>> imputer.transform(X)
+    array([['a'],
+           ['b'],
+           ['missing'],
+           ['c']], dtype='<U7')
+    """
+
+    def __init__(self, imputer_value='missing'):
+        self.imputer_value = imputer_value
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        """Transform input filling missing values with imputer_value.
+
+        Parameters
+        ----------
+        X: numpy array
+            Categorical feature to transform
+        """
+        X_ = X.copy()
+
+        # convert to DataFrame to handle mixed types
+        if isinstance(X_, np.ndarray):
+            X_ = pd.DataFrame(X_)
+
+        mask = pd.notnull(X_)
+        X_ = X_.where(mask, self.imputer_value)
+
+        return X_.values.astype(str)
+
+
+# Read data from Titanic dataset
+titanic_url = ('https://raw.githubusercontent.com/amueller/'
+               'scipy-2017-sklearn/master/notebooks/datasets/titanic3.csv')
+data = pd.read_csv(titanic_url)
+
+# We will train our classifier with the following features
+num_feats = ['age', 'fare']
+cat_feats = ['embarked', 'sex']
+
+# We create the preprocessing pipelines for both numerical and categorical data
+num_pl = make_pipeline(SimpleImputer(strategy='median'), StandardScaler())
+cat_pl = make_pipeline(
+    CustomCategoryImputer(),
+    CategoricalEncoder('onehot-dense')
 )
 
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import CategoricalEncoder, StandardScaler
-
-# For this example, we will train our classifier with a subset of columns: 
-# (numerical), and [0, 5] (categorical). 
-# We standard-scale the numerical features and one-hot-encode the categorical ones.
-ct = ColumnTransformer(
+preprocessing_pl = ColumnTransformer(
     [
-        ('sc', StandardScaler(), [1, 2]),
-        ('ohe', CategoricalEncoder('onehot-dense'), [0, 5])
+        ('num', num_pl, num_feats),
+        ('cat', cat_pl, cat_feats)
     ],
     remainder='drop'
 )
 
-from sklearn.pipeline import make_pipeline
-from sklearn.ensemble import RandomForestClassifier
+# Append classifier to preprocessing process. Use RF with default params
+clf_pl = make_pipeline(preprocessing_pl, RandomForestClassifier())
 
-# We can put our column transformer into a pipeline object with the classifier
-pl = make_pipeline(ct, RandomForestClassifier())
+X = data.drop('survived', axis=1)
+y = data.survived.values
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,
+                                                    shuffle=True)
 
-pl.fit(X_train, y_train)
-y_pred = pl.predict_proba(X_test)[:, 1]
+# Fit classifier
+clf_pl.fit(X_train, y_train)
 
-from sklearn.metrics import roc_auc_score
-
+# Assess model performance
+y_pred = clf_pl.predict_proba(X_test)[:, 1]
 score = roc_auc_score(y_test, y_pred)
 print(score)
