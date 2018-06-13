cc9aa6f20b78b84f5ba27620f25ef02880a63624
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-06-13 18:44:28 +0200

    COSMIT

--- examples/compose/plot_column_transformer_mixed_types.py
+++ examples/compose/plot_column_transformer_mixed_types.py
@@ -75,7 +75,7 @@ clf = Pipeline(steps=[('preprocessor', preprocessor),
                       ('classifier', LogisticRegression())])
 
 X = data.drop('survived', axis=1)
-y = data.survived.values
+y = data['survived']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,
                                                     shuffle=True)
