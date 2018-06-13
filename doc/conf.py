1bfe1367bc1e0dae9e7ef63a98f428394c892d1a
Joel Nothman <joel.nothman@gmail.com>
2018-06-13 12:26:41 +1000

    Fix orphan ('not included in any toctree') warnings

--- doc/conf.py
+++ doc/conf.py
@@ -99,7 +99,7 @@ release = sklearn.__version__
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'templates', 'includes']
+exclude_patterns = ['_build', 'templates', 'includes', 'themes']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
