cec17b250bb7ee2eb9c437acddc4ac6ea425654d
shangwuyao <shangwuy@andrew.cmu.edu>
2018-06-17 23:52:27 -0400

    Refactorize PRFS with multilabel_confusion_matrix

--- sklearn/metrics/classification.py
+++ sklearn/metrics/classification.py
@@ -304,8 +304,12 @@ def multilabel_confusion_matrix(y_true, y_pred, sample_weight=None,
 
     Parameters
     ----------
-    y_true : array or sparse, shape (n_samples, n_outputs) or (n_samples,)
-    y_pred : array or sparse, shape (n_samples, n_outputs) or (n_samples,)
+    y_true : 1d array-like, or label indicator array / sparse matrix
+        Shape (n_samples, n_outputs) or (n_samples,)
+        Ground truth (correct) target values.
+    y_pred : 1d array-like, or label indicator array / sparse matrix
+        Shape (n_samples, n_outputs) or (n_samples,)
+        Estimated targets as returned by a classifier
     sample_weight : array, (n_samples,), optional
     labels : array-like
         A list of classes or column indices to select some (or to force
@@ -332,19 +336,19 @@ def multilabel_confusion_matrix(y_true, y_pred, sample_weight=None,
     if y_type not in ("binary", "multiclass", "multilabel-indicator"):
         raise ValueError("%s is not supported" % y_type)
 
+    present_labels = unique_labels(y_true, y_pred)
+    if labels is None:
+        labels = present_labels
+        n_labels = None
+    else:
+        n_labels = len(labels)
+        labels = np.hstack([labels, np.setdiff1d(present_labels, labels,
+                                                 assume_unique=True)])
+
     if y_true.ndim == 1:
         if samplewise:
             raise ValueError("Samplewise confusion is not useful outside of "
                              "multilabel classification.")
-
-        present_labels = unique_labels(y_true, y_pred)
-        if labels is None:
-            labels = present_labels
-            n_labels = None
-        else:
-            n_labels = len(labels)
-            labels = np.hstack([labels, np.setdiff1d(present_labels, labels,
-                                                     assume_unique=True)])
 
         le = LabelEncoder()
         le.fit(labels)
@@ -379,65 +383,43 @@ def multilabel_confusion_matrix(y_true, y_pred, sample_weight=None,
         true_sum = true_sum[indices]
         pred_sum = pred_sum[indices]
 
-        fp = pred_sum - tp_sum
-        fn = true_sum - tp_sum
-        tp = tp_sum
-
-        if sample_weight is not None:
-            tn = sample_weight.sum() - tp - fp - fn
-        else:
-            tn = y_true.shape[0] - tp - fp - fn
-
     else:
-        # check labels
-        if labels is not None:
-            max_label = y_true.shape[1] - 1
-            if np.max(labels) > max_label:
+        sum_axis = 1 if samplewise else 0
+
+        # All labels are index integers for multilabel.
+        # Select labels:
+        if not np.all(labels == present_labels):
+            if np.max(labels) > np.max(present_labels):
                 raise ValueError('All labels must be in [0, n labels). '
                                  'Got %d > %d' %
-                                 (np.max(labels), max_label))
+                                 (np.max(labels), np.max(present_labels)))
             if np.min(labels) < 0:
                 raise ValueError('All labels must be in [0, n labels). '
                                  'Got %d < 0' % np.min(labels))
 
-            n_labels = len(labels)
+        if n_labels is not None:
             y_true = y_true[:, labels[:n_labels]]
             y_pred = y_pred[:, labels[:n_labels]]
 
-        # account for sample weight
-        def _normal_or_sparse_row_multiply(A, w):
-            if issparse(A):
-                return A.multiply(w)
-            elif issparse(w):
-                return w.multiply(A)
-            else:
-                return np.multiply(A, w)
+        # calculate weighted counts
+        true_and_pred = y_true.multiply(y_pred)
+        tp_sum = count_nonzero(true_and_pred, axis=sum_axis,
+                               sample_weight=sample_weight)
+        pred_sum = count_nonzero(y_pred, axis=sum_axis,
+                                 sample_weight=sample_weight)
+        true_sum = count_nonzero(y_true, axis=sum_axis,
+                                 sample_weight=sample_weight)
 
-        true_and_pred = _normal_or_sparse_row_multiply(y_true, y_pred)
+    fp = pred_sum - tp_sum
+    fn = true_sum - tp_sum
+    tp = tp_sum
 
-        if sample_weight is not None:
-            sample_weight = sample_weight.reshape((len(sample_weight), 1))
-            true_and_pred = _normal_or_sparse_row_multiply(true_and_pred,
-                                                           sample_weight)
-            y_true = _normal_or_sparse_row_multiply(y_true, sample_weight)
-            y_pred = _normal_or_sparse_row_multiply(y_pred, sample_weight)
-
-        if samplewise:
-            y_true = y_true.T
-            y_pred = y_pred.T
-            true_and_pred = true_and_pred.T
-
-        tp = true_and_pred.sum(axis=0)
-
-        if y_true.dtype.kind in {'i', 'u', 'b'}:
-            tp = tp.astype(np.int64)
-        fp = y_pred.sum(axis=0) - tp
-        fn = y_true.sum(axis=0) - tp
-
-        if sample_weight is not None:
-            tn = sample_weight.sum() - tp - fp - fn
-        else:
-            tn = y_true.shape[0] - tp - fp - fn
+    if sample_weight is not None:
+        tn = sample_weight.sum() - tp - fp - fn
+    elif samplewise:
+        tn = y_true.shape[1] - tp - fp - fn
+    else:
+        tn = y_true.shape[0] - tp - fp - fn
 
     return np.array([tn, fp, fn, tp]).T.reshape(-1, 2, 2)
 
@@ -1171,8 +1153,9 @@ def precision_recall_fscore_support(y_true, y_pred, beta=1.0, labels=None,
     >>> precision_recall_fscore_support(y_true, y_pred, average=None,
     ... labels=['pig', 'dog', 'cat'])
     ... # doctest: +ELLIPSIS,+NORMALIZE_WHITESPACE
-    (array([0.        , 0.        , 0.66...]),
-     array([0., 0., 1.]), array([0. , 0. , 0.8]),
+    (array([0. , 0. , 0.66...]),
+     array([0., 0., 1.]),
+     array([0. , 0. , 0.8]),
      array([2, 2, 2]))
 
     """
@@ -1206,80 +1189,14 @@ def precision_recall_fscore_support(y_true, y_pred, beta=1.0, labels=None,
                       "labels=[pos_label] to specify a single positive class."
                       % (pos_label, average), UserWarning)
 
-    if labels is None:
-        labels = present_labels
-        n_labels = None
-    else:
-        n_labels = len(labels)
-        labels = np.hstack([labels, np.setdiff1d(present_labels, labels,
-                                                 assume_unique=True)])
-
     # Calculate tp_sum, pred_sum, true_sum ###
-
-    if y_type.startswith('multilabel'):
-        sum_axis = 1 if average == 'samples' else 0
-
-        # All labels are index integers for multilabel.
-        # Select labels:
-        if not np.all(labels == present_labels):
-            if np.max(labels) > np.max(present_labels):
-                raise ValueError('All labels must be in [0, n labels). '
-                                 'Got %d > %d' %
-                                 (np.max(labels), np.max(present_labels)))
-            if np.min(labels) < 0:
-                raise ValueError('All labels must be in [0, n labels). '
-                                 'Got %d < 0' % np.min(labels))
-
-        if n_labels is not None:
-            y_true = y_true[:, labels[:n_labels]]
-            y_pred = y_pred[:, labels[:n_labels]]
-
-        # calculate weighted counts
-        true_and_pred = y_true.multiply(y_pred)
-        tp_sum = count_nonzero(true_and_pred, axis=sum_axis,
-                               sample_weight=sample_weight)
-        pred_sum = count_nonzero(y_pred, axis=sum_axis,
-                                 sample_weight=sample_weight)
-        true_sum = count_nonzero(y_true, axis=sum_axis,
-                                 sample_weight=sample_weight)
-
-    elif average == 'samples':
-        raise ValueError("Sample-based precision, recall, fscore is "
-                         "not meaningful outside multilabel "
-                         "classification. See the accuracy_score instead.")
-    else:
-        le = LabelEncoder()
-        le.fit(labels)
-        y_true = le.transform(y_true)
-        y_pred = le.transform(y_pred)
-        sorted_labels = le.classes_
-
-        # labels are now from 0 to len(labels) - 1 -> use bincount
-        tp = y_true == y_pred
-        tp_bins = y_true[tp]
-        if sample_weight is not None:
-            tp_bins_weights = np.asarray(sample_weight)[tp]
-        else:
-            tp_bins_weights = None
-
-        if len(tp_bins):
-            tp_sum = np.bincount(tp_bins, weights=tp_bins_weights,
-                              minlength=len(labels))
-        else:
-            # Pathological case
-            true_sum = pred_sum = tp_sum = np.zeros(len(labels))
-        if len(y_pred):
-            pred_sum = np.bincount(y_pred, weights=sample_weight,
-                                minlength=len(labels))
-        if len(y_true):
-            true_sum = np.bincount(y_true, weights=sample_weight,
-                                minlength=len(labels))
-
-        # Retain only selected labels
-        indices = np.searchsorted(sorted_labels, labels[:n_labels])
-        tp_sum = tp_sum[indices]
-        true_sum = true_sum[indices]
-        pred_sum = pred_sum[indices]
+    samplewise = average == 'samples'
+    C = multilabel_confusion_matrix(y_true, y_pred,
+                                    sample_weight=sample_weight,
+                                    labels=labels, samplewise=samplewise)
+    tp_sum = C[:, 1, 1]
+    pred_sum = C[:, 1, 1] + C[:, 0, 1]
+    true_sum = C[:, 1, 1] + C[:, 1, 0]
 
     if average == 'micro':
         tp_sum = np.array([tp_sum.sum()])
