02fad7ab24959e59e8f7791bd9c3a353115ba7c8
Joel Nothman <joel.nothman@gmail.com>
2018-06-20 14:33:12 +1000

    Fix skipping in conftest.py (#11318)

--- doc/conftest.py
+++ doc/conftest.py
@@ -70,17 +70,19 @@ def setup_compose():
 
 def pytest_runtest_setup(item):
     fname = item.fspath.strpath
-    if fname.endswith('datasets/labeled_faces.rst'):
+    is_index = fname.endswith('datasets/index.rst')
+    if fname.endswith('datasets/labeled_faces.rst') or is_index:
         setup_labeled_faces()
-    elif fname.endswith('datasets/mldata.rst'):
+    elif fname.endswith('datasets/mldata.rst') or is_index:
         setup_mldata()
-    elif fname.endswith('datasets/rcv1.rst'):
+    elif fname.endswith('datasets/rcv1.rst') or is_index:
         setup_rcv1()
-    elif fname.endswith('datasets/twenty_newsgroups.rst'):
+    elif fname.endswith('datasets/twenty_newsgroups.rst') or is_index:
         setup_twenty_newsgroups()
-    elif fname.endswith('tutorial/text_analytics/working_with_text_data.rst'):
+    elif fname.endswith('tutorial/text_analytics/working_with_text_data.rst')\
+            or is_index:
         setup_working_with_text_data()
-    elif fname.endswith('modules/compose.rst'):
+    elif fname.endswith('modules/compose.rst') or is_index:
         setup_compose()
 
 
