7b4a6a3b42e95b8fe04d95f9f2050ba8422b0a72
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-06-21 14:51:26 +0200

    Update data.py

--- sklearn/preprocessing/data.py
+++ sklearn/preprocessing/data.py
@@ -704,7 +704,7 @@ class StandardScaler(BaseEstimator, TransformerMixin):
                     _incremental_mean_and_var(X, self.mean_, self.var_,
                                               self.n_samples_seen_)
 
-        # for back-compatibility, reduce n_samples_seen_ to an integer if the
+        # for backward-compatibility, reduce n_samples_seen_ to an integer if the
         # number of samples is the same for each feature (i.e. no missing
         # values)
         if np.ptp(self.n_samples_seen_) == 0:
