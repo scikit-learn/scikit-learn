93382cc41fb95abbbf534aed4c4cf2405c38d601
Sergey Feldman <sergeyfeldman@gmail.com>
2018-06-22 01:01:39 -0700

    Rename `MICEImputer` to `ChainedImputer` (#11314)

--- sklearn/utils/estimator_checks.py
+++ sklearn/utils/estimator_checks.py
@@ -77,7 +77,7 @@ MULTI_OUTPUT = ['CCA', 'DecisionTreeRegressor', 'ElasticNet',
                 'RANSACRegressor', 'RadiusNeighborsRegressor',
                 'RandomForestRegressor', 'Ridge', 'RidgeCV']
 
-ALLOW_NAN = ['Imputer', 'SimpleImputer', 'MICEImputer',
+ALLOW_NAN = ['Imputer', 'SimpleImputer', 'ChainedImputer',
              'MinMaxScaler', 'StandardScaler', 'PowerTransformer',
              'QuantileTransformer']
 
