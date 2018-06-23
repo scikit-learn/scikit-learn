97a15dbad3ef1ae8fd35a9ff4cb33b3a522b7341
aishgrt1 <31906139+aishgrt1@users.noreply.github.com>
2018-06-23 18:51:06 -0400

    Fix LogisticRegression with warm_start, multinomial and binary data (#10986)

--- sklearn/linear_model/tests/test_logistic.py
+++ sklearn/linear_model/tests/test_logistic.py
@@ -10,6 +10,7 @@ from sklearn.model_selection import StratifiedKFold
 from sklearn.preprocessing import LabelEncoder
 from sklearn.utils import compute_class_weight
 from sklearn.utils.testing import assert_almost_equal
+from sklearn.utils.testing import assert_allclose
 from sklearn.utils.testing import assert_array_almost_equal
 from sklearn.utils.testing import assert_array_equal
 from sklearn.utils.testing import assert_equal
@@ -1238,3 +1239,23 @@ def test_dtype_match():
             lr_64.fit(X_64, y_64)
             assert_equal(lr_64.coef_.dtype, X_64.dtype)
             assert_almost_equal(lr_32.coef_, lr_64.coef_.astype(np.float32))
+
+
+def test_warm_start_converge_LR():
+    # Test to see that the logistic regression converges on warm start,
+    # with multi_class='multinomial'. Non-regressive test for #10836
+
+    rng = np.random.RandomState(0)
+    X = np.concatenate((rng.randn(100, 2) + [1, 1], rng.randn(100, 2)))
+    y = np.array([1] * 100 + [-1] * 100)
+    lr_no_ws = LogisticRegression(multi_class='multinomial',
+                                  solver='sag', warm_start=False)
+    lr_ws = LogisticRegression(multi_class='multinomial',
+                               solver='sag', warm_start=True)
+
+    lr_no_ws_loss = log_loss(y, lr_no_ws.fit(X, y).predict_proba(X))
+    lr_ws_loss = [log_loss(y, lr_ws.fit(X, y).predict_proba(X)) 
+                 for _ in range(5)]
+
+    for i in range(5):
+        assert_allclose(lr_no_ws_loss, lr_ws_loss[i], rtol=1e-5)
