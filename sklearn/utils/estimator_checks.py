5ca5cfc6f44a8117140b5d7f8903c7382b1a04fa
Joel Nothman <joel.nothman@gmail.com>
2018-06-24 21:07:41 +1000

    Revert change to estimator_checks about GaussianProcessRegressor

--- sklearn/utils/estimator_checks.py
+++ sklearn/utils/estimator_checks.py
@@ -136,8 +136,9 @@ def _yield_classifier_checks(name, classifier):
 
         yield check_supervised_y_2d
     yield check_supervised_y_no_nan
-    # test if NotFittedError is raised
-    yield check_estimators_unfitted
+    if name != "GaussianProcessRegressor":
+        # test if NotFittedError is raised
+        yield check_estimators_unfitted
     if 'class_weight' in classifier.get_params().keys():
         yield check_class_weight_classifiers
 
