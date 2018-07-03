c42ecd3bdf635d7f7cad5d689da8a111a6ed02c4
Wenhao <wenhaoz@ucla.edu>
2018-07-02 20:14:58 -0700

    Fix: requested changes

--- examples/model_selection/plot_grid_search_refit_callable.py
+++ examples/model_selection/plot_grid_search_refit_callable.py
@@ -90,7 +90,7 @@ n_components = grid.cv_results_['param_reduce_dim__n_components']
 test_scores = grid.cv_results_['mean_test_score']
 
 plt.figure()
-plt.bar(n_components, test_scores, width=1.3, color='bgrc')
+plt.bar(n_components, test_scores, width=1.3, color='b')
 
 lower = lower_bound(grid.cv_results_)
 plt.axhline(np.max(test_scores), linestyle='--', color='y',
