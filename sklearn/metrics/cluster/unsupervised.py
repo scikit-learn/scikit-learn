52713c006acf340836b2bc4e379067f01c7fff19
Sai Raj Kishore <36800537+sairajk@users.noreply.github.com>
2018-07-08 22:37:33 +0530

    Added Documentation to Score Function
    Added Documentation to Score Function in unsupervised.py

--- sklearn/metrics/cluster/unsupervised.py
+++ sklearn/metrics/cluster/unsupervised.py
@@ -347,6 +347,24 @@ def davies_bouldin_score(X, labels):
 def score_function(X, labels):
     """Computes Score Function.
 
+    Score Function is a Cluster Validity Index. It is based on standard
+    cluster properties and is proved to be better than or atleast as good as
+    existing validation indices for hyperspheroidal clusters.
+
+    DRAWBACKS OF EXISTING VALIDITY INDICES :
+    Most current validity indices only cover a subset of important aspects of
+    clusters. Moreover, these indices are relevant only for data sets
+    containing at least two clusters. As a result, these indices are useful in
+    certain situations and are not of general-purpose.
+
+    The score function is tested against four existing validity indices, that
+    are Dunn index, Davies-Bouldin index, Silhouette index, Maulik-
+    Bandyopadhyay index on several artificial and real-life data sets to
+    evaluate the performance of the score function. It is found to be always
+    as good or better than these indices in the case of hyperspheroidal
+    clusters. It also works well on multidimensional data sets and is able to
+    accommodate unique and sub-cluster cases.
+
     Parameters
     ----------
     X : array-like, shape (``n_samples``, ``n_features``)
@@ -366,7 +384,7 @@ def score_function(X, labels):
     .. [1] Saitta S., Raphael B., Smith I.F.C. (2007).
         `"A Bounded Index for Cluster Validity"
         <https://dl.acm.org/citation.cfm?id=1420344>`__.
-        Perner P. (eds) Machine Learning and Data Mining in Pattern Recognition.
+        Perner P.(eds) Machine Learning and Data Mining in Pattern Recognition.
         Lecture Notes in Computer Science, vol 4571.
     """
     # Pre-processing and validation of input data and labels
@@ -382,17 +400,22 @@ def score_function(X, labels):
     # Mean of the entire data. Same as z_tot in the original paper.
     data_mean = np.mean(X, axis=0)
 
-    # cluster_size : is a 1D-array of length "n_labels" (i.e. number of unique labels or number of unique clusters)
+    # cluster_size : is a 1D-array of length "n_labels"
+    # (i.e. number of unique labels or number of unique clusters)
     # It stores the number of data points in each cluster.
     cluster_size = np.zeros(n_labels)
-    # intra_dists: It stores the mean euclidean distance between all points belonging to a cluster and their cluster
-    # centroid.
-    # Since there are "n_labels" unique clusters, the variable is a 1D-array of length "n_labels".
+    # intra_dists: It stores the mean euclidean distance between
+    # all points belonging to a cluster and their cluster centroid.
+    # Since there are "n_labels" unique clusters, the variable is
+    # a 1D-array of length "n_labels".
     intra_dists = np.zeros(n_labels)
-    # intra_centroid_dists: It stores the product of euclidean distance between a cluster centroid and the centroid of
+    # intra_centroid_dists: It stores the product of euclidean distance
+    # between a cluster centroid and the centroid of
     # the entire data to the number of elements of that cluster.
-    # Acc. to paper : intra_centroid_dists[i] = ||z_i - z_tot|| * n_i, where "i" is a particular cluster.
-    # Since there are "n_labels" unique clusters, the variable is a 1D-array of length "n_labels".
+    # Acc. to paper : intra_centroid_dists[i] = ||z_i - z_tot|| * n_i,
+    # where "i" is a particular cluster.
+    # Since there are "n_labels" unique clusters, the variable is a
+    # 1D-array of length "n_labels".
     intra_centroid_dists = np.zeros(n_labels)
     # centroids: stores the centroid of each cluster.
     centroids = np.zeros((n_labels, len(X[0])), dtype=np.float)
@@ -406,10 +429,15 @@ def score_function(X, labels):
         centroids[k] = cluster_k.mean(axis=0)
 
         # Compute intra_centroid_dists[k] acc. to the formula in the paper :
-        # intra_centroid_dists[k] = ||z_k - z_tot|| * n_k, where "k" is a particular cluster.
-        intra_centroid_dists[k] = pairwise_distances([centroids[k]], [data_mean], metric='euclidean') * cluster_size[k]
-        # Compute mean euclidean distance between all points belonging to cluster "k" and the centroid of cluster "k".
-        intra_dists[k] = np.mean(pairwise_distances(cluster_k, [centroids[k]], metric='euclidean'))
+        # intra_centroid_dists[k] = ||z_k - z_tot|| * n_k,
+        # where "k" is a particular cluster.
+        intra_centroid_dists[k] = \
+            pairwise_distances([centroids[k]], [data_mean],
+                               metric='euclidean') * cluster_size[k]
+        # Compute mean euclidean distance between all points belonging to
+        # cluster "k" and the centroid of cluster "k".
+        intra_dists[k] = np.mean(pairwise_distances(cluster_k, [centroids[k]],
+                                                    metric='euclidean'))
 
     # Compute "between class distance"(bcd).
     bcd = np.mean(intra_centroid_dists) / n_samples
