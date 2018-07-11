84cc33bf33aa5acdf6b11c6f01106dbb4883887e
adrin <adrin.jalali@gmail.com>
2018-07-11 13:47:18 +0200

    example added to AdditiveChi2Sampler docstring

--- sklearn/kernel_approximation.py
+++ sklearn/kernel_approximation.py
@@ -274,6 +274,26 @@ class AdditiveChi2Sampler(BaseEstimator, TransformerMixin):
     sample_interval : float, optional
         Sampling interval. Must be specified when sample_steps not in {1,2,3}.
 
+    Examples
+    --------
+    >>> from sklearn.kernel_approximation import AdditiveChi2Sampler 
+    >>> from sklearn.linear_model import SGDClassifier
+    >>> X = [[0, 0], [1, 1], [2, 0], [0, 4]]
+    >>> y = [0, 0, 1, 1] 
+    >>> chi2_feature = AdditiveChi2Sampler(sample_steps=1)
+    >>> X_features = chi2_feature.fit_transform(X, y)
+    >>> clf = SGDClassifier(max_iter=5)
+    >>> clf.fit(X_features, y)
+    ... # doctest: +NORMALIZE_WHITESPACE
+    SGDClassifier(alpha=0.0001, average=False, class_weight=None,
+           early_stopping=False, epsilon=0.1, eta0=0.0, fit_intercept=True,
+           l1_ratio=0.15, learning_rate='optimal', loss='hinge', max_iter=5,
+           n_iter=None, n_iter_no_change=5, n_jobs=1, penalty='l2',
+           power_t=0.5, random_state=None, shuffle=True, tol=None,
+           validation_fraction=0.1, verbose=0, warm_start=False)
+    >>> clf.score(X_features, y)
+    1.0
+
     Notes
     -----
     This estimator approximates a slightly different version of the additive
