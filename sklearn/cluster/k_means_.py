1618d5fc24947796e81ad2ad3d816a4cb74948ff
adrin jalali <anjalali@ancud.de>
2018-07-13 17:27:10 +0200

    MiniBatchKMeans example added to docsctring

--- sklearn/cluster/k_means_.py
+++ sklearn/cluster/k_means_.py
@@ -1404,6 +1404,21 @@ class MiniBatchKMeans(KMeans):
         defined as the sum of square distances of samples to their nearest
         neighbor.
 
+    Examples
+    --------
+    >>> from sklearn.cluster import MiniBatchKMeans
+    >>> import numpy as np
+    >>> X = np.array([[1, 2], [1, 4], [1, 0],
+    ...               [4, 2], [4, 4], [4, 0]])
+    >>> kmeans = MiniBatchKMeans(n_clusters=2, random_state=0).fit(X)
+    >>> kmeans.labels_
+    array([0, 0, 0, 1, 1, 1], dtype=int32)
+    >>> kmeans.predict([[0, 0], [4, 4]])
+    array([0, 1], dtype=int32)
+    >>> kmeans.cluster_centers_
+    array([[1.        , 2.01023891],
+           [4.        , 1.96451613]])
+
     See also
     --------
 
