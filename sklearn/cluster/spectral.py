48fd36492b85f4cb6344808e1e6dd895f37c1183
adrin jalali <anjalali@ancud.de>
2018-07-13 22:39:19 +0200

    SpectralClustering example added to docstring

--- sklearn/cluster/spectral.py
+++ sklearn/cluster/spectral.py
@@ -371,6 +371,22 @@ class SpectralClustering(BaseEstimator, ClusterMixin):
     labels_ :
         Labels of each point
 
+    Examples
+    --------
+    >>> from sklearn.cluster import SpectralClustering
+    >>> import numpy as np
+    >>> X = np.array([[1, 1], [2, 1], [1, 0],
+    ...               [4, 7], [3, 5], [3, 6]])
+    >>> clustering = SpectralClustering(n_clusters=2).fit(X)
+    >>> clustering.labels_
+    array([0, 0, 0, 1, 1, 1], dtype=int32)
+    >>> clustering
+    ... # doctest: +NORMALIZE_WHITESPACE
+    SpectralClustering(affinity='rbf', assign_labels='kmeans', coef0=1, degree=3,
+              eigen_solver=None, eigen_tol=0.0, gamma=1.0, kernel_params=None,
+              n_clusters=2, n_init=10, n_jobs=1, n_neighbors=10,
+              random_state=None)
+
     Notes
     -----
     If you have an affinity matrix, such as a distance matrix,
