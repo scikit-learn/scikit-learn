9acc22965210c2796286a623fffc01717f7256d5
adrin jalali <anjalali@ancud.de>
2018-07-14 09:31:43 +0200

    add random_state to SpectralCoclustering example

--- sklearn/cluster/bicluster.py
+++ sklearn/cluster/bicluster.py
@@ -262,17 +262,17 @@ class SpectralCoclustering(BaseSpectral):
     --------
     >>> from sklearn.cluster import SpectralCoclustering
     >>> import numpy as np
-    >>> X = np.array([[1, 2], [1, 4], [1, 0],
-    ...               [4, 2], [4, 4], [4, 0]])
-    >>> clustering = SpectralCoclustering(n_clusters=2).fit(X)
+    >>> X = np.array([[1, 1], [2, 1], [1, 0],
+    ...               [4, 7], [3, 5], [3, 6]])
+    >>> clustering = SpectralCoclustering(n_clusters=2, random_state=0).fit(X)
     >>> clustering.row_labels_
-    array([1, 1, 0, 0, 1, 0], dtype=int32)
+    array([0, 1, 1, 0, 0, 0], dtype=int32)
     >>> clustering.column_labels_
-    array([0, 1], dtype=int32)
+    array([0, 0], dtype=int32)
     >>> clustering
     ... # doctest: +NORMALIZE_WHITESPACE
     SpectralCoclustering(init='k-means++', mini_batch=False, n_clusters=2,
-               n_init=10, n_jobs=1, n_svd_vecs=None, random_state=None,
+               n_init=10, n_jobs=1, n_svd_vecs=None, random_state=0,
                svd_method='randomized')
 
     References
