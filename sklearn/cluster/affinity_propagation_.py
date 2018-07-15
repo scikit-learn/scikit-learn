9e994bf1834a609a739c727388e4d23770cc2511
adrin jalali <anjalali@ancud.de>
2018-07-15 19:49:24 +0200

    fix extra ellipsis in the example doc output

--- sklearn/cluster/affinity_propagation_.py
+++ sklearn/cluster/affinity_propagation_.py
@@ -297,8 +297,7 @@ class AffinityPropagation(BaseEstimator, ClusterMixin):
     >>> X = np.array([[1, 2], [1, 4], [1, 0],
     ...               [4, 2], [4, 4], [4, 0]])
     >>> clustering = AffinityPropagation().fit(X)
-    >>> clustering
-    ... # doctest: +NORMALIZE_WHITESPACE
+    >>> clustering # doctest: +NORMALIZE_WHITESPACE
     AffinityPropagation(affinity='euclidean', convergence_iter=15, copy=True,
               damping=0.5, max_iter=200, preference=None, verbose=False)
     >>> clustering.labels_
