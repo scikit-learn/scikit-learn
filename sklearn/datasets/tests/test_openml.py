6a834e827a41386627d4d89027e2e8c90575d01a
janvanrijn <janvanrijn@gmail.com>
2018-07-15 17:10:48 -0400

    mocked ALL unit tests

--- sklearn/datasets/tests/test_openml.py
+++ sklearn/datasets/tests/test_openml.py
@@ -7,26 +7,35 @@ import scipy.sparse
 import sklearn
 
 from sklearn.datasets import fetch_openml
-from sklearn.datasets.openml import _get_data_features
 from sklearn.utils.testing import (assert_warns_message,
                                    assert_raise_message)
 from sklearn.externals._liacarff.arff import load
+from sklearn.externals.six.moves.urllib.error import HTTPError
+
+
+try:
+    # Python 2
+    from urllib2 import urlopen
+except ImportError:
+    # Python 3+
+    from urllib.request import urlopen
 
 
 def fetch_dataset_from_openml(data_id, data_name, data_version,
                               expected_observations, expected_features,
                               expect_sparse):
     # fetch with version
-    data_by_name_id = fetch_openml(name=data_name, version=data_version)
+    data_by_name_id = fetch_openml(name=data_name, version=data_version,
+                                   cache=False)
     assert int(data_by_name_id.details['id']) == data_id
 
     # fetch without version
-    fetch_openml(name=data_name)
+    fetch_openml(name=data_name, cache=False)
     # without specifying the version, there is no guarantee that the data id
     # will be the same
 
     # fetch with dataset id
-    data_by_id = fetch_openml(data_id)
+    data_by_id = fetch_openml(data_id, cache=False)
     assert data_by_id.details['name'] == data_name
     assert data_by_id.data.shape == (expected_observations, expected_features)
     assert data_by_id.target.shape == (expected_observations, )
@@ -35,9 +44,11 @@ def fetch_dataset_from_openml(data_id, data_name, data_version,
     else:
         assert isinstance(data_by_id.data, np.ndarray)
 
-    # check numeric features:
+    # check numeric features. Be sure to call to the mocked version, because
+    # urlopen is also mocked.
     feature_name_type = {feature['name']: feature['data_type']
-                         for feature in _get_data_features(data_id)}
+                         for feature in
+                         sklearn.datasets.openml._get_data_features(data_id)}
     for idx, feature_name in enumerate(data_by_id.feature_names):
         if feature_name_type[feature_name] == 'numeric':
             # casting trick according to Jaime at
@@ -74,14 +85,31 @@ def _monkey_patch_webbased_functions(context, data_id):
         arff_fp = open(path, 'r').read()
         return load(arff_fp)
 
-    def _mock_get_data_info_by_name(name_or_id, version):
-        path = os.path.join(testdir_path,
-                            'mock_openml/%d/%s_%s.json' % (data_id,
-                                                           name_or_id,
-                                                           version))
-        data_info = open(path, 'r').read()
-        data_info_json = json.loads(data_info)
-        return data_info_json['data']['dataset'][0]
+    def _mock_urlopen(url):
+        # url contains key value pairs of attributes, e.g.,
+        # openml.org/api/v1/json/data_name/iris/data_version/1 should
+        # ideally become {data_name: 'iris', data_version: '1'}
+        api_prefix = "https://openml.org/api/v1/json/data/list/"
+        assert(url.startswith(api_prefix))
+        att_list = url[len(api_prefix):].split('/')
+        key_val_dict = dict(zip(att_list[::2], att_list[1::2]))
+        # add defaults, so we can make assumptions about the content
+        if 'data_version' not in key_val_dict:
+            key_val_dict['data_version'] = None
+        if 'status' not in key_val_dict:
+            key_val_dict['status'] = "active"
+        mock_file = "%s_%s_%s.json" % (key_val_dict['data_name'],
+                                       key_val_dict['data_version'],
+                                       key_val_dict['status'])
+        json_file_path = os.path.join(testdir_path, 'mock_openml',
+                                      str(data_id), mock_file)
+        # load the file itself, to simulate a http error
+        json_data = json.loads(open(json_file_path, 'r').read())
+        if 'error' in json_data:
+            raise HTTPError(url=None, code=412,
+                            msg='Simulated mock error',
+                            hdrs=None, fp=None)
+        return urlopen('file://' + json_file_path)
 
     context.setattr(sklearn.datasets.openml,
                     '_get_data_description_by_id',
@@ -91,9 +119,7 @@ def _monkey_patch_webbased_functions(context, data_id):
                     _mock_data_features)
     context.setattr(sklearn.datasets.openml, '_download_data',
                     _mock_download_data)
-    context.setattr(sklearn.datasets.openml,
-                    '_get_data_info_by_name',
-                    _mock_get_data_info_by_name)
+    context.setattr(sklearn.datasets.openml, 'urlopen', _mock_urlopen)
 
 
 def test_fetch_openml_iris(monkeypatch):
@@ -147,30 +173,41 @@ def test_fetch_openml_australian(monkeypatch):
     expected_observations = 690
     expected_features = 14
     _monkey_patch_webbased_functions(monkeypatch, data_id)
-    fetch_dataset_from_openml(data_id, data_name, data_version,
-                              expected_observations, expected_features,
-                              expect_sparse=False)
+    assert_warns_message(
+        UserWarning,
+        "Version 1 of dataset Australian is inactive,",
+        fetch_dataset_from_openml,
+        **{'data_id': data_id, 'data_name': data_name,
+           'data_version': data_version,
+           'expected_observations': expected_observations,
+           'expected_features': expected_features,
+           'expect_sparse': False}
+    )
 
 
-def test_fetch_openml_inactive():
+def test_fetch_openml_inactive(monkeypatch):
     # makes contact with openml server. not mocked
     # fetch inactive dataset by id
+    data_id = 40675
+    _monkey_patch_webbased_functions(monkeypatch, data_id)
     glas2 = assert_warns_message(
         UserWarning, "Version 1 of dataset glass2 is inactive,", fetch_openml,
-        40675)
+        data_id, cache=False)
     # fetch inactive dataset by name and version
     assert glas2.data.shape == (163, 9)
     glas2_by_version = assert_warns_message(
         UserWarning, "Version 1 of dataset glass2 is inactive,", fetch_openml,
-        None, "glass2", 1)
-    assert glas2_by_version.details['id'] == '40675'
+        None, "glass2", 1, cache=False)
+    assert int(glas2_by_version.details['id']) == data_id
 
 
-def test_fetch_nonexiting():
+def test_fetch_nonexiting(monkeypatch):
     # makes contact with openml server. not mocked
     # there is no active version of glass2
+    data_id = 40675
+    _monkey_patch_webbased_functions(monkeypatch, data_id)
     assert_raise_message(ValueError, "No active dataset glass2 found",
-                         fetch_openml, None, 'glass2')
+                         fetch_openml, None, 'glass2', cache=False)
 
 
 def test_fetch_openml_raises_illegal_argument():
