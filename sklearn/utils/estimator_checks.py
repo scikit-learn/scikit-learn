46913adf0757d1a6cae3fff0210a973e9d995bac
absolutelyNoWarranty <absolutelyNoWarranty@users.noreply.github.com>
2018-07-16 23:58:55 +0800

    [MRG+2] Add common test for set_params behavior (#7760)
    * Add set_param check and test
    
    Add check_set_params to test_common
    Add assert_is
    
    * Drop extraneous clone
    
    * Put get_params outside loop
    
    * Use more readable name new_params
    
    * Reset params dict every loop
    
    * Make test_values much shorter
    
    * Fix flake8 line too long
    
    * Add extra dict check
    
    * Use deep=False
    
    * Order invariant comparison of dictionary keys
    
    * Raise warnings
    
    * Edit whats new
    
    * Double backticks
    
    * Remove broken link
    
    * fix flake8
    
    * Rewrite whats_new to not start with :
    
    * Fix links to check_estimator
    
    * TST: remove assert_is
    
    No longer needed as we are using pytests

--- sklearn/utils/estimator_checks.py
+++ sklearn/utils/estimator_checks.py
@@ -261,6 +261,7 @@ def _yield_all_checks(name, estimator):
     yield check_fit2d_1feature
     yield check_fit1d
     yield check_get_params_invariance
+    yield check_set_params
     yield check_dict_unchanged
     yield check_dont_overwrite_parameters
 
@@ -2178,6 +2179,59 @@ def check_get_params_invariance(name, estimator_orig):
 
     assert_true(all(item in deep_params.items() for item in
                     shallow_params.items()))
+
+
+@ignore_warnings(category=(DeprecationWarning, FutureWarning))
+def check_set_params(name, estimator_orig):
+    # Check that get_params() returns the same thing
+    # before and after set_params() with some fuzz
+    estimator = clone(estimator_orig)
+
+    orig_params = estimator.get_params(deep=False)
+    msg = ("get_params result does not match what was passed to set_params")
+
+    estimator.set_params(**orig_params)
+    curr_params = estimator.get_params(deep=False)
+    assert_equal(set(orig_params.keys()), set(curr_params.keys()), msg)
+    for k, v in curr_params.items():
+        assert orig_params[k] is v, msg
+
+    # some fuzz values
+    test_values = [-np.inf, np.inf, None]
+
+    test_params = deepcopy(orig_params)
+    for param_name in orig_params.keys():
+        default_value = orig_params[param_name]
+        for value in test_values:
+            test_params[param_name] = value
+            try:
+                estimator.set_params(**test_params)
+            except (TypeError, ValueError) as e:
+                e_type = e.__class__.__name__
+                # Exception occurred, possibly parameter validation
+                warnings.warn("{} occurred during set_params. "
+                              "It is recommended to delay parameter "
+                              "validation until fit.".format(e_type))
+
+                change_warning_msg = "Estimator's parameters changed after " \
+                                     "set_params raised {}".format(e_type)
+                params_before_exception = curr_params
+                curr_params = estimator.get_params(deep=False)
+                try:
+                    assert_equal(set(params_before_exception.keys()),
+                                 set(curr_params.keys()))
+                    for k, v in curr_params.items():
+                        assert params_before_exception[k] is v
+                except AssertionError:
+                    warnings.warn(change_warning_msg)
+            else:
+                curr_params = estimator.get_params(deep=False)
+                assert_equal(set(test_params.keys()),
+                             set(curr_params.keys()),
+                             msg)
+                for k, v in curr_params.items():
+                    assert test_params[k] is v, msg
+        test_params[param_name] = default_value
 
 
 @ignore_warnings(category=(DeprecationWarning, FutureWarning))
