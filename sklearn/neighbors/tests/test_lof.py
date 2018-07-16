37e4bf0cfb244f07458d842a14916d630a21377f
Albert Thomas <albertthomas88@gmail.com>
2018-07-16 09:53:56 +0200

    add non regression tests checking availability of prediction methods depending on the novelty parameter

--- sklearn/neighbors/tests/test_lof.py
+++ sklearn/neighbors/tests/test_lof.py
@@ -186,6 +186,27 @@ def test_novelty_training_scores():
     assert_array_almost_equal(scores_1, scores_2)
 
 
+def test_hasattr_prediction():
+    # check availability of prediction methods depending on novelty value.
+    X = [[1, 1], [1, 2], [2, 1]]
+
+    # when novelty=True
+    clf = neighbors.LocalOutlierFactor(novelty=True)
+    clf.fit(X)
+    assert hasattr(clf, 'predict')
+    assert hasattr(clf, 'decision_function')
+    assert hasattr(clf, 'score_samples')
+    assert not hasattr(clf, 'fit_predict')
+
+    # when novelty=False
+    clf = neighbors.LocalOutlierFactor(novelty=False)
+    clf.fit(X)
+    assert hasattr(clf, 'fit_predict')
+    assert not hasattr(clf, 'predict')
+    assert not hasattr(clf, 'decision_function')
+    assert not hasattr(clf, 'score_samples')
+
+
 def test_novelty_true_common_tests():
     # the common tests are run for the default LOF (novelty=False).
     # here we run these common tests for LOF when novelty=True
