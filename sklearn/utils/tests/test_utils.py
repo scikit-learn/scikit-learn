d3149848f04cf00d47dec40445de086765bd5ab6
Ivan PANICO <ivpanico@gmail.com>
2018-07-16 16:37:15 +0200

    change approx for allclose to pass travis

--- sklearn/utils/tests/test_utils.py
+++ sklearn/utils/tests/test_utils.py
@@ -286,5 +286,5 @@ def test_init_arpack_v0():
             assert not any(np.equal(v0s[i], v0s[i-1]))
 
     v0 = np.concatenate(v0s)
-    assert np.mean(v0) == approx(0, abs=1e-2)
-    assert np.std(v0) == approx(1/np.sqrt(3), abs=1e-3)
+    assert np.allclose(np.mean(v0), 0, atol=1e-2)
+    assert np.allclose(np.std(v0), 1/np.sqrt(3), atol=1e-3)
