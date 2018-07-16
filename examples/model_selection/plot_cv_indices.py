f6a3b4a5717d532680365ccfe293edb78b56acb1
Chris Holdgraf <choldgraf@berkeley.edu>
2018-07-16 14:37:09 -0700

    cv indices example down to one dataset

--- examples/model_selection/plot_cv_indices.py
+++ examples/model_selection/plot_cv_indices.py
@@ -17,38 +17,49 @@ from sklearn.model_selection import (TimeSeriesSplit, KFold, ShuffleSplit,
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Patch
+np.random.seed(1337)
+cmap_data = plt.cm.tab10
+cmap_cv = plt.cm.coolwarm
 
 ###############################################################################
 # Visualize our data
 # ------------------
 #
-# First, we must understand the structure of our data. We'll take a look at
-# several datasets. They'll each have 100 randomly generated datapoints,
-# but different "labels" that assign the data to groups.
+# First, we must understand the structure of our data. It has 100 randomly
+# generated input datapoints, 5 labels split unevenly across datapoints,
+# and five "groups" split unevenly across datapoints.
 #
 # As we'll see, some cross-validation objects do specific things with
-# labeled data, while others do not.
+# labeled data, others behave differently with grouped data, and others
+# do not use this information.
 #
-# To begin, we'll use a dataset in which each datapoint belongs to the same
-# group (or, put another way, there are no groups).
+# To begin, create an visualize our data
 
+# Generate the label/group data
 n_points = 100
 X = np.random.randn(100, 10)
-y = np.ones(100)
-y[50:] = 0
 
-groups = np.ones(n_points)
+percentiles_labels = [.1, .2, .3, .4]
+y = np.hstack([[ii] * int(100 * perc)
+               for ii, perc in enumerate(percentiles_labels)])
 
-def visualize_groups(groups, name):
+# Evenly spaced groups repeated once
+groups = np.hstack([[ii] * 10 for ii in range(5)])
+groups = np.hstack([groups, groups])
+
+
+def visualize_groups(labels, groups, name):
     # Visualize dataset groups
     fig, ax = plt.subplots()
     ax.scatter(range(len(groups)),  [.5] * len(groups), c=groups, marker='_',
-               lw=50, cmap='rainbow')
-    ax.set(ylim=[-3, 4], title="Data groups (color = label)\n{}".format(name),
-           xlabel="Sample index")
-    plt.setp(ax.get_yticklabels() + ax.yaxis.majorTicks, visible=False)
+               lw=50, cmap=cmap_data)
+    ax.scatter(range(len(groups)),  [3.5] * len(groups), c=labels, marker='_',
+               lw=50, cmap=cmap_data)
+    ax.set(ylim=[-1, 5], yticks=[.5, 3.5],
+           yticklabels=['Data groups', 'Data label'], xlabel="Sample index")
 
-visualize_groups(groups, 'no groups')
+
+visualize_groups(y, groups, 'no groups')
 
 ###############################################################################
 # Define a function to visualize cross-validation behavior
@@ -58,6 +69,7 @@ visualize_groups(groups, 'no groups')
 # cross-validation object. We'll perform 5 splits of the data. On each
 # split, we'll visualize the indices chosen for the training set
 # (in blue) and the test set (in red).
+
 
 def plot_cv_indices(cv, X, y, group, ax, n_splits, lw=10):
     """Create a sample plot for indices of a cross-validation object."""
@@ -71,19 +83,21 @@ def plot_cv_indices(cv, X, y, group, ax, n_splits, lw=10):
 
         # Visualize the results
         ax.scatter(range(len(indices)), [ii + .5] * len(indices),
-                   c=indices, marker='_', lw=lw, cmap=plt.cm.coolwarm,
+                   c=indices, marker='_', lw=lw, cmap=cmap_cv,
                    vmin=-.2, vmax=1.2)
 
-
-    # Plot the data at the end
+    # Plot the data labels and groups at the end
     ax.scatter(range(len(X)), [ii + 1.5] * len(X),
-               c=group, marker='_', lw=lw, cmap=plt.cm.rainbow)
+               c=y, marker='_', lw=lw, cmap=cmap_data)
+
+    ax.scatter(range(len(X)), [ii + 2.5] * len(X),
+               c=group, marker='_', lw=lw, cmap=cmap_data)
 
     # Formatting
-    yticklabels = list(range(n_splits)) + ['groups']
-    ax.set(yticks=np.arange(n_splits+1) + .5, yticklabels=yticklabels,
+    yticklabels = list(range(n_splits)) + ['labels', 'groups']
+    ax.set(yticks=np.arange(n_splits+2) + .5, yticklabels=yticklabels,
            xlabel='Sample index', ylabel="CV iteration",
-           ylim=[n_splits+1.2, -.2], xlim=[0, 100],
+           ylim=[n_splits+2.2, -.2], xlim=[0, 100],
            title='{}'.format(type(cv).__name__))
     return ax
 
@@ -92,11 +106,24 @@ def plot_cv_indices(cv, X, y, group, ax, n_splits, lw=10):
 # Let's see how it looks for the `KFold` cross-validation object:
 
 fig, ax = plt.subplots()
-n_splits = 5
+n_splits = 4
 cv = KFold(n_splits)
 plot_cv_indices(cv, X, y, groups, ax, n_splits)
 
 ###############################################################################
+# As you can see, by default the KFold cross-validation iterator does not
+# take either datapoint label or group into consideration. We can change this
+# by using the ``StratifiedKFold`` like so.
+
+fig, ax = plt.subplots()
+cv = StratifiedKFold(n_splits)
+plot_cv_indices(cv, X, y, groups, ax, n_splits)
+
+###############################################################################
+# In this case, the cross-validation retained the same ratio of labels across
+# each CV split. Next we'll visualize this behavior for a number of CV
+# iterators.
+#
 # Visualize cross-validation indices for many CV objects
 # ------------------------------------------------------
 #
@@ -104,90 +131,19 @@ plot_cv_indices(cv, X, y, groups, ax, n_splits)
 # scikit-learn cross-validation objects. Below we will loop through several
 # common cross-validation objects, visualizing the behavior of each.
 #
-# In this case, there is only a single group, so grouping behavior won't
-# really apply.
+# Note how some use the group/label information while others do not.
 
-cvs = [ShuffleSplit(n_splits=5), KFold(n_splits=5),
-       TimeSeriesSplit(n_splits=5)]
+cvs = [KFold(n_splits=n_splits), GroupKFold(n_splits=n_splits),
+       ShuffleSplit(n_splits=n_splits), StratifiedKFold(n_splits=n_splits),
+       GroupShuffleSplit(n_splits=n_splits),
+       TimeSeriesSplit(n_splits=n_splits)]
 
 
-fig, axs = plt.subplots(len(cvs), 1, figsize=(6, 3*len(cvs)), sharex=True)
-for ax, cv in zip(axs, cvs):
+for cv in cvs:
+    fig, ax = plt.subplots(figsize=(6, 3))
     plot_cv_indices(cv, X, y, groups, ax, n_splits)
 
-cmap = plt.cm.coolwarm
-axs[-1].legend([Patch(color=cmap(.8)), Patch(color=cmap(.2))],
-               ['Testing set', 'Training set'], loc=(.7, .8))
-plt.setp([ax for ax in axs[1:-1]], xlabel='')
-plt.tight_layout()
-
-###############################################################################
-# Using data with balanced groups
-# -------------------------------
-#
-# Next we'll take a look at some data that has several groups, each with the
-# same number of members. Here's what the data look like.
-
-groups_even = np.hstack([[ii] * 10 for ii in range(5)])
-groups_even = np.hstack([groups_even, groups_even])
-y = groups_even
-visualize_groups(groups_even, 'balanced groups')
-
-###############################################################################
-# We'll visualize these groups with several cross-validation objects that
-# are relevant to grouped data.
-#
-# Note that some keep groups together, while others ignore
-# label identity completely. Some have overlapping test sets between CV
-# splits, while others do not.
-
-
-cvs = [ShuffleSplit(n_splits=5), GroupShuffleSplit(n_splits=5),
-       KFold(n_splits=5), GroupKFold(n_splits=5), StratifiedKFold(n_splits=5),
-       TimeSeriesSplit(n_splits=5)]
-
-
-fig, axs = plt.subplots(len(cvs), 1, figsize=(6, 3*len(cvs)), sharex=True)
-for ax, cv in zip(axs, cvs):
-    plot_cv_indices(cv, X, y, groups_even, ax, n_splits)
-
-axs[-1].legend([Patch(color=cmap(.8)), Patch(color=cmap(.2))],
-               ['Testing set', 'Training set'], loc=(.7, .8))
-plt.setp([ax for ax in axs[1:-1]], xlabel='')
-plt.tight_layout()
-
-###############################################################################
-# Using data in with imbalanced groups
-# ------------------------------------
-#
-# Finally, let's see how these cross-validation objects behave with imbalanced
-# groups.
-
-percentiles = [.05, .1, .15, .2, .5]
-groups_imbalanced = np.hstack([[ii] * int(100 * perc)
-                               for ii, perc in enumerate(percentiles)])
-y = groups_imbalanced
-visualize_groups(groups_imbalanced, 'imbalanced groups')
-
-###############################################################################
-# We'll visualize these groups with several cross-validation objects that
-# are relevant to grouped data with imbalanced groups.
-#
-# Several scikit-learn CV objects take special consideration to maintain
-# the ratio of group membership present in the data.
-
-
-cvs = [ShuffleSplit(n_splits=5), GroupShuffleSplit(n_splits=5),
-       KFold(n_splits=5), GroupKFold(n_splits=5), StratifiedKFold(n_splits=5),
-       TimeSeriesSplit(n_splits=5)]
-
-
-fig, axs = plt.subplots(len(cvs), 1, figsize=(6, 3*len(cvs)), sharex=True)
-for ax, cv in zip(axs, cvs):
-    plot_cv_indices(cv, X, y, groups_imbalanced, ax, n_splits)
-
-axs[-1].legend([Patch(color=cmap(.8)), Patch(color=cmap(.2))],
-               ['Testing set', 'Training set'], loc=(.7, .8))
-plt.setp([ax for ax in axs[1:-1]], xlabel='')
-plt.tight_layout()
+    ax.legend([Patch(color=cmap_cv(.8)), Patch(color=cmap_cv(.2))],
+              ['Testing set', 'Training set'], loc=(1.02, .8))
+    plt.tight_layout()
 plt.show()
