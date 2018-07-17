6e987cc0e4d10d5ce167bd88474df16e7d1005a0
Loïc Estève <loic.esteve@ymail.com>
2018-07-18 01:29:27 +0200

    TST skip test_deprecated_grid_scores on Python 2.
    For some unknown reasons, the DeprecationWarning is not issued on Python 2.

--- sklearn/covariance/tests/test_graphical_lasso.py
+++ sklearn/covariance/tests/test_graphical_lasso.py
@@ -16,6 +16,7 @@ from sklearn.datasets.samples_generator import make_sparse_spd_matrix
 from sklearn.externals.six.moves import StringIO
 from sklearn.utils import check_random_state
 from sklearn import datasets
+from sklearn.utils.fixes import PY3_OR_LATER
 
 from numpy.testing import assert_equal
 
@@ -137,6 +138,8 @@ def test_graphical_lasso_cv(random_state=1):
     GraphicalLassoCV(alphas=[0.8, 0.5], tol=1e-1, n_jobs=1).fit(X)
 
 
+@pytest.mark.skipif(not PY3_OR_LATER,
+                    reason='On Python 2 DeprecationWarning is not issued for some unkown reason.')
 def test_deprecated_grid_scores(random_state=1):
     dim = 5
     n_samples = 6
