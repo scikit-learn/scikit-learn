31962d2d0e897985ae2bf5ac4d278419f2ddb88e
Alexandre Boucaud <boucaud.alexandre@gmail.com>
2018-07-18 16:54:28 +0200

    update unit tests

--- sklearn/utils/tests/test_print_versions.py
+++ sklearn/utils/tests/test_print_versions.py
@@ -1,26 +1,24 @@
-import pytest
 
-from sklearn.utils._print_versions import get_sys_info
-from sklearn.utils._print_versions import get_deps_info
+from sklearn.utils._print_versions import _get_sys_info
+from sklearn.utils._print_versions import _get_deps_info
 from sklearn.utils._print_versions import show_versions
 
 
 def test_get_sys_info():
-    sys_info = get_sys_info()
+    sys_info = _get_sys_info()
 
     assert 'python' in sys_info
-    assert 'OS' in sys_info
+    assert 'executable' in sys_info
+    assert 'compiler' in sys_info
     assert 'machine' in sys_info
-    assert 'processor' in sys_info
-    assert 'LANG' in sys_info
-    assert 'LOCALE' in sys_info
 
 
 def test_get_deps_info():
-    deps_info = get_deps_info()
+    deps_info = _get_deps_info()
 
     assert 'pip' in deps_info
     assert 'setuptools' in deps_info
+    assert 'sklearn' in deps_info
     assert 'numpy' in deps_info
     assert 'scipy' in deps_info
     assert 'Cython' in deps_info
@@ -28,15 +26,8 @@ def test_get_deps_info():
     assert 'matplotlib' in deps_info
 
 
-def test_show_versions(capsys):
-    show_versions()
-    out, err = capsys.readouterr()
-    assert 'python' in out
-    assert 'numpy' in out
-
-
 def test_show_versions_with_blas(capsys):
-    show_versions(with_blas=True)
+    show_versions()
     out, err = capsys.readouterr()
     assert 'python' in out
     assert 'numpy' in out
