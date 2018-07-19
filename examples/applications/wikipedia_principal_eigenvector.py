be841e554cdf3c723f72ecf6c8a96c3fa497d980
ZJ Poh <jabryth@gmail.com>
2018-07-18 22:39:41 -0700

    Fix PEP8

--- examples/applications/wikipedia_principal_eigenvector.py
+++ examples/applications/wikipedia_principal_eigenvector.py
@@ -206,7 +206,7 @@ def centrality_scores(X, alpha=0.85, max_iter=100, tol=1e-10):
         X.data[X.indptr[i]:X.indptr[i + 1]] *= 1.0 / incoming_counts[i]
     dangle = np.asarray(np.where(X.sum(axis=1) == 0, 1.0 / n, 0)).ravel()
 
-    scores = np.full(n, 1./ n, dtype=np.float32)  # initial guess
+    scores = np.full(n, 1. / n, dtype=np.float32)  # initial guess
     for i in range(max_iter):
         print("power iteration #%d" % i)
         prev_scores = scores
