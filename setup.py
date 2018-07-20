1984dac41990e783d6ba0f510fdf7f5ba5789c24
Joel Nothman <joel.nothman@gmail.com>
2018-07-21 02:11:20 +1000

    MAINT CI [scipy-dev] now uses python 3.7 (#11409)

--- setup.py
+++ setup.py
@@ -189,6 +189,7 @@ def setup_package():
                                  'Programming Language :: Python :: 3.4',
                                  'Programming Language :: Python :: 3.5',
                                  'Programming Language :: Python :: 3.6',
+                                 'Programming Language :: Python :: 3.7',
                                  ('Programming Language :: Python :: '
                                   'Implementation :: CPython'),
                                  ('Programming Language :: Python :: '
