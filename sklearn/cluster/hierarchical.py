a8b6cabb45b5d137ba70a12be630d93b186c7d42
adrin jalali <anjalali@ancud.de>
2018-07-21 14:31:17 +0200

    show transform for featureagglomeratoin

--- sklearn/cluster/hierarchical.py
+++ sklearn/cluster/hierarchical.py
@@ -920,13 +920,29 @@ class FeatureAgglomeration(AgglomerativeClustering, AgglomerationTransform):
 
     Examples
     --------
-    >>> from sklearn.cluster import FeatureAgglomeration
     >>> import numpy as np
-    >>> X = np.array([[1, 5], [2, 4], [2, 4],
-    ...               [1, 7], [2, 8], [3, 8]])
-    >>> clustering = FeatureAgglomeration(n_clusters=2).fit(X)
-    >>> clustering.labels_
-    array([1, 0])
+    >>> from sklearn import datasets, cluster
+    >>> from sklearn.linear_model import SGDClassifier
+    >>> #from sklearn.feature_extraction.image import grid_to_graph
+    >>> digits = datasets.load_digits()
+    >>> images = digits.images
+    >>> X = np.reshape(images, (len(images), -1))
+    >>> agglo = cluster.FeatureAgglomeration(n_clusters=32)
+    >>> agglo.fit(X)
+    FeatureAgglomeration(affinity='euclidean', compute_full_tree='auto',
+               connectivity=None, linkage='ward', memory=None, n_clusters=32,
+               pooling_func=<function mean at 0x7f5aa2e6e158>)
+    >>> X_reduced = agglo.transform(X)
+    >>> clf = SGDClassifier(max_iter=5, random_state=0)
+    >>> clf.fit(X_reduced, digits.target) # doctest: +NORMALIZE_WHITESPACE
+    SGDClassifier(alpha=0.0001, average=False, class_weight=None,
+           early_stopping=False, epsilon=0.1, eta0=0.0, fit_intercept=True,
+           l1_ratio=0.15, learning_rate='optimal', loss='hinge', max_iter=5,
+           n_iter=None, n_iter_no_change=5, n_jobs=1, penalty='l2',
+           power_t=0.5, random_state=0, shuffle=True, tol=None,
+           validation_fraction=0.1, verbose=0, warm_start=False)
+    >>> clf.score(X_reduced, digits.target) # doctest: +ELLIPSIS
+    0.9226...
     """
 
     def __init__(self, n_clusters=2, affinity="euclidean",
