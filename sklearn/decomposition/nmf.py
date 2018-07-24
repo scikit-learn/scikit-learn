9caee6267772ce89f64da9c4021a6c19a5d1de80
ZJ Poh <jabryth@gmail.com>
2018-07-23 22:24:38 -0700

    Reverted pydoc changes

--- sklearn/decomposition/nmf.py
+++ sklearn/decomposition/nmf.py
@@ -1193,12 +1193,9 @@ class NMF(BaseEstimator, TransformerMixin):
     >>> import numpy as np
     >>> X = np.array([[1, 1], [2, 1], [3, 1.2], [4, 1], [5, 0.8], [6, 1]])
     >>> from sklearn.decomposition import NMF
-    >>> X = np.random.random((1, 3))
-    >>> model = NMF(n_components=2, init=None, random_state=0)
-    >>> #NMF(2, init='random').fit_transform(np.random.random((1, 3)))
+    >>> model = NMF(n_components=2, init='random', random_state=0)
     >>> W = model.fit_transform(X)
     >>> H = model.components_
-    >>> print(X, '\n', np.dot(W, H))
 
     References
     ----------
