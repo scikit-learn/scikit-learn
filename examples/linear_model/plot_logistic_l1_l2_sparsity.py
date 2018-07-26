202f900165cd927c3fd447ff5e40f2e720a0f1d5
Nicolas Hug <contact@nicolas-hug.com>
2018-07-26 13:49:39 -0400

    Updated example to plot elastic net sparsity

--- examples/linear_model/plot_logistic_l1_l2_sparsity.py
+++ examples/linear_model/plot_logistic_l1_l2_sparsity.py
@@ -4,10 +4,11 @@ L1 Penalty and Sparsity in Logistic Regression
 ==============================================
 
 Comparison of the sparsity (percentage of zero coefficients) of solutions when
-L1 and L2 penalty are used for different values of C. We can see that large
-values of C give more freedom to the model.  Conversely, smaller values of C
-constrain the model more. In the L1 penalty case, this leads to sparser
-solutions.
+L1, L2 and elastic-net penalty are used for different values of C. We can see
+that large values of C give more freedom to the model.  Conversely, smaller
+values of C constrain the model more. In the L1 penalty case, this leads to
+sparser solutions. As expected, the elastic-net penalty sparsity is between
+that of L1 and L2.
 
 We classify 8x8 images of digits into two classes: 0-4 against 5-9.
 The visualization shows coefficients of the models for varying C.
@@ -41,39 +42,56 @@ for i, C in enumerate((100, 1, 0.01)):
     # turn down tolerance for short training time
     clf_l1_LR = LogisticRegression(C=C, penalty='l1', tol=0.01)
     clf_l2_LR = LogisticRegression(C=C, penalty='l2', tol=0.01)
+    clf_EN_LR = LogisticRegression(C=C, l1_ratio=.3, penalty='elastic-net',
+                                   solver='saga', tol=0.01)
     clf_l1_LR.fit(X, y)
     clf_l2_LR.fit(X, y)
+    clf_EN_LR.fit(X, y)
 
     coef_l1_LR = clf_l1_LR.coef_.ravel()
     coef_l2_LR = clf_l2_LR.coef_.ravel()
+    coef_EN_LR = clf_EN_LR.coef_.ravel()
 
     # coef_l1_LR contains zeros due to the
     # L1 sparsity inducing norm
 
     sparsity_l1_LR = np.mean(coef_l1_LR == 0) * 100
     sparsity_l2_LR = np.mean(coef_l2_LR == 0) * 100
+    sparsity_EN_LR = np.mean(coef_EN_LR == 0) * 100
 
     print("C=%.2f" % C)
-    print("Sparsity with L1 penalty: %.2f%%" % sparsity_l1_LR)
-    print("score with L1 penalty: %.4f" % clf_l1_LR.score(X, y))
-    print("Sparsity with L2 penalty: %.2f%%" % sparsity_l2_LR)
-    print("score with L2 penalty: %.4f" % clf_l2_LR.score(X, y))
+    print("{:<40} {:.2f}%".format("Sparsity with L1 penalty:", sparsity_l1_LR))
+    print("{:<40} {:.2f}%".format("Sparsity with L2 penalty:", sparsity_l2_LR))
+    print("{:<40} {:.2f}%".format("Sparsity with elastic-net penalty:",
+                                 sparsity_EN_LR))
+    print("{:<40} {:.2f}".format("Score with L1 penalty:",
+                                  clf_l1_LR.score(X, y)))
+    print("{:<40} {:.2f}".format("Score with L2 penalty:",
+                                  clf_l2_LR.score(X, y)))
+    print("{:<40} {:.2f}".format("Score with elastic-net penalty:",
+                                  clf_EN_LR.score(X, y)))
 
-    l1_plot = plt.subplot(3, 2, 2 * i + 1)
-    l2_plot = plt.subplot(3, 2, 2 * (i + 1))
+    l1_plot = plt.subplot(3, 3, 3 * i + 1)
+    l2_plot = plt.subplot(3, 3, 3 * i + 2)
+    en_plot = plt.subplot(3, 3, 3 * i + 3)
     if i == 0:
         l1_plot.set_title("L1 penalty")
         l2_plot.set_title("L2 penalty")
+        en_plot.set_title("Elastic-net with l1_ratio = 0.3")
 
     l1_plot.imshow(np.abs(coef_l1_LR.reshape(8, 8)), interpolation='nearest',
                    cmap='binary', vmax=1, vmin=0)
     l2_plot.imshow(np.abs(coef_l2_LR.reshape(8, 8)), interpolation='nearest',
                    cmap='binary', vmax=1, vmin=0)
-    plt.text(-8, 3, "C = %.2f" % C)
+    en_plot.imshow(np.abs(coef_EN_LR.reshape(8, 8)), interpolation='nearest',
+                   cmap='binary', vmax=1, vmin=0)
+    plt.text(-40, 3, "C = %.2f" % C)
 
     l1_plot.set_xticks(())
     l1_plot.set_yticks(())
     l2_plot.set_xticks(())
     l2_plot.set_yticks(())
+    en_plot.set_xticks(())
+    en_plot.set_yticks(())
 
 plt.show()
