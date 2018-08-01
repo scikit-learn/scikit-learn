e85e8f1b2c8a40bbfcaed632cc47ccbdfcd811bc
adrin jalali <anjalali@ancud.de>
2018-08-01 17:48:26 +0200

    fix number representations

--- sklearn/linear_model/ridge.py
+++ sklearn/linear_model/ridge.py
@@ -1362,7 +1362,7 @@ class RidgeClassifierCV(LinearClassifierMixin, _BaseRidgeCV):
     >>> from sklearn.datasets import load_breast_cancer
     >>> from sklearn.linear_model import RidgeClassifierCV
     >>> X, y = load_breast_cancer(return_X_y=True)
-    >>> clf = RidgeClassifierCV(alphas=[1, .01, .001, .0001]).fit(X, y)
+    >>> clf = RidgeClassifierCV(alphas=[1e-3, 1e-2, 1e-1, 1]).fit(X, y)
     >>> clf.score(X, y) # doctest: +ELLIPSIS
     0.9630...
 
