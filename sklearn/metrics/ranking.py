66fe397024a0e9ceeb39fa90d04d53677ce5cac0
ZJ Poh <jabryth@gmail.com>
2018-07-31 22:58:12 -0700

    Initial change to open PR

--- sklearn/metrics/ranking.py
+++ sklearn/metrics/ranking.py
@@ -242,7 +242,7 @@ def average_precision_score(y_true, y_score, average="macro", pos_label=1,
 
 
 def roc_auc_score(y_true, y_score, average="macro", sample_weight=None,
-                  max_fpr=None):
+                  max_fpr=None, min_tpr=None):
     """Compute Area Under the Receiver Operating Characteristic Curve (ROC AUC)
     from prediction scores.
 
