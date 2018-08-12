f28306aa1faffd50d5a1bfd5690d65d355e58238
Anubhav Marwaha <33462670+Anubhav2000@users.noreply.github.com>
2018-08-12 06:25:03 -0700

    Update Perceptron.py

--- sklearn/linear_model/perceptron.py
+++ sklearn/linear_model/perceptron.py
@@ -125,7 +125,7 @@ class Perceptron(BaseSGDClassifier):
     >>> data=digits.data
     >>> label=digits.target
     >>> X_train,X_test,y_train,y_test=train_test_split(data,label,random_state=123)
-    >>> model=perceptron.Perceptron(penalty='l2',alpha=0.001,random_state=124) #Applying l2 regularization with alpha=0.001
+    >>> model=perceptron.Perceptron(penalty='l2',alpha=0.001,random_state=124,max_iter=600,tol=0.23) #Applying l2 regularization with alpha=0.001
     >>> model.fit(X_train,y_train)
     
         Perceptron(alpha=0.001, class_weight=None, eta0=1.0, fit_intercept=True,
