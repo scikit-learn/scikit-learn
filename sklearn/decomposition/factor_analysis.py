3e879b6d6d8000b3a5851b10c5f0e43419a02b6e
adrin jalali <anjalali@ancud.de>
2018-08-13 17:04:42 +0200

    add missing import

--- sklearn/decomposition/factor_analysis.py
+++ sklearn/decomposition/factor_analysis.py
@@ -110,6 +110,7 @@ class FactorAnalysis(BaseEstimator, TransformerMixin):
 
     Examples
     --------
+    >>> from sklearn.datasets import load_digits
     >>> from sklearn.decomposition import FactorAnalysis
     >>> X, _ = load_digits(return_X_y=True)
     >>> transformer = FactorAnalysis(n_components=7, random_state=0)
