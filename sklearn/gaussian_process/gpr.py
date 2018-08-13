f661ffaaf03c5630980a473533d83e4c346a57ed
adrin jalali <anjalali@ancud.de>
2018-08-13 10:43:33 +0200

    remove extra line

--- sklearn/gaussian_process/gpr.py
+++ sklearn/gaussian_process/gpr.py
@@ -138,7 +138,6 @@ class GaussianProcessRegressor(BaseEstimator, RegressorMixin):
     >>> from sklearn.gaussian_process import GaussianProcessRegressor
     >>> from sklearn.gaussian_process.kernels import DotProduct, WhiteKernel
     >>> X, y = make_friedman2(n_samples=500, noise=0, random_state=0)
-    >>> #kernel = DotProduct(sigma_0_bounds=(1e-3, 1e3))
     >>> kernel = DotProduct() + WhiteKernel()
     >>> gpr = GaussianProcessRegressor(kernel=kernel,
     ...         random_state=0).fit(X, y)
