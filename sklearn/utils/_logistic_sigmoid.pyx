8603cdef49a4a7c12be64f153c68518adcb2f57f
Hanmin Qin <qinhanmin2005@sina.com>
2018-08-18 19:26:37 +0800

    MNT log1p -> log in cython files

--- sklearn/utils/_logistic_sigmoid.pyx
+++ sklearn/utils/_logistic_sigmoid.pyx
@@ -2,7 +2,7 @@
 #cython: cdivision=True
 #cython: wraparound=False
 
-from libc.math cimport log1p, exp
+from libc.math cimport log, exp
 
 import numpy as np
 cimport numpy as np
@@ -13,9 +13,9 @@ ctypedef np.float64_t DTYPE_t
 cdef DTYPE_t _inner_log_logistic_sigmoid(DTYPE_t x):
     """Log of the logistic sigmoid function log(1 / (1 + e ** -x))"""
     if x > 0:
-        return -log1p(exp(-x))
+        return -log(1 + exp(-x))
     else:
-        return x - log1p(exp(x))
+        return x - log(1 + exp(x))
 
 
 def _log_logistic_sigmoid(int n_samples, int n_features, 
