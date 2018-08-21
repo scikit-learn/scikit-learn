c11206dfb9d320a261b05c2c4cb7696ec8893464
Max Copeland <maxcopeland88@gmail.com>
2018-08-21 12:36:32 -0700

    [MRG] Added 'l2' as acceptable input for loss in l1_min_c() (#11871)
    * Adding 'l2' as acceptable input for loss in l1_min_c()
    
    * removed "l2" from error msg in l1_min_c()
    
    removed `"l2"` from error msg in l1_min_c(), consistent with acceptable `loss` params

--- sklearn/svm/bounds.py
+++ sklearn/svm/bounds.py
@@ -52,7 +52,7 @@ def l1_min_c(X, y, loss='squared_hinge', fit_intercept=True,
         minimum value for C
     """
     if loss not in ('squared_hinge', 'log'):
-        raise ValueError('loss type not in ("squared_hinge", "log", "l2")')
+        raise ValueError('loss type not in ("squared_hinge", "log")')
 
     X = check_array(X, accept_sparse='csc')
     check_consistent_length(X, y)
