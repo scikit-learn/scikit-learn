824e0ccc9c72c752957873e287afe7dcd5afaf84
agamemnonc <agamemnon.krasoulis@gmail.com>
2018-08-21 14:18:05 +0100

    test_lda_predict_proba non-regression test

--- sklearn/tests/test_discriminant_analysis.py
+++ sklearn/tests/test_discriminant_analysis.py
@@ -91,6 +91,23 @@ def test_lda_predict():
     clf = LinearDiscriminantAnalysis(solver="dummy")
     assert_raises(ValueError, clf.fit, X, y)
 
+def test_lda_predict_proba():
+    # Test LDA posterior probabilities
+    # Binary case
+    clf = LinearDiscriminantAnalysis(solver='svd').fit(X6,y6)
+    probas = clf.predict_proba(X6)
+    assert_array_almost_equal(
+        probas[0],
+        np.array([0.91942108, 0.08057892])
+    )
+    # Multiclass case
+    clf = LinearDiscriminantAnalysis(solver='svd').fit(X6,y7)
+    probas = clf.predict_proba(X6)
+    assert_array_almost_equal(
+        probas[0],
+        np.array([0.25128617, 0.36876296, 0.37995087])
+    )
+
 
 def test_lda_priors():
     # Test priors (negative priors)
