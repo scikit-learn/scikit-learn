354a57a83ad7981ee4ba9dbea526872c9af797f3
Thomas Fan <thomasjpfan@gmail.com>
2018-08-23 12:02:05 -0400

    ENH: Adds detail to warning

--- sklearn/pipeline.py
+++ sklearn/pipeline.py
@@ -164,14 +164,16 @@ class Pipeline(_BaseComposition):
             if (not (hasattr(t, "fit") or hasattr(t, "fit_transform")) or not
                     hasattr(t, "transform")):
                 raise TypeError("All intermediate steps should be "
-                                "transformers and implement fit and transform."
-                                " '%s' (type %s) doesn't" % (t, type(t)))
+                                "transformers and implement fit and transform "
+                                "or be the string 'passthrough' "
+                                "'%s' (type %s) doesn't" % (t, type(t)))
 
         # We allow last estimator to be None as an identity transformation
         if (estimator is not None and estimator != 'passthrough'
                 and not hasattr(estimator, "fit")):
             raise TypeError(
-                "Last step of Pipeline should implement fit. "
+                "Last step of Pipeline should implement fit "
+                "or be the string 'passthrough'. "
                 "'%s' (type %s) doesn't" % (estimator, type(estimator)))
 
     def _iter(self, with_final=True):
