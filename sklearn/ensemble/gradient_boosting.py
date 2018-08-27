39f11d6dc91cca579a6af9ae3515602bbd7eb975
Nicolas Hug <contact@nicolas-hug.com>
2018-08-27 10:57:49 -0400

    Mimimal refactoring of gradient_boosting.py

--- sklearn/ensemble/gradient_boosting.py
+++ sklearn/ensemble/gradient_boosting.py
@@ -1190,22 +1190,14 @@ class BaseGradientBoosting(six.with_metaclass(ABCMeta, BaseEnsemble)):
                 # no inplace multiplication!
                 sample_weight = sample_weight * sample_mask.astype(np.float64)
 
-            if X_csc is not None:
-                tree.fit(X_csc, residual, sample_weight=sample_weight,
-                         check_input=False, X_idx_sorted=X_idx_sorted)
-            else:
-                tree.fit(X, residual, sample_weight=sample_weight,
-                         check_input=False, X_idx_sorted=X_idx_sorted)
+            X = X_csr if X_csr is not None else X
+            tree.fit(X, residual, sample_weight=sample_weight,
+                     check_input=False, X_idx_sorted=X_idx_sorted)
 
             # update tree leaves
-            if X_csr is not None:
-                loss.update_terminal_regions(tree.tree_, X_csr, y, residual, y_pred,
-                                             sample_weight, sample_mask,
-                                             self.learning_rate, k=k)
-            else:
-                loss.update_terminal_regions(tree.tree_, X, y, residual, y_pred,
-                                             sample_weight, sample_mask,
-                                             self.learning_rate, k=k)
+            loss.update_terminal_regions(tree.tree_, X, y, residual, y_pred,
+                                         sample_weight, sample_mask,
+                                         self.learning_rate, k=k)
 
             # add tree to ensemble
             self.estimators_[i, k] = tree
@@ -1545,17 +1537,13 @@ class BaseGradientBoosting(six.with_metaclass(ABCMeta, BaseEnsemble)):
                                      X_csc, X_csr)
 
             # track deviance (= loss)
+            self.train_score_[i] = loss_(y[sample_mask], y_pred[sample_mask],
+                                         sample_weight[sample_mask])
             if do_oob:
-                self.train_score_[i] = loss_(y[sample_mask],
-                                             y_pred[sample_mask],
-                                             sample_weight[sample_mask])
                 self.oob_improvement_[i] = (
                     old_oob_score - loss_(y[~sample_mask],
                                           y_pred[~sample_mask],
                                           sample_weight[~sample_mask]))
-            else:
-                # no need to fancy index w/ no subsampling
-                self.train_score_[i] = loss_(y, y_pred, sample_weight)
 
             if self.verbose > 0:
                 verbose_reporter.update(i, self)
