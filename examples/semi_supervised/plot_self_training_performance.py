f41d8ef4f00792538fc86563c42452fae47c2495
Oliver Rausch <oliverrausch99@gmail.com>
2018-08-29 16:49:10 +0200

    Update example

--- examples/semi_supervised/plot_self_training_performance.py
+++ examples/semi_supervised/plot_self_training_performance.py
@@ -1,3 +1,24 @@
+"""
+===================================================
+Self-Training: Comparing performance
+===================================================
+This example demonstrates the performance of the SelfTraining classifier.
+
+The iris dataset is loaded, and a SVC classifier is created. Then, a
+SelfTraining classifier is initialised, using the same SVC as its
+base estimator.
+
+The dataset contains 150 data points, and the SelfTraining classifier is trained
+using all 150 data points, although most are unlabeled. The normal SVC is
+trained using only the labeled data points.
+
+The graph shows that the SelfTraining classifier outperforms the normal SVC when
+only few labeled data points are available.
+"""
+# Authors: Oliver Rausch <oliverrausch99@gmail.com>
+#          Patrice Becker <patrice@5becker.de>
+# License: BSD 3 clause
+print(__doc__)
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.semi_supervised.self_training import SelfTraining
@@ -5,33 +26,29 @@ from sklearn.utils import shuffle
 from sklearn.svm import SVC
 from sklearn.datasets import load_iris
 from sklearn.model_selection import StratifiedKFold
-from sklearn.semi_supervised.label_propagation import LabelPropagation
 from sklearn.metrics import f1_score
 from sklearn.base import clone
 
 supervised_score = []
 self_training_score = []
-label_propagation_score = []
 x_values = []
 
-clf = SVC(probability=True, C=100, gamma=0.7, kernel='rbf')
+clf = SVC(probability=True, C=100, gamma=0.8, kernel='rbf')
 self_training_clf = SelfTraining(
     clone(clf, safe=True), max_iter=100, threshold=0.8
 )
-ls = LabelPropagation()
 
-for t in range(20, 80):
+X, y = load_iris(return_X_y=True)
+X, y = shuffle(X, y, random_state=42)
+y_true = y.copy()
+for t in range(80, 15, -5):
     x_values.append(t)
-    X, y = load_iris(return_X_y=True)
-    X, y = shuffle(X, y, random_state=42)
-    y_true = y.copy()
 
     lim = t
     y[lim:] = -1
 
     supervised_score_temp = []
     self_training_score_temp = []
-    label_propagation_score_temp = []
 
     skfolds = StratifiedKFold(n_splits=3, random_state=42)
     for train_index, test_index in skfolds.split(X, y):
@@ -56,25 +73,14 @@ for t in range(20, 80):
             f1_score(y_test_true, y_pred, average='macro')
         )
 
-        ls.fit(X_train, y_train)
-        y_pred = ls.predict(X_test)
-        label_propagation_score_temp.append(
-            f1_score(y_test_true, y_pred, average='macro')
-        )
-
     supervised_score.append(np.array(supervised_score_temp).mean())
     self_training_score.append(np.array(self_training_score_temp).mean())
-    label_propagation_score.append(
-        np.array(label_propagation_score_temp).mean()
-    )
-
 
 plt.figure(1)
-plt.plot(x_values, supervised_score, label='Supervised')
-plt.plot(x_values, self_training_score, label='Self-training')
-plt.plot(x_values, label_propagation_score, label='Label Propagation')
+plt.plot(x_values, supervised_score, label='Supervised (SVC)')
+plt.plot(x_values, self_training_score, label='SelfTraining')
 plt.legend()
-plt.ylabel("Accuracy")
-plt.title("Comparision of classifiers on limited labeled data")
+plt.ylabel("f1_score (macro average)")
+plt.title("Comparison of classifiers on limited labeled data")
 plt.xlabel("Amount of Labeled Data")
 plt.show()
