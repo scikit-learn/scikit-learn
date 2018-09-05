cc00e02c79c382244b0adb2acd99b78982ce999f
Olivier Grisel <olivier.grisel@ensta.org>
2018-09-05 11:25:01 +0200

    Joblib 0.12.4

--- sklearn/externals/joblib/__init__.py
+++ sklearn/externals/joblib/__init__.py
@@ -106,7 +106,7 @@ Main features
 # Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
 # 'X.Y.dev0' is the canonical version of 'X.Y.dev'
 #
-__version__ = '0.12.3'
+__version__ = '0.12.4'
 
 
 from .memory import Memory, MemorizedResult, register_store_backend
