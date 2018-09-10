2aa24764ccd32aba810a8ad91b6776b9ec967bc8
adrinjalali <adrin.jalali@gmail.com>
2018-09-10 16:19:33 +0200

    give a simple example for LinearRegression

--- sklearn/linear_model/base.py
+++ sklearn/linear_model/base.py
@@ -404,15 +404,17 @@ class LinearRegression(LinearModel, RegressorMixin):
     >>> import numpy as np
     >>> from sklearn.linear_model import LinearRegression
     >>> X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
-    >>> # y = 1 * x_0 + 2 * x_1
-    >>> y = np.dot(X, np.array([1, 2]))
+    >>> # y = 1 * x_0 + 2 * x_1 + 3
+    >>> y = np.dot(X, np.array([1, 2])) + 3
     >>> reg = LinearRegression().fit(X, y)
     >>> reg.score(X, y)
     1.0
     >>> reg.coef_
     array([1., 2.])
+    >>> reg.intercept_ # doctest: +ELLIPSIS
+    3.0000...
     >>> reg.predict(np.array([[3, 5]]))
-    array([13.])
+    array([16.])
 
     Notes
     -----
