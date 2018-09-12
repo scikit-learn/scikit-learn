42b14ff39b2c2b0be71ba6888c14a40cef86db61
Joel Nothman <joel.nothman@gmail.com>
2018-09-12 15:08:02 +1000

    Remove OPTICS from 0.20
    Due to too many proposed backwards-incompatible changes

--- sklearn/cluster/setup.py
+++ sklearn/cluster/setup.py
@@ -23,10 +23,6 @@ def configuration(parent_package='', top_path=None):
                          sources=['_dbscan_inner.pyx'],
                          include_dirs=[numpy.get_include()],
                          language="c++")
-    config.add_extension('_optics_inner',
-                         sources=['_optics_inner.pyx'],
-                         include_dirs=[numpy.get_include()],
-                         libraries=libraries)
 
     config.add_extension('_hierarchical',
                          sources=['_hierarchical.pyx'],
