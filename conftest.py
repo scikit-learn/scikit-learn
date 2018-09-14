d8c45e11c209b2495e26b3a3a988e6f92ff9e3b2
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-09-14 08:53:29 +0200

    iter

--- conftest.py
+++ conftest.py
@@ -29,7 +29,8 @@ def pytest_collection_modifyitems(config, items):
                 item.add_marker(skip_marker)
 
     # numpy changed the str/repr formatting of numpy arrays in 1.14. We want to
-    # run doctests only for numpy >= 1.14.
+    # run doctests only for numpy >= 1.14. We want to skip the doctest for
+    # python 2 due to unicode.
     skip_doctests = True
     if sys.version_info[:1] >= (3,):
         skip_doctests = False
