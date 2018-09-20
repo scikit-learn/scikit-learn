f26ee2f80b7f1f7a7a399e6edcff488ce5028000
adrinjalali <adrin.jalali@gmail.com>
2018-09-20 22:20:53 +0200

    add missing random states

--- sklearn/covariance/elliptic_envelope.py
+++ sklearn/covariance/elliptic_envelope.py
@@ -74,10 +74,11 @@ class EllipticEnvelope(MinCovDet, OutlierMixin):
     >>> from sklearn.covariance import EllipticEnvelope
     >>> real_cov = np.array([[.8, .3],
     ...                      [.3, .4]])
+    >>> np.random.seed(0)
     >>> X = np.random.multivariate_normal(mean=[0, 0],
     ...                                   cov=real_cov,
     ...                                   size=300)
-    >>> cov = EllipticEnvelope().fit(X)
+    >>> cov = EllipticEnvelope(random_state=0).fit(X)
     >>> cov.covariance_ # doctest: +ELLIPSIS
     array([[0.7411..., 0.2535...],
            [0.2535..., 0.3053...]])
