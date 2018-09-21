e6176761f5fcfa15bc4e1f681e4781c5b0da3e85
adrinjalali <adrin.jalali@gmail.com>
2018-09-21 08:57:53 +0200

    remove elliptic example

--- sklearn/covariance/elliptic_envelope.py
+++ sklearn/covariance/elliptic_envelope.py
@@ -68,25 +68,6 @@ class EllipticEnvelope(MinCovDet, OutlierMixin):
         such a way we obtain the expected number of outliers (samples with
         decision function < 0) in training.
 
-    Examples
-    --------
-    >>> import numpy as np
-    >>> from sklearn.covariance import EllipticEnvelope
-    >>> real_cov = np.array([[.8, .3],
-    ...                      [.3, .4]])
-    >>> np.random.seed(0)
-    >>> X = np.random.multivariate_normal(mean=[0, 0],
-    ...                                   cov=real_cov,
-    ...                                   size=300)
-    >>> cov = EllipticEnvelope(random_state=0).fit(X)
-    >>> cov.covariance_ # doctest: +ELLIPSIS
-    array([[0.7411..., 0.2535...],
-           [0.2535..., 0.3053...]])
-    >>> cov.location_
-    array([0.0813... , 0.0427...])
-    >>> cov.predict(np.array([[1, 1], [5, 6]]))
-    array([ 1, -1])
-
     See Also
     --------
     EmpiricalCovariance, MinCovDet
