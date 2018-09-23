d6a92cec8795302bba5abd1c5ba0d0c153a7e172
Marc Wouts <marc.wouts@gmail.com>
2018-09-24 01:37:21 +0200

    Sphinx galleries supported in Jupytext v0.7

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -1,1 +1,3 @@
 c.NotebookApp.contents_manager_class = 'jupytext.TextFileContentsManager'  # noqa
+c.ContentsManager.preferred_jupytext_formats_read = 'py:sphinx'  # noqa
+c.ContentsManager.sphinx_convert_rst2md = True  # noqa
