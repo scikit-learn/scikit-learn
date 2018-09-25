661a8b42bd93a4b77a14d89aa7b96c2df158cf9d
Jan Koch <Jan.Koch@tu-dortmund.de>
2018-09-25 16:43:05 +0200

    add sparse_threshold to make_columns_transformer (#12152)
    fixes issue #12149

--- sklearn/compose/tests/test_column_transformer.py
+++ sklearn/compose/tests/test_column_transformer.py
@@ -453,11 +453,13 @@ def test_make_column_transformer_kwargs():
     scaler = StandardScaler()
     norm = Normalizer()
     ct = make_column_transformer(('first', scaler), (['second'], norm),
-                                 n_jobs=3, remainder='drop')
+                                 n_jobs=3, remainder='drop',
+                                 sparse_threshold=0.3)
     assert_equal(ct.transformers, make_column_transformer(
         ('first', scaler), (['second'], norm)).transformers)
     assert_equal(ct.n_jobs, 3)
     assert_equal(ct.remainder, 'drop')
+    assert_equal(ct.sparse_threshold, 0.3)
     # invalid keyword parameters should raise an error message
     assert_raise_message(
         TypeError,
