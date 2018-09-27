224ec676b4ca5c5065599c7cf2df661644d60c73
Rohan Singh <rohans1@andrew.cmu.edu>
2018-09-26 23:33:49 -0700

    Fix Issue #12179
    OneHotEncoder "only non-negative integers" message should suggest using
    categories='auto'

--- sklearn/preprocessing/_encoders.py
+++ sklearn/preprocessing/_encoders.py
@@ -421,10 +421,8 @@ class OneHotEncoder(_BaseEncoder):
         dtype = getattr(X, 'dtype', None)
         X = check_array(X, dtype=np.int)
         if np.any(X < 0):
-            raise ValueError("X needs to contain only non-negative integers,"
-                             "this behavior is now deprecated. New behaviour"
-                             "supports negative input. Instantiate using"
-                             "`categories='auto'`.")
+            raise ValueError("Instantiate using categories='auto' to"
+                             "handle negative integer categories.")
         n_samples, n_features = X.shape
         if (isinstance(self.n_values, six.string_types) and
                 self.n_values == 'auto'):
@@ -507,10 +505,8 @@ class OneHotEncoder(_BaseEncoder):
         """Assumes X contains only categorical features."""
         X = check_array(X, dtype=np.int)
         if np.any(X < 0):
-            raise ValueError("X needs to contain only non-negative integers,"
-                             "this behavior is now deprecated. New behaviour"
-                             "supports negative input. Instantiate using"
-                             "`categories='auto'`.")
+            raise ValueError("Instantiate using categories='auto' to"
+                             "handle negative integer categories.")
         n_samples, n_features = X.shape
 
         indices = self._feature_indices_
