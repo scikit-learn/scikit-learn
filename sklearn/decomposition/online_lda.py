a52f26b14386473f9ad25e4440e4584ba89d734b
melsyt <melinda.y.song@gmail.com>
2018-09-29 17:21:38 -0400

    fix lda docstring -topic_word_prior

--- sklearn/decomposition/online_lda.py
+++ sklearn/decomposition/online_lda.py
@@ -154,7 +154,7 @@ class LatentDirichletAllocation(BaseEstimator, TransformerMixin):
     topic_word_prior : float, optional (default=None)
         Prior of topic word distribution `beta`. If the value is None, defaults
         to `1 / n_components`.
-        In the literature, this is called `beta`.
+        In the literature, this is called `eta`.
 
     learning_method : 'batch' | 'online', default='batch'
         Method used to update `_component`. Only used in `fit` method.
