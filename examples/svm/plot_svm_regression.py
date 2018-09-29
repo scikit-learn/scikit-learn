db3228610f59562c72f6076998bfa66cdfcfe444
Rita Li <ml4713@nyu.edu>
2018-09-29 12:52:01 -0400

    Revert "Svr plot to update colors"

--- examples/svm/plot_svm_regression.py
+++ examples/svm/plot_svm_regression.py
@@ -23,9 +23,9 @@ y[::5] += 3 * (0.5 - np.random.rand(8))
 
 # #############################################################################
 # Fit regression model
-svr_rbf = SVR(kernel='rbf', C=100, gamma=0.1, epsilon=.1)
-svr_lin = SVR(kernel='linear', C=100)
-svr_poly = SVR(kernel='poly', C=100, degree=3, epsilon=.1, coef0=1)
+svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
+svr_lin = SVR(kernel='linear', C=1e3)
+svr_poly = SVR(kernel='poly', C=1e3, degree=2)
 y_rbf = svr_rbf.fit(X, y).predict(X)
 y_lin = svr_lin.fit(X, y).predict(X)
 y_poly = svr_poly.fit(X, y).predict(X)
@@ -33,18 +33,10 @@ y_poly = svr_poly.fit(X, y).predict(X)
 # #############################################################################
 # Look at the results
 lw = 2
-
-plt.scatter(X[svr_rbf.support_], y[svr_rbf.support_], facecolor="none",
-            edgecolor="m", marker='8', label='rbf support vectors', s=40)
-plt.scatter(X[svr_lin.support_], y[svr_lin.support_], facecolor="none",
-            edgecolor="c", marker='^', label='linear support vectors', s=40)
-plt.scatter(X[svr_poly.support_], y[svr_poly.support_], facecolor="none",
-            edgecolor="g", marker='s', label='poly support vectors', s=40)
-
-plt.hold('on')
-plt.plot(X, y_rbf, color='m', lw=lw, label='RBF model')
+plt.scatter(X, y, color='darkorange', label='data')
+plt.plot(X, y_rbf, color='navy', lw=lw, label='RBF model')
 plt.plot(X, y_lin, color='c', lw=lw, label='Linear model')
-plt.plot(X, y_poly, color='g', lw=lw, label='Polynomial model')
+plt.plot(X, y_poly, color='cornflowerblue', lw=lw, label='Polynomial model')
 plt.xlabel('data')
 plt.ylabel('target')
 plt.title('Support Vector Regression')
