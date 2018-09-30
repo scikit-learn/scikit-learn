d0fdf6e4ef0eba216c15b3246150f9373ef7b043
Sandra Mitrovic <sandra@Sandras-MacBook-Pro-2.local>
2018-09-30 12:23:29 -0400

    Solving E501 line too long.

--- sklearn/metrics/classification.py
+++ sklearn/metrics/classification.py
@@ -1678,7 +1678,8 @@ def hamming_loss(y_true, y_pred, labels=None, sample_weight=None):
         warnings.warn(
                       "This parameter was deprecated in version 0.21"
                       " and will be removed in version 0.23 as the"
-                      " number of labels is identical to y_true.shape[1].",
+                      " number of labels is identical"
+                      " to y_true.shape[1].",
                       DeprecationWarning)
 
     if sample_weight is None:
