42a96c245437c75ada4db333b72a4c0557b5b585
Sandra Mitrovic <sandra@Sandras-MacBook-Pro-2.local>
2018-09-30 10:41:58 -0400

    Deprecation warning message updated.

--- sklearn/metrics/tests/test_classification.py
+++ sklearn/metrics/tests/test_classification.py
@@ -996,9 +996,9 @@ def test_multilabel_hamming_loss():
     # sp_hamming only works with 1-D arrays
     assert_equal(hamming_loss(y1[0], y2[0]), sp_hamming(y1[0], y2[0]))
     assert_warns_message(DeprecationWarning,
-                        "This parameter was deprecated in version 0.21" 
-                        " and will be removed in version 0.23" 
-                        " as the number of labels is identical to y_true.shape[1].", 
+                        "This parameter was deprecated in version 0.21 \
+                        and will be removed in version 0.23 \
+                        as the number of labels is identical to y_true.shape[1].", 
                         hamming_loss, y1, y2, labels=[0, 1])
 
 def test_multilabel_jaccard_similarity_score():
