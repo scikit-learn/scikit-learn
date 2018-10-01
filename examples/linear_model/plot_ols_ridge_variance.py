91363111d5eadbeef3add7cae5b6b375e5240c18
Hanmin Qin <qinhanmin2005@sina.com>
2018-10-01 22:32:42 +0800

    MNT Use name instead of float to specify colors (#12199)

--- examples/linear_model/plot_ols_ridge_variance.py
+++ examples/linear_model/plot_ols_ridge_variance.py
@@ -53,12 +53,12 @@ for name, clf in classifiers.items():
         this_X = .1 * np.random.normal(size=(2, 1)) + X_train
         clf.fit(this_X, y_train)
 
-        ax.plot(X_test, clf.predict(X_test), color='.5')
-        ax.scatter(this_X, y_train, s=3, c='.5', marker='o', zorder=10)
+        ax.plot(X_test, clf.predict(X_test), color='gray')
+        ax.scatter(this_X, y_train, s=3, c='gray', marker='o', zorder=10)
 
     clf.fit(X_train, y_train)
     ax.plot(X_test, clf.predict(X_test), linewidth=2, color='blue')
-    ax.scatter(X_train, y_train, s=30, c='r', marker='+', zorder=10)
+    ax.scatter(X_train, y_train, s=30, c='red', marker='+', zorder=10)
 
     ax.set_xticks(())
     ax.set_yticks(())
