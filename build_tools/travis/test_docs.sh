ea0dd69f0169c9167bd66c866f634cb1225c323e
Thomas Fan <thomasjpfan@gmail.com>
2018-10-02 13:27:39 -0400

    TST: Moves soft pytest dependency test

--- build_tools/travis/test_docs.sh
+++ build_tools/travis/test_docs.sh
@@ -2,18 +2,6 @@
 
 set -e
 
-python --version
-python -c "import numpy; print('numpy %s' % numpy.__version__)"
-python -c "import scipy; print('scipy %s' % scipy.__version__)"
-python -c "\
-try:
-    import pandas
-    print('pandas %s' % pandas.__version__)
-except ImportError:
-    pass
-"
-python -c "import multiprocessing as mp; print('%d CPUs' % mp.cpu_count())"
-
 if [[ "$SKIP_TESTS" != "true" ]]; then
     set -x
     make test-doc
