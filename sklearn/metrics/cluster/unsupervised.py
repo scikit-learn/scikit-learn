7df723c980c755873560f95a4d9aba6d2ed3497e
Stephen Tierney <sjtrny@gmail.com>
2018-10-04 10:57:20 +1000

    Formatted to pep8

--- sklearn/metrics/cluster/unsupervised.py
+++ sklearn/metrics/cluster/unsupervised.py
@@ -211,7 +211,7 @@ def silhouette_samples(X, labels, metric='euclidean', **kwds):
 
     """
     X, labels = check_X_y(X, labels, accept_sparse=['csc', 'csr'])
-    
+
     # Check and warn for diagonal entries in precomputed distance matrix
     if metric == 'precomputed':
         diag_indices = np.diag_indices(X.shape[0])
@@ -219,7 +219,7 @@ def silhouette_samples(X, labels, metric='euclidean', **kwds):
             warnings.warn('The precomputed distance matrix contains non-zero '
                           'elements on the diagonal. This causes incorrect '
                           'calculation of silhouette scores.', UserWarning)
-    
+
     le = LabelEncoder()
     labels = le.fit_transform(labels)
     n_samples = len(labels)
