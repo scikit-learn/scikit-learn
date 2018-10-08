a995da62fdcf12142df71ef5c58f6026c836e299
Prokopios Gryllos <prokopis.gryllos@sentiance.com>
2018-10-08 09:37:59 +0200

    remove unused import

--- sklearn/tests/test_calibration.py
+++ sklearn/tests/test_calibration.py
@@ -12,8 +12,7 @@ from sklearn.utils.testing import (assert_array_almost_equal, assert_equal,
                                    assert_greater, assert_almost_equal,
                                    assert_greater_equal,
                                    assert_array_equal,
-                                   assert_raises,
-                                   ignore_warnings)
+                                   assert_raises)
 from sklearn.datasets import make_classification, make_blobs
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import MultinomialNB
