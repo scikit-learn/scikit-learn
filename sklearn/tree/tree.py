510f1389d84f18a6dc19ad1ae9795271d5fc773d
Gabriel Marzinotto <gabriel.marzinotto@gmail.com>
2018-10-12 11:21:28 +0200

    fix decisionTree issue #12259

--- sklearn/tree/tree.py
+++ sklearn/tree/tree.py
@@ -62,10 +62,13 @@ CRITERIA_REG = {"mse": _criterion.MSE, "friedman_mse": _criterion.FriedmanMSE,
                 "mae": _criterion.MAE}
 
 DENSE_SPLITTERS = {"best": _splitter.BestSplitter,
-                   "random": _splitter.RandomSplitter}
+                   "random": _splitter.RandomSplitter,
+                   "best2": _splitter.BestSplitter2}
 
 SPARSE_SPLITTERS = {"best": _splitter.BestSparseSplitter,
-                    "random": _splitter.RandomSparseSplitter}
+                    "random": _splitter.RandomSparseSplitter,
+                    "best2": _splitter.BestSparseSplitter2}
+
 
 # =============================================================================
 # Base decision tree
