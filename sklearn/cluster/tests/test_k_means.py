06dc5fd7403921e099e8332901c4c2f85edaf072
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2018-10-12 14:02:11 +0200

    typo

--- sklearn/cluster/tests/test_k_means.py
+++ sklearn/cluster/tests/test_k_means.py
@@ -621,7 +621,7 @@ def test_fit_transform():
 @pytest.mark.parametrize('algo', ['full', 'elkan'])
 def test_predict_equal_labels(algo):
     km = KMeans(random_state=13, n_jobs=1, n_init=1, max_iter=1,
-                algorithm='algo')
+                algorithm=algo)
     km.fit(X)
     assert_array_equal(km.predict(X), km.labels_)
 
