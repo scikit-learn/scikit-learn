88d3fa4daa35a2d41b06560a13f6deef55c815ef
Roman Yurchak <rth.yurchak@pm.me>
2018-10-16 14:44:26 +0200

    Reduce nesting

--- sklearn/feature_extraction/text.py
+++ sklearn/feature_extraction/text.py
@@ -280,31 +280,31 @@ class VectorizerMixin(object):
                         performed (e.g. because of the use of a custom
                         preprocessor / tokenizer)
         """
-        # NB: stop_words is validated, unlike self.stop_words
-        if id(self.stop_words) != getattr(self, '_stop_words_id', None):
-            try:
-                inconsistent = set()
-                for w in stop_words or ():
-                    tokens = list(tokenize(preprocess(w)))
-                    for token in tokens:
-                        if token not in stop_words:
-                            inconsistent.add(token)
-                self._stop_words_id = id(self.stop_words)
-
-                if inconsistent:
-                    warnings.warn('Your stop_words may be inconsistent with '
-                                  'your preprocessing. Tokenizing the stop '
-                                  'words generated tokens %r not in '
-                                  'stop_words.' % sorted(inconsistent))
-                return not inconsistent
-            except Exception:
-                # Failed to check stop words consistency (e.g. because a custom
-                # preprocessor or tokenizer was used)
-                self._stop_words_id = id(self.stop_words)
-                return 'error'
-        else:
+        if id(self.stop_words) == getattr(self, '_stop_words_id', None):
             # Stop words are were previously validated
             return None
+
+        # NB: stop_words is validated, unlike self.stop_words
+        try:
+            inconsistent = set()
+            for w in stop_words or ():
+                tokens = list(tokenize(preprocess(w)))
+                for token in tokens:
+                    if token not in stop_words:
+                        inconsistent.add(token)
+            self._stop_words_id = id(self.stop_words)
+
+            if inconsistent:
+                warnings.warn('Your stop_words may be inconsistent with '
+                              'your preprocessing. Tokenizing the stop '
+                              'words generated tokens %r not in '
+                              'stop_words.' % sorted(inconsistent))
+            return not inconsistent
+        except Exception:
+            # Failed to check stop words consistency (e.g. because a custom
+            # preprocessor or tokenizer was used)
+            self._stop_words_id = id(self.stop_words)
+            return 'error'
 
     def build_analyzer(self):
         """Return a callable that handles preprocessing and tokenization"""
