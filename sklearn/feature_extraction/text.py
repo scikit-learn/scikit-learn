5961e0aa5c8e4023cf6e2c27f135e97eca7a71d2
Roman Yurchak <rth.yurchak@pm.me>
2018-10-16 11:04:14 +0200

    Fix regression in text vectorizers due to stop word validation

--- sklearn/feature_extraction/text.py
+++ sklearn/feature_extraction/text.py
@@ -270,21 +270,41 @@ class VectorizerMixin(object):
         return _check_stop_list(self.stop_words)
 
     def _check_stop_words_consistency(self, stop_words, preprocess, tokenize):
+        """Check if stop words are consistent
+
+        Returns
+        -------
+        is_consistent : True if stop words are consistent with the preprocessor
+                        and tokenizer, False if they are not, None if the check
+                        was previously performed, "error" if it could not be
+                        performed (e.g. because of the use of a custom
+                        preprocessor / tokenizer)
+        """
         # NB: stop_words is validated, unlike self.stop_words
         if id(self.stop_words) != getattr(self, '_stop_words_id', None):
-            inconsistent = set()
-            for w in stop_words or ():
-                tokens = list(tokenize(preprocess(w)))
-                for token in tokens:
-                    if token not in stop_words:
-                        inconsistent.add(token)
-            self._stop_words_id = id(self.stop_words)
+            try:
+                inconsistent = set()
+                for w in stop_words or ():
+                    tokens = list(tokenize(preprocess(w)))
+                    for token in tokens:
+                        if token not in stop_words:
+                            inconsistent.add(token)
+                self._stop_words_id = id(self.stop_words)
 
-            if inconsistent:
-                warnings.warn('Your stop_words may be inconsistent with your '
-                              'preprocessing. Tokenizing the stop words '
-                              'generated tokens %r not in stop_words.' %
-                              sorted(inconsistent))
+                if inconsistent:
+                    warnings.warn('Your stop_words may be inconsistent with '
+                                  'your preprocessing. Tokenizing the stop '
+                                  'words generated tokens %r not in '
+                                  'stop_words.' % sorted(inconsistent))
+                return not inconsistent
+            except Exception:
+                # Failed to check stop words consistency (e.g. because a custom
+                # preprocessor or tokenizer was used)
+                self._stop_words_id = id(self.stop_words)
+                return 'error'
+        else:
+            # Stop words are were previously validated
+            return None
 
     def build_analyzer(self):
         """Return a callable that handles preprocessing and tokenization"""
