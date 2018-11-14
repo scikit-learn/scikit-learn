c4fa53a186eb676268307a5a01aa6c4352386fe7
Gabriel Marzinotto <gabriel.marzinotto@gmail.com>
2018-11-14 10:22:40 +0100

    Update test_encoders.py

--- sklearn/preprocessing/tests/test_encoders.py
+++ sklearn/preprocessing/tests/test_encoders.py
@@ -276,7 +276,6 @@ def test_one_hot_encoder_no_categorical_features():
 def test_one_hot_encoder_handle_unknown_strings():
     X = np.array(['11111111', '22', '333', '4444']).reshape((-1, 1))
     X2 = np.array(['55555', '22']).reshape((-1, 1))
-    
     # Non Regression test for the issue #12470
     # Test the ignore option, when categories are numpy string dtype
     # particularly when the known category strings are larger
