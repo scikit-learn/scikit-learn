29597707cc8a9d83c10fbae4f9f560cd455231a4
thoo <theinhtay@gmail.com>
2018-11-15 11:17:03 -0500

    minor change in conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -94,7 +94,7 @@ release = sklearn.__version__
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
-# today = ''
+#today = ''
 # Else, today_fmt is used as the format for a strftime call.
 #today_fmt = '%B %d, %Y'
 
