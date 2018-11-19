888d0dc595151dbe4f183131235c18dcdd9ef192
Oliver Rausch <oliverrausch99@gmail.com>
2018-11-19 17:05:02 +0100

    Fix build issues

--- examples/semi_supervised/plot_self_training_varying_threshold.py
+++ examples/semi_supervised/plot_self_training_varying_threshold.py
@@ -32,11 +32,7 @@ from sklearn.semi_supervised import SelfTrainingClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.utils import shuffle
 
-rng = np.random.RandomState(0)
-
 n_splits = 3
-X, y = datasets.load_digits(return_X_y=True)
-
 
 X, y = datasets.load_breast_cancer(return_X_y=True)
 X, y = shuffle(X, y, random_state=42)
@@ -82,22 +78,22 @@ for (i, threshold) in enumerate(x_values):
 ax1 = plt.subplot(211)
 ax1.errorbar(x_values, scores.mean(axis=1),
              yerr=scores.std(axis=1),
-             capsize=2, color='tab:blue')
-ax1.set_ylabel('Accuracy', color='tab:blue')
-ax1.tick_params('y', colors='tab:blue')
+             capsize=2, color='b')
+ax1.set_ylabel('Accuracy', color='b')
+ax1.tick_params('y', colors='b')
 
 ax2 = ax1.twinx()
 ax2.errorbar(x_values, amount_labeled.mean(axis=1),
              yerr=amount_labeled.std(axis=1),
-             capsize=2, color='tab:green')
-ax2.set_ylabel('Amount of self-labeled samples', color='tab:green')
-ax2.tick_params('y', colors='tab:green')
+             capsize=2, color='g')
+ax2.set_ylabel('Amount of self-labeled samples', color='g')
+ax2.tick_params('y', colors='g')
 
 ax3 = plt.subplot(212, sharex=ax1)
 ax3.errorbar(x_values, amount_iterations.mean(axis=1),
              yerr=amount_iterations.std(axis=1),
-             capsize=2)
+             capsize=2, color='b')
 ax3.set_ylabel('Amount of iterations')
-ax3.set_xlabel("Threshold")
+ax3.set_xlabel('Threshold')
 
 plt.show()
