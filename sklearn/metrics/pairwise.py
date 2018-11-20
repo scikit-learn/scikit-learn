14949c1ffc706904463a0e225f5a0f84bd2e5fa6
Emmanuel Arias <earias@conae.gov.ar>
2018-11-20 10:09:10 -0300

    add @whiletruelearn entry comments

--- sklearn/metrics/pairwise.py
+++ sklearn/metrics/pairwise.py
@@ -1085,6 +1085,7 @@ def distance_metrics():
     'cityblock'      metrics.pairwise.manhattan_distances
     'cosine'         metrics.pairwise.cosine_distances
     'euclidean'      metrics.pairwise.euclidean_distances
+    'haversine'      metrics.pairwise.haversine_distances
     'l1'             metrics.pairwise.manhattan_distances
     'l2'             metrics.pairwise.euclidean_distances
     'manhattan'      metrics.pairwise.manhattan_distances
