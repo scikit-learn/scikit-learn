de752050a914e4abb41de20e992fd128657db009
Nicolas Hug <contact@nicolas-hug.com>
2018-11-20 09:30:28 -0500

    fixed randomness of test_logreg_cv_penalty

--- sklearn/linear_model/tests/test_logistic.py
+++ sklearn/linear_model/tests/test_logistic.py
@@ -1152,12 +1152,15 @@ def test_logreg_l1_sparse_data():
 @pytest.mark.filterwarnings('ignore: You should specify a value')  # 0.22
 def test_logreg_cv_penalty():
     # Test that the correct penalty is passed to the final fit.
-    X, y = make_classification(n_samples=50, n_features=20, random_state=0)
-    lr_cv = LogisticRegressionCV(penalty="l1", Cs=[1.0], solver='saga')
+    rng = np.random.RandomState(6)
+    X, y = make_classification(n_samples=50, n_features=20, random_state=rng)
+    lr_cv = LogisticRegressionCV(penalty="l2", Cs=[1.0], solver='saga',
+                                 random_state=rng)
     lr_cv.fit(X, y)
-    lr = LogisticRegression(penalty="l1", C=1.0, solver='saga')
+    lr = LogisticRegression(penalty="l2", C=1.0, solver='saga',
+                            random_state=rng)
     lr.fit(X, y)
-    assert_equal(np.count_nonzero(lr_cv.coef_), np.count_nonzero(lr.coef_))
+    assert_array_almost_equal(lr_cv.coef_, lr.coef_, decimal=3)
 
 
 def test_logreg_predict_proba_multinomial():
