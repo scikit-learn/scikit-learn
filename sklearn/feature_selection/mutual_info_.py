8aa75f13f66a12b41ff88fdf6b9501654ef18032
Andreas Mueller <andreas.mueller@columbia.edu>
2018-11-21 16:31:41 -0500

    typo

--- sklearn/feature_selection/mutual_info_.py
+++ sklearn/feature_selection/mutual_info_.py
@@ -285,7 +285,7 @@ def _estimate_mi(X, y, discrete_features='auto', discrete_target=False,
         y += 1e-10 * np.maximum(1, np.mean(np.abs(y))) * rng.randn(n_samples)
 
     mi = [_compute_mi(x, y, discrete_feature, discrete_target, n_neighbors) for
-          x, discrete_feature zip(_iterate_columns(X), discrete_mask)]
+          x, discrete_feature in zip(_iterate_columns(X), discrete_mask)]
 
     return np.array(mi)
 
