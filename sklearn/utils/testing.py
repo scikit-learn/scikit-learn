6ae2b103472d063c135be0c4d305edb66a76316c
Andrei Mouraviev <andreimouraviev@Andreis-MacBook-Pro.local>
2018-12-03 22:09:08 -0500

    removed extra blank lines

--- sklearn/utils/testing.py
+++ sklearn/utils/testing.py
@@ -58,7 +58,6 @@ from sklearn.utils._joblib import joblib
 from sklearn.utils._unittest_backport import TestCase
 from sklearn.utils.fixes import signature
 
-
 additional_names_in_all = []
 try:
     from nose.tools import raises as _nose_raises
@@ -95,7 +94,6 @@ __all__.extend(additional_names_in_all)
 _dummy = TestCase('__init__')
 assert_equal = _dummy.assertEqual
 assert_not_equal = _dummy.assertNotEqual
-
 assert_raises = _dummy.assertRaises
 SkipTest = unittest.case.SkipTest
 assert_dict_equal = _dummy.assertDictEqual
