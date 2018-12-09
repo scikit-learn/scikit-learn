585214b766f330b99c839eab0f58b66f08ffd9d4
Pierre TALLOTTE <pierre.tallotte@gmx.com>
2018-12-09 18:21:14 +0100

    Fix features selection example

--- examples/feature_selection/plot_feature_selection.py
+++ examples/feature_selection/plot_feature_selection.py
@@ -25,7 +25,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn import datasets, svm
-from sklearn.feature_selection import SelectPercentile, f_classif
+from sklearn.feature_selection import SelectKBest, f_classif
 
 # #############################################################################
 # Import some data to play with
@@ -47,8 +47,9 @@ X_indices = np.arange(X.shape[-1])
 
 # #############################################################################
 # Univariate feature selection with F-test for feature scoring
-# We use the default selection function: the 10% most significant features
-selector = SelectPercentile(f_classif, percentile=10)
+# We use the default selection function to select the four 
+# most significant features
+selector = SelectKBest(f_classif, k=4)
 selector.fit(X, y)
 scores = -np.log10(selector.pvalues_)
 scores /= scores.max()
@@ -61,8 +62,8 @@ plt.bar(X_indices - .45, scores, width=.2,
 clf = svm.SVC(kernel='linear')
 clf.fit(X, y)
 
-svm_weights = (clf.coef_ ** 2).sum(axis=0)
-svm_weights /= svm_weights.max()
+svm_weights = (np.abs(clf.coef_)).sum(axis=0)
+svm_weights /= svm_weights.sum()
 
 plt.bar(X_indices - .25, svm_weights, width=.2, label='SVM weight',
         color='navy', edgecolor='black')
@@ -70,8 +71,8 @@ plt.bar(X_indices - .25, svm_weights, width=.2, label='SVM weight',
 clf_selected = svm.SVC(kernel='linear')
 clf_selected.fit(selector.transform(X), y)
 
-svm_weights_selected = (clf_selected.coef_ ** 2).sum(axis=0)
-svm_weights_selected /= svm_weights_selected.max()
+svm_weights_selected = (np.abs(clf_selected.coef_)).sum(axis=0)
+svm_weights_selected /= svm_weights_selected.sum()
 
 plt.bar(X_indices[selector.get_support()] - .05, svm_weights_selected,
         width=.2, label='SVM weights after selection', color='c',
