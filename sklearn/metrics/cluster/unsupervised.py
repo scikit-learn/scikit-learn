1bec083087387d070a4d8d3bc54e5d56561c47ad
Ian Sanders <isanders@chromalloy.com>
2018-12-12 09:49:32 -0500

    Update davies_bouldin_score to better handle zeros in centroid_distances Replaces 0s with infinity to make division by 0 result in 0

--- sklearn/metrics/cluster/unsupervised.py
+++ sklearn/metrics/cluster/unsupervised.py
@@ -347,6 +347,7 @@ def davies_bouldin_score(X, labels):
     if np.allclose(intra_dists, 0) or np.allclose(centroid_distances, 0):
         return 0.0
 
-    score = (intra_dists[:, None] + intra_dists) / centroid_distances
-    score[score == np.inf] = np.nan
-    return np.mean(np.nanmax(score, axis=1))
+    centroid_distances[centroid_distances == 0] = np.inf
+    combined_intra_dists = intra_dists[:, None] + intra_dists
+    scores = np.amax(combined_intra_dists / centroid_distances, axis=1)
+    return np.mean(scores)
