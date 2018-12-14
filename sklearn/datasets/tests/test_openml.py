03bf639e497619a4838498d3057a7556147be222
Andreas Mueller <andreas.mueller@columbia.edu>
2018-12-14 17:13:03 -0500

    remove six import from openml test

--- sklearn/datasets/tests/test_openml.py
+++ sklearn/datasets/tests/test_openml.py
@@ -17,7 +17,6 @@ from sklearn.datasets.openml import (_open_openml_url,
                                      _retry_with_clean_cache)
 from sklearn.utils.testing import (assert_warns_message,
                                    assert_raise_message)
-from sklearn.externals.six import str
 from urllib.error import HTTPError
 from sklearn.datasets.tests.test_common import check_return_X_y
 from functools import partial
