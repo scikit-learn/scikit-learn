cb9e1d181ae64d6942c9bda4752ba258344cff1d
Pierre Glaser <pierreglaser@msn.com>
2018-12-14 13:51:40 +0100

    DOC do not mention "callback"

--- sklearn/linear_model/stochastic_gradient.py
+++ sklearn/linear_model/stochastic_gradient.py
@@ -671,8 +671,8 @@ class BaseSGDClassifier(six.with_metaclass(ABCMeta, BaseSGD,
 
         Internally, this method uses ``max_iter = 1``. Therefore, it is not
         guaranteed that a minimum of the cost function is reached after calling
-        it once. Objective convergence and callbacks such as early stopping
-        should be monitored by the user.
+        it once. Matters such as objective convergence and early stopping
+        should be handled by the user.
 
         Parameters
         ----------
@@ -1169,8 +1169,8 @@ class BaseSGDRegressor(BaseSGD, RegressorMixin):
 
         Internally, this method uses ``max_iter = 1``. Therefore, it is not
         guaranteed that a minimum of the cost function is reached after calling
-        it once. Objective convergence and callbacks such as early stopping
-        should be monitored by the user.
+        it once. Matters such as objective convergence and early stopping
+        should be handled by the user.
 
         Parameters
         ----------
