abf8273b80c5ae5098829b06e83cc3136e60c213
reshamas <rs2715@gmail.com>
2018-12-17 00:00:35 -0500

    added blank line after classes_ attributes in LogRegCV

--- sklearn/linear_model/logistic.py
+++ sklearn/linear_model/logistic.py
@@ -1648,6 +1648,7 @@ class LogisticRegressionCV(LogisticRegression, BaseEstimator,
     ----------
     classes_ : array, shape (n_classes, )
         A list of class labels known to the classifier.
+
     coef_ : array, shape (1, n_features) or (n_classes, n_features)
         Coefficient of the features in the decision function.
 
