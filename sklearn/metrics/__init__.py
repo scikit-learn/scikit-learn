73068ebd204295d1e64fb6652841242ed937cba6
Thomas Fan <thomasjpfan@gmail.com>
2018-12-21 15:02:20 -0500

    ENH: Adds to __init__

--- sklearn/metrics/__init__.py
+++ sklearn/metrics/__init__.py
@@ -50,6 +50,7 @@ from .cluster import v_measure_score
 from .cluster import davies_bouldin_score
 
 from .pairwise import euclidean_distances
+from .pairwise import masked_euclidean_distances
 from .pairwise import pairwise_distances
 from .pairwise import pairwise_distances_argmin
 from .pairwise import pairwise_distances_argmin_min
@@ -103,6 +104,7 @@ __all__ = [
     'label_ranking_loss',
     'log_loss',
     'make_scorer',
+    'masked_euclidean_distances',
     'matthews_corrcoef',
     'max_error',
     'mean_absolute_error',
