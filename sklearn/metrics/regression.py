27e0ac6afc93c7e3f832687b7d6a9632e4a79384
Pawel Sendyk <pawelsendyk@berkeley.edu>
2018-12-22 14:53:00 +0100

    fix import indent

--- sklearn/metrics/regression.py
+++ sklearn/metrics/regression.py
@@ -27,7 +27,7 @@ import numpy as np
 import warnings
 
 from ..utils.validation import (check_array, check_consistent_length,
-    _num_samples)
+                                _num_samples)
 from ..utils.validation import column_or_1d
 from ..externals.six import string_types
 from ..exceptions import DataDimensionalityWarning
