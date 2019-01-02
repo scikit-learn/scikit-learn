3ae6c5a8ca7b2581d329a894a2827b0cb9fdeab9
adrinjalali <adrin.jalali@gmail.com>
2019-01-02 17:04:47 +0100

    fix criterion docstring for y's dtype

--- sklearn/tree/_criterion.pyx
+++ sklearn/tree/_criterion.pyx
@@ -287,7 +287,7 @@ cdef class ClassificationCriterion(Criterion):
 
         Parameters
         ----------
-        y : array-like, dtype=DOUBLE_t[:, :]
+        y : array-like, dtype=const DOUBLE_t[:, ::1]
             The target stored as a buffer for memory efficiency
         sample_weight : array-like, dtype=DTYPE_t
             The weight of each sample
