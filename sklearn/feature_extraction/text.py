a3acfe0d530b41eff87b09b3ef287051df1b440f
reshamas <rs2715@gmail.com>
2019-01-05 19:32:32 -0500

    removed '\text' from math formula

--- sklearn/feature_extraction/text.py
+++ sklearn/feature_extraction/text.py
@@ -1487,7 +1487,7 @@ class TfidfVectorizer(CountVectorizer):
 
         The formula that is used to compute the tf-idf of term t is​
         :math:`tf-idf(d, t) = tf(t) * idf(d, t)` and the idf is computed
-        as​ :math:`\text{idf}(d, t) = log{\frac{n}{df(d, t)} + 1}`
+        as​ :math:`idf(d, t) = log{\frac{n}{df(d, t)} + 1}`
         (if ``smooth_idf=False``)
         where `n` is the total number of documents and ​`df(d, t)​`​ is the
         document frequency; the document frequency is the number of documents
@@ -1495,8 +1495,8 @@ class TfidfVectorizer(CountVectorizer):
         in the equation above is that terms with zero idf, i.e., terms that
         occur in all documents in a training set, will not be entirely ignored.
         (Note that the idf formula above differs from the standard textbook
-        notation that defines the idf as​:
-        :math:`\text{idf}(d, t) = log{\frac{n}{df(d, t) + 1}}`
+        notation that defines the idf as​
+        :math:`idf(d, t) = log{\frac{n}{df(d, t) + 1}}`
 
     Notes
     -----
