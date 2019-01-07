561ab5479c0089193df6f52a3afc87544c13fedd
Wenhao <wenhaoz@ucla.edu>
2019-01-07 23:37:05 +0000

    fixing travis failure

--- sklearn/model_selection/tests/test_search.py
+++ sklearn/model_selection/tests/test_search.py
@@ -647,9 +647,11 @@ def test_refit_callable_invalid_type():
     clf = GridSearchCV(LinearSVC(random_state=42), {'C': [0.1, 1]},
                        scoring='precision', refit=refit_callable_invalid_type,
                        cv=5)
-    with pytest.raises(TypeError,
-                       match='best_index_ returned is not an integer'):
-        clf.fit(X, y)
+#    with pytest.raises(TypeError,
+#                       match='best_index_ returned is not an integer'):
+#        clf.fit(X, y)
+    assert_raise_message(TypeError, "best_index_ returned is not an integer",
+                         clf.fit, X, y)
 
 
 def test_refit_callable_out_bound():
@@ -669,8 +671,10 @@ def test_refit_callable_out_bound():
     clf = GridSearchCV(LinearSVC(random_state=42), {'C': [0.1, 1]},
                        scoring='precision', refit=refit_callable_out_bound,
                        cv=5)
-    with pytest.raises(IndexError, match='best_index_ index out of range'):
-        clf.fit(X, y)
+#    with pytest.raises(IndexError, match='best_index_ index out of range'):
+#        clf.fit(X, y)
+    assert_raise_message(IndexError, "best_index_ index out of range",
+                         clf.fit, X, y)
 
 
 def test_refit_callable_multi_metric():
