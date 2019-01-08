641af973dbbaaab9809990105060ec9156c9df18
Kuai Yu <yukuairoy@gmail.com>
2019-01-07 22:57:40 -0500

    fix

--- sklearn/covariance/graph_lasso_.py
+++ sklearn/covariance/graph_lasso_.py
@@ -20,10 +20,8 @@ from .empirical_covariance_ import (empirical_covariance, EmpiricalCovariance,
 from ..exceptions import ConvergenceWarning
 from ..utils.validation import check_random_state, check_array
 from ..utils import deprecated
-from ..utils.fixes import _Sequence as Sequence
 from ..linear_model import cd_fast
 from ..linear_model import lars_path_gram
-
 from ..model_selection import check_cv, cross_val_score
 from ..utils._joblib import Parallel, delayed
 
