c3f973b1de156509b7c59e2d5fafdef1c28f74dd
Bryan Woods <bryanwoods@comcast.net>
2019-01-08 17:52:10 -0500

    FIX supports 64 bit group ID and indexes (#12736)

--- sklearn/datasets/tests/test_svmlight_format.py
+++ sklearn/datasets/tests/test_svmlight_format.py
@@ -186,6 +186,19 @@ def test_load_with_qid():
         assert_array_equal(X.toarray(), [[.53, .12], [.13, .1], [.87, .12]])
 
 
+@pytest.mark.skip("testing the overflow of 32 bit sparse indexing requires a"
+                  " large amount of memory")
+def test_load_large_qid():
+    """
+    load large libsvm / svmlight file with qid attribute. Tests 64-bit query ID
+    """
+    data = b"\n".join(("3 qid:{0} 1:0.53 2:0.12\n2 qid:{0} 1:0.13 2:0.1"
+                      .format(i).encode() for i in range(1, 40*1000*1000)))
+    X, y, qid = load_svmlight_file(BytesIO(data), query_id=True)
+    assert_array_equal(y[-4:], [3, 2, 3, 2])
+    assert_array_equal(np.unique(qid), np.arange(1, 40*1000*1000))
+
+
 def test_load_invalid_file2():
     assert_raises(ValueError, load_svmlight_files,
                   [datafile, invalidfile, datafile])
