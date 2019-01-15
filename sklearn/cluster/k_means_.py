ce1e95211ee5a02e907fa66e1fd2a537ff76cc93
Prabakaran Kumaresshan <k_prabakaran+github@hotmail.com>
2019-01-15 20:43:01 +0530

    Update docstring test to be more deterministic

--- sklearn/cluster/k_means_.py
+++ sklearn/cluster/k_means_.py
@@ -868,7 +868,7 @@ class KMeans(BaseEstimator, ClusterMixin, TransformerMixin):
     >>> from sklearn.cluster import KMeans
     >>> import numpy as np
     >>> X = np.array([[1, 2], [1, 4], [1, 0],
-    ...               [4, 2], [4, 4], [4, 0]])
+    ...               [10, 2], [10, 4], [10, 0]])
     >>> kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
     >>> kmeans.labels_
     array([0, 0, 0, 1, 1, 1], dtype=int32)
