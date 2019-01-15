9058b24cb665d6e207668e74f933e279072a62d1
Thomas Fan <thomasjpfan@gmail.com>
2019-01-15 09:30:55 -0500

    RFC: Rewords error msg

--- sklearn/metrics/ranking.py
+++ sklearn/metrics/ranking.py
@@ -404,8 +404,8 @@ def roc_auc_score(y_true, y_score, labels=None,
                 raise ValueError("Parameter 'labels' must be unique")
             if len(unique_labels) != y_score.shape[1]:
                 raise ValueError(
-                    "Parameter 'labels' not equal to the number of columns in "
-                    "'y_score'")
+                    "Number of given labels not equal to the number of "
+                    "columns in 'y_score'")
             if set(np.unique(y_true)) > set(unique_labels):
                 raise ValueError(
                     "'y_true' contains labels not in parameter 'labels'")
