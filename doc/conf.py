775025bee30c4d02d1351924d63a60042ef9458d
Thomas Fan <thomasjpfan@gmail.com>
2019-01-16 22:53:00 -0500

    STY: Flake8

--- doc/conf.py
+++ doc/conf.py
@@ -15,7 +15,6 @@
 from __future__ import print_function
 import sys
 import os
-import logging
 
 # If extensions (or modules to document with autodoc) are in another
 # directory, add these directories to sys.path here. If the directory
