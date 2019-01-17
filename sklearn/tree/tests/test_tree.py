286b04bbf7d8eae8ebc2137fdd66c5046457c89a
adrinjalali <adrin.jalali@gmail.com>
2019-01-17 17:05:46 +0100

    add some tests

--- sklearn/tree/tests/test_tree.py
+++ sklearn/tree/tests/test_tree.py
@@ -1853,3 +1853,94 @@ def test_empty_leaf_infinite_threshold():
         infinite_threshold = np.where(~np.isfinite(tree.tree_.threshold))[0]
         assert len(infinite_threshold) == 0
         assert len(empty_leaf) == 0
+
+
+def _make_categorical(n_rows: int, n_numerical: int, n_categorical: int,
+                      cat_size: int, n_num_meaningful: int,
+                      n_cat_meaningful: int, regression: bool,
+                      return_tuple: bool, random_state: int):
+
+    from sklearn.preprocessing import OneHotEncoder
+    np.random.seed(random_state)
+    numeric = np.random.standard_normal((n_rows, n_numerical))
+    categorical = np.random.randint(0, cat_size, (n_rows, n_categorical))
+    categorical_ohe = OneHotEncoder(categories='auto').fit_transform(
+        categorical[:, :n_cat_meaningful])
+
+    data_meaningful = np.hstack((numeric[:, :n_num_meaningful],
+                                 categorical_ohe.todense()))
+    _, cols = data_meaningful.shape
+    coefs = np.random.standard_normal(cols)
+    y = np.dot(data_meaningful, coefs)
+    y = np.asarray(y).reshape(-1)
+    X = np.hstack((numeric, categorical))
+
+    if not regression:
+        y = (y < y.mean()).astype(int)
+
+    meaningful_features = np.r_[np.arange(n_num_meaningful),
+                                np.arange(n_cat_meaningful)
+                                + n_numerical]
+
+    if return_tuple:
+        return X, y, meaningful_features
+    else:
+        return {'X': X,
+                'y': y,
+                'meaningful_features': meaningful_features}
+
+
+@pytest.mark.parametrize('model', ALL_TREES)
+@pytest.mark.parametrize('data_params', [
+    {'n_rows': 1000,
+     'n_numerical': 5,
+     'n_categorical': 5,
+     'cat_size': 3,
+     'n_num_meaningful': 2,
+     'n_cat_meaningful': 3},
+    {'n_rows': 1000,
+     'n_numerical': 0,
+     'n_categorical': 5,
+     'cat_size': 3,
+     'n_num_meaningful': 0,
+     'n_cat_meaningful': 3},
+    {'n_rows': 1000,
+     'n_numerical': 5,
+     'n_categorical': 5,
+     'cat_size': 64,
+     'n_num_meaningful': 1,
+     'n_cat_meaningful': 2},
+    {'n_rows': 1000,
+     'n_numerical': 5,
+     'n_categorical': 5,
+     'cat_size': 3,
+     'n_num_meaningful': 0,
+     'n_cat_meaningful': 3}])
+def test_categorical_data(model, data_params):
+    # DecisionTrees are too slow for large category sizes.
+    if data_params['cat_size'] > 8 and 'DecisionTree' in model:
+        pass
+
+    X, y, meaningful_features = _make_categorical(
+        **data_params,
+        regression=model in REG_TREES,
+        return_tuple=True,
+        random_state=42)
+    rows, cols = X.shape
+    categorical_features = (np.arange(data_params['n_categorical']) +
+                            data_params['n_numerical'])
+
+    model = ALL_TREES[model](random_state=42,
+                             categorical=categorical_features).fit(X, y)
+    fi = model.feature_importances_
+    bad_features = np.array([True]*cols)
+    bad_features[meaningful_features] = False
+
+    good_ones = fi[meaningful_features]
+    bad_ones = fi[bad_features]
+
+    # all good features should be more important than all bad features.
+    assert np.all([np.all(x > bad_ones) for x in good_ones])
+
+    leaves = model.tree_.children_left < 0
+    assert(np.all(model.tree_.impurity[leaves] < 1e-6))
