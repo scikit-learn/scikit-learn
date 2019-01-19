fd85cc28db9c87126fb713ff87ef748cfe6b911b
baluyotraf <baluyotraf@outlook.com>
2019-01-19 08:43:41 +0800

    Renamed safe_acc_op to _safe_accumulator_op and moved it to sklearn.utils.extmath. Also fixed some line lengths to fit the 80 limit (#13007)

--- sklearn/utils/__init__.py
+++ sklearn/utils/__init__.py
@@ -639,35 +639,3 @@ def is_scalar_nan(x):
     # Redondant np.floating is needed because numbers can't match np.float32
     # in python 2.
     return bool(isinstance(x, (numbers.Real, np.floating)) and np.isnan(x))
-
-
-# Use at least float64 for the accumulating functions to avoid precision issues;
-# see https://github.com/numpy/numpy/issues/9393
-# The float64 is also retained as it is in case the float overflows
-def safe_acc_op(op, x, *args, **kwargs):
-    """
-    This function provides numpy accumulator functions
-    with a float64 dtype when used on a floating point
-    input. This prevents accumulator overflow
-    on smaller floating point dtypes.
-
-    Parameters
-    ----------
-    op : function
-        A numpy accumulator function such as np.mean or np.sum
-    x : numpy array
-        A numpy array to apply the accumulator function
-    *args : positional arguments
-        Positional arguments passed to the accumulator function after the input x
-    **kwargs : keyword arguments
-        Keyword arguments passed to the accumulator function
-
-    Returns
-    -------
-    result : The output of the accumulator function passed to this function
-    """
-    if np.issubdtype(x.dtype, np.floating) and x.dtype.itemsize < 8:
-        result = op(x, *args, **kwargs, dtype=np.float64)
-    else:
-        result = op(x, *args, **kwargs)
-    return result
