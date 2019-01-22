00aab5f6d05474425186f3f8cd237e83dde17be9
Nicolas Hug <contact@nicolas-hug.com>
2019-01-22 16:37:44 -0500

    minor comments

--- sklearn/_fast_gradient_boosting/types.pyx
+++ sklearn/_fast_gradient_boosting/types.pyx
@@ -1,8 +1,11 @@
 import numpy as np
 
+# Y_DYTPE is the dtype to which the targets y are converted to. This is also
+# the dtype for gradients, hessians, leaf values, etc. because they are all
+# homogeneous to a target.
 Y_DTYPE = np.float64
 X_DTYPE = np.float64
-X_BINNED_DTYPE = np.uint8
+X_BINNED_DTYPE = np.uint8  # hence max_bins == 256
 
 HISTOGRAM_DTYPE = np.dtype([
     ('sum_gradients', Y_DTYPE),  # sum of sample gradients in bin
