b16b4a21822a0133769adec9b9d06a7424c7001f
zwl <7330385+esvhd@users.noreply.github.com>
2019-01-23 21:16:48 +0000

    fix regex escape char for flake8

--- sklearn/model_selection/tests/test_search.py
+++ sklearn/model_selection/tests/test_search.py
@@ -1747,6 +1747,6 @@ def test_empty_cv_iterator_error():
     # assert that this raises an error
     with pytest.raises(ValueError,
                        match='No fits were performed. '
-                             'Was the CV iterator empty\? '
-                             'Were there no candidates\?'):
+                             'Was the CV iterator empty\\? '
+                             'Were there no candidates\\?'):
         ridge.fit(X[:train_size], y[:train_size])
