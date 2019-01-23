2142dc2d639e0ab932ed3e95feb0d97755e2e31c
Raf Baluyot <baluyotraf@outlook.com>
2019-01-23 13:46:14 +0800

    Improved _encoding test for _nanencoding and added test for _nanencoding with missing_values

--- sklearn/preprocessing/tests/test_label.py
+++ sklearn/preprocessing/tests/test_label.py
@@ -623,6 +623,46 @@ def test_nanencode_util_as_encode(values, expected):
     assert_array_equal(uniques, expected)
     assert_array_equal(encoded, np.array([1, 0, 2, 0, 2]))
     assert_array_equal(na_mask, np.zeros_like(values, dtype=np.bool))
-    _, encoded, na_mask = _nanencode(values, uniques, encode=True)
+    uniques_, encoded, na_mask = _nanencode(values, uniques, encode=True)
+    assert_array_equal(uniques_, uniques)
     assert_array_equal(encoded, np.array([1, 0, 2, 0, 2]))
     assert_array_equal(na_mask, np.zeros_like(values, dtype=np.bool))
+
+
+@pytest.mark.parametrize(
+        "values, expected, missing_value",
+        [(np.array([2, 1, 3, 1, 3, np.nan, 2, np.nan], dtype='float64'),
+          np.array([1, 2, 3], dtype='float64'),
+          np.nan),
+         (np.array([2, 1, np.nan, 1, np.nan, 3, 2, 3], dtype='float64'),
+          np.array([1, 2, np.nan], dtype='float64'),
+          3),
+         (np.array([2, 1, 3, 1, 3, 4, 2, 4], dtype='int64'),
+          np.array([1, 2, 3], dtype='int64'),
+          4),
+         (np.array(['b', 'a', 'c', 'a', 'c', None, 'b', None,], dtype=object),
+          np.array(['a', 'b', 'c'], dtype=object),
+          None),
+         (np.array(['b', 'a', None, 'a', None, 'c', 'b', 'c', ], dtype=object),
+          np.array(['a', 'b', None], dtype=object),
+          'c'),
+         (np.array(['b', 'a', 'c', 'a', 'c', 'd', 'b', 'd']),
+          np.array(['a', 'b', 'c']),
+          'd')],
+        ids=['float64_nan', 'float64_value', 'int64_value',
+             'object_none', 'object_value', 'str_value'])
+def test_nanencode_util_with_missing(values, expected, missing_value):
+    encoding_answer = np.array([1, 0, 2, 0, 2, 1])
+    na_mask_answer = np.array([0, 0, 0, 0, 0, 1, 0, 1], dtype=np.bool)
+    uniques = _nanencode(values, missing_value=missing_value)
+    assert_array_equal(uniques, expected)
+    uniques, encoded, na_mask = _nanencode(values, encode=True,
+                                           missing_value=missing_value)
+    assert_array_equal(uniques, expected)
+    assert_array_equal(na_mask, na_mask_answer)
+    assert_array_equal(encoded[~na_mask], encoding_answer)
+    uniques_, encoded, na_mask = _nanencode(values, uniques, encode=True,
+                                            missing_value=missing_value)
+    assert_array_equal(uniques_, uniques)
+    assert_array_equal(na_mask, na_mask_answer)
+    assert_array_equal(encoded[~na_mask], encoding_answer)
