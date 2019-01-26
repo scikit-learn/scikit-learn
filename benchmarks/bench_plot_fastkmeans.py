b6c0c4aa4262c482c5aa8143fb96b93885d85d8a
Hanmin Qin <qinhanmin2005@sina.com>
2019-01-26 15:12:13 +0800

    Revert "MNT Remove six.py"
    This reverts commit 4d1eba8e6086ec3e21d546b3e4dd8f0adfdb71df.

--- benchmarks/bench_plot_fastkmeans.py
+++ benchmarks/bench_plot_fastkmeans.py
@@ -3,6 +3,8 @@ from __future__ import print_function
 from collections import defaultdict
 from time import time
 
+import six
+
 import numpy as np
 from numpy import random as nr
 
--- benchmarks/bench_plot_omp_lars.py
+++ benchmarks/bench_plot_omp_lars.py
@@ -9,6 +9,8 @@ import gc
 import sys
 from time import time
 
+import six
+
 import numpy as np
 
 from sklearn.linear_model import lars_path, orthogonal_mp
