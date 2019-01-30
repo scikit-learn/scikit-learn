94020101175adfa8e7c91c1328cc8dc73a99e79f
agamemnonc <agamemnon.krasoulis@gmail.com>
2019-01-30 16:07:11 +0000

    comment typo fix

--- sklearn/decomposition/tests/test_online_lda.py
+++ sklearn/decomposition/tests/test_online_lda.py
@@ -180,7 +180,7 @@ def test_lda_negative_input():
 
 
 def test_lda_no_component_error():
-    # test and `perplexity` before `fit`
+    # test `perplexity` before `fit`
     rng = np.random.RandomState(0)
     X = rng.randint(4, size=(20, 10))
     lda = LatentDirichletAllocation()
