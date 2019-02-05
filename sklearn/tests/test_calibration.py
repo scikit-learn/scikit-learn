601466ef5ff1c454e2474efa33d346913623e667
Scott Cole <scott.cole0@gmail.com>
2019-02-04 20:05:36 -0800

    remove forgotten line from calibration_curve test

--- sklearn/tests/test_calibration.py
+++ sklearn/tests/test_calibration.py
@@ -264,7 +264,6 @@ def test_calibration_curve():
     y_pred2 = np.array([0., 0.1, 0.2, 0.5, 0.9, 1.])
     prob_true_quantile, prob_pred_quantile = calibration_curve(
         y_true2, y_pred2, n_bins=2, strategy='quantile')
-        calibration_curve(y_true2, y_pred2, n_bins=2, strategy='quantile')
 
     assert len(prob_true_quantile) == len(prob_pred_quantile)
     assert len(prob_true_quantile) == 2
