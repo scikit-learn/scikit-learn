00c56dede40832e4fc95271992fad14a57b9c101
Thomas Fan <thomasjpfan@gmail.com>
2019-02-08 08:28:43 -0500

    ENH Adds files

---
+++ sklearn/inspect/permutation_importance.py
@@ -0,0 +1,6 @@
+"""Permutation importance for estimators"""
+
+
+def permutation_importance(
+        model, X_valid, y_valid, features=None, metric=None):
+    pass
---
+++ sklearn/inspect/tests/test_permutation_importance.py
@@ -0,0 +1,2 @@
+def test_permutation_importance():
+    pass
