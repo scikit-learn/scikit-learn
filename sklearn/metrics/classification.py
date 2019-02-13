b17d632d263c32b39c6cebf93389fc2d5f99c88e
Alex Xu <ayx2@case.edu>
2019-02-13 09:16:08 -0700

    Cleared blank lines

--- sklearn/metrics/classification.py
+++ sklearn/metrics/classification.py
@@ -2242,29 +2242,37 @@ def brier_score_loss(y_true, y_prob, sample_weight=None, pos_label=None):
     y_true = _check_binary_probabilistic_predictions(y_true, y_prob)
     return np.average((y_true - y_prob) ** 2, weights=sample_weight)
 
+
 def min_precision(y_true, y_pred):
     """
     Precision (p) and recall (r) must satisfy,
-    
+
     .. math::
-        {p} \geq \\frac{{s}{r}}{1 - {s} + {s r}}
+        {p} \\geq \\frac{{s}{r}}{1 - {s} + {s r}}
     where :math:`s` is the skew and :math:`r` is the recall
-    
+
+    We can derive the minimum precision possible with skew of the data, given the recall values.
+
     Theorem 1 from Paper:
         Unachievable Region in Precision-Recall Space by Boyd, Costa, Davis, and Page 2012
-        
+
     Parameters
     ----------
-    y_true :
-    y_pred :
-        
+    y_true : array, shape (n_samples,)
+        True targets.
+
+    y_pred : array, shape (n_samples,)
+        Classications or Probabilities of the positive class.
+
     Returns
     -------
-    References
+    min_precision : 
+
+        References
     ----------
     .. [1] `Unachievable Region in Precision-Recall Space by Boyd, Costa, Davis, and Page 2012
             <https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3858955/>
-    
+
     Examples
     --------
     >>> y_true = np.array([0, 1, 1, 0, 1, 1])
@@ -2272,50 +2280,50 @@ def min_precision(y_true, y_pred):
     >>> min_precision(y_true, y_pred)
     ... # doctest: +ELLIPSIS
     array([0.66666667, 0.6       ])
-    
     """
     # Get float or array of Recall Scores
     _, r, _, _ = precision_recall_fscore_support(y_true, y_pred)
     # Calculate skew
     s = np.count_nonzero(y_true)/y_true.size
     # Calculate minimum precision for given recall score and skew of labels
-    min_precision = np.array( list( map( lambda r: (s*r)/(1-s+s*r), r ) ) )
+    min_precision = np.array(list(map(lambda r: (s*r)/(1-s+s*r), r)))
     return(min_precision)
-    
+
+
 def aucpr_min(y_true):
     """
     Minimum AUC for Precision Recall Curve, for given a skew between positive and negative labels.
-    
+
     .. math::
         \\text{AUCPRmin} = \\ 1 + \\frac{(1 - {s}) \\ ln(1 - {s})}{s}
     where :math:`s` is the skew of the data
-        
+
     Theorem 2 from Paper:
         Unachievable Region in Precision-Recall Space by Boyd, Costa, Davis, and Page 2012
-        
+
     Parameters
     ----------
     y_true : array, shape = [n_samples,]
         True binary labels or binary label indicators. 1=Positive Labels, 0=Negative Labels
-    
+
     Returns
     -------
     aucpr_min : float
-    
+
     References
     ----------
     .. [1] `Unachievable Region in Precision-Recall Space by Boyd, Costa, Davis, and Page 2012
             <https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3858955/>
-    
+
     Relevance
     ---------
     "The existence of the minimum AUCPR and minimum AP can affect the qualitative interpretation of a model's
     performance. For example, changing the skew of a data set from 0.01 to 0.5 (e.g., by subsampling the negative
     examples) increases the minimum AUCPR by approximately 0.3. (See Examples)
-    
+
     "This leads to an automatic jump of 0.3 in AUCPR simply by changing the data set and with absolutely
     no change to the learning algorithm"
-    
+
     Examples
     --------
     >>> y_true = np.append(np.zeros(99),1) 
@@ -2330,43 +2338,44 @@ def aucpr_min(y_true):
     # Calculate skew
     s = np.count_nonzero(y_true)/y_true.size
     # Calculate AUCPRmin
-    aucpr_min = 1 + ( (1-s)*log(1-s) )/s
+    aucpr_min = 1 + ((1-s)*log(1-s))/s
     return(aucpr_min)
+
 
 def min_average_precision(y_true):
     """
-    The minimum Average Precision (not to be confused with sklearn.metrics.average_precision_score), for positive
+    The minimum Average Precision Score (like sklearn.metrics.average_precision_score), for positive
     (pos) and negative (neg) examples, respectively, is
-    
+
     .. math::
         \\text{APmin} = \\frac{1}{pos} \\sum_{i=1}^{pos}\\frac{i}{i + {neg}}
-        
+
     Theorem 3 from Paper:
         Unachievable Region in Precision-Recall Space by Boyd, Costa, Davis, and Page 2012
-    
+
     Parameters
     ----------
     y_true : array, shape = [n_samples,]
         True binary labels or binary label indicators. 1=Positive Labels, 0=Negative Labels
-    
+
     Returns
     -------
     min_average_precision : float
-    
+
     References
     ----------
     .. [1] `Unachievable Region in Precision-Recall Space by Boyd, Costa, Davis, and Page 2012
             <https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3858955/>
-    
+
     Relevance
     ---------
     "The existence of the minimum AUCPR and minimum AP can affect the qualitative interpretation of a model's
     performance. For example, changing the skew of a data set from 0.01 to 0.5 (e.g., by subsampling the negative
     examples) increases the minimum AUCPR by approximately 0.3. (See Examples)
-    
+
     "This leads to an automatic jump of 0.3 in AUCPR simply by changing the data set and with absolutely
     no change to the learning algorithm"
-    
+
     Examples
     --------
     >>> y_true = np.append(np.zeros(99),1) 
@@ -2384,6 +2393,6 @@ def min_average_precision(y_true):
     neg = y_true.size - pos
     # Create i to sum over
     i = np.arange(1,pos+1)
-    min_average_precision = (1/pos) * np.sum( i/(i+neg) )
+    min_average_precision = (1/pos) * np.sum(i/(i+neg))
     
     return(min_average_precision)
