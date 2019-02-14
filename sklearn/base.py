40eab3242033ace0a00866ad600bb5ac87a740b6
Hanmin Qin <qinhanmin2005@sina.com>
2019-02-14 09:17:44 +0800

    note in doc

--- sklearn/base.py
+++ sklearn/base.py
@@ -322,6 +322,15 @@ class RegressorMixin:
         -------
         score : float
             R^2 of self.predict(X) wrt. y.
+
+        Notes
+        -----
+        The default value of ``multioutput`` in the ``score`` method of will
+        change from ``variance_weighted`` to ``uniform_average`` in 0.23 to
+        keep consistent with ``metrics.r2_score``. This will influence the
+        ``score`` method of all the regressors. To use the new default, please
+        either call ``metrics.r2_score`` directly or make a custom scorer with
+        ``metric.make_scorer``.
         """
 
         from .metrics import r2_score
