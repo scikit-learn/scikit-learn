1d0db904668c4f17c3238c7fc7bdeb1375589f48
Thomas Fan <thomasjpfan@gmail.com>
2019-02-20 14:21:00 -0500

    BLD Fix

--- build_tools/azure/install.sh
+++ build_tools/azure/install.sh
@@ -53,10 +53,12 @@ if [[ "$DISTRIB" == "conda" ]]; then
 	make_conda $TO_INSTALL
 
 elif [[ "$DISTRIB" == "ubuntu" ]]; then
+    set -x
     sudo apt-get install python3-scipy libatlas3-base libatlas-base-dev libatlas-dev virtualenv
     virtualenv --system-site-packages --python=python3 $VIRTUALENV
     source $VIRTUALENV/bin/activate
     pip install pytest pytest-cov cython joblib==$JOBLIB_VERSION
+    set +x
 fi
 
 python --version
