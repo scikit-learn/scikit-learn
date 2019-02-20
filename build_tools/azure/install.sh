a40418f6f36b7673c77fdefb4b1fa03b729e85e7
Thomas Fan <thomasjpfan@gmail.com>
2019-02-20 14:35:41 -0500

    BLD Fix

--- build_tools/azure/install.sh
+++ build_tools/azure/install.sh
@@ -54,7 +54,7 @@ if [[ "$DISTRIB" == "conda" ]]; then
 elif [[ "$DISTRIB" == "ubuntu" ]]; then
     set -x
     sudo apt-get install python3-scipy libatlas3-base libatlas-base-dev libatlas-dev virtualenv
-    python -c "import numpy; print('numpy %s' % numpy.__version__)"
+    python3 -c "import numpy; print('numpy %s' % numpy.__version__)"
     echo "$PYTHONPATH"
     virtualenv --system-site-packages --python=python3 $VIRTUALENV
     source $VIRTUALENV/bin/activate
