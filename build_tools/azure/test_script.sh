a282585d262363d60960095493375d0fce419e53
Thomas Fan <thomasjpfan@gmail.com>
2019-02-20 13:46:52 -0500

    BLD Fix

--- build_tools/azure/test_script.sh
+++ build_tools/azure/test_script.sh
@@ -8,6 +8,13 @@ python --version
 python -c "import numpy; print('numpy %s' % numpy.__version__)"
 python -c "import scipy; print('scipy %s' % scipy.__version__)"
 python -c "import multiprocessing as mp; print('%d CPUs' % mp.cpu_count())"
+python -c "
+try:
+    import pandas
+    print('pandas %s' % pandas.__version__)
+except ImportError:
+    pass
+"
 
 run_tests() {
     TEST_CMD="pytest --showlocals --durations=20 --pyargs"
