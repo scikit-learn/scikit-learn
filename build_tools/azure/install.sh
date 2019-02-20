c5e0e06b93a2c6f30293a81b1b5f57462285ff9a
Thomas Fan <thomasjpfan@gmail.com>
2019-02-20 13:47:52 -0500

    BLD Fix

--- build_tools/azure/install.sh
+++ build_tools/azure/install.sh
@@ -17,16 +17,21 @@ make_conda() {
     ./miniconda.sh -b
 }
 
-echo "$VIRTUALENV_DIR"
-
 if [[ "$DISTRIB" == "ubuntu" ]]; then
     pip install virtualenv numpy==$NUMPY_VERSION scipy==$SCIPY_VERSION
     virtualenv --system-site-packages --python=python3 $VIRTUALENV_DIR
     source $VIRTUALENV_DIR/bin/activate
-    pip install pytest  cython joblib==$JOBLIB_VERSION
-fI
+    pip install pytest pytest-cov cython joblib==$JOBLIB_VERSION \
+fi
 
-# python --version
-# python -c "import numpy; print('numpy %s' % numpy.__version__)"
-# python -c "import scipy; print('scipy %s' % scipy.__version__)"
-# python setup.py develop
+python --version
+python -c "import numpy; print('numpy %s' % numpy.__version__)"
+python -c "import scipy; print('scipy %s' % scipy.__version__)"
+python -c "\
+try:
+    import pandas
+    print('pandas %s' % pandas.__version__)
+except ImportError:
+    pass
+"
+python setup.py develop
