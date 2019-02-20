20200dc039e30533e05a1b4be575eacf5c73b332
Thomas Fan <thomasjpfan@gmail.com>
2019-02-20 13:54:20 -0500

    BLD Fix

--- build_tools/azure/test_pytest_soft_dependency.sh
+++ build_tools/azure/test_pytest_soft_dependency.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 set -e
+
+if [[ "$CHECK_PYTEST_SOFT_DEPENDENCY" == "true" ]]; then
+
+fi
--- build_tools/azure/test_script.sh
+++ build_tools/azure/test_script.sh
@@ -8,7 +8,7 @@ python --version
 python -c "import numpy; print('numpy %s' % numpy.__version__)"
 python -c "import scipy; print('scipy %s' % scipy.__version__)"
 python -c "import multiprocessing as mp; print('%d CPUs' % mp.cpu_count())"
-python -c "
+python -c "\
 try:
     import pandas
     print('pandas %s' % pandas.__version__)
