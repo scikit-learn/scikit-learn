1d587433bfd1d3cbd5925f6c83cf9f7108ca7926
Thomas Fan <thomasjpfan@gmail.com>
2019-02-20 14:24:11 -0500

    BLD Fix

--- build_tools/azure/install.sh
+++ build_tools/azure/install.sh
@@ -62,6 +62,7 @@ elif [[ "$DISTRIB" == "ubuntu" ]]; then
     set +x
 fi
 
+which python
 python --version
 python -c "import numpy; print('numpy %s' % numpy.__version__)"
 python -c "import scipy; print('scipy %s' % scipy.__version__)"
