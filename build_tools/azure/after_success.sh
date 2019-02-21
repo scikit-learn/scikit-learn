bc06bf2db36220e7fc536462424a759ed4ce97d6
Thomas Fan <thomasjpfan@gmail.com>
2019-02-20 20:10:34 -0500

    BLD Windows

--- build_tools/azure/after_success.sh
+++ build_tools/azure/after_success.sh
@@ -11,11 +11,11 @@ if [[ "$COVERAGE" == "true" ]]; then
     fi
     # Need to run codecov from a git checkout, so we copy .coverage
     # from TEST_DIR where pytest has been run
-    cp $TEST_DIR/.coverage $TRAVIS_BUILD_DIR
+    cp $TEST_DIR/.coverage $BUILD_REPOSITORY_LOCALPATH
 
     # Ignore codecov failures as the codecov server is not
     # very reliable but we don't want travis to report a failure
     # in the github UI just because the coverage report failed to
     # be published.
-    codecov --root $TRAVIS_BUILD_DIR || echo "codecov upload failed"
+    codecov --root $BUILD_REPOSITORY_LOCALPATH || echo "codecov upload failed"
 fi
