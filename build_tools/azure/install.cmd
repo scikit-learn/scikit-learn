1e8a72b8620b10729c8ae3af2954c877cef0b761
Thomas Fan <thomasjpfan@gmail.com>
2019-02-20 19:37:51 -0500

    BLD Test

--- build_tools/azure/install.cmd
+++ build_tools/azure/install.cmd
@@ -19,6 +19,8 @@ python -m pip install -U pip
 python --version
 pip --version
 
+pip install --timeout=60 --trusted-host 28daf2247a33ed269873-7b1aad3fab3cc330e1fd9d109892382a.r6.cf2.rackcdn.com -r build_tools\appveyor\requirements.txt
+
 @rem Install the build and runtime dependencies of the project.
 python setup.py bdist_wheel bdist_wininst -b doc\logos\scikit-learn-logo.bmp
 
--- build_tools/azure/test_script.cmd
+++ build_tools/azure/test_script.cmd
@@ -1,7 +1,5 @@
 call activate %VIRTUALENV%
 
-pip install --timeout=60 --trusted-host 28daf2247a33ed269873-7b1aad3fab3cc330e1fd9d109892382a.r6.cf2.rackcdn.com -r build_tools\appveyor\requirements.txt
-
 mkdir %TMP_FOLDER%
 cd %TMP_FOLDER%
 
