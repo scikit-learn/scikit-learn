b014885425f4559d832bce5c5ec8c7f32c9a20d5
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2019-02-21 13:32:07 +0100

    fix show_version

--- sklearn/setup.py
+++ sklearn/setup.py
@@ -1,12 +1,15 @@
 import os
 
 from sklearn._build_utils import maybe_cythonize_extensions
-from sklearn._build_utils import get_blas_info  # noqa
 
 
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
+    from numpy.distutils.system_info import get_info
     import numpy
+
+    # needs to be called during build otherwise show_version may fail sometimes
+    get_info('blas_opt', 0)
 
     libraries = []
     if os.name == 'posix':
