d96834709c391cb10d7fee130e55df8e6787dbf6
Thomas Fan <thomasjpfan@gmail.com>
2019-02-20 20:43:22 -0500

    BLD Windows

--- build_tools/azure/test_script.cmd
+++ build_tools/azure/test_script.cmd
@@ -3,7 +3,7 @@ call activate %VIRTUALENV%
 mkdir %TMP_FOLDER%
 cd %TMP_FOLDER%
 
-if "%CHECK_WARNINGS" == "true" (
+if "%CHECK_WARNINGS%" == "true" (
     pytest --junitxml=%JUNITXML% --showlocals --durations=20 -Werror::DeprecationWarning -Werror::FutureWarning --pyargs sklearn.preprocessing
 ) else (
     pytest --junitxml=%JUNITXML% --showlocals --durations=20 --pyargs sklearn.preprocessing
