32b3b6b573a5045d047ce241e65841c5ee884181
Thomas Fan <thomasjpfan@gmail.com>
2019-02-21 20:03:36 -0500

    BLD Fast fail

--- build_tools/travis/travis_fastfail.sh
+++ build_tools/travis/travis_fastfail.sh
@@ -4,11 +4,13 @@
 curlhdr="Accept: application/vnd.travis-ci.2+json"
 endpoint="https://api.travis-ci.org/repos/$TRAVIS_REPO_SLUG"
 
+echo "TRAVIS_PULL_REQUEST"
 echo "$TRAVIS_PULL_REQUEST"
 # Fail fast for superseded builds to PR's
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   newestbuildforthisPR=$(curl -H "$curlhdr" $endpoint/builds?event_type=pull_request | \
       jq ".builds | map(select(.pull_request_number == $TRAVIS_PULL_REQUEST))[0].number")
+  echo "newestbuildforthisPR"
   echo "$newestbuildforthisPR"
   if [ $newestbuildforthisPR != null -a $newestbuildforthisPR != \"$TRAVIS_BUILD_NUMBER\" ]; then
     echo "There are newer queued builds for this pull request, failing early."
@@ -22,6 +24,7 @@ else
     *)
       if [ \"$TRAVIS_BUILD_NUMBER\" != $(curl -H "$curlhdr" \
           $endpoint/branches/$TRAVIS_BRANCH | jq ".branch.number") ]; then
+        echo "TRAVIS_BUILD_NUMBER"
         echo "$TRAVIS_BUILD_NUMBER"
         echo "There are newer queued builds for this branch, failing early."
         exit 1
