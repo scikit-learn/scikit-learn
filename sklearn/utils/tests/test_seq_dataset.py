191da7a2b1ba060d801518a2cd0af854c07f96ce
Joan Massich <sik@visor.udg.edu>
2019-02-25 18:47:13 +0100

    Use parametrized to cover ArrayDaset-CSRDataset-32-64 matrix

--- sklearn/utils/tests/test_seq_dataset.py
+++ sklearn/utils/tests/test_seq_dataset.py
@@ -1,15 +1,18 @@
 # Author: Tom Dupre la Tour
+#         Joan Massich <mailsik@gmail.com>
 #
 # License: BSD 3 clause
 
+import pytest
 import numpy as np
 from numpy.testing import assert_array_equal
 from sklearn.utils.testing import assert_allclose
 import scipy.sparse as sp
 
 from sklearn.utils.seq_dataset import ArrayDataset64
-from sklearn.utils.seq_dataset import CSRDataset64
 from sklearn.utils.seq_dataset import ArrayDataset32
+from sklearn.utils.seq_dataset import CSRDataset64
+from sklearn.utils.seq_dataset import CSRDataset32
 
 from sklearn.datasets import load_iris
 
@@ -25,76 +28,106 @@ X_csr32 = sp.csr_matrix(X32)
 sample_weight32 = np.arange(y32.size, dtype=np.float32)
 
 
-def assert_csr_equal(X, Y):
-    X.eliminate_zeros()
-    Y.eliminate_zeros()
-    assert X.shape[0] == Y.shape[0]
-    assert X.shape[1] == Y.shape[1]
-    assert_array_equal(X.data, Y.data)
-    assert_array_equal(X.indices, Y.indices)
-    assert_array_equal(X.indptr, Y.indptr)
+def assert_csr_equal_values(current, expected):
+    current.eliminate_zeros()
+    expected.eliminate_zeros()
+    expected = expected.astype(current.dtype)
+    assert current.shape[0] == expected.shape[0]
+    assert current.shape[1] == expected.shape[1]
+    assert_array_equal(current.data, expected.data)
+    assert_array_equal(current.indices, expected.indices)
+    assert_array_equal(current.indptr, expected.indptr)
 
 
-def test_seq_dataset():
-    dataset1 = ArrayDataset64(X, y, sample_weight, seed=42)
-    dataset2 = CSRDataset64(X_csr.data, X_csr.indptr, X_csr.indices,
-                          y, sample_weight, seed=42)
+@pytest.mark.parametrize(
+    'dataset',
+    [
+        ArrayDataset64(X, y, sample_weight, seed=42),
+        ArrayDataset32(X32, y32, sample_weight32, seed=42),
+        CSRDataset64(X_csr.data, X_csr.indptr, X_csr.indices, y, sample_weight,
+                     seed=42),
+        CSRDataset32(X_csr32.data, X_csr32.indptr, X_csr32.indices, y32,
+                     sample_weight32, seed=42),
+    ],
+    ids=['ArrayDataset64', 'ArrayDataset32', 'CSRDataset64', 'CSRDataset32']
+)
+def test_seq_dataset_basic_iteration(dataset):
+    NUMBER_OF_RUNS = 5
+    for _ in range(NUMBER_OF_RUNS):
+        # next sample
+        xi_, yi, swi, idx = dataset._next_py()
+        xi = sp.csr_matrix((xi_), shape=(1, X.shape[1]))
 
-    for dataset in (dataset1, dataset2):
-        for i in range(5):
-            # next sample
-            xi_, yi, swi, idx = dataset._next_py()
-            xi = sp.csr_matrix((xi_), shape=(1, X.shape[1]))
+        assert_csr_equal_values(xi, X_csr[idx])
+        assert yi == y[idx]
+        assert swi == sample_weight[idx]
 
-            assert_csr_equal(xi, X_csr[idx])
-            assert yi == y[idx]
-            assert swi == sample_weight[idx]
+        # random sample
+        xi_, yi, swi, idx = dataset._random_py()
+        xi = sp.csr_matrix((xi_), shape=(1, X.shape[1]))
 
-            # random sample
-            xi_, yi, swi, idx = dataset._random_py()
-            xi = sp.csr_matrix((xi_), shape=(1, X.shape[1]))
-
-            assert_csr_equal(xi, X_csr[idx])
-            assert yi == y[idx]
-            assert swi == sample_weight[idx]
+        assert_csr_equal_values(xi, X_csr[idx])
+        assert yi == y[idx]
+        assert swi == sample_weight[idx]
 
 
-def test_seq_dataset_shuffle():
-    dataset1 = ArrayDataset64(X, y, sample_weight, seed=42)
-    dataset2 = CSRDataset64(X_csr.data, X_csr.indptr, X_csr.indices,
-                          y, sample_weight, seed=42)
-
+@pytest.mark.parametrize(
+    'dense_dataset,sparse_dataset',
+    [
+        (ArrayDataset64(X, y, sample_weight, seed=42),
+        CSRDataset64(X_csr.data, X_csr.indptr, X_csr.indices, y, sample_weight,
+                     seed=42)),
+        (ArrayDataset32(X32, y32, sample_weight32, seed=42),
+        CSRDataset32(X_csr32.data, X_csr32.indptr, X_csr32.indices, y32,
+                     sample_weight32, seed=42))
+    ],
+    ids=['float 64', 'float 32']
+)
+def test_seq_dataset_shuffle(dense_dataset, sparse_dataset):
     # not shuffled
     for i in range(5):
-        _, _, _, idx1 = dataset1._next_py()
-        _, _, _, idx2 = dataset2._next_py()
+        _, _, _, idx1 = dense_dataset._next_py()
+        _, _, _, idx2 = sparse_dataset._next_py()
         assert idx1 == i
         assert idx2 == i
 
     for i in range(5):
-        _, _, _, idx1 = dataset1._random_py()
-        _, _, _, idx2 = dataset2._random_py()
+        _, _, _, idx1 = dense_dataset._random_py()
+        _, _, _, idx2 = sparse_dataset._random_py()
         assert idx1 == idx2
 
     seed = 77
-    dataset1._shuffle_py(seed)
-    dataset2._shuffle_py(seed)
+    dense_dataset._shuffle_py(seed)
+    sparse_dataset._shuffle_py(seed)
 
     for i in range(5):
-        _, _, _, idx1 = dataset1._next_py()
-        _, _, _, idx2 = dataset2._next_py()
+        _, _, _, idx1 = dense_dataset._next_py()
+        _, _, _, idx2 = sparse_dataset._next_py()
         assert idx1 == idx2
 
-        _, _, _, idx1 = dataset1._random_py()
-        _, _, _, idx2 = dataset2._random_py()
+        _, _, _, idx1 = dense_dataset._random_py()
+        _, _, _, idx2 = sparse_dataset._random_py()
         assert idx1 == idx2
 
 
-def test_fused_types_consistency():
-    dataset32 = ArrayDataset32(X32, y32, sample_weight32, seed=42)
-    dataset64 = ArrayDataset64(X, y, sample_weight, seed=42)
-
-    for i in range(5):
+@pytest.mark.parametrize(
+    'dataset32,dataset64',
+    [
+        (
+            ArrayDataset32(X32, y32, sample_weight32, seed=42),
+            ArrayDataset64(X, y, sample_weight, seed=42)
+        ),(
+            CSRDataset32(X_csr32.data, X_csr32.indptr, X_csr32.indices, y32,
+                         sample_weight32, seed=42),
+            CSRDataset64(X_csr.data, X_csr.indptr, X_csr.indices, y,
+                         sample_weight, seed=42)
+        )
+    ],
+    ids=['ArrayDataset', 'CSRDataset']
+)
+def test_fused_types_consistency(dataset32, dataset64):
+    NUMBER_OF_RUNS = 5
+    for _ in range(NUMBER_OF_RUNS):
         # next sample
         xi32, yi32, _, _ = dataset32._next_py()
         xi64, yi64, _, _ = dataset64._next_py()
@@ -104,8 +137,6 @@ def test_fused_types_consistency():
 
         assert xi_data32.dtype == np.float32
         assert xi_data64.dtype == np.float64
-        assert isinstance(yi32, float)
-        assert isinstance(yi64, float)
 
         assert_allclose(xi_data64, xi_data32, rtol=1e-5)
         assert_allclose(yi64, yi32, rtol=1e-5)
