751b33d1e9f18932adba7b40b87a4335e28bebbe
adrinjalali <adrin.jalali@gmail.com>
2019-02-26 11:40:47 +0100

    fix docstrings

--- sklearn/cluster/optics_.py
+++ sklearn/cluster/optics_.py
@@ -348,22 +348,12 @@ class OPTICS(BaseEstimator, ClusterMixin):
        the Conference "Lernen, Wissen, Daten, Analysen" (LWDA) (2018): 318-329.
     """
 
-    def __init__(self, min_samples=5, max_eps=np.inf, metric='minkowski',
-                 p=2, metric_params=None, cluster_method='dbscan',
-                 eps=0.5, maxima_ratio=.75,
-                 rejection_ratio=.7, similarity_threshold=0.4,
-                 significant_min=.003, min_cluster_size=.005,
-                 min_maxima_ratio=0.001, algorithm='auto',
-                 leaf_size=30, n_jobs=None):
+    def __init__(self, min_samples=5, max_eps=np.inf, metric='minkowski', p=2,
+                 metric_params=None, cluster_method='dbscan', eps=0.5,
+                 algorithm='auto', leaf_size=30, n_jobs=None):
 
         self.max_eps = max_eps
         self.min_samples = min_samples
-        self.maxima_ratio = maxima_ratio
-        self.rejection_ratio = rejection_ratio
-        self.similarity_threshold = similarity_threshold
-        self.significant_min = significant_min
-        self.min_cluster_size = min_cluster_size
-        self.min_maxima_ratio = min_maxima_ratio
         self.algorithm = algorithm
         self.metric = metric
         self.metric_params = metric_params
@@ -491,6 +481,108 @@ def _compute_core_distances_(X, neighbors, min_samples, working_memory):
 
 def compute_optics_graph(X, min_samples, algorithm, leaf_size,
                          metric, metric_params, p, n_jobs, max_eps):
+    """Computes the OPTICS reachability graph.
+
+    Read more in the :ref:`User Guide <optics>`.
+
+    Parameters
+    ----------
+    X : array, shape (n_samples, n_features)
+        The data.
+
+    min_samples : int (default=5)
+        The number of samples in a neighborhood for a point to be considered
+        as a core point.
+
+    max_eps : float, optional (default=np.inf)
+        The maximum distance between two samples for them to be considered
+        as in the same neighborhood. Default value of "np.inf" will identify
+        clusters across all scales; reducing `max_eps` will result in
+        shorter run times.
+
+    metric : string or callable, optional (default='minkowski')
+        metric to use for distance computation. Any metric from scikit-learn
+        or scipy.spatial.distance can be used.
+
+        If metric is a callable function, it is called on each
+        pair of instances (rows) and the resulting value recorded. The callable
+        should take two arrays as input and return one value indicating the
+        distance between them. This works for Scipy's metrics, but is less
+        efficient than passing the metric name as a string.
+
+        Distance matrices are not supported.
+
+        Valid values for metric are:
+
+        - from scikit-learn: ['cityblock', 'cosine', 'euclidean', 'l1', 'l2',
+          'manhattan']
+
+        - from scipy.spatial.distance: ['braycurtis', 'canberra', 'chebyshev',
+          'correlation', 'dice', 'hamming', 'jaccard', 'kulsinski',
+          'mahalanobis', 'minkowski', 'rogerstanimoto', 'russellrao',
+          'seuclidean', 'sokalmichener', 'sokalsneath', 'sqeuclidean',
+          'yule']
+
+        See the documentation for scipy.spatial.distance for details on these
+        metrics.
+
+    p : integer, optional (default=2)
+        Parameter for the Minkowski metric from
+        :class:`sklearn.metrics.pairwise_distances`. When p = 1, this is
+        equivalent to using manhattan_distance (l1), and euclidean_distance
+        (l2) for p = 2. For arbitrary p, minkowski_distance (l_p) is used.
+
+    metric_params : dict, optional (default=None)
+        Additional keyword arguments for the metric function.
+
+    algorithm : {'auto', 'ball_tree', 'kd_tree', 'brute'}, optional
+        Algorithm used to compute the nearest neighbors:
+
+        - 'ball_tree' will use :class:`BallTree`
+        - 'kd_tree' will use :class:`KDTree`
+        - 'brute' will use a brute-force search.
+        - 'auto' will attempt to decide the most appropriate algorithm
+          based on the values passed to :meth:`fit` method. (default)
+
+        Note: fitting on sparse input will override the setting of
+        this parameter, using brute force.
+
+    leaf_size : int, optional (default=30)
+        Leaf size passed to :class:`BallTree` or :class:`KDTree`. This can
+        affect the speed of the construction and query, as well as the memory
+        required to store the tree. The optimal value depends on the
+        nature of the problem.
+
+    n_jobs : int or None, optional (default=None)
+        The number of parallel jobs to run for neighbors search.
+        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
+        ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
+        for more details.
+
+    Returns
+    -------
+    ordering_ : array, shape (n_samples,)
+        The cluster ordered list of sample indices.
+
+    core_distances_ : array, shape (n_samples,)
+        Distance at which each sample becomes a core point, indexed by object
+        order. Points which will never be core have a distance of inf. Use
+        ``clust.core_distances_[clust.ordering_]`` to access in cluster order.
+
+    reachability_ : array, shape (n_samples,)
+        Reachability distances per sample, indexed by object order. Use
+        ``clust.reachability_[clust.ordering_]`` to access in cluster order.
+
+    predecessor_ : array, shape (n_samples,)
+        Point that a sample was reached from, indexed by object order.
+        Seed points have a predecessor of -1.
+
+    References
+    ----------
+    .. [1] Ankerst, Mihael, Markus M. Breunig, Hans-Peter Kriegel,
+       and Jörg Sander. "OPTICS: ordering points to identify the clustering
+       structure." ACM SIGMOD Record 28, no. 2 (1999): 49-60.
+    """
     n_samples = len(X)
     # Start all points as 'unprocessed' ##
     reachability_ = np.empty(n_samples)
