33c46df54b05bb98c96a31589ccf1418a6c9e06d
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2019-02-27 15:11:08 +0100

    pep8

--- sklearn/__init__.py
+++ sklearn/__init__.py
@@ -51,11 +51,11 @@ __version__ = '0.21.dev0'
 # On OSX, we can get a runtime error due to multiple OpenMP libraries loaded
 # simultaneously. This can happen for instance when calling BLAS inside a
 # prange. Setting the following environment variable allows multiple OpenMP
-# libraries. It should not degrade performances since we manually take care of
-# potential over-subcription performance issues, in sections of the code where
-# nested OpenMP loops can happen, by dynamically reconfiguring the inner OpenMP
-# runtime to temporarily disable it while under the scope of the outer OpenMP
-# parallel section.
+# libraries to be loaded. It should not degrade performances since we manually
+# take care of potential over-subcription performance issues, in sections of
+# the code where nested OpenMP loops can happen, by dynamically reconfiguring
+# the inner OpenMP runtime to temporarily disable it while under the scope of
+# the outer OpenMP parallel section.
 os.environ.setdefault("KMP_DUPLICATE_LIB_OK", "True")
 
 
