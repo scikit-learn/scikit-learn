607dd9a0943c20d6911fcbe04c74ead742432661
xavier dupré <xavier.dupre@gmail.com>
2019-02-27 02:51:10 +0100

    pep8

--- sklearn/preprocessing/data.py
+++ sklearn/preprocessing/data.py
@@ -1574,8 +1574,10 @@ class PolynomialFeatures(BaseEstimator, TransformerMixin):
                         for i in range(0, n):
                             a = index[i]
                             new_index.append(pos)
-                            start = a + (index[i+1] - index[i] 
-                                if self.interaction_only else 0)
+                            if self.interaction_only:
+                                start = a + index[i+1] - index[i]
+                            else:
+                                start = a
                             new_pos = pos + end - start
                             if new_pos <= pos:
                                 break
