de051a9391b6f2b410cb257a809f0c6d7e6df1d7
Nicolas Hug <contact@nicolas-hug.com>
2019-02-27 08:30:40 -0500

    added experimental in sklearn.__init__.__all__

--- sklearn/__init__.py
+++ sklearn/__init__.py
@@ -75,6 +75,7 @@ else:
                'naive_bayes', 'neighbors', 'neural_network', 'pipeline',
                'preprocessing', 'random_projection', 'semi_supervised',
                'svm', 'tree', 'discriminant_analysis', 'impute', 'compose',
+               'experimental',
                # Non-modules:
                'clone', 'get_config', 'set_config', 'config_context',
                'show_versions']
