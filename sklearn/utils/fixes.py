4646d2f25e5c2ca8fe3891df08832e7f8dc7b58c
Roman Yurchak <rth.yurchak@pm.me>
2019-02-27 16:50:20 +0100

    More merge conflict fixes

--- sklearn/utils/fixes.py
+++ sklearn/utils/fixes.py
@@ -243,16 +243,3 @@ def _joblib_parallel_args(**kwargs):
         if require == 'sharedmem':
             args['backend'] = 'threading'
     return args
-
-
-# To be removed once this fix is included in six
-try:
-    from collections.abc import Sequence as _Sequence  # noqa
-    from collections.abc import Iterable as _Iterable  # noqa
-    from collections.abc import Mapping as _Mapping  # noqa
-    from collections.abc import Sized as _Sized  # noqa
-except ImportError:  # python <3.3
-    from collections import Sequence as _Sequence  # noqa
-    from collections import Iterable as _Iterable  # noqa
-    from collections import Mapping as _Mapping  # noqa
-    from collections import Sized as _Sized  # noqa
