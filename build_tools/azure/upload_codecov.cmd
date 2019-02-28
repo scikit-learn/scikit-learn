95c8b20b8e58d9e2f2c04c6106e6986a90850ed4
Thomas Fan <thomasjpfan@gmail.com>
2019-02-28 19:36:59 +0100

    TST Debug

--- build_tools/azure/upload_codecov.cmd
+++ build_tools/azure/upload_codecov.cmd
@@ -4,8 +4,4 @@ call activate %VIRTUALENV%
 
 copy %TEST_DIR%\.coverage %BUILD_REPOSITORY_LOCALPATH%
 
-codecov --root %BUILD_REPOSITORY_LOCALPATH% -t %CODECOV_TOKEN% && (
-  echo codecov was successful
-) || (
-  echo codecov failed
-)
+codecov --root %BUILD_REPOSITORY_LOCALPATH% -t %CODECOV_TOKEN%
