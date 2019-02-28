4341e104f1da0096715aad48bc6177f0a9670e17
Thomas Fan <thomasjpfan@gmail.com>
2019-02-28 18:24:26 +0100

    ENH Adds codecov to windows

--- build_tools/azure/upload_codecov.cmd
+++ build_tools/azure/upload_codecov.cmd
@@ -1,8 +1,8 @@
 call activate %VIRTUALENV%
 
-copy $TEST_DIR\.coverage $BUILD_REPOSITORY_LOCALPATH
+copy %TEST_DIR%\.coverage %BUILD_REPOSITORY_LOCALPATH%
 
-codecov --root $BUILD_REPOSITORY_LOCALPATH -t $CODECOV_TOKEN && (
+codecov --root %BUILD_REPOSITORY_LOCALPATH% -t %CODECOV_TOKEN% && (
   echo codecov was successful
 ) || (
   echo codecov failed
