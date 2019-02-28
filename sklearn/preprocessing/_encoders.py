451d27e2af19e0ddb9403da02af443d0732d9017
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2019-02-28 17:30:56 +0100

    final edits

--- sklearn/preprocessing/_encoders.py
+++ sklearn/preprocessing/_encoders.py
@@ -40,12 +40,12 @@ class _BaseEncoder(BaseEstimator, TransformerMixin):
           not do that)
         - return list of features (arrays): this list of features is
           constructed feature by feature to preserve the data types
-          of pandas DataFrame columns), as elsewhere information is lost
-          and cannot be used, eg for the `categories`_ attribute.
+          of pandas DataFrame columns, as otherwise information is lost
+          and cannot be used, eg for the `categories_` attribute.
 
         """
-        # if not a dataframe, do normal check_array validation
         if not (hasattr(X, 'iloc') and getattr(X, 'ndim', 0) == 2):
+            # if not a dataframe, do normal check_array validation
             X_temp = check_array(X, dtype=None)
             if (not hasattr(X, 'dtype')
                     and np.issubdtype(X_temp.dtype, np.str_)):
@@ -55,8 +55,7 @@ class _BaseEncoder(BaseEstimator, TransformerMixin):
             needs_validation = False
         else:
             # pandas dataframe, do validation later column by column, in order
-            # to keep the dtype information to be used
-            # in the encoders attributes.
+            # to keep the dtype information to be used in the encoder.
             needs_validation = True
 
         n_samples, n_features = X.shape
@@ -89,11 +88,11 @@ class _BaseEncoder(BaseEstimator, TransformerMixin):
             if len(self._categories) != n_features:
                 raise ValueError("Shape mismatch: if n_values is an array,"
                                  " it has to be of shape (n_features,).")
+
         self.categories_ = []
 
         for i in range(n_features):
             Xi = X_list[i]
-
             if self._categories == 'auto':
                 cats = _encode(Xi)
             else:
@@ -112,12 +111,12 @@ class _BaseEncoder(BaseEstimator, TransformerMixin):
 
     def _transform(self, X, handle_unknown='error'):
         X_list, n_samples, n_features = self._check_X(X)
+
         X_int = np.zeros((n_samples, n_features), dtype=np.int)
         X_mask = np.ones((n_samples, n_features), dtype=np.bool)
 
         for i in range(n_features):
             Xi = X_list[i]
-
             diff, valid_mask = _encode_check_unknown(Xi, self.categories_[i],
                                                      return_mask=True)
 
