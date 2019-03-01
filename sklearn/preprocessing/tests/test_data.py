2ad8735c500343d76339c1b06a247193bce06df8
Albert Thomas <albertthomas88@gmail.com>
2019-03-01 11:23:56 +0100

    FIX set n_quantiles to min(n_samples, n_quantiles) in QuantileTransformer (#13333)

--- sklearn/preprocessing/tests/test_data.py
+++ sklearn/preprocessing/tests/test_data.py
@@ -1260,6 +1260,13 @@ def test_quantile_transform_check_error():
     assert_raise_message(ValueError,
                          'Expected 2D array, got scalar array instead',
                          transformer.transform, 10)
+    # check that a warning is raised is n_quantiles > n_samples
+    transformer = QuantileTransformer(n_quantiles=100)
+    warn_msg = "n_quantiles is set to n_samples"
+    with pytest.warns(UserWarning, match=warn_msg) as record:
+        transformer.fit(X)
+    assert len(record) == 1
+    assert transformer.n_quantiles_ == X.shape[0]
 
 
 def test_quantile_transform_sparse_ignore_zeros():
