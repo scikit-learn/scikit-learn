4e4e770d534e4212682f589e1495bb0a5f3e54b0
mathurinm <mathurin.massias@gmail.com>
2019-03-12 16:33:45 +0900

    use stem, plot non zero coefs only

--- examples/linear_model/plot_lasso_and_elasticnet.py
+++ examples/linear_model/plot_lasso_and_elasticnet.py
@@ -58,12 +58,12 @@ r2_score_enet = r2_score(y_test, y_pred_enet)
 print(enet)
 print("r^2 on test data : %f" % r2_score_enet)
 
-plt.plot(enet.coef_, color='lightgreen', linewidth=2,
-         label='Elastic net coefficients')
-plt.plot(lasso.coef_, color='gold', linewidth=2,
-         label='Lasso coefficients')
-plt.plot(coef, '--', color='navy', label='original coefficients')
+plt.stem(np.where(enet.coef_)[0], enet.coef_[enet.coef_ != 0], 'C1--',
+         markerfmt='C1x', label='Elastic net coefficients')
+plt.stem(np.where(lasso.coef_)[0], lasso.coef_[lasso.coef_ != 0], 'C2--',
+         markerfmt='C2x', label='Lasso coefficients')
+plt.stem(np.where(coef)[0], coef[coef != 0], label='original coefficients')
 plt.legend(loc='best')
-plt.title("Lasso R^2: %f, Elastic Net R^2: %f"
-          % (r2_score_lasso, r2_score_enet))
+plt.title("Lasso $R^2$: %.3f, Elastic Net $R^2$: %.3f"
+        % (r2_score_lasso, r2_score_enet))
 plt.show()
