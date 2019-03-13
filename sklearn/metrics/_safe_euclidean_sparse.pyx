17fa839bf2f766396bc16147efa2336b545b846a
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2019-03-13 09:55:35 +0100

    clean and comments safe euclidean sparse dense

--- sklearn/metrics/_safe_euclidean_sparse.pyx
+++ sklearn/metrics/_safe_euclidean_sparse.pyx
@@ -1,15 +1,12 @@
 #cython: language_level=3
 #cython: boundscheck=False, cdivision=True, wraparound=False
 
-
 import numpy as np
 cimport numpy as np
 from cython cimport floating
 from libc.math cimport fmax
 
-
 np.import_array()
-
 
 ctypedef fused INT:
     np.int32_t
@@ -21,6 +18,7 @@ def _euclidean_sparse_dense_exact(floating[::1] X_data,
                                   INT[::1] X_indptr,
                                   np.ndarray[floating, ndim=2, mode='c'] Y,
                                   floating[::1] y_squared_norms):
+    """Euclidean distances between X (CSR matrix) and Y (dense)."""
     cdef:
         int n_samples_X = X_indptr.shape[0] - 1
         int n_samples_Y = Y.shape[0]
@@ -42,28 +40,28 @@ def _euclidean_sparse_dense_exact(floating[::1] X_data,
     return np.asarray(D)
 
 
-cdef floating _euclidean_sparse_dense_exact_1d(floating *y_data,
-                                               INT *y_indices,
-                                               int y_nnz,
-                                               floating *x,
-                                               floating x_squared_norm) nogil:
-    """Euclidean distance between x dense and y sparse"""
+cdef floating _euclidean_sparse_dense_exact_1d(floating *x_data,
+                                               INT *x_indices,
+                                               int x_nnz,
+                                               floating *y,
+                                               floating y_squared_norm) nogil:
+    """Euclidean distance between vectors x sparse and y dense"""
     cdef:
         int i
-        floating xi
+        floating yi
         floating tmp = 0.0
         floating result = 0.0
-        floating partial_x_squared_norm = 0.0
+        floating partial_y_squared_norm = 0.0
     
     # Split the loop to avoid unsafe compiler auto optimizations
-    for i in range(y_nnz):
-        xi = x[y_indices[i]]
-        partial_x_squared_norm += xi * xi
+    for i in range(x_nnz):
+        yi = y[x_indices[i]]
+        partial_y_squared_norm += yi * yi
 
     for i in range(y_nnz):
-        tmp = y_data[i] - x[y_indices[i]]
+        tmp = x_data[i] - y[x_indices[i]]
         result += tmp * tmp 
 
-    result += x_squared_norm - partial_x_squared_norm
+    result += y_squared_norm - partial_y_squared_norm
 
     return fmax(result, 0)
