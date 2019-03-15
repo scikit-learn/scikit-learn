2d2c081ceda3d8ef4cfb424b4072e73dd11b3519
Nicolas Hug <contact@nicolas-hug.com>
2019-03-15 10:17:21 -0400

    removed __all__ from _fast.../__init__.py

--- sklearn/_fast_gradient_boosting/__init__.py
+++ sklearn/_fast_gradient_boosting/__init__.py
@@ -3,7 +3,3 @@
 The implementation is a port from pygbm which is itself strongly inspired
 from LightGBM.
 """
-from .gradient_boosting import HistGradientBoostingClassifier
-from .gradient_boosting import HistGradientBoostingRegressor
-
-__all__ = ["HistGradientBoostingClassifier", "HistGradientBoostingRegressor"]
--- sklearn/experimental/__init__.py
+++ sklearn/experimental/__init__.py
@@ -3,7 +3,9 @@ The :mod:`sklearn.experimetal` module includes estimator and tools whose API
 and behaviour might change without a deprecation cycle.
 """
 
-from .._fast_gradient_boosting import HistGradientBoostingClassifier
-from .._fast_gradient_boosting import HistGradientBoostingRegressor
+from .._fast_gradient_boosting.gradient_boosting import (
+    HistGradientBoostingClassifier,
+    HistGradientBoostingRegressor
+)
 
 __all__ = ['HistGradientBoostingRegressor', 'HistGradientBoostingClassifier']
