bdbb14a28f9f9a7db9d2893b28bb43caa65aceba
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2019-03-21 15:08:56 +0000

    Edited n_components_ deprecation message to add double backticks

--- sklearn/cluster/hierarchical.py
+++ sklearn/cluster/hierarchical.py
@@ -766,10 +766,9 @@ class AgglomerativeClustering(BaseEstimator, ClusterMixin):
         self.pooling_func = pooling_func
 
     @property
-    @deprecated("The `n_components` parameter changed to "
-                "`n_connected_components`. This will be "
-                "deprecated in version 0.21 and removed "
-                "in 0.23.")
+    @deprecated("The ``n_components_`` attribute was deprecated "
+                "in favor of ``n_connected_components_`` in 0.21 "
+                "and will be removed in 0.23.")
     def n_components_(self):
         return self.n_connected_components_
 
