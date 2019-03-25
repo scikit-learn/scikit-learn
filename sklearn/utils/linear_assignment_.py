e2d364f8a7ed665579c1b0fb9eed7f8ead736a18
Prathmesh Savale <prathmesh.savale@gmail.com>
2019-03-25 08:20:02 -0400

    review changes #3

--- sklearn/utils/linear_assignment_.py
+++ sklearn/utils/linear_assignment_.py
@@ -15,7 +15,7 @@ import warnings
 
 # Deprecation warning for module
 warnings.warn(
-    "The linear_assignment function is deprecated in 0.21 "
+    "The linear_assignment_ module is deprecated in 0.21 "
     "and will be removed from 0.23. Use "
     "scipy.optimize.linear_sum_assignment instead.",
     DeprecationWarning)
