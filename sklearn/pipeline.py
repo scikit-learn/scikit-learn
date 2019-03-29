c150f29332fface619b6cf54a51f3a3d35ad3350
Osman Zubair <osmanzubair658@gmail.com>
2019-03-29 08:00:25 +0000

    Revert "Added error check and message for pipeline parameter if not found"
    This reverts commit 95b6d9f0eb3c8ed009faca65fd1f73101ce365ad.

--- sklearn/pipeline.py
+++ sklearn/pipeline.py
@@ -254,9 +254,6 @@ class Pipeline(_BaseComposition):
                             if step is not None}
         for pname, pval in fit_params.items():
             step, param = pname.split('__', 1)
-            if step not in fit_params_steps:
-                raise TypeError("Step '{}' not found for parameter '{}'. Use naming convention step__parameter."
-                                .format(step, param))
             fit_params_steps[step][param] = pval
         Xt = X
         for step_idx, name, transformer in self._iter(with_final=False):
