7a8a2cfb36099d5f309d78022ee65199c8e5bafe
Osman Zubair <osmanzubair658@gmail.com>
2019-03-29 08:00:10 +0000

    Revert "Using input argument rather than separated param name"
    This reverts commit a5d5b0f845fc111347c4dc0eed97b6b3b79c11f5.

--- sklearn/pipeline.py
+++ sklearn/pipeline.py
@@ -256,7 +256,7 @@ class Pipeline(_BaseComposition):
             step, param = pname.split('__', 1)
             if step not in fit_params_steps:
                 raise TypeError("Step '{}' not found for parameter '{}'. Use naming convention step__parameter."
-                                .format(step, pname))
+                                .format(step, param))
             fit_params_steps[step][param] = pval
         Xt = X
         for step_idx, name, transformer in self._iter(with_final=False):
