dfeceb4c8447640bb554a34600ad35ba87cce724
Aditya Vyas <adityavyas17@gmail.com>
2019-04-05 20:58:29 -0400

    remove space around =

--- sklearn/neural_network/tests/test_mlp.py
+++ sklearn/neural_network/tests/test_mlp.py
@@ -177,7 +177,7 @@ def test_gradient():
     for n_labels in [2, 3]:
         n_samples = 100
         n_features = 10
-        random_state = np.random.RandomState(seed = 42)
+        random_state = np.random.RandomState(seed=42)
         X = random_state.rand(n_samples, n_features)
         y = 1 + np.mod(np.arange(n_samples) + 1, n_labels)
         Y = LabelBinarizer().fit_transform(y)
