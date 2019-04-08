19a4fe87fc658e630144b4547e71bd52a0988a06
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2019-04-08 22:36:30 +0100

    Replaced LinearSVC with SVC in test_gridsearch_pipeline(), adapted expected string using pytest test_pprint -vvv and removed LinearSVC class import

--- sklearn/utils/tests/test_pprint.py
+++ sklearn/utils/tests/test_pprint.py
@@ -7,7 +7,6 @@ from sklearn.utils._pprint import _EstimatorPrettyPrinter
 from sklearn.pipeline import make_pipeline
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.feature_selection import SelectKBest, chi2
-from sklearn.svm import LinearSVC
 from sklearn import set_config
 
 
@@ -305,7 +304,7 @@ def test_gridsearch_pipeline():
 
     pipeline = Pipeline([
         ('reduce_dim', PCA()),
-        ('classify', LinearSVC())
+        ('classify', SVC())
     ])
     N_FEATURES_OPTIONS = [2, 4, 8]
     C_OPTIONS = [1, 10, 100, 1000]
@@ -332,15 +331,14 @@ GridSearchCV(cv=3, error_score='raise-deprecating',
                                             svd_solver='auto', tol=0.0,
                                             whiten=False)),
                                        ('classify',
-                                        LinearSVC(C=1.0, class_weight=None,
-                                                  dual=True, fit_intercept=True,
-                                                  intercept_scaling=1,
-                                                  loss='squared_hinge',
-                                                  max_iter=1000,
-                                                  multi_class='ovr',
-                                                  penalty='l2',
-                                                  random_state=None, tol=0.0001,
-                                                  verbose=0))]),
+                                        SVC(C=1.0, cache_size=200,
+                                            class_weight=None, coef0=0.0,
+                                            decision_function_shape='ovr',
+                                            degree=3, gamma='auto_deprecated',
+                                            kernel='rbf', max_iter=-1,
+                                            probability=False,
+                                            random_state=None, shrinking=True,
+                                            tol=0.001, verbose=False))]),
              iid='warn', n_jobs=1,
              param_grid=[{'classify__C': [1, 10, 100, 1000],
                           'reduce_dim': [PCA(copy=True, iterated_power=7,
