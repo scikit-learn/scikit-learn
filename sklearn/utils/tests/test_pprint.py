e2d9168c71936eabdb7aac6ab94a824e55251cc9
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2019-04-08 20:54:59 +0100

    Refactored GridSearchCV class constructor to only inherit from BaseEstimator base class

--- sklearn/utils/tests/test_pprint.py
+++ sklearn/utils/tests/test_pprint.py
@@ -68,17 +68,22 @@ class RFE(BaseEstimator):
         self.verbose = verbose
 
 
-class GridSearchCV(BaseSearchCV):
+class GridSearchCV(BaseEstimator):
     def __init__(self, estimator, param_grid, scoring=None,
                  n_jobs=None, iid='warn', refit=True, cv='warn', verbose=0,
                  pre_dispatch='2*n_jobs', error_score='raise-deprecating',
                  return_train_score=False):
-        super().__init__(
-            estimator=estimator, scoring=scoring,
-            n_jobs=n_jobs, iid=iid, refit=refit, cv=cv, verbose=verbose,
-            pre_dispatch=pre_dispatch, error_score=error_score,
-            return_train_score=return_train_score)
+        self.estimator = estimator
         self.param_grid = param_grid
+        self.scoring = scoring
+        self.n_jobs = n_jobs
+        self.iid = iid
+        self.refit = refit
+        self.cv = cv
+        self.verbose = verbose
+        self.pre_dispatch = pre_dispatch
+        self.error_score = error_score
+        self.return_train_score = return_train_score
 
 
 class CountVectorizer(BaseEstimator):
