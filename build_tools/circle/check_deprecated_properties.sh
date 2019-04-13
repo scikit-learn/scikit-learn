501720fe108b0fa76d0047b0fee176e9d3f16565
Nicolas Hug <contact@nicolas-hug.com>
2019-04-13 10:19:05 -0400

    Better handling of deprecated properties

---
+++ build_tools/circle/check_deprecated_properties.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# For docstrings and warnings of deprecated attributes to be rendered
+# properly, the property decorator must come before the deprecated decorator
+# (else they are treated as functions)
+bad_deprecation_property_order=`git grep -A 10 "@property" | awk '/@property/,/def /' | grep -B1 "@deprecated"`
+
+if [ ! -z "$bad_deprecation_property_order" ]
+then
+    echo "property decorator should come before deprecated decorator"
+    echo "found the following occurrencies:"
+    echo $bad_deprecation_property_order
+    exit 1
+fi
