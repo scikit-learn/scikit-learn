717952a1f892e6085a3e6a967deb5417faafed5a
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2019-04-16 10:28:49 +0200

    remove openmp cython check

--- sklearn/_build_utils/check_openmp.pyx
+++
@@ -1,9 +0,0 @@
-# the build will fail if OpenMP support is not handled correctly.
-
-IF SKLEARN_OPENMP_SUPPORTED:
-    cimport openmp
-
-
-def check_openmp():
-    IF SKLEARN_OPENMP_SUPPORTED:
-        openmp.omp_get_max_threads()
