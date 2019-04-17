34b959840fa9e9a17b1cbba968ff015f8d5c70fa
lobpcg <42650045+lobpcg@users.noreply.github.com>
2019-04-16 21:22:14 -0400

    comment on normal matrix clarified

--- sklearn/decomposition/pca.py
+++ sklearn/decomposition/pca.py
@@ -177,7 +177,8 @@ class PCA(_BasePCA):
             run randomized SVD by the method of Halko et al.
         lobpcg :
             run Locally Optimal Block Preconditioned Conjugate Gradient
-            (LOBPCG) by Knyazev 2001 for a normal matrix of the smallest size.
+            (LOBPCG) by Knyazev 2001 for a normal matrix X'*X or X*X',
+            whichever of the two is of the smallest size.
 
         .. versionadded:: 0.18.0
 
