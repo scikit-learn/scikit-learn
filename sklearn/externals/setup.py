7474f35798ef92b32e1de8b1618277dbc2caa028
Roman Yurchak <roman.yurchak@symerio.com>
2019-04-17 11:42:50 +0200

    Address review comments

--- sklearn/externals/setup.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-def configuration(parent_package='', top_path=None):
-    from numpy.distutils.misc_util import Configuration
-    config = Configuration('externals', parent_package, top_path)
-    config.add_subpackage('joblib')
-
-    return config
