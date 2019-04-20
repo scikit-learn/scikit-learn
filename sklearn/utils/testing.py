1da9941364b9c6844e5ec2fcba6c0d172f68f5a1
Nicolas Hug <contact@nicolas-hug.com>
2019-04-20 09:27:52 -0400

    put back helper in experimental/test_  :/

--- sklearn/utils/testing.py
+++ sklearn/utils/testing.py
@@ -17,9 +17,6 @@ import pkgutil
 import warnings
 import sys
 import functools
-import tempfile
-from subprocess import check_output, STDOUT, CalledProcessError
-from subprocess import TimeoutExpired
 
 
 import scipy as sp
@@ -32,7 +29,6 @@ from urllib.error import HTTPError
 
 import tempfile
 import shutil
-import os.path as op
 import atexit
 import unittest
 
@@ -975,56 +971,3 @@ def check_docstring_parameters(func, doc=None, ignore=None, class_name=None):
             if n1 != n2:
                 incorrect += [func_name + ' ' + n1 + ' != ' + n2]
     return incorrect
-
-
-def assert_run_python_script(source_code, timeout=60):
-    """Utility to check assertions in an independent Python subprocess.
-
-    The script provided in the source code should return 0 and not print
-    anything on stderr or stdout.
-
-    This is a port from cloudpickle https://github.com/cloudpipe/cloudpickle
-
-    Parameters
-    ----------
-    source_code : str
-        The Python source code to execute.
-    timeout : int
-        Time in seconds before timeout.
-    """
-    def _make_cwd_env():
-        """Helper to prepare environment for the child processes"""
-        sklearn_repo_folder = op.normpath(
-            op.join(op.dirname(__file__), '../..'))
-        env = os.environ.copy()
-        pythonpath = "{src}{sep}tests{pathsep}{src}".format(
-            src=sklearn_repo_folder, sep=os.sep, pathsep=os.pathsep)
-        env['PYTHONPATH'] = pythonpath
-        return sklearn_repo_folder, env
-
-    fd, source_file = tempfile.mkstemp(suffix='_src_test_sklearn.py')
-    os.close(fd)
-    try:
-        with open(source_file, 'wb') as f:
-            f.write(source_code.encode('utf-8'))
-        cmd = [sys.executable, source_file]
-        cwd, env = _make_cwd_env()
-        kwargs = {
-            'cwd': cwd,
-            'stderr': STDOUT,
-            'env': env,
-        }
-        kwargs['timeout'] = timeout
-        try:
-            try:
-                out = check_output(cmd, **kwargs)
-            except CalledProcessError as e:
-                raise RuntimeError(u"script errored with output:\n%s"
-                                   % e.output.decode('utf-8'))
-            if out != b"":
-                raise AssertionError(out.decode('utf-8'))
-        except TimeoutExpired as e:
-            raise RuntimeError(u"script timeout, output so far:\n%s"
-                               % e.output.decode('utf-8'))
-    finally:
-        os.unlink(source_file)
