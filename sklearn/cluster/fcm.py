2288f7832016664dd85dc5e14a7359faf1a8b977
Sean <lijy263@mail2.sysu.edu.cn>
2019-04-22 17:45:23 +0800

    explaination for fcm function added

--- sklearn/cluster/fcm.py
+++ sklearn/cluster/fcm.py
@@ -12,7 +12,6 @@ from ..base import BaseEstimator, ClusterMixin, TransformerMixin
 from ..metrics.pairwise import euclidean_distances
 from ..metrics.pairwise import pairwise_distances_argmin_min
 from ..utils.extmath import row_norms, squared_norm, stable_cumsum
-from ..utils.sparsefuncs_fast import assign_rows_csr
 from ..utils.sparsefuncs import mean_variance_axis
 from ..utils.validation import _num_samples
 from ..utils import check_array
@@ -24,12 +23,45 @@ from ..utils._joblib import Parallel
 from ..utils._joblib import delayed
 from ..utils._joblib import effective_n_jobs
 from ..exceptions import ConvergenceWarning
-from . import _k_means
-from ._k_means_elkan import k_means_elkan
 
+def fcm(X, n_clusters, m=2, eps=10, random_state=None, max_iter=300, sample_weight=None):
+    """Fuzzy CMeans clustering
 
-def fcm(X, c_clusters, m=2, eps=10, random_state=None, max_iter=300, sample_weight=None):
+    Parameters
+    ----------
+    X : array or sparse matrix, shape (n_samples, n_features)
+        The data to pick seeds for. To avoid memory copy, the input data
+        should be double precision (dtype=np.float64).
+
+    n_clusters : int, optional, default: 3
+        The number of clusters to form as well as the number of
+        centroids to generate.
+
+    max_iter : int, default: 300
+        Maximum number of iterations of the Fuzzy c means algorithm for a
+        single run.
+
+    random_state : int, RandomState instance or None (default)
+        Determines random number generation for centroid initialization. Use
+        an int to make the randomness deterministic.
+        See :term:`Glossary <random_state>`.
+
+    m : int, optional, default: 2
+        A key number for the update the centroids and the cluster-probability matrix.
     
+    eps : float, optional, default: 10
+        If the sum of the abs of the (new_cluster_probability_matrix - old_cluster_probability_matrix) is smaller than eps, then the algorithm will stop.
+        
+
+    Attributes
+    ----------
+    cluster_centers_ : array, [n_clusters, n_features]
+        Coordinates of cluster centers. 
+
+    labels_ :
+        Labels of each point
+
+    """
     if m <= 1:
         raise ValueError("Invalid number of m."
                          " m=%d must be bigger than 1." % m)
@@ -44,23 +76,26 @@ def fcm(X, c_clusters, m=2, eps=10, random_state=None, max_iter=300, sample_weig
     
     X = check_array(X, accept_sparse='csr', dtype=FLOAT_DTYPES)
 
-    membership_mat = np.random.random((len(X), c_clusters))
+    membership_mat = np.random.random((len(X), n_clusters))
     membership_mat = np.divide(membership_mat, np.sum(membership_mat, axis=1)[:, np.newaxis])
 
     for iter_time in range(max_iter):
         working_membership_mat = membership_mat ** m
-        Centroids = np.divide(np.dot(working_membership_mat.T, X), np.sum(working_membership_mat.T, axis=1)[:, np.newaxis])
+        Centroids = np.divide(np.dot(working_membership_mat.T, 
+                X), np.sum(working_membership_mat.T, axis=1)[:, np.newaxis])
         
-        n_c_distance_mat = np.zeros((len(X), c_clusters))
+        n_c_distance_mat = np.zeros((len(X), n_clusters))
         for i, x in enumerate(X):
             for j, c in enumerate(Centroids):
                 n_c_distance_mat[i][j] = np.linalg.norm(x-c, 2)
         
-        new_membership_mat = np.zeros((len(X), c_clusters))
+        new_membership_mat = np.zeros((len(X), n_clusters))
         
         for i, x in enumerate(X):
             for j, c in enumerate(Centroids):
-                new_membership_mat[i][j] = 1. / np.sum((n_c_distance_mat[i][j] / n_c_distance_mat[i]) ** (2 / (m - 1)))
+                new_membership_mat[i][j] = 1. / np.sum(
+                    (n_c_distance_mat[i][j] / n_c_distance_mat[i]) ** (2 / (m - 1))
+                    )
         if np.sum(abs(new_membership_mat - membership_mat)) < eps:
             break
         membership_mat =  new_membership_mat
@@ -110,8 +145,6 @@ class FCM(BaseEstimator, ClusterMixin):
     labels_ :
         Labels of each point
 
-    Examples
-    --------
 
     Notes
     --------
@@ -160,12 +193,12 @@ class FCM(BaseEstimator, ClusterMixin):
             are assigned equal weight (default: None)
 
         """
+
         random_state = check_random_state(self.random_state)
-        X = check_array(X, accept_sparse='csr')
         self.cluster_centers_, self.labels_, self.membership_mat = \
             fcm(
                 X, 
-                c_clusters = self.n_clusters,
+                n_clusters = self.n_clusters,
                 m = self.m,
                 eps = self.eps,
                 random_state = random_state, 
