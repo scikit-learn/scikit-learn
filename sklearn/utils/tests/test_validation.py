Fixed double precision check as per review, and added corresponding test.

sklearn/utils/tests/test_validation.py
@@ -840,10 +840,23 @@ def test_check_scalar_invalid(x, target_name, target_type, min_val, max_val,
 
 _psd_cases_valid = {
     'nominal': ((1, 2), np.array([1, 2]), None, ""),
+    'nominal_np_array': (np.array([1, 2]), np.array([1, 2]), None, ""),
     'insignificant_imag': ((5, 5e-5j), np.array([5, 0]), None, ""),
     'significant neg': ((5, -1), np.array([5, 0]), PSDSpectrumWarning,
                         "There are significant negative eigenvalues"),
-    'insignificant neg': ((5, -5e-5), np.array([5, 0]), None, "")
+    'significant neg float32': (np.array([3e-4, -2e-6], dtype=np.float32),
+                                np.array([3e-4, 0], dtype=np.float32),
+                                PSDSpectrumWarning,
+                                "There are significant negative eigenvalues"),
+    'significant neg float64': (np.array([1e-5, -2e-10], dtype=np.float64),
+                                np.array([1e-5, 0], dtype=np.float64),
+                                PSDSpectrumWarning,
+                                "There are significant negative eigenvalues"),
+    'insignificant neg': ((5, -5e-5), np.array([5, 0]), None, ""),
+    'insignificant neg float32': (np.array([1, -1e-6], dtype=np.float32),
+                                  np.array([1, 0], dtype=np.float32), None, ""),
+    'insignificant neg float64': (np.array([1, -1e-10], dtype=np.float64),
+                                  np.array([1, 0], dtype=np.float64), None, ""),
 }
 
 

sklearn/utils/validation.py
@@ -1051,7 +1051,13 @@ def check_psd_eigenvalues(lambdas, warn_on_zeros=False):
     """
     # is the provided array in double precision (float64) ?
     if isinstance(lambdas, np.ndarray):
-        is_double_precision = (lambdas.dtype == np.dtype('f'))
+        # From https://docs.scipy.org/doc/numpy/reference/arrays.dtypes.html
+        # and https://docs.scipy.org/doc/numpy/user/basics.types.html
+        if lambdas.dtype.kind == 'f':
+            is_double_precision = (lambdas.dtype.itemsize >= 8)
+        else:
+            # default for non-float dtypes
+            is_double_precision = True
     else:
         # default for non-numpy inputs
         is_double_precision = True
@@ -1062,9 +1068,8 @@ def check_psd_eigenvalues(lambdas, warn_on_zeros=False):
 
     # the various thresholds used for validation
     # we may wish to change the value according to precision.
-    # currently we do it only for the 'absolute' threshold.
     significant_imag_ratio = 1e-5
-    significant_neg_ratio = 1e-5  # if is_double_precision else 5e-3
+    significant_neg_ratio = 1e-5 if is_double_precision else 5e-3
     significant_neg_value = 1e-10 if is_double_precision else 1e-6
     small_pos_ratio = 1e-12
 