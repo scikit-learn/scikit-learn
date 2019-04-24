f23d5ab00d0e7f894e8a0c50e37b9a93c4df97e5
Roddy <rlm72@cam.ac.uk>
2019-04-24 17:10:54 +0100

    Reformatted assertion in test

--- sklearn/feature_selection/tests/test_variance_threshold.py
+++ sklearn/feature_selection/tests/test_variance_threshold.py
@@ -2,7 +2,7 @@ import numpy as np
 import pytest
 
 from sklearn.utils.testing import (assert_array_equal, assert_equal,
-                                   assert_not_equal, assert_raises)
+                                   assert_raises)
 
 from scipy.sparse import bsr_matrix, csc_matrix, csr_matrix
 
@@ -38,7 +38,7 @@ def test_zero_variance_floating_point_error():
     # See #13691
 
     data = [[-0.13725701]] * 10
-    assert_not_equal(np.var(data), 0.)
+    assert np.var(data) != 0
     for X in [data, csr_matrix(data), csc_matrix(data), bsr_matrix(data)]:
         msg = "No feature in X meets the variance threshold 0.00000"
         with pytest.raises(ValueError, match=msg):
