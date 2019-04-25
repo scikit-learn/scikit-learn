20c79297a9ae175ed7fffd3c517c506d9391be6d
Samuel O. Ronsin <samuel.ronsin@dataiku.com>
2019-04-26 01:32:54 +0200

    Cleanup

--- sklearn/ensemble/forest.py
+++ sklearn/ensemble/forest.py
@@ -976,8 +976,9 @@ class RandomForestClassifier(ForestClassifier):
                            max_features='auto', max_leaf_nodes=None,
                            min_impurity_decrease=0.0, min_impurity_split=None,
                            min_samples_leaf=1, min_samples_split=2,
-                           min_weight_fraction_leaf=0.0, n_estimators=100, n_jobs=None,
-                           oob_score=False, random_state=0, verbose=0, warm_start=False)
+                           min_weight_fraction_leaf=0.0, n_estimators=100,
+                           n_jobs=None, oob_score=False, random_state=0,
+                           verbose=0, warm_start=False)
     >>> print(clf.feature_importances_)
     [0.14205973 0.76664038 0.0282433  0.06305659]
     >>> print(clf.predict([[0, 0, 0, 0]]))
