6438f09d3d66fc8bff2076d921ea1343d2d1981a
Andrew Nystrom <awnystrom@gmail.com>
2019-04-30 22:24:43 -0700

    Max of 79 columns

--- sklearn/preprocessing/data.py
+++ sklearn/preprocessing/data.py
@@ -1479,16 +1479,17 @@ class PolynomialFeatures(BaseEstimator, TransformerMixin):
             The data to transform, row by row.
             
             Prefer CSR over CSC for sparse input (for speed), but CSC is
-            required if the degree is 4 or higher. If the degree is less than 4
-            and the input format is CSC, it will be converted to CSR, have its
-            polynomial features generated, then converted back to CSC.
+            required if the degree is 4 or higher. If the degree is less than
+            4 and the input format is CSC, it will be converted to CSR, have
+            its polynomial features generated, then converted back to CSC.
 
-            If the degree is 2 or 3, the method described in "Leveraging Sparsity
-            to Speed Up Polynomial Feature Expansions of CSR Matrices Using
-            K-Simplex Numbers" by Andrew Nystrom and John Hughes is used, which
-            is much faster than the method used on CSC input. For this reason, a
-            CSC input will be converted to CSR, and the output will be converted
-            back to CSC prior to being returned, hence the preference of CSR.
+            If the degree is 2 or 3, the method described in "Leveraging
+            Sparsity to Speed Up Polynomial Feature Expansions of CSR Matrices
+            Using K-Simplex Numbers" by Andrew Nystrom and John Hughes is
+            used, which is much faster than the method used on CSC input. For
+            this reason, a CSC input will be converted to CSR, and the output
+            will be converted back to CSC prior to being returned, hence the
+            preference of CSR.
 
         Returns
         -------
