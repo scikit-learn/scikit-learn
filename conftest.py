b8438d6c88e2d69ba9d23a570a5251454b0c5bcd
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2019-05-03 14:30:33 +0200

    DOC: add fixture documentation

--- conftest.py
+++ conftest.py
@@ -80,6 +80,11 @@ def plt():
     This fixture check if we can import matplotlib. If not, the tests will be
     skipped. Otherwise, we setup matplotlib backend and close the figures
     after running the functions.
+
+    Returns
+    -------
+    plt : module
+        Return the imported ``matplotlib.pyplot`` module.
     """
     plt = pytest.importorskip('matplotlib.pyplot')
     import matplotlib
