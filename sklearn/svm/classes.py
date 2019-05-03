6273d31ae3ee063ef5a92cc646976ee8c0b7ca17
Lukas Stappen <24368716+lstappen@users.noreply.github.com>
2019-05-03 13:51:34 +0200

    Change 'scale' to 'auto' in example
    Example returns TypeError:
    "sklearn/svm/libsvm.pyx in sklearn.svm.libsvm.fit()
    TypeError: a float is required"
    Change to auto because 'scale' deprecated:
    "The current default of gamma, 'auto', will change to 'scale' in version 0.22. 'auto_deprecated', a deprecated version of 'auto' is used as a default indicating that no explicit value of gamma was passed."

--- sklearn/svm/classes.py
+++ sklearn/svm/classes.py
@@ -885,7 +885,7 @@ class SVR(BaseLibSVM, RegressorMixin):
     >>> rng = np.random.RandomState(0)
     >>> y = rng.randn(n_samples)
     >>> X = rng.randn(n_samples, n_features)
-    >>> clf = SVR(gamma='scale', C=1.0, epsilon=0.2)
+    >>> clf = SVR(gamma='auto', C=1.0, epsilon=0.2)
     >>> clf.fit(X, y) #doctest: +NORMALIZE_WHITESPACE
     SVR(C=1.0, cache_size=200, coef0=0.0, degree=3, epsilon=0.2, gamma='scale',
         kernel='rbf', max_iter=-1, shrinking=True, tol=0.001, verbose=False)
