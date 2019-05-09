6a9721d9c28d60468c35ae362afda8c99e076c56
c56pony <nimbus1after2a1sun7shower@gmail.com>
2019-05-09 09:20:19 +0900

    update example again

--- examples/linear_model/plot_bayesian_ridge_curvefit.py
+++ examples/linear_model/plot_bayesian_ridge_curvefit.py
@@ -10,7 +10,7 @@ See :ref:`bayesian_ridge_regression` for more information on the regressor.
 In general, when fitting a curve with a polynomial by Bayesian ridge
 regression, the selection of initial values of
 the regularization parameters (alpha, lambda) may be important.
-This is because the regularization parameter is determined by an iterative
+This is because the regularization parameters are determined by an iterative
 procedure that depends on initial values.
 
 In this example, the sinusoid is approximated by a polynomial using different
@@ -51,16 +51,16 @@ X_train = np.vander(x_train, n_order+1, increasing=True)
 X_test = np.vander(x_test, n_order+1, increasing=True)
 
 # #############################################################################
-# Bayesian ridge regression with different initial value pairs
-inits = [(1. / np.var(y_train), 1.), (1., 1.e-3)]
-regs = [BayesianRidge(tol=1e-6, fit_intercept=False, compute_score=True),
-        BayesianRidge(tol=1e-6, fit_intercept=False, compute_score=True,
-                      alpha_init=inits[1][0], lambda_init=inits[1][1])]
-
-# #############################################################################
 # Plot the true and predicted curves with log marginal likelihood (L)
+reg = BayesianRidge(tol=1e-6, fit_intercept=False, compute_score=True)
 fig, axes = plt.subplots(1, 2, figsize=(8, 4))
-for i, (reg, ax) in enumerate(zip(regs, axes)):
+for i, ax in enumerate(axes):
+    # Bayesian ridge regression with different initial value pairs
+    if i == 0:
+        init = [1 / np.var(y_train), 1.]  # Default values
+    elif i == 1:
+        init = [1., 1e-3]
+        reg.set_params(alpha_init=init[0], lambda_init=init[1])
     reg.fit(X_train, y_train)
     ymean, ystd = reg.predict(X_test, return_std=True)
 
@@ -72,9 +72,9 @@ for i, (reg, ax) in enumerate(zip(regs, axes)):
     ax.set_ylim(-1.3, 1.3)
     ax.legend()
     title = "$\\alpha$_init$={:.2f},\\ \\lambda$_init$={}$".format(
-            inits[i][0], inits[i][1])
+            init[0], init[1])
     if i == 0:
-        title = title+" (Default)"
+        title += " (Default)"
     ax.set_title(title, fontsize=12)
     text = "$\\alpha={:.1f}$\n$\\lambda={:.3f}$\n$L={:.1f}$".format(
            reg.alpha_, reg.lambda_, reg.scores_[-1])
