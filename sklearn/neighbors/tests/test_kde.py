5539ff8a80bd54abf5bd0044d2414d3326d7930e
Olivier Grisel <olivier.grisel@ensta.org>
2019-05-14 09:01:20 +0200

    Factorize KDE pickling tests via pytest.mark.parametrize

--- sklearn/neighbors/tests/test_kde.py
+++ sklearn/neighbors/tests/test_kde.py
@@ -205,38 +205,20 @@ def test_kde_sample_weights():
                     assert_allclose(scores_scaled_weight, scores_weight)
 
 
-def test_pickling(tmpdir):
+@pytest.mark.parametrize('sample_weight', [None, [0.1, 0.2, 0.3]])
+def test_pickling(tmpdir, sample_weight):
     # Make sure that predictions are the same before and after pickling. Used
     # to be a bug because sample_weights wasn't pickled and the resulting tree
     # would miss some info.
 
     kde = KernelDensity()
     data = np.reshape([1., 2., 3.], (-1, 1))
-    kde.fit(data)
+    kde.fit(data, sample_weight=sample_weight)
 
     X = np.reshape([1.1, 2.1], (-1, 1))
     scores = kde.score_samples(X)
 
     file_path = str(tmpdir.join('dump.pkl'))
-    _joblib.dump(kde, file_path)
-    kde = _joblib.load(file_path)
-    scores_pickled = kde.score_samples(X)
-
-    assert_allclose(scores, scores_pickled)
-
-
-def test_pickling_with_sample_weights(tmpdir):
-    # Test to see if KernelDensity object is pickled if supplied with
-    # sample weights.
-
-    kde = KernelDensity()
-    data = np.reshape([1., 2., 3.], (-1, 1))
-    kde.fit(data, sample_weight=[0.1, 0.2, 0.3])
-
-    X = np.reshape([1.1, 2.1], (-1, 1))
-    scores = kde.score_samples(X)
-
-    file_path = str(tmpdir.join('dump_with_sample_weights.pkl'))
     _joblib.dump(kde, file_path)
     kde = _joblib.load(file_path)
     scores_pickled = kde.score_samples(X)
