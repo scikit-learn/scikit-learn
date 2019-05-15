95f764b0739265d7e35174afff12cb78e002935f
trevorstephens <trev.stephens@gmail.com>
2019-05-15 20:10:51 +1000

    batches, though a short bath is better than no bath

--- sklearn/utils/estimator_checks.py
+++ sklearn/utils/estimator_checks.py
@@ -829,7 +829,7 @@ def _apply_on_subsets(func, X):
 @ignore_warnings(category=(DeprecationWarning, FutureWarning))
 def check_methods_subset_invariance(name, estimator_orig):
     # check that method gives invariant results if applied
-    # on mini bathes or the whole set
+    # on mini batches or the whole set
     tags = _safe_tags(estimator_orig)
     rnd = np.random.RandomState(0)
     X = 3 * rnd.uniform(size=(20, 3))
