cf1b01695811258712eb9146d7fc71fefbdee367
H4dr1en <h4dr1en@pm.me>
2019-05-24 13:53:29 +0200

    Removed more trailing whitespace

--- sklearn/model_selection/_validation.py
+++ sklearn/model_selection/_validation.py
@@ -1209,7 +1209,7 @@ def learning_curve(estimator, X, y, groups=None,
         Scores on training sets.
 
     test_scores : array, shape (n_ticks, n_cv_folds)
-        Scores on test set.    
+        Scores on test set.
 
     fit_times : array, shape (n_ticks, n_cv_folds)
         Times spent for fitting in seconds.
