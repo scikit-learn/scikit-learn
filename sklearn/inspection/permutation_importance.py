f0beac610379f668e7acf2facaed4bd924bd50c2
Thomas Fan <thomasjpfan@gmail.com>
2019-05-29 12:09:24 -0400

    ENH Parallel

--- sklearn/inspection/permutation_importance.py
+++ sklearn/inspection/permutation_importance.py
@@ -1,14 +1,35 @@
 """Permutation importance for estimators"""
 import numpy as np
 import scipy.sparse as sp
+from joblib import Parallel, delayed
 
 from ..utils import check_random_state
 from ..utils.validation import check_array
 from ..metrics import check_scoring
 
 
+def _calculate_permutation_scores(estimator, X, y, col_idx, random_state,
+                                  n_rounds, scorer):
+    """Calcuate permutation scores for a column"""
+    if hasattr(X, 'iloc'):  # pandas dataframe
+        X_iloc = X.iloc
+    else:
+        X_iloc = X
+
+    scores = np.zeros(n_rounds)
+    temp = X_iloc[:, col_idx].copy()
+
+    for n_round in range(n_rounds):
+        random_state.shuffle(temp)
+        X_iloc[:, col_idx] = temp
+
+        feature_score = scorer(estimator, X, y)
+        scores[n_round] = feature_score
+    return scores
+
+
 def permutation_importance(estimator, X, y, scoring=None, n_rounds=1,
-                           random_state=None):
+                           n_jobs=None, random_state=None):
     """Permutation importance for feature evaluation. [BRE]_
 
     The permutation importance of a feature is calculated as follows. First,
@@ -28,7 +49,7 @@ def permutation_importance(estimator, X, y, scoring=None, n_rounds=1,
         ``scorer``.
 
     X : array-like or DataFrame, shape = (n_samples, n_features)
-        Data on which permutaiton importance will be computed.
+        Data on which permutation importance will be computed.
 
     y : array-like, shape = (n_samples, ...)
         Targets for supervised learning.
@@ -40,6 +61,12 @@ def permutation_importance(estimator, X, y, scoring=None, n_rounds=1,
 
     n_rounds : int, optional (default=1)
         Number of times to permute a feature.
+
+    n_jobs : int or None, optional (default=None)
+        The number of jobs to use for the computation.
+        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
+        ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
+        for more details.
 
     random_state : int, RandomState instance or None, optional, default None
         Pseudo-random number generator to control the permutations.
@@ -56,32 +83,18 @@ def permutation_importance(estimator, X, y, scoring=None, n_rounds=1,
         2001.https://doi.org/10.1023/A:1010933404324
     """
     if not (hasattr(X, '__array__') or sp.issparse(X)):
-        X = check_array(X, force_all_finite='allow-nan', dtype=np.object)
-
-    n_features = X.shape[1]
+        X = check_array(X, force_all_finite='allow-nan',
+                        dtype=np.object, copy=True)
+    else:
+        X = X.copy()
 
     random_state = check_random_state(random_state)
     scorer = check_scoring(estimator, scoring=scoring)
-    scores = np.empty(shape=(n_features, n_rounds), dtype=np.float)
-
-    if hasattr(X, 'iloc'):  # pandas dataframe
-        # Need to copy in case where the dataframe is a view
-        X = X.copy()
-        X_iloc = X.iloc
-    else:
-        X_iloc = X
 
     baseline_score = scorer(estimator, X, y)
-    for col_idx in range(n_features):
-        original_feature = X_iloc[:, col_idx].copy()
+    scores_returned = Parallel(n_jobs=n_jobs)(
+        delayed(_calculate_permutation_scores)(estimator, X, y, col_idx,
+                                               random_state, n_rounds, scorer)
+        for col_idx in range(X.shape[1]))
 
-        for n_round in range(n_rounds):
-            X_perm = random_state.permutation(original_feature)
-            X_iloc[:, col_idx] = X_perm
-
-            feature_score = scorer(estimator, X, y)
-            scores[col_idx, n_round] = baseline_score - feature_score
-
-        X_iloc[:, col_idx] = original_feature
-
-    return scores
+    return baseline_score - np.array(scores_returned)
