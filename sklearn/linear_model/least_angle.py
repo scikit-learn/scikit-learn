1ef4d9edde345e4a8e09846b0d9b29c5fb62f2cf
Roman Yurchak <rth.yurchak@pm.me>
2019-05-30 12:39:04 +0200

    Add sklearn/linear_model/least_angle.py

--- sklearn/linear_model/least_angle.py
+++ sklearn/linear_model/least_angle.py
@@ -413,12 +413,6 @@ def _lars_path_solver(X, y, Xy=None, Gram=None, n_samples=None, max_iter=500,
         Gram = None
         if X is None:
             raise ValueError('X and Gram cannot both be unspecified.')
-        if copy_X:
-            # force copy. setting the array to be fortran-ordered
-            # speeds up the calculation of the (partial) Gram matrix
-            # and allows to easily swap columns
-            X = X.copy('F')
-
     elif isinstance(Gram, str) and Gram == 'auto' or Gram is True:
         if Gram is True or X.shape[0] > X.shape[1]:
             Gram = np.dot(X.T, X)
@@ -426,6 +420,12 @@ def _lars_path_solver(X, y, Xy=None, Gram=None, n_samples=None, max_iter=500,
             Gram = None
     elif copy_Gram:
         Gram = Gram.copy()
+
+    if X is not None and copy_X:
+        # force copy. setting the array to be fortran-ordered
+        # speeds up the calculation of the (partial) Gram matrix
+        # and allows to easily swap columns
+        X = X.copy('F')
 
     if Gram is None:
         n_features = X.shape[1]
