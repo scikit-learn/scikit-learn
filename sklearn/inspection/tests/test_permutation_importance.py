aa6c79d0434e6404424989367d71ae494dce37aa
Thomas Fan <thomasjpfan@gmail.com>
2019-05-30 16:23:29 -0400

    BUG Fix

--- sklearn/inspection/tests/test_permutation_importance.py
+++ sklearn/inspection/tests/test_permutation_importance.py
@@ -15,48 +15,48 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.utils.testing import assert_array_almost_equal
 
 
-# @pytest.mark.parametrize("convert_to_df", [True, False])
-# @pytest.mark.parametrize("load_dataset,clf", [
-#     (load_boston, RandomForestRegressor),
-#     (load_iris, RandomForestClassifier)
-# ])
-# @pytest.mark.parametrize("n_rounds", [3, 5])
-# def test_permutation_importance_correlated_feature_regression(
-#         convert_to_df, load_dataset, RandomForest, n_rounds):
-#     # Make sure that feature highly correlated to the target have a higher
-#     # importance
-#     rng = np.random.RandomState(42)
+@pytest.mark.parametrize("convert_to_df", [True, False])
+@pytest.mark.parametrize("load_dataset,clf", [
+    (load_boston, RandomForestRegressor),
+    (load_iris, RandomForestClassifier)
+])
+@pytest.mark.parametrize("n_rounds", [3, 5])
+def test_permutation_importance_correlated_feature_regression(
+        convert_to_df, load_dataset, RandomForest, n_rounds):
+    # Make sure that feature highly correlated to the target have a higher
+    # importance
+    rng = np.random.RandomState(42)
 
-#     dataset = load_dataset()
-#     X, y = dataset.data, dataset.target
-#     y_with_little_noise = (
-#         y + rng.normal(scale=0.001, size=y.shape[0])).reshape(-1, 1)
+    dataset = load_dataset()
+    X, y = dataset.data, dataset.target
+    y_with_little_noise = (
+        y + rng.normal(scale=0.001, size=y.shape[0])).reshape(-1, 1)
 
-#     # Adds feature correlated with y as the last column
-#     if convert_to_df:
-#         pd = pytest.importorskip("pandas")
-#         X = pd.DataFrame(X, columns=dataset.feature_names)
-#         X['correlated_feature'] = y_with_little_noise
-#     else:
-#         X = np.hstack([X, y_with_little_noise])
+    # Adds feature correlated with y as the last column
+    if convert_to_df:
+        pd = pytest.importorskip("pandas")
+        X = pd.DataFrame(X, columns=dataset.feature_names)
+        X['correlated_feature'] = y_with_little_noise
+    else:
+        X = np.hstack([X, y_with_little_noise])
 
-#     clf = RandomForest(n_estimators=10, random_state=42)
-#     clf.fit(X, y)
+    clf = RandomForest(n_estimators=10, random_state=42)
+    clf.fit(X, y)
 
-#     X_before = X.copy()
-#     permute_imp = permutation_importance(clf, X, y, n_rounds=n_rounds,
-#                                          random_state=rng)
+    X_before = X.copy()
+    permute_imp = permutation_importance(clf, X, y, n_rounds=n_rounds,
+                                         random_state=rng)
 
-#     assert permute_imp.shape == (X.shape[1], n_rounds)
+    assert permute_imp.shape == (X.shape[1], n_rounds)
 
-#     # permutation_importance does not change X
-#     assert_array_almost_equal(X_before, X)
+    # permutation_importance does not change X
+    assert_array_almost_equal(X_before, X)
 
-#     permute_score_means = np.mean(permute_imp, axis=-1)
+    permute_score_means = np.mean(permute_imp, axis=-1)
 
-#     # the correlated feature with y was added as the last column and should
-#     # have the highest importance
-#     assert np.all(permute_score_means[-1] > permute_score_means[:-1])
+    # the correlated feature with y was added as the last column and should
+    # have the highest importance
+    assert np.all(permute_score_means[-1] > permute_score_means[:-1])
 
 
 # @pytest.mark.parametrize("convert_to_df", [True, False])
