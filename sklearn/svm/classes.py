5ed391bd9b3136c22b1ea68b1c6faf406f7c1507
veerlosar <45338519+veerlosar@users.noreply.github.com>
2019-05-30 17:05:33 +0200

    DOC add doc example to OneClassSVM

--- sklearn/svm/classes.py
+++ sklearn/svm/classes.py
@@ -1140,6 +1140,15 @@ class OneClassSVM(BaseLibSVM, OutlierMixin):
         The offset is the opposite of `intercept_` and is provided for
         consistency with other outlier detection algorithms.
 
+    Examples
+    --------
+    >>> from sklearn.svm import OneClassSVM
+    >>> X = [[0], [0.44], [0.45], [0.46], [1]]
+    >>> clf = OneClassSVM(gamma='auto').fit(X)
+    >>> clf.predict(X)
+    array([-1,  1,  1,  1, -1])
+    >>> clf.score_samples(X)  # doctest: +ELLIPSIS
+    array([1.7798..., 2.0547..., 2.0556..., 2.0561..., 1.7332...])
     """
 
     _impl = 'one_class'
