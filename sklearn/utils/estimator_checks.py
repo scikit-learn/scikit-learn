19c73132f4f6274ac39617cacaf40fa8b43c583e
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2019-06-13 11:25:16 +0200

    fix

--- sklearn/utils/estimator_checks.py
+++ sklearn/utils/estimator_checks.py
@@ -663,7 +663,7 @@ def check_dtype_object(name, estimator_orig):
         if "Unknown label type" not in str(e):
             raise
 
-    if 'string' not in _safe_tags(estimator, 'X_types'):
+    if 'string' not in tags['X_types']:
         X[0, 0] = {'foo': 'bar'}
         msg = "argument must be a string.* number"
         assert_raises_regex(TypeError, msg, estimator.fit, X, y)
