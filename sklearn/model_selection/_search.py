0891c8e24d83558cc0bc36bf64db2e6b590945bb
kpolonijfelinto <kpolonijfelinto@gmail.com>
2019-06-14 22:23:14 -0300

    fixed long line

--- sklearn/model_selection/_search.py
+++ sklearn/model_selection/_search.py
@@ -1331,7 +1331,7 @@ class RandomizedSearchCV(BaseSearchCV):
                    iid='deprecated', imbalanced_model=None, n_iter=20,
                    n_jobs=None,
                    param_distributions={'C': <scipy.stats._distn_infrastructure
-                                           .rv_frozen object at 0x7f6467a4feb8>,
+                                         .rv_frozen object at 0x7f6467a4feb8>,
                                         'degree': <scipy.stats.
                                          _distn_infrastructure.
                                          rv_frozen object at 0x7f64673b75f8>,
