d3033a4ab00c25211e05987adb5acfb7e4706b9f
stefan-matcovici <stefan.matcovici@yahoo.com>
2019-06-19 00:27:19 +0300

    some changes to brier_score_loss

--- sklearn/metrics/classification.py
+++ sklearn/metrics/classification.py
@@ -2369,7 +2369,7 @@ def brier_score_loss(y_true, y_prob, sample_weight=None, pos_label=None):
         raise ValueError("y_prob contains values less than 0.")
 
     # if pos_label=None, when y_true is in {-1, 1} or {0, 1},
-    # pos_labe is set to 1 (consistent with precision_recall_curve/roc_curve),
+    # pos_label is set to 1 (consistent with precision_recall_curve/roc_curve),
     # otherwise pos_label is set to the greater label
     # (different from precision_recall_curve/roc_curve,
     # the purpose is to keep backward compatibility).
@@ -2378,6 +2378,6 @@ def brier_score_loss(y_true, y_prob, sample_weight=None, pos_label=None):
                 np.array_equal(labels, [-1])):
             pos_label = 1
         else:
-            pos_label = y_true.max()
+            pos_label = labels.max()
     y_true = np.array(y_true == pos_label, int)
     return np.average((y_true - y_prob) ** 2, weights=sample_weight)
