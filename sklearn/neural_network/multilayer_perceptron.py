d53194e0c5e06efd83e6f406988cde30d59d38ee
Marian Meyer <marian.meyer@bonding.de>
2019-06-20 22:03:10 +0200

    Do not import unused function safe_indexing

--- sklearn/neural_network/multilayer_perceptron.py
+++ sklearn/neural_network/multilayer_perceptron.py
@@ -21,7 +21,6 @@ from ..preprocessing import LabelBinarizer
 from ..utils import gen_batches, check_random_state
 from ..utils import shuffle
 from ..utils import check_array, check_X_y, column_or_1d
-from ..utils import safe_indexing
 from ..exceptions import ConvergenceWarning
 from ..utils.extmath import safe_sparse_dot
 from ..utils.validation import check_is_fitted
