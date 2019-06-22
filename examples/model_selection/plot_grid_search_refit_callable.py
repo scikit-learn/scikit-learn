8e4c068da8ad5e086ac34fe99c289fbf89bb10d3
Ruth Waithera Wachira <rwachira@ke.ibm.com>
2019-06-22 11:10:48 +0300

    resolve convergence warning by reducing the default penalty

--- examples/model_selection/plot_grid_search_refit_callable.py
+++ examples/model_selection/plot_grid_search_refit_callable.py
@@ -77,7 +77,7 @@ def best_low_complexity(cv_results):
 
 pipe = Pipeline([
         ('reduce_dim', PCA(random_state=42)),
-        ('classify', LinearSVC(random_state=42)),
+        ('classify', LinearSVC(random_state=42, C=0.01)),
 ])
 
 param_grid = {
