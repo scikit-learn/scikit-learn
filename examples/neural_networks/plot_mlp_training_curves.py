45651760a0f69d7b9734392aeea1b4f9fc27d3da
Martin Oywa <martinoywa2@gmail.com>
2019-06-24 21:08:52 +0300

    context manager fix

--- examples/neural_networks/plot_mlp_training_curves.py
+++ examples/neural_networks/plot_mlp_training_curves.py
@@ -58,27 +58,29 @@ def plot_on_dataset(X, y, ax, name):
     print("\nlearning on dataset %s" % name)
     ax.set_title(name)
 
-    # some parameter combinations will not converge as can be seen on the
-    # plots so they are ignored here
-    with warnings.catch_warnings():
-        warnings.filterwarnings("ignore", category=ConvergenceWarning,
-                                module="sklearn")
-        X = MinMaxScaler().fit_transform(X)
-        mlps = []
-        if name == "digits":
-            # digits is larger but converges fairly quickly
-            max_iter = 15
-        else:
-            max_iter = 400
+    X = MinMaxScaler().fit_transform(X)
+    mlps = []
+    if name == "digits":
+        # digits is larger but converges fairly quickly
+        max_iter = 15
+    else:
+        max_iter = 400
 
-        for label, param in zip(labels, params):
-            print("training: %s" % label)
-            mlp = MLPClassifier(verbose=0, random_state=0,
-                                max_iter=max_iter, **param)
+    for label, param in zip(labels, params):
+        print("training: %s" % label)
+        mlp = MLPClassifier(verbose=0, random_state=0,
+                            max_iter=max_iter, **param)
+        
+        # some parameter combinations will not converge as can be seen on the
+        # plots so they are ignored here
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", category=ConvergenceWarning,
+                                    module="sklearn")
             mlp.fit(X, y)
-            mlps.append(mlp)
-            print("Training set score: %f" % mlp.score(X, y))
-            print("Training set loss: %f" % mlp.loss_)
+
+        mlps.append(mlp)
+        print("Training set score: %f" % mlp.score(X, y))
+        print("Training set loss: %f" % mlp.loss_)
         for mlp, label, args in zip(mlps, labels, plot_args):
             ax.plot(mlp.loss_curve_, label=label, **args)
 
