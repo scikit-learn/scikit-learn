155e6016a8a40aa93bbd7920c77cb4d2638f40de
Roman Yurchak <rth.yurchak@pm.me>
2019-06-24 13:53:29 +0200

    Check that Python implementation is PyPy

--- build_tools/circle/build_test_pypy.sh
+++ build_tools/circle/build_test_pypy.sh
@@ -32,6 +32,13 @@ export OMP_NUM_THREADS="1"
 
 pip install -e .
 
+# Check that Python implementation is PyPy
+python - << EOL
+import platform
+from sklearn.utils import IS_PYPY
+assert IS_PYPY is True, "platform={}!=PyPy".format(platform.python_implementation())
+EOL
+
 python -m pytest sklearn/
 python -m pytest doc/sphinxext/
 python -m pytest $(find doc -name '*.rst' | sort)
