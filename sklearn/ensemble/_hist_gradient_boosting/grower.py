9b82c9fa42e409fe123c715978a2e47fc66381b8
Joel Nothman <joel.nothman@gmail.com>
2019-06-26 09:54:03 +1000

    FIX hist gradient boosting change relied on refactoring

--- sklearn/ensemble/_hist_gradient_boosting/grower.py
+++ sklearn/ensemble/_hist_gradient_boosting/grower.py
@@ -15,7 +15,6 @@ from .splitting import Splitter
 from .histogram import HistogramBuilder
 from .predictor import TreePredictor, PREDICTOR_RECORD_DTYPE
 from .utils import sum_parallel
-from .types import PREDICTOR_RECORD_DTYPE
 from .types import Y_DTYPE
 
 
