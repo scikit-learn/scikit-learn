e674fb2ebe8397783a3a11142fae80d0aba7f417
Thomas Fan <thomasjpfan@gmail.com>
2019-06-25 12:04:50 -0400

    TST Increases tolerance to match float32 resolution

--- sklearn/linear_model/tests/test_ridge.py
+++ sklearn/linear_model/tests/test_ridge.py
@@ -1100,12 +1100,12 @@ def test_dtype_match(solver):
     y_32 = y_64.astype(np.float32)
 
     # Check type consistency 32bits
-    ridge_32 = Ridge(alpha=alpha, solver=solver, max_iter=500, tol=1e-10,)
+    ridge_32 = Ridge(alpha=alpha, solver=solver, max_iter=500, tol=1e-6)
     ridge_32.fit(X_32, y_32)
     coef_32 = ridge_32.coef_
 
     # Check type consistency 64 bits
-    ridge_64 = Ridge(alpha=alpha, solver=solver, max_iter=500, tol=1e-10,)
+    ridge_64 = Ridge(alpha=alpha, solver=solver, max_iter=500, tol=1e-6)
     ridge_64.fit(X_64, y_64)
     coef_64 = ridge_64.coef_
 
