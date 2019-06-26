9cb9ef8ed47661fd540a3afaeceb9b4581b02d46
stefan-matcovici <stefan.matcovici@yahoo.com>
2019-06-26 20:46:50 +0300

    remove unnecessary pass

--- sklearn/metrics/scorer.py
+++ sklearn/metrics/scorer.py
@@ -55,7 +55,6 @@ class _BaseScorer(metaclass=ABCMeta):
 
     @abstractmethod
     def __call__(self, estimator, X, y, sample_weight=None):
-        pass
         if self._deprecation_msg is not None:
             warnings.warn(self._deprecation_msg,
                           category=DeprecationWarning,
