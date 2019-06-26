425f6be5cfd407ed57a022bea063f8dc36025877
Joel Nothman <joel.nothman@gmail.com>
2019-06-26 11:03:49 +1000

    TST remove test for functionality not included here

--- sklearn/ensemble/_hist_gradient_boosting/tests/test_gradient_boosting.py
+++ sklearn/ensemble/_hist_gradient_boosting/tests/test_gradient_boosting.py
@@ -147,32 +147,6 @@ def test_should_stop(scores, n_iter_no_change, tol, stopping):
     assert gbdt._should_stop(scores) == stopping
 
 
-def test_binning_train_validation_are_separated():
-    # Make sure training and validation data are binned separately.
-    # See issue 13926
-
-    rng = np.random.RandomState(0)
-    validation_fraction = .2
-    gb = HistGradientBoostingClassifier(
-        n_iter_no_change=5,
-        validation_fraction=validation_fraction,
-        random_state=rng
-    )
-    gb.fit(X_classification, y_classification)
-    mapper_training_data = gb.bin_mapper_
-
-    # Note that since the data is small there is no subsampling and the
-    # random_state doesn't matter
-    mapper_whole_data = _BinMapper(random_state=0)
-    mapper_whole_data.fit(X_classification)
-
-    n_samples = X_classification.shape[0]
-    assert np.all(mapper_training_data.actual_n_bins_ ==
-                  int((1 - validation_fraction) * n_samples))
-    assert np.all(mapper_training_data.actual_n_bins_ !=
-                  mapper_whole_data.actual_n_bins_)
-
-
 @pytest.mark.parametrize('data', [
     make_classification(random_state=0, n_classes=2),
     make_classification(random_state=0, n_classes=3, n_informative=3)
