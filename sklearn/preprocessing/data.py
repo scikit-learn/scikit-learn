c21e04e37ed19ce26eec1009333ee9db2d4fb2d5
xavier dupré <xavier.dupre@gmail.com>
2019-06-26 17:20:18 +0200

    More comments.

--- sklearn/preprocessing/data.py
+++ sklearn/preprocessing/data.py
@@ -1548,6 +1548,15 @@ class PolynomialFeatures(BaseEstimator, TransformerMixin):
                 # What follows is a faster implementation of:
                 # for i, comb in enumerate(combinations):
                 #     XP[:, i] = X[:, comb].prod(1)
+                # This new implementation uses two optimisations.
+                # First one is broadcasting,
+                # multiply ([X1, ..., Xn], X1) -> [X1 X1, ..., Xn X1]
+                # multiply ([X2, ..., Xn], X2) -> [X2 X2, ..., Xn X2]
+                # ...
+                # multiply ([X[:, start:end], X[:, start]) -> ...
+                # Second optimisation happens for degrees >= 3.
+                # Xi^3 is computed reusing previous computation:
+                # Xi^3 = Xi^2 * Xi.
 
                 if self.include_bias:
                     XP[:, 0] = 1
@@ -1563,7 +1572,7 @@ class PolynomialFeatures(BaseEstimator, TransformerMixin):
                 index.append(current_col)
 
                 # d >= 1
-                for d in range(1, self.degree):
+                for _ in range(1, self.degree):
                     new_index = []
                     end = index[-1]
                     for feature_idx in range(n_features):
@@ -1575,6 +1584,8 @@ class PolynomialFeatures(BaseEstimator, TransformerMixin):
                         next_col = current_col + end - start
                         if next_col <= current_col:
                             break
+                        # XP[:, start:end] are terms of degree d - 1
+                        # that exclude feature #feature_idx.
                         np.multiply(XP[:, start:end],
                                     X[:, feature_idx:feature_idx + 1],
                                     out=XP[:, current_col:next_col],
