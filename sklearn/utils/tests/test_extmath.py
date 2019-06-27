6ef8dadd4cb19c4fd39ae4e98904846528a9a28e
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2019-06-27 19:31:19 +0200

    TST refactor some test

--- sklearn/utils/tests/test_extmath.py
+++ sklearn/utils/tests/test_extmath.py
@@ -82,7 +82,10 @@ def test_random_weights():
     assert_array_almost_equal(score.ravel(), w[:, :5].sum(1))
 
 
-def check_randomized_svd_low_rank(dtype):
+@pytest.mark.parametrize('dtype', [np.int32, np.int64, np.float32, np.float64])
+@pytest.mark.parametrize('normalizer', ['auto', 'LU', 'QR'])
+@pytest.mark.parametrize('preconditioner', [None, 'lobpcg'])
+def test_randomized_svd_low_rank(dtype, normalizer, preconditioner):
     # Check that extmath.randomized_svd is consistent with linalg.svd
     n_samples = 100
     n_features = 500
@@ -96,7 +99,7 @@ def check_randomized_svd_low_rank(dtype):
     X = make_low_rank_matrix(n_samples=n_samples, n_features=n_features,
                              effective_rank=rank, tail_strength=0.0,
                              random_state=0).astype(dtype, copy=False)
-    assert_equal(X.shape, (n_samples, n_features))
+    assert X.shape == (n_samples, n_features)
 
     # compute the singular values of X using the slow exact method
     U, s, V = linalg.svd(X, full_matrices=False)
@@ -106,58 +109,50 @@ def check_randomized_svd_low_rank(dtype):
     s = s.astype(dtype, copy=False)
     V = V.astype(dtype, copy=False)
 
-    for normalizer in ['auto', 'LU', 'QR']:  # 'none' would not be stable
-        # compute the singular values of X using the fast approximate method
-        Ua, sa, Va = randomized_svd(
-            X, k, power_iteration_normalizer=normalizer, random_state=0)
+    # compute the singular values of X using the fast approximate method
+    Ua, sa, Va = randomized_svd(
+        X, k, power_iteration_normalizer=normalizer,
+        preconditioner=preconditioner, random_state=0
+    )
 
-        # If the input dtype is float, then the output dtype is float of the
-        # same bit size (f32 is not upcast to f64)
-        # But if the input dtype is int, the output dtype is float64
-        if dtype.kind == 'f':
-            assert Ua.dtype == dtype
-            assert sa.dtype == dtype
-            assert Va.dtype == dtype
-        else:
-            assert Ua.dtype == np.float64
-            assert sa.dtype == np.float64
-            assert Va.dtype == np.float64
+    # If the input dtype is float, then the output dtype is float of the
+    # same bit size (f32 is not upcast to f64)
+    # But if the input dtype is int, the output dtype is float64
+    expected_dtype = dtype if dtype.kind == 'f' else np.float64
+    assert Ua.dtype == expected_dtype
+    assert sa.dtype == expected_dtype
+    assert Va.dtype == expected_dtype
 
-        assert_equal(Ua.shape, (n_samples, k))
-        assert_equal(sa.shape, (k,))
-        assert_equal(Va.shape, (k, n_features))
+    assert Ua.shape == (n_samples, k)
+    assert sa.shape == (k,)
+    assert Va.shape == (k, n_features)
 
-        # ensure that the singular values of both methods are equal up to the
-        # real rank of the matrix
-        assert_almost_equal(s[:k], sa, decimal=decimal)
+    # ensure that the singular values of both methods are equal up to the
+    # real rank of the matrix
+    assert_almost_equal(s[:k], sa, decimal=decimal)
 
-        # check the singular vectors too (while not checking the sign)
-        assert_almost_equal(np.dot(U[:, :k], V[:k, :]), np.dot(Ua, Va),
-                            decimal=decimal)
+    # check the singular vectors too (while not checking the sign)
+    assert_almost_equal(np.dot(U[:, :k], V[:k, :]), np.dot(Ua, Va),
+                        decimal=decimal)
 
-        # check the sparse matrix representation
-        X = sparse.csr_matrix(X)
+    # check the sparse matrix representation
+    X = sparse.csr_matrix(X)
 
-        # compute the singular values of X using the fast approximate method
-        Ua, sa, Va = \
-            randomized_svd(X, k, power_iteration_normalizer=normalizer,
-                           random_state=0)
-        if dtype.kind == 'f':
-            assert Ua.dtype == dtype
-            assert sa.dtype == dtype
-            assert Va.dtype == dtype
-        else:
-            assert Ua.dtype.kind == 'f'
-            assert sa.dtype.kind == 'f'
-            assert Va.dtype.kind == 'f'
+    # compute the singular values of X using the fast approximate method
+    Ua, sa, Va = randomized_svd(
+        X, k, power_iteration_normalizer=normalizer,
+        preconditioner=preconditioner, random_state=0
+    )
+    if dtype.kind == 'f':
+        assert Ua.dtype == dtype
+        assert sa.dtype == dtype
+        assert Va.dtype == dtype
+    else:
+        assert Ua.dtype.kind == 'f'
+        assert sa.dtype.kind == 'f'
+        assert Va.dtype.kind == 'f'
 
-        assert_almost_equal(s[:rank], sa[:rank], decimal=decimal)
-
-
-@pytest.mark.parametrize('dtype',
-                         (np.int32, np.int64, np.float32, np.float64))
-def test_randomized_svd_low_rank_all_dtypes(dtype):
-    check_randomized_svd_low_rank(dtype)
+    assert_almost_equal(s[:rank], sa[:rank], decimal=decimal)
 
 
 @pytest.mark.parametrize('dtype',
