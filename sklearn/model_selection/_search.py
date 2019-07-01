b15dde53d8229bb0b2809cbd705ae219ee3ff9c5
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2019-07-01 11:59:31 +0200

    FIX make deterministic RandomizedSearchCV example

--- sklearn/model_selection/_search.py
+++ sklearn/model_selection/_search.py
@@ -1414,12 +1414,14 @@ class RandomizedSearchCV(BaseSearchCV):
 
     Examples
     --------
+    >>> import numpy as np
     >>> from sklearn.datasets import load_iris
     >>> from sklearn.linear_model import LogisticRegression
     >>> from sklearn.model_selection import RandomizedSearchCV
     >>> from scipy.stats import uniform
     >>> iris = load_iris()
     >>> logistic = LogisticRegression(solver='saga', tol=1e-2, max_iter=200)
+    >>> np.random.seed(seed=233423)  # make sure uniform will be deterministic
     >>> distributions = dict(C=uniform(loc=0, scale=4),
     ...                      penalty=['l2', 'l1'])
     >>> clf = RandomizedSearchCV(logistic, distributions, random_state=0)
