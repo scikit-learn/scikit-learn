6c517d3c1773a0ed04e631bf781215f713c57ab1
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2019-07-02 15:02:16 +0200

    checkout file

--- sklearn/conftest.py
+++ sklearn/conftest.py
@@ -4,9 +4,11 @@ import pytest
 @pytest.fixture(scope='function')
 def pyplot():
     """Setup and teardown fixture for matplotlib.
+
     This fixture checks if we can import matplotlib. If not, the tests will be
     skipped. Otherwise, we setup matplotlib backend and close the figures
     after running the functions.
+
     Returns
     -------
     pyplot : module
