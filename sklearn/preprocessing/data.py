8a3e1f8ca2aca22f021985f814dd1701a5f4111a
Oliver Rausch <oliverrausch99@gmail.com>
2019-07-03 10:44:37 +0200

    Docs error

--- sklearn/preprocessing/data.py
+++ sklearn/preprocessing/data.py
@@ -3028,10 +3028,10 @@ class NaNFilter(BaseEstimator):
     >>> nan = float('nan')
     >>> data = [[1, 2], [1, nan], [nan, nan]]
     >>> y = [0, 0, 0]
-    >>> Xr, yr, _ = NaNFilter().fit_resample(data)
+    >>> Xr, yr, _ = NaNFilter().fit_resample(data, y)
     >>> print(Xr)
     [[1. 2.]]
-    >>> Xr, yr, _ = NaNFilter(count=2).fit_resample(data)
+    >>> Xr, yr, _ = NaNFilter(count=2).fit_resample(data, y)
     >>> print(Xr)
     [[ 1.  2.]
      [ 1. nan]]
