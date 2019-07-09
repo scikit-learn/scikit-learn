a4ffddf275b49069300f57ed34c76fe64e190028
alexshacked <al.shacked@gmail.com>
2019-07-09 12:03:56 +0300

    Update sklearn/svm/src/libsvm/svm.cpp
    Co-Authored-By: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- sklearn/svm/src/libsvm/svm.cpp
+++ sklearn/svm/src/libsvm/svm.cpp
@@ -3139,7 +3139,6 @@ const char *PREFIX(check_parameter)(const PREFIX(problem) *prob, const svm_param
         free(newprob.W);
         if(msg != NULL)
             return msg;
-        }
 	}
 
 	return NULL;
