66f616a2ed194f95003b691ebc7cf3b9ec15abb5
Alex Itkes <38556752+alexitkes@users.noreply.github.com>
2019-07-13 18:41:31 +0300

    Update fastica_.py
    Remove trailing whitespace

--- sklearn/decomposition/fastica_.py
+++ sklearn/decomposition/fastica_.py
@@ -447,7 +447,7 @@ class FastICA(BaseEstimator, TransformerMixin):
     whitening_ : array, shape (n_components, n_features)
         Only set if whiten is 'True'. This is the pre-whitening matrix that projects
         data onto the first `n_components` principal components.
-    
+
     Examples
     --------
     >>> from sklearn.datasets import load_digits
