c2648d94c422cb49a7b86cc480d02312ad0df311
Timothy Li <li.timothy.h@gmail.com>
2019-07-13 17:26:22 -0500

    hyperparameter tuning

--- examples/compose/plot_digits_pipe.py
+++ examples/compose/plot_digits_pipe.py
@@ -44,8 +44,8 @@ y_digits = digits.target
 
 # Parameters of pipelines can be set using ‘__’ separated parameter names:
 param_grid = {
-    'pca__n_components': [5, 20, 30, 40, 50, 64],
-    'logistic__alpha': np.logspace(-4, 4, 5),
+    'pca__n_components': [5, 15, 30, 45, 64],
+    'logistic__alpha': np.logspace(-4, 4, 4),
 }
 search = GridSearchCV(pipe, param_grid, cv=5)
 search.fit(X_digits, y_digits)
