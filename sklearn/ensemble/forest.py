feaf01951926937fdb600ae3b25bbc07c35bc676
rwoolston.admin <rwoolston.admin@LXO-DS-DEV.afcucorp.local>
2019-07-14 09:34:16 -0600

    flake8 required shorter doc strings

--- sklearn/ensemble/forest.py
+++ sklearn/ensemble/forest.py
@@ -916,7 +916,8 @@ class RandomForestClassifier(ForestClassifier):
     Attributes
     ----------
     base_estimator_ : DecisionTreeClassifier
-        The child estimator template used to create the collection of fitted sub-estimators.
+        The child estimator template used to create the collection of fitted
+        sub-estimators.
 
     estimators_ : list of DecisionTreeClassifier
         The collection of fitted sub-estimators.
@@ -1178,7 +1179,8 @@ class RandomForestRegressor(ForestRegressor):
     Attributes
     ----------
     base_estimator_ : DecisionTreeRegressor
-        The child estimator template used to create the collection of fitted sub-estimators.
+        The child estimator template used to create the collection of fitted
+        sub-estimators.
 
     estimators_ : list of DecisionTreeRegressor
         The collection of fitted sub-estimators.
@@ -1451,7 +1453,8 @@ class ExtraTreesClassifier(ForestClassifier):
     Attributes
     ----------
     base_estimator_ : ExtraTreeClassifier
-        The child estimator template used to create the collection of fitted sub-estimators.
+        The child estimator template used to create the collection of fitted
+        sub-estimators.
 
     estimators_ : list of DecisionTreeClassifier
         The collection of fitted sub-estimators.
@@ -1690,7 +1693,8 @@ class ExtraTreesRegressor(ForestRegressor):
     Attributes
     ----------
     base_estimator_ : ExtraTreeRegressor
-        The child estimator template used to create the collection of fitted sub-estimators.
+        The child estimator template used to create the collection of fitted
+        sub-estimators.
 
     estimators_ : list of DecisionTreeRegressor
         The collection of fitted sub-estimators.
