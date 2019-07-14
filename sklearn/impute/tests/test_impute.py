ea9b8a063eb042d9e47a46ff4a1d2aa0c1777865
Harsh Soni <devilincarcerated020@gmail.com>
2019-07-15 03:12:35 +0530

    Correctly indent fit_transform test

--- sklearn/impute/tests/test_impute.py
+++ sklearn/impute/tests/test_impute.py
@@ -1008,11 +1008,9 @@ def test_missing_indicator_new(missing_values, arr_type, dtype, param_features,
     X_fit_expected = X_fit_expected.astype(dtype)
     X_trans_expected = X_trans_expected.astype(dtype)
 
-    indicator_test_ft = MissingIndicator(missing_values=missing_values, \
-                                 features=param_features, \
-                                 sparse=False)
-    assert_array_equal(indicator_test_ft.fit(X_fit).transform(X_fit), \
-                            indicator_test_ft.fit_transform(X_fit))
+    indicator_test_ft = MissingIndicator(missing_values=missing_values, features=param_features, sparse=False)
+    assert_array_equal(indicator_test_ft.fit(X_fit).transform(X_fit),
+    indicator_test_ft.fit_transform(X_fit))
 
     indicator = MissingIndicator(missing_values=missing_values,
                                  features=param_features,
