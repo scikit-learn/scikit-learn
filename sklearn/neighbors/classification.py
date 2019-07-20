688d38e18368dd8a4b4d19d63863ad7e1d95049d
Abhishek Jana <48491625+abhishek-jana@users.noreply.github.com>
2019-07-20 03:46:34 -0500

    Update sklearn/neighbors/classification.py
    Co-Authored-By: Thomas J Fan <thomasjpfan@gmail.com>

--- sklearn/neighbors/classification.py
+++ sklearn/neighbors/classification.py
@@ -88,7 +88,7 @@ class KNeighborsClassifier(NeighborsBase, KNeighborsMixin,
         Class labels known to the classifier
         
     effective_metric_ : string or callble
-        The distance metric to use. It will be same as
+        The distance metric used. It will be same as
         the `metric` parameter or a synonym of it, e.g.
         'euclidean' if the `metric` parameter set to
         'minkowski' and `p` parameter set to 2.
