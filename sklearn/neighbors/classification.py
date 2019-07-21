4f344c8c935ba3ed0037ce786c61a698c61db622
Abhishek Jana <48491625+abhishek-jana@users.noreply.github.com>
2019-07-20 20:06:34 -0500

    Update sklearn/neighbors/classification.py
    Co-Authored-By: Thomas J Fan <thomasjpfan@gmail.com>

--- sklearn/neighbors/classification.py
+++ sklearn/neighbors/classification.py
@@ -90,7 +90,6 @@ class KNeighborsClassifier(NeighborsBase, KNeighborsMixin,
         The distance metric used. It will be same as the `metric` parameter 
         or a synonym of it, e.g. 'euclidean' if the `metric` parameter set to
         'minkowski' and `p` parameter set to 2.
-        
     effective_metric_params_ : dict
         Additional keyword arguments for the metric function. For most metrics 
         will be same with `metric_params` parameter, but may also contain the
