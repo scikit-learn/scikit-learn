81eae5d42eeab01c9a23716d1576020c4bf816a6
Benedikt Boecking <ben.boecking@gmail.com>
2019-07-24 19:39:20 -0400

    Update comments in spectral.py
    Added comment to clarify exact eigenproblem that is solved.

--- sklearn/cluster/spectral.py
+++ sklearn/cluster/spectral.py
@@ -255,11 +255,13 @@ def spectral_clustering(affinity, n_clusters=8, n_components=None,
     random_state = check_random_state(random_state)
     n_components = n_clusters if n_components is None else n_components
 
+
+    # We now obtain the real valued solution matrix to the
+    # relaxed Ncut problem, solving the eigenvalue problem 
+    # L_sym x = lambda x  and recovering u = D^-1/2 x.
     # The first eigen vector is constant only for fully connected graphs
     # and should be kept for spectral clustering (drop_first = False)
     # See spectral_embedding documentation.
-    # The array maps will contain the real valued solution matrix to the
-    # relaxed Ncut problem. 
     maps = spectral_embedding(affinity, n_components=n_components,
                               eigen_solver=eigen_solver,
                               random_state=random_state,
