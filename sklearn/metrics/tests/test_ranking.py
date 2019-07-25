3d4f2ccff6d2027d8bf1860bccd0029e6756022c
Jerome Dockes <jerome@dockes.org>
2019-07-25 10:43:30 +0200

    parametrize test_ndcg_toy_examples with ignore_ties

--- sklearn/metrics/tests/test_ranking.py
+++ sklearn/metrics/tests/test_ranking.py
@@ -1313,37 +1313,41 @@ def test_ndcg_invariant():
     assert ndcg_score(y_true, y_score) == pytest.approx(1.)
 
 
-def test_ndcg_toy_examples():
+@pytest.mark.parametrize('ignore_ties', [True, False])
+def test_ndcg_toy_examples(ignore_ties):
     y_true = 3 * np.eye(7)[:5]
     y_score = np.tile(np.arange(6, -1, -1), (5, 1))
     y_score_noisy = y_score + np.random.RandomState(0).uniform(
         -.2, .2, size=y_score.shape)
-    assert _dcg_sample_scores(y_true, y_score) == pytest.approx(
-        3 / np.log2(np.arange(2, 7)))
-    assert _dcg_sample_scores(y_true, y_score) == pytest.approx(
-        _dcg_sample_scores(y_true, y_score, ignore_ties=True))
-    assert _dcg_sample_scores(y_true, y_score_noisy) == pytest.approx(
-        3 / np.log2(np.arange(2, 7)))
-    assert _dcg_sample_scores(y_true, y_score_noisy) == pytest.approx(
-        _dcg_sample_scores(y_true, y_score_noisy, ignore_ties=True))
-    assert _ndcg_sample_scores(y_true, y_score) == pytest.approx(
-        1 / np.log2(np.arange(2, 7)))
-    assert _dcg_sample_scores(y_true, y_score, log_base=10) == pytest.approx(
-        3 / np.log10(np.arange(2, 7)))
-    assert ndcg_score(y_true, y_score) == pytest.approx(
-        (1 / np.log2(np.arange(2, 7))).mean())
-    assert dcg_score(y_true, y_score) == pytest.approx(
+    assert _dcg_sample_scores(
+        y_true, y_score, ignore_ties=ignore_ties) == pytest.approx(
+            3 / np.log2(np.arange(2, 7)))
+    assert _dcg_sample_scores(
+        y_true, y_score_noisy, ignore_ties=ignore_ties) == pytest.approx(
+            3 / np.log2(np.arange(2, 7)))
+    assert _ndcg_sample_scores(
+        y_true, y_score, ignore_ties=ignore_ties) == pytest.approx(
+            1 / np.log2(np.arange(2, 7)))
+    assert _dcg_sample_scores(y_true, y_score, log_base=10,
+                              ignore_ties=ignore_ties) == pytest.approx(
+                                  3 / np.log10(np.arange(2, 7)))
+    assert ndcg_score(
+        y_true, y_score, ignore_ties=ignore_ties) == pytest.approx(
+            (1 / np.log2(np.arange(2, 7))).mean())
+    assert dcg_score(y_true, y_score, ignore_ties=ignore_ties) == pytest.approx(
         (3 / np.log2(np.arange(2, 7))).mean())
     y_true = 3 * np.ones((5, 7))
     expected_dcg_score = (3 / np.log2(np.arange(2, 9))).sum()
-    assert _dcg_sample_scores(y_true, y_score) == pytest.approx(
-        expected_dcg_score * np.ones(5))
-    assert _ndcg_sample_scores(y_true, y_score) == pytest.approx(np.ones(5))
-    assert dcg_score(y_true, y_score) == pytest.approx(expected_dcg_score)
+    assert _dcg_sample_scores(
+        y_true, y_score, ignore_ties=ignore_ties) == pytest.approx(
+            expected_dcg_score * np.ones(5))
+    assert _ndcg_sample_scores(
+        y_true, y_score, ignore_ties=ignore_ties) == pytest.approx(np.ones(5))
     assert dcg_score(
-        y_true, y_score, ignore_ties=True) == pytest.approx(expected_dcg_score)
-    assert ndcg_score(y_true, y_score) == pytest.approx(1.)
-    assert ndcg_score(y_true, y_score, ignore_ties=True) == pytest.approx(1.)
+        y_true, y_score, ignore_ties=ignore_ties) == pytest.approx(
+            expected_dcg_score)
+    assert ndcg_score(
+        y_true, y_score, ignore_ties=ignore_ties) == pytest.approx(1.)
 
 
 def test_ndcg_score():
