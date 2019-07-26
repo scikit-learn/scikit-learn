bbf7f38fa69a9335397fa1cdfd73d153cbc3670d
Roman Yurchak <rth.yurchak@pm.me>
2019-07-26 11:29:54 +0200

    Use dataframe OpenML fetcher

--- examples/linear_model/plot_poisson_regression_non_normal_loss.py
+++ examples/linear_model/plot_poisson_regression_non_normal_loss.py
@@ -25,6 +25,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+from sklearn.datasets import fetch_openml
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import PoissonRegressor, LinearRegression
 from sklearn.model_selection import train_test_split
@@ -46,20 +47,16 @@ def load_mtpl2(n_samples=100000):
       number of samples to select (for faster run time).
     """
 
-    # Note: this should use the OpenML DataFrame fetcher in the future
-    df_freq = pd.read_csv(
-        "https://www.openml.org/data/get_csv/20649148/freMTPL2freq.csv",
-        dtype={"IDpol": np.int},
-        index_col=0,
-    )
+    # freMTPL2freq dataset from https://www.openml.org/d/41214
+    df_freq = fetch_openml(data_id=41214, as_frame=True)['data']
+    df_freq['IDpol'] = df_freq['IDpol'].astype(np.int)
+    df_freq.set_index('IDpol', inplace=True)
 
-    df_sev = pd.read_csv(
-        "https://www.openml.org/data/get_csv/20649149/freMTPL2sev.arff",
-        index_col=0,
-    )
+    # freMTPL2sev dataset from https://www.openml.org/d/41215
+    df_sev = fetch_openml(data_id=41215, as_frame=True)['data']
 
     # sum ClaimAmount over identical IDs
-    df_sev = df_sev.groupby(level=0).sum()
+    df_sev = df_sev.groupby('IDpol').sum()
 
     df = df_freq.join(df_sev, how="left")
     df["ClaimAmount"].fillna(0, inplace=True)
