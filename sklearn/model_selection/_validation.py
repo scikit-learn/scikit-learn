a6d474d491da238f6098a23ad72aab70f6ff75cd
Alex Itkes <38556752+alexitkes@users.noreply.github.com>
2019-07-27 10:39:05 +0300

    Update _validation.py
    `int, or str` -> `int or str`

--- sklearn/model_selection/_validation.py
+++ sklearn/model_selection/_validation.py
@@ -111,11 +111,7 @@ def cross_validate(estimator, X, y=None, *, groups=None, scoring=None, cv=None,
     fit_params : dict, default=None
         Parameters to pass to the fit method of the estimator.
 
-<<<<<<< HEAD
     pre_dispatch : int or str, default='2*n_jobs'
-=======
-    pre_dispatch : int, or str, default='2*n_jobs'
->>>>>>> More parameter documentation improved in model_selection._validation submodule.
         Controls the number of jobs that get dispatched during parallel
         execution. Reducing this number can be useful to avoid an
         explosion of memory consumption when more jobs get dispatched
@@ -387,10 +383,14 @@ def cross_val_score(estimator, X, y=None, *, groups=None, scoring=None,
         Parameters to pass to the fit method of the estimator.
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     pre_dispatch : int or str, default='2*n_jobs'
 =======
     pre_dispatch : int, or str, default='2*n_jobs'
 >>>>>>> More parameter documentation improved in model_selection._validation submodule.
+=======
+    pre_dispatch : int or str, default='2*n_jobs'
+>>>>>>> Update _validation.py
         Controls the number of jobs that get dispatched during parallel
         execution. Reducing this number can be useful to avoid an
         explosion of memory consumption when more jobs get dispatched
