0030d0984cd7e52c4fd78dfe888a5efc9151594e
arpanchowdhry <arpanchowdhry@gmail.com>
2019-07-28 21:22:05 -0700

    Update sklearn/linear_model/coordinate_descent.py
    Co-Authored-By: Thomas J Fan <thomasjpfan@gmail.com>

--- sklearn/linear_model/coordinate_descent.py
+++ sklearn/linear_model/coordinate_descent.py
@@ -1285,7 +1285,7 @@ class LassoCV(LinearModelCV, RegressorMixin):
     copy_X : bool, default=True
         If ``True``, X will be copied; else, it may be overwritten.
 
-    cv : int, cross-validation generator or an iterable, default=None
+    cv : int, cross-validation generator, iterable or None, default=None
         Determines the cross-validation splitting strategy.
         Possible inputs for cv are:
 
