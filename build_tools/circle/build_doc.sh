345d8abb574bd46c83c0e624caee283ad3425da5
Loïc Estève <loic.esteve@ymail.com>
2019-07-29 17:39:39 +0200

    [doc build] Use sphinx-gallery 0.3.1.

--- build_tools/circle/build_doc.sh
+++ build_tools/circle/build_doc.sh
@@ -133,7 +133,7 @@ conda create -n $CONDA_ENV_NAME --yes --quiet python="${PYTHON_VERSION:-*}" \
   joblib
 
 source activate testenv
-pip install "sphinx-gallery>=0.2,<0.3"
+pip install sphinx-gallery==0.3.1
 pip install numpydoc==0.9
 
 # Build and install scikit-learn in dev mode
