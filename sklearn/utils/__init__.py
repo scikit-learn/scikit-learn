dcf8dac33d8634577136b5c8d469dd40bcfd38ba
adrinjalali <adrin.jalali@gmail.com>
2019-07-29 15:53:25 +0200

    fix init

--- sklearn/utils/__init__.py
+++ sklearn/utils/__init__.py
@@ -23,6 +23,7 @@ from .validation import (as_float_array,
                          check_random_state, column_or_1d, check_array,
                          check_consistent_length, check_X_y, indexable,
                          check_symmetric, check_scalar)
+from ._namedarray import NamedArray
 from .. import get_config
 
 
@@ -67,7 +68,7 @@ __all__ = ["murmurhash3_32", "as_float_array",
            "check_symmetric", "indices_to_mask", "deprecated",
            "cpu_count", "Parallel", "Memory", "delayed", "parallel_backend",
            "register_parallel_backend", "hash", "effective_n_jobs",
-           "resample", "shuffle", "check_matplotlib_support"]
+           "resample", "shuffle", "check_matplotlib_support", "NamedArray"]
 
 IS_PYPY = platform.python_implementation() == 'PyPy'
 _IS_32BIT = 8 * struct.calcsize("P") == 32
