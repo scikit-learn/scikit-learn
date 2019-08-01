2607235fbaee6530bc05c35ee7ab0f83daaca82b
Thomas Fan <thomasjpfan@gmail.com>
2019-08-01 15:54:47 -0400

    TST Adds test on dropping features

--- sklearn/impute/tests/test_knn.py
+++ sklearn/impute/tests/test_knn.py
@@ -579,3 +579,15 @@ def test_knn_imputer_not_enough_valid_distances(na):
     X2 = np.array([[4, na]])
     X2_imputed = np.array([[4, 6]])
     assert_allclose(knn.transform(X2), X2_imputed)
+
+
+@pytest.mark.parametrize("na", [-1, np.nan])
+def test_knn_imputer_drops_all_nan_features(na):
+    X1 = np.array([[na, 1], [na, 2]])
+    knn = KNNImputer(missing_values=na, n_neighbors=1)
+    X1_expected = np.array([[1], [2]])
+    assert_allclose(knn.fit_transform(X1), X1_expected)
+
+    X2 = np.array([[1, 2], [3, na]])
+    X2_expected = np.array([[2], [1.5]])
+    assert_allclose(knn.transform(X2), X2_expected)
