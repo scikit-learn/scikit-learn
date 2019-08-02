8d77de60e8449918dacd74f9dddc6aa4b39fcb3e
Roman Yurchak <rth.yurchak@pm.me>
2019-08-02 17:28:52 +0200

    Remove vendored lgamma implementation

--- sklearn/utils/lgamma.pyx
+++ sklearn/utils/lgamma.pyx
@@ -1,8 +1,10 @@
-cdef extern from "src/gamma.h":
-    cdef double sklearn_lgamma(double x)
+from libc.math cimport lgamma
 
 
 cdef double lgamma(double x):
+    msg = ("sklearn.utils.lgamma is deprecated in v0.22 and will "
+           "be removed in v0.24. Use libc.math.lgamma instead.")
+    warnings.warn(msg, category=DeprecationWarning)
     if x <= 0:
         raise ValueError("x must be strictly positive, got %f" % x)
-    return sklearn_lgamma(x)
+    return lgamma(x)
