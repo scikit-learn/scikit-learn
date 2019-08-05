7db52679e5302e02b1efcaffe4b74a8095ff9f38
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2019-08-05 10:45:42 -0700

    Update examples/gaussian_process/plot_gpr_on_structured_data.py
    convention-conforming variable names
    
    Co-Authored-By: Joel Nothman <joel.nothman@gmail.com>

--- examples/gaussian_process/plot_gpr_on_structured_data.py
+++ examples/gaussian_process/plot_gpr_on_structured_data.py
@@ -76,7 +76,7 @@ class SequenceKernel(StructureOrGenericKernelMixin, Kernel):
 
 kernel = SequenceKernel()
 
-seqs = np.array(['AGCT', 'AGC', 'AACT', 'TAA', 'AAA', 'GAACA'])
+X = ['AGCT', 'AGC', 'AACT', 'TAA', 'AAA', 'GAACA']
 vals = np.array([1.0, 1.0, 2.0, 2.0, 3.0, 3.0])
 
 '''
