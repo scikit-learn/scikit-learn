43e630e890570a881ad283e2c847d047f68d5196
Guillem García Subies <37592763+GuillemGSubies@users.noreply.github.com>
2019-08-06 08:51:22 +0200

    Update test_text.py

--- sklearn/feature_extraction/tests/test_text.py
+++ sklearn/feature_extraction/tests/test_text.py
@@ -506,9 +506,9 @@ def test_tfidf_vectorizer_setters():
 
 # FIXME Remove copy parameter support in 0.24
 def test_tfidf_vectorizer_deprecationwarning():
-    msg = ("'copy' param is unused and has been deprecated since version 0.22."
-           " Backward compatibility for 'copy' will be removed in"
-           " 0.24.")
+    msg = ("'copy' param is unused and has been deprecated since "
+           "version 0.22. Backward compatibility for 'copy' will "
+           "be removed in 0.24.")
     with pytest.warns(DeprecationWarning, match=msg):
         tv = TfidfVectorizer()
         train_data = JUNK_FOOD_DOCS
