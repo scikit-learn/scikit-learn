7050a6b75585ef1939d0bd9416d7979cb457f132
Krishna <kskrishnasangeeth@gmail.com>
2019-08-07 21:19:30 +0530

    Add mae loss to MLP Regressor

--- sklearn/neural_network/_base.py
+++ sklearn/neural_network/_base.py
@@ -7,7 +7,6 @@
 import numpy as np
 
 from scipy.special import expit as logistic_sigmoid
-from scipy.special import xlogy
 
 
 def identity(X):
@@ -194,6 +193,18 @@ def squared_loss(y_true, y_pred):
     """
     return ((y_true - y_pred) ** 2).mean() / 2
 
+def mae_loss(y_true, y_pred):
+    """Compute the mean absolute error loss
+
+    Parameters
+    ----------
+    y_true : array-like or label indicator matrix
+         Ground truth (correct) labels.
+    y_pred : array-like or label indicator matrix
+         predicted values.
+    """
+    return np.average(np.abs(y_true - y_pred))
+
 
 def log_loss(y_true, y_prob):
     """Compute Logistic loss for classification.
@@ -212,13 +223,15 @@ def log_loss(y_true, y_prob):
     loss : float
         The degree to which the samples are correctly predicted.
     """
+    y_prob = np.clip(y_prob, 1e-10, 1 - 1e-10)
+
     if y_prob.shape[1] == 1:
         y_prob = np.append(1 - y_prob, y_prob, axis=1)
 
     if y_true.shape[1] == 1:
         y_true = np.append(1 - y_true, y_true, axis=1)
 
-    return - xlogy(y_true, y_prob).sum() / y_prob.shape[0]
+    return -np.sum(y_true * np.log(y_prob)) / y_prob.shape[0]
 
 
 def binary_log_loss(y_true, y_prob):
@@ -241,9 +254,11 @@ def binary_log_loss(y_true, y_prob):
     loss : float
         The degree to which the samples are correctly predicted.
     """
-    return -(xlogy(y_true, y_prob) +
-             xlogy(1 - y_true, 1 - y_prob)).sum() / y_prob.shape[0]
+    y_prob = np.clip(y_prob, 1e-10, 1 - 1e-10)
+
+    return -np.sum(y_true * np.log(y_prob) +
+                   (1 - y_true) * np.log(1 - y_prob)) / y_prob.shape[0]
 
 
 LOSS_FUNCTIONS = {'squared_loss': squared_loss, 'log_loss': log_loss,
-                  'binary_log_loss': binary_log_loss}
+                  'binary_log_loss': binary_log_loss, 'mae_loss': mae_loss}
