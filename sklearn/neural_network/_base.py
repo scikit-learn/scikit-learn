Correct the loss formula

sklearn/neural_network/_base.py
@@ -194,6 +194,7 @@ def squared_loss(y_true, y_pred):
     """
     return ((y_true - y_pred) ** 2).mean() / 2
 
+
 def mae_loss(y_true, y_pred):
     """Compute the mean absolute error loss
 
@@ -204,7 +205,8 @@ def mae_loss(y_true, y_pred):
     y_pred : array-like or label indicator matrix
          predicted values.
     """
-    return np.average(np.abs(y_true - y_pred))
+    return (1/y_true.shape[0]) * np.sum(np.abs(y_true - y_pred)) 
+
 
 def log_loss(y_true, y_prob):
     """Compute Logistic loss for classification.
@@ -257,4 +259,4 @@ def binary_log_loss(y_true, y_prob):
 
 
 LOSS_FUNCTIONS = {'squared_loss': squared_loss, 'log_loss': log_loss,
-                  'binary_log_loss': binary_log_loss, 'mae_loss':mae_loss}
+                  'binary_log_loss': binary_log_loss, 'mae_loss': mae_loss}