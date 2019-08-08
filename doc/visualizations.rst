b4efb4ea35dcd1cbdf1150b6eb7d5af077da9c50
Thomas Fan <thomasjpfan@gmail.com>
2019-08-08 00:14:26 -0400

    DOC Create a new figure for second plot

--- doc/visualizations.rst
+++ doc/visualizations.rst
@@ -44,7 +44,7 @@ method of the `Display` object.
     rfc = RandomForestClassifier(random_state=42)
     rfc.fit(X_train, y_train)
 
-    ax = plt.gca()
+    _, ax = plt.subplots()
     rfc_disp = plot_roc_curve(rfc, X_test, y_test, ax=ax, alpha=0.8)
     svc_disp.plot(ax=ax, alpha=0.8)
 
--- examples/plot_roc_curve_visualization_api.py
+++ examples/plot_roc_curve_visualization_api.py
@@ -47,9 +47,9 @@ plt.show()
 # without recomputing the values of the roc curve itself. Futhermore, we
 # pass `alpha=0.8` to the plot functions to adjust the alpha values of the
 # curves.
+_, ax = plt.subplots()
 rfc = RandomForestClassifier(n_estimators=10, random_state=42)
 rfc.fit(X_train, y_train)
-ax = plt.gca()
 rfc_disp = plot_roc_curve(rfc, X_test, y_test, ax=ax, alpha=0.8)
 svc_disp.plot(ax=ax, alpha=0.8)
 plt.show()
