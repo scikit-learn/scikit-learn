7bbfa9f1c86334fcf65aed09acfc509f8c2d9f2c
Thomas Fan <thomasjpfan@gmail.com>
2019-08-09 16:29:46 -0400

    DOC

--- sklearn/svm/classes.py
+++ sklearn/svm/classes.py
@@ -487,9 +487,9 @@ class SVC(BaseSVC):
 
     probability : boolean, optional (default=False)
         Whether to enable probability estimates. This must be enabled prior
-        to calling `fit`, and will slow down that method. When enabled,
-        `predict_proba` may be inconsistent with `predict` and will slow down
-        fitting as it internally uses 5-fold cross-validation. Read more in the
+        to calling `fit` and will slow down that method as it internally uses
+        5-fold cross-validation. When enabled, `predict_proba` may be
+        inconsistent with `predict`. Read more in the
         :ref:`User Guide <scores_probabilities>`.
 
     tol : float, optional (default=1e-3)
@@ -689,9 +689,9 @@ class NuSVC(BaseSVC):
 
     probability : boolean, optional (default=False)
         Whether to enable probability estimates. This must be enabled prior
-        to calling `fit`, and will slow down that method. When enabled,
-        `predict_proba` may be inconsistent with `predict` and will slow down
-        fitting as it internally uses 5-fold cross-validation. Read more in the
+        to calling `fit` and will slow down that method as it internally uses
+        5-fold cross-validation. When enabled, `predict_proba` may be
+        inconsistent with `predict`. Read more in the
         :ref:`User Guide <scores_probabilities>`.
 
     tol : float, optional (default=1e-3)
