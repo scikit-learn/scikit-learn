c74bcc565b0aeceb5c7821df80e2c7c588702efc
Thomas Fan <thomasjpfan@gmail.com>
2019-08-09 14:54:19 -0400

    DOC Cleaner

--- build_tools/azure/install.sh
+++ build_tools/azure/install.sh
@@ -58,7 +58,7 @@ if [[ "$DISTRIB" == "conda" ]]; then
     # Old packages coming from the 'free' conda channel have been removed but
     # we are using them for testing Python 3.5. See
     # https://www.anaconda.com/why-we-removed-the-free-channel-in-conda-4-7/
-    # for more details. restore_free_channel only defined in conda 4.7
+    # for more details. restore_free_channel is defined starting from conda 4.7
     conda_version=$(conda -V | awk '{print $2}')
     if version_ge "$conda_version" "4.7.0" && [[ "$PYTHON_VERSION" == "3.5" ]]; then
         conda config --set restore_free_channel true
