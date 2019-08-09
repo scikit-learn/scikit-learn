28ce5ec71c93e176832255cd2a05aa51befe6e29
Thomas Fan <thomasjpfan@gmail.com>
2019-08-09 14:21:53 -0400

    MAINT Only uses free on legacy python (3.5)

--- build_tools/azure/install.sh
+++ build_tools/azure/install.sh
@@ -52,10 +52,12 @@ if [[ "$DISTRIB" == "conda" ]]; then
     fi
 
     # Old packages coming from the 'free' conda channel have been removed but
-    # we are using them for testing. See
+    # we are using them for testing Python 3.5. See
     # https://www.anaconda.com/why-we-removed-the-free-channel-in-conda-4-7/
     # for more details.
-    conda config --set restore_free_channel true
+    if [[ "$PYTHON_VERSION" == "3.5" ]]; then
+        conda config --set restore_free_channel true
+    fi
 
 	make_conda $TO_INSTALL
     if [[ "$PYTHON_VERSION" == "*" ]]; then
