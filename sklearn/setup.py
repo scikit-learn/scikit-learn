5129289ae545aa76f32d540364eaaf502f2d9f21
Thomas Fan <thomasjpfan@gmail.com>
2019-08-13 16:13:50 -0400

    CLN Adds _plot into setup

--- sklearn/setup.py
+++ sklearn/setup.py
@@ -32,6 +32,7 @@ def configuration(parent_package='', top_path=None):
     config.add_subpackage('impute')
     config.add_subpackage('impute/tests')
     config.add_subpackage('inspection')
+    config.add_subpackage('inspection/_plot')
     config.add_subpackage('inspection/tests')
     config.add_subpackage('mixture')
     config.add_subpackage('mixture/tests')
