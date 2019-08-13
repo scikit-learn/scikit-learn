a466a17e66205ff3367caf5647531118f96e45bc
Mehrnaz Motamed <memotame@eng.ucsd.edu>
2019-08-12 23:27:28 -0700

    Update test_spectral_embedding.py

--- sklearn/manifold/tests/test_spectral_embedding.py
+++ sklearn/manifold/tests/test_spectral_embedding.py
@@ -283,6 +283,7 @@ def test_spectral_embedding_first_eigen_vector():
         assert np.std(embedding[:, 0]) == pytest.approx(0)
         assert np.std(embedding[:, 1]) > 1e-3
 
+
 def test_spectral_embedding_copy_variable(seed=36):
     # Test that when "copy" input variable is set to False
     # spectral_embedding returns the correct value
