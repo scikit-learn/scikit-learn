dbf38581cf53ae5f8694a17534c2de6fbdf113e2
Omar Saleem <omarsaleem003@gmail.com>
2019-08-14 09:49:38 +0700

    Update sklearn/ensemble/bagging.py
    Co-Authored-By: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- sklearn/ensemble/bagging.py
+++ sklearn/ensemble/bagging.py
@@ -512,7 +512,7 @@ class BaggingClassifier(BaseBagging, ClassifierMixin):
         The base estimator from which the ensemble is grown.
 
     n_features_ : int
-        The number of features when ``fit`` is performed.
+        The number of features when `fit` is performed.
 
     estimators_ : list of estimators
         The collection of fitted base estimators.
