2c7ad8c485c749e3889cb1f917bfde97539da7cb
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2019-08-19 18:16:37 +0200

    don't get tags twice

--- sklearn/utils/estimator_checks.py
+++ sklearn/utils/estimator_checks.py
@@ -267,7 +267,8 @@ def _yield_all_checks(name, estimator):
     yield check_dict_unchanged
     yield check_dont_overwrite_parameters
     yield check_fit_idempotent
-    yield check_fit_non_negative
+    if tags["requires_positive_X"]:
+        yield check_fit_non_negative
 
 
 def _construct_instance(Estimator):
@@ -2490,12 +2491,11 @@ def check_outliers_fit_predict(name, estimator_orig):
 def check_fit_non_negative(name, estimator_orig):
     # Check that proper warning is raised for non-negative X
     # when tag requires_positive_X is present
-    if _safe_tags(estimator_orig, "requires_positive_X"):
-        X = np.array([[-1., 1], [-1., 1]])
-        y = np.array([1, 2])
-        estimator = clone(estimator_orig)
-        assert_raises_regex(ValueError, "Negative values in data passed to",
-                            estimator.fit, X, y)
+    X = np.array([[-1., 1], [-1., 1]])
+    y = np.array([1, 2])
+    estimator = clone(estimator_orig)
+    assert_raises_regex(ValueError, "Negative values in data passed to",
+                        estimator.fit, X, y)
 
 
 
