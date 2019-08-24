52b0b1487f96109db895c4c73b56fa942d51647d
Ingrid Spielman <ingridspielman@Ingrids-MacBook-Pro.local>
2019-08-24 11:51:15 -0400

    update docstring

--- sklearn/linear_model/ridge.py
+++ sklearn/linear_model/ridge.py
@@ -1757,6 +1757,13 @@ class RidgeClassifierCV(LinearClassifierMixin, _BaseRidgeCV):
 
     Attributes
     ----------
+    cv_values_ : array, shape = [n_samples, n_targets, n_alphas], optional
+        Cross-validation values for each alpha (if ``store_cv_values=True`` and
+        ``cv=None``). After ``fit()`` has been called, this attribute will
+        contain the mean squared errors (by default) or the values of the
+        ``{loss,score}_func`` function (if provided in the constructor). This 
+        attribute exists only when store_cv_values is True. 
+
     coef_ : array, shape (1, n_features) or (n_targets, n_features)
         Coefficient of the features in the decision function.
 
