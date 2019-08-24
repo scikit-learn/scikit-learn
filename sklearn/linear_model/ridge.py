4353f9dd9ce3a5e7007431712ad448ad9b581e00
Ingrid Spielman <ingridspielman@Ingrids-MacBook-Pro.local>
2019-08-24 12:47:31 -0400

    remove trailing white space and update to docstring

--- sklearn/linear_model/ridge.py
+++ sklearn/linear_model/ridge.py
@@ -1762,7 +1762,7 @@ class RidgeClassifierCV(LinearClassifierMixin, _BaseRidgeCV):
         ``cv=None``). After ``fit()`` has been called, this attribute will
         contain the mean squared errors (by default) or the values of the
         ``{loss,score}_func`` function (if provided in the constructor). This 
-        attribute exists only when store_cv_values is True. 
+        attribute exists only when ``store_cv_values`` is True.
 
     coef_ : array, shape (1, n_features) or (n_targets, n_features)
         Coefficient of the features in the decision function.
