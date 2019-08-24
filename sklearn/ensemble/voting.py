376f118f450fe78ceb1778ce535b22fe0f066919
Hannah Bruce Macdonald <hannah.brucemacdonald@choderalab.org>
2019-08-24 13:23:49 -0400

    adding verbose option and message to VotingClassifier and VotingRegressor
    addresses issue #10360

--- sklearn/ensemble/voting.py
+++ sklearn/ensemble/voting.py
@@ -24,28 +24,30 @@ from ..base import RegressorMixin
 from ..base import TransformerMixin
 from ..base import clone
 from ..preprocessing import LabelEncoder
-from ..utils import Bunch
+from ..utils import Bunch, _print_elapsed_time
 from ..utils.validation import check_is_fitted
 from ..utils.metaestimators import _BaseComposition
 from ..utils.multiclass import check_classification_targets
 from ..utils.validation import column_or_1d
 
 
-def _parallel_fit_estimator(estimator, X, y, sample_weight=None):
+def _parallel_fit_estimator(estimator, X, y, sample_weight=None,
+                            message_clsname='', message=None):
     """Private function used to fit an estimator within a job."""
-    if sample_weight is not None:
-        try:
-            estimator.fit(X, y, sample_weight=sample_weight)
-        except TypeError as exc:
-            if "unexpected keyword argument 'sample_weight'" in str(exc):
-                raise ValueError(
-                    "Underlying estimator {} does not support sample weights."
-                    .format(estimator.__class__.__name__)
-                ) from exc
-            raise
-    else:
-        estimator.fit(X, y)
-    return estimator
+    with _print_elapsed_time(message_clsname, message):
+        if sample_weight is not None:
+            try:
+                estimator.fit(X, y, sample_weight=sample_weight)
+            except TypeError as exc:
+                if "unexpected keyword argument 'sample_weight'" in str(exc):
+                    raise ValueError(
+                        "Underlying estimator {} does not support sample weights."
+                        .format(estimator.__class__.__name__)
+                    ) from exc
+                raise
+        else:
+            estimator.fit(X, y)
+        return estimator
 
 
 class _BaseVoting(_BaseComposition, TransformerMixin):
@@ -116,7 +118,7 @@ class _BaseVoting(_BaseComposition, TransformerMixin):
     def _log_message(self, name, idx, total):
         if not self.verbose:
             return None
-        return '(classifier %d of %d) Processing %s' % (idx, total, name)
+        return '(classifier %d of %d) Processing %s' % (idx+1, total, name)
 
     def set_params(self, **params):
         """ Setting the parameters for the ensemble estimator
