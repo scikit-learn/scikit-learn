93e184093b6c417ef8fde899ff74a54e467b9642
matt <mhancock743@gmail.com>
2019-08-26 20:02:59 -0500

    Refactor conditional structures

--- sklearn/ensemble/forest.py
+++ sklearn/ensemble/forest.py
@@ -93,19 +93,21 @@ def _get_n_samples_bootstrap(n_samples, max_samples):
     """
     if max_samples is None:
         return n_samples
-    elif isinstance(max_samples, numbers.Integral):
+
+    if isinstance(max_samples, numbers.Integral):
         if not (1 <= max_samples <= n_samples):
             msg = "`max_samples` must be in range 1 to {} but got value {}"
             raise ValueError(msg.format(n_samples, max_samples))
         return max_samples
-    elif isinstance(max_samples, numbers.Real):
-        if not (0 < max_samples < 1.0):
+
+    if isinstance(max_samples, numbers.Real):
+        if not (0 < max_samples < 1):
             msg = "`max_samples` must be in range (0, 1) but got value {}"
             raise ValueError(msg.format(max_samples))
         return int(round(n_samples * max_samples))
-    else:
-        msg = "`max_samples` should be int or float, but got type '{}'"
-        raise TypeError(msg.format(type(max_samples)))
+
+    msg = "`max_samples` should be int or float, but got type '{}'"
+    raise TypeError(msg.format(type(max_samples)))
 
 
 def _generate_sample_indices(random_state, n_samples, n_samples_bootstrap):
