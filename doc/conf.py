baf6cc3a9772401cc2c26122fa510fff8feec972
Thomas Fan <thomasjpfan@gmail.com>
2019-08-30 00:40:05 -0400

    DOC Updates website design

--- doc/conf.py
+++ doc/conf.py
@@ -141,7 +141,8 @@ html_theme = 'scikit-learn-modern'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {'google_analytics': True }
+html_theme_options = {'google_analytics': True,
+                      'mathjax_path': mathjax_path[0]}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ['themes']
