100a1a5d3d496f6003d60e8327b4442ba22d344d
Venkatachalam N <n.venkatachalam@ericsson.com>
2019-09-04 19:43:40 +0530

    reversing_changes

--- sklearn/linear_model/tests/test_ridge.py
+++ sklearn/linear_model/tests/test_ridge.py
@@ -602,7 +602,7 @@ def _test_ridge_loo(filter_):
     assert ridge_gcv2.alpha_ == pytest.approx(alpha_)
 
     # check that we get same best alpha with custom score_func
-    def func(x, y): return -mean_squared_error(x, y)
+    func = lambda x, y: -mean_squared_error(x, y)
     scoring = make_scorer(func)
     ridge_gcv3 = RidgeCV(fit_intercept=False, scoring=scoring)
     f(ridge_gcv3.fit)(filter_(X_diabetes), y_diabetes)
