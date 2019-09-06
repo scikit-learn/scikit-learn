db8e5764391edcd1e710f5f1eb51970b99addf00
Kelly Carmody <teamuggles@gmail.com>
2019-09-05 21:51:59 -0400

    Added spaces to #'s to pass pep8 conventions

--- examples/applications/plot_out_of_core_classification.py
+++ examples/applications/plot_out_of_core_classification.py
@@ -47,8 +47,8 @@ def _not_in_sphinx():
 # Reuters Dataset related routines
 # --------------------------------
 #
-#The dataset used in this example is Reuters-21578 as provided by the UCI ML
-#repository. It will be automatically downloaded and uncompressed on first run.
+# The dataset used in this example is Reuters-21578 as provided by the UCI ML
+# repository. It will be automatically downloaded and uncompressed on first run.
 
 
 
@@ -314,12 +314,12 @@ for i, (X_train_text, y_train) in enumerate(minibatch_iterators):
 # Plot results
 # ------------
 #
-#The plot represents the learning curve of the classifier: the evolution
-#of classification accuracy over the course of the mini-batches. Accuracy is
-#measured on the first 1000 samples, held out as a validation set.
+# The plot represents the learning curve of the classifier: the evolution
+# of classification accuracy over the course of the mini-batches. Accuracy is
+# measured on the first 1000 samples, held out as a validation set.
 #
-#To limit the memory consumption, we queue examples up to a fixed amount before
-#feeding them to the learner.
+# To limit the memory consumption, we queue examples up to a fixed amount before
+# feeding them to the learner.
 
 
 def plot_accuracy(x, y, x_legend):
