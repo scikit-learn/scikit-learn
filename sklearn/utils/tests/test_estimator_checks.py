92d6bf6f3cbe5b44abc3a0ca3514d2cd6cd03443
Nicolas Hug <contact@nicolas-hug.com>
2019-09-06 13:30:31 -0400

    made new test file

--- sklearn/utils/tests/test_estimator_checks.py
+++ sklearn/utils/tests/test_estimator_checks.py
@@ -4,7 +4,6 @@ import sys
 import numpy as np
 import scipy.sparse as sp
 import joblib
-import pytest
 
 from io import StringIO
 
@@ -22,7 +21,6 @@ from sklearn.utils.estimator_checks import set_checking_parameters
 from sklearn.utils.estimator_checks import check_estimators_unfitted
 from sklearn.utils.estimator_checks import check_fit_score_takes_y
 from sklearn.utils.estimator_checks import check_no_attributes_set_in_init
-from sklearn.utils.estimator_checks import choose_check_classifiers_labels
 from sklearn.utils.validation import check_is_fitted
 from sklearn.utils.estimator_checks import check_outlier_corruption
 from sklearn.ensemble import RandomForestClassifier
@@ -558,11 +556,6 @@ def test_check_class_weight_balanced_linear_classifier():
                         check_class_weight_balanced_linear_classifier,
                         'estimator_name',
                         BadBalancedWeightsClassifier)
-
-
-def test_choose_check_classifiers_labels_deprecated():
-    with pytest.warns(DeprecationWarning, match="removed in version 0.24"):
-        choose_check_classifiers_labels(None, None, None)
 
 
 if __name__ == '__main__':
