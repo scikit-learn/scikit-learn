a9895f9c65c101b6f1af41eda394b4fe4fd2ec20
Nicolas Hug <contact@nicolas-hug.com>
2019-09-06 10:58:33 -0400

    again

--- sklearn/neural_network/multilayer_perceptron.py
+++ sklearn/neural_network/multilayer_perceptron.py
@@ -1,6 +1,6 @@
 import warnings
 
-from ._multilayer_perceptron import *
+from ._multilayer_perceptron import *  # noqa
 
 
 msg = ("Any import from sklearn.neural_nework.multilayer_perceptron is "
@@ -8,5 +8,5 @@ msg = ("Any import from sklearn.neural_nework.multilayer_perceptron is "
        "0.22 and will not work anymore in version 0.24. You should only "
        "import from sklearn.neural_network directly.")
 
-#TODO: remove entire file in 0.24
+# TODO: remove entire file in 0.24
 warnings.warn(msg, DeprecationWarning)
--- sklearn/neural_network/rbm.py
+++ sklearn/neural_network/rbm.py
@@ -1,11 +1,11 @@
 import warnings
 
-from ._rbm import *
+from ._rbm import *  # noqa
 
 
 msg = ("Any import from sklearn.neural_nework.rbm is deprecated in version "
        "0.22 and will not work anymore in version 0.24. You should only "
        "import from sklearn.neural_network directly.")
 
-#TODO: remove entire file in 0.24
+# TODO: remove entire file in 0.24
 warnings.warn(msg, DeprecationWarning)
