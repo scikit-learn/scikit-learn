8bc766068cbc7d50eb552e0fcda4c2e9dfade6fc
Nicolas Hug <contact@nicolas-hug.com>
2019-09-09 08:26:46 -0400

    change warning msg

--- sklearn/neural_network/multilayer_perceptron.py
+++ sklearn/neural_network/multilayer_perceptron.py
@@ -6,7 +6,9 @@ from ._multilayer_perceptron import *  # noqa
 msg = ("The `sklearn.neural_nework.multilayer_perceptron` module is "
        "deprecated in version 0.22 and will be removed in version 0.24. "
        "The corresponding classes / functions "
-       "should instead be imported from sklearn.neural_nework")
+       "should instead be imported from sklearn.neural_nework. "
+       "Anything that cannot be imported from sklearn.neural_network is now ")
+       "part of the private API.")
 
 # TODO: remove entire file in 0.24
 warnings.warn(msg, DeprecationWarning)
