2957b4e5f1fc042e97518f9a0a0214abc36f825a
Nicolas Hug <contact@nicolas-hug.com>
2019-09-10 13:46:32 -0400

    pep8

--- sklearn/utils/tests/test_deprecated_utils.py
+++ sklearn/utils/tests/test_deprecated_utils.py
@@ -13,6 +13,7 @@ def test_choose_check_classifiers_labels_deprecated():
     with pytest.warns(DeprecationWarning, match="removed in version 0.24"):
         choose_check_classifiers_labels(None, None, None)
 
+
 def test_enforce_estimator_tags_y():
     with pytest.warns(DeprecationWarning, match="removed in version 0.24"):
         enforce_estimator_tags_y(DummyClassifier(), np.array([0, 1]))
