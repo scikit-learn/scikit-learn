51b127a46281c5e9a5997e053412be0e7a65fc9a
Leonard Binet <leonardbinet@gmail.com>
2019-09-10 09:49:49 +0200

    update test_label to use preferred pytest.raises

--- sklearn/preprocessing/tests/test_label.py
+++ sklearn/preprocessing/tests/test_label.py
@@ -24,7 +24,6 @@ from sklearn.preprocessing.label import _inverse_binarize_thresholding
 from sklearn.preprocessing.label import _inverse_binarize_multiclass
 from sklearn.preprocessing.label import _encode
 
-from sklearn.utils.testing import assert_raises, assert_raise_message
 from sklearn import datasets
 
 iris = datasets.load_iris()
@@ -603,12 +602,10 @@ def test_label_binarize_multilabel():
 def test_invalid_input_label_binarize():
     with pytest.raises(ValueError):
         label_binarize([0, 2], classes=[0, 2], pos_label=0, neg_label=1)
-    assert_raise_message(ValueError, "continuous target data is not "
-                         "supported with label binarization", label_binarize,
-                         [1.2, 2.7], classes=[0, 1])
-    assert_raise_message(ValueError, "classes [1 2 3] mismatch with the "
-                                     "labels [0 1] found in the data",
-                         label_binarize, [[1, 3]], classes=[1, 2, 3])
+    with pytest.raises(ValueError, match="continuous target data is not "):
+        label_binarize([1.2, 2.7], classes=[0, 1])
+    with pytest.raises(ValueError, match="mismatch with the labels"):
+        label_binarize([[1, 3]], classes=[1, 2, 3])
 
 
 def test_inverse_binarize_multiclass():
