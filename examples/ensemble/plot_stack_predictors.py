dbae5e85e66ba57c7caa0af82c0a65115bfb5e75
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2019-09-12 16:42:08 +0200

    EXA improve example

--- examples/ensemble/plot_stack_predictors.py
+++ examples/ensemble/plot_stack_predictors.py
@@ -7,35 +7,16 @@ Stacking refers to a method to blend estimators. In this strategy, some
 estimators are individually fitted on some training data while a final
 estimator is trained using the stacked predictions of these base estimators.
 
-In this example, we illustrate the use case in which different regressors
-(linear and non-linear) are stacked together and a final non-linear regressor
-is used to output the prediction. We compare the performance of each individual
-regressor with the stacking strategy. Stacking improves the overall
-performance.
+In this example, we illustrate the use case in which different regressors are
+stacked together and a final linear penalized regressor is used to output the
+prediction. We compare the performance of each individual regressor with the
+stacking strategy. Stacking slightly improves the overall performance.
 
 """
 print(__doc__)
+
 # Authors: Guillaume Lemaitre <g.lemaitre58@gmail.com>
 # License: BSD 3 clause
-
-import numpy as np
-
-###############################################################################
-# Stack of predictors on a single data set
-###############################################################################
-# Linear (ridge and lasso) and non-linear (RBF-SVR) are stacked together using
-# a gradient-boosting regressor.
-
-from sklearn.linear_model import LassoCV, RidgeCV
-from sklearn.svm import SVR
-from sklearn.ensemble import StackingRegressor, GradientBoostingRegressor
-
-estimators = [('Lasso', LassoCV(random_state=42)),
-              ('Ridge', RidgeCV()),
-              ('SVR', SVR(C=1, gamma=1e-6, kernel='rbf'))]
-stacking_regressor = StackingRegressor(
-    estimators=estimators,
-    final_estimator=GradientBoostingRegressor(random_state=0))
 
 ###############################################################################
 # The function ``plot_regression_results`` is used to plot the predicted and
@@ -66,12 +47,37 @@ def plot_regression_results(ax, y_true, y_pred, title, scores):
     ax.legend([extra], [scores], loc='upper left')
     ax.set_title(title)
 
+###############################################################################
+# Stack of predictors on a single data set
+###############################################################################
+# It is sometimes tedious to find the best regressor which will best perform
+# on a given dataset. Stacking allows to combine several learners outputs.
+# The performance of stacking is usually close to the best model and sometimes
+# it can outperform each individual model.
+#
+# Here, we combine 3 learners (linear and non-linear) and use a ridge regressor
+# to combine their outputs together.
+
+from sklearn.ensemble import StackingRegressor
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.linear_model import LassoCV
+from sklearn.linear_model import RidgeCV
+from sklearn.neighbors import KNeighborsRegressor
+
+estimators = [('KNN', KNeighborsRegressor(n_neighbors=1)),
+              ('Lasso', LassoCV()),
+              ('AdaBoost', AdaBoostRegressor(random_state=0))]
+stacking_regressor = StackingRegressor(
+    estimators=estimators, final_estimator=RidgeCV()
+)
+
 
 ###############################################################################
 # We used the Boston data set (prediction of house prices). We check the
 # performance of each individual predictor as well as the stack of the
 # regressors.
 
+import numpy as np
 from sklearn.datasets import load_boston
 from sklearn.model_selection import cross_validate, cross_val_predict
 
@@ -100,3 +106,7 @@ plt.suptitle('Single predictors versus stacked predictors')
 plt.tight_layout()
 plt.subplots_adjust(top=0.9)
 plt.show()
+
+###############################################################################
+# The stacked regressor will discard the `KNearestNeighborsRegressor` and
+# combine the strength of `LassoCV` and `AdaBoostRegressor`.
