ab9d0db450cbc6742726d23c6e6af643eed4ee40
Thomas Fan <thomasjpfan@gmail.com>
2019-09-12 11:02:02 -0400

    BLD [doc build] [scipy-dev]

--- build_tools/circle/build_doc.sh
+++ build_tools/circle/build_doc.sh
@@ -135,7 +135,7 @@ conda create -n $CONDA_ENV_NAME --yes --quiet python="${PYTHON_VERSION:-*}" \
 source activate testenv
 # TODO: Remove openssl ssl fix. This is a temporary fix to get `fetch_*`
 # to work on the CI.
-conda install -c rdonnelly openssl
+conda install -c rdonnelly --yes openssl
 
 pip install sphinx-gallery==0.3.1
 pip install numpydoc==0.9
