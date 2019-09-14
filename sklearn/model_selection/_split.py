f03b56ab58f664ede33e277c039492e87be4f981
DrGFreeman <DrGFreeman@users.noreply.github.com>
2019-09-14 10:47:05 -0400

    Get values of parameters in cvargs attribute (#14970)
    Modified _build_repr to include values of parameters stored in _RepeatedSplit class attributes.

--- sklearn/model_selection/_split.py
+++ sklearn/model_selection/_split.py
@@ -2161,6 +2161,8 @@ def _build_repr(self):
         try:
             with warnings.catch_warnings(record=True) as w:
                 value = getattr(self, key, None)
+                if value is None and hasattr(self, 'cvargs'):
+                    value = self.cvargs.get(key, None)
             if len(w) and w[0].category == DeprecationWarning:
                 # if the parameter is deprecated, don't show it
                 continue
