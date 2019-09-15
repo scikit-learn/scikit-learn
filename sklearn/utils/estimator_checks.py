9ecc396079fa43f887a62414578bbddec59c130d
Nicolas Hug <contact@nicolas-hug.com>
2019-09-15 18:24:33 -0400

    remove print

--- sklearn/utils/estimator_checks.py
+++ sklearn/utils/estimator_checks.py
@@ -2671,7 +2671,6 @@ def check_n_features_in(name, estimator_orig):
     # Make sure that n_features_in_ attribute doesn't exist until fit is
     # called, and that its value is correct.
 
-    print(name)
     rng = np.random.RandomState(0)
 
     estimator = clone(estimator_orig)
