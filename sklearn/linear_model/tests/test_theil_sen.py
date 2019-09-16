060e1f179682c84b5697ac16ac368fca6099ca3e
Samesh Lakhotia <samesh.lakhotia@gmail.com>
2019-09-16 14:50:55 +0530

    fix assert_raises in test_theil_sen.py

--- sklearn/linear_model/tests/test_theil_sen.py
+++ sklearn/linear_model/tests/test_theil_sen.py
@@ -6,6 +6,7 @@ Testing for Theil-Sen module (sklearn.linear_model.theil_sen)
 # License: BSD 3 clause
 import os
 import sys
+import pytest
 from contextlib import contextmanager
 import numpy as np
 from numpy.testing import assert_array_equal, assert_array_less
@@ -16,7 +17,7 @@ from sklearn.exceptions import ConvergenceWarning
 from sklearn.linear_model import LinearRegression, TheilSenRegressor
 from sklearn.linear_model.theil_sen import _spatial_median, _breakdown_point
 from sklearn.linear_model.theil_sen import _modified_weiszfeld_step
-from sklearn.utils.testing import assert_almost_equal, assert_raises
+from sklearn.utils.testing import assert_almost_equal
 
 
 @contextmanager
@@ -200,19 +201,22 @@ def test_calc_breakdown_point():
 def test_checksubparams_negative_subpopulation():
     X, y, w, c = gen_toy_problem_1d()
     theil_sen = TheilSenRegressor(max_subpopulation=-1, random_state=0)
-    assert_raises(ValueError, theil_sen.fit, X, y)
+    with pytest.raises(ValueError):
+        theil_sen.fit(X, y)
 
 
 def test_checksubparams_too_few_subsamples():
     X, y, w, c = gen_toy_problem_1d()
     theil_sen = TheilSenRegressor(n_subsamples=1, random_state=0)
-    assert_raises(ValueError, theil_sen.fit, X, y)
+    with pytest.raises(ValueError):
+        theil_sen.fit(X, y)
 
 
 def test_checksubparams_too_many_subsamples():
     X, y, w, c = gen_toy_problem_1d()
     theil_sen = TheilSenRegressor(n_subsamples=101, random_state=0)
-    assert_raises(ValueError, theil_sen.fit, X, y)
+    with pytest.raises(ValueError):
+        theil_sen.fit(X, y)
 
 
 def test_checksubparams_n_subsamples_if_less_samples_than_features():
@@ -221,7 +225,8 @@ def test_checksubparams_n_subsamples_if_less_samples_than_features():
     X = random_state.normal(size=(n_samples, n_features))
     y = random_state.normal(size=n_samples)
     theil_sen = TheilSenRegressor(n_subsamples=9, random_state=0)
-    assert_raises(ValueError, theil_sen.fit, X, y)
+    with pytest.raises(ValueError):
+        theil_sen.fit(X, y)
 
 
 def test_subpopulation():
