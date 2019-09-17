f1231f55f29e9171c8421d3fc8c02dcc8ec068f2
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2019-09-17 17:00:20 +0200

    improve docstring

--- sklearn/utils/openmp_helpers.pyx
+++ sklearn/utils/openmp_helpers.pyx
@@ -7,13 +7,14 @@ cpdef _openmp_effective_n_threads(n_threads=None):
     """Determine the effective number of threads used for parallel OpenMP calls
 
     - For ``n_threads = None``, returns the minimum between
-      openmp.omp_get_max_threads() and joblib.effective_n_jobs(-1).
+      ``openmp.omp_get_max_threads()`` and ``joblib.cpu_count()``.
       The result of ``omp_get_max_threads`` can be influenced by environment
       variable ``OMP_NUM_THREADS`` or at runtime by ``omp_set_num_threads``.
     - For ``n_threads > 0``, use this as the maximal number of threads for
       parallel OpenMP calls.
     - For ``n_threads < 0``, use the maximal number of threads minus
-      ``|n_threads + 1|``.
+      ``|n_threads + 1|``. In particular ``n_threads=-1`` will use as many
+      threads as there are available cores on the machine.
     - Raise a ValueError for ``n_threads = 0``.
 
     If scikit-learn is built without OpenMP support, always return 1.
