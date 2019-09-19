333be5aad01b8d3ee5d0a0490d5fdff37010d702
Johann Faouzi <johann.faouzi@gmail.com>
2019-09-19 08:43:41 +0200

    Update seed testing

--- sklearn/ensemble/_hist_gradient_boosting/tests/test_warm_start.py
+++ sklearn/ensemble/_hist_gradient_boosting/tests/test_warm_start.py
@@ -154,13 +154,15 @@ def test_warm_start_clear(GradientBoosting, X, y):
     (HistGradientBoostingClassifier, X_classification, y_classification),
     (HistGradientBoostingRegressor, X_regression, y_regression)
 ])
-@pytest.mark.parametrize('rng_type', ('int', 'instance'))
+@pytest.mark.parametrize('rng_type', ('none', 'int', 'instance'))
 def test_random_seeds_warm_start(GradientBoosting, X, y, rng_type):
     # Make sure the seeds for train/val split and small trainset subsampling
     # are correctly set in a warm start context.
     def _get_rng(rng_type):
         # Helper to avoid consuming rngs
-        if rng_type == 'int':
+        if rng_type == 'none':
+            return None
+        elif rng_type == 'int':
             return 42
         else:
             return np.random.RandomState(0)
@@ -169,16 +171,30 @@ def test_random_seeds_warm_start(GradientBoosting, X, y, rng_type):
     gb_1 = GradientBoosting(n_iter_no_change=5, max_iter=2,
                             random_state=random_state)
     gb_1.fit(X, y)
-    random_seed_1 = gb_1._random_seed
+    random_seed_1_1 = gb_1._random_seed
+
+    gb_1.fit(X, y)
+    random_seed_1_2 = gb_1._random_seed  # clear the old state, different seed
 
     random_state = _get_rng(rng_type)
     gb_2 = GradientBoosting(n_iter_no_change=5, max_iter=2,
                             random_state=random_state, warm_start=True)
     gb_2.fit(X, y)  # inits state
-    random_seed_2 = gb_2._random_seed
+    random_seed_2_1 = gb_2._random_seed
     gb_2.fit(X, y)  # clears old state and equals est
-    random_seed_3 = gb_2._random_seed
+    random_seed_2_2 = gb_2._random_seed
 
-    # Check that all seeds are equal
-    assert random_seed_1 == random_seed_2
-    assert random_seed_2 == random_seed_3
+    # Without warm starting, the seeds should be
+    # * all different if random state is None
+    # * all equal if random state is an integer
+    # * different when refitting and equal with a new estimator (because
+    #   the random state is mutated)
+    if rng_type == 'none':
+        assert random_seed_1_1 != random_seed_1_2 != random_seed_2_1
+    elif rng_type == 'int':
+        assert random_seed_1_1 == random_seed_1_2 == random_seed_2_1
+    else:
+        assert random_seed_1_1 == random_seed_2_1 != random_seed_1_2
+
+    # With warm starting, the seeds must be equal
+    assert random_seed_2_1 == random_seed_2_2
