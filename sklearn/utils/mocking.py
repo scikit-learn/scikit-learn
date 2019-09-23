763ce3ae08dcfe4c79f4ca584d4c8f487500411e
Thomas Fan <thomasjpfan@gmail.com>
2019-09-23 16:20:24 -0400

    ENH Remove mocking

--- sklearn/utils/mocking.py
+++
@@ -1,7 +0,0 @@
-from ._mocking import *  # noqa
-from ..utils.deprecated import _raise_dep_warning_if_not_pytest
-
-deprecated_path = 'sklearn.utils.mocking'
-correct_path = 'sklearn.utils'
-
-_raise_dep_warning_if_not_pytest(deprecated_path, correct_path)
