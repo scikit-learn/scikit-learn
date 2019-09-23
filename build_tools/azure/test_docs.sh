1af99be28b5ea023fa5d02316abaa9c1e762a297
adrinjalali <adrin.jalali@gmail.com>
2019-09-23 16:48:34 +0200

    install scikit-image if we test doc on azure

--- build_tools/azure/test_docs.sh
+++ build_tools/azure/test_docs.sh
@@ -4,8 +4,10 @@ set -e
 
 if [[ "$DISTRIB" =~ ^conda.* ]]; then
     source activate $VIRTUALENV
+    conda install scikit-image -y
 elif [[ "$DISTRIB" == "ubuntu" ]]; then
     source $VIRTUALENV/bin/activate
+    pip install scikit-image
 fi
 
 make test-doc
