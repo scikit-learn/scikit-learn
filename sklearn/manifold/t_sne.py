2c2b88d2dc3759fa82a34a336b0ec8d83e4dae04
Roman Yurchak <rth.yurchak@gmail.com>
2019-09-24 17:09:04 +0200

    Lint

--- sklearn/manifold/t_sne.py
+++ sklearn/manifold/t_sne.py
@@ -627,7 +627,8 @@ class TSNE(BaseEstimator):
                  early_exaggeration=12.0, learning_rate=200.0, n_iter=1000,
                  n_iter_without_progress=300, min_grad_norm=1e-7,
                  metric="euclidean", init="random", verbose=0,
-                 random_state=None, method='barnes_hut', angle=0.5, n_jobs=None):
+                 random_state=None, method='barnes_hut', angle=0.5,
+                 n_jobs=None):
         self.n_components = n_components
         self.perplexity = perplexity
         self.early_exaggeration = early_exaggeration
