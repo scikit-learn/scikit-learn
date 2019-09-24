8eabe3b4674aa7061c977c9590d55dcf12d5881a
Rok <rok@mihevc.org>
2019-09-24 18:09:01 +0200

    Review feedback.

--- sklearn/utils/estimator_checks.py
+++ sklearn/utils/estimator_checks.py
@@ -1500,25 +1500,29 @@ def check_estimators_partial_fit_n_features(name, estimator_orig):
 
 @ignore_warnings(category=(DeprecationWarning, FutureWarning))
 def check_classifier_multioutput(name, estimator):
-    n_samples, n_labels = 42, 5
+    n_samples, n_labels, n_classes = 42, 5, 3
     tags = _safe_tags(estimator)
     estimator = clone(estimator)
     X, y = make_multilabel_classification(random_state=42,
                                           n_samples=n_samples,
-                                          n_labels=n_labels)
+                                          n_labels=n_labels,
+                                          n_classes=n_classes)
     estimator.fit(X, y)
     y_pred = estimator.predict(X)
 
-    assert (y_pred.shape == (n_samples, n_labels),
-            "The shape of the prediction for multioutput data is "
-            " incorrect. Expected {}, got {}."
-            .format((n_samples, n_labels), y_pred.shape))
+    assert y_pred.shape == (n_samples, n_classes), (
+        "The shape of the prediction for multioutput data is "
+        "incorrect. Expected {}, got {}."
+        .format((n_samples, n_labels), y_pred.shape))
     assert y_pred.dtype.kind == 'i'
 
     if hasattr(estimator, "decision_function"):
         decision = estimator.decision_function(X)
         assert isinstance(decision, np.ndarray)
-        assert decision.shape == (n_samples, n_labels)
+        assert decision.shape == (n_samples, n_classes), (
+            "The shape of the decision function output for "
+            "multioutput data is incorrect. Expected {}, got {}."
+            .format((n_samples, n_classes), decision.shape))
 
         dec_pred = (decision > 0).astype(np.int)
         dec_exp = estimator.classes_[dec_pred]
@@ -1528,19 +1532,25 @@ def check_classifier_multioutput(name, estimator):
         y_prob = estimator.predict_proba(X)
 
         if isinstance(y_prob, list) and not tags['poor_score']:
-            for i in range(n_labels):
-                y_prob[i].shape == (n_samples, n_labels)
+            for i in range(n_classes):
+                assert y_prob[i].shape == (n_samples, 2), (
+                    "The shape of the probability for multioutput data is"
+                    " incorrect. Expected {}, got {}."
+                    .format((n_samples, 2), y_prob[i].shape))
                 assert_array_equal(
                     np.argmax(y_prob[i], axis=1).astype(np.int),
                     y_pred[:, i]
                 )
         elif not tags['poor_score']:
-            y_prob.shape == (n_samples, n_labels)
+            assert y_prob.shape == (n_samples, n_classes), (
+                "The shape of the probability for multioutput data is"
+                " incorrect. Expected {}, got {}."
+                .format((n_samples, n_classes), y_prob.shape))
             assert_array_equal(y_prob.round().astype(int), y_pred)
 
     if (hasattr(estimator, "decision_function") and
             hasattr(estimator, "predict_proba")):
-        for i in range(n_labels):
+        for i in range(n_classes):
             y_proba = estimator.predict_proba(X)[:, i]
             y_decision = estimator.decision_function(X)
             assert_array_equal(rankdata(y_proba), rankdata(y_decision[:, i]))
@@ -1561,8 +1571,12 @@ def check_regressor_multioutput(name, estimator):
     estimator.fit(X, y)
     y_pred = estimator.predict(X)
 
-    assert y_pred.dtype == np.dtype('float')
-    assert y_pred.shape == y.shape
+    assert y_pred.dtype == np.dtype('float64'), (
+        "Multioutput predictions by a regressor are expected to be"
+        " floating-point precision. Got {} instead".format(y_pred.dtype))
+    assert y_pred.shape == y.shape, (
+        "The shape of the orediction for multioutput data is incorrect."
+        " Expected {}, got {}.")
 
 
 @ignore_warnings(category=(DeprecationWarning, FutureWarning))
