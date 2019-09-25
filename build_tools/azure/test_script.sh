d6e1e493a9fbfca08bbf22cff184d2195bc5e29a
Nicolas Hug <contact@nicolas-hug.com>
2019-09-25 10:38:37 -0400

    removed -Werror

--- build_tools/azure/test_script.sh
+++ build_tools/azure/test_script.sh
@@ -29,7 +29,7 @@ if [[ "$COVERAGE" == "true" ]]; then
 fi
 
 if [[ -n "$CHECK_WARNINGS" ]]; then
-    TEST_CMD="$TEST_CMD -Werror::sklearn.exceptions.VisibleDeprecationWarning -Werror::FutureWarning"
+    TEST_CMD="$TEST_CMD -Werror::FutureWarning"
 fi
 
 if [[ "$PYTHON_VERSION" == "*" ]]; then
