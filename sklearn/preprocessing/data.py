494a12cacdbfdca7269da4fa00b8c8191a0982fa
Parth Sharma <sharmaparth17@gmail.com>
2019-09-27 22:49:41 +0530

    Update sklearn/preprocessing/data.py
    Co-Authored-By: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- sklearn/preprocessing/data.py
+++ sklearn/preprocessing/data.py
@@ -549,7 +549,7 @@ class StandardScaler(BaseEstimator, TransformerMixin):
         memory.
 
     with_std : boolean, 1 or 2, True by default
-        If 1 or True, scale the data to unit variance (or equivalently,
+        - If `1` or `True`, scale the data to unit variance (or equivalently,
         unit standard deviation);
         - otherwise `2`, scale the data to using scaling factor 2
         standard deviation. See [1] for `with_std=2`
