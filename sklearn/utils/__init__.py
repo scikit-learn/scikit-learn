359fbeb55e39be29a5e00cd22b2984d6ed0f32fd
Nicolas Hug <contact@nicolas-hug.com>
2019-09-27 13:37:57 -0400

    fixed some

--- sklearn/utils/__init__.py
+++ sklearn/utils/__init__.py
@@ -594,7 +594,7 @@ def resample(*arrays, **options):
 
     # convert sparse matrices to CSR for row-based indexing
     arrays = [a.tocsr() if issparse(a) else a for a in arrays]
-    resampled_arrays = [safe_indexing(a, indices) for a in arrays]
+    resampled_arrays = [_safe_indexing(a, indices) for a in arrays]
     if len(resampled_arrays) == 1:
         # syntactic sugar for the unit argument case
         return resampled_arrays[0]
