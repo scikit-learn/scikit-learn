5f186872ead3d1a126ba7cafe85bbfecc62383cd
Thomas J Fan <thomasjpfan@gmail.com>
2019-10-01 21:20:04 -0400

    ENH Updates Makefile to clean generated files

--- sklearn/_build_utils/deprecated_modules.py
+++ sklearn/_build_utils/deprecated_modules.py
@@ -1,22 +1,29 @@
 """Generates submodule to allow deprecation of submodules and keeping git
 blame."""
 from pathlib import Path
+from contextlib import suppress
 
 # This is a set of 3-tuples consisting of
-# (new_module_path, deprecated_path, correct_path)
+# (new_module_name, deprecated_path, correct_import_path)
 _DEPRECATED_MODULES = {
     # TODO: Remove in 0.24
     ('_mocking', 'sklearn.utils.mocking', 'sklearn.utils')
 }
 
-_FILE_CONTENT_TEMPLATE = """from .{new_module_path} import *  # noqa
+_FILE_CONTENT_TEMPLATE = """from .{new_module_name} import *  # noqa
 from {relative_dots}utils.deprecation import _raise_dep_warning_if_not_pytest
 
 deprecated_path = '{deprecated_path}'
-correct_path = '{correct_path}'
+correct_import_path = '{correct_import_path}'
 
-_raise_dep_warning_if_not_pytest(deprecated_path, correct_path)
+_raise_dep_warning_if_not_pytest(deprecated_path, correct_import_path)
 """
+
+
+def _get_deprecated_path(deprecated_path):
+    deprecated_parts = deprecated_path.split(".")
+    deprecated_parts[-1] = deprecated_parts[-1] + ".py"
+    return Path(*deprecated_parts)
 
 
 def _create_deprecated_modules_files():
@@ -24,15 +31,25 @@ def _create_deprecated_modules_files():
     on the deprecated submodule's name. When this submodule is imported a
     deprecation warning will be raised.
     """
-    for new_module_path, deprecated_path, correct_path in _DEPRECATED_MODULES:
+    for (new_module_name, deprecated_path,
+         correct_import_path) in _DEPRECATED_MODULES:
         relative_dots = deprecated_path.count(".") * "."
         deprecated_content = _FILE_CONTENT_TEMPLATE.format(
-            new_module_path=new_module_path,
+            new_module_name=new_module_name,
             relative_dots=relative_dots,
             deprecated_path=deprecated_path,
-            correct_path=correct_path)
-        deprecated_parts = deprecated_path.split(".")
-        deprecated_parts[-1] = deprecated_parts[-1] + ".py"
+            correct_import_path=correct_import_path)
 
-        with Path(*deprecated_parts).open('w') as f:
+        with _get_deprecated_path(deprecated_path).open('w') as f:
             f.write(deprecated_content)
+
+
+def _clean_deprecated_modules_files():
+    """Removes submodules created by _create_deprecated_modules_files."""
+    for (_, deprecated_path, _) in _DEPRECATED_MODULES:
+        with suppress(FileNotFoundError):
+            _get_deprecated_path(deprecated_path).unlink()
+
+
+if __name__ == "__main__":
+    _clean_deprecated_modules_files()
