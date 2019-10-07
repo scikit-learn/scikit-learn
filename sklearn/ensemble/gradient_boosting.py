1ec0128b7921cbad4f8e3bfe44209b465ab5346c
Philipp Spachtholz <pspachtholz@gmx.de>
2019-10-07 23:04:16 +0200

    Add examples to GradientBoosting classifier and regressor

--- sklearn/ensemble/gradient_boosting.py
+++ sklearn/ensemble/gradient_boosting.py
@@ -2041,6 +2041,34 @@ shape (n_estimators, ``loss_.K``)
     split. To obtain a deterministic behaviour during fitting,
     ``random_state`` has to be fixed.
 
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from sklearn.datasets import make_classification
+    >>> from sklearn.ensemble import GradientBoostingClassifier
+    >>> from sklearn.model_selection import train_test_split
+    >>> X, y = make_classification(n_samples=20, random_state=22)
+    >>> X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=5,
+    ...                                                     random_state=22)
+    >>> clf = GradientBoostingClassifier(n_estimators=2, max_depth=1,
+    ...                                  warm_start=True, random_state=22)
+    >>> clf.fit(X_train, y_train)
+    GradientBoostingClassifier(max_depth=1, n_estimators=2, random_state=22,
+                               warm_start=True)
+    >>> clf.predict(X_test)
+    array([0, 1, 0, 1, 1])
+    >>> clf.score(X_test, y_test)
+    0.8
+    >>> clf.set_params(n_estimators=4).fit(X_train, y_train)  # add 2 stages
+    GradientBoostingClassifier(max_depth=1, n_estimators=4, random_state=22,
+                               warm_start=True)
+    >>> np.array(list(clf.staged_predict(X_test)))
+    array([[0, 0, 0, 0, 0],
+           [0, 1, 0, 1, 1],
+           [0, 1, 0, 1, 1],
+           [1, 1, 0, 1, 1]])
+
+
     See also
     --------
     sklearn.ensemble.HistGradientBoostingClassifier,
@@ -2504,6 +2532,35 @@ class GradientBoostingRegressor(RegressorMixin, BaseGradientBoosting):
     identical for several splits enumerated during the search of the best
     split. To obtain a deterministic behaviour during fitting,
     ``random_state`` has to be fixed.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from sklearn.datasets import make_regression
+    >>> from sklearn.ensemble import GradientBoostingRegressor
+    >>> from sklearn.model_selection import train_test_split
+    >>> X, y = make_regression(n_samples=12, random_state=22)
+    >>> X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=3,
+    ...                                                     random_state=22)
+    >>> reg = GradientBoostingRegressor(n_estimators=2, max_depth=1,
+    ...                                 warm_start=True, random_state=22)
+    >>> reg.fit(X_train, y_train)
+    GradientBoostingRegressor(max_depth=1, n_estimators=2, random_state=22,
+                              warm_start=True)
+    >>> reg.predict(X_test)
+    array([-14.15181029, -64.81580953, -14.15181029])
+    >>> reg.score(X_test, y_test)
+    0.10...
+    >>> reg.set_params(n_estimators=4).fit(X_train, y_train)  # add 2 stages
+    GradientBoostingRegressor(max_depth=1, n_estimators=4, random_state=22,
+                              warm_start=True)
+    >>> np.array(list(reg.staged_predict(X_test)))
+    array([[-24.8179154 , -51.48317815, -24.8179154 ],
+           [-14.15181029, -64.81580953, -14.15181029],
+           [ -4.5523157 , -76.81517777,  -4.5523157 ],
+           [  4.08722943, -87.61460918,   4.08722943]])
+    >>> reg.score(X_test, y_test)
+    0.36...
 
     See also
     --------
