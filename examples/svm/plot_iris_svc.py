228b8787245065739348c445e0ce18df63b0e5fe
Kalathiappan K <31460778+harish988@users.noreply.github.com>
2019-10-10 10:05:12 +0530

    Update plot_iris_svc.py

--- examples/svm/plot_iris_svc.py
+++ examples/svm/plot_iris_svc.py
@@ -89,7 +89,7 @@ C = 1.0  # SVM regularization parameter
 models = (svm.SVC(kernel='linear', C=C),
           svm.LinearSVC(C=C, max_iter=10000),
           svm.SVC(kernel='rbf', gamma=0.7, C=C),
-          (svm.SVC(kernel='sigmoid', C=C),
+          svm.SVC(kernel='sigmoid', C=C),
           svm.SVC(kernel='poly', degree=3, gamma='auto', C=C))
 models = (clf.fit(X, y) for clf in models)
 
