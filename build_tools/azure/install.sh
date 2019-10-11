c8fc3a2e1fb9e99fa93abc405c83cbcd846664ac
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2019-10-11 14:46:16 +0200

    macOS CI job without OpenMP

--- build_tools/azure/install.sh
+++ build_tools/azure/install.sh
@@ -4,7 +4,7 @@ set -e
 
 UNAMESTR=`uname`
 
-if [[ "$UNAMESTR" == "Darwin" ]]; then
+if [[ "$UNAMESTR" == "Darwin" ]] && [[ ! -z "$USE_OPENMP" ]]; then
     # install OpenMP not present by default on osx
     HOMEBREW_NO_AUTO_UPDATE=1 brew install libomp
 
