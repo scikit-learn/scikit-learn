b30ab8af441d5289ac1ae7da39f3347fa08cef70
federicopisanu <56429453+federicopisanu@users.noreply.github.com>
2019-10-12 13:05:59 +0200

    fix for CircleCI test

--- sklearn/feature_extraction/text.py
+++ sklearn/feature_extraction/text.py
@@ -1341,7 +1341,7 @@ class TfidfTransformer(TransformerMixin, BaseEstimator):
     idf_ : array, shape (n_features)
         The inverse document frequency (IDF) vector; only defined
         if  ``use_idf`` is True.
-        
+
     Examples
     --------
     >>> from sklearn.feature_extraction.text import TfidfTransformer
@@ -1369,7 +1369,6 @@ class TfidfTransformer(TransformerMixin, BaseEstimator):
     >>> sparse_tfidf_matrix = tfidftransformer.transform(count_matrix)
     >>> sparse_tfidf_matrix.shape
     (4, 8)
-
 
     References
     ----------
