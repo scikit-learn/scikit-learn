90c64c1455c9ebc86e30a53436803145e847eb1b
Simon Weiss <simon.weiss@alexanderthamm.com>
2019-10-13 15:24:32 +0200

    Retrigger build pipeline

--- sklearn/ensemble/iforest.py
+++ sklearn/ensemble/iforest.py
@@ -147,7 +147,7 @@ class IsolationForest(OutlierMixin, BaseBagging):
     Examples
     --------
     >>> from sklearn.ensemble import IsolationForest
-    >>> X = [[-1.1], [0.2], [0.5], [100]]
+    >>> X = [[-1.1], [0.3], [0.5], [100]]
     >>> clf = IsolationForest(random_state=0).fit(X)
     >>> clf.predict([[0.1], [0], [90]])
     array([ 1,  1, -1])
