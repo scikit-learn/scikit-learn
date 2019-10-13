faa757dacb4fc32cedce0fb1adc176eeb6f1cd53
BenjaminBossan <b.bossan@gmail.com>
2019-10-13 11:11:18 +0200

    Add test for CheckingClassifier
    After adding the predict_proba method on CheckingClassifier, the test
    coverage decreased. Therefore, a test for predict_proba was
    added. More tests for CheckingClassifier will be added in a separate
    PR.

---
+++ sklearn/utils/tests/test_mocking.py
@@ -0,0 +1,25 @@
+import pytest
+from sklearn.datasets import make_classification
+from sklearn.utils._mocking import CheckingClassifier
+
+
+X, y = make_classification(n_samples=20, random_state=42)
+
+
+@pytest.mark.foobar
+def test_check_X_on_predict_proba_success():
+    def fail(X):
+        return True
+
+    clf = CheckingClassifier(check_X=fail).fit(X, y)
+    clf.predict_proba(X)  # does not raise
+
+
+@pytest.mark.foobar
+def test_check_X_on_predict_proba_fail():
+    def fail(X):
+        return False
+
+    clf = CheckingClassifier(check_X=fail)
+    with pytest.raises(AssertionError):
+        clf.predict_proba(X)
