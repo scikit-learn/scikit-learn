deccc6150c2e3ca956ccc9f6bd6fc312320a1851
Sönke Behrends <s.behrends@math.uni-goettingen.de>
2019-10-13 17:43:14 +0200

    Fix flake8 warning

--- sklearn/datasets/base.py
+++ sklearn/datasets/base.py
@@ -848,8 +848,8 @@ def load_sample_image(image_name):
 def _pkl_filepath(*args, **kwargs):
     """Return filename for Python 3 pickles
 
-    args[-1] is expected to be the ".pkl" filename. For compatibility with older 
-    scikit-learn versions, a suffix is inserted before the extension.
+    args[-1] is expected to be the ".pkl" filename. For compatibility with
+    older scikit-learn versions, a suffix is inserted before the extension.
 
     _pkl_filepath('/path/to/folder', 'filename.pkl') returns
     '/path/to/folder/filename_py3.pkl'
