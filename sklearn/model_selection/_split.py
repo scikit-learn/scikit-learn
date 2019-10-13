2f286739bb233f90119638edd26f37d721aa4d01
Leandro Hermida <hermidal@cs.umd.edu>
2019-10-13 16:52:17 -0400

    Update split method parameter doc

--- sklearn/model_selection/_split.py
+++ sklearn/model_selection/_split.py
@@ -1809,8 +1809,13 @@ class StratifiedGroupShuffleSplit(StratifiedShuffleSplit):
             Training data, where n_samples is the number of samples
             and n_features is the number of features.
 
-        y : array-like, shape (n_samples,), optional
+            Note that providing ``y`` is sufficient to generate the splits and
+            hence ``np.zeros(n_samples)`` may be used as a placeholder for
+            ``X`` instead of actual training data.
+
+        y : array-like, shape (n_samples,)
             The target variable for supervised learning problems.
+            Stratification is done based on the y labels.
 
         groups : array-like, with shape (n_samples,)
             Group labels for the samples used while splitting the dataset into
