2b297172cf46b43905a01f3ae4663a039bcdf3c3
Nicolas Hug <contact@nicolas-hug.com>
2019-10-14 10:26:25 -0400

    put back __all__ in bicluster.py

--- sklearn/cluster/_bicluster.py
+++ sklearn/cluster/_bicluster.py
@@ -20,6 +20,10 @@ from ..utils.extmath import (make_nonnegative, randomized_svd,
 from ..utils.validation import assert_all_finite, check_array
 
 
+__all__ = ['SpectralCoclustering',
+           'SpectralBiclustering']
+
+
 def _scale_normalize(X):
     """Normalize ``X`` by scaling rows and columns independently.
 
