e661e8aa45bf8547ce98caf88d9e65cff2261d55
Benjamin Bossan <benjamin.bossan@gmail.com>
2019-10-15 20:39:10 +0200

    Address reviewer comments
    * parametrize tests where possible
    * add test for classes_ attribute
    * use pytest.approx for float comparison

--- sklearn/utils/tests/test_mocking.py
+++ sklearn/utils/tests/test_mocking.py
@@ -1,3 +1,4 @@
+from numpy.testing import assert_array_equal
 import pytest
 from sklearn.datasets import make_classification
 from sklearn.utils._mocking import CheckingClassifier
@@ -6,62 +7,52 @@ from sklearn.utils._mocking import CheckingClassifier
 X, y = make_classification(n_samples=20, random_state=42)
 
 
-def test_no_checks():
-    # check that none of the method calls raises
-    clf = CheckingClassifier().fit(X, y)
-    clf.predict(X)
-    clf.score(X, y)
+def _success(x):
+    return True
 
 
-def test_check_X_on_fit_success():
-    def success(X):
-        return True
+def _fail(x):
+    return False
 
+
+@pytest.mark.parametrize('kwargs', [
+    {},
+    {'check_X': _success},
+    {'check_y': _success},
+    {'check_X': _success, 'check_y': _success},
+])
+def test_check_on_fit_success(kwargs):
     # does not raise
-    CheckingClassifier(check_X=success).fit(X, y)
+    CheckingClassifier(**kwargs).fit(X, y)
 
 
-def test_check_X_on_fit_fail():
-    def fail(X):
-        return False
-
-    clf = CheckingClassifier(check_X=fail)
-    with pytest.raises(AssertionError):
-        clf.fit(X, y)
-
-
-def test_check_y_on_fit_success():
-    def success(y):
-        return True
-
-    # does not raise
-    CheckingClassifier(check_y=success).fit(X, y)
-
-
-def test_check_y_on_fit_fail():
-    def fail(y):
-        return False
-
-    clf = CheckingClassifier(check_y=fail)
+@pytest.mark.parametrize('kwargs', [
+    {'check_X': _fail},
+    {'check_y': _fail},
+    {'check_X': _success, 'check_y': _fail},
+    {'check_X': _fail, 'check_y': _success},
+    {'check_X': _fail, 'check_y': _fail},
+])
+def test_check_on_fit_fail(kwargs):
+    clf = CheckingClassifier(**kwargs)
     with pytest.raises(AssertionError):
         clf.fit(X, y)
 
 
 def test_check_X_on_predict_success():
-    def success(X):
-        return True
-
-    clf = CheckingClassifier(check_X=success).fit(X, y)
+    clf = CheckingClassifier(check_X=_success).fit(X, y)
     clf.predict(X)  # does not raise
 
 
 def test_check_X_on_predict_fail():
-    def fail(X):
-        return False
-
-    clf = CheckingClassifier(check_X=fail)
+    clf = CheckingClassifier(check_X=_fail)
     with pytest.raises(AssertionError):
         clf.predict(X)
+
+
+def test_classes_set_in_fit():
+    clf = CheckingClassifier().fit(X, y)
+    assert_array_equal(clf.classes_, [0, 1])
 
 
 def test_expected_fit_params_success():
@@ -92,13 +83,11 @@ def test_expected_fit_params_fails_wrong_length():
     assert exc.value.args[0] == msg
 
 
-def test_foo_param_score_1():
-    clf = CheckingClassifier(foo_param=2)
+@pytest.mark.parametrize('foo_param, expected', [
+    (2, 1.),
+    (0, 0.),
+])
+def test_foo_param_score(foo_param, expected):
+    clf = CheckingClassifier(foo_param=foo_param)
     score = clf.score(X, y)
-    assert score == 1.
-
-
-def test_foo_param_score_0():
-    clf = CheckingClassifier(foo_param=0)
-    score = clf.score(X, y)
-    assert score == 0.
+    assert score == pytest.approx(expected)
