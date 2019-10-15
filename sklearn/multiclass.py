368992a3c757a5cddc7373a73ca0163a2eee763f
Jonathan Rahn <rahn.jonathan@gmail.com>
2019-10-15 12:19:26 +0200

    Update multiclass.py

--- sklearn/multiclass.py
+++ sklearn/multiclass.py
@@ -694,7 +694,7 @@ class OutputCodeClassifier(MetaEstimatorMixin, ClassifierMixin, BaseEstimator):
     >>> from sklearn.multiclass import OutputCodeClassifier
     >>> from sklearn.ensemble import RandomForestClassifier
     >>> from sklearn.datasets import make_classification
-    >>> X, y = make_classification(n_samples=1000, n_features=4,
+    >>> X, y = make_classification(n_samples=100, n_features=4,
     ...                            n_informative=2, n_redundant=0,
     ...                            random_state=0, shuffle=False)
     >>> clf = OutputCodeClassifier(
