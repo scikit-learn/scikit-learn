73549c7b98704bab9a1543985e1b3f86fadc79f1
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2019-10-16 12:06:15 +0200

    more detailed comment

--- sklearn/utils/_openmp_helpers.pyx
+++ sklearn/utils/_openmp_helpers.pyx
@@ -1,4 +1,5 @@
 def _openmp_supported():
     """Determines whether scikit-learn has been built with OpenMP support"""
     # SKLEARN_OPENMP_SUPPORTED is resolved at compile time during cythonization
+    # The cythonize parameter `compile_time_env` works like a #define in C.
     return SKLEARN_OPENMP_SUPPORTED
