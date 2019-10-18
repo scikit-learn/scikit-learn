2017bb8cfc2f370d457e3ebe39bd3da0af73a082
Thomas J Fan <thomasjpfan@gmail.com>
2019-10-18 11:11:39 -0400

    DOC Remove comments

--- sklearn/utils/deprecation.py
+++ sklearn/utils/deprecation.py
@@ -128,9 +128,6 @@ def _raise_dep_warning_if_not_pytest(deprecated_path, correct_path):
     # Raise a deprecation warning with standardized deprecation message.
     # Useful because we are now deprecating # anything that isn't explicitly
     # in an __init__ file.
-    # We don't want to raise a dep warning if we are in a pytest session else
-    # the CIs with -Werror::DeprecationWarning would fail. The deprecations are
-    # still properly tested in sklearn/tests/test_import_deprecations.py
 
     # TODO: remove in 0.24 since this shouldn't be needed anymore.
 
