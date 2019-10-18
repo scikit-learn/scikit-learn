2cff341edf08f42253bd0821a3fc73530ae4ba6a
Jaime Ferrando Huertas <jaime.ferrando_huertas@sandvik.com>
2019-10-18 20:14:41 +0000

    Fixed whitespaces on black line

--- sklearn/cross_decomposition/tests/test_pls.py
+++ sklearn/cross_decomposition/tests/test_pls.py
@@ -78,7 +78,7 @@ def test_pls():
                               err_msg="rotation on X failed")
     assert_array_almost_equal(Yr, plsca.y_scores_,
                               err_msg="rotation on Y failed")
-    
+
     # Check that inverse_transform works
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     Xreconstructed = plsca.inverse_transform(Xr)
