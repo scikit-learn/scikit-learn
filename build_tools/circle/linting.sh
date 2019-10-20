c565e0b5924130451a711dc85018ddbf4f2fe1a7
Thomas J Fan <thomasjpfan@gmail.com>
2019-10-20 09:40:07 -0400

    CLN Address comments [doc build]

--- build_tools/circle/linting.sh
+++ build_tools/circle/linting.sh
@@ -143,3 +143,18 @@ else
         --config ./examples/.flake8
 fi
 echo -e "No problem detected by flake8\n"
+
+# For docstrings and warnings of deprecated attributes to be rendered
+# properly, the property decorator must come before the deprecated decorator
+# (else they are treated as functions)
+bad_deprecation_property_order=`git grep -A 10 "@property" | awk '/@property/,/def /' | grep -B1 "@deprecated"`
+# exclude this file from the matches
+bad_deprecation_property_order=`echo $bad_deprecation_property_order | grep -v check_deprecated_properties`
+
+if [ ! -z "$bad_deprecation_property_order" ]
+then
+    echo "property decorator should come before deprecated decorator"
+    echo "found the following occurrencies:"
+    echo $bad_deprecation_property_order
+    exit 1
+fi
