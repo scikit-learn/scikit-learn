add87dc4d612ed61e7779fda4c85355b1b5a7b23
Thomas J Fan <thomasjpfan@gmail.com>
2019-10-22 11:37:34 -0400

    REV

--- build_tools/circle/linting.sh
+++ build_tools/circle/linting.sh
@@ -150,7 +150,7 @@ echo -e "No problem detected by flake8\n"
 
 # do not error when grep -B1 "@property" finds nothing
 set +e
-bad_deprecation_property_order=`git grep -A 10 "@property" -- '*.py' | awk '/@deprecated/,/def /' | grep -B1 "@property"`
+bad_deprecation_property_order=`git grep -A 10 "@property"  -- "*.py" | awk '/@property/,/def /' | grep -B1 "@deprecated"`
 
 if [ ! -z "$bad_deprecation_property_order" ]
 then
