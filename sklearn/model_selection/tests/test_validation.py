3dc67eb7f2ccb3c9b3c86e33280a3192f85436ac
Nicolas Hug <contact@nicolas-hug.com>
2019-10-23 10:40:21 -0400

    fixed some more

--- sklearn/model_selection/tests/test_validation.py
+++ sklearn/model_selection/tests/test_validation.py
@@ -52,7 +52,7 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import precision_recall_fscore_support
 from sklearn.metrics import precision_score
 from sklearn.metrics import r2_score
-from sklearn.metrics.scorer import check_scoring
+from sklearn.metrics check_scoring
 
 from sklearn.linear_model import Ridge, LogisticRegression, SGDClassifier
 from sklearn.linear_model import PassiveAggressiveClassifier, RidgeClassifier
