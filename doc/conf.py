3e3acd0377827420a64b5f3c9ae66b552a597203
Thomas J Fan <thomasjpfan@gmail.com>
2019-10-25 11:48:05 -0400

    DOC Fix broken link to docs [doc build]

--- doc/conf.py
+++ doc/conf.py
@@ -171,7 +171,8 @@ html_static_path = ['images']
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
-html_additional_pages = {'index': 'index.html'}
+html_additional_pages = {'index': 'index.html',
+                         'documentation': 'documentation.html'}
 
 # If false, no module index is generated.
 html_domain_indices = False
