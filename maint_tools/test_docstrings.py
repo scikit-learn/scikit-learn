53b2578764924d9ba5e1b7a1ae34b6f7bfb45ee0
lbfin <lpmb_8@hotmail.com>
2019-11-02 19:12:55 +0000

    DOC Improve RidgeClassifier docstring with numpydoc (#15466)

--- maint_tools/test_docstrings.py
+++ maint_tools/test_docstrings.py
@@ -20,6 +20,7 @@ DOCSTRING_WHITELIST = [
     "PassiveAggressiveClassifier.decision_function",
     "Perceptron.decision_function",
     "RidgeClassifier.decision_function",
+    "RidgeClassifier.fit",
     "RidgeClassifierCV.decision_function",
     "SGDClassifier.decision_function",
     "KernelDensity",
