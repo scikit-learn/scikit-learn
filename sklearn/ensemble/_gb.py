440f318009449ab311578a5f0585b427d985115c
Matteo Visconti dOC <mvdoc@berkeley.edu>
2019-11-02 13:26:51 -0700

    FIX doctest

--- sklearn/ensemble/_gb.py
+++ sklearn/ensemble/_gb.py
@@ -2552,10 +2552,17 @@ class GradientBoostingRegressor(RegressorMixin, BaseGradientBoosting):
 
     Examples
     --------
+    >>> from sklearn.metrics import mean_squared_error
+    >>> from sklearn.datasets import make_friedman1
     >>> from sklearn.ensemble import GradientBoostingRegressor
-    >>> GradientBoostingRegressor(n_estimators=100, learning_rate=0.1,
-    ...     max_depth=1, random_state=0, loss='ls')
-    GradientBoostingRegressor(max_depth=1, random_state=0)
+
+    >>> X, y = make_friedman1(n_samples=1200, random_state=0, noise=1.0)
+    >>> X_train, X_test = X[:200], X[200:]
+    >>> y_train, y_test = y[:200], y[200:]
+    >>> est = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1,
+    ...     max_depth=1, random_state=0, loss='ls').fit(X_train, y_train)
+    >>> mean_squared_error(y_test, est.predict(X_test))
+    5.00...
     """
 
     _SUPPORTED_LOSS = ('ls', 'lad', 'huber', 'quantile')
