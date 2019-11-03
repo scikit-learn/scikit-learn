58ee3531fecf610783f7f68e2a6f62b5907812a9
louis <@>
2019-11-03 00:53:04 +0000

    black formatting to address lint issues.

--- sklearn/neighbors/_unsupervised.py
+++ sklearn/neighbors/_unsupervised.py
@@ -93,8 +93,7 @@ class NearestNeighbors(
       >>> neigh.kneighbors([[0, 0, 1.3]], 2, return_distance=False)
       array([[2, 0]]...)
 
-      >>> nbrs = neigh.radius_neighbors([[0, 0, 1.3]], 0.4,
-                                        return_distance=False)
+      >>> nbrs = neigh.radius_neighbors([[0, 0, 1.3]], 0.4, return_distance=False)
       >>> np.asarray(nbrs[0][0])
       array(2)
     """
