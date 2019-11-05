58bb3ac613b5a617717d8bcb8266d2f0ce38550b
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2019-11-05 10:32:23 +0100

    remove test_warning_init

--- sklearn/tests/test_init.py
+++ sklearn/tests/test_init.py
@@ -5,16 +5,11 @@ __author__ = 'Yaroslav Halchenko'
 __license__ = 'BSD'
 
 
-import pytest
-
 try:
     from sklearn import *  # noqa
     _top_import_error = None
 except Exception as e:
     _top_import_error = e
-
-from sklearn.utils.testing import assert_run_python_script
-from sklearn.utils._openmp_helpers import _openmp_supported
 
 
 def test_import_skl():
@@ -22,16 +17,3 @@ def test_import_skl():
     # "import *" is discouraged outside of the module level, hence we
     # rely on setting up the variable above
     assert _top_import_error is None
-
-
-def test_init_openmp_warning():
-    # Check that a warning is printed when sklearn has been built without
-    # OpenMP
-    if _openmp_supported():
-        assert_run_python_script("import sklearn", ignore_init_warnings=False)
-    else:
-        with pytest.raises(AssertionError,
-                           match="Scikit-learn has been built"
-                                 " without OpenMP support"):
-            assert_run_python_script("import sklearn",
-                                     ignore_init_warnings=False)
