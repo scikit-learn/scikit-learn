ed0c937dbbb9c1bffd1f9d71191f104c7f69d14d
Will Price <will.price94@gmail.com>
2019-11-05 21:49:56 +0000

    Rename top_k_accuracy_score parameters to be consistent with sklearn

--- sklearn/metrics/_classification.py
+++ sklearn/metrics/_classification.py
@@ -193,35 +193,34 @@ def accuracy_score(y_true, y_pred, normalize=True, sample_weight=None):
     return _weighted_sum(score, sample_weight, normalize)
 
 
-def top_k_accuracy_score(y_true, y_pred, k=5, normalize=True):
-    """Top k Accuracy classification score.
+def top_k_accuracy_score(y_true, y_score, k=5, normalize=True):
+    """Top-k Accuracy classification score.
     For multiclass classification tasks, this metric returns the
-    number of times that the correct class was among the top k classes
+    number of times that the correct class was among the top-k classes
     predicted.
 
     Parameters
     ----------
-    y_true : 1d array-like, or class indicator array / sparse matrix
-        shape num_samples or [num_samples, num_classes]
+    y_true : 1d array-like, or class indicator array / sparse matrix,
+        shape num_samples or [n_samples, n_classes]
         Ground truth (correct) classes.
 
-    y_pred : array-like, shape [num_samples, num_classes]
-        For each sample, each row represents the
-        likelihood of each possible class.
-        The number of columns must be at least as large as the set of possible
-        classes.
-    k : int, optional (default=5) predictions are counted as correct if
-        probability of correct class is in the top k classes.
+    y_score : 2d array-like, shape [n_samples, n_classes]
+        For each sample, each row represents the likelihood of each possible class.
+        The number of columns must be equal to number of classes.
+    k : int, optional (default=5)
+        Predictions are counted as correct if probability of correct class is in the
+        top-k classes.
 
     normalize : bool, optional (default=True)
-        If ``False``, return the number of top k correctly classified samples.
-        Otherwise, return the fraction of top k correctly classified samples.
+        If ``False``, return the number of top-k correctly classified samples.
+        Otherwise, return the fraction of top-k correctly classified samples.
 
     Returns
     -------
     score : float
-        If ``normalize == True``, return the proportion of top k correctly
-        classified samples, (float), else it returns the number of top k
+        If ``normalize == True``, return the proportion of top-k correctly
+        classified samples, (float), else it returns the number of top-k
         correctly classified samples (int.)
 
         The best performance is 1 with ``normalize == True`` and the number
@@ -246,32 +245,32 @@ def top_k_accuracy_score(y_true, y_pred, k=5, normalize=True):
     --------
     >>> import numpy as np
     >>> from sklearn.metrics import top_k_accuracy_score
-    >>> y_pred = np.array([[0.1, 0.3, 0.4, 0.2],
+    >>> y_scores = np.array([[0.1, 0.3, 0.4, 0.2],
     ...                    [0.4, 0.3, 0.2, 0.1],
     ...                    [0.2, 0.3, 0.4, 0.1],
     ...                    [0.8, 0.1, 0.025, 0.075]])
     >>> y_true = np.array([2, 2, 2, 1])
-    >>> top_k_accuracy_score(y_true, y_pred, k=1)
+    >>> top_k_accuracy_score(y_true, y_scores, k=1)
     0.5
-    >>> top_k_accuracy_score(y_true, y_pred, k=2)
+    >>> top_k_accuracy_score(y_true, y_scores, k=2)
     0.75
-    >>> top_k_accuracy_score(y_true, y_pred, k=3)
+    >>> top_k_accuracy_score(y_true, y_scores, k=3)
     1.0
-    >>> top_k_accuracy_score(y_true, y_pred, k=2, normalize=False)
+    >>> top_k_accuracy_score(y_true, y_scores, k=2, normalize=False)
     3
     """
     if len(y_true.shape) == 2:
         y_true = np.argmax(y_true, axis=1)
 
-    num_obs, num_labels = y_pred.shape
-    idx = num_labels - k - 1
+    n_samples, n_labels = y_score.shape
+    idx = n_labels - k - 1
     counter = 0
-    argsorted = np.argsort(y_pred, axis=1)
-    for i in range(num_obs):
+    argsorted = np.argsort(y_score, axis=1)
+    for i in range(n_samples):
         if y_true[i] in argsorted[i, idx+1:]:
             counter += 1
     if normalize:
-        return counter / num_obs
+        return counter / n_samples
     else:
         return counter
 
