365e9fd1ad2b7cfbe3ff105bf863a9ca1f94566a
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2019-11-05 10:47:13 +0100

    add test sklearn built with openmp support

---
+++ sklearn/utils/tests/test_openmp_helpers.py
@@ -0,0 +1,14 @@
+import os
+import pytest
+
+from sklearn.utils._openmp_helpers import _openmp_supported
+
+
+def test_openmp_supported():
+    # Check that sklearn is built with OpenMP supported.
+    # This test can be disabled by setting the environment variable
+    # ``SKLEARN_SKIP_OPENMP_TEST``.
+    if os.getenv("SKLEARN_SKIP_OPENMP_TEST"):
+        pytest.skip("test explicitly disabled (SKLEARN_SKIP_OPENMP_TEST)")
+
+    assert _openmp_supported()
