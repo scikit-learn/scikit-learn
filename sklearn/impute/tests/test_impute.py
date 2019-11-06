c96e0958da46ebef482a4084cdda3285d5f5ad23
Venkatachalam N <venky.yuvy@gmail.com>
2019-11-07 02:09:05 +0530

    FIX a bug where ascending and descending were inverted (#15393)

--- sklearn/impute/tests/test_impute.py
+++ sklearn/impute/tests/test_impute.py
@@ -26,6 +26,7 @@ from sklearn.pipeline import make_union
 from sklearn.model_selection import GridSearchCV
 from sklearn import tree
 from sklearn.random_projection import _sparse_random_matrix
+from sklearn.exceptions import ConvergenceWarning
 
 
 def _check_statistics(X, X_true,
@@ -863,6 +864,7 @@ def test_iterative_imputer_transform_recovery(rank):
     X_test = X_missing[n:]
 
     imputer = IterativeImputer(max_iter=5,
+                               imputation_order='descending',
                                verbose=1,
                                random_state=rng).fit(X_train)
     X_test_est = imputer.transform(X_test)
@@ -1271,3 +1273,27 @@ def test_simple_imputation_add_indicator_sparse_matrix(arr_type):
     assert sparse.issparse(X_trans)
     assert X_trans.shape == X_true.shape
     assert_allclose(X_trans.toarray(), X_true)
+
+
+@pytest.mark.parametrize(
+    "order, idx_order",
+    [
+        ("ascending", [3, 4, 2, 0, 1]),
+        ("descending", [1, 0, 2, 4, 3])
+    ]
+)
+def test_imputation_order(order, idx_order):
+    # regression test for #15393
+    rng = np.random.RandomState(42)
+    X = rng.rand(100, 5)
+    X[:50, 1] = np.nan
+    X[:30, 0] = np.nan
+    X[:20, 2] = np.nan
+    X[:10, 4] = np.nan
+
+    with pytest.warns(ConvergenceWarning):
+        trs = IterativeImputer(max_iter=1,
+                               imputation_order=order,
+                               random_state=0).fit(X)
+        idx = [x.feat_idx for x in trs.imputation_sequence_]
+        assert idx == idx_order
