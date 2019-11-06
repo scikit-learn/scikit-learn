32b3e768c5b9781ab12a800cace055c816a5d32a
Thomas J Fan <thomasjpfan@gmail.com>
2019-11-06 18:24:52 -0500

    BUG Fixes positive label

--- sklearn/metrics/_plot/roc_curve.py
+++ sklearn/metrics/_plot/roc_curve.py
@@ -2,6 +2,8 @@ from .. import auc
 from .. import roc_curve
 
 from ...utils import check_matplotlib_support
+from ...base import is_classifier
+from ...utils.validation import check_is_fitted
 
 
 class RocCurveDisplay:
@@ -101,7 +103,7 @@ class RocCurveDisplay:
         return self
 
 
-def plot_roc_curve(estimator, X, y, pos_label=None, sample_weight=None,
+def plot_roc_curve(estimator, X, y, sample_weight=None,
                    drop_intermediate=True, response_method="auto",
                    name=None, ax=None, **kwargs):
     """Plot Receiver operating characteristic (ROC) curve.
@@ -120,11 +122,6 @@ def plot_roc_curve(estimator, X, y, pos_label=None, sample_weight=None,
 
     y : array-like of shape (n_samples,)
         Target values.
-
-    pos_label : int or str, default=None
-        The label of the positive class.
-        When `pos_label=None`, if y_true is in {-1, 1} or {0, 1},
-        `pos_label` is set to 1, otherwise an error will be raised.
 
     sample_weight : array-like of shape (n_samples,), default=None
         Sample weights.
@@ -167,10 +164,21 @@ def plot_roc_curve(estimator, X, y, pos_label=None, sample_weight=None,
     >>> plt.show()                                   # doctest: +SKIP
     """
     check_matplotlib_support('plot_roc_curve')
+    check_is_fitted(estimator)
 
     if response_method not in ("predict_proba", "decision_function", "auto"):
         raise ValueError("response_method must be 'predict_proba', "
                          "'decision_function' or 'auto'")
+
+    classificaiton_error = ("{} should be a binary classifer".format(
+        estimator.__class__.__name__))
+
+    if is_classifier(estimator):
+        if len(estimator.classes_) != 2:
+            raise ValueError(classificaiton_error)
+        pos_label = estimator.classes_[1]
+    else:
+        raise ValueError(classificaiton_error)
 
     if response_method != "auto":
         prediction_method = getattr(estimator, response_method, None)
@@ -188,10 +196,8 @@ def plot_roc_curve(estimator, X, y, pos_label=None, sample_weight=None,
     y_pred = prediction_method(X)
 
     if y_pred.ndim != 1:
-        if y_pred.shape[1] > 2:
-            raise ValueError("Estimator should solve a "
-                             "binary classification problem")
         y_pred = y_pred[:, 1]
+
     fpr, tpr, _ = roc_curve(y, y_pred, pos_label=pos_label,
                             sample_weight=sample_weight,
                             drop_intermediate=drop_intermediate)
