e759e5388f40f8ddfa72266b36c307ad1275b527
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2019-11-08 10:59:40 -0600

    dealt with refactoring remnants

--- sklearn/_distributor_init.py
+++ sklearn/_distributor_init.py
@@ -5,6 +5,6 @@ of scikit-learn.
 
 For example, this is a good place to put any checks for hardware requirements.
 
-The numpy standard source distribution will not put code in this file, so you
-can safely replace this file with your own version.
+The scikit-learn standard source distribution will not put code in this file, 
+so you can safely replace this file with your own version.
 """
