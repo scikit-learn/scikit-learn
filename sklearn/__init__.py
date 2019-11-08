6992edaa917f92f53d0b2d884f59a741942d325c
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2019-11-08 10:46:33 -0600

    added noqa: F401

--- sklearn/__init__.py
+++ sklearn/__init__.py
@@ -91,7 +91,7 @@ else:
                'show_versions']
 
     # Allow distributors to run custom init code
-    from . import _distributor_init
+    from . import _distributor_init # noqa: F401
 
 
 def setup_module(module):
