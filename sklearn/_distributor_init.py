661c14aedef91808d831269dabfbed95631b9fb1
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2019-11-08 11:03:10 -0600

    removing trailing space

--- sklearn/_distributor_init.py
+++ sklearn/_distributor_init.py
@@ -5,6 +5,6 @@ of scikit-learn.
 
 For example, this is a good place to put any checks for hardware requirements.
 
-The scikit-learn standard source distribution will not put code in this file, 
+The scikit-learn standard source distribution will not put code in this file,
 so you can safely replace this file with your own version.
 """
