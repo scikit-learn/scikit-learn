70827d352fa0a845737b5d4f4714913ef447bb0b
Nicolas Hug <contact@nicolas-hug.com>
2019-11-11 10:57:13 -0500

    simplify example code

--- examples/ensemble/plot_monotonic_constraints.py
+++ examples/ensemble/plot_monotonic_constraints.py
@@ -22,32 +22,28 @@ y = (5 * f_0 + np.sin(10 * np.pi * f_0) -
      5 * f_1 - np.cos(10 * np.pi * f_1) +
      noise)
 
-fig, (ax0, ax1) = plt.subplots(ncols=2)
+fig, ax = plt.subplots()
 
 
 gbdt = HistGradientBoostingRegressor()
 gbdt.fit(X, y)
-unconstrained_disp0 = plot_partial_dependence(
-    gbdt, X, features=[0], line_kw={'linewidth': 4, 'label': 'unconstrained'},
-    ax=ax0)
-unconstrained_disp1 = plot_partial_dependence(
-    gbdt, X, features=[1], line_kw={'linewidth': 4, 'label': 'unconstrained'},
-    ax=ax1)
+disp = plot_partial_dependence(
+    gbdt, X, features=[0, 1],
+    line_kw={'linewidth': 4, 'label': 'unconstrained'},
+    ax=ax)
 
 gbdt = HistGradientBoostingRegressor()
 gbdt._monotonic_cst = [1, -1]  # INC, DEC
 gbdt.fit(X, y)
-constrained_disp0 = plot_partial_dependence(
-    gbdt, X, features=[0], line_kw={'linewidth': 4, 'label': 'constrained'},
-    ax=unconstrained_disp0.axes_)
-constrained_disp1 = plot_partial_dependence(
-    gbdt, X, features=[1], line_kw={'linewidth': 4, 'label': 'constrained'},
-    ax=unconstrained_disp1.axes_)
+plot_partial_dependence(
+    gbdt, X, features=[0, 1],
+    feature_names=('POS constraint', 'NEG constraint'),
+    line_kw={'linewidth': 4, 'label': 'unconstrained'},
+    ax=disp.axes_)
 
-constrained_disp0.axes_[0, 0].plot(X[:, 0], y, 'o', alpha=.6, zorder=-1)
-constrained_disp0.axes_[0, 0].set_ylim(-6, 6)
-constrained_disp1.axes_[0, 0].plot(X[:, 1], y, 'o', alpha=.6, zorder=-1)
-constrained_disp1.axes_[0, 0].set_ylim(-6, 6)
+for f_idx in (0, 1):
+    disp.axes_[0, f_idx].plot(X[:, f_idx], y, 'o', alpha=.3, zorder=-1)
+    disp.axes_[0, f_idx].set_ylim(-6, 6)
 
 plt.legend()
 fig.suptitle("Monotonic constraints illustration")
