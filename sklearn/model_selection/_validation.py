c240a1fd4e437a74cd8da6338e91904a4664c56d
Gregory Morse <gregory.morse@live.com>
2019-11-13 20:00:49 +0100

    Yield stack trace information in resilient mode warnings
    Fixes #15621

--- sklearn/model_selection/_validation.py
+++ sklearn/model_selection/_validation.py
@@ -532,7 +532,8 @@ def _fit_and_score(estimator, X, y, scorer, train, test, verbose,
             warnings.warn("Estimator fit failed. The score on this train-test"
                           " partition for these parameters will be set to %f. "
                           "Details: \n%s" %
-                          (error_score, format_exception_only(type(e), e)[0]),
+                          (error_score, format_exception_only(type(e), e)[0],
+                           extract_stack(e)),
                           FitFailedWarning)
         else:
             raise ValueError("error_score must be the string 'raise' or a"
