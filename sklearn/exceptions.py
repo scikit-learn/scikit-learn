b0152d946274ce8de34b3d7a52c9ab3960af5b15
Gregory Morse <gregory.morse@live.com>
2019-11-14 02:09:38 +0100

    Update exceptions.py

--- sklearn/exceptions.py
+++ sklearn/exceptions.py
@@ -136,7 +136,7 @@ class FitFailedWarning(RuntimeWarning):
     ...     except ValueError:
     ...         pass
     ...     msg = repr(w[-1].message)
-    ...     # if msg.find('Traceback (most recent call last):\n') > -1:
+    ...     # msg.find('Traceback (most recent call last):\n') > -1
     ...     if msg.count('\n') > 1:  # has stacktrace in middle of string
     ...         split = msg.splitlines()  # note: handles more than '\n'
     ...         msg = split[0] + '\n' + split[-1] + '\n'
