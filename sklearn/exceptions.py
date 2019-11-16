7d1ca46574f9d28019c9c3cd2d22df7e102f9a17
Gregory Morse <gregory.morse@live.com>
2019-11-16 06:33:31 +0100

    Update exceptions.py

--- sklearn/exceptions.py
+++ sklearn/exceptions.py
@@ -142,7 +142,7 @@ class FitFailedWarning(RuntimeWarning):
     ...     if ct > 1 or msg.count('\\\\n') > 1:  # has stacktrace
     ...         split = msg.splitlines() if ct > 1 else msg.split('\\\\n')
     ...         msg = split[0] + ('\\n' + split[-1] + '\\n' if ct > 1
-    ...                           else '\\\\n' + split[-2] + '\\\\n\'...)')
+    ...                           else '\\\\n' + split[-2] + '\\\\n\\'...)')
     ...     print(msg)
     FitFailedWarning('Estimator fit failed. The score on this train-test
     partition for these parameters will be set to 0.000000.
