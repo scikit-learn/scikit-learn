3e50b1795027b7cc62c0edaf2f03085d2a16a0d0
Sylvain MARIE <sylvain.marie@se.com>
2019-11-19 15:28:03 +0100

    Fixed init: old `check_psd_eigenvalues` reference was not removed

--- sklearn/utils/__init__.py
+++ sklearn/utils/__init__.py
@@ -24,7 +24,7 @@ from ..exceptions import DataConversionWarning
 from .deprecation import deprecated
 from .fixes import np_version
 from .validation import (as_float_array,
-                         assert_all_finite, check_psd_eigenvalues,
+                         assert_all_finite,
                          check_random_state, column_or_1d, check_array,
                          check_consistent_length, check_X_y, indexable,
                          check_symmetric, check_scalar)
@@ -65,7 +65,7 @@ class Parallel(_joblib.Parallel):
 
 __all__ = ["murmurhash3_32", "as_float_array",
            "assert_all_finite", "check_array",
-           "check_random_state", "check_psd_eigenvalues",
+           "check_random_state",
            "compute_class_weight", "compute_sample_weight",
            "column_or_1d", "safe_indexing",
            "check_consistent_length", "check_X_y", "check_scalar", 'indexable',
