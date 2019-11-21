f47a8e91f53531863f83b3a2f8a0e45aa0c2b3f9
Leland McInnes <leland.mcinnes@gmail.com>
2019-11-21 16:32:47 -0500

    Switch to 2D plotting for hierarchical benchmark

--- benchmarks/bench_plot_hierarchical.py
+++ benchmarks/bench_plot_hierarchical.py
@@ -41,32 +41,36 @@ def compute_bench(samples_range, features_range):
 
 
 if __name__ == '__main__':
-    from mpl_toolkits.mplot3d import axes3d  # noqa: F401
     import matplotlib.pyplot as plt
 
-    samples_range = np.linspace(1000, 15000, 5).astype(np.int)
-    features_range = np.linspace(2, 50, 5).astype(np.int)
+    samples_range = np.linspace(1000, 15000, 8).astype(np.int)
+    features_range = np.array([2, 10, 20, 50])
 
     results = compute_bench(samples_range, features_range)
 
     max_time = max([max(i) for i in [t for (label, t) in results.items()]])
 
-    fig = plt.figure('scikit-learn agglomerative clustering benchmark results')
-    n = 1
-    for c, (label, timings) in zip('brcy',
+    colors = plt.get_cmap('Vega10')(np.linspace(0, 1, 10))[:4]
+    lines = {linkage:None for linkage in results.keys()}
+    fig, axs = plt.subplots(2, 2, sharex=True, sharey=True)
+    fig.suptitle('Scikit-learn agglomerative clustering benchmark results', fontsize=16)
+    for c, (label, timings) in zip(colors,
                                    sorted(results.items())):
-        ax = fig.add_subplot(2, 2, n, projection='3d')
-        ax.set_zlim3d(0.0, max_time * 1.1)
-
-        X, Y = np.meshgrid(samples_range, features_range)
-        Z = np.asarray(timings).reshape(samples_range.shape[0],
+        timing_by_samples = np.asarray(timings).reshape(samples_range.shape[0],
                                         features_range.shape[0])
-        ax.plot_surface(X, Y, Z.T, cstride=1, rstride=1, color=c, alpha=0.5)
-        ax.set_xlabel('n_samples')
-        ax.set_ylabel('n_features')
-        ax.set_zlabel('time (s)')
-        ax.set_title(label.capitalize() + " linkage")
 
-        n += 1
+        for n in range(timing_by_samples.shape[1]):
+            ax = axs.flatten()[n]
+            lines[label], = ax.plot(samples_range, timing_by_samples[:, n], color=c,
+                            label=label)
+            ax.set_title('n_features = %d' % features_range[n])
+            if n >= 2:
+                ax.set_xlabel('n_samples')
+            if n % 2 == 0:
+                ax.set_ylabel('time (s)')
+
+    fig.subplots_adjust(right=0.8)
+    fig.legend([lines[link] for link in sorted(results.keys())],
+               sorted(results.keys()), loc="center right", fontsize=8)
 
     plt.show()
