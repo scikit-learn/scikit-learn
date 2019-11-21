82ed115c3d8b59958ea4c8bc8a4481f5b7bfdb66
Hanmin Qin <qinhanmin2005@sina.com>
2019-11-21 22:13:34 +0800

    remove redundant test (we no longer return weighted error)

--- sklearn/linear_model/tests/test_ridge.py
+++ sklearn/linear_model/tests/test_ridge.py
@@ -493,66 +493,6 @@ def test_ridge_loo_cv_asym_scoring():
     assert_allclose(gcv_ridge.intercept_, loo_ridge.intercept_, rtol=1e-3)
 
 
-@pytest.mark.parametrize('gcv_mode', ['svd', 'eigen'])
-@pytest.mark.parametrize('X_constructor', [np.asarray, sp.csr_matrix])
-@pytest.mark.parametrize('n_features', [8, 20])
-@pytest.mark.parametrize('y_shape, fit_intercept, noise',
-                         [((11,), True, 1.),
-                          ((11, 1), True, 20.),
-                          ((11, 3), True, 150.),
-                          ((11, 3), False, 30.)])
-def test_ridge_gcv_sample_weights(
-        gcv_mode, X_constructor, fit_intercept, n_features, y_shape, noise):
-    alphas = [1e-3, .1, 1., 10., 1e3]
-    rng = np.random.RandomState(0)
-    n_targets = y_shape[-1] if len(y_shape) == 2 else 1
-    X, y = _make_sparse_offset_regression(
-        n_samples=11, n_features=n_features, n_targets=n_targets,
-        random_state=0, shuffle=False, noise=noise)
-    y = y.reshape(y_shape)
-
-    sample_weight = 3 * rng.randn(len(X))
-    sample_weight = (sample_weight - sample_weight.min() + 1).astype(int)
-    indices = np.repeat(np.arange(X.shape[0]), sample_weight)
-    sample_weight = sample_weight.astype(float)
-    X_tiled, y_tiled = X[indices], y[indices]
-
-    cv = GroupKFold(n_splits=X.shape[0])
-    splits = cv.split(X_tiled, y_tiled, groups=indices)
-    kfold = RidgeCV(
-        alphas=alphas, cv=splits, scoring='neg_mean_squared_error',
-        fit_intercept=fit_intercept)
-    # ignore warning from GridSearchCV: FutureWarning: The default
-    # of the `iid` parameter will change from True to False in version 0.22
-    # and will be removed in 0.24
-    with ignore_warnings(category=FutureWarning):
-        kfold.fit(X_tiled, y_tiled)
-
-    ridge_reg = Ridge(alpha=kfold.alpha_, fit_intercept=fit_intercept)
-    splits = cv.split(X_tiled, y_tiled, groups=indices)
-    predictions = cross_val_predict(ridge_reg, X_tiled, y_tiled, cv=splits)
-    kfold_errors = (y_tiled - predictions)**2
-    kfold_errors = [
-        np.sum(kfold_errors[indices == i], axis=0) for
-        i in np.arange(X.shape[0])]
-    kfold_errors = np.asarray(kfold_errors)
-
-    X_gcv = X_constructor(X)
-    gcv_ridge = RidgeCV(
-        alphas=alphas, store_cv_values=True,
-        gcv_mode=gcv_mode, fit_intercept=fit_intercept)
-    gcv_ridge.fit(X_gcv, y, sample_weight=sample_weight)
-    if len(y_shape) == 2:
-        gcv_errors = gcv_ridge.cv_values_[:, :, alphas.index(kfold.alpha_)]
-    else:
-        gcv_errors = gcv_ridge.cv_values_[:, alphas.index(kfold.alpha_)]
-
-    assert kfold.alpha_ == pytest.approx(gcv_ridge.alpha_)
-    assert_allclose(gcv_errors, kfold_errors, rtol=1e-3)
-    assert_allclose(gcv_ridge.coef_, kfold.coef_, rtol=1e-3)
-    assert_allclose(gcv_ridge.intercept_, kfold.intercept_, rtol=1e-3)
-
-
 @pytest.mark.parametrize('mode', [True, 1, 5, 'bad', 'gcv'])
 def test_check_gcv_mode_error(mode):
     X, y = make_regression(n_samples=5, n_features=2)
