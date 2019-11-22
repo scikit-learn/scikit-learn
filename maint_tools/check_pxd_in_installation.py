6d713dadcf92381fd52e9f236e53f3988111800e
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2019-11-22 17:21:52 +0100

    simplify stuff + use pathlib

--- maint_tools/check_pxd_in_installation.py
+++ maint_tools/check_pxd_in_installation.py
@@ -7,66 +7,58 @@ python check_pxd_in_installation.py path/to/install_dir/of/scikit-learn
 
 import os
 import sys
-import glob
+import pathlib
 import tempfile
 import textwrap
 import subprocess
 
 
-skl_dir = sys.argv[1]
-skl_dir = os.path.abspath(skl_dir)
+sklearn_dir = pathlib.Path(sys.argv[1]).absolute()
+print(sklearn_dir)
 
 # find all .pxd files
-pxd_files = glob.glob(os.path.join(skl_dir, '**', '*.pxd'), recursive=True)
-pxd_files = [f.replace(skl_dir, 'sklearn') for f in pxd_files]
+pxd_files = list(sklearn_dir.glob("**/*.pxd"))
 
 print("> Found pxd files:")
-for file in pxd_files:
-    print(' -', file)
+for pxd_file in pxd_files:
+    print(' -', pxd_file)
 
 print("\n> Trying to compile a cython extension cimporting all corresponding "
       "modules\n")
 start_dir = os.path.abspath('.')
 
 with tempfile.TemporaryDirectory() as tmpdir:
-    try:
-        os.chdir(tmpdir)
+    tmpdir = pathlib.Path(tmpdir)
+    # A cython test file which cimports all modules corresponding to found
+    # pxd files.
+    # e.g. sklearn/tree/_utils.pxd becomes `cimport sklearn.tree._utils`
+    with open(tmpdir / 'tst.pyx', 'w') as f:
+        for pxd_file in pxd_files:
+            to_import = str(pxd_file.relative_to(sklearn_dir))
+            to_import = to_import.replace(os.path.sep, '.')
+            to_import = to_import.replace('.pxd', '')
+            f.write('cimport sklearn.' + to_import + '\n')
 
-        # A cython test file which cimports all modules corresponding to found
-        # pxd files.
-        # e.g. sklearn/tree/_utils.pxd becomes `cimport sklearn.tree._utils`
-        with open('tst.pyx', 'w') as f:
-            for file in pxd_files:
-                to_import = file.replace(os.path.sep, '.')
-                to_import = to_import.replace('.pxd', '')
-                f.write('cimport ' + to_import + '\n')
+    # A basic setup file to build the test file.
+    # We set the language to c++ and we use numpy.get_include() because
+    # some modules require it.
+    with open(tmpdir / 'setup_tst.py', 'w') as f:
+        f.write(textwrap.dedent(
+            """
+            from distutils.core import setup
+            from distutils.extension import Extension
+            from Cython.Build import cythonize
+            import numpy
 
-        # A basic setup file to build the test file.
-        # We set the language to c++ and we use numpy.get_include() because
-        # some modules require it.
-        with open('setup_tst.py', 'w') as f:
-            f.write(textwrap.dedent(
-                """
-                from distutils.core import setup
-                from distutils.extension import Extension
-                from Cython.Build import cythonize
-                import numpy
+            extensions = [Extension("tst",
+                                    sources=["tst.pyx"],
+                                    language="c++",
+                                    include_dirs=[numpy.get_include()])]
 
-                extensions = [Extension("tst",
-                                        sources=["tst.pyx"],
-                                        language="c++",
-                                        include_dirs=[numpy.get_include()])]
+            setup(ext_modules=cythonize(extensions))
+            """))
 
-                setup(ext_modules=cythonize(extensions))
-                """))
-
-        subprocess.run(["python", "setup_tst.py", "build_ext", "-i"],
-                       check=True)
-
-    except subprocess.CalledProcessError:
-        raise
-
-    finally:
-        os.chdir(start_dir)
+    subprocess.run(["python", "setup_tst.py", "build_ext", "-i"],
+                   check=True, cwd=tmpdir)
 
     print("\n> Compilation succeeded !")
