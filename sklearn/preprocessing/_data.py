b47a78699f638c8a8c19b44e3f6646aa2c9d6052
Tirth Patel <tirthasheshpatel@gmail.com>
2019-12-02 21:24:17 +0530

    Update _data.py

--- sklearn/preprocessing/_data.py
+++ sklearn/preprocessing/_data.py
@@ -2262,6 +2262,7 @@ class QuantileTransformer(TransformerMixin, BaseEstimator):
                 col = col.take(subsample_idx, mode='clip')
             self.quantiles_.append(np.nanpercentile(col, references))
         self.quantiles_ = np.transpose(self.quantiles_)
+        # make sure the quantiles are monotonically increasing
         self.quantiles_ = np.maximum.accumulate(self.quantiles_)
 
     def _sparse_fit(self, X, random_state):
