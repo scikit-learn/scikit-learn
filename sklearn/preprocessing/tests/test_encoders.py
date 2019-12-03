8dba3e17bce00bff9cd6bc1660accdc3360388c0
Thomas J Fan <thomasjpfan@gmail.com>
2019-12-02 22:48:43 -0500

    STY

--- sklearn/preprocessing/tests/test_encoders.py
+++ sklearn/preprocessing/tests/test_encoders.py
@@ -647,7 +647,8 @@ def test_encoders_unicode_categories(input_dtype, category_dtype, array_type):
     # encoders are correct for both string and object dtypes
 
     X = np.array([['b'], ['a']], dtype=input_dtype)
-    ohe = OneHotEncoder(categories=[np.array(['b', 'a'], dtype=category_dtype)],
+    ohe = OneHotEncoder(categories=[np.array(['b', 'a'],
+                                             dtype=category_dtype)],
                         sparse=False).fit(X)
 
     X_test = _convert_container([['a'], ['a'], ['b'], ['a']], array_type)
