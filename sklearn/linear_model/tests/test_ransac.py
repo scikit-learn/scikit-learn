2bc9103edf38a9a0ecd30281e8be85199ce8dfa1
Jeremy Alexandre <Jeremy.Alexandr@gmail.com>
2019-12-04 10:18:06 -0500

    added imports

--- sklearn/linear_model/tests/test_ransac.py
+++ sklearn/linear_model/tests/test_ransac.py
@@ -10,6 +10,8 @@ from sklearn.utils._testing import assert_warns
 from sklearn.utils._testing import assert_almost_equal
 from sklearn.utils._testing import assert_raises_regexp
 from sklearn.utils._testing import assert_raises
+from sklearn.utils._testing import assert_allclose
+from sklearn.datasets import make_regression
 from sklearn.linear_model import LinearRegression, RANSACRegressor, Lasso
 from sklearn.linear_model._ransac import _dynamic_max_trials
 from sklearn.exceptions import ConvergenceWarning
@@ -501,10 +503,7 @@ def test_ransac_final_model_fit_sample_weight():
     rng = check_random_state(42)
     sample_weight = rng.randint(1, 4, size=y.shape[0])
     sample_weight = sample_weight / sample_weight.sum()
-    ransac = RANSACRegressor(
-        base_estimator=LinearRegression(), 
-        random_state=0
-        )
+    ransac = RANSACRegressor(base_estimator=LinearRegression(), random_state=0)
     ransac.fit(X, y, sample_weight=sample_weight)
 
     final_model = LinearRegression()
