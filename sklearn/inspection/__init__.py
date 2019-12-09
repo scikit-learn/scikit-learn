fae0ef705df2915a704cd1e46e79789395dc9e0f
Olivier Grisel <olivier.grisel@ensta.org>
2019-12-09 15:49:46 +0100

    Do not shadow public functions with deprecated modules
    Fix #15842.
    
    This can happen for:
    
    - sklearn.decomposition.dict_learning
    - sklearn.inspection.partial_dependence

--- sklearn/inspection/__init__.py
+++ sklearn/inspection/__init__.py
@@ -1,8 +1,20 @@
 """The :mod:`sklearn.inspection` module includes tools for model inspection."""
-from ._partial_dependence import partial_dependence
-from ._partial_dependence import plot_partial_dependence
-from ._partial_dependence import PartialDependenceDisplay
-from ._permutation_importance import permutation_importance
+
+# TODO: remove me in 0.24:
+# pre-cache the import of the deprecated module to avoid showing
+# the partial_dependence function.
+# https://github.com/scikit-learn/scikit-learn/issues/15842
+import warnings
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore", category=FutureWarning)
+    from . import partial_dependence as _ignored
+    del _ignored
+
+from ._partial_dependence import partial_dependence  # noqa
+from ._partial_dependence import plot_partial_dependence  # noqa
+from ._partial_dependence import PartialDependenceDisplay  # noqa
+from ._permutation_importance import permutation_importance  # noqa
+
 
 __all__ = [
     'partial_dependence',
