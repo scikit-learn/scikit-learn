be260a0200791b33c03395be219e8bb2964efbcb
Nicolas Hug <contact@nicolas-hug.com>
2019-12-11 08:52:39 -0500

    pep

--- sklearn/inspection/tests/test_partial_dependence.py
+++ sklearn/inspection/tests/test_partial_dependence.py
@@ -38,7 +38,7 @@ from sklearn.utils._testing import assert_array_equal
 from sklearn.utils._testing import ignore_warnings
 from sklearn.utils._testing import assert_array_almost_equal
 from sklearn.utils._testing import assert_almost_equal
-from sklearn.tree._tree import TREE_LEAF, TREE_UNDEFINED
+from sklearn.tree._tree import TREE_UNDEFINED
 
 
 # toy sample
