fe82e78df8df05a39fc36b41c12bf31f35054495
Olivier Grisel <olivier.grisel@ensta.org>
2019-12-16 11:15:43 +0100

    Use the 0.21 imports

--- sklearn/decomposition/__init__.py
+++ sklearn/decomposition/__init__.py
@@ -4,16 +4,17 @@ algorithms, including among others PCA, NMF or ICA. Most of the algorithms of
 this module can be regarded as dimensionality reduction techniques.
 """
 
-# TODO: remove me in 0.24 (as well as the noqa markers)
-# pre-cache the import of the deprecated module so that import
+# TODO: remove me in 0.24 (as well as the noqa markers) and
+# import the dict_learning func directly from the ._dict_learning
+# module instead.
+# Pre-cache the import of the deprecated module so that import
 # sklearn.decomposition.dict_learning returns the function as in
 # 0.21, instead of the module.
 # https://github.com/scikit-learn/scikit-learn/issues/15842
 import warnings
 with warnings.catch_warnings():
     warnings.simplefilter("ignore", category=FutureWarning)
-    from . import dict_learning as _ignored
-    del _ignored
+    from .dict_learning import dict_learning
 
 
 from ._nmf import NMF, non_negative_factorization  # noqa
@@ -23,7 +24,7 @@ from ._kernel_pca import KernelPCA  # noqa
 from ._sparse_pca import SparsePCA, MiniBatchSparsePCA  # noqa
 from ._truncated_svd import TruncatedSVD  # noqa
 from ._fastica import FastICA, fastica  # noqa
-from ._dict_learning import (dict_learning, dict_learning_online,
+from ._dict_learning import (dict_learning_online,
                              sparse_encode, DictionaryLearning,
                              MiniBatchDictionaryLearning, SparseCoder)  # noqa
 from ._factor_analysis import FactorAnalysis  # noqa
--- sklearn/inspection/__init__.py
+++ sklearn/inspection/__init__.py
@@ -1,17 +1,17 @@
 """The :mod:`sklearn.inspection` module includes tools for model inspection."""
 
-# TODO: remove me in 0.24 (as well as the noqa markers)
-# pre-cache the import of the deprecated module so that import
+# TODO: remove me in 0.24 (as well as the noqa markers) and
+# import the partial_dependence func directly from the
+# ._partial_dependence module instead.
+# Pre-cache the import of the deprecated module so that import
 # sklearn.inspection.partial_dependence returns the function as in
 # 0.21, instead of the module
 # https://github.com/scikit-learn/scikit-learn/issues/15842
 import warnings
 with warnings.catch_warnings():
     warnings.simplefilter("ignore", category=FutureWarning)
-    from . import partial_dependence as _ignored
-    del _ignored
+    from .partial_dependence import partial_dependence
 
-from ._partial_dependence import partial_dependence  # noqa
 from ._partial_dependence import plot_partial_dependence  # noqa
 from ._partial_dependence import PartialDependenceDisplay  # noqa
 from ._permutation_importance import permutation_importance  # noqa
