c722102b838d2ada5a6828acc088b07bc08f9e3a
Chiara Marmo <chiara.marmo@inria.fr>
2019-12-20 16:52:04 +0100

    Minor rephrasing.

--- examples/inspection/plot_linear_model_coefficient_interpretation.py
+++ examples/inspection/plot_linear_model_coefficient_interpretation.py
@@ -220,7 +220,8 @@ plt.subplots_adjust(left=.3)
 
 ###############################################################################
 # Soon we realize that we cannot compare different coefficients since we did
-# not scale the data before the fit, features having different value ranges.
+# not scale the data before the fit, features having different value ranges
+# because of their different unit of measure.
 # For instance, the AGE coefficient is expressed in $/hours/leaving years
 # while the EDUCATION is expressed in $/hours/years of education.
 # This is evident if we compare feature standard deviations.
