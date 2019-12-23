a89d3dc0297aae7cb5765dea3633fbc219b83b71
Thomas J Fan <thomasjpfan@gmail.com>
2019-12-22 21:25:58 -0500

    MNT Updates pypy

--- build_tools/circle/build_test_pypy.sh
+++ build_tools/circle/build_test_pypy.sh
@@ -18,10 +18,7 @@ source pypy-env/bin/activate
 python --version
 which python
 
-# XXX: numpy version pinning can be reverted once PyPy
-#      compatibility is resolved for numpy v1.6.x. For instance,
-#      when PyPy3 >6.0 is released (see numpy/numpy#12740)
-pip install --extra-index https://antocuni.github.io/pypy-wheels/ubuntu numpy Cython pytest
+pip install --extra-index https://antocuni.github.io/pypy-wheels/manylinux2010 numpy Cython pytest
 pip install scipy sphinx numpydoc docutils joblib pillow
 
 ccache -M 512M
