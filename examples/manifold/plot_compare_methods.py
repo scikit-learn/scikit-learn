444fa1e5223f969e31269d7c7d08b18b5be0f156
DavidBreuer <DavidBreuer@users.noreply.github.com>
2019-12-23 17:23:55 +0100

    Use OrderedDict to ensure ordering of methods in manifold example and use more modern matplotlib axis definition

--- examples/manifold/plot_compare_methods.py
+++ examples/manifold/plot_compare_methods.py
@@ -23,6 +23,7 @@ representation of the data in the low-dimensional space.
 
 print(__doc__)
 
+from collections import OrderedDict
 from functools import partial
 from time import time
 
@@ -40,20 +41,25 @@ X, color = datasets.make_s_curve(n_points, random_state=0)
 n_neighbors = 10
 n_components = 2
 
-fig = plt.figure(figsize=(15, 8))
-plt.suptitle("Manifold Learning with %i points, %i neighbors"
+# Create figure
+fig, axis = plt.subplots(nrows=2, ncols=5, figsize=(15, 8))
+fig.suptitle("Manifold Learning with %i points, %i neighbors"
              % (1000, n_neighbors), fontsize=14)
 
-
-ax = fig.add_subplot(251, projection='3d')
+# Add 3d scatter plot
+axis[0, 0].remove()
+ax = fig.add_subplot(2, 5, 1, projection='3d')
 ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=color, cmap=plt.cm.Spectral)
 ax.view_init(4, -72)
+
+# Hide axis for unused plot
+axis[1, 0].set_axis_off()
 
 # Set-up manifold methods
 LLE = partial(manifold.LocallyLinearEmbedding,
               n_neighbors, n_components, eigen_solver='auto')
 
-methods = dict()
+methods = OrderedDict()
 methods['LLE'] = LLE(method='standard')
 methods['LTSA'] = LLE(method='ltsa')
 methods['Hessian LLE'] = LLE(method='hessian')
@@ -71,11 +77,11 @@ for i, (label, method) in enumerate(methods.items()):
     Y = method.fit_transform(X)
     t1 = time()
     print("%s: %.2g sec" % (label, t1 - t0))
-    ax = fig.add_subplot(2, 5, 2 + i + (i > 3))
-    plt.scatter(Y[:, 0], Y[:, 1], c=color, cmap=plt.cm.Spectral)
-    plt.title("%s (%.2g sec)" % (label, t1 - t0))
+    ax = axis[1*(i > 3), (i % 4) + 1]  # fig.add_subplot(2, 5, 2 + i + (i > 3))
+    ax.scatter(Y[:, 0], Y[:, 1], c=color, cmap=plt.cm.Spectral)
+    ax.set_title("%s (%.2g sec)" % (label, t1 - t0))
     ax.xaxis.set_major_formatter(NullFormatter())
     ax.yaxis.set_major_formatter(NullFormatter())
-    plt.axis('tight')
+    ax.axis('tight')
 
 plt.show()
