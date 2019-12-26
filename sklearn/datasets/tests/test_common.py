66c96123274d23fc731c60ad87549fe99667715f
Will Connell <wconnell93@gmail.com>
2019-12-26 15:42:07 -0800

    generalized check_as_frame test
    Co-authored-by: @reganconnell <reganconnell@berkeley.edu>

--- sklearn/datasets/tests/test_common.py
+++ sklearn/datasets/tests/test_common.py
@@ -1,5 +1,6 @@
 """Test loaders for common functionality.
 """
+import pytest
 
 
 def check_return_X_y(bunch, fetch_func_partial):
@@ -7,3 +8,14 @@ def check_return_X_y(bunch, fetch_func_partial):
     assert isinstance(X_y_tuple, tuple)
     assert X_y_tuple[0].shape == bunch.data.shape
     assert X_y_tuple[1].shape == bunch.target.shape
+
+
+def check_as_frame(bunch, fetch_func_partial):
+    pd = pytest.importorskip('pandas')
+    frame_bunch = fetch_func_partial(as_frame=True)
+    assert hasattr(frame_bunch, 'frame') is True
+    assert isinstance(frame_bunch.frame, pd.DataFrame)
+    assert isinstance(frame_bunch.data, pd.DataFrame)
+    assert frame_bunch.data.shape == bunch.data.shape
+    assert isinstance(frame_bunch.target, pd.DataFrame)
+    assert frame_bunch.target.shape == bunch.target.shape
