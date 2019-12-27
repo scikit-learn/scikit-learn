24e5011627de706207ddf6e8a301670ef53c5d43
Roman Yurchak <rth.yurchak@pm.me>
2019-12-27 10:23:39 +0100

    DOC Address comments

--- sklearn/utils/validation.py
+++ sklearn/utils/validation.py
@@ -859,7 +859,7 @@ def check_is_fitted(estimator, attributes=None, msg=None, all_or_any=all):
     fitted attributes (ending with a trailing underscore) and otherwise
     raises a NotFittedError with the given message.
 
-    This utility is meant to be used internally by estimators them-selves,
+    This utility is meant to be used internally by estimators themselves,
     typically in their own predict / transform methods.
 
     Parameters
