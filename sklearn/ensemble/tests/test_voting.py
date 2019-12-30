d80c414cc78e6fc3c67a3ca3550e5619b87dd053
Reshama <rs2715@gmail.com>
2019-12-30 12:57:37 -0500

    adding back imports

--- sklearn/ensemble/tests/test_voting.py
+++ sklearn/ensemble/tests/test_voting.py
@@ -2,6 +2,7 @@
 
 import pytest
 import numpy as np
+import re
 
 from sklearn.utils._testing import assert_almost_equal, assert_array_equal
 from sklearn.utils._testing import assert_array_almost_equal
