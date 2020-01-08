cd9439ee1ab1ea410ac0c0e70890d53b692d8182
Thomas J Fan <thomasjpfan@gmail.com>
2020-01-08 16:45:03 -0500

    REV [scipy-dev]

--- examples/linear_model/plot_sgd_iris.py
+++ examples/linear_model/plot_sgd_iris.py
@@ -58,8 +58,7 @@ plt.axis('tight')
 for i, color in zip(clf.classes_, colors):
     idx = np.where(y == i)
     plt.scatter(X[idx, 0], X[idx, 1], c=color, label=iris.target_names[i],
-                cmap=plt.cm.Paired, edgecolor='black', s=20,
-                reponse_method='predict')
+                cmap=plt.cm.Paired, edgecolor='black', s=20)
 plt.title("Decision surface of multi-class SGD")
 plt.axis('tight')
 
