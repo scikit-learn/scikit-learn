83df8c6bff33c8e7add6ab5b88cb7f2813a553b3
alexshacked <al.shacked@gmail.com>
2020-01-10 20:20:04 +0200

    Update sklearn/neighbors/_base.py
    Co-Authored-By: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- sklearn/neighbors/_base.py
+++ sklearn/neighbors/_base.py
@@ -944,7 +944,7 @@ class RadiusNeighborsMixin:
                 # See https://github.com/numpy/numpy/issues/5456
                 # to understand why this is initialized this way.
                 neigh_dist = to_object_array(neigh_dist_list)
-                neigh_ind = to_object_array(neigh_ind_list)
+                neigh_ind = _to_object_array(neigh_ind_list)
                 results = neigh_dist, neigh_ind
             else:
                 neigh_ind_list = sum(chunked_results, [])
