fab45389c0550cbd133cf3e8f7b328422ba16097
ivannz <ivannz@yandex.ru>
2020-01-10 11:41:46 +0300

    DOC new docstring guidelines in svdd (according to #16060)

--- sklearn/svm/_classes.py
+++ sklearn/svm/_classes.py
@@ -1823,7 +1823,7 @@ class OneClassSVM(OutlierMixin, BaseLibSVM):
 
 
 class SVDD(OutlierMixin, BaseLibSVM):
-    """Support Vector Data Description (SVDD) for Unsupervised Outlier Detection.
+    """Support Vector Data Description for Unsupervised Outlier Detection.
 
     Estimate the support of a high-dimensional distribution by finding the
     tightest soft hypersphere around a data set, which permits at most a
@@ -1835,18 +1835,18 @@ class SVDD(OutlierMixin, BaseLibSVM):
 
     Parameters
     ----------
-    kernel : string, optional (default='rbf')
+    kernel : {'linear', 'poly', 'rbf', 'sigmoid', 'precomputed'}, default='rbf'
          Specifies the kernel type to be used in the algorithm.
          It must be one of 'linear', 'poly', 'rbf', 'sigmoid', 'precomputed' or
          a callable.
          If none is given, 'rbf' will be used. If a callable is given it is
          used to precompute the kernel matrix.
 
-    degree : int, optional (default=3)
+    degree : int, default=3
         Degree of the polynomial kernel function ('poly').
         Ignored by all other kernels.
 
-    gamma : {'scale', 'auto'} or float, optional (default='scale')
+    gamma : {'scale', 'auto'} or float, default='scale'
         Kernel coefficient for 'rbf', 'poly' and 'sigmoid'.
 
         - if ``gamma='scale'`` (default) is passed then it uses
@@ -1856,52 +1856,52 @@ class SVDD(OutlierMixin, BaseLibSVM):
         .. versionchanged:: 0.22
            The default value of ``gamma`` changed from 'auto' to 'scale'.
 
-    coef0 : float, optional (default=0.0)
+    coef0 : float, default=0.0
         Independent term in kernel function.
         It is only significant in 'poly' and 'sigmoid'.
 
-    tol : float, optional
+    tol : float, default=1e-3
         Tolerance for stopping criterion.
 
-    nu : float, optional
+    nu : float, default=0.5
         An upper bound on the fraction of training
         errors and a lower bound of the fraction of support
         vectors. Should be in the interval (0, 1]. By default 0.5
         will be taken.
 
-    shrinking : boolean, optional
+    shrinking : bool, default=True
         Whether to use the shrinking heuristic.
 
-    cache_size : float, optional
+    cache_size : float, default=200
         Specify the size of the kernel cache (in MB).
 
-    verbose : bool, default: False
+    verbose : bool, default=False
         Enable verbose output. Note that this setting takes advantage of a
         per-process runtime setting in libsvm that, if enabled, may not work
         properly in a multithreaded context.
 
-    max_iter : int, optional (default=-1)
+    max_iter : int, default=-1
         Hard limit on iterations within solver, or -1 for no limit.
 
     Attributes
     ----------
-    support_ : array-like, shape = [n_SV]
+    support_ : ndarray of shape (n_SV,)
         Indices of support vectors.
 
-    support_vectors_ : array-like, shape = [nSV, n_features]
+    support_vectors_ : ndarray of shape (n_SV, n_features)
         Support vectors.
 
-    dual_coef_ : array, shape = [1, n_SV]
+    dual_coef_ : ndarray of shape (1, n_SV)
         Coefficients of the support vectors in the decision function.
 
-    coef_ : array, shape = [1, n_features]
+    coef_ : ndarray of shape (1, n_features)
         Weights assigned to the features (coefficients in the primal
         problem). This is only available in the case of a linear kernel.
 
         `coef_` is readonly property derived from `dual_coef_` and
         `support_vectors_`
 
-    intercept_ : array, shape = [1,]
+    intercept_ : ndarray of shape (1,)
         The constant in the decision function.
 
     offset_ : float
@@ -1909,6 +1909,9 @@ class SVDD(OutlierMixin, BaseLibSVM):
         We have the relation: decision_function = score_samples - `offset_`.
         The offset is the opposite of `intercept_` and is provided for
         consistency with other outlier detection algorithms.
+
+    fit_status_ : int
+        0 if correctly fitted, 1 otherwise (will raise warning)
 
     Examples
     --------
@@ -1949,18 +1952,20 @@ class SVDD(OutlierMixin, BaseLibSVM):
 
         Parameters
         ----------
-        X : {array-like, sparse matrix}, shape (n_samples, n_features)
+        X : {array-like, sparse matrix} of shape (n_samples, n_features)
             Set of samples, where n_samples is the number of samples and
             n_features is the number of features.
 
-        sample_weight : array-like, shape (n_samples,)
-            Per-sample weights. Higher weights force the novelty detector
-            to put more emphasis on these points.
+        sample_weight : array-like of shape (n_samples,), default=None
+            Per-sample weights. Rescale C per sample. Higher weights
+            force the classifier to put more emphasis on these points.
+
+        y : Ignored
+            not used, present for API consistency by convention.
 
         Returns
         -------
         self : object
-            Returns self.
 
         Notes
         -----
@@ -1979,11 +1984,12 @@ class SVDD(OutlierMixin, BaseLibSVM):
 
         Parameters
         ----------
-        X : array-like, shape (n_samples, n_features)
+        X : array-like of shape (n_samples, n_features)
+            The data matrix.
 
         Returns
         -------
-        X : array-like, shape (n_samples,)
+        dec : ndarray of shape (n_samples,)
             Returns the decision function of the samples.
         """
         dec = self._decision_function(X).ravel()
@@ -1994,31 +2000,32 @@ class SVDD(OutlierMixin, BaseLibSVM):
 
         Parameters
         ----------
-        X : array-like, shape (n_samples, n_features)
+        X : array-like of shape (n_samples, n_features)
+            The data matrix.
 
         Returns
         -------
-        score_samples : array-like, shape (n_samples,)
+        score_samples : ndarray of shape (n_samples,)
             Returns the (unshifted) scoring function of the samples.
         """
         score_samples = self.decision_function(X) + self.offset_
         return score_samples
 
     def predict(self, X):
-        """
-        Perform classification on samples in X.
+        """Perform classification on samples in X.
 
-        For an one-class model, +1 or -1 is returned.
+        For a one-class model, +1 or -1 is returned.
 
         Parameters
         ----------
-        X : {array-like, sparse matrix}, shape (n_samples, n_features)
+        X : {array-like, sparse matrix} of shape (n_samples, n_features) or \
+                (n_samples_test, n_samples_train)
             For kernel="precomputed", the expected shape of X is
-            [n_samples_test, n_samples_train]
+            (n_samples_test, n_samples_train).
 
         Returns
         -------
-        y_pred : array, shape (n_samples,)
+        y_pred : ndarray of shape (n_samples,)
             Class labels for samples in X.
         """
         y = super().predict(X)
