fa67ed1490dab34d6725e64ce218901ee9bd3044
mghah <31044045+mghah@users.noreply.github.com>
2020-01-14 13:16:12 -0800

    Update _shrunk_covariance.py

--- sklearn/covariance/_shrunk_covariance.py
+++ sklearn/covariance/_shrunk_covariance.py
@@ -32,9 +32,9 @@ def shrunk_covariance(emp_cov, shrinkage=0.1):
     emp_cov : array-like of shape (n_features, n_features)
         Covariance matrix to be shrunk
 
-    shrinkage : float in [0,1], default=0.1
+    shrinkage : float, default=0.1
         Coefficient in the convex combination used for the computation
-        of the shrunk estimate.
+        of the shrunk estimate. Range is [0, 1].
 
     Returns
     -------
