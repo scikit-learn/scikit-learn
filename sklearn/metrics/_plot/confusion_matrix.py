db163e243f4655da89b53ccdb12224aaa6f985d5
Rick Mackenbach <rick.mackenbach@nxp.com>
2020-01-17 15:33:39 +0100

    "Fixes for confusion_matrix values_format."

--- sklearn/metrics/_plot/confusion_matrix.py
+++ sklearn/metrics/_plot/confusion_matrix.py
@@ -61,7 +61,7 @@ class ConfusionMatrixDisplay:
 
         values_format : str, default=None
             Format specification for values in confusion matrix. If `None`,
-            the format specification is 'd' if < 1e7, '.2g' otherwise.
+            the format specification is 'd' or '.2g', whichever is shorter. 
 
         ax : matplotlib axes, default=None
             Axes object to plot on. If `None`, a new figure and axes is
@@ -89,7 +89,7 @@ class ConfusionMatrixDisplay:
         if include_values:
             self.text_ = np.empty_like(cm, dtype=object)
             if values_format is None:
-                values_format = lambda x: '.2g' if (log10(x) > 7) else 'd'
+                values_format = lambda x: 'd' if (x == 0 or log10(x) < 7) else '.2g'
 
             # print text with appropriate color depending on background
             thresh = (cm.max() + cm.min()) / 2.0
@@ -164,7 +164,7 @@ def plot_confusion_matrix(estimator, X, y_true, labels=None,
 
     values_format : str, default=None
         Format specification for values in confusion matrix. If `None`,
-        the format specification is 'd' if < 1e7, '.2g' otherwise.
+        the format specification is 'd' or '.2g', whichever is shorter. 
 
     cmap : str or matplotlib Colormap, default='viridis'
         Colormap recognized by matplotlib.
