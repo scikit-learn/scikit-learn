709e558195e2b01caa3a3d5d4e5b4af3c844302b
Nicolas Hug <contact@nicolas-hug.com>
2020-01-21 11:09:59 -0500

    better doc for calibrtion_curbe

--- sklearn/calibration.py
+++ sklearn/calibration.py
@@ -523,7 +523,8 @@ def calibration_curve(y_true, y_prob, normalize=False, n_bins=5,
                       strategy='uniform'):
     """Compute true and predicted probabilities for a calibration curve.
 
-    The method assumes the inputs come from a binary classifier.
+    The method assumes the inputs come from a binary classifier, and
+    discretize the [0, 1] interval into bins.
 
     Calibration curves may also be referred to as reliability diagrams.
 
@@ -531,36 +532,38 @@ def calibration_curve(y_true, y_prob, normalize=False, n_bins=5,
 
     Parameters
     ----------
-    y_true : array, shape (n_samples,)
+    y_true : array-like of shape (n_samples,)
         True targets.
 
-    y_prob : array, shape (n_samples,)
+    y_prob : array-like of shape (n_samples,)
         Probabilities of the positive class.
 
-    normalize : bool, optional, default=False
-        Whether y_prob needs to be normalized into the bin [0, 1], i.e. is not
-        a proper probability. If True, the smallest value in y_prob is mapped
-        onto 0 and the largest one onto 1.
+    normalize : bool, default=False
+        Whether y_prob needs to be normalized into the [0, 1] interval, i.e.
+        is not a proper probability. If True, the smallest value in y_prob
+        is linearly mapped onto 0 and the largest one onto 1.
 
-    n_bins : int
-        Number of bins. A bigger number requires more data. Bins with no data
-        points (i.e. without corresponding values in y_prob) will not be
-        returned, thus there may be fewer than n_bins in the return value.
+    n_bins : int, default=5
+        Number of bins to discretize the [0, 1] interval. A bigger number
+        requires more data. Bins with no samples (i.e. without
+        corresponding values in `y_prob`) will not be returned, thus the
+        returned arrays may have less than `n_bins` values.
 
-    strategy : {'uniform', 'quantile'}, (default='uniform')
+    strategy : {'uniform', 'quantile'}, default='uniform'
         Strategy used to define the widths of the bins.
 
         uniform
-            All bins have identical widths.
+            The bins have identical widths.
         quantile
-            All bins have the same number of points.
+            The bins have the same number of samples and depend on `y_prob`.
 
     Returns
     -------
-    prob_true : array, shape (n_bins,) or smaller
-        The true probability in each bin (fraction of positives).
+    prob_true : ndarray of shape (n_bins,) or smaller
+        The proportion of samples whose class is the positive class, in each
+        bin (fraction of positives).
 
-    prob_pred : array, shape (n_bins,) or smaller
+    prob_pred : ndarray of shape (n_bins,) or smaller
         The mean predicted probability in each bin.
 
     References
@@ -603,7 +606,7 @@ def calibration_curve(y_true, y_prob, normalize=False, n_bins=5,
     bin_total = np.bincount(binids, minlength=len(bins))
 
     nonzero = bin_total != 0
-    prob_true = (bin_true[nonzero] / bin_total[nonzero])
-    prob_pred = (bin_sums[nonzero] / bin_total[nonzero])
+    prob_true = bin_true[nonzero] / bin_total[nonzero]
+    prob_pred = bin_sums[nonzero] / bin_total[nonzero]
 
     return prob_true, prob_pred
