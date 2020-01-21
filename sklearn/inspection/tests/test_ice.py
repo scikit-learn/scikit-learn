ad39813c19043214801d3c26fd76989b7d8d982e
Madhura Jayaratne <madhura@predictivehire.com>
2020-01-21 16:13:40 +1100

    Fix line length

--- sklearn/inspection/tests/test_ice.py
+++ sklearn/inspection/tests/test_ice.py
@@ -119,8 +119,9 @@ def test_output_shape(Estimator, data, grid_resolution, features):
     n_instances = X.shape[0]
 
     est.fit(X, y)
-    pdp, axes = individual_conditional_expectation(est, X=X, features=features,
-                                                   grid_resolution=grid_resolution)
+    pdp, axes = individual_conditional_expectation(
+        est, X=X, features=features, grid_resolution=grid_resolution
+    )
 
     expected_ice_shape = (n_targets, n_instances,
                           *[grid_resolution for _ in range(len(features))])
