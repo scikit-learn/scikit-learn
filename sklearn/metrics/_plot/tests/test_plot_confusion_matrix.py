1c6244c5566a230ee826ec77f8dc06a3090227c6
Rick Mackenbach <rick.mackenbach@nxp.com>
2020-01-21 10:54:04 +0100

    "New changes"

--- sklearn/metrics/_plot/tests/test_plot_confusion_matrix.py
+++ sklearn/metrics/_plot/tests/test_plot_confusion_matrix.py
@@ -246,38 +246,39 @@ def test_confusion_matrix_pipeline(pyplot, clf, data, n_classes):
     assert disp.text_.shape == (n_classes, n_classes)
 
 
-@pytest.mark.parametrize("values_format", [None, 'e', 'd', '.2g'])
+@pytest.mark.parametrize("values_format", [None, 'e', 'd', 'n'])
 def test_confusion_matrix_text_format(pyplot, data, y_pred, n_classes,
                                       fitted_clf, values_format):
 
+    def test_function_format(x):
+        if (x == 0 or np.log10(x) < 7):
+            return 'd'
+        else:
+            return '.2g'
+
     # Make sure plot text is formatted with 'values_format'.
     X, y = data
+
     cm = confusion_matrix(y, y_pred)
     disp = plot_confusion_matrix(fitted_clf, X, y,
                                  include_values=True,
                                  values_format=values_format)
 
-    cm2 = confusion_matrix([1, 1]*5000000, [0, 0]*5000000)
-    disp2 = plot_confusion_matrix(fitted_clf, X, y,
-                                  include_values=True,
-                                  values_format=values_format)
-    disp2.confusion_matrix = np.array([
-                                [0, 0],
-                                [10000000, 0]], dtype=int)
-    disp2.plot()
     assert disp.text_.shape == (n_classes, n_classes)
 
     if values_format is None:
 
-        def test_function_format(x):
-            if (x == 0 or np.log10(x) < 7):
-                return 'd'
-            else:
-                return '.2g'
+        cm2 = confusion_matrix([1, 1]*5000000, [0, 0]*5000000)
+        disp2 = plot_confusion_matrix(fitted_clf, X, y,
+                                      include_values=True,
+                                      values_format=values_format)
+        disp2.confusion_matrix = np.array([
+                                    [0, 0],
+                                    [10000000, 0]], dtype=int)
+        disp2.plot()
 
         expected_text = np.array([format(v, test_function_format(v))
                                  for v in cm2.ravel()])
-
         text_text = np.array([
             t.get_text() for t in disp2.text_.ravel()])
 
