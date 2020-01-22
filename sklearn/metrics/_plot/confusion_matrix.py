7b007d3607fcc011db8092f15e715acd874a8da5
Rick Mackenbach <rick.mackenbach@nxp.com>
2020-01-22 13:08:42 +0100

    "Forgot to add the improved ConfusionMatrixDisplay"

--- sklearn/metrics/_plot/confusion_matrix.py
+++ sklearn/metrics/_plot/confusion_matrix.py
@@ -83,8 +83,6 @@ class ConfusionMatrixDisplay:
         n_classes = cm.shape[0]
         self.im_ = ax.imshow(cm, interpolation='nearest', cmap=cmap)
         self.text_ = None
-        if (values_format is None and cm.dtype.kind == "f"):
-            values_format = '.2g'
 
         cmap_min, cmap_max = self.im_.cmap(0), self.im_.cmap(256)
 
@@ -94,27 +92,20 @@ class ConfusionMatrixDisplay:
             # print text with appropriate color depending on background
             thresh = (cm.max() + cm.min()) / 2.0
 
-            if values_format is None:
-                for i, j in product(range(n_classes), range(n_classes)):
+            for i, j in product(range(n_classes), range(n_classes)):
 
-                    if (cm[i, j] == 0 or np.log10(cm[i, j]) < 7):
-                        values_format = 'd'
-                    else:
-                        values_format = '.2g'
+                if values_format is None and cm.dtype.kind == "f":
+                    values_format = '.2g'
+                if cm[i, j] == 0 or np.log10(cm[i, j]) < 7:
+                    values_format = 'd'
+                else:
+                    values_format = '.2g'
 
-                    color = cmap_max if cm[i, j] < thresh else cmap_min
-
-                    self.text_[i, j] = ax.text(
-                        j, i, format(cm[i, j], values_format),
-                        ha="center", va="center",
-                        color=color)
-            else:
-                for i, j in product(range(n_classes), range(n_classes)):
-                    color = cmap_max if cm[i, j] < thresh else cmap_min
-                    self.text_[i, j] = ax.text(j, i,
-                                               format(cm[i, j], values_format),
-                                               ha="center", va="center",
-                                               color=color)
+                color = cmap_max if cm[i, j] < thresh else cmap_min
+                self.text_[i, j] = ax.text(
+                    j, i, format(cm[i, j], values_format),
+                    ha="center", va="center",
+                    color=color)
 
         fig.colorbar(self.im_, ax=ax)
         ax.set(xticks=np.arange(n_classes),
