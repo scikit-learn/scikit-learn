67ee16fe123d8ae5a6b75ff5b6c2edb9085a87b6
Ellen König <ellen@ellenkoenig.de>
2020-01-25 13:00:16 +0100

    Removed (probably) unecessarily newline

--- sklearn/multiclass.py
+++ sklearn/multiclass.py
@@ -162,7 +162,6 @@ class OneVsRestClassifier(MultiOutputMixin, ClassifierMixin,
         ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
         ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
         for more details.
-
         .. versionchanged:: v0.20
        `n_jobs` change from ``1`` to ``None``
 
@@ -488,7 +487,6 @@ class OneVsOneClassifier(MetaEstimatorMixin, ClassifierMixin, BaseEstimator):
         ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
         ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
         for more details.
-
         .. versionchanged:: v0.20
        `n_jobs` change from ``1`` to ``None``
 
