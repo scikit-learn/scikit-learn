99c969d4a0af165e13fe3a9ece35748f9454523f
Ekaterina <borovikova@zoho.eu>
2020-01-25 13:56:15 +0100

    Add versionchanged to OutputCodeClassifier

--- sklearn/multiclass.py
+++ sklearn/multiclass.py
@@ -701,6 +701,9 @@ class OutputCodeClassifier(MetaEstimatorMixin, ClassifierMixin, BaseEstimator):
         ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
         ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
         for more details.
+        
+        ..versionchanged:: v0.20
+           `n_jobs` change from ``1`` to ``None``        
 
     Attributes
     ----------
