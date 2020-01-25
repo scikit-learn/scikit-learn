8aec2a9b58d812b93e033e106c88e66ad60c6b55
Mojca <mojca.bertoncelj@7learnings.com>
2020-01-25 16:59:28 +0100

    linting

--- sklearn/inspection/_permutation_importance.py
+++ sklearn/inspection/_permutation_importance.py
@@ -79,7 +79,7 @@ def permutation_importance(estimator, X, y, scoring=None, n_repeats=5,
         for more details.
 
     random_state : int, RandomState instance or None (default)
-        Pseudo-random number generator to control the permutations of each 
+        Pseudo-random number generator to control the permutations of each
         feature.
         Pass an int to get reproducible results across function calls.
         See :term: `Glossary <random_state>`.
