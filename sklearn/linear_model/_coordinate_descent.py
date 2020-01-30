4e1dc477ab732c892806d2e7140f09033fb6360c
Hirofumi Suzuki <hs-nazuna@users.noreply.github.com>
2020-01-30 14:53:34 +0100

    Modified docstring in sklearn/linear_model/_coordinate_descent.py
    Co-Authored-By: Jérémie du Boisberranger <34657725+jeremiedbb@users.noreply.github.com>

--- sklearn/linear_model/_coordinate_descent.py
+++ sklearn/linear_model/_coordinate_descent.py
@@ -2023,7 +2023,7 @@ class MultiTaskElasticNetCV(RegressorMixin, LinearModelCV):
 
     Attributes
     ----------
-    intercept_ : array of shape (n_tasks,)
+    intercept_ : ndarray of shape (n_tasks,)
         Independent term in decision function.
 
     coef_ : array of shape (n_tasks, n_features)
