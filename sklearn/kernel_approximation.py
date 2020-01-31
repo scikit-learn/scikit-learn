6b646da89c516a14adf36bad2994b5782df67050
judithabk6 <judithabk6@users.noreply.github.com>
2020-01-31 18:58:04 +0100

    fix space missing around parameter in documentation (#16351)

--- sklearn/kernel_approximation.py
+++ sklearn/kernel_approximation.py
@@ -46,11 +46,12 @@ class RBFSampler(TransformerMixin, BaseEstimator):
 
     Attributes
     ----------
-    random_offset_: ndarray of shape (n_components,), dtype=float64
+    random_offset_ : ndarray of shape (n_components,), dtype=float64
         Random offset used to compute the projection in the `n_components`
         dimensions of the feature space.
 
-    random_weights_: ndarray of shape (n_features, n_components), dtype=float64
+    random_weights_ : ndarray of shape (n_features, n_components),\
+        dtype=float64
         Random projection directions drawn from the Fourier transform
         of the RBF kernel.
 
