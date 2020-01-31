4b4a2c1641e8695931af60c1bf2f407b458f8389
Sebastian Kießling <sebkies@gmail.com>
2020-01-31 14:25:39 +0100

    add check_non_negative to additivechi2Sampler

--- sklearn/kernel_approximation.py
+++ sklearn/kernel_approximation.py
@@ -19,6 +19,7 @@ from .utils import check_array, check_random_state, as_float_array
 from .utils.extmath import safe_sparse_dot
 from .utils.validation import check_is_fitted
 from .metrics.pairwise import pairwise_kernels, KERNEL_PARAMS
+from .utils.validation import check_non_negative
 
 
 class RBFSampler(TransformerMixin, BaseEstimator):
@@ -324,7 +325,9 @@ class AdditiveChi2Sampler(TransformerMixin, BaseEstimator):
         self : object
             Returns the transformer.
         """
-        check_array(X, accept_sparse='csr')
+        X = check_array(X, accept_sparse='csr')
+        check_non_negative(X, 'X in AdditiveChi2Sampler.fit')
+
         if self.sample_interval is None:
             # See reference, figure 2 c)
             if self.sample_steps == 1:
@@ -359,11 +362,9 @@ class AdditiveChi2Sampler(TransformerMixin, BaseEstimator):
         check_is_fitted(self, msg=msg)
 
         X = check_array(X, accept_sparse='csr')
+        check_non_negative(X, 'X in AdditiveChi2Sampler.transform')
         sparse = sp.issparse(X)
 
-        # check if X has negative values. Doesn't play well with np.log.
-        if ((X.data if sparse else X) < 0).any():
-            raise ValueError("Entries of X must be non-negative.")
         # zeroth component
         # 1/cosh = sech
         # cosh(0) = 1.0
