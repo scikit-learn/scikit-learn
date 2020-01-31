b6e124ba10fef8486e925b398f8f1a26c1b52300
Pierre Delanoue <delanoue.pierre@gmail.com>
2020-01-31 16:20:35 +0100

    Update sklearn/utils/__init__.py
    Co-Authored-By: Jérémie du Boisberranger <34657725+jeremiedbb@users.noreply.github.com>

--- sklearn/utils/__init__.py
+++ sklearn/utils/__init__.py
@@ -620,7 +620,7 @@ def shuffle(*arrays, **options):
     Other Parameters
     ----------------
     random_state : int, RandomState instance or None, optional (default=None)
-        Determines the pseudo random number generator to use when shuffling
+        Determines random number generation for shuffling
         the data.
         Pass an int for reproducible results across multiple function calls.
         See :term:`Glossary <random_state>`.
