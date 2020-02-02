9f4adfb34a09e5b6af3c221ce8a66c2144955fcb
talgatomarov <37542479+talgatomarov@users.noreply.github.com>
2020-02-02 08:51:11 +0600

    Update sklearn/metrics/_plot/confusion_matrix.py
    Co-Authored-By: Thomas J Fan <thomasjpfan@gmail.com>

--- sklearn/metrics/_plot/confusion_matrix.py
+++ sklearn/metrics/_plot/confusion_matrix.py
@@ -190,7 +190,7 @@ def plot_confusion_matrix(estimator, X, y_true, labels=None,
     >>> clf = SVC(random_state=0)
     >>> clf.fit(X_train, y_train)
     >>> plot_confusion_matrix(clf, X_test, y_test)  # doctest: +SKIP
-    >>> plt.show()
+    >>> plt.show()  # doctest: +SKIP
     """
     check_matplotlib_support("plot_confusion_matrix")
 
