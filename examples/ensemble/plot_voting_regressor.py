73efbad6277850d34d73b6290b429969969c694f
maikia <maja_ka@hotmail.com>
2020-02-05 15:49:58 +0100

    flake8

--- examples/ensemble/plot_voting_regressor.py
+++ examples/ensemble/plot_voting_regressor.py
@@ -84,13 +84,13 @@ plt.figure()
 plt.plot(pred1, 'gd', label='GradientBoostingRegressor')
 plt.plot(pred2, 'b^', label='RandomForestRegressor')
 plt.plot(pred3, 'ys', label='LinearRegression')
-plt.plot(pred4, 'r*', ms = 10, label='VotingRegressor')
+plt.plot(pred4, 'r*', ms=10, label='VotingRegressor')
 
 plt.tick_params(axis='x', which='both', bottom=False, top=False,
                 labelbottom=False)
 plt.ylabel('predicted')
 plt.xlabel('training samples')
-plt.xlim([-0.5,20.5])
+plt.xlim([-0.5, 20.5])
 plt.legend(loc="best")
 plt.title('Regressor predictions and their average')
 
