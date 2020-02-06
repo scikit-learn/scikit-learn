9502dd7c150fe4c472230772f866c6bbc6583fa4
maikia <maja_ka@hotmail.com>
2020-02-06 10:19:26 +0100

    changed heading

--- examples/ensemble/plot_gradient_boosting_regression.py
+++ examples/ensemble/plot_gradient_boosting_regression.py
@@ -3,14 +3,18 @@
 Gradient Boosting regression
 ============================
 
-Demonstrate Gradient Boosting on the Diabetes dataset.
+This example demonstrate Gradient Boosting. Gradient boosting can be used for
+regression and classification problems. Here, we will use Diabetes (regression)
+and breast cancer (classification) datasets.
+We will obtain the results from
+:class:`~sklearn.ensemble.GradientBoostingRegressor` with least squares loss
+and 500 regression trees of depth 4.
 
-This example fits a Gradient Boosting model with least squares loss and
-500 regression trees of depth 4.
 """
 print(__doc__)
 
 # Author: Peter Prettenhofer <peter.prettenhofer@gmail.com>
+#         Maria Telenczuk <https://github.com/maikia>
 #
 # License: BSD 3 clause
 
@@ -22,10 +26,29 @@ from sklearn import datasets
 from sklearn.utils import shuffle
 from sklearn.metrics import mean_squared_error
 
-# #############################################################################
-# Load data
+##############################################################################
+# Load the data
+# -------------------------------------
+#
+# First we need to load the data. We will work on the two datasets
+# simultaneously.
+
 diabetes = datasets.load_diabetes()
-X, y = shuffle(diabetes.data, diabetes.target, random_state=13)
+cancer = datasets.load_breast_cancer()
+
+
+# 
+#  
+# 
+# Center target to avoid gradient boosting init bias: gradient
+# boosting
+# with the 'recursion' method does not account for the initial estimator
+# (here the average target, by default)
+
+
+Xd, yd = shuffle(diabetes.data, diabetes.target, random_state=13)
+X, y = shuffle(cancer.data, cancer.target, random_state=13)
+Xd = Xd.astype(np.float32)
 X = X.astype(np.float32)
 offset = int(X.shape[0] * 0.9)
 X_train, y_train = X[:offset], y[:offset]
@@ -75,7 +98,8 @@ sorted_idx = np.argsort(feature_importance)
 pos = np.arange(sorted_idx.shape[0]) + .5
 plt.subplot(1, 2, 2)
 plt.barh(pos, feature_importance[sorted_idx], align='center')
-plt.yticks(pos, np.array(diabetes.feature_names)[sorted_idx])
+#plt.yticks(pos, np.array(diabetes.feature_names)[sorted_idx])
+plt.yticks(pos, np.array(cancer.feature_names)[sorted_idx])
 plt.xlabel('Relative Importance')
 plt.title('Variable Importance')
 plt.show()
