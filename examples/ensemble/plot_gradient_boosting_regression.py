c92a0de99ddee39ca1a86b6b011eb2a5d6713e6d
maikia <maja_ka@hotmail.com>
2020-02-06 11:15:09 +0100

    added infor about data preprocessing

--- examples/ensemble/plot_gradient_boosting_regression.py
+++ examples/ensemble/plot_gradient_boosting_regression.py
@@ -3,9 +3,11 @@
 Gradient Boosting regression
 ============================
 
-This example demonstrate Gradient Boosting. Gradient boosting can be used for
-regression and classification problems. Here, we will use Diabetes (regression)
-and breast cancer (classification) datasets.
+This example demonstrate Gradient Boosting which will produce a prediction
+model from ensemble of weak prediction models. Gradient boosting can be used
+for regression and classification problems. Here, we will use Diabetes
+(regression) and breast cancer (classification) datasets.
+
 We will obtain the results from
 :class:`~sklearn.ensemble.GradientBoostingRegressor` with least squares loss
 and 500 regression trees of depth 4.
@@ -25,39 +27,63 @@ from sklearn import ensemble
 from sklearn import datasets
 from sklearn.utils import shuffle
 from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 
 ##############################################################################
 # Load the data
 # -------------------------------------
 #
-# First we need to load the data. We will work on the two datasets
-# simultaneously.
+# First we need to load the data. We will work on both datasets
+# simultaneously. We set random state to be consistent with the result.
 
 diabetes = datasets.load_diabetes()
 cancer = datasets.load_breast_cancer()
 
+Xd, yd = diabetes.data, diabetes.target
+Xc, yc = cancer.data, cancer.target
 
-# 
-#  
-# 
-# Center target to avoid gradient boosting init bias: gradient
-# boosting
-# with the 'recursion' method does not account for the initial estimator
-# (here the average target, by default)
+##############################################################################
+# Data preprocessing
+# -------------------------------------
+#
+# Next, we will split our datasets to 90% for training and leave the rest for
+# testing. We will also prepare the parameters we want to use to fit our
+# regression model. You can play with those parameters to see how the
+# results change:
+#
+# Here:
+# n_estimators : is the number of boosting stages which will be performed.
+#     Later, we will plot and see how the deviance changes with those boosting
+#     operations.
+# max_depth : this limits the number of nodes in the tree. The best value
+#     depends on the interaction of the input variables.
+# min_samples_split : is the minimum number of samples required to split an
+#     internal node.
+# learning_rate: tells how much the contribution of each tree will shrink
+# loss: here, we decided to use least squeares as a loss function, however
+#     there are many other options (check
+#     :class:`~sklearn.ensemble.GradientBoostingRegressor` to see what are
+#     other possibilities)
+# In this example we will use the same parameters for both datasets even if
+# they might not be the most optimal
 
+X_train, X_test, y_train, y_test = train_test_split(Xd, yd,
+                                                        test_size=0.1,
+                                                        random_state=13)
 
-Xd, yd = shuffle(diabetes.data, diabetes.target, random_state=13)
-X, y = shuffle(cancer.data, cancer.target, random_state=13)
-Xd = Xd.astype(np.float32)
-X = X.astype(np.float32)
-offset = int(X.shape[0] * 0.9)
-X_train, y_train = X[:offset], y[:offset]
-X_test, y_test = X[offset:], y[offset:]
+Xc_train, Xc_test, yc_train, yc_test = train_test_split(Xc, yc,
+                                                        test_size=0.1,
+                                                        random_state=13)
+
+params = {'n_estimators': 500,
+          'max_depth': 4,
+          'min_samples_split': 5,
+          'learning_rate': 0.01,
+          'loss': 'ls'}
 
 # #############################################################################
 # Fit regression model
-params = {'n_estimators': 500, 'max_depth': 4, 'min_samples_split': 2,
-          'learning_rate': 0.01, 'loss': 'ls'}
+
 clf = ensemble.GradientBoostingRegressor(**params)
 
 clf.fit(X_train, y_train)
