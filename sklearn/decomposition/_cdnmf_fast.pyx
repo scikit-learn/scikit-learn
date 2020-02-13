b0737fc04c4f1499fedb96a4dc93fefaa68287aa
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2020-02-13 15:17:42 +0100

    cln

--- sklearn/decomposition/_cdnmf_fast.pyx
+++ sklearn/decomposition/_cdnmf_fast.pyx
@@ -1,7 +1,5 @@
-# cython: cdivision=True
-# cython: boundscheck=False
-# cython: wraparound=False
-
+# cython: cdivision=True, boundscheck=False, wraparound=False
+#
 # Author: Mathieu Blondel, Tom Dupre la Tour
 # License: BSD 3 clause
 
@@ -9,58 +7,45 @@ from cython cimport floating
 from libc.math cimport fabs
 from cython.parallel import prange
 
-
-cdef inline double _update_cdnmf_samples(unsigned n_components,double xht, double [] HHt,double[] W,double hess,unsigned t) nogil:
-    cdef double violation = 0
-    cdef double grad = 0 - xht
-    cdef double pg
-    for r in range(n_components):
-    # for(int r =0;r<n_components;r++)
-        grad += HHt[r] * W[r]
-    pg = grad
-    if (W[t] == 0) and (pg > 0):
-        pg = 0
-    violation = fabs(pg)
-    if hess != 0:
-        W[t] -= grad/hess
-        if W[t] < 0 :
-            W[t] = 0
-    return violation
+from ..utils._cython_blas cimport _dot
+from ..utils._openmp_helpers import _openmp_effective_n_threads
 
 
-def _update_cdnmf_fast(floating[:, ::1] W, floating[:, :] HHt,
-                       floating[:, :] XHt, Py_ssize_t[::1] permutation):
+def _update_cdnmf_fast(floating[:, ::1] W, floating[:, ::1] HHt,
+                       floating[:, ::1] XHt, Py_ssize_t[::1] permutation):
     cdef:
         floating violation = 0
         Py_ssize_t n_components = W.shape[1]
         Py_ssize_t n_samples = W.shape[0]  # n_features for H update
-        floating grad, pg, hess
-        Py_ssize_t i, r, s, t
+        Py_ssize_t i, s, t
+        int num_threads = _openmp_effective_n_threads()
 
-    # print "in nogil!!"
-    with nogil:
-        for s in range(n_components):
-            t = permutation[s]
-            # Hessian
-            hess = HHt[t, t]
+    for s in range(n_components):
+        t = permutation[s]
 
-            for i in prange(n_samples):
-                violation += _update_cdnmf_samples(n_components,XHt[i, t],&HHt[t,0],&W[i,0],hess,t)
-                # gradient = GW[t, i] where GW = np.dot(W, HHt) - XHt
-                # grad = -XHt[i, t]
+        for i in prange(n_samples, num_threads=num_threads, nogil=True):
+            violation += _update_cdnmf_sample(
+                n_components, &HHt[t, 0], &W[i, 0], XHt[i, t], t)
 
-                # for r in range(n_components):
-                #     grad += HHt[t, r] * W[i, r]
+    return violation
 
-                # projected gradient
-                # pg = min(0., grad) if W[i, t] == 0 else grad
-                # violation += fabs(pg)
 
-                # Hessian
-                # hess = HHt[t, t]
+cdef floating _update_cdnmf_sample(Py_ssize_t n_components,
+                                   floating* HHt,
+                                   floating* W,
+                                   floating xht,
+                                   Py_ssize_t t) nogil:
+    cdef:
+        floating hess = HHt[t]
+        floating grad, pg, violation
 
-                # if hess != 0:
-                #     W[i, t] = max(W[i, t] - grad / hess, 0.)
-                
-    # print "out nogil!!"
+    # np.dot(W[i, :], HHt[t, :]) - XHt[i, t]
+    grad = _dot(n_components, HHt, 1, W, 1) - xht
+
+    pg = min(grad, 0) if W[t] == 0 else grad
+    violation = fabs(pg)
+
+    if hess != 0:
+        W[t] = max(W[t] - grad / hess, 0)
+
     return violation
