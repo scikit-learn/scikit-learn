333c31303cbffeaaa3b066cb581850e9c03e5231
CastaChick <casta46chick@gmail.com>
2020-02-15 22:45:51 +0900

    Added links to utils.Bunch in sklearn.inspection

--- sklearn/inspection/_permutation_importance.py
+++ sklearn/inspection/_permutation_importance.py
@@ -87,7 +87,8 @@ def permutation_importance(estimator, X, y, scoring=None, n_repeats=5,
     Returns
     -------
     result : Bunch
-        Dictionary-like object, with attributes:
+        Dictionary-like object, with attributes.
+        For more information, see :class:`sklearn.utils.Bunch`:
 
         importances_mean : ndarray, shape (n_features, )
             Mean of feature importance over `n_repeats`.
