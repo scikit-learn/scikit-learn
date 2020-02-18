deacc408b69e700f663b4b696b044e854886cfdb
George Armstrong <garmstrong@colgate.edu>
2020-02-18 14:16:20 -0800

    extract arguments min_categories tests

--- sklearn/tests/test_naive_bayes.py
+++ sklearn/tests/test_naive_bayes.py
@@ -707,39 +707,55 @@ def test_categoricalnb_with_min_categories():
 
     X_n_categories = np.array([[0, 0], [0, 1], [0, 0], [1, 1]])
     y_n_categories = np.array([1, 1, 2, 2])
+    expected_prediction = np.array([1])
 
     # Check min_categories with int
-    clf = CategoricalNB(alpha=1, fit_prior=False, min_categories=3)
+    min_categories = 3
+    clf = CategoricalNB(alpha=1, fit_prior=False,
+                        min_categories=min_categories)
     clf.fit(X_n_categories, y_n_categories)
     X1_count, X2_count = clf.category_count_
-    assert_array_equal(X1_count, np.array([[2, 0, 0], [1, 1, 0]]))
-    assert_array_equal(X2_count, np.array([[1, 1, 0], [1, 1, 0]]))
-    data_with_unseen_feature_2 = np.array([[0, 2]])
-    predictions = clf.predict(data_with_unseen_feature_2)
-    assert_array_equal(predictions, np.array([1]))
-    assert_array_equal(clf.n_categories_, np.array([3, 3]))
+    exp_X1_count = np.array([[2, 0, 0], [1, 1, 0]])
+    exp_X2_count = np.array([[1, 1, 0], [1, 1, 0]])
+    assert_array_equal(X1_count, exp_X1_count)
+    assert_array_equal(X2_count, exp_X2_count)
+    new_X = np.array([[0, 2]])
+    predictions = clf.predict(new_X)
+    assert_array_equal(predictions, expected_prediction)
+    expected_n_categories_ = np.array([3, 3])
+    assert_array_equal(clf.n_categories_, expected_n_categories_)
 
     # Check min_categories with list
-    clf = CategoricalNB(alpha=1, fit_prior=False, min_categories=[3, 4])
+    min_categories = [3, 4]
+    clf = CategoricalNB(alpha=1, fit_prior=False,
+                        min_categories=min_categories)
     clf.fit(X_n_categories, y_n_categories)
     X1_count, X2_count = clf.category_count_
-    assert_array_equal(X1_count, np.array([[2, 0, 0], [1, 1, 0]]))
-    assert_array_equal(X2_count, np.array([[1, 1, 0, 0], [1, 1, 0, 0]]))
-    data_with_unseen_feature_3 = np.array([[0, 3]])
-    predictions = clf.predict(data_with_unseen_feature_3)
-    assert_array_equal(predictions, np.array([1]))
-    assert_array_equal(clf.n_categories_, np.array([3, 4]))
+    exp_X1_count = np.array([[2, 0, 0], [1, 1, 0]])
+    exp_X2_count = np.array([[1, 1, 0, 0], [1, 1, 0, 0]])
+    assert_array_equal(X1_count, exp_X1_count)
+    assert_array_equal(X2_count, exp_X2_count)
+    new_X = np.array([[0, 3]])
+    predictions = clf.predict(new_X)
+    assert_array_equal(predictions, expected_prediction)
+    expected_n_categories_ = np.array([3, 4])
+    assert_array_equal(clf.n_categories_, expected_n_categories_)
 
     # check min_categories with min less than actual
-    clf = CategoricalNB(alpha=1, fit_prior=False, min_categories=1)
+    min_categories = 1
+    clf = CategoricalNB(alpha=1, fit_prior=False,
+                        min_categories=min_categories)
     clf.fit(X_n_categories, y_n_categories)
     X1_count, X2_count = clf.category_count_
-    assert_array_equal(X1_count, np.array([[2, 0], [1, 1]]))
-    assert_array_equal(X2_count, np.array([[1, 1], [1, 1]]))
+    exp_X1_count = np.array([[2, 0], [1, 1]])
+    exp_X2_count = np.array([[1, 1], [1, 1]])
+    assert_array_equal(X1_count, exp_X1_count)
+    assert_array_equal(X2_count, exp_X2_count)
     new_X = np.array([[0, 1]])
     predictions = clf.predict(new_X)
-    assert_array_equal(predictions, np.array([1]))
-    assert_array_equal(clf.n_categories_, np.array([2, 2]))
+    assert_array_equal(predictions, expected_prediction)
+    expected_n_categories_ = np.array([2, 2])
+    assert_array_equal(clf.n_categories_, expected_n_categories_)
 
 
 @pytest.mark.parametrize(
