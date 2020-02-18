d39697ec4b5fb6daff1e4ca1ef44cc8672cab1ef
Kyle Parsons <parsons.kyle.89@gmail.com>
2020-02-17 19:03:30 -0500

    fix labels=None case

--- sklearn/metrics/tests/test_classification.py
+++ sklearn/metrics/tests/test_classification.py
@@ -919,9 +919,8 @@ def test_confusion_matrix_multiclass_subset_labels():
     ids=['None', 'binary', 'multiclass']
 )
 def test_confusion_matrix_on_zero_length_input(labels):
-    labels = [] if not labels else labels
-    n_classes = len(labels)
-    expected = np.zeros((n_classes, n_classes), dtype=np.int)
+    expected_n_classes = len(labels) if labels else 0
+    expected = np.zeros((expected_n_classes, expected_n_classes), dtype=np.int)
     cm = confusion_matrix([], [], labels)
     assert_array_equal(cm, expected)
 
