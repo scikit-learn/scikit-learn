25acd7b5476cf2cc657733b6dc880edc8e5e4366
Thomas J Fan <thomasjpfan@gmail.com>
2020-02-20 13:48:07 -0500

    STY Formating

--- examples/covariance/plot_sparse_cov.py
+++ examples/covariance/plot_sparse_cov.py
@@ -129,8 +129,7 @@ for i, (name, this_prec) in enumerate(precs):
 # plot the model selection metric
 plt.figure(figsize=(4, 3))
 plt.axes([.2, .15, .75, .7])
-plt.plot(model.cv_results_["alphas"],
-         model.cv_results_["mean_score"], 'o-')
+plt.plot(model.cv_results_["alphas"], model.cv_results_["mean_score"], 'o-')
 plt.axvline(model.alpha_, color='.5')
 plt.title('Model selection')
 plt.ylabel('Cross-validation score')
