d71ecae304dfa81008e574f6fa78e8e441089bfa
Thomas J Fan <thomasjpfan@gmail.com>
2020-02-21 16:15:20 -0500

    Double quotes are better

--- sklearn/cluster/setup.py
+++ sklearn/cluster/setup.py
@@ -25,7 +25,7 @@ def configuration(parent_package='', top_path=None):
                          include_dirs=[numpy.get_include()],
                          libraries=libraries)
 
-    config.add_extension('_k_means_fast',
+    config.add_extension("_k_means_fast",
                          sources=['_k_means_fast.pyx'],
                          include_dirs=[numpy.get_include()],
                          libraries=libraries)
