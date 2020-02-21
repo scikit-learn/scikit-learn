67f857ef6bee5edd1f981e1a324ed794e46a25ba
Ashutosh Hathidara <ashutoshhathidara98@gmail.com>
2020-02-21 12:54:07 +0530

    Resolving r2_scorer object error

--- sklearn/model_selection/tests/test_validation.py
+++ sklearn/model_selection/tests/test_validation.py
@@ -422,12 +422,12 @@ def check_cross_validate_single_metric(clf, X, y, scores):
         # Single metric passed as a list
         if return_train_score:
             # It must be True by default - deprecated
-            r2_scores_dict = cross_validate(clf, X, y, scoring=['r2'],
+            r2_scores_dict = cross_validate(clf, X, y, scoring=['r2_score'],
                                             return_train_score=True)
             assert_array_almost_equal(r2_scores_dict['train_r2'],
                                       train_r2_scores, True)
         else:
-            r2_scores_dict = cross_validate(clf, X, y, scoring=['r2'],
+            r2_scores_dict = cross_validate(clf, X, y, scoring=['r2_score'],
                                             return_train_score=False)
         assert isinstance(r2_scores_dict, dict)
         assert len(r2_scores_dict) == dict_len
@@ -446,8 +446,8 @@ def check_cross_validate_multi_metric(clf, X, y, scores):
     # Test multimetric evaluation when scoring is a list / dict
     (train_mse_scores, test_mse_scores, train_r2_scores,
      test_r2_scores, fitted_estimators) = scores
-    all_scoring = (('r2', 'neg_mean_squared_error'),
-                   {'r2': make_scorer(r2_score),
+    all_scoring = (('r2_score', 'neg_mean_squared_error'),
+                   {'r2_score': make_scorer(r2_score),
                     'neg_mean_squared_error': 'neg_mean_squared_error'})
 
     keys_sans_train = {'test_r2', 'test_neg_mean_squared_error',
