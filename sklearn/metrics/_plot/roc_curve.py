9666fa79687a9f0ee2fffddf17ce9116d0e50252
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2020-02-23 16:47:44 +0100

    BUG ensure that estimator_name is properly stored in the ROC display (#16500)

--- sklearn/metrics/_plot/roc_curve.py
+++ sklearn/metrics/_plot/roc_curve.py
@@ -165,8 +165,9 @@ def plot_roc_curve(estimator, X, y, sample_weight=None,
     """
     check_matplotlib_support('plot_roc_curve')
 
-    classification_error = ("{} should be a binary classifer".format(
-        estimator.__class__.__name__))
+    classification_error = (
+        "{} should be a binary classifier".format(estimator.__class__.__name__)
+    )
     if not is_classifier(estimator):
         raise ValueError(classification_error)
 
@@ -185,5 +186,8 @@ def plot_roc_curve(estimator, X, y, sample_weight=None,
                             sample_weight=sample_weight,
                             drop_intermediate=drop_intermediate)
     roc_auc = auc(fpr, tpr)
-    viz = RocCurveDisplay(fpr, tpr, roc_auc, estimator.__class__.__name__)
+    name = estimator.__class__.__name__ if name is None else name
+    viz = RocCurveDisplay(
+        fpr=fpr, tpr=tpr, roc_auc=roc_auc, estimator_name=name
+    )
     return viz.plot(ax=ax, name=name, **kwargs)
