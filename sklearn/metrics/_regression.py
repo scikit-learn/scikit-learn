156e6336f7a92235f3cdd302149a51d4cca4c5a1
Ashutosh Hathidara <ashutoshhathidara98@gmail.com>
2020-02-25 12:45:32 +0530

    Update sklearn/metrics/_regression.py
    Co-Authored-By: Thomas J Fan <thomasjpfan@gmail.com>

--- sklearn/metrics/_regression.py
+++ sklearn/metrics/_regression.py
@@ -206,7 +206,7 @@ def mean_absolute_percentage_error(y_true, y_pred,
     y_pred : array-like of shape (n_samples,) or (n_samples, n_outputs)
         Estimated target values.
 
-    sample_weight : array-like of shape = (n_samples,), optional
+    sample_weight : array-like of shape (n_samples,), default=None
         Sample weights.
 
     multioutput : string in ['raw_values', 'uniform_average'] or list.
