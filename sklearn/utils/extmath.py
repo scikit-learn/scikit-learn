ea1c0fb87eaa326fbb0bf31d0d391ac9210dacdc
Thomas J Fan <thomasjpfan@gmail.com>
2020-02-27 13:50:58 -0500

    WIP Testing nep37 [skip ci]

--- sklearn/utils/extmath.py
+++ sklearn/utils/extmath.py
@@ -523,6 +523,7 @@ def svd_flip(u, v, u_based_decision=True):
     if u_based_decision:
         # columns of u, rows of v
         max_abs_cols = np.argmax(np.abs(u), axis=0)
+        print(max_abs_cols)
         print(u.shape[1])
         print(range(u.shape[1]))
         print(list(range(u.shape[1])))
