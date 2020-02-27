4fe65fee8dfa6e83755fc5b1a45738a24aa6613d
Thomas J Fan <thomasjpfan@gmail.com>
2020-02-27 13:53:11 -0500

    WIP Testing nep37 [skip ci]

--- sklearn/utils/extmath.py
+++ sklearn/utils/extmath.py
@@ -523,10 +523,6 @@ def svd_flip(u, v, u_based_decision=True):
     if u_based_decision:
         # columns of u, rows of v
         max_abs_cols = np.argmax(np.abs(u), axis=0)
-        print(max_abs_cols)
-        print(u.shape[1])
-        print(range(u.shape[1]))
-        print(list(range(u.shape[1])))
         # signs = np.sign(u[max_abs_cols, list(range(u.shape[1]))])
         signs = np.sign(u[max_abs_cols, list(range(u.shape[1]))])
         u *= signs
@@ -534,7 +530,7 @@ def svd_flip(u, v, u_based_decision=True):
     else:
         # rows of v, columns of u
         max_abs_rows = np.argmax(np.abs(v), axis=1)
-        signs = np.sign(v[range(v.shape[0]), max_abs_rows])
+        signs = np.sign(v[list(range(v.shape[0])), max_abs_rows])
         u *= signs
         v *= signs[:, np.newaxis]
     return u, v
