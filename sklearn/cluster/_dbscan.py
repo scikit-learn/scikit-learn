1d80902bbbe74b1a30af4531614a156a56549628
Johan Hansson <johan@anyfin.com>
2020-03-02 20:19:06 +0100

    update for linting

--- sklearn/cluster/_dbscan.py
+++ sklearn/cluster/_dbscan.py
@@ -84,10 +84,10 @@ def dbscan(X, eps=0.5, min_samples=5, metric='minkowski', metric_params=None,
 
     n_jobs : int or None, optional (default=None)
         The number of parallel jobs to run for neighbors search. ``None`` means
-        1 unless in a :obj:`joblib.parallel_backend` context. ``-1`` means using
-        all processors. See :term:`Glossary <n_jobs>` for more details. If
-        precomputed distance are used, parallel execution is not available and
-        thus n_jobs will have no effect.
+        1 unless in a :obj:`joblib.parallel_backend` context. ``-1`` means 
+        using all processors. See :term:`Glossary <n_jobs>` for more details. 
+        If precomputed distance are used, parallel execution is not available 
+        and thus n_jobs will have no effect.
 
     Returns
     -------
