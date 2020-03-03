29431cbad00ee678bd172698422227869161eedf
lrjball <50599110+lrjball@users.noreply.github.com>
2020-03-03 22:28:36 +0000

    Fixed test so that whole test isn't skipped if pandas not installed
    As raised by @thomasjpfan in #14048, the remainder of the test will be skipped if pandas is not installed.
    
    This has been fixed in this PR by separating the test into two functions, on for arrays and one for dataframes.

--- sklearn/compose/tests/test_column_transformer.py
+++ sklearn/compose/tests/test_column_transformer.py
@@ -1040,7 +1040,7 @@ def test_column_transformer_no_estimators_set_params():
 
 
 def test_column_transformer_callable_specifier():
-    # assert that function gets the full array / dataframe
+    # assert that function gets the full array
     X_array = np.array([[0, 1, 2], [2, 4, 6]]).T
     X_res_first = np.array([[0, 1, 2]]).T
 
@@ -1055,7 +1055,13 @@ def test_column_transformer_callable_specifier():
     assert callable(ct.transformers[0][2])
     assert ct.transformers_[0][2] == [0]
 
+
+def test_column_transformer_callable_specifier_dataframe():
+    # assert that function gets the full dataframe
     pd = pytest.importorskip('pandas')
+    X_array = np.array([[0, 1, 2], [2, 4, 6]]).T
+    X_res_first = np.array([[0, 1, 2]]).T
+
     X_df = pd.DataFrame(X_array, columns=['first', 'second'])
 
     def func(X):
