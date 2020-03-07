b3e4136931c590f404d85e848c64efc8a59e78a7
lrjball <50599110+lrjball@users.noreply.github.com>
2020-03-07 16:57:38 +0000

    fixed linting issue

--- sklearn/decomposition/tests/test_kernel_pca.py
+++ sklearn/decomposition/tests/test_kernel_pca.py
@@ -285,7 +285,8 @@ def test_kernel_conditioning():
     assert np.all(kpca.lambdas_ == _check_psd_eigenvalues(kpca.lambdas_))
 
 
-@pytest.mark.parametrize("kernel", ["linear", "poly", "rbf", "sigmoid", "cosine"])
+@pytest.mark.parametrize("kernel",
+                         ["linear", "poly", "rbf", "sigmoid", "cosine"])
 def test_kernel_pca_inverse_transform_mean(kernel):
     X, *_ = make_blobs(n_samples=100, n_features=4, centers=[[1, 1, 1, 1]],
                        random_state=0)
