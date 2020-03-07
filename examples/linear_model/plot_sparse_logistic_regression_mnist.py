d18e84b89c3662f7249b269d69f1ec5f45f1ba53
Thomas J Fan <thomasjpfan@gmail.com>
2020-03-07 10:30:59 -0500

    TST Removes example text [doc build]

--- examples/linear_model/plot_sparse_logistic_regression_mnist.py
+++ examples/linear_model/plot_sparse_logistic_regression_mnist.py
@@ -36,44 +36,44 @@ t0 = time.time()
 n_samples = 6000
 
 # Load data from https://www.openml.org/d/554
-X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
-X, y = X[:n_samples], y[:n_samples]
+# X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
+# X, y = X[:n_samples], y[:n_samples]
 
-random_state = check_random_state(0)
-permutation = random_state.permutation(X.shape[0])
-X = X[permutation]
-y = y[permutation]
-X = X.reshape((X.shape[0], -1))
+# random_state = check_random_state(0)
+# permutation = random_state.permutation(X.shape[0])
+# X = X[permutation]
+# y = y[permutation]
+# X = X.reshape((X.shape[0], -1))
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, stratify=y, test_size=0.2, random_state=0)
+# X_train, X_test, y_train, y_test = train_test_split(
+#     X, y, stratify=y, test_size=0.2, random_state=0)
 
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+# scaler = StandardScaler()
+# X_train = scaler.fit_transform(X_train)
+# X_test = scaler.transform(X_test)
 
-# Turn up tolerance for faster convergence
-clf = LogisticRegression(
-    C=50. / n_samples, penalty='l1', solver='saga', tol=0.1)
-clf.fit(X_train, y_train)
-sparsity = np.mean(clf.coef_ == 0) * 100
-score = clf.score(X_test, y_test)
-# print('Best C % .4f' % clf.C_)
-print("Sparsity with L1 penalty: %.2f%%" % sparsity)
-print("Test score with L1 penalty: %.4f" % score)
+# # Turn up tolerance for faster convergence
+# clf = LogisticRegression(
+#     C=50. / n_samples, penalty='l1', solver='saga', tol=0.1)
+# clf.fit(X_train, y_train)
+# sparsity = np.mean(clf.coef_ == 0) * 100
+# score = clf.score(X_test, y_test)
+# # print('Best C % .4f' % clf.C_)
+# print("Sparsity with L1 penalty: %.2f%%" % sparsity)
+# print("Test score with L1 penalty: %.4f" % score)
 
-coef = clf.coef_.copy()
-plt.figure(figsize=(10, 5))
-scale = np.abs(coef).max()
-for i in range(10):
-    l1_plot = plt.subplot(2, 5, i + 1)
-    l1_plot.imshow(coef[i].reshape(28, 28), interpolation='nearest',
-                   cmap=plt.cm.RdBu, vmin=-scale, vmax=scale)
-    l1_plot.set_xticks(())
-    l1_plot.set_yticks(())
-    l1_plot.set_xlabel('Class %i' % i)
-plt.suptitle('Classification vector for...')
+# coef = clf.coef_.copy()
+# plt.figure(figsize=(10, 5))
+# scale = np.abs(coef).max()
+# for i in range(10):
+#     l1_plot = plt.subplot(2, 5, i + 1)
+#     l1_plot.imshow(coef[i].reshape(28, 28), interpolation='nearest',
+#                    cmap=plt.cm.RdBu, vmin=-scale, vmax=scale)
+#     l1_plot.set_xticks(())
+#     l1_plot.set_yticks(())
+#     l1_plot.set_xlabel('Class %i' % i)
+# plt.suptitle('Classification vector for...')
 
-run_time = time.time() - t0
-print('Example run in %.3f s' % run_time)
-plt.show()
+# run_time = time.time() - t0
+# print('Example run in %.3f s' % run_time)
+# plt.show()
