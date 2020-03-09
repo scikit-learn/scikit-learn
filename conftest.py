c23505bc564d3789fd5f1b8f6f4053157de488cd
Chiara Marmo <chiara.marmo@inria.fr>
2020-03-09 15:47:40 +0100

    Tentative general solution for Linux and Windows builds.

--- conftest.py
+++ conftest.py
@@ -87,6 +87,10 @@ def pytest_collection_modifyitems(config, items):
 def pytest_configure(config):
     import sys
     sys._is_pytest_session = True
+    config.addinivalue_line(
+        "markers",
+        "network: mark a test for execution if network available."
+    )
 
 
 def pytest_unconfigure(config):
