999ce14e707138f4dc0c0e46de72cbf9e65514f2
Lucy Liu <jliu176@gmail.com>
2020-03-16 22:48:45 +0100

    separate ex, red legend

--- examples/applications/plot_outlier_detection_wine.py
+++ examples/applications/plot_outlier_detection_wine.py
@@ -32,18 +32,6 @@ out the presence of two clusters (Support Vector Machines, Gaussian Mixture
 Models, univariate outlier detection, ...). But had it been a high-dimensional
 example, none of these could be applied that easily.
 
-Second example
---------------
-The second example shows the ability of the Minimum Covariance Determinant
-robust estimator of covariance to concentrate on the main mode of the data
-distribution: the location seems to be well estimated, although the covariance
-is hard to estimate due to the banana-shaped distribution. Anyway, we can
-get rid of some outlying observations.
-The One-Class SVM is able to capture the real data structure, but the
-difficulty is to adjust its kernel bandwidth parameter so as to obtain
-a good compromise between the shape of the data scatter matrix and the
-risk of over-fitting the data.
-
 """
 print(__doc__)
 
@@ -57,10 +45,6 @@ import matplotlib.pyplot as plt
 import matplotlib.font_manager
 from sklearn.datasets import load_wine
 
-# Get data
-X1 = load_wine()['data'][:, [1, 2]]  # two clusters
-X2 = load_wine()['data'][:, [6, 9]]  # "banana"-shaped
-
 # Define "classifiers" to be used
 classifiers = {
     "Empirical Covariance": EllipticEnvelope(support_fraction=1.,
@@ -72,9 +56,11 @@ colors = ['m', 'g', 'b']
 legend1 = {}
 legend2 = {}
 
+# Get data
+X1 = load_wine()['data'][:, [1, 2]]  # two clusters
+
 # Learn a frontier for outlier detection with several classifiers
 xx1, yy1 = np.meshgrid(np.linspace(0, 6, 500), np.linspace(1, 4.5, 500))
-xx2, yy2 = np.meshgrid(np.linspace(-1, 5.5, 500), np.linspace(-2.5, 19, 500))
 for i, (clf_name, clf) in enumerate(classifiers.items()):
     plt.figure(1)
     clf.fit(X1)
@@ -82,12 +68,6 @@ for i, (clf_name, clf) in enumerate(classifiers.items()):
     Z1 = Z1.reshape(xx1.shape)
     legend1[clf_name] = plt.contour(
         xx1, yy1, Z1, levels=[0], linewidths=2, colors=colors[i])
-    plt.figure(2)
-    clf.fit(X2)
-    Z2 = clf.decision_function(np.c_[xx2.ravel(), yy2.ravel()])
-    Z2 = Z2.reshape(xx2.shape)
-    legend2[clf_name] = plt.contour(
-        xx2, yy2, Z2, levels=[0], linewidths=2, colors=colors[i])
 
 legend1_values_list = list(legend1.values())
 legend1_keys_list = list(legend1.keys())
@@ -98,7 +78,7 @@ plt.title("Outlier detection on a real data set (wine recognition)")
 plt.scatter(X1[:, 0], X1[:, 1], color='black')
 bbox_args = dict(boxstyle="round", fc="0.8")
 arrow_args = dict(arrowstyle="->")
-plt.annotate("several confounded points", xy=(4, 2),
+plt.annotate("outlying points", xy=(4, 2),
              xycoords="data", textcoords="data",
              xytext=(3, 1.25), bbox=bbox_args, arrowprops=arrow_args)
 plt.xlim((xx1.min(), xx1.max()))
@@ -108,14 +88,43 @@ plt.legend((legend1_values_list[0].collections[0],
             legend1_values_list[2].collections[0]),
            (legend1_keys_list[0], legend1_keys_list[1], legend1_keys_list[2]),
            loc="upper center",
-           prop=matplotlib.font_manager.FontProperties(size=12))
+           prop=matplotlib.font_manager.FontProperties(size=11))
 plt.ylabel("ash")
 plt.xlabel("malic_acid")
+
+plt.show()
+
+##############################################################################
+# Second example
+# --------------
+# The second example shows the ability of the Minimum Covariance Determinant
+# robust estimator of covariance to concentrate on the main mode of the data
+# distribution: the location seems to be well estimated, although the covariance
+# is hard to estimate due to the banana-shaped distribution. Anyway, we can
+# get rid of some outlying observations.
+# The One-Class SVM is able to capture the real data structure, but the
+# difficulty is to adjust its kernel bandwidth parameter so as to obtain
+# a good compromise between the shape of the data scatter matrix and the
+# risk of over-fitting the data.
+
+# Get data
+X2 = load_wine()['data'][:, [6, 9]]  # "banana"-shaped
+
+# Learn a frontier for outlier detection with several classifiers
+xx2, yy2 = np.meshgrid(np.linspace(-1, 5.5, 500), np.linspace(-2.5, 19, 500))
+for i, (clf_name, clf) in enumerate(classifiers.items()):
+    plt.figure(2)
+    clf.fit(X2)
+    Z2 = clf.decision_function(np.c_[xx2.ravel(), yy2.ravel()])
+    Z2 = Z2.reshape(xx2.shape)
+    legend2[clf_name] = plt.contour(
+        xx2, yy2, Z2, levels=[0], linewidths=2, colors=colors[i])
 
 legend2_values_list = list(legend2.values())
 legend2_keys_list = list(legend2.keys())
 
-plt.figure(2)  # "banana" shape
+# Plot the results (= shape of the data points cloud)
+plt.figure(2, figsize=(10,10))  # "banana" shape
 plt.title("Outlier detection on a real data set (wine recognition)")
 plt.scatter(X2[:, 0], X2[:, 1], color='black')
 plt.xlim((xx2.min(), xx2.max()))
@@ -125,7 +134,7 @@ plt.legend((legend2_values_list[0].collections[0],
             legend2_values_list[2].collections[0]),
            (legend2_keys_list[0], legend2_keys_list[1], legend2_keys_list[2]),
            loc="upper center",
-           prop=matplotlib.font_manager.FontProperties(size=12))
+           prop=matplotlib.font_manager.FontProperties(size=11))
 plt.ylabel("color_intensity")
 plt.xlabel("flavanoids")
 
