5a673a924e90971dad32ec06326ea2ed2d28746a
Jan Niklas Böhm <mail@jnboehm.com>
2020-03-19 09:54:34 +0100

    Remove unnecessary print params

--- sklearn/tests/test_pipeline.py
+++ sklearn/tests/test_pipeline.py
@@ -405,8 +405,7 @@ def test_pipeline_methods_pca_tsne():
     pca_for_pipeline = PCA(n_components=2, random_state=0)
     tsne_for_pipeline = TSNE(random_state=0)
     msg = ("Intermediate step .*\\bTSNE\\b.* does not have "
-           "transform, pipeline is not reusable on test data."
-           % (tsne_for_pipeline, type(tsne_for_pipeline)))
+           "transform, pipeline is not reusable on test data.")
 
     with pytest.warns(UserWarning, match=msg):
         pipe = make_pipeline(pca_for_pipeline, tsne_for_pipeline,
