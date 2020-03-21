cdc1b581e2ddfee483f34098abe440f165d69171
Johann Faouzi <johann.faouzi@gmail.com>
2020-03-21 15:03:33 +0100

    Fix backslashes

--- sklearn/ensemble/_forest.py
+++ sklearn/ensemble/_forest.py
@@ -885,8 +885,8 @@ class RandomForestClassifier(ForestClassifier):
     classifiers on various sub-samples of the dataset and uses averaging to
     improve the predictive accuracy and control over-fitting.
     The sub-sample size is controlled with the `max_samples` parameter if
-    bootstrap=True (default), otherwise the whole dataset is used to build each
-    tree.
+    `bootstrap=True` (default), otherwise the whole dataset is used to build
+    each tree.
 
     Read more in the :ref:`User Guide <forest>`.
 
@@ -1202,9 +1202,9 @@ class RandomForestRegressor(ForestRegressor):
     A random forest is a meta estimator that fits a number of classifying
     decision trees on various sub-samples of the dataset and uses averaging
     to improve the predictive accuracy and control over-fitting.
-    The sub-sample size is controlled with the `max_samples parameter` if
-    bootstrap=True (default), otherwise the whole dataset is used to build each
-    tree.
+    The sub-sample size is controlled with the `max_samples` parameter if
+    `bootstrap=True` (default), otherwise the whole dataset is used to build
+    each tree.
 
     Read more in the :ref:`User Guide <forest>`.
 
