b1989d99c2d5c45eca34fd33d4e78b8e919474e1
Sanika <sanika23feb@gmail.com>
2020-03-25 18:16:27 +0100

    Indentation changes in _split.py

--- sklearn/model_selection/_split.py
+++ sklearn/model_selection/_split.py
@@ -1551,8 +1551,8 @@ class GroupShuffleSplit(ShuffleSplit):
             raise ValueError("The 'groups' parameter should not be None.")
         groups = check_array(groups, ensure_2d=False, dtype=None)
         classes, group_indices = np.unique(groups, return_inverse=True)
-        for group_train, group_test, \
-        group_validation in super()._iter_indices(X=classes):
+        for group_train, group_test, group_validation in \
+                                        super()._iter_indices(X=classes):
             # these are the indices of classes in the partition
             # invert them into data indices
             train = np.flatnonzero(np.in1d(group_indices, group_train))
