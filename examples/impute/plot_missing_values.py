fb903d33f8b47c038341de2f85af161bffe0121c
maikia <maja_ka@hotmail.com>
2020-03-25 15:23:50 +0100

    restructuring text

--- examples/impute/plot_missing_values.py
+++ examples/impute/plot_missing_values.py
@@ -31,7 +31,7 @@ print(__doc__)
 
 ###############################################################################
 # Download the data and make missing values sets
-###############################################################################
+################################################
 #
 # First we download the two datasets. Diabetes dataset is shipped with
 # scikit-learn. It has 442 entries, each with 10 features. California Housing
@@ -56,15 +56,14 @@ y_california = y_california[:500]
 
 def add_missing_values(X_full, y_full):
     n_samples, n_features = X_full.shape
-    n_features = X_full.shape[1]
 
     # Add missing values in 75% of the lines
     missing_rate = 0.75
     n_missing_samples = int(n_samples * missing_rate)
-    missing_samples = np.hstack((np.zeros(n_samples - n_missing_samples,
-                                          dtype=np.bool),
-                                 np.ones(n_missing_samples,
-                                         dtype=np.bool)))
+
+    missing_samples = np.zeros(n_samples, dtype=np.bool)
+    missing_samples[: n_missing_samples] = True
+
     rng.shuffle(missing_samples)
     missing_features = rng.randint(0, n_features, n_missing_samples)
     X_missing = X_full.copy()
@@ -83,7 +82,7 @@ X_miss_diabetes, y_miss_diabetes = add_missing_values(
 
 ###############################################################################
 # Impute the missing data and score
-# ########################################
+# #################################
 # Now we will write a function which will score the results on the differently
 # prepared data. Let's look at each imputer separately
 #
@@ -103,9 +102,9 @@ from sklearn.pipeline import make_pipeline, make_union
 N_SPLITS = 5
 REGRESSOR = RandomForestRegressor(random_state=0)
 
-##############################################################################
+###############################################################################
 # Missing information
-# -----------------------------------------------------------------------------
+# -------------------
 # In addition to using an imputing method, we can also keep an indication of
 # the missing information using :func:`sklearn.impute.MissingIndicator` which
 # might carry some information.
@@ -133,10 +132,9 @@ stds_california = np.zeros(5)
 mses_diabetes = np.zeros(5)
 stds_diabetes = np.zeros(5)
 
-
-##############################################################################
+###############################################################################
 # Estimate the score
-# -----------------------------------------------------------------------------
+# ------------------
 # First, we want to estimate the score on the original data
 #
 
@@ -153,9 +151,9 @@ mses_california[0], stds_california[0] = get_full_score(X_california,
 mses_diabetes[0], stds_diabetes[0] = get_full_score(X_diabetes, y_diabetes)
 
 
-##############################################################################
+###############################################################################
 # Replace missing values by 0
-# -----------------------------------------------------------------------------
+# ---------------------------
 #
 # Now we will estimate the score on the data where the missing values are
 # replaced by 0
@@ -179,7 +177,7 @@ mses_diabetes[1], stds_diabetes[1] = get_impute_zero_score(X_miss_diabetes,
 
 ###############################################################################
 # kNN-imputation of the missing values
-# -----------------------------------------------------------------------------
+# ------------------------------------
 #
 # With ``KNNImputer``, missing values can be imputed using the weighted
 # or unweighted mean of the desired number of nearest neighbors.
@@ -198,7 +196,7 @@ mses_diabetes[2], stds_diabetes[2] = get_impute_knn_score(X_miss_diabetes,
 
 ###############################################################################
 # Impute missing values with mean
-# -----------------------------------------------------------------------------
+# -------------------------------
 #
 
 def get_impute_mean(X_missing, y_missing):
@@ -215,7 +213,7 @@ mses_diabetes[3], stds_diabetes[3] = get_impute_mean(X_miss_diabetes,
 
 ###############################################################################
 # Iterative imputation of the missing values
-# -----------------------------------------------------------------------------
+# ------------------------------------------
 #
 # Another option is the :class:`sklearn.impute.IterativeImputer`. This uses
 # round-robin linear regression, treating every variable as an output in turn.
@@ -245,7 +243,7 @@ mses_california = mses_california * -1
 
 ###############################################################################
 # Plot the results
-###############################################################################
+##################
 #
 # Finally we are going to visualize the score
 #
