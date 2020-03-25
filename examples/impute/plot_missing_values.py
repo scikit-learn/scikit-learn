5ef729c43a31b94a0cbaa282cc686efb599f7785
maikia <maja_ka@hotmail.com>
2020-03-25 15:13:43 +0100

    changed the naming

--- examples/impute/plot_missing_values.py
+++ examples/impute/plot_missing_values.py
@@ -184,15 +184,15 @@ mses_diabetes[1], stds_diabetes[1] = get_impute_zero_score(X_miss_diabetes,
 # With ``KNNImputer``, missing values can be imputed using the weighted
 # or unweighted mean of the desired number of nearest neighbors.
 
-def get_impute_KNN_score(X_missing, y_missing):
+def get_impute_knn_score(X_missing, y_missing):
     imputer = KNNImputer(missing_values=0)
     knn_impute_scores = get_scores_for_imputer(imputer, X_missing, y_missing)
     return knn_impute_scores.mean(), knn_impute_scores.std()
 
 
-mses_california[2], stds_california[2] = get_impute_KNN_score(
+mses_california[2], stds_california[2] = get_impute_knn_score(
     X_miss_california, y_miss_california)
-mses_diabetes[2], stds_diabetes[2] = get_impute_KNN_score(X_miss_diabetes,
+mses_diabetes[2], stds_diabetes[2] = get_impute_knn_score(X_miss_diabetes,
                                                           y_miss_diabetes)
 
 
