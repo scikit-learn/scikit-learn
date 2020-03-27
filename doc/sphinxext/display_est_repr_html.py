f656d8bb274f5c7afb6bce69e63e836dfaac2905
Thomas J Fan <thomasjpfan@gmail.com>
2020-03-26 21:54:16 -0400

    CLN Outputs repr in latex

--- doc/sphinxext/display_est_repr_html.py
+++ doc/sphinxext/display_est_repr_html.py
@@ -2,22 +2,22 @@
 Primary used to display the html output of `_repr_html_` of estimators
 """
 import sys
-from docutils.parsers.rst import Directive
+from sphinx.util.docutils import SphinxDirective
 from docutils import nodes
 from io import StringIO
 
 
-class DisplayEstimatorRepr(Directive):
+class DisplayEstimatorRepr(SphinxDirective):
     "Execute Python code and includes stdout as HTML"
 
     has_content = True
     required_arguments = 0
     optional_arguments = 0
 
-    def execute(self, code):
+    def execute(self, code, format_str):
         code_parts = code.split('\n')
         final_output = code_parts[-1]
-        code_parts[-1] = f'print({final_output}._repr_html_())'
+        code_parts[-1] = format_str.format(final_output)
         code = '\n'.join(code_parts)
         orig_stdout, orig_stderr = sys.stdout, sys.stderr
 
@@ -32,23 +32,31 @@ class DisplayEstimatorRepr(Directive):
     def run(self):
         output = []
         code = "\n".join(self.content)
-        code_results = self.execute(code)
+        html_repr = self.execute(code, format_str='print({}._repr_html_())')
 
         input_code = nodes.literal_block(code, code)
         input_code['language'] = 'python'
         output.append(input_code)
 
-        html_node = nodes.raw('', code_results, format='html')
+        html_node = nodes.raw('', html_repr, format='html')
         output.append(html_node)
 
-        code_results_latex = r"""
-        \begin{sphinxadmonition}{note}{Note:}
-        The HTML output of this code snippet can only been seen on the HTML
-        version of the documentation
-        \end{sphinxadmonition}
-        """
-        latex_node = nodes.raw('', code_results_latex, format='latex')
-        output.append(latex_node)
+        if self.env.app.builder.name == 'latex':
+            code_results_latex = r"""
+            \begin{sphinxadmonition}{note}{Note:}
+            The HTML output of this code snippet can only been seen on the HTML
+            version of the documentation. The following is a string
+            representation.
+            \end{sphinxadmonition}
+            """
+            latex_node = nodes.raw('', code_results_latex, format='latex')
+            output.append(latex_node)
+
+            str_repr = self.execute(code, format_str='print(repr({}))')
+            str_repr_node = nodes.literal_block(str_repr, str_repr)
+            str_repr_node['language'] = 'python'
+            output.append(str_repr_node)
+
         return output
 
 
