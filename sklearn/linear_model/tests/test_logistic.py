d207c05e070cb4b9dd5b7fff23cc6943d8030b84
Christian Lorentzen <lorentzen.ch@googlemail.com>
2020-03-28 19:18:47 +0100

    TST add test for symmetric identifiability constraint

--- sklearn/linear_model/tests/test_logistic.py
+++ sklearn/linear_model/tests/test_logistic.py
@@ -1825,3 +1825,43 @@ def test_scores_attribute_layout_elasticnet():
 
             avg_score_lr = cross_val_score(lr, X, y, cv=cv).mean()
             assert avg_scores_lrcv[i, j] == pytest.approx(avg_score_lr)
+
+
+@pytest.mark.parametrize("fit_intercept", [False, True])
+def test_multinomial_identifiability_on_iris(fit_intercept):
+    """Test that the multinomial classification is identifiable.
+
+    A multinomial with c classes can be modeled with
+    probability_k = exp(X@coef_k) / sum(X@coef_l, l=1..c) for k=1..c.
+    This is not identifiable, unless one chooses a further constraint.
+    According to [1], the maximum of the L2 penalized likelihood automatically
+    satisfies the symmetric constraint:
+    sum(coef_k, k=1..c) = 0
+
+    Further details can be found in the appendix of [2].
+
+    Reference
+    ---------
+    .. [1] Zhu, Ji and Trevor J. Hastie. "Classification of gene microarrays by
+    penalized logistic regression". Biostatistics 5 3 (2004): 427-43.
+    https://doi.org/10.1093/biostatistics%2Fkxg046
+
+    .. [2] Powers, Scott, Trevor J. Hastie and Robert Tibshirani. "Nuclear
+    penalized multinomial regression with an application to predicting at bat
+    outcomes in baseball." Statistical modelling 18 5-6 (2017): 388-410 .
+    https://arxiv.org/pdf/1706.10272.pdf
+    """
+    # Test logistic regression with the iris dataset
+    n_samples, n_features = iris.data.shape
+    target = iris.target_names[iris.target]
+
+    clf = LogisticRegression(C=len(iris.data), solver='lbfgs', max_iter=300,
+                             multi_class='multinomial',
+                             fit_intercept=fit_intercept
+                             )
+    clf.fit(iris.data, target)
+
+    # axis=0 is sum over classes
+    assert_array_almost_equal(clf.coef_.sum(axis=0), np.zeros(n_features))
+    if fit_intercept:
+        assert_array_almost_equal(clf.intercept_.sum(axis=0), 0)
