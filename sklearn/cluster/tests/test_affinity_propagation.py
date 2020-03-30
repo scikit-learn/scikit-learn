b474ab8ca61d49237c028d99abe15bb4a576d90c
Chiara Marmo <chiara.marmo@u-psud.fr>
2020-03-30 17:03:28 +0200

    Address comment.

--- sklearn/cluster/tests/test_affinity_propagation.py
+++ sklearn/cluster/tests/test_affinity_propagation.py
@@ -197,9 +197,7 @@ def test_affinity_propagation_random_state():
     ap.fit(X)
     centers76 = ap.cluster_centers_
 
-    abs_val_diff = np.sum((centers0 - centers76) ** 2)
-
-    assert abs_val_diff > 0.01
+    assert np.mean((centers0 - centers76) ** 2) > 1
 
 
 @pytest.mark.parametrize('centers', [csr_matrix(np.zeros((1, 10))),
