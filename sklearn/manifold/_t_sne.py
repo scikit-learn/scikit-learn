5a4340834d23c4bdcd813ccda24a690ae174c168
Roman Yurchak <rth.yurchak@gmail.com>
2020-03-30 15:41:05 +0200

    Fix mypy errors (#16726)

--- sklearn/manifold/_t_sne.py
+++ sklearn/manifold/_t_sne.py
@@ -22,7 +22,8 @@ from ..utils.validation import check_non_negative
 from ..decomposition import PCA
 from ..metrics.pairwise import pairwise_distances
 from . import _utils
-from . import _barnes_hut_tsne
+# mypy error: Module 'sklearn.manifold' has no attribute '_barnes_hut_tsne'
+from . import _barnes_hut_tsne  # type: ignore
 
 
 MACHINE_EPSILON = np.finfo(np.double).eps
