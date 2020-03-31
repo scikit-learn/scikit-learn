7a16094e7cbe2444b3f4a45822c82b8ffca410e3
Chiara Marmo <chiara.marmo@u-psud.fr>
2020-03-31 21:57:38 +0200

    Tentative fix for failures.

--- sklearn/cluster/_affinity_propagation.py
+++ sklearn/cluster/_affinity_propagation.py
@@ -322,7 +322,9 @@ class AffinityPropagation(ClusterMixin, BaseEstimator):
     ...               [4, 2], [4, 4], [4, 0]])
     >>> clustering = AffinityPropagation(random_state=45).fit(X)
     >>> clustering
-    AffinityPropagation()
+    AffinityPropagation(affinity='euclidean', convergence_iter=15, copy=True,
+                        damping=0.5, max_iter=200, preference=None,
+                        random_state=45, verbose=False)
     >>> clustering.labels_
     array([0, 0, 0, 1, 1, 1])
     >>> clustering.predict([[0, 0], [4, 4]])
