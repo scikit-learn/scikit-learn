a172192101b4b39f04c9a8389b545d902802ffdc
wornbb <wornbb@gmail.com>
2020-04-03 19:59:35 -0400

    conforming with PEP8

--- benchmarks/bench_tsvd_transform_csc.py
+++ benchmarks/bench_tsvd_transform_csc.py
@@ -1,18 +1,18 @@
 """
-Benchmarks of with vs without CSR conversion   
+Benchmarks of with vs without CSR conversion
 
-The performances are evaluated in various input dimensions.   
+The performances are evaluated in various input dimensions.
 
 The input array X is a n x n square matrix.
 
-Time is calculated by runing the transform 10 times respectively for each method.
+Time is calculated by runing the transform 10 times for each method.
 
 Performance Comparison
 ======================
 Input_Dimensions Time_With_CSR_Conversion Time_Without_Conversion
 -----------------------------------------------------------------
-      1000               0.3052s                  0.2708s        
-      2000               1.6780s                  1.7055s        
+      1000               0.3052s                  0.2708s
+      2000               1.6780s                  1.7055s
       3000               3.9639s                  4.0052s
       4000               7.5915s                  7.2595s
       5000               12.2266s                 12.1459s
@@ -25,7 +25,9 @@ import scipy.sparse as ss
 from sklearn.decomposition import TruncatedSVD
 from time import time
 from sklearn.utils import check_array
-def benchmark_converted_transform(dim, trials=10):
+
+
+def benchmark_converted(dim, trials=10):
     start_time = time()
     for i in range(trials):
         X = ss.random(dim, dim, format='csc')
@@ -33,22 +35,30 @@ def benchmark_converted_transform(dim, trials=10):
         tsvd = TruncatedSVD().fit(X)
         tsvd.transform(X)
     return time() - start_time
-def benchmark_direct_transform(dim, trials=10):
+
+
+def benchmark_direct(dim, trials=10):
     start_time = time()
     for i in range(trials):
         X = ss.random(dim, dim, format='csc')
         tsvd = TruncatedSVD().fit(X)
         tsvd.transform(X)
     return time() - start_time
+
+
 if __name__ == '__main__':
     print("Performance Comparison")
     print("======================")
-    headers = ("Input_Dimensions", "Time_With_CSR_Conversion", "Time_Without_Conversion")
+    headers = ("Input_Dimensions",
+               "Time_With_CSR_Conversion",
+               "Time_Without_Conversion")
     header_len = [len(header) for header in headers]
     print("%s %s %s"
           % headers)
     print("-" * (sum(header_len) + 2))
     for dim in range(1000, 10000, 1000):
-        print("%s %s %s" % ( ("%d" % dim).center(header_len[0]),
-                            ("%.4fs" % benchmark_converted_transform(dim)).center(header_len[1]),
-                            ("%.4fs" % benchmark_direct_transform(dim)).center(header_len[2])))
+        t1 = benchmark_converted(dim)
+        t2 = benchmark_direct(dim)
+        print("%s %s %s" % (("%d" % dim).center(header_len[0]),
+                            ("%.4fs" % t1).center(header_len[1]),
+                            ("%.4fs" % t2).center(header_len[2])))
