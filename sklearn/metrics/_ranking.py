13030bd01ebbd8e6bfe44828e84e8b9ada14c545
gbolmier <geoffrey.bolmier@gmail.com>
2020-04-10 20:55:24 -0400

    Fix top k accuracy score docstring test bug

--- sklearn/metrics/_ranking.py
+++ sklearn/metrics/_ranking.py
@@ -1481,6 +1481,7 @@ def top_k_accuracy_score(y_true, y_score, k=3, normalize=True,
     ...                     [0.7, 0.2, 0.1]]) # 2 isn't in top 2
     >>> top_k_accuracy_score(y_true, y_score, k=2)
     0.75
+
     # Not normalizing gives the number of "correctly" classified samples
     >>> top_k_accuracy_score(y_true, y_score, k=2, normalize=False)
     3
