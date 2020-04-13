b72c18c62b61b071d5351131a39e10c57a38f734
mdabbah <34287255+mdabbah@users.noreply.github.com>
2020-04-13 12:03:08 +0300

    solved bug in _initialize precisions_cholesky_
    precisions_cholesky_ in GaussianMixture._initialize should be the square root of the given precision matrix in the diagonal case

--- sklearn/mixture/_gaussian_mixture.py
+++ sklearn/mixture/_gaussian_mixture.py
@@ -656,7 +656,7 @@ class GaussianMixture(BaseMixture):
             self.precisions_cholesky_ = linalg.cholesky(self.precisions_init,
                                                         lower=True)
         else:
-            self.precisions_cholesky_ = self.precisions_init
+            self.precisions_cholesky_ = np.sqrt(self.precisions_init)
 
     def _m_step(self, X, log_resp):
         """M step.
