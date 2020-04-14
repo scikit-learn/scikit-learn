fb427eaa7915bc17692c4e242fdb5a1095fb1108
adrinjalali <adrin.jalali@gmail.com>
2020-04-14 10:40:14 +0200

    allow positional n_features

--- sklearn/feature_extraction/_hash.py
+++ sklearn/feature_extraction/_hash.py
@@ -86,7 +86,7 @@ class FeatureHasher(TransformerMixin, BaseEstimator):
     sklearn.preprocessing.OneHotEncoder : handles nominal/categorical features.
     """
     @_deprecate_positional_args
-    def __init__(self, *, n_features=(2 ** 20), input_type="dict",
+    def __init__(self, n_features=(2 ** 20), *, input_type="dict",
                  dtype=np.float64, alternate_sign=True):
         self._validate_params(n_features, input_type)
 
