f257ff108ba88fa5084675465676411e387a772f
Lucy Liu <jliu176@gmail.com>
2020-04-16 12:31:26 +0200

    pytest network

--- sklearn/ensemble/tests/test_gradient_boosting.py
+++ sklearn/ensemble/tests/test_gradient_boosting.py
@@ -245,6 +245,7 @@ def check_california(loss, subsample):
         last_y_pred = y_pred
 
 
+@pytest.mark.network
 @pytest.mark.parametrize('loss', ('ls', 'lad', 'huber'))
 @pytest.mark.parametrize('subsample', (1.0, 0.5))
 def test_california(loss, subsample):
