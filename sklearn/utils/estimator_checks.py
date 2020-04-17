e4e95a994bd9453c9eb7e9013d9550cb781cba34
Nicolas Hug <contact@nicolas-hug.com>
2020-04-17 15:52:58 -0400

    hopefully fixed docstring

--- sklearn/utils/estimator_checks.py
+++ sklearn/utils/estimator_checks.py
@@ -389,9 +389,9 @@ def parametrize_with_checks(estimators):
     >>> from sklearn.linear_model import LogisticRegression
     >>> from sklearn.tree import DecisionTreeRegressor
 
-    >>> @parametrize_with_checks([LogisticRegression, DecisionTreeRegressor])
+    ... @parametrize_with_checks([LogisticRegression, DecisionTreeRegressor])
     >>> def test_sklearn_compatible_estimator(estimator, check):
-    >>>     check(estimator)
+    ...    check(estimator)
 
     """
     import pytest
