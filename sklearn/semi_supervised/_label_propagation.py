622e7fc9627571c18604fc3f5c07651faafc8833
Thomas J Fan <thomasjpfan@gmail.com>
2020-04-22 15:48:28 -0400

    CLN Address comments

--- sklearn/semi_supervised/_label_propagation.py
+++ sklearn/semi_supervised/_label_propagation.py
@@ -107,7 +107,7 @@ class BaseLabelPropagation(ClassifierMixin, BaseEstimator, metaclass=ABCMeta):
     """
 
     @_deprecate_positional_args
-    def __init__(self, *, kernel='rbf', gamma=20, n_neighbors=7,
+    def __init__(self, kernel='rbf', *, gamma=20, n_neighbors=7,
                  alpha=1, max_iter=30, tol=1e-3, n_jobs=None):
 
         self.max_iter = max_iter
@@ -381,7 +381,7 @@ class LabelPropagation(BaseLabelPropagation):
     _variant = 'propagation'
 
     @_deprecate_positional_args
-    def __init__(self, *, kernel='rbf', gamma=20, n_neighbors=7,
+    def __init__(self, kernel='rbf', *, gamma=20, n_neighbors=7,
                  max_iter=1000, tol=1e-3, n_jobs=None):
         super().__init__(kernel=kernel, gamma=gamma,
                          n_neighbors=n_neighbors, max_iter=max_iter,
@@ -495,7 +495,7 @@ class LabelSpreading(BaseLabelPropagation):
     _variant = 'spreading'
 
     @_deprecate_positional_args
-    def __init__(self, *, kernel='rbf', gamma=20, n_neighbors=7, alpha=0.2,
+    def __init__(self, kernel='rbf', *, gamma=20, n_neighbors=7, alpha=0.2,
                  max_iter=30, tol=1e-3, n_jobs=None):
 
         # this one has different base parameters
