a28aacbba02e03d9e781271f04adab6e0593dc99
Thomas J Fan <thomasjpfan@gmail.com>
2020-04-26 14:49:34 -0400

    REV Less diffs

--- sklearn/exceptions.py
+++ sklearn/exceptions.py
@@ -22,6 +22,17 @@ class NotFittedError(ValueError, AttributeError):
     This class inherits from both ValueError and AttributeError to help with
     exception handling and backward compatibility.
 
+    Examples
+    --------
+    >>> from sklearn.svm import LinearSVC
+    >>> from sklearn.exceptions import NotFittedError
+    >>> try:
+    ...     LinearSVC().predict([[1, 2], [2, 3], [3, 4]])
+    ... except NotFittedError as e:
+    ...     print(repr(e))
+    NotFittedError("This LinearSVC instance is not fitted yet. Call 'fit' with
+    appropriate arguments before using this estimator."...)
+
     .. versionchanged:: 0.18
        Moved from sklearn.utils.validation.
     """
