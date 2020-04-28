af53e2b76f8cb34264e46e5969924948a6ca4863
Thomas J Fan <thomasjpfan@gmail.com>
2020-04-28 12:45:14 -0400

    DOC Adds release highlights to main page

--- doc/conf.py
+++ doc/conf.py
@@ -17,6 +17,7 @@ import os
 import warnings
 import re
 from packaging.version import parse
+from pathlib import Path
 
 # If extensions (or modules to document with autodoc) are in another
 # directory, add these directories to sys.path here. If the directory
@@ -207,6 +208,16 @@ htmlhelp_basename = 'scikit-learndoc'
 
 # If true, the reST sources are included in the HTML build as _sources/name.
 html_copy_source = True
+
+# Adds variables into templates
+html_context = {}
+# finds latest release highlights and places it into HTML context for
+# index.html
+release_highlights_dir = Path("..") / "examples" / "release_highlights"
+latest_highlights = sorted(release_highlights_dir.glob("*.py"))[-1]
+latest_highlights = latest_highlights.with_suffix('').name
+html_context["release_highlights"] = \
+    f"auto_examples/release_highlights/{latest_highlights}"
 
 # -- Options for LaTeX output ------------------------------------------------
 latex_elements = {
