01d6a7be3fe82e30783843533b4cdb8c491f918f
Thomas J Fan <thomasjpfan@gmail.com>
2020-04-29 18:31:58 -0400

    ENH Adds HTML visualizations for estimators (#14180)

--- sklearn/base.py
+++ sklearn/base.py
@@ -17,9 +17,11 @@ import re
 import numpy as np
 
 from . import __version__
+from ._config import get_config
 from .utils import _IS_32BIT
 from .utils.validation import check_X_y
 from .utils.validation import check_array
+from .utils._estimator_html_repr import estimator_html_repr
 from .utils.validation import _deprecate_positional_args
 
 _DEFAULT_TAGS = {
@@ -434,6 +436,17 @@ class BaseEstimator:
             self._check_n_features(X, reset=reset)
 
         return out
+
+    def _repr_html_(self):
+        """HTML representation of estimator"""
+        return estimator_html_repr(self)
+
+    def _repr_mimebundle_(self, **kwargs):
+        """Mime bundle used by jupyter kernels to display estimator"""
+        output = {"text/plain": repr(self)}
+        if get_config()["display"] == 'diagram':
+            output["text/html"] = estimator_html_repr(self)
+        return output
 
 
 class ClassifierMixin:
