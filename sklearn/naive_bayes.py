927d40154348fbc5e80ccb2ea070f780b981ded0
raibosome <raimi.bkarim@gmail.com>
2020-05-04 21:08:27 +0800

    Move prior checking to fit

--- sklearn/naive_bayes.py
+++ sklearn/naive_bayes.py
@@ -221,6 +221,21 @@ class GeneralNB(_BaseNB, _BaseComposition, ClassifierMixin):
             for (name, nb_model, _), cols
             in zip(self.models, self._cols)]
 
+        # Obtain the log priors from each fitted estimator
+        all_log_priors = [
+            nb_model.class_log_prior_
+            if hasattr(nb_model, 'class_log_prior_')
+            else np.log(nb_model.class_prior_)
+            for _, nb_model, _ in self.models_]
+
+        # Ensure class log priors are the same for all estimators
+        all_log_priors = np.hstack([all_log_priors])
+        if np.max(np.ptp(all_log_priors, axis=0)) < 1e-6:
+            log_prior = all_log_priors[0]
+        else:
+            raise ValueError("Class priors for every estimator "
+                             "must be the same.")
+
         self._is_fitted = True
 
         return self
@@ -247,21 +262,6 @@ class GeneralNB(_BaseNB, _BaseComposition, ClassifierMixin):
         if not self._is_fitted:
             raise NotFittedError("Call the fit() method first "
                                  "before calling predict().")
-
-        # Obtain the log priors from each fitted estimator
-        all_log_priors = [
-            nb_model.class_log_prior_
-            if hasattr(nb_model, 'class_log_prior_')
-            else np.log(nb_model.class_prior_)
-            for _, nb_model, _ in self.models_]
-
-        # Ensure class log priors are the same for all estimators
-        all_log_priors = np.hstack([all_log_priors])
-        if np.max(np.ptp(all_log_priors, axis=0)) < 1e-6:
-            log_prior = all_log_priors[0]
-        else:
-            raise ValueError("Class priors for every estimator "
-                             "must be the same.")
 
         # FIXME
         # Obtain the jll of each fitted estimator
