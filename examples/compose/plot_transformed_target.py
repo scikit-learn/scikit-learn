349f26551a67682aacc94ccfbac8cb55717625c6
Lucy Liu <jliu176@gmail.com>
2020-05-05 21:58:41 +0200

    tight layout

--- examples/compose/plot_transformed_target.py
+++ examples/compose/plot_transformed_target.py
@@ -159,7 +159,7 @@ ax1.set_xlabel('Target')
 ax1.text(s='Transformed target distribution', x=-6.8, y=0.4888, fontsize=12)
 
 f.suptitle("Ames housing data: selling price", y=0.022)
-f.tight_layout(rect=[0.05, 0.05, 0.95, 0.95])
+f.tight_layout(rect=[0., 0., 0.95, 0.95])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
 
