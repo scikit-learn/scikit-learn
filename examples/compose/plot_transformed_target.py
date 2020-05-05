37de7135b986acd487bc9a8025b45cee88ddbed2
Lucy Liu <jliu176@gmail.com>
2020-05-05 19:13:34 +0200

    plot formatting

--- examples/compose/plot_transformed_target.py
+++ examples/compose/plot_transformed_target.py
@@ -6,28 +6,20 @@
 Effect of transforming the targets in regression model
 ======================================================
 
-In this example, we give an overview of the
-:class:`sklearn.compose.TransformedTargetRegressor`. Two examples
-illustrate the benefit of transforming the targets before learning a linear
+In this example, we give an overview of
+:class:`~sklearn.compose.TransformedTargetRegressor`. We use two examples
+to illustrate the benefit of transforming the targets before learning a linear
 regression model. The first example uses synthetic data while the second
-example is based on the Boston housing data set.
-
+example is based on the Ames housing data set.
 """
 
 # Author: Guillaume Lemaitre <guillaume.lemaitre@inria.fr>
 # License: BSD 3 clause
 
-
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 from distutils.version import LooseVersion
-
-print(__doc__)
-
-###############################################################################
-# Synthetic example
-###############################################################################
 
 from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
@@ -35,6 +27,9 @@ from sklearn.linear_model import RidgeCV
 from sklearn.compose import TransformedTargetRegressor
 from sklearn.metrics import median_absolute_error, r2_score
 
+###############################################################################
+# Synthetic example
+##############################################################################
 
 # `normed` is being deprecated in favor of `density` in histograms
 if LooseVersion(matplotlib.__version__) >= '2.1':
@@ -43,10 +38,12 @@ else:
     density_param = {'normed': True}
 
 ###############################################################################
-# A synthetic random regression problem is generated. The targets ``y`` are
-# modified by: (i) translating all targets such that all entries are
-# non-negative and (ii) applying an exponential function to obtain non-linear
-# targets which cannot be fitted using a simple linear model.
+# A synthetic random regression dataset is generated. The targets ``y`` are
+# modified by:
+#   1. translating all targets such that all entries are
+#      non-negative (by adding the absolute value of the lowest ``y``) and
+#   2. applying an exponential function to obtain non-linear
+#      targets which cannot be fitted using a simple linear model.
 #
 # Therefore, a logarithmic (`np.log1p`) and an exponential function
 # (`np.expm1`) will be used to transform the targets before training a linear
@@ -57,7 +54,7 @@ y = np.exp((y + abs(y.min())) / 200)
 y_trans = np.log1p(y)
 
 ###############################################################################
-# The following illustrate the probability density functions of the target
+# Below we plot the probability density functions of the target
 # before and after applying the logarithmic functions.
 
 f, (ax0, ax1) = plt.subplots(1, 2)
@@ -73,7 +70,7 @@ ax1.set_ylabel('Probability')
 ax1.set_xlabel('Target')
 ax1.set_title('Transformed target distribution')
 
-f.suptitle("Synthetic data", y=0.035)
+f.suptitle("Synthetic data", y=0.06, x=0.53)
 f.tight_layout(rect=[0.05, 0.05, 0.95, 0.95])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
@@ -86,11 +83,11 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 # reported by the median absolute error (MAE).
 
 f, (ax0, ax1) = plt.subplots(1, 2, sharey=True)
-
+# Use linear model
 regr = RidgeCV()
 regr.fit(X_train, y_train)
 y_pred = regr.predict(X_test)
-
+# Plot results
 ax0.scatter(y_test, y_pred)
 ax0.plot([0, 2000], [0, 2000], '--k')
 ax0.set_ylabel('Target predicted')
@@ -100,7 +97,7 @@ ax0.text(100, 1750, r'$R^2$=%.2f, MAE=%.2f' % (
     r2_score(y_test, y_pred), median_absolute_error(y_test, y_pred)))
 ax0.set_xlim([0, 2000])
 ax0.set_ylim([0, 2000])
-
+# Transform targets and use same linear model
 regr_trans = TransformedTargetRegressor(regressor=RidgeCV(),
                                         func=np.log1p,
                                         inverse_func=np.expm1)
@@ -127,15 +124,15 @@ f.tight_layout(rect=[0.05, 0.05, 0.95, 0.95])
 ###############################################################################
 # In a similar manner, the Ames housing data set is used to show the impact
 # of transforming the targets before learning a model. In this example, the
-# targets to be predicted is the selling price of each house.
+# target to be predicted is the selling price of each house.
 
 from sklearn.datasets import fetch_openml
 from sklearn.preprocessing import QuantileTransformer, quantile_transform
 
 ames = fetch_openml(name="house_prices", as_frame=True)
-# keep only numeric columns
+# Keep only numeric columns
 X = ames.data.select_dtypes(np.number)
-# remove columns with NaN or Inf values
+# Remove columns with NaN or Inf values
 X = X.drop(columns=['LotFrontage', 'GarageYrBlt', 'MasVnrArea'])
 y = ames.target
 y_trans = quantile_transform(y.to_frame(),
@@ -144,16 +141,17 @@ y_trans = quantile_transform(y.to_frame(),
                              copy=True).squeeze()
 
 ###############################################################################
-# A :class:`sklearn.preprocessing.QuantileTransformer` is used such that the
-# targets follows a normal distribution before applying a
-# :class:`sklearn.linear_model.RidgeCV` model.
+# A :class:`~sklearn.preprocessing.QuantileTransformer` is used to normalize
+# the target distribution before applying a
+# :class:`~sklearn.linear_model.RidgeCV` model.
 
-f, (ax0, ax1) = plt.subplots(1, 2)
+f, (ax0, ax1) = plt.subplots(1, 2, sharey='row')
 
 ax0.hist(y, bins=100, **density_param)
 ax0.set_ylabel('Probability')
 ax0.set_xlabel('Target')
 ax0.set_title('Target distribution')
+ax0.ticklabel_format(axis="x", style="sci", scilimits=(0, 0))
 
 ax1.hist(y_trans, bins=100, **density_param)
 ax1.set_ylabel('Probability')
