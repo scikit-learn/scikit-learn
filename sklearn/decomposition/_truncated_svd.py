eb271401b994d278ee0ad200ce0127f6561e727f
Nicolas Hug <contact@nicolas-hug.com>
2020-05-06 11:14:33 -0400

    fixed docstring

--- sklearn/decomposition/_truncated_svd.py
+++ sklearn/decomposition/_truncated_svd.py
@@ -88,7 +88,6 @@ class TruncatedSVD(TransformerMixin, BaseEstimator):
     --------
     >>> from sklearn.decomposition import TruncatedSVD
     >>> from scipy.sparse import random as sparse_random
-    >>> from sklearn.random_projection import sparse_random_matrix
     >>> X = sparse_random(100, 100, density=0.01, format='csr',
     ...                   random_state=42)
     >>> svd = TruncatedSVD(n_components=5, n_iter=7, random_state=42)
