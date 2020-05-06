91b35cb1ad5c1e0263c4be47d0b581ddbc0c1ded
Nicolas Hug <contact@nicolas-hug.com>
2020-05-06 09:30:50 -0400

    removed iid parameter of search estimators

--- sklearn/manifold/_isomap.py
+++ sklearn/manifold/_isomap.py
@@ -168,16 +168,6 @@ class Isomap(TransformerMixin, BaseEstimator):
 
         self.embedding_ = self.kernel_pca_.fit_transform(G)
 
-    # mypy error: Decorated property not supported
-    @deprecated(  # type: ignore
-        "Attribute `training_data_` was deprecated in version 0.22 and"
-        " will be removed in 0.24."
-    )
-    @property
-    def training_data_(self):
-        check_is_fitted(self)
-        return self.nbrs_._fit_X
-
     def reconstruction_error(self):
         """Compute the reconstruction error for the embedding.
 
