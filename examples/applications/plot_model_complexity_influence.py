06efb4a6066033239869dccc8cf0219cc8235d2e
maikia <maja_ka@hotmail.com>
2020-05-11 10:49:44 +0200

    Update examples/applications/plot_model_complexity_influence.py
    Co-authored-by: Lucy Liu <jliu176@gmail.com>

--- examples/applications/plot_model_complexity_influence.py
+++ examples/applications/plot_model_complexity_influence.py
@@ -97,7 +97,7 @@ classification_data = generate_data('classification')
 # Next, we can calculate the influence of the parameters on the given
 # estimator. In each lap we will set the estimator with the new value of
 # ``changing_param`` and we will be collecting the prediction times, prediction
-# powers and complexities to see how those changes affect the estimator.  We
+# performance and complexities to see how those changes affect the estimator.  We
 # will calculate the complexity using 'complexity_computer' passed as a
 # parameter.
 #
