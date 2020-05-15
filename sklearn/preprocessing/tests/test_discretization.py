107da4a2fe3c854105791499fb9dec38a40b98e5
Mateusz Górski <mateuszg122@gmail.com>
2020-05-15 15:39:53 +0200

    Correct tests

--- sklearn/preprocessing/tests/test_discretization.py
+++ sklearn/preprocessing/tests/test_discretization.py
@@ -38,13 +38,13 @@ def test_valid_n_bins():
 
 
 def test_invalid_n_bins():
-    est = KBinsDiscretizer(n_bins=1)
+    est = KBinsDiscretizer(n_bins=1, subsample=None)
     err_msg = ("KBinsDiscretizer received an invalid "
                "number of bins. Received 1, expected at least 2.")
     with pytest.raises(ValueError, match=err_msg):
         est.fit_transform(X)
 
-    est = KBinsDiscretizer(n_bins=1.1)
+    est = KBinsDiscretizer(n_bins=1.1, subsample=None)
     err_msg = ("KBinsDiscretizer received an invalid "
                "n_bins type. Received float, expected int.")
     with pytest.raises(ValueError, match=err_msg):
@@ -54,21 +54,21 @@ def test_invalid_n_bins():
 def test_invalid_n_bins_array():
     # Bad shape
     n_bins = np.full((2, 4), 2.)
-    est = KBinsDiscretizer(n_bins=n_bins)
+    est = KBinsDiscretizer(n_bins=n_bins, subsample=None)
     err_msg = r"n_bins must be a scalar or array of shape \(n_features,\)."
     with pytest.raises(ValueError, match=err_msg):
         est.fit_transform(X)
 
     # Incorrect number of features
     n_bins = [1, 2, 2]
-    est = KBinsDiscretizer(n_bins=n_bins)
+    est = KBinsDiscretizer(n_bins=n_bins, subsample=None)
     err_msg = r"n_bins must be a scalar or array of shape \(n_features,\)."
     with pytest.raises(ValueError, match=err_msg):
         est.fit_transform(X)
 
     # Bad bin values
     n_bins = [1, 2, 2, 1]
-    est = KBinsDiscretizer(n_bins=n_bins)
+    est = KBinsDiscretizer(n_bins=n_bins, subsample=None)
     err_msg = ("KBinsDiscretizer received an invalid number of bins "
                "at indices 0, 3. Number of bins must be at least 2, "
                "and must be an int.")
@@ -77,7 +77,7 @@ def test_invalid_n_bins_array():
 
     # Float bin values
     n_bins = [2.1, 2, 2.1, 2]
-    est = KBinsDiscretizer(n_bins=n_bins)
+    est = KBinsDiscretizer(n_bins=n_bins, subsample=None)
     err_msg = ("KBinsDiscretizer received an invalid number of bins "
                "at indices 0, 2. Number of bins must be at least 2, "
                "and must be an int.")
@@ -147,12 +147,14 @@ def test_numeric_stability(i):
 
     # Test up to discretizing nano units
     X = X_init / 10**i
-    Xt = KBinsDiscretizer(n_bins=2, encode='ordinal').fit_transform(X)
+    Xt = KBinsDiscretizer(n_bins=2, encode='ordinal', subsample=None)\
+        .fit_transform(X)
     assert_array_equal(Xt_expected, Xt)
 
 
 def test_invalid_encode_option():
-    est = KBinsDiscretizer(n_bins=[2, 3, 3, 3], encode='invalid-encode')
+    est = KBinsDiscretizer(n_bins=[2, 3, 3, 3], encode='invalid-encode',
+                           subsample=None)
     err_msg = (r"Valid options for 'encode' are "
                r"\('onehot', 'onehot-dense', 'ordinal'\). "
                r"Got encode='invalid-encode' instead.")
@@ -178,13 +180,14 @@ def test_encode_options():
     assert sp.issparse(Xt_3)
     assert_array_equal(OneHotEncoder(
                            categories=[np.arange(i) for i in [2, 3, 3, 3]],
-                           sparse=True)
+                           sparse=True, subsample=None)
                        .fit_transform(Xt_1).toarray(),
                        Xt_3.toarray())
 
 
 def test_invalid_strategy_option():
-    est = KBinsDiscretizer(n_bins=[2, 3, 3, 3], strategy='invalid-strategy')
+    est = KBinsDiscretizer(n_bins=[2, 3, 3, 3], strategy='invalid-strategy',
+                           subsample=None)
     err_msg = (r"Valid options for 'strategy' are "
                r"\('uniform', 'quantile', 'kmeans'\). "
                r"Got strategy='invalid-strategy' instead.")
@@ -214,7 +217,8 @@ def test_nonuniform_strategies(
     assert_array_equal(expected_3bins, Xt.ravel())
 
     # with 5 bins
-    est = KBinsDiscretizer(n_bins=5, strategy=strategy, encode='ordinal')
+    est = KBinsDiscretizer(n_bins=5, strategy=strategy, encode='ordinal',
+                           subsample=None)
     Xt = est.fit_transform(X)
     assert_array_equal(expected_5bins, Xt.ravel())
 
@@ -231,7 +235,8 @@ def test_nonuniform_strategies(
                    [0.5, 4., -1.5, 1.25], [0.5, 4., -1.5, 1.25]])])
 @pytest.mark.parametrize('encode', ['ordinal', 'onehot', 'onehot-dense'])
 def test_inverse_transform(strategy, encode, expected_inv):
-    kbd = KBinsDiscretizer(n_bins=3, strategy=strategy, encode=encode)
+    kbd = KBinsDiscretizer(n_bins=3, strategy=strategy, encode=encode,
+                           subsample=None)
     Xt = kbd.fit_transform(X)
     Xinv = kbd.inverse_transform(Xt)
     assert_array_almost_equal(expected_inv, Xinv)
@@ -254,7 +259,8 @@ def test_overwrite():
     X = np.array([0, 1, 2, 3])[:, None]
     X_before = X.copy()
 
-    est = KBinsDiscretizer(n_bins=3, encode="ordinal", subsample=None)
+    est = KBinsDiscretizer(n_bins=3, encode="ordinal",
+                           subsample=None)
     Xt = est.fit_transform(X)
     assert_array_equal(X, X_before)
 
@@ -269,7 +275,7 @@ def test_overwrite():
     [('quantile', [0, 1, 3]), ('kmeans', [0, 1.5, 3])])
 def test_redundant_bins(strategy, expected_bin_edges):
     X = [[0], [0], [0], [0], [3], [3]]
-    kbd = KBinsDiscretizer(n_bins=3, strategy=strategy)
+    kbd = KBinsDiscretizer(n_bins=3, strategy=strategy, subsample=None)
     msg = ("Bins whose width are too small (i.e., <= 1e-8) in feature 0 "
            "are removed. Consider decreasing the number of bins.")
     assert_warns_message(UserWarning, msg, kbd.fit, X)
@@ -281,7 +287,7 @@ def test_percentile_numeric_stability():
     bin_edges = np.array([0.05, 0.23, 0.41, 0.59, 0.77, 0.95])
     Xt = np.array([0, 0, 4]).reshape(-1, 1)
     kbd = KBinsDiscretizer(n_bins=10, encode='ordinal',
-                           strategy='quantile')
+                           strategy='quantile', subsample=None)
     msg = ("Bins whose width are too small (i.e., <= 1e-8) in feature 0 "
            "are removed. Consider decreasing the number of bins.")
     assert_warns_message(UserWarning, msg, kbd.fit, X)
