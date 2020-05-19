eb0f302feaafeeba05d8a6a9eb2c9eabfb1e7d8d
Bharat123rox <bharatraghunthan9767@gmail.com>
2020-05-19 19:12:35 +0530

    DOC: Fix trailing comma

--- sklearn/metrics/pairwise.py
+++ sklearn/metrics/pairwise.py
@@ -674,7 +674,7 @@ def haversine_distances(X, Y=None):
 
     The Haversine (or great circle) distance is the angular distance between
     two points on the surface of a sphere. The first coordinate of each point
-    is assumed to be the latitude, the second is the longitude given
+    is assumed to be the latitude, the second is the longitude, given
     in radians. The dimension of the data must be 2.
 
     .. math::
