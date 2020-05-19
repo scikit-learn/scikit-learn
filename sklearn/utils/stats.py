5b933832792b3f61bd53be191faeacd50aab66f9
Lucy Liu <jliu176@gmail.com>
2020-05-19 16:35:13 +0200

    fix logic

--- sklearn/utils/stats.py
+++ sklearn/utils/stats.py
@@ -12,32 +12,25 @@ def _weighted_percentile(array, sample_weight, percentile=50):
     n_dim = array.ndim
     if n_dim == 0:
         return array[()]
-    # Remove trailing single-dimensional entries from the array shape
-    if n_dim == 2 and array.shape[1] == 1:
-        array.reshape((array.shape[0],))
+    if array.ndim == 1:
+        array = array.reshape((-1, 1))
+    if array.shape != sample_weight.shape:
+        sample_weight = sample_weight.reshape(array.shape)
     sorted_idx = np.argsort(array, axis=0)
     sorted_weights = _take_along_axis(sample_weight, sorted_idx, axis=0)
 
     # Find index of median prediction for each sample
     weight_cdf = stable_cumsum(sorted_weights, axis=0)
     adjusted_percentile = (percentile / 100.) * weight_cdf[-1]
-    if n_dim == 1:
-        percentile_idx = [np.searchsorted(weight_cdf, adjusted_percentile)]
-    elif n_dim == 2:
-        percentile_idx = [np.searchsorted(weight_cdf[:, i],
-                                          adjusted_percentile[i])
-                          for i in range(weight_cdf.shape[1])]
+    percentile_idx = [np.searchsorted(weight_cdf[:, i], adjusted_percentile[i])
+                      for i in range(weight_cdf.shape[1])]
     percentile_idx = np.array(percentile_idx)
     # In rare cases, percentile_idx equals to sorted_idx.shape[0]
     max_idx = sorted_idx.shape[0] - 1
     percentile_idx = np.apply_along_axis(lambda x: np.clip(x, 0, max_idx),
                                          axis=0, arr=percentile_idx)
-    if n_dim == 1:
-        percentile = array[sorted_idx[percentile_idx]][0]
-    elif n_dim == 2:
-        col_index = np.arange(array.shape[1])
-        fancy_index_percentile = tuple([percentile_idx, col_index])
-        percentile_in_sorted = sorted_idx[fancy_index_percentile]
-        fancy_index_sorted = tuple([percentile_in_sorted, col_index])
-        percentile = array[fancy_index_sorted]
-    return percentile
+
+    col_index = np.arange(array.shape[1])
+    percentile_in_sorted = sorted_idx[percentile_idx, col_index]
+    percentile = array[percentile_in_sorted, col_index]
+    return percentile[0] if ndim == 1 else percentile
