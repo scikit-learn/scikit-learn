3ee6c259a880ed3ce2ff8376087572864aa45964
Lucy Liu <jliu176@gmail.com>
2020-05-20 12:08:08 +0200

    lint

--- sklearn/utils/tests/test_stats.py
+++ sklearn/utils/tests/test_stats.py
@@ -45,7 +45,7 @@ def test_weighted_median_equal_weights():
     # Checks weighted percentile=0.5 is same as median when weights equal
     rng = np.random.RandomState(0)
     x = rng.randint(10, size=10)
-    weights = np.ones(x1.shape)
+    weights = np.ones(x.shape)
 
     median = np.median(x)
     w_median = _weighted_percentile(x, weights)
