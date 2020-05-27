0083ac66dd0fab084dc3b86d3af863137eba3935
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2020-05-27 18:38:57 +0200

    Update sklearn/utils/estimator_checks.py
    Co-authored-by: Thomas J. Fan <thomasjpfan@gmail.com>

--- sklearn/utils/estimator_checks.py
+++ sklearn/utils/estimator_checks.py
@@ -500,7 +500,7 @@ def _regression_dataset(n_samples=200):
     global REGRESSION_DATASET
     if REGRESSION_DATASET is None:
         X, y = make_regression(
-            n_samples=n_samples, n_features=10, n_informative=1, n_targets=1,
+            n_samples=n_samples, n_features=10, n_informative=1,
             bias=5.0, noise=20, random_state=42,
         )
         X = StandardScaler().fit_transform(X)
