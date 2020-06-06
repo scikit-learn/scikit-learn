cb9b030f25341de854e3a99d2a0d91f0cf18f5db
Martin Oywa <martinoywa2@gmail.com>
2020-06-06 18:28:09 +0300

    Add link to DecisionTreeRegressor object in BaggingRegressor docstrings

--- sklearn/ensemble/_bagging.py
+++ sklearn/ensemble/_bagging.py
@@ -865,7 +865,8 @@ class BaggingRegressor(RegressorMixin, BaseBagging):
     ----------
     base_estimator : object, default=None
         The base estimator to fit on random subsets of the dataset.
-        If None, then the base estimator is a decision tree.
+        If None, then the base estimator is a
+        :class:`~sklearn.tree.DecisionTreeRegressor`.
 
     n_estimators : int, default=10
         The number of base estimators in the ensemble.
