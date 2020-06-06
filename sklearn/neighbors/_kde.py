78ed1b1487730811e437280d11ba3e0dbb462fb3
Saurabh Jain <28838017+saurabhsjain@users.noreply.github.com>
2020-06-06 22:31:30 +0530

    updated _kde.py
    Added full stop

--- sklearn/neighbors/_kde.py
+++ sklearn/neighbors/_kde.py
@@ -146,7 +146,7 @@ class KernelDensity(BaseEstimator):
             Ignored. This parameter exists only for compatibility with
             :class:`~sklearn.pipeline.Pipeline`.
         sample_weight : array-like, shape (n_samples,), optional
-            List of sample weights attached to the data X
+            List of sample weights attached to the data X.
 
             .. versionadded:: 0.20
 
