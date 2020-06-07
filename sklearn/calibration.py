0393964c89cb4d4ce5824a097e4e5dd54af9cbdf
Lucy Liu <jliu176@gmail.com>
2020-06-07 14:50:05 +0200

    add example in docstring

--- sklearn/calibration.py
+++ sklearn/calibration.py
@@ -771,6 +771,20 @@ def plot_calibration_curve(estimator, X, y, *,
     -------
     display : :class:`~sklearn.metrics.CalibrationDisplay`.
         Object that stores computed values.
+
+    Examples
+    --------
+    >>> import matplotlib.pyplot as plt  # doctest: +SKIP
+    >>> from sklearn import (datasets, calibration, model_selection,
+    ...                      linear_model)
+    >>> X, y = datasets.make_classification(random_state=0)
+    >>> X_train, X_test, y_train, y_test = model_selection.train_test_split(
+    ...     X, y, random_state=0)
+    >>> clf = linear_model.LogisticRegression(random_state=0)
+    >>> clf.fit(X_train, y_train)
+    LogisticRegression(random_state=0)
+    >>> metrics.plot_calibration_curve(clf, X_test, y_test)  # doctest: +SKIP
+    >>> plt.show()                                           # doctest: +SKIP
     """
     check_matplotlib_support("plot_calibration_curve")
     binary_error = "Only binary classification is supported."
