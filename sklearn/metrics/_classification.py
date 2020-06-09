48a5908594a2c73ceeb35c089b38fbc36ebf441d
genvalen <genvalen@protonmail.com>
2020-06-09 17:02:15 -0400

    Update sklearn/metrics/_classification.py
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- sklearn/metrics/_classification.py
+++ sklearn/metrics/_classification.py
@@ -1545,8 +1545,8 @@ def precision_score(y_true, y_pred, *, labels=None, pos_label=1,
         setting ``labels=[pos_label]`` and ``average != 'binary'`` will report
         scores for that label only.
 
-    average : string, [None, 'micro', 'macro', 'samples', 'weighted', \
-            default='binary']
+    average : {'micro', 'macro', 'samples', 'weighted', 'binary'} \
+            default='binary'
         This parameter is required for multiclass/multilabel targets.
         If ``None``, the scores for each class are returned. Otherwise, this
         determines the type of averaging performed on the data:
