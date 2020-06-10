8990c612f3c6efdfb7d5fd8b21ddb0e1e5fc3937
Madhura Jayaratne <madhura.cj@gmail.com>
2020-06-10 21:59:09 +1000

    Improve documentation formatting
    Co-authored-by: Joel Nothman <joel.nothman@gmail.com>

--- examples/inspection/plot_partial_dependence.py
+++ examples/inspection/plot_partial_dependence.py
@@ -13,7 +13,7 @@ Similarly, an individual conditional expectation (ICE) plot [3]_
 shows the dependence between the target function and a feature of interest.
 However, unlike partial dependence plots, which show the average effect of the
 features of interest, ICE plots visualize the dependence of the prediction on a
-feature for each :term:sample: separately, with one line per :term:sample:.
+feature for each :term:`sample` separately, with one line per sample.
 Only one feature of interest is supported for ICE plots.
 
 This example shows how to obtain partial dependence and ICE plots from a
