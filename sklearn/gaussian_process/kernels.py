168e75882a8fb3f7d55a55e5e8b557723d4758b6
Allan <allanbutler9@gmail.com>
2020-06-11 14:55:10 -0500

    fixed tab error causing build error

--- sklearn/gaussian_process/kernels.py
+++ sklearn/gaussian_process/kernels.py
@@ -1485,7 +1485,7 @@ class RBF(StationaryKernelMixin, NormalizedKernelMixin, Kernel):
         if self.anisotropic:
             return "{0}(length_scale=[{1}])".format(
                 self.__class__.__name__, ", ".join(map("{0:.3g}".format,
-                                                       self.length_scale)))
+                                                   self.length_scale)))
         else:  # isotropic
             return "{0}(length_scale={1:.3g})".format(
                 self.__class__.__name__, np.ravel(self.length_scale)[0])
