f4f43bea06588530318b615532588c162355b5c5
Uwe F. Mayer <uwe_f_mayer@yahoo.com>
2020-06-12 09:51:56 -0700

    Merged tests/test_supervised.py into metrics/cluster/tests/test_supervised.py

--- sklearn/metrics/cluster/tests/test_supervised.py
+++ sklearn/metrics/cluster/tests/test_supervised.py
@@ -3,8 +3,10 @@ import pytest
 
 from sklearn.metrics.cluster import adjusted_mutual_info_score
 from sklearn.metrics.cluster import adjusted_rand_score
+from sklearn.metrics.cluster import rand_score
 from sklearn.metrics.cluster import completeness_score
 from sklearn.metrics.cluster import contingency_matrix
+from sklearn.metrics.cluster import pair_confusion_matrix
 from sklearn.metrics.cluster import entropy
 from sklearn.metrics.cluster import expected_mutual_information
 from sklearn.metrics.cluster import fowlkes_mallows_score
@@ -18,11 +20,13 @@ from sklearn.metrics.cluster._supervised import _generalized_average
 from sklearn.utils import assert_all_finite
 from sklearn.utils._testing import (
         assert_almost_equal, ignore_warnings)
-from numpy.testing import assert_array_almost_equal
+from numpy.testing import (assert_array_equal,
+        assert_array_almost_equal)
 
 
 score_funcs = [
     adjusted_rand_score,
+    rand_score,
     homogeneity_score,
     completeness_score,
     v_measure_score,
@@ -159,6 +163,11 @@ def test_non_consecutive_labels():
     ari_2 = adjusted_rand_score([0, 0, 0, 1, 1, 1], [0, 4, 0, 4, 2, 2])
     assert_almost_equal(ari_1, 0.24, 2)
     assert_almost_equal(ari_2, 0.24, 2)
+
+    ri_1 = rand_score([0, 0, 0, 1, 1, 1], [0, 1, 0, 1, 2, 2])
+    ri_2 = rand_score([0, 0, 0, 1, 1, 1], [0, 4, 0, 4, 2, 2])
+    assert_almost_equal(ri_1, 0.66, 2)
+    assert_almost_equal(ri_2, 0.66, 2)
 
 
 @ignore_warnings(category=FutureWarning)
@@ -348,3 +357,73 @@ def test_fowlkes_mallows_score_properties():
 def test_mutual_info_score_positive_constant_label(labels_true, labels_pred):
     # non-regression test for #16355
     assert mutual_info_score(labels_true, labels_pred) >= 0
+
+
+def test_pair_confusion_matrix():
+    n = 10
+    N = n * n
+
+    # edge case: every element is its own cluster
+    clustering1 = list(range(N))
+    clustering2 = clustering1
+    expected = np.array([[N * (N - 1), 0], [0, 0]])
+    assert_array_equal(expected, pair_confusion_matrix(clustering1,
+                                                       clustering2))
+
+    # edge case: only one cluster
+    clustering1 = np.full((N,), fill_value=0)
+    clustering2 = clustering1
+    expected = np.array([[0, 0], [0, N * (N - 1)]])
+    assert_array_equal(expected, pair_confusion_matrix(clustering1,
+                                                       clustering2))
+
+    # regular case: different non-trivial clusterings
+    clustering1 = np.array([i+1 for i in range(n) for j in range(n)])
+    clustering2 = np.array([i+1 for i in range(n) for j in range(n+1)][:N])
+    # basic quadratic implementation
+    expected = np.full(shape=(2, 2), fill_value=0, dtype=np.int64)
+    for i in range(len(clustering1)):
+        for j in range(len(clustering2)):
+            if i != j:
+                same_cluster_1 = int(clustering1[i] == clustering1[j])
+                same_cluster_2 = int(clustering2[i] == clustering2[j])
+                expected[same_cluster_1, same_cluster_2] += 1
+    assert_array_equal(expected, pair_confusion_matrix(clustering1,
+                                                       clustering2))
+
+
+def test_rand_score():
+    n = 10
+    N = n * n
+
+    # edge case: every element is its own cluster
+    clustering1 = list(range(N))
+    clustering2 = clustering1
+    expected = np.array([1.])
+    assert_array_almost_equal(expected, rand_score(clustering1,
+                                                   clustering2))
+
+    # edge case: only one cluster
+    clustering1 = np.full((N,), fill_value=0)
+    clustering2 = clustering1
+    expected = np.array([1.])
+    assert_array_almost_equal(expected, rand_score(clustering1,
+                                                   clustering2))
+
+    # regular case: different non-trivial clusterings
+    clustering1 = np.array([i+1 for i in range(n) for j in range(n)])
+    clustering2 = np.array([i+1 for i in range(n) for j in range(n+1)][:N])
+    # basic quadratic implementation
+    expected = np.full(shape=(2, 2), fill_value=0, dtype=np.int64)
+    for i in range(len(clustering1)):
+        for j in range(len(clustering2)):
+            if i != j:
+                same_cluster_1 = int(clustering1[i] == clustering1[j])
+                same_cluster_2 = int(clustering2[i] == clustering2[j])
+                expected[same_cluster_1, same_cluster_2] += 1
+    expected_numerator = expected[0, 0] + expected[1, 1]
+    expected_denominator = (expected[0, 0] + expected[1, 1] + expected[0, 1] +
+                            expected[1, 0])
+    expected_score = expected_numerator / expected_denominator
+    assert_array_almost_equal(expected_score, rand_score(clustering1,
+                                                         clustering2))
