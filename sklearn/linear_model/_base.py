174de83e9fe93b588983322f4e940400281a1bbe
Chiara Marmo <chiara.marmo@u-psud.fr>
2020-06-14 17:29:58 +0200

    Remove maxiter keywords in nnls().

--- sklearn/linear_model/_base.py
+++ sklearn/linear_model/_base.py
@@ -541,12 +541,12 @@ class LinearRegression(MultiOutputMixin, RegressorMixin, LinearModel):
         if self.positive:
             if y.ndim < 2:
                 self.coef_, self._residues = optimize.nnls(
-                                               X, y, maxiter=self.maxiter)
+                                               X, y, self.maxiter)
             else:
                 # scipy.optimize.nnls cannot handle y with shape (M, K)
                 outs = Parallel(n_jobs=n_jobs_)(
                         delayed(optimize.nnls)(
-                                X, y[:, j], maxiter=self.maxiter)
+                                X, y[:, j], self.maxiter)
                         for j in range(y.shape[1]))
                 self.coef_, self._residues = map(np.vstack, zip(*outs))
         elif sp.issparse(X):
