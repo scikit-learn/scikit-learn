ff9f3eb8b5078a77d0772507c871f4b38d117824
Lucy Liu <jliu176@gmail.com>
2020-06-16 14:27:53 +0200

    lint

--- sklearn/calibration.py
+++ sklearn/calibration.py
@@ -28,6 +28,7 @@ from .isotonic import IsotonicRegression
 from .svm import LinearSVC
 from .model_selection import check_cv
 from .utils.validation import _deprecate_positional_args
+from sklearn.exceptions import NotFittedError
 
 
 class CalibratedClassifierCV(BaseEstimator, ClassifierMixin,
