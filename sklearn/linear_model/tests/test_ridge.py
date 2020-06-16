08796a4692e60e32966492c220b293d032e3db33
Rithvik Rao <rithvik321@gmail.com>
2020-06-16 15:17:28 -0700

    Refactored ridge solvers into separate file, so Cholesky can be used for linear regression
    Author:    Rithvik Rao <rithvik321@gmail.com>

--- sklearn/linear_model/tests/test_ridge.py
+++ sklearn/linear_model/tests/test_ridge.py
@@ -28,10 +28,12 @@ from sklearn.linear_model._ridge import _RidgeGCV
 from sklearn.linear_model import RidgeCV
 from sklearn.linear_model import RidgeClassifier
 from sklearn.linear_model import RidgeClassifierCV
-from sklearn.linear_model._ridge import _solve_cholesky
-from sklearn.linear_model._ridge import _solve_cholesky_kernel
-from sklearn.linear_model._ridge import _solve_svd
-from sklearn.linear_model._ridge import _solve_lbfgs
+from sklearn.linear_model._ridge_solvers import (
+    _solve_cholesky,
+    _solve_cholesky_kernel,
+    _solve_svd,
+    _solve_lbfgs,
+)
 from sklearn.linear_model._ridge import _check_gcv_mode
 from sklearn.linear_model._ridge import _X_CenterStackOp
 from sklearn.datasets import make_regression
