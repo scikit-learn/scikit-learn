4c78402a6327a2671b904553f40327660eafdcaa
d3b0unce <hi@srimukh.com>
2020-06-16 14:01:45 +0000

    Added inverse_transform method to SimpleImputer

--- sklearn/impute/_base.py
+++ sklearn/impute/_base.py
@@ -465,10 +465,40 @@ class SimpleImputer(_BaseImputer):
             n_missing = np.sum(mask, axis=0)
             values = np.repeat(valid_statistics, n_missing)
             coordinates = np.where(mask.transpose())[::-1]
-
             X[coordinates] = values
 
         return super()._concatenate_indicator(X, X_indicator)
+    
+    def inverse_transform(self, X):
+        """Convert the data back to the original representation.
+        
+        Inverts the `fit_transform` operation performed on an array.
+        This operation can only be performed after :class:`SimpleImputer` is
+        used with `add_indicator` set to `True`.
+
+        Parameters
+        ----------
+        X : array-like, 
+            shape (n_samples, n_features + missing_feature_count)
+            The imputed data to be reverted to original data. It has to be
+            an augmented array of imputed data and the missing indicator mask.
+        """
+        check_is_fitted(self)
+        missing_feature_count = len(self.indicator_.features_)
+        
+        # Split the augmented array into imputed array and it's missing
+        # indicator mask.
+        feature_count = X.shape[1] - missing_feature_count
+        imputed_arr = X[:, :feature_count].copy()
+        missing_mask = X[:, feature_count:].copy()
+        missing_mask = missing_mask.astype(np.bool)
+        
+        # Iterate over features and replace the imputed values
+        # with original missing value types.
+        for i in range(missing_feature_count):
+            f_idx = self.indicator_.features_[i]
+            imputed_arr[:,f_idx][missing_mask[:,i]] = self.missing_values
+        return imputed_arr
 
 
 class MissingIndicator(TransformerMixin, BaseEstimator):
