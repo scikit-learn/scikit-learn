38103ee8670e16b23f063f6dee19beaf4a33ed73
Thomas J. Fan <thomasjpfan@gmail.com>
2020-06-18 11:23:13 -0400

    BUG Fix

--- sklearn/utils/_parallel.py
+++ sklearn/utils/_parallel.py
@@ -3,19 +3,19 @@ from .._config import config_context, get_config
 from joblib import delayed as joblib_delayed
 
 
-def delayed(f):
-    """Decorator used to capture the arguments of a function while passing
-    the global configuration options."""
-    return joblib_delayed(_FuncWrapper(f))
+def delayed(function, *args, **kwargs):
+    """Wrapper around joblib.delayed to pass around the global configuration.
+    """
+    return joblib_delayed(_FuncWrapper(function), *args, **kwargs)
 
 
 class _FuncWrapper:
-    """"Load the global configuration before calling the function"""
-    def __init__(self, func):
-        self.func = func
+    """"Load the global configuration before calling the function."""
+    def __init__(self, function):
+        self.function = function
         self.config = get_config()
-        update_wrapper(self, self.func)
+        update_wrapper(self, self.function)
 
     def __call__(self, *args, **kwargs):
         with config_context(**self.config):
-            return self.func(*args, **kwargs)
+            return self.function(*args, **kwargs)
