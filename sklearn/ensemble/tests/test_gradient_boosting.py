ab174b841aafc58772a188f6e147ae774c1de5a0
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2020-06-22 23:31:13 +0200

    increase learning rate since it was too low

--- sklearn/ensemble/tests/test_gradient_boosting.py
+++ sklearn/ensemble/tests/test_gradient_boosting.py
@@ -1143,10 +1143,11 @@ def test_non_uniform_weights_toy_edge_case_reg(loss):
     # ignore the first 2 training samples by setting their weight to 0
     sample_weight = [0, 0, 1, 1]
     gb = GradientBoostingRegressor(
-        learning_rate=0.01, n_estimators=50, loss=loss
+        learning_rate=0.1, n_estimators=200, loss=loss
     )
     gb.fit(X, y, sample_weight=sample_weight)
     assert np.all(np.diff(gb.train_score_) <= 0)
+    print(gb.predict([[1, 0]]))
     assert gb.predict([[1, 0]])[0] > 0.5
 
 
