617db98262162e30f74350ba3d4f0bf5d472e34d
t.kusanagi <t.kusanagi@fujitsu.com>
2020-06-23 10:16:35 +0000

    DOC document params of PLSRegression

--- sklearn/cross_decomposition/_pls.py
+++ sklearn/cross_decomposition/_pls.py
@@ -584,6 +584,18 @@ class PLSRegression(_PLS):
     y_rotations_ : array, [q, n_components]
         Y block to latents rotations.
 
+    x_mean_ : array, [p]
+        X mean for each predictor.
+
+    y_mean_ : array, [q]
+        Y mean for each response variable.
+
+    x_std_ : array, [p]
+        X standard deviation for each predictor.
+
+    y_std_ : array, [q]
+        Y standard deviation for each response variable.
+
     coef_ : array, [p, q]
         The coefficients of the linear model: ``Y = X coef_ + Err``
 
