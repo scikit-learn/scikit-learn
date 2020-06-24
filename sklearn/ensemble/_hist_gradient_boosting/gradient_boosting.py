c04356a6b4cf9cd78a054f488922eb8ff17858f9
Simona Maggio <maggio.simona@gmail.com>
2020-06-24 12:12:18 +0200

    rm newlines

--- sklearn/ensemble/_hist_gradient_boosting/gradient_boosting.py
+++ sklearn/ensemble/_hist_gradient_boosting/gradient_boosting.py
@@ -887,22 +887,18 @@ class HistGradientBoostingRegressor(RegressorMixin, BaseHistGradientBoosting):
     ----------
     do_early_stopping_ : bool
         Indicates whether early stopping is used during training.
-
     n_iter_ : int
         The number of iterations as selected by early stopping, depending on
         the ``early_stopping`` parameter. Otherwise it corresponds to ``max_iter``.
-    
     n_trees_per_iteration_ : int
         The number of tree that are built at each iteration. For regressors,
         this is always 1.
-
     train_score_ : ndarray, shape (n_iter_+1,)
         The scores at each iteration on the training data. The first entry
         is the score of the ensemble before the first iteration. Scores are
         computed according to the ``scoring`` parameter. If ``scoring`` is
         not 'loss', scores are computed on a subset of at most 10 000
         samples. Empty if no early stopping.
-    
     validation_score_ : ndarray, shape (n_iter_+1,)
         The scores at each iteration on the held-out validation data. The
         first entry is the score of the ensemble before the first iteration.
