41488fcabeb28340157d6aebc8a7b957627f96ed
Thomas J. Fan <thomasjpfan@gmail.com>
2020-06-25 05:57:11 -0400

    MNT Remaining numpy alias (#17712)

--- sklearn/utils/_random.pyx
+++ sklearn/utils/_random.pyx
@@ -83,8 +83,7 @@ cpdef _sample_without_replacement_with_tracking_selection(
 
     cdef np.int_t i
     cdef np.int_t j
-    cdef np.ndarray[np.int_t, ndim=1] out = np.empty((n_samples, ),
-                                                     dtype=np.int)
+    cdef np.ndarray[np.int_t, ndim=1] out = np.empty((n_samples, ), dtype=int)
 
     rng = check_random_state(random_state)
     rng_randint = rng.randint
@@ -139,11 +138,10 @@ cpdef _sample_without_replacement_with_pool(np.int_t n_population,
 
     cdef np.int_t i
     cdef np.int_t j
-    cdef np.ndarray[np.int_t, ndim=1] out = np.empty((n_samples, ),
-                                                     dtype=np.int)
+    cdef np.ndarray[np.int_t, ndim=1] out = np.empty((n_samples, ), dtype=int)
 
     cdef np.ndarray[np.int_t, ndim=1] pool = np.empty((n_population, ),
-                                                      dtype=np.int)
+                                                      dtype=int)
 
     rng = check_random_state(random_state)
     rng_randint = rng.randint
@@ -202,8 +200,7 @@ cpdef _sample_without_replacement_with_reservoir_sampling(
 
     cdef np.int_t i
     cdef np.int_t j
-    cdef np.ndarray[np.int_t, ndim=1] out = np.empty((n_samples, ),
-                                                     dtype=np.int)
+    cdef np.ndarray[np.int_t, ndim=1] out = np.empty((n_samples, ), dtype=int)
 
     rng = check_random_state(random_state)
     rng_randint = rng.randint
