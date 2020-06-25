ba82f4bf1ed52d4f3204a95f7a8d9b4dfd48e7c8
Allan <allanbutler9@gmail.com>
2020-06-24 20:04:01 -0500

    Fixed value error for incremental PCA

--- sklearn/decomposition/_incremental_pca.py
+++ sklearn/decomposition/_incremental_pca.py
@@ -289,8 +289,8 @@ class IncrementalPCA(_BasePCA):
             X -= col_batch_mean
             # Build matrix of combined previous basis and new data
             mean_correction = \
-                np.sqrt((self.n_samples_seen_ * n_samples) /
-                        n_total_samples) * (self.mean_ - col_batch_mean)
+                np.sqrt((n_total_samples) * (self.mean_ - col_batch_mean) /
+                self.n_samples_seen_ * n_samples)
             X = np.vstack((self.singular_values_.reshape((-1, 1)) *
                            self.components_, X, mean_correction))
 
