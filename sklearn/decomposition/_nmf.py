a3d9a50fcb08380c662ae8a476f576bb1c87ed5a
Chiara Marmo <chiara.marmo@u-psud.fr>
2020-06-25 10:52:36 +0200

    Revert.

--- sklearn/decomposition/_nmf.py
+++ sklearn/decomposition/_nmf.py
@@ -1914,8 +1914,7 @@ class MiniBatchNMF(TransformerMixin, BaseEstimator):
     def partial_fit(self, X, y=None, **params):
         if hasattr(self, 'components_'):
             W = np.ones((X.shape[0], self.n_components_))
-            #  commented only to check tests
-            #W *= np.maximum(1e-6, X.sum(axis=1).A)
+            W *= np.maximum(1e-6, X.sum(axis=1) * self._components_numerator_)
             W /= W.sum(axis=1, keepdims=True)
             W, H, A, B, n_iter_ = non_negative_factorization_online(
                 X=X, W=W, H=self.components_,
