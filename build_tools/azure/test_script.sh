69b8d8f4c4cb419c4ec065afde52b852bce1217c
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-06-25 10:30:16 +0200

    FIX Add scape characters [scipy-dev]

--- build_tools/azure/test_script.sh
+++ build_tools/azure/test_script.sh
@@ -33,7 +33,7 @@ if [[ -n "$CHECK_WARNINGS" ]]; then
     # numpy's 1.19.0's tostring() deprecation is ignored until scipy and joblib removes its usage
     TEST_CMD="$TEST_CMD -Wignore:tostring:DeprecationWarning"
     # numpy's 1.20.0's np.bool, np.int, np.float and np.object deprecations are ignored until pandas removes its usage
-    TEST_CMD="$TEST_CMD -Wignore:`np.*`:DeprecationWarning:pandas"
+    TEST_CMD="$TEST_CMD -Wignore:\`np.*\`:DeprecationWarning:pandas"
 fi
 
 if [[ "$PYTEST_XDIST" == "true" ]]; then
