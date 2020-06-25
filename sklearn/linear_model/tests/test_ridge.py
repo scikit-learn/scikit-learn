de1ac2cfdca8ff3f29e4df67c09b939df22a2603
maikia <maja_ka@hotmail.com>
2020-06-25 20:39:47 +0200

    added fixme to tests

--- sklearn/linear_model/tests/test_ridge.py
+++ sklearn/linear_model/tests/test_ridge.py
@@ -400,6 +400,7 @@ def _make_sparse_offset_regression(
     return X, y
 
 
+# FIXME: 'normalize' to be removed in 0.26
 @pytest.mark.parametrize(
     'solver, sparse_X',
     ((solver, sparse_X) for
@@ -435,6 +436,7 @@ def test_solver_consistency(
         ridge.intercept_, svd_ridge.intercept_, atol=1e-3, rtol=1e-3)
 
 
+# FIXME: 'normalize' to be removed in 0.26
 @pytest.mark.parametrize('gcv_mode', ['svd', 'eigen'])
 @pytest.mark.parametrize('X_constructor', [np.asarray, sp.csr_matrix])
 @pytest.mark.parametrize('X_shape', [(11, 8), (11, 20)])
@@ -475,6 +477,7 @@ def test_ridge_gcv_vs_ridge_loo_cv(
     assert_allclose(gcv_ridge.intercept_, loo_ridge.intercept_, rtol=1e-3)
 
 
+# FIXME: 'normalize' to be removed in 0.26
 def test_ridge_loo_cv_asym_scoring():
     # checking on asymmetric scoring
     scoring = 'explained_variance'
@@ -639,6 +642,7 @@ def _test_ridge_loo(filter_):
     return ret
 
 
+# FIXME: 'normalize' to be removed in 0.26
 def _test_ridge_cv_normalize(filter_):
     ridge_cv = RidgeCV(normalize=True, cv=3)
     ridge_cv.fit(filter_(10. * X_diabetes), y_diabetes)
