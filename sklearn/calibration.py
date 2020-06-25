08fb0a90e59be1a388b53a81ec190380d50abd51
Lucy Liu <jliu176@gmail.com>
2020-06-25 21:55:15 +0200

    order data

--- sklearn/calibration.py
+++ sklearn/calibration.py
@@ -626,7 +626,7 @@ def calibration_curve(y_true, y_prob, *, normalize=False, n_bins=5,
     >>> import numpy as np
     >>> from sklearn.calibration import calibration_curve
     >>> y_true = np.array([0, 0, 0, 0, 1, 1, 1, 1, 1])
-    >>> y_pred = np.array([0.1, 0.2, 0.4, 0.3, 0.8, 0.7, 0.9, 0.65, 1.])
+    >>> y_pred = np.array([0.1, 0.2, 0.3, 0.4, 0.65, 0.7, 0.8, 0.9,  1.])
     >>> prob_true, prob_pred = calibration_curve(y_true, y_pred, n_bins=3)
     >>> prob_true
     array([0., 0.5, 1.])
