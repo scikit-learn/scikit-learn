78204e84b7628d9e8ef73aa1d6696fec1614ee63
t-kusanagi2 <61999178+t-kusanagi2@users.noreply.github.com>
2020-06-26 16:32:09 +0900

    Update sklearn/ensemble/tests/test_gradient_boosting_loss_functions.py
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- sklearn/ensemble/tests/test_gradient_boosting_loss_functions.py
+++ sklearn/ensemble/tests/test_gradient_boosting_loss_functions.py
@@ -165,7 +165,7 @@ def test_multinomial_deviance(n_classes, n_samples):
     loss_wo_sw = loss(y, p)
     assert loss_wo_sw > 0
     loss_w_sw = loss(y, p, np.ones(p.shape[0], dtype=np.float32))
-    assert_almost_equal(loss_wo_sw, loss_w_sw)
+    assert loss_wo_sw == pytest.approx(loss_w_sw)
     loss_w_sw = loss(y, p, 0.5 * np.ones(p.shape[0], dtype=np.float32))
     assert_almost_equal(loss_wo_sw, loss_w_sw)
 
