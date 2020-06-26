58982e610a8f0eeec35e98a9cfcada56f7105fcc
Simona Maggio <maggio.simona@gmail.com>
2020-06-26 17:27:05 +0200

    fix linting

--- sklearn/ensemble/tests/test_gradient_boosting_loss_functions.py
+++ sklearn/ensemble/tests/test_gradient_boosting_loss_functions.py
@@ -26,7 +26,7 @@ def test_binomial_deviance():
 
     # pred has the same BD for y in {0, 1}
     assert (bd(np.array([0.0]), np.array([0.0])) ==
-                bd(np.array([1.0]), np.array([0.0])))
+                 bd(np.array([1.0]), np.array([0.0])))
 
     assert_almost_equal(bd(np.array([1.0, 1.0, 1.0]),
                            np.array([100.0, 100.0, 100.0])),
