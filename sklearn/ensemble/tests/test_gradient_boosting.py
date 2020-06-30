68b044abdbf1c654527e766c01259b4d6ba85163
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2020-06-30 11:43:05 +0200

    revert the legacy nearest-rank

--- sklearn/ensemble/tests/test_gradient_boosting.py
+++ sklearn/ensemble/tests/test_gradient_boosting.py
@@ -229,8 +229,7 @@ def check_regression_dataset(loss, subsample):
 
         y_pred = reg.predict(X_reg)
         mse = mean_squared_error(y_reg, y_pred)
-        print(mse)
-        assert mse < 0.05
+        assert mse < 0.04
 
         if last_y_pred is not None:
             # FIXME: We temporarily bypass this test. This is due to the fact
