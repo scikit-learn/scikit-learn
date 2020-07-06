ebebdf557a9cdda1c52a4c93c1f8bd851933a728
fujiaxiang <fujiaxiang1995@gmail.com>
2020-07-06 11:17:40 +0800

    added tests for estimator attributes

--- sklearn/tests/test_isotonic.py
+++ sklearn/tests/test_isotonic.py
@@ -548,6 +548,13 @@ def test_input_shape_validation():
     iso_reg = IsotonicRegression().fit(X, y)
     iso_reg_2d = IsotonicRegression().fit(X_2d, y)
 
+    assert iso_reg.X_max_ == iso_reg_2d.X_max_
+    assert iso_reg.X_min_ == iso_reg_2d.X_min_
+    assert iso_reg.y_max == iso_reg_2d.y_max
+    assert iso_reg.y_min == iso_reg_2d.y_min
+    assert_array_equal(iso_reg.X_thresholds_, iso_reg_2d.X_thresholds_)
+    assert_array_equal(iso_reg.y_thresholds_, iso_reg_2d.y_thresholds_)
+
     y_pred1 = iso_reg.predict(X)
     y_pred2 = iso_reg_2d.predict(X_2d)
     assert_allclose(y_pred1, y_pred2)
