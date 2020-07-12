edca515b104a6f5f48b1148913871ef5a32c8c41
Forrest Koch <forrest.koch@unsw.edu.au>
2020-07-12 12:04:35 +1000

    Update sklearn/metrics/tests/test_pairwise.py
    Co-authored-by: Thomas J. Fan <thomasjpfan@gmail.com>

--- sklearn/metrics/tests/test_pairwise.py
+++ sklearn/metrics/tests/test_pairwise.py
@@ -1304,7 +1304,7 @@ def test_pairwise_distances_data_derived_params(n_jobs, metric, dist_function,
             'sqeuclidean', 'cityblock', 'cosine', 'euclidean'])
 @pytest.mark.parametrize(
         "dtype",
-        [np.half, np.float, np.double, np.longdouble])
+        [np.float16, float, np.float64, np.float128])
 @pytest.mark.parametrize("y_is_x", [True, False], ids=["Y is X", "Y is not X"])
 def test_numeric_pairwise_distances_datatypes(metric, dtype, y_is_x):
     # Check that pairwise distances gives the same result as pdist and cdist
