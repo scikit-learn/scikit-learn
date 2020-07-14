ccb0ddd95897a6b7a1da030355256504de76a1db
Forrest Koch <forrest.c.koch@gmail.com>
2020-07-14 16:02:38 +1000

    making the linter happy

--- sklearn/metrics/tests/test_pairwise.py
+++ sklearn/metrics/tests/test_pairwise.py
@@ -1330,4 +1330,4 @@ def test_numeric_pairwise_distances_datatypes(metric, dtype, y_is_x):
     # the default rtol=1e-7 is too close to the float32 precision
     # and fails due to rounding errors when metric=='cosine'.
     rtol = 1e-5 if dtype is np.float32 and metric=='cosine' else 1e-7
-    assert_allclose(dist, expected_dist,rtol=rtol)
+    assert_allclose(dist, expected_dist, rtol=rtol)
