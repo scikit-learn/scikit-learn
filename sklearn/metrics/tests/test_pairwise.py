68a5638e3d6f35b09d9dbcfcad155efc67bf11a6
Forrest Koch <forrest.c.koch@gmail.com>
2020-07-14 21:29:10 +1000

    fix logic mistake, and try only for cosine again

--- sklearn/metrics/tests/test_pairwise.py
+++ sklearn/metrics/tests/test_pairwise.py
@@ -1325,17 +1325,17 @@ def test_numeric_pairwise_distances_datatypes(metric, dtype, y_is_x):
     else:
         Y = rng.random_sample((5, 4)).astype(dtype)
         expected_dist = cdist(X, Y, metric=metric)
+        # precompute parameters for seuclidean & mahalanobis when x is not y
+        if metric == 'seuclidean' and not y_is_x:
+            params = {'V': np.var(np.vstack([X, Y]),
+                                  axis=0, ddof=1, dtype=np.float64)}
+        elif metric == 'mahalanobis' and not y_is_x:
+            params = {'VI': np.linalg.inv(np.cov(np.vstack([X, Y]).T)).T}
 
-    # precompute parameters for seuclidean & mahalanobis when x is not y
-    if metric == 'seuclidean':
-        params = {'V': np.var(np.vstack([X, Y]),
-                              axis=0, ddof=1, dtype=np.float64)}
-    elif metric == 'mahalanobis':
-        params = {'VI': np.linalg.inv(np.cov(np.vstack([X, Y]).T)).T}
 
     dist = pairwise_distances(X, Y, metric=metric, **params)
 
     # the default rtol=1e-7 is too close to the float32 precision
     # and fails due to rounding errors
-    rtol = 1e-5 if dtype is np.float32 else 1e-7
+    rtol = 1e-5 if dtype is np.float32 and metric == 'cosine' else 1e-7
     assert_allclose(dist, expected_dist, rtol=rtol)
