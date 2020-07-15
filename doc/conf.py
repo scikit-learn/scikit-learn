f53cffeb6ec88aac0b7b40b0dd949df9bbacafa7
Joel Nothman <joel.nothman@gmail.com>
2020-07-15 23:36:17 +1000

    Use include in prolog instead of direct code

--- doc/conf.py
+++ doc/conf.py
@@ -60,15 +60,7 @@ else:
                     'tex-chtml.js')
 
 rst_prolog = r"""
-.. role:: raw-html(raw)
-   :format: html
-
-.. role:: raw-latex(raw)
-   :format: latex
-
-.. |ControlParameter| replace:: :raw-html:`<a class="paramtype" href="../../glossary.html#term-control-parameter" title="Glossary on Control Parameter"><span class="badge badge-primary">Control</span></a>` :raw-latex:`{\small\sc [Control Parameter]}`
-.. |TuningParameter| replace:: :raw-html:`<a class="paramtype" href="../../glossary.html#term-control-parameter" title="Glossary on Tuning Parameter"><span class="badge badge-warning">Tuning</span></a>` :raw-latex:`{\small\sc [Tuning Parameter]}`
-.. |ResourcesParameter| replace:: :raw-html:`<a class="paramtype" href="../../glossary.html#term-control-parameter" title="Glossary on Resources Parameter"><span class="badge badge-secondary">Resources</span></a>` :raw-latex:`{\small\sc [Resources Parameter]}`
+.. include:: /_prolog.rst
 """
 
 autodoc_default_options = {
