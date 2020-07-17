3c55f676efea1b73264b68d6bd3835859c82054a
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-07-17 13:05:17 +0200

    FIX Fix linting issues

--- sklearn/feature_selection/_rfe.py
+++ sklearn/feature_selection/_rfe.py
@@ -14,7 +14,6 @@ from ..utils.metaestimators import if_delegate_has_method
 from ..utils.metaestimators import _safe_split
 from ..utils.validation import check_is_fitted
 from ..utils.validation import _deprecate_positional_args
-from ..utils.deprecation import deprecated
 from ..base import BaseEstimator
 from ..base import MetaEstimatorMixin
 from ..base import clone
