67c7a3ebb510a18d15772e224964eeb1cee78504
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2020-07-17 11:49:06 +0200

    PEP8

--- sklearn/metrics/tests/test_ranking.py
+++ sklearn/metrics/tests/test_ranking.py
@@ -11,7 +11,6 @@ from sklearn.datasets import make_multilabel_classification
 from sklearn.datasets import load_breast_cancer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
 from sklearn.random_projection import _sparse_random_matrix
 from sklearn.utils import shuffle
 from sklearn.utils.extmath import softmax
