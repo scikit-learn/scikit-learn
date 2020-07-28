9706300910743daf7b7ce6b4e3a3e6e2c3bdc705
Thomas J. Fan <thomasjpfan@gmail.com>
2020-07-28 15:18:53 -0400

    MNT Adjust miniconda path

--- build_tools/circle/build_doc.sh
+++ build_tools/circle/build_doc.sh
@@ -146,13 +146,14 @@ if [[ `type -t deactivate` ]]; then
   deactivate
 fi
 
+MINICONDA_PATH=$HOME/miniconda
 # Install dependencies with miniconda
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
    -O miniconda.sh
 chmod +x miniconda.sh && ./miniconda.sh -b -p $MINICONDA_PATH
 export PATH="/usr/lib/ccache:$MINICONDA_PATH/bin:$PATH"
 
-ls $HOME/miniconda/bin
+ls $MINICONDA_PATH/bin
 echo $PATH
 
 conda
