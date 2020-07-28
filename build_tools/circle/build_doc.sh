407afc9be985b72f9469441f8fdb57cb393b2810
Thomas J. Fan <thomasjpfan@gmail.com>
2020-07-28 15:10:43 -0400

    MNT Look into miniconda path

--- build_tools/circle/build_doc.sh
+++ build_tools/circle/build_doc.sh
@@ -152,6 +152,8 @@ wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
 chmod +x miniconda.sh && ./miniconda.sh -b -p $MINICONDA_PATH
 export PATH="/usr/lib/ccache:$MINICONDA_PATH/bin:$PATH"
 
+ls $HOME/miniconda/bin
+
 conda
 
 ccache -M 512M
