0e2300893b448669f8a7a568209386aaa844c485
Thomas J. Fan <thomasjpfan@gmail.com>
2020-07-28 14:50:13 -0400

    MNT Pin version of conda

--- build_tools/circle/build_doc.sh
+++ build_tools/circle/build_doc.sh
@@ -147,7 +147,7 @@ if [[ `type -t deactivate` ]]; then
 fi
 
 # Install dependencies with miniconda
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
+wget https://repo.anaconda.com/miniconda/Miniconda3-py37_4.8.3-Linux-x86_64.sh \
    -O miniconda.sh
 chmod +x miniconda.sh && ./miniconda.sh -b -p $MINICONDA_PATH
 export PATH="/usr/lib/ccache:$MINICONDA_PATH/bin:$PATH"
@@ -165,7 +165,6 @@ fi
 
 # imports get_dep
 source build_tools/shared.sh
-which conda
 
 # packaging won't be needed once setuptools starts shipping packaging>=17.0
 conda create -n $CONDA_ENV_NAME --yes --quiet \
