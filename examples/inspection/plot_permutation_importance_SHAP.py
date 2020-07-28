dd7ca9ea0734c7242bad906189527c75ee7e5bc3
Lucy Liu <jliu176@gmail.com>
2020-07-28 12:18:17 +0200

    wip

---
+++ examples/inspection/plot_permutation_importance_SHAP.py
@@ -0,0 +1,11 @@
+"""
+==============================
+Permutation Importance vs SHAP
+==============================
+
+In this example, we will compare the 
+.. topic:: References:
+
+   [1] L. Breiman, "Random Forests", Machine Learning, 45(1), 5-32,
+       2001. https://doi.org/10.1023/A:1010933404324
+"""
