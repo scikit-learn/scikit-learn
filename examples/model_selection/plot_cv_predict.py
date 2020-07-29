11fe8c28b3066e05a9d5bfbf8365b334ab8bd778
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2020-07-29 14:24:07 +0200

    change another example

--- examples/model_selection/plot_cv_predict.py
+++ examples/model_selection/plot_cv_predict.py
@@ -6,23 +6,20 @@ Plotting Cross-Validated Predictions
 This example shows how to use
 :func:`~sklearn.model_selection.cross_val_predict` to visualize prediction
 errors.
-
 """
-from sklearn import datasets
-from sklearn.model_selection import cross_val_predict
-from sklearn import linear_model
 import matplotlib.pyplot as plt
+from sklearn.datasets import load_diabetes
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import PredictionErrorDisplay
+from sklearn.model_selection import cross_val_predict
 
-lr = linear_model.LinearRegression()
-X, y = datasets.load_diabetes(return_X_y=True)
+X, y = load_diabetes(return_X_y=True)
+lr = LinearRegression()
 
 # cross_val_predict returns an array of the same size as `y` where each entry
 # is a prediction obtained by cross validation:
-predicted = cross_val_predict(lr, X, y, cv=10)
+y_pred = cross_val_predict(lr, X, y, cv=10)
 
-fig, ax = plt.subplots()
-ax.scatter(y, predicted, edgecolors=(0, 0, 0))
-ax.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=4)
-ax.set_xlabel('Measured')
-ax.set_ylabel('Predicted')
+display = PredictionErrorDisplay(y_true=y, y_pred=y_pred)
+display.plot()
 plt.show()
