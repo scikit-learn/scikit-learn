4aa878c7b3eec60709ee971bb6c626e1e4e5ed04
Olivier Grisel <olivier.grisel@ensta.org>
2020-07-30 12:01:41 +0200

    DEBUG try to remove the function

--- build_tools/travis/test_script.sh
+++ build_tools/travis/test_script.sh
@@ -6,6 +6,8 @@
 
 # License: 3-clause BSD
 
+set -e
+
 python --version
 python -c "import numpy; print('numpy %s' % numpy.__version__)"
 python -c "import scipy; print('scipy %s' % scipy.__version__)"
@@ -24,47 +26,41 @@ if [[ "$BUILD_WITH_ICC" == "true" ]]; then
     source /opt/intel/inteloneapi/setvars.sh
 fi
 
-run_tests() {
-    # TEST_CMD="pytest --showlocals --durations=20 --pyargs"
-    TEST_CMD="pytest --pyargs"
+# TEST_CMD="pytest --showlocals --durations=20 --pyargs"
+TEST_CMD="pytest --pyargs"
 
-    # Get into a temp directory to run test from the installed scikit-learn and
-    # check if we do not leave artifacts
-    mkdir -p $TEST_DIR
-    cp test_hanging.py $TEST_DIR
-    # We need the setup.cfg for the pytest settings
-    # cp setup.cfg $TEST_DIR
-    cd $TEST_DIR
+# Get into a temp directory to run test from the installed scikit-learn and
+# check if we do not leave artifacts
+mkdir -p $TEST_DIR
+cp test_hanging.py $TEST_DIR
+# We need the setup.cfg for the pytest settings
+# cp setup.cfg $TEST_DIR
+cd $TEST_DIR
 
 
-    if [[ "$TRAVIS_CPU_ARCH" == "arm64" ]]; then
-        # use pytest-xdist for faster tests
-        # TEST_CMD="$TEST_CMD -n $CI_CPU_COUNT"
-        echo "disabled pytest-xdist"
-    else
-        # Tests that require large downloads over the networks are skipped in CI.
-        # Here we make sure, that they are still run on a regular basis.
-        #
-        # Note that using pytest-xdist is currently not compatible
-        # with fetching datasets in tests due to datasets cache corruptions issues.
-        export SKLEARN_SKIP_NETWORK_TESTS=0
-    fi
+if [[ "$TRAVIS_CPU_ARCH" == "arm64" ]]; then
+    # use pytest-xdist for faster tests
+    # TEST_CMD="$TEST_CMD -n $CI_CPU_COUNT"
+    echo "disabled pytest-xdist"
+else
+    # Tests that require large downloads over the networks are skipped in CI.
+    # Here we make sure, that they are still run on a regular basis.
+    #
+    # Note that using pytest-xdist is currently not compatible
+    # with fetching datasets in tests due to datasets cache corruptions issues.
+    export SKLEARN_SKIP_NETWORK_TESTS=0
+fi
 
-    if [[ "$COVERAGE" == "true" ]]; then
-        TEST_CMD="$TEST_CMD --cov sklearn"
-    fi
+if [[ "$COVERAGE" == "true" ]]; then
+    TEST_CMD="$TEST_CMD --cov sklearn"
+fi
 
-    if [[ -n "$CHECK_WARNINGS" ]]; then
-        TEST_CMD="$TEST_CMD -Werror::DeprecationWarning -Werror::FutureWarning"
-    fi
+if [[ -n "$CHECK_WARNINGS" ]]; then
+    TEST_CMD="$TEST_CMD -Werror::DeprecationWarning -Werror::FutureWarning"
+fi
 
-    pip uninstall -y pytest-xdist pytest-forked pytest-cov
-    echo "About to run pytest --version"
-    pytest --version
-    echo "About to run pytest with faulthandler"
-    pytest -s -v test_hanging.py || travis_terminate 1
-}
-
-echo "before run_tests function"
-run_tests
-echo "after run_tests function"
+pip uninstall -y pytest-xdist pytest-forked pytest-cov
+echo "About to run pytest --version"
+pytest --version
+echo "About to run pytest with faulthandler"
+pytest -s -v test_hanging.py
