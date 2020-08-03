73be8048b20dda4ac1fb5a4621733779a36080e6
Olivier Grisel <olivier.grisel@ensta.org>
2020-08-03 14:16:03 +0200

    FIX test_full_vs_elkan to avoid failure with Py 3.9b5

--- sklearn/cluster/tests/test_k_means.py
+++ sklearn/cluster/tests/test_k_means.py
@@ -746,7 +746,9 @@ def test_full_vs_elkan():
     km1 = KMeans(algorithm='full', random_state=13).fit(X)
     km2 = KMeans(algorithm='elkan', random_state=13).fit(X)
 
-    assert homogeneity_score(km1.predict(X), km2.predict(X)) == 1.0
+    assert homogeneity_score(
+        km1.predict(X), km2.predict(X)
+    ) == pytest.approx(1.0)
 
 
 def test_n_init():
