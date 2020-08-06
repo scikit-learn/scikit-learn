282ac37c12b62d70898a1f1dc0e832e770854e74
Arthur Colombini Gusmão <arthurcgusmao@hotmail.com>
2020-08-06 02:22:38 +0200

    DOC MCC-F1-Curve Documentation
    Wrote documentation for the MCC-F1 Curve function.
    
    Possible further improvements:
    1. Drop intermediate points that would not appear on a plotted curve,
    similarly to how this is implemented in the `roc_curve`. See if this
    impacts the calculation of the MCC-F1 metric afterwards.

--- sklearn/metrics/_ranking.py
+++ sklearn/metrics/_ranking.py
@@ -827,9 +827,59 @@ def roc_curve(y_true, y_score, *, pos_label=None, sample_weight=None,
 
 
 @_deprecate_positional_args
-def mcc_f1_curve(y_true, y_score, *, pos_label=None, sample_weight=None):
-    """foapispfjaoisd
-    """
+def mcc_f1_curve(y_true, y_score, *, pos_label=None, sample_weight=None,
+                 unit_normalize_mcc=True):
+    """Compute the MCC-F1 curve
+
+    The MCC-F1 curve combines the Matthews correlation coefficient and the
+    F1-Score to clearly differentiate good and bad *binary* classifiers,
+    especially with imbalanced ground truths.
+
+    It has been recently proposed as a better alternative for the receiver
+    operating characteristic (ROC) curve and the precision-recall (PR) curves.
+    [1]
+
+    Parameters
+    ----------
+    y_true : array, shape = [n_samples]
+        True binary labels. If labels are not either {-1, 1} or {0, 1}, then
+        pos_label should be explicitly given.
+
+    y_score : array, shape = [n_samples]
+        Estimated probabilities or decision function.
+
+    pos_label : int or str, default=None
+        The label of the positive class.
+        When ``pos_label=None``, if y_true is in {-1, 1} or {0, 1},
+        ``pos_label`` is set to 1, otherwise an error will be raised.
+
+    sample_weight : array-like of shape (n_samples,), default=None
+        Sample weights.
+
+    unit_normalize_mcc : bool, default=True
+        Whether to unit-normalize the MCC values, as in the original paper.
+
+    Returns
+    -------
+    mcc : array, shape = [n_thresholds]
+        MCC values such that element i is the MCC of predictions with
+        score >= thresholds[i].
+
+    f1 : array, shape = [n_thresholds]
+        F1-Score values such that element i is the F1-Score of predictions with
+        score >= thresholds[i].
+
+    thresholds : array, shape = [n_thresholds <= len(np.unique(probas_pred))]
+        Increasing thresholds on the decision function used to compute
+        MCC and F1.
+
+    References
+    ----------
+    .. [1] `Chang Cao and Davide Chicco and Michael M. Hoffman. (2020)
+            The MCC-F1 curve: a performance evaluation technique for binary
+            classification.
+            <https://arxiv.org/pdf/2006.11278>`_
+"""
     fps, tps, thresholds = _binary_clf_curve(
         y_true, y_score, pos_label=pos_label, sample_weight=sample_weight)
 
@@ -841,9 +891,10 @@ def mcc_f1_curve(y_true, y_score, *, pos_label=None, sample_weight=None):
     with np.errstate(divide='ignore', invalid='ignore'):
         mccs = np.divide(n*tps - p*fps, np.sqrt(p*n*ps*(p + n - ps)))
     np.nan_to_num(mccs, nan=0., copy=False)
-    mccs = (mccs + 1) / 2   # Unit-normalize MCC values
+    if unit_normalize_mcc:
+        mccs = (mccs + 1) / 2   # Unit-normalize MCC values
 
-    # Compute F1-Score
+    # Compute F1
     f1s = 2*tps / (ps + p)
 
     return mccs, f1s, thresholds
