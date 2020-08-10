4d7bb052f30c1b96f3159e03e6d5ca31a726d542
Nicolas Hug <contact@nicolas-hug.com>
2020-08-10 13:25:52 -0400

    Apply suggestions from code review
    Co-authored-by: Tom Dupré la Tour <tom.dupre-la-tour@m4x.org>

--- examples/cross_decomposition/plot_pcr_vs_pls.py
+++ examples/cross_decomposition/plot_pcr_vs_pls.py
@@ -109,18 +109,19 @@ pca = pcr.named_steps['pca']  # retrieve the PCA step of the pipeline
 pls = PLSRegression(n_components=1)
 pls.fit(X_train, y_train)
 
-fig, axes = plt.subplots(1, 2, figsize=(10, 2))
+fig, axes = plt.subplots(1, 2, figsize=(10, 3))
 axes[0].scatter(pca.transform(X_test), y_test, alpha=.3, label='ground truth')
 axes[0].scatter(pca.transform(X_test), pcr.predict(X_test), alpha=.3,
                 label='predictions')
 axes[0].set(xlabel='Projected data onto first PCA component',
             ylabel='y', title='PCR / PCA')
+axes[0].legend()
 axes[1].scatter(pls.transform(X_test), y_test, alpha=.3, label='ground truth')
 axes[1].scatter(pls.transform(X_test), pls.predict(X_test), alpha=.3,
                 label='predictions')
 axes[1].set(xlabel='Projected data onto first PLS component',
             ylabel='y', title='PLS')
-plt.legend()
+axes[1].legend()
 plt.tight_layout()
 plt.show()
 
