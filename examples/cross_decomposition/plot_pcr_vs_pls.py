ce7e65e5a5e355192b75400dae0f075de9f56a60
Nicolas Hug <contact@nicolas-hug.com>
2020-08-10 13:32:09 -0400

    small green component wasn't super obvious

--- examples/cross_decomposition/plot_pcr_vs_pls.py
+++ examples/cross_decomposition/plot_pcr_vs_pls.py
@@ -58,7 +58,7 @@ plt.scatter(X[:, 0], X[:, 1], alpha=.3, label='samples')
 for i, (comp, var) in enumerate(zip(pca.components_, pca.explained_variance_)):
     comp = comp * var  # scale component by its variance explanation power
     plt.plot([0, comp[0]], [0, comp[1]], label=f"Component {i}", linewidth=5,
-             color=f"C{i + 1}")
+             color=f"C{i + 2}")
 plt.gca().set(aspect='equal',
               title="2-dimensional dataset with principal components",
               xlabel='first feature', ylabel='second feature')
