cb35132da38d0f1f57776ce5fa7b65babb709791
Bharat123rox <bharatraghunthan9767@gmail.com>
2020-08-15 00:30:47 +0530

    MNT: Tweak Error Message depending on config

--- sklearn/neighbors/_base.py
+++ sklearn/neighbors/_base.py
@@ -614,11 +614,18 @@ class KNeighborsMixin:
 
         n_samples_fit = self.n_samples_fit_
         if n_neighbors > n_samples_fit:
-            raise ValueError(
-                "Expected n_neighbors < n_samples, "
-                " but n_samples = %d, n_neighbors = %d" %
-                (n_samples_fit, original_n_neighbors)
-            )
+            if X is None:
+                raise ValueError(
+                    "Expected n_neighbors < n_samples, "
+                    " but n_samples = %d, n_neighbors = %d" %
+                    (n_samples_fit, original_n_neighbors)
+                )
+            else:
+                raise ValueError(
+                    "Expected n_neighbors <= n_samples, "
+                    " but n_samples = %d, n_neighbors = %d" %
+                    (n_samples_fit, original_n_neighbors)
+                )
 
         n_jobs = effective_n_jobs(self.n_jobs)
         chunked_results = None
