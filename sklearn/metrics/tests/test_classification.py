6a12a1ff396e7891763bfd9f1f50fd784d24900f
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2020-08-19 15:11:36 +0200

    PEP8

--- sklearn/metrics/tests/test_classification.py
+++ sklearn/metrics/tests/test_classification.py
@@ -2262,8 +2262,8 @@ def test_brier_score_loss():
     y_true = np.array([0, 1, 2, 0])
     y_pred = np.array([0.8, 0.6, 0.4, 0.2])
     error_message = (
-        "Only binary classification is supported. Labels in y_true: "
-        "\[0 1 2\]"
+        r"Only binary classification is supported. Labels in y_true: "
+        r"\[0 1 2\]"
     )
 
     with pytest.raises(ValueError, match=error_message):
