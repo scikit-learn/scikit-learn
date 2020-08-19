8630620a7ef14283be5c6e0c1f37d07391b6e705
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2020-08-19 11:27:31 +0200

    iter

--- sklearn/metrics/tests/test_common.py
+++ sklearn/metrics/tests/test_common.py
@@ -1415,33 +1415,57 @@ def test_thresholded_metric_permutation_invariance(name):
         assert_almost_equal(score, current_score)
 
 
-@pytest.mark.parametrize(
-    "metric",
-    [
-        average_precision_score,
-        brier_score_loss,
-        f1_score,
-        fbeta_score,
-        jaccard_score,
-        precision_recall_curve,
-        precision_score,
-        recall_score,
-        roc_curve,
-    ],
-)
-def test_metrics_pos_label_error_str(metric):
-    # check that the error message if `pos_label` is not specified and the
-    # targets is made of strings.
+@pytest.mark.parametrize("metric_name", CLASSIFICATION_METRICS)
+def test_metrics_consistent_type_error(metric_name):
+    # check that an understable message is raised when the type between y_true
+    # and y_pred mismatch
     rng = np.random.RandomState(42)
     y1 = np.array(["spam"] * 3 + ["eggs"] * 2, dtype=object)
     y2 = rng.randint(0, 2, size=y1.size)
+
+    err_msg = "Labels in y_true and y_pred should be of the same type."
+    with pytest.raises(TypeError, match=err_msg):
+        CLASSIFICATION_METRICS[metric_name](y1, y2)
+
+
+@pytest.mark.parametrize(
+    "metric, y_pred_threshold",
+    [
+        (average_precision_score, True),
+        (brier_score_loss, True),
+        (f1_score, False),
+        (partial(fbeta_score, beta=1), False),
+        (jaccard_score, False),
+        (precision_recall_curve, True),
+        (precision_score, False),
+        (recall_score, False),
+        (roc_curve, True),
+    ],
+)
+@pytest.mark.parametrize("dtype_y_str", [None, object])
+def test_metrics_pos_label_error_str(metric, y_pred_threshold, dtype_y_str):
+    # check that the error message if `pos_label` is not specified and the
+    # targets is made of strings.
+    rng = np.random.RandomState(42)
+    y1 = np.array(["spam"] * 3 + ["eggs"] * 2)
+    if dtype_y_str is not None:
+        y1 = y1.astype(dtype_y_str)
+    y2 = rng.randint(0, 2, size=y1.size)
+
+    if not y_pred_threshold:
+        y2 = np.array(["spam", "eggs"])[y2]
+        if dtype_y_str is not None:
+            y2 = y2.astype(dtype_y_str)
 
     err_msg_pos_label_None = (
         "y_true takes value in {'eggs', 'spam'} and pos_label is not "
         "specified: either make y_true take value in {0, 1} or {-1, 1} or "
         "pass pos_label explicit"
     )
-    err_msg_pos_label_1 = "pos_label=1 is invalid. Set it to a label in y_true"
+    err_msg_pos_label_1 = (
+        r"pos_label=1 is not a valid label. It should be one of "
+        r"\['eggs', 'spam'\]"
+    )
 
     pos_label_default = signature(metric).parameters["pos_label"].default
 
