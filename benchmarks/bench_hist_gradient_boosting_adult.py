c3061b5e22c0043d84c76f7fe8b8d2a99f3bc627
Thomas J. Fan <thomasjpfan@gmail.com>
2020-08-24 21:26:23 -0400

    ENH Uses mask instead of pandas features in benchmark

--- benchmarks/bench_hist_gradient_boosting_adult.py
+++ benchmarks/bench_hist_gradient_boosting_adult.py
@@ -1,6 +1,7 @@
 import argparse
 from time import time
 
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import fetch_openml
 from sklearn.metrics import accuracy_score, roc_auc_score
@@ -12,7 +13,7 @@ from sklearn.ensemble._hist_gradient_boosting.utils import (
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--n-leaf-nodes', type=int, default=31)
-parser.add_argument('--n-trees', type=int, default=10)
+parser.add_argument('--n-trees', type=int, default=50)
 parser.add_argument('--lightgbm', action="store_true", default=False)
 parser.add_argument('--learning-rate', type=float, default=1.)
 parser.add_argument('--max-bins', type=int, default=255)
@@ -25,10 +26,10 @@ lr = args.learning_rate
 max_bins = args.max_bins
 
 
-def fit(est, data_train, target_train, libname):
+def fit(est, data_train, target_train, libname, **fit_params):
     print(f"Fitting a {libname} model...")
     tic = time()
-    est.fit(data_train, target_train)
+    est.fit(data_train, target_train, **fit_params)
     toc = time()
     print(f"fitted in {toc - tic:.3f}s")
 
@@ -52,30 +53,36 @@ data, target = fetch_openml(data_id=179, as_frame=True, return_X_y=True)
 target = target.cat.codes
 
 n_features = data.shape[1]
-n_categorical_features = (data.dtypes == 'category').sum()
+is_categorical = data.dtypes == 'category'
+n_categorical_features = is_categorical.sum()
 n_numerical_features = (data.dtypes == 'float').sum()
 print(f"Number of features: {data.shape[1]}")
-print(f"Number of categorical featuers: {n_categorical_features}")
-print(f"Number of numerical featuers: {n_numerical_features}")
+print(f"Number of categorical features: {n_categorical_features}")
+print(f"Number of numerical features: {n_numerical_features}")
+
+categorical_features = np.flatnonzero(data.dtypes == 'category')
+for i in categorical_features:
+    data.iloc[:, i] = data.iloc[:, i].cat.codes
 
 data_train, data_test, target_train, target_test = train_test_split(
     data, target, test_size=.2, random_state=0)
-
 
 est = HistGradientBoostingClassifier(loss='binary_crossentropy',
                                      learning_rate=lr,
                                      max_iter=n_trees,
                                      max_bins=max_bins,
-                                     categorical_features='pandas',
+                                     categorical_features=categorical_features,
                                      max_leaf_nodes=n_leaf_nodes,
                                      early_stopping=False,
                                      random_state=0,
                                      verbose=1)
+
 fit(est, data_train, target_train, 'sklearn')
 predict(est, data_test, target_test)
 
-# lightgbm infers the cateogires from the dtype
+# lightgbm infers the categories from the dtype
 if args.lightgbm:
     est = get_equivalent_estimator(est, lib='lightgbm')
-    fit(est, data_train, target_train, 'lightgbm')
+    fit(est, data_train, target_train, 'lightgbm',
+        categorical_feature=is_categorical.index.tolist())
     predict(est, data_test, target_test)
