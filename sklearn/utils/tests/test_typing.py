667518e528f1f6a6d598447c1013b0bf54658725
Thomas J. Fan <thomasjpfan@gmail.com>
2020-08-27 14:57:48 -0400

    TST Uses double quotes for strings

--- sklearn/utils/tests/test_typing.py
+++ sklearn/utils/tests/test_typing.py
@@ -36,21 +36,21 @@ def test_get_annotation_class_name(annotation, expected_class):
 
 
 @pytest.mark.parametrize("annotation, expected_str", [
-    (None, 'None'),
-    (BaseEstimator, 'estimator instance'),
-    (np.random.RandomState, 'RandomState instance'),
-    (int, 'int'),
+    (None, "None"),
+    (BaseEstimator, "estimator instance"),
+    (np.random.RandomState, "RandomState instance"),
+    (int, "int"),
     (float, 'float'),
-    (list, 'list'),
-    (str, 'str'),
-    (List[int], 'list of int'),
-    (Optional[List[int]], 'list of int or None'),
-    (List[BaseEstimator], 'list of estimator instance'),
-    (Optional[BaseEstimator], 'estimator instance or None'),
-    (Union[int, float], 'int or float'),
-    (Literal['cat', 'dog'], '{\'cat\', \'dog\'}'),
-    (Literal['cat'], '\'cat\''),
-    (RandomState, 'int, RandomState instance or None')
+    (list, "list"),
+    (str, "str"),
+    (List[int], "list of int"),
+    (Optional[List[int]], "list of int or None"),
+    (List[BaseEstimator], "list of estimator instance"),
+    (Optional[BaseEstimator], "estimator instance or None"),
+    (Union[int, float], "int or float"),
+    (Literal['cat', 'dog'], "{'cat', 'dog'}"),
+    (Literal['cat'], "'cat'"),
+    (RandomState, "int, RandomState instance or None")
 ])
 def test_format_docstring_annotation(annotation, expected_str):
     assert _format_docstring_annotation(annotation) == expected_str
@@ -68,10 +68,10 @@ class TestObject:
 
 def test_get_docstring_annotations():
     annotations = get_docstring_annotations(TestObject.__init__)
-    assert annotations['estimator'] == 'estimator instance'
-    assert annotations['num'] == 'int, default=10'
-    assert annotations['float_num'] == 'float, default='
-    assert annotations['union_num'] == 'int or float, default='
-    assert annotations['pet'] == '{\'cat\', \'dog\'}, default=\'dog\''
-    assert annotations['random_state'] == ('int, RandomState instance or None'
-                                           ', default=None')
+    assert annotations['estimator'] == "estimator instance"
+    assert annotations['num'] == "int, default=10"
+    assert annotations['float_num'] == "float, default="
+    assert annotations['union_num'] == "int or float, default="
+    assert annotations['pet'] == "{'cat', 'dog'}, default='dog'"
+    assert annotations['random_state'] == ("int, RandomState instance or None"
+                                           ", default=None")
