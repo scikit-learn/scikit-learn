42d4892aa56cc19421220c19e667e6a02495109a
Harsh Soni <devilincarcerated020@yahoo.com>
2020-08-27 17:50:26 +0530

    Add new ValueError tests in test_base

--- sklearn/impute/tests/test_base.py
+++ sklearn/impute/tests/test_base.py
@@ -31,6 +31,23 @@ class NoTransformIndicatorImputer(_BaseImputer):
         return self._concatenate_indicator(X, None)
 
 
+class NoPrecomputedMaskFit(_BaseImputer):
+    def fit(self, X, y=None):
+        super()._fit_indicator(X)
+        return self
+    def transform(self, X):
+        return self._concatenate_indicator(X, self._transform_indicator(X))
+
+
+class NoPrecomputedMaskTransform(_BaseImputer):
+    def fit(self, X, y=None):
+        mask = _get_mask(X, value_to_mask=np.nan)
+        super()._fit_indicator(X)
+        return self
+    def transform(self, X):
+        return self._concatenate_indicator(X, self._transform_indicator(X))
+
+
 def test_base_imputer_not_fit(data):
     imputer = NoFitIndicatorImputer(add_indicator=True)
     err_msg = "Make sure to call _fit_indicator before _transform_indicator"
@@ -44,6 +61,26 @@ def test_base_imputer_not_transform(data):
     imputer = NoTransformIndicatorImputer(add_indicator=True)
     err_msg = ("Call _fit_indicator and _transform_indicator in the "
                "imputer implementation")
+    with pytest.raises(ValueError, match=err_msg):
+        imputer.fit(data).transform(data)
+    with pytest.raises(ValueError, match=err_msg):
+        imputer.fit_transform(data)
+
+
+def test_base_no_precomputed_mask_fit(data):
+    imputer = NoPrecomputedMaskFit(add_indicator=True)
+    err_msg = "precomputed is True but the input data is not a mask"
+    with pytest.raises(ValueError, match=err_msg):
+        imputer.fit(data)
+    with pytest.raises(ValueError, match=err_msg):
+        imputer.fit(data).transform(data)
+    with pytest.raises(ValueError, match=err_msg):
+        imputer.fit_transform(data)
+
+
+def test_base_no_precomputed_mask_transform(data):
+    imputer = NoPrecomputedMaskTransform(add_indicator=True)
+    err_msg = "precomputed is True but the input data is not a mask"
     with pytest.raises(ValueError, match=err_msg):
         imputer.fit(data).transform(data)
     with pytest.raises(ValueError, match=err_msg):
