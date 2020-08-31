c156ddfe066130b0d17b0693b34f33e8d4eac1dc
Norbert Preining <norbert@preining.info>
2020-09-01 00:04:15 +0900

    Fix incorrect redefinition of classes_
    in case that incremental == False and warm_start == True, the current
    status would redefine classes_ even if they are already present.
    
    Rework the test logic to only redefine in cases where it is meant to
    be (not incremental, not warm_start, or classes_ not defined)

--- sklearn/neural_network/_multilayer_perceptron.py
+++ sklearn/neural_network/_multilayer_perceptron.py
@@ -982,24 +982,42 @@ class MLPClassifier(ClassifierMixin, BaseMultilayerPerceptron):
         if y.ndim == 2 and y.shape[1] == 1:
             y = column_or_1d(y, warn=True)
 
-        if self.warm_start and (incremental or hasattr(self, "classes_")):
-            classes = unique_labels(y)
-            if set(classes) != set(self.classes_):
-                raise ValueError("warm_start can only be used where `y` has "
-                                 "the same classes as in the previous "
-                                 "call to fit. Previously got %s, `y` has %s" %
-                                 (self.classes_, classes))
-
-        if not incremental:
+        # Matrix of actions to be taken under the possible combinations:
+        # The case that incremental == True and classes_ not defined is
+        # already checked by _check_partial_fit_first_call that is called
+        # in _partial_fit below.
+        # The cases are already grouped into the respective if blocks below.
+        #
+        # incremental warm_start classes_ def  action
+        #    0            0         0        define classes_
+        #    0            1         0        define classes_
+        #    0            0         1        redefine classes_
+        #
+        #    0            1         1        check compat warm_start
+        #    1            1         1        check compat warm_start
+        #
+        #    1            0         1        check compat last fit
+        #
+        # Note the reliance on short-circuiting here, so that the second
+        # or part implies that classes_ is defined.
+        if (not hasattr(self, "classes_")) or (not self.warm_start and
+                                               not incremental):
             self._label_binarizer = LabelBinarizer()
             self._label_binarizer.fit(y)
             self.classes_ = self._label_binarizer.classes_
         else:
             classes = unique_labels(y)
-            if len(np.setdiff1d(classes, self.classes_, assume_unique=True)):
-                raise ValueError("`y` has classes not in `self.classes_`."
-                                 " `self.classes_` has %s. 'y' has %s." %
-                                 (self.classes_, classes))
+            if self.warm_start:
+                if set(classes) != set(self.classes_):
+                    raise ValueError("warm_start can only be used where `y` has "
+                                     "the same classes as in the previous "
+                                     "call to fit. Previously got %s, `y` has %s" %
+                                     (self.classes_, classes))
+            else:
+                if len(np.setdiff1d(classes, self.classes_, assume_unique=True)):
+                    raise ValueError("`y` has classes not in `self.classes_`."
+                                     " `self.classes_` has %s. 'y' has %s." %
+                                     (self.classes_, classes))
 
         # This downcast to bool is to prevent upcasting when working with
         # float32 data
