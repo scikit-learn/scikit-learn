507b391a629d1e3bb16bcc6d8d301779444678c2
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2020-08-31 15:14:14 +0200

    iter

--- sklearn/metrics/_ranking.py
+++ sklearn/metrics/_ranking.py
@@ -403,13 +403,12 @@ def roc_auc_score(y_true, y_score, *, average="macro", sample_weight=None,
     >>> from sklearn.multioutput import MultiOutputClassifier
     >>> X, y = make_multilabel_classification(random_state=0)
     >>> clf = MultiOutputClassifier(clf).fit(X, y)
-    >>> roc_auc_score(
-    ...     y,
-    ...     np.transpose([
-    ...         y_pred[:, 1] for y_pred in clf.predict_proba(X)
-    ...     ]),
-    ...     average=None
-    ... )
+    >>> # get a list of n_output containing probability arrays of shape
+    >>> # (n_samples, n_classes)
+    >>> y_pred = clf.predict_proba(X)
+    >>> # extract the positive columns for each output
+    >>> y_pred = np.transpose([pred[:, 1] for pred in y_pred])
+    >>> roc_auc_score(y, y_pred, average=None)
     array([0.82..., 0.86..., 0.94..., 0.85... , 0.94...])
     >>> from sklearn.linear_model import RidgeClassifierCV
     >>> clf = RidgeClassifierCV().fit(X, y)
