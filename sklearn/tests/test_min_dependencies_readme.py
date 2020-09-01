b82320ea073ac027b7396df86ef0d9bd36fdc915
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-09-01 12:49:02 +0200

    FIX Skip test if file not found

--- sklearn/tests/test_min_dependencies_readme.py
+++ sklearn/tests/test_min_dependencies_readme.py
@@ -1,27 +1,38 @@
 """Tests for the minimum dependencies in the README.rst file."""
 
+import os
 import re
-from pathlib import Path
 from packaging.version import parse
 
+import pytest
+import sklearn
 from sklearn._build_utils.min_dependencies import dependent_packages
 
 
 def test_min_dependencies_readme():
-    # Test that the minimum dependencies in the README.rst file
-    # are compliant with the minimum dependencies for scikit-learn
+    # Test that the minimum dependencies in the README.rst file are
+    # consistent with the minimum dependencies defined at the file:
+    # sklearn/_build_utils/min_dependencies.py
+
     pattern = re.compile(r"(\.\. \|)" +
                          r"(([A-Za-z]+\-?)+)" +
                          r"(MinVersion\| replace::)" +
                          r"( [0-9]+\.[0-9]+(\.[0-9]+)?)")
 
-    with (Path(__file__).parents[2] / "README.rst").open("r") as f:
+    readme_path = os.path.abspath(os.path.join(sklearn.__path__[0], ".."))
+    readme_filename = os.path.join(readme_path, "README.rst")
+
+    if not os.path.exists(readme_filename):
+        # Skip because the README.rst
+        # file is not found in Windows
+        pytest.skip("README.rst is not available.")
+
+    with open(readme_filename, "r") as f:
         for line in f:
             if pattern.match(line):
                 dependency = pattern.sub(r"\2\5", line)
                 (package, version) = dependency.lower().split(" ")
 
-                # Avoid non-defined packages
                 if package in dependent_packages:
                     version = parse(version)
                     min_version = parse(dependent_packages[package][0])
