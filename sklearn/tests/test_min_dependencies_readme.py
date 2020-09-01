8c6d898c52435789221f2b35899a0599e307ada2
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-09-01 09:53:31 +0200

    FIX Fix path issues in Windows

--- sklearn/tests/test_min_dependencies_readme.py
+++ sklearn/tests/test_min_dependencies_readme.py
@@ -1,10 +1,10 @@
 """Tests for the minimum dependencies in the README.rst file."""
 
-import os
+
 import re
 from packaging.version import parse
+from pathlib import Path
 
-import sklearn
 from sklearn._build_utils.min_dependencies import dependent_packages
 
 
@@ -16,7 +16,7 @@ def test_min_dependencies_readme():
                          r"(MinVersion\| replace::)" +
                          r"( [0-9]+\.[0-9]+(\.[0-9]+)?)")
 
-    with open(os.path.join(sklearn.__path__[0], "..", "README.rst"), "r") as f:
+    with (Path("../..") / "README.rst").open("r") as f:
         for line in f:
             if pattern.match(line):
                 dependency = pattern.sub(r"\2\5", line)
