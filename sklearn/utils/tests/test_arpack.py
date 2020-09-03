1d5c108d175f6dccee272eeb058545d785334ba6
Gaurav Desai <gauravdesai111@gmail.com>
2020-09-03 16:27:21 +0530

    Reverted test case changes

--- sklearn/utils/tests/test_arpack.py
+++ sklearn/utils/tests/test_arpack.py
@@ -5,7 +5,8 @@ from sklearn.utils.arpack import _init_arpack_v0
 
 def test_init_arpack_v0():
     v0s = [_init_arpack_v0(1000, 0)]
-    for i in range(1, 101):
+    for i in range(1, 100):
+        v0s.append(_init_arpack_v0(1000, i))
         assert not any(np.equal(v0s[i], v0s[i-1]))
 
     v0 = np.concatenate(v0s)
