49d26cb63fefe43c9b310136e4f2c172d8c433cb
Roman Yurchak <rth.yurchak@gmail.com>
2020-09-03 15:42:43 +0200

    Use ccache for faster CI builds (#16434)

--- build_tools/azure/install.sh
+++ build_tools/azure/install.sh
@@ -11,12 +11,23 @@ make_conda() {
     source activate $VIRTUALENV
 }
 
+setup_ccache() {
+    echo "Setting up ccache"
+    mkdir /tmp/ccache/
+    which ccache
+    for name in gcc g++ cc c++ x86_64-linux-gnu-gcc x86_64-linux-gnu-c++; do
+      ln -s $(which ccache) "/tmp/ccache/${name}"
+    done
+    export PATH="/tmp/ccache/:${PATH}"
+    ccache -M 256M
+}
+
 # imports get_dep
 source build_tools/shared.sh
 
 if [[ "$DISTRIB" == "conda" ]]; then
 
-    TO_INSTALL="python=$PYTHON_VERSION pip blas[build=$BLAS]"
+    TO_INSTALL="python=$PYTHON_VERSION ccache pip blas[build=$BLAS]"
 
     TO_INSTALL="$TO_INSTALL $(get_dep numpy $NUMPY_VERSION)"
     TO_INSTALL="$TO_INSTALL $(get_dep scipy $SCIPY_VERSION)"
@@ -38,22 +49,25 @@ if [[ "$DISTRIB" == "conda" ]]; then
         fi
     fi
 	make_conda $TO_INSTALL
+    setup_ccache
 
 elif [[ "$DISTRIB" == "ubuntu" ]]; then
     sudo add-apt-repository --remove ppa:ubuntu-toolchain-r/test
     sudo apt-get update
-    sudo apt-get install python3-scipy python3-matplotlib libatlas3-base libatlas-base-dev python3-virtualenv
+    sudo apt-get install python3-scipy python3-matplotlib libatlas3-base libatlas-base-dev python3-virtualenv ccache
     python3 -m virtualenv --system-site-packages --python=python3 $VIRTUALENV
     source $VIRTUALENV/bin/activate
+    setup_ccache
     python -m pip install $(get_dep cython $CYTHON_VERSION) \
                           $(get_dep joblib $JOBLIB_VERSION)
 
 elif [[ "$DISTRIB" == "ubuntu-32" ]]; then
     apt-get update
-    apt-get install -y python3-dev python3-scipy python3-matplotlib libatlas3-base libatlas-base-dev python3-virtualenv python3-pandas
+    apt-get install -y python3-dev python3-scipy python3-matplotlib libatlas3-base libatlas-base-dev python3-virtualenv python3-pandas ccache
 
     python3 -m virtualenv --system-site-packages --python=python3 $VIRTUALENV
     source $VIRTUALENV/bin/activate
+    setup_ccache
     python -m pip install $(get_dep cython $CYTHON_VERSION) \
                           $(get_dep joblib $JOBLIB_VERSION)
 
@@ -61,7 +75,8 @@ elif [[ "$DISTRIB" == "conda-pip-latest" ]]; then
     # Since conda main channel usually lacks behind on the latest releases,
     # we use pypi to test against the latest releases of the dependencies.
     # conda is still used as a convenient way to install Python and pip.
-    make_conda "python=$PYTHON_VERSION"
+    make_conda "ccache python=$PYTHON_VERSION"
+    setup_ccache
     python -m pip install -U pip
 
     python -m pip install pandas matplotlib pyamg scikit-image
@@ -69,12 +84,13 @@ elif [[ "$DISTRIB" == "conda-pip-latest" ]]; then
     # and install a version less than 3.0.0 until the issue #18316 is solved.
     python -m pip install "lightgbm<3.0.0" --no-deps
 elif [[ "$DISTRIB" == "conda-pip-scipy-dev" ]]; then
-    make_conda "python=$PYTHON_VERSION"
+    make_conda "ccache python=$PYTHON_VERSION"
     python -m pip install -U pip
     echo "Installing numpy and scipy master wheels"
     dev_anaconda_url=https://pypi.anaconda.org/scipy-wheels-nightly/simple
     pip install --pre --upgrade --timeout=60 --extra-index $dev_anaconda_url numpy scipy pandas
     pip install --pre cython
+    setup_ccache
     echo "Installing joblib master"
     pip install https://github.com/joblib/joblib/archive/master.zip
     echo "Installing pillow master"
@@ -123,3 +139,5 @@ else
     python setup.py build_ext --inplace -j 3
     python setup.py develop
 fi
+
+ccache -s
