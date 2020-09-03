acea59d2d7bd6c251163fbe632bfc5ccff83e69b
Gaurav Desai <35183589+gauravkdesai@users.noreply.github.com>
2020-09-03 15:30:35 +0530

    Update sklearn/utils/tests/test_arpack.py
    Co-authored-by: Thomas J. Fan <thomasjpfan@gmail.com>

--- sklearn/utils/tests/test_arpack.py
+++ sklearn/utils/tests/test_arpack.py
@@ -4,11 +4,9 @@ from sklearn.utils.arpack import _init_arpack_v0
 
 
 def test_init_arpack_v0():
-    v0s = []
-    for i in range(100):
-        v0s.append(_init_arpack_v0(1000, i))
-        if i > 0:
-            assert not any(np.equal(v0s[i], v0s[i-1]))
+    v0s = [_init_arpack_v0(1000, 0)]
+    for i in range(1, 101):
+        assert not any(np.equal(v0s[i], v0s[i-1]))
 
     v0 = np.concatenate(v0s)
     assert np.allclose(np.mean(v0), 0, atol=1e-2)
