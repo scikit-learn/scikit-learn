61ce664d4db5574ba9f97160900be1b4f2171cf7
Jonathan Banda <jonathanbanda00@gmail.com>
2020-09-05 03:06:29 -0500

    deleted duplicated word {the} and and made sentences more clear.

--- examples/linear_model/plot_ols.py
+++ examples/linear_model/plot_ols.py
@@ -8,11 +8,11 @@ Linear Regression Example
 The example below uses only the first feature of the `diabetes` dataset,
 in order to illustrate the data points within the two-dimensional plot.
 The straight line can be seen in the plot, showing how linear regression
-attempts to draw a straight line that will best minimize the 
+attempts to draw a straight line that will best minimize the
 residual sum of squares between the observed responses in the dataset,
 and the responses predicted by the linear approximation.
 
-The coefficients, residual sum of squares and the coefficient of 
+The coefficients, residual sum of squares and the coefficient of
 determination are also calculated.
 
 
