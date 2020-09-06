52524faf4dd9ba26487c6dacc1f0585ce2382e6d
Gaurav Desai <gauravdesai111@gmail.com>
2020-09-06 19:31:33 +0530

    Reverted test cases to original

--- sklearn/utils/tests/test_arpack.py
+++ sklearn/utils/tests/test_arpack.py
@@ -4,8 +4,11 @@ from sklearn.utils._arpack import _init_arpack_v0
 
 
 def test_init_arpack_v0():
-    v0s = [_init_arpack_v0(1000, i) for i in range(100)]
-    assert not any([np.equal(v0s[i], v0s[i-1]) for i in range(1, 100)])
+    v0s = []
+    for i in range(100):
+        v0s.append(_init_arpack_v0(1000, i))
+        if i > 0:
+            assert not any(np.equal(v0s[i], v0s[i-1]))
 
     v0 = np.concatenate(v0s)
     assert np.allclose(np.mean(v0), 0, atol=1e-2)
