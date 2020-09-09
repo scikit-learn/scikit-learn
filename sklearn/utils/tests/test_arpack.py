2d4ad045865bda5ca453ca8b60471512d91096bd
Gaurav Desai <gauravdesai111@gmail.com>
2020-09-09 09:42:00 +0530

    Updated test case to include range check

--- sklearn/utils/tests/test_arpack.py
+++ sklearn/utils/tests/test_arpack.py
@@ -10,6 +10,8 @@ def test_init_arpack_v0():
         if i > 0:
             assert not any(np.equal(v0s[i], v0s[i-1]))
 
+    assert all(-1 <= v <= 1 for v0 in v0s for v in v0)
+
     v0 = np.concatenate(v0s)
     assert np.allclose(np.mean(v0), 0, atol=1e-2)
     assert np.allclose(np.std(v0), 1/np.sqrt(3), atol=1e-3)
