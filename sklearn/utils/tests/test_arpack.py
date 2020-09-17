c90eb16f53cf8184448317b0d2b65cd85c013e97
Gaurav Desai <gauravdesai111@gmail.com>
2020-09-18 02:18:30 +0530

    Optimized test case code as per review comments

--- sklearn/utils/tests/test_arpack.py
+++ sklearn/utils/tests/test_arpack.py
@@ -1,12 +1,14 @@
 import numpy as np
 
 from sklearn.utils._arpack import _init_arpack_v0
+from numpy.testing import assert_allclose
 
 
 def test_init_arpack_v0():
     v0s = [_init_arpack_v0(1000, i) for i in range(100)]
-    assert all(-1 <= v <= 1 for v0 in v0s for v in v0)
-
     v0 = np.concatenate(v0s)
-    assert np.allclose(np.mean(v0), 0, atol=1e-2)
-    assert np.allclose(np.std(v0), 1/np.sqrt(3), atol=1e-3)
+    assert np.all(v0 <= 1)
+    assert np.all(v0 >= -1)
+
+    assert_allclose(np.mean(v0), 0, atol=1e-2)
+    assert_allclose(np.std(v0), 1 / np.sqrt(3), atol=1e-3)
