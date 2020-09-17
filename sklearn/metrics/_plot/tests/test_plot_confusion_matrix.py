8f30e586e8dffe1332bbbc0d779b3e1ab786717e
kyouma <yakovassembler@gmail.com>
2020-09-18 00:01:07 +0300

    Fix for test_error_on_a_dataset_with_unseen_labels()

--- sklearn/metrics/_plot/tests/test_plot_confusion_matrix.py
+++ sklearn/metrics/_plot/tests/test_plot_confusion_matrix.py
@@ -317,7 +317,7 @@ def test_default_labels(pyplot, display_labels, expected_labels):
     assert_array_equal(y_ticks, expected_labels)
 
 
-def test_error_on_a_dataset_with_unseen_labels(fitted_clf, data):
+def test_error_on_a_dataset_with_unseen_labels(pyplot, fitted_clf, data):
     X, y = data
 
     # get values that are not present in 'fitted_clf.classes_'
