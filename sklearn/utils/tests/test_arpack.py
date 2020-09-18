cfb6f4015b577296831a3c084168ad690dee8353
Gaurav Desai <gauravdesai111@gmail.com>
2020-09-18 16:33:33 +0530

    Further optimisation on Test

--- sklearn/utils/tests/test_arpack.py
+++ sklearn/utils/tests/test_arpack.py
@@ -7,8 +7,7 @@ from numpy.testing import assert_allclose
 def test_init_arpack_v0():
     v0s = [_init_arpack_v0(1000, i) for i in range(100)]
     v0 = np.concatenate(v0s)
-    assert np.all(v0 <= 1)
-    assert np.all(v0 >= -1)
+    assert np.all(-1 <= v0.all() <= 1)
 
     assert_allclose(np.mean(v0), 0, atol=1e-2)
     assert_allclose(np.std(v0), 1 / np.sqrt(3), atol=1e-3)
