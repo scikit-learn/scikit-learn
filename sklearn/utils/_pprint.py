102433cba1939eea8602ae00633bd28e0a0bb0de
nathan <nathan@sancare.fr>
2020-10-02 17:38:43 +0200

    Add a non-regression test and a release note entry

--- sklearn/utils/_pprint.py
+++ sklearn/utils/_pprint.py
@@ -99,10 +99,11 @@ def _changed_params(estimator):
             return True
         if init_params[k] == inspect._empty:  # k has no default value
             return True
-        # avoid `repr` on nested estimators, it causes nonlinear complexity
+        # try to avoid calling repr on nested estimators
         if (isinstance(v, BaseEstimator) and
            v.__class__ != init_params[k].__class__):
             return True
+        # Use repr as a last resort. It may be expensive.
         if (repr(v) != repr(init_params[k]) and
            not (is_scalar_nan(init_params[k]) and is_scalar_nan(v))):
             return True
