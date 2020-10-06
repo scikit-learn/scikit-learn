e4ea219a376eb05cd12bd274a427f8ceb327edca
maikia <maja_ka@hotmail.com>
2020-10-06 12:08:04 +0200

    more tests

--- sklearn/preprocessing/tests/test_data.py
+++ sklearn/preprocessing/tests/test_data.py
@@ -648,36 +648,31 @@ def test_standard_scaler_partial_fit(add_sample_weight):
     X = X_2d
     n = X.shape[0]
 
+    sample_weight_batch = None
+    sample_weight = None
     for chunk_size in [1, 2, 50, n, n + 42]:
         # Test mean at the end of the process
         if add_sample_weight:
-            sample_weight_batch = np.ones(len(X))
-            sample_weight_incr = np.ones(chunk_size)
-        else:
-            sample_weight_batch = None
-            sample_weight_incr = None
+            sample_weight = np.ones(len(X))
 
         scaler_batch = StandardScaler(with_std=False).fit(
-            X, sample_weight=sample_weight_batch)
+            X, sample_weight=sample_weight)
 
         scaler_incr = StandardScaler(with_std=False)
         for batch in gen_batches(n_samples, chunk_size):
-            try:
-                scaler_incr = scaler_incr.partial_fit(
-                  X[batch], sample_weight=sample_weight_incr)
-
-            except:
-                import pdb; pdb.set_trace()
-
+            if add_sample_weight:
+                sample_weight_batch = np.ones(min(n_samples, chunk_size))
+            scaler_incr = scaler_incr.partial_fit(
+                X[batch], sample_weight=sample_weight_batch)
         assert_array_almost_equal(scaler_batch.mean_, scaler_incr.mean_)
         assert scaler_batch.var_ == scaler_incr.var_  # Nones
         assert scaler_batch.n_samples_seen_ == scaler_incr.n_samples_seen_
 
         # Test std after 1 step
         batch0 = slice(0, chunk_size)
-
         scaler_incr = StandardScaler().partial_fit(
-            X[batch0], sample_weight=sample_weight_incr)
+            X[batch0], sample_weight=sample_weight_batch)
+
         if chunk_size == 1:
             assert_array_almost_equal(np.zeros(n_features, dtype=np.float64),
                                       scaler_incr.var_)
@@ -690,12 +685,14 @@ def test_standard_scaler_partial_fit(add_sample_weight):
                                       scaler_incr.scale_)  # no constants
 
         # Test std until the end of partial fits, and
-        scaler_batch = StandardScaler().fit(X,
-                                            sample_weight=sample_weight_batch)
+        scaler_batch = StandardScaler().fit(X, sample_weight=sample_weight)
         scaler_incr = StandardScaler()  # Clean estimator
+        sample_weight_batch = None
         for i, batch in enumerate(gen_batches(n_samples, chunk_size)):
+            if add_sample_weight:
+                sample_weight_batch = np.ones(min(n_samples, chunk_size))
             scaler_incr = scaler_incr.partial_fit(
-                X[batch], sample_weight=sample_weight_incr)
+                X[batch], sample_weight=sample_weight_batch)
             assert_correct_incr(i, batch_start=batch.start,
                                 batch_stop=batch.stop, n=n,
                                 chunk_size=chunk_size,
