987fd8e35ce149bc0d6fd84282a20b2fcc683b3c
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2020-10-06 18:28:46 +0200

    TST add tests for plotting ConfusionMatrixDisplay

--- sklearn/metrics/_plot/tests/test_confusion_matrix_display.py
+++ sklearn/metrics/_plot/tests/test_confusion_matrix_display.py
@@ -2,6 +2,7 @@ from numpy.testing import (
     assert_allclose,
     assert_array_equal,
 )
+import numpy as np
 import pytest
 
 from sklearn.datasets import make_classification
@@ -132,3 +133,77 @@ def test_confusion_matrix_display_custom_labels(
     assert_array_equal(disp.display_labels, expected_display_labels)
     assert_array_equal(x_ticks, expected_display_labels_str)
     assert_array_equal(y_ticks, expected_display_labels_str)
+
+
+@pytest.mark.parametrize("normalize", ["true", "pred", "all", None])
+@pytest.mark.parametrize("include_values", [True, False])
+def test_confusion_matrix_display_plotting(
+    pyplot,
+    confusion_matrix_display_fxt,
+    data,
+    y_pred,
+    n_classes,
+    fitted_clf,
+    normalize,
+    include_values,
+):
+    X, y = data
+    constructor, *params = confusion_matrix_display_fxt
+    ax = pyplot.gca()
+    cmap = "plasma"
+
+    cm = confusion_matrix(y, y_pred)
+    disp = constructor(
+        *params,
+        normalize=normalize,
+        cmap=cmap,
+        ax=ax,
+        include_values=include_values
+    )
+
+    assert disp.ax_ == ax
+
+    if normalize == "true":
+        cm = cm / cm.sum(axis=1, keepdims=True)
+    elif normalize == "pred":
+        cm = cm / cm.sum(axis=0, keepdims=True)
+    elif normalize == "all":
+        cm = cm / cm.sum()
+
+    assert_allclose(disp.confusion_matrix, cm)
+    import matplotlib as mpl
+
+    assert isinstance(disp.im_, mpl.image.AxesImage)
+    assert disp.im_.get_cmap().name == cmap
+    assert isinstance(disp.ax_, pyplot.Axes)
+    assert isinstance(disp.figure_, pyplot.Figure)
+
+    assert disp.ax_.get_ylabel() == "True label"
+    assert disp.ax_.get_xlabel() == "Predicted label"
+
+    x_ticks = [tick.get_text() for tick in disp.ax_.get_xticklabels()]
+    y_ticks = [tick.get_text() for tick in disp.ax_.get_yticklabels()]
+
+    expected_display_labels = list(range(n_classes))
+
+    expected_display_labels_str = [
+        str(name) for name in expected_display_labels
+    ]
+
+    assert_array_equal(disp.display_labels, expected_display_labels)
+    assert_array_equal(x_ticks, expected_display_labels_str)
+    assert_array_equal(y_ticks, expected_display_labels_str)
+
+    image_data = disp.im_.get_array().data
+    assert_allclose(image_data, cm)
+
+    if include_values:
+        assert disp.text_.shape == (n_classes, n_classes)
+        fmt = ".2g"
+        expected_text = np.array([format(v, fmt) for v in cm.ravel(order="C")])
+        text_text = np.array(
+            [t.get_text() for t in disp.text_.ravel(order="C")]
+        )
+        assert_array_equal(expected_text, text_text)
+    else:
+        assert disp.text_ is None
