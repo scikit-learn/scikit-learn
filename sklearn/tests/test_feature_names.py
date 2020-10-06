be480b61eae2c9a16ef116a9dba300e2ee10d84a
Thomas J. Fan <thomasjpfan@gmail.com>
2020-10-06 16:55:52 -0400

    ENH Restricts column names to string

--- sklearn/tests/test_feature_names.py
+++ sklearn/tests/test_feature_names.py
@@ -12,16 +12,28 @@ def _construct_array(array_type, column_names):
     if array_type == "dataframe":
         pd = pytest.importorskip("pandas")
         return pd.DataFrame(X, columns=column_names)
-    elif array_type == "dataarray":
+    else:
         xr = pytest.importorskip("xarray")
         return xr.DataArray(X, dims=('index', 'columns'),
                             coords={'columns': column_names})
 
 
 @pytest.mark.parametrize("array_type", ["dataframe", "dataarray"])
-def test_pandas_get_feature_names(array_type):
+def test_get_feature_names(array_type):
     column_names = [f'col_{i}' for i in range(3)]
     X = _construct_array(array_type, column_names)
-    names = _get_feature_names(X)
 
+    names = _get_feature_names(X)
     assert_array_equal(names, column_names)
+
+
+@pytest.mark.parametrize("array_type", ["dataframe", "dataarray"])
+@pytest.mark.parametrize("column_names", [
+    np.array(["one", 2, "tree"], dtype=object),
+    np.array([1, 2, 3], dtype=object)
+])
+def test_get_feature_names_non_str(array_type, column_names):
+    X = _construct_array(array_type, column_names)
+    msg = "X contains non-string feature names"
+    with pytest.raises(ValueError, match=msg):
+        _get_feature_names(X)
--- sklearn/utils/_feature_names.py
+++ sklearn/utils/_feature_names.py
@@ -17,11 +17,26 @@ def _get_feature_names(X):
 
     Returns
     -------
-    names: array-like of shape (n_features,) or None
+    names: ndarray of shape (n_features,) or None
         Column names of `X`. Unrecognized array containers will return `None`.
+
+    Raises
+    ------
+    ValueError
+        If column names consist of a non-string data type.
     """
-    if hasattr(X, "columns"):  # pandas
-        return np.array(X.columns, dtype=object)
+    if hasattr(X, "columns"):
+        # pandas
+        out = np.array(X.columns, dtype=object)
     elif hasattr(X, "dims") and isinstance(X.dims, tuple) and len(X.dims) == 2:
         # xarray DataArray
-        return np.array(X.coords[X.dims[1]], dtype=object)
+        out = np.array(X.coords[X.dims[1]], dtype=object)
+    else:
+        # unrecognized array container
+        return None
+
+    # check out for strings
+    if any(not isinstance(item, str) for item in out):
+        raise ValueError("X contains non-string feature names")
+
+    return out
