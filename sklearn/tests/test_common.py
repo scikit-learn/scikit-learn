45a8498c651ff6c481feb33c950cbfd03b256f47
Olivier Grisel <olivier.grisel@ensta.org>
2020-10-07 16:40:15 +0200

    feature consistency for TruncatedSVD

--- sklearn/tests/test_common.py
+++ sklearn/tests/test_common.py
@@ -323,4 +323,5 @@ N_FEATURES_IN_AFTER_FIT_ESTIMATORS = [
 @pytest.mark.parametrize("estimator", N_FEATURES_IN_AFTER_FIT_ESTIMATORS,
                          ids=_get_check_estimator_ids)
 def test_check_n_features_in_after_fitting(estimator):
+    _set_checking_parameters(estimator)
     check_n_features_in_after_fitting(estimator.__class__.__name__, estimator)
