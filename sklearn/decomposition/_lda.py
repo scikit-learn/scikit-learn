d6dcc608ce291bbad0f5bfd062fadbede68886f3
Olivier Grisel <olivier.grisel@ensta.org>
2020-10-08 10:51:19 +0200

    Fixed typo

--- sklearn/decomposition/_lda.py
+++ sklearn/decomposition/_lda.py
@@ -515,8 +515,9 @@ class LatentDirichletAllocation(TransformerMixin, BaseEstimator):
         # string, which is not the same one raised by check_n_features. So we
         # don't check n_features_in_ here for now (it's done with adhoc code in
         # the estimator anyway).
-        X = self._check_non_neg_array(X, reset=first_time,
-                                      "LatentDirichletAllocation.partial_fit")
+        X = self._check_non_neg_array(
+            X, reset=first_time,
+            whom="LatentDirichletAllocation.partial_fit")
         n_samples, n_features = X.shape
         batch_size = self.batch_size
 
