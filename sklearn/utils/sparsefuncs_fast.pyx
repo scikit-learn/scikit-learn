719b467c6109d9d98416ce6cb25063b9920ef8f8
Maria Telenczuk <maja_ka@hotmail.com>
2020-10-13 15:55:57 +0200

    Update sklearn/utils/sparsefuncs_fast.pyx
    Co-authored-by: Alexandre Gramfort <alexandre.gramfort@m4x.org>

--- sklearn/utils/sparsefuncs_fast.pyx
+++ sklearn/utils/sparsefuncs_fast.pyx
@@ -226,7 +226,7 @@ def incr_mean_variance_axis0(X, last_mean, last_var, last_n, weights):
         X = X.astype(np.float64)
     X_dtype = X.dtype
     if weights is None:
-       weights = np.ones(X.shape[0]).astype(X_dtype)
+       weights = np.ones(X.shape[0], dtype=X_dtype)
     elif weights.dtype not in [np.float32, np.float64]:
         weights = weights.astype(np.float64)
     if last_n.dtype not in [np.float32, np.float64]:
