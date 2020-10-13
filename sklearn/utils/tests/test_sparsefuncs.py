d83fc257f4b87be479f55dcd47efd9ad99b0d265
maikia <maja_ka@hotmail.com>
2020-10-13 09:05:42 +0200

    added argument names when passing

--- sklearn/utils/tests/test_sparsefuncs.py
+++ sklearn/utils/tests/test_sparsefuncs.py
@@ -150,21 +150,23 @@ def test_incr_mean_variance_axis_weighted(Xw, X, sample_weight,
                                                      sample_weight=None)
 
     means_w0, vars_w0, n_incr_w0 = incr_mean_variance_axis(
-            Xw_sparse, axis, last_mean, last_var,
-            last_n, sample_weight=sample_weight)
+            X=Xw_sparse, axis=axis, last_mean=last_mean, last_var=last_var,
+            last_n=last_n, sample_weight=sample_weight)
 
     assert_array_almost_equal(means0, means_w0)
     assert_array_almost_equal(vars0, vars_w0)
     assert_array_almost_equal(n_incr0, n_incr_w0)
 
     # check second round for incremental
-    means1, vars1, n_incr1 = incr_mean_variance_axis(X_sparse, axis, means0,
-                                                     vars0, n_incr0,
+    means1, vars1, n_incr1 = incr_mean_variance_axis(X=X_sparse, axis=axis,
+                                                     last_mean=means0,
+                                                     last_var=vars0,
+                                                     last_n=n_incr0,
                                                      sample_weight=None)
 
     means_w1, vars_w1, n_incr_w1 = incr_mean_variance_axis(
-            Xw_sparse, axis, means_w0, vars_w0,
-            n_incr_w0, sample_weight=sample_weight)
+            X=Xw_sparse, axis=axis, last_mean=means_w0, last_var=vars_w0,
+            last_n=n_incr_w0, sample_weight=sample_weight)
 
     assert_array_almost_equal(means1, means_w1)
     assert_array_almost_equal(vars1, vars_w1)
