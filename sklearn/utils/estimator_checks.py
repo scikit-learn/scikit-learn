6b61d62c6a7f83b7d91a031bfc196775fcfcc6c9
Thomas J. Fan <thomasjpfan@gmail.com>
2020-10-14 10:46:01 -0400

    MNT Uses a less strict error message for n_features_in_ checks (#18585)

--- sklearn/utils/estimator_checks.py
+++ sklearn/utils/estimator_checks.py
@@ -3174,7 +3174,7 @@ def check_n_features_in_after_fitting(name, estimator_orig, strict_mode=True):
                      "predict_proba"]
     X_bad = X[:, [1]]
 
-    msg = (f"X has 1 features, but {name} is expecting {X.shape[1]} "
+    msg = (f"X has 1 features, but \\w+ is expecting {X.shape[1]} "
            "features as input")
     for method in check_methods:
         if not hasattr(estimator, method):
