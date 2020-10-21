33582e077593ddb58c11fcba878e68209a3e0eea
gbolmier <geoffrey.bolmier@gmail.com>
2020-10-21 09:18:29 +0200

    Fix test_average_precision_score_pos_label_errors

--- sklearn/metrics/tests/test_ranking.py
+++ sklearn/metrics/tests/test_ranking.py
@@ -900,8 +900,9 @@ def test_average_precision_score_pos_label_errors():
     y_true = np.array([[1, 0], [0, 1], [0, 1], [1, 0]])
     y_pred = np.array([[0.9, 0.1], [0.1, 0.9], [0.8, 0.2], [0.2, 0.8]])
     err_msg = (
-        "Parameter pos_label is fixed to 1 for multilabel-indicator y_true. "
-        "Do not set pos_label or set pos_label to 1."
+        "Parameter pos_label is fixed to 1 for multiclass or "
+        "multilabel-indicator y_true. Do not set pos_label or set pos_label "
+        "to 1."
     )
     with pytest.raises(ValueError, match=err_msg):
         average_precision_score(y_true, y_pred, pos_label=0)
