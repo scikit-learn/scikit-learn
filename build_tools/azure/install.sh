56c5867a464740d9f665cd9bf465e0437b37a40e
Olivier Grisel <olivier.grisel@ensta.org>
2020-10-22 15:44:07 +0200

    Remove explicit conda-forge prefix

--- build_tools/azure/install.sh
+++ build_tools/azure/install.sh
@@ -50,8 +50,7 @@ if [[ "$DISTRIB" == "conda" ]]; then
             # TODO: Remove !=1.1.0 when the following is fixed:
             # sklearn/svm/_libsvm.cpython-38-darwin.so,
             # 2): Symbol not found: _svm_check_parameter error
-            TO_INSTALL="$TO_INSTALL conda-forge::compilers>=1.0.4,!=1.1.0 \
-                        conda-forge::llvm-openmp"
+            TO_INSTALL="$TO_INSTALL compilers>=1.0.4,!=1.1.0 llvm-openmp"
         fi
     fi
 	make_conda $TO_INSTALL
