33d77b9bd4b79f2c00ec3c8039a76f097fbd98d4
davidechicco <you@example.com>
2020-10-29 15:02:51 -0400

    Added new scientific reference of MCC in the _classification.py page

--- sklearn/metrics/_classification.py
+++ sklearn/metrics/_classification.py
@@ -841,8 +841,8 @@ def matthews_corrcoef(y_true, y_pred, *, sample_weight=None):
         Error Measures in MultiClass Prediction
         <https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0041882>`_.
         
-    .. [5] `Chicco, Jurman, (2020). The advantages of the Matthews correlation 
-        coefficient (MCC) over F1 score and accuracy in binary 
+    .. [5] `Chicco, Jurman, (2020). The advantages of the Matthews correlation
+        coefficient (MCC) over F1 score and accuracy in binary
         classification evaluation<https://doi.org/10.1186/s12864-019-6413-7>`_.
 
     Examples
