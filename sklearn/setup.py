28052018fafbed8687e32c4a747eea25667f6c8c
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-11-03 17:39:29 +0100

    MNT Minor changes [cd build]

--- sklearn/setup.py
+++ sklearn/setup.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 from sklearn._build_utils import cythonize_extensions
-from sklearn._build_utils.vendor_vcomp140 import add_vcomp140
+from sklearn._build_utils.vendor_vcomp140 import embed_vcomp140
 
 
 def configuration(parent_package='', top_path=None):
@@ -88,7 +88,7 @@ def configuration(parent_package='', top_path=None):
             os.getenv("SKLEARN_VENDOR_VCOMP140_DLL")):
         # Embed vcomp140.dll before generating the Windows
         # wheel and after building the package from source
-        add_vcomp140()
+        embed_vcomp140()
 
     return config
 
