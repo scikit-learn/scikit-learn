06ebd30f5d6ea6bea0939c02b542b625b92ab5f4
Gaurav Desai <gauravdesai111@gmail.com>
2020-11-04 19:52:51 +0530

    Revert "Removed unreachable code"
    This reverts commit 69b6cd14

--- sklearn/cluster/_bicluster.py
+++ sklearn/cluster/_bicluster.py
@@ -143,6 +143,20 @@ class BaseSpectral(BiclusterMixin, BaseEstimator, metaclass=ABCMeta):
             random_state = check_random_state(self.random_state)
             v0 = _init_arpack_v0(min(array.shape), random_state)
             u, _, vt = svds(array, k=n_components, ncv=self.n_svd_vecs, v0=v0)
+            if np.any(np.isnan(vt)):
+                # some eigenvalues of A * A.T are negative, causing
+                # sqrt() to be np.nan. This causes some vectors in vt
+                # to be np.nan.
+                A = safe_sparse_dot(array.T, array)
+                # We have to renitialize v0 differently because the shape
+                # here can be different from the previous init
+                v0 = _init_arpack_v0(A.shape[0], random_state)
+                _, v = eigsh(A, ncv=self.n_svd_vecs, v0=v0)
+                vt = v.T
+            if np.any(np.isnan(u)):
+                A = safe_sparse_dot(array, array.T)
+                v0 = _init_arpack_v0(A.shape[0], random_state)
+                _, u = eigsh(A, ncv=self.n_svd_vecs, v0=v0)
 
         assert_all_finite(u)
         assert_all_finite(vt)
