c6cd2c0c5f88a4e098ed741731df29ccf73dcc12
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-11-05 10:10:55 +0100

    FIX Fix variable assigment statements [cd build]

--- build_tools/github/check_build_trigger.sh
+++ build_tools/github/check_build_trigger.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-COMMIT_MSG=$(git log --format=%B -n 1 $GITHUB_SHA)
+COMMIT_MSG=$(git log --no-merges -1 --oneline)
 
 # The commit marker "[cd build]" will trigger the build when required
 if [[ "$GITHUB_EVENT_NAME" == push ||
