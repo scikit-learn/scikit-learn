d264f7b4767368fdbd811fa687245dd22a7eb02c
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-11-05 10:08:43 +0100

    FIX Fix variable assigment statements [cd build]

--- build_tools/github/check_build_trigger.sh
+++ build_tools/github/check_build_trigger.sh
@@ -3,14 +3,11 @@
 set -e
 set -x
 
-COMMIT_MSG=$(git log --no-merges -1 --oneline)
-
-ON_PUSH = $("$GITHUB_EVENT_NAME" == "push")
-ON_SCHEDULE = $("$GITHUB_EVENT_NAME" == "schedule")
+COMMIT_MSG=$(git log --format=%B -n 1 $GITHUB_SHA)
 
 # The commit marker "[cd build]" will trigger the build when required
-ON_COMMIT_MARKER = $("$COMMIT_MSG" =~ "[cd build]")
-
-if [ "$ON_PUSH" | "$ON_SCHEDULE" | "$ON_COMMIT_MARKER" ]; then
+if [[ "$GITHUB_EVENT_NAME" == push ||
+      "$GITHUB_EVENT_NAME" == schedule ||
+      "$COMMIT_MSG" =~ \[cd\ build\] ]]; then
     echo "::set-output name=build::true"
 fi
