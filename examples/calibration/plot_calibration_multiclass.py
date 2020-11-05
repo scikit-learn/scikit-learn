d6dd4bb83d8463810e5d9e260779d8716a40c314
Lucy Liu <jliu176@gmail.com>
2020-11-05 21:37:13 +1300

    update doc

--- examples/calibration/plot_calibration_multiclass.py
+++ examples/calibration/plot_calibration_multiclass.py
@@ -3,88 +3,113 @@
 Probability Calibration for 3-class classification
 ==================================================
 
-This example illustrates how sigmoid calibration changes predicted
-probabilities for a 3-class classification problem. Illustrated is the standard
-2-simplex, where the three corners correspond to the three classes. Arrows
-point from the probability vectors predicted by an uncalibrated classifier to
-the probability vectors predicted by the same classifier after sigmoid
-calibration on a hold-out validation set. Colors indicate the true class of an
-instance (red: class 1, green: class 2, blue: class 3).
+This example illustrates how sigmoid :ref:`calibration <calibration>` changes
+predicted probabilities for a 3-class classification problem. Illustrated is
+the standard 2-simplex, where the three corners correspond to the three
+classes. Arrows point from the probability vectors predicted by an uncalibrated
+classifier to the probability vectors predicted by the same classifier after
+sigmoid calibration on a hold-out validation set. Colors indicate the true
+class of an instance (red: class 1, green: class 2, blue: class 3).
 
-The base classifier is a random forest classifier with 25 base estimators
-(trees). If this classifier is trained on all 1000 training datapoints, it is
-overly confident in its predictions and thus incurs a large log-loss.
+Author: Jan Hendrik Metzen <jhm@informatik.uni-bremen.de>
+License: BSD Style.
 
-Calibrating an identical classifier, which was trained on 600 datapoints, with
-method='sigmoid' on the remaining 400 datapoints reduces the confidence of the
-predictions, i.e., moves the probability vectors from the edges of the simplex
-towards the center. This calibration results in a lower log-loss. Note that an
-alternative would have been to increase the number of base estimators which
-would have resulted in a similar decrease in log-loss.
+Data
+----
 
+Below, we generate a classification dataset with 2000 samples, 2 features
+and 3 target classes. We then split the data as follows:
+
+* train: 600 samples (for training the classifier)
+* valid: 400 samples (for calibrating predicted probabilities)
+* test: 1000 samples
+
+Note that we also create `X_train_valid` and `y_train_valid`, which consists
+of both the train and valid subsets. This is used when we only want to train
+the classifier but not calibrate the predicted probabilities.
 """
-print(__doc__)
 
-# Author: Jan Hendrik Metzen <jhm@informatik.uni-bremen.de>
-# License: BSD Style.
-
-# %%
 import numpy as np
 from sklearn.datasets import make_blobs
 
 np.random.seed(0)
 
-# Generate data
-X, y = make_blobs(n_samples=2000, random_state=42, cluster_std=5.0)
+X, y = make_blobs(n_samples=2000, n_features=2, centers=3, random_state=42,
+                  cluster_std=5.0)
 X_train, y_train = X[:600], y[:600]
 X_valid, y_valid = X[600:1000], y[600:1000]
 X_train_valid, y_train_valid = X[:1000], y[:1000]
 X_test, y_test = X[1000:], y[1000:]
 
+
+# The base classifier is a random forest classifier with 25 base estimators
+# (trees). If this classifier is trained on all 1000 training datapoints, it is
+# overly confident in its predictions and thus incurs a large log-loss.
+
+# Calibrating an identical classifier, which was trained on 600 datapoints, with
+# method='sigmoid' on the remaining 400 datapoints reduces the confidence of the
+# predictions, i.e., moves the probability vectors from the edges of the simplex
+# towards the center. This calibration results in a lower log-loss. Note that an
+# alternative would have been to increase the number of base estimators which
+# would have resulted in a similar decrease in log-loss.
+
+
 # %%
-# Train uncalibrated random forest classifier on the concatenated train plus
-# validation data.
+# Fitting and calibration
+# -----------------------
+#
+# First, we will train a :class:`~sklearn.ensemble.RandomForestClassifier`
+# with 25 base estimators (trees) on the concatenated train and validation
+# data (1000 samples). This is the uncalibrated classifier.
+
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.calibration import CalibratedClassifierCV
-from sklearn.metrics import log_loss
 
 clf = RandomForestClassifier(n_estimators=25)
 clf.fit(X_train_valid, y_train_valid)
 
 # %%
-# Train a random forest classifier and calibrate on validation data in a
-# 2-stage process:
+# To train the calibrated classifier, with start with the same
+# :class:`~sklearn.ensemble.RandomForestClassifier` but train it using
+# the train data subset (600 samples) then calibrate, with `method='sigmoid'`
+# using the valid data subset (400 samples) in a 2-stage process.
+
+from sklearn.calibration import CalibratedClassifierCV
+
 clf = RandomForestClassifier(n_estimators=25)
 clf.fit(X_train, y_train)
-sig_clf = CalibratedClassifierCV(clf, method="sigmoid", cv="prefit")
-sig_clf.fit(X_valid, y_valid)
+cal_clf = CalibratedClassifierCV(clf, method="sigmoid", cv="prefit")
+cal_clf.fit(X_valid, y_valid)
 
 # %%
-# Compare the calibration of the predictions 1000 held-out test samples.
-clf_probs = clf.predict_proba(X_test)
-score = log_loss(y_test, clf_probs)
+# Compare probabilities
+# ---------------------
+#
+# The uncalibrated classifier is overly confident in its predictions and
+# incurs a large :ref:`log_loss`. The calibrated classifier produces
+# probabilities that are less confident, i.e., the probabilities are further
+# away from 0 and 1. This calibration results in a lower log loss.
+#
+# Below we plot a 2-simplex with arrows showing the change in predicted
+# probabilities of the test samples. Each vertex of the simplex represents
+# perfectly predicting one of the three classes (e.g., 1, 0, 0). The mid point
+# inside the simplex represents predicting the three classes with equal
+# probability (i.e., 1/3, 1/3, 1/3). Each arrow starts at the
+# uncalibrated probabilities and end with the arrow head at the calibrated
+# probability. The color of the arrow represents the true class of that test
+# sample.
 
-sig_clf_probs = sig_clf.predict_proba(X_test)
-sig_score = log_loss(y_test, sig_clf_probs)
-
-print("Log-loss of")
-print(f" * uncalibrated classifier: {score:.3f}")
-print(f" * calibrated classifier: {sig_score:.3f}")
-
-# %%
-# Plot changes in predicted probabilities on test samples using
-# arrows with the color of the true class.
 import matplotlib.pyplot as plt
 
 plt.figure(figsize=(10, 10))
 colors = ["r", "g", "b"]
+# Plot arrows
 for i in range(clf_probs.shape[0]):
     plt.arrow(clf_probs[i, 0], clf_probs[i, 1],
-              sig_clf_probs[i, 0] - clf_probs[i, 0],
-              sig_clf_probs[i, 1] - clf_probs[i, 1],
+              cal_clf_probs[i, 0] - clf_probs[i, 0],
+              cal_clf_probs[i, 1] - clf_probs[i, 1],
               color=colors[y_test[i]], head_width=1e-2)
 
-# Plot perfect predictions
+# Plot perfect predictions, at each vertex
 plt.plot([1.0], [0.0], 'ro', ms=20, label="Class 1")
 plt.plot([0.0], [1.0], 'go', ms=20, label="Class 2")
 plt.plot([0.0], [0.0], 'bo', ms=20, label="Class 3")
@@ -92,7 +117,7 @@ plt.plot([0.0], [0.0], 'bo', ms=20, label="Class 3")
 # Plot boundaries of unit simplex
 plt.plot([0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0], 'k', label="Simplex")
 
-# Annotate points on the simplex
+# Annotate points 6 points around the simplex, and mid point inside simplex
 plt.annotate(r'($\frac{1}{3}$, $\frac{1}{3}$, $\frac{1}{3}$)',
              xy=(1.0/3, 1.0/3), xytext=(1.0/3, .23), xycoords='data',
              arrowprops=dict(facecolor='black', shrink=0.05),
@@ -138,16 +163,44 @@ plt.ylim(-0.05, 1.05)
 _ = plt.legend(loc="best")
 
 # %%
-# Generate a grid over the 2-simplex to plot the learned calibration map:
+# Notice in the figure above that the arrows generally point away from the
+# edges of the simplex, where the probability of one class is 0. Further, a
+# large proportion of the arrows point towards the true
+# class, e.g., green arrows (samples where the true class is 'green')
+# generally point towards the green vertex. These result in fewer
+# over-confident 0 predicted probabilities and at the same time an increase
+# in the the probabilities for the correct class.
+#
+# We can show this objectively by comparing the log loss of the uncalibrated
+# and calibrated classifiers on the predictions of the 1000 test samples.
+
+from sklearn.metrics import log_loss
+
+clf_probs = clf.predict_proba(X_test)
+score = log_loss(y_test, clf_probs)
+
+cal_clf_probs = cal_clf.predict_proba(X_test)
+cal_score = log_loss(y_test, cal_clf_probs)
+
+print("Log-loss of")
+print(f" * uncalibrated classifier: {score:.3f}")
+print(f" * calibrated classifier: {sig_score:.3f}")
+
+# %%
+# Finally we generate a grid of possibile uncalibrated probabilities, over
+# the 2-simplex, compute the corresponding calibrated probabilities and
+# plot arrows for each. This illustrates the learned calibration map:
+
 plt.figure(figsize=(10, 10))
+# Generate grid of points
 p1d = np.linspace(0, 1, 20)
 p0, p1 = np.meshgrid(p1d, p1d)
 p2 = 1 - p0 - p1
 p = np.c_[p0.ravel(), p1.ravel(), p2.ravel()]
 p = p[p[:, 2] >= 0]
 
-# Use the class-wise calibrators to compute where those points are mapped.
-calibrated_classifier = sig_clf.calibrated_classifiers_[0]
+# Use the three class-wise calibrators to compute where each point is mapped
+calibrated_classifier = cal_clf.calibrated_classifiers_[0]
 prediction = np.vstack([calibrator.predict(this_p)
                         for calibrator, this_p in
                         zip(calibrated_classifier.calibrators, p.T)]).T
