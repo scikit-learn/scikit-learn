4316732ca284f4604d3d177d8c72df89800bea89
Chiara Marmo <chiara.marmo@u-psud.fr>
2020-11-06 15:49:40 +0100

    First commit for release highlights.

---
+++ examples/release_highlights/plot_release_highlights_0_24_0.py
@@ -0,0 +1,34 @@
+# flake8: noqa
+"""
+.. meta::
+   :keywords: highlights, v0.24
+
+========================================
+Release Highlights for scikit-learn 0.24
+========================================
+
+.. currentmodule:: sklearn
+
+We are pleased to announce the release of scikit-learn 0.24! Many bug fixes
+and improvements were added, as well as some new key features. We detail
+below a few of the major features of this release. **For an exhaustive list of
+all the changes**, please refer to the :ref:`release notes <changes_0_24>`.
+
+To install the latest version (with pip)::
+
+    pip install --upgrade scikit-learn
+
+or with conda::
+
+    conda install -c conda-forge scikit-learn
+"""
+
+##############################################################################
+# Searching parameter space with successive halving
+# -------------------------------------------------
+
+##############################################################################
+# Retrieving datasets from literature as pandas dataframes
+# --------------------------------------------------------
+
+
