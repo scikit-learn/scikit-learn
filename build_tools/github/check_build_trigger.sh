9f3e554326260ee9b5a8d6a185d8ae5a356c329b
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-11-06 15:25:07 +0100

    FIX Trigger CD build on master branch

--- build_tools/github/check_build_trigger.sh
+++ build_tools/github/check_build_trigger.sh
@@ -4,9 +4,10 @@ set -e
 set -x
 
 COMMIT_MSG=$(git log --no-merges -1 --oneline)
+BRANCH_NAME=$(echo "${GITHUB_BRANCH##*/}")
 
 # The commit marker "[cd build]" will trigger the build when required
-if [[ "$GITHUB_EVENT_NAME" == push ||
+if [[ "$GITHUB_EVENT_NAME" == push && "$BRANCH_NAME" != master ||
       "$GITHUB_EVENT_NAME" == schedule ||
       "$COMMIT_MSG" =~ \[cd\ build\] ]]; then
     echo "::set-output name=build::true"
