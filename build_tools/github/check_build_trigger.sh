2a28527b1c7bb3cdcd020d218aa7024d3b79ab85
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-11-06 15:37:24 +0100

    CLN Apply suggested changes

--- build_tools/github/check_build_trigger.sh
+++ build_tools/github/check_build_trigger.sh
@@ -4,11 +4,9 @@ set -e
 set -x
 
 COMMIT_MSG=$(git log --no-merges -1 --oneline)
-BRANCH_NAME=$(echo "${GITHUB_REF##*/}")
 
 # The commit marker "[cd build]" will trigger the build when required
-if [[ "$GITHUB_EVENT_NAME" == push && "$BRANCH_NAME" != master ||
-      "$GITHUB_EVENT_NAME" == schedule ||
+if [[ "$GITHUB_EVENT_NAME" == schedule ||
       "$COMMIT_MSG" =~ \[cd\ build\] ]]; then
     echo "::set-output name=build::true"
 fi
