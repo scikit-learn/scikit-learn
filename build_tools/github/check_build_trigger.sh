dcacc2256d6b0f4b65149911253fdb82340157ef
Thomas J. Fan <thomasjpfan@gmail.com>
2020-11-08 10:53:03 -0500

    DOC Adds comment

--- build_tools/github/check_build_trigger.sh
+++ build_tools/github/check_build_trigger.sh
@@ -7,8 +7,9 @@ set -x
 # By default pull requests use refs/pull/PULL_ID/merge as the source branch
 # which has a "Merge ID into ID" as a commit message. The latest commit
 # message is the second to last commit
-COMMIT_ID=$(echo $GITHUB_SHA | awk '{print $2}')
-COMMIT_MESSAGE=$(git log $COMMIT_ID -1 --pretty=%B)
+MERGE_MSG=$(git log $GITHUB_SHA -1 --pretty=%B)
+COMMIT_ID=$(echo $MERGE_MSG | awk '{print $2}')
+COMMIT_MSG=$(git log $COMMIT_ID -1 --pretty=%B)
 
 # The commit marker "[cd build]" will trigger the build when required
 if [[ "$GITHUB_EVENT_NAME" == schedule ||
