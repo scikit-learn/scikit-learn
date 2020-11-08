23c66b8e5343126b45515f58ac5e523ebbafc623
Thomas J. Fan <thomasjpfan@gmail.com>
2020-11-08 10:50:03 -0500

    CI Use fun bash scripting

--- build_tools/github/check_build_trigger.sh
+++ build_tools/github/check_build_trigger.sh
@@ -3,7 +3,8 @@
 set -e
 set -x
 
-COMMIT_MSG=$(git log $GITHUB_SHA -1 --pretty=%B)
+COMMIT_ID=$(echo $GITHUB_SHA | awk '{print $2}')
+COMMIT_MESSAGE=$(git log $COMMIT_ID -1 --pretty=%B)
 
 # The commit marker "[cd build]" will trigger the build when required
 if [[ "$GITHUB_EVENT_NAME" == schedule ||
