f77fb7265ff9425efb355890107d31012b2c8f33
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-11-09 11:12:23 +0100

    FIX Properly select the directory to upload the wheels [cd build] (#18788)

--- build_tools/github/upload_anaconda.sh
+++ build_tools/github/upload_anaconda.sh
@@ -14,5 +14,5 @@ fi
 pip install git+https://github.com/Anaconda-Server/anaconda-client
 
 # Force a replacement if the remote file already exists
-anaconda -t $ANACONDA_TOKEN upload --force -u $ANACONDA_ORG dist/*
+anaconda -t $ANACONDA_TOKEN upload --force -u $ANACONDA_ORG dist/artifact/*
 echo "Index: https://pypi.anaconda.org/$ANACONDA_ORG/simple"
