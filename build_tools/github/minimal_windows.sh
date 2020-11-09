2581b106ea46ec7f60359e2e9a2213faee6f6f75
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-11-10 00:50:21 +0100

    MNT Use Windows container to test the wheel

---
+++ build_tools/github/minimal_windows.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+set -x
+
+PYTHON_VERSION=${{ matrix.python }}
+
+# Remove the dot from the Python version for naming the wheel
+TRIM_PYTHON_VERSION=$(echo $PYTHON_VERSION | tr -d ".")
+
+if [[ $PYTHON_VERSION != 3.7 ]]; then
+    # Different identifier for Python 3.8 and newer
+    IDENTIFIER=cp"$TRIM_PYTHON_VERSION"-win_amd64.whl
+else
+    IDENTIFIER=cp"$TRIM_PYTHON_VERSION"m-win_amd64.whl
+
+WHEEL=scikit_learn-$SCIKIT_LEARN_VERSION-cp$TRIM_PYTHON_VERSION-$IDENTIFIER
+
+docker build --build-arg PYTHON_VERSION=$PYTHON_VERSION \
+             --build-arg WHEEL=$WHEEL \
+             -t scikit-learn/minimal-windows \
+             -f build_tools/github/Windows .
+
+docker run --rm scikit-learn/minimal-windows
