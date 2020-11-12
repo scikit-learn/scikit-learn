00d18ab0ba748dc509d51b2d77868d0198acb671
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-11-12 16:12:09 +0100

    MNT Set environment variables [cd build]

--- build_tools/travis/install_wheels.sh
+++ build_tools/travis/install_wheels.sh
@@ -4,6 +4,4 @@ set -e
 set -x
 
 python -m pip install cibuildwheel
-
-# Extend the waiting time to avoid that the tests stall
-travis_wait 30 python -m cibuildwheel --output-dir wheelhouse
+python -m cibuildwheel --output-dir wheelhouse
