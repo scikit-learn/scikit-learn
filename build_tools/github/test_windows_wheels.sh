24cc152d61917c93b4424eb09ef2b79cc5c5b505
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-11-13 18:18:45 +0100

    MNT Explictly specify the test command for Windows [cd build]

--- build_tools/github/test_windows_wheels.sh
+++ build_tools/github/test_windows_wheels.sh
@@ -7,9 +7,12 @@ PYTHON_VERSION=$1
 BITNESS=$2
 
 if [[ "$PYTHON_VERSION" == "36" || "$BITNESS" == "32" ]]; then
-    # Python 3.6 and 32-bit architectures are not
-    # yet supported by the official Docker images
-    $CIBW_TEST_COMMAND
+    # For Python 3.6 and 32-bit architecture use the regular
+    # test command (outside of the minimal Docker container)
+    pytest --pyargs sklearn
+
+    # Test that there are no links to system libraries
+    python -m threadpoolctl -i sklearn
 else
     docker run --rm scikit-learn/minimal_windows
 fi
