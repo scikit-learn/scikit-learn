b5fc3c7b763fe54d21b9844be0c1262b15b4361c
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-11-13 18:23:31 +0100

    MNT Properly specify the CMD command [cd build]

--- build_tools/github/test_windows_wheels.sh
+++ build_tools/github/test_windows_wheels.sh
@@ -10,8 +10,6 @@ if [[ "$PYTHON_VERSION" == "36" || "$BITNESS" == "32" ]]; then
     # For Python 3.6 and 32-bit architecture use the regular
     # test command (outside of the minimal Docker container)
     pytest --pyargs sklearn
-
-    # Test that there are no links to system libraries
     python -m threadpoolctl -i sklearn
 else
     docker run --rm scikit-learn/minimal_windows
