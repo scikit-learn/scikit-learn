049fb88dfc90d0d9765543fc70f91b50a6e38b4a
reshamas <reshama.stat@gmail.com>
2020-11-13 12:42:53 -0500

    pipeline: added SVM+PCA example, fix flake8 errors

--- examples/miscellaneous/plot_display.py
+++ examples/miscellaneous/plot_display.py
@@ -6,7 +6,7 @@ Pipeline: chaining a SVC and PCA
 =================================================================
 
 This example constructs a pipeline that does support vector
-classifier followed by dimensionality reduction.  
+classifier followed by dimensionality reduction.
 
 The default configuration for displaying a pipeline is 'text' where
 `set_config(display='text')`.  To visualize the diagram in Jupyter Notebook,
@@ -22,11 +22,13 @@ This section illustrates the use of a ``Pipeline`` with ``SVC`` and ``PCA``
 from sklearn.pipeline import Pipeline
 from sklearn.svm import SVC
 from sklearn.decomposition import PCA
+from sklearn import set_config
+
 estimators = [('reduce_dim', PCA()), ('clf', SVC())]
 pipe = Pipeline(estimators)
 pipe
 
-from sklearn import set_config
+# to visualize the diagram, change display='diagram'
 set_config(display='diagram')
 
-pipe 
+pipe
