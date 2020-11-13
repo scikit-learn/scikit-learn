b3de81b7cd0ae947a7ca9cf4f781acc2e65c906c
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-11-13 10:32:39 +0100

    MNT Minor changes [cd build]

--- build_tools/github/minimal_windows.sh
+++ build_tools/github/minimal_windows.sh
@@ -6,7 +6,7 @@ set -x
 PYTHON_VERSION=$1
 BITNESS=$2
 
-if [[ "$PYTHON_VERSION" == "36" || "$BITNESS" == "32" ]]; then
+if [[ "$PYTHON_VERSION" == "36" || "$BITNESS" == "32" ]]; then
     # Python 3.6 and 32-bit architectures are not
     # yet supported by the official Docker images
     exit 0
