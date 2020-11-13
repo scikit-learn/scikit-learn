cae16982671393e763ab58e7d1705a41377d1017
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-11-14 00:39:26 +0100

    MNT Properly specify the build arguments [cd build]

--- build_tools/github/build_minimal_windows_image.sh
+++ build_tools/github/build_minimal_windows_image.sh
@@ -16,24 +16,25 @@ if [[ "$PYTHON_VERSION" == "37" ]]; then
     IDENTIFIER=cp"$PYTHON_VERSION"m-win_amd64.whl
 else
     # Different identifier for Python 3.8 and newer
-    IDENTIFIER=cp"$PYTHON_VERSION"-win_amd64.whl
+    WHEEL_NAME=cp"$PYTHON_VERSION"-win_amd64.whl
 fi
 
-IDENTIFIER=scikit_learn-$SCIKIT_LEARN_VERSION-cp$PYTHON_VERSION-$IDENTIFIER
+WHEEL_NAME=scikit_learn-$SCIKIT_LEARN_VERSION-cp$PYTHON_VERSION-$WHEEL_NAME
 
 # Find the repaired wheel because there is no a way
 # to access to the path in a straightforward manner
-BASE_PATH="C:/Users/RUNNER~1/AppData/Local/Temp"
-WHEEL_PATH=$(find $BASE_PATH -type d -name "repaired_wheel")
+WHEEL_PATH="C:/Users/RUNNER~1/AppData/Local/Temp"
+WHEEL_PATH=$(find $WHEEL_PATH -type d -name "repaired_wheel")
 WHEEL_PATH=$(realpath $WHEEL_PATH)
-WHEEL="$WHEEL_PATH/$IDENTIFIER"
+WHEEL_PATH="$WHEEL_PATH/$WHEEL_NAME"
 
 # Dot the Python version for identyfing the base Docker image
 PYTHON_VERSION=$(echo ${PYTHON_VERSION:0:1}.${PYTHON_VERSION:1:2})
 
 # Build a minimal Windows Docker image for testing the wheels
 docker build --build-arg PYTHON_VERSION=$PYTHON_VERSION \
-             --build-arg WHEEL="$WHEEL" \
+             --build-arg WHEEL_PATH="$WHEEL_PATH" \
+             --build-arg WHEEL_NAME="$WHEEL_NAME" \
              --build-arg CIBW_TEST_REQUIRES="$CIBW_TEST_REQUIRES" \
              -f build_tools/github/Windows \
              -t scikit-learn/minimal-windows .
