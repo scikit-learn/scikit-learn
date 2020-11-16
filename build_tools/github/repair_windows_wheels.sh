53845e2c2edcddf5a9bcbe32e8a79b06ee220ee4
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-11-16 22:15:54 +0100

    FIX Fix path [cd build]

--- build_tools/github/repair_windows_wheels.sh
+++ build_tools/github/repair_windows_wheels.sh
@@ -9,6 +9,6 @@ DEST_DIR=$2
 # By default, the Windows wheels are not repaired.
 # In this case, we need to vendor VCRUNTIME140.dll
 wheel unpack "$WHEEL"
-python build_tools/github/vendor.py "$WHEEL_DIRNAME"
+python build_tools/github/vendor_vcruntime140.py "$WHEEL_DIRNAME"
 wheel pack "$WHEEL_DIRNAME" -d "$DEST_DIR"
 rm -rf "$WHEEL_DIRNAME"
