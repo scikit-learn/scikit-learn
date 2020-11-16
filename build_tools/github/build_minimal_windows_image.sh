cd8d57ba2ced683f9eefafab48048e761a895dea
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-11-16 17:17:36 +0100

    CLN Apply suggested changes [cd build]

--- build_tools/github/build_minimal_windows_image.sh
+++ build_tools/github/build_minimal_windows_image.sh
@@ -7,8 +7,9 @@ PYTHON_VERSION=$1
 BITNESS=$2
 
 if [[ "$PYTHON_VERSION" == "36" || "$BITNESS" == "32" ]]; then
-    # Python 3.6 and 32-bit architectures are not
-    # yet supported by the official Docker images
+    # Python 3.6 and 32-bit architectures are not supported
+    # by the official Docker images: Tests will just be run
+    # on the host (instead of the minimal Docker container).
     exit 0
 fi
 
