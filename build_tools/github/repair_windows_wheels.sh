ac3c390054efbdf41229b806f99e2c955b693706
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-11-16 23:41:31 +0100

    MNT Properly vendor the DLL for each architecture [cd build]

--- build_tools/github/repair_windows_wheels.sh
+++ build_tools/github/repair_windows_wheels.sh
@@ -5,10 +5,11 @@ set -x
 
 WHEEL=$1
 DEST_DIR=$2
+BITNESS=$3
 
 # By default, the Windows wheels are not repaired.
 # In this case, we need to vendor VCRUNTIME140.dll
 wheel unpack "$WHEEL"
-python build_tools/github/vendor.py "$WHEEL_DIRNAME"
+python build_tools/github/vendor.py "$WHEEL_DIRNAME" "$BITNESS"
 wheel pack "$WHEEL_DIRNAME" -d "$DEST_DIR"
 rm -rf "$WHEEL_DIRNAME"
--- build_tools/github/vendor.py
+++ build_tools/github/vendor.py
@@ -1,4 +1,8 @@
-"""Embed vcomp140.dll and VCRUNTIME140.dll after building the wheels."""
+"""Embed vcomp140.dll, VCRUNTIME140.dll and VCRUNTIME140_1.dll.
+
+Note that VCRUNTIME140_1.dll is only required (and available)
+for 64-bit architectures.
+"""
 
 
 import os
@@ -12,11 +16,13 @@ TARGET_FOLDER = op.join("sklearn", ".libs")
 DISTRIBUTOR_INIT = op.join("sklearn", "_distributor_init.py")
 VCOMP140_SRC_PATH = "C:\\Windows\\System32\\vcomp140.dll"
 VCRUNTIME140_SRC_PATH = "C:\\Windows\\System32\\VCRUNTIME140.dll"
+VCRUNTIME140_1_SRC_PATH = "C:\\Windows\\System32\\VCRUNTIME140_1.dll"
 
 
-def make_distributor_init(distributor_init, vcomp140, vcruntime140):
-    """Create a _distributor_init.py file for vcomp140.dll and
-    VCRUNTIME140.dll.
+def make_distributor_init_32_bits(distributor_init,
+                                  vcomp140_dll_filename,
+                                  vcruntime140_dll_filename):
+    """Create a _distributor_init.py file for 32-bit architectures.
 
     This file is imported first when importing the sklearn package
     so as to pre-load the vendored vcomp140.dll and VCRUNTIME140.dll.
@@ -39,27 +45,73 @@ def make_distributor_init(distributor_init, vcomp140, vcruntime140):
 
 
             if os.name == "nt":
-                # Load vcomp140.dll and VCRUNTIME140.dll in "sklearn/.libs"
-                vcomp140 = op.join(op.dirname(__file__), ".libs", "{0}")
-                vcruntime140 = op.join(op.dirname(__file__), ".libs", "{1}")
-                WinDLL(op.abspath(vcomp140))
-                WinDLL(op.abspath(vcruntime140))
-            """.format(vcomp140, vcruntime140)))
+                # Load vcomp140.dll and VCRUNTIME140.dll
+                libs_path = op.join(op.dirname(__file__), ".libs")
+                vcomp140_dll_filename = op.join(libs_path, "{0}")
+                vcruntime140_dll_filename = op.join(libs_path, "{1}")
+                WinDLL(op.abspath(vcomp140_dll_filename))
+                WinDLL(op.abspath(vcruntime140_dll_filename))
+            """.format(vcomp140_dll_filename, vcruntime140_dll_filename)))
 
 
-def main(wheel_dirname):
-    """Embed vcomp140.dll and VCRUNTIME140.dll after building the wheels."""
+def make_distributor_init_64_bits(distributor_init,
+                                  vcomp140_dll_filename,
+                                  vcruntime140_dll_filename,
+                                  vcruntime140_1_dll_filename):
+    """Create a _distributor_init.py file for 64-bit architectures.
+
+    This file is imported first when importing the sklearn package
+    so as to pre-load the vendored vcomp140.dll, VCRUNTIME140.dll
+    and VCRUNTIME140_1.dll.
+    """
+    with open(distributor_init, "wt") as f:
+        f.write(textwrap.dedent("""
+            '''Helper to preload vcomp140.dll, VCRUNTIME140.dll and
+            VCRUNTIME140_1.dll to prevent "not found" errors.
+
+            Once vcomp140.dll, VCRUNTIME140.dll and VCRUNTIME140_1.dll are
+            preloaded, the namespace is made available to any subsequent
+            vcomp140.dll, VCRUNTIME140.dll and VCRUNTIME140_1.dll. This is
+            created as part of the scripts that build the wheel.
+            '''
+
+
+            import os
+            import os.path as op
+            from ctypes import WinDLL
+
+
+            if os.name == "nt":
+                # Load vcomp140.dll, VCRUNTIME140.dll and VCRUNTIME140_1.dll
+                libs_path = op.join(op.dirname(__file__), ".libs")
+                vcomp140_dll_filename = op.join(libs_path, "{0}")
+                vcruntime140_dll_filename = op.join(libs_path, "{1}")
+                vcruntime140_1_dll_filename = op.join(libs_path, "{2}")
+                WinDLL(op.abspath(vcomp140_dll_filename))
+                WinDLL(op.abspath(vcruntime140_dll_filename))
+                WinDLL(op.abspath(vcruntime140_1_dll_filename))
+            """.format(vcomp140_dll_filename,
+                       vcruntime140_dll_filename,
+                       vcruntime140_1_dll_filename)))
+
+
+def main(wheel_dirname, bitness):
+    """Embed vcomp140.dll, VCRUNTIME140.dll and VCRUNTIME140_1.dll."""
     if not op.exists(VCOMP140_SRC_PATH):
         raise ValueError(f"Could not find {VCOMP140_SRC_PATH}.")
 
     if not op.exists(VCRUNTIME140_SRC_PATH):
         raise ValueError(f"Could not find {VCRUNTIME140_SRC_PATH}.")
 
+    if not op.exists(VCRUNTIME140_1_SRC_PATH) and bitness == "64":
+        raise ValueError(f"Could not find {VCRUNTIME140_1_SRC_PATH}.")
+
     if not op.isdir(wheel_dirname):
         raise RuntimeError(f"Could not find {wheel_dirname} file.")
 
-    vcomp140_dll = op.basename(VCOMP140_SRC_PATH)
-    vcruntime140_dll = op.basename(VCRUNTIME140_SRC_PATH)
+    vcomp140_dll_filename = op.basename(VCOMP140_SRC_PATH)
+    vcruntime140_dll_filename = op.basename(VCRUNTIME140_SRC_PATH)
+    vcruntime140_1_dll_filename = op.basename(VCRUNTIME140_1_SRC_PATH)
 
     target_folder = op.join(wheel_dirname, TARGET_FOLDER)
     distributor_init = op.join(wheel_dirname, DISTRIBUTOR_INIT)
@@ -74,11 +126,23 @@ def main(wheel_dirname):
     print(f"Copying {VCRUNTIME140_SRC_PATH} to {target_folder}.")
     shutil.copy2(VCRUNTIME140_SRC_PATH, target_folder)
 
+    if bitness == "64":
+        print(f"Copying {VCRUNTIME140_1_SRC_PATH} to {target_folder}.")
+        shutil.copy2(VCRUNTIME140_1_SRC_PATH, target_folder)
+
     # Generate the _distributor_init file in the source tree
     print("Generating the '_distributor_init.py' file.")
-    make_distributor_init(distributor_init, vcomp140_dll, vcruntime140_dll)
+    if bitness == "32":
+        make_distributor_init_32_bits(distributor_init,
+                                      vcomp140_dll_filename,
+                                      vcruntime140_dll_filename)
+    else:
+        make_distributor_init_64_bits(distributor_init,
+                                      vcomp140_dll_filename,
+                                      vcruntime140_dll_filename,
+                                      vcruntime140_1_dll_filename)
 
 
 if __name__ == "__main__":
-    _, wheel_file = sys.argv
-    main(wheel_file)
+    _, wheel_file, bitness = sys.argv
+    main(wheel_file, bitness)
