75dbe4a897d54c8de3e3cf34c89792a5cd68a6d2
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-11-17 11:56:45 +0100

    MNT Set the environment variable in docker exec [cd build]

--- build_tools/github/test_windows_wheels.sh
+++ build_tools/github/test_windows_wheels.sh
@@ -12,17 +12,13 @@ if [[ "$PYTHON_VERSION" == "36" || "$BITNESS" == "32" ]]; then
     pytest --pyargs sklearn
     python -m threadpoolctl -i sklearn
 else
-    docker run -e SKLEARN_SKIP_NETWORK_TESTS=1 \
-               -e OMP_NUM_THREADS=2 \
-               -e OPENBLAS_NUM_THREADS=2 \
-               --name minimal_windows \
+    docker run --name minimal_windows \
                -d -ti --rm scikit-learn/minimal-windows
 
-    docker exec minimal_windows powershell -NoProfile -Command "$env:SKLEARN_SKIP_NETWORK_TESTS"
-    docker exec minimal_windows powershell -NoProfile -Command "$env:OMP_NUM_THREADS"
-    docker exec minimal_windows powershell -NoProfile -Command "$env:OPENBLAS_NUM_THREADS"
-
-    docker exec minimal_windows powershell -NoProfile -Command "pytest --pyargs sklearn"
+    docker exec -e SKLEARN_SKIP_NETWORK_TESTS=1 \
+                -e OMP_NUM_THREADS=2 \
+                -e OPENBLAS_NUM_THREADS=2 \
+                minimal_windows powershell -NoProfile -Command "pytest --pyargs sklearn"
 
     # Test that there are no links to system libraries
     docker exec minimal_windows powershell -NoProfile -Command "python -m threadpoolctl -i sklearn"
