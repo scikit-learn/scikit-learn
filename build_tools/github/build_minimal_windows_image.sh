c2cb3e06a29ba9e884804c8215a03b1ee67ccce1
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-11-18 16:24:47 +0100

    CLN Apply suggested changes [cd build]

--- build_tools/github/build_minimal_windows_image.sh
+++ build_tools/github/build_minimal_windows_image.sh
@@ -13,31 +13,9 @@ if [[ "$PYTHON_VERSION" == "36" || "$BITNESS" == "32" ]]; then
     exit 0
 fi
 
-if [[ "$PYTHON_VERSION" == "37" ]]; then
-    WHEEL_NAME=cp"$PYTHON_VERSION"m-win_amd64.whl
-else
-    # Different name for Python 3.8 and newer
-    WHEEL_NAME=cp"$PYTHON_VERSION"-win_amd64.whl
-fi
-
-WHEEL_NAME=scikit_learn-$SCIKIT_LEARN_VERSION-cp$PYTHON_VERSION-$WHEEL_NAME
-
-# Find the repaired wheel because there is no a way
-# to access to the path in a straightforward manner
-
-# The repaired wheel is under a temporary directory that may
-# remove folders concurrently with respect to "find" command
 TEMP_FOLDER="$HOME/AppData/Local/Temp"
-WHEEL_PATH=$(find $TEMP_FOLDER -type d -name "repaired_wheel" 2>/dev/null); \
-           echo "Ignoring concurrently deleted temporary folders."
-
-if [[ -z "$WHEEL_PATH" ]]; then
-    echo "Failed to locate repaired_wheel under $TEMP_FOLDER."
-    exit 1
-fi
-
-WHEEL_PATH=$(realpath $WHEEL_PATH)
-WHEEL_PATH="$WHEEL_PATH/$WHEEL_NAME"
+WHEEL_NAME=$(ls $TEMP_FOLDER/*/repaired_wheel)
+WHEEL_PATH=$(realpath $WHEEL_NAME)
 
 cp $WHEEL_PATH $WHEEL_NAME
 
