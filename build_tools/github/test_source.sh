a4c9cf8ba23917b0fb212c8ec531dcd8d5d11607
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-11-18 17:15:49 +0100

    MNT Move up two levels [cd build]

--- build_tools/github/test_source.sh
+++ build_tools/github/test_source.sh
@@ -3,14 +3,16 @@
 set -e
 set -x
 
+cd ../../
+
 python -m venv test_env
 source test_env/bin/activate
 
-python -m pip install dist/*.tar.gz
+python -m pip install scikit-learn/scikit-learn/dist/*.tar.gz
 python -m pip install pytest pandas
 
 # Run the tests on the installed source distribution
-mkdir ../../tmp_for_test
-cd ../../tmp_for_test
+mkdir tmp_for_test
+cd tmp_for_test
 
 pytest --pyargs sklearn
