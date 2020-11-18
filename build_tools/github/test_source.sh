8d1a247c0d14b277502f21965cf8924fa86331d2
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-11-18 12:20:13 +0100

    MNT Minor changes [cd build]

--- build_tools/github/test_source.sh
+++ build_tools/github/test_source.sh
@@ -6,11 +6,11 @@ set -x
 python -m venv test_env
 source test_env/bin/activate
 
+python -m pip install dist/*.tar.gz
+python -m pip install pytest pandas
+
 # Run the tests on the installed source distribution
 mkdir tmp_for_test
 cd tmp_for_test
-
-python -m pip install pytest pandas
-python -m pip install dist/*.tar.gz
 
 pytest --pyargs sklearn
