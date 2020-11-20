f76dae7d3d9a599801d34395ba34a70533d672ed
Julien Jerphanion (@jjerphan) <git@jjerphan.xyz>
2020-11-20 19:58:26 +0000

    Use_check_finite=False for the call to solve
    X and Y contain finite values by assumption.
    Hence so do C, G and trace.
    v contain finite values by assumption.
    
    Thus we can remove the check on the call to `solve`.

--- sklearn/manifold/_locally_linear.py
+++ sklearn/manifold/_locally_linear.py
@@ -67,7 +67,7 @@ def barycenter_weights(X, Y, indices, reg=1e-3):
         else:
             R = reg
         G.flat[::n_neighbors + 1] += R
-        w = solve(G, v, sym_pos=True)
+        w = solve(G, v, sym_pos=True, check_finite=False)
         B[i, :] = w / np.sum(w)
     return B
 
