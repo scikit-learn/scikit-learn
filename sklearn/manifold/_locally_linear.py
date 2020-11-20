751376a0e4b1315be15da4353044d67cfb8b79ee
Julien Jerphanion (@jjerphan) <git@jjerphan.xyz>
2020-11-20 19:58:26 +0000

    Use_check_finite=False for the call to eigh (hessian)
    This is possible as Ci is constructed from Gi
    which is constructed from X which has been validated
    in `NeighborsBase._fit`.

--- sklearn/manifold/_locally_linear.py
+++ sklearn/manifold/_locally_linear.py
@@ -358,7 +358,7 @@ def locally_linear_embedding(
                 U = svd(Gi, full_matrices=0, check_finite=False)[0]
             else:
                 Ci = np.dot(Gi, Gi.T)
-                U = eigh(Ci)[1][:, ::-1]
+                U = eigh(Ci, check_finite=False)[1][:, ::-1]
 
             Yi[:, 1:1 + n_components] = U[:, :n_components]
 
