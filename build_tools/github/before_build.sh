341022d9cd2499f7e719c92f0a65cf19c8c719b5
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-11-20 12:29:01 +0100

    MNT Install ATLAS and LAPLACK [cd build]

--- build_tools/github/before_build.sh
+++ build_tools/github/before_build.sh
@@ -7,5 +7,5 @@ PYTHON_VERSION=$1
 
 if [[ $PYTHON_VERSION == pp36 ]]; then
     # Install the system build tools (ATLAS and LAPLACK) for PyPy implementation
-    apt-get -yq install libatlas-base-dev liblapack-dev gfortran libopenblas-dev
+    sudo apt-get -yq install libatlas-base-dev liblapack-dev gfortran libopenblas-dev
 fi
