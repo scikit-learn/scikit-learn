b511437e98709aad090017660322649b62c98ab1
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-11-20 12:38:42 +0100

    MNT Install ATLAS and LAPLACK using yum [cd build]

--- build_tools/github/before_build.sh
+++ build_tools/github/before_build.sh
@@ -7,5 +7,5 @@ PYTHON_VERSION=$1
 
 if [[ $PYTHON_VERSION == pp36 ]]; then
     # Install the system build tools (ATLAS and LAPLACK) for PyPy implementation
-    yum -yq install libatlas-base-dev liblapack-dev gfortran libopenblas-dev
+    yum -y -q install atlas-devel lapack-devel gcc-gfortran openblas-devel
 fi
