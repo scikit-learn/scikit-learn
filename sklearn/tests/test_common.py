61cdfcab7ec482337e7e6ba2cfb68312c309b3ca
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2020-11-26 14:59:39 +0100

    mark as xfail

--- sklearn/tests/test_common.py
+++ sklearn/tests/test_common.py
@@ -483,7 +483,7 @@ def _generate_minimal_compatible_instances():
 
 
 # FIXME: hopefully in 0.25
-@pytest.mark.skip(
+@pytest.mark.xfail(
     reason=("This test is currently failing because checks are granular "
             "enough. Once checks are split with some kind of only API tests, "
             "this test should enabled.")
