cf720b5359bcd2a4c6ff4035428a959fa62c79a1
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-11-27 16:45:06 +0100

    MNT Remove installation of ATLAS and LAPLACK [cd build]

--- build_tools/github/before_build.sh
+++ build_tools/github/before_build.sh
@@ -7,5 +7,5 @@ PYTHON_VERSION=$1
 
 if [[ $PYTHON_VERSION == pp36 ]]; then
     # Install the system build tools (ATLAS and LAPLACK) for PyPy implementation
-    yum -y -q install atlas-devel lapack-devel gcc-gfortran openblas-devel
+    # yum -y -q install atlas-devel lapack-devel gcc-gfortran openblas-devel
 fi
