d27d8a4d204671e34ad91f50deb707dbbd814415
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2020-11-27 17:11:16 +0100

    MNT Only install OpenBLAS [cd build]

--- build_tools/github/before_build.sh
+++ build_tools/github/before_build.sh
@@ -6,6 +6,6 @@ set -x
 PYTHON_VERSION=$1
 
 if [[ $PYTHON_VERSION == pp36 ]]; then
-    # Install the system build tools (ATLAS and LAPLACK) for PyPy implementation
-    yum -y -q install atlas-devel lapack-devel gcc-gfortran openblas-devel
+    # Install the system build tools
+    yum -y -q install openblas-devel
 fi
