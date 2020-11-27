6b4b5c7f6c5c891d906cc5dbc3e884e43020f764
David Poznik <dpoznik@23andme.com>
2020-11-26 19:34:25 -0500

    Handle degenerate single-class case correctly in naive_bayes.py

--- sklearn/naive_bayes.py
+++ sklearn/naive_bayes.py
@@ -560,8 +560,16 @@ class _BaseDiscreteNB(_BaseNB):
             raise ValueError(msg % (n_features, self.n_features_))
 
         Y = label_binarize(y, classes=self.classes_)
+        # Y has shape (n_samples, 1) when the number of classes is two or one.
+        # For binary problems, we add a second indicator column.
+        # In the degenerate single-class case, we complement the single
+        # indicator column from all zeroes to all ones.
         if Y.shape[1] == 1:
-            Y = np.concatenate((1 - Y, Y), axis=1)
+            first_class_indicator = 1 - Y
+            if len(self.classes_) == 2:
+                Y = np.concatenate((first_class_indicator, Y), axis=1)
+            else:
+                Y = first_class_indicator
 
         if X.shape[0] != Y.shape[0]:
             msg = "X.shape[0]=%d and y.shape[0]=%d are incompatible."
@@ -616,8 +624,16 @@ class _BaseDiscreteNB(_BaseNB):
         labelbin = LabelBinarizer()
         Y = labelbin.fit_transform(y)
         self.classes_ = labelbin.classes_
+        # Y has shape (n_samples, 1) when the number of classes is two or one.
+        # For binary problems, we add a second indicator column.
+        # In the degenerate single-class case, we complement the single
+        # indicator column from all zeroes to all ones.
         if Y.shape[1] == 1:
-            Y = np.concatenate((1 - Y, Y), axis=1)
+            first_class_indicator = 1 - Y
+            if len(self.classes_) == 2:
+                Y = np.concatenate((first_class_indicator, Y), axis=1)
+            else:
+                Y = first_class_indicator
 
         # LabelBinarizer().fit_transform() returns arrays with dtype=np.int64.
         # We convert it to np.float64 to support sample_weight consistently;
