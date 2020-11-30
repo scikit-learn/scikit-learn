eb9c41b4a6f4d62fdf8ca8e46ec81dc9ae295e6b
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2020-11-30 20:34:26 +0100

    PEP8

--- sklearn/model_selection/tests/test_search.py
+++ sklearn/model_selection/tests/test_search.py
@@ -69,7 +69,6 @@ from sklearn.metrics import make_scorer
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import r2_score
-from sklearn.metrics import accuracy_score
 from sklearn.metrics.pairwise import euclidean_distances
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
