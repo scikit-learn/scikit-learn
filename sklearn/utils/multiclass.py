0967470f741f260ee5fdaa2081fca578486c1a94
ZhihuiChen0903 <57449793+ZhihuiChen0903@users.noreply.github.com>
2020-11-30 14:09:02 +0800

    Update multiclass.py

--- sklearn/utils/multiclass.py
+++ sklearn/utils/multiclass.py
@@ -180,7 +180,8 @@ def check_classification_targets(y):
     y_type = type_of_target(y)
     if y_type not in ['binary', 'multiclass', 'multiclass-multioutput',
                       'multilabel-indicator', 'multilabel-sequences']:
-        raise ValueError("Unknown label type: %r" % y_type)
+        raise ValueError("Unknown label type: %r.Expected 'binary', 'multiclass', 'multiclass-multioutput',
+                         'multilabel-indicator', 'multilabel-sequences'." % y_type)
 
 
 def type_of_target(y):
