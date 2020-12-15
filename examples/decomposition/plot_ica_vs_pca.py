28d26ea7043f632d3f4673c6b46ecd674c28585a
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2020-12-15 10:49:10 +0100

    revert issue

--- examples/decomposition/plot_ica_vs_pca.py
+++ examples/decomposition/plot_ica_vs_pca.py
@@ -101,5 +101,5 @@ plt.subplot(2, 2, 4)
 plot_samples(S_ica_ / np.std(S_ica_))
 plt.title('ICA recovered signals')
 
-plt.subplots_adjust(0.09, 0.04, 0.94, 0.94, 1.1, 0.36)
+plt.subplots_adjust(0.09, 0.04, 0.94, 0.94, 0.26, 0.36)
 plt.show()
