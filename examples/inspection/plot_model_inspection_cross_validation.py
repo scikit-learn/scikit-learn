8794d838ffa3745933432658ef67501dcfa4daa9
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2020-12-19 18:32:02 +0100

    style

--- examples/inspection/plot_model_inspection_cross_validation.py
+++ examples/inspection/plot_model_inspection_cross_validation.py
@@ -30,8 +30,7 @@ X, y = fetch_california_housing(as_frame=True, return_X_y=True)
 y -= y.mean()
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.1, random_state=0
-)
+    X, y, test_size=0.1, random_state=0)
 
 # %%
 # Predictive model
