edc5510c3cbb0ceeff85ed4d9431c4416b02851f
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2021-01-05 17:51:30 +0100

    [TST] Disable parallel testing for the nightly build [scipy-dev]

--- build_tools/azure/test_script.sh
+++ build_tools/azure/test_script.sh
@@ -38,6 +38,10 @@ if [[ -n "$CHECK_WARNINGS" ]]; then
     TEST_CMD="$TEST_CMD -Werror::DeprecationWarning -Werror::FutureWarning -Wignore:tostring:DeprecationWarning"
 fi
 
+if [[ "$PYTEST_XDIST_VERSION" != "none" ]]; then
+    TEST_CMD="$TEST_CMD -n2"
+fi
+
 mkdir -p $TEST_DIR
 cp setup.cfg $TEST_DIR
 cd $TEST_DIR
