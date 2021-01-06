861d53827072a8e4ba7ea4d69f280c6316086063
ly648499246 <648499246@qq.com>
2021-01-06 21:34:29 +0800

    Update sklearn/feature_extraction/tests/test_text.py
    Co-authored-by: Jérémie du Boisberranger <34657725+jeremiedbb@users.noreply.github.com>

--- sklearn/feature_extraction/tests/test_text.py
+++ sklearn/feature_extraction/tests/test_text.py
@@ -1387,7 +1387,7 @@ def test_tie_breaking_sample_order_invariance():
     assert vocab1 == vocab2
 
 # add test for pr 19035
-def test_nonnegative_hashing_vectorizer_result_indeices():
+def test_nonnegative_hashing_vectorizer_result_indices():
     hashing = HashingVectorizer(n_features=1000000, ngram_range=(2, 3))
     indices = hashing.transform(['22pcs efuture']).indices
     assert indices[0] >= 0
