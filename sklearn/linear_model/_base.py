f969696cc625b527f4bcf67542d3e143c4caf597
Maria Telenczuk <maja_ka@hotmail.com>
2021-01-14 14:19:09 +0100

    Update sklearn/linear_model/_base.py
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- sklearn/linear_model/_base.py
+++ sklearn/linear_model/_base.py
@@ -762,8 +762,8 @@ def _pre_fit(X, y, Xy, precompute, normalize, fit_intercept, copy,
     # FIXME: 'normalize' to be removed in 1.2
     if hasattr(precompute, '__array__'):
         if (fit_intercept and not np.allclose(X_offset, np.zeros(n_features))
-                or normalize and not np.allclose(X_scale, np.ones(n_features)
-                                                 )):
+                or normalize and not np.allclose(X_scale, np.ones(n_features))
+            ):
             warnings.warn("Gram matrix was provided but X was centered"
                           " to fit intercept, "
                           "or X was normalized : recomputing Gram matrix.",
