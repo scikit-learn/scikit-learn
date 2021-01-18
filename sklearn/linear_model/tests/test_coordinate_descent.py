5efe0de66af7d934c0cf4ef7db6660137118f909
Maria Telenczuk <maja_ka@hotmail.com>
2021-01-18 14:06:04 +0100

    Update sklearn/linear_model/tests/test_coordinate_descent.py
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- sklearn/linear_model/tests/test_coordinate_descent.py
+++ sklearn/linear_model/tests/test_coordinate_descent.py
@@ -411,7 +411,7 @@ def test_linear_model_sample_weights_normalize_in_pipeline(
     assert_allclose(
         reg_with_normalize.coef_ * reg_with_scaler[0].scale_,
         reg_with_scaler[1].coef_
-        )
+    )
     assert_allclose(y_pred_norm, y_pred_pip)
 
 
