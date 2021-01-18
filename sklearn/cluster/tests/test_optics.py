a17b40eaf86562f53660837b34b97c4990dca790
Surya Prakash <46415184+jdsurya@users.noreply.github.com>
2021-01-18 11:52:37 +0530

    Update sklearn/cluster/tests/test_optics.py
    Co-authored-by: Nicolas Hug <contact@nicolas-hug.com>

--- sklearn/cluster/tests/test_optics.py
+++ sklearn/cluster/tests/test_optics.py
@@ -211,6 +211,8 @@ def test_bad_reachability():
 
 
 def test_nowarn_if_metric_bool_data_bool():
+    # make sure no warning is raised if metric and data are both boolean
+    # non-regression test for https://github.com/scikit-learn/scikit-learn/issues/18996
     pairwise_metric = 'rogerstanimoto'
     X = np.random.randint(2, size=(5, 2), dtype=np.bool)
 
