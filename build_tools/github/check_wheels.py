c59e62e525fe01499b44802e71fa7ad335397049
Thomas J. Fan <thomasjpfan@gmail.com>
2021-01-21 15:08:54 -0500

    ENH Adds manylinux1 wheels [cd build] [ci skip]

--- build_tools/github/check_wheels.py
+++ build_tools/github/check_wheels.py
@@ -8,10 +8,11 @@ gh_wheel_path = Path.cwd() / ".github" / "workflows" / "wheels.yml"
 with gh_wheel_path.open('r') as f:
     wheel_config = yaml.safe_load(f)
 
-build_matrix = wheel_config['jobs']['build_wheels']['strategy']['matrix']
+build_matrix = wheel_config['jobs'][
+    'build_wheels_windows_osx']['strategy']['matrix']
 n_python_versions = len(build_matrix['python'])
 
-# For each python version we have: 5 wheels
+# For each python version we have: 7 wheels
 # 1 osx wheel (x86_64)
 # 4 linux wheel (i686 + x86_64) * (manylinux1 + manylinux2010)
 # 2 windows wheel (win32 + wind_amd64)
