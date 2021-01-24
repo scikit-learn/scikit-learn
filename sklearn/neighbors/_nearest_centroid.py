8c3a87bf913125820bf03d7e68de29fac0514983
aerodynamic-saucepan <theo.larcher@insa-rouen.fr>
2021-01-24 20:09:53 +0100

    Fixed AZure error ModuleNotFoundError(No module named 'sklearn.neighbors.nearest_centroid')

--- sklearn/neighbors/_nearest_centroid.py
+++ sklearn/neighbors/_nearest_centroid.py
@@ -55,7 +55,7 @@ class NearestCentroid(BaseEstimator, ClassifierMixin):
 
     Examples
     --------
-    >>> from sklearn.neighbors.nearest_centroid import NearestCentroid
+    >>> from sklearn.neighbors._nearest_centroid import NearestCentroid
     >>> import numpy as np
     >>> X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
     >>> y = np.array([1, 1, 1, 2, 2, 2])
