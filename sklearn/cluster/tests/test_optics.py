6e023c79d22dec26836cb42f10b30da3532ba0fb
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2021-01-25 10:56:36 +0100

    MNT use bool instead of np.bool_ to silence FutureWarning [scipy-dev] (#19267)
    Co-authored-by: Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>

--- sklearn/cluster/tests/test_optics.py
+++ sklearn/cluster/tests/test_optics.py
@@ -220,7 +220,7 @@ def test_nowarn_if_metric_bool_data_bool():
     # https://github.com/scikit-learn/scikit-learn/issues/18996
 
     pairwise_metric = 'rogerstanimoto'
-    X = np.random.randint(2, size=(5, 2), dtype=np.bool)
+    X = np.random.randint(2, size=(5, 2), dtype=bool)
 
     with pytest.warns(None) as warn_record:
         OPTICS(metric=pairwise_metric).fit(X)
@@ -234,7 +234,7 @@ def test_warn_if_metric_bool_data_no_bool():
     # https://github.com/scikit-learn/scikit-learn/issues/18996
 
     pairwise_metric = 'rogerstanimoto'
-    X = np.random.randint(2, size=(5, 2), dtype=np.int)
+    X = np.random.randint(2, size=(5, 2), dtype=np.int32)
     msg = f"Data will be converted to boolean for metric {pairwise_metric}"
 
     with pytest.warns(DataConversionWarning, match=msg) as warn_record:
@@ -246,8 +246,8 @@ def test_nowarn_if_metric_no_bool():
     # make sure no conversion warning is raised if
     # metric isn't boolean, no matter what the data type is
     pairwise_metric = 'minkowski'
-    X_bool = np.random.randint(2, size=(5, 2), dtype=np.bool)
-    X_num = np.random.randint(2, size=(5, 2), dtype=np.int)
+    X_bool = np.random.randint(2, size=(5, 2), dtype=bool)
+    X_num = np.random.randint(2, size=(5, 2), dtype=np.int32)
 
     with pytest.warns(None) as warn_record:
         # fit boolean data
