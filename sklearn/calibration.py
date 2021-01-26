edfc91778f193934162a30d3f5fa9584353bedb0
Lucy Liu <jliu176@gmail.com>
2021-01-26 19:21:14 +1100

    add example and see also docstring

--- sklearn/calibration.py
+++ sklearn/calibration.py
@@ -928,6 +928,33 @@ class CalibrationDisplay:
 
     figure_ : matplotlib Figure
         Figure containing the curve.
+
+    See Also
+    --------
+    calibration_curve : Compute true and predicted probabilities for a
+        calibration curve.
+
+    CalibrationDisplay.from_predictions : Plot calibration curve using true
+        and predicted labels.
+
+    CalibrationDisplay.from_estimator : Plot calibration curve using an
+        estimator and data.
+
+    Examples
+    --------
+    >>> from sklearn.datasets import make_classification
+    >>> from sklearn.model_selection import train_test_split
+    >>> from sklearn.linear_model import LogisticRegression
+    >>> from sklearn.calibration import CalibrationDisplay
+    >>> X, y = make_classification(random_state=0)
+    >>> X_train, X_test, y_train, y_test = train_test_split(
+    ...     X, y, random_state=0)
+    >>> clf = LogisticRegression(random_state=0)
+    >>> clf.fit(X_train, y_train)
+    LogisticRegression(random_state=0)
+    >>> y_prob = clf.predict_proba(X_test)[:, 1]
+    >>> disp = CalibrationDisplay.from_predictions(y_test, y_prob)
+    >>> disp.plot() # doctest: +SKIP
     """
     def __init__(self, prob_true, prob_pred, y_prob):
         self.prob_true = prob_true
