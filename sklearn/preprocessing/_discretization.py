dc6b095ce3f9fe28c0bcd3aba6b018cc1112b24a
Gleb Levitskiy <36483986+GLevV@users.noreply.github.com>
2021-01-28 16:47:43 +0000

    Update _discretization.py

--- sklearn/preprocessing/_discretization.py
+++ sklearn/preprocessing/_discretization.py
@@ -158,7 +158,8 @@ class KBinsDiscretizer(TransformerMixin, BaseEstimator):
         if self.n_bins == 'warn':
             warnings.warn("The default value of n_bins will change from "
                           "5 to 'auto' in 0.25.", FutureWarning)
-        self.n_bins = 5
+            self.n_bins = 5
+        
         X = self._validate_data(X, dtype='numeric')
 
         supported_dtype = (np.float64, np.float32)
