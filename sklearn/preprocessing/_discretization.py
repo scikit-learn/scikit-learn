0d7cc149ddf24c8f529eed6d8dd37385e96246b2
Gleb Levitskiy <36483986+GLevV@users.noreply.github.com>
2021-01-29 13:40:23 +0000

    Update sklearn/preprocessing/_discretization.py
    Co-authored-by: Jérémie du Boisberranger <34657725+jeremiedbb@users.noreply.github.com>

--- sklearn/preprocessing/_discretization.py
+++ sklearn/preprocessing/_discretization.py
@@ -32,7 +32,7 @@ class KBinsDiscretizer(TransformerMixin, BaseEstimator):
         The number of bins to produce. Raises ValueError if ``n_bins < 2``.
         For 'auto' option Sturges formula is used: bins are log(n_samples) + 1.
 
-        .. versionadded:: 0.24
+        .. versionadded:: 1.0
             Added 'auto' option
 
     encode : {'onehot', 'onehot-dense', 'ordinal'}, default='onehot'
