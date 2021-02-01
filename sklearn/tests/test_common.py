96a96f19579c71da80a14f33a15a0402b2f797b4
Thomas J. Fan <thomasjpfan@gmail.com>
2021-02-01 10:40:57 -0500

    ENH Adds n_features_in_ checking in cross_decomposition (#18741)

--- sklearn/tests/test_common.py
+++ sklearn/tests/test_common.py
@@ -267,7 +267,6 @@ N_FEATURES_IN_AFTER_FIT_MODULES_TO_IGNORE = {
     'calibration',
     'compose',
     'covariance',
-    'cross_decomposition',
     'discriminant_analysis',
     'ensemble',
     'feature_extraction',
