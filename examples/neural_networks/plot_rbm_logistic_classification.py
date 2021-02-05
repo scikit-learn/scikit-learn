Fix incorrect 0-1 scaling in the RBM example
Fix https://github.com/scikit-learn/scikit-learn/issues/19362

examples/neural_networks/plot_rbm_logistic_classification.py
@@ -79,7 +79,7 @@ def nudge_dataset(X, Y):
 X, y = datasets.load_digits(return_X_y=True)
 X = np.asarray(X, 'float32')
 X, Y = nudge_dataset(X, y)
-X = (X - np.min(X, 0)) / (np.max(X, 0) + 0.0001)  # 0-1 scaling
+X = (X - np.min(X, 0)) / (np.max(X, 0) - np.min(X, 0))  # 0-1 scaling
 
 X_train, X_test, Y_train, Y_test = train_test_split(
     X, Y, test_size=0.2, random_state=0)