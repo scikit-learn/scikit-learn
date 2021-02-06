db3db627930d7adc91962052693223a9c1a7dcc1
Alihan Zihna <a.zihna@ckhgbdp.onmicrosoft.com>
2021-02-06 11:43:45 +0000

    Add the previous example in the new format

--- examples/ensemble/plot_forest_importances.py
+++ examples/ensemble/plot_forest_importances.py
@@ -38,17 +38,46 @@ X, y = make_classification(n_samples=1000,
 # %%
 # Feature permutation importance on full model
 # --------------------------------------------
-# Another way to calculate the feature importances is by
-# using permutation_importance from the inspection submodule.
+# Feature importances can be calculated by calculating mean
+# and standard deviation of feature importance based on impurty
+# or by using permutation_importance from the inspection submodule.
 # Here, we'll fit an Extra-Trees Classifier to the data and
 # calculate the feature importances on the full data set.
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.inspection import permutation_importance
+# First we'll start by impurity based importance.
+# .. warning::
+#     Impurity-based feature importances can be misleading for high cardinality
+#     features (many unique values). See :ref:`permutation_importance` as
+#     an alternative below
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
 feature_names = [f'feature {i}' for i in range(X.shape[1])]
+forest = RandomForestClassifier(n_estimators=250,
+                                random_state=0)
 
-forest = ExtraTreesClassifier(n_estimators=250,
-                              random_state=42)
+start_time = time.time()
+forest.fit(X, y)
+importances = forest.feature_importances_
+std = np.std([tree.feature_importances_ for tree in forest.estimators_],
+             axis=0)
+forest_importances = pd.Series(
+    importances, index=feature_names
+)
+elapsed_time = time.time() - start_time
+print(f"Elapsed time to fit and compute the importances: "
+      f"{elapsed_time:.3f} seconds")
+
+# Let's plot the purity based importance.
+ax = forest_importances.plot.bar(yerr=std)
+ax.set_title("Feature importances using MDI on full model")
+ax.set_ylabel("Mean accuracy decrease")
+plt.show()
+
+# Let's do the same calculation using :ref:`permutation_importance`
+from sklearn.inspection import permutation_importance
+
+forest = RandomForestClassifier(n_estimators=250,
+                                random_state=42)
 
 start_time = time.time()
 forest.fit(X, y)
@@ -72,5 +101,6 @@ ax.set_title("Feature importances using permutation on full model")
 ax.set_ylabel("Mean accuracy decrease")
 plt.show()
 
-# Same as in the OOB feature importance three most important features
-# are detected.
+# Same features are detected as most important using both methods.
+# Although the relative importances vary. As seen on the plots, MDI
+# is less likely than permutation importance to fully omit a feature.
