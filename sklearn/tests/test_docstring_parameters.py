1fe2dc518b6f9756b233de51477eba30c044b36a
Thomas J. Fan <thomasjpfan@gmail.com>
2021-02-08 04:13:33 -0500

    DOC Documents n_features_in_ in cross_decomposition (#19351)

--- sklearn/tests/test_docstring_parameters.py
+++ sklearn/tests/test_docstring_parameters.py
@@ -174,6 +174,40 @@ def _construct_searchcv_instance(SearchCV):
     return SearchCV(LogisticRegression(), {"C": [0.1, 1]})
 
 
+N_FEATURES_MODULES_TO_IGNORE = {
+    'calibration',
+    'cluster',
+    'compose',
+    'covariance',
+    'decomposition',
+    'discriminant_analysis',
+    'dummy',
+    'ensemble',
+    'feature_extraction',
+    'feature_selection',
+    'gaussian_process',
+    'impute',
+    'isotonic',
+    'kernel_approximation',
+    'kernel_ridge',
+    'linear_model',
+    'manifold',
+    'mixture',
+    'model_selection',
+    'multiclass',
+    'multioutput',
+    'naive_bayes',
+    'neighbors',
+    'neural_network',
+    'pipeline',
+    'preprocessing',
+    'random_projection',
+    'semi_supervised',
+    'svm',
+    'tree'
+}
+
+
 @pytest.mark.parametrize('name, Estimator',
                          all_estimators())
 def test_fit_docstring_attributes(name, Estimator):
@@ -234,9 +268,12 @@ def test_fit_docstring_attributes(name, Estimator):
     else:
         est.fit(X, y)
 
-    skipped_attributes = {'n_features_in_',
-                          'x_scores_',  # For PLS, TODO remove in 1.1
+    skipped_attributes = {'x_scores_',  # For PLS, TODO remove in 1.1
                           'y_scores_'}  # For PLS, TODO remove in 1.1
+
+    module = est.__module__.split(".")[1]
+    if module in N_FEATURES_MODULES_TO_IGNORE:
+        skipped_attributes.add("n_features_in_")
 
     for attr in attributes:
         if attr.name in skipped_attributes:
