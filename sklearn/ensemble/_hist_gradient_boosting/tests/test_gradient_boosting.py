56b1dbc93876c9ab75731f4857c3729309d7ab58
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2021-02-09 17:01:17 +0100

    Update sklearn/ensemble/_hist_gradient_boosting/tests/test_gradient_boosting.py

--- sklearn/ensemble/_hist_gradient_boosting/tests/test_gradient_boosting.py
+++ sklearn/ensemble/_hist_gradient_boosting/tests/test_gradient_boosting.py
@@ -207,6 +207,7 @@ def test_least_absolute_deviation_sample_weight():
     # non regression test for issue #19400
     # make sure no error is thrown during fit of
     # HistGradientBoostingRegressor with least_absolute_deviation loss function
+    # and passing sample_weight
     rng = np.random.RandomState(0)
     n_samples = 100
     X = rng.uniform(-1, 1, size=(n_samples, 2))
