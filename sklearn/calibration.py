4b0bc51472da3517e9010609a2d87961d3fe77e3
t-kusanagi <46693526+t-kusanagi@users.noreply.github.com>
2021-02-10 19:03:43 +0900

    Make it more readable
    Co-authored-by: Olivier Grisel <olivier.grisel@ensta.org>

--- sklearn/calibration.py
+++ sklearn/calibration.py
@@ -658,8 +658,9 @@ class _CalibratedClassifier:
         else:
             denominator = np.sum(proba, axis=1)[:, np.newaxis]
             # XXX : for some reason all probas can be 0
-            proba = np.divide(proba, denominator, out=np.full_like(
-                proba, 1 / n_classes), where=denominator != 0)
+            uniform_proba = np.full_like(proba, 1 / n_classes)
+            proba = np.divide(proba, denominator, out=uniform_proba,
+                              where=denominator != 0)
 
         # Deal with cases where the predicted probability minimally exceeds 1.0
         proba[(1.0 < proba) & (proba <= 1.0 + 1e-5)] = 1.0
