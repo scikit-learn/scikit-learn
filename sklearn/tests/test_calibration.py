f0f2ede77e7f947249cf10f933c78785dc283a08
t.kusanagi <tk.tk.0515@gmail.com>
2021-02-10 23:54:48 +0900

    Add comment

--- sklearn/tests/test_calibration.py
+++ sklearn/tests/test_calibration.py
@@ -277,6 +277,10 @@ def test_calibration_multiclass(method, ensemble, seed):
 
 
 def test_calibration_zero_probability():
+    # Test an edge case where _CalibratedClassifier avoids numerical  errors
+    # in the multiclass normalization step if the all the calibrators output
+    # zero all at once for a given sample and instead fallback to uniform
+    # probabilities.
     class ZeroCalibrator():
         # This function is called from _CalibratedClassifier.predict_proba.
         def predict(self, X):
