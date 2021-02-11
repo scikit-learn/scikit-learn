2615a98329090f0ff80fc48d5562250cd53cbbbf
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2021-02-11 10:52:26 +0100

    nitpicks

--- sklearn/svm/tests/test_sparse.py
+++ sklearn/svm/tests/test_sparse.py
@@ -347,9 +347,10 @@ def test_sparse_svc_clone_with_callable_kernel():
 def test_timeout():
     sp = svm.SVC(C=1, kernel=lambda x, y: x * y.T,
                  probability=True, random_state=0, max_iter=1)
-    warning_msg = r'Solver terminated early \(max_iter=1\).' \
-                  '  Consider pre-processing your data with' \
-                  ' StandardScaler or MinMaxScaler.'
+    warning_msg = (
+        r'Solver terminated early \(max_iter=1\).  Consider pre-processing '
+        r'your data with StandardScaler or MinMaxScaler.'
+    )
     with pytest.warns(ConvergenceWarning, match=warning_msg):
         sp.fit(X_sp, Y)
 
--- sklearn/svm/tests/test_svm.py
+++ sklearn/svm/tests/test_svm.py
@@ -21,7 +21,6 @@ from sklearn.metrics.pairwise import rbf_kernel
 from sklearn.utils import check_random_state
 from sklearn.utils._testing import assert_raise_message
 from sklearn.utils._testing import ignore_warnings
-from sklearn.utils._testing import assert_no_warnings
 from sklearn.utils.validation import _num_samples
 from sklearn.utils import shuffle
 from sklearn.exceptions import ConvergenceWarning
@@ -978,9 +977,10 @@ def test_svc_bad_kernel():
 def test_timeout():
     a = svm.SVC(kernel=lambda x, y: np.dot(x, y.T), probability=True,
                 random_state=0, max_iter=1)
-    warning_msg = r'Solver terminated early \(max_iter=1\).' \
-                  '  Consider pre-processing your data with' \
-                  ' StandardScaler or MinMaxScaler.'
+    warning_msg = (
+        r'Solver terminated early \(max_iter=1\).  Consider pre-processing '
+        r'your data with StandardScaler or MinMaxScaler.'
+    )
     with pytest.warns(ConvergenceWarning, match=warning_msg):
         a.fit(np.array(X), Y)
 
@@ -1011,8 +1011,9 @@ def test_linear_svm_convergence_warnings():
     # Test that warnings are raised if model does not converge
 
     lsvc = svm.LinearSVC(random_state=0, max_iter=2)
-    warning_msg = "Liblinear failed to converge, increase" \
-                  " the number of iterations."
+    warning_msg = (
+        "Liblinear failed to converge, increase the number of iterations."
+    )
     with pytest.warns(ConvergenceWarning, match=warning_msg):
         lsvc.fit(X, Y)
     assert lsvc.n_iter_ == 2
@@ -1167,21 +1168,30 @@ def test_svc_ovr_tie_breaking(SVCClass):
 def test_gamma_auto():
     X, y = [[0.0, 1.2], [1.0, 1.3]], [0, 1]
 
-    assert_no_warnings(svm.SVC(kernel='linear').fit, X, y)
-    assert_no_warnings(svm.SVC(kernel='precomputed').fit, X, y)
+    with pytest.warns(None) as record:
+        svm.SVC(kernel='linear').fit(X, y)
+    assert not len(record)
+
+    with pytest.warns(None) as record:
+        svm.SVC(kernel='precomputed').fit(X, y)
+    assert not len(record)
 
 
 def test_gamma_scale():
     X, y = [[0.], [1.]], [0, 1]
 
     clf = svm.SVC()
-    assert_no_warnings(clf.fit, X, y)
+    with pytest.warns(None) as record:
+        clf.fit(X, y)
+    assert not len(record)
     assert_almost_equal(clf._gamma, 4)
 
     # X_var ~= 1 shouldn't raise warning, for when
     # gamma is not explicitly set.
     X, y = [[1, 2], [3, 2 * np.sqrt(6) / 3 + 2]], [0, 1]
-    assert_no_warnings(clf.fit, X, y)
+    with pytest.warns(None) as record:
+        clf.fit(X, y)
+    assert not len(record)
 
 
 @pytest.mark.parametrize(
