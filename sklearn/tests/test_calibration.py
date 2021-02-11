61699edd82a41c06f71ca63f7e94ef118e793a5f
t-kusanagi <46693526+t-kusanagi@users.noreply.github.com>
2021-02-11 10:13:44 +0900

    Fix comment
    Co-authored-by: Jérémie du Boisberranger <34657725+jeremiedbb@users.noreply.github.com>

--- sklearn/tests/test_calibration.py
+++ sklearn/tests/test_calibration.py
@@ -277,7 +277,7 @@ def test_calibration_multiclass(method, ensemble, seed):
 
 
 def test_calibration_zero_probability():
-    # Test an edge case where _CalibratedClassifier avoids numerical  errors
+    # Test an edge case where _CalibratedClassifier avoids numerical errors
     # in the multiclass normalization step if the all the calibrators output
     # zero all at once for a given sample and instead fallback to uniform
     # probabilities.
