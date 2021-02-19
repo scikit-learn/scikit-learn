bugfix RANSAC - residual threshold calculation #16147

sklearn/linear_model/_ransac.py
@@ -384,7 +384,7 @@ class RANSACRegressor(MetaEstimatorMixin, RegressorMixin,
             residuals_subset = loss_function(y, y_pred)
 
             # classify data into inliers and outliers
-            inlier_mask_subset = residuals_subset < residual_threshold
+            inlier_mask_subset = residuals_subset <= residual_threshold
             n_inliers_subset = np.sum(inlier_mask_subset)
 
             # less inliers -> skip current random sample