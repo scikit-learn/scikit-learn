5ce0eab0bda08f84788881da2efce0063ff345b8
Dmitry Kobak <dmitry.kobak@uni-tuebingen.de>
2021-02-24 21:51:07 +0100

    Update the docstring example to avoid warnings

--- sklearn/manifold/_t_sne.py
+++ sklearn/manifold/_t_sne.py
@@ -640,7 +640,8 @@ class TSNE(BaseEstimator):
     >>> import numpy as np
     >>> from sklearn.manifold import TSNE
     >>> X = np.array([[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 1]])
-    >>> X_embedded = TSNE(n_components=2).fit_transform(X)
+    >>> X_embedded = TSNE(n_components=2, learning_rate='auto',
+                          init='random').fit_transform(X)
     >>> X_embedded.shape
     (4, 2)
 
