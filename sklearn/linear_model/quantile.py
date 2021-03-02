075296669905be2555ea0b232460a6f12fc14806
david <dale.david@mail.ru>
2021-03-02 03:01:19 +0300

    replace the approximated nonlinear solver with the linear solver

--- sklearn/linear_model/quantile.py
+++ sklearn/linear_model/quantile.py
@@ -2,75 +2,11 @@
 # License: BSD 3 clause
 
 import numpy as np
-from scipy import optimize
+from scipy.optimize import linprog
 
 from ..base import BaseEstimator, RegressorMixin
 from ._base import LinearModel
-from ..utils.optimize import _check_optimize_result
 from ..utils.validation import _check_sample_weight
-from ..utils.extmath import safe_sparse_dot
-
-
-def _smooth_quantile_loss_and_gradient(
-        w, X, y, quantile, alpha, l1_ratio, fit_intercept,
-        sample_weight, gamma=0):
-    """ Smooth approximation to quantile regression loss, gradient and hessian.
-    Main loss and l1 penalty are both approximated by the same trick
-    from Chen & Wei, 2005
-    """
-    _, n_features = X.shape
-    if fit_intercept:
-        intercept = w[-1]
-    w = w[:n_features]
-
-    # Discriminate positive, negative and small residuals
-    linear_loss = y - safe_sparse_dot(X, w)
-    if fit_intercept:
-        linear_loss -= intercept
-    positive_error = linear_loss > quantile * gamma
-    negative_error = linear_loss < (quantile - 1) * gamma
-    small_error = ~ (positive_error | negative_error)
-
-    # Calculate loss due to regression error
-    regression_loss = (
-        positive_error * (linear_loss*quantile - 0.5*gamma*quantile**2) +
-        small_error * 0.5*linear_loss**2 / (gamma if gamma != 0 else 1) +
-        negative_error * (linear_loss*(quantile-1) - 0.5*gamma*(quantile-1)**2)
-        ) * sample_weight
-    loss = np.sum(regression_loss)
-
-    if fit_intercept:
-        grad = np.empty(n_features + 1)
-    else:
-        grad = np.empty(n_features + 0)
-
-    # Gradient due to the regression error
-    weighted_grad = (positive_error * quantile +
-                     small_error * linear_loss / (gamma if gamma != 0 else 1) +
-                     negative_error * (quantile-1)) * sample_weight
-    grad[:n_features] = -safe_sparse_dot(weighted_grad, X)
-
-    if fit_intercept:
-        grad[-1] = -np.sum(weighted_grad)
-
-    # Gradient and loss due to the ridge penalty
-    grad[:n_features] += alpha * (1 - l1_ratio) * 2. * w
-    loss += alpha * (1 - l1_ratio) * np.dot(w, w)
-
-    # Gradient and loss due to the lasso penalty
-    # for smoothness replace abs(w) with w^2/(2*gamma)+gamma/2 for abs(w)<gamma
-    if gamma > 0:
-        large_coef = np.abs(w) > gamma
-        small_coef = ~large_coef
-        loss += alpha*l1_ratio*np.sum(large_coef*np.abs(w) +
-                                      small_coef*(w**2/(2*gamma) + gamma/2))
-        grad[:n_features] += alpha*l1_ratio*(large_coef*np.sign(w) +
-                                             small_coef*w/gamma)
-    else:
-        loss += alpha * l1_ratio * np.sum(np.abs(w))
-        grad[:n_features] += alpha * l1_ratio * np.sign(w)
-
-    return loss, grad
 
 
 class QuantileRegressor(LinearModel, RegressorMixin, BaseEstimator):
@@ -91,27 +27,8 @@ class QuantileRegressor(LinearModel, RegressorMixin, BaseEstimator):
     quantile : float, strictly between 0.0 and 1.0, default 0.5
         The quantile that the model predicts.
 
-    max_iter : int, default 100
-        Maximum number of iterations that scipy.optimize.minimize
-        should run for.
-
     alpha : float, default 0.0001
-        Constant that multiplies ElasticNet penalty term.
-
-    l1_ratio : float, default 0.0
-        The ElasticNet mixing parameter, with ``0 <= l1_ratio <= 1``. For
-        ``l1_ratio = 0`` the penalty is an L2 penalty. ``For l1_ratio = 1`` it
-        is an L1 penalty.  For ``0 < l1_ratio < 1``, the penalty is a
-        combination of L1 and L2.
-
-    warm_start : bool, default False
-        This is useful if the stored attributes of a previously used model
-        has to be reused. If set to False, then the coefficients will
-        be rewritten for every call to fit.
-        ``warm_start`` does not significantly speed up the convergence
-        if the model optimizes different cost functions, as ``gamma`` converges
-        to 0. It is therefore recommended to set small ``gamma``
-        if ``warm_start`` is set to True.
+        Constant that multiplies L1 penalty term.
 
     fit_intercept : bool, default True
         Whether or not to fit the intercept. This can be set to False
@@ -125,33 +42,6 @@ class QuantileRegressor(LinearModel, RegressorMixin, BaseEstimator):
     copy_X : boolean, optional, default True
         If True, X will be copied; else, it may be overwritten.
 
-    gamma : float, default 1e-2
-        Starting value for smooth approximation.
-        Absolute loss is replaced with quadratic for ``|error| < gamma``.
-        Lasso penalty is replaced with quadratic for ``|w| < gamma``.
-        ``gamma = 0`` gives exact non-smooth loss function.
-        The algorithm performs consecutive optimizations with gamma
-        decreasing by factor of ``gamma_decrease``,
-        until ``xtol`` criterion is met,
-        or until ``max_iter`` is exceeded.
-
-    gtol : float, default 1e-4
-        The smooth optimizing iteration will stop when
-        ``max{|proj g_i | i = 1, ..., n}`` <= ``gtol``
-        where pg_i is the i-th component of the projected gradient.
-
-    xtol : float, default 1e-6
-        Global optimization will stop when ``|w_{t-1} - w_t|`` < ``xtol``
-        where w_t is result of t'th approximated optimization.
-
-    gamma_decrease : float, default 0.1
-        The factor by which ``gamma`` is multiplied at each iteration.
-
-    n_gamma_decreases : int, default 10
-        Maximal number of iterations of approximation of the cost function.
-        At each iteration, ``gamma`` is multiplied by a factor
-        of ``gamma_decrease``
-
     Attributes
     ----------
     coef_ : array, shape (n_features,)
@@ -161,7 +51,7 @@ class QuantileRegressor(LinearModel, RegressorMixin, BaseEstimator):
         Bias.
 
     n_iter_ : int
-        Number of iterations that scipy.optimize.mimimize has run for.
+        Number of iterations that scipy.optimize.linprog has run for.
 
     References
     ----------
@@ -173,25 +63,23 @@ class QuantileRegressor(LinearModel, RegressorMixin, BaseEstimator):
            Sankhya: The Indian Journal of Statistics, 399-417.
     """
 
-    def __init__(self, quantile=0.5,
-                 max_iter=10000, alpha=0.0001, l1_ratio=0.0,
-                 warm_start=False, fit_intercept=True,
-                 normalize=False, copy_X=True,
-                 gamma=1e-2, gtol=1e-4, xtol=1e-6,
-                 gamma_decrease=0.1, n_gamma_decreases=100):
+    def __init__(
+            self,
+            quantile=0.5,
+            alpha=0.0001,
+            fit_intercept=True,
+            normalize=False,
+            copy_X=True,
+            method='revised simplex',
+            solver_options=None,
+    ):
         self.quantile = quantile
-        self.max_iter = max_iter
         self.alpha = alpha
-        self.l1_ratio = l1_ratio
-        self.warm_start = warm_start
         self.fit_intercept = fit_intercept
         self.copy_X = copy_X
         self.normalize = normalize
-        self.gtol = gtol
-        self.xtol = xtol
-        self.gamma = gamma
-        self.gamma_decrease = gamma_decrease
-        self.n_gamma_decreases = n_gamma_decreases
+        self.method = method
+        self.solver_options = solver_options
 
     def fit(self, X, y, sample_weight=None):
         """Fit the model according to the given training data.
@@ -228,80 +116,56 @@ class QuantileRegressor(LinearModel, RegressorMixin, BaseEstimator):
                 "Quantile should be strictly between 0.0 and 1.0, got %f"
                 % self.quantile)
 
-        if self.warm_start and hasattr(self, 'coef_'):
-            parameters = np.concatenate(
-                (self.coef_, [self.intercept_]))
-        else:
-            if self.fit_intercept:
-                parameters = np.zeros(X.shape[1] + 1)
-            else:
-                parameters = np.zeros(X.shape[1] + 0)
+        n_obs, n_slopes = X.shape
+        n_params = n_slopes
 
-        # solve sequence of optimization problems
-        # with different smoothing parameter
-        total_iter = []
-        loss_args = (X, y, self.quantile, self.alpha, self.l1_ratio,
-                     self.fit_intercept, sample_weight)
-        for i in range(self.n_gamma_decreases):
-            gamma = self.gamma * self.gamma_decrease ** i
-            result = optimize.minimize(
-                _smooth_quantile_loss_and_gradient,
-                parameters,
-                args=loss_args + (gamma, ),
-                method='L-BFGS-B',
-                jac=True,
-                options={
-                    'gtol': self.gtol,
-                    'maxiter': self.max_iter - sum(total_iter),
-                }
-                )
-            total_iter.append(result['nit'])
-            prev_parameters = parameters
-            parameters = result['x']
+        X_full = X
+        if self.fit_intercept:
+            n_params += 1
+            X_full = np.concatenate([np.ones([n_obs, 1]), X], axis=1)
 
-            # for lasso, replace parameters with exact zero,
-            # if this decreases the cost function
-            if self.alpha * self.l1_ratio > 0:
-                value, _ = _smooth_quantile_loss_and_gradient(parameters,
-                                                              *loss_args,
-                                                              gamma=0)
-                for j in range(len(parameters)):
-                    new_parameters = parameters.copy()
-                    old_param = new_parameters[j]
-                    new_parameters[j] = 0
-                    new_value, _ = _smooth_quantile_loss_and_gradient(
-                        new_parameters, *loss_args, gamma=0)
-                    # check if the cost function decreases,
-                    # or increases, but by little, and param is small anyway
-                    if new_value <= value \
-                            or np.abs(old_param) < self.xtol \
-                            and new_value < value + self.gtol:
-                        value = new_value
-                        parameters = new_parameters
+        # the linear programming formulation of quantile regression
+        # follows https://stats.stackexchange.com/questions/384909/
+        c_vector = np.concatenate([
+            np.ones(n_params * 2) * self.alpha,
+            sample_weight * self.quantile,
+            sample_weight * (1 - self.quantile),
+        ])
+        # do not penalize the intercept
+        if self.fit_intercept:
+            c_vector[0] = 0
+            c_vector[n_params] = 0
 
-            # stop if solution does not change between subproblems
-            if np.linalg.norm(prev_parameters-parameters) < self.xtol:
-                break
-            # stop if maximum number of iterations is exceeded
-            if sum(total_iter) >= self.max_iter:
-                break
-            # stop if gamma is already zero
-            if gamma == 0:
-                break
-        # do I really need to issue this warning?
-        # Its reason is lineSearchError, which cannot be easily fixed
-        if not result['success']:
-            _check_optimize_result('lbfgs', result)
-        self.n_iter_ = sum(total_iter)
-        self.gamma_ = gamma
-        self.total_iter_ = total_iter
-        self.coef_ = parameters[:X.shape[1]]
+        a_eq_matrix = np.concatenate([
+            X_full,
+            -X_full,
+            np.eye(n_obs),
+            -np.eye(n_obs),
+        ], axis=1)
+        b_eq_vector = y
+
+        result = linprog(
+            c=c_vector,
+            A_eq=a_eq_matrix,
+            b_eq=b_eq_vector,
+            method=self.method,
+            options=self.solver_options
+        )
+        # todo: check the optimization result for convergence
+
+        params_pos = result.x[:n_params]
+        params_neg = result.x[n_params:2 * n_params]
+        params = params_pos - params_neg
+
+        self.n_iter_ = result.nit
+
+        self.coef_ = params[self.fit_intercept:]
         # do not use self.set_intercept_, because it assumes intercept is zero
         # if the data is normalized, which is false in this case
         if self.fit_intercept:
             self.coef_ = self.coef_ / X_scale
-            self.intercept_ = parameters[-1] + y_offset \
-                - np.dot(X_offset, self.coef_.T)
+            self.intercept_ = params[0] + y_offset \
+                              - np.dot(X_offset, self.coef_.T)
         else:
             self.intercept_ = 0.0
         return self
--- sklearn/linear_model/tests/test_quantile.py
+++ sklearn/linear_model/tests/test_quantile.py
@@ -1,10 +1,8 @@
 # Authors: David Dale dale.david@mail.ru
 # License: BSD 3 clause
 
-import pytest
 import numpy as np
 
-from sklearn.exceptions import ConvergenceWarning
 from sklearn.utils._testing import assert_allclose, assert_raises
 from sklearn.datasets import make_regression
 from sklearn.linear_model import HuberRegressor, QuantileRegressor
@@ -31,21 +29,15 @@ def test_quantile_toy_example():
     assert_allclose(model.intercept_, 1, atol=1e-2)
     assert_allclose(model.coef_[0], 1, atol=1e-2)
 
-    # for a small ridge penalty, the slope is also minimal
+    # for a small lasso penalty, the slope is also minimal
     model = QuantileRegressor(quantile=0.5, alpha=0.01).fit(X, y)
     assert_allclose(model.intercept_, 1, atol=1e-2)
     assert_allclose(model.coef_[0], 1, atol=1e-2)
 
-    # for a small lasso penalty, the slope is also minimal
-    model = QuantileRegressor(quantile=0.5, alpha=0.01, l1_ratio=1).fit(X, y)
-    assert_allclose(model.intercept_, 1, atol=1e-2)
-    assert_allclose(model.coef_[0], 1, atol=1e-2)
-
-    # for a large ridge penalty, the model no longer minimizes MAE
-    # (1.75, 0.25) minimizes c^2 + 0.5 (abs(1-b) + abs(2-b-c) + abs(11-b-c))
-    model = QuantileRegressor(quantile=0.5, alpha=1).fit(X, y)
-    assert_allclose(model.intercept_, 1.75, atol=1e-2)
-    assert_allclose(model.coef_[0], 0.25, atol=1e-2)
+    # for a large lasso penalty, the model predicts constant median
+    model = QuantileRegressor(quantile=0.5, alpha=100).fit(X, y)
+    assert_allclose(model.intercept_, 2, atol=1e-2)
+    assert_allclose(model.coef_[0], 0, atol=1e-2)
 
 
 def test_quantile_equals_huber_for_low_epsilon():
@@ -65,16 +57,6 @@ def test_quantile_estimates_fraction():
         quant = QuantileRegressor(quantile=q, alpha=0).fit(X, y)
         fraction_below = np.mean(y < quant.predict(X))
         assert_allclose(fraction_below, q, atol=1e-2)
-
-
-def test_quantile_is_approximately_sparse():
-    # Now most of coefficients are not exact zero,
-    # but with large n_samples they are close enough
-    X, y = make_regression(n_samples=3000, n_features=100, n_informative=10,
-                           random_state=0, noise=1.0)
-    q = QuantileRegressor(l1_ratio=1, alpha=0.1).fit(X, y)
-    share_zeros = np.mean(np.abs(q.coef_) > 1e-1)
-    assert_allclose(share_zeros, 0.1, atol=1e-2)
 
 
 def test_quantile_without_intercept():
@@ -127,44 +109,9 @@ def test_normalize():
     X += rng.normal(size=X.shape[1], scale=3)
     X *= rng.normal(size=X.shape[1], scale=3)
     y = y * 10 + 100
-    model1 = QuantileRegressor(alpha=1e-6, normalize=False, max_iter=10000)
-    model2 = QuantileRegressor(alpha=1e-6, normalize=True, max_iter=10000)
+    model1 = QuantileRegressor(alpha=1e-6, normalize=False)
+    model2 = QuantileRegressor(alpha=1e-6, normalize=True)
     cvs1 = cross_val_score(model1, X, y, cv=3).mean()
     cvs2 = cross_val_score(model2, X, y, cv=3).mean()
     assert cvs1 > 0.99
     assert cvs2 > 0.99
-
-
-def test_quantile_warm_start():
-    # test that warm restart leads to the same point
-    X, y = make_regression(random_state=0, n_samples=1000)
-    warm = QuantileRegressor(fit_intercept=True, alpha=1.0, max_iter=10000,
-                             warm_start=True, gamma=1e-10,
-                             xtol=1e-10, gtol=1e-10)
-    warm.fit(X, y)
-    warm_coef = warm.coef_.copy()
-    warm_iter = sum(warm.total_iter_)
-    warm.fit(X, y)
-
-    # SciPy performs the tol check after doing the coef updates, so
-    # these would be almost same but not necessarily equal.
-    assert_allclose(warm.coef_, warm_coef, atol=1e-1)
-    # assert a smaller number of iterations than the first fit
-    assert sum(warm.total_iter_) < warm_iter
-
-
-def test_quantile_convergence():
-    # Quantile loss may not converge to unique solution
-    # if there is no regularization
-    # need to check that warning is not thrown if model has converged.
-    X, y = make_regression(n_samples=300, n_features=20, random_state=0,
-                           noise=1.0)
-
-    # check that for small n_iter, warning is thrown
-    with pytest.warns(ConvergenceWarning, match="lbfgs failed to converge"):
-        QuantileRegressor(max_iter=1).fit(X, y)
-
-    # check that for large n_iter, it is not thrown
-    with pytest.warns(None) as record:
-        QuantileRegressor(max_iter=10000).fit(X, y)
-        assert len(record) == 0
