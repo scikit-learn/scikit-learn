fb415a11f9c880407ff5e907f0c1852ef27841be
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2021-03-03 11:12:22 +0100

    MNT Test that the changes are working [cd build]

--- build_tools/github/upload_anaconda.sh
+++ build_tools/github/upload_anaconda.sh
@@ -4,10 +4,10 @@ set -e
 set -x
 
 if [ "$GITHUB_EVENT_NAME" == "schedule" ]; then
-    ANACONDA_ORG="scipy-wheels-nightly"
+    #ANACONDA_ORG="scipy-wheels-nightly"
     ANACONDA_TOKEN="$SCIKIT_LEARN_NIGHTLY_UPLOAD_TOKEN"
 else
-    ANACONDA_ORG="scikit-learn-wheels-staging"
+    #ANACONDA_ORG="scikit-learn-wheels-staging"
     ANACONDA_TOKEN="$SCIKIT_LEARN_STAGING_UPLOAD_TOKEN"
 fi
 
@@ -15,6 +15,8 @@ export PATH=$CONDA/bin:$PATH
 conda create -n upload -y python
 source activate upload
 conda install -y anaconda-client
+
+ANACONDA_ORG="alfaro96"
 
 # Force a replacement if the remote file already exists
 anaconda -t $ANACONDA_TOKEN upload --force -u $ANACONDA_ORG dist/artifact/*
