efcb89d4a466666191c60a1bf8cb06dd177574f6
vishwaa.patel@mail.utoronto.ca <vishwaa.patel@mail.utoronto.ca>
2021-03-06 18:41:54 -0500

    Modifying the method to test font_size in bu3.py

--- sklearn/bu3.py
+++ sklearn/bu3.py
@@ -24,10 +24,38 @@ np.set_printoptions(precision=2)
 titles_options = [("Confusion matrix, without normalization", 'true'),
                   ("Normalized confusion matrix", 'true')]
 for title, normalize in titles_options:
-    disp = plot_confusion_matrix(classifier, X_test, y_test,
+
+    display_labels = class_names
+    include_values = True
+    cmap = plt.cm.Blues
+    if not cmap :
+        cmap = 'viridis'
+
+    y_pred = classifier.predict(X_test)
+    cm = confusion_matrix(y_test, y_pred, sample_weight=None,
+                          labels=display_labels, normalize=normalize)
+
+    '''if display_labels is None:
+        if labels is None:
+            display_labels = unique_labels(y_test, y_pred)
+        else:
+            display_labels = labels'''
+
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm,
+                                  display_labels=display_labels, font_size=30)
+
+    disp.from_predictions(classifier, y_test, y_pred, display_labels=class_names,
+                                 cmap=plt.cm.Blues,
+                                 normalize=normalize)
+
+    '''disp.plot(include_values=include_values,
+                     cmap=plt.cm.Blues, ax=None, xticks_rotation='horizontal',
+                     values_format=None, colorbar=True)'''
+
+   ''' disp = plot_confusion_matrix(classifier, X_test, y_test,
                                  display_labels=class_names,
                                  cmap=plt.cm.Blues,
-                                 normalize=normalize, font_size=None)
+                                 normalize=normalize, font_size=None)'''
     disp.ax_.set_title(title)
 
 
