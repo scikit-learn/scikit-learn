83b61d9faa51d14a8c528501b443f3f66c175cb3
Julien Jerphanion <git@jjerphan.xyz>
2021-03-12 23:29:36 +0100

    Revert to previous multiplication chain for sparse matrices
    Co-authored-by: Christian Lorentzen <lorentzen.ch@gmail.com>

--- sklearn/linear_model/_logistic.py
+++ sklearn/linear_model/_logistic.py
@@ -234,8 +234,7 @@ def _logistic_grad_hess(w, X, y, alpha, sample_weight=None):
     def Hs(s):
         ret = np.empty_like(s)
         if sparse.issparse(X):
-            XTdX = safe_sparse_dot(X.T, dX)
-            ret[:n_features] = safe_sparse_dot(XTdX, s[:n_features])
+            ret[:n_features] = X.T.dot(dX.dot(s[:n_features]))
         else:
             ret[:n_features] = np.linalg.multi_dot([X.T, dX, s[:n_features]])
         ret[:n_features] += alpha * s[:n_features]
