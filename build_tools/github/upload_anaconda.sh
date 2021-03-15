69261508005a773036cc87efb5da3ba49bbf90cc
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2021-03-15 18:53:28 +0100

    [MNT] Comment unnecesary code [cd build].

--- build_tools/github/upload_anaconda.sh
+++ build_tools/github/upload_anaconda.sh
@@ -3,13 +3,13 @@
 set -e
 set -x
 
-if [ "$GITHUB_EVENT_NAME" == "schedule" ]; then
-    #ANACONDA_ORG="scipy-wheels-nightly"
-    ANACONDA_TOKEN="$SCIKIT_LEARN_NIGHTLY_UPLOAD_TOKEN"
-else
-    #ANACONDA_ORG="scikit-learn-wheels-staging"
-    ANACONDA_TOKEN="$SCIKIT_LEARN_STAGING_UPLOAD_TOKEN"
-fi
+# if [ "$GITHUB_EVENT_NAME" == "schedule" ]; then
+# ANACONDA_ORG="scipy-wheels-nightly"
+# ANACONDA_TOKEN="$SCIKIT_LEARN_NIGHTLY_UPLOAD_TOKEN"
+# else
+# ANACONDA_ORG="scikit-learn-wheels-staging"
+# ANACONDA_TOKEN="$SCIKIT_LEARN_STAGING_UPLOAD_TOKEN"
+# fi
 
 export PATH=$CONDA/bin:$PATH
 conda create -n upload -y python
@@ -17,6 +17,7 @@ source activate upload
 conda install -y anaconda-client
 
 ANACONDA_ORG="alfaro96"
+ANACONDA_TOKEN="$SCIKIT_LEARN_STAGING_UPLOAD_TOKEN"
 
 # Force a replacement if the remote file already exists
 anaconda -t $ANACONDA_TOKEN upload --force -u $ANACONDA_ORG dist/artifact/*
