2d23fe2f8f6f6250a0bdea987cd1bd1fb8c14d73
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2021-03-16 11:29:17 +0100

    Trigger build [cd build]

--- build_tools/github/upload_anaconda.sh
+++ build_tools/github/upload_anaconda.sh
@@ -4,10 +4,11 @@ set -e
 set -x
 
 if [ "$GITHUB_EVENT_NAME" == "schedule" ]; then
-    #ANACONDA_ORG="scipy-wheels-nightly"
+    ANACONDA_ORG="scipy-wheels-nightly"
     ANACONDA_TOKEN="$SCIKIT_LEARN_NIGHTLY_UPLOAD_TOKEN"
 else
-    #ANACONDA_ORG="scikit-learn-wheels-staging"
+    ANACONDA_ORG="scikit-learn-wheels-staging"
+    ANACONDA_TOKEN="$SCIKIT_LEARN_STAGING_UPLOAD_TOKEN"
 fi
 
 # Install Python 3.8 because of a bug with Python 3.9
