dcb3701661353da7fa5ed3ce44e51e170d3a8d13
Sean Benhur J <43300345+seanbenhur@users.noreply.github.com>
2021-03-16 20:20:30 +0530

    Cross link gen_batches and gen_even_slices in See Also section

--- sklearn/utils/__init__.py
+++ sklearn/utils/__init__.py
@@ -690,6 +690,10 @@ def gen_batches(n, batch_size, *, min_batch_size=0):
     Yields
     ------
     slice of batch_size elements
+    
+    See Also
+    ------
+    gen_even_slices: Generator to create n_packs slices going up to n.
 
     Examples
     --------
@@ -739,6 +743,10 @@ def gen_even_slices(n, n_packs, *, n_samples=None):
     Yields
     ------
     slice
+    
+    See also
+    ------
+    gen_batches: Generator to create slices containing batch_size elements, from 0 to n.
 
     Examples
     --------
