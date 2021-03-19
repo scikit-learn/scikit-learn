8aa467aaf52a31f33f5206bd5e50ffba88dc104f
avigupta2612 <avigupta2612@gmail.com>
2021-03-19 21:08:49 +0530

    changed to traditional comment

--- sklearn/utils/tests/test_estimator_checks.py
+++ sklearn/utils/tests/test_estimator_checks.py
@@ -1,7 +1,6 @@
-"""We can not use pytest here, because we run
-build_tools/azure/test_pytest_soft_dependency.sh on these
-tests to make sure estimator_checks works without pytest.
-"""
+# We can not use pytest here, because we run
+# build_tools/azure/test_pytest_soft_dependency.sh on these
+# tests to make sure estimator_checks works without pytest.
 
 import unittest
 import sys
