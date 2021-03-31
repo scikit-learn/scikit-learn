5109283ddf2df9ed4fe4e227de8287b1fc5c8cdb
Nicolas Hug <nicolashug@fb.com>
2021-03-31 16:55:07 +0100

    hopefully fixed new test

--- sklearn/experimental/tests/test_enable_hist_gradient_boosting.py
+++ sklearn/experimental/tests/test_enable_hist_gradient_boosting.py
@@ -1,8 +1,14 @@
 """Tests for making sure experimental imports work as expected."""
 
-import pytest
+import textwrap
+
+from sklearn.utils._testing import assert_run_python_script
 
 
 def test_import_raises_warning():
-    with pytest.warns(UserWarning, match="It is not needed to import"):
+    code = """
+    import pytest
+    with pytest.warns(UserWarning, match="it is not needed to import"):
         from sklearn.experimental import enable_hist_gradient_boosting  # noqa
+    """
+    assert_run_python_script(textwrap.dedent(code))
