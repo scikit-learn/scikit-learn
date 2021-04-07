d93e5b67cfc4bdfbecdbd233ed8014313d38845c
Julien Jerphanion <git@jjerphan.xyz>
2021-04-07 13:56:44 +0200

    Add comment motivating the use of C++

--- sklearn/neighbors/_partition_nodes.pyx
+++ sklearn/neighbors/_partition_nodes.pyx
@@ -1,5 +1,23 @@
 # distutils : language = c++
 
+# KDTrees rely on partial sorts to partition their nodes.
+#
+# The C++ std library exposes nth_element, an efficient partial sort for this
+# situation which has a linear time complexity as well as the best performances.
+#
+# To use std::algorithm::nth_element, a few fixture are defined using Cython:
+# - partition_node_indices, a cython function used within KDTree, that calls
+# - partition_node_indices_inner, a C++ function that wraps nth_element and uses
+# - an IndexComparator to state how to compare KDTrees' indices
+#
+# IndexComparator has been defined so that partial sorts are stable with
+# respect to the nodes initial indices.
+#
+# See for reference:
+#  - https://en.cppreference.com/w/cpp/algorithm/nth_element.
+#  - https://github.com/scikit-learn/scikit-learn/pull/11103
+#  - https://github.com/scikit-learn/scikit-learn/pull/19473
+
 cdef extern from *:
     """
     #include <algorithm>
