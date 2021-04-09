9b20598951d6de18183513f9521d4e74025ade8c
Alihan Zihna <alihanz@gmail.com>
2021-04-09 09:37:15 +0100

    DOC add explicit message regarding shuffling in default CV (#19776)
    Co-authored-by: Alihan Zihna <a.zihna@ckhgbdp.onmicrosoft.com>

--- sklearn/model_selection/_search.py
+++ sklearn/model_selection/_search.py
@@ -1065,7 +1065,8 @@ class GridSearchCV(BaseSearchCV):
 
         For integer/None inputs, if the estimator is a classifier and ``y`` is
         either binary or multiclass, :class:`StratifiedKFold` is used. In all
-        other cases, :class:`KFold` is used.
+        other cases, :class:`KFold` is used. These splitters are instantiated
+        with `shuffle=False` so the splits will be the same across calls.
 
         Refer :ref:`User Guide <cross_validation>` for the various
         cross-validation strategies that can be used here.
@@ -1407,7 +1408,8 @@ class RandomizedSearchCV(BaseSearchCV):
 
         For integer/None inputs, if the estimator is a classifier and ``y`` is
         either binary or multiclass, :class:`StratifiedKFold` is used. In all
-        other cases, :class:`KFold` is used.
+        other cases, :class:`KFold` is used. These splitters are instantiated
+        with `shuffle=False` so the splits will be the same across calls.
 
         Refer :ref:`User Guide <cross_validation>` for the various
         cross-validation strategies that can be used here.
