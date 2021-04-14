3d9c800d2e8eb0be89f68d873ccf237797ea093f
Christian Lorentzen <lorentzen.ch@gmail.com>
2021-04-14 19:21:52 +0200

    CLN remove line that was commented out

--- sklearn/_loss/setup.py
+++ sklearn/_loss/setup.py
@@ -4,13 +4,9 @@ from numpy.distutils.misc_util import Configuration
 
 def configuration(parent_package="", top_path=None):
     config = Configuration("_loss", parent_package, top_path)
-
     config.add_extension(
         "_loss", sources=["_loss.pyx"], include_dirs=[numpy.get_include()]
     )
-
-    # config.add_subpackage("tests")
-
     return config
 
 
