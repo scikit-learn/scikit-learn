022e4185b02fa7b029687b995f8ee681c82f1453
Christian Lorentzen <lorentzen.ch@gmail.com>
2021-04-14 19:50:23 +0200

    ENH set NPY_NO_DEPRECATED_API

--- sklearn/_loss/setup.py
+++ sklearn/_loss/setup.py
@@ -5,7 +5,10 @@ from numpy.distutils.misc_util import Configuration
 def configuration(parent_package="", top_path=None):
     config = Configuration("_loss", parent_package, top_path)
     config.add_extension(
-        "_loss", sources=["_loss.pyx"], include_dirs=[numpy.get_include()]
+        "_loss",
+        sources=["_loss.pyx"],
+        include_dirs=[numpy.get_include()],
+        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_13_API_VERSION")],
     )
     return config
 
