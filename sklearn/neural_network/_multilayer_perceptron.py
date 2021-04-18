ba3e4ef942d2b2c6ef2d447193516f19efb3af0c
Isaack Mungui <munguiisaack@gmail.com>
2021-04-18 22:11:50 +0300

    Fixed whitespace error

--- sklearn/neural_network/_multilayer_perceptron.py
+++ sklearn/neural_network/_multilayer_perceptron.py
@@ -826,7 +826,7 @@ class MLPClassifier(ClassifierMixin, BaseMultilayerPerceptron):
         ``n_iter_no_change`` consecutive epochs. The split is stratified,
         except in a multilabel setting.
         If early stopping is False, then the training stops when the training
-        loss does not improve by more than tol for n_iter_no_change consecutive 
+        loss does not improve by more than tol for n_iter_no_change consecutive
         passes over the training set.
         Only effective when solver='sgd' or 'adam'
 
