697a22f8eaf0a1cbe05141eef79abedf9aa2f5a0
kmatt10 <kmatt10@gmail.com>
2021-04-30 23:45:54 +0800

    Update sklearn/impute/_base.py
    Co-authored-by: Thomas J. Fan <thomasjpfan@gmail.com>

--- sklearn/impute/_base.py
+++ sklearn/impute/_base.py
@@ -429,7 +429,8 @@ class SimpleImputer(_BaseImputer):
 
         Returns
         -------
-        X_imputed : {ndarray, sparse matrix} of shape (n_samples, n_features)
+        X_imputed : {ndarray, sparse matrix} of shape (n_samples, \
+                    n_features_out)
             input X with imputed values
         """
         check_is_fitted(self)
