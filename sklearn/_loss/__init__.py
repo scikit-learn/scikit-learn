e6505224a9372bd74901590eaded0c4a1e5db51a
Christian Lorentzen <lorentzen.ch@gmail.com>
2021-05-09 20:22:43 +0200

    MNT add classes to __init__

--- sklearn/_loss/__init__.py
+++ sklearn/_loss/__init__.py
@@ -2,3 +2,26 @@
 The :mod:`sklearn._loss` module includes loss function classes suitable for
 fitting classification and regression tasks.
 """
+
+from ._loss import (
+    HalfSquaredError,
+    AbsoluteError,
+    PinballLoss,
+    HalfPoissonLoss,
+    HalfGammaLoss,
+    HalfTweedieLoss,
+    BinaryCrossEntropy,
+    CategoricalCrossEntropy,
+)
+
+
+__all__ = [
+    "HalfSquaredError",
+    "AbsoluteError",
+    "PinballLoss",
+    "HalfPoissonLoss",
+    "HalfGammaLoss",
+    "HalfTweedieLoss",
+    "BinaryCrossEntropy",
+    "CategoricalCrossEntropy",
+]
