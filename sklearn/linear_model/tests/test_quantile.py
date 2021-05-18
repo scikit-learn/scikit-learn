1f244e19101c2f79141b1f2f5c1e549da93056e2
Christian Lorentzen <lorentzen.ch@gmail.com>
2021-05-18 19:56:00 +0200

    TST assure convergence in test_equivariance

--- sklearn/linear_model/tests/test_quantile.py
+++ sklearn/linear_model/tests/test_quantile.py
@@ -209,29 +209,30 @@ def test_equivariance(quantile):
     )
     # make y asymmetric
     y += rng.exponential(scale=100, size=y.shape)
-    model1 = QuantileRegressor(quantile=quantile, alpha=0).fit(X, y)
+    params = dict(alpha=0, solver_options={"lstsq": True, "tol": 1e-10})
+    model1 = QuantileRegressor(quantile=quantile, **params).fit(X, y)
 
     # coef(q; a*y, X) = a * coef(q; y, X)
     a = 2.5
-    model2 = QuantileRegressor(quantile=quantile, alpha=0).fit(X, a * y)
+    model2 = QuantileRegressor(quantile=quantile, **params).fit(X, a * y)
     assert model2.intercept_ == approx(a * model1.intercept_, rel=1e-5)
     assert_allclose(model2.coef_, a * model1.coef_, rtol=1e-5)
 
     # coef(1-q; -a*y, X) = -a * coef(q; y, X)
-    model2 = QuantileRegressor(quantile=1 - quantile, alpha=0).fit(X, -a * y)
+    model2 = QuantileRegressor(quantile=1 - quantile, **params).fit(X, -a * y)
     assert model2.intercept_ == approx(-a * model1.intercept_, rel=1e-5)
     assert_allclose(model2.coef_, -a * model1.coef_, rtol=1e-5)
 
     # coef(q; y + X @ g, X) = coef(q; y, X) + g
     g_intercept, g_coef = rng.randn(), rng.randn(n_features)
-    model2 = QuantileRegressor(quantile=quantile, alpha=0)
+    model2 = QuantileRegressor(quantile=quantile, **params)
     model2.fit(X, y + X @ g_coef + g_intercept)
     assert model2.intercept_ == approx(model1.intercept_ + g_intercept)
     assert_allclose(model2.coef_, model1.coef_ + g_coef, rtol=1e-6)
 
     # coef(q; y, X @ A) = A^-1 @ coef(q; y, X)
     A = rng.randn(n_features, n_features)
-    model2 = QuantileRegressor(quantile=quantile, alpha=0)
+    model2 = QuantileRegressor(quantile=quantile, **params)
     model2.fit(X @ A, y)
     assert model2.intercept_ == approx(model1.intercept_, rel=1e-5)
     assert_allclose(model2.coef_, np.linalg.solve(A, model1.coef_), rtol=1e-5)
