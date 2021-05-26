c99cc01c639e51a7c2c8ccaae99a126888c47f3a
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2021-05-26 10:32:02 +0200

    iter

--- examples/linear_model/plot_quantile_regression.py
+++ examples/linear_model/plot_quantile_regression.py
@@ -25,8 +25,9 @@ print(__doc__)
 # ------------------
 #
 # To illustrate the behaviour of quantile regression, we will generate two
-# synthetic datasets. The true generative random processess for both datasets will be
-# composed by an expected value with a linear relationship with a single feature `x`.
+# synthetic datasets. The true generative random processess for both datasets
+# will be composed by an expected value with a linear relationship with a
+# single feature `x`.
 import numpy as np
 
 rng = np.random.RandomState(42)
@@ -35,51 +36,54 @@ X = x[:, np.newaxis]
 y_true_mean = 10 + 0.5 * x
 
 # %%
-# We will create two subsequent problems by changing the distribution of the target `y`
-# while keeping the same expected value:
+# We will create two subsequent problems by changing the distribution of the
+# target `y` while keeping the same expected value:
 #
 # - in the first case, a heteroscedastic Normal noise is added;
 # - in the second case, an asymmetric Pareto noise is added.
-y_normal = y_true + rng.normal
+y_normal = y_true_mean + rng.normal(
     loc=0, scale=0.5 + 0.5 * x, size=x.shape[0]
 )
 a = 5
-y_pareto = y_true + 10 * (rng.pareto(a, size=x.shape[0]) - 1 / (a - 1))
+y_pareto = y_true_mean + 10 * (rng.pareto(a, size=x.shape[0]) - 1 / (a - 1))
 
 # %%
-# Let's first visualize the datasets as well as the distribution of the residuals `y - mean(y)`.
+# Let's first visualize the datasets as well as the distribution of the
+# residuals `y - mean(y)`.
 import matplotlib.pyplot as plt
 
 _, axs = plt.subplots(
-    nrows=2, ncols=2, figsize=(15, 10), sharex="col", sharey="col"
+    nrows=2, ncols=2, figsize=(15, 11), sharex="row", sharey="row"
 )
 
-axs[0, 0].plot(x, y_true, label="True generative process")
+axs[0, 0].plot(x, y_true_mean, label="True mean")
 axs[0, 0].scatter(
-    x, y_noisy_normal, color="black", alpha=0.5, label="Noisy observations"
+    x, y_normal, color="black", alpha=0.5, label="Observations"
 )
-axs[0, 1].hist(y_true - y_noisy_normal, edgecolor="black")
+axs[1, 0].hist(y_true_mean - y_normal, edgecolor="black")
 
 
-axs[1, 0].plot(x, y_true, label="True generative process")
-axs[1, 0].scatter(
-    x, y_noisy_pareto, color="black", alpha=0.5, label="Noisy observations"
+axs[0, 1].plot(x, y_true_mean, label="True mean")
+axs[0, 1].scatter(
+    x, y_pareto, color="black", alpha=0.5, label="Observations"
 )
-axs[1, 1].hist(y_true - y_noisy_pareto, edgecolor="black")
+axs[1, 1].hist(y_true_mean - y_pareto, edgecolor="black")
 
 axs[0, 0].set_title("Dataset with heteroscedastic Normal distributed targets")
-axs[1, 0].set_title("Dataset with asymmetric Pareto distributed target")
-axs[0, 1].set_title(
+axs[0, 1].set_title("Dataset with asymmetric Pareto distributed target")
+axs[1, 0].set_title(
     "Residuals distribution for heteroscedastic Normal distributed targets"
 )
 axs[1, 1].set_title(
     "Residuals distribution for asymmetric Pareto distributed target"
 )
 axs[0, 0].legend()
-axs[1, 0].legend()
+axs[0, 1].legend()
 axs[0, 0].set_ylabel("y")
-axs[1, 0].set_ylabel("y")
-axs[1, 0].set_xlabel("x")
+axs[1, 0].set_ylabel("Counts")
+axs[0, 1].set_xlabel("x")
+axs[0, 0].set_xlabel("x")
+axs[1, 0].set_xlabel("Residuals")
 _ = axs[1, 1].set_xlabel("Residuals")
 
 # %%
@@ -117,32 +121,32 @@ from sklearn.linear_model import QuantileRegressor
 
 quantiles = [0.05, 0.5, 0.95]
 predictions = {}
-out_bounds_predictions = np.zeros_like(y_true, dtype=np.bool_)
+out_bounds_predictions = np.zeros_like(y_true_mean, dtype=np.bool_)
 for quantile in quantiles:
     qr = QuantileRegressor(quantile=quantile, alpha=0)
-    y_pred = qr.fit(X, y_noisy_normal).predict(X)
+    y_pred = qr.fit(X, y_normal).predict(X)
     predictions[quantile] = y_pred
 
     if quantile == min(quantiles):
         out_bounds_predictions = np.logical_or(
-            out_bounds_predictions, y_pred >= y_noisy_normal
+            out_bounds_predictions, y_pred >= y_normal
         )
     elif quantile == max(quantiles):
         out_bounds_predictions = np.logical_or(
-            out_bounds_predictions, y_pred <= y_noisy_normal
+            out_bounds_predictions, y_pred <= y_normal
         )
 
 # %%
 # Now, we can plot the three linear models and the distinguished samples that
 # are within the 5%-95% quantiles from samples that are outside these bounds.
-plt.plot(X, y_true, color="black", linestyle="dashed", label="True mean")
+plt.plot(X, y_true_mean, color="black", linestyle="dashed", label="True mean")
 
 for quantile, y_pred in predictions.items():
     plt.plot(X, y_pred, label=f"Quantile: {quantile}")
 
 plt.scatter(
     x[out_bounds_predictions],
-    y_noisy_normal[out_bounds_predictions],
+    y_normal[out_bounds_predictions],
     color="black",
     marker="+",
     alpha=0.5,
@@ -150,7 +154,7 @@ plt.scatter(
 )
 plt.scatter(
     x[~out_bounds_predictions],
-    y_noisy_normal[~out_bounds_predictions],
+    y_normal[~out_bounds_predictions],
     color="black",
     alpha=0.5,
     label="Inside interval",
@@ -162,40 +166,41 @@ plt.ylabel("y")
 _ = plt.title("Quantiles of heteroscedastic Normal distributed target")
 
 # %%
-# Since the noise is still Normally distributed, in particular is symmetric, the true conditional mean
-# and the true conditional median coincide. Indeed, we see that the estimated median almost
-# hits the true mean. We observe the effect of having an
-# increasing noise variance on the 5% and 95% quantiles: the slopes of those quantiles is
-# very different and the interval between them becomes wider with increasing `x`.
+# Since the noise is still Normally distributed, in particular is symmetric,
+# the true conditional mean and the true conditional median coincide. Indeed,
+# we see that the estimated median almost hits the true mean. We observe the
+# effect of having an increasing noise variance on the 5% and 95% quantiles:
+# the slopes of those quantiles is very different and the interval between them
+# becomes wider with increasing `x`.
 #
 # We can repeat the same experiment using the asymmetric Pareto distributed
 # target.
 quantiles = [0.05, 0.5, 0.95]
 predictions = {}
-out_bounds_predictions = np.zeros_like(y_true, dtype=np.bool_)
+out_bounds_predictions = np.zeros_like(y_true_mean, dtype=np.bool_)
 for quantile in quantiles:
     qr = QuantileRegressor(quantile=quantile, alpha=0)
-    y_pred = qr.fit(X, y_noisy_pareto).predict(X)
+    y_pred = qr.fit(X, y_pareto).predict(X)
     predictions[quantile] = y_pred
 
     if quantile == min(quantiles):
         out_bounds_predictions = np.logical_or(
-            out_bounds_predictions, y_pred >= y_noisy_pareto
+            out_bounds_predictions, y_pred >= y_pareto
         )
     elif quantile == max(quantiles):
         out_bounds_predictions = np.logical_or(
-            out_bounds_predictions, y_pred <= y_noisy_pareto
+            out_bounds_predictions, y_pred <= y_pareto
         )
 
 # %%
-plt.plot(X, y_true, color="black", linestyle="dashed", label="True mean")
+plt.plot(X, y_true_mean, color="black", linestyle="dashed", label="True mean")
 
 for quantile, y_pred in predictions.items():
     plt.plot(X, y_pred, label=f"Quantile: {quantile}")
 
 plt.scatter(
     x[out_bounds_predictions],
-    y_noisy_pareto[out_bounds_predictions],
+    y_pareto[out_bounds_predictions],
     color="black",
     marker="+",
     alpha=0.5,
@@ -203,7 +208,7 @@ plt.scatter(
 )
 plt.scatter(
     x[~out_bounds_predictions],
-    y_noisy_pareto[~out_bounds_predictions],
+    y_pareto[~out_bounds_predictions],
     color="black",
     alpha=0.5,
     label="Inside interval",
@@ -241,7 +246,8 @@ _ = plt.title("Quantiles of asymmetric Pareto distributed target")
 #
 # Let's first compute the training errors of such models in terms of mean
 # squared error and mean absolute error. We will use the asymmetric Pareto
-# distributed target to make it more interesting as mean and median are not equal.
+# distributed target to make it more interesting as mean and median are not
+# equal.
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
@@ -249,17 +255,17 @@ from sklearn.metrics import mean_squared_error
 linear_regression = LinearRegression()
 quantile_regression = QuantileRegressor(quantile=0.5, alpha=0)
 
-y_pred_lr = linear_regression.fit(X, y_noisy_pareto).predict(X)
-y_pred_qr = quantile_regression.fit(X, y_noisy_pareto).predict(X)
+y_pred_lr = linear_regression.fit(X, y_pareto).predict(X)
+y_pred_qr = quantile_regression.fit(X, y_pareto).predict(X)
 
 print(
     f"""Training error (in-sample performance)
     {linear_regression.__class__.__name__}:
-    MAE = {mean_absolute_error(y_noisy_pareto, y_pred_lr):.3f}
-    MSE = {mean_squared_error(y_noisy_pareto, y_pred_lr):.3f}
+    MAE = {mean_absolute_error(y_pareto, y_pred_lr):.3f}
+    MSE = {mean_squared_error(y_pareto, y_pred_lr):.3f}
     {quantile_regression.__class__.__name__}:
-    MAE = {mean_absolute_error(y_noisy_pareto, y_pred_qr):.3f}
-    MSE = {mean_squared_error(y_noisy_pareto, y_pred_qr):.3f}
+    MAE = {mean_absolute_error(y_pareto, y_pred_qr):.3f}
+    MSE = {mean_squared_error(y_pareto, y_pred_qr):.3f}
     """
 )
 
@@ -280,14 +286,14 @@ from sklearn.model_selection import cross_validate
 cv_results_lr = cross_validate(
     linear_regression,
     X,
-    y_noisy_pareto,
+    y_pareto,
     cv=3,
     scoring=["neg_mean_absolute_error", "neg_mean_squared_error"],
 )
 cv_results_qr = cross_validate(
     quantile_regression,
     X,
-    y_noisy_pareto,
+    y_pareto,
     cv=3,
     scoring=["neg_mean_absolute_error", "neg_mean_squared_error"],
 )
