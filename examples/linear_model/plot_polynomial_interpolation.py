c476dfaa205a4ca564191174a1e90d4e01bf8d9e
Olivier Grisel <olivier.grisel@ensta.org>
2021-05-27 09:13:38 +0200

    Fix number of splines in legend of example plot

--- examples/linear_model/plot_polynomial_interpolation.py
+++ examples/linear_model/plot_polynomial_interpolation.py
@@ -128,7 +128,7 @@ axes[0].set_title("PolynomialFeatures")
 
 splt = SplineTransformer(n_knots=4, degree=3).fit(X_train)
 axes[1].plot(x_plot, splt.transform(X_plot))
-axes[1].legend(axes[1].lines, [f"spline {n}" for n in range(4)])
+axes[1].legend(axes[1].lines, [f"spline {n}" for n in range(6)])
 axes[1].set_title("SplineTransformer")
 
 # plot knots of spline
