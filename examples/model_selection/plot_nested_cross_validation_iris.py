2266c8c307f9a936a0747fdc9fabbf74b94d68ca
kobaski <cobacco@gmail.com>
2021-05-27 22:18:08 +0900

    Update examples/model_selection/plot_nested_cross_validation_iris.py
    make it more explict
    
    Co-authored-by: Olivier Grisel <olivier.grisel@ensta.org>

--- examples/model_selection/plot_nested_cross_validation_iris.py
+++ examples/model_selection/plot_nested_cross_validation_iris.py
@@ -85,7 +85,7 @@ for i in range(NUM_TRIALS):
     non_nested_scores[i] = clf.best_score_
 
     # Nested CV with parameter optimization
-    clf.set_params(cv=inner_cv)
+    clf = GridSearchCV(estimator=svm, param_grid=p_grid, cv=inner_cv)
     nested_score = cross_val_score(clf, X=X_iris, y=y_iris, cv=outer_cv)
     nested_scores[i] = nested_score.mean()
 
