71ee583f36fe4d56ffed3e2d8a360a91122f1017
takoika <>
2021-05-28 10:36:25 +0900

    Add model to be tested and add args to avoid warnings

--- sklearn/linear_model/tests/test_least_angle.py
+++ sklearn/linear_model/tests/test_least_angle.py
@@ -14,7 +14,7 @@ from sklearn.exceptions import ConvergenceWarning
 from sklearn import linear_model, datasets
 from sklearn.linear_model._least_angle import _lars_path_residues
 from sklearn.linear_model import LassoLarsIC, lars_path
-from sklearn.linear_model import Lars, LassoLars
+from sklearn.linear_model import Lars, LassoLars, LarsCV, LassoLarsCV
 
 # TODO: use another dataset that has multiple drops
 diabetes = datasets.load_diabetes()
@@ -779,19 +779,23 @@ def test_copy_X_with_auto_gram():
     assert_allclose(X, X_before)
 
 
-@pytest.mark.parametrize("LARS, has_coef_path", ((Lars, True),
-                                                 (LassoLars, True),
-                                                 (LassoLarsIC, False)))
+@pytest.mark.parametrize("LARS, has_coef_path, args",
+                         ((Lars, True, {}),
+                          (LassoLars, True, {}),
+                          (LassoLarsIC, False, {}),
+                          (LarsCV, True, {}),
+                          # max_iter=5 is for avoiding ConvergenceWarning
+                          (LassoLarsCV, True, {"max_iter": 5})))
 @pytest.mark.parametrize("x_data_type, y_data_type, expected_type", (
     (np.float32, np.float32, np.float32),
     (np.float64, np.float64, np.float64)))
-def test_lars_dtype_match(LARS, has_coef_path,
+def test_lars_dtype_match(LARS, has_coef_path, args,
                           x_data_type, y_data_type, expected_type):
     rng = np.random.RandomState(0)
     X = rng.rand(6, 6).astype(x_data_type)
     y = rng.rand(6).astype(y_data_type)
 
-    model = LARS()
+    model = LARS(**args)
     model.fit(X, y)
     assert model.coef_.dtype == expected_type
     if has_coef_path:
@@ -799,16 +803,21 @@ def test_lars_dtype_match(LARS, has_coef_path,
     assert model.intercept_.dtype == expected_type
 
 
-@pytest.mark.parametrize("LARS, has_coef_path", ((Lars, True),
-                                                 (LassoLars, True),
-                                                 (LassoLarsIC, False)))
-def test_lars_numeric_consistency(LARS, has_coef_path):
+@pytest.mark.parametrize("LARS, has_coef_path, args",
+                         ((Lars, True, {}),
+                          (LassoLars, True, {}),
+                          (LassoLarsIC, False, {}),
+                          (LarsCV, True, {}),
+                          # max_iter=5 is for avoiding ConvergenceWarning
+                          (LassoLarsCV, True, {"max_iter": 5})))
+def test_lars_numeric_consistency(LARS, has_coef_path, args):
     rng = np.random.RandomState(0)
     X_64 = rng.rand(6, 6)
     y_64 = rng.rand(6)
 
-    model_64 = LARS().fit(X_64, y_64)
-    model_32 = LARS().fit(X_64.astype(np.float32), y_64.astype(np.float32))
+    model_64 = LARS(**args).fit(X_64, y_64)
+    model_32 = LARS(**args).fit(X_64.astype(np.float32),
+                                y_64.astype(np.float32))
 
     assert_array_almost_equal(model_64.coef_, model_32.coef_)
     if has_coef_path:
