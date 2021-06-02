4d3c560d58c5ada4ed4264a872a9d1a40b3d4fe3
murata.yu <murata.yu@jp.fujitsu.com>
2021-06-02 04:43:09 +0000

    fix lint in docstring

--- sklearn/feature_selection/_sequential.py
+++ sklearn/feature_selection/_sequential.py
@@ -37,10 +37,10 @@ class SequentialFeatureSelector(SelectorMixin, MetaEstimatorMixin,
         it is the fraction of features to select.
 
     tol : float, default=0.0
-        If the score is not incremented by at least `tol` between two consecutive
-        feature addition or removal, stop adding or removing even if
-        `n_features_to_select` has not been reached. `tol` is enabled only when
-        `n_features_to_select` is `'auto'`
+        If the score is not incremented by at least `tol` between two
+        consecutive feature addition or removal, stop adding or removing even
+        if `n_features_to_select` has not been reached. `tol` is enabled only
+        when `n_features_to_select` is `'auto'`.
 
     direction : {'forward', 'backward'}, default='forward'
         Whether to perform forward selection or backward selection.
