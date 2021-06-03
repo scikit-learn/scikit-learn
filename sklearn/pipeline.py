7b4aa7bde5c8fa69307be40a156287275f489c70
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2021-06-03 13:10:48 +0200

    typo

--- sklearn/pipeline.py
+++ sklearn/pipeline.py
@@ -836,7 +836,7 @@ class FeatureUnion(TransformerMixin, _BaseComposition):
     ----------
     n_features_in_ : int
         Number of features seen during :term:`fit`. Only defined if the
-        underlying the first trasnsformer in `transformer_list` exposes such an
+        underlying the first transformer in `transformer_list` exposes such an
         attribute when fit.
         .. versionadded:: 0.24
 
