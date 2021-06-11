8a468d1668cd665e3fdda1aed751776ad47f0e0d
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2021-06-11 16:00:30 +0200

    fix

--- examples/gaussian_process/plot_gpr_co2.py
+++ examples/gaussian_process/plot_gpr_co2.py
@@ -224,8 +224,8 @@ gaussian_process.kernel_
 # Thus, most of the target signal, with the mean substracted, is explained by a
 # long-term rising trend for ~45 ppm and a length-scale of ~52 years. The
 # periodic component has an amplitude of ~2.6ppm, a decay time of ~90 years and
-# a length-scale of ~1.5. The long decay time indicates that we have a locally
-# very close to periodic seasonal component. The correlated noise has an
+# a length-scale of ~1.5. The long decay time indicates that we have a
+# component very close to a seasonal periodicity. The correlated noise has an
 # amplitude of ~0.2 ppm with a length scale of ~0.12 years and a white-noise
 # contribution of ~0.04 ppm. Thus, the overall noise level is very small,
 # indicating that the data can be very well explained by the model.
