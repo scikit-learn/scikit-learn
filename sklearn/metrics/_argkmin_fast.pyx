13442b618886772116e61e780d8f0b1e6eabd92b
Julien Jerphanion <git@jjerphan.xyz>
2021-06-14 13:40:40 +0200

    Add minimal documentation
    Also perform some renaming.

--- sklearn/metrics/_argkmin_fast.pyx
+++ sklearn/metrics/_argkmin_fast.pyx
@@ -50,6 +50,7 @@ cpdef int _openmp_effective_n_threads(n_threads=None):
 
 ### Heaps utilities, minified from sklearn internals NeighborsHeap
 # https://github.com/scikit-learn/scikit-learn/blob/e4bb9fa86b0df873ad750b6d59090843d9d23d50/sklearn/neighbors/_binary_tree.pxi#L513
+# TODO: factor those utilities
 
 cdef int _push(
     floating* dist,
@@ -57,7 +58,7 @@ cdef int _push(
     integral size,
     floating val,
     integral i_val,
-) nogil except -1:
+) nogil:
     """push (val, i_val) into the heap (dist, idx) of the given size"""
     cdef:
         integral current_idx, left_child_idx, right_child_idx, swap_idx
@@ -127,7 +128,7 @@ cdef int _simultaneous_sort(
     floating* dist,
     integral* idx,
     integral size
-) nogil except -1:
+) nogil:
     """
     Perform a recursive quicksort on the dist array, simultaneously
     performing the same swaps on the idx array.
@@ -187,9 +188,11 @@ cdef int _simultaneous_sort(
                                size - pivot_idx - 1)
     return 0
 
+### End: Heaps utilities
+
 ### argkmin helpers
 
-cdef void _k_argkmin_on_chunk(
+cdef void _argkmin_on_chunk(
     floating[:, ::1] X_c,                  # IN
     floating[:, ::1] Y_c,                  # IN
     floating[::1] Y_sq_norms,              # IN
@@ -200,6 +203,12 @@ cdef void _k_argkmin_on_chunk(
     # ID of the first element of Y_c
     integral Y_idx_offset,
 ) nogil:
+    """
+    Critical part of the computation of pairwise distances.
+
+    "Fast Squared Euclidean" distances strategy relying
+    on the gemm-trick.
+    """
     cdef:
         integral i, j
     # Instead of computing the full pairwise squared distances matrix,
@@ -234,16 +243,19 @@ cdef void _k_argkmin_on_chunk(
 
 
 cdef int _argkmin_on_X(
-    floating[:, ::1] X,                   # IN
-    floating[:, ::1] Y,                   # IN
-    floating[::1] Y_sq_norms,             # IN
-    integral chunk_size,                  # IN
-    integral effective_n_threads,         # IN
-    integral[:, ::1] knn_indices,         # OUT
-    floating[:, ::1] knn_red_distances,   # OUT
-) nogil except -1:
+    floating[:, ::1] X,                       # IN
+    floating[:, ::1] Y,                       # IN
+    floating[::1] Y_sq_norms,                 # IN
+    integral chunk_size,                      # IN
+    integral effective_n_threads,             # IN
+    integral[:, ::1] argkmin_indices,         # OUT
+    floating[:, ::1] argkmin_red_distances,   # OUT
+) nogil:
+    """Computes the argkmin of each vector (row) of X on Y
+    by parallelising computation on chunks of X.
+    """
     cdef:
-        integral k = knn_indices.shape[1]
+        integral k = argkmin_indices.shape[1]
         integral d = X.shape[1]
         integral sf = sizeof(floating)
         integral si = sizeof(integral)
@@ -302,23 +314,22 @@ cdef int _argkmin_on_X(
                 else:
                     Y_end = Y_start + Y_n_samples_chunk
 
-                _k_argkmin_on_chunk(
+                _argkmin_on_chunk(
                     X[X_start:X_end, :],
                     Y[Y_start:Y_end, :],
                     Y_sq_norms[Y_start:Y_end],
                     dist_middle_terms_chunks,
                     heaps_red_distances_chunks,
-                    &knn_indices[X_start, 0],
+                    &argkmin_indices[X_start, 0],
                     k,
                     Y_start
                 )
 
-            # Getting the indices of the k-argkmin points in
-            # the sorted order
+            # Sorting indices so that the closests' come first.
             for idx in range(X_end - X_start):
                 _simultaneous_sort(
                     heaps_red_distances_chunks + idx * k,
-                    &knn_indices[X_start + idx, 0],
+                    &argkmin_indices[X_start + idx, 0],
                     k
                 )
 
@@ -331,16 +342,23 @@ cdef int _argkmin_on_X(
 
 
 cdef int _argkmin_on_Y(
-    floating[:, ::1] X,                   # IN
-    floating[:, ::1] Y,                   # IN
-    floating[::1] Y_sq_norms,             # IN
-    integral chunk_size,                  # IN
-    integral effective_n_threads,         # IN
-    integral[:, ::1] knn_indices,         # OUT
-    floating[:, ::1] knn_red_distances,   # OUT
-) nogil except -1:
+    floating[:, ::1] X,                       # IN
+    floating[:, ::1] Y,                       # IN
+    floating[::1] Y_sq_norms,                 # IN
+    integral chunk_size,                      # IN
+    integral effective_n_threads,             # IN
+    integral[:, ::1] argkmin_indices,         # OUT
+    floating[:, ::1] argkmin_red_distances,   # OUT
+) nogil:
+    """Computes the argkmin of each vector (row) of X on Y
+    by parallelising computation on chunks of Y.
+
+    This parallelisation strategy is more costly (as we need
+    extra heaps and synchronisation), yet it is useful in
+    most contexts.
+    """
     cdef:
-        integral k = knn_indices.shape[1]
+        integral k = argkmin_indices.shape[1]
         integral d = X.shape[1]
         integral sf = sizeof(floating)
         integral si = sizeof(integral)
@@ -372,6 +390,10 @@ cdef int _argkmin_on_Y(
 
         floating *dist_middle_terms_chunks
         floating *heaps_red_distances_chunks
+
+        # As chunks of X are shared across threads, so must their
+        # heaps. To solve this, each thread has its own locals
+        # heaps which are then synchronised back in the main ones. 
         integral *heaps_indices_chunks
 
     for X_chunk_idx in range(X_n_chunks):
@@ -392,7 +414,7 @@ cdef int _argkmin_on_Y(
             heaps_indices_chunks = <integral*> malloc(
                 X_n_samples_chunk * k * sf)
 
-            # Initialising heep (memset isn't suitable here)
+            # Initialising heaps (memset can't be used here)
             for idx in range(X_n_samples_chunk * k):
                 heaps_red_distances_chunks[idx] = FLOAT_INF
                 heaps_indices_chunks[idx] = -1
@@ -405,7 +427,7 @@ cdef int _argkmin_on_Y(
                 else:
                     Y_end = Y_start + Y_n_samples_chunk
 
-                _k_argkmin_on_chunk(
+                _argkmin_on_chunk(
                     X[X_start:X_end, :],
                     Y[Y_start:Y_end, :],
                     Y_sq_norms[Y_start:Y_end],
@@ -418,12 +440,13 @@ cdef int _argkmin_on_Y(
 
             # end: for Y_chunk_idx
             with gil:
-                # Synchronising with the main heaps
+                # Synchronising the thread local heaps
+                # with the main heaps
                 for idx in range(X_end - X_start):
                     for jdx in range(k):
                         _push(
-                            &knn_red_distances[X_start + idx, 0],
-                            &knn_indices[X_start + idx, 0],
+                            &argkmin_red_distances[X_start + idx, 0],
+                            &argkmin_indices[X_start + idx, 0],
                             k,
                             heaps_red_distances_chunks[idx * k + jdx],
                             heaps_indices_chunks[idx * k + jdx],
@@ -434,16 +457,16 @@ cdef int _argkmin_on_Y(
             free(heaps_indices_chunks)
 
         # end: with nogil, parallel
-        # Sortting indices of the k-nn for each query vector of X
+        # Sorting indices of the argkmin for each query vector of X
         for idx in prange(n_test,schedule='static',
                           nogil=True, num_threads=num_threads):
             _simultaneous_sort(
-                &knn_red_distances[idx, 0],
-                &knn_indices[idx, 0],
+                &argkmin_red_distances[idx, 0],
+                &argkmin_indices[idx, 0],
                 k,
             )
-
-        # end: with nogil, parallel
+        # end: prange
+    
     # end: for X_chunk_idx
     return Y_n_chunks
 
@@ -458,7 +481,7 @@ cdef inline floating _euclidean_dist(
         integral k
         integral upper_unrolled_idx = (X.shape[1] // 4) * 4
 
-    # Unrolling loop to potentially help with vectorisation?
+    # Unrolling loop to help with vectorisation
     for k in range(0, upper_unrolled_idx, 4):
         dist += (X[i, k] - Y[j, k]) * (X[i, k] - Y[j, k])
         dist += (X[i, k + 1] - Y[j, k + 1]) * (X[i, k + 1] - Y[j, k + 1])
@@ -470,21 +493,31 @@ cdef inline floating _euclidean_dist(
 
     return sqrt(dist)
 
-cdef int _compute_exact_distances(
+cdef int _exact_euclidean_dist(
     floating[:, ::1] X,                  # IN
     floating[:, ::1] Y,                  # IN
-    integral[:, ::1] knn_indices,        # IN
+    integral[:, ::1] Y_indices,          # IN
     integral effective_n_threads,        # IN
-    floating[:, ::1] knn_distances,      # OUT
-) nogil except -1:
+    floating[:, ::1] distances,          # OUT
+) nogil:
+    """
+    Compute exact pairwise euclidean distances in parallel.
+
+    The pairwise distances considered are X vectors
+    and a subset of Y given for each row if X given in
+    Y_indices.
+
+    Notes: the body of this function could have been inlined,
+    but we use a function to have a cdef nogil context.
+    """
     cdef:
         integral i, k
 
     for i in prange(X.shape[0], schedule='static',
                     nogil=True, num_threads=effective_n_threads):
-        for k in range(knn_indices.shape[1]):
-            knn_distances[i, k] = _euclidean_dist(X, Y,
-                                                  i, knn_indices[i, k])
+        for k in range(Y_indices.shape[1]):
+            distances[i, k] = _euclidean_dist(X, Y, i,
+                                              Y_indices[i, k])
 
 
 # Python interface
@@ -497,44 +530,90 @@ def _argkmin(
     str strategy = "auto",
     bint return_distance = False,
 ):
+    """Computes the argkmin of vectors (rows) of X on Y for 
+    the euclidean distance.
+    
+    The implementation is parallelised on chunks whose size can
+    be set using ``chunk_size``.
+
+    Parameters
+    ----------
+    X: ndarray of shape (n, d)
+        Rows represent vectors
+
+    Y: ndarray of shape (m, d)  
+        Rows represent vectors
+
+    chunk_size: int
+        The number of vectors per chunk.
+
+    strategy: str, {'auto', 'chunk_on_X', 'chunk_on_Y'}
+        The chunking strategy defining which dataset
+        parallelisation are made on.
+
+         - 'chunk_on_X' is embarassingly parallel but 
+        is less used in practice.
+         - 'chunk_on_Y' comes with synchronisation but
+        is more useful in practice.
+         -'auto' relies on a simple heuristic to choose
+        between 'chunk_on_X' and 'chunk_on_Y'.
+
+    return_distance: boolean
+        Return distances between each X vectory and its
+        argkmin if set to True.
+
+    Returns
+    -------
+    distances: ndarray of shape (n, k)
+        Distances between each X vector and its argkmin
+        in Y. Only returned if ``return_distance=True``.
+
+    indices: ndarray of shape (n, k)
+        Indices of each X vector argkmin in Y.
+    """
     int_dtype = np.intp
     float_dtype = np.float32 if floating is float else np.float64
     cdef:
-        integral[:, ::1] knn_indices = np.full((X.shape[0], k), 0,
+        integral[:, ::1] argkmin_indices = np.full((X.shape[0], k), 0,
                                                dtype=int_dtype)
-        floating[:, ::1] knn_distances = np.full((X.shape[0], k),
+        floating[:, ::1] argkmin_distances = np.full((X.shape[0], k),
                                                   FLOAT_INF,
                                                   dtype=float_dtype)
         floating[::1] Y_sq_norms = np.einsum('ij,ij->i', Y, Y)
         integral effective_n_threads = _openmp_effective_n_threads()
 
     if strategy == 'auto':
+        # This is a simple heuristic whose constant for the
+        # comparison has been chosen based on experiments.
         if 4 * chunk_size * effective_n_threads < X.shape[0]:
             strategy = 'chunk_on_X'
         else:
             strategy = 'chunk_on_Y'
 
     if strategy == 'chunk_on_Y':
-        n_parallel_chunks = _argkmin_on_Y(
+        _argkmin_on_Y(
             X, Y, Y_sq_norms,
             chunk_size, effective_n_threads,
-            knn_indices, knn_distances
+            argkmin_indices, argkmin_distances
         )
     elif strategy == 'chunk_on_X':
-        n_parallel_chunks = _argkmin_on_X(
+        _argkmin_on_X(
             X, Y, Y_sq_norms,
             chunk_size, effective_n_threads,
-            knn_indices, knn_distances
+            argkmin_indices, argkmin_distances
         )
     else:
         raise RuntimeError(f"strategy '{strategy}' not supported.")
 
     if return_distance:
-        # We need to recompute distances because we relied on reduced distances
-        # using _gemm, which are missing a term for squarred norms and which are
-        # not the most precise (catastrophic cancellation might have happened).
-        _compute_exact_distances(X, Y, knn_indices,
-                                 effective_n_threads, knn_distances)
-        return (np.asarray(knn_distances), np.asarray(knn_indices)), n_parallel_chunks
+        # We need to recompute distances because we relied on 
+        # reduced distances using _gemm, which are missing a
+        # term for squarred norms and which are not the most 
+        # precise (catastrophic cancellation might have happened).
+        _exact_euclidean_dist(X, Y, argkmin_indices, 
+                              effective_n_threads,
+                              argkmin_distances)
+        return (np.asarray(argkmin_distances), 
+                np.asarray(argkmin_indices))
 
-    return np.asarray(knn_indices), n_parallel_chunks
+    return np.asarray(argkmin_indices)
