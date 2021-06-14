4348971578d7eb1026f91ca1f6ad6f141befd939
Christian Lorentzen <lorentzen.ch@gmail.com>
2021-06-14 20:20:39 +0200

    address some review comments

--- sklearn/preprocessing/tests/test_polynomial.py
+++ sklearn/preprocessing/tests/test_polynomial.py
@@ -470,7 +470,7 @@ def test_polynomial_features_input_validation(params, err_msg):
 
 
 @pytest.fixture()
-def single_feature_degree2():
+def single_feature_degree3():
     X = np.arange(6)[:, np.newaxis]
     P = np.hstack([np.ones_like(X), X, X ** 2, X ** 3])
     return X, P
@@ -494,7 +494,7 @@ def single_feature_degree2():
     [False, sparse.csr_matrix, sparse.csc_matrix],
 )
 def test_polynomial_features_one_feature(
-    single_feature_degree2,
+    single_feature_degree3,
     degree,
     include_bias,
     interaction_only,
@@ -502,7 +502,7 @@ def test_polynomial_features_one_feature(
     sparse_X,
 ):
     """Test PolynomialFeatures on single feature up to degree 3."""
-    X, P = single_feature_degree2
+    X, P = single_feature_degree3
     if sparse_X:
         X = sparse_X(X)
     tf = PolynomialFeatures(
@@ -597,6 +597,7 @@ def test_polynomial_feature_names():
     assert_array_equal(['1', 'x0', 'x1', 'x2', 'x0^2', 'x0 x1',
                         'x0 x2', 'x1^2', 'x1 x2', 'x2^2'],
                        feature_names)
+    assert len(feature_names) == poly.transform(X).shape[1]
 
     poly = PolynomialFeatures(degree=3, include_bias=False).fit(X)
     feature_names = poly.get_feature_names(["a", "b", "c"])
@@ -604,6 +605,7 @@ def test_polynomial_feature_names():
                         'b c', 'c^2', 'a^3', 'a^2 b', 'a^2 c',
                         'a b^2', 'a b c', 'a c^2', 'b^3', 'b^2 c',
                         'b c^2', 'c^3'], feature_names)
+    assert len(feature_names) == poly.transform(X).shape[1]
 
     poly = PolynomialFeatures(degree=(2, 3), include_bias=False).fit(X)
     feature_names = poly.get_feature_names(["a", "b", "c"])
@@ -614,11 +616,13 @@ def test_polynomial_feature_names():
         ],
         feature_names
     )
+    assert len(feature_names) == poly.transform(X).shape[1]
 
     poly = PolynomialFeatures(
         degree=(3, 3), include_bias=True, interaction_only=True).fit(X)
     feature_names = poly.get_feature_names(["a", "b", "c"])
     assert_array_equal(['1', 'a b c'], feature_names)
+    assert len(feature_names) == poly.transform(X).shape[1]
 
     # test some unicode
     poly = PolynomialFeatures(degree=1, include_bias=True).fit(X)
