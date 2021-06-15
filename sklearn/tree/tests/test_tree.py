0913ca5f78a8f80c77c5889f31430fb9246a2f80
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2021-06-15 18:54:55 +0200

    deprecate n_features_ in trees

--- sklearn/tree/tests/test_tree.py
+++ sklearn/tree/tests/test_tree.py
@@ -2075,3 +2075,14 @@ def test_criterion_deprecated(Tree, old_criterion, new_criterion):
 
     tree_new = Tree(criterion=new_criterion).fit(X, y)
     assert_allclose(tree.predict(X), tree_new.predict(X))
+
+
+@pytest.mark.parametrize("Tree", ALL_TREES.values())
+def test_n_features_deprecated(Tree):
+    # check that we raise a deprecation warning when accessing `n_features_`.
+    # FIXME: remove in 1.2
+    depr_msg = ("The attribute 'n_features_' is deprecated in 1.0 and will be "
+                "removed in 1.2. Use 'n_features_in_' instead.")
+
+    with pytest.warns(FutureWarning, match=depr_msg):
+        Tree().fit(X, y).n_features_
