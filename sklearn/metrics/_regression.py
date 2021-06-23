881b541ad0de4cfcd1a50797a9d82a653a27f53d
Uttam kumar <72701081+helper-uttam@users.noreply.github.com>
2021-06-23 22:30:12 +0530

    Update sklearn/metrics/_regression.py
    Co-authored-by: Olivier Grisel <olivier.grisel@ensta.org>

--- sklearn/metrics/_regression.py
+++ sklearn/metrics/_regression.py
@@ -469,8 +469,8 @@ def mean_squared_log_error(
             Errors of all outputs are averaged with uniform weight.
                         
     squared : bool, default=True
-        If True returns MSLE (mean squared log error) value. 
-        If False returns RMSLE (root mean squared log error) value. 
+        If True returns MSLE (mean squared log error) value.
+        If False returns RMSLE (root mean squared log error) value.
 
     Returns
     -------
