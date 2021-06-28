b497a1a61fb0a63f4b1a4ec9b8755e730257f467
Hugo Defois <hugo.defois@symphonyretailai.com>
2021-06-28 14:54:37 +0200

    add array checks to ledoit_wolf covariances

--- sklearn/covariance/_shrunk_covariance.py
+++ sklearn/covariance/_shrunk_covariance.py
@@ -198,6 +198,7 @@ def ledoit_wolf_shrinkage(X, assume_centered=False, block_size=1000):
     where mu = trace(cov) / n_features
     """
     X = np.asarray(X)
+    X = check_array(X, accept_sparse=True)
     # for only one feature, the result is the same whatever the shrinkage
     if len(X.shape) == 2 and X.shape[1] == 1:
         return 0.0
@@ -297,6 +298,7 @@ def ledoit_wolf(X, *, assume_centered=False, block_size=1000):
     where mu = trace(cov) / n_features
     """
     X = np.asarray(X)
+    X = check_array(X, accept_sparse=True)
     # for only one feature, the result is the same whatever the shrinkage
     if len(X.shape) == 2 and X.shape[1] == 1:
         if not assume_centered:
