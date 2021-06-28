0aa73d04c6928d2b55070ddf4e653ab861e7d04a
Joel Nothman <joel.nothman@gmail.com>
2021-06-28 11:31:53 +1000

    TST

--- sklearn/utils/metaestimators.py
+++ sklearn/utils/metaestimators.py
@@ -128,6 +128,29 @@ def available_if(check):
         When passed the object with the decorated method, this should return
         a truthy value if the attribute is available, and either return False
         or raise an AttributeError if not available.
+
+    Examples
+    --------
+    >>> from sklearn.utils.metaestimators import available_if
+    >>> class HelloIfEven:
+    ...    def __init__(self, x):
+    ...        self.x = x
+    ...
+    ...    def _x_is_even(self):
+    ...        return self.x % 2 == 0
+    ...
+    ...    @available_if(_x_is_even)
+    ...    def say_hello(self):
+    ...        print("Hello")
+    ...
+    >>> obj = HelloIfEven(1)
+    >>> hasattr(obj, "say_hello")
+    False
+    >>> obj.x = 2
+    >>> hasattr(obj, "say_hello")
+    True
+    >>> obj.say_hello()
+    Hello
     """
     return lambda fn: _AvailableIfDescriptor(fn, check, attribute_name=fn.__name__)
 
