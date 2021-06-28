b0b397e324bc72b2dd44b89c096ca29d9f224969
Loïc Estève <loic.esteve@ymail.com>
2021-06-28 10:14:19 +0200

    [scipy-dev] add pytest issue number.

--- sklearn/conftest.py
+++ sklearn/conftest.py
@@ -160,7 +160,9 @@ def pytest_collection_modifyitems(config, items):
         for item in items:
             if isinstance(item, DoctestItem):
                 # work-around an internal error with pytest if adding a skip
-                # mark to a doctest in a contextmanager
+                # mark to a doctest in a contextmanager, see
+                # https://github.com/pytest-dev/pytest/issues/8796 for more
+                # details.
                 if item.name != "sklearn._config.config_context":
                     item.add_marker(skip_marker)
     elif not _pilutil.pillow_installed:
