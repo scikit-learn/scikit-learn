dd93c5198efb6f6e00abe51fcbfe14abf9c0a8a5
Paweł Olszewski <olspaw@gmail.com>
2021-07-02 17:45:53 +0200

    Add comment in corrected_std
    Explain the kr variable

--- examples/model_selection/plot_grid_search_stats.py
+++ examples/model_selection/plot_grid_search_stats.py
@@ -190,7 +190,7 @@ def corrected_std(differences, n_train, n_test):
     corrected_std : int
         Variance-corrected standard deviation of the set of differences.
     """
-    kr = len(differences)
+    kr = len(differences)  # k times r, r times repeated k-fold crossvalidation
     corrected_var = (
         np.var(differences, ddof=1) * (1 / kr + n_test / n_train)
     )
