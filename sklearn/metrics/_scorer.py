2f53838ff60a57365d323f7e501cd7ebbb9e8fba
adrinjalali <adrin.jalali@gmail.com>
2021-07-09 14:24:25 +0200

    MNT cleaup a try...except in scorer

--- sklearn/metrics/_scorer.py
+++ sklearn/metrics/_scorer.py
@@ -527,12 +527,9 @@ def _check_multimetric_scoring(estimator, scoring):
         err_msg = (
             "The list/tuple elements must be unique strings of predefined scorers. "
         )
-        invalid = False
         try:
             keys = set(scoring)
         except TypeError:
-            invalid = True
-        if invalid:
             raise ValueError(err_msg)
 
         if len(keys) != len(scoring):
