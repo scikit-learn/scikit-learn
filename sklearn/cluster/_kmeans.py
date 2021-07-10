d3b101999365c89c68ef5204facffc1e8510505e
Alberto Ceballos-Arroyo <alceballosa@unal.edu.co>
2021-07-09 21:13:10 -0500

    Additional black linting

--- sklearn/cluster/_kmeans.py
+++ sklearn/cluster/_kmeans.py
@@ -84,7 +84,7 @@ def kmeans_plusplus(
     indices : ndarray of shape (n_clusters,)
         The index location of the chosen centers in the data array X. For a
         given index and center, X[index] = center.
-        
+
     Notes
     -----
     Selects initial cluster centers for k-mean clustering in a smart way
