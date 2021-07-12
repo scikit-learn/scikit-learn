6330fbfd3f00465aa20fe6b7ea9c6e8eb043023b
Mitzi <46392144+embandera@users.noreply.github.com>
2021-07-12 13:23:24 -0700

    Remove IsotonicRegression from docstrings_ignore_list

--- maint_tools/test_docstrings.py
+++ maint_tools/test_docstrings.py
@@ -55,7 +55,6 @@ DOCSTRING_IGNORE_LIST = [
     "HuberRegressor",
     "IncrementalPCA",
     "Isomap",
-    "IsotonicRegression",
     "IterativeImputer",
     "KBinsDiscretizer",
     "KNNImputer",
