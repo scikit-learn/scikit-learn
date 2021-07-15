d8914ace145aa1c2a4f7fd48303734ca3453cce2
reshamas <reshama.stat@gmail.com>
2021-07-15 14:50:27 -0400

    commented out AdditiveChi2Sampler

--- maint_tools/test_docstrings.py
+++ maint_tools/test_docstrings.py
@@ -9,7 +9,7 @@ numpydoc_validation = pytest.importorskip("numpydoc.validate")
 
 # List of modules ignored when checking for numpydoc validation.
 DOCSTRING_IGNORE_LIST = [
-    "AdditiveChi2Sampler",
+    #"AdditiveChi2Sampler",
     "AffinityPropagation",
     "AgglomerativeClustering",
     "Birch",
