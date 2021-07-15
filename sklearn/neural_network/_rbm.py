ba8df84b06eef2c8a37931ac53497b09002ff791
Reshama Shaikh <reshama.stat@gmail.com>
2021-07-15 09:27:00 -0400

    Update sklearn/neural_network/_rbm.py
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- sklearn/neural_network/_rbm.py
+++ sklearn/neural_network/_rbm.py
@@ -78,7 +78,7 @@ class BernoulliRBM(TransformerMixin, BaseEstimator):
 
     components_ : array-like of shape (n_components, n_features)
         Weight matrix, where `n_features` is the number of
-        visible units and n_components is the number of hidden units.
+        visible units and `n_components` is the number of hidden units.
 
     h_samples_ : array-like of shape (batch_size, n_components)
         Hidden Activation sampled from the model distribution,
