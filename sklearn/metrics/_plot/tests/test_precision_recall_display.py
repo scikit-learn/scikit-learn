abfdb1a6c0ef58aa80d17d9d87e23df079cb205b
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2021-07-19 16:01:51 +0200

    TST check strings

--- sklearn/metrics/_plot/tests/test_precision_recall_display.py
+++ sklearn/metrics/_plot/tests/test_precision_recall_display.py
@@ -3,7 +3,7 @@ import pytest
 
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.compose import make_column_transformer
-from sklearn.datasets import make_classification
+from sklearn.datasets import load_breast_cancer, make_classification
 from sklearn.exceptions import NotFittedError
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import average_precision_score, precision_recall_curve
@@ -219,3 +219,30 @@ def test_precision_recall_display_pipeline(pyplot, clf):
     clf.fit(X, y)
     display = PrecisionRecallDisplay.from_estimator(clf, X, y)
     assert display.estimator_name == clf.__class__.__name__
+
+
+def test_precision_recall_display_string_labels(pyplot):
+    # regression test #15738
+    cancer = load_breast_cancer()
+    X, y = cancer.data, cancer.target_names[cancer.target]
+
+    lr = make_pipeline(StandardScaler(), LogisticRegression())
+    lr.fit(X, y)
+    for klass in cancer.target_names:
+        assert klass in lr.classes_
+    display = PrecisionRecallDisplay.from_estimator(lr, X, y)
+
+    y_pred = lr.predict_proba(X)[:, 1]
+    avg_prec = average_precision_score(y, y_pred, pos_label=lr.classes_[1])
+
+    assert display.average_precision == pytest.approx(avg_prec)
+    assert display.estimator_name == lr.__class__.__name__
+
+    err_msg = r"y_true takes value in {'benign', 'malignant'}"
+    with pytest.raises(ValueError, match=err_msg):
+        PrecisionRecallDisplay.from_predictions(y, y_pred)
+
+    display = PrecisionRecallDisplay.from_predictions(
+        y, y_pred, pos_label=lr.classes_[1]
+    )
+    assert display.average_precision == pytest.approx(avg_prec)
