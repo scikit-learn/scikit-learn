5c0563277ac06077edc2eb17e80e6679b044f17e
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2021-07-19 23:49:30 +0200

    iter

--- sklearn/metrics/_plot/tests/test_plot_roc_curve.py
+++ sklearn/metrics/_plot/tests/test_plot_roc_curve.py
@@ -19,7 +19,8 @@ from sklearn.compose import make_column_transformer
 # TODO: Remove when https://github.com/numpy/numpy/issues/14397 is resolved
 pytestmark = pytest.mark.filterwarnings(
     "ignore:In future, it will be an error for 'np.bool_':DeprecationWarning:"
-    "matplotlib.*"
+    "matplotlib.*",
+    "ignore:Function plot_roc_curve is deprecated",
 )
 
 
