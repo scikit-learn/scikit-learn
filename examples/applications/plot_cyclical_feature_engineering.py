26051c45059f89f676d261fa471d64de2b3430ce
Olivier Grisel <olivier.grisel@ensta.org>
2021-07-19 13:39:59 +0200

    Move spline peak locations to get balanced coverage of the feature range

--- examples/applications/plot_cyclical_feature_engineering.py
+++ examples/applications/plot_cyclical_feature_engineering.py
@@ -423,13 +423,13 @@ from sklearn.preprocessing import SplineTransformer
 def periodic_spline_transformer(period, n_splines=None, degree=3):
     if n_splines is None:
         n_splines = period
-    n_knots = n_splines + 2  # periodic and include_bias is False
+    n_knots = n_splines + 1  # periodic and include_bias is True
     return SplineTransformer(
         degree=degree,
         n_knots=n_knots,
         knots=np.linspace(0, period, n_knots).reshape(n_knots, 1),
         extrapolation="periodic",
-        include_bias=False,
+        include_bias=True,
     )
 
 
