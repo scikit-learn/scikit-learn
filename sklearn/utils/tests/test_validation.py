7bc3d8bdd93c0374b75d30d96cff7f5d3ab372df
Thomas J. Fan <thomasjpfan@gmail.com>
2021-07-23 22:22:32 -0400

    CLN Move back to validation

--- sklearn/utils/tests/test_validation.py
+++ sklearn/utils/tests/test_validation.py
@@ -49,6 +49,7 @@ from sklearn.utils.validation import (
     _allclose_dense_sparse,
     _num_features,
     FLOAT_DTYPES,
+    _get_feature_names,
 )
 from sklearn.utils.validation import _check_fit_params
 
@@ -1445,3 +1446,40 @@ def test_check_array_deprecated_matrix():
     )
     with pytest.warns(FutureWarning, match=msg):
         check_array(X)
+
+
+@pytest.mark.parametrize(
+    "names",
+    [
+        list(range(2)),
+        range(2),
+        [["a", "b"], ["c", "d"]],
+    ],
+    ids=["list-int", "range", "multi-index"],
+)
+def test_get_feature_names_pandas_warns(names):
+    """Get feature names with pandas dataframes with warnings."""
+    pd = pytest.importorskip("pandas")
+    X = pd.DataFrame([[1, 2], [4, 5], [5, 6]], columns=names)
+
+    msg = "Feature name support requires all feature names to be strings"
+    with pytest.warns(FutureWarning, match=msg):
+        names = _get_feature_names(X)
+    assert names is None
+
+
+def test_get_feature_names_pandas():
+    """Get feature names with pandas dataframes."""
+    pd = pytest.importorskip("pandas")
+    columns = [f"col_{i}" for i in range(3)]
+    X = pd.DataFrame([[1, 2, 3], [4, 5, 6]], columns=columns)
+    feature_names = _get_feature_names(X)
+
+    assert_array_equal(feature_names, columns)
+
+
+def test_get_feature_names_numpy():
+    """Get feature names return None for numpy arrays."""
+    X = np.array([[1, 2, 3], [4, 5, 6]])
+    names = _get_feature_names(X)
+    assert names is None
--- sklearn/utils/validation.py
+++ sklearn/utils/validation.py
@@ -1587,3 +1587,37 @@ def _check_fit_params(X, fit_params, indices=None):
             )
 
     return fit_params_validated
+
+
+def _get_feature_names(X):
+    """Get feature names from X.
+
+    Support for other array containers should place its implementation here.
+
+    Parameters
+    ----------
+    X : {ndarray, dataframe} of shape (n_samples, n_features)
+        Array container to extract feature names.
+
+        - pandas dataframe : The columns will be considered to be feature
+          names. If the dataframe contains non-string feature names, `None` is
+          returned.
+        - All other array containers will return `None`.
+
+    Returns
+    -------
+    names: ndarray or None
+        Feature names of `X`. Unrecognized array containers will return `None`.
+    """
+    if hasattr(X, "columns"):
+        feature_names = np.asarray(X.columns)
+        # Only strings are supported
+        if not all(isinstance(item, str) for item in feature_names):
+            warnings.warn(
+                "Feature name support requires all feature names to be strings. "
+                "Passing non-str feature names will raise an error in 1.2",
+                FutureWarning,
+            )
+
+            return
+        return feature_names
