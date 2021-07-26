e78723342c7e2aa9472294fef34abf083ffe2b0f
murata-yu <67666318+murata-yu@users.noreply.github.com>
2021-07-26 10:52:00 +0900

    Update sklearn/feature_selection/_sequential.py
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- sklearn/feature_selection/_sequential.py
+++ sklearn/feature_selection/_sequential.py
@@ -32,7 +32,7 @@ class SequentialFeatureSelector(SelectorMixin, MetaEstimatorMixin, BaseEstimator
         An unfitted estimator.
 
     n_features_to_select : "auto", int or float, default='warn'
-        If 'auto', the behaviour depends on the `tol` parameter:
+        If `"auto"`, the behaviour depends on the `tol` parameter:
 
         - if `tol` is not None, then features are selected until the score
           improvement does not exceed `tol`.
