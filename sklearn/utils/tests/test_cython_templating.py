1bc6be834ccd3e14bee17cf98f60509cf4149158
mbatoul <batoulmathis@gmail.com>
2021-07-26 11:10:13 +0200

    Read `.gitignore` file content and check files generated by Tempita templates are included.

--- sklearn/utils/tests/test_cython_templating.py
+++ sklearn/utils/tests/test_cython_templating.py
@@ -9,7 +9,12 @@ def test_files_generated_by_templates_are_git_ignored():
     gitignore_file = pathlib.Path(sklearn.__file__).parent.parent / ".gitignore"
     if not gitignore_file.exists():
         pytest.skip("Tests are not run from the source folder")
+
     base_dir = pathlib.Path(sklearn.__file__).parent
+    ignored_files = open(gitignore_file, "r").readlines()
+    ignored_files = list(map(lambda line: line.strip("\n"), ignored_files))
+
     for filename in base_dir.glob(f"**/*.{TEMPITA_EXTENSION}"):
-        filename_wo_tempita_suffix = filename.with_suffix("")
-        assert not filename_wo_tempita_suffix.exists()
+        filename = str(filename).split("scikit-learn/")[-1]
+        filename_wo_tempita_suffix = filename.strip(f".{TEMPITA_EXTENSION}")
+        assert filename_wo_tempita_suffix in ignored_files
