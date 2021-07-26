85bfddd7c7a61baff29b87a6dba9e43a50666e59
murata-yu <67666318+murata-yu@users.noreply.github.com>
2021-07-26 10:53:57 +0900

    Update sklearn/feature_selection/tests/test_sequential.py
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- sklearn/feature_selection/tests/test_sequential.py
+++ sklearn/feature_selection/tests/test_sequential.py
@@ -50,7 +50,7 @@ def test_n_features_to_select(direction, n_features_to_select):
 
 @pytest.mark.parametrize("direction", ("forward", "backward"))
 @pytest.mark.parametrize("tol", (1e-3, None))
-def test_n_features_to_auto_select(direction, tol):
+def test_n_features_to_select_auto(direction, tol):
     """Check the behaviour of `n_features_to_select="auto"` with different 
     values for the parameter `tol`.
     """
