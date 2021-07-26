d772d43389352ecf45019508c08ef4a8f6dc7b7e
mbatoul <batoulmathis@gmail.com>
2021-07-26 14:24:31 +0200

    Parametrize `test_type_invariance`.

--- sklearn/utils/tests/test_weight_vector.py
+++ sklearn/utils/tests/test_weight_vector.py
@@ -3,21 +3,23 @@
 # License: BSD 3 clause
 
 import numpy as np
+import pytest
 from sklearn.utils._weight_vector import (
     WeightVector32,
     WeightVector64,
 )
 
 
-def test_type_invariance():
-    weights32 = np.random.rand(100).astype(np.float32)
-    average_weights32 = np.random.rand(100).astype(np.float32)
-    weight_vector32 = WeightVector32(weights32, average_weights32)
-    assert weight_vector32.w.dtype is np.dtype(np.float32)
-    assert weight_vector32.aw.dtype is np.dtype(np.float32)
-
-    weights64 = np.random.rand(100).astype(np.float64)
-    average_weights64 = np.random.rand(100).astype(np.float64)
-    weight_vector64 = WeightVector64(weights64, average_weights64)
-    assert weight_vector64.w.dtype is np.dtype(np.float64)
-    assert weight_vector64.aw.dtype is np.dtype(np.float64)
+@pytest.mark.parametrize(
+    "dtype, weight_vector_class",
+    [
+        (np.float32, WeightVector32),
+        (np.float64, WeightVector64),
+    ],
+)
+def test_type_invariance(dtype, weight_vector_class):
+    weights = np.random.rand(100).astype(dtype)
+    average_weights = np.random.rand(100).astype(dtype)
+    weight_vector = weight_vector_class(weights, average_weights)
+    assert weight_vector.w.dtype is np.dtype(dtype)
+    assert weight_vector.aw.dtype is np.dtype(dtype)
