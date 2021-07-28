75c4f4f4c06a8e25c76a6b98fbc6c85744d32d51
shinehide <418545838@qq.com>
2021-07-28 19:03:07 +0800

    update test message
    Update test message and doc, make test message same as error message

--- sklearn/metrics/_plot/tests/test_plot_curve_common.py
+++ sklearn/metrics/_plot/tests/test_plot_curve_common.py
@@ -31,7 +31,7 @@ def test_plot_curve_error_non_binary(pyplot, data, plot_func):
     clf = DecisionTreeClassifier()
     clf.fit(X, y)
 
-    msg = "DecisionTreeClassifier should be a binary classifier"
+    msg = "Expected a binary classifier, but got DecisionTreeClassifier"
     with pytest.raises(ValueError, match=msg):
         plot_func(clf, X, y)
 
