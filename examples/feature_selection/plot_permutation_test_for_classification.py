a826c78097fd33dd7fed821da896d4d733b228ab
Gael Varoquaux <gael.varoquaux@normalesup.org>
2021-07-28 09:23:15 +0200

    Add redirect

---
+++ examples/feature_selection/plot_permutation_test_for_classification.py
@@ -0,0 +1,9 @@
+"""
+
+This example was moved to
+:ref:`sphx_glr_auto_examples_model_selection_plot_permutation_tests_for_classification.py`
+
+.. meta::
+   :http-equiv=refresh: 5; ../feature_selection/plot_permutation_tests_for_classification.html
+
+"""
