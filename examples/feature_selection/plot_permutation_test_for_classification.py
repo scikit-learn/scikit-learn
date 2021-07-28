1e476369e2e578af115b00efe1f96f7b3bb3fb0e
Gael Varoquaux <gael.varoquaux@normalesup.org>
2021-07-28 09:53:12 +0200

    Fix rediction link

--- examples/feature_selection/plot_permutation_test_for_classification.py
+++ examples/feature_selection/plot_permutation_test_for_classification.py
@@ -3,7 +3,9 @@
 This example was moved to
 :ref:`sphx_glr_auto_examples_model_selection_plot_permutation_tests_for_classification.py`
 
+You will be redirected to the new example in 5 seconds.
+
 .. meta::
-   :http-equiv=refresh: 5; ../feature_selection/plot_permutation_tests_for_classification.html
+   :http-equiv=refresh: 5; ../model_selection/plot_permutation_tests_for_classification.html
 
 """ # noqa
