882c0f00320bca2377129e05863247680b2008e1
shinehide <418545838@qq.com>
2021-07-29 23:16:59 +0800

    The lines too long
    the lines too long to pass CI

--- sklearn/metrics/_plot/tests/test_plot_precision_recall.py
+++ sklearn/metrics/_plot/tests/test_plot_precision_recall.py
@@ -40,12 +40,14 @@ def test_errors(pyplot):
     multi_clf = DecisionTreeClassifier().fit(X, y_multiclass)
 
     # Fitted multiclass classifier with binary data
-    msg = "Expected 'estimator' to be a binary classifier, but got DecisionTreeClassifier"
+    msg = "Expected 'estimator' to be a binary classifier," \
+          " but got DecisionTreeClassifier"
     with pytest.raises(ValueError, match=msg):
         plot_precision_recall_curve(multi_clf, X, y_binary)
 
     reg = DecisionTreeRegressor().fit(X, y_multiclass)
-    msg = "Expected 'estimator' to be a binary classifier, but got DecisionTreeClassifier"
+    msg = "Expected 'estimator' to be a binary classifier," \
+          " but got DecisionTreeClassifier"
     with pytest.raises(ValueError, match=msg):
         plot_precision_recall_curve(reg, X, y_binary)
 
