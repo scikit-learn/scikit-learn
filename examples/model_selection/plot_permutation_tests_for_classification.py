62f97d60c49cd1e38a95214bdf68ede1a543eec1
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2021-07-31 09:39:11 +0200

    iter

--- examples/model_selection/plot_permutation_tests_for_classification.py
+++ examples/model_selection/plot_permutation_tests_for_classification.py
@@ -92,7 +92,7 @@ fig, ax = plt.subplots()
 ax.hist(perm_scores_iris, bins=20, density=True)
 ax.axvline(score_iris, ls="--", color="r")
 score_label = f"Score on original\ndata: {score_iris:.2f}\n(p-value: {pvalue_iris:.3f})"
-ax.text(0.7, 260, score_label, fontsize=12)
+ax.text(0.7, 10, score_label, fontsize=12)
 ax.set_xlabel("Accuracy score")
 _ = ax.set_ylabel("Probability")
 
@@ -113,7 +113,7 @@ ax.hist(perm_scores_rand, bins=20, density=True)
 ax.set_xlim(0.13)
 ax.axvline(score_rand, ls="--", color="r")
 score_label = f"Score on original\ndata: {score_rand:.2f}\n(p-value: {pvalue_rand:.3f})"
-ax.text(0.14, 125, score_label, fontsize=12)
+ax.text(0.14, 7.5, score_label, fontsize=12)
 ax.set_xlabel("Accuracy score")
 ax.set_ylabel("Probability")
 plt.show()
