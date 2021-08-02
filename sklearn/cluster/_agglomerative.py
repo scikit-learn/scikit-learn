a11aad3d4a3e7605b5ffbf0cba3d86eb992fe073
reshamas <reshama.stat@gmail.com>
2021-08-02 11:18:39 -0400

    try (n_samples,)

--- sklearn/cluster/_agglomerative.py
+++ sklearn/cluster/_agglomerative.py
@@ -904,7 +904,7 @@ class AgglomerativeClustering(ClusterMixin, BaseEstimator):
 
         Parameters
         ----------
-        X : array-like, shape (n_samples, n_features)
+        X : array-like, shape (n_samples, n_features) or (n_samples,)
             Training instances to cluster, or distances between instances if
             ``affinity='precomputed'``.
 
