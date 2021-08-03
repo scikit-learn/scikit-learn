a0fae710cf1c10b9e567f5e8cd8c18017ee1128e
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2021-08-03 21:10:43 +0200

    cover for negative number as well

--- sklearn/inspection/tests/test_permutation_importance.py
+++ sklearn/inspection/tests/test_permutation_importance.py
@@ -525,7 +525,7 @@ def test_permutation_importance_multi_metric(list_single_scorer, multi_scorer):
         assert_allclose(multi_result.importances, single_result.importances)
 
 
-@pytest.mark.parametrize("max_samples", [5])
+@pytest.mark.parametrize("max_samples", [-1, 5])
 def test_permutation_importance_max_samples_error(max_samples):
     """Check that a proper error message is raised when `max_samples` is not
     set to a valid input value.
@@ -536,7 +536,7 @@ def test_permutation_importance_max_samples_error(max_samples):
     clf = LogisticRegression()
     clf.fit(X, y)
 
-    err_msg = "max\\_samples must be in \\(0, n\\_samples\\]"
+    err_msg = r"max_samples must be in \(0, n_samples\]"
 
     with pytest.raises(ValueError, match=err_msg):
         permutation_importance(clf, X, y, max_samples=max_samples)
