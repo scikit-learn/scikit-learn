fe7c9abc1e98e8a9118aad417e824284730d7845
Oliver Pfaffel <opfaffel@gmail.com>
2021-08-03 17:10:15 +0200

    Update sklearn/inspection/tests/test_permutation_importance.py
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- sklearn/inspection/tests/test_permutation_importance.py
+++ sklearn/inspection/tests/test_permutation_importance.py
@@ -63,7 +63,8 @@ def test_permutation_importance_correlated_feature_regression(n_jobs, max_sample
     assert np.all(result.importances_mean[-1] > result.importances_mean[:-1])
 
 
-@pytest.mark.parametrize("n_jobs, max_samples", product([1, 2], [0.5, 1.0]))
+@pytest.mark.parametrize("n_jobs", [1, 2])
+@pytest.mark.parametrize("max_samples", [0.5, 1.0])
 def test_permutation_importance_correlated_feature_regression_pandas(
     n_jobs, max_samples
 ):
