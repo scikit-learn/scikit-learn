1043fa091db0bf169218af8c43f8c4d913cee015
Mathis Batoul <batoulmathis@gmail.com>
2021-08-04 11:00:16 +0200

    Pass class name argument to parametrize`test_weight_vector`.
    Co-authored-by: Julien Jerphanion <git@jjerphan.xyz>

--- sklearn/utils/tests/test_weight_vector.py
+++ sklearn/utils/tests/test_weight_vector.py
@@ -7,15 +7,17 @@ from sklearn.utils._weight_vector import (
 
 
 @pytest.mark.parametrize(
-    "dtype, weight_vector_class",
+    "dtype, WeightVector",
     [
         (np.float32, WeightVector32),
         (np.float64, WeightVector64),
     ],
 )
-def test_type_invariance(dtype, weight_vector_class):
+def test_type_invariance(dtype, WeightVector):
     weights = np.random.rand(100).astype(dtype)
     average_weights = np.random.rand(100).astype(dtype)
-    weight_vector = weight_vector_class(weights, average_weights)
+    
+    weight_vector = WeightVector(weights, average_weights)
+    
     assert np.asarray(weight_vector.w).dtype is np.dtype(dtype)
     assert np.asarray(weight_vector.aw).dtype is np.dtype(dtype)
