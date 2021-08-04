cdc486a91affd19b603e8090b46eb5ca262f3569
Felix Hafner <felixhafner2161997@googlemail.com>
2021-08-04 23:10:22 +0200

    DOC fix note in accuracy docstring regading relation with jaccard score (#19347)
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- sklearn/metrics/_classification.py
+++ sklearn/metrics/_classification.py
@@ -181,8 +181,8 @@ def accuracy_score(y_true, y_pred, *, normalize=True, sample_weight=None):
 
     Notes
     -----
-    In binary and multiclass classification, this function is equal
-    to the ``jaccard_score`` function.
+    In binary classification, this function is equal to the `jaccard_score`
+    function.
 
     Examples
     --------
