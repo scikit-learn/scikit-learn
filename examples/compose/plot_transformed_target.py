e2cc928e27ee9d76be3f186ebb293e9656a85ac2
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2021-08-07 17:03:09 +0200

    iter

--- examples/compose/plot_transformed_target.py
+++ examples/compose/plot_transformed_target.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 """
 ======================================================
 Effect of transforming the targets in regression model
@@ -16,12 +13,11 @@ example is based on the Ames housing data set.
 # Author: Guillaume Lemaitre <guillaume.lemaitre@inria.fr>
 # License: BSD 3 clause
 
-# %%
 print(__doc__)
 
-# %% [markdown]
+# %%
 # Synthetic example
-##############################################################################
+###################
 #
 # A synthetic random regression dataset is generated. The targets ``y`` are
 # modified by:
@@ -34,8 +30,6 @@ print(__doc__)
 # Therefore, a logarithmic (`np.log1p`) and an exponential function
 # (`np.expm1`) will be used to transform the targets before training a linear
 # regression model and using it for prediction.
-
-# %%
 import numpy as np
 from sklearn.datasets import make_regression
 
@@ -43,11 +37,9 @@ X, y = make_regression(n_samples=10000, noise=100, random_state=0)
 y = np.expm1((y + abs(y.min())) / 200)
 y_trans = np.log1p(y)
 
-# %% [mardown]
+# %%
 # Below we plot the probability density functions of the target
 # before and after applying the logarithmic functions.
-
-# %%
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
@@ -69,30 +61,43 @@ f.tight_layout(rect=[0.05, 0.05, 0.95, 0.95])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
-# %% [markdown]
+# %%
 # At first, a linear model will be applied on the original targets. Due to the
 # non-linearity, the model trained will not be precise during
 # prediction. Subsequently, a logarithmic function is used to linearize the
 # targets, allowing better prediction even with a similar linear model as
 # reported by the median absolute error (MAE).
+from sklearn.metrics import mean_absolute_error, r2_score
+
+
+def compute_score(y_true, y_pred):
+    return {
+        "R2 score": f"{r2_score(y_true, y_pred):.3f}",
+        "MAE": f"{mean_absolute_error(y_true, y_pred):.3f}",
+    }
+
 
 # %%
 from sklearn.compose import TransformedTargetRegressor
 from sklearn.linear_model import RidgeCV
-from sklearn.metrics import plot_prediction_error
+from sklearn.metrics import PredictionErrorDisplay
 
 f, (ax0, ax1) = plt.subplots(1, 2, sharey=True)
 
 ridge_cv = RidgeCV().fit(X_train, y_train)
+y_pred_ridge = ridge_cv.predict(X_test)
+
 ridge_cv_with_trans_target = TransformedTargetRegressor(
     regressor=RidgeCV(), func=np.log1p, inverse_func=np.expm1
 ).fit(X_train, y_train)
+y_pred_ridge_with_trans_target = ridge_cv_with_trans_target.predict(X_test)
 
-scorers = {"R2": "r2", "MAE": "neg_median_absolute_error"}
-
-plot_prediction_error(ridge_cv, X_test, y_test, scoring=scorers, ax=ax0)
-plot_prediction_error(
-    ridge_cv_with_trans_target, X_test, y_test, scoring=scorers, ax=ax1
+PredictionErrorDisplay.from_predictions(
+    y_test, y_pred_ridge, scores=compute_score(y_test, y_pred_ridge), ax=ax0
+)
+PredictionErrorDisplay.from_predictions(
+    y_test, y_pred_ridge_with_trans_target,
+    scores=compute_score(y_test, y_pred_ridge_with_trans_target), ax=ax1
 )
 
 ax0.set_title('Ridge regression \n without target transformation')
@@ -100,9 +105,9 @@ ax1.set_title('Ridge regression \n with target transformation')
 f.suptitle("Synthetic data", y=0.035)
 f.tight_layout(rect=[0.05, 0.05, 0.95, 0.95])
 
-# %% [markdown]
+# %%
 # Real-world data set
-###############################################################################
+#####################
 #
 # In a similar manner, the Ames housing data set is used to show the impact
 # of transforming the targets before learning a model. In this example, the
@@ -123,12 +128,10 @@ y_trans = quantile_transform(y.to_frame(),
                              n_quantiles=900,
                              output_distribution='normal',
                              copy=True).squeeze()
-# %% [markdown]
+# %%
 # A :class:`~sklearn.preprocessing.QuantileTransformer` is used to normalize
 # the target distribution before applying a
 # :class:`~sklearn.linear_model.RidgeCV` model.
-
-# %%
 f, (ax0, ax1) = plt.subplots(1, 2)
 
 ax0.hist(y, bins=100, density=True)
@@ -147,7 +150,7 @@ f.tight_layout(rect=[0.05, 0.05, 0.95, 0.95])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
 
-# %% [markdown]
+# %%
 # The effect of the transformer is weaker than on the synthetic data. However,
 # the transformation results in an increase in :math:`R^2` and large decrease
 # of the MAE. The residual plot (predicted target - true target vs predicted
@@ -155,25 +158,28 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
 # shape due to residual values that vary depending on the value of predicted
 # target. With target transformation, the shape is more linear indicating
 # better model fit.
-
-# %%
 from sklearn.preprocessing import QuantileTransformer
 
 f, (ax0, ax1) = plt.subplots(2, 2, sharey="row", figsize=(6.5, 8))
 
 ridge_cv = RidgeCV().fit(X_train, y_train)
+y_pred_ridge = ridge_cv.predict(X_test)
+
 ridge_cv_with_trans_target = TransformedTargetRegressor(
     regressor=RidgeCV(),
     transformer=QuantileTransformer(
         n_quantiles=900, output_distribution="normal"
     ),
 ).fit(X_train, y_train)
+y_pred_ridge_with_trans_target = ridge_cv_with_trans_target.predict(X_test)
 
 # plot the actual vs predicted values
-scorers = {"R2": "r2", "MAE": "neg_median_absolute_error"}
-plot_prediction_error(ridge_cv, X_test, y_test, scoring=scorers, ax=ax0[0])
-plot_prediction_error(
-    ridge_cv_with_trans_target, X_test, y_test, scoring=scorers, ax=ax0[1]
+PredictionErrorDisplay.from_predictions(
+    y_test, y_pred_ridge, scores=compute_score(y_test, y_pred_ridge), ax=ax0[0]
+)
+PredictionErrorDisplay.from_predictions(
+    y_test, y_pred_ridge_with_trans_target,
+    scores=compute_score(y_test, y_pred_ridge_with_trans_target), ax=ax0[1]
 )
 ax0[0].set_title("Ridge regression \n without target transformation")
 ax0[1].set_title("Ridge regression \n with target transformation")
@@ -194,3 +200,5 @@ ax1[1].ticklabel_format(axis="both", style="sci", scilimits=(0, 0))
 f.suptitle("Ames housing data: selling price", y=0.035)
 
 plt.show()
+
+# %%
