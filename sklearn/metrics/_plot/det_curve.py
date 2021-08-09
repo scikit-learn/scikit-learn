32d271d8f059138a3cca101c65b1836596084b34
harupy <hkawamura0130@gmail.com>
2021-08-10 00:07:24 +0900

    Increase n_samples and test_size

--- sklearn/metrics/_plot/det_curve.py
+++ sklearn/metrics/_plot/det_curve.py
@@ -60,9 +60,9 @@ class DetCurveDisplay:
     >>> from sklearn.metrics import det_curve, DetCurveDisplay
     >>> from sklearn.model_selection import train_test_split
     >>> from sklearn.svm import SVC
-    >>> X, y = make_classification(random_state=0)
+    >>> X, y = make_classification(n_samples=1000, random_state=0)
     >>> X_train, X_test, y_train, y_test = train_test_split(
-    ...     X, y, random_state=0)
+    ...     X, y, test_size=0.4, random_state=0)
     >>> clf = SVC(random_state=0).fit(X_train, y_train)
     >>> y_pred = clf.decision_function(X_test)
     >>> fpr, fnr, _ = det_curve(y_test, y_pred)
@@ -157,9 +157,9 @@ class DetCurveDisplay:
         >>> from sklearn.metrics import DetCurveDisplay
         >>> from sklearn.model_selection import train_test_split
         >>> from sklearn.svm import SVC
-        >>> X, y = make_classification(random_state=0)
+        >>> X, y = make_classification(n_samples=1000, random_state=0)
         >>> X_train, X_test, y_train, y_test = train_test_split(
-        ...     X, y, random_state=0)
+        ...     X, y, test_size=0.4, random_state=0)
         >>> clf = SVC(random_state=0).fit(X_train, y_train)
         >>> DetCurveDisplay.from_estimator(
         ...    clf, X_test, y_test)
@@ -254,9 +254,9 @@ class DetCurveDisplay:
         >>> from sklearn.metrics import DetCurveDisplay
         >>> from sklearn.model_selection import train_test_split
         >>> from sklearn.svm import SVC
-        >>> X, y = make_classification(random_state=0)
+        >>> X, y = make_classification(n_samples=1000, random_state=0)
         >>> X_train, X_test, y_train, y_test = train_test_split(
-        ...     X, y, random_state=0)
+        ...     X, y, test_size=0.4, random_state=0)
         >>> clf = SVC(random_state=0).fit(X_train, y_train)
         >>> y_pred = clf.decision_function(X_test)
         >>> DetCurveDisplay.from_predictions(
