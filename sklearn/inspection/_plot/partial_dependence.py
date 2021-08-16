3aeb4c876b8bb0f9b29ae699deebebbd9eac19dd
Harutaka Kawamura <hkawamura0130@gmail.com>
2021-08-17 07:53:21 +0900

    Update sklearn/inspection/_plot/partial_dependence.py
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- sklearn/inspection/_plot/partial_dependence.py
+++ sklearn/inspection/_plot/partial_dependence.py
@@ -248,6 +248,7 @@ def plot_partial_dependence(
     >>> import matplotlib.pyplot as plt
     >>> from sklearn.datasets import make_friedman1
     >>> from sklearn.ensemble import GradientBoostingRegressor
+    >>> from sklearn.inspection import plot_partial_dependence
     >>> X, y = make_friedman1()
     >>> clf = GradientBoostingRegressor(n_estimators=10).fit(X, y)
     >>> plot_partial_dependence(clf, X, [0, (0, 1)])
