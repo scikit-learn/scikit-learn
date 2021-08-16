57d15492289c0417c8c8f9b0c7289d0403650295
harupy <hkawamura0130@gmail.com>
2021-08-16 22:39:26 +0900

    Skip doctest

--- sklearn/metrics/_plot/det_curve.py
+++ sklearn/metrics/_plot/det_curve.py
@@ -444,7 +444,7 @@ def plot_det_curve(
     >>> X_train, X_test, y_train, y_test = train_test_split(
     ...     X, y, test_size=0.4, random_state=0)
     >>> clf = SVC(random_state=0).fit(X_train, y_train)
-    >>> plot_det_curve(clf, X_test, y_test)
+    >>> plot_det_curve(clf, X_test, y_test)  # doctest: +SKIP
     <...>
     >>> plt.show()
     """
