1c15ad3f5aaf4d2a46201880db28fa624f646357
shivank070701 <shivankbhattal07@gmail.com>
2021-08-18 11:45:11 +0200

    implement of nbytes or __sizeof__

--- sklearn/base.py
+++ sklearn/base.py
@@ -1006,3 +1006,35 @@ def _is_pairwise(estimator):
 
     # use pairwise tag when the attribute is not present
     return pairwise_tag
+import sys
+from numbers import Number
+from collections import deque
+from collections.abc import Set, Mapping
+
+
+ZERO_DEPTH_BASES = (str, bytes, Number, range, bytearray)
+
+
+def __sizeof__(obj_0):
+    """Recursively iterate to sum size of object & members."""
+    _seen_ids = set()
+    def inner(obj):
+        obj_id = id(obj)
+        if obj_id in _seen_ids:
+            return 0
+        _seen_ids.add(obj_id)
+        size = sys.getsizeof(obj)
+        if isinstance(obj, ZERO_DEPTH_BASES):
+            pass # bypass remaining control flow and return
+        elif isinstance(obj, (tuple, list, Set, deque)):
+            size += sum(inner(i) for i in obj)
+        elif isinstance(obj, Mapping) or hasattr(obj, 'items'):
+            size += sum(inner(k) + inner(v) for k, v in getattr(obj, 'items')())
+        # Check for custom object instances - may subclass above too
+        if hasattr(obj, '__dict__'):
+            size += inner(vars(obj))
+        if hasattr(obj, '__slots__'): # can have __slots__ with __dict__
+            size += sum(inner(getattr(obj, s)) for s in obj.__slots__ if hasattr(obj, s))
+        return size
+    return inner(obj_0)
+
