4b245c89ac6aa6b79281fee161706d79e89fb578
RAVANv2 <chauhanharsh3363334@gmail.com>
2021-08-19 22:28:53 +0530

    fixes #20754 | added magnitute of smallest negative number in affinity matrix

--- sklearn/cluster/_spectral.py
+++ sklearn/cluster/_spectral.py
@@ -596,11 +596,16 @@ class SpectralClustering(ClusterMixin, BaseEstimator):
                 X, metric=self.affinity, filter_params=True, **params
             )
 
-            if (self.affinity == 'linear') and (np.any(self.affinity_matrix_ < 0)):
-                raise ValueError(
+            unacceptable_kernels = ["linear"]
+            if (self.affinity in unacceptable_kernels) and (
+                np.any(self.affinity_matrix_ < 0)
+            ):
+                self.affinity_matrix_ += abs(self.affinity_matrix_.min())
+                warnings.warn(
                     f"The affinity matrix generated "
                     f"for your input ndarray contains negative values,"
-                    f" which is not compatible with affinity = {self.affinity}")
+                    f" which is not compatible with affinity = {self.affinity}"
+                )
 
         random_state = check_random_state(self.random_state)
         self.labels_ = spectral_clustering(
