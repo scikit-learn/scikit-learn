39925a1e2c224b8216b0a2bd4b1d1b02ed993771
Chiara Marmo <marmochiaskl@gmail.com>
2021-08-20 19:03:03 +0200

    Add script to build wheels

---
+++ build_tools/circle/build_whell_arm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+set -x
+
+sudo add-apt-repository --remove ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get install python3-virtualenv ccache
+python3 -m virtualenv --system-site-packages --python=python3 testenv
+source testenv/bin/activate
+pip install --upgrade pip
+for pyversion in 37 38 39; do
+    pywheel=`echo "cp"$pyversion"-manylinux_aarch64"`
+    export CIBW_BUILD=$pywheel
+    python -m pip install cibuildwheel
+        python -m cibuildwheel --output-dir wheelhouse
