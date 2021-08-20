c997a0b638e49bc40a05b32e6c7a311e5039ab41
Olivier Grisel <olivier.grisel@ensta.org>
2021-08-20 18:01:08 +0200

    Document feature_names_in_ in SelfTrainingClassifier's docstring

--- sklearn/semi_supervised/_self_training.py
+++ sklearn/semi_supervised/_self_training.py
@@ -92,6 +92,11 @@ class SelfTrainingClassifier(MetaEstimatorMixin, BaseEstimator):
 
         .. versionadded:: 0.24
 
+    feature_names_in_ : ndarray of shape (`n_features_in_`,)
+        Names of features seen during :term:`fit`. Defined only when `X`
+        has feature names that are strings.
+        .. versionadded:: 1.0
+
     n_iter_ : int
         The number of rounds of self-training, that is the number of times the
         base estimator is fitted on relabeled variants of the training set.
