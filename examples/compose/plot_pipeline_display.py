aa6a30897e231f38775ce431afd2ad0c813d9dff
reshamas <reshama.stat@gmail.com>
2021-08-21 10:18:36 -0400

    added a complex example with column transformer

--- examples/compose/plot_pipeline_display.py
+++ examples/compose/plot_pipeline_display.py
@@ -97,3 +97,56 @@ pipe
 set_config(display="diagram")
 
 pipe
+
+"""
+=================================================================
+Displaying a Complex Pipeline Chaining a Column Transformer
+=================================================================
+
+This example constructs a pipeline that does column transformation followed
+by a logistic regression classifier.
+
+The default configuration for displaying a pipeline is 'text' where
+`set_config(display='text')`.  To visualize the diagram in Jupyter Notebook,
+use `set_config(display='diagram')` and then output the pipeline object.
+"""
+
+# %%
+# Illustration of `Pipeline` and `ColumnTransformer` and `LogisticRegression`
+###############################################################################
+# This section constructs a pipeline and displays its text representation.
+
+from sklearn.compose import make_column_selector as selector
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import make_pipeline
+import pandas as pd
+from sklearn import set_config
+
+numerical_columns_selector = selector(dtype_exclude=object)
+categorical_columns_selector = selector(dtype_include=object)
+
+data = pd.DataFrame()
+
+numerical_columns = numerical_columns_selector(data)
+categorical_columns = categorical_columns_selector(data)
+
+categorical_preprocessor = OneHotEncoder(handle_unknown="ignore")
+numerical_preprocessor = StandardScaler()
+
+preprocessor = ColumnTransformer(
+    [
+        ("one-hot-encoder", categorical_preprocessor, categorical_columns),
+        ("standard-scaler", numerical_preprocessor, numerical_columns),
+    ]
+)
+
+pipe = make_pipeline(preprocessor, LogisticRegression(max_iter=500))
+pipe
+
+# %%
+# To visualize the diagram, change display='diagram'
+set_config(display="diagram")
+
+pipe
