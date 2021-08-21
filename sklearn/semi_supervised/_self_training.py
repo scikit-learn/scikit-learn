e65759a40545baa6db353954a867db3996569377
Olivier Grisel <olivier.grisel@ensta.org>
2021-08-21 10:02:12 +0200

    Apply suggestions from code review
    Co-authored-by: Thomas J. Fan <thomasjpfan@gmail.com>

--- sklearn/semi_supervised/_self_training.py
+++ sklearn/semi_supervised/_self_training.py
@@ -94,7 +94,8 @@ class SelfTrainingClassifier(MetaEstimatorMixin, BaseEstimator):
 
     feature_names_in_ : ndarray of shape (`n_features_in_`,)
         Names of features seen during :term:`fit`. Defined only when `X`
-        has feature names that are strings.
+        has feature names that are all strings.
+
         .. versionadded:: 1.0
 
     n_iter_ : int
