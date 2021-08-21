d488d733ea5d7b1130d9b2b538f92c310e1c2165
Juan Martin Loyola <jmloyola@outlook.com>
2021-08-21 12:09:58 -0300

    DOC Ensures that RidgeClassifierCV passes numpydoc validation (#20800)
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- maint_tools/test_docstrings.py
+++ maint_tools/test_docstrings.py
@@ -88,7 +88,6 @@ DOCSTRING_IGNORE_LIST = [
     "RadiusNeighborsTransformer",
     "RandomizedSearchCV",
     "RegressorChain",
-    "RidgeClassifierCV",
     "RobustScaler",
     "SGDOneClassSVM",
     "SGDRegressor",
