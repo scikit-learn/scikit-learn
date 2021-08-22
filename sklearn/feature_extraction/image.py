1392f32ccf85a32ca80e23c8f55a9c13c6712d84
bthirion <bertrand.thirion@inria.fr>
2021-08-22 18:03:09 +0200

    Addressed TomDLTs comment

--- sklearn/feature_extraction/image.py
+++ sklearn/feature_extraction/image.py
@@ -77,8 +77,7 @@ def _mask_edges_weights(mask, edges, weights=None):
         maxval = edges.max()
     else:
         maxval = 0
-    order = np.searchsorted(np.unique(np.where(mask.ravel())),
-                            np.arange(maxval + 1))
+    order = np.searchsorted(np.flatnonzero(mask), np.arange(maxval + 1))
     edges = order[edges]
     if weights is None:
         return edges
