905c40890926515a2ff169651eca56f5ee0069ec
genvalen <genvalen@protonmail.com>
2021-08-23 03:49:54 -0400

    Remove KernelPCA from DOCSTRING_IGNORE_LIST

--- maint_tools/test_docstrings.py
+++ maint_tools/test_docstrings.py
@@ -38,7 +38,6 @@ DOCSTRING_IGNORE_LIST = [
     "IterativeImputer",
     "KBinsDiscretizer",
     "KNNImputer",
-    "KernelPCA",
     "LabelBinarizer",
     "LabelPropagation",
     "LabelSpreading",
