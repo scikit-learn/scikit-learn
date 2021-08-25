1e4b641243833c6366aaed10d55528894af45bb7
baam25simo <baam25simo@gmail.com>
2021-08-25 15:42:48 +0200

    "ColumnTransformer" removed from DOCSTRING_IGNORE_LIST in test_docstring.py

--- maint_tools/test_docstrings.py
+++ maint_tools/test_docstrings.py
@@ -14,7 +14,6 @@ DOCSTRING_IGNORE_LIST = [
     "Birch",
     "CalibratedClassifierCV",
     "ClassifierChain",
-    "ColumnTransformer",
     "DecisionTreeRegressor",
     "DictVectorizer",
     "DictionaryLearning",
