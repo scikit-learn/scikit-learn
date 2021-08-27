ad76897ff71a12ac350e86e1eb8295891aa015a8
Bhavya Bhardwaj <bhavya_bhardwaj@bhavyabhardwaj.Lenovo.Y520.15IKBN>
2021-08-27 19:09:23 +0530

    Adding Fall-out, Miss rate, specificity as metrics #5516 v13

--- sklearn/metrics/__init__.py
+++ sklearn/metrics/__init__.py
@@ -35,6 +35,10 @@ from ._classification import recall_score
 from ._classification import zero_one_loss
 from ._classification import brier_score_loss
 from ._classification import multilabel_confusion_matrix
+from ._classification import sensitivity
+from ._classification import specificity
+from ._classification import miss_rate
+from ._classification import fallout_rate
 
 from . import cluster
 from .cluster import adjusted_mutual_info_score
