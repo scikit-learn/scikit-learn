fdc0a254bc9427a144c69a8a3f2a2a892421ee3a
baam <83430343+baam25simo@users.noreply.github.com>
2021-08-31 18:57:20 +0200

    DOC Ensures that FactorAnalysis passes numpydoc validation (#20845)
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- maint_tools/test_docstrings.py
+++ maint_tools/test_docstrings.py
@@ -16,7 +16,6 @@ DOCSTRING_IGNORE_LIST = [
     "ClassifierChain",
     "DictVectorizer",
     "DictionaryLearning",
-    "FactorAnalysis",
     "FeatureAgglomeration",
     "FeatureHasher",
     "FeatureUnion",
