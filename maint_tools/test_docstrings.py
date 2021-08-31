b8986ebbf03147754516ead91e4d3b96b3efecbb
baam <83430343+baam25simo@users.noreply.github.com>
2021-08-31 17:51:42 +0200

    DOC Ensures that ElasticNetCV passes numpydoc validation (#20844)

--- maint_tools/test_docstrings.py
+++ maint_tools/test_docstrings.py
@@ -16,7 +16,6 @@ DOCSTRING_IGNORE_LIST = [
     "ClassifierChain",
     "DictVectorizer",
     "DictionaryLearning",
-    "ElasticNetCV",
     "FactorAnalysis",
     "FeatureAgglomeration",
     "FeatureHasher",
