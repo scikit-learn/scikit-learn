43c9c66ebe75de97e2c97bc8a61d9f95fb4d1c97
genvalen <genvalen@protonmail.com>
2021-08-31 12:31:34 -0400

    DOC Ensures that MiniBatchSparsePCA passes numpydoc validation (#20870)

--- maint_tools/test_docstrings.py
+++ maint_tools/test_docstrings.py
@@ -43,7 +43,6 @@ DOCSTRING_IGNORE_LIST = [
     "MeanShift",
     "MiniBatchDictionaryLearning",
     "MiniBatchKMeans",
-    "MiniBatchSparsePCA",
     "MissingIndicator",
     "MultiLabelBinarizer",
     "MultiOutputClassifier",
