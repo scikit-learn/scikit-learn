1c1359a44ecd71d418be66cdd60f6e48c019144f
Christian Lorentzen <lorentzen.ch@gmail.com>
2021-09-01 20:29:59 +0200

    TST cleaner test

--- sklearn/utils/tests/test_readonly_wrapper.py
+++ sklearn/utils/tests/test_readonly_wrapper.py
@@ -6,18 +6,21 @@ from sklearn.utils._readonly_array_wrapper import ReadonlyArrayWrapper, _test_su
 from sklearn.utils._testing import create_memmap_backed_data
 
 
-@pytest.mark.parametrize("readonly", ["flag", "memmap"])
+def _readonly_array_copy(x):
+    """Return a copy of x with flag writeable set to false."""
+    y = x.copy()
+    y.flags["WRITEABLE"] = False
+    return y
+
+
+@pytest.mark.parametrize("readonly", [_readonly_array_copy, create_memmap_backed_data])
 @pytest.mark.parametrize("dtype", [np.float32, np.float64, np.int32, np.int64])
 def test_readonly_array_wrapper(readonly, dtype):
     """Test that ReadonlyWrapper allows working with fused-typed."""
     x = np.arange(10).astype(dtype)
     sum_origin = _test_sum(x)
 
-    if readonly == "flag":
-        x_readonly = x.copy()
-        x_readonly.flags["WRITEABLE"] = False
-    else:
-        x_readonly = create_memmap_backed_data(x)
+    x_readonly = readonly(x)
 
     with pytest.raises(ValueError, match="buffer source array is read-only"):
         _test_sum(x_readonly)
