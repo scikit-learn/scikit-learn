f46190846c5d8c0bd898d1447a2a07fc50fcee2a
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2021-09-03 17:19:46 +0200

    API add from_estimator and from_predictions to RocCurveDisplay (#20569)
    Co-authored-by: Thomas J. Fan <thomasjpfan@gmail.com>
    Co-authored-by: Adrin Jalali <adrin.jalali@gmail.com>
    Co-authored-by: Olivier Grisel <olivier.grisel@ensta.org>

--- sklearn/metrics/_plot/tests/test_plot_precision_recall.py
+++ sklearn/metrics/_plot/tests/test_plot_precision_recall.py
@@ -185,7 +185,7 @@ def test_precision_recall_curve_string_labels(pyplot):
 
 def test_plot_precision_recall_curve_estimator_name_multiple_calls(pyplot):
     # non-regression test checking that the `name` used when calling
-    # `plot_roc_curve` is used as well when calling `disp.plot()`
+    # `plot_precision_recall_curve` is used as well when calling `disp.plot()`
     X, y = make_classification(n_classes=2, n_samples=50, random_state=0)
     clf_name = "my hand-crafted name"
     clf = LogisticRegression().fit(X, y)
