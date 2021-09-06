92c07da66f00d5694873d133b233756c56c8f299
Olivier Grisel <olivier.grisel@ensta.org>
2021-09-06 19:59:08 +0200

    Reenable full linux/arm64 wheel [cd build]ing

--- build_tools/circle/build_wheels_arm.sh
+++ build_tools/circle/build_wheels_arm.sh
@@ -14,14 +14,9 @@ bash Mambaforge-$(uname)-$(uname -m).sh -b -f -p $CONDA
 export PATH=$CONDA/bin:$PATH
 mamba update --yes conda
 pip install --upgrade pip
-# for pyversion in 37 38 39; do
-#     pywheel=`echo "cp"$pyversion"-manylinux_aarch64"`
-#     export CIBW_BUILD=$pywheel
-#     python -m pip install cibuildwheel
-#     python -m cibuildwheel --output-dir wheelhouse
-# done
-
-# XXX: fake building step to check the installation of anaconda-client in the
-# next step
-mkdir wheelhouse
-touch wheelhouse/fake.whl
+for pyversion in 37 38 39; do
+    pywheel=`echo "cp"$pyversion"-manylinux_aarch64"`
+    export CIBW_BUILD=$pywheel
+    python -m pip install cibuildwheel
+    python -m cibuildwheel --output-dir wheelhouse
+done
