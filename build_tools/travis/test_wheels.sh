b0aa4e440857ff40ebca22e64fad92d0a22284cd
Olivier Grisel <olivier.grisel@ensta.org>
2021-09-06 18:47:47 +0200

    Move pip install command into test command [cd build]
    To workaround travis arm64 buffering issues causing the job
    to be wrongly detected as stalled after 10min by travis.

--- build_tools/travis/test_wheels.sh
+++ build_tools/travis/test_wheels.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 set -e
+pip install --upgrade pip
+pip install pytest pytest-xdist
 
 # Faster run of the source code tests
-pytest -n $CPU_COUNT --pyargs sklearn
+python -m pytest -n $CPU_COUNT --pyargs sklearn
 
 # Test that there are no links to system libraries
 python -m threadpoolctl -i sklearn
