d55bbd65a34d78a63c4e8abbbb6a5c110855eb64
Olivier Grisel <olivier.grisel@ensta.org>
2021-09-06 20:24:29 +0200

    Try to parallelism in tests

--- build_tools/travis/test_wheels.sh
+++ build_tools/travis/test_wheels.sh
@@ -5,7 +5,9 @@ pip install --upgrade pip
 pip install pytest pytest-xdist
 
 # Faster run of the source code tests
-python -m pytest -n $CPU_COUNT --pyargs sklearn
+export USABLE_CPU_COUNT=`python -c "import joblib; print(joblib.cpu_count())"`
+echo "Usable number of CPUs according to joblib: $USABLE_CPU_COUNT"
+python -m pytest -n $USABLE_CPU_COUNT --pyargs sklearn
 
 # Test that there are no links to system libraries
 python -m threadpoolctl -i sklearn
