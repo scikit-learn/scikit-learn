248f65f69fe32512d25a31d27c24cc26a73e61b5
Olivier Grisel <olivier.grisel@ensta.org>
2021-09-07 11:05:09 +0200

    restrict tests to sklearn.cluster [cd build]

--- build_tools/travis/test_wheels.sh
+++ build_tools/travis/test_wheels.sh
@@ -6,7 +6,10 @@ pip install pytest pytest-xdist || travis_terminate $?
 # Faster run of the source code tests
 export USABLE_CPU_COUNT=`python -c "import joblib; print(joblib.cpu_count())"`
 echo "Usable number of CPUs according to joblib: $USABLE_CPU_COUNT"
-python -m pytest -n $USABLE_CPU_COUNT --pyargs sklearn || travis_terminate $?
+
+# XXX: limit to sklearn.cluster instead of full sklearn to see if overall
+# test duration is causing the problem.
+python -m pytest -n $USABLE_CPU_COUNT --pyargs sklearn.cluster || travis_terminate $?
 
 # Test that there are no links to system libraries
 python -m threadpoolctl -i sklearn || travis_terminate $?
