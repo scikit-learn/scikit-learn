a9d40e9446d70ecb5650f02a262abdb30040b64c
Olivier Grisel <olivier.grisel@ensta.org>
2021-09-07 11:36:33 +0200

    Run full sklearn test suite  [cd build]

--- build_tools/travis/test_wheels.sh
+++ build_tools/travis/test_wheels.sh
@@ -3,9 +3,7 @@
 pip install --upgrade pip || travis_terminate $?
 pip install pytest pytest-xdist || travis_terminate $?
 
-# XXX: limit to sklearn.cluster instead of full sklearn to see if overall
-# test duration is causing the problem.
-python -m pytest -n $CPU_COUNT --pyargs sklearn.cluster || travis_terminate $?
+python -m pytest -n $CPU_COUNT --pyargs sklearn || travis_terminate $?
 
 # Test that there are no links to system libraries
 python -m threadpoolctl -i sklearn || travis_terminate $?
