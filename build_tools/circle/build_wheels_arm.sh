a5169dbdc651cfb0fb100ae0f7889fef9a356e79
Olivier Grisel <olivier.grisel@ensta.org>
2021-09-07 10:02:08 +0200

    This PATH and CONDA vars should be defined correctly [cd build]

--- build_tools/circle/build_wheels_arm.sh
+++ build_tools/circle/build_wheels_arm.sh
@@ -9,8 +9,10 @@ sudo apt-get install -y wget
 # Install Python 3.8 with conda from miniforge because this is
 # the simplest way to get a working anaconda-client for the wheel
 # upload step to the staging area on the anaconda.org service.
+export CONDA=$HOME/mambaforge
 wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh
 bash Mambaforge-$(uname)-$(uname -m).sh -b -f -p $CONDA
+echo 'export CONDA=$CONDA' >> $BASH_ENV  # to be reused in a later step
 export PATH=$CONDA/bin:$PATH
 mamba update --yes conda
 pip install --upgrade pip
