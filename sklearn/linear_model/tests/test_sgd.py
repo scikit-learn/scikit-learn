559a9934d0c2824e55e61ed9fb89ce321ae3387c
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2021-09-09 15:10:14 +0200

    TST solve issues in PyPy (#20978)
    * TST fix brittle test in pypy for error message
    
    * iter
    
    * Update test_sgd.py
    
    * Update test_sgd.py
    
    * remove pypy3 in PRs

--- sklearn/linear_model/tests/test_sgd.py
+++ sklearn/linear_model/tests/test_sgd.py
@@ -254,7 +254,7 @@ def test_sgd_estimator_params_validation(klass, fit_method, params, err_msg):
     try:
         sgd_estimator = klass(**params)
     except TypeError as err:
-        if "__init__() got an unexpected keyword argument" in str(err):
+        if "unexpected keyword argument" in str(err):
             # skip test if the parameter is not supported by the estimator
             return
         raise err
