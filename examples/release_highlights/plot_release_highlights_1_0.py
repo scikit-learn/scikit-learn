da41d0a2ab90b905dfd17f256772b6ff9b64f87a
Adrin Jalali <adrin.jalali@gmail.com>
2021-09-09 12:06:57 +0200

    Update examples/release_highlights/plot_release_highlights_1_0.py
    Co-authored-by: Tom Dupré la Tour <tom.dupre-la-tour@m4x.org>

--- examples/release_highlights/plot_release_highlights_1_0.py
+++ examples/release_highlights/plot_release_highlights_1_0.py
@@ -71,7 +71,7 @@ or with conda::
 # Spline Transformers
 # ---------------------------------------------------------
 # One way to add nonlinear terms to the dataset's feature set is to generate
-# spline basis functions for each feature with the
+# spline basis functions for each feature with the new
 # :class:`preprocessing.SplineTransformer`. Splines are piecewise polynomials,
 # parametrized by their polynomial degree and the positions of the knots. The
 # :class:`preprocessingSplineTransformer` implements a B-spline basis
