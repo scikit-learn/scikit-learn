2fcbf40d80257842338ce1cc627fc6f216b17037
EricEllwanger <52634304+EricEllwanger@users.noreply.github.com>
2021-09-13 05:12:51 -0400

    DOC - Ensures that GridSearchCV passes numpydoc validation (#21003)

--- maint_tools/test_docstrings.py
+++ maint_tools/test_docstrings.py
@@ -9,7 +9,6 @@ numpydoc_validation = pytest.importorskip("numpydoc.validate")
 
 # List of modules ignored when checking for numpydoc validation.
 DOCSTRING_IGNORE_LIST = [
-    "GridSearchCV",
     "HalvingGridSearchCV",
     "HalvingRandomSearchCV",
     "HashingVectorizer",
