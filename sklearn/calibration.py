1423fb7e624dc8105aa2521a9bf0368bc6c782fc
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2021-09-14 13:43:27 +0200

    ENH add pos_label to CalibrationDisplay

--- sklearn/calibration.py
+++ sklearn/calibration.py
@@ -996,6 +996,13 @@ class CalibrationDisplay:
     estimator_name : str, default=None
         Name of estimator. If None, the estimator name is not shown.
 
+    pos_label : str or int, default=None
+        The class considered as the positive class when computing the
+        calibration curve. By default, `estimators.classes_[1]` is considered
+        as the positive class.
+
+        .. versionadded:: 1.1
+
     Attributes
     ----------
     line_ : matplotlib Artist
@@ -1035,11 +1042,14 @@ class CalibrationDisplay:
     <...>
     """
 
-    def __init__(self, prob_true, prob_pred, y_prob, *, estimator_name=None):
+    def __init__(
+        self, prob_true, prob_pred, y_prob, *, estimator_name=None, pos_label=None
+    ):
         self.prob_true = prob_true
         self.prob_pred = prob_pred
         self.y_prob = y_prob
         self.estimator_name = estimator_name
+        self.pos_label = pos_label
 
     def plot(self, *, ax=None, name=None, ref_line=True, **kwargs):
         """Plot visualization.
@@ -1106,6 +1116,7 @@ class CalibrationDisplay:
         *,
         n_bins=5,
         strategy="uniform",
+        pos_label=None,
         name=None,
         ref_line=True,
         ax=None,
@@ -1150,6 +1161,13 @@ class CalibrationDisplay:
             - `'uniform'`: The bins have identical widths.
             - `'quantile'`: The bins have the same number of samples and depend
               on predicted probabilities.
+
+        pos_label : str or int, default=None
+            The class considered as the positive class when computing the
+            calibration curve. By default, `estimators.classes_[1]` is
+            considered as the positive class.
+
+            .. versionadded:: 1.1
 
         name : str, default=None
             Name for labeling curve. If `None`, the name of the estimator is
@@ -1198,10 +1216,8 @@ class CalibrationDisplay:
         if not is_classifier(estimator):
             raise ValueError("'estimator' should be a fitted classifier.")
 
-        # FIXME: `pos_label` should not be set to None
-        # We should allow any int or string in `calibration_curve`.
-        y_prob, _ = _get_response(
-            X, estimator, response_method="predict_proba", pos_label=None
+        y_prob, pos_label = _get_response(
+            X, estimator, response_method="predict_proba", pos_label=pos_label
         )
 
         name = name if name is not None else estimator.__class__.__name__
@@ -1210,6 +1226,7 @@ class CalibrationDisplay:
             y_prob,
             n_bins=n_bins,
             strategy=strategy,
+            pos_label=pos_label,
             name=name,
             ref_line=ref_line,
             ax=ax,
@@ -1224,6 +1241,7 @@ class CalibrationDisplay:
         *,
         n_bins=5,
         strategy="uniform",
+        pos_label=None,
         name=None,
         ref_line=True,
         ax=None,
@@ -1263,6 +1281,13 @@ class CalibrationDisplay:
             - `'uniform'`: The bins have identical widths.
             - `'quantile'`: The bins have the same number of samples and depend
               on predicted probabilities.
+
+        pos_label : str or int, default=None
+            The class considered as the positive class when computing the
+            calibration curve. By default, `estimators.classes_[1]` is
+            considered as the positive class.
+
+            .. versionadded:: 1.1
 
         name : str, default=None
             Name for labeling curve.
@@ -1311,9 +1336,14 @@ class CalibrationDisplay:
         prob_true, prob_pred = calibration_curve(
             y_true, y_prob, n_bins=n_bins, strategy=strategy
         )
-        name = name if name is not None else "Classifier"
+        name = "Classifier" if name is None else name
+        pos_label = _check_pos_label_consistency(pos_label, y_true)
 
         disp = cls(
-            prob_true=prob_true, prob_pred=prob_pred, y_prob=y_prob, estimator_name=name
+            prob_true=prob_true,
+            prob_pred=prob_pred,
+            y_prob=y_prob,
+            estimator_name=name,
+            pos_label=pos_label,
         )
         return disp.plot(ax=ax, ref_line=ref_line, **kwargs)
