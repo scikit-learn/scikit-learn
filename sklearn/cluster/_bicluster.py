2aa25fa060a6d44cf2aa76facfb035511444586a
Nadirhan Şahin <73655475+creatornadiran@users.noreply.github.com>
2021-09-16 02:23:50 +0300

    Update _bicluster.py

--- sklearn/cluster/_bicluster.py
+++ sklearn/cluster/_bicluster.py
@@ -527,8 +527,8 @@ class SpectralBiclustering(BaseSpectral):
                 )
             )
         check_scalar(self.n_clusters, "n_clusters",
-                     target_type=(numbers.Integral, 
-                     (numbers.Integral, numbers.Integral)),
+                     target_type=(numbers.Integral,
+                    (numbers.Integral, numbers.Integral)),
                      min_val=1,
                      max_val=n_samples)
         try:
