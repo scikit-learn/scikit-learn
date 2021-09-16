d6b16cf8bbc6e7073275a7180af0bcdc87740d03
EricEllwanger <52634304+EricEllwanger@users.noreply.github.com>
2021-09-16 17:49:09 -0400

    Update _huber.py
    Added suggestions

--- sklearn/linear_model/_huber.py
+++ sklearn/linear_model/_huber.py
@@ -209,6 +209,7 @@ class HuberRegressor(LinearModel, RegressorMixin, BaseEstimator):
     --------
     RANSACRegressor : RANSAC (RANdom SAmple Consensus) algorithm.
     TheilSenRegressor : Theil-Sen Estimator robust multivariate regression model.
+    SGDRegressor : Fitted by minimizing a regularized empirical loss with SGD
 
     References
     ----------
