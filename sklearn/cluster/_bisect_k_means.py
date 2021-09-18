71677742e52d1080ab954cf77b177ff44887f837
michalkrawczyk <mkrwczyk.1@gmail.com>
2021-09-18 13:41:43 +0200

    feature_names_in

--- sklearn/cluster/_bisect_k_means.py
+++ sklearn/cluster/_bisect_k_means.py
@@ -195,6 +195,11 @@ class BisectKMeans(KMeans):
 
         .. versionadded:: 0.24
 
+    feature_names_in_ : ndarray of shape (`n_features_in_`,)
+        Names of features seen during :term:`fit`. Defined only when `X`
+        has feature names that are all strings.
+        .. versionadded:: 1.0
+
     See Also
     --------
     KMeans : Original implementation of K-Means algorithm.
