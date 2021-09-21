d63722f9e639797e0c31597d70abcd2402410655
Dani El-Ayyass <dayyass@yandex.ru>
2021-09-21 22:39:43 +0300

    add utils functions

--- sklearn/cluster/_graph_based.py
+++ sklearn/cluster/_graph_based.py
@@ -2,3 +2,67 @@
 
 # Authors: Dani El-Ayyass <dayyass@yandex.ru>
 # License: BSD 3 clause
+
+
+from typing import Callable, Optional, Tuple, Union
+
+import numpy as np
+from ..metrics import pairwise_distances
+
+# from .check import _check_matrix, check_symmetric
+
+
+def _pairwise_distances(
+    X: np.ndarray,
+    metric: Union[str, Callable] = "euclidean",
+    n_jobs: Optional[int] = None,
+) -> np.ndarray:
+    """TODO"""
+
+    # assert _check_matrix(X)
+
+    distances = pairwise_distances(X=X, metric=metric, n_jobs=n_jobs)
+
+    return distances
+
+
+def distances_to_adjacency_matrix(
+    distances: np.ndarray,
+    threshold: float,
+) -> np.ndarray:
+    """TODO"""
+
+    # assert check_symmetric(distances)
+
+    N = distances.shape[0]
+
+    adjacency_matrix = (distances < threshold).astype(int) - np.eye(N, dtype=int)
+
+    return adjacency_matrix
+
+
+def span_tree_top_n_weights_idx(
+    span_tree: np.ndarray,
+    n: int,
+) -> Tuple[np.ndarray, np.ndarray]:
+    """TODO"""
+
+    span_tree_shape = span_tree.shape
+    N = span_tree_shape[0]
+
+    if n == 0:
+        array_1_to_N = np.array(range(N))
+        unravel_idx = (array_1_to_N, array_1_to_N)
+
+    else:
+        ravel_span_tree_top_n_weights_idx = np.argpartition(
+            a=span_tree.ravel(),
+            kth=-n,
+        )[-n:]
+
+        unravel_idx = np.unravel_index(
+            indices=ravel_span_tree_top_n_weights_idx,
+            shape=span_tree_shape,
+        )
+
+    return unravel_idx
