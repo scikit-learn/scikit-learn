b424204a8a77e124ea4dbe07918e8ee4d23d8d65
nikJ <niketjaina4@gmail.com>
2021-09-24 13:21:48 +0530

    removed_randomized_from_list

--- maint_tools/test_docstrings.py
+++ maint_tools/test_docstrings.py
@@ -32,7 +32,6 @@ DOCSTRING_IGNORE_LIST = [
     "PatchExtractor",
     "PolynomialFeatures",
     "QuadraticDiscriminantAnalysis",
-    #"RandomizedSearchCV",
     "RobustScaler",
     "SGDRegressor",
     "SelfTrainingClassifier",
