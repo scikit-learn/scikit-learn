e5887b00ea3fcee05614716d0af06bb945b7e9b5
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2021-09-24 17:04:51 +0200

    [CI] Add push step.

--- build_tools/circle/push_doc.sh
+++ build_tools/circle/push_doc.sh
@@ -1,50 +1,47 @@
 #!/bin/bash
-# This script is meant to be called in the "deploy" step defined in
-# circle.yml. See https://circleci.com/docs/ for more details.
-# The behavior of the script is controlled by environment variable defined
-# in the circle.yml in the top level folder of the project.
+
+# The behavior of the script is controlled by environment variable
+# defined in the docs.yml in the actions level folder of the project.
 
 set -ex
 
 DOC_REPO="scikit-learn.github.io"
 
-if [ "$CIRCLE_BRANCH" = "main" ]
+if [ "$GITHUB_REF" =~ "main" ]
 then
-    dir=dev
+    DIR=dev
 else
     # Strip off .X
-    dir="${CIRCLE_BRANCH::-2}"
+    DIR="${GITHUB_REF::-2}"
 fi
 
-MSG="Pushing the docs to $dir/ for branch: $CIRCLE_BRANCH, commit $CIRCLE_SHA1"
-
-cd $HOME
-if [ ! -d $DOC_REPO ];
-then git clone --depth 1 --no-checkout "git@github.com:scikit-learn/"$DOC_REPO".git";
-fi
-cd $DOC_REPO
+MSG="Pushing the docs to $DIR/ for branch: $GITHUB_REF, commit $GITHUB_SHA"
 
 # check if it's a new branch
+echo $DIR > .git/info/sparse-checkout
 
-echo $dir > .git/info/sparse-checkout
-if ! git show HEAD:$dir >/dev/null
+if ! git show HEAD:$DIR >/dev/null
 then
 	# directory does not exist. Need to make it so sparse checkout works
-	mkdir $dir
-	touch $dir/index.html
-	git add $dir
+	mkdir $DIR
+	touch $DIR/index.html
+	git add $DIR
 fi
 git checkout main
 git reset --hard origin/main
-if [ -d $dir ]
+if [ -d $DIR ]
 then
-	git rm -rf $dir/ && rm -rf $dir/
+	git rm -rf $DIR/ && rm -rf $DIR/
 fi
-cp -R $GENERATED_DOC_DIR $dir
+
+cp -R $GITHUB_WORKSPACE/docs $DIR
+
 git config user.email "olivier.grisel+sklearn-ci@gmail.com"
 git config user.name "sklearn-ci"
 git config push.default matching
-git add -f $dir/
-git commit -m "$MSG" $dir
+
+git add -f $DIR/
+git commit -m "$MSG" $DIR
 git push
+
 echo $MSG
