360b2f71528a4c3bcbbed02eda3a9acd3db3a57a
Andrew Knyazev <andrew.knyazev@ucdenver.edu>
2021-09-25 14:43:00 -0400

    Update test_spectral.py
    lint fix

--- sklearn/cluster/tests/test_spectral.py
+++ sklearn/cluster/tests/test_spectral.py
@@ -12,7 +12,6 @@ from sklearn.utils import check_random_state
 from sklearn.utils._testing import assert_array_equal
 
 from sklearn.cluster import SpectralClustering, spectral_clustering
-from sklearn.cluster._spectral import cluster_qr
 from sklearn.cluster._spectral import discretize
 from sklearn.feature_extraction import img_to_graph
 from sklearn.metrics import pairwise_distances
