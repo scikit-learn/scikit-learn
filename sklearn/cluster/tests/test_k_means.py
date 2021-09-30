2c8d09c589d9b11ae54d08370432478c472e1fa2
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2021-09-30 17:57:21 +0200

    more sanity check

--- sklearn/cluster/tests/test_k_means.py
+++ sklearn/cluster/tests/test_k_means.py
@@ -1179,6 +1179,7 @@ def test_kmeans_plusplus_dataorder():
 def test_is_same_clustering():
     # Sanity check for the _is_same_clustering utility function
     labels1 = np.array([1, 0, 0, 1, 2, 0, 2, 1], dtype=np.int32)
+    assert _is_same_clustering(labels1, labels1, 3)
 
     # these other labels represent the same clustering since we can retrive the first
     # labels by simply renaming the labels: 0 -> 1, 1 -> 2, 2 -> 0.
