13008678cdf9923ca71035ad871e57b5f9d500ee
adrinjalali <adrin.jalali@gmail.com>
2021-10-01 17:05:53 +0200

    model -> est in _ridge.py

--- sklearn/linear_model/_ridge.py
+++ sklearn/linear_model/_ridge.py
@@ -1977,9 +1977,9 @@ class _BaseRidgeCV(LinearModel):
                 raise ValueError("cv!=None and alpha_per_target=True are incompatible")
             parameters = {"alpha": self.alphas}
             solver = "sparse_cg" if sparse.issparse(X) else "auto"
-            model = RidgeClassifier if is_classifier(self) else Ridge
-            model = (
-                model(
+            est = RidgeClassifier if is_classifier(self) else Ridge
+            est = (
+                est(
                     fit_intercept=self.fit_intercept,
                     normalize=self.normalize,
                     solver=solver,
@@ -1991,7 +1991,7 @@ class _BaseRidgeCV(LinearModel):
             # Do we want to "fix" the issue, or keep the old behavior?
 
             gs = GridSearchCV(
-                model,
+                est,
                 parameters,
                 cv=cv,
                 scoring=self.scoring,
