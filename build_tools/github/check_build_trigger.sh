578d849807bbc6f357c23147876032943ed74115
Thomas J. Fan <thomasjpfan@gmail.com>
2021-10-03 18:59:55 -0400

    BLD Test matrix [cd build gh]

--- build_tools/github/check_build_trigger.sh
+++ build_tools/github/check_build_trigger.sh
@@ -3,11 +3,12 @@
 set -e
 set -x
 
-COMMIT_MSG=$(git log --no-merges -1 --oneline)
+# COMMIT_MSG=$(git log --no-merges -1 --oneline)
+COMMIT_MSG="wow"
 
 # The commit marker "[cd build]" will trigger the build when required
 if [[ "$GITHUB_EVENT_NAME" == schedule ||
       "$COMMIT_MSG" =~ \[cd\ build\] ||
-      "$COMMIT_MSG" =~ \[cd\ build gh\] ]]; then
+      "$COMMIT_MSG" =~ \[cd\ build\ gh\] ]]; then
     echo "::set-output name=build::true"
 fi
