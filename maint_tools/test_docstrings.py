0401bd5a7cfd87d1e8f5d50646aa81a896e012c0
Juan Martin Loyola <jmloyola@outlook.com>
2021-10-04 10:52:32 -0300

    DOC Ensures that PatchExtractor passes numpydoc validation (#21233)

--- maint_tools/test_docstrings.py
+++ maint_tools/test_docstrings.py
@@ -22,7 +22,6 @@ DOCSTRING_IGNORE_LIST = [
     "OrthogonalMatchingPursuitCV",
     "PassiveAggressiveClassifier",
     "PassiveAggressiveRegressor",
-    "PatchExtractor",
     "PolynomialFeatures",
     "QuadraticDiscriminantAnalysis",
     "SelfTrainingClassifier",
