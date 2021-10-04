6471b97ec6abc5978ab40fa27edbd4326c1db4c3
Thomas J. Fan <thomasjpfan@gmail.com>
2021-10-03 22:38:19 -0400

    BLD Test build matrix [cd build gh]

--- build_tools/github/check_wheels.py
+++ build_tools/github/check_wheels.py
@@ -1,33 +1,22 @@
-"""Checks that dist/* contains the number of wheels built from the
-.github/workflows/wheels.yml config."""
-import yaml
+"""Checks that dist/* contains the number of wheels."""
 from pathlib import Path
 import sys
 
-gh_wheel_path = Path.cwd() / ".github" / "workflows" / "wheels.yml"
-with gh_wheel_path.open("r") as f:
-    wheel_config = yaml.safe_load(f)
-
-build_matrix = wheel_config["jobs"]["build_wheels"]["strategy"]["matrix"]
-n_python_versions = len(build_matrix["python"])
-
-# For each python version we have: 7 wheels
+# Python 3.7, 3.8, 3.9 each has 7 wheels
 # 1 osx wheel (x86_64)
 # 4 linux wheel (i686 + x86_64) * (manylinux1 + manylinux2010)
 # 2 windows wheel (win32 + wind_amd64)
-n_wheels = 7 * n_python_versions
+n_wheels = 3 * 7
+
+# NumPy on Python 3.10 only supports 64bit and is only avaliable with manylinux2014
+# With macos and window support the number of wheels should go up to 3
+n_wheels += 1
 
 # plus one more for the sdist
 n_wheels += 1
 
-# aarch64 builds from travis
-travis_config_path = Path.cwd() / ".travis.yml"
-with travis_config_path.open("r") as f:
-    travis_config = yaml.safe_load(f)
-
-jobs = travis_config["jobs"]["include"]
-travis_builds = [j for j in jobs if any("CIBW_BUILD" in env for env in j["env"])]
-n_wheels += len(travis_builds)
+# aarch64 builds from travis has builds for Python 3.7, 3.8, 3.9
+n_wheels += 3
 
 dist_files = list(Path("dist").glob("**/*"))
 n_dist_files = len(dist_files)
