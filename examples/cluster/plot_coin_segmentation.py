02d380450f17dac88c44b33bebfb4f25f0ffda01
Andrew Knyazev <andrew.knyazev@ucdenver.edu>
2021-10-07 15:47:44 -0400

    Update plot_coin_segmentation.py
    3 -> 4

--- examples/cluster/plot_coin_segmentation.py
+++ examples/cluster/plot_coin_segmentation.py
@@ -105,7 +105,7 @@ for assign_labels in ("kmeans", "discretize", "cluster_qr"):
     print(title)
     plt.title(title)
     for l in range(n_regions):
-        colors = [plt.cm.nipy_spectral((l + 3) / float(n_regions + 3))]
+        colors = [plt.cm.nipy_spectral((l + 4) / float(n_regions + 4))]
         plt.contour(labels == l, colors=colors)
         plt.pause(0.5)
 plt.show()
