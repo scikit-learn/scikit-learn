94a94ced3ddfcb0243149479b98874e80e51f3b9
michalkrawczyk <mkrwczyk.1@gmail.com>
2021-10-07 20:08:59 +0200

    Added Missing Tests

--- sklearn/cluster/tests/test_bisect_k_means.py
+++ sklearn/cluster/tests/test_bisect_k_means.py
@@ -1,3 +1,6 @@
+from re import search
+from warnings import simplefilter
+
 import numpy as np
 import scipy.sparse as sp
 
@@ -50,6 +53,10 @@ def test_sparse():
     bisect_means = BisectKMeans(n_clusters=3, random_state=0)
     bisect_means.fit(X_csr)
 
+    # Check if labels obtained from fit(X) are equal to fit(X).predict(X)
+    # X_csr is passed here to check also predict for sparse
+    assert_array_almost_equal(bisect_means.labels_, bisect_means.predict(X_csr))
+
     sparse_centers = bisect_means.cluster_centers_
 
     bisect_means.fit(X)
@@ -57,9 +64,6 @@ def test_sparse():
 
     # Check if results is the same for dense and sparse data
     assert_array_almost_equal(normal_centers, sparse_centers)
-
-    # Check if labels obtained from fit(X) are equal to fit(X).predict(X)
-    assert_array_almost_equal(bisect_means.labels_, bisect_means.predict(X))
 
 
 @pytest.mark.parametrize("n_clusters", [4, 5])
@@ -73,3 +77,77 @@ def test_n_clusters(n_clusters):
     bisect_means.fit(X)
 
     assert_array_equal(np.unique(bisect_means.labels_), np.arange(n_clusters))
+
+
+def test_one_cluster():
+    """Test warnings and performance for n_cluster = 1"""
+
+    X = np.array([[1, 2], [10, 2], [10, 8]])
+
+    with pytest.warns(None) as w:
+        bisect_means = BisectKMeans(n_clusters=1, random_state=0)
+        bisect_means.fit(X)
+        msg = (
+            "BisectKMeans might be inefficient for n_cluster smaller than 3 "
+            + " - Use Normal KMeans from sklearn.cluster instead."
+        )
+        assert str(w[0].message) == msg
+
+        msg = "Bisection won't be performed - needs at least two clusters to run."
+        assert str(w[1].message) == msg
+
+        # All labels from fit or predict should be equal 0
+        assert all(bisect_means.predict(X) == 0)
+
+
+@pytest.mark.parametrize(
+    "param, match, single_value",
+    [
+        # Test bisect_strategy param
+        (
+            {"bisect_strategy": "None"},
+            r"Bisect Strategy must be 'biggest_sse', or 'largest_cluster'",
+            False,
+        ),
+        # Test init array
+        (
+            {"init": np.ones((5, 2))},
+            "BisectKMeans does not support init as array.",
+            False,
+        ),
+        # Test single X value
+        (
+            {"n_clusters": 1},
+            "BisectKMeans needs more than one sample to perform bisection.",
+            True,
+        ),
+    ],
+)
+def test_wrong_params(param, match, single_value):
+    """Test Exceptions at check_params function"""
+
+    simplefilter("ignore")
+
+    if single_value:
+        X = np.ones((1, 1))
+    else:
+        rng = np.random.RandomState(0)
+        X = rng.rand(5, 2)
+
+    with pytest.raises(ValueError, match=match):
+        bisect_means = BisectKMeans(n_clusters=3, n_init=1)
+        bisect_means.set_params(**param)
+        bisect_means.fit(X)
+
+
+def test_verbose(capsys):
+    """Test Verbose mode"""
+    rng = np.random.RandomState(0)
+    X = rng.rand(5, 2)
+
+    bisect_means = BisectKMeans(n_clusters=3, verbose=1)
+    bisect_means.fit(X)
+
+    captured = capsys.readouterr()
+
+    assert search(r"Running Bisecting K-Means", captured.out)
