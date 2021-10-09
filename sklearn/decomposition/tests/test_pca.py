334412b887bf6247bf9c9d4acbdb0be54ccf869c
x-shadow-man <1494445739@qq.com>
2021-10-09 15:07:05 +0800

    update test_pca

--- sklearn/decomposition/tests/test_pca.py
+++ sklearn/decomposition/tests/test_pca.py
@@ -682,8 +682,8 @@ def test_pca_svd_output(nfeat, seed):
     U2, s2, Vh2 = svd(X, full_matrices=False)
     X_tranformed2 = U2[:, 0] * s2[0]
 
-    assert_allclose(X_tranformed, X_tranformed1, rtol=10)
-    assert_allclose(X_tranformed, X_tranformed2, rtol=10)
+    assert not all(X_tranformed - X_tranformed1)
+    assert not all(X_tranformed - X_tranformed2)
 
 
 @pytest.mark.parametrize(
@@ -691,13 +691,13 @@ def test_pca_svd_output(nfeat, seed):
     [
         (
             X,
-            {"n_oversamples_rate": 0},
+            {"n_components": 1, "n_oversamples_rate": -1},
             ValueError,
             "n_oversamples_rate must be >= 0",
         ),
         (
             X,
-            {"n_oversamples_rate": 1},
+            {"n_components": 1, "n_oversamples_rate": 2},
             ValueError,
             "n_oversamples_rate must be <= 1",
         ),
