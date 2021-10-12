6b3d12eb1261fb5367a8387afe09bcb48a9623b4
Patrick de Carvalho Tavares Rezende Ferreira <patrickctrf@gmail.com>
2021-10-12 02:29:40 -0300

    refactor: Undo Black changes.

--- sklearn/gaussian_process/tests/test_gpr.py
+++ sklearn/gaussian_process/tests/test_gpr.py
@@ -45,7 +45,7 @@ kernels = [
     + C(1e-5, (1e-5, 1e2)),
     C(0.1, (1e-2, 1e2)) * RBF(length_scale=1.0, length_scale_bounds=(1e-3, 1e3))
     + C(1e-5, (1e-5, 1e2)),
-]
+    ]
 non_fixed_kernels = [kernel for kernel in kernels if kernel != fixed_kernel]
 
 
@@ -179,7 +179,7 @@ def test_sample_statistics(kernel):
         np.diag(y_cov) / np.diag(y_cov).max(),
         np.var(samples, 1) / np.diag(y_cov).max(),
         1,
-    )
+        )
 
 
 def test_no_optimizer():
@@ -223,9 +223,9 @@ def test_random_starts():
     rng = np.random.RandomState(0)
     X = rng.randn(n_samples, n_features) * 2 - 1
     y = (
-        np.sin(X).sum(axis=1)
-        + np.sin(3 * X).sum(axis=1)
-        + rng.normal(scale=0.1, size=n_samples)
+            np.sin(X).sum(axis=1)
+            + np.sin(3 * X).sum(axis=1)
+            + rng.normal(scale=0.1, size=n_samples)
     )
 
     kernel = C(1.0, (1e-2, 1e2)) * RBF(
@@ -234,7 +234,9 @@ def test_random_starts():
     last_lml = -np.inf
     for n_restarts_optimizer in range(5):
         gp = GaussianProcessRegressor(
-            kernel=kernel, n_restarts_optimizer=n_restarts_optimizer, random_state=0
+            kernel=kernel,
+            n_restarts_optimizer=n_restarts_optimizer,
+            random_state=0,
         ).fit(X, y)
         lml = gp.log_marginal_likelihood(gp.kernel_.theta)
         assert lml > last_lml - np.finfo(np.float32).eps
@@ -383,9 +385,8 @@ def test_custom_optimizer(kernel):
     # Define a dummy optimizer that simply tests 50 random hyperparameters
     def optimizer(obj_func, initial_theta, bounds):
         rng = np.random.RandomState(0)
-        theta_opt, func_min = (
-            initial_theta,
-            obj_func(initial_theta, eval_gradient=False),
+        theta_opt, func_min = initial_theta, obj_func(
+            initial_theta, eval_gradient=False
         )
         for _ in range(50):
             theta = np.atleast_1d(
@@ -410,10 +411,10 @@ def test_gpr_correct_error_message():
     kernel = DotProduct()
     gpr = GaussianProcessRegressor(kernel=kernel, alpha=0.0)
     message = (
-        "The kernel, %s, is not returning a "
-        "positive definite matrix. Try gradually increasing "
-        "the 'alpha' parameter of your "
-        "GaussianProcessRegressor estimator." % kernel
+            "The kernel, %s, is not returning a "
+            "positive definite matrix. Try gradually increasing "
+            "the 'alpha' parameter of your "
+            "GaussianProcessRegressor estimator." % kernel
     )
     with pytest.raises(np.linalg.LinAlgError, match=re.escape(message)):
         gpr.fit(X, y)
@@ -482,23 +483,23 @@ def test_warning_bounds():
 
     assert len(record) == 2
     assert (
-        record[0].message.args[0]
-        == "The optimal value found for "
-        "dimension 0 of parameter "
-        "k1__noise_level is close to the "
-        "specified upper bound 0.001. "
-        "Increasing the bound and calling "
-        "fit again may find a better value."
+            record[0].message.args[0]
+            == "The optimal value found for "
+               "dimension 0 of parameter "
+               "k1__noise_level is close to the "
+               "specified upper bound 0.001. "
+               "Increasing the bound and calling "
+               "fit again may find a better value."
     )
 
     assert (
-        record[1].message.args[0]
-        == "The optimal value found for "
-        "dimension 0 of parameter "
-        "k2__length_scale is close to the "
-        "specified lower bound 1000.0. "
-        "Decreasing the bound and calling "
-        "fit again may find a better value."
+            record[1].message.args[0]
+            == "The optimal value found for "
+               "dimension 0 of parameter "
+               "k2__length_scale is close to the "
+               "specified lower bound 1000.0. "
+               "Decreasing the bound and calling "
+               "fit again may find a better value."
     )
 
     X_tile = np.tile(X, 2)
@@ -513,23 +514,23 @@ def test_warning_bounds():
 
     assert len(record) == 2
     assert (
-        record[0].message.args[0]
-        == "The optimal value found for "
-        "dimension 0 of parameter "
-        "length_scale is close to the "
-        "specified lower bound 10.0. "
-        "Decreasing the bound and calling "
-        "fit again may find a better value."
+            record[0].message.args[0]
+            == "The optimal value found for "
+               "dimension 0 of parameter "
+               "length_scale is close to the "
+               "specified lower bound 10.0. "
+               "Decreasing the bound and calling "
+               "fit again may find a better value."
     )
 
     assert (
-        record[1].message.args[0]
-        == "The optimal value found for "
-        "dimension 1 of parameter "
-        "length_scale is close to the "
-        "specified lower bound 10.0. "
-        "Decreasing the bound and calling "
-        "fit again may find a better value."
+            record[1].message.args[0]
+            == "The optimal value found for "
+               "dimension 1 of parameter "
+               "length_scale is close to the "
+               "specified lower bound 10.0. "
+               "Decreasing the bound and calling "
+               "fit again may find a better value."
     )
 
 
@@ -619,12 +620,12 @@ def test_gpr_consistency_std_cov_non_invertible_kernel():
         ({"kernel": RBF(), "optimizer": "unknown"}, ValueError, "Unknown optimizer"),
         ({"alpha": np.zeros(100)}, ValueError, "alpha must be a scalar or an array"),
         (
-            {
-                "kernel": WhiteKernel(noise_level_bounds=(-np.inf, np.inf)),
-                "n_restarts_optimizer": 2,
-            },
-            ValueError,
-            "requires that all bounds are finite",
+                {
+                    "kernel": WhiteKernel(noise_level_bounds=(-np.inf, np.inf)),
+                    "n_restarts_optimizer": 2,
+                },
+                ValueError,
+                "requires that all bounds are finite",
         ),
     ],
 )
