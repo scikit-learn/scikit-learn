8e6b7e3c8347308aceaada93eb63d008ac32ce44
spikebh <spikebh@gmail.com>
2021-10-21 10:56:37 -0300

    fixing validation errors for MultiTaskElasticNetCV estimator test

--- maint_tools/test_docstrings.py
+++ maint_tools/test_docstrings.py
@@ -17,7 +17,6 @@ DOCSTRING_IGNORE_LIST = [
     "LabelPropagation",
     "LabelSpreading",
     "LocallyLinearEmbedding",
-    "MultiTaskElasticNetCV",
     "MultiTaskLassoCV",
     "OrthogonalMatchingPursuitCV",
     "PassiveAggressiveRegressor",
