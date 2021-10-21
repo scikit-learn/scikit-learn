941938cb75246f08189f422eb509bb0f900a5b2b
KUNAL.MANDALYA <KUNAL.MANDALYA@lntinfotech.com>
2021-10-21 10:29:10 +0530

    fix numpydoc validation

--- sklearn/cluster/_bicluster.py
+++ sklearn/cluster/_bicluster.py
@@ -445,7 +445,6 @@ class SpectralBiclustering(BaseSpectral):
 
     References
     ----------
-
     * Kluger, Yuval, et. al., 2003. `Spectral biclustering of microarray
       data: coclustering genes and conditions
       <http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.135.1608>`__.
