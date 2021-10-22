713e7445a23f8b6d1ff2e7df0a280cb3287de1a8
Gabriel Stefanini Vicente <gabrielusvicente@gmail.com>
2021-10-22 15:24:50 -0400

    DOC Ensures that SpectralEmbedding passes numpydoc validation

--- maint_tools/test_docstrings.py
+++ maint_tools/test_docstrings.py
@@ -18,7 +18,6 @@ DOCSTRING_IGNORE_LIST = [
     "OrthogonalMatchingPursuitCV",
     "PassiveAggressiveRegressor",
     "SpectralCoclustering",
-    "SpectralEmbedding",
     "StackingRegressor",
 ]
 
