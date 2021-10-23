41e0e84e622215f7bb11382b1fefa624cac2dd4d
Alberto Ceballos <alceballosa@unal.edu.co>
2021-10-23 12:39:43 -0400

    Added description for the default=None case of the n_components parameter in MiniBatchSparsePCA.

--- sklearn/decomposition/_sparse_pca.py
+++ sklearn/decomposition/_sparse_pca.py
@@ -248,7 +248,8 @@ class MiniBatchSparsePCA(SparsePCA):
     Parameters
     ----------
     n_components : int, default=None
-        Number of sparse atoms to extract.
+        Number of sparse atoms to extract. If None, then ``n_components``
+        is set to ``n_features_in_``.
 
     alpha : int, default=1
         Sparsity controlling parameter. Higher values lead to sparser
