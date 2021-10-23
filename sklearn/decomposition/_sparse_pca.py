f39b695bb5ebaea2309416e2d3429904d4fb7c8e
Alberto Mario Ceballos-Arroyo <alceballosa@unal.edu.co>
2021-10-23 12:35:23 -0400

    DOC Added the meaning of the default=None case in SparsePCA (#21421)
    Co-authored-by: Thomas J. Fan <thomasjpfan@gmail.com>

--- sklearn/decomposition/_sparse_pca.py
+++ sklearn/decomposition/_sparse_pca.py
@@ -23,7 +23,8 @@ class SparsePCA(TransformerMixin, BaseEstimator):
     Parameters
     ----------
     n_components : int, default=None
-        Number of sparse atoms to extract.
+        Number of sparse atoms to extract. If None, then ``n_components``
+        is set to ``n_features_in_``.
 
     alpha : float, default=1
         Sparsity controlling parameter. Higher values lead to sparser
