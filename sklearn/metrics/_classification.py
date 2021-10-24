565678aabd1752d886831364a02899e488c8fe5b
Mitzi <46392144+embandera@users.noreply.github.com>
2021-10-24 09:55:27 -0700

    Update sklearn/metrics/_classification.py
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- sklearn/metrics/_classification.py
+++ sklearn/metrics/_classification.py
@@ -178,12 +178,12 @@ def accuracy_score(y_true, y_pred, *, normalize=True, sample_weight=None):
     See Also
     --------
     balanced_accuracy_score : Compute the balanced accuracy to deal with
-    imbalanced datasets.
+        imbalanced datasets.
     jaccard_score : Compute the Jaccard similarity coefficient score.
     hamming_loss : Compute the average Hamming loss or Hamming distance between
-    two sets of samples.
+        two sets of samples.
     zero_one_loss : Compute the Zero-one classification loss. By default, the
-    function will return the percentage of imperfectly predicted subsets.
+        function will return the percentage of imperfectly predicted subsets.
 
     Notes
     -----
