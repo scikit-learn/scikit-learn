b924d3458d118c971f4411784d6a13259cd315b5
genvalen <genvalen@protonmail.com>
2021-10-29 15:08:49 -0400

    Update sklearn/ensemble/_weight_boosting.py
    Co-authored-by: Thomas J. Fan <thomasjpfan@gmail.com>

--- sklearn/ensemble/_weight_boosting.py
+++ sklearn/ensemble/_weight_boosting.py
@@ -499,7 +499,7 @@ class AdaBoostClassifier(ClassifierMixin, BaseWeightBoosting):
         # Check that algorithm is supported
         if self.algorithm not in ("SAMME", "SAMME.R"):
             raise ValueError(
-                f"Algorithm must be 'SAMME' or 'SAMME.R'. Got {self.algorithm} instead."
+                f"Algorithm must be 'SAMME' or 'SAMME.R'. Got {self.algorithm!r} instead."
             )
 
         # Fit
