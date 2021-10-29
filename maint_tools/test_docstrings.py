c3b9d529db44c642572b4c707f150779dd9b69f4
miwojc <32404415+miwojc@users.noreply.github.com>
2021-10-29 19:15:37 +0200

    DOC Ensures that config_context passes numpydoc validation (#21426)
    Co-authored-by: majauhar <siddmohamed19@gmail.com>
    Co-authored-by: Thomas J. Fan <thomasjpfan@gmail.com>

--- maint_tools/test_docstrings.py
+++ maint_tools/test_docstrings.py
@@ -12,7 +12,6 @@ import sklearn
 numpydoc_validation = pytest.importorskip("numpydoc.validate")
 
 FUNCTION_DOCSTRING_IGNORE_LIST = [
-    "sklearn._config.config_context",
     "sklearn._config.get_config",
     "sklearn.base.clone",
     "sklearn.cluster._affinity_propagation.affinity_propagation",
