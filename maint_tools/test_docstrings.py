53016b854f87e4dddc85b1a7c91baeebc7313083
MaggieChege <shikooh.maggie@gmail.com>
2021-10-29 18:16:40 +0300

    DOC Ensures that as_float_array passes numpydoc validation (#21502)
    Co-authored-by: ciku <maggie@hellotractor.com>

--- maint_tools/test_docstrings.py
+++ maint_tools/test_docstrings.py
@@ -220,7 +220,6 @@ FUNCTION_DOCSTRING_IGNORE_LIST = [
     "sklearn.utils.sparsefuncs.mean_variance_axis",
     "sklearn.utils.sparsefuncs.min_max_axis",
     "sklearn.utils.tosequence",
-    "sklearn.utils.validation.as_float_array",
     "sklearn.utils.validation.assert_all_finite",
     "sklearn.utils.validation.check_is_fitted",
     "sklearn.utils.validation.check_memory",
