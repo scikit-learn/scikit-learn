c6496aa2b6239cdbce9325ad47fea4dcc3eb0c7b
Rishabh <54768821+anonymousr007@users.noreply.github.com>
2021-10-30 13:47:08 +0000

    Ensure that functions's docstrings pass numpydoc validation

--- sklearn/utils/extmath.py
+++ sklearn/utils/extmath.py
@@ -668,6 +668,11 @@ def cartesian(arrays, out=None):
         2-D array of shape (M, len(arrays)) containing cartesian products
         formed of input arrays.
 
+    Notes
+    -----
+    This function may not be used on more than 32 arrays
+    because the underlying numpy functions do not support it.
+
     Examples
     --------
     >>> from sklearn.utils.extmath import cartesian
@@ -684,11 +689,6 @@ def cartesian(arrays, out=None):
            [3, 4, 7],
            [3, 5, 6],
            [3, 5, 7]])
-
-    Notes
-    -----
-    This function may not be used on more than 32 arrays
-    because the underlying numpy functions do not support it.
     """
     arrays = [np.asarray(x) for x in arrays]
     shape = (len(x) for x in arrays)
