915e7609e4b11710f94c93f5767d8be145261e27
Thomas J. Fan <thomasjpfan@gmail.com>
2021-10-30 21:29:16 -0400

    CI Fork could be causing errors  [cd build]

--- build_tools/travis/test_wheels.sh
+++ build_tools/travis/test_wheels.sh
@@ -3,7 +3,12 @@
 pip install --upgrade pip || travis_terminate $?
 pip install pytest pytest-xdist || travis_terminate $?
 
-python -m pytest -n $CPU_COUNT --pyargs sklearn || travis_terminate $?
+XDIST_ARGS=""
+if [[ "$CPU_COUNT" != "1" ]]; then
+    XDIST_ARGS="-n $CPU_COUNT"
+fi
+
+python -m pytest $XDIST_ARGS --pyargs sklearn || travis_terminate $?
 
 # Test that there are no links to system libraries
 python -m threadpoolctl -i sklearn || travis_terminate $?
