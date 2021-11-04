43738841605874bf3eea913c8afe9282f79bb747
Julien Jerphanion <git@jjerphan.xyz>
2021-11-04 17:38:42 +0100

    Parametrize test_inverse_transform and test "arbitrary-variance"
    Co-authored-by: Olivier Grisel <olivier.grisel@ensta.org>

--- sklearn/decomposition/tests/test_fastica.py
+++ sklearn/decomposition/tests/test_fastica.py
@@ -254,32 +254,32 @@ def test_fit_transform():
 
 
 @pytest.mark.filterwarnings("ignore:Ignoring n_components with whiten=False.")
-def test_inverse_transform():
+@pytest.mark.parametrize(
+    "whiten, n_components, expected_shape",
+    [
+        ("arbitrary-variance", 5, (12, 5)),
+        ("arbitrary-variance", 10, (12, 10)),
+        ("unit-variance", 5, (12, 5)),
+        ("unit-variance", 10, (12, 10)),
+        (False, 5, (12, 12)),
+        (False, 10, (12, 12)),
+    ],
+)
+def test_inverse_transform(whiten, n_components, expected_shape):
     # Test FastICA.inverse_transform
-    n_features = 10
     n_samples = 100
-    n1, n2 = 5, 10
     rng = np.random.RandomState(0)
-    X = rng.random_sample((n_samples, n_features))
-    expected = {
-        ("unit-variance", n1): (n_features, n1),
-        ("unit-variance", n2): (n_features, n2),
-        (False, n1): (n_features, n2),
-        (False, n2): (n_features, n2),
-    }
-    for whiten in ["unit-variance", False]:
-        for n_components in [n1, n2]:
-            n_components_ = n_components if n_components is not None else X.shape[1]
-            ica = FastICA(n_components=n_components, random_state=rng, whiten=whiten)
-            Xt = ica.fit_transform(X)
-            expected_shape = expected[(whiten, n_components_)]
-            assert ica.mixing_.shape == expected_shape
-            X2 = ica.inverse_transform(Xt)
-            assert X.shape == X2.shape
+    X = rng.random_sample((n_samples, 12))
 
-            # reversibility test in non-reduction case
-            if n_components == X.shape[1]:
-                assert_array_almost_equal(X, X2)
+    ica = FastICA(n_components=n_components, random_state=rng, whiten=whiten)
+    Xt = ica.fit_transform(X)
+    assert ica.mixing_.shape == expected_shape
+    X2 = ica.inverse_transform(Xt)
+    assert X.shape == X2.shape
+
+    # reversibility test in non-reduction case
+    if n_components == X.shape[1]:
+        assert_array_almost_equal(X, X2)
 
 
 # FIXME remove filter in 1.3
