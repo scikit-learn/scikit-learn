cd33ec1ee858ee4988c75ae11635d4cdec239f2d
Mrinal Tyagi <mrinaltyagi24@gmail.com>
2021-11-07 17:17:31 +0530

    Update roc_curve.py

--- sklearn/metrics/_plot/roc_curve.py
+++ sklearn/metrics/_plot/roc_curve.py
@@ -368,17 +368,16 @@ def plot_roc_curve(
     pos_label=None,
     **kwargs,
 ):
-    """.. deprecated:: 1.0
-      `plot_roc_curve` is deprecated in 1.0 and will be removed in
-       1.2. Use one of the following class methods:
-       :func:`~sklearn.metrics.RocCurveDisplay.from_predictions` or
-       :func:`~sklearn.metrics.RocCurveDisplay.from_estimator`.
-
-    Plot Receiver operating characteristic (ROC) curve.
+    """Plot Receiver operating characteristic (ROC) curve.
 
     Extra keyword arguments will be passed to matplotlib's `plot`.
 
     Read more in the :ref:`User Guide <visualizations>`.
+
+    `plot_roc_curve` is deprecated in 1.0 and will be removed in
+    1.2. Use one of the following class methods:
+    :func:`~sklearn.metrics.RocCurveDisplay.from_predictions` or
+    :func:`~sklearn.metrics.RocCurveDisplay.from_estimator`.
 
     Parameters
     ----------
