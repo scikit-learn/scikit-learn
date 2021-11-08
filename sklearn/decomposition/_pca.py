6253b0edc206e54770ee8c077266d44a0afa286a
x-shadow-man <1494445739@qq.com>
2021-11-08 16:30:43 +0800

    _pca.py modified

--- sklearn/decomposition/_pca.py
+++ sklearn/decomposition/_pca.py
@@ -473,11 +473,7 @@ class PCA(_BasePCA):
         if self._fit_svd_solver == "full":
             return self._fit_full(X, n_components)
         elif self._fit_svd_solver in ["arpack", "randomized"]:
-            return self._fit_truncated(
-                X,
-                n_components,
-                self._fit_svd_solver
-            )
+            return self._fit_truncated(X, n_components, self._fit_svd_solver)
         else:
             raise ValueError(
                 "Unrecognized svd_solver='{0}'".format(self._fit_svd_solver)
