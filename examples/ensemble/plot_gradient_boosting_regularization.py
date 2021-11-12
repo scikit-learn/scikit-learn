3c9d342957eb9e28cbf76263d2dec6a0524daa53
sply88 <sebastian.pauly@tu-dortmund.de>
2021-11-12 20:10:44 +0100

    speed up by less samples and less trees

--- examples/ensemble/plot_gradient_boosting_regularization.py
+++ examples/ensemble/plot_gradient_boosting_regularization.py
@@ -32,17 +32,17 @@ from sklearn import ensemble
 from sklearn import datasets
 
 
-X, y = datasets.make_hastie_10_2(n_samples=12000, random_state=1)
+X, y = datasets.make_hastie_10_2(n_samples=3000, random_state=1)
 X = X.astype(np.float32)
 
 # map labels from {-1, 1} to {0, 1}
 labels, y = np.unique(y, return_inverse=True)
 
-X_train, X_test = X[:1500], X[1500:]
-y_train, y_test = y[:1500], y[1500:]
+X_train, X_test = X[:1000], X[1000:]
+y_train, y_test = y[:1000], y[1000:]
 
 original_params = {
-    "n_estimators": 600,
+    "n_estimators": 400,
     "max_leaf_nodes": 4,
     "max_depth": None,
     "random_state": 2,
