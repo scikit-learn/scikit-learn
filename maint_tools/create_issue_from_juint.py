ae7402040328c4c8d56703a73af325389cf7e11f
Thomas J. Fan <thomasjpfan@gmail.com>
2021-11-13 16:21:02 -0500

    BUG Fix upstream updater when test collection fails (#21655)

--- maint_tools/create_issue_from_juint.py
+++ maint_tools/create_issue_from_juint.py
@@ -73,6 +73,12 @@ if not junit_path.exists():
 tree = ET.parse(args.junit_file)
 failure_cases = []
 
+# Check if test collection failed
+error = tree.find("./testsuite/testcase/error")
+if error is not None:
+    # Get information for test collection error
+    failure_cases.append({"title": "Test Collection Failure", "body": error.text})
+
 for item in tree.iter("testcase"):
     failure = item.find("failure")
     if failure is None:
