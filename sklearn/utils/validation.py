f4833059f5320c275617048741cccf6801dc8a3a
reshamas <reshama.stat@gmail.com>
2021-11-13 14:18:34 -0500

    commented out wrong function

--- sklearn/utils/validation.py
+++ sklearn/utils/validation.py
@@ -1172,7 +1172,7 @@ def has_fit_parameter(estimator, parameter):
 
     Returns
     -------
-    is_parameter : bool
+    is_parameter: bool
         Whether the parameter was found to be a named parameter of the
         estimator's fit method.
 
