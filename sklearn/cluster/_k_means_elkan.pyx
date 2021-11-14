945ad48073b936d2b6e551afb4598afca94e1542
Christian Lorentzen <lorentzen.ch@gmail.com>
2021-11-14 12:28:36 +0100

    FIX out of bounds error on X_indptr

--- sklearn/cluster/_k_means_elkan.pyx
+++ sklearn/cluster/_k_means_elkan.pyx
@@ -523,7 +523,7 @@ def elkan_iter_chunked_sparse(
             _update_chunk_sparse(
                 X_data[X_indptr[start]: X_indptr[end]],
                 X_indices[X_indptr[start]: X_indptr[end]],
-                X_indptr[start: end],
+                X_indptr[start: end+1],
                 sample_weight[start: end],
                 centers_old,
                 centers_squared_norms,
