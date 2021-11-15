6077d52b706d118c0d9fb1e69c254bc67e15b078
Reshama Shaikh <reshama.stat@gmail.com>
2021-11-15 13:03:57 -0500

    DOC Ensures that function passes numpydoc validation: column_or_1d function (#21591)
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>
    Co-authored-by: Julien Jerphanion <git@jjerphan.xyz>

--- sklearn/utils/validation.py
+++ sklearn/utils/validation.py
@@ -1107,6 +1107,7 @@ def column_or_1d(y, *, warn=False):
     Parameters
     ----------
     y : array-like
+       Input data.
 
     warn : bool, default=False
        To control display of warnings.
@@ -1114,7 +1115,12 @@ def column_or_1d(y, *, warn=False):
     Returns
     -------
     y : ndarray
+       Output data.
 
+    Raises
+    -------
+    ValueError
+        If `y` is not a 1D array or a 2D array with a single row or column.
     """
     y = np.asarray(y)
     shape = np.shape(y)
