694b3822c6ff85990344332b5c9a06e4baf69287
mandjevant <38689620+mandjevant@users.noreply.github.com>
2021-11-15 15:33:13 +0100

    DOC Add default=None to constant parameter of dummy clf (#21652)

--- sklearn/dummy.py
+++ sklearn/dummy.py
@@ -58,7 +58,7 @@ class DummyClassifier(MultiOutputMixin, ClassifierMixin, BaseEstimator):
         Pass an int for reproducible output across multiple function calls.
         See :term:`Glossary <random_state>`.
 
-    constant : int or str or array-like of shape (n_outputs,)
+    constant : int or str or array-like of shape (n_outputs,), default=None
         The explicit constant as predicted by the "constant" strategy. This
         parameter is useful only for the "constant" strategy.
 
