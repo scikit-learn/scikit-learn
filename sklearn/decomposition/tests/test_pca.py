75285adbd45f434508d38b8b71d65ecdfc466b16
Jay Stanley <jay.s.stanley.3@gmail.com>
2021-11-18 14:23:37 -0600

    updates to tests

--- sklearn/decomposition/tests/test_pca.py
+++ sklearn/decomposition/tests/test_pca.py
@@ -557,7 +557,7 @@ def check_pca_float_dtype_preservation(svd_solver):
     # conda-forge: PR#15775
     # see: https://github.com/conda-forge/scikit-learn-feedstock/pull/113
     assert_allclose(pca_64.components_, pca_32.components_, rtol=2e-4)
-    assert_allclose(pca_64.U_, pca_32.U_, rtol=2e-4)
+    assert_allclose(np.abs(pca_64.U_), np.abs(pca_32.U_), rtol=1e-2)
 
 
 def check_pca_int_dtype_upcast_to_double(svd_solver):
@@ -692,7 +692,7 @@ def test_pca_randomized_svd_n_oversamples():
     assert_allclose(np.abs(pca_full.U_), np.abs(pca_arpack.U_))
 
     assert_allclose(np.abs(pca_randomized.components_), np.abs(pca_arpack.components_))
-    assert_allclose(np.abs(pca_randomized.U_), np.abs(pca_arpack.components_))
+    assert_allclose(np.abs(pca_randomized.U_), np.abs(pca_arpack.U_))
 
 
 @pytest.mark.parametrize(
