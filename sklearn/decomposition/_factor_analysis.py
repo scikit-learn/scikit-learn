bcd20833961c335b55b49a114c5ffcddbb50e10c
Haya Almutairi <ihaya.almutairi@gmail.com>
2021-11-21 23:24:36 +0300

    FIX validate parameter in fit for FactorAnalysis

--- sklearn/decomposition/_factor_analysis.py
+++ sklearn/decomposition/_factor_analysis.py
@@ -176,7 +176,6 @@ class FactorAnalysis(_ClassNamePrefixFeaturesOutMixin, TransformerMixin, BaseEst
         self.copy = copy
         self.tol = tol
         self.max_iter = max_iter
-
         self.svd_method = svd_method
 
         self.noise_variance_init = noise_variance_init
@@ -203,7 +202,8 @@ class FactorAnalysis(_ClassNamePrefixFeaturesOutMixin, TransformerMixin, BaseEst
 
         if self.svd_method not in ["lapack", "randomized"]:
             raise ValueError(
-                f"SVD method {self.svd_method!r} is not supported. Please consider the documentation"
+                f"SVD method {self.svd_method!r} is not supported. Possible methods "
+                "are either 'lapack' or 'randomized'."
             )
 
         X = self._validate_data(X, copy=self.copy, dtype=np.float64)
