f7fd69367ddd60dea82e6707cb42be4574351d84
ArthDh <arth.dharaskar97@gmail.com>
2021-11-20 23:06:48 -0800

    Updated plot_digits_pipe

--- examples/compose/plot_digits_pipe.py
+++ examples/compose/plot_digits_pipe.py
@@ -25,20 +25,20 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
 
-
 # Define a pipeline to search for the best combination of PCA truncation
 # and classifier regularization.
 pca = PCA()
 # set the tolerance to a large value to make the example faster
-logistic = LogisticRegression(max_iter=10000, tol=0.1)
+logistic = LogisticRegression(max_iter=1000, tol=0.2)
 pipe = Pipeline(steps=[("pca", pca), ("logistic", logistic)])
 
 X_digits, y_digits = datasets.load_digits(return_X_y=True)
-
+X_digits = X_digits[::4, :]  # Working with a subset of training data
+y_digits = y_digits[::4]  # Equivalent subset from training labels
 # Parameters of pipelines can be set using ‘__’ separated parameter names:
 param_grid = {
-    "pca__n_components": [5, 15, 30, 45, 64],
-    "logistic__C": np.logspace(-4, 4, 4),
+    "pca__n_components": [5, 15, 30, 45, 60],
+    "logistic__C": np.logspace(-3, 3, 3),
 }
 search = GridSearchCV(pipe, param_grid, n_jobs=-1)
 search.fit(X_digits, y_digits)
