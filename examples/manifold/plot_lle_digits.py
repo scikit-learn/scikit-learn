6fe25def5201176d153e2420e41b32cd14198c58
siavrez <siavash.rezazadeh@gmail.com>
2021-11-23 15:54:57 +0330

    Added zorder for annotationBox and scatter, changed n_jobs=2 and MDS max_iter=125

--- examples/manifold/plot_lle_digits.py
+++ examples/manifold/plot_lle_digits.py
@@ -50,14 +50,14 @@ from sklearn.preprocessing import MinMaxScaler
 
 def plot_embedding(X, title, ax):
     X = MinMaxScaler().fit_transform(X)
-
     for digit in digits.target_names:
         ax.scatter(
             *X[y == digit].T,
             marker=f"${digit}$",
             s=60,
             color=plt.cm.Dark2(digit),
-            alpha=0.7,
+            alpha=0.425,
+            zorder=2,
         )
     shown_images = np.array([[1.0, 1.0]])  # just something big
     for i in range(X.shape[0]):
@@ -69,7 +69,7 @@ def plot_embedding(X, title, ax):
             continue
         shown_images = np.concatenate([shown_images, [X[i]]], axis=0)
         imagebox = offsetbox.AnnotationBbox(
-            offsetbox.OffsetImage(digits.images[i], cmap=plt.cm.gray_r), X[i]
+            offsetbox.OffsetImage(digits.images[i], cmap=plt.cm.gray_r), X[i], zorder=1
         )
         ax.add_artist(imagebox)
 
@@ -131,7 +131,7 @@ embeddings = {
     "LTSA LLE embedding": LocallyLinearEmbedding(
         n_neighbors=n_neighbors, n_components=2, method="ltsa"
     ),
-    "MDS embedding": MDS(n_components=2, n_init=1, max_iter=100, n_jobs=-1),
+    "MDS embedding": MDS(n_components=2, n_init=1, max_iter=120, n_jobs=2),
     "Random Trees embedding": make_pipeline(
         RandomTreesEmbedding(n_estimators=200, max_depth=5, random_state=0),
         TruncatedSVD(n_components=2),
@@ -145,7 +145,7 @@ embeddings = {
         learning_rate="auto",
         n_iter=500,
         n_iter_without_progress=150,
-        n_jobs=-1,
+        n_jobs=2,
         random_state=0,
     ),
     "NCA embedding": NeighborhoodComponentsAnalysis(
