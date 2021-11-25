c3da2b1f907067a0f60a2a16a688a26b613357fe
Arth <arth.dharaskar97@gmail.com>
2021-11-25 04:05:54 -0800

    DOC speed up example plot_digits_pipe.py (#21728)
    * Updated plot_digits_pipe
    
    * Updated plot_digits_pipe with StandardScaler preprocessing

--- examples/compose/plot_digits_pipe.py
+++ examples/compose/plot_digits_pipe.py
@@ -24,20 +24,22 @@ from sklearn.decomposition import PCA
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
-
+from sklearn.preprocessing import StandardScaler
 
 # Define a pipeline to search for the best combination of PCA truncation
 # and classifier regularization.
 pca = PCA()
+# Define a Standard Scaler to normalize inputs
+scaler = StandardScaler()
+
 # set the tolerance to a large value to make the example faster
 logistic = LogisticRegression(max_iter=10000, tol=0.1)
-pipe = Pipeline(steps=[("pca", pca), ("logistic", logistic)])
+pipe = Pipeline(steps=[("scaler", scaler), ("pca", pca), ("logistic", logistic)])
 
 X_digits, y_digits = datasets.load_digits(return_X_y=True)
-
 # Parameters of pipelines can be set using ‘__’ separated parameter names:
 param_grid = {
-    "pca__n_components": [5, 15, 30, 45, 64],
+    "pca__n_components": [5, 15, 30, 45, 60],
     "logistic__C": np.logspace(-4, 4, 4),
 }
 search = GridSearchCV(pipe, param_grid, n_jobs=2)
