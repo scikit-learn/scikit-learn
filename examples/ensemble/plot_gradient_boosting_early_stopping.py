3b27ad937731f7aad2996bcc7bb1437b735d129c
sply88 <43181038+sply88@users.noreply.github.com>
2021-11-27 20:08:18 +0100

    Fix random state for plot_gradient_boosting_early_stopping.py
    Makes example results deterministic.
    
    Co-authored-by: Thomas J. Fan <thomasjpfan@gmail.com>

--- examples/ensemble/plot_gradient_boosting_early_stopping.py
+++ examples/ensemble/plot_gradient_boosting_early_stopping.py
@@ -45,11 +45,11 @@ from sklearn import ensemble
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 
-data_list = []
-
-data_list += [datasets.load_iris(return_X_y=True)]
-data_list += [datasets.make_classification(n_samples=800)]
-data_list += [datasets.make_hastie_10_2(n_samples=2000)]
+data_list = [
+    datasets.load_iris(return_X_y=True),
+    datasets.make_classification(n_samples=800, random_state=0),
+    datasets.make_hastie_10_2(n_samples=2000, random_state=0),
+]
 names = ["Iris Data", "Classification Data", "Hastie Data"]
 
 n_gb = []
