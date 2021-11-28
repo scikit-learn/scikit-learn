db4d0558f66e589cb9894275392210606446d496
sply88 <sebastian.pauly@tu-dortmund.de>
2021-11-28 11:36:10 +0100

    n_estimators back to 20

--- examples/model_selection/plot_successive_halving_iterations.py
+++ examples/model_selection/plot_successive_halving_iterations.py
@@ -29,7 +29,7 @@ rng = np.random.RandomState(0)
 
 X, y = datasets.make_classification(n_samples=400, n_features=12, random_state=rng)
 
-clf = RandomForestClassifier(n_estimators=15, random_state=rng)
+clf = RandomForestClassifier(n_estimators=20, random_state=rng)
 
 param_dist = {
     "max_depth": [3, None],
