eee1851030f13ae5b2e406327ffd746b944901ff
Thomas J. Fan <thomasjpfan@gmail.com>
2021-11-29 11:55:37 -0500

    FIX Remove warnings when fitting a dataframe (#21578)
    Co-authored-by: Olivier Grisel <olivier.grisel@ensta.org>

--- sklearn/ensemble/_forest.py
+++ sklearn/ensemble/_forest.py
@@ -519,8 +519,10 @@ class BaseForest(MultiOutputMixin, BaseEnsemble, metaclass=ABCMeta):
         oob_pred : ndarray of shape (n_samples, n_classes, n_outputs) or \
                 (n_samples, 1, n_outputs)
             The OOB predictions.
-      """
-        X = self._validate_data(X, dtype=DTYPE, accept_sparse="csr", reset=False)
+        """
+        # Prediction requires X to be in CSR format
+        if issparse(X):
+            X = X.tocsr()
 
         n_samples = y.shape[0]
         n_outputs = self.n_outputs_
