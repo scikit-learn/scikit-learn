6b723217d16043645be397c2188ec7e507781d24
MrinalTyagi <mrinaltyagi24@gmail.com>
2021-11-29 19:45:14 +0530

    DOC Ensures that plot_roc_curve passes numpydoc validation (#21547)
    Co-authored-by: Olivier Grisel <olivier.grisel@ensta.org>

--- maint_tools/test_docstrings.py
+++ maint_tools/test_docstrings.py
@@ -94,7 +94,6 @@ FUNCTION_DOCSTRING_IGNORE_LIST = [
     "sklearn.metrics._plot.confusion_matrix.plot_confusion_matrix",
     "sklearn.metrics._plot.det_curve.plot_det_curve",
     "sklearn.metrics._plot.precision_recall_curve.plot_precision_recall_curve",
-    "sklearn.metrics._plot.roc_curve.plot_roc_curve",
     "sklearn.metrics._ranking.auc",
     "sklearn.metrics._ranking.average_precision_score",
     "sklearn.metrics._ranking.coverage_error",
