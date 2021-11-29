5229bab73285064e02b717f9994dd423c26f1119
jeannedionisi <jeanne.dionisi@insa-rouen.fr>
2021-11-29 09:24:17 +0100

    add doc

--- sklearn/manifold/tests/test_t_sne.py
+++ sklearn/manifold/tests/test_t_sne.py
@@ -1287,6 +1287,7 @@ def test_tsne_n_jobs(method):
 
     assert_allclose(X_tr_ref, X_tr)
 
+
 def test_tsne_with_mahalanobis_distance():
     """Make sure that method_parameters works with mahalanobis distance"""
     random_state = check_random_state(0)
@@ -1296,19 +1297,39 @@ def test_tsne_with_mahalanobis_distance():
     X = random_state.randn(n_samples, n_features)
 
     # 1. raises error here (original issue)
-    tsne = TSNE(verbose=1, perplexity=40, n_iter=250, learning_rate=50,
-                n_components=n_embedding, random_state=0, metric='mahalanobis')
+    tsne = TSNE(
+        verbose=1,
+        perplexity=40,
+        n_iter=250,
+        learning_rate=50,
+        n_components=n_embedding,
+        random_state=0,
+        metric="mahalanobis",
+    )
     ref = "Must provide either V or VI for Mahalanobis distance"
     with pytest.raises(ValueError, match=ref):
         tsne.fit_transform(X)
 
     # 2. check for correct answer
-    precomputed_X = squareform(pdist(X, metric='mahalanobis'), checks=True)
-    ref = TSNE(verbose=1, perplexity=40, n_iter=250, learning_rate=50,
-               n_components=n_embedding, random_state=0,
-               metric='precomputed').fit_transform(precomputed_X)
+    precomputed_X = squareform(pdist(X, metric="mahalanobis"), checks=True)
+    ref = TSNE(
+        verbose=1,
+        perplexity=40,
+        n_iter=250,
+        learning_rate=50,
+        n_components=n_embedding,
+        random_state=0,
+        metric="precomputed",
+    ).fit_transform(precomputed_X)
 
-    now = TSNE(verbose=1, perplexity=40, n_iter=250, learning_rate=50,
-               n_components=n_embedding, random_state=0, metric='mahalanobis',
-               metric_params={'V': np.cov(X.T)}).fit_transform(X)
+    now = TSNE(
+        verbose=1,
+        perplexity=40,
+        n_iter=250,
+        learning_rate=50,
+        n_components=n_embedding,
+        random_state=0,
+        metric="mahalanobis",
+        metric_params={"V": np.cov(X.T)},
+    ).fit_transform(X)
     assert_array_equal(ref, now)
