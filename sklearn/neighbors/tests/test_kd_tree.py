24c8b2f392df69dd6104cfd56f82c7a5b5c66e55
Thomas J. Fan <thomasjpfan@gmail.com>
2021-11-30 18:32:04 -0500

    FIX Fixes joblib memmap error for KDTree and BallTree

--- sklearn/neighbors/tests/test_kd_tree.py
+++ sklearn/neighbors/tests/test_kd_tree.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pytest
+from joblib import Parallel, delayed
 
 from sklearn.neighbors._kd_tree import KDTree
 
@@ -13,3 +14,15 @@ def test_array_object_type():
     X = np.array([(1, 2, 3), (2, 5), (5, 5, 1, 2)], dtype=object)
     with pytest.raises(ValueError, match="setting an array element with a sequence"):
         KDTree(X)
+
+
+def test_kdtree_picklable_with_joblib():
+    """Make sure that KDTree queries work when joblib memmaps.
+
+    Non-regression test for #21685 and #21228."""
+    rng = np.random.RandomState(0)
+    X = rng.random_sample((10, 3))
+    tree = KDTree(X, leaf_size=2)
+
+    # Does not error
+    Parallel(n_jobs=2, max_nbytes=1)(delayed(tree.query)(data) for data in 2 * [X])
