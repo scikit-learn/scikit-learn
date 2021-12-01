418c18dbfa2b67b212c78148a1f1d2e85f6cb1a2
ArturoAmorQ <arturo.amor-quiroz@polytechnique.edu>
2021-12-01 15:22:38 +0100

    Give shape of X and y arrays explicitly

--- sklearn/datasets/_base.py
+++ sklearn/datasets/_base.py
@@ -578,9 +578,10 @@ def load_iris(*, return_X_y=False, as_frame=False):
             .. versionadded:: 0.20
 
     (data, target) : tuple if ``return_X_y`` is True
-        A tuple of two ndarray of shape (n_samples, n_features)
-        A 2D array with each row representing one sample and each column
-        representing the features and/or target of a given sample.
+        A tuple of two ndarray. The first containing a 2D array of shape
+        (n_samples, n_features) with each row representing one sample and
+        each column representing the features. The second ndarray of shape
+        (n_samples,) containing the target samples.
 
         .. versionadded:: 0.18
 
