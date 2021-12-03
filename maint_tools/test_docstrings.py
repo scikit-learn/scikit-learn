572ef1df989c08e85b90da0a606477832423ab8c
mlant <43914720+mlant@users.noreply.github.com>
2021-12-03 08:05:26 +0100

    DOC Ensures that sklearn.metrics.cluster._unsupervised.davies_bouldin_score passes numpydoc validation #21350 (#21850)

--- maint_tools/test_docstrings.py
+++ maint_tools/test_docstrings.py
@@ -120,7 +120,6 @@ FUNCTION_DOCSTRING_IGNORE_LIST = [
     "sklearn.metrics.cluster._supervised.pair_confusion_matrix",
     "sklearn.metrics.cluster._supervised.rand_score",
     "sklearn.metrics.cluster._supervised.v_measure_score",
-    "sklearn.metrics.cluster._unsupervised.davies_bouldin_score",
     "sklearn.metrics.cluster._unsupervised.silhouette_samples",
     "sklearn.metrics.pairwise.additive_chi2_kernel",
     "sklearn.metrics.pairwise.check_paired_arrays",
