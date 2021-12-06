76817c94493f3cc71ec9d13b0bf78ed0adfbcfdc
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2021-12-06 17:43:51 +0100

    cln

--- sklearn/decomposition/_dict_learning.py
+++ sklearn/decomposition/_dict_learning.py
@@ -16,6 +16,7 @@ from joblib import Parallel, effective_n_jobs
 
 from ..base import BaseEstimator, TransformerMixin, _ClassNamePrefixFeaturesOutMixin
 from ..utils import check_array, check_random_state, gen_even_slices, gen_batches
+from ..utils import deprecated
 from ..utils.extmath import randomized_svd, row_norms, svd_flip
 from ..utils.validation import check_is_fitted
 from ..utils.fixes import delayed
