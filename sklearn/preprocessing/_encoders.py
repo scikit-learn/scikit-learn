2b4fe719a0a15cdc84aad42dd739e864591fc67e
pelennor <9261828+pelennor@users.noreply.github.com>
2021-12-07 22:32:43 +0900

    Doc fix typos in sklearn.preprocessing._encoders (#21910)

--- sklearn/preprocessing/_encoders.py
+++ sklearn/preprocessing/_encoders.py
@@ -36,7 +36,7 @@ class _BaseEncoder(TransformerMixin, BaseEstimator):
         - return list of features (arrays): this list of features is
           constructed feature by feature to preserve the data types
           of pandas DataFrame columns, as otherwise information is lost
-          and cannot be used, eg for the `categories_` attribute.
+          and cannot be used, e.g. for the `categories_` attribute.
 
         """
         if not (hasattr(X, "iloc") and getattr(X, "ndim", 0) == 2):
@@ -218,7 +218,7 @@ class OneHotEncoder(_BaseEncoder):
 
         .. versionadded:: 0.20
 
-    drop : {'first', 'if_binary'} or a array-like of shape (n_features,), \
+    drop : {'first', 'if_binary'} or an array-like of shape (n_features,), \
             default=None
         Specifies a methodology to use to drop one of the categories per
         feature. This is useful in situations where perfectly collinear
@@ -564,7 +564,7 @@ class OneHotEncoder(_BaseEncoder):
 
         When unknown categories are encountered (all zeros in the
         one-hot encoding), ``None`` is used to represent this category. If the
-        feature with the unknown category has a dropped caregory, the dropped
+        feature with the unknown category has a dropped category, the dropped
         category will be its inverse.
 
         Parameters
