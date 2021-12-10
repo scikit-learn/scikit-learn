0335ea2177749e79117ec952bb1a14417aee938a
genvalen <genvalen@protonmail.com>
2021-12-10 17:41:15 -0500

    Remove redundant checks from AdaBoostClassifer

--- sklearn/ensemble/_weight_boosting.py
+++ sklearn/ensemble/_weight_boosting.py
@@ -493,22 +493,6 @@ class AdaBoostClassifier(ClassifierMixin, BaseWeightBoosting):
         self : object
             Fitted estimator.
         """
-        check_scalar(
-            self.n_estimators,
-            "n_estimators",
-            target_type=numbers.Integral,
-            min_val=1,
-            include_boundaries="left",
-        )
-
-        check_scalar(
-            self.learning_rate,
-            "learning_rate",
-            target_type=numbers.Real,
-            min_val=0,
-            include_boundaries="neither",
-        )
-
         # Check that algorithm is supported
         if self.algorithm not in ("SAMME", "SAMME.R"):
             raise ValueError(
