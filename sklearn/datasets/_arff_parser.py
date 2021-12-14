e2f6940988c27833fff3dce54dfdc04c0de69856
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2021-12-14 15:03:20 +0100

    iter

--- sklearn/datasets/_arff_parser.py
+++ sklearn/datasets/_arff_parser.py
@@ -98,7 +98,27 @@ def _cast_frame(frame, columns_info):
 
 
 def _post_process_frame(frame, feature_names, target_names):
-    """Post process a dataframe to select the desired columns in `X` and `y`."""
+    """Post process a dataframe to select the desired columns in `X` and `y`.
+
+    Parameters
+    ----------
+    frame : dataframe
+        The dataframe to split into `X` and `y`.
+
+    feature_names : list of str
+        The list of feature names to populate `X`.
+
+    target_names : list of str
+        The list of target names to populate `y`.
+
+    Returns
+    -------
+    X : dataframe
+        The dataframe containing the features.
+
+    y : {series, dataframe} or None
+        The series or dataframe containing the target.
+    """
     X = frame[feature_names]
     if len(target_names) >= 2:
         y = frame[target_names]
--- sklearn/datasets/tests/test_arff_parser.py
+++ sklearn/datasets/tests/test_arff_parser.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pytest
 
-from sklearn.datasets._arff_parser import _cast_frame
+from sklearn.datasets._arff_parser import (
+    _cast_frame,
+    _post_process_frame,
+)
 
 
 def test_cast_frame():
@@ -67,3 +70,60 @@ def test_cast_frame():
         X_casted["col_categorical"].cat.categories, pd.Index(["a", "b", "c"])
     )
     assert X_casted_dtypes["col_string"].kind == "O"
+
+
+@pytest.mark.parametrize(
+    "feature_names, target_names",
+    [
+        (
+            [
+                "col_int_as_integer",
+                "col_int_as_numeric",
+                "col_float_as_real",
+                "col_float_as_numeric",
+            ],
+            ["col_categorical", "col_string"],
+        ),
+        (
+            [
+                "col_int_as_integer",
+                "col_int_as_numeric",
+                "col_float_as_real",
+                "col_float_as_numeric",
+            ],
+            ["col_categorical"],
+        ),
+        (
+            [
+                "col_int_as_integer",
+                "col_int_as_numeric",
+                "col_float_as_real",
+                "col_float_as_numeric",
+            ],
+            [],
+        ),
+    ],
+)
+def test_post_process_frame(feature_names, target_names):
+    """Check the behaviour of the post-processing function for splitting a dataframe."""
+    pd = pytest.importorskip("pandas")
+
+    X_original = pd.DataFrame(
+        {
+            "col_int_as_integer": [1, 2, 3],
+            "col_int_as_numeric": [1, 2, 3],
+            "col_float_as_real": [1.0, 2.0, 3.0],
+            "col_float_as_numeric": [1.0, 2.0, 3.0],
+            "col_categorical": ["a", "b", "c"],
+            "col_string": ["a", "b", "c"],
+        }
+    )
+
+    X, y = _post_process_frame(X_original, feature_names, target_names)
+    assert isinstance(X, pd.DataFrame)
+    if len(target_names) == 2:
+        assert isinstance(y, pd.DataFrame)
+    elif len(target_names) == 1:
+        assert isinstance(y, pd.Series)
+    else:
+        assert y is None
