77c284cfdd6c4e102b2083161e3ce8ab2a3dd046
Julien Jerphanion <git@jjerphan.xyz>
2021-12-15 15:38:45 +0100

    Refactor fixtures prior to PairwiseDistancesReduction introduction

--- sklearn/utils/setup.py
+++ sklearn/utils/setup.py
@@ -95,6 +95,13 @@ def configuration(parent_package="", top_path=None):
         libraries=libraries,
     )
 
+    config.add_extension(
+        "_heap",
+        sources=["_heap.pyx"],
+        include_dirs=[numpy.get_include()],
+        libraries=libraries,
+    )
+
     config.add_subpackage("tests")
 
     return config
