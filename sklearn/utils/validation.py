787e4d2e5fc8da51cf132eb22fa81ee58185baa4
Sachin Yadav <40866128+yadav-sachin@users.noreply.github.com>
2021-12-17 14:03:20 +0530

    DOC Add interval notation for include_boundaries in documentation check_scalar (#21955)

--- sklearn/utils/validation.py
+++ sklearn/utils/validation.py
@@ -1385,11 +1385,14 @@ def check_scalar(
         Whether the interval defined by `min_val` and `max_val` should include
         the boundaries. Possible choices are:
 
-        - `"left"`: only `min_val` is included in the valid interval;
-        - `"right"`: only `max_val` is included in the valid interval;
-        - `"both"`: `min_val` and `max_val` are included in the valid interval;
+        - `"left"`: only `min_val` is included in the valid interval.
+          It is equivalent to the interval `[ min_val, max_val )`.
+        - `"right"`: only `max_val` is included in the valid interval.
+          It is equivalent to the interval `( min_val, max_val ]`.
+        - `"both"`: `min_val` and `max_val` are included in the valid interval.
+          It is equivalent to the interval `[ min_val, max_val ]`.
         - `"neither"`: neither `min_val` nor `max_val` are included in the
-          valid interval.
+          valid interval. It is equivalent to the interval `( min_val, max_val )`.
 
     Returns
     -------
