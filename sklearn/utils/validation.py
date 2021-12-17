e603adb2d4fe45dbf56b2de24328a7ecf4aa605d
yadav-sachin <sachinyadav7024@gmail.com>
2021-12-17 10:17:46 +0530

    replace semicolons with full stops

--- sklearn/utils/validation.py
+++ sklearn/utils/validation.py
@@ -1386,11 +1386,11 @@ def check_scalar(
         the boundaries. Possible choices are:
 
         - `"left"`: only `min_val` is included in the valid interval.
-          It is equivalent to the interval `[ min_val, max_val )`;
+          It is equivalent to the interval `[ min_val, max_val )`.
         - `"right"`: only `max_val` is included in the valid interval.
-          It is equivalent to the interval `( min_val, max_val ]`;
+          It is equivalent to the interval `( min_val, max_val ]`.
         - `"both"`: `min_val` and `max_val` are included in the valid interval.
-          It is equivalent to the interval `[ min_val, max_val ]`;
+          It is equivalent to the interval `[ min_val, max_val ]`.
         - `"neither"`: neither `min_val` nor `max_val` are included in the
           valid interval. It is equivalent to the interval `( min_val, max_val )`.
 
