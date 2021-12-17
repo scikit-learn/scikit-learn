3f606550c5be2fb0cc556d43fc62c7098c976752
Sylvain Marié <sylvain.marie@schneider-electric.com>
2021-12-17 12:26:50 +0100

    Update sklearn/metrics/_regression.py
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- sklearn/metrics/_regression.py
+++ sklearn/metrics/_regression.py
@@ -869,7 +869,7 @@ def r2_score(
 def _assemble_r2_explained_variance(
     numerator, denominator, nb_outputs, multioutput, force_finite
 ):
-    """Common part used by explained variance score and :math:`R^2` score"""
+    """Common part used by explained variance score and :math:`R^2` score."""
 
     if not force_finite:
         # Standard formula, that may lead to NaN or -Inf
