9189ff25f881054cfff6f13674bf54893ac2045e
melemo2 <67433064+melemo2@users.noreply.github.com>
2021-12-19 23:12:42 +0900

    DOC Ensures that mean_shift passes numpydoc validation (#22019)

--- sklearn/tests/test_docstrings.py
+++ sklearn/tests/test_docstrings.py
@@ -14,7 +14,6 @@ numpydoc_validation = pytest.importorskip("numpydoc.validate")
 FUNCTION_DOCSTRING_IGNORE_LIST = [
     "sklearn.cluster._kmeans.kmeans_plusplus",
     "sklearn.cluster._mean_shift.get_bin_seeds",
-    "sklearn.cluster._mean_shift.mean_shift",
     "sklearn.cluster._optics.cluster_optics_xi",
     "sklearn.cluster._optics.compute_optics_graph",
     "sklearn.cluster._spectral.spectral_clustering",
