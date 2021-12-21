FIX compute correctly the negative gradient of AdaBoost loss in GBDT

sklearn/ensemble/_gb_losses.py
@@ -936,7 +936,7 @@ class ExponentialLoss(ClassificationLossFunction):
             tree ensemble at iteration ``i - 1``.
         """
         y_ = -(2.0 * y - 1.0)
-        return y_ * np.exp(y_ * raw_predictions.ravel())
+        return -y_ * np.exp(y_ * raw_predictions.ravel())
 
     def _update_terminal_region(
         self,