ff12ae4f26ca8657563c395bdde711cd8906c669
Geoffrey Paris <geoffrey.paris@insa-rouen.fr>
2021-12-28 18:11:41 +0100

    FIX unexpected keyword argument for _check_y

--- sklearn/utils/validation.py
+++ sklearn/utils/validation.py
@@ -1078,13 +1078,13 @@ def check_X_y(
     return X, y
 
 
-def _check_y(y, multi_output=False, y_numeric=False, estimator=None):
+def _check_y(y, multi_output=False, y_numeric=False, estimator=None, force_all_finite=True):
     """Isolated part of check_X_y dedicated to y validation"""
     if multi_output:
         y = check_array(
             y,
             accept_sparse="csr",
-            force_all_finite=True,
+            force_all_finite=force_all_finite,
             ensure_2d=False,
             dtype=None,
             input_name="y",
@@ -1093,7 +1093,8 @@ def _check_y(y, multi_output=False, y_numeric=False, estimator=None):
     else:
         estimator_name = _check_estimator_name(estimator)
         y = column_or_1d(y, warn=True)
-        _assert_all_finite(y, input_name="y", estimator_name=estimator_name)
+        if force_all_finite:
+            _assert_all_finite(y, input_name="y", estimator_name=estimator_name)
         _ensure_no_complex_data(y)
     if y_numeric and y.dtype.kind == "O":
         y = y.astype(np.float64)
