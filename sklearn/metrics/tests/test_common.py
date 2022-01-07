1f9ccf716f744d956739265ffb6788e9fc35d59c
ohadmich <ohadmich@gmail.com>
2022-01-06 21:48:42 -0800

    Add d2_pinball_loss)_score tests

--- sklearn/metrics/tests/test_common.py
+++ sklearn/metrics/tests/test_common.py
@@ -30,7 +30,7 @@ from sklearn.metrics import cohen_kappa_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import coverage_error
 from sklearn.metrics import d2_tweedie_score
-from sklearn.metrics import d2_absolute_error_score
+from sklearn.metrics import d2_pinball_loss_score
 from sklearn.metrics import det_curve
 from sklearn.metrics import explained_variance_score
 from sklearn.metrics import f1_score
@@ -113,7 +113,7 @@ REGRESSION_METRICS = {
     "mean_gamma_deviance": mean_gamma_deviance,
     "mean_compound_poisson_deviance": partial(mean_tweedie_deviance, power=1.4),
     "d2_tweedie_score": partial(d2_tweedie_score, power=1.4),
-    "d2_absolute_error_score": d2_absolute_error_score,
+    "d2_pinball_loss_score": d2_pinball_loss_score,
 }
 
 CLASSIFICATION_METRICS = {
@@ -448,7 +448,7 @@ MULTIOUTPUT_METRICS = {
     "explained_variance_score",
     "mean_absolute_percentage_error",
     "mean_pinball_loss",
-    "d2_absolute_error_score",
+    "d2_pinball_loss_score",
 }
 
 # Symmetric with respect to their input arguments y_true and y_pred
@@ -516,7 +516,7 @@ NOT_SYMMETRIC_METRICS = {
     "mean_poisson_deviance",
     "mean_compound_poisson_deviance",
     "d2_tweedie_score",
-    "d2_absolute_error_score",
+    "d2_pinball_loss_score",
     "mean_absolute_percentage_error",
 }
 
