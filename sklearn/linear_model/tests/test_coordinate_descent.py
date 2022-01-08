e684fe7a0cd93c3dd73d346fbed93c468e31ceca
Yar Khine Phyo <yarkhinephyo@gmail.com>
2022-01-08 10:26:32 +0800

    Apply suggestions from code review
    Co-authored-by: Julien Jerphanion <git@jjerphan.xyz>

--- sklearn/linear_model/tests/test_coordinate_descent.py
+++ sklearn/linear_model/tests/test_coordinate_descent.py
@@ -1282,9 +1282,10 @@ def test_enet_l1_ratio():
 
     # Test that l1_ratio=0 with alpha>0 produces user warning
     warning_message = (
-        "Coordinate descent with no L1 regularization may lead to"
-        " unexpected results and is discouraged."
-    )
+        "Coordinate descent without L1 regularization may "
+        "lead to unexpected results and is discouraged. "
+        "Set l1_ratio > 0 to add L1 regularization."
+   )
     with pytest.warns(UserWarning, match=warning_message):
         ElasticNetCV(l1_ratio=[0], alphas=[1]).fit(X, y)
 
