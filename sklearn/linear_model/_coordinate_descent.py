758c6d453eeba7c18a6bb1a04e2c1c8ac277535f
chritter <christian.h.ritter@gmail.com>
2022-01-08 10:36:05 -0500

    test for non-finite params in coord descent.
    Co-authored-by: Norbert Preining <norbert@preining.info>

--- sklearn/linear_model/_coordinate_descent.py
+++ sklearn/linear_model/_coordinate_descent.py
@@ -1075,6 +1075,16 @@ class ElasticNet(MultiOutputMixin, RegressorMixin, LinearModel):
         # workaround since _set_intercept will cast self.coef_ into X.dtype
         self.coef_ = np.asarray(self.coef_, dtype=X.dtype)
 
+        # check for finiteness of coefficients
+        num_nonfinite = (~np.isfinite(self.coef_)).sum() + (
+            ~np.isfinite(self.intercept_)
+        ).sum()
+        if num_nonfinite > 0:
+            raise ValueError(
+                f"Coordinate descent iterations resulted in {num_nonfinite} non-finite"
+                " parameter values."
+            )
+
         # return self for chaining fit and predict calls
         return self
 
