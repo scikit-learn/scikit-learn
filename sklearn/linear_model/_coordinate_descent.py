ed1179e876c65ca7336e73627cb0c855f85d196e
Geoffrey Paris <geoffrey.paris@insa-rouen.fr>
2022-01-09 15:16:15 +0100

    Reverting changes to reduce the scope of the PR

--- sklearn/linear_model/_coordinate_descent.py
+++ sklearn/linear_model/_coordinate_descent.py
@@ -942,19 +942,9 @@ class ElasticNet(MultiOutputMixin, RegressorMixin, LinearModel):
                 multi_output=True,
                 y_numeric=True,
             )
-            if self.warm_start and hasattr(self, "coef_"):
-                y = check_array(
-                    y,
-                    order="F",
-                    copy=False,
-                    dtype=X.dtype.type,
-                    ensure_2d=False,
-                    force_all_finite=False,
-                )
-            else:
-                y = check_array(
-                    y, order="F", copy=False, dtype=X.dtype.type, ensure_2d=False
-                )
+            y = check_array(
+                y, order="F", copy=False, dtype=X.dtype.type, ensure_2d=False
+            )
 
         n_samples, n_features = X.shape
         alpha = self.alpha
@@ -2401,21 +2391,14 @@ class MultiTaskElasticNet(Lasso):
             self.normalize, default=False, estimator_name=self.__class__.__name__
         )
 
-        # Doesn't check if values are finite if warm_start=True
-        # and the estimator is already fitted
-        force_all_finite = False if self.warm_start and hasattr(self, "coef_") else True
-
         # Need to validate separately here.
         # We can't pass multi_ouput=True because that would allow y to be csr.
         check_X_params = dict(
             dtype=[np.float64, np.float32],
             order="F",
             copy=self.copy_X and self.fit_intercept,
-            force_all_finite=force_all_finite,
         )
-        check_y_params = dict(
-            ensure_2d=False, order="F", force_all_finite=force_all_finite
-        )
+        check_y_params = dict(ensure_2d=False, order="F")
         X, y = self._validate_data(
             X, y, validate_separately=(check_X_params, check_y_params)
         )
--- sklearn/utils/validation.py
+++ sklearn/utils/validation.py
@@ -1078,15 +1078,13 @@ def check_X_y(
     return X, y
 
 
-def _check_y(
-    y, multi_output=False, y_numeric=False, estimator=None, force_all_finite=True
-):
+def _check_y(y, multi_output=False, y_numeric=False, estimator=None):
     """Isolated part of check_X_y dedicated to y validation"""
     if multi_output:
         y = check_array(
             y,
             accept_sparse="csr",
-            force_all_finite=force_all_finite,
+            force_all_finite=True,
             ensure_2d=False,
             dtype=None,
             input_name="y",
@@ -1095,8 +1093,7 @@ def _check_y(
     else:
         estimator_name = _check_estimator_name(estimator)
         y = column_or_1d(y, warn=True)
-        if force_all_finite:
-            _assert_all_finite(y, input_name="y", estimator_name=estimator_name)
+        _assert_all_finite(y, input_name="y", estimator_name=estimator_name)
         _ensure_no_complex_data(y)
     if y_numeric and y.dtype.kind == "O":
         y = y.astype(np.float64)
