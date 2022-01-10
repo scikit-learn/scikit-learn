4ec8b78d41ebfea7b901970c1ace2163056ad72b
Micky774 <zainmeekail@gmail.com>
2022-01-10 15:22:44 -0500

    Removed trailing whitespace

--- sklearn/metrics/pairwise.py
+++ sklearn/metrics/pairwise.py
@@ -798,11 +798,11 @@ def manhattan_distances(X, Y=None, *, sum_over_features=True):
 
     Parameters
     ----------
-    X : array-like of shape (n_samples_X, n_features) 
+    X : array-like of shape (n_samples_X, n_features)
         An array where each row is a sample and each column is a feature.
 
     Y : array-like of shape=(n_samples_Y, n_features), default=None
-        An array where each row is a sample and each column is a feature. 
+        An array where each row is a sample and each column is a feature.
         If `None`, method uses `Y=X`.
 
     sum_over_features : bool, default=True
