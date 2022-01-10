8038ec7929264ea4e37685a19dfd2e618d6e3064
edubov <evgeni.dubov@intel.com>
2022-01-10 22:00:36 +0200

    added dereference import to _criterion.pyx

--- sklearn/tree/_criterion.pyx
+++ sklearn/tree/_criterion.pyx
@@ -23,6 +23,7 @@ np.import_array()
 from numpy.math cimport INFINITY
 
 from scipy.special.cython_special cimport xlogy
+from cython.operator cimport dereference as deref
 
 from ._utils cimport log
 from ._utils cimport WeightedMedianCalculator
