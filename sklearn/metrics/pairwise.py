b6cf0cebca566f60887fde3b79d134611b4d2756
Micky774 <zainmeekail@gmail.com>
2022-01-10 15:25:08 -0500

    Removed trailing whitespaces

--- sklearn/metrics/pairwise.py
+++ sklearn/metrics/pairwise.py
@@ -959,12 +959,12 @@ def paired_cosine_distances(X, Y):
 
     Parameters
     ----------
-    X : array-like of shape (n_samples_X, n_features) 
-        An array where each row is a sample and each column is a feature. 
+    X : array-like of shape (n_samples_X, n_features)
+        An array where each row is a sample and each column is a feature.
 
     Y : array-like of shape=(n_samples_Y, n_features), default=None
-        An array where each row is a sample and each column is a feature. 
-        If `None`, method uses `Y=X`. 
+        An array where each row is a sample and each column is a feature.
+        If `None`, method uses `Y=X`.
 
     Returns
     -------
