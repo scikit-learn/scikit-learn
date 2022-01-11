612172977ce8ef85acf8eccc63ba39d82917b912
Loïc Estève <loic.esteve@ymail.com>
2022-01-11 06:24:12 +0100

    Use CONDA_PREFIX which is more documented

--- build_tools/shared.sh
+++ build_tools/shared.sh
@@ -19,7 +19,7 @@ show_installed_libraries(){
     # use conda list when inside a conda environment. conda list shows more
     # info than pip list, e.g. whether OpenBLAS or MKL is installed as well as
     # the version of OpenBLAS or MKL
-    if [[ -n "$CONDA_DEFAULT_ENV" ]]; then
+    if [[ -n "$CONDA_PREFIX" ]]; then
         conda list
     else
         python -m pip list
