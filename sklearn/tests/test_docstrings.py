e72dcc0a559c534f13e619ecb7e7b26251dd3b3c
Atharva Patil <atharva7275@gmail.com>
2022-01-12 15:46:41 +0530

    DOC Ensures that kmeans_plusplus passes numpydoc validation

--- sklearn/tests/test_docstrings.py
+++ sklearn/tests/test_docstrings.py
@@ -12,7 +12,6 @@ import sklearn
 numpydoc_validation = pytest.importorskip("numpydoc.validate")
 
 FUNCTION_DOCSTRING_IGNORE_LIST = [
-    "sklearn.cluster._kmeans.kmeans_plusplus",
     "sklearn.cluster._optics.cluster_optics_xi",
     "sklearn.cluster._optics.compute_optics_graph",
     "sklearn.cluster._spectral.spectral_clustering",
