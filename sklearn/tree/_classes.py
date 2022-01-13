3a424c241ad3b05f1a281c2faa6989ea0747f674
genvalen <genvalen@protonmail.com>
2022-01-12 19:01:39 -0500

    Update sklearn/tree/_classes.py
    Co-authored-by: Thomas J. Fan <thomasjpfan@gmail.com>

--- sklearn/tree/_classes.py
+++ sklearn/tree/_classes.py
@@ -264,7 +264,7 @@ class BaseDecisionTree(MultiOutputMixin, BaseEstimator, metaclass=ABCMeta):
                 self.min_samples_split,
                 name="min_samples_split",
                 target_type=numbers.Integral,
-                min_val=1,
+                min_val=2,
             )
             min_samples_split = self.min_samples_split
         else:  # float
