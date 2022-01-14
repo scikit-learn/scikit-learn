46720aee27f879bef1d18dca82ca1ca51745623a
Christian Lorentzen <lorentzen.ch@gmail.com>
2022-01-14 23:50:31 +0100

    CLN no second check for sparse

--- sklearn/linear_model/_quantile.py
+++ sklearn/linear_model/_quantile.py
@@ -252,7 +252,7 @@ class QuantileRegressor(LinearModel, RegressorMixin, BaseEstimator):
             c[0] = 0
             c[n_params] = 0
 
-        if sparse.issparse(X) or self.solver in ["highs", "highs-ds", "highs-ipm"]:
+        if self.solver in ["highs", "highs-ds", "highs-ipm"]:
             # Note that highs methods always use a sparse CSC memory layout internally,
             # even for optimization problems parametrized using dense numpy arrays.
             # Therefore, we work with CSC matrices as early as possible to limit
