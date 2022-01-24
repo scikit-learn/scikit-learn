381c72cfc9cf1c69363e7faec12bc9858cab5dc5
edubov <evgeni.dubov@intel.com>
2022-01-24 23:41:58 +0200

    test typo fix

--- sklearn/ensemble/tests/test_forest.py
+++ sklearn/ensemble/tests/test_forest.py
@@ -196,7 +196,7 @@ def check_imbalanced_criterion(name, criterion):
 
 @pytest.mark.parametrize("name", ('RandomForestClassifier'))
 @pytest.mark.parametrize("criterion", ("hellinger"))
-def test_imbalanced_criterions(name):
+def test_imbalanced_criterions(name, criterion):
     check_imbalanced_criterion(name, criterion)
 
 
