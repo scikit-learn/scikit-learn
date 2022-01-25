3278b8df2847bfc2fd7c60745c7fa4bc657a7c49
edubov <evgeni.dubov@intel.com>
2022-01-25 21:55:04 +0200

    testing hellinger on all forest classifiers

--- sklearn/ensemble/tests/test_forest.py
+++ sklearn/ensemble/tests/test_forest.py
@@ -194,7 +194,7 @@ def check_imbalanced_criterion(name, criterion):
     )
 
 
-@pytest.mark.parametrize("name", ["RandomForestClassifier"])
+@pytest.mark.parametrize("name", FOREST_CLASSIFIERS)
 @pytest.mark.parametrize("criterion", ["hellinger"])
 def test_imbalanced_criterions(name, criterion):
     check_imbalanced_criterion(name, criterion)
