a844b3910ad44ec9af970f58c77f55a2021cbc3c
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2022-01-27 10:55:55 +0100

    add comments regarding pinning

--- build_tools/circle/build_test_arm.sh
+++ build_tools/circle/build_test_arm.sh
@@ -54,6 +54,8 @@ mamba install --verbose -y  ccache \
 setup_ccache
 
 if [[ "$COVERAGE" == "true" ]]; then
+    # coverage is pinned to 6.2 because 6.3 multiprocessing fork-safe
+    # cf. https://github.com/nedbat/coveragepy/issues/1310
     mamba install --verbose -y codecov pytest-cov coverage==6.2
 fi
 
