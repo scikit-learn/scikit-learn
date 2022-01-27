865c17bb215c5b3c790a443ba2ac59665c78c31c
Purna Chandra Mansingh <purnachandramansingh135@gmail.com>
2022-01-27 17:34:43 +0530

    added deprecation cycle to kneighbors and kneighbors_graph

--- sklearn/neighbors/_base.py
+++ sklearn/neighbors/_base.py
@@ -672,6 +672,7 @@ class KNeighborsMixin:
             result = neigh_ind
         return result
 
+    @deprecated("`kneighbors` is deprecated in 1.0 and will be removed in 1.2.")
     def kneighbors(self, X=None, n_neighbors=None, return_distance=True):
         """Find the K-neighbors of a point.
 
@@ -847,6 +848,7 @@ class KNeighborsMixin:
                 return neigh_dist, neigh_ind
             return neigh_ind
 
+    @deprecated("`kneighbors_graph` is deprecated in 1.0 and will be removed in 1.2.")
     def kneighbors_graph(self, X=None, n_neighbors=None, mode="connectivity"):
         """Compute the (weighted) graph of k-Neighbors for points in X.
 
