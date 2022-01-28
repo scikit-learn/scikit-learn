728b9d659fe17db1bb0d133d15d9ee6447f0aaf8
zero <zero@BULLSEYE-debian-live-builder>
2022-01-29 03:05:04 +0530

    fixed description on page sklearn.covariance._graph_lasso.graphical_lasso

--- sklearn/tests/test_docstrings.py
+++ sklearn/tests/test_docstrings.py
@@ -12,7 +12,6 @@ import sklearn
 numpydoc_validation = pytest.importorskip("numpydoc.validate")
 
 FUNCTION_DOCSTRING_IGNORE_LIST = [
-    "sklearn.covariance._graph_lasso.graphical_lasso",
     "sklearn.covariance._robust_covariance.fast_mcd",
     "sklearn.covariance._shrunk_covariance.ledoit_wolf",
     "sklearn.covariance._shrunk_covariance.ledoit_wolf_shrinkage",
