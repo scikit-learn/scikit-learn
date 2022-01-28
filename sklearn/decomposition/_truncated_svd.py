cd892f96883396f71cd6135ee3c56c5f619c53c4
Sultan Orazbayev <contact@econpoint.com>
2022-01-28 22:55:55 +0600

    DOC use the arxiv directive in the docstrings (#21418)
    Co-authored-by: aufarkari <aufar.di.sini@gmail.com>
    Co-authored-by: Chiara Marmo <cmarmo@users.noreply.github.com>
    Co-authored-by: Julien Jerphanion <git@jjerphan.xyz>
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- sklearn/decomposition/_truncated_svd.py
+++ sklearn/decomposition/_truncated_svd.py
@@ -12,7 +12,7 @@ import scipy.sparse as sp
 from scipy.sparse.linalg import svds
 
 from ..base import BaseEstimator, TransformerMixin, _ClassNamePrefixFeaturesOutMixin
-from ..utils import check_array, check_random_state, check_scalar
+from ..utils import check_array, check_random_state
 from ..utils._arpack import _init_arpack_v0
 from ..utils.extmath import randomized_svd, safe_sparse_dot, svd_flip
 from ..utils.sparsefuncs import mean_variance_axis
@@ -130,9 +130,9 @@ class TruncatedSVD(_ClassNamePrefixFeaturesOutMixin, TransformerMixin, BaseEstim
 
     References
     ----------
-    :arxiv:`"Finding structure with randomness:
+    :arxiv:`Halko, et al. (2009). "Finding structure with randomness:
     Stochastic algorithms for constructing approximate matrix decompositions"
-    <0909.4061>`, Halko, et al. (2009)
+    <0909.4061>`
 
     Examples
     --------
