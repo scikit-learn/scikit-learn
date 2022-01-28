9a4de1afa94ca3a7ec8b74e8e2dba7e990d8432f
Meekail Zain <zainmeekail@gmail.com>
2022-01-28 14:54:45 -0500

    Added new benchmark file

--- tmp_bench/benchmark_sparsity.py
+++ tmp_bench/benchmark_sparsity.py
@@ -44,33 +44,18 @@ def main() -> pd.DataFrame:
     m = Memory(cachedir=".")
 
     # %%
-    """
-    TOY_DSETS = {
-        'iris':functools.partial(datasets.load_iris,return_X_y=True),
-        'wine':functools.partial(datasets.load_wine,return_X_y=True),
-        'breast_cancer':functools.partial(datasets.load_breast_cancer,return_X_y=True),
-        'news':functools.partial(datasets.fetch_20newsgroups_vectorized,return_X_y=True)
-        }
-    """
-    TOY_DSETS = {}
-    BASE_SYNTH_DSETS = {
-        "base": datasets.make_classification,
-        "dense": functools.partial(datasets.make_classification, n_informative=15),
-        "redundant": functools.partial(datasets.make_classification, n_redundant=15),
-        "noisy": functools.partial(datasets.make_classification, n_features=200),
-        "repeated": functools.partial(datasets.make_classification, n_repeated=15),
-    }
-    N_SAMPLES = (10, 50, 250, 1000, 5000, 15000)
-    N_CLASSES = (2, 5, 15)
-    SYNTH_DSETS = {
-        f"{base}_{n_samples}_{n_classes}": functools.partial(
-            BASE_SYNTH_DSETS[base], n_samples=n_samples
+    def make_classification_scaled():
+        X, y = datasets.make_classification(
+            n_samples=15000, n_informative=15, n_classes=15
         )
-        for base in BASE_SYNTH_DSETS
-        for n_samples in N_SAMPLES
-        for n_classes in N_CLASSES
+        return StandardScaler().fit_transform(X), y
+
+    DSETS = {
+        "dense": make_classification_scaled,
+        "sparse": functools.partial(
+            datasets.fetch_20newsgroups_vectorized, return_X_y=True
+        ),
     }
-    DSETS = TOY_DSETS | SYNTH_DSETS
 
     # %%
     @m.cache()
@@ -79,7 +64,7 @@ def main() -> pd.DataFrame:
 
     # %%
     MULTI_CLASS = ("ovr", "multinomial")
-    SOLVER = ("lbfgs", "trust-ncg", "sag", "saga", "newton-cg")
+    SOLVER = ("lbfgs", "trust-ncg")
     PENALTY = ("l2", "none")
     CONFIG = tuple(
         {"multi_class": mc, "solver": s, "penalty": p}
@@ -88,7 +73,6 @@ def main() -> pd.DataFrame:
         for p in PENALTY
     )
     _LogisticRegression = functools.partial(LogisticRegression, max_iter=1000)
-    scaler = StandardScaler()
 
     # %%
     def build_row(X, y, conf, dset: str):
@@ -96,14 +80,11 @@ def main() -> pd.DataFrame:
         t_start = time.perf_counter()
         lr.fit(X, y)
         t_total = time.perf_counter() - t_start
-        base, n_samples, n_classes = dset.split("_")
         return {
             "cpu_time": t_total,
             "n_iter_": np.mean(lr.n_iter_),
             "NLL": metrics.log_loss(lr.predict(X), y),
-            "dset": base,
-            "n_samples": n_samples,
-            "n_classes": n_classes,
+            "dset": dset,
             **conf,
         }
 
@@ -113,10 +94,8 @@ def main() -> pd.DataFrame:
         df = on_load(args.load)
     else:
         data = []
-        for i, dset in enumerate(DSETS):
-            print(f"Progress: {i*100./len(DSETS):.2f}")
+        for dset in DSETS:
             X, y = make_dset(dset)
-            X = scaler.fit_transform(X)
             data.extend([build_row(X, y, conf, dset) for conf in CONFIG])
         df = pd.DataFrame(data)
 
@@ -125,13 +104,10 @@ def main() -> pd.DataFrame:
         print(f"Dataframe saved to {args.save}")
 
     dset_option = st.selectbox(
-        "Which dataset would you like to view?", ("base", "dense", "noisy")
+        "Which dataset would you like to view?", ("dense", "sparse")
     )
     y_option = st.selectbox(
         "Which metric would you like charted?", ("NLL", "cpu_time", "n_iter_")
-    )
-    n_classes_option = st.selectbox(
-        "Choose the number of classes in the dataset.", ("2", "5", "15")
     )
     multi_class_option = st.selectbox(
         "Which multi_class strategy should the solvers use?", ("ovr", "multinomial")
@@ -141,25 +117,15 @@ def main() -> pd.DataFrame:
     )
 
     conditions = (
-        (df["n_classes"] == n_classes_option)
-        & (df["multi_class"] == multi_class_option)
+        (df["multi_class"] == multi_class_option)
         & (df["penalty"] == penalty_option)
         & (df["dset"] == dset_option)
     )
     chart = (
         alt.Chart(df[conditions], height=500, width=900)
-        .mark_line()
-        .encode(
-            x="n_samples",
-            y=y_option,
-            color="solver",
-        )
-        .properties(
-            title=(
-                f"{y_option} vs. n_samples using the {dset_option}-style dataset with"
-                f" {n_classes_option} classes."
-            )
-        )
+        .mark_bar()
+        .encode(x="solver", y=y_option, column="multi_class", color="penalty")
+        .properties(title=f"{y_option} using the {dset_option}-style dataset")
     )
 
     st.altair_chart(chart)
