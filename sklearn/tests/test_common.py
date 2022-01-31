afc6884657394680705bf8da01b27f15715d388c
iofall <50991099+iofall@users.noreply.github.com>
2022-01-31 15:50:11 +0530

    FIX Remove validation for FeatureUnion from __init__ (#21954)
    Co-authored-by: iofall <50991099+iofall@users.noreply.github.com>
    Co-authored-by: arisayosh <15692997+arisayosh@users.noreply.github.com>
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- sklearn/tests/test_common.py
+++ sklearn/tests/test_common.py
@@ -419,7 +419,6 @@ def test_transformers_get_feature_names_out(transformer):
 
 VALIDATE_ESTIMATOR_INIT = [
     "ColumnTransformer",
-    "FeatureUnion",
     "SGDOneClassSVM",
     "TheilSenRegressor",
     "TweedieRegressor",
