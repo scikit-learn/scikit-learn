8991c3d7870df692fe01510e0fe6de62ea550cad
Thomas J. Fan <thomasjpfan@gmail.com>
2022-02-01 11:50:29 -0500

    ENH Adds get_feature_names to isotonic module (#22249)

--- sklearn/tests/test_common.py
+++ sklearn/tests/test_common.py
@@ -381,7 +381,6 @@ def test_pandas_column_name_consistency(estimator):
 # from this list to be tested
 GET_FEATURES_OUT_MODULES_TO_IGNORE = [
     "ensemble",
-    "isotonic",
     "kernel_approximation",
     "preprocessing",
     "neural_network",
