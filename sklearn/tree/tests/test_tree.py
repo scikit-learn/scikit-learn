97670a622ebbc21b31792b186f45eb8bb6c16f60
edubov <evgeni.dubov@intel.com>
2022-02-04 08:57:25 +0200

    fixed lint issues

--- sklearn/tree/tests/test_tree.py
+++ sklearn/tree/tests/test_tree.py
@@ -1582,7 +1582,8 @@ def test_sparse_parameters(tree_type, dataset):
     check_sparse_parameters(tree_type, dataset)
 
 
-@pytest.mark.parametrize('tree_type, criterion',
+@pytest.mark.parametrize(
+    'tree_type, criterion',
     itertools.chain(
         itertools.product(CLF_TREES, ["gini", "entropy"]),
         itertools.product(REG_TREES, REG_CRITERIONS)
@@ -1593,7 +1594,8 @@ def test_sparse_criterion(tree_type, criterion, dataset):
     check_sparse_criterion(tree_type, criterion, dataset)
 
 
-@pytest.mark.parametrize('tree_type, criterion',
+@pytest.mark.parametrize(
+    'tree_type, criterion',
     itertools.product(CLF_TREES, ["hellinger"])
 )
 @pytest.mark.parametrize("dataset", ["sparse-pos-binary", "sparse-neg-binary", "sparse-mix-binary", "zeros-binary"])
