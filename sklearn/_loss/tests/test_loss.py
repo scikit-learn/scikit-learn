6b3efe89210f18e2b6e92d6a428898bc30630245
Christian Lorentzen <lorentzen.ch@gmail.com>
2022-02-07 22:08:06 +0100

    CLN address review comments

--- sklearn/_loss/tests/test_loss.py
+++ sklearn/_loss/tests/test_loss.py
@@ -1055,31 +1055,31 @@ def test_init_gradient_and_hessian_raises(loss, params, err_msg):
             PinballLoss,
             {"quantile": None},
             "PinballLoss aka quantile loss only accepts numbers 0 < quantile < 1;"
-            f" {None} was given.",
+            " None was given.",
         ),
         (
             PinballLoss,
             {"quantile": 0},
             "PinballLoss aka quantile loss only accepts numbers 0 < quantile < 1;"
-            f" {0} was given.",
+            " 0 was given.",
         ),
         (
             PinballLoss,
             {"quantile": 1.1},
             "PinballLoss aka quantile loss only accepts numbers 0 < quantile < 1;"
-            f" {1.1} was given.",
+            " 1.1 was given.",
         ),
         (
             HalfTweedieLoss,
             {"power": None},
             "HalfTweedieLoss only accepts finite numbers as power parameter;"
-            f" {None} was given.",
+            " None was given.",
         ),
         (
             HalfTweedieLoss,
             {"power": np.inf},
             "HalfTweedieLoss only accepts finite numbers as power parameter;"
-            f" {np.inf} was given.",
+            " inf was given.",
         ),
     ],
 )
