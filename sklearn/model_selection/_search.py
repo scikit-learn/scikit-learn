376d30bcd228865c6115c66ab3e699d0786893d8
Jérémie du Boisberranger <34657725+jeremiedbb@users.noreply.github.com>
2022-02-09 18:02:12 +0100

    Update sklearn/model_selection/_search.py

--- sklearn/model_selection/_search.py
+++ sklearn/model_selection/_search.py
@@ -773,7 +773,7 @@ class BaseSearchCV(MetaEstimatorMixin, BaseEstimator, metaclass=ABCMeta):
             If a fit parameter is an array-like whose length is equal to
             `num_samples` then it will be split across CV groups along with `X`
             and `y`. For example, the :term:`sample_weight` parameter is split
-            becuase `len(sample_weights) = len(X)`.
+            because `len(sample_weights) = len(X)`.
 
         Returns
         -------
