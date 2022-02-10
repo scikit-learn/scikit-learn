301d9cb6ff82dcb0467632c81df69b9af8429303
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2022-02-10 14:43:56 +0100

    iter

--- build_tools/azure/install.sh
+++ build_tools/azure/install.sh
@@ -92,13 +92,14 @@ elif [[ "$DISTRIB" == "ubuntu" ]]; then
 
 elif [[ "$DISTRIB" == "debian-32" ]]; then
     apt-get update
-    apt-get install -y python3-dev python3-numpy python3-scipy python3-matplotlib libatlas3-base libatlas-base-dev python3-virtualenv python3-pandas ccache
+    apt-get install -y python3-dev python3-numpy python3-scipy python3-matplotlib libatlas3-base libatlas-base-dev python3-virtualenv ccache
 
     python3 -m virtualenv --system-site-packages --python=python3 $VIRTUALENV
     source $VIRTUALENV/bin/activate
     setup_ccache
     python -m pip install $(get_dep cython $CYTHON_VERSION) \
-                          $(get_dep joblib $JOBLIB_VERSION)
+                          $(get_dep joblib $JOBLIB_VERSION) \
+                          $(get_dep pandas $PANDAS_VERSION)
 
 elif [[ "$DISTRIB" == "conda-pip-latest" ]]; then
     # FIXME: temporary fix to link against system libraries on linux
