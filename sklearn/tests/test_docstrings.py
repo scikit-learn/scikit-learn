0cb06ea33ead6bc191e6721942606a6b17ecc01a
Reshama Shaikh <reshama.stat@gmail.com>
2022-02-11 11:18:23 -0500

    DOC Ensures that function passes numpydoc validation: paired_distances (#22380)

--- sklearn/tests/test_docstrings.py
+++ sklearn/tests/test_docstrings.py
@@ -106,7 +106,6 @@ FUNCTION_DOCSTRING_IGNORE_LIST = [
     "sklearn.metrics.pairwise.haversine_distances",
     "sklearn.metrics.pairwise.kernel_metrics",
     "sklearn.metrics.pairwise.laplacian_kernel",
-    "sklearn.metrics.pairwise.paired_distances",
     "sklearn.metrics.pairwise.paired_euclidean_distances",
     "sklearn.metrics.pairwise.paired_manhattan_distances",
     "sklearn.metrics.pairwise.pairwise_distances_argmin",
