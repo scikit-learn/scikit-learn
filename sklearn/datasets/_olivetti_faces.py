68ac8e81b21674dfc3c21c88e9048cb6dde696ea
Matias Rodriguez Vazquez <mrodvaz@gmail.com>
2022-02-14 09:06:56 +0000

    fix typo in return description

--- sklearn/datasets/_olivetti_faces.py
+++ sklearn/datasets/_olivetti_faces.py
@@ -101,7 +101,7 @@ def fetch_olivetti_faces(
             Description of the modified Olivetti Faces Dataset.
 
     (data, target) : tuple if `return_X_y=True`
-        Tuple with the `data` and `target` object described above.
+        Tuple with the `data` and `target` objects described above.
 
         .. versionadded:: 0.22
     """
