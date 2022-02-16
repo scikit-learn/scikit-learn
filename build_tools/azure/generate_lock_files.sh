d2a6dea3d960807a8f14df30dd6de723e7648845
Loïc Estève <loic.esteve@ymail.com>
2022-02-16 18:09:02 +0100

    Use lock file in py37_conda_defaults_openblas build.
    Also regenerate lock files

--- build_tools/azure/generate_lock_files.sh
+++ build_tools/azure/generate_lock_files.sh
@@ -2,12 +2,22 @@
 
 set -e
 
+function conda_lock_func() {
+    build_name=$1
+    platform=$2
+    environment_base_name=${3:-${build_name}}
+    conda-lock lock \
+        --platform ${platform} \
+        --file ${environment_base_name}_environment.yml \
+        --filename-template "${build_name}_${platform}.lock"
+}
+
 build_name=pylatest_conda_forge_mkl
 platforms="linux-64 osx-64"
 
 for platform in ${platforms}; do
-    conda-lock \
-        --platform ${platform} \
-        --file ${build_name}_${platform}_environment.yml \
-        --filename-template "${build_name}_${platform}.lock"
+    conda_lock_func ${build_name} ${platform} ${build_name}_${platform}
 done
+
+build_name=py37_conda_defaults_openblas
+conda_lock_func ${build_name} linux-64
