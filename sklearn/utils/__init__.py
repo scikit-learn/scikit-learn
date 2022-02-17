6a16763c008446953ed9380e084ed70a285a9f7e
Julien Jerphanion <git@jjerphan.xyz>
2022-02-17 10:37:22 +0100

    ENH Introduce `PairwiseDistancesReduction` and `PairwiseDistancesArgKmin` (feature branch) (#22134)
    * MAINT Introduce Pairwise Distances Reductions private submodule  (#22064)
    
    * MAINT Introduce FastEuclideanPairwiseArgKmin  (#22065)
    
    * fixup! Merge branch 'main' into pairwise-distances-argkmin
    
    Remove duplicated Bunch
    
    * MAINT Plug `PairwiseDistancesArgKmin` as a back-end (#22288)
    
    * Forward pairwise_dist_chunk_size in the configuration
    
    * Flip finalized results for PairwiseDistancesArgKmin
    
    The previous would have made the code more complex
    by introducing some boilerplate for the interface plugs.
    
    Having it this way actually simplifies the code.
    
    This also removes the haversine branch for
    test_pairwise_distances_argkmin
    
    * Plug PairwiseDistancesArgKmin as a back-end
    
    * Adapt test accordingly
    
    * Add whats_new entry
    
    * Change input validation order for kneighbors
    
    * Remove duplicated test_neighbors_distance_metric_deprecation
    
    * Adapt the documentation
    
    * Add mahalanobis case to test fixtures
    
    * Correct whats_new entry
    
    * CLN Remove unneeded private metric attribute
    
    This was needed when 'fast_sqeuclidean' and 'fast_euclidean'
    were present to choose the best implementation based on the user
    specification.
    
    Those metric have been removed since then, making this attribute
    useless.
    
    * TST Assert FutureWarning instead of DeprecationWarning in
    test_neighbors_metrics
    
    * MAINT Add use_pairwise_dist_activate to scikit-learn config
    
    * TST Add a test for the 'brute' backends' results' consistency
    
    Co-authored-by: Thomas J. Fan <thomasjpfan@gmail.com>
    
    * fixup! MAINT Add use_pairwise_dist_activate to scikit-learn config
    
    * fixup! fixup! MAINT Add use_pairwise_dist_activate to scikit-learn config
    
    * TST Filter FutureWarning for WMinkowskiDistance
    
    * MAINT pin numpydoc in arm for now (#22292)
    
    * fixup! TST Filter FutureWarning for WMinkowskiDistance
    
    * Revert keywords arguments removal for the GEMM trick for 'euclidean'
    
    * MAINT pin max numpydoc for now (#22286)
    
    * Add 'haversine' to CDIST_PAIRWISE_DISTANCES_REDUCTION_COMMON_METRICS
    
    * fixup! Add 'haversine' to CDIST_PAIRWISE_DISTANCES_REDUCTION_COMMON_METRICS
    
    * Apply suggestions from code review
    
    * MAINT Document some config parameters for maintenance
    
    Also rename one of them.
    
    * FIX Support and test one of 'sqeuclidean' specification
    
    Co-authored-by: Olivier Grisel <olivier.grisel@ensta.org>
    
    * FIX Various typos fix and correct haversine
    
    'haversine' is not supported by cdist.
    
    * Directly use get_config
    
    * CLN Apply comments from review
    
    * Motivate swapped returned values
    
    * TST Remove mahalanobis from test fixtures
    
    * MNT Add comment regaduction functions' signatures
    
    * TST Complete test for `pairwise_distance_{argmin,argmin_min}` (#22371)
    
    * DOC Add sub-pull requests to the whats_new entry
    
    * DOC place comment inside functions
    
    * DOC move up whatsnew entry
    
    Co-authored-by: Thomas J. Fan <thomasjpfan@gmail.com>
    Co-authored-by: Christian Lorentzen <lorentzen.ch@gmail.com>
    Co-authored-by: Olivier Grisel <olivier.grisel@ensta.org>
    Co-authored-by: Jérémie du Boisberranger <jeremiedbb@users.noreply.github.com>

--- sklearn/utils/__init__.py
+++ sklearn/utils/__init__.py
@@ -26,7 +26,7 @@ from .class_weight import compute_class_weight, compute_sample_weight
 from . import _joblib
 from ..exceptions import DataConversionWarning
 from .deprecation import deprecated
-from .fixes import np_version, parse_version
+from .fixes import np_version, parse_version, threadpool_info
 from ._estimator_html_repr import estimator_html_repr
 from .validation import (
     as_float_array,
@@ -81,6 +81,39 @@ __all__ = [
 
 IS_PYPY = platform.python_implementation() == "PyPy"
 _IS_32BIT = 8 * struct.calcsize("P") == 32
+
+
+def _in_unstable_openblas_configuration():
+    """Return True if in an unstable configuration for OpenBLAS"""
+
+    # Import libraries which might load OpenBLAS.
+    import numpy  # noqa
+    import scipy  # noqa
+
+    modules_info = threadpool_info()
+
+    open_blas_used = any(info["internal_api"] == "openblas" for info in modules_info)
+    if not open_blas_used:
+        return False
+
+    # OpenBLAS 0.3.16 fixed unstability for arm64, see:
+    # https://github.com/xianyi/OpenBLAS/blob/1b6db3dbba672b4f8af935bd43a1ff6cff4d20b7/Changelog.txt#L56-L58 # noqa
+    openblas_arm64_stable_version = parse_version("0.3.16")
+    for info in modules_info:
+        if info["internal_api"] != "openblas":
+            continue
+        openblas_version = info.get("version")
+        openblas_architecture = info.get("architecture")
+        if openblas_version is None or openblas_architecture is None:
+            # Cannot be sure that OpenBLAS is good enough. Assume unstable:
+            return True
+        if (
+            openblas_architecture == "neoversen1"
+            and parse_version(openblas_version) < openblas_arm64_stable_version
+        ):
+            # See discussions in https://github.com/numpy/numpy/issues/19411
+            return True
+    return False
 
 
 def safe_mask(X, mask):
