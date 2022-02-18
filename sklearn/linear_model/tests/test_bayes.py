d579aa2d1a0f7e55949ee0d1301190000989aae8
Meekail Zain <zainmeekail@gmail.com>
2022-02-18 18:50:36 -0500

    Parameterized test and used np.assert_all_close

--- sklearn/linear_model/tests/test_bayes.py
+++ sklearn/linear_model/tests/test_bayes.py
@@ -308,10 +308,10 @@ def test_dtype_match(dtype, model):
     assert y_std.dtype == X.dtype
 
 
-def test_dtype_correctness():
+@pytest.mark.parametrize("model", [BayesianRidge(), ARDRegression()])
+def test_dtype_correctness(model):
     X = np.array([[1, 1], [3, 4], [5, 7], [4, 1], [2, 6], [3, 10], [3, 2]])
     y = np.array([1, 2, 3, 2, 0, 4, 5]).T
-    for model in (BayesianRidge(), ARDRegression()):
-        coef_32 = model.fit(X.astype(np.float32), y).coef_
-        coef_64 = model.fit(X.astype(np.float64), y).coef_
-        assert_array_almost_equal(coef_32, coef_64, decimal=5)
+    coef_32 = model.fit(X.astype(np.float32), y).coef_
+    coef_64 = model.fit(X.astype(np.float64), y).coef_
+    np.testing.assert_allclose(coef_32, coef_64, rtol=1e-5)
