886b794946ba3904f3a7c1095e64bcecfefeb52b
Thomas J. Fan <thomasjpfan@gmail.com>
2022-02-19 17:29:54 -0500

    CLN Rename variable

--- doc/conf.py
+++ doc/conf.py
@@ -166,7 +166,7 @@ html_theme = "scikit-learn-modern"
 html_theme_options = {
     "google_analytics": True,
     "mathjax_path": mathjax_path,
-    "link_to_live_dev_in_nav_bar": not parsed_version.is_devrelease,
+    "link_to_live_dev_in_navbar": not parsed_version.is_devrelease,
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
