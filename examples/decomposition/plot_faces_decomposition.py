86ad843a46887396b6cded609053681abfe644ac
Oleksandr Husak <oleksandr.husak@mailbox.tu-dresden.de>
2022-02-20 18:12:46 +0100

    fix markdown, add colorbar and desc for dict learning

--- examples/decomposition/plot_faces_decomposition.py
+++ examples/decomposition/plot_faces_decomposition.py
@@ -15,7 +15,7 @@ matrix decomposition (dimension reduction) methods from the module
 
 # %%
 # Dataset preparation
-# --------------------
+# -------------------
 #
 # Loading and preprocessing the Olivetti faces dataset.
 
@@ -25,7 +25,7 @@ from numpy.random import RandomState
 import matplotlib.pyplot as plt
 
 from sklearn.datasets import fetch_olivetti_faces
-from sklearn.cluster import MiniBatchKMeans
+from sklearn import cluster
 from sklearn import decomposition
 
 rng = RandomState(0)
@@ -44,7 +44,6 @@ faces_centered -= faces_centered.mean(axis=1).reshape(n_samples, -1)
 
 print("Dataset consists of %d faces" % n_samples)
 
-
 # %%
 # Define a base function to plot the gallery of faces.
 
@@ -54,23 +53,28 @@ image_shape = (64, 64)
 
 
 def plot_gallery(title, images, n_col=n_col, n_row=n_row, cmap=plt.cm.gray):
-    fig = plt.figure(
-        figsize=(2.0 * n_col, 2.26 * n_row),
+    fig, axs = plt.subplots(
+        nrows=n_row,
+        ncols=n_col,
+        figsize=(2.0 * n_col, 2.3 * n_row),
         facecolor="white",
-        tight_layout={"pad": 0.3},
+        constrained_layout=True,
     )
-    fig.suptitle(title, size=16, wrap=True)
-    for i, comp in enumerate(images):
-        plt.subplot(n_row, n_col, i + 1)
-        vmax = max(comp.max(), -comp.min())
-        plt.imshow(
-            comp.reshape(image_shape),
+    fig.set_constrained_layout_pads(w_pad=0.01, h_pad=0.02, hspace=0, wspace=0)
+    fig.set_edgecolor("black")
+    fig.suptitle(title, size=16)
+    for ax, vec in zip(axs.flat, images):
+        vmax = max(vec.max(), -vec.min())
+        im = ax.imshow(
+            vec.reshape(image_shape),
             cmap=cmap,
             interpolation="nearest",
             vmin=-vmax,
             vmax=vmax,
         )
-        plt.axis("off")
+        ax.axis("off")
+
+    fig.colorbar(im, ax=axs, orientation="horizontal", shrink=0.99, aspect=40, pad=0.01)
     plt.show()
 
 
@@ -82,18 +86,18 @@ plot_gallery("Faces from dataset", faces_centered[:n_components])
 
 # %%
 # Decomposition
-# --------------
+# -------------
 #
 # Initialise different estimators for decomposition and fit each
 # of them on all images and plot some results. Each estimator extracts
-# 6 components as vectors :math:`h \in \mathbf{R}^{4096}`.
+# 6 components as vectors :math:`h \in \R^{4096}`.
 # We just displayed these vectors in human-friendly visualisation as 64x64 pixel images.
 #
 # Read more in the :ref:`User Guide <decompositions>`.
 
 # %%
 # Eigenfaces - PCA using randomized SVD
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 # Linear dimensionality reduction using Singular Value Decomposition (SVD) of the data
 # to project it to a lower dimensional space.
 #
@@ -115,7 +119,7 @@ plot_gallery(
 
 # %%
 # Non-negative components - NMF
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 #
 # Estimate non- negative original data as production of two non-negative matrices.
 
@@ -126,20 +130,22 @@ plot_gallery("Non-negative components - NMF", nmf_estimator.components_[:n_compo
 
 # %%
 # Independent components - FastICA
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 # Independent component analysis separates a multivariate vectors into additive
 # subcomponents that are maximally independent.
 
 # %%
-ica_estimator = decomposition.FastICA(n_components=n_components, whiten=True)
+ica_estimator = decomposition.FastICA(
+    n_components=n_components, max_iter=400, whiten="arbitrary-variance", tol=15e-5
+)
 ica_estimator.fit(faces_centered)
 plot_gallery(
     "Independent components - FastICA", ica_estimator.components_[:n_components]
 )
 
 # %%
-# Sparse comp. - MiniBatchSparsePCA
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# Sparse components - MiniBatchSparsePCA
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 #
 # Mini-batch sparse PCA (`MiniBatchSparsePCA`) extracts the set of sparse
 # components that best reconstruct the data. This variant is faster but
@@ -147,11 +153,7 @@ plot_gallery(
 
 # %%
 batch_pca_estimator = decomposition.MiniBatchSparsePCA(
-    n_components=n_components,
-    alpha=0.8,
-    n_iter=100,
-    batch_size=3,
-    random_state=rng,
+    n_components=n_components, alpha=0.1, n_iter=100, batch_size=3, random_state=rng
 )
 batch_pca_estimator.fit(faces_centered)
 plot_gallery(
@@ -160,7 +162,7 @@ plot_gallery(
 
 # %%
 # Dictionary learning
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# ^^^^^^^^^^^^^^^^^^^
 #
 # By default, :class:`MiniBatchDictionaryLearning` divides the data into
 # mini-batches and optimizes in an online manner by cycling over the
@@ -168,21 +170,21 @@ plot_gallery(
 
 # %%
 batch_dict_estimator = decomposition.MiniBatchDictionaryLearning(
-    n_components=15, alpha=0.1, n_iter=50, batch_size=3, random_state=rng
+    n_components=n_components, alpha=0.1, n_iter=50, batch_size=3, random_state=rng
 )
 batch_dict_estimator.fit(faces_centered)
 plot_gallery("Dictionary learning", batch_dict_estimator.components_[:n_components])
 
 # %%
 # Cluster centers - MiniBatchKMeans
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 #
 # `MiniBatchKMeans` is computationally efficient and implements on-line
-# learning with a partial_fit method. That is why it could be beneficial
+# learning with a `partial_fit` method. That is why it could be beneficial
 # to enhance some time-consuming algorithms with  `MiniBatchKMeans`.
 
 # %%
-kmeans_estimator = MiniBatchKMeans(
+kmeans_estimator = cluster.MiniBatchKMeans(
     n_clusters=n_components,
     tol=1e-3,
     batch_size=20,
@@ -198,7 +200,7 @@ plot_gallery(
 
 # %%
 # Factor Analysis components - FA
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 #
 # `Factor Analysis` is similar to `PCA` but has the advantage of modelling the
 # variance in every direction of the input space independently
@@ -223,17 +225,24 @@ plt.imshow(
 )
 plt.axis("off")
 plt.title("Pixelwise variance from \n Factor Analysis (FA)", size=16, wrap=True)
+plt.colorbar(orientation="horizontal", shrink=0.8, pad=0.03)
 plt.show()
 
 # %%
 # Decomposition: Dictionary learning
-# -----------------------------------
+# ----------------------------------
 #
 # In the further section, let's consider :ref:`DictionaryLearning` more precisely.
 # Dictionary learning is a problem that amounts to finding a sparse representation
 # of the input data as a combination of simple elements. These simple elements form
-# a dictionary. It is possible to constrain the dictionary and/or code to be positive
-# to match constraints that may be present in the data.
+# a dictionary. It is possible to constrain the dictionary and/or coding coefficients
+# to be positive to match constraints that may be present in the data.
+#
+# :class:`MiniBatchDictionaryLearning` implements a faster, but less accurate
+# version of the dictionary learning algorithm that is better suited for large
+# datasets.
+#
+# Read more in the :ref:`User Guide <_MiniBatchDictionaryLearning>`.
 
 # %%
 # Plot the same samples from our dataset but with another colormap.
@@ -243,18 +252,25 @@ plt.show()
 plot_gallery("Faces from dataset", faces_centered[:n_components], cmap=plt.cm.RdBu)
 
 # %%
-# Similar to the previous examples, we cahnge parameters and train
-# `MiniBatchDictionaryLearning` estimator on all images. The results
-# of the different positivity constraints are presented below.
+# Similar to the previous examples, we change parameters and train
+# `MiniBatchDictionaryLearning` estimator on all images. Generally,
+# the dictionary learning and sparse encoding decompose input data
+# into the dictionary and the coding coefficients matrices.
+# :math:`X \approx UV`, where :math:`X = [x_1, . . . , x_n]`,
+# :math:`X \in R^{m×n}`, dictionary :math:`U \in R^{m×k}`, coding
+# coefficients :math:`V \in R^{k×n}`.
+#
+# Also below are the results when the dictionary and coding
+# coefficients are positively constrained.
 
 # %%
 # Dictionary learning - positive dictionary
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 #
-
+# In the following section we enforce positivity when finding the dictionary.
 # %%
 dict_pos_dict_estimator = decomposition.MiniBatchDictionaryLearning(
-    n_components=15,
+    n_components=n_components,
     alpha=0.1,
     n_iter=50,
     batch_size=3,
@@ -270,12 +286,13 @@ plot_gallery(
 
 # %%
 # Dictionary learning - positive code
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 #
+# Below we constrain the coding coefficients as a positive matrix.
 
 # %%
 dict_pos_code_estimator = decomposition.MiniBatchDictionaryLearning(
-    n_components=15,
+    n_components=n_components,
     alpha=0.1,
     n_iter=50,
     batch_size=3,
@@ -292,12 +309,14 @@ plot_gallery(
 
 # %%
 # Dictionary learning - positive dictionary & code
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 #
+# Also below are the results if the dictionary values and coding
+# coefficients are positively constrained.
 
 # %%
 dict_pos_estimator = decomposition.MiniBatchDictionaryLearning(
-    n_components=15,
+    n_components=n_components,
     alpha=0.1,
     n_iter=50,
     batch_size=3,
