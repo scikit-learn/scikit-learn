d2798fadc44af3c90eacdbeaacca953397c47b88
Micky774 <zainmeekail@gmail.com>
2022-02-25 22:51:27 -0500

    Updated variable names in tests per review suggestions

--- sklearn/linear_model/tests/test_bayes.py
+++ sklearn/linear_model/tests/test_bayes.py
@@ -297,7 +297,7 @@ def test_dtype_match(dtype, Estimator):
     X = np.array([[1, 1], [3, 4], [5, 7], [4, 1], [2, 6], [3, 10], [3, 2]], dtype=dtype)
     y = np.array([1, 2, 3, 2, 0, 4, 5]).T
 
-    model = _model()
+    model = Estimator()
     # check type consistency
     model.fit(X, y)
     y_mean, y_std = model.predict(X, return_std=True)
@@ -310,7 +310,7 @@ def test_dtype_match(dtype, Estimator):
 def test_dtype_correctness(Estimator):
     X = np.array([[1, 1], [3, 4], [5, 7], [4, 1], [2, 6], [3, 10], [3, 2]])
     y = np.array([1, 2, 3, 2, 0, 4, 5]).T
-    model = _model()
+    model = Estimator()
     coef_32 = model.fit(X.astype(np.float32), y).coef_
     coef_64 = model.fit(X.astype(np.float64), y).coef_
     np.testing.assert_allclose(coef_32, coef_64, rtol=1e-4)
