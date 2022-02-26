aa069c970730c668ec71d85a2b487ef05f8b59f2
Venkatachalam Natchiappan <venky.yuvy@gmail.com>
2022-02-26 08:40:42 +0530

    asymmetric error testing

--- sklearn/linear_model/tests/test_sgd.py
+++ sklearn/linear_model/tests/test_sgd.py
@@ -4,6 +4,8 @@ import joblib
 import pytest
 import numpy as np
 import scipy.sparse as sp
+from pytest import approx
+from scipy.optimize import minimize
 
 from sklearn.utils._testing import assert_allclose
 from sklearn.utils._testing import assert_array_equal
@@ -24,6 +26,7 @@ from sklearn.model_selection import StratifiedShuffleSplit, ShuffleSplit
 from sklearn.linear_model import _sgd_fast as sgd_fast
 from sklearn.linear_model import QuantileRegressor
 from sklearn.model_selection import RandomizedSearchCV
+from sklearn.metrics import mean_pinball_loss
 
 
 def _update_kwargs(kwargs):
@@ -2231,3 +2234,68 @@ def test_pinball_loss_w_partial_fit():
         est.partial_fit(X[inds], y[inds])
 
     assert_allclose(est.coef_, coef, rtol=2)
+
+
+@pytest.mark.skipif(
+    sp_version < parse_version("1.6.0"),
+    reason="The `highs` solver is available from the 1.6.0 scipy version",
+)
+@pytest.mark.parametrize("quantile", [0.2, 0.5, 0.8])
+def test_asymmetric_error(quantile):
+    """Test for asymmetric distributed targets."""
+    n_samples = 1000
+    rng = np.random.RandomState(42)
+    X = np.concatenate(
+        (
+            np.abs(rng.randn(n_samples)[:, None]),
+            -rng.randint(2, size=(n_samples, 1)),
+        ),
+        axis=1,
+    )
+    intercept = 1.23
+    coef = np.array([0.5, -2])
+    assert np.min(X @ coef + intercept) > 0
+    # For an exponential distribution with rate lambda, e.g. exp(-lambda * x),
+    # the quantile at level q is:
+    #   quantile(q) = - log(1 - q) / lambda
+    #   scale = 1/lambda = -quantile(q) / log(1 - q)
+    y = rng.exponential(
+        scale=-(X @ coef + intercept) / np.log(1 - quantile), size=n_samples
+    )
+    model = SGDRegressor(
+        penalty="l1",
+        alpha=0,
+        loss="pinball",
+        quantile=quantile,
+        eta0=1,
+        learning_rate="adaptive",
+        tol=None,
+        random_state=42,
+    ).fit(X, y)
+
+    assert model.intercept_ == approx(intercept, rel=0.2)
+    assert_allclose(model.coef_, coef, rtol=0.6)
+    assert_allclose(np.mean(model.predict(X) > y), quantile, atol=1e-2)
+
+    # Now compare to Nelder-Mead optimization with L1 penalty
+    alpha = 0.01
+    model.set_params(alpha=alpha).fit(X, y)
+    model_coef = np.r_[model.intercept_, model.coef_]
+
+    def func(coef):
+        loss = mean_pinball_loss(y, X @ coef[1:] + coef[0], alpha=quantile)
+        L1 = np.sum(np.abs(coef[1:]))
+        return loss + alpha * L1
+
+    res = minimize(
+        fun=func,
+        x0=[1, 0, -1],
+        method="Nelder-Mead",
+        tol=1e-12,
+        options={"maxiter": 2000},
+    )
+
+    assert func(model_coef) == approx(func(res.x))
+    assert_allclose(model.intercept_, res.x[0])
+    assert_allclose(model.coef_, res.x[1:])
+    assert_allclose(np.mean(model.predict(X) > y), quantile, atol=1e-2)
