a85c7752086558d9333309078be47babf987aec0
Thomas J. Fan <thomasjpfan@gmail.com>
2022-02-25 20:20:33 -0500

    DOC Fixes doc build [doc build]

--- build_tools/circle/build_doc.sh
+++ build_tools/circle/build_doc.sh
@@ -134,8 +134,8 @@ make_args="SPHINXOPTS=-T $make_args"  # show full traceback on exception
 
 # Installing required system packages to support the rendering of math
 # notation in the HTML documentation and to optimize the image files
-apt-get -yq update --allow-releaseinfo-change
-apt-get -yq --no-install-suggests --no-install-recommends \
+sudo -E apt-get -yq update --allow-releaseinfo-change
+sudo -E apt-get -yq --no-install-suggests --no-install-recommends \
     install dvipng gsfonts ccache zip optipng
 
 # deactivate circleci virtualenv and setup a miniconda env instead
@@ -177,11 +177,6 @@ mamba create -n $CONDA_ENV_NAME --yes --quiet \
     compilers
 
 source activate testenv
-# Pin PyWavelet to 1.1.1 that is the latest version that support our minumum
-# NumPy version required. If PyWavelets 1.2+ is installed, it would require
-# NumPy 1.17+ that trigger a bug with Pandas 0.25:
-# https://github.com/numpy/numpy/issues/18355#issuecomment-774610226
-pip install PyWavelets==1.1.1
 pip install "$(get_dep scikit-image $SCIKIT_IMAGE_VERSION)"
 pip install "$(get_dep sphinx-gallery $SPHINX_GALLERY_VERSION)"
 pip install "$(get_dep numpydoc $NUMPYDOC_VERSION)"
