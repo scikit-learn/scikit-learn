f83fe62cc6f343771e238d439ee9a129e14edc4f
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2022-02-28 19:39:25 +0100

    black

--- examples/cluster/plot_coin_segmentation.py
+++ examples/cluster/plot_coin_segmentation.py
@@ -45,7 +45,9 @@ orig_coins = coins()
 # Applying a Gaussian filter for smoothing prior to down-scaling
 # reduces aliasing artifacts.
 smoothened_coins = gaussian_filter(orig_coins, sigma=2)
-rescaled_coins = rescale(smoothened_coins, 0.2, mode="reflect", anti_aliasing=False, multichannel=False)
+rescaled_coins = rescale(
+    smoothened_coins, 0.2, mode="reflect", anti_aliasing=False, multichannel=False
+)
 
 # Convert the image into a graph with the value of the gradient on the
 # edges.
