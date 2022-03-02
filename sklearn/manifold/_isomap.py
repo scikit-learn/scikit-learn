2bf5f02e7ad6c1a221ddc0c5771164e02cbfb208
Jérémie du Boisberranger <34657725+jeremiedbb@users.noreply.github.com>
2022-03-02 11:32:06 +0100

    MNT Clean fixes and compat for old versions of our dependencies (#22642)
    Co-authored-by: Olivier Grisel <olivier.grisel@ensta.org>

--- sklearn/manifold/_isomap.py
+++ sklearn/manifold/_isomap.py
@@ -6,7 +6,6 @@ import warnings
 
 import numpy as np
 
-import scipy
 from scipy.sparse import issparse
 from scipy.sparse.csgraph import shortest_path
 from scipy.sparse.csgraph import connected_components
@@ -18,7 +17,6 @@ from ..utils.validation import check_is_fitted
 from ..decomposition import KernelPCA
 from ..preprocessing import KernelCenterer
 from ..utils.graph import _fix_connected_components
-from ..externals._packaging.version import parse as parse_version
 
 
 class Isomap(_ClassNamePrefixFeaturesOutMixin, TransformerMixin, BaseEstimator):
@@ -275,11 +273,6 @@ class Isomap(_ClassNamePrefixFeaturesOutMixin, TransformerMixin, BaseEstimator):
                 metric=self.nbrs_.effective_metric_,
                 **self.nbrs_.effective_metric_params_,
             )
-
-        if parse_version(scipy.__version__) < parse_version("1.3.2"):
-            # make identical samples have a nonzero distance, to account for
-            # issues in old scipy Floyd-Warshall implementation.
-            nbg.data += 1e-15
 
         self.dist_matrix_ = shortest_path(nbg, method=self.path_method, directed=False)
 
