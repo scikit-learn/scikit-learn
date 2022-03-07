71a669b1f5a4f9d2a05418c594f749938cd8c04f
Loïc Estève <loic.esteve@ymail.com>
2022-03-07 21:28:20 +0100

    fix

--- build_tools/azure/install.sh
+++ build_tools/azure/install.sh
@@ -61,7 +61,7 @@ pre_python_environment_install() {
 
 python_environment_install_and_activate() {
     if [[ -n "$LOCK_FILE" ]]; then
-        if [[ "$DISTRIB" == "conda" ]]; then
+        if [[ "$DISTRIB" == "conda" || "$DISTRIB" == "conda-pip-latest" ]]; then
             conda update -n base conda -y
             conda install pip -y
             conda list
