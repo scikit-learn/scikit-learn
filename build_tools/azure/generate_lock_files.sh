71653a1c46e2967ed6e60a63b3c25c46ffb487dd
Loïc Estève <loic.esteve@ymail.com>
2022-03-07 21:10:27 +0100

    Update script + regenerate lock files

--- build_tools/azure/generate_lock_files.sh
+++ build_tools/azure/generate_lock_files.sh
@@ -9,12 +9,12 @@ function conda_lock_func() {
     conda-lock lock \
         --platform ${platform} \
         --file ${environment_base_name}_environment.yml \
-        --lockfile "${build_name}_${platform}_lock.yml"
+        --lockfile "${build_name}_${platform}_conda-lock.yml"
 }
 
 function pip_lock_func() {
     build_name=$1
-    pip-compile "${build_name}_requirements.txt" -o "{build_name}_lock.txt"
+    pip-compile "${build_name}_requirements.txt" -o "${build_name}_lock.txt"
 }
 
 build_name=pylatest_conda_forge_mkl
@@ -25,6 +25,9 @@ for platform in ${platforms}; do
 done
 
 build_name=py38_conda_defaults_openblas
+conda_lock_func ${build_name} linux-64
+
+build_name=pylatest_pip_openblas_pandas
 conda_lock_func ${build_name} linux-64
 
 # TODO: how to make sure that python 3.8 (Ubuntu 20.04 has python 3.8.10)is
