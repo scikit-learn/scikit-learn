9778afed5d6df05afc8314ba90a06ed8f2d0811b
ArturoAmorQ <arturo.amor-quiroz@polytechnique.edu>
2022-03-08 11:36:49 +0100

    First draft

--- examples/linear_model/plot_ard.py
+++ examples/linear_model/plot_ard.py
@@ -24,33 +24,26 @@ samples.
 
 """
 
+# %%
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import stats
 
 from sklearn.linear_model import ARDRegression, LinearRegression
+from sklearn.datasets import make_regression
 
 # %%
 # Generating simulated data with Gaussian weights
+n_features = 100
 
-# Parameters of the example
-np.random.seed(0)
-n_samples, n_features = 100, 100
-# Create Gaussian data
-X = np.random.randn(n_samples, n_features)
-# Create weights with a precision lambda_ of 4.
-lambda_ = 4.0
-w = np.zeros(n_features)
-# Only keep 10 weights of interest
-relevant_features = np.random.randint(0, n_features, 10)
-for i in relevant_features:
-    w[i] = stats.norm.rvs(loc=0, scale=1.0 / np.sqrt(lambda_))
-# Create noise with a precision alpha of 50.
-alpha_ = 50.0
-noise = stats.norm.rvs(loc=0, scale=1.0 / np.sqrt(alpha_), size=n_samples)
-# Create the target
-y = np.dot(X, w) + noise
-
+X, y, w = make_regression(
+    n_samples=100,
+    n_features=100,
+    n_informative=10,
+    noise=1.0,
+    coef=True,
+    random_state=42,
+)
+relevant_features = np.nonzero(w)
 # %%
 # Fit the ARD Regression
 clf = ARDRegression(compute_score=True)
@@ -59,6 +52,10 @@ clf.fit(X, y)
 ols = LinearRegression()
 ols.fit(X, y)
 
+clf.coef_[relevant_features]
+
+# %%
+w[relevant_features]
 # %%
 # Plot the true weights, the estimated weights, the histogram of the
 # weights, and predictions with standard deviations
