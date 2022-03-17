831b3e778b314e6f555beb2a99417b7efdcb3ec3
Julien Jerphanion <git@jjerphan.xyz>
2022-03-17 09:52:42 +0100

    C'mon, Julien

--- sklearn/utils/_testing.py
+++ sklearn/utils/_testing.py
@@ -433,6 +433,7 @@ def assert_allclose(
     Examples
     --------
     >>> import numpy as np
+    >>> from sklearn.utils._testing import assert_allclose
     >>> x = [1e-5, 1e-3, 1e-1]
     >>> y = np.arccos(np.cos(x))
     >>> assert_allclose(x, y, rtol=1e-5, atol=0)
