25c307b86bbb6cffa1f89f9172d2177a56984eed
Julien Jerphanion <git@jjerphan.xyz>
2022-03-17 14:25:17 +0100

    TST Add minimal setup to be able to run test suite on float32 (#22690)
    Co-authored-by: Thomas J. Fan <thomasjpfan@gmail.com>
    Co-authored-by: Olivier Grisel <olivier.grisel@ensta.org>
    Co-authored-by: Jérémie du Boisberranger <jeremiedbb@users.noreply.github.com>

--- sklearn/conftest.py
+++ sklearn/conftest.py
@@ -4,6 +4,7 @@ import platform
 import sys
 
 import pytest
+import numpy as np
 from threadpoolctl import threadpool_limits
 from _pytest.doctest import DoctestItem
 
@@ -40,6 +41,17 @@ dataset_fetchers = {
     "fetch_olivetti_faces_fxt": fetch_olivetti_faces,
     "fetch_rcv1_fxt": fetch_rcv1,
 }
+
+_SKIP32_MARK = pytest.mark.skipif(
+    environ.get("SKLEARN_RUN_FLOAT32_TESTS", "0") != "1",
+    reason="Set SKLEARN_RUN_FLOAT32_TESTS=1 to run float32 dtype tests",
+)
+
+
+# Global fixtures
+@pytest.fixture(params=[pytest.param(np.float32, marks=_SKIP32_MARK), np.float64])
+def global_dtype(request):
+    yield request.param
 
 
 def _fetch_fixture(f):
