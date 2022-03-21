736c43653e14eb7f21b287f7653a31adbb68673f
Adam Li <adam2392@gmail.com>
2022-03-21 11:15:01 -0400

    Add asv benchmark

--- asv_benchmarks/benchmarks/ensemble.py
+++ asv_benchmarks/benchmarks/ensemble.py
@@ -2,6 +2,7 @@ from sklearn.ensemble import (
     RandomForestClassifier,
     GradientBoostingClassifier,
     HistGradientBoostingClassifier,
+    ObliqueRandomForestClassifier,
 )
 
 from .common import Benchmark, Estimator, Predictor
@@ -11,6 +12,43 @@ from .datasets import (
     _synth_classification_dataset,
 )
 from .utils import make_gen_classif_scorers
+
+
+class ObliqueRandomForestClassifierBenchmark(Predictor, Estimator, Benchmark):
+    """
+    Benchmarks for RandomForestClassifier.
+    """
+
+    param_names = ["representation", "n_jobs"]
+    params = (["dense"], Benchmark.n_jobs_vals)
+
+    def setup_cache(self):
+        super().setup_cache()
+
+    def make_data(self, params):
+        representation, n_jobs = params
+
+        data = _20newsgroups_lowdim_dataset()
+
+        return data
+
+    def make_estimator(self, params):
+        representation, n_jobs = params
+
+        n_estimators = 500 if Benchmark.data_size == "large" else 100
+
+        estimator = ObliqueRandomForestClassifier(
+            n_estimators=n_estimators,
+            min_samples_split=10,
+            max_features="log2",
+            n_jobs=n_jobs,
+            random_state=0,
+        )
+
+        return estimator
+
+    def make_scorers(self):
+        make_gen_classif_scorers(self)
 
 
 class RandomForestClassifierBenchmark(Predictor, Estimator, Benchmark):
