333b3fe65ef91f3503892b689fce5544ceea8846
Kevin Doucet <89160717+kungfudeuce@users.noreply.github.com>
2022-03-21 15:24:36 -0500

    DOC Ensure that ledoit_wolf is passing numpydoc validation (#22496)
    Co-authored-by: Jérémie du Boisberranger <34657725+jeremiedbb@users.noreply.github.com>

--- sklearn/tests/test_docstrings.py
+++ sklearn/tests/test_docstrings.py
@@ -12,7 +12,6 @@ import sklearn
 numpydoc_validation = pytest.importorskip("numpydoc.validate")
 
 FUNCTION_DOCSTRING_IGNORE_LIST = [
-    "sklearn.covariance._shrunk_covariance.ledoit_wolf",
     "sklearn.datasets._covtype.fetch_covtype",
     "sklearn.datasets._kddcup99.fetch_kddcup99",
     "sklearn.datasets._lfw.fetch_lfw_pairs",
