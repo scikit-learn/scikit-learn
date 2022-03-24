32aa43f6cbfe94de3dd6559601c6e25620ba3d44
Julien Jerphanion <git@jjerphan.xyz>
2022-03-24 11:39:29 +0100

    TST Use global_dtype

--- sklearn/manifold/tests/test_isomap.py
+++ sklearn/manifold/tests/test_isomap.py
@@ -1,11 +1,6 @@
 from itertools import product
 import numpy as np
 import math
-from numpy.testing import (
-    assert_almost_equal,
-    assert_array_almost_equal,
-    assert_array_equal,
-)
 import pytest
 
 from sklearn import datasets
@@ -15,35 +10,41 @@ from sklearn import pipeline
 from sklearn import preprocessing
 from sklearn.datasets import make_blobs
 from sklearn.metrics.pairwise import pairwise_distances
-from sklearn.utils._testing import assert_allclose, assert_allclose_dense_sparse
-
+from sklearn.utils._testing import (
+    assert_almost_equal,
+    assert_allclose,
+    assert_allclose_dense_sparse,
+    assert_array_equal,
+)
 from scipy.sparse import rand as sparse_rand
 
 eigen_solvers = ["auto", "dense", "arpack"]
 path_methods = ["auto", "FW", "D"]
 
-DTYPES = (np.float64, np.float32)
+# TODO: Isomap fit/fit_transform must preserve dtype
 
 
-@pytest.mark.parametrize("dtype", DTYPES)
 def create_sample_data(dtype, n_pts=25, add_noise=False):
     # grid of equidistant points in 2D, n_components = n_dim
     n_per_side = int(math.sqrt(n_pts))
-    X = np.array(list(product(range(n_per_side), repeat=2))).astype(dtype)
+    X = np.array(list(product(range(n_per_side), repeat=2))).astype(dtype, copy=False)
     if add_noise:
         # add noise in a third dimension
         rng = np.random.RandomState(0)
-        noise = 0.1 * rng.randn(n_pts, 1).astype(dtype)
+        noise = 0.1 * rng.randn(n_pts, 1).astype(dtype, copy=False)
         X = np.concatenate((X, noise), 1)
     return X
 
 
 @pytest.mark.parametrize("n_neighbors, radius", [(24, None), (None, np.inf)])
-@pytest.mark.parametrize("dtype", DTYPES)
-def test_isomap_simple_grid(dtype, n_neighbors, radius):
+@pytest.mark.parametrize("eigen_solver", eigen_solvers)
+@pytest.mark.parametrize("path_method", path_methods)
+def test_isomap_simple_grid(
+    global_dtype, n_neighbors, radius, eigen_solver, path_method
+):
     # Isomap should preserve distances when all neighbors are used
     n_pts = 25
-    X = create_sample_data(n_pts=n_pts, add_noise=False, dtype=dtype)
+    X = create_sample_data(global_dtype, n_pts=n_pts, add_noise=False)
 
     # distances from each point to all others
     if n_neighbors is not None:
@@ -51,34 +52,33 @@ def test_isomap_simple_grid(dtype, n_neighbors, radius):
     else:
         G = neighbors.radius_neighbors_graph(X, radius, mode="distance")
 
-    for eigen_solver in eigen_solvers:
-        for path_method in path_methods:
-            clf = manifold.Isomap(
-                n_neighbors=n_neighbors,
-                radius=radius,
-                n_components=2,
-                eigen_solver=eigen_solver,
-                path_method=path_method,
-            )
-            clf.fit(X)
+    clf = manifold.Isomap(
+        n_neighbors=n_neighbors,
+        radius=radius,
+        n_components=2,
+        eigen_solver=eigen_solver,
+        path_method=path_method,
+    )
+    clf.fit(X)
 
-            if n_neighbors is not None:
-                G_iso = neighbors.kneighbors_graph(
-                    clf.embedding_, n_neighbors, mode="distance"
-                )
-            else:
-                G_iso = neighbors.radius_neighbors_graph(
-                    clf.embedding_, radius, mode="distance"
-                )
-            assert_allclose_dense_sparse(G, G_iso)
+    if n_neighbors is not None:
+        G_iso = neighbors.kneighbors_graph(clf.embedding_, n_neighbors, mode="distance")
+    else:
+        G_iso = neighbors.radius_neighbors_graph(
+            clf.embedding_, radius, mode="distance"
+        )
+    assert_allclose_dense_sparse(G, G_iso)
 
 
 @pytest.mark.parametrize("n_neighbors, radius", [(24, None), (None, np.inf)])
-@pytest.mark.parametrize("dtype", DTYPES)
-def test_isomap_reconstruction_error(dtype, n_neighbors, radius):
+@pytest.mark.parametrize("eigen_solver", eigen_solvers)
+@pytest.mark.parametrize("path_method", path_methods)
+def test_isomap_reconstruction_error(
+    global_dtype, n_neighbors, radius, eigen_solver, path_method
+):
     # Same setup as in test_isomap_simple_grid, with an added dimension
     n_pts = 25
-    X = create_sample_data(n_pts=n_pts, add_noise=True, dtype=dtype)
+    X = create_sample_data(global_dtype, n_pts=n_pts, add_noise=True)
 
     # compute input kernel
     if n_neighbors is not None:
@@ -88,37 +88,32 @@ def test_isomap_reconstruction_error(dtype, n_neighbors, radius):
     centerer = preprocessing.KernelCenterer()
     K = centerer.fit_transform(-0.5 * G**2)
 
-    for eigen_solver in eigen_solvers:
-        for path_method in path_methods:
-            clf = manifold.Isomap(
-                n_neighbors=n_neighbors,
-                radius=radius,
-                n_components=2,
-                eigen_solver=eigen_solver,
-                path_method=path_method,
-            )
-            clf.fit(X)
+    clf = manifold.Isomap(
+        n_neighbors=n_neighbors,
+        radius=radius,
+        n_components=2,
+        eigen_solver=eigen_solver,
+        path_method=path_method,
+    )
+    clf.fit(X)
 
-            # compute output kernel
-            if n_neighbors is not None:
-                G_iso = neighbors.kneighbors_graph(
-                    clf.embedding_, n_neighbors, mode="distance"
-                )
-            else:
-                G_iso = neighbors.radius_neighbors_graph(
-                    clf.embedding_, radius, mode="distance"
-                )
-            G_iso = G_iso.toarray()
-            K_iso = centerer.fit_transform(-0.5 * G_iso**2)
+    # compute output kernel
+    if n_neighbors is not None:
+        G_iso = neighbors.kneighbors_graph(clf.embedding_, n_neighbors, mode="distance")
+    else:
+        G_iso = neighbors.radius_neighbors_graph(
+            clf.embedding_, radius, mode="distance"
+        )
+    G_iso = G_iso.toarray()
+    K_iso = centerer.fit_transform(-0.5 * G_iso**2)
 
-            # make sure error agrees
-            reconstruction_error = np.linalg.norm(K - K_iso) / n_pts
-            assert_almost_equal(reconstruction_error, clf.reconstruction_error())
+    # make sure error agrees
+    reconstruction_error = np.linalg.norm(K - K_iso) / n_pts
+    assert_almost_equal(reconstruction_error, clf.reconstruction_error())
 
 
 @pytest.mark.parametrize("n_neighbors, radius", [(2, None), (None, 0.5)])
-@pytest.mark.parametrize("dtype", DTYPES)
-def test_transform(dtype, n_neighbors, radius):
+def test_transform(global_dtype, n_neighbors, radius):
     n_samples = 200
     n_components = 10
     noise_scale = 0.01
@@ -126,7 +121,7 @@ def test_transform(dtype, n_neighbors, radius):
     # Create S-curve dataset
     X, y = datasets.make_s_curve(n_samples, random_state=0)
 
-    X = X.astype(dtype)
+    X = X.astype(global_dtype, copy=False)
 
     # Compute isomap embedding
     iso = manifold.Isomap(
@@ -144,13 +139,12 @@ def test_transform(dtype, n_neighbors, radius):
 
 
 @pytest.mark.parametrize("n_neighbors, radius", [(2, None), (None, 10.0)])
-@pytest.mark.parametrize("dtype", DTYPES)
-def test_pipeline(n_neighbors, radius, dtype):
+def test_pipeline(n_neighbors, radius, global_dtype):
     # check that Isomap works fine as a transformer in a Pipeline
     # only checks that no error is raised.
     # TODO check that it actually does something useful
     X, y = datasets.make_blobs(random_state=0)
-    X = X.astype(dtype)
+    X = X.astype(global_dtype, copy=False)
     clf = pipeline.Pipeline(
         [
             ("isomap", manifold.Isomap(n_neighbors=n_neighbors, radius=radius)),
@@ -161,8 +155,7 @@ def test_pipeline(n_neighbors, radius, dtype):
     assert 0.9 < clf.score(X, y)
 
 
-@pytest.mark.parametrize("dtype", DTYPES)
-def test_pipeline_with_nearest_neighbors_transformer(dtype):
+def test_pipeline_with_nearest_neighbors_transformer(global_dtype):
     # Test chaining NearestNeighborsTransformer and Isomap with
     # neighbors_algorithm='precomputed'
     algorithm = "auto"
@@ -171,8 +164,8 @@ def test_pipeline_with_nearest_neighbors_transformer(dtype):
     X, _ = datasets.make_blobs(random_state=0)
     X2, _ = datasets.make_blobs(random_state=1)
 
-    X = X.astype(dtype)
-    X2 = X2.astype(dtype)
+    X = X.astype(global_dtype, copy=False)
+    X2 = X2.astype(global_dtype, copy=False)
 
     # compare the chained version and the compact version
     est_chain = pipeline.make_pipeline(
@@ -187,75 +180,69 @@ def test_pipeline_with_nearest_neighbors_transformer(dtype):
 
     Xt_chain = est_chain.fit_transform(X)
     Xt_compact = est_compact.fit_transform(X)
-    assert_array_almost_equal(Xt_chain, Xt_compact)
+    assert_allclose(Xt_chain, Xt_compact)
 
     Xt_chain = est_chain.transform(X2)
     Xt_compact = est_compact.transform(X2)
-    assert_array_almost_equal(Xt_chain, Xt_compact)
+    assert_allclose(Xt_chain, Xt_compact)
 
 
-@pytest.mark.parametrize("dtype", DTYPES)
-def test_different_metric(dtype):
-    # Test that the metric parameters work correctly, and default to euclidean
-    def custom_metric(x1, x2):
-        return np.sqrt(np.sum(x1**2 + x2**2))
-
-    # metric, p, is_euclidean
-    metrics = [
+@pytest.mark.parametrize(
+    "metric, p, is_euclidean",
+    [
         ("euclidean", 2, True),
         ("manhattan", 1, False),
         ("minkowski", 1, False),
         ("minkowski", 2, True),
-        (custom_metric, 2, False),
-    ]
-
+        (lambda x1, x2: np.sqrt(np.sum(x1**2 + x2**2)), 2, False),
+    ],
+)
+def test_different_metric(global_dtype, metric, p, is_euclidean):
+    # Isomap must work on various metric parameters work correctly
+    # and must default to euclidean.
     X, _ = datasets.make_blobs(random_state=0)
-    X = X.astype(dtype)
+    X = X.astype(global_dtype, copy=False)
+
     reference = manifold.Isomap().fit_transform(X)
+    embedding = manifold.Isomap(metric=metric, p=p).fit_transform(X)
 
-    for metric, p, is_euclidean in metrics:
-        embedding = manifold.Isomap(metric=metric, p=p).fit_transform(X)
-
-        if is_euclidean:
-            assert_array_almost_equal(embedding, reference)
-        else:
-            with pytest.raises(AssertionError, match="not almost equal"):
-                assert_array_almost_equal(embedding, reference)
+    if is_euclidean:
+        assert_allclose(embedding, reference)
+    else:
+        with pytest.raises(AssertionError, match="Not equal to tolerance rtol=1e-07"):
+            assert_allclose(embedding, reference)
 
 
-@pytest.mark.parametrize("dtype", DTYPES)
-def test_isomap_clone_bug(dtype):
+def test_isomap_clone_bug():
     # regression test for bug reported in #6062
     model = manifold.Isomap()
     for n_neighbors in [10, 15, 20]:
         model.set_params(n_neighbors=n_neighbors)
-        model.fit(np.random.rand(50, 2).astype(dtype))
+        model.fit(np.random.rand(50, 2))
         assert model.nbrs_.n_neighbors == n_neighbors
 
 
-@pytest.mark.parametrize("dtype", DTYPES)
-def test_sparse_input(dtype):
-    X = sparse_rand(100, 3, density=0.1, format="csr", dtype=dtype)
+@pytest.mark.parametrize("eigen_solver", eigen_solvers)
+@pytest.mark.parametrize("path_method", path_methods)
+def test_sparse_input(global_dtype, eigen_solver, path_method):
+    X = sparse_rand(100, 3, density=0.1, format="csr", dtype=global_dtype)
 
     # Should not error
-    for eigen_solver in eigen_solvers:
-        for path_method in path_methods:
-            clf = manifold.Isomap(
-                n_components=2,
-                eigen_solver=eigen_solver,
-                path_method=path_method,
-                n_neighbors=8,
-            )
-            clf.fit(X)
+    clf = manifold.Isomap(
+        n_components=2,
+        eigen_solver=eigen_solver,
+        path_method=path_method,
+        n_neighbors=8,
+    )
+    clf.fit(X)
 
 
-@pytest.mark.parametrize("dtype", DTYPES)
-def test_isomap_fit_precomputed_radius_graph(dtype):
+def test_isomap_fit_precomputed_radius_graph(global_dtype):
     # Isomap.fit_transform must yield similar result when using
     # a precomputed distance matrix.
 
     X, y = datasets.make_s_curve(200, random_state=0)
-    X = X.astype(dtype)
+    X = X.astype(global_dtype, copy=False)
     radius = 10
 
     g = neighbors.radius_neighbors_graph(X, radius=radius, mode="distance")
@@ -268,33 +255,28 @@ def test_isomap_fit_precomputed_radius_graph(dtype):
     assert_allclose(precomputed_result, result)
 
 
-@pytest.mark.parametrize("dtype", DTYPES)
-def test_isomap_raise_error_when_neighbor_and_radius_both_set(dtype):
+def test_isomap_raise_error_when_neighbor_and_radius_both_set():
     # Isomap.fit_transform must raise a ValueError if
     # radius and n_neighbors are provided.
 
     X, _ = datasets.load_digits(return_X_y=True)
-    X = X.astype(dtype)
     isomap = manifold.Isomap(n_neighbors=3, radius=5.5)
     msg = "Both n_neighbors and radius are provided"
     with pytest.raises(ValueError, match=msg):
         isomap.fit_transform(X)
 
 
-@pytest.mark.parametrize("dtype", DTYPES)
-def test_multiple_connected_components(dtype):
+def test_multiple_connected_components():
     # Test that a warning is raised when the graph has multiple components
     X = np.array([0, 1, 2, 5, 6, 7])[:, None]
-    X = X.astype(dtype)
     with pytest.warns(UserWarning, match="number of connected components"):
         manifold.Isomap(n_neighbors=2).fit(X)
 
 
-@pytest.mark.parametrize("dtype", DTYPES)
-def test_multiple_connected_components_metric_precomputed(dtype):
+def test_multiple_connected_components_metric_precomputed(global_dtype):
     # Test that an error is raised when the graph has multiple components
     # and when X is a precomputed neighbors graph.
-    X = np.array([0, 1, 2, 5, 6, 7])[:, None].astype(dtype)
+    X = np.array([0, 1, 2, 5, 6, 7])[:, None].astype(global_dtype, copy=False)
 
     # works with a precomputed distance matrix (dense)
     X_distances = pairwise_distances(X)
@@ -307,11 +289,9 @@ def test_multiple_connected_components_metric_precomputed(dtype):
         manifold.Isomap(n_neighbors=1, metric="precomputed").fit(X_graph)
 
 
-@pytest.mark.parametrize("dtype", DTYPES)
-def test_get_feature_names_out(dtype):
+def test_get_feature_names_out():
     """Check get_feature_names_out for Isomap."""
     X, y = make_blobs(random_state=0, n_features=4)
-    X = X.astype(dtype)
     n_components = 2
 
     iso = manifold.Isomap(n_components=n_components)
