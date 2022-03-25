d7734d45d0b58d28558f59af726711c5a12e49b6
Micky774 <zainmeekail@gmail.com>
2022-03-25 18:29:23 -0400

    Fixed sphinx lists

--- sklearn/cluster/_hdbscan/hdbscan_.py
+++ sklearn/cluster/_hdbscan/hdbscan_.py
@@ -929,12 +929,12 @@ class HDBSCAN(BaseEstimator, ClusterMixin):
         The metric to use when calculating distance between instances in a
         feature array.
 
-        If metric is a string or callable, it must be one of
-        the options allowed by `metrics.pairwise.pairwise_distances` for its
-        metric parameter.
+        - If metric is a string or callable, it must be one of
+          the options allowed by `metrics.pairwise.pairwise_distances` for its
+          metric parameter.
 
-        If metric is "precomputed", X is assumed to be a distance matrix and
-        must be square.
+        - If metric is "precomputed", X is assumed to be a distance matrix and
+          must be square.
 
     alpha : float, default=1.0
         A distance scaling parameter as used in robust single linkage.
@@ -946,12 +946,12 @@ class HDBSCAN(BaseEstimator, ClusterMixin):
         to `best` which chooses the "best" algorithm given the nature of
         the data. You can force other options if you believe you know
         better. Options are:
-            * `best`
-            * `generic`
-            * `prims_kdtree`
-            * `prims_balltree`
-            * `boruvka_kdtree`
-            * `boruvka_balltree`
+          - `best`
+          - `generic`
+          - `prims_kdtree`
+          - `prims_balltree`
+          - `boruvka_kdtree`
+          - `boruvka_balltree`
 
     leaf_size : int, default=40
         If using a space tree algorithm (kdtree, or balltree) the number
@@ -986,8 +986,8 @@ class HDBSCAN(BaseEstimator, ClusterMixin):
         to find the most persistent clusters. Alternatively you can instead
         select the clusters at the leaves of the tree -- this provides the
         most fine grained and homogeneous clusters. Options are:
-            * `eom`
-            * `leaf`
+          - `eom`
+          - `leaf`
 
     allow_single_cluster : bool, default=False
         By default HDBSCAN* will not produce a single cluster, setting this
