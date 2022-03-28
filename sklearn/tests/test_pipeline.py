274f675bcb1bea3e11fb76ac5b68bd7f2d2fb356
Random Geek <randomgeek78@gmail.com>
2022-03-28 12:10:01 -0700

    Addressed additional feedback

--- sklearn/tests/test_pipeline.py
+++ sklearn/tests/test_pipeline.py
@@ -1527,6 +1527,7 @@ def test_pipeline_check_if_fitted():
 
 
 def test_feature_union_check_if_fitted():
+    """Check __sklearn_is_fitted__ is defined correctly."""
 
     X = [[1, 2], [3, 4], [5, 6]]
     y = [0, 1, 2]
