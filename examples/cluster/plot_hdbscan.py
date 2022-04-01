e7291a8384d6481b9cda06a8bd31eb9424ae7ce0
Micky774 <zainmeekail@gmail.com>
2022-04-01 17:53:51 -0400

    Improved plotting w/ perturbation examples

--- examples/cluster/plot_hdbscan.py
+++ examples/cluster/plot_hdbscan.py
@@ -27,46 +27,58 @@ X = StandardScaler().fit_transform(X)
 # %%
 # Compute HDBSCAN
 # ---------------
-hdb = HDBSCAN().fit(X)
-labels = hdb.labels_
+KWARGS = ({}, {"min_samples": 2}, {"min_cluster_size": 25})
+models = []
 
-# Number of clusters in labels, ignoring noise if present.
-n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-n_noise_ = list(labels).count(-1)
+for kwargs in KWARGS:
+    hdb = HDBSCAN(**kwargs).fit(X)
+    models.append((hdb.labels_, hdb.probabilities_, kwargs))
+    labels = hdb.labels_
 
-print("Estimated number of clusters: %d" % n_clusters_)
-print("Estimated number of noise points: %d" % n_noise_)
-print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
-print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
-print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
-print("Adjusted Rand Index: %0.3f" % metrics.adjusted_rand_score(labels_true, labels))
-print(
-    "Adjusted Mutual Information: %0.3f"
-    % metrics.adjusted_mutual_info_score(labels_true, labels)
-)
-print("Silhouette Coefficient: %0.3f" % metrics.silhouette_score(X, labels))
+    # Number of clusters in labels, ignoring noise
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+    n_noise_ = list(labels).count(-1)
 
+    print(f"\nFor {kwargs=}")
+    print("Estimated number of clusters: %d" % n_clusters_)
+    print("Estimated number of noise points: %d" % n_noise_)
+    print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
+    print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
+    print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
+    print(
+        "Adjusted Rand Index: %0.3f" % metrics.adjusted_rand_score(labels_true, labels)
+    )
+    print(
+        "Adjusted Mutual Information: %0.3f"
+        % metrics.adjusted_mutual_info_score(labels_true, labels)
+    )
+    print("Silhouette Coefficient: %0.3f" % metrics.silhouette_score(X, labels))
 # %%
 # Plot result
 # -----------
 import matplotlib.pyplot as plt
 
-# Black removed and is used for noise instead.
-unique_labels = set(labels)
-colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
-for k, col in zip(unique_labels, colors):
-    if k == -1:
-        # Black used for noise.
-        col = [0, 0, 0, 1]
+for labels, probabilities, kwargs in models:
+    # Black removed and is used for noise instead.
+    unique_labels = set(labels)
+    colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
+    # The probability of a point belonging to its labeled cluster determines
+    # the size of its marker
+    proba_map = {idx: probabilities[idx] for idx in range(len(labels))}
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            # Black used for noise.
+            col = [0, 0, 0, 1]
 
-    xy = X[labels == k]
-    plt.plot(
-        xy[:, 0],
-        xy[:, 1],
-        "o",
-        markerfacecolor=tuple(col),
-        markeredgecolor="k",
-        markersize=8,
-    )
-plt.title("Estimated number of clusters: %d" % n_clusters_)
-plt.show()
+        class_index = np.where(labels == k)[0]
+        for ci in class_index:
+            plt.plot(
+                X[ci, 0],
+                X[ci, 1],
+                "x" if k == -1 else "o",
+                markerfacecolor=tuple(col),
+                markeredgecolor="k",
+                markersize=4 if k == -1 else 1 + 5 * proba_map[ci],
+            )
+    plt.title(f"Estimated number of clusters: {n_clusters_} | {kwargs=}")
+    plt.show()
