50101f74e8d153ecdb5f4a7ba9b530278b34c2aa
adamgonzo <86032875+adamgonzo@users.noreply.github.com>
2022-04-01 17:34:30 -0700

    Update sklearn/utils/tests/test_validation.py
    Co-authored-by: Thomas J. Fan <thomasjpfan@gmail.com>

--- sklearn/utils/tests/test_validation.py
+++ sklearn/utils/tests/test_validation.py
@@ -1617,7 +1617,7 @@ def test_get_feature_names_pandas_with_ints_no_warning(names):
     X = pd.DataFrame([[1, 2], [4, 5], [5, 6]], columns=names)
 
     with warnings.catch_warnings():
-        warnings.simplefilter("error", UserWarning)
+        warnings.simplefilter("error", FutureWarning)
         names = _get_feature_names(X)
     assert names is None
 
