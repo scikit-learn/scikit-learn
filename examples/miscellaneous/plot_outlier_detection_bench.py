1d00170e44d46cebc470a320b8cf85f007a44d94
Mai <54995188+MaiRajborirug@users.noreply.github.com>
2022-04-07 11:02:28 +0700

    Apply RocCurveDisplay.from_estimator

--- examples/miscellaneous/plot_outlier_detection_bench.py
+++ examples/miscellaneous/plot_outlier_detection_bench.py
@@ -13,26 +13,29 @@ contain outliers.
 
 2. The ROC curve is computed on the same dataset using the knowledge
 of the labels.
-
-There is no particular reason to choose algorithms LOF and IForest. The goal
-is to show that different algorithm performs well on different datasets.
 """
 
 # Author: Pharuj Rajborirug <pharuj.ra@kmitl.ac.th>
 # License: BSD 3 clause
 
-import numpy as np
-
 print(__doc__)
-
-rng = np.random.RandomState(42)
 
 # %%
 # Generate datasets
 # -----------------
 #
-# The example uses real world datasets available in `scikit-learn.datasets`.
-datasets = [
+# The example uses real-world datasets available in `scikit-learn.datasets`
+# and the sample size of some datasets to speed up computation.
+# After the data preprocessing, the datasets' targets will have two classes,
+# 0 representing inliers and 1 representing outliers.
+
+import numpy as np
+from sklearn.datasets import fetch_kddcup99, fetch_covtype, fetch_openml
+from sklearn.preprocessing import LabelBinarizer
+import pandas as pd
+
+rng = np.random.RandomState(42)
+datasets_name = [
     "http",
     "smtp",
     "SA",
@@ -42,37 +45,10 @@ datasets = [
     "wdbc",
     "cardiotocography",
 ]
+Xs = []
+ys = []
 
-# %%
-# Generate outlier detection models
-# ---------------------------------
-
-from sklearn.neighbors import LocalOutlierFactor
-from sklearn.ensemble import IsolationForest
-
-models = [
-    ("LOF", LocalOutlierFactor(n_neighbors=20, contamination="auto")),
-    ("IForest", IsolationForest(random_state=rng, contamination="auto")),
-]
-
-# %%
-# Preprocess data and compute ROC curves
-# --------------------------------------
-#
-# During the data preprocessing, we reduce the sample size of some datasets to
-# speed up computation.
-
-import math
-import matplotlib.pyplot as plt
-from sklearn.datasets import fetch_kddcup99, fetch_covtype, fetch_openml
-from sklearn.preprocessing import LabelBinarizer
-from time import time
-from sklearn.metrics import roc_curve, auc
-
-rows = math.ceil(len(datasets) / 2)
-plt.figure(figsize=(10, rows * 3))
-for dataset_idx, dataset_name in enumerate(datasets):
-    plt.subplot(rows, 2, dataset_idx + 1)
+for dataset_name in datasets_name:
 
     # loading and vectorization
     print(f"Loading {dataset_name} data")
@@ -81,7 +57,6 @@ for dataset_idx, dataset_name in enumerate(datasets):
         X = dataset.data
         y = dataset.target
 
-    print("vectorizing data")
     if dataset_name == "SF":
         idx = rng.choice(X.shape[0], int(X.shape[0] * 0.1), replace=False)
         X = X[idx]  # reduce the sample size
@@ -113,8 +88,8 @@ for dataset_idx, dataset_name in enumerate(datasets):
         X = X[idx]  # reduce the sample size
         y = y[idx]
 
-        # normal data are those with attribute 2
-        # abnormal those with attribute 4
+        # inliers are those with attribute 2
+        # outliers are those with attribute 4
         s = (y == 2) + (y == 4)
         X = X[s, :]
         y = y[s]
@@ -146,41 +121,77 @@ for dataset_idx, dataset_name in enumerate(datasets):
             s = y == "3"
             y = s.astype(int)
 
-    print("Estimator processing...")
-    for model_name, model in models:
-        tstart = time()
-        model.fit(X)
-        fit_time = time() - tstart
-
-        # use oppositve of default scoring so that the lower score, the more
-        # normal
-        if model_name == "LOF":
-            scoring = -model.negative_outlier_factor_
-
-        if model_name == "IForest":
-            scoring = -model.fit(X).decision_function(X)
-
-        fpr, tpr, thresholds = roc_curve(y, scoring)
-        area = auc(fpr, tpr)
-        label_ = f"{model_name} (AUC = {area:0.3f}, train time: {fit_time:0.2f})"
-        plt.plot(fpr, tpr, lw=1, label=label_)
-    plt.plot([0, 1], [0, 1], lw=1, linestyle=":")
-
-    plt.xlim([-0.05, 1.05])
-    plt.ylim([-0.05, 1.05])
-    plt.legend(loc="lower right")
-    plt.title(dataset_name)
-    plt.xlabel("False Positive Rate")
-    plt.ylabel("True Positive Rate")
-plt.tight_layout(pad=2.0)  # spacing between subplots
-plt.show()
+    # 0 represents inliers, and 1 represents outliers
+    y = pd.Series(y, dtype="category")
+    Xs.append(X)
+    ys.append(y)
 
 # %%
-# Interpret the ROC plot
-# ----------------------
+# Define outlier detection models
+# -------------------------------
+# There is no particular reason to choose algorithms LOF and IForest. The goal
+# is to show that different algorithm performs well on different datasets.
+
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.ensemble import IsolationForest
+
+models = [
+    ("LOF", LocalOutlierFactor(n_neighbors=20, contamination="auto")),
+    ("IForest", IsolationForest(random_state=rng, contamination="auto")),
+]
+
+# %%
+# Compute ROC curves
+# ------------------
+ys_pred = []
+pos_label = 0  # mean 0 belongs to positive class
+
+print("Estimators processing...")
+for i, dataset_name in enumerate(datasets_name):
+
+    ys_pred_ = []
+    for (model_name, clf) in models:
+        clf.fit(Xs[i])
+        if model_name == "LOF":
+            y_pred = clf.negative_outlier_factor_
+
+        if model_name == "IForest":
+            y_pred = clf.fit(Xs[i]).decision_function(Xs[i])
+
+        ys_pred_.append(y_pred)
+    ys_pred.append(ys_pred_)
+
+# %%
+# Plot and interpret results
+# --------------------------
 #
 # The algorithm performance relates to how good the true positive rate (TPR)
 # is at low value of the false positive rate (FPR). The best algorithms
 # have the curve on the top-left of the plot and the area under curve (AUC)
 # close to 1. The diagonal dashed line represents a random classification
 # of outliers and inliers.
+
+import math
+import matplotlib.pyplot as plt
+from sklearn.metrics import RocCurveDisplay
+
+cols = 2
+lw = 1
+pos_label = 0
+rows = math.ceil(len(datasets_name) / cols)
+
+fig, axs = plt.subplots(rows, cols, figsize=(10, rows * 3))
+
+for i, dataset_name in enumerate(datasets_name):
+    for j, (model_name, _) in enumerate(models):
+        display = RocCurveDisplay.from_predictions(ys[i], ys_pred[i][j],
+                                                   pos_label=pos_label,
+                                                   name=model_name, lw=lw,
+                                                   ax=axs[i // cols, i % cols])
+
+    axs[i // cols, i % cols].plot([0, 1], [0, 1], lw=lw, linestyle=":")
+    axs[i // cols, i % cols].set_title(dataset_name)
+    axs[i // cols, i % cols].set_xlabel("False Positive Rate")
+    axs[i // cols, i % cols].set_ylabel("True Positive Rate")
+plt.tight_layout(pad=2.0)  # spacing between subplots
+plt.show()
