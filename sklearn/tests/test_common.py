67bf7f3f83ed4de26c83e33a5efa4d7273772cda
Nikhil Suresh <nikhilsuresh097@gmail.com>
2022-04-07 19:27:18 -0400

    fixed linting error

--- sklearn/tests/test_common.py
+++ sklearn/tests/test_common.py
@@ -373,7 +373,8 @@ def test_pandas_column_name_consistency(estimator):
             )
         for warning in record:
             assert "was fitted without feature names" not in str(warning.message)
-            
+
+
 # TODO: As more modules support get_feature_names_out they should be removed
 # from this list to be tested
 GET_FEATURES_OUT_MODULES_TO_IGNORE = [
