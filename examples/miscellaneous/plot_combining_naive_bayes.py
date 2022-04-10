31b47a035171eb20c47b0fa2937478ec444e0851
avm19 <52547519avm19@users.noreply.github.com>
2022-04-10 10:04:41 -0400

    Fix formatting in the gallery example

--- examples/miscellaneous/plot_combining_naive_bayes.py
+++ examples/miscellaneous/plot_combining_naive_bayes.py
@@ -12,10 +12,8 @@ discrete and continuous features.
 
 We consider the titanic dataset, in which:
 
-    - numerical (continous) features "age" and "fare" are handled by
-:class:`~naive_bayes.GaussianNB`;
-    - categorical (discrete) features "embarked", "sex", and "pclass" are handled
-by :class:`~naive_bayes.CategoricalNB`.
+    - numerical (continous) features "age" and "fare" are handled by :class:`~naive_bayes.GaussianNB`;
+    - categorical (discrete) features "embarked", "sex", and "pclass" are handled by :class:`~naive_bayes.CategoricalNB`.
 """
 
 # Author: Andrey V. Melnik <andrey.melnik.maths@gmail.com>
