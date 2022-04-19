c7c41b365fece5200a6ae7ff3e175aec6d13abc0
Maxwell <zl2480@columbia.edu>
2022-04-19 20:25:39 +0800

    change atol to 1e-8

--- sklearn/linear_model/tests/test_ridge.py
+++ sklearn/linear_model/tests/test_ridge.py
@@ -273,7 +273,7 @@ def test_ridge_regression_vstacked_X(
     coef = coef[:-1]
 
     assert model.intercept_ == pytest.approx(intercept)
-    assert_allclose(model.coef_, coef, atol=1e-3)
+    assert_allclose(model.coef_, coef, atol=1e-8)
 
 
 @pytest.mark.parametrize("solver", SOLVERS)
