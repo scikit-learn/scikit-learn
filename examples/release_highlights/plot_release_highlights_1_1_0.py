772d1e6684fe9e74c517be257beebea12465e41f
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2022-04-26 16:45:58 +0200

    lint

--- examples/release_highlights/plot_release_highlights_1_1_0.py
+++ examples/release_highlights/plot_release_highlights_1_1_0.py
@@ -191,7 +191,7 @@ H = nmf.components_
 X_reconstructed = W @ H
 
 # relative reconstruction error
-np.sum((X - X_reconstructed) ** 2) / np.sum(X ** 2)
+np.sum((X - X_reconstructed) ** 2) / np.sum(X**2)
 
 # %%
 # BisectingKMeans
@@ -211,12 +211,12 @@ km = KMeans(n_clusters=5, random_state=0).fit(X)
 bisect_km = BisectingKMeans(n_clusters=5, random_state=0).fit(X)
 
 fig, ax = plt.subplots(1, 2, figsize=(10, 5))
-ax[0].scatter(X[:,0], X[:,1], s=10, c=km.labels_)
-ax[0].scatter(km.cluster_centers_[:,0], km.cluster_centers_[:,1], s=20, c="r")
+ax[0].scatter(X[:, 0], X[:, 1], s=10, c=km.labels_)
+ax[0].scatter(km.cluster_centers_[:, 0], km.cluster_centers_[:, 1], s=20, c="r")
 ax[0].set_title("KMeans")
 
-ax[1].scatter(X[:,0], X[:,1], s=10, c=bisect_km.labels_)
+ax[1].scatter(X[:, 0], X[:, 1], s=10, c=bisect_km.labels_)
 ax[1].scatter(
-    bisect_km.cluster_centers_[:,0], bisect_km.cluster_centers_[:,1], s=20, c="r"
+    bisect_km.cluster_centers_[:, 0], bisect_km.cluster_centers_[:, 1], s=20, c="r"
 )
 ax[1].set_title("BisectingKMeans")
