a5b7d741df5758d0cf9e8ba2256135f3f4e711fe
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2022-04-26 17:48:56 +0200

    lint

--- examples/release_highlights/plot_release_highlights_1_1_0.py
+++ examples/release_highlights/plot_release_highlights_1_1_0.py
@@ -192,8 +192,8 @@ H = nmf.components_
 X_reconstructed = W @ H
 
 print(
-      f"relative reconstruction error: ",
-      f"{np.sum((X - X_reconstructed) ** 2) / np.sum(X**2):.5f}"
+    f"relative reconstruction error: ",
+    f"{np.sum((X - X_reconstructed) ** 2) / np.sum(X**2):.5f}",
 )
 
 # %%
