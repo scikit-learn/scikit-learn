712753d90f4cd1914cf15ca7d7b8ed5b69ad7330
stehles <svenstehle@web.de>
2022-04-27 21:31:19 +0200

    update example for colorblindness and with train_test_split

--- examples/ensemble/plot_adaboost_hastie_10_2.py
+++ examples/ensemble/plot_adaboost_hastie_10_2.py
@@ -33,7 +33,7 @@ whereas real SAMME.R uses the predicted class probabilities.
 
 from sklearn import datasets
 
-X, y = datasets.make_hastie_10_2(n_samples=12000, random_state=1)
+X, y = datasets.make_hastie_10_2(n_samples=12_000, random_state=1)
 
 # %%
 # Now, we set the hyperparameters for our AdaBoost classifiers.
@@ -46,11 +46,12 @@ learning_rate = 1.0
 # We split the data into a training and a test set.
 # Then, we train our baseline classifiers, a `DecisionTreeClassifier` with `depth=9`
 # and a "stump" `DecisionTreeClassifier` with `depth=1` and compute the test error.
-
+from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 
-X_test, y_test = X[2000:], y[2000:]
-X_train, y_train = X[:2000], y[:2000]
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=2_000, shuffle=False
+)
 
 dt_stump = DecisionTreeClassifier(max_depth=1, min_samples_leaf=1)
 dt_stump.fit(X_train, y_train)
@@ -75,6 +76,8 @@ ada_discrete = AdaBoostClassifier(
     algorithm="SAMME",
 )
 ada_discrete.fit(X_train, y_train)
+
+# %%
 
 ada_real = AdaBoostClassifier(
     base_estimator=dt_stump,
@@ -115,6 +118,7 @@ for i, y_pred in enumerate(ada_real.staged_predict(X_train)):
 # and of the discrete and real AdaBoost classifiers
 
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
@@ -122,40 +126,41 @@ ax = fig.add_subplot(111)
 ax.plot([1, n_estimators], [dt_stump_err] * 2, "k-", label="Decision Stump Error")
 ax.plot([1, n_estimators], [dt_err] * 2, "k--", label="Decision Tree Error")
 
+colors = sns.color_palette("colorblind")
+
 ax.plot(
     np.arange(n_estimators) + 1,
     ada_discrete_err,
     label="Discrete AdaBoost Test Error",
-    color="red",
+    color=colors[0],
 )
 ax.plot(
     np.arange(n_estimators) + 1,
     ada_discrete_err_train,
     label="Discrete AdaBoost Train Error",
-    color="blue",
+    color=colors[1],
 )
 ax.plot(
     np.arange(n_estimators) + 1,
     ada_real_err,
     label="Real AdaBoost Test Error",
-    color="orange",
+    color=colors[2],
 )
 ax.plot(
     np.arange(n_estimators) + 1,
     ada_real_err_train,
     label="Real AdaBoost Train Error",
-    color="green",
+    color=colors[4],
 )
 
 ax.set_ylim((0.0, 0.5))
-ax.set_xlabel("n_estimators")
+ax.set_xlabel("Number of weak learners")
 ax.set_ylabel("error rate")
 
 leg = ax.legend(loc="upper right", fancybox=True)
 leg.get_frame().set_alpha(0.7)
 
 plt.show()
-
 # %%
 #
 # Concluding remarks
