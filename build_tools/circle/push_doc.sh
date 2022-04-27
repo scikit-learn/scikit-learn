6c1d5b4594369918cab35ab2b1434d0427093dd1
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2022-04-27 15:15:28 +0200

    [CI] Monkey-patch build_doc.sh to work with both documentation builders

--- build_tools/circle/push_doc.sh
+++ build_tools/circle/push_doc.sh
@@ -1,55 +1,65 @@
 #!/bin/bash
-
-# The behavior of the script is controlled by environment variable
-# defined in the docs.yml in the actions level folder of the project.
+# This script is meant to be called in the "deploy" step defined in
+# circle.yml. See https://circleci.com/docs/ for more details.
+# The behavior of the script is controlled by environment variable defined
+# in the circle.yml in the top level folder of the project.
 
 set -ex
 
-cd scikit-learn.github.io
+if [ -z $CIRCLE_PROJECT_USERNAME ];
+then USERNAME="sklearn-ci";
+else USERNAME=$CIRCLE_PROJECT_USERNAME;
+fi
 
 DOC_REPO="scikit-learn.github.io"
+GENERATED_DOC_DIR=$1
 
-if [[ -z "$GITHUB_BASE_REF" ]]
-then
-	REF="$GITHUB_REF"
-else
-	REF="$GITHUB_BASE_REF"
+if [[ -z "$GENERATED_DOC_DIR" ]]; then
+    echo "Need to pass directory of the generated doc as argument"
+    echo "Usage: $0 <generated_doc_dir>"
+    exit 1
 fi
 
-if [[ "$REF" =~ "main" ]]
+# Absolute path needed because we use cd further down in this script
+GENERATED_DOC_DIR=$(readlink -f $GENERATED_DOC_DIR)
+
+if [ "$CIRCLE_BRANCH" = "main" ]
 then
-    DIR=dev
+    dir=dev
 else
     # Strip off .X
-    DIR="${REF::-2}"
+    dir="${CIRCLE_BRANCH::-2}"
 fi
 
-MSG="Pushing the docs to $DIR/ for branch: $REF, commit $GITHUB_SHA"
+MSG="Pushing the docs to $dir/ for branch: $CIRCLE_BRANCH, commit $CIRCLE_SHA1"
+
+cd $HOME
+if [ ! -d $DOC_REPO ];
+then git clone --depth 1 --no-checkout "git@github.com:scikit-learn/"$DOC_REPO".git";
+fi
+cd $DOC_REPO
 
 # check if it's a new branch
-echo $DIR > .git/info/sparse-checkout
-if ! git show HEAD:$DIR >/dev/null
+
+echo $dir > .git/info/sparse-checkout
+if ! git show HEAD:$dir >/dev/null
 then
 	# directory does not exist. Need to make it so sparse checkout works
-	mkdir $DIR
-	touch $DIR/index.html
-	git add $DIR
+	mkdir $dir
+	touch $dir/index.html
+	git add $dir
 fi
 git checkout main
 git reset --hard origin/main
-if [ -d $DIR ]
+if [ -d $dir ]
 then
-	git rm -rf $DIR/ && rm -rf $DIR/
+	git rm -rf $dir/ && rm -rf $dir/
 fi
-
-cp -R $GITHUB_WORKSPACE/docs $DIR
-
+cp -R $GENERATED_DOC_DIR $dir
 git config user.email "olivier.grisel+sklearn-ci@gmail.com"
-git config user.name "sklearn-ci"
+git config user.name $USERNAME
 git config push.default matching
-
-git add -f $DIR/
-git commit -m "$MSG" $DIR
-# git push
-
+git add -f $dir/
+git commit -m "$MSG" $dir
+git push
 echo $MSG
