8b70d031c7aefbcb74e8096f3df70c7691d470eb
Juan Carlos Alfaro Jiménez <JuanCarlos.Alfaro@uclm.es>
2022-04-29 08:59:51 +0200

    [FIX] Fix environment variables

--- build_tools/github/trigger_hosting.sh
+++ build_tools/github/trigger_hosting.sh
@@ -4,9 +4,9 @@ set -e
 set -x
 
 curl --request POST \
-     --url https://circleci.com/api/v2/project/gh/$GITHUB_REPOSITORY/continue \
+     --url https://circleci.com/api/v2/project/gh/$GITHUB_REPOSITORY/pipeline \
      --header "Circle-Token: $CCI_TOKEN" \
      --header "content-type: application/json" \
      --header "x-attribution-actor-id: github_actions" \
      --header "x-attribution-login: github_actions" \
-     --data \{\"parameters\":\{\"GITHUB_RUN_URL\":\"$GITHUB_RUN_URL\"\}\}
+     --data \{\"parameters\":\{\"GITHUB_RUN_URL\":\"https://nightly.link/$GITHUB_REPOSITORY/actions/runs/$RUN_ID\"\}\}
