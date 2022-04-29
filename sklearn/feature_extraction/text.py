356862255a41463029bce283adf17c5732f5288e
henrymooresc <56162021+henrymooresc@users.noreply.github.com>
2022-04-29 14:28:36 -0400

    DOC Ensures that strip_tags passes numpydoc validation (#23248)

--- sklearn/feature_extraction/text.py
+++ sklearn/feature_extraction/text.py
@@ -171,7 +171,7 @@ def strip_accents_ascii(s):
 
 
 def strip_tags(s):
-    """Basic regexp based HTML / XML tag stripper function
+    """Basic regexp based HTML / XML tag stripper function.
 
     For serious HTML/XML preprocessing you should rather use an external
     library such as lxml or BeautifulSoup.
@@ -179,7 +179,12 @@ def strip_tags(s):
     Parameters
     ----------
     s : str
-        The string to strip
+        The string to strip.
+
+    Returns
+    -------
+    s : str
+        The stripped string.
     """
     return re.compile(r"<([^>]+)>", flags=re.UNICODE).sub(" ", s)
 
