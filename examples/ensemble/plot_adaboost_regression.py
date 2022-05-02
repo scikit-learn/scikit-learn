8f94fee77ddf18f67e34e70104829e124df80ad9
stehles <svenstehle@web.de>
2022-05-02 19:45:17 +0200

    change ensemble/plot_adaboost_regression to notebook-style

--- examples/ensemble/plot_adaboost_regression.py
+++ examples/ensemble/plot_adaboost_regression.py
@@ -9,26 +9,33 @@ sinusoidal dataset with a small amount of Gaussian noise.
 regressor. As the number of boosts is increased the regressor can fit more
 detail.
 
-.. [1] H. Drucker, "Improving Regressors using Boosting Techniques", 1997.
+.. [1] H Drucker, "Improving Regressors using Boosting Techniques", 1997.
 
 """
+
+# %%
+# Create dummy dataset with a sinusoidal relationship and some gaussian noise
+# ---------------------------------------------------------------------------
 
 # Author: Noel Dawe <noel.dawe@gmail.com>
 #
 # License: BSD 3 clause
 
-# importing necessary libraries
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import AdaBoostRegressor
 
-# Create the dataset
 rng = np.random.RandomState(1)
 X = np.linspace(0, 6, 100)[:, np.newaxis]
 y = np.sin(X).ravel() + np.sin(6 * X).ravel() + rng.normal(0, 0.1, X.shape[0])
 
-# Fit regression model
+# %%
+# Train and predict with AdaBoost and DecisionTree Regressors
+# -----------------------------------------------------------
+# The base learner is a DecisionTreeRegressor with `max_depth=4`.
+# AdaBoostRegressor will be built with `n_estimators=300` of those base learners.
+
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.tree import DecisionTreeRegressor
+
 regr_1 = DecisionTreeRegressor(max_depth=4)
 
 regr_2 = AdaBoostRegressor(
@@ -38,15 +45,23 @@ regr_2 = AdaBoostRegressor(
 regr_1.fit(X, y)
 regr_2.fit(X, y)
 
-# Predict
 y_1 = regr_1.predict(X)
 y_2 = regr_2.predict(X)
 
+# %%
 # Plot the results
+# ----------------
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# reshape the colors as 1D array for matplotlib
+colors = np.asarray(sns.color_palette("colorblind")).reshape(10, 1, 3)
+
 plt.figure()
-plt.scatter(X, y, c="k", label="training samples")
-plt.plot(X, y_1, c="g", label="n_estimators=1", linewidth=2)
-plt.plot(X, y_2, c="r", label="n_estimators=300", linewidth=2)
+plt.scatter(X, y, c=colors[0], label="training samples")
+plt.plot(X, y_1, c=colors[1], label="n_estimators=1", linewidth=2)
+plt.plot(X, y_2, c=colors[2], label="n_estimators=300", linewidth=2)
 plt.xlabel("data")
 plt.ylabel("target")
 plt.title("Boosted Decision Tree Regression")
