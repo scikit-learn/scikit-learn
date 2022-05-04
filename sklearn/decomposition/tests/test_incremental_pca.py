11647a157bd97a649b92f3edd14df92dc83068d1
lucy liu <jliu176@gmail.com>
2022-05-04 12:42:34 +1000

    review suggestions

--- sklearn/decomposition/tests/test_incremental_pca.py
+++ sklearn/decomposition/tests/test_incremental_pca.py
@@ -1,6 +1,7 @@
 """Tests for Incremental PCA."""
 import numpy as np
 import pytest
+import warnings
 
 from sklearn.utils._testing import assert_almost_equal
 from sklearn.utils._testing import assert_array_almost_equal
@@ -152,8 +153,12 @@ def test_incremental_pca_validation():
 def test_n_samples_equal_n_components():
     # Ensures no warning is raised when n_samples==n_components
     ipca = IncrementalPCA(n_components=5)
-    ipca.partial_fit(np.random.randn(5, 7))
-    ipca.fit(np.random.randn(5, 7))
+    with warnings.catch_warnings():
+        warnings.simplefilter("error", RuntimeWarning)
+        ipca.partial_fit(np.random.randn(5, 7))
+    with warnings.catch_warnings():
+        warnings.simplefilter("error", RuntimeWarning)
+        ipca.fit(np.random.randn(5, 7))
 
 
 def test_n_components_none():
