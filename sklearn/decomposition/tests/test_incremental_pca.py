5e9e9b6c79878f59682ec4b6b4d831030515b10d
lucy liu <jliu176@gmail.com>
2022-05-05 18:52:48 +1000

    review suggestions

--- sklearn/decomposition/tests/test_incremental_pca.py
+++ sklearn/decomposition/tests/test_incremental_pca.py
@@ -152,6 +152,7 @@ def test_incremental_pca_validation():
 
 def test_n_samples_equal_n_components():
     # Ensures no warning is raised when n_samples==n_components
+    # Non-regression test for gh-19050
     ipca = IncrementalPCA(n_components=5)
     with warnings.catch_warnings():
         warnings.simplefilter("error", RuntimeWarning)
