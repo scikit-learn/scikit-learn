4b6bbf0056a64ffa917c1a06ec068b449768bb41
Kernc <kerncece@gmail.com>
2022-05-05 20:52:37 +0200

    Update test

--- sklearn/utils/tests/test_class_weight.py
+++ sklearn/utils/tests/test_class_weight.py
@@ -287,6 +287,6 @@ def test_class_weight_does_not_contains_more_classses():
 
 def test_compute_sample_weight_sparse():
     """Check that we can compute weight for sparse `y`."""
-    y = sparse.csc_matrix(np.asarray([0, 0, 0, 1, 1, 1])).T
+    y = sparse.csc_matrix(np.asarray([0, 1, 1])).T
     sample_weight = compute_sample_weight("balanced", y)
-    assert_allclose(sample_weight, [1, 1, 1, 1, 1, 1])
+    assert_allclose(sample_weight, [1.5, .75, .75])
