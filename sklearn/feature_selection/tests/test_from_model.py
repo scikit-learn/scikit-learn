9be3c8fd98d34feac69e273ca65b8b62a1873328
LongBao <longbaomail@yahoo.com>
2022-05-07 17:23:33 +0900

    add test cases

--- sklearn/feature_selection/tests/test_from_model.py
+++ sklearn/feature_selection/tests/test_from_model.py
@@ -609,7 +609,15 @@ def test_estimator_does_not_support_feature_names():
         selector.transform(X.iloc[1:3])
 
 
-def test_partial_fit_validate_max_features():
+@pytest.mark.parametrize(
+    "error, err_msg, max_features",
+    (
+        [ValueError, "max_features == 10, must be <= 4.", 10],
+        [TypeError, "'max_features' must be either an int or a callable", "a"],
+        [ValueError, "max_features\(X\) == 5, must be <= 4.", lambda x: x.shape[1] + 1],
+    ),
+)
+def test_partial_fit_validate_max_features(error, err_msg, max_features):
     """Test that partial_fit from SelectFromModel validates `max_features`."""
     X, y = datasets.make_classification(
         n_samples=100,
@@ -617,27 +625,15 @@ def test_partial_fit_validate_max_features():
         random_state=0,
     )
 
-    # Case 1: Check if partial_fit raises ValueError when the value of
-    # `max_features` is out of the boundaries
-    err_msg = "max_features == 10, must be <= 4."
-    with pytest.raises(ValueError, match=err_msg):
-        SelectFromModel(estimator=SGDClassifier(), max_features=10).partial_fit(
-            X, y, classes=[0, 1]
-        )
-
-    # Case 2: Check if partial_fit raises TypeError when `max_features`
-    # is neither an int nor a callable
-    err_msg = "'max_features' must be either an int or a callable"
-    with pytest.raises(TypeError, match=err_msg):
-        SelectFromModel(estimator=SGDClassifier(), max_features="a").partial_fit(
-            X, y, classes=[0, 1]
-        )
+    with pytest.raises(error, match=err_msg):
+        SelectFromModel(
+            estimator=SGDClassifier(), max_features=max_features
+        ).partial_fit(X, y, classes=[0, 1])
 
 
 def test_partial_fit_validate_feature_names():
     """Test that partial_fit from SelectFromModel validates `feature_names_in_`."""
-    # Case 1: Check if `feature_names_in_` is defined only when `X` has
-    # feature names that are all strings.
+    # Case 1: Test array like data.
     X, y = datasets.make_classification(
         n_samples=100,
         n_features=4,
@@ -649,8 +645,21 @@ def test_partial_fit_validate_feature_names():
     )
     assert not hasattr(selector.estimator_, "feature_names_in_")
 
-    # Case 2: Check if `feature_names_in_` is defined only when `X`
-    # has feature names that are all strings.
+    # Case 2: Test List data
+    X = [
+        [0.87, -1.34, 0.31, 0.11],
+        [-2.79, -0.02, -0.85, 3.11],
+        [-1.34, -0.48, -2.55, 2.13],
+        [1.92, 1.48, 0.65, -1.41],
+    ]
+    y = [0, 1, 0, 1]
+
+    selector = SelectFromModel(estimator=SGDClassifier(), max_features=4).partial_fit(
+        X, y, classes=[0, 1]
+    )
+    assert not hasattr(selector.estimator_, "feature_names_in_")
+
+    # Case 3: Test Frame data
     pytest.importorskip("pandas")
     X, y = datasets.load_iris(as_frame=True, return_X_y=True)
 
