b016b734dbee2a2da8e165cda1ec18667b18385c
Loïc Estève <loic.esteve@ymail.com>
2022-05-19 15:23:57 +0200

    Put the code back as it was

--- sklearn/utils/extmath.py
+++ sklearn/utils/extmath.py
@@ -216,7 +216,6 @@ def randomized_range_finder(
 
     # Generating normal random vectors with shape: (A.shape[1], size)
     Q = random_state.normal(size=(A.shape[1], size))
-
     if hasattr(A, "dtype") and A.dtype.kind == "f":
         # Ensure f32 is preserved as f32
         Q = Q.astype(A.dtype, copy=False)
