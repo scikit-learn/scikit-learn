bf503b27bfac5b0e569e05d7fa308ebd015a8532
ArturoAmorQ <arturo.amor-quiroz@polytechnique.edu>
2022-05-24 11:56:13 +0200

    Format

--- examples/ensemble/plot_feature_transformation.py
+++ examples/ensemble/plot_feature_transformation.py
@@ -72,10 +72,10 @@ gradient_boosting = GradientBoostingClassifier(
 _ = gradient_boosting.fit(X_train_ensemble, y_train_ensemble)
 
 # %%
-# Notice that :class:`~sklearn.ensemble.HistGradientBoostingClassifier`
-# is much faster than :class:`~sklearn.ensemble.GradientBoostingClassifier`
-# starting with intermediate datasets (`n_samples >= 10_000`), which is
-# not the case of the present example.
+# Notice that :class:`~sklearn.ensemble.HistGradientBoostingClassifier` is much
+# faster than :class:`~sklearn.ensemble.GradientBoostingClassifier` starting
+# with intermediate datasets (`n_samples >= 10_000`), which is not the case of
+# the present example.
 #
 # The :class:`~sklearn.ensemble.RandomTreesEmbedding` is an unsupervised method
 # and thus does not required to be trained independently.
