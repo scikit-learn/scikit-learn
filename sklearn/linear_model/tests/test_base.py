6a6ac94aa1d35f879f3a0e82e2b1fa2d2eb73313
stehles <svenstehle@web.de>
2022-05-25 20:12:49 +0200

    add global_random_seed fixture to test_linear_regression_sample_weights, which fails on a few seeds now

--- sklearn/linear_model/tests/test_base.py
+++ sklearn/linear_model/tests/test_base.py
@@ -4,28 +4,26 @@
 #
 # License: BSD 3 clause
 
-import pytest
 import warnings
 
 import numpy as np
-from scipy import sparse
-from scipy import linalg
-
-from sklearn.utils._testing import assert_array_almost_equal
-from sklearn.utils._testing import assert_array_equal
-from sklearn.utils._testing import assert_allclose
-from sklearn.utils import check_random_state
-
+import pytest
+from scipy import linalg, sparse
+from sklearn.datasets import load_iris, make_regression, make_sparse_uncorrelated
 from sklearn.linear_model import LinearRegression
-from sklearn.linear_model._base import _deprecate_normalize
-from sklearn.linear_model._base import _preprocess_data
-from sklearn.linear_model._base import _rescale_data
-from sklearn.linear_model._base import make_dataset
-from sklearn.datasets import make_sparse_uncorrelated
-from sklearn.datasets import make_regression
-from sklearn.datasets import load_iris
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import add_dummy_feature
+from sklearn.linear_model._base import (
+    _deprecate_normalize,
+    _preprocess_data,
+    _rescale_data,
+    make_dataset,
+)
+from sklearn.preprocessing import StandardScaler, add_dummy_feature
+from sklearn.utils import check_random_state
+from sklearn.utils._testing import (
+    assert_allclose,
+    assert_array_almost_equal,
+    assert_array_equal,
+)
 
 rng = np.random.RandomState(0)
 rtol = 1e-6
@@ -57,8 +55,10 @@ def test_linear_regression():
 
 @pytest.mark.parametrize("array_constr", [np.array, sparse.csr_matrix])
 @pytest.mark.parametrize("fit_intercept", [True, False])
-def test_linear_regression_sample_weights(array_constr, fit_intercept):
-    rng = np.random.RandomState(0)
+def test_linear_regression_sample_weights(
+    array_constr, fit_intercept, global_random_seed
+):
+    rng = np.random.RandomState(global_random_seed)
 
     # It would not work with under-determined systems
     n_samples, n_features = 6, 5
