4acfe88889c3a35fac18401fcce67b45be04e75e
Thomas J. Fan <thomasjpfan@gmail.com>
2022-06-01 00:44:36 -0400

    FIX Improve sort for edge cases

--- maint_tools/get_pr_number_from_run_id.py
+++ maint_tools/get_pr_number_from_run_id.py
@@ -3,16 +3,15 @@ import argparse
 from github import Github
 
 parser = argparse.ArgumentParser()
+parser.add_argument("token")
 parser.add_argument("repo")
 parser.add_argument("run_id")
-
 args = parser.parse_args()
 
-gh = Github()
-
+gh = Github(args.token)
 sk_repo = gh.get_repo(args.repo)
 run = sk_repo.get_workflow_run(args.run_id)
 
 head = f"{run.head_repository.owner.login}:{run.head_branch}"
-prs = list(sk_repo.get_pulls(state="all", head=head))
+prs = list(sk_repo.get_pulls(state="all", sort="updated", direction="desc", head=head))
 print(prs[0].number)
