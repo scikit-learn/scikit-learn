0def502e96f1b7dd84af4b7f800a2ca0bb099a1a
angela-maennel <77272162+angela-maennel@users.noreply.github.com>
2022-06-01 09:35:42 +0200

    DOC Ensure that label_ranking_average_precision_score passes numpydoc validation (#23504)
    Co-authored-by: Angela <angela.maennel@dida.do>

--- sklearn/tests/test_docstrings.py
+++ sklearn/tests/test_docstrings.py
@@ -40,7 +40,6 @@ FUNCTION_DOCSTRING_IGNORE_LIST = [
     "sklearn.metrics._plot.precision_recall_curve.plot_precision_recall_curve",
     "sklearn.metrics._ranking.coverage_error",
     "sklearn.metrics._ranking.dcg_score",
-    "sklearn.metrics._ranking.label_ranking_average_precision_score",
     "sklearn.metrics._ranking.roc_auc_score",
     "sklearn.metrics._ranking.roc_curve",
     "sklearn.metrics._ranking.top_k_accuracy_score",
