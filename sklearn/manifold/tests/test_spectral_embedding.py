00143c9f896d65670c2d53863e6ff3405727e6a7
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2022-06-02 12:06:10 +0200

    fix

--- sklearn/manifold/tests/test_spectral_embedding.py
+++ sklearn/manifold/tests/test_spectral_embedding.py
@@ -508,11 +508,5 @@ def test_spectral_eigen_tol_auto(monkeypatch, solver):
     spectral_embedding(S, random_state=42, eigen_solver=solver, eigen_tol="auto")
     mocked_solver.assert_called()
 
-    print(mocked_solver.call_args)
-
-    args, kwargs = mocked_solver.call_args
-
-    print(kwargs)
-    print(type(kwargs))
-
-    assert mocked_solver.call_args.kwargs["tol"] == default_value
+    _, kwargs = mocked_solver.call_args
+    assert kwargs["tol"] == default_value
