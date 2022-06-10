24d571fae6743efa0b9d181cd7f4b7d871a92075
Sangam Swadi K <sangamswadik@users.noreply.github.com>
2022-06-10 22:39:37 +0530

    regroup standard imports and remove check for negative threshold

--- sklearn/feature_selection/_variance_threshold.py
+++ sklearn/feature_selection/_variance_threshold.py
@@ -1,8 +1,8 @@
 # Author: Lars Buitinck
 # License: 3-clause BSD
+from numbers import Real
 
 import numpy as np
-from numbers import Real
 from ..base import BaseEstimator
 from ._base import SelectorMixin
 from ..utils.sparsefuncs import mean_variance_axis, min_max_axis
@@ -115,8 +115,6 @@ class VarianceThreshold(SelectorMixin, BaseEstimator):
             # for constant features
             compare_arr = np.array([self.variances_, peak_to_peaks])
             self.variances_ = np.nanmin(compare_arr, axis=0)
-        elif self.threshold < 0.0:
-            raise ValueError(f"Threshold must be non-negative. Got: {self.threshold}")
 
         if np.all(~np.isfinite(self.variances_) | (self.variances_ <= self.threshold)):
             msg = "No feature in X meets the variance threshold {0:.5f}"
