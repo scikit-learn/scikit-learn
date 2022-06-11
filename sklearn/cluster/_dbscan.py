cf7b2a15e2b7956ef3c33020af7c6de0870362a0
Kian Eliasi <kian.elbo@gmail.com>
2022-06-11 14:17:05 +0430

    Update valid values of metric parameter in DBSCAN

--- sklearn/cluster/_dbscan.py
+++ sklearn/cluster/_dbscan.py
@@ -302,7 +302,10 @@ class DBSCAN(ClusterMixin, BaseEstimator):
     _parameter_constraints = {
         "eps": [Interval(Real, 0.0, None, closed="neither")],
         "min_samples": [Interval(Integral, 1, None, closed="left")],
-        "metric": [StrOptions({metric for metric in _VALID_METRICS + ["precomputed"]}), callable],
+        "metric": [
+            StrOptions({metric for metric in _VALID_METRICS + ["precomputed"]}),
+            callable,
+        ],
         "metric_params": [dict, None],
         "algorithm": [StrOptions({"kd_tree", "ball_tree", "auto", "brute"})],
         "leaf_size": [Interval(Integral, 1, None, closed="left")],
