0b1829c89dd038375535a0d6906cfa01db5addbb
avm19 <52547519avm19@users.noreply.github.com>
2022-06-18 18:03:28 -0400

    Implement _BaseNB.predict_joint_log_proba method and test for it

--- sklearn/tests/test_naive_bayes.py
+++ sklearn/tests/test_naive_bayes.py
@@ -5,6 +5,8 @@ import scipy.sparse
 import pytest
 import warnings
 
+from scipy.special import logsumexp
+
 from sklearn.datasets import load_digits, load_iris
 
 from sklearn.model_selection import train_test_split
@@ -945,3 +947,12 @@ def test_n_features_deprecation(Estimator):
 
     with pytest.warns(FutureWarning, match="`n_features_` was deprecated"):
         est.n_features_
+
+
+@pytest.mark.parametrize("Estimator", ALL_NAIVE_BAYES_CLASSES)
+def test_predict_joint_proba(Estimator):
+    est = Estimator().fit(X2, y2)
+    jll = est.predict_joint_log_proba(X2)
+    log_prob_x = logsumexp(jll, axis=1)
+    log_prob_x_y = jll - np.atleast_2d(log_prob_x).T
+    assert_array_almost_equal(log_prob_x_y, est.predict_log_proba(X2), 8)
