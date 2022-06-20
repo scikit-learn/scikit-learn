052b567881bba7f3d23a9f266713bc3979da29d8
Meekail Zain <zainmeekail@gmail.com>
2022-06-20 13:55:26 -0400

    Incorporated review feedback

--- sklearn/utils/_isfinite.pyx
+++ sklearn/utils/_isfinite.pyx
@@ -2,34 +2,40 @@
 
 from libc.math cimport isnan, isinf
 cimport cython
+from cython cimport floating
+from cython.parallel cimport prange
 
 cpdef enum FiniteStatus:
     all_finite = 0
     has_nan = 1
     has_infinite = 2
 
-def cy_isfinite(cython.floating[::1] a, bint allow_nan=False):
+def cy_isfinite(floating[::1] a, bint allow_nan=False):
     cdef int result
     with nogil:
         result = _isfinite(a, allow_nan)
     return result
 
-cdef inline int _isfinite(cython.floating[::1] a, bint allow_nan) nogil:
+cdef inline int _isfinite(floating[::1] a, bint allow_nan) nogil:
+    cdef floating* a_ptr = &a[0]
+    cdef Py_ssize_t length = len(a)
     if allow_nan:
-        return _isfinite_allow_nan(a)
+        return _isfinite_allow_nan(a_ptr, length)
     else:
-        return _isfinite_disable_nan(a)
+        return _isfinite_disable_nan(a_ptr, length)
 
-cdef inline int _isfinite_allow_nan(cython.floating[::1] a) nogil:
-    for i in range(len(a)):
-        if isinf(a[i]) != 0:
+cdef inline int _isfinite_allow_nan(floating* a_ptr, Py_ssize_t length) nogil:
+    cdef Py_ssize_t i
+    for i in range(length):
+        if isinf(a_ptr[i]) != 0:
             return FiniteStatus.has_infinite
     return FiniteStatus.all_finite
 
-cdef inline int _isfinite_disable_nan(cython.floating[::1] a) nogil:
-    for i in range(len(a)):
-        if isnan(a[i]) != 0:
+cdef inline int _isfinite_disable_nan(floating* a_ptr, Py_ssize_t length) nogil:
+    cdef Py_ssize_t i
+    for i in range(length):
+        if isnan(a_ptr[i]) != 0:
             return FiniteStatus.has_nan
-        if isinf(a[i]) != 0:
+        if isinf(a_ptr[i]) != 0:
             return FiniteStatus.has_infinite
     return FiniteStatus.all_finite
