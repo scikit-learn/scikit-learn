859df2c9b57a400032c923e5bd1db39bef4f62e1
Vicente Reyes-Puerta <vr.github@outlook.com>
2022-06-21 10:45:35 +0200

    Write real values with decimals for the sake of clarity

--- sklearn/cluster/_dbscan.py
+++ sklearn/cluster/_dbscan.py
@@ -301,13 +301,13 @@ class DBSCAN(ClusterMixin, BaseEstimator):
     """
 
     _parameter_constraints = {
-        "eps": [Interval(Real, 0, None, closed="neither")],
+        "eps": [Interval(Real, 0.0, None, closed="neither")],
         "min_samples": [Interval(Integral, 1, None, closed="left")],
         "metric": [str, callable],
         "metric_params": [dict, None],
         "algorithm": [StrOptions({"auto", "ball_tree", "kd_tree", "brute"})],
         "leaf_size": [Interval(Integral, 1, None, closed="left")],
-        "p": [Interval(Real, 0, None, closed="left"), None],
+        "p": [Interval(Real, 0.0, None, closed="left"), None],
         "n_jobs": [Interval(Integral, -1, None, closed="left"), None],
     }
 
