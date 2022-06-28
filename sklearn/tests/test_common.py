4e8ad3468abef7d5f4fb97b01e338b715c35331b
Vicente Reyes-Puerta <vr.github@outlook.com>
2022-06-28 17:28:40 +0200

    MAINT Use _validate_params in DBSCAN (#23704)
    Co-authored-by: Meekail Zain <34613774+Micky774@users.noreply.github.com>
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>
    Co-authored-by: jeremiedbb <jeremiedbb@yahoo.fr>

--- sklearn/tests/test_common.py
+++ sklearn/tests/test_common.py
@@ -460,7 +460,6 @@ PARAM_VALIDATION_ESTIMATORS_TO_IGNORE = [
     "CalibratedClassifierCV",
     "ClassifierChain",
     "CountVectorizer",
-    "DBSCAN",
     "DictVectorizer",
     "DictionaryLearning",
     "ElasticNet",
