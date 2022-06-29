eae7e22eb149774bfdaba029fc0bb610e74ec1bb
Sandro Ephrem <sandro.ephrem@gmail.com>
2022-06-29 18:09:46 +0300

    MAINT validate parameters in LocalOutlierFactor  (#23777)
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>
    Co-authored-by: Jérémie du Boisberranger <34657725+jeremiedbb@users.noreply.github.com>

--- sklearn/tests/test_common.py
+++ sklearn/tests/test_common.py
@@ -499,7 +499,6 @@ PARAM_VALIDATION_ESTIMATORS_TO_IGNORE = [
     "LassoLarsIC",
     "LatentDirichletAllocation",
     "LedoitWolf",
-    "LocalOutlierFactor",
     "LocallyLinearEmbedding",
     "MLPClassifier",
     "MLPRegressor",
