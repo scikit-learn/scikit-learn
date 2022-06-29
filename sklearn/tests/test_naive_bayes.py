cae04de5a8878c6e614662686aa36be0c802deb5
avm19 <52547519avm19@users.noreply.github.com>
2022-06-29 14:54:59 -0400

    Correct a mistake in calling assert_allclose

--- sklearn/tests/test_naive_bayes.py
+++ sklearn/tests/test_naive_bayes.py
@@ -929,4 +929,4 @@ def test_predict_joint_proba(Estimator):
     jll = est.predict_joint_log_proba(X2)
     log_prob_x = logsumexp(jll, axis=1)
     log_prob_x_y = jll - np.atleast_2d(log_prob_x).T
-    assert_allclose(log_prob_x_y, est.predict_log_proba(X2), 8)
+    assert_allclose(est.predict_log_proba(X2), log_prob_x_y, atol=1e-7)
