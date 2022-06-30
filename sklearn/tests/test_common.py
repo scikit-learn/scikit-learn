6fe1f465b66b01947786951f6d57f52221819e2c
Nihal Thukarama Rao <nihal_rao@outlook.com>
2022-06-30 18:41:45 +0530

    MAINT Added parameter validation for KernelRidge (#23790)

--- sklearn/tests/test_common.py
+++ sklearn/tests/test_common.py
@@ -483,7 +483,6 @@ PARAM_VALIDATION_ESTIMATORS_TO_IGNORE = [
     "KNNImputer",
     "KNeighborsTransformer",
     "KernelPCA",
-    "KernelRidge",
     "LabelBinarizer",
     "LabelPropagation",
     "LabelSpreading",
