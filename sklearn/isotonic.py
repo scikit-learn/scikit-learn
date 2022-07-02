82aa51b925a7964d71eb8316b70aa71575d830b7
Sangam <SaSangamSwadiK@users.noreply.github.com>
2022-07-02 12:16:20 +0530

    fix type

--- sklearn/isotonic.py
+++ sklearn/isotonic.py
@@ -231,7 +231,7 @@ class IsotonicRegression(RegressorMixin, TransformerMixin, BaseEstimator):
     _parameter_constraints = {
         "y_min": [Real, None],
         "y_max": [Real, None],
-        "increasing": ["boolean", "auto"],
+        "increasing": ["boolean", StrOptions({"auto"})],
         "out_of_bounds": [StrOptions({"nan", "clip", "raise"})],
     }
 
