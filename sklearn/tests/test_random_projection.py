70275fbaf1223a0b243c8fa8ffdd12599b4b9037
Kian Eliasi <kian.elbo@gmail.com>
2022-07-05 00:15:52 +0430

    Use _validate_params in RandomProjection family

--- sklearn/tests/test_random_projection.py
+++ sklearn/tests/test_random_projection.py
@@ -206,14 +206,7 @@ def test_sparse_random_matrix():
 ###############################################################################
 
 
-@pytest.mark.parametrize("density", [1.1, 0, -0.1])
-def test_sparse_random_projection_transformer_invalid_density(density):
-    for RandomProjection in all_SparseRandomProjection:
-        with pytest.raises(ValueError):
-            RandomProjection(density=density).fit(data)
-
-
-@pytest.mark.parametrize("n_components, fit_data", [("auto", [[0, 1, 2]]), (-10, data)])
+@pytest.mark.parametrize("n_components, fit_data", [("auto", [[0, 1, 2]])])
 def test_random_projection_transformer_invalid_input(n_components, fit_data):
     for RandomProjection in all_RandomProjection:
         with pytest.raises(ValueError):
