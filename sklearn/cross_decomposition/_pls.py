1ea8e12298c2adb9e638adaae83291a0692b58f5
fractionalhare <dylan@breakingbits.net>
2022-07-09 14:48:17 -0400

    we also need to make the algorithm externally accessible from the child class

--- sklearn/cross_decomposition/_pls.py
+++ sklearn/cross_decomposition/_pls.py
@@ -695,14 +695,14 @@ class PLSRegression(_PLS):
     #     - "pls" with function oscorespls.fit(X, Y)
 
     def __init__(
-        self, n_components=2, *, scale=True, max_iter=500, tol=1e-06, copy=True
+        self, n_components=2, *, scale=True, max_iter=500, tol=1e-06, copy=True, algorithm="nipals"
     ):
         super().__init__(
             n_components=n_components,
             scale=scale,
             deflation_mode="regression",
             mode="A",
-            algorithm="nipals",
+            algorithm=algorithm,
             max_iter=max_iter,
             tol=tol,
             copy=copy,
