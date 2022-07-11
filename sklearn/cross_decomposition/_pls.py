93ceb83a3eaf38ac7a6efa56708cc4f5d28c310d
fractionalhare <dylan@breakingbits.net>
2022-07-11 12:46:00 -0400

    formatting

--- sklearn/cross_decomposition/_pls.py
+++ sklearn/cross_decomposition/_pls.py
@@ -406,7 +406,7 @@ class _PLS(
                 # Y is only deflated for NIPALS and SVD
                 self.y_scores_ = self._y_scores
         # TODO(1.3): change `self._coef_` to `self.coef_`
-        self._coef_ = ((self.x_rotations_ @ self.y_loadings_.T) * self._y_std)
+        self._coef_ = (self.x_rotations_ @ self.y_loadings_.T) * self._y_std
         self.intercept_ = self._y_mean
         self._n_features_out = self.x_rotations_.shape[1]
         return self
