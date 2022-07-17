a21f611e7561cef415f353eccaaf41dfd7940a9c
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2022-07-17 14:51:37 +0100

    FIX make SparsePCA components_ deterministic

--- sklearn/decomposition/tests/test_sparse_pca.py
+++ sklearn/decomposition/tests/test_sparse_pca.py
@@ -313,3 +313,29 @@ def test_spca_early_stopping(global_random_seed):
         max_iter=100, tol=1e-6, max_no_improvement=100, random_state=global_random_seed
     ).fit(X)
     assert model_early_stopped.n_iter_ < model_not_early_stopped.n_iter_
+
+
+def test_equivalence_components_pca_spca():
+    """Check the equivalence of the components found by PCA and SparsePCA.
+
+    Non-regression test for:
+    https://github.com/scikit-learn/scikit-learn/issues/23932
+    """
+    rng = np.random.RandomState(0)
+    X = rng.randn(50, 4)
+
+    n_components = 2
+    pca = PCA(
+        n_components=n_components,
+        svd_solver="randomized",
+        random_state=0,
+    ).fit(X)
+    spca = SparsePCA(
+        n_components=n_components,
+        method="lars",
+        ridge_alpha=0,
+        alpha=0,
+        random_state=0,
+    ).fit(X)
+
+    assert_allclose(pca.components_, spca.components_)
