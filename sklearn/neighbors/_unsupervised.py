c2c125041422b5fa313e8a2dbde0a82988a7a449
JulioScholz <julio.scholz@gmail.com>
2022-07-18 20:39:35 -0300

    Linting done

--- sklearn/neighbors/_unsupervised.py
+++ sklearn/neighbors/_unsupervised.py
@@ -125,9 +125,7 @@ class NearestNeighbors(KNeighborsMixin, RadiusNeighborsMixin, NeighborsBase):
     array(2)
     """
 
-    _parameter_constraints = {
-        **NeighborsBase._parameter_constraints
-    }
+    _parameter_constraints = {**NeighborsBase._parameter_constraints}
 
     def __init__(
         self,
@@ -149,7 +147,7 @@ class NearestNeighbors(KNeighborsMixin, RadiusNeighborsMixin, NeighborsBase):
             metric=metric,
             p=p,
             metric_params=metric_params,
-            n_jobs=n_jobs,  
+            n_jobs=n_jobs,
         )
 
     def fit(self, X, y=None):
@@ -169,6 +167,7 @@ class NearestNeighbors(KNeighborsMixin, RadiusNeighborsMixin, NeighborsBase):
         self : NearestNeighbors
             The fitted nearest neighbors estimator.
         """
+
         self._validate_params()
-        
+
         return self._fit(X)
