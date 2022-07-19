280a30fae2006437dc36ab0fa9030c5ca5738bab
Tom Mathews <tommathews2007@gmail.com>
2022-07-19 16:24:56 +0530

    Remove `distance_metrics` from test ignore list.

--- sklearn/tests/test_docstrings.py
+++ sklearn/tests/test_docstrings.py
@@ -53,7 +53,6 @@ FUNCTION_DOCSTRING_IGNORE_LIST = [
     "sklearn.metrics.pairwise.chi2_kernel",
     "sklearn.metrics.pairwise.cosine_distances",
     "sklearn.metrics.pairwise.cosine_similarity",
-    "sklearn.metrics.pairwise.distance_metrics",
     "sklearn.metrics.pairwise.kernel_metrics",
     "sklearn.metrics.pairwise.pairwise_distances_argmin",
     "sklearn.metrics.pairwise.pairwise_distances_argmin_min",
