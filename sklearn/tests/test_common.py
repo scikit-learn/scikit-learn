c99d14def0d06fb071cda652c6ff681df1198656
SangamSwadik <sangamswadi.k18@gmail.com>
2022-07-19 17:51:35 +0530

    MAINT Param validation for knnimputer (#23809)
    Co-authored-by: Jérémie du Boisberranger <34657725+jeremiedbb@users.noreply.github.com>

--- sklearn/tests/test_common.py
+++ sklearn/tests/test_common.py
@@ -470,7 +470,6 @@ PARAM_VALIDATION_ESTIMATORS_TO_IGNORE = [
     "Isomap",
     "IsotonicRegression",
     "IterativeImputer",
-    "KNNImputer",
     "KNeighborsTransformer",
     "KernelPCA",
     "LabelPropagation",
