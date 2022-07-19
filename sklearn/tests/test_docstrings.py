18d1627ce19c8b25c09d72b0e4fa36de022b9617
Tom Mathews <tommathews2007@gmail.com>
2022-07-19 16:43:08 +0530

    Remove `pairwise_distances_argmin` from test ignore list.

--- sklearn/tests/test_docstrings.py
+++ sklearn/tests/test_docstrings.py
@@ -55,7 +55,6 @@ FUNCTION_DOCSTRING_IGNORE_LIST = [
     "sklearn.metrics.pairwise.cosine_similarity",
     "sklearn.metrics.pairwise.distance_metrics",
     "sklearn.metrics.pairwise.kernel_metrics",
-    "sklearn.metrics.pairwise.pairwise_distances_argmin",
     "sklearn.metrics.pairwise.pairwise_distances_argmin_min",
     "sklearn.metrics.pairwise.pairwise_distances_chunked",
     "sklearn.metrics.pairwise.polynomial_kernel",
