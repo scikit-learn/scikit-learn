aa881e39355f935c82d8efce89a3c64d4d533149
Tom Mathews <tommathews2007@gmail.com>
2022-07-19 15:12:33 +0530

    Remove check_paired_arrays from test ignore list.

--- sklearn/tests/test_docstrings.py
+++ sklearn/tests/test_docstrings.py
@@ -49,7 +49,6 @@ FUNCTION_DOCSTRING_IGNORE_LIST = [
     "sklearn.metrics.cluster._supervised.rand_score",
     "sklearn.metrics.cluster._supervised.v_measure_score",
     "sklearn.metrics.pairwise.additive_chi2_kernel",
-    "sklearn.metrics.pairwise.check_paired_arrays",
     "sklearn.metrics.pairwise.chi2_kernel",
     "sklearn.metrics.pairwise.cosine_distances",
     "sklearn.metrics.pairwise.cosine_similarity",
