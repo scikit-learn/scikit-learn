93c3830c79f18f79ccd2d5f5d1326ba49305d6fe
Maxwell <zl2480@columbia.edu>
2022-07-20 09:23:12 +0800

    update test comment
    Co-authored-by: Thomas J. Fan <thomasjpfan@gmail.com>

--- sklearn/linear_model/tests/test_sgd.py
+++ sklearn/linear_model/tests/test_sgd.py
@@ -2160,7 +2160,7 @@ def test_validation_mask_correctly_subsets(monkeypatch):
 
 def test_sgd_error_on_zero_validation_weight():
     # Test that SGDClassifier raises error when all the validation samples
-    # have zero sample_weight
+    # have zero sample_weight. Non-regression test for #17229.
     X, Y = iris.data, iris.target
     sample_weight = np.zeros_like(Y)
     validation_fraction = 0.4
