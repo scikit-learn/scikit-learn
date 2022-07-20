d85f7d2c2c9bd7314f40ab67e1579f17d49f0060
Tom Mathews <9562152+Mathews-Tom@users.noreply.github.com>
2022-07-21 00:39:06 +0530

    Update sklearn/metrics/pairwise.py
    Co-authored-by: Meekail Zain <34613774+Micky774@users.noreply.github.com>

--- sklearn/metrics/pairwise.py
+++ sklearn/metrics/pairwise.py
@@ -1187,8 +1187,10 @@ def linear_kernel(X, Y=None, dense_output=True):
 
 
 def polynomial_kernel(X, Y=None, degree=3, gamma=None, coef0=1):
-    """Compute the polynomial kernel between X and Y.
-        K(X, Y) = (gamma <X, Y> + coef0)^degree
+    """
+    Compute the polynomial kernel between X and Y.
+
+    :math:`K(X, Y) = (gamma <X, Y> + coef0)^degree`
 
     Read more in the :ref:`User Guide <polynomial_kernel>`.
 
