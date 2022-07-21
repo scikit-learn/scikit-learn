e480b09ff1f2f75c545d762aa480650c49c3a418
EvgeniDubov <32032278+EvgeniDubov@users.noreply.github.com>
2022-07-21 21:34:31 +0300

    deleted debug prints

--- sklearn/ensemble/tests/test_forest.py
+++ sklearn/ensemble/tests/test_forest.py
@@ -348,7 +348,6 @@ def check_importances(name, criterion, tolerance, X, y):
 
     # The forest estimator can detect that only the first 3 features of the
     # dataset are informative:
-    print(importances)
     n_important = np.sum(importances > 0.1)
     assert importances.shape[0] == 10
     assert n_important == 3
