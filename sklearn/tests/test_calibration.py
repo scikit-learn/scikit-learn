dc702161d996ed6e3657d8ea643f0f88b74eb1ec
Benjamin Bossan <benjamin.bossan@gmail.com>
2022-08-03 17:55:40 +0200

    Make CalibratedClassifierCV work with SLEP006
    This PR adds metadata routing to CalibratedClassifierCV (CCV). CCV uses
    a subestimator to create (out of sample) probabilities, which are in
    turn used to calibrate the probabilities.
    
    The metaestimator uses sample_weight. The subestimator may or may not
    use sample_weight and additional metadata. So far, it was checked if the
    subestimator has sample_weight in its signature and then they were
    routed, otherwise not. This is, however, not always ideal, e.g. when the
    subestimator is itself a
    pipeline (https://github.com/scikit-learn/scikit-learn/issues/21134).
    With routing, this problem disappears.
    
    In addition to these changes, the tests in
    test_metaestimator_metadata_routing.py have been amended to make them
    more generic, as right now, they are specific to multioutput.

--- sklearn/tests/test_calibration.py
+++ sklearn/tests/test_calibration.py
@@ -50,6 +50,10 @@ from sklearn.utils._testing import _convert_container
 N_SAMPLES = 200
 
 
+def _weighted(estimator):
+    return estimator.set_fit_request(sample_weight=True)
+
+
 @pytest.fixture(scope="module")
 def data():
     X, y = make_classification(n_samples=N_SAMPLES, n_features=6, random_state=42)
@@ -83,7 +87,9 @@ def test_calibration(data, method, ensemble):
         (X_train, X_test),
         (sparse.csr_matrix(X_train), sparse.csr_matrix(X_test)),
     ]:
-        cal_clf = CalibratedClassifierCV(clf, method=method, cv=5, ensemble=ensemble)
+        cal_clf = CalibratedClassifierCV(
+            _weighted(clf), method=method, cv=5, ensemble=ensemble
+        )
         # Note that this fit overwrites the fit on the entire training
         # set
         cal_clf.fit(this_X_train, y_train, sample_weight=sw_train)
@@ -175,7 +181,7 @@ def test_sample_weight(data, method, ensemble):
     X_train, y_train, sw_train = X[:n_samples], y[:n_samples], sample_weight[:n_samples]
     X_test = X[n_samples:]
 
-    estimator = LinearSVC(random_state=42)
+    estimator = _weighted(LinearSVC(random_state=42))
     calibrated_clf = CalibratedClassifierCV(estimator, method=method, ensemble=ensemble)
     calibrated_clf.fit(X_train, y_train, sample_weight=sw_train)
     probs_with_sw = calibrated_clf.predict_proba(X_test)
@@ -909,7 +915,7 @@ def test_calibrated_classifier_cv_double_sample_weights_equivalence(method, ense
     y_twice[::2] = y
     y_twice[1::2] = y
 
-    estimator = LogisticRegression()
+    estimator = _weighted(LogisticRegression())
     calibrated_clf_without_weights = CalibratedClassifierCV(
         estimator,
         method=method,
@@ -951,7 +957,9 @@ def test_calibration_with_fit_params(fit_params_type, data):
         "b": _convert_container(y, fit_params_type),
     }
 
-    clf = CheckingClassifier(expected_fit_params=["a", "b"])
+    clf = CheckingClassifier(expected_fit_params=["a", "b"]).set_fit_request(
+        a=True, b=True
+    )
     pc_clf = CalibratedClassifierCV(clf)
 
     pc_clf.fit(X, y, **fit_params)
@@ -969,32 +977,10 @@ def test_calibration_with_sample_weight_base_estimator(sample_weight, data):
     estimator.
     """
     X, y = data
-    clf = CheckingClassifier(expected_sample_weight=True)
+    clf = _weighted(CheckingClassifier(expected_sample_weight=True))
     pc_clf = CalibratedClassifierCV(clf)
 
     pc_clf.fit(X, y, sample_weight=sample_weight)
-
-
-def test_calibration_without_sample_weight_base_estimator(data):
-    """Check that even if the estimator doesn't support
-    sample_weight, fitting with sample_weight still works.
-
-    There should be a warning, since the sample_weight is not passed
-    on to the estimator.
-    """
-    X, y = data
-    sample_weight = np.ones_like(y)
-
-    class ClfWithoutSampleWeight(CheckingClassifier):
-        def fit(self, X, y, **fit_params):
-            assert "sample_weight" not in fit_params
-            return super().fit(X, y, **fit_params)
-
-    clf = ClfWithoutSampleWeight()
-    pc_clf = CalibratedClassifierCV(clf)
-
-    with pytest.warns(UserWarning):
-        pc_clf.fit(X, y, sample_weight=sample_weight)
 
 
 def test_calibration_with_fit_params_inconsistent_length(data):
@@ -1029,7 +1015,7 @@ def test_calibrated_classifier_cv_zeros_sample_weights_equivalence(method, ensem
     sample_weight = np.zeros_like(y)
     sample_weight[::2] = 1
 
-    estimator = LogisticRegression()
+    estimator = _weighted(LogisticRegression())
     calibrated_clf_without_weights = CalibratedClassifierCV(
         estimator,
         method=method,
--- sklearn/tests/test_metaestimators_metadata_routing.py
+++ sklearn/tests/test_metaestimators_metadata_routing.py
@@ -1,6 +1,11 @@
+import re
+import warnings
+
 import numpy as np
 import pytest
 from sklearn.base import RegressorMixin, ClassifierMixin, BaseEstimator
+from sklearn.calibration import CalibratedClassifierCV
+from sklearn.exceptions import UnsetMetadataPassedError
 from sklearn.multioutput import MultiOutputRegressor, MultiOutputClassifier
 from sklearn.utils.metadata_routing import MetadataRouter
 from sklearn.tests.test_metadata_routing import (
@@ -17,109 +22,277 @@ metadata = np.random.randint(0, 10, size=N)
 sample_weight = np.random.rand(N)
 
 
+class _Registry(list):
+    # This list is used to get a reference to the sub-estimators, which are not
+    # necessarily stored on the metaestimator. We need to override __deepcopy__
+    # because the sub-estimators are probably cloned, which would result in a
+    # new copy of the list.
+    def __deepcopy__(self, memo):
+        return self
+
+
 class ConsumingRegressor(RegressorMixin, BaseEstimator):
-    """A regressor consuming metadata."""
+    """A regressor consuming metadata.
+
+    Parameters
+    ----------
+    registry : list or None, default=None
+        If a list, that estimator will append itself to the list in order to
+        have a reference to the estimator later on. Since that reference is not
+        required in all tests, registration can be skipped by leaving this value
+        as None.
+
+    """
+    def __init__(self, registry=None):
+        self.registry = registry
 
     def partial_fit(self, X, y, sample_weight=None, metadata=None):
+        if self.registry is not None:
+            self.registry.append(self)
+
         record_metadata(
             self, "partial_fit", sample_weight=sample_weight, metadata=metadata
         )
         return self
 
     def fit(self, X, y, sample_weight=None, metadata=None):
+        if self.registry is not None:
+            self.registry.append(self)
+
         record_metadata(self, "fit", sample_weight=sample_weight, metadata=metadata)
         return self
 
-    def predict(self, X, y, sample_weight=None, metadata=None):
+    def predict(self, X, sample_weight=None, metadata=None):
+        if self.registry is not None:
+            self.registry.append(self)
+
         record_metadata(self, "predict", sample_weight=sample_weight, metadata=metadata)
-        return np.zeros(shape=(len(X)))
+        return np.zeros(shape=(len(X),))
 
 
 class ConsumingClassifier(ClassifierMixin, BaseEstimator):
-    """A classifier consuming metadata."""
+    """A classifier consuming metadata.
+
+    Parameters
+    ----------
+    registry : list or None, default=None
+        If a list, that estimator will append itself to the list in order to
+        have a reference to the estimator later on. Since that reference is not
+        required in all tests, registration can be skipped by leaving this value
+        as None.
+
+    """
+    def __init__(self, registry=None):
+        self.registry = registry
 
     def partial_fit(self, X, y, sample_weight=None, metadata=None):
+        if self.registry is not None:
+            self.registry.append(self)
+
         record_metadata(
             self, "partial_fit", sample_weight=sample_weight, metadata=metadata
         )
-        self.classes_ = [1]
+        self.classes_ = [0, 1]
         return self
 
     def fit(self, X, y, sample_weight=None, metadata=None):
+        if self.registry is not None:
+            self.registry.append(self)
+
         record_metadata(self, "fit", sample_weight=sample_weight, metadata=metadata)
-        self.classes_ = [1]
+        self.classes_ = [0, 1]
         return self
 
-    def predict(self, X, y, sample_weight=None, metadata=None):
-        record_metadata(self, "predict", sample_weight=sample_weight, metadata=metadata)
-        return np.zeros(shape=(len(X)))
+    def predict(self, X, sample_weight=None, metadata=None):
+        if self.registry is not None:
+            self.registry.append(self)
 
-    def predict_proba(self, X, y, sample_weight=None, metadata=None):
+        record_metadata(self, "predict", sample_weight=sample_weight, metadata=metadata)
+        return np.zeros(shape=(len(X),))
+
+    def predict_proba(self, X, sample_weight=None, metadata=None):
+        if self.registry is not None:
+            self.registry.append(self)
+
         record_metadata(
             self, "predict_proba", sample_weight=sample_weight, metadata=metadata
         )
-        return np.zeros(shape=(len(X)))
+        return np.asarray([[0.0, 1.0]] * len(X))
 
-    def predict_log_proba(self, X, y, sample_weight=None, metadata=None):
+    def predict_log_proba(self, X, sample_weight=None, metadata=None):
+        if self.registry is not None:
+            self.registry.append(self)
+
         record_metadata(
             self, "predict_log_proba", sample_weight=sample_weight, metadata=metadata
         )
-        return np.zeros(shape=(len(X)))
+        return np.zeros(shape=(len(X), 2))
 
 
-def get_empty_metaestimators():
-    yield MultiOutputRegressor(estimator=ConsumingRegressor())
-    yield MultiOutputClassifier(estimator=ConsumingClassifier())
+METAESTIMATORS = [
+    {
+        "metaestimator": MultiOutputRegressor,
+        "estimator_name": "estimator",
+        "estimator": ConsumingRegressor,
+        "X": X,
+        "y": y_multi,
+        "routing_methods": ["fit", "partial_fit"],
+        "fit": {"warns_on": ["sample_weight"]},
+    },
+    {
+        "metaestimator": MultiOutputClassifier,
+        "estimator_name": "estimator",
+        "estimator": ConsumingClassifier,
+        "X": X,
+        "y": y_multi,
+        "routing_methods": ["fit", "partial_fit"],
+        "fit": {"warns_on": ["sample_weight"]},
+    },
+    {
+        "metaestimator": CalibratedClassifierCV,
+        "estimator_name": "estimator",
+        "estimator": ConsumingClassifier,
+        "X": X,
+        "y": y,
+        "routing_methods": ["fit"],
+        "fit": {"warns_on": "all"},
+        "preserves_metadata": False,
+    },
+]
+"""List containing all metaestimators to be tested and their settings
+
+The keys are as follows:
+
+- metaestimator: The metaestmator to be tested
+- estimator_name: The name of the argument for the sub-estimator
+- estimator: The sub-estimator
+- X: X-data to fit and predict
+- y: y-data to fit
+- routing_methods: list of all methods to check for routing
+- <method_name>: How said method behaves, e.g. "fit": {"warns_on":
+  ["sample_weight"]} means fit warns when sample weights are passed but not
+  requested.
+- preserves_metadata: Whether the metaestimator passes the metadata to the
+  sub-estimator without modification or not. If it does, we check that the
+  values are identical. If it doesn', no check is performed. TODO Maybe
+  something smarter could be done if the data is modified.
+
+"""
 
 
 @pytest.mark.parametrize(
     "metaestimator",
-    get_empty_metaestimators(),
-    ids=[str(x) for x in get_empty_metaestimators()],
+    METAESTIMATORS,
+    ids=[str(row["metaestimator"].__class__.__name__) for row in METAESTIMATORS],
 )
 def test_default_request(metaestimator):
     # Check that by default request is empty and the right type
-    assert_request_is_empty(metaestimator.get_metadata_routing())
-    assert isinstance(metaestimator.get_metadata_routing(), MetadataRouter)
+    cls = metaestimator["metaestimator"]
+    estimator = metaestimator["estimator"]()
+    estimator_name = metaestimator["estimator_name"]
+    instance = cls(**{estimator_name: estimator})
+    assert_request_is_empty(instance.get_metadata_routing())
+    assert isinstance(instance.get_metadata_routing(), MetadataRouter)
 
 
 @pytest.mark.parametrize(
-    "MultiOutput, Estimator",
-    [
-        (MultiOutputClassifier, ConsumingClassifier),
-        (MultiOutputRegressor, ConsumingRegressor),
-    ],
-    ids=["Classifier", "Regressor"],
+    "metaestimator",
+    METAESTIMATORS,
+    ids=[str(row["metaestimator"].__class__.__name__) for row in METAESTIMATORS],
 )
-def test_multioutput_metadata_routing(MultiOutput, Estimator):
-    # Check routing of metadata
-    metaest = MultiOutput(Estimator())
-    warn_msg = (
-        "You are passing metadata for which the request values are not explicitly"
-        " set: sample_weight, metadata."
-    )
-    with pytest.warns(FutureWarning, match=(warn_msg)):
-        metaest.fit(X, y_multi, sample_weight=sample_weight, metadata=metadata)
-        check_recorded_metadata(
-            metaest.estimators_[0],
-            "fit",
-            sample_weight=sample_weight,
-            metadata=metadata,
+def test_warning_for_indicated_methods(metaestimator):
+    # Check that the indicated methods give a warning
+    # TODO: After deprecation period, always error
+    cls = metaestimator["metaestimator"]
+    registry = _Registry()
+    estimator = metaestimator["estimator"](registry=registry)
+    estimator_name = metaestimator["estimator_name"]
+    instance = cls(**{estimator_name: estimator})
+    X = metaestimator["X"]
+    y = metaestimator["y"]
+    routing_methods = metaestimator["routing_methods"]
+
+    for method_name in routing_methods:
+        if not metaestimator.get(method_name, {}).get("warns_on"):
+            # this method is not expected to warn
+            continue
+
+        warn_msg = (
+            "You are passing metadata for which the request values are not explicitly "
+            "set: sample_weight, metadata. From version 1.4 this results in the "
+            "following error: [sample_weight, metadata] are passed but are not "
+            "explicitly set as requested or not for "
+            f"{estimator.__class__.__name__}.{method_name}"
         )
+        with pytest.warns(FutureWarning, match=re.escape(warn_msg)):
+            method = getattr(instance, method_name)
+            method(X, y, sample_weight=sample_weight, metadata=metadata)
 
-    metaest = MultiOutput(
-        Estimator()
-        .set_fit_request(sample_weight=True, metadata="alias")
-        .set_partial_fit_request(sample_weight=True, metadata=True)
-    ).fit(X, y_multi, alias=metadata)
-    # if an estimator requests a metadata but it's not passed, no errors are
-    # raised. Therefore here we don't pass `sample_weight` to test nothing is
-    # raised.
-    check_recorded_metadata(
-        metaest.estimators_[0], "fit", sample_weight=None, metadata=metadata
-    )
+        if metaestimator.get("preserves_metadata", True):
+            check_recorded_metadata(
+                registry[-1],
+                method_name,
+                sample_weight=sample_weight,
+                metadata=metadata,
+            )
 
-    metaest.partial_fit(X, y_multi, metadata=metadata)
-    check_recorded_metadata(
-        metaest.estimators_[0], "partial_fit", sample_weight=None, metadata=metadata
-    )
+
+@pytest.mark.parametrize(
+    "metaestimator",
+    METAESTIMATORS,
+    ids=[str(row["metaestimator"].__class__.__name__) for row in METAESTIMATORS],
+)
+def test_error_for_other_methods(metaestimator):
+    # This test complements test_warning_for_indicated_methods but checks for
+    # UnsetMetadataPassedError instead of FutureWarning
+    cls = metaestimator["metaestimator"]
+    estimator = metaestimator["estimator"]()
+    estimator_name = metaestimator["estimator_name"]
+    instance = cls(**{estimator_name: estimator})
+    X = metaestimator["X"]
+    y = metaestimator["y"]
+    routing_methods = metaestimator["routing_methods"]
+
+    for method_name in routing_methods:
+        if metaestimator.get(method_name, {}).get("warns_on"):
+            # this method is not expected to warn
+            continue
+
+        msg = (
+            "[sample_weight, metadata] are passed but are not explicitly set as "
+            "requested or not for "
+            f"{estimator.__class__.__name__}.{method_name}"
+        )
+        with pytest.raises(UnsetMetadataPassedError, match=re.escape(msg)):
+            method = getattr(instance, method_name)
+            method(X, y, sample_weight=sample_weight, metadata=metadata)
+
+
+@pytest.mark.parametrize(
+    "metaestimator",
+    METAESTIMATORS,
+    ids=[str(row["metaestimator"].__class__.__name__) for row in METAESTIMATORS],
+)
+def test_setting_request_removes_warning_or_error(metaestimator):
+    # When the metadata is explicitly requested, there should be no warning and
+    # no error.
+    def set_request(estimator, method_name):
+        # e.g. call set_fit_request on estimator
+        method = getattr(estimator, f"set_{method_name}_request")
+        method(sample_weight=True, metadata=True)
+
+    cls = metaestimator["metaestimator"]
+    estimator_name = metaestimator["estimator_name"]
+    X = metaestimator["X"]
+    y = metaestimator["y"]
+    routing_methods = metaestimator["routing_methods"]
+
+    for method_name in routing_methods:
+        estimator = metaestimator["estimator"]()
+        set_request(estimator, method_name)
+        instance = cls(**{estimator_name: estimator})
+        # lines below to ensure that there are no warnings
+        with warnings.catch_warnings():
+            warnings.simplefilter("error")
+            method = getattr(instance, method_name)
+            method(X, y, sample_weight=sample_weight, metadata=metadata)
