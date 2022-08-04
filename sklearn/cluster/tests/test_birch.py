55e515df8f43a46365d0d0be8b2a6aba8dc74dac
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2022-08-04 17:00:43 +0200

    other merge conflicts

--- sklearn/cluster/tests/test_birch.py
+++ sklearn/cluster/tests/test_birch.py
@@ -14,6 +14,7 @@ from sklearn.exceptions import ConvergenceWarning
 from sklearn.linear_model import ElasticNet
 from sklearn.metrics import pairwise_distances_argmin, v_measure_score
 
+from sklearn.utils._testing import assert_allclose
 from sklearn.utils._testing import assert_almost_equal
 from sklearn.utils._testing import assert_array_equal
 from sklearn.utils._testing import assert_array_almost_equal
