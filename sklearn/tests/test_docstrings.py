dc8d444dbbaea1cf6d93a4a816f8d20fab0ef2ab
fcharras <29153872+fcharras@users.noreply.github.com>
2022-08-12 18:44:23 +0200

    DOC Ensures that load_svmlight_file passes numpydoc validation (#24163)
    Co-authored-by: Franck Charras <franck.charras@inria.fr>

--- sklearn/tests/test_docstrings.py
+++ sklearn/tests/test_docstrings.py
@@ -12,7 +12,6 @@ import sklearn
 numpydoc_validation = pytest.importorskip("numpydoc.validate")
 
 FUNCTION_DOCSTRING_IGNORE_LIST = [
-    "sklearn.datasets._svmlight_format_io.load_svmlight_file",
     "sklearn.decomposition._dict_learning.dict_learning",
     "sklearn.decomposition._dict_learning.dict_learning_online",
     "sklearn.decomposition._nmf.non_negative_factorization",
