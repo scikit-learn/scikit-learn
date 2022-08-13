e9c12f98f714a80256253f8f4d33632924cfbfd8
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2022-08-13 12:20:18 -0400

    MAINT Add parameter validation to FeatureHeasher.

--- sklearn/tests/test_common.py
+++ sklearn/tests/test_common.py
@@ -469,7 +469,6 @@ PARAM_VALIDATION_ESTIMATORS_TO_IGNORE = [
     "CalibratedClassifierCV",
     "ClassifierChain",
     "DictionaryLearning",
-    "FeatureHasher",
     "FunctionTransformer",
     "GenericUnivariateSelect",
     "HashingVectorizer",
