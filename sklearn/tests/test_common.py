754f0d996afc06db8e6dc94a546a4151d8273f25
OmarSalman <omersalman@A006-00870.local>
2022-08-29 15:37:44 +0500

    MAINT Parameters validation for MultiTaskElasticNet and MultiTaskLasso

--- sklearn/tests/test_common.py
+++ sklearn/tests/test_common.py
@@ -485,8 +485,6 @@ PARAM_VALIDATION_ESTIMATORS_TO_IGNORE = [
     "LatentDirichletAllocation",
     "LedoitWolf",
     "MiniBatchDictionaryLearning",
-    "MultiTaskElasticNet",
-    "MultiTaskLasso",
     "NeighborhoodComponentsAnalysis",
     "Nystroem",
     "OAS",
