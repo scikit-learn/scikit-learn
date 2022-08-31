42fa09e103861de2d96b40a9de8c40a4a738e7de
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2022-08-31 16:33:31 -0400

    MAINT Add parameter validation to `LatentDirichletAllocation` (#24212)
    Co-authored-by: Thomas J. Fan <thomasjpfan@gmail.com>

--- sklearn/tests/test_common.py
+++ sklearn/tests/test_common.py
@@ -467,7 +467,6 @@ PARAM_VALIDATION_ESTIMATORS_TO_IGNORE = [
     "ClassifierChain",
     "DictionaryLearning",
     "HashingVectorizer",
-    "LatentDirichletAllocation",
     "MiniBatchDictionaryLearning",
     "MultiTaskElasticNet",
     "MultiTaskLasso",
