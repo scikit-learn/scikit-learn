a8032c38015a09232ac124c7bed14346b2cf1572
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2022-09-02 12:13:49 +0200

    cln

--- sklearn/decomposition/tests/test_dict_learning.py
+++ sklearn/decomposition/tests/test_dict_learning.py
@@ -244,6 +244,21 @@ def test_dict_learning_lassocd_readonly_data():
         )
 
 
+def test_dict_learning_nonzero_coefs():
+    n_components = 4
+    dico = DictionaryLearning(
+        n_components,
+        transform_algorithm="lars",
+        transform_n_nonzero_coefs=3,
+        random_state=0,
+    )
+    code = dico.fit(X).transform(X[np.newaxis, 1])
+    assert len(np.flatnonzero(code)) == 3
+
+    dico.set_params(transform_algorithm="omp")
+    code = dico.transform(X[np.newaxis, 1])
+    assert len(np.flatnonzero(code)) == 3
+
 def test_dict_learning_split():
     n_components = 5
     dico = DictionaryLearning(
