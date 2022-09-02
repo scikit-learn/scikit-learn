863bc3dada78aeec487720d2905ef57115dabe65
Tim Head <betatim@gmail.com>
2022-09-02 12:16:54 +0200

    Add markers to make plot colorblind friendly

--- examples/cluster/plot_kmeans_stability_low_dim_dense.py
+++ examples/cluster/plot_kmeans_stability_low_dim_dense.py
@@ -74,13 +74,13 @@ plots = []
 legends = []
 
 cases = [
-    (KMeans, "k-means++", {}),
-    (KMeans, "random", {}),
-    (MiniBatchKMeans, "k-means++", {"max_no_improvement": 3}),
-    (MiniBatchKMeans, "random", {"max_no_improvement": 3, "init_size": 500}),
+    (KMeans, "k-means++", {}, "^-"),
+    (KMeans, "random", {}, "o-"),
+    (MiniBatchKMeans, "k-means++", {"max_no_improvement": 3}, "x-"),
+    (MiniBatchKMeans, "random", {"max_no_improvement": 3, "init_size": 500}, "d-"),
 ]
 
-for factory, init, params in cases:
+for factory, init, params, format in cases:
     print("Evaluation of %s with %s init" % (factory.__name__, init))
     inertia = np.empty((len(n_init_range), n_runs))
 
@@ -95,7 +95,9 @@ for factory, init, params in cases:
                 **params,
             ).fit(X)
             inertia[i, run_id] = km.inertia_
-    p = plt.errorbar(n_init_range, inertia.mean(axis=1), inertia.std(axis=1))
+    p = plt.errorbar(
+        n_init_range, inertia.mean(axis=1), inertia.std(axis=1), fmt=format
+    )
     plots.append(p[0])
     legends.append("%s with %s init" % (factory.__name__, init))
 
