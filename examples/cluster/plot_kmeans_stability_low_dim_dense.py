9b0083d839f986dca4ffb1731908ca7949548da9
Tim Head <betatim@gmail.com>
2022-09-02 12:38:06 +0200

    Remove redundant marker definition

--- examples/cluster/plot_kmeans_stability_low_dim_dense.py
+++ examples/cluster/plot_kmeans_stability_low_dim_dense.py
@@ -117,7 +117,7 @@ plt.figure()
 for k in range(n_clusters):
     my_members = km.labels_ == k
     color = cm.nipy_spectral(float(k) / n_clusters, 1)
-    plt.plot(X[my_members, 0], X[my_members, 1], "o", marker=".", c=color)
+    plt.plot(X[my_members, 0], X[my_members, 1], ".", c=color)
     cluster_center = km.cluster_centers_[k]
     plt.plot(
         cluster_center[0],
