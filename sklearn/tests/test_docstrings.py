7612eea81eb4c1ff626dd43a2784f1a0deca6097
maikia <maria.telenczuk@owkin.com>
2022-09-02 14:15:50 +0200

    correct docstring

--- sklearn/tests/test_docstrings.py
+++ sklearn/tests/test_docstrings.py
@@ -16,7 +16,6 @@ FUNCTION_DOCSTRING_IGNORE_LIST = [
     "sklearn.externals._packaging.version.parse",
     "sklearn.inspection._plot.partial_dependence.plot_partial_dependence",
     "sklearn.linear_model._omp.orthogonal_mp_gram",
-    "sklearn.manifold._locally_linear.locally_linear_embedding",
     "sklearn.manifold._t_sne.trustworthiness",
     "sklearn.metrics._plot.det_curve.plot_det_curve",
     "sklearn.metrics._plot.precision_recall_curve.plot_precision_recall_curve",
