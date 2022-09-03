d216c4e19e5aa867b57bdaae85da36a2f9d45a4a
safikh <safiuddin.khaja@gmail.com>
2022-09-03 10:44:46 +0530

    remove adjusted mutual info from doctring ignore list

--- sklearn/tests/test_docstrings.py
+++ sklearn/tests/test_docstrings.py
@@ -20,7 +20,6 @@ FUNCTION_DOCSTRING_IGNORE_LIST = [
     "sklearn.metrics._ranking.dcg_score",
     "sklearn.metrics._ranking.roc_curve",
     "sklearn.metrics.cluster._bicluster.consensus_score",
-    "sklearn.metrics.cluster._supervised.adjusted_mutual_info_score",
     "sklearn.metrics.cluster._supervised.adjusted_rand_score",
     "sklearn.metrics.cluster._supervised.entropy",
     "sklearn.metrics.cluster._supervised.fowlkes_mallows_score",
