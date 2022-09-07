17b55e69aabf02d201508afecb9de7754a1d04e1
Z Adil Khwaja <zadilkhwaja@gmail.com>
2022-09-07 17:07:16 +0530

    DOC ensures sklearn.utils.shuffle passes numpydoc validation (#24367)
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- sklearn/tests/test_docstrings.py
+++ sklearn/tests/test_docstrings.py
@@ -46,7 +46,6 @@ FUNCTION_DOCSTRING_IGNORE_LIST = [
     "sklearn.utils.multiclass.unique_labels",
     "sklearn.utils.safe_mask",
     "sklearn.utils.safe_sqr",
-    "sklearn.utils.shuffle",
     "sklearn.utils.sparsefuncs.count_nonzero",
     "sklearn.utils.sparsefuncs.csc_median_axis_0",
     "sklearn.utils.sparsefuncs.incr_mean_variance_axis",
