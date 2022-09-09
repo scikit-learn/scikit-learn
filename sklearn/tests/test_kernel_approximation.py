92f555ea158c3785cbe1e7e918334b90aa1a0680
Tim Head <betatim@gmail.com>
2022-09-09 14:38:44 +0200

    ENH Preserves dtype in `RBFSampler` (#24317)
    Co-authored-by: Olivier Grisel <olivier.grisel@ensta.org>
    Co-authored-by: Thomas J. Fan <thomasjpfan@gmail.com>
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- sklearn/tests/test_kernel_approximation.py
+++ sklearn/tests/test_kernel_approximation.py
@@ -215,6 +215,33 @@ def test_rbf_sampler():
     assert np.mean(error) <= 0.05  # mean is fairly close
 
 
+def test_rbf_sampler_fitted_attributes_dtype(global_dtype):
+    """Check that the fitted attributes are stored accordingly to the
+    data type of X."""
+    rbf = RBFSampler()
+
+    X = np.array([[1, 2], [3, 4], [5, 6]], dtype=global_dtype)
+
+    rbf.fit(X)
+
+    assert rbf.random_offset_.dtype == global_dtype
+    assert rbf.random_weights_.dtype == global_dtype
+
+
+def test_rbf_sampler_dtype_equivalence():
+    """Check the equivalence of the results with 32 and 64 bits input."""
+    rbf32 = RBFSampler(random_state=42)
+    X32 = np.array([[1, 2], [3, 4], [5, 6]], dtype=np.float32)
+    rbf32.fit(X32)
+
+    rbf64 = RBFSampler(random_state=42)
+    X64 = np.array([[1, 2], [3, 4], [5, 6]], dtype=np.float64)
+    rbf64.fit(X64)
+
+    assert_allclose(rbf32.random_offset_, rbf64.random_offset_)
+    assert_allclose(rbf32.random_weights_, rbf64.random_weights_)
+
+
 def test_skewed_chi2_sampler_fitted_attributes_dtype(global_dtype):
     """Check that the fitted attributes are stored accordingly to the
     data type of X."""
