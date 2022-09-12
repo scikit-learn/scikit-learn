faf704aae97927c5fae50077d450ec3a37b17424
Julien Jerphanion <git@jjerphan.xyz>
2022-09-12 11:12:14 +0200

    Test and document LocalOutlierFactor on sparse data

--- sklearn/neighbors/tests/test_lof.py
+++ sklearn/neighbors/tests/test_lof.py
@@ -5,6 +5,8 @@
 from math import sqrt
 
 import numpy as np
+from scipy.sparse import csr_matrix
+
 from sklearn import neighbors
 import re
 import pytest
@@ -226,3 +228,17 @@ def test_predicted_outlier_number(expected_outliers):
     if num_outliers != expected_outliers:
         y_dec = clf.negative_outlier_factor_
         check_outlier_corruption(num_outliers, expected_outliers, y_dec)
+
+
+def test_sparse():
+    # LocalOutlierFactor must support CSR inputs
+    X = csr_matrix(iris.data)
+
+    lof = neighbors.LocalOutlierFactor(novelty=True)
+    lof.fit(X)
+    lof.predict(X)
+    lof.score_samples(X)
+    lof.decision_function(X)
+
+    lof = neighbors.LocalOutlierFactor(novelty=False)
+    lof.fit_predict(X)
