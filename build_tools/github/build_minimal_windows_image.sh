99c99b9f2a5af83a182ec53946c6101d851b5d9e
Chiara Marmo <marmochiaskl@gmail.com>
2022-09-13 09:45:24 -1000

    [cd build] Add wheel builds for python 3.11

--- build_tools/github/build_minimal_windows_image.sh
+++ build_tools/github/build_minimal_windows_image.sh
@@ -22,10 +22,21 @@ cp $WHEEL_PATH $WHEEL_NAME
 # Dot the Python version for identyfing the base Docker image
 PYTHON_VERSION=$(echo ${PYTHON_VERSION:0:1}.${PYTHON_VERSION:1:2})
 
+# TODO: Remove when 3.11 images will be available for
+# windows
+# scipy
+# pandas
+if [[ "$PYTHON_VERSION" == "3.11" ]]; then
+    PYTHON_VERSION=$(echo ${PYTHON_VERSION}-rc)
+    CIBW_TEST_REQUIRES="pytest threadpoolctl"
+    python -m pip install scipy==1.10.0.dev0
+fi
+
 # Build a minimal Windows Docker image for testing the wheels
 docker build --build-arg PYTHON_VERSION=$PYTHON_VERSION \
              --build-arg WHEEL_NAME=$WHEEL_NAME \
              --build-arg CONFTEST_NAME=$CONFTEST_NAME \
              --build-arg CIBW_TEST_REQUIRES="$CIBW_TEST_REQUIRES" \
+             --build-arg PIP_EXTRA_INDEX_URL="$PIP_EXTRA_INDEX_URL" \
              -f build_tools/github/Windows \
              -t scikit-learn/minimal-windows .
