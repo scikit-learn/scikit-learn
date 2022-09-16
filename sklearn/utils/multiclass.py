98ab9a34ccd93a155f93ec04b960874a166fcabe
rprkh <r.parikh@somaiya.edu>
2022-09-16 10:13:17 +0530

    raise ValueError on invalid inputs

--- sklearn/utils/multiclass.py
+++ sklearn/utils/multiclass.py
@@ -316,7 +316,10 @@ def type_of_target(y, input_name=""):
 
     # Invalid inputs
     if y.ndim > 2 or (y.dtype == object and len(y) and not isinstance(y.flat[0], str)):
-        return "unknown"  # [[[1, 2]]] or [obj_1] and not ["label_1"]
+        raise ValueError( # [[[1, 2]]] or [obj_1] and not ["label_1"]
+            "Use an object array containing strings or encode the values in a"
+            " contiguous numerical array."
+        ) 
 
     if y.ndim == 2 and y.shape[1] == 0:
         return "unknown"  # [[]]
