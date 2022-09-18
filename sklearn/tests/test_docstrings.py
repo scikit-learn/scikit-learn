15599753b63f10748ffb374aacd37dbb37806a37
b0rxington <40011462+b0rxington@users.noreply.github.com>
2022-09-18 15:48:05 -0400

    DOC ensures sklearn.utils.sparsefuncs.csc_median_axis_0 passes numpydoc validation (#24461)

--- sklearn/tests/test_docstrings.py
+++ sklearn/tests/test_docstrings.py
@@ -36,7 +36,6 @@ FUNCTION_DOCSTRING_IGNORE_LIST = [
     "sklearn.utils.metaestimators.if_delegate_has_method",
     "sklearn.utils.multiclass.type_of_target",
     "sklearn.utils.multiclass.unique_labels",
-    "sklearn.utils.sparsefuncs.csc_median_axis_0",
     "sklearn.utils.sparsefuncs.incr_mean_variance_axis",
     "sklearn.utils.sparsefuncs.inplace_swap_row_csc",
     "sklearn.utils.sparsefuncs.inplace_swap_row_csr",
