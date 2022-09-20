c33accf668cd6e0532714b949aae28ce90f63bc5
Meekail Zain <zainmeekail@gmail.com>
2022-09-20 10:54:33 -0400

    Tightened check for pairwise tag

--- sklearn/utils/estimator_checks.py
+++ sklearn/utils/estimator_checks.py
@@ -3302,7 +3302,7 @@ def _enforce_estimator_tags_x(estimator, X):
     # Pairwise estimators only accept
     # X of shape (`n_samples`, `n_samples`)
     if _safe_tags(estimator, key="pairwise"):
-        if X.shape[0] != X.shape[1]:
+        if X.shape[0] != X.shape[1] or not np.allclose(X, X.T):
             X = X.dot(X.T)
     # Estimators with `1darray` in `X_types` tag only accept
     # X of shape (`n_samples`,)
