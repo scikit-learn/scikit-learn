f94870e3b536d8057e1b65874bc93aaff1710449
Thomas J. Fan <thomasjpfan@gmail.com>
2022-09-20 17:05:51 -0700

    REV Revert unneeded change

--- sklearn/utils/estimator_checks.py
+++ sklearn/utils/estimator_checks.py
@@ -4133,7 +4133,14 @@ def check_param_validation(name, estimator_orig):
                     continue
 
                 with raises(ValueError, match=match, err_msg=err_msg):
-                    getattr(estimator, method)(X, y)
+                    if any(
+                        X_type.endswith("labels")
+                        for X_type in _safe_tags(estimator, key="X_types")
+                    ):
+                        # The estimator is a label transformer and take only `y`
+                        getattr(estimator, method)(y)
+                    else:
+                        getattr(estimator, method)(X, y)
 
 
 def check_set_output(name, transformer_orig):
