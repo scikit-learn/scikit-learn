dcecdb022b8253bcc32d8e0164e886fbd0b81f8e
jeremiedbb <jeremiedbb@yahoo.fr>
2022-09-25 15:54:53 +0200

    check [all random seeds] test_n_samples_leaves_roots test_partial_fit test_birch_predict test_n_clusters test_sparse_X test_branching_factor test_threshold test_transform_match_across_dtypes

--- sklearn/cluster/tests/test_birch.py
+++ sklearn/cluster/tests/test_birch.py
@@ -13,9 +13,7 @@ from sklearn.datasets import make_blobs
 from sklearn.exceptions import ConvergenceWarning
 from sklearn.metrics import pairwise_distances_argmin, v_measure_score
 
-from sklearn.utils._testing import assert_almost_equal
 from sklearn.utils._testing import assert_array_equal
-from sklearn.utils._testing import assert_array_almost_equal
 from sklearn.utils._testing import assert_allclose
 
 
