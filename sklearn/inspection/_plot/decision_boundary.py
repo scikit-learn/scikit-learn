ddb7d6d4ad8d7df6420ae62cc0809b94e511423c
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2022-09-26 17:33:11 +0200

    fix

--- sklearn/inspection/_plot/decision_boundary.py
+++ sklearn/inspection/_plot/decision_boundary.py
@@ -127,6 +127,7 @@ class DecisionBoundaryDisplay:
     >>> display.ax_.scatter(
     ...     iris.data[:, 0], iris.data[:, 1], c=iris.target, edgecolor="black"
     ... )
+    <...>
     >>> plt.show()
     """
 
