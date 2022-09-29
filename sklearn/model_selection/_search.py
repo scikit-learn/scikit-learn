af3056b4c2fe12fecb7039f66845de29b705adbb
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2022-09-29 15:33:05 +0200

    MAINT use nanmin to replace nan by finite values in ranking of SearchCV

--- sklearn/model_selection/_search.py
+++ sklearn/model_selection/_search.py
@@ -968,7 +968,7 @@ class BaseSearchCV(MetaEstimatorMixin, BaseEstimator, metaclass=ABCMeta):
                 # when input is nan, scipy >= 1.10 rankdata returns nan. To
                 # keep previous behaviour nans are set to be smaller than the
                 # minimum value in the array before ranking
-                min_array_means = min(array_means) - 1
+                min_array_means = np.nanmin(array_means) - 1
                 array_means = np.nan_to_num(array_means, copy=True, nan=min_array_means)
                 rank_result = rankdata(-array_means, method="min")
                 rank_result = np.asarray(rank_result, dtype=np.int32)
