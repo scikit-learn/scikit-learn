69541a2b58c763453d26bb81c9ed49b008b72174
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2022-09-29 11:33:15 +0200

    TST check behaviour of plotting kwargs

--- sklearn/model_selection/tests/test_plot.py
+++ sklearn/model_selection/tests/test_plot.py
@@ -313,3 +313,43 @@ def test_learning_curve_display_std_display_style(pyplot, data):
     assert display.fill_between_ is None
     _, legend_label = display.ax_.get_legend_handles_labels()
     assert len(legend_label) == 1
+
+
+def test_learning_curve_display_plot_kwargs(pyplot, data):
+    """Check the behaviour of the different plotting keyword arguments: `line_kw`,
+    `fill_between_kw`, and `errorbar_kw`."""
+    X, y = data
+    estimator = DecisionTreeClassifier(random_state=0)
+
+    train_sizes = [0.3, 0.6, 0.9]
+    std_display_style = "fill_between"
+    line_kw = {"color": "red"}
+    fill_between_kw = {"color": "red", "alpha": 1.0}
+    display = LearningCurveDisplay.from_estimator(
+        estimator,
+        X,
+        y,
+        train_sizes=train_sizes,
+        std_display_style=std_display_style,
+        line_kw=line_kw,
+        fill_between_kw=fill_between_kw,
+    )
+
+    assert display.lines_[0].get_color() == "red"
+    assert_allclose(
+        display.fill_between_[0].get_facecolor(),
+        [[1.0, 0.0, 0.0, 1.0]],  # trust me, it's red
+    )
+
+    std_display_style = "errorbar"
+    errorbar_kw = {"color": "red"}
+    display = LearningCurveDisplay.from_estimator(
+        estimator,
+        X,
+        y,
+        train_sizes=train_sizes,
+        std_display_style=std_display_style,
+        errorbar_kw=errorbar_kw,
+    )
+
+    assert display.errorbar_[0].lines[0].get_color() == "red"
