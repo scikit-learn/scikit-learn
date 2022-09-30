b855864d901212b4f74217728ea453c0cb3aaa67
Christian Lorentzen <lorentzen.ch@gmail.com>
2022-09-30 14:02:50 +0200

    Update sklearn/ensemble/_hist_gradient_boosting/gradient_boosting.py
    Co-authored-by: Meekail Zain <34613774+Micky774@users.noreply.github.com>

--- sklearn/ensemble/_hist_gradient_boosting/gradient_boosting.py
+++ sklearn/ensemble/_hist_gradient_boosting/gradient_boosting.py
@@ -1176,8 +1176,8 @@ class HistGradientBoostingRegressor(RegressorMixin, BaseHistGradientBoosting):
 
         For each categorical feature, there must be at most `max_bins` unique
         categories, and each categorical value must be in [0, max_bins -1].
-        During prediction, categories encoded as a negative value is treated as
-        a missing value.
+        During prediction, categories encoded as a negative value are treated as
+        missing values.
 
         Read more in the :ref:`User Guide <categorical_support_gbdt>`.
 
