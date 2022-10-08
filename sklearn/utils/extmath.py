251744ea9a4716b8d5d834518439a7d4fa73a712
awinml <97467100+awinml@users.noreply.github.com>
2022-10-08 20:26:31 +0530

    Ensures that sklearn.utils.extmath.randomized_svd passes numpydoc validation.

--- sklearn/utils/extmath.py
+++ sklearn/utils/extmath.py
@@ -393,6 +393,7 @@ def randomized_svd(
 
     Examples
     --------
+    >>> import numpy as np
     >>> from sklearn.utils.extmath import randomized_svd
     >>> a = np.array([[1, 2, 3, 5],
     ...               [3, 4, 5, 6],
