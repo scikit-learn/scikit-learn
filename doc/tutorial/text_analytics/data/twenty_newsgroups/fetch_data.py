accd8b500683473cca4d2f1cc411bc9e7ac16233
Olivier Grisel <olivier.grisel@ensta.org>
2022-10-10 10:46:29 +0200

    Check sha256 digests of tarballs in tutorial and examples before extraction
    - 20Newsgroups/20news-bydate.tar.gz
    - reuters21578-mld/reuters21578.tar.gz
    - movie-review-data/review_polarity.tar.gz

--- doc/tutorial/text_analytics/data/twenty_newsgroups/fetch_data.py
+++ doc/tutorial/text_analytics/data/twenty_newsgroups/fetch_data.py
@@ -1,27 +1,33 @@
 """Script to download the 20 newsgroups text classification set"""
 
-import os
+from pathlib import Path
+from hashlib import sha256
 import tarfile
 from contextlib import closing
 from urllib.request import urlopen
 
-URL = ("http://people.csail.mit.edu/jrennie/"
-       "20Newsgroups/20news-bydate.tar.gz")
+URL = "http://people.csail.mit.edu/jrennie/20Newsgroups/20news-bydate.tar.gz"
 
-ARCHIVE_NAME = URL.rsplit('/', 1)[1]
-TRAIN_FOLDER = "20news-bydate-train"
-TEST_FOLDER = "20news-bydate-test"
+ARCHIVE_SHA256 = "8f1b2514ca22a5ade8fbb9cfa5727df95fa587f4c87b786e15c759fa66d95610"
+ARCHIVE_NAME = Path(URL.rsplit("/", 1)[1])
+TRAIN_FOLDER = Path("20news-bydate-train")
+TEST_FOLDER = Path("20news-bydate-test")
 
 
-if not os.path.exists(TRAIN_FOLDER) or not os.path.exists(TEST_FOLDER):
-
-    if not os.path.exists(ARCHIVE_NAME):
+if not TRAIN_FOLDER.exists() or not TEST_FOLDER.exists():
+    if not ARCHIVE_NAME.exists():
         print("Downloading dataset from %s (14 MB)" % URL)
         opener = urlopen(URL)
-        with open(ARCHIVE_NAME, 'wb') as archive:
+        with open(ARCHIVE_NAME, "wb") as archive:
             archive.write(opener.read())
 
-    print("Decompressing %s" % ARCHIVE_NAME)
-    with closing(tarfile.open(ARCHIVE_NAME, "r:gz")) as archive:
-        archive.extractall(path='.')
-    os.remove(ARCHIVE_NAME)
+    try:
+        print("Checking the integrity of the archive")
+        assert sha256(ARCHIVE_NAME.read_bytes()).hexdigest() == ARCHIVE_SHA256
+
+        print("Decompressing %s" % ARCHIVE_NAME)
+        with closing(tarfile.open(ARCHIVE_NAME, "r:gz")) as archive:
+            archive.extractall(path=".")
+
+    finally:
+        ARCHIVE_NAME.unlink()
