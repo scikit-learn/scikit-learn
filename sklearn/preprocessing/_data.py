0a4811677ee378c07a17062cedfbdcf3f9f40975
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2022-10-23 20:25:53 +0200

    DOC fix sphinx directive in  function (#24733)

--- sklearn/preprocessing/_data.py
+++ sklearn/preprocessing/_data.py
@@ -2879,7 +2879,7 @@ def quantile_transform(
         input is already a numpy array). If True, a copy of `X` is transformed,
         leaving the original `X` unchanged
 
-        ..versionchanged:: 0.23
+        .. versionchanged:: 0.23
             The default value of `copy` changed from False to True in 0.23.
 
     Returns
