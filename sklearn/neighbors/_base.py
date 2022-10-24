630e7340b0786d5fc9e029f68fbb9028b3893ad0
RudreshVeerkhare <veerkharerudresh@gmail.com>
2022-10-24 17:10:41 +0530

    modified constraints to allow p < 1

--- sklearn/neighbors/_base.py
+++ sklearn/neighbors/_base.py
@@ -382,7 +382,7 @@ class NeighborsBase(MultiOutputMixin, BaseEstimator, metaclass=ABCMeta):
         "radius": [Interval(Real, 0, None, closed="both"), None],
         "algorithm": [StrOptions({"auto", "ball_tree", "kd_tree", "brute"})],
         "leaf_size": [Interval(Integral, 1, None, closed="left")],
-        "p": [Interval(Real, 1, None, closed="both"), None],
+        "p": [Interval(Real, 0, None, closed="both"), None],
         "metric": [StrOptions(set(itertools.chain(*VALID_METRICS.values()))), callable],
         "metric_params": [dict, None],
         "n_jobs": [Integral, None],
