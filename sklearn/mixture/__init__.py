7b6561c5ad58ec6cf4ad32b23c7a749245263b2a
Emir Karamehmetoglu <emir.k@phys.au.dk>
2022-11-02 21:07:29 +0100

    update changelog & add `get_responsibilities` to mixture init.

--- sklearn/mixture/__init__.py
+++ sklearn/mixture/__init__.py
@@ -4,6 +4,7 @@ The :mod:`sklearn.mixture` module implements mixture modeling algorithms.
 
 from ._gaussian_mixture import GaussianMixture
 from ._bayesian_mixture import BayesianGaussianMixture
+from ._base import get_responsibilities
 
 
-__all__ = ["GaussianMixture", "BayesianGaussianMixture"]
+__all__ = ["GaussianMixture", "BayesianGaussianMixture", "get_responsibilities"]
