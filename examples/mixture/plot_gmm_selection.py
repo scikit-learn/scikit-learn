0e29e23ddbb788d968c257303d356fc9587751e2
ArturoAmorQ <arturo.amor-quiroz@polytechnique.edu>
2022-11-03 15:50:32 +0100

    Use np.concatenate instead of np.r_

--- examples/mixture/plot_gmm_selection.py
+++ examples/mixture/plot_gmm_selection.py
@@ -28,11 +28,10 @@ import numpy as np
 n_samples = 500
 np.random.seed(0)
 C = np.array([[0.0, -0.1], [1.7, 0.4]])
+component_1 = np.dot(np.random.randn(n_samples, 2), C)
+component_2 = 0.7 * np.random.randn(n_samples, 2) + np.array([-6, 3])
 
-X = np.r_[
-    np.dot(np.random.randn(n_samples, 2), C),
-    0.7 * np.random.randn(n_samples, 2) + np.array([-6, 3]),
-]
+X = np.concatenate([component_1, component_2])
 
 # %%
 # Model training and selection
