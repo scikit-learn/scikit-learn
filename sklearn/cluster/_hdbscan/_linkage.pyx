cc31a2a39faf1e63a0f34d633a0c6d74dcd1daff
Meekail Zain <zainmeekail@gmail.com>
2022-11-05 15:52:50 -0400

    Initial cleanup

--- sklearn/cluster/_hdbscan/_linkage.pyx
+++ sklearn/cluster/_hdbscan/_linkage.pyx
@@ -3,8 +3,9 @@
 #          Steve Astels <sastels@gmail.com>
 # License: 3-clause BSD
 
-import numpy as np
 cimport numpy as cnp
+
+import numpy as np
 import cython
 
 from libc.float cimport DBL_MAX
@@ -14,106 +15,92 @@ from ...cluster._hierarchical_fast cimport UnionFind
 from ...utils._typedefs cimport ITYPE_t, DTYPE_t
 from ...utils._typedefs import ITYPE, DTYPE
 
-cpdef cnp.ndarray[cnp.double_t, ndim=2] mst_from_distance_matrix(
-    cnp.ndarray[cnp.double_t, ndim=2] distance_matrix
+cpdef cnp.ndarray[cnp.float64_t, ndim=2] mst_from_distance_matrix(
+    cnp.ndarray[cnp.float64_t, ndim=2] distance_matrix
 ):
 
     cdef:
         cnp.ndarray[cnp.intp_t, ndim=1] node_labels
         cnp.ndarray[cnp.intp_t, ndim=1] current_labels
-        cnp.ndarray[cnp.double_t, ndim=1] current_distances
-        cnp.ndarray[cnp.double_t, ndim=1] left
-        cnp.ndarray[cnp.double_t, ndim=1] right
-        cnp.ndarray[cnp.double_t, ndim=2] result
+        cnp.ndarray[cnp.float64_t, ndim=1] current_distances, left, right
+        cnp.ndarray[cnp.float64_t, ndim=2] result
 
         cnp.ndarray label_filter
 
-        cnp.intp_t current_node
-        cnp.intp_t new_node_index
-        cnp.intp_t new_node
-        cnp.intp_t i
+        cnp.intp_t n_samples = distance_matrix.shape[0]
+        cnp.intp_t current_node, new_node_index, new_node, i
 
-    result = np.zeros((distance_matrix.shape[0] - 1, 3))
-    node_labels = np.arange(distance_matrix.shape[0], dtype=np.intp)
+    result = np.zeros((n_samples - 1, 3))
+    node_labels = np.arange(n_samples, dtype=np.intp)
     current_node = 0
-    current_distances = np.infty * np.ones(distance_matrix.shape[0])
+    current_distances = np.infty * np.ones(n_samples)
     current_labels = node_labels
-    for i in range(1, node_labels.shape[0]):
+    for i in range(1, n_samples):
         label_filter = current_labels != current_node
         current_labels = current_labels[label_filter]
         left = current_distances[label_filter]
         right = distance_matrix[current_node][current_labels]
-        current_distances = np.where(left < right, left, right)
+        current_distances = np.minimum(left, right)
 
         new_node_index = np.argmin(current_distances)
         new_node = current_labels[new_node_index]
-        result[i - 1, 0] = <double> current_node
-        result[i - 1, 1] = <double> new_node
+        result[i - 1, 0] = <cnp.float64_t> current_node
+        result[i - 1, 1] = <cnp.float64_t> new_node
         result[i - 1, 2] = current_distances[new_node_index]
         current_node = new_node
 
     return result
 
 
-cpdef cnp.ndarray[cnp.double_t, ndim=2] mst_from_data_matrix(
-    cnp.ndarray[cnp.double_t, ndim=2, mode='c'] raw_data,
-    cnp.ndarray[cnp.double_t, ndim=1, mode='c'] core_distances,
+cpdef cnp.ndarray[cnp.float64_t, ndim=2] mst_from_data_matrix(
+    cnp.ndarray[cnp.float64_t, ndim=2, mode='c'] raw_data,
+    cnp.ndarray[cnp.float64_t, ndim=1, mode='c'] core_distances,
     DistanceMetric dist_metric,
-    cnp.double_t alpha=1.0
+    cnp.float64_t alpha=1.0
 ):
 
     cdef:
-        cnp.ndarray[cnp.double_t, ndim=1] current_distances_arr
-        cnp.ndarray[cnp.double_t, ndim=1] current_sources_arr
+        cnp.ndarray[cnp.float64_t, ndim=1] current_distances_arr
+        cnp.ndarray[cnp.float64_t, ndim=1] current_sources_arr
         cnp.ndarray[cnp.int8_t, ndim=1] in_tree_arr
-        cnp.ndarray[cnp.double_t, ndim=2] result_arr
+        cnp.ndarray[cnp.float64_t, ndim=2] result_arr
 
-        cnp.double_t * current_distances
-        cnp.double_t * current_sources
-        cnp.double_t * current_core_distances
-        cnp.double_t * raw_data_ptr
+        cnp.float64_t * current_distances
+        cnp.float64_t * current_sources
+        cnp.float64_t * current_core_distances
+        cnp.float64_t * raw_data_ptr
         cnp.int8_t * in_tree
-        cnp.double_t[:, ::1] raw_data_view
-        cnp.double_t[:, ::1] result
+        cnp.float64_t[:, ::1] raw_data_view
+        cnp.float64_t[:, ::1] result
 
         cnp.ndarray label_filter
 
-        cnp.intp_t current_node
-        cnp.intp_t source_node
-        cnp.intp_t right_node
-        cnp.intp_t left_node
-        cnp.intp_t new_node
-        cnp.intp_t i
-        cnp.intp_t j
-        cnp.intp_t dim
-        cnp.intp_t num_features
+        cnp.intp_t current_node, source_node, right_node, left_node, new_node
+        cnp.intp_t i, j, n_samples, num_features
 
-        double current_node_core_distance
-        double right_value
-        double left_value
-        double core_value
-        double new_distance
+        cnp.float64_t current_node_core_distance, new_distance
+        cnp.float64_t right_value, left_value, core_value
 
-    dim = raw_data.shape[0]
+    n_samples = raw_data.shape[0]
     num_features = raw_data.shape[1]
 
-    raw_data_view = (<cnp.double_t[:raw_data.shape[0], :raw_data.shape[1]:1]> (
-        <cnp.double_t *> raw_data.data))
-    raw_data_ptr = (<cnp.double_t *> &raw_data_view[0, 0])
+    raw_data_view = (<cnp.float64_t[:n_samples, :num_features:1]> (
+        <cnp.float64_t *> raw_data.data))
+    raw_data_ptr = (<cnp.float64_t *> &raw_data_view[0, 0])
 
-    result_arr = np.zeros((dim - 1, 3))
-    in_tree_arr = np.zeros(dim, dtype=np.int8)
+    result_arr = np.zeros((n_samples - 1, 3))
+    in_tree_arr = np.zeros(n_samples, dtype=np.int8)
     current_node = 0
-    current_distances_arr = np.infty * np.ones(dim)
-    current_sources_arr = np.ones(dim)
+    current_distances_arr = np.infty * np.ones(n_samples)
+    current_sources_arr = np.ones(n_samples)
 
-    result = (<cnp.double_t[:dim - 1, :3:1]> (<cnp.double_t *> result_arr.data))
+    result = (<cnp.float64_t[:n_samples - 1, :3:1]> (<cnp.float64_t *> result_arr.data))
     in_tree = (<cnp.int8_t *> in_tree_arr.data)
-    current_distances = (<cnp.double_t *> current_distances_arr.data)
-    current_sources = (<cnp.double_t *> current_sources_arr.data)
-    current_core_distances = (<cnp.double_t *> core_distances.data)
+    current_distances = (<cnp.float64_t *> current_distances_arr.data)
+    current_sources = (<cnp.float64_t *> current_sources_arr.data)
+    current_core_distances = (<cnp.float64_t *> core_distances.data)
 
-    for i in range(1, dim):
+    for i in range(1, n_samples):
 
         in_tree[current_node] = 1
 
@@ -123,7 +110,7 @@ cpdef cnp.ndarray[cnp.double_t, ndim=2] mst_from_data_matrix(
         source_node = 0
         new_node = 0
 
-        for j in range(dim):
+        for j in range(n_samples):
             if in_tree[j]:
                 continue
 
@@ -169,26 +156,26 @@ cpdef cnp.ndarray[cnp.double_t, ndim=2] mst_from_data_matrix(
                     source_node = right_source
                     new_node = j
 
-        result[i - 1, 0] = <double> source_node
-        result[i - 1, 1] = <double> new_node
+        result[i - 1, 0] = <cnp.float64_t> source_node
+        result[i - 1, 1] = <cnp.float64_t> new_node
         result[i - 1, 2] = new_distance
         current_node = new_node
 
     return result_arr
 
 @cython.wraparound(True)
-cpdef cnp.ndarray[cnp.double_t, ndim=2] label(cnp.double_t[:,:] L):
+cpdef cnp.ndarray[cnp.float64_t, ndim=2] label(cnp.float64_t[:,:] L):
 
     cdef:
-        cnp.ndarray[cnp.double_t, ndim=2] result_arr
-        cnp.double_t[:, ::1] result
+        cnp.ndarray[cnp.float64_t, ndim=2] result_arr
+        cnp.float64_t[:, ::1] result
 
         cnp.intp_t N, a, aa, b, bb, index
-        cnp.double_t delta
+        cnp.float64_t delta
 
     result_arr = np.zeros((L.shape[0], L.shape[1] + 1))
-    result = (<cnp.double_t[:L.shape[0], :4:1]> (
-        <cnp.double_t *> result_arr.data))
+    result = (<cnp.float64_t[:L.shape[0], :4:1]> (
+        <cnp.float64_t *> result_arr.data))
     N = L.shape[0] + 1
     U = UnionFind(N)
 
