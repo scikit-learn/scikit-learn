c4782564c9f446aff20c9dad4895a0f028e211b5
Loïc Estève <loic.esteve@ymail.com>
2022-11-10 17:58:03 +0100

    work-around for float32 tests [float32] [all random seeds] test_fastica_simple

--- sklearn/decomposition/tests/test_fastica.py
+++ sklearn/decomposition/tests/test_fastica.py
@@ -84,8 +84,7 @@ def test_fastica_simple(add_noise, global_random_seed, global_dtype):
         pytest.xfail(
             "fastica instability with Ubuntu Atlas build with float32 "
             "global_dtype. For more details, see "
-            "https://github.com/scikit-learn/scikit-learn/issues/24131"
-            "#issuecomment-1208091119"
+            "https://github.com/scikit-learn/scikit-learn/issues/24131#issuecomment-1208091119"  # noqa
         )
 
     # Test the FastICA algorithm on very simple data.
