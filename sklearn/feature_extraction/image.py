6f8fa5fe66eb4274d13e9d01e7480d67c3f9dd58
Emir <emirkmo@users.noreply.github.com>
2022-11-11 12:34:34 +0100

    Apply suggestions from code review
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- sklearn/feature_extraction/image.py
+++ sklearn/feature_extraction/image.py
@@ -140,7 +140,7 @@ def _to_graph(
 
 @validate_params(
     {
-        "img": ["array-like"],  # Could it also be "sparse matrix"?
+        "img": ["array-like"],
         "mask": [None, np.ndarray],
         "return_as": [type(np.ndarray), type(sparse.spmatrix)],
         "dtype": "no_validation",  # validation delegated to numpy
