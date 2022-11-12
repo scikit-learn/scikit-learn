3d5a712a648e7b31f73a19d3d06dc8b70216fc28
choo8 <choo8@illinois.edu>
2022-11-12 01:01:44 -0800

    Removed change to OOB computation

--- sklearn/ensemble/_forest.py
+++ sklearn/ensemble/_forest.py
@@ -472,11 +472,7 @@ class BaseForest(MultiOutputMixin, BaseEnsemble, metaclass=ABCMeta):
             # Collect newly grown trees
             self.estimators_.extend(trees)
 
-        # Only perform OOB computation again if there are newly grown trees or
-        # if it was not previously calculated due to oob_score parameter being False
-        if self.oob_score and (
-            n_more_estimators > 0 or not hasattr(self, "oob_score_")
-        ):
+        if self.oob_score:
             y_type = type_of_target(y)
             if y_type in ("multiclass-multioutput", "unknown"):
                 # FIXME: we could consider to support multiclass-multioutput if
