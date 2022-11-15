c05cc216105d1e08f4304e66adfcafc14bf5ea69
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2022-11-15 10:26:11 +0100

    Update sklearn/inspection/_plot/decision_boundary.py
    Co-authored-by: Tim Head <betatim@gmail.com>

--- sklearn/inspection/_plot/decision_boundary.py
+++ sklearn/inspection/_plot/decision_boundary.py
@@ -91,7 +91,7 @@ class DecisionBoundaryDisplay:
     surface_ : matplotlib `QuadContourSet` or `QuadMesh`
         If `plot_method` is 'contour' or 'contourf', `surface_` is a
         :class:`QuadContourSet <matplotlib.contour.QuadContourSet>`. If
-        `plot_method is "pcolormesh"`, `surface_` is a
+        `plot_method` is 'pcolormesh', `surface_` is a
         :class:`QuadMesh <matplotlib.collections.QuadMesh>`.
 
     ax_ : matplotlib Axes
