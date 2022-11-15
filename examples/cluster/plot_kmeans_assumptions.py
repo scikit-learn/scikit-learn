15b27f01f6ddf087dc18913e37066d2a6a838ae7
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2022-11-15 17:08:46 +0100

    iter

--- examples/cluster/plot_kmeans_assumptions.py
+++ examples/cluster/plot_kmeans_assumptions.py
@@ -36,7 +36,9 @@ plt.title("Incorrect Number of Blobs")
 # Anisotropicly distributed data
 transformation = [[0.60834549, -0.63667341], [-0.40887718, 0.85253229]]
 X_aniso = np.dot(X, transformation)
-y_pred = KMeans(n_clusters=3, n_init=10, random_state=random_state).fit_predict(X_aniso)
+y_pred = KMeans(n_clusters=3, n_init="auto", random_state=random_state).fit_predict(
+    X_aniso
+)
 
 plt.subplot(222)
 plt.scatter(X_aniso[:, 0], X_aniso[:, 1], c=y_pred)
@@ -56,8 +58,9 @@ plt.title("Unequal Variance")
 
 # Unevenly sized blobs
 X_filtered = np.vstack((X[y == 0][:500], X[y == 1][:100], X[y == 2][:10]))
-clusterer = KMeans(n_clusters=3, n_init=10, random_state=random_state)
-y_pred = clusterer.fit_predict(X_filtered)
+y_pred = KMeans(n_clusters=3, n_init=10, random_state=random_state).fit_predict(
+    X_filtered
+)
 
 plt.subplot(224)
 plt.scatter(X_filtered[:, 0], X_filtered[:, 1], c=y_pred)
