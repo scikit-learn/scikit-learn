eca5fd0e446e3912036b8d736c6eadf1ba3ba55f
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2022-11-16 10:57:49 +0100

    TST/DOC add non-regression test + changelog

--- sklearn/datasets/_lfw.py
+++ sklearn/datasets/_lfw.py
@@ -265,8 +265,9 @@ def fetch_lfw_people(
     funneled : bool, default=True
         Download and use the funneled variant of the dataset.
 
-    resize : float, default=0.5
-        Ratio used to resize the each face picture.
+    resize : float or None, default=0.5
+        Ratio used to resize the each face picture. If `None`, no resizing is
+        performed.
 
     min_faces_per_person : int, default=None
         The extracted dataset will only retain pictures of people that have at
