b5c2027e8c225da4b2281a5f3ba521c3ed0fb5eb
krishna vamsi <krishnavamsivv58@gmail.com>
2022-11-17 17:39:26 +0530

    Update examples/manifold/plot_manifold_sphere.py
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- examples/manifold/plot_manifold_sphere.py
+++ examples/manifold/plot_manifold_sphere.py
@@ -111,7 +111,7 @@ plt.axis("tight")
 
 # Perform Multi-dimensional scaling.
 t0 = time()
-mds = manifold.MDS(2, max_iter=100, n_init=1,normalized_stress="auto")
+mds = manifold.MDS(2, max_iter=100, n_init=1, normalized_stress="auto")
 trans_data = mds.fit_transform(sphere_data).T
 t1 = time()
 print("MDS: %.2g sec" % (t1 - t0))
