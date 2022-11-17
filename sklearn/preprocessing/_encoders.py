852f74d83caf1c1f4994aedd9ef96a5d5a7c1a31
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2022-11-17 19:55:11 +0100

    DOC Add notes in OrdinalEncoder regardin Python version (#24959)
    Co-authored-by: Tim Head <betatim@gmail.com>
    closes https://github.com/scikit-learn/scikit-learn/issues/24840

--- sklearn/preprocessing/_encoders.py
+++ sklearn/preprocessing/_encoders.py
@@ -1131,6 +1131,13 @@ class OrdinalEncoder(OneToOneFeatureMixin, _BaseEncoder):
     LabelEncoder : Encodes target labels with values between 0 and
         ``n_classes-1``.
 
+    Notes
+    -----
+    With a high proportion of `nan` values, inferring categories becomes slow with
+    Python versions before 3.10. The handling of `nan` values was improved
+    from Python 3.10 onwards, (c.f.
+    `bpo-43475 <https://github.com/python/cpython/issues/87641>`_).
+
     Examples
     --------
     Given a dataset with two features, we let the encoder find the unique
