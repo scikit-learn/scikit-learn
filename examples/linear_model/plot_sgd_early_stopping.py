aedc946aad0e708b8e5dec73bfc3d090a0259ec4
Jérémie du Boisberranger <34657725+jeremiedbb@users.noreply.github.com>
2022-11-18 13:23:01 +0100

    Update examples/linear_model/plot_sgd_early_stopping.py

--- examples/linear_model/plot_sgd_early_stopping.py
+++ examples/linear_model/plot_sgd_early_stopping.py
@@ -48,7 +48,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
-from sklearn.externals._packaging.version import parse as parse_version
+from sklearn.utils.fixes import parse_version
 from sklearn import linear_model
 from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
