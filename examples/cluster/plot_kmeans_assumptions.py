03baf3086b4907222ca58fa643227ec26b60a785
ArturoAmorQ <arturo.amor-quiroz@polytechnique.edu>
2022-11-18 14:23:00 +0100

    Add co-author

--- examples/cluster/plot_kmeans_assumptions.py
+++ examples/cluster/plot_kmeans_assumptions.py
@@ -25,6 +25,7 @@ unintuitive and possibly undesirable clusters.
 """
 
 # Author: Phil Roth <mr.phil.roth@gmail.com>
+#         Arturo Amor <david-arturo.amor-quiroz@inria.fr>
 # License: BSD 3 clause
 
 # %%
