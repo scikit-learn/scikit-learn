cf0280ed2f572348a5c98755388fa3aa9b9aaf53
ArturoAmorQ <arturo.amor-quiroz@polytechnique.edu>
2022-11-21 17:40:37 +0100

    First step to improve notebook style

--- examples/preprocessing/plot_scaling_importance.py
+++ examples/preprocessing/plot_scaling_importance.py
@@ -1,106 +1,79 @@
-# -*- coding: utf-8 -*-
 """
-=========================================================
+=============================
 Importance of Feature Scaling
-=========================================================
+=============================
 
-Feature scaling through standardization (or Z-score normalization)
-can be an important preprocessing step for many machine learning
-algorithms. Standardization involves rescaling the features such
-that they have the properties of a standard normal distribution
-with a mean of zero and a standard deviation of one.
+Feature scaling through standardization (or Z-score normalization) can be an
+important preprocessing step for many machine learning algorithms.
+Standardization involves rescaling each feature such that it has a standard
+deviation of one and a mean of zero.
 
 While many algorithms (such as SVM, K-nearest neighbors, and logistic
-regression) require features to be normalized, intuitively we can
-think of Principle Component Analysis (PCA) as being a prime example
-of when normalization is important. In PCA we are interested in the
-components that maximize the variance. If one component (e.g. human
-height) varies less than another (e.g. weight) because of their
-respective scales (meters vs. kilos), PCA might determine that the
-direction of maximal variance more closely corresponds with the
-'weight' axis, if those features are not scaled. As a change in
-height of one meter can be considered much more important than the
-change in weight of one kilogram, this is clearly incorrect.
+regression) require features to be normalized, intuitively we can think of
+Principle Component Analysis (PCA) as being a prime example of when
+normalization is important. In PCA we are interested in the components that
+maximize the variance. If one component (e.g. human height) varies less than
+another (e.g. weight) because of their respective scales (meters vs. kilos), PCA
+might determine that the direction of maximal variance more closely corresponds
+with the 'weight' axis, if those features are not scaled. As a change in height
+of one meter can be considered much more important than the change in weight of
+one kilogram, this is clearly incorrect.
 
-To illustrate this, :class:`PCA <sklearn.decomposition.PCA>`
-is performed comparing the use of data with
-:class:`StandardScaler <sklearn.preprocessing.StandardScaler>` applied,
-to unscaled data. The results are visualized and a clear difference noted.
-The 1st principal component in the unscaled set can be seen. It can be seen
-that feature #13 dominates the direction, being a whole two orders of
-magnitude above the other features. This is contrasted when observing
-the principal component for the scaled version of the data. In the scaled
-version, the orders of magnitude are roughly the same across all the features.
-
-The dataset used is the Wine Dataset available at UCI. This dataset
-has continuous features that are heterogeneous in scale due to differing
-properties that they measure (i.e. alcohol content and malic acid).
-
-The transformed data is then used to train a naive Bayes classifier, and a
-clear difference in prediction accuracies is observed wherein the dataset
-which is scaled before PCA vastly outperforms the unscaled version.
+To illustrate this, in the present example we compare the principal components
+found using :class:`~klearn.decomposition.PCA` on unscaled data with those
+obatined when using a :class:`~sklearn.preprocessing.StandardScaler` to scale
+data first.
 
 """
-import matplotlib.pyplot as plt
 
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
-from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import accuracy_score
-from sklearn.datasets import load_wine
-from sklearn.pipeline import make_pipeline
-
-# Code source: Tyler Lanigan <tylerlanigan@gmail.com>
-#              Sebastian Raschka <mail@sebastianraschka.com>
+# Author: Tyler Lanigan <tylerlanigan@gmail.com>
+#         Sebastian Raschka <mail@sebastianraschka.com>
 
 # License: BSD 3 clause
 
-RANDOM_STATE = 42
-FIG_SIZE = (10, 7)
+# %%
+# Load and prepare data
+# =====================
+#
+# The dataset used is the :ref:`wine_dataset` available at UCI. This dataset has
+# continuous features that are heterogeneous in scale due to differing
+# properties that they measure (e.g. alcohol content and malic acid). We can
+# inspect the first principal components of the dataset:
 
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
 features, target = load_wine(return_X_y=True)
+scaler = StandardScaler()
 
-# Make a train/test split using 30% test size
 X_train, X_test, y_train, y_test = train_test_split(
-    features, target, test_size=0.30, random_state=RANDOM_STATE
+    features, target, test_size=0.30, random_state=42
 )
+scaled_X_train = scaler.fit_transform(X_train)
 
-# Fit to data and predict using pipelined GNB and PCA
-unscaled_clf = make_pipeline(PCA(n_components=2), GaussianNB())
-unscaled_clf.fit(X_train, y_train)
-pred_test = unscaled_clf.predict(X_test)
+pca = PCA(n_components=2).fit(X_train)
+scaled_pca = PCA(n_components=2).fit(scaled_X_train)
 
-# Fit to data and predict using pipelined scaling, GNB and PCA
-std_clf = make_pipeline(StandardScaler(), PCA(n_components=2), GaussianNB())
-std_clf.fit(X_train, y_train)
-pred_test_std = std_clf.predict(X_test)
-
-# Show prediction accuracies in scaled and unscaled data.
-print("\nPrediction accuracy for the normal test dataset with PCA")
-print(f"{accuracy_score(y_test, pred_test):.2%}\n")
-
-print("\nPrediction accuracy for the standardized test dataset with PCA")
-print(f"{accuracy_score(y_test, pred_test_std):.2%}\n")
-
-# Extract PCA from pipeline
-pca = unscaled_clf.named_steps["pca"]
-pca_std = std_clf.named_steps["pca"]
-
-# Show first principal components
 print(f"\nPC 1 without scaling:\n{pca.components_[0]}")
-print(f"\nPC 1 with scaling:\n{pca_std.components_[0]}")
+print(f"\nPC 1 with scaling:\n{scaled_pca.components_[0]}")
 
-# Use PCA without and with scale on X_train data for visualization.
+# %%
+# One can notice that feature #13 dominates the direction, being two orders of
+# magnitude above the other features. This is contrasted when observing
+# the principal component for the scaled version of the data. In the scaled
+# version, the orders of magnitude are roughly the same across all the features.
+#
+# Plot PCA components
+# ===================
+
+import matplotlib.pyplot as plt
+
 X_train_transformed = pca.transform(X_train)
+X_train_std_transformed = scaled_pca.transform(scaled_X_train)
 
-scaler = std_clf.named_steps["standardscaler"]
-scaled_X_train = scaler.transform(X_train)
-X_train_std_transformed = pca_std.transform(scaled_X_train)
-
-# visualize standardized vs. untouched dataset with PCA performed
-fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=FIG_SIZE)
+fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(10, 7))
 
 target_classes = range(0, 3)
 colors = ("blue", "red", "green")
@@ -137,3 +110,32 @@ for ax in (ax1, ax2):
 plt.tight_layout()
 
 plt.show()
+
+# %%
+# Model training and scoring
+# ==========================
+#
+# Here we show prediction accuracies in scaled and unscaled data.
+
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+from sklearn.pipeline import make_pipeline
+
+unscaled_clf = make_pipeline(pca, GaussianNB())
+unscaled_clf.fit(X_train, y_train)
+y_pred = unscaled_clf.predict(X_test)
+
+scaled_clf = make_pipeline(scaler, pca, GaussianNB())
+scaled_clf.fit(X_train, y_train)
+y_pred_scaled = scaled_clf.predict(X_test)
+
+
+print("\nPrediction accuracy for the normal test dataset with PCA")
+print(f"{accuracy_score(y_test, y_pred):.2%}\n")
+
+print("\nPrediction accuracy for the standardized test dataset with PCA")
+print(f"{accuracy_score(y_test, y_pred_scaled):.2%}\n")
+
+# %%
+# A clear difference in prediction accuracies is observed when the data is
+# scaled before PCA, as it vastly outperforms the unscaled version.
