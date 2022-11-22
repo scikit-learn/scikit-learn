f2636d4518a3a9a0e4ae63f01123a4108f13c67a
ArturoAmorQ <arturo.amor-quiroz@polytechnique.edu>
2022-11-22 18:02:50 +0100

    Add KNeighbors section and refactor narrative

--- examples/preprocessing/plot_scaling_importance.py
+++ examples/preprocessing/plot_scaling_importance.py
@@ -3,32 +3,32 @@
 Importance of Feature Scaling
 =============================
 
-Feature scaling through standardization (or Z-score normalization) can be an
-important preprocessing step for many machine learning algorithms.
-Standardization involves rescaling each feature such that it has a standard
-deviation of one and a mean of zero.
+Feature scaling through standardization (also called Z-score normalization) can
+be an important preprocessing step for many machine learning algorithms. It
+involves rescaling each feature such that it has a standard deviation equals to
+1 and a mean equals to 0.
 
-While many algorithms (such as SVM, K-nearest neighbors, and logistic
-regression) require features to be normalized, intuitively we can think of
-Principle Component Analysis (PCA) as being a prime example of when
-normalization is important. In PCA we are interested in the components that
-maximize the variance. If one component (e.g. human height) varies less than
-another (e.g. weight) because of their respective scales (meters vs. kilos), PCA
-might determine that the direction of maximal variance more closely corresponds
-with the 'weight' axis, if those features are not scaled. As a change in height
-of one meter can be considered much more important than the change in weight of
-one kilogram, this is clearly incorrect.
+Many algorithms require features to be normalized, either to ease the
+convergence (such as the logistic regression) or because fitting scaled data
+leads to a completely different model than using non-scaled data (such as
+KNeighbors models). The latter is demoed on the first part of the present
+example.
 
-To illustrate this, in the present example we compare the principal components
-found using :class:`~klearn.decomposition.PCA` on unscaled data with those
-obatined when using a :class:`~sklearn.preprocessing.StandardScaler` to scale
-data first.
+On the second part of the example we show how Principle Component Analysis (PCA)
+is impacted by normalization of features. To illustrate this, we compare the
+principal components found using :class:`~sklearn.decomposition.PCA` on unscaled
+data with those obatined when using a
+:class:`~sklearn.preprocessing.StandardScaler` to scale data first.
+
+In the last part of the example we show the effect of the normalization on the
+accuracy of a model trained on PCA-reduced data.
 
 """
 
 # Author: Tyler Lanigan <tylerlanigan@gmail.com>
 #         Sebastian Raschka <mail@sebastianraschka.com>
-
+#         Arturo Amor <david-arturo.amor-quiroz@inria.fr>
+#
 # License: BSD 3 clause
 
 # %%
@@ -37,43 +37,108 @@ data first.
 #
 # The dataset used is the :ref:`wine_dataset` available at UCI. This dataset has
 # continuous features that are heterogeneous in scale due to differing
-# properties that they measure (e.g. alcohol content and malic acid). We can
-# inspect the first principal components of the dataset:
+# properties that they measure (e.g. alcohol content and malic acid).
 
 from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
-from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
-features, target = load_wine(return_X_y=True)
+X, y = load_wine(return_X_y=True, as_frame=True)
 scaler = StandardScaler()
 
 X_train, X_test, y_train, y_test = train_test_split(
-    features, target, test_size=0.30, random_state=42
+    X, y, test_size=0.30, random_state=42
 )
 scaled_X_train = scaler.fit_transform(X_train)
 
+# %%
+# Effect of rescaling on a KNeighbors model
+# =========================================
+#
+# For the sake of visualizing the decision boundary of a KNeighbors models, in
+# this section we select a subset of 2 features that have values with different
+# orders of magnitude.
+
+import matplotlib.pyplot as plt
+from sklearn.inspection import DecisionBoundaryDisplay
+from sklearn.neighbors import KNeighborsClassifier
+
+
+X_plot = X[["proline", "hue"]].to_numpy()
+X_plot_scaled = StandardScaler().fit_transform(X_plot)
+clf = KNeighborsClassifier(n_neighbors=20)
+
+
+def fit_and_plot_model(X_plot, y, clf, ax):
+    clf.fit(X_plot, y)
+    disp = DecisionBoundaryDisplay.from_estimator(
+        clf,
+        X_plot,
+        response_method="predict",
+        alpha=0.5,
+        ax=ax,
+    )
+    disp.ax_.scatter(X_plot[:, 0], X_plot[:, 1], c=y, s=20, edgecolor="k")
+    disp.ax_.set_xlim((X_plot[:, 0].min(), X_plot[:, 0].max()))
+    disp.ax_.set_ylim((X_plot[:, 1].min(), X_plot[:, 1].max()))
+    return disp.ax_
+
+
+fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(12, 6))
+
+fit_and_plot_model(X_plot, y, clf, ax1)
+ax1.set_xlabel("proline")
+ax1.set_ylabel("hue")
+ax1.set_title("KNN w/o scaling")
+
+fit_and_plot_model(X_plot_scaled, y, clf, ax2)
+ax2.set_xlabel("scaled proline")
+ax2.set_ylabel("scaled hue")
+ax2.set_title("KNN with scaling")
+
+plt.show()
+
+# %%
+# Here the desicion boundary shows that fitting scaled or non-scaled data lead
+# to completely different KNeighbors models. The reason is that the variable
+# "proline" has values which vary between 0 and 1,000; whereas the variable
+# "hue" varies between 1 and 10. Because of this, distances between samples are
+# mostly impacted by differences in values of "proline", while values of the
+# "hue" will be comparatively ignored. If one uses
+# :class:`~sklearn.preprocessing.StandardScaler` to normalize this database,
+# both scaled values lay approximately between -3 and 3 and the neighbors
+# structure will be impacted more or less equivalently by both variables.
+#
+# Effect of rescaling on a PCA dimensional reduction
+# ==================================================
+#
+# Dimensional reduction using PCA consists of finding the features that maximize
+# the variance. If one feature varies more than another only because of their
+# respective scales, :class:`~sklearn.decomposition.PCA` would determine that
+# such feature dominates the direction of the principal components.
+#
+# We can inspect the first principal components on the full dataset:
+
+from sklearn.decomposition import PCA
+
 pca = PCA(n_components=2).fit(X_train)
 scaled_pca = PCA(n_components=2).fit(scaled_X_train)
+X_train_transformed = pca.transform(X_train)
+X_train_std_transformed = scaled_pca.transform(scaled_X_train)
 
 print(f"\nPC 1 without scaling:\n{pca.components_[0]}")
 print(f"\nPC 1 with scaling:\n{scaled_pca.components_[0]}")
 
 # %%
-# One can notice that feature #13 dominates the direction, being two orders of
-# magnitude above the other features. This is contrasted when observing
-# the principal component for the scaled version of the data. In the scaled
-# version, the orders of magnitude are roughly the same across all the features.
+# Indeed we find that the feature #13 (corresponding to the "proline" variable)
+# dominates the direction of the first principal component, being about two
+# orders of magnitude above the other features. This is contrasted when
+# observing the first principal component for the scaled version of the data,
+# where the orders of magnitude are roughly the same across all the features.
 #
-# Plot PCA components
-# ===================
+# We can visualize the distribution of the principal components in both cases:
 
-import matplotlib.pyplot as plt
-
-X_train_transformed = pca.transform(X_train)
-X_train_std_transformed = scaled_pca.transform(scaled_X_train)
-
-fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(10, 7))
+fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(10, 7))
 
 target_classes = range(0, 3)
 colors = ("blue", "red", "green")
@@ -112,23 +177,22 @@ plt.tight_layout()
 plt.show()
 
 # %%
-# Model training and scoring
-# ==========================
+# Effect of rescaling on model's accuracy
+# =======================================
 #
 # Here we show prediction accuracies in scaled and unscaled data.
 
-from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import LogisticRegression
 
-unscaled_clf = make_pipeline(pca, GaussianNB())
+unscaled_clf = make_pipeline(pca, LogisticRegression())
 unscaled_clf.fit(X_train, y_train)
 y_pred = unscaled_clf.predict(X_test)
 
-scaled_clf = make_pipeline(scaler, pca, GaussianNB())
+scaled_clf = make_pipeline(scaler, pca, LogisticRegression())
 scaled_clf.fit(X_train, y_train)
 y_pred_scaled = scaled_clf.predict(X_test)
-
 
 print("\nPrediction accuracy for the normal test dataset with PCA")
 print(f"{accuracy_score(y_test, y_pred):.2%}\n")
@@ -138,4 +202,15 @@ print(f"{accuracy_score(y_test, y_pred_scaled):.2%}\n")
 
 # %%
 # A clear difference in prediction accuracies is observed when the data is
-# scaled before PCA, as it vastly outperforms the unscaled version.
+# scaled before PCA, as it vastly outperforms the unscaled version. This
+# corresponds to the intuition obtained from the plot in the previous section,
+# where the components become linearly separable when scaling before using PCA.
+#
+# Notice that in this case the models with scaled features perform better than
+# the models with non-scaled features because all the variables are expected to
+# be predictive and we rather avoid some of them being comparatively ignored.
+
+# If the variables in lower scales were not predictive, one may experience a
+# decrease of the performance after scaling the features: noisy features would
+# contribute more to the prediction after scaling and therefore scaling would
+# increase overfitting.
