57ea13bb41c73b7722b3e35cc35e268a76cb4895
Julien Jerphanion <git@jjerphan.xyz>
2022-11-25 15:09:44 +0100

    DOC Add more details regarding the improved efficiency in 1.1 and 1.2

--- examples/release_highlights/plot_release_highlights_1_2_0.py
+++ examples/release_highlights/plot_release_highlights_1_2_0.py
@@ -120,10 +120,13 @@ X.head()
 # Improved efficiency of many estimators
 # --------------------------------------
 # In version 1.1 the efficiency of many estimators relying on the computation of
-# pairwise distances was greatly improved for float64 dense input. In version 1.2,
-# the efficiency of these estimators was further improved for all combinations of
-# float32/float64 and dense/sparse input (for all metrics except euclidean). It
-# concerns essentially clustering, manifold learning and neighbor search algorithms.
+# pairwise distances (essentially estimators related to clustering, manifold
+# learning and neighbors search algorithms) was greatly improved for float64
+# dense input. Efficiency improvement especially were a reduced memory footprint
+# a much better scalability on multi-core machines.
+# In version 1.2, the efficiency of these estimators was further improved for all
+# combinations of dense and CSR inputs on float32 and float64 datasets, except
+# the CSR-dense and dense-CSR combinations for the Euclidean and Squared Euclidean
+# Distance metrics.
 # A detailed list of the impacted estimators can be found in the
-# :ref:`changelog <changes_1_2>`. The main benefits are a reduced memory footprint
-# and a much better scalability on multi-core machines.
+# :ref:`changelog <changes_1_2>`.
