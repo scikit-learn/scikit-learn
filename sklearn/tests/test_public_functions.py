7e0ed1ec00f1e9a937eee1d89893fbd5e08ef290
tspeng <tishun.peng@gmail.com>
2022-11-30 06:09:56 -0500

    MAINT Parameters validation for sklearn.metrics.auc (#25008)

--- sklearn/tests/test_public_functions.py
+++ sklearn/tests/test_public_functions.py
@@ -11,6 +11,7 @@ from sklearn.utils._param_validation import make_constraint
 PARAM_VALIDATION_FUNCTION_LIST = [
     "sklearn.cluster.kmeans_plusplus",
     "sklearn.metrics.accuracy_score",
+    "sklearn.metrics.auc",
     "sklearn.metrics.mean_absolute_error",
     "sklearn.metrics.zero_one_loss",
     "sklearn.svm.l1_min_c",
