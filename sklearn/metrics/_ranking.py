4344c491fbaaa6ffc126ea4746cd5e6a5c994b34
Mikhail Iljin <mike@Mikhails-MacBook-Pro.local>
2022-12-06 18:47:32 +0000

    changed types in docstring, removed wrong type "sparse matrix" from validation

--- sklearn/metrics/_ranking.py
+++ sklearn/metrics/_ranking.py
@@ -906,7 +906,7 @@ def precision_recall_curve(y_true, probas_pred, *, pos_label=None, sample_weight
 
 @validate_params(
     {
-        "y_true": ["array-like", "sparse matrix"],
+        "y_true": ["array-like"],
         "y_score": ["array-like"],
         "pos_label": [Integral, str, None],
         "sample_weight": ["array-like", None],
@@ -924,11 +924,11 @@ def roc_curve(
 
     Parameters
     ----------
-    y_true : ndarray of shape (n_samples,)
+    y_true : array-like of shape (n_samples,)
         True binary labels. If labels are not either {-1, 1} or {0, 1}, then
         pos_label should be explicitly given.
 
-    y_score : ndarray of shape (n_samples,)
+    y_score : array-like of shape (n_samples,)
         Target scores, can either be probability estimates of the positive
         class, confidence values, or non-thresholded measure of decisions
         (as returned by "decision_function" on some classifiers).
