b217323c8e53314f0243586f15eeaf35f1344f14
Tim Head <betatim@gmail.com>
2022-12-14 14:29:27 +0100

    Fix tests

--- sklearn/preprocessing/tests/test_encoders.py
+++ sklearn/preprocessing/tests/test_encoders.py
@@ -1346,7 +1346,7 @@ def test_one_hot_encoder_sparse_deprecated():
 
 # deliberately omit 'OS' as an invalid combo
 @pytest.mark.parametrize(
-    "input_dtype, category_dtype", ["OO", "OU", "UO", "UU", "US", "SO", "SU", "SS"]
+    "input_dtype, category_dtype", ["OO", "OU", "UO", "UU", "SO", "SU", "SS"]
 )
 @pytest.mark.parametrize("array_type", ["list", "array", "dataframe"])
 def test_encoders_string_categories(input_dtype, category_dtype, array_type):
@@ -1973,4 +1973,4 @@ def test_predefined_categories_dtype():
     assert len(categories) == len(enc.categories_)
     for n, cat in enumerate(enc.categories_):
         assert cat.dtype == object
-        assert np.array_equal(categories[n], cat)
+        assert_array_equal(categories[n], cat)
