7a8fc6cfa48c1727d57a7648640c092c0d48cb7e
Meekail Zain <zainmeekail@gmail.com>
2022-12-14 17:19:11 -0500

    Corrected dtype of probability array

--- sklearn/neighbors/_base.py
+++ sklearn/neighbors/_base.py
@@ -475,7 +475,9 @@ class NeighborsBase(MultiOutputMixin, BaseEstimator, metaclass=ABCMeta):
 
                 check_classification_targets(y)
                 self.classes_ = []
-                self._y = np.empty(y.shape, dtype=np.int64)
+                # This is necessary since np.bincount in _classification.py
+                # fails when dealing with a 64bit array on 32bit systems.
+                self._y = np.empty(y.shape, dtype=np.intp)
                 for k in range(self._y.shape[1]):
                     classes, self._y[:, k] = np.unique(y[:, k], return_inverse=True)
                     self.classes_.append(classes)
