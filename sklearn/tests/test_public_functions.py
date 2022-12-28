5aa9b99114400b1032631b357825f066af420194
Omar Salman <omar.salman@arbisoft.com>
2022-12-28 23:35:32 +0500

    MAINT Parameters validation for decomposition.dict_learning (#24871)
    Co-authored-by: jeremiedbb <jeremiedbb@yahoo.fr>
    Co-authored-by: Jérémie du Boisberranger <34657725+jeremiedbb@users.noreply.github.com>

--- sklearn/tests/test_public_functions.py
+++ sklearn/tests/test_public_functions.py
@@ -133,11 +133,12 @@ def test_function_param_validation(func_module):
 
 
 PARAM_VALIDATION_CLASS_WRAPPER_LIST = [
-    ("sklearn.decomposition.non_negative_factorization", "sklearn.decomposition.NMF"),
     ("sklearn.cluster.affinity_propagation", "sklearn.cluster.AffinityPropagation"),
     ("sklearn.covariance.ledoit_wolf", "sklearn.covariance.LedoitWolf"),
     ("sklearn.covariance.oas", "sklearn.covariance.OAS"),
+    ("sklearn.decomposition.dict_learning", "sklearn.decomposition.DictionaryLearning"),
     ("sklearn.decomposition.fastica", "sklearn.decomposition.FastICA"),
+    ("sklearn.decomposition.non_negative_factorization", "sklearn.decomposition.NMF"),
 ]
 
 
