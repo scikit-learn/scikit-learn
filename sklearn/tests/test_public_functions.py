8256a48519b7ff0a29c46862de533cefa9ad7f48
Xiao Yuan <yuanx749@gmail.com>
2022-12-28 20:44:23 +0800

    MAINT Parameters validation for metrics.mutual_info_score (#25243)

--- sklearn/tests/test_public_functions.py
+++ sklearn/tests/test_public_functions.py
@@ -108,6 +108,7 @@ PARAM_VALIDATION_FUNCTION_LIST = [
     "sklearn.metrics.confusion_matrix",
     "sklearn.metrics.mean_absolute_error",
     "sklearn.metrics.mean_tweedie_deviance",
+    "sklearn.metrics.mutual_info_score",
     "sklearn.metrics.r2_score",
     "sklearn.metrics.roc_curve",
     "sklearn.metrics.zero_one_loss",
