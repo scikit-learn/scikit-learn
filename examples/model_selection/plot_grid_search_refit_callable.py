3b9bd3c1b64e751edb492a5d01badfb764854df0
dpys <dpisner@clairity.com>
2022-12-28 22:12:28 -0600

    feat: update refit example to use wilcoxon constraints

--- examples/model_selection/plot_grid_search_refit_callable.py
+++ examples/model_selection/plot_grid_search_refit_callable.py
@@ -10,15 +10,15 @@ components [1].
 Based on the handwritten digits dataset and a Support Vector Classifier estimator,
 the first figure shows the trade-off between cross-validated score and the number of
 PCA components, when using a 1 standard-error rule to optimize model selection with
-`GridSearchCV`. Under these constraints, the balanced case is when n_components=10 and
-accuracy=0.88, which falls into the range within 1 standard error of the best accuracy
+`GridSearchCV`. Under these constraints, the balanced case is when n_components=12 and
+accuracy=0.90, which falls into the range within 1 standard error of the best accuracy
 score.
 
 Based on the Diabetes dataset and a Gradient Boosted Regressor, the second figure
 shows the trade-off between cross-validated score and the number of tree estimators,
-when using a signed rank sum rule with an alpha-level of 0.01 to optimize model
+when using a signed rank sum rule with an alpha-level of 0.05 to optimize model
 selection with `RandomizedSearchCV`. In this case, the balanced case is when
-n_estimators=100 and R2=0.38, which is "not significantly different" from the best R2
+n_estimators=150 and R2=0.41, which is "not significantly different" from the best R2
 score, but is nevertheless a more parsimonious model.
 
 As an exploratory analysis that follows from the second example, we can observe that
@@ -86,8 +86,13 @@ plt.bar(
 )
 
 lower = np.max(test_scores)
-plt.axhline(np.max(test_scores), linestyle="--", color="y", label="Best score")
 plt.axhline(lower, linestyle="--", color=".5", label="Best score - 1 SE")
+plt.axhline(
+    np.max(grid.cv_results_["mean_test_score"]),
+    linestyle="--",
+    color="y",
+    label="Best score",
+)
 
 plt.title("Balance model complexity and cross-validated score")
 plt.xlabel("Number of PCA components used")
@@ -97,13 +102,6 @@ plt.ylim((0.8, 1.0))
 plt.legend(loc="upper left")
 
 best_index_ = np.where(grid.cv_results_["mean_test_score"] == lower)[0][0]
-
-print("The n_components selected is %d" % n_components.data[best_index_])
-print(
-    "The corresponding accuracy score is %.2f"
-    % grid.cv_results_["mean_test_score"][best_index_]
-)
-
 
 # Signed Rank - Diabetes dataset - RandomizedSearchCV - GradientBoostingRegressor -
 # Regression
@@ -131,7 +129,7 @@ grid = RandomizedSearchCV(
     cv=10,
     n_jobs=-1,
     scoring="r2",
-    refit=constrain("n_estimators", by_signed_rank(alpha=0.01)),
+    refit=constrain("n_estimators", by_signed_rank(alpha=0.05)),
 )
 grid.fit(X_train, y_train)
 
@@ -140,6 +138,7 @@ test_scores = grid.cv_results_["mean_test_score"][
     ~grid.cv_results_["param_n_estimators"].mask
 ]
 
+plt.figure()
 plt.bar(
     params["n_estimators"],
     grid.cv_results_["mean_test_score"],
@@ -171,30 +170,23 @@ plt.legend(loc="upper left")
 
 best_index_ = np.where(grid.cv_results_["mean_test_score"] == lower)[0][0]
 
-print("The n_estimators selected is %d" % n_estimators.data[best_index_])
-print(
-    "The corresponding accuracy score is %.2f"
-    % grid.cv_results_["mean_test_score"][best_index_]
-)
-plt.show()
-
 # Show the effect on train-test deviance of using `Razors.simplify`
-best_estimator_razor = grid.estimator
-best_estimator_razor.n_estimators = params["n_estimators"][best_index_]
-best_estimator_razor.fit(X_train, y_train)
+best_estimator_refitted = grid.estimator
+best_estimator_refitted.n_estimators = params["n_estimators"][best_index_]
+best_estimator_refitted.fit(X_train, y_train)
 
 # Compute test set deviance for razored and non-razored models
 # razored model
 test_score = np.zeros((params["n_estimators"][best_index_],), dtype=np.float64)
-for i, y_pred in enumerate(best_estimator_razor.staged_predict(X_test)):
+for i, y_pred in enumerate(best_estimator_refitted.staged_predict(X_test)):
     test_score[i] = mean_squared_error(y_test, y_pred)
 
 fig = plt.figure(figsize=(6, 6))
 plt.subplot(1, 1, 1)
-plt.title("Deviance of Razored Model")
+plt.title("Deviance of Simpler Model")
 plt.plot(
     np.arange(params["n_estimators"][best_index_]) + 1,
-    best_estimator_razor.train_score_,
+    best_estimator_refitted.train_score_,
     "b-",
     label="Training Set Deviance",
 )
@@ -208,7 +200,6 @@ plt.legend(loc="upper right")
 plt.xlabel("Boosting Iterations")
 plt.ylabel("Deviance")
 fig.tight_layout()
-plt.show()
 
 best_estimator = grid.best_estimator_
 best_estimator.fit(X_train, y_train)
@@ -220,7 +211,7 @@ for i, y_pred in enumerate(best_estimator.staged_predict(X_test)):
 
 fig = plt.figure(figsize=(6, 6))
 plt.subplot(1, 1, 1)
-plt.title("Deviance of Non-Razored Model")
+plt.title("Deviance of Complex Model")
 plt.plot(
     np.arange(best_estimator.n_estimators) + 1,
     best_estimator.train_score_,
@@ -237,4 +228,5 @@ plt.legend(loc="upper right")
 plt.xlabel("Boosting Iterations")
 plt.ylabel("Deviance")
 fig.tight_layout()
+
 plt.show()
