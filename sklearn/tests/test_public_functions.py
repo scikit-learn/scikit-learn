f6414ea771b030b6796d93da71ec546f9ad72ae6
admdemiraj <admdemiraj@langaware.com>
2022-12-30 18:14:21 +0200

    Added parameter validation to mean_squared_error function

--- sklearn/tests/test_public_functions.py
+++ sklearn/tests/test_public_functions.py
@@ -117,6 +117,7 @@ PARAM_VALIDATION_FUNCTION_LIST = [
     "sklearn.metrics.zero_one_loss",
     "sklearn.model_selection.train_test_split",
     "sklearn.svm.l1_min_c",
+    "sklearn.metrics.mean_squared_error",
 ]
 
 
