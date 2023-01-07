880d82374018e82596272b98de62917c876904f4
Zach Shu <zhongzheng.shu@ibm.com>
2023-01-07 16:48:52 -0500

    test: add unit test for sample weight

--- sklearn/neural_network/_base.py
+++ sklearn/neural_network/_base.py
@@ -164,7 +164,8 @@ def squared_loss(y_true, y_pred, sample_weight=None):
     y_pred : array-like or label indicator matrix
         Predicted values, as returned by a regression estimator.
 
-    sample_weight: array-like of shape (n_samples,) sample weights.
+    sample_weight: array-like of shape (n_samples,)
+        sample weights.
 
     Returns
     -------
@@ -191,7 +192,8 @@ def log_loss(y_true, y_prob, sample_weight=None):
         Predicted probabilities, as returned by a classifier's
         predict_proba method.
 
-    sample_weight: array-like of shape (n_samples,) sample weights.
+    sample_weight: array-like of shape (n_samples,)
+        sample weights.
 
     Returns
     -------
@@ -229,7 +231,8 @@ def binary_log_loss(y_true, y_prob, sample_weight=None):
         Predicted probabilities, as returned by a classifier's
         predict_proba method.
 
-    sample_weight: array-like of shape (n_samples,) sample weights.
+    sample_weight: array-like of shape (n_samples,)
+        sample weights.
 
     Returns
     -------
