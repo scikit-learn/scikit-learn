89698eabdf40f08244c62508af55a2b420a0ffec
Tim Head <betatim@gmail.com>
2023-01-12 16:23:17 +0100

    Protect test from missing pandas
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- sklearn/manifold/tests/test_t_sne.py
+++ sklearn/manifold/tests/test_t_sne.py
@@ -1199,6 +1199,7 @@ def test_tsne_works_with_pandas_output():
 
     Non-regression test for gh-25365.
     """
+    pytest.importorskip("pandas")
     with config_context(transform_output="pandas"):
         arr = np.arange(35 * 4).reshape(35, 4)
         TSNE(n_components=2).fit_transform(arr)
