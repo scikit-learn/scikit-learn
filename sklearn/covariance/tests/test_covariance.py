22beec4e52a7ccc36d3e311aa49c7c38943c65cb
qbarthelemy <q.barthelemy@gmail.com>
2023-01-17 20:26:52 +0100

    split tests and parametrize

--- sklearn/covariance/tests/test_covariance.py
+++ sklearn/covariance/tests/test_covariance.py
@@ -81,20 +81,24 @@ def test_covariance():
     assert_array_equal(cov.location_, np.zeros(X.shape[1]))
 
 
-def test_shrunk_covariance():
-    # Test shrunk_covariance on a simple 2x2 matrix
+@pytest.mark.parametrize("n_matrices", [1, 3])
+def test_shrunk_covariance_func(n_matrices):
+    """Check `shrunk_covariance` function."""
+
     n_features = 2
     cov = np.ones((n_features, n_features))
-    cov_shrunk = shrunk_covariance(cov, 0.5)
     cov_target = np.array([[1, 0.5], [0.5, 1]])
+
+    if n_matrices > 1:
+        cov = np.repeat(cov[np.newaxis, ...], n_matrices, axis=0)
+        cov_target = np.repeat(cov_target[np.newaxis, ...], n_matrices, axis=0)
+
+    cov_shrunk = shrunk_covariance(cov, 0.5)
     assert_array_equal(cov_shrunk, cov_target)
 
-    # Test shrunk_covariance on a 3d array, repeating the previous matrix
-    n_matrices = 3
-    covs = np.repeat(cov[np.newaxis, ...], n_matrices, axis=0)
-    covs_shrunk = shrunk_covariance(covs, 0.5)
-    covs_target = np.repeat(cov_target[np.newaxis, ...], n_matrices, axis=0)
-    assert_array_equal(covs_shrunk, covs_target)
+
+def test_shrunk_covariance():
+    """Check consistency between `ShrunkCovariance` and `shrunk_covariance`."""
 
     # Tests ShrunkCovariance module on a simple dataset.
     # compare shrunk covariance obtained from data and from MLE estimate
