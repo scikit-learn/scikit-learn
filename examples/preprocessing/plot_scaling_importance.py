de3dad1854e6277f9a4a562ab3aed3c54587f070
Arturo Amor <86408019+ArturoAmorQ@users.noreply.github.com>
2023-01-19 14:33:24 +0100

    Apply suggestions from code review
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- examples/preprocessing/plot_scaling_importance.py
+++ examples/preprocessing/plot_scaling_importance.py
@@ -212,8 +212,7 @@ unscaled_clf.fit(X_train, y_train)
 scaled_clf = make_pipeline(scaler, pca, LogisticRegressionCV(Cs=Cs))
 scaled_clf.fit(X_train, y_train)
 
-print(f"Optimal C for the unscaled PCA: {unscaled_clf[-1].C_[0]:.4f}")
-print()
+print(f"Optimal C for the unscaled PCA: {unscaled_clf[-1].C_[0]:.4f}\n")
 print(f"Optimal C for the standardized data with PCA: {scaled_clf[-1].C_[0]:.2f}")
 
 # %%
@@ -230,14 +229,11 @@ y_proba = unscaled_clf.predict_proba(X_test)
 y_proba_scaled = scaled_clf.predict_proba(X_test)
 
 print("Test accuracy for the unscaled PCA")
-print(f"{accuracy_score(y_test, y_pred):.2%}")
-print()
+print(f"{accuracy_score(y_test, y_pred):.2%}\n")
 print("Test accuracy for the standardized data with PCA")
-print(f"{accuracy_score(y_test, y_pred_scaled):.2%}")
-print()
+print(f"{accuracy_score(y_test, y_pred_scaled):.2%}\n")
 print("Log-loss for the unscaled PCA")
-print(f"{log_loss(y_test, y_proba):.3}")
-print()
+print(f"{log_loss(y_test, y_proba):.3}\n")
 print("Log-loss for the standardized data with PCA")
 print(f"{log_loss(y_test, y_proba_scaled):.3}")
 
