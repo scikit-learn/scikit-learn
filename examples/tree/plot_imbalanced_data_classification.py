0e11ce1a9c801bf3adbe1818ee1acdedcfb5af3f
EvgeniDubov <Evgeni.Dubov@intel.com>
2023-01-22 13:46:38 +0200

    capitalized comments plot_imbalanced_data_classification

--- examples/tree/plot_imbalanced_data_classification.py
+++ examples/tree/plot_imbalanced_data_classification.py
@@ -11,13 +11,13 @@ Hellinger distance is sensitive to the ratio between the classes.
 
 """
 
-# import the necessary modules and libraries
+# Import the necessary modules and libraries
 import numpy as np
 from sklearn import datasets
 from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
 
-# create imbalanced dataset
+# Create imbalanced dataset
 minority_class_ratio = 0.001
 n_classes = 2
 X, y = datasets.make_classification(
@@ -33,41 +33,41 @@ X, y = datasets.make_classification(
     random_state=0,
 )
 
-# criteria to compare
+# Criteria to compare
 criterions = ["gini", "entropy", "hellinger"]
 
-# plot parameters
+# Plot parameters
 fig = plt.figure(figsize=(10, 6))
 max_subplots = len(criterions) * 2 - 1
 plot_colors = ["darkgrey", "yellow"]
 target_names = ["majority", "minority"]
 markers = [".", "o"]
 
-# create mesh grid on feature space to draw classifier predictions
+# Create mesh grid on feature space to draw classifier predictions
 x_min, x_max = X[:, 0].min() - 0.2, X[:, 0].max() + 0.2
 y_min, y_max = X[:, 1].min() - 2, X[:, 1].max() + 0.5
 xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1), np.arange(y_min, y_max, 0.01))
 
-# create plot per criterion
+# Create plot per criterion
 for sub_plot_idx, criterion in enumerate(criterions):
 
-    # add subplot to figure
+    # Add subplot to figure
     fig.add_subplot(1, max_subplots, sub_plot_idx * 2 + 1)
 
-    # train classifier
+    # Train classifier
     clf = DecisionTreeClassifier(criterion=criterion)
     clf.fit(X, y)
 
-    # draw tree classifier prediction probability for minority class on feature space
+    # Draw tree classifier prediction probability for minority class on feature space
     Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1].reshape(xx.shape)
     plt.contourf(xx, yy, Z, cmap=plt.cm.rainbow)
     plt.colorbar(label="minority class prediction probability")
 
-    # draw axis labels
+    # Draw axis labels
     plt.xlabel("feature_1")
     plt.ylabel("feature_2")
 
-    # draw training points
+    # Draw training points
     for i, color, marker in zip(range(n_classes), plot_colors, markers):
         idx = np.where(y == i)
         plt.scatter(
@@ -80,7 +80,7 @@ for sub_plot_idx, criterion in enumerate(criterions):
             edgecolor="black",
         )
 
-    # draw subplot legend and title
+    # Draw subplot legend and title
     plt.legend(
         title="classes",
         handletextpad=0,
