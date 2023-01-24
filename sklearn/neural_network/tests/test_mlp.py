129466f23d353aa6dd56ed51fa29a19e1b0ccbc9
Zach Shu <zhongzheng.shu@ibm.com>
2023-01-23 22:23:44 -0500

    test: add an additional test for same as check_class_weight_classifiers

--- sklearn/neural_network/tests/test_mlp.py
+++ sklearn/neural_network/tests/test_mlp.py
@@ -19,7 +19,8 @@ from numpy.testing import (
     assert_allclose,
 )
 
-from sklearn.datasets import load_digits, load_iris
+from sklearn import clone
+from sklearn.datasets import load_digits, load_iris, make_blobs
 from sklearn.datasets import make_regression, make_multilabel_classification
 from sklearn.exceptions import ConvergenceWarning
 from io import StringIO
@@ -30,8 +31,9 @@ from sklearn.neural_network import MLPRegressor
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.preprocessing import MinMaxScaler, scale
 from scipy.sparse import csr_matrix
-from sklearn.utils._testing import ignore_warnings
 
+from sklearn.utils._tags import _safe_tags
+from sklearn.utils._testing import ignore_warnings, set_random_state
 
 ACTIVATION_TYPES = ["identity", "logistic", "tanh", "relu"]
 
@@ -987,3 +989,44 @@ def test_mlp_classifier_with_sample_and_class_weights(weighted_class):
 
     # Test that class_weight and sample_weight have the same effect
     assert (class_weighted_score != sample_weighted_score).sum() == 0
+
+
+def check_class_weight_same_as_in_estimator_checks():
+    """Conduct the same test as check_class_weight_classifiers in estimator_checks.py"""
+    classifier_orig = MLPClassifier()
+    if _safe_tags(classifier_orig, key="binary_only"):
+        problems = [2]
+    else:
+        problems = [2, 3]
+
+    for n_centers in problems:
+        # create a very noisy dataset
+        X, y = make_blobs(centers=n_centers, random_state=0, cluster_std=20)
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.5, random_state=0
+        )
+
+        n_centers = len(np.unique(y_train))
+
+        if n_centers == 2:
+            class_weight = {0: 1000, 1: 0.0001}
+        else:
+            class_weight = {0: 1000, 1: 0.0001, 2: 0.0001}
+
+        classifier = clone(classifier_orig)
+        if hasattr(classifier, "n_iter"):
+            classifier.set_params(n_iter=100)
+        if hasattr(classifier, "max_iter"):
+            classifier.set_params(max_iter=1000)
+        if hasattr(classifier, "min_weight_fraction_leaf"):
+            classifier.set_params(min_weight_fraction_leaf=0.01)
+        if hasattr(classifier, "n_iter_no_change"):
+            classifier.set_params(n_iter_no_change=20)
+
+        set_random_state(classifier)
+        classifier.fit(X_train, y_train, class_weight=class_weight)
+        y_pred = classifier.predict(X_test)
+        # XXX: Generally can use 0.89 here. On Windows, LinearSVC gets
+        #      0.88 (Issue #9111)
+        if not _safe_tags(classifier_orig, key="poor_score"):
+            assert np.mean(y_pred == 0) > 0.87
