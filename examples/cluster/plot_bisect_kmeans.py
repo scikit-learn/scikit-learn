cb8f20d8b3234011601473b8a0da9fc6511e857f
Gael Varoquaux <gael.varoquaux@normalesup.org>
2023-01-27 11:33:51 +0100

    Update examples/cluster/plot_bisect_kmeans.py
    Co-authored-by: Arturo Amor <86408019+ArturoAmorQ@users.noreply.github.com>

--- examples/cluster/plot_bisect_kmeans.py
+++ examples/cluster/plot_bisect_kmeans.py
@@ -6,9 +6,9 @@ Bisecting K-Means and Regular K-Means Performance Comparison
 This example shows differences between Regular K-Means algorithm and Bisecting K-Means.
 
 While K-Means clusterings are different when with increasing n_clusters,
-Bisecting K-Means clustering builds on top of the previous ones. As a
-result, it tends to create clusters that have a more consistent
-large-scale structure across the clusters.
+Bisecting K-Means clustering builds on top of the previous ones. As a result, it
+tends to create clusters that have a more regular large-scale structure. This
+difference can be visually observed: for all numbers of clusters, there is a
 
 This difference can visually be observed: for all numbers of clusters,
 there is a dividing line cutting the overall data cloud in two for
