21f2d3aae8e67e023f4434dae39e7ef730e85e42
jona.sassenhagen <jona.sassenhagen@datarobot.com>
2023-01-30 22:12:08 +0100

    fix test

--- sklearn/metrics/tests/test_pairwise.py
+++ sklearn/metrics/tests/test_pairwise.py
@@ -117,7 +117,8 @@ def test_pairwise_distances(global_dtype):
     Y = rng.randn(5, 3)
     with pytest.raises(
             ValueError,
-            match=r'For paired distances and the metric `haversine`, X and Y should both be of shape (n_samples, 2), but X.shape is (5, 3) and Y.shape is (5, 3)'
+            r"For paired distances and the metric `haversine`, X and Y should both be"
+            r" of shape \(n_samples, 2\), but X.shape is \(5, 3\) and Y.shape is \(5, 3\)"
     ):
         paired_haversine_distances(X, Y)
 
