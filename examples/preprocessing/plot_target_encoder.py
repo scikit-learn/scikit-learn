a7cc80fa097a29c994589ab7ccde8037fdf03abe
Thomas J. Fan <thomasjpfan@gmail.com>
2023-02-02 11:40:52 -0500

    DOC Address comments

--- examples/preprocessing/plot_target_encoder.py
+++ examples/preprocessing/plot_target_encoder.py
@@ -70,7 +70,7 @@ categorical_preprocessors = [
     ("ordinal", OrdinalEncoder(handle_unknown="use_encoded_value", unknown_value=-1)),
     (
         "one_hot",
-        OneHotEncoder(handle_unknown="ignore", max_categories=10, sparse_output=False),
+        OneHotEncoder(handle_unknown="ignore", max_categories=20, sparse_output=False),
     ),
     ("target", TargetEncoder(target_type="continuous")),
 ]
@@ -78,12 +78,36 @@ categorical_preprocessors = [
 # %%
 # Next, we evaluate the models using cross validation and record the results:
 from sklearn.pipeline import make_pipeline
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_validate
 from sklearn.ensemble import HistGradientBoostingRegressor
 
-results = []
 n_cv_folds = 3
 max_iter = 20
+results = []
+
+
+def evaluate_model_and_store(name, pipe):
+    result = cross_validate(
+        pipe,
+        X,
+        y,
+        scoring="neg_root_mean_squared_error",
+        cv=n_cv_folds,
+        return_train_score=True,
+    )
+    rmse_test_score = -result["test_score"]
+    rmse_train_score = -result["train_score"]
+    results.append(
+        {
+            "preprocessor": name,
+            "rmse_test_mean": rmse_test_score.mean(),
+            "rmse_test_std": rmse_train_score.std(),
+            "rmse_train_mean": rmse_train_score.mean(),
+            "rmse_train_std": rmse_train_score.std(),
+        }
+    )
+
+
 for name, categorical_preprocessor in categorical_preprocessors:
     preprocessor = ColumnTransformer(
         [
@@ -94,16 +118,8 @@ for name, categorical_preprocessor in categorical_preprocessors:
     pipe = make_pipeline(
         preprocessor, HistGradientBoostingRegressor(random_state=0, max_iter=max_iter)
     )
-    rmse_scores = -cross_val_score(
-        pipe, X, y, scoring="neg_root_mean_squared_error", cv=n_cv_folds
-    )
-    results.append(
-        {
-            "preprocessor": name,
-            "rmse_mean": rmse_scores.mean(),
-            "rmse_std": rmse_scores.std(),
-        }
-    )
+    evaluate_model_and_store(name, pipe)
+
 
 # %%
 # Native Categorical Feature Support
@@ -113,6 +129,7 @@ for name, categorical_preprocessor in categorical_preprocessors:
 # which only supports up to 255 unique categories. In our dataset, the most of
 # the categorical features have more than 255 unique categories:
 n_unique_categories = df[categorical_features].nunique().sort_values(ascending=False)
+n_unique_categories
 
 # %%
 # To workaround the limitation above, we group the categorical features into
@@ -151,47 +168,57 @@ mixed_pipe
 
 # %%
 # Finally, we evaluate the pipeline using cross validation and record the results:
-rmse_scores = -cross_val_score(
-    mixed_pipe, X, y, scoring="neg_root_mean_squared_error", cv=n_cv_folds
-)
-results.append(
-    {
-        "preprocessor": "mixed_target_encoder",
-        "rmse_mean": rmse_scores.mean(),
-        "rmse_std": rmse_scores.std(),
-    }
-)
-
+evaluate_model_and_store("mixed_target", mixed_pipe)
 
 # %%
 # Plotting the Results
 # --------------------
 # In this section, we display the results for all the encoders. When evaluating the
 # predictive performance on the test set, dropping the categories perform the
-# worst and the target encoder performs the best. The target encoding provides
-# more information about the target, which the gradient boosted model at the end
-# of the pipeline can take advantage of.
+# worst and the target encoders performs the best.
+# - Dropping the categorical features makes the pipeline less expressive and
+#   underfitting as a result;
+# - Due to the high cardinality and to reduce the training time, the one-hot
+#   encoding scheme uses `max_categories=20` which prevents the features from
+#   expanding too much, which can result in underfitting.
+# - The one-hot encoding scheme makes the pipeline overfitting as the larger
+#   number of features explodes with rare category occurrences that are correlated
+#   with the target by chance (on the training set only);
+# - The ordinal encoding imposes an arbitrary order to the features which are then
+#   treated as numerical values by the
+#   :class:`~sklearn.ensemble.HistGradientBoostingRegressor`. Since this
+#   model groups numerical features in 256 bins per feature, many unrelated categories
+#   can be grouped together and as a result overall pipeline can underfit;
+# - When using the target encoder, the same binning happens, but since the encoded
+#   values are statistically ordered by marginal association with the target variable,
+#   the binning use by the :class:`~sklearn.ensemble.HistGradientBoostingRegressor`
+#   makes sense and leads to good results: the combination of smoothed target
+#   encoding and binning works as a good regularizing strategy against overfitting while
+#   not limiting the expressiveness of the pipeline too much.
+
 import matplotlib.pyplot as plt
 import pandas as pd
 
 results_df = (
-    pd.DataFrame(results)
-    .set_index("preprocessor")
-    .sort_values("rmse_mean", ascending=False)
+    pd.DataFrame(results).set_index("preprocessor").sort_values("rmse_test_mean")
 )
 
-fig, ax = plt.subplots()
-yticks = range(len(results_df))
-ax.barh(
-    y=yticks,
-    width=results_df["rmse_mean"],
-    xerr=results_df["rmse_std"],
-    height=0.9,
-    color=["C0", "C1", "C2", "C3", "C4"],
+fig, (ax1, ax2) = plt.subplots(
+    1, 2, figsize=(12, 8), sharey=True, constrained_layout=True
 )
-ax.set(
-    xlabel="Root Mean Squared Error",
-    ylabel="Model",
-    yticks=yticks,
-    yticklabels=results_df.index,
-)
+xticks = range(len(results_df))
+
+for subset, ax in zip(["test", "train"], [ax1, ax2]):
+    ax.bar(
+        x=xticks,
+        height=results_df[f"rmse_{subset}_mean"],
+        yerr=results_df[f"rmse_{subset}_std"],
+        width=0.9,
+        color=["C0", "C1", "C2", "C3", "C4"],
+    )
+    ax.set(
+        title=f"RMSE ({subset.title()} Scores)",
+        xlabel="Encoding Scheme",
+        xticks=xticks,
+        xticklabels=results_df.index,
+    )
