86d357ec83f9bccee4702bb0745ff64a8e1c0ef0
Thomas J. Fan <thomasjpfan@gmail.com>
2023-02-02 11:08:19 -0500

    DOC Adds mixed encoder for high and low cardinality

--- examples/preprocessing/plot_target_encoder.py
+++ examples/preprocessing/plot_target_encoder.py
@@ -54,17 +54,6 @@ y = df[target_name]
 _ = y.hist()
 
 # %%
-# Then, we split the dataset into a training and test set.
-from sklearn.model_selection import train_test_split
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-
-print(
-    f"Data points in training set: {len(X_train)}\n"
-    f"Data points in test set: {len(X_test)}"
-)
-
-# %%
 # Training and Evaluating Pipelines with Different Encoders
 # =========================================================
 # In this section, we will evaluate pipelines with
@@ -81,19 +70,20 @@ categorical_preprocessors = [
     ("ordinal", OrdinalEncoder(handle_unknown="use_encoded_value", unknown_value=-1)),
     (
         "one_hot",
-        OneHotEncoder(handle_unknown="ignore", max_categories=50, sparse_output=False),
+        OneHotEncoder(handle_unknown="ignore", max_categories=10, sparse_output=False),
     ),
     ("target", TargetEncoder(target_type="continuous")),
 ]
 
 # %%
-# Next, we create and fit the pipelines to the training data and evaluate them on the
-# test set:
+# Next, we evaluate the models using cross validation and record the results:
 from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import HistGradientBoostingRegressor
-from sklearn.metrics import mean_squared_error
 
 results = []
+n_cv_folds = 3
+max_iter = 20
 for name, categorical_preprocessor in categorical_preprocessors:
     preprocessor = ColumnTransformer(
         [
@@ -101,26 +91,107 @@ for name, categorical_preprocessor in categorical_preprocessors:
             ("categorical", categorical_preprocessor, categorical_features),
         ]
     )
-    pipe = make_pipeline(preprocessor, HistGradientBoostingRegressor(random_state=0))
-    pipe.fit(X_train, y_train)
-
-    y_pred = pipe.predict(X_test)
-    rmse = mean_squared_error(y_test, y_pred, squared=False)
-    results.append({"preprocessor": name, "root mean squared error": rmse})
+    pipe = make_pipeline(
+        preprocessor, HistGradientBoostingRegressor(random_state=0, max_iter=max_iter)
+    )
+    rmse_scores = -cross_val_score(
+        pipe, X, y, scoring="neg_root_mean_squared_error", cv=n_cv_folds
+    )
+    results.append(
+        {
+            "preprocessor": name,
+            "rmse_mean": rmse_scores.mean(),
+            "rmse_std": rmse_scores.std(),
+        }
+    )
 
 # %%
-# Finally, we display the results for all the encoders. When evaluating the
+# Native Categorical Feature Support
+# ----------------------------------
+# In this section, we build and evaluate a pipeline that uses native categorical
+# feature support in :class:`~sklearn.ensemble.HistGradientBoostingRegressor`,
+# which only supports up to 255 unique categories. In our dataset, the most of
+# the categorical features have more than 255 unique categories:
+n_unique_categories = df[categorical_features].nunique().sort_values(ascending=False)
+
+# %%
+# To workaround the limitation above, we group the categorical features into
+# low cardinality and high cardinality features. The high cardinality features
+# will be target encoded and the low cardinality features will use the native
+# categorical feature in gradient boosting.
+high_cardinality_features = n_unique_categories[n_unique_categories > 255].index
+low_cardinality_features = n_unique_categories[n_unique_categories <= 255].index
+mixed_encoded_preprocessor = ColumnTransformer(
+    [
+        ("numerical", "passthrough", numerical_features),
+        (
+            "high_cardinality",
+            TargetEncoder(target_type="continuous"),
+            high_cardinality_features,
+        ),
+        (
+            "low_cardinality",
+            OrdinalEncoder(handle_unknown="use_encoded_value", unknown_value=-1),
+            low_cardinality_features,
+        ),
+    ],
+    verbose_feature_names_out=False,
+)
+
+# The output of the of the preprocessor must be set to pandas so the
+# gradient boosting model can detect the low cardinality features.
+mixed_encoded_preprocessor.set_output(transform="pandas")
+mixed_pipe = make_pipeline(
+    mixed_encoded_preprocessor,
+    HistGradientBoostingRegressor(
+        random_state=0, max_iter=max_iter, categorical_features=low_cardinality_features
+    ),
+)
+mixed_pipe
+
+# %%
+# Finally, we evaluate the pipeline using cross validation and record the results:
+rmse_scores = -cross_val_score(
+    mixed_pipe, X, y, scoring="neg_root_mean_squared_error", cv=n_cv_folds
+)
+results.append(
+    {
+        "preprocessor": "mixed_target_encoder",
+        "rmse_mean": rmse_scores.mean(),
+        "rmse_std": rmse_scores.std(),
+    }
+)
+
+
+# %%
+# Plotting the Results
+# --------------------
+# In this section, we display the results for all the encoders. When evaluating the
 # predictive performance on the test set, dropping the categories perform the
 # worst and the target encoder performs the best. The target encoding provides
 # more information about the target, which the gradient boosted model at the end
-# of the pipeline can take advantage of. In this example, we evaluate with a single
-# test split to reduce the runtime. We recommend using cross-validation to check
-# the significance of the improvement.
+# of the pipeline can take advantage of.
+import matplotlib.pyplot as plt
 import pandas as pd
 
-_ = (
+results_df = (
     pd.DataFrame(results)
-    .sort_values("root mean squared error", ascending=False)
     .set_index("preprocessor")
-    .plot(kind="barh")
+    .sort_values("rmse_mean", ascending=False)
+)
+
+fig, ax = plt.subplots()
+yticks = range(len(results_df))
+ax.barh(
+    y=yticks,
+    width=results_df["rmse_mean"],
+    xerr=results_df["rmse_std"],
+    height=0.9,
+    color=["C0", "C1", "C2", "C3", "C4"],
+)
+ax.set(
+    xlabel="Root Mean Squared Error",
+    ylabel="Model",
+    yticks=yticks,
+    yticklabels=results_df.index,
 )
