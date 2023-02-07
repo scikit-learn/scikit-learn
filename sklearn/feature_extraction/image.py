ba16dbe72b3edfc40b8a82deb132d8dede5904cf
ouss1508 <121971998+ouss1508@users.noreply.github.com>
2023-02-07 14:44:06 +0100

    MAINT Parameters validation for reconstruct_from_patches_2d (#25384)
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- sklearn/feature_extraction/image.py
+++ sklearn/feature_extraction/image.py
@@ -16,7 +16,7 @@ from scipy import sparse
 from numpy.lib.stride_tricks import as_strided
 
 from ..utils import check_array, check_random_state
-from ..utils._param_validation import Interval, validate_params
+from ..utils._param_validation import Hidden, Interval, validate_params
 from ..base import BaseEstimator
 
 __all__ = [
@@ -447,6 +447,7 @@ def extract_patches_2d(image, patch_size, *, max_patches=None, random_state=None
         return patches
 
 
+@validate_params({"patches": [np.ndarray], "image_size": [tuple, Hidden(list)]})
 def reconstruct_from_patches_2d(patches, image_size):
     """Reconstruct the image from all of its patches.
 
