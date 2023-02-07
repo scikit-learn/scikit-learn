21cf642c03e87f4c688558ae53359b713a29b9e0
OmarManzoor <omar.salman@arbisoft.com>
2023-02-07 17:26:05 +0500

    Add TODO to remove redundant y check in _check_input

--- sklearn/tree/_tree.pyx
+++ sklearn/tree/_tree.pyx
@@ -109,6 +109,9 @@ cdef class TreeBuilder:
             # since we have to copy we will make it fortran for efficiency
             X = np.asfortranarray(X, dtype=DTYPE)
 
+        # TODO: This check for y seems to be redundant, as it is also
+        #  present in the BaseDecisionTree's fit method, and therefore
+        #  can be removed.
         if y.base.dtype != DOUBLE or not y.base.flags.contiguous:
             y = np.ascontiguousarray(y, dtype=DOUBLE)
 
