acd51fae71b839a527e8ef204a71aa6eb818dd4b
Thomas J. Fan <thomasjpfan@gmail.com>
2023-02-16 17:13:44 -0500

    ENH Adds support for negative values in categorical features in gradient boosting

--- sklearn/ensemble/_hist_gradient_boosting/gradient_boosting.py
+++ sklearn/ensemble/_hist_gradient_boosting/gradient_boosting.py
@@ -272,6 +272,10 @@ class BaseHistGradientBoosting(BaseEstimator, ABC):
                 if missing.any():
                     categories = categories[~missing]
 
+                negative_categories = categories < 0
+                if categories.any():
+                    categories = categories[~negative_categories]
+
                 if hasattr(self, "feature_names_in_"):
                     feature_name = f"'{self.feature_names_in_[f_idx]}'"
                 else:
@@ -1268,9 +1272,8 @@ class HistGradientBoostingRegressor(RegressorMixin, BaseHistGradientBoosting):
           data has feature names).
 
         For each categorical feature, there must be at most `max_bins` unique
-        categories, and each categorical value must be in [0, max_bins -1].
-        During prediction, categories encoded as a negative value are treated as
-        missing values.
+        categories, and each categorical value must be less then `max_bins - 1`.
+        Categories encoded as a negative value are treated as missing values.
 
         Read more in the :ref:`User Guide <categorical_support_gbdt>`.
 
@@ -1632,9 +1635,8 @@ class HistGradientBoostingClassifier(ClassifierMixin, BaseHistGradientBoosting):
           data has feature names).
 
         For each categorical feature, there must be at most `max_bins` unique
-        categories, and each categorical value must be in [0, max_bins -1].
-        During prediction, categories encoded as a negative value are treated as
-        missing values.
+        categories, and each categorical value must be less then `max_bins - 1`.
+        Categories encoded as a negative value are treated as missing values.
 
         Read more in the :ref:`User Guide <categorical_support_gbdt>`.
 
