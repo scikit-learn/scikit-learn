fbb9b9b15fb037a0e2794a6d5c8caa78dc2eed82
Carlo <carlo_lemos@hotmail.com>
2023-02-18 13:21:52 -0300

    docstring example import error

--- sklearn/inspection/_metric_threshold_curve.py
+++ sklearn/inspection/_metric_threshold_curve.py
@@ -91,6 +91,7 @@ def metric_threshold_curve(
     --------
     >>> import numpy as np
     >>> from sklearn.metrics import accuracy_score
+    >>> from sklearn.inspection import metric_threshold_curve
     >>> y_true = np.array([0, 0, 1, 1])
     >>> y_scores = np.array([0.1, 0.4, 0.35, 0.8])
     >>> accuracy_values, thresholds = metric_threshold_curve(
