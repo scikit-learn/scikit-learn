f8063af40d11064080eab54ff2a762fc7a8a745b
Haesun Park <haesunrpark@gmail.com>
2023-02-20 14:46:01 +0900

    DOC fix typo in KMeans's param.

--- examples/cluster/plot_kmeans_stability_low_dim_dense.py
+++ examples/cluster/plot_kmeans_stability_low_dim_dense.py
@@ -10,7 +10,7 @@ distances to the nearest cluster center).
 
 The first plot shows the best inertia reached for each combination
 of the model (``KMeans`` or ``MiniBatchKMeans``), and the init method
-(``init="random"`` or ``init="kmeans++"``) for increasing values of the
+(``init="random"`` or ``init="k-means++"``) for increasing values of the
 ``n_init`` parameter that controls the number of initializations.
 
 The second plot demonstrates one single run of the ``MiniBatchKMeans``
