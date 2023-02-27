9f706c92e93618fd66cf160543706aab7aead7ab
Thomas J. Fan <thomasjpfan@gmail.com>
2023-02-27 11:23:23 -0500

    Update sklearn/neural_network/tests/test_mlp.py
    Co-authored-by: Jérémie du Boisberranger <34657725+jeremiedbb@users.noreply.github.com>

--- sklearn/neural_network/tests/test_mlp.py
+++ sklearn/neural_network/tests/test_mlp.py
@@ -941,6 +941,7 @@ def test_mlp_warm_start_no_convergence(MLPEstimator, solver):
         warm_start=True,
         early_stopping=False,
         max_iter=10,
+        n_iter_no_change=np.inf,
         random_state=0,
     )
 
