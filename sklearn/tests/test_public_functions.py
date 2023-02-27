afc451dfa866ef8c2e2a24e3074dfc90cf120bce
zeeshan <through.learning@gmail.com>
2023-02-28 00:47:53 +0530

    Added parameter validation for feature_selection.chi2 and test for the same function in test_public_functions.py

--- sklearn/tests/test_public_functions.py
+++ sklearn/tests/test_public_functions.py
@@ -145,6 +145,7 @@ PARAM_VALIDATION_FUNCTION_LIST = [
     "sklearn.random_projection.johnson_lindenstrauss_min_dim",
     "sklearn.svm.l1_min_c",
     "sklearn.metrics.precision_score",
+    "sklearn.feature_selection.chi2",
 ]
 
 
