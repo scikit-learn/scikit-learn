7186eabd419e4f6fe65f83d02a21e7246808a5b1
hhnnhh <info@hannahbohle.de>
2023-03-01 21:58:50 +0100

    notebook style, comments, structure, random seed

--- examples/cluster/plot_cluster_comparison.py
+++ examples/cluster/plot_cluster_comparison.py
@@ -3,25 +3,23 @@
 Comparing different clustering algorithms on toy datasets
 =========================================================
 
-The following example shows the characteristic of 
-different clustering algorithms applied to different two 
+The following example shows the characteristic of
+different clustering algorithms applied to different two
 dimensional datasets.
 
-With the exception of the last dataset, the data are 
-parametrically generated in such a way that 
-the results of the respective clusterings turn out to 
-be clearly distinguishable for the human reader. The 
-aim is to give an intuition about how the different 
-algorithms work. (It should be noted, however, that 
-these impression might quickly turn invalid on 
-higher dimensional data). 
+With the exception of the last dataset, the data are
+parametrically generated in such a way that
+the results of the respective clusterings turn out to be
+clearly distinguishable for the human reader. The aim is
+to give an intuition about how the different algorithms
+work. (It should be noted, however, that these impression
+might quickly turn invalid on higher dimensional data).
 
-The last dataset on the other hand is just rectangular 
-noise, which can hold as the absence of any objectively 
-usefull inherent cluster in the data. Thus, any 
-clustering shown is a method artefact by the 
-respective cluster algorithm. 
-
+The last dataset on the other hand is just rectangular
+noise, which can hold as the absence of any objectively
+useful inherent cluster in the data. Thus, any clustering
+shown is a method artefact by the respective cluster
+algorithm.
 
 """
 
@@ -36,33 +34,46 @@ from sklearn.neighbors import kneighbors_graph
 from sklearn.preprocessing import StandardScaler
 from itertools import cycle, islice
 
-np.random.seed(0)
 
-# ============
-# Generate datasets. We choose the size big enough to see the scalability
-# of the algorithms, but not too big to avoid too long running times
-# ============
+# %%
+# Generate sample data
+# ---------------------------------
+
+# We choose the size big enough to see the scalability
+# of the algorithms, but not too big to avoid too long running times.
+# Sample data consists of
+# 1. noisy circles 2. noisy moons 3. blobs with varied variance
+# 4. anisotrophic distributions ('stripes') 5. blobs with smaller variance
+# 6. rectangular noise
+
 n_samples = 500
+# 1. make large circle containing smaller circle
 noisy_circles = datasets.make_circles(n_samples=n_samples, factor=0.5, noise=0.05)
+# 2. make two half circles
 noisy_moons = datasets.make_moons(n_samples=n_samples, noise=0.05)
-blobs = datasets.make_blobs(n_samples=n_samples, random_state=8)
-no_structure = np.random.rand(n_samples, 2), None
-
-# Anisotropicly distributed data
-random_state = 170
-X, y = datasets.make_blobs(n_samples=n_samples, random_state=random_state)
+# 3. blobs with varied variances
+rng = np.random.RandomState(0)
+varied = datasets.make_blobs(
+    n_samples=n_samples, cluster_std=[1.0, 2.5, 0.5], random_state=rng
+)
+# 4. Anisotropicly distributed data (stripelike structure)
+X, y = datasets.make_blobs(n_samples=n_samples, random_state=rng)
 transformation = [[0.6, -0.6], [-0.4, 0.8]]
 X_aniso = np.dot(X, transformation)
 aniso = (X_aniso, y)
+# 5. make isotropic Gaussian blobs
+blobs = datasets.make_blobs(n_samples=n_samples, random_state=rng)
+# 6. make random noise
+no_structure = np.random.rand(n_samples, 2), None
 
-# blobs with varied variances
-varied = datasets.make_blobs(
-    n_samples=n_samples, cluster_std=[1.0, 2.5, 0.5], random_state=random_state
-)
-
-# ============
+# %%
 # Set up cluster parameters
-# ============
+# ---------------------------------
+
+# Prepare figure containing
+# 1. circles, 2. moons, 3. Gaussian blobs with variance 4. anisotropic distribution
+# 5. Gaussian blobs without less variance 6. random noise
+
 plt.figure(figsize=(9 * 2 + 3, 13))
 plt.subplots_adjust(
     left=0.02, right=0.98, bottom=0.001, top=0.95, wspace=0.05, hspace=0.01
@@ -128,6 +139,10 @@ datasets = [
     (no_structure, {}),
 ]
 
+# %%
+# Prepare samples for clustering
+# ---------------------------------
+
 for i_dataset, (dataset, algo_params) in enumerate(datasets):
     # update parameters with dataset-specific values
     params = default_base.copy()
@@ -148,27 +163,33 @@ for i_dataset, (dataset, algo_params) in enumerate(datasets):
     # make connectivity symmetric
     connectivity = 0.5 * (connectivity + connectivity.T)
 
-    # ============
+    # %%
     # Create cluster objects
-    # ============
-    ms = cluster.MeanShift(bandwidth=bandwidth, bin_seeding=True)
+    # ---------------------------------
+
+    # set up clustering models:
+    # 1. Mini Batch KMeans
+    # 2. Affinity Propagation
+    # 3. Mean Shift
+    # 4. Spectral Clustering
+    # 5. Ward
+    # 6. Agglomerative Clustering
+    # 7. DBSCAN
+    # 8. OPTICS
+    # 9. BIRCH
+    # 10. Gaussian Mixture
     two_means = cluster.MiniBatchKMeans(n_clusters=params["n_clusters"], n_init="auto")
-    ward = cluster.AgglomerativeClustering(
-        n_clusters=params["n_clusters"], linkage="ward", connectivity=connectivity
+    affinity_propagation = cluster.AffinityPropagation(
+        damping=params["damping"], preference=params["preference"], random_state=0
     )
+    ms = cluster.MeanShift(bandwidth=bandwidth, bin_seeding=True)
     spectral = cluster.SpectralClustering(
         n_clusters=params["n_clusters"],
         eigen_solver="arpack",
         affinity="nearest_neighbors",
     )
-    dbscan = cluster.DBSCAN(eps=params["eps"])
-    optics = cluster.OPTICS(
-        min_samples=params["min_samples"],
-        xi=params["xi"],
-        min_cluster_size=params["min_cluster_size"],
-    )
-    affinity_propagation = cluster.AffinityPropagation(
-        damping=params["damping"], preference=params["preference"], random_state=0
+    ward = cluster.AgglomerativeClustering(
+        n_clusters=params["n_clusters"], linkage="ward", connectivity=connectivity
     )
     average_linkage = cluster.AgglomerativeClustering(
         linkage="average",
@@ -176,10 +197,20 @@ for i_dataset, (dataset, algo_params) in enumerate(datasets):
         n_clusters=params["n_clusters"],
         connectivity=connectivity,
     )
+    dbscan = cluster.DBSCAN(eps=params["eps"])
+    optics = cluster.OPTICS(
+        min_samples=params["min_samples"],
+        xi=params["xi"],
+        min_cluster_size=params["min_cluster_size"],
+    )
     birch = cluster.Birch(n_clusters=params["n_clusters"])
     gmm = mixture.GaussianMixture(
         n_components=params["n_clusters"], covariance_type="full"
     )
+
+    # %%
+    # Define clustering algorithms
+    # ---------------------------------
 
     clustering_algorithms = (
         ("MiniBatch\nKMeans", two_means),
@@ -219,6 +250,10 @@ for i_dataset, (dataset, algo_params) in enumerate(datasets):
             y_pred = algorithm.labels_.astype(int)
         else:
             y_pred = algorithm.predict(X)
+
+        # %%
+        # Generate plot
+        # ---------------------------------
 
         plt.subplot(len(datasets), len(clustering_algorithms), plot_num)
         if i_dataset == 0:
