2c867b8f822eb7a684f0d5c4359e4426e1c9cfe0
Sortofamudkip <wishyutp0328@gmail.com>
2023-03-02 13:01:09 +0100

    MAINT Parameters validation for metrics.label_ranking_loss (#25742)
    Co-authored-by: Jérémie du Boisberranger <34657725+jeremiedbb@users.noreply.github.com>

--- sklearn/metrics/_ranking.py
+++ sklearn/metrics/_ranking.py
@@ -1224,6 +1224,13 @@ def coverage_error(y_true, y_score, *, sample_weight=None):
     return np.average(coverage, weights=sample_weight)
 
 
+@validate_params(
+    {
+        "y_true": ["array-like", "sparse matrix"],
+        "y_score": ["array-like"],
+        "sample_weight": ["array-like", None],
+    }
+)
 def label_ranking_loss(y_true, y_score, *, sample_weight=None):
     """Compute Ranking loss measure.
 
@@ -1242,10 +1249,10 @@ def label_ranking_loss(y_true, y_score, *, sample_weight=None):
 
     Parameters
     ----------
-    y_true : {ndarray, sparse matrix} of shape (n_samples, n_labels)
+    y_true : {array-like, sparse matrix} of shape (n_samples, n_labels)
         True binary labels in binary indicator format.
 
-    y_score : ndarray of shape (n_samples, n_labels)
+    y_score : array-like of shape (n_samples, n_labels)
         Target scores, can either be probability estimates of the positive
         class, confidence values, or non-thresholded measure of decisions
         (as returned by "decision_function" on some classifiers).
