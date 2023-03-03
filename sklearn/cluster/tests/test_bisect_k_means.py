4c54226b6d275c9bb8c8d471c97fe3f7e1024dc5
Gleb Levitski <36483986+glevv@users.noreply.github.com>
2023-03-03 11:26:21 +0000

    hotfix

--- sklearn/cluster/tests/test_bisect_k_means.py
+++ sklearn/cluster/tests/test_bisect_k_means.py
@@ -137,8 +137,8 @@ def test_float32_float64_equivalence(is_sparse):
 
 def test_sample_weight_init_bisect():
     """Check that sample weight is used during init."""
-    X, _ = make_blobs(n_samples=200, n_features=10, centers=10, random_state=0)
-    sample_weight = np.random.uniform(size=200)
+    X = rng.rand(20, 2)
+    sample_weight = np.random.uniform(size=20)
     kmeans = BisectingKMeans(n_clusters=3, random_state=0, init="random")
     y_pred_sw = kmeans.fit(X, sample_weight=sample_weight).cluster_centers_
     kmeans = BisectingKMeans(n_clusters=3, random_state=0, init="random")
