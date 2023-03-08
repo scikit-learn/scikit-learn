15bd8f1e12454d08e1574497e6cc8a953bca1ee1
Gleb Levitski <36483986+glevv@users.noreply.github.com>
2023-03-08 12:39:37 +0000

    rename vars

--- sklearn/cluster/tests/test_bisect_k_means.py
+++ sklearn/cluster/tests/test_bisect_k_means.py
@@ -136,6 +136,6 @@ def test_sample_weight_init_bisect(global_random_seed):
     X = rng.rand(20, 2)
     sample_weight = rng.uniform(size=20)
     kmeans = BisectingKMeans(n_clusters=3, random_state=global_random_seed, init="random")
-    y_pred_sw = kmeans.fit(X, sample_weight=sample_weight).cluster_centers_
-    y_pred = kmeans.fit(X).cluster_centers_
-    assert (y_pred_sw != y_pred).any()
+    clusters_weighted = kmeans.fit(X, sample_weight=sample_weight).cluster_centers_
+    clusters = kmeans.fit(X).cluster_centers_
+    assert (clusters_weighted != clusters).any()
