304d7d87a6270a9c0bebba6d587930e7d30126dd
Julien Jerphanion <git@jjerphan.xyz>
2023-03-09 20:02:33 +0100

    TST Increase atol for test_euclidean_distances_extreme_values

--- sklearn/metrics/tests/test_pairwise.py
+++ sklearn/metrics/tests/test_pairwise.py
@@ -941,7 +941,7 @@ def test_euclidean_distances_extreme_values(dtype, eps, rtol, dim):
     distances = euclidean_distances(X, Y)
     expected = cdist(X, Y)
 
-    assert_allclose(distances, expected, rtol=1e-5, atol=4e-4)
+    assert_allclose(distances, expected, rtol=1e-5, atol=6e-4)
 
 
 @pytest.mark.parametrize("squared", [True, False])
