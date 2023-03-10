7a2f5ca3a8478333f194a085b0c3635d75fcdf4d
Théophile Baranger <39696928+tbaranger@users.noreply.github.com>
2023-03-10 11:06:20 +0100

    MAINT Parameters validation for sklearn.datasets.fetch_20newsgroups_vectorized (#25796)

--- sklearn/tests/test_public_functions.py
+++ sklearn/tests/test_public_functions.py
@@ -104,6 +104,7 @@ PARAM_VALIDATION_FUNCTION_LIST = [
     "sklearn.covariance.shrunk_covariance",
     "sklearn.datasets.dump_svmlight_file",
     "sklearn.datasets.fetch_20newsgroups",
+    "sklearn.datasets.fetch_20newsgroups_vectorized",
     "sklearn.datasets.fetch_california_housing",
     "sklearn.datasets.fetch_covtype",
     "sklearn.datasets.fetch_kddcup99",
