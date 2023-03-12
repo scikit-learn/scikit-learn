837ea322d195c9d00b3113bc4e7f1964fc83bbf2
wishyut.pitawanik <wpitawanik@mpiwg-berlin.mpg.de>
2023-03-12 20:30:04 +0100

    added parameter validation for metrics.top_k_accuracy_score

--- sklearn/tests/test_public_functions.py
+++ sklearn/tests/test_public_functions.py
@@ -158,6 +158,7 @@ PARAM_VALIDATION_FUNCTION_LIST = [
     "sklearn.metrics.precision_score",
     "sklearn.metrics.r2_score",
     "sklearn.metrics.roc_curve",
+    "sklearn.metrics.top_k_accuracy_score",
     "sklearn.metrics.zero_one_loss",
     "sklearn.model_selection.train_test_split",
     "sklearn.random_projection.johnson_lindenstrauss_min_dim",
