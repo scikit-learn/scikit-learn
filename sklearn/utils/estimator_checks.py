5b21afdabc08a59925bea1795ce334b5bb79300b
Jérémie du Boisberranger <34657725+jeremiedbb@users.noreply.github.com>
2023-03-20 23:12:35 +0100

    TST Speed-up common tests of DictionaryLearning (#25892)

--- sklearn/utils/estimator_checks.py
+++ sklearn/utils/estimator_checks.py
@@ -676,7 +676,7 @@ def _set_checking_parameters(estimator):
             estimator.set_params(max_iter=500)
         # DictionaryLearning
         if name == "DictionaryLearning":
-            estimator.set_params(max_iter=200, transform_algorithm="lasso_lars")
+            estimator.set_params(max_iter=20, transform_algorithm="lasso_lars")
         # MiniBatchNMF
         if estimator.__class__.__name__ == "MiniBatchNMF":
             estimator.set_params(max_iter=20, fresh_restarts=True)
