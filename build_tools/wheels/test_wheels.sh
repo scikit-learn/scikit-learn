c1467246a50e67c6684faa0035334f524d6d380f
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2023-03-21 22:23:59 +0100

    fix??? [azure parallel] [cd build]

--- build_tools/wheels/test_wheels.sh
+++ build_tools/wheels/test_wheels.sh
@@ -18,10 +18,7 @@ python -c "import joblib; print(f'Number of cores (physical): \
 # threadpoolctl output section of the show_versions output:
 python -c "import sklearn; sklearn.show_versions()"
 
-# pip show will return 0 isthe package is installed and 1 otherwise
-pip show pytest-xdist
-XDIST_INSTALLED=$?
-if [[ $XDIST_INSTALLED -eq 0 ]]; then
+if pip show -qq pytest-xdist; then
     XDIST_WORKERS=$(python -c "import joblib; print(joblib.cpu_count(only_physical_cores=True))")
     pytest --pyargs sklearn -n $XDIST_WORKERS
 else
