30f448012bb714980fbea468ceb77aff95d0edab
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2023-03-21 19:28:32 +0100

    add checksum

--- build_tools/azure/upload_codecov.sh
+++ build_tools/azure/upload_codecov.sh
@@ -13,15 +13,24 @@ coverage combine --append
 popd
 cp $TEST_DIR/.coverage $BUILD_REPOSITORY_LOCALPATH
 
+# The checksum for each codecov binary is available at
+# https://uploader.codecov.io
+# When updating the binary, we also need to update the checksum
 if [[ $OSTYPE == *"linux"* ]]; then
     curl -Os https://uploader.codecov.io/v0.3.5/linux/codecov
+    SHA256SUM="080b43eaec3434326bb0f61653a82d27aba15c311ddde9d3f68cb364314f7aae  codecov"
+    echo "$SHA256SUM" | shasum -a256 -c
     chmod +x codecov
     ./codecov -t ${CODECOV_TOKEN} --rootDir $BUILD_REPOSITORY_LOCALPATH
 elif [[ $OSTYPE == *"darwin"* ]]; then
     curl -Os https://uploader.codecov.io/v0.3.5/macos/codecov
+    SHA256SUM="dfd7b0e3b245967477933c7f0c2b9f8f11dce775bc121a44de89b2b8b04251cd  codecov"
+    echo "$SHA256SUM" | shasum -a256 -c
     chmod +x codecov
     ./codecov -t ${CODECOV_TOKEN} --rootDir $BUILD_REPOSITORY_LOCALPATH
 else
     curl -Os https://uploader.codecov.io/v0.3.5/windows/codecov.exe
+    SHA256SUM="b3a5971ac1d5c2b50476bb9f1079e6e6fc967c4f0d1ede9a9d0ae2311d5d3dad codecov.exe"
+    echo "$SHA256SUM" | shasum -a256 -c
     ./codecov.exe -t ${CODECOV_TOKEN} --rootDir $BUILD_REPOSITORY_LOCALPATH
 fi
