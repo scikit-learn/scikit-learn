ff40900be4e8beec5f152573f49e4f3ec25c3a99
Loïc Estève <loic.esteve@ymail.com>
2023-03-22 08:04:45 +0100

    Update to codecov 0.4.0

--- build_tools/azure/upload_codecov.sh
+++ build_tools/azure/upload_codecov.sh
@@ -13,24 +13,30 @@ coverage combine --append
 popd
 cp $TEST_DIR/.coverage $BUILD_REPOSITORY_LOCALPATH
 
+# When we update the codecov uploader version, we need to update the checksums.
 # The checksum for each codecov binary is available at
-# https://uploader.codecov.io
-# When updating the binary, we also need to update the checksum
+# https://uploader.codecov.io e.g. for linux
+# https://uploader.codecov.io/v0.4.0/linux/codecov.SHA256SUM. In principle we
+# need to check the signatures with the codecov gpg key as well, see
+# https://docs.codecov.com/docs/codecov-uploader#integrity-checking-the-uploader
+# for more details
+CODECOV_UPLOADER_VERSION=0.4.0
+CODECOV_BASE_URL="https://uploader.codecov.io/v$CODECOV_UPLOADER_VERSION"
 if [[ $OSTYPE == *"linux"* ]]; then
-    curl -Os https://uploader.codecov.io/v0.3.5/linux/codecov
-    SHA256SUM="080b43eaec3434326bb0f61653a82d27aba15c311ddde9d3f68cb364314f7aae  codecov"
+    curl -Os "$CODECOV_BASE_URL/linux/codecov"
+    SHA256SUM="671cf0d89d1c149f57e1a9a31f3fb567ab4209e4d5829f13ff7b8c104db7131f  codecov"
     echo "$SHA256SUM" | shasum -a256 -c
     chmod +x codecov
     ./codecov -t ${CODECOV_TOKEN} --rootDir $BUILD_REPOSITORY_LOCALPATH
 elif [[ $OSTYPE == *"darwin"* ]]; then
-    curl -Os https://uploader.codecov.io/v0.3.5/macos/codecov
-    SHA256SUM="dfd7b0e3b245967477933c7f0c2b9f8f11dce775bc121a44de89b2b8b04251cd  codecov"
+    curl -Os "$CODECOV_BASE_URL/macos/codecov"
+    SHA256SUM="7549819f0fe115e113ec3538e259d748e87d84f68afa5deadc798967ec716b8d  codecov"
     echo "$SHA256SUM" | shasum -a256 -c
     chmod +x codecov
     ./codecov -t ${CODECOV_TOKEN} --rootDir $BUILD_REPOSITORY_LOCALPATH
 else
-    curl -Os https://uploader.codecov.io/v0.3.5/windows/codecov.exe
-    SHA256SUM="b3a5971ac1d5c2b50476bb9f1079e6e6fc967c4f0d1ede9a9d0ae2311d5d3dad codecov.exe"
+    curl -Os "$CODECOV_BASE_URL/windows/codecov.exe"
+    SHA256SUM="15fb34be4eb9949ad4e964a0e21c4efc79657de05b2c799e041d7293dccf60eb  codecov.exe"
     echo "$SHA256SUM" | sha256sum -c
     ./codecov.exe -t ${CODECOV_TOKEN} --rootDir $BUILD_REPOSITORY_LOCALPATH
 fi
