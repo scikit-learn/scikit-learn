297ff7bd7a45096e2dcfe5a6858a4b923a20f8a9
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2023-03-24 20:22:53 +0100

    forgot to add a file

---
+++ sklearn/utils/_plot.py
@@ -0,0 +1,53 @@
+from . import check_consistent_length, check_matplotlib_support
+from .multiclass import type_of_target
+from .validation import _check_pos_label_consistency
+from ._response import _get_response_values_binary
+
+
+class BinaryClassifierCurveDisplayMixin:
+    """Mixin class to be used in Displays requiring a binary classifier.
+
+    The aim of this class is to make some validations regarding the estimator and the
+    target and gather the response of the estimator.
+    """
+
+    def plot(self, *, name=None):
+        check_matplotlib_support(f"{self.__class__.__name__}.plot")
+        name = self.estimator_name if name is None else name
+        return name
+
+    @classmethod
+    def from_estimator(
+        cls, estimator, X, y, *, response_method="auto", pos_label=None, name=None
+    ):
+        check_matplotlib_support(f"{cls.__name__}.from_estimator")
+
+        name = estimator.__class__.__name__ if name is None else name
+
+        y_pred, pos_label = _get_response_values_binary(
+            estimator,
+            X,
+            response_method=response_method,
+            pos_label=pos_label,
+        )
+
+        return y_pred, pos_label, name
+
+    @classmethod
+    def from_predictions(
+        cls, y_true, y_pred, *, sample_weight=None, pos_label=None, name=None
+    ):
+        check_matplotlib_support(f"{cls.__name__}.from_predictions")
+
+        if type_of_target(y_true) != "binary":
+            raise ValueError(
+                f"The target y is not binary. Got {type_of_target(y_true)} type of"
+                " target."
+            )
+
+        check_consistent_length(y_true, y_pred, sample_weight)
+        pos_label = _check_pos_label_consistency(pos_label, y_true)
+
+        name = name if name is not None else "Classifier"
+
+        return pos_label, name
