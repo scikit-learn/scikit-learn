a9451776961e2df31d8a20b9d7cdb95fd578e485
Christian Lorentzen <lorentzen.ch@gmail.com>
2023-03-24 19:12:36 +0100

    ENH add exponential loss

--- sklearn/_loss/tests/test_link.py
+++ sklearn/_loss/tests/test_link.py
@@ -5,6 +5,7 @@ import pytest
 from sklearn._loss.link import (
     _LINKS,
     _inclusive_low_high,
+    HalfLogitLink,
     MultinomialLogit,
     Interval,
 )
@@ -71,6 +72,8 @@ def test_link_inverse_identity(link, global_random_seed):
         raw_prediction = rng.uniform(low=-20, high=20, size=(n_samples, n_classes))
         if isinstance(link, MultinomialLogit):
             raw_prediction = link.symmetrize_raw_prediction(raw_prediction)
+    elif isinstance(link, HalfLogitLink):
+        raw_prediction = rng.uniform(low=-10, high=10, size=(n_samples))
     else:
         raw_prediction = rng.uniform(low=-20, high=20, size=(n_samples))
 
@@ -93,7 +96,7 @@ def test_link_out_argument(link):
     else:
         # So far, the valid interval of raw_prediction is (-inf, inf) and
         # we do not need to distinguish.
-        raw_prediction = rng.normal(loc=0, scale=10, size=(n_samples))
+        raw_prediction = rng.uniform(low=-10, high=10, size=(n_samples))
 
     y_pred = link.inverse(raw_prediction, out=None)
     out = np.empty_like(raw_prediction)
