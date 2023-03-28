12498236100575c2dfd5bdc4bce4b858a8a93378
Maxim Darii <dariimaxim@gmail.com>
2023-03-28 21:09:58 +0200

    Fixed linting error

--- examples/applications/plot_digits_denoising.py
+++ examples/applications/plot_digits_denoising.py
@@ -100,7 +100,12 @@ from sklearn.decomposition import PCA, KernelPCA
 
 pca = PCA(n_components=32, random_state=42)
 kernel_pca = KernelPCA(
-    n_components=400, kernel="rbf", gamma=1e-3, fit_inverse_transform=True, alpha=5e-3, random_state=42
+    n_components=400,
+    kernel="rbf",
+    gamma=1e-3,
+    fit_inverse_transform=True,
+    alpha=5e-3,
+    random_state=42,
 )
 
 pca.fit(X_train_noisy)
