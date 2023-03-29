c1a2106b74f69a8c976d72493dd4d8fa75b9f7a5
mdarii <dariimaxim@gmail.com>
2023-03-29 10:09:04 +0200

    MAINT make plot_digits_denoising deterministic by fixing random state (#26004)

--- examples/applications/plot_digits_denoising.py
+++ examples/applications/plot_digits_denoising.py
@@ -98,9 +98,14 @@ plot_digits(
 # uses a radial basis function (RBF) kernel.
 from sklearn.decomposition import PCA, KernelPCA
 
-pca = PCA(n_components=32)
+pca = PCA(n_components=32, random_state=42)
 kernel_pca = KernelPCA(
-    n_components=400, kernel="rbf", gamma=1e-3, fit_inverse_transform=True, alpha=5e-3
+    n_components=400,
+    kernel="rbf",
+    gamma=1e-3,
+    fit_inverse_transform=True,
+    alpha=5e-3,
+    random_state=42,
 )
 
 pca.fit(X_train_noisy)
