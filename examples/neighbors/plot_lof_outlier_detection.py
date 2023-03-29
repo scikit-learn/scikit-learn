ad362af6dc608c828359ab4721ae9a21a5431f41
ArturoAmorQ <arturo.amor-quiroz@polytechnique.edu>
2023-03-29 17:54:37 +0200

    Fix format

--- examples/neighbors/plot_lof_outlier_detection.py
+++ examples/neighbors/plot_lof_outlier_detection.py
@@ -6,21 +6,19 @@ Outlier detection with Local Outlier Factor (LOF)
 The Local Outlier Factor (LOF) algorithm is an unsupervised anomaly detection
 method which computes the local density deviation of a given data point with
 respect to its neighbors. It considers as outliers the samples that have a
-substantially lower density than their neighbors. This example shows how to
-use LOF for outlier detection which is the default use case of this estimator
-in scikit-learn. Note that when LOF is used for outlier detection it has no
-predict, decision_function and score_samples methods. See
-:ref:`User Guide <outlier_detection>`: for details on the difference between
-outlier detection and novelty detection and how to use LOF for novelty
-detection.
+substantially lower density than their neighbors. This example shows how to use
+LOF for outlier detection which is the default use case of this estimator in
+scikit-learn. Note that when LOF is used for outlier detection it has no
+predict, `decision_function` and `score_samples` methods. See the :ref:`User
+Guide <outlier_detection>` for details on the difference between outlier
+detection and novelty detection and how to use LOF for novelty detection.
 
-The number of neighbors considered (parameter n_neighbors) is typically
-set 1) greater than the minimum number of samples a cluster has to contain,
-so that other samples can be local outliers relative to this cluster, and 2)
-smaller than the maximum number of close by samples that can potentially be
-local outliers.
-In practice, such information is generally not available, and taking
-n_neighbors=20 appears to work well in general.
+The number of neighbors considered (parameter `n_neighbors`) is typically set 1)
+greater than the minimum number of samples a cluster has to contain, so that
+other samples can be local outliers relative to this cluster, and 2) smaller
+than the maximum number of close by samples that can potentially be local
+outliers. In practice, such information is generally not available, and taking
+`n_neighbors=20` appears to work well in general.
 
 """
 
