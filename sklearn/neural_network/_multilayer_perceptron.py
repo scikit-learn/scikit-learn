86ecaf4a805cc233a256f53188798649de35af31
Zach Shu <zhongzheng.shu@ibm.com>
2023-03-31 00:18:31 -0400

    fix: fix sample_weight implementation

--- sklearn/neural_network/_multilayer_perceptron.py
+++ sklearn/neural_network/_multilayer_perceptron.py
@@ -458,9 +458,14 @@ class BaseMultilayerPerceptron(BaseEstimator, metaclass=ABCMeta):
             not self.warm_start and not incremental
         )
 
-        X, y, sample_weight = self._validate_input(
-            X, y, incremental, reset=first_pass, sample_weight=sample_weight
-        )
+        X, y = self._validate_input(X, y, incremental, reset=first_pass)
+        # Handle sample_weight
+        if sample_weight is not None:
+            sample_weight = _check_sample_weight(sample_weight, X, dtype=X.dtype)
+            nonzero_mask = sample_weight != 0
+            X = X[nonzero_mask]
+            y = y[nonzero_mask]
+            sample_weight = sample_weight[nonzero_mask]
 
         n_samples, n_features = X.shape
 
@@ -1196,7 +1201,7 @@ class MLPClassifier(ClassifierMixin, BaseMultilayerPerceptron):
             max_fun=max_fun,
         )
 
-    def _validate_input(self, X, y, incremental, reset, sample_weight=None):
+    def _validate_input(self, X, y, incremental, reset):
         X, y = self._validate_data(
             X,
             y,
@@ -1249,13 +1254,7 @@ class MLPClassifier(ClassifierMixin, BaseMultilayerPerceptron):
         # float32 data
         y = self._label_binarizer.transform(y).astype(bool)
 
-        if sample_weight is None:
-            return X, y, None
-
-        # check sample weight
-        sample_weight = _check_sample_weight(sample_weight, X, dtype=X.dtype)
-
-        return X, y, sample_weight
+        return X, y
 
     def predict(self, X):
         """Predict using the multi-layer perceptron classifier.
@@ -1742,7 +1741,7 @@ class MLPRegressor(RegressorMixin, BaseMultilayerPerceptron):
         y_pred = self._predict(X, check_input=False)
         return r2_score(y, y_pred, sample_weight=sample_weight)
 
-    def _validate_input(self, X, y, incremental, reset, sample_weight=None):
+    def _validate_input(self, X, y, incremental, reset):
         X, y = self._validate_data(
             X,
             y,
@@ -1755,13 +1754,7 @@ class MLPRegressor(RegressorMixin, BaseMultilayerPerceptron):
         if y.ndim == 2 and y.shape[1] == 1:
             y = column_or_1d(y, warn=True)
 
-        if sample_weight is None:
-            return X, y, None
-
-        # check sample weight
-        sample_weight = _check_sample_weight(sample_weight, X, dtype=X.dtype)
-
-        return X, y, sample_weight
+        return X, y
 
     @available_if(lambda est: est._check_solver)
     @_fit_context(prefer_skip_nested_validation=True)
--- sklearn/neural_network/tests/test_base.py
+++ sklearn/neural_network/tests/test_base.py
@@ -31,66 +31,73 @@ def test_log_loss_1_prob_finite(y_true, y_prob):
 
 def test_squared_loss():
     y_true = np.array([[1, 0, 0], [0, 1, 0]])
-    y_pred = np.array([[0.0, 1.0, 0.0], [0.9, 0.05, 0.05]])
+    y_prob = np.array([[0.0, 1.0, 0.0], [0.9, 0.05, 0.05]])
 
-    assert squared_loss(y_true, y_pred) == pytest.approx(0.3095833333333333)
+    assert squared_loss(y_true, y_prob) == pytest.approx(0.3095833333333333)
 
     sample_weight = np.array([1, 2])
-    assert squared_loss(y_true, y_pred, sample_weight) == pytest.approx(0.4525)
+    assert squared_loss(y_true, y_prob, sample_weight) == pytest.approx(0.4525)
 
 
 def test_log_loss():
     y_true = np.array([[1, 0, 0], [0, 1, 0]])
-    y_pred = np.array([[0.0, 1.0, 0.0], [0.9, 0.05, 0.05]])
+    y_prob = np.array([[0.0, 1.0, 0.0], [0.9, 0.05, 0.05]])
 
-    assert log_loss(y_true, y_pred) == pytest.approx(19.519692831335572)
+    assert log_loss(y_true, y_prob) == pytest.approx(19.519692831335572)
 
     sample_weight = np.array([1, 2])
-    assert log_loss(y_true, y_pred, sample_weight) == pytest.approx(21.017558968112567)
+    assert log_loss(y_true, y_prob, sample_weight) == pytest.approx(21.017558968112567)
 
 
 def test_binary_log_loss():
     y_true = np.array([[0], [1]])
-    y_pred = np.array([[0.1], [0.9]])
+    y_prob = np.array([[0.1], [0.9]])
 
     assert (
-        log_loss(y_true, y_pred)
-        == pytest.approx(binary_log_loss(y_true, y_pred))
+        log_loss(y_true, y_prob)
+        == pytest.approx(binary_log_loss(y_true, y_prob))
         == pytest.approx(0.10536051565782628)
     )
 
     sample_weight = np.array([1, 2])
     assert (
-        log_loss(y_true, y_pred, sample_weight)
-        == pytest.approx(binary_log_loss(y_true, y_pred, sample_weight))
+        log_loss(y_true, y_prob, sample_weight)
+        == pytest.approx(binary_log_loss(y_true, y_prob, sample_weight))
         == pytest.approx(0.15804077348673942)
     )
 
 
 def test_binary_log_loss_multi_label():
     y_true = np.array([[1, 0, 0], [0, 1, 0]])
-    y_pred = np.array([[0.0, 1.0, 0.0], [0.9, 0.05, 0.05]])
+    y_prob = np.array([[0.0, 1.0, 0.0], [0.9, 0.05, 0.05]])
 
-    assert binary_log_loss(y_true, y_pred) == pytest.approx(38.71845871958495)
+    assert binary_log_loss(y_true, y_prob) == pytest.approx(38.71845871958495)
 
     sample_weight = np.array([1, 2])
-    assert binary_log_loss(y_true, y_pred, sample_weight) == pytest.approx(
+    assert binary_log_loss(y_true, y_prob, sample_weight) == pytest.approx(
         41.39326405005274
     )
 
 
+@pytest.mark.parametrize(
+    "y_true, y_prob",
+    [
+        (
+            np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]),
+            np.array([[0.0, 1.0, 0.0], [0.9, 0.05, 0.05], [0.1, 0.1, 0.8]]),
+        ),
+        (np.array([[0, 0, 1]]).T, np.array([[0.9, 1.0, 1.0]]).T),
+    ],
+)
 @pytest.mark.parametrize("loss", [squared_loss, log_loss, binary_log_loss])
-def test_sample_weight_effects(loss):
-    y_true = np.array([[1, 0, 0], [0, 1, 0]])
-    y_pred = np.array([[0.0, 1.0, 0.0], [0.9, 0.05, 0.05]])
+def test_sample_weight_effects(y_true, y_prob, loss):
+    sample_weight = np.array([0, 0, 0])
+    assert loss(y_true, y_prob, sample_weight) == pytest.approx(0.0)
 
-    sample_weight = np.array([0, 0])
-    assert loss(y_true, y_pred, sample_weight) == pytest.approx(0.0)
-
-    sample_weight = np.array([0, 1])
-    assert loss(y_true, y_pred, sample_weight) == pytest.approx(
-        loss(y_true[1:], y_pred[1:], sample_weight[1:])
+    sample_weight = np.array([0, 1, 1])
+    assert loss(y_true, y_prob, sample_weight) == pytest.approx(
+        loss(y_true[1:], y_prob[1:], sample_weight[1:])
     )
 
-    sample_weight = np.array([1, 1])
-    assert loss(y_true, y_pred, sample_weight) == pytest.approx(loss(y_true, y_pred))
+    sample_weight = np.array([1, 1, 1])
+    assert loss(y_true, y_prob, sample_weight) == pytest.approx(loss(y_true, y_prob))
