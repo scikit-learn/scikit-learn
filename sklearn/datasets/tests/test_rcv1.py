63e6fafb5bc70351b201cfe0c76101fb7b581601
Maren Westermann <maren.westermann@gmail.com>
2023-04-01 16:16:49 +0200

    fix linting [all random seeds] test_fetch_rcv1

--- sklearn/datasets/tests/test_rcv1.py
+++ sklearn/datasets/tests/test_rcv1.py
@@ -42,7 +42,9 @@ def test_fetch_rcv1(fetch_rcv1_fxt, global_random_seed):
         assert num == Y1[:, j].data.size
 
     # test shuffling and subset
-    data2 = fetch_rcv1_fxt(shuffle=True, subset="train", random_state=global_random_seed)
+    data2 = fetch_rcv1_fxt(
+        shuffle=True, subset="train", random_state=global_random_seed
+    )
     X2, Y2 = data2.data, data2.target
     s2 = data2.sample_id
 
