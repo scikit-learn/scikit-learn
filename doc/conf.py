23d477f2fd6098212f4273888a4f702c9da0a2d4
Loïc Estève <loic.esteve@ymail.com>
2023-04-03 11:04:13 +0200

    DOC Use Scientific Python Plausible instance for analytics (#25547)

--- doc/conf.py
+++ doc/conf.py
@@ -171,7 +171,8 @@ html_theme = "scikit-learn-modern"
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    "google_analytics": True,
+    "legacy_google_analytics": True,
+    "analytics": True,
     "mathjax_path": mathjax_path,
     "link_to_live_contributing_page": not parsed_version.is_devrelease,
 }
