1e942b2ec730864c28459d10bee17492c9fd6636
Yuchen Zhou <72342196+ROMEEZHOU@users.noreply.github.com>
2023-04-03 06:42:39 -0400

    MAINT Parameters validation for sklearn.preprocessing.add_dummy_feature (#26058)

--- sklearn/tests/test_public_functions.py
+++ sklearn/tests/test_public_functions.py
@@ -214,6 +214,7 @@ PARAM_VALIDATION_FUNCTION_LIST = [
     "sklearn.metrics.top_k_accuracy_score",
     "sklearn.metrics.zero_one_loss",
     "sklearn.model_selection.train_test_split",
+    "sklearn.preprocessing.add_dummy_feature",
     "sklearn.preprocessing.scale",
     "sklearn.random_projection.johnson_lindenstrauss_min_dim",
     "sklearn.svm.l1_min_c",
