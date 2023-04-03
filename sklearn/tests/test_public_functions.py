c75b200bc570b820ab81ae6dcacc3e51653980a5
Yao Xiao <108576690+Charlie-XIAO@users.noreply.github.com>
2023-04-03 05:57:54 -0400

    MAINT Parameters validation for sklearn.preprocessing.scale (#26036)

--- sklearn/tests/test_public_functions.py
+++ sklearn/tests/test_public_functions.py
@@ -210,6 +210,7 @@ PARAM_VALIDATION_FUNCTION_LIST = [
     "sklearn.metrics.top_k_accuracy_score",
     "sklearn.metrics.zero_one_loss",
     "sklearn.model_selection.train_test_split",
+    "sklearn.preprocessing.scale",
     "sklearn.random_projection.johnson_lindenstrauss_min_dim",
     "sklearn.svm.l1_min_c",
     "sklearn.tree.export_text",
