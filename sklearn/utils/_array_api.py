5e737e4540debc7f6c63273cae40a4582abf5170
Thomas J. Fan <thomasjpfan@gmail.com>
2023-04-04 10:37:28 -0400

    STY Fix linting

--- sklearn/utils/_array_api.py
+++ sklearn/utils/_array_api.py
@@ -282,21 +282,6 @@ class _NumPyAPIWrapper:
     def isdtype(self, dtype, kind):
         return isdtype(dtype, kind, xp=self)
 
-    def reshape(self, x, shape, *, copy=None):
-        """Gives a new shape to an array without changing its data.
-
-        The Array API specification requires shape to be a tuple.
-        https://data-apis.org/array-api/latest/API_specification/generated/array_api.reshape.html
-        """
-        if not isinstance(shape, tuple):
-            raise TypeError(
-                f"shape must be a tuple, got {shape!r} of type {type(shape)}"
-            )
-
-        if copy is True:
-            x = x.copy()
-        return numpy.reshape(x, shape)
-
 
 _NUMPY_API_WRAPPER_INSTANCE = _NumPyAPIWrapper()
 
