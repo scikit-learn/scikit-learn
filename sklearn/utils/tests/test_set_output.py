a711745a3fac1a15d97477787ed5e544bb1f2847
Thomas J. Fan <thomasjpfan@gmail.com>
2023-04-12 09:16:51 -0400

    TST Check for namedtuple names

--- sklearn/utils/tests/test_set_output.py
+++ sklearn/utils/tests/test_set_output.py
@@ -305,9 +305,11 @@ class EstimatorReturnTuple(_SetOutputMixin):
 
 def test_set_output_named_tuple_out():
     """Check that namedtuples are kept by default."""
-    Output = namedtuple("Output", "X, y")
+    Output = namedtuple("Output", "X, Y")
     X = np.asarray([[1, 2, 3]])
     est = EstimatorReturnTuple(OutputTuple=Output)
     X_trans = est.transform(X)
 
     assert isinstance(X_trans, Output)
+    assert_array_equal(X_trans.X, X)
+    assert_array_equal(X_trans.Y, 2 * X)
