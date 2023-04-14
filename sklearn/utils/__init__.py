7b0a2b201db7afb8f47aee9e21820565511c6d03
elabongaatuo <elabongaatuo@gmail.com>
2023-04-14 19:43:01 +0300

    modified validate-mask parameter value

--- sklearn/utils/__init__.py
+++ sklearn/utils/__init__.py
@@ -471,6 +471,14 @@ def _get_column_indices(X, key):
         )
 
 
+@validate_params(
+    {
+        "replace": ["boolean"],
+        "n_samples": [Interval(numbers.Integral, 1, None, closed="left"), None],
+        "random_state": ["random_state"],
+        "stratify": ["array-like", None],
+    }
+)
 def resample(*arrays, replace=True, n_samples=None, random_state=None, stratify=None):
     """Resample arrays or sparse matrices in a consistent way.
 
