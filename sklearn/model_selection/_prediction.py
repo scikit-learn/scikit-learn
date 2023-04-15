c9d7a221243ec22365ad3ba0dacbd05d1df41af3
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2023-04-15 20:54:02 +0200

    TST make sure to pass the common test

--- sklearn/model_selection/_prediction.py
+++ sklearn/model_selection/_prediction.py
@@ -1,3 +1,4 @@
+from inspect import signature
 from numbers import Integral, Real
 
 import numpy as np
@@ -10,28 +11,137 @@ from ..utils._param_validation import HasMethods, Interval, StrOptions
 from ..utils._response import _get_response_values_binary
 from ..utils.multiclass import type_of_target
 from ..utils.parallel import Parallel, delayed
-from ..utils.validation import _check_sample_weight, _num_samples
+from ..utils.validation import (
+    _check_sample_weight,
+    _num_samples,
+    check_is_fitted,
+    indexable,
+)
 
 from ._split import check_cv
 
 
-def _fit_and_score(classifier, X, y, train_idx, val_idx, scorer, score_method):
+def _fit_and_score(
+    classifier, X, y, sample_weight, train_idx, val_idx, scorer, score_method
+):
+    fit_parameters = signature(classifier.fit).parameters
+    supports_sw = "sample_weight" in fit_parameters
+
     if train_idx is not None:
         X_train, X_val = _safe_indexing(X, train_idx), _safe_indexing(X, val_idx)
         y_train, y_val = _safe_indexing(y, train_idx), _safe_indexing(y, val_idx)
-        classifier.fit(X_train, y_train)
+        if sample_weight is not None:
+            sw_train, sw_val = (
+                _safe_indexing(sample_weight, train_idx),
+                _safe_indexing(sample_weight, val_idx),
+            )
+        else:
+            sw_train, sw_val = None, None
+        if supports_sw:
+            classifier.fit(X_train, y_train, sample_weight=sw_train)
+        else:
+            classifier.fit(X_train, y_train)
     else:  # prefit estimator, only a validation set is provided
-        X_val, y_val = X, y
+        X_val, y_val, sw_val = X, y, sample_weight
 
     if score_method == {"tnr", "tpr"}:
-        fpr, tpr, potential_thresholds = scorer(classifier, X_val, y_val)
+        fpr, tpr, potential_thresholds = scorer(
+            classifier, X_val, y_val, sample_weight=sw_val
+        )
         if score_method == "tnr":
             return potential_thresholds[::-1], (1 - fpr)[::-1]
         return potential_thresholds, tpr
-    return scorer(classifier, X_val, y_val)
+    return scorer(classifier, X_val, y_val, sample_weight=sw_val)
 
 
 class CutOffClassifier(ClassifierMixin, MetaEstimatorMixin, BaseEstimator):
+    """Decision threshold calibration for binary classification.
+
+    Estimator that calibrates the decision threshold (cutoff point) that is
+    used for prediction. The methods for picking cutoff points make use of
+    traditional binary classification evaluation statistics such as the true
+    positive and true negative rates or any metrics accepting true labels and
+    the output of a scoring function from a scikit-learn estimator.
+
+    Parameters
+    ----------
+    estimator : estimator object
+        The classifier, fitted or not fitted, from which we want to optimize
+        the decision threshold used during `predict`.
+
+    objective_metric : {"tpr", "tnr"}, str or callable,  \
+            default="balanced_accuracy"
+        The objective metric to be optimized. Can be one of:
+
+        * a string associated to a scoring function (see model evaluation
+          documentation);
+        * a scorer callable object / function with the signature
+          `metric(estimator, X, y)`;
+        * `"tpr"`: find the decision threshold for a true positive ratio (TPR)
+          of `objective_value`;
+        * `"tnr"`: find the decision threshold for a true negative ratio (TNR)
+          of `objective_value`.
+
+    objective_value : float, default=None
+        The value associated with the `objective_metric` metric for which we
+        want to find the decision threshold when `objective_metric` is equal to
+        `"tpr"` or `"tnr"`.
+
+    response_method : {"auto", "decision_function", "predict_proba"}, \
+            default="auto"
+        Methods by the classifier `base_estimator` corresponding to the
+        decision function for which we want to find a threshold. It can be:
+
+        * if `"auto"`, it will try to invoke, for each classifier,
+          `"predict_proba"` or `"decision_function"` in that order.
+        * otherwise, one of `"predict_proba"` or `"decision_function"`.
+          If the method is not implemented by the classifier, it will raise an
+          error.
+
+    n_thresholds : int, default=1000
+        The number of decision threshold to use when discretizing the output
+        of the classifier `method`.
+
+    cv : int, float, cross-validation generator, iterable or "prefit", \
+            default=None
+        Determines the cross-validation splitting strategy used in
+        `cross_val_predict` to train classifier. Possible inputs for cv are:
+
+        * None, to use the default 5-fold stratified K-fold cross validation;
+        * An integer number, to specify the number of folds in a stratified
+          k-fold;
+        * A float number, to specify a single shuffle split. The floating
+          number should be in (0, 1) and represent the size of the validation
+          set;
+        * An object to be used as a cross-validation generator;
+        * An iterable yielding train, test splits;
+        * "prefit", to bypass the cross-validation.
+
+        Refer :ref:`User Guide <cross_validation>` for the various
+        cross-validation strategies that can be used here.
+
+    n_jobs : int, default=None
+        The number of jobs to run in parallel all `estimators` `fit`.
+        `None` means 1 unless in a `joblib.parallel_backend` context. -1 means
+        using all processors. See Glossary for more details.
+
+    Attributes
+    ----------
+    decision_threshold_ : float
+        The new decision threshold.
+
+    classes_ : ndarray of shape (n_classes,)
+        The class labels.
+
+    n_features_in_ : int
+        Number of features seen during :term:`fit`. Only defined if the
+        underlying estimator exposes such an attribute when fit.
+
+    feature_names_in_ : ndarray of shape (`n_features_in_`,)
+        Names of features seen during :term:`fit`. Only defined if the
+        underlying estimator exposes such an attribute when fit.
+    """
+
     def __init__(
         self,
         estimator,
@@ -69,11 +179,12 @@ class CutOffClassifier(ClassifierMixin, MetaEstimatorMixin, BaseEstimator):
 
     def fit(self, X, y, sample_weight=None, **fit_params):
         self._validate_params()
+        X, y = indexable(X, y)
 
         y_type = type_of_target(y, input_name="y")
         if y_type != "binary":
             raise ValueError(
-                f"Only binary classification is supported. Got {y_type} instead."
+                f"Only binary classification is supported. Unknown label type: {y_type}"
             )
 
         if sample_weight is not None:
@@ -124,6 +235,7 @@ class CutOffClassifier(ClassifierMixin, MetaEstimatorMixin, BaseEstimator):
                     classifier,
                     X,
                     y,
+                    sample_weight,
                     train_idx,
                     val_idx,
                     self._scorer,
@@ -152,6 +264,11 @@ class CutOffClassifier(ClassifierMixin, MetaEstimatorMixin, BaseEstimator):
             best_idx = np.searchsorted(mean_score, objective_value)
         self.decision_threshold_ = thresholds_interpolated[best_idx]
 
+        if hasattr(self.estimator_, "n_features_in_"):
+            self.n_features_in_ = self.estimator_.n_features_in_
+        if hasattr(self.estimator_, "feature_names_in_"):
+            self.feature_names_in_ = self.estimator_.feature_names_in_
+
         return self
 
     @property
@@ -160,9 +277,13 @@ class CutOffClassifier(ClassifierMixin, MetaEstimatorMixin, BaseEstimator):
         return self.estimator_.classes_
 
     def predict(self, X):
+        check_is_fitted(self, "estimator_")
         pos_label = self._scorer._get_pos_label()
         y_score, _ = _get_response_values_binary(
             self.estimator_, X, self._response_method, pos_label=pos_label
         )
         y_pred = (y_score >= self.decision_threshold_).astype(int)
         return self.classes_[y_pred]
+
+    def _more_tags(self):
+        return {"binary_only": True}
