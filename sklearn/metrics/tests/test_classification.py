127e68a7a362ef1f546711afa4f62f7409cc31bd
Sean (Seok-Won) Yi <seantuokc@gmail.com>
2023-04-16 17:22:14 +0900

    Added two tests for output_pred scenarios.

--- sklearn/metrics/tests/test_classification.py
+++ sklearn/metrics/tests/test_classification.py
@@ -39,6 +39,7 @@ from sklearn.metrics import jaccard_score
 from sklearn.metrics import log_loss
 from sklearn.metrics import matthews_corrcoef
 from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import precision_recall_fscore_support_pred
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import zero_one_loss
@@ -168,6 +169,78 @@ def test_classification_report_dictionary_output():
     assert type(expected_report["macro avg"]["support"]) == int
 
 
+def test_classification_report_dictionary_output_with_preds():
+    # Test performance report with dictionary output
+    iris = datasets.load_iris()
+    y_true, y_pred, _ = make_prediction(dataset=iris, binary=False)
+
+    # print classification report with class names
+    expected_report = {
+        "setosa": {
+            "precision": 0.82608695652173914,
+            "recall": 0.79166666666666663,
+            "f1-score": 0.8085106382978724,
+            "support": 24,
+            "predicted": 23,
+        },
+        "versicolor": {
+            "precision": 0.33333333333333331,
+            "recall": 0.096774193548387094,
+            "f1-score": 0.15000000000000002,
+            "support": 31,
+            "predicted": 9,
+        },
+        "virginica": {
+            "precision": 0.41860465116279072,
+            "recall": 0.90000000000000002,
+            "f1-score": 0.57142857142857151,
+            "support": 20,
+            "predicted": 43,
+        },
+        "macro avg": {
+            "f1-score": 0.5099797365754813,
+            "precision": 0.5260083136726211,
+            "recall": 0.596146953405018,
+            "support": 75,
+            "predicted": 75,
+        },
+        "accuracy": 0.5333333333333333,
+        "weighted avg": {
+            "f1-score": 0.47310435663627154,
+            "precision": 0.5137535108414785,
+            "recall": 0.5333333333333333,
+            "support": 75,
+            "predicted": 75,
+        },
+    }
+
+    report = classification_report(
+        y_true,
+        y_pred,
+        labels=np.arange(len(iris.target_names)),
+        target_names=iris.target_names,
+        output_dict=True,
+        output_pred=True,
+    )
+
+    # assert the 2 dicts are equal.
+    assert report.keys() == expected_report.keys()
+    for key in expected_report:
+        if key == "accuracy":
+            assert isinstance(report[key], float)
+            assert report[key] == expected_report[key]
+        else:
+            assert report[key].keys() == expected_report[key].keys()
+            for metric in expected_report[key]:
+                assert_almost_equal(expected_report[key][metric], report[key][metric])
+
+    assert type(expected_report["setosa"]["precision"]) == float
+    assert type(expected_report["macro avg"]["precision"]) == float
+    assert type(expected_report["setosa"]["support"]) == int
+    assert type(expected_report["macro avg"]["support"]) == int
+    assert type(expected_report["setosa"]["predicted"]) == int
+
+
 def test_classification_report_output_dict_empty_input():
     report = classification_report(y_true=[], y_pred=[], output_dict=True)
     expected_report = {
@@ -183,6 +256,40 @@ def test_classification_report_output_dict_empty_input():
             "precision": np.nan,
             "recall": np.nan,
             "support": 0,
+        },
+    }
+    assert isinstance(report, dict)
+    # assert the 2 dicts are equal.
+    assert report.keys() == expected_report.keys()
+    for key in expected_report:
+        if key == "accuracy":
+            assert isinstance(report[key], float)
+            assert report[key] == expected_report[key]
+        else:
+            assert report[key].keys() == expected_report[key].keys()
+            for metric in expected_report[key]:
+                assert_almost_equal(expected_report[key][metric], report[key][metric])
+
+
+def test_classification_report_output_dict_pred_empty_input():
+    report = classification_report(
+        y_true=[], y_pred=[], output_dict=True, output_pred=True
+    )
+    expected_report = {
+        "accuracy": 0.0,
+        "macro avg": {
+            "f1-score": np.nan,
+            "precision": np.nan,
+            "recall": np.nan,
+            "support": 0,
+            "predicted": 0,
+        },
+        "weighted avg": {
+            "f1-score": np.nan,
+            "precision": np.nan,
+            "recall": np.nan,
+            "support": 0,
+            "predicted": 0,
         },
     }
     assert isinstance(report, dict)
@@ -399,6 +506,22 @@ def test_precision_recall_f_unused_pos_label():
     )
     with pytest.warns(UserWarning, match=msg):
         precision_recall_fscore_support(
+            [1, 2, 1], [1, 2, 2], pos_label=2, average="macro"
+        )
+
+
+def test_precision_recall_f_pred():
+    # Check warning that pos_label unused when set to non-default value
+    # but average != 'binary'; even if data is binary.
+
+    msg = (
+        r"Note that pos_label \(set to 2\) is "
+        r"ignored when average != 'binary' \(got 'macro'\). You "
+        r"may use labels=\[pos_label\] to specify a single "
+        "positive class."
+    )
+    with pytest.warns(UserWarning, match=msg):
+        precision_recall_fscore_support_pred(
             [1, 2, 1], [1, 2, 2], pos_label=2, average="macro"
         )
 
