7a43b6ae8027153fa5789c14af155140b55b44cd
Christian Lorentzen <lorentzen.ch@gmail.com>
2023-04-19 23:18:38 +0200

    DOC meth -> term

--- sklearn/ensemble/_gb.py
+++ sklearn/ensemble/_gb.py
@@ -949,7 +949,7 @@ class GradientBoostingClassifier(ClassifierMixin, BaseGradientBoosting):
 
     init : estimator or 'zero', default=None
         An estimator object that is used to compute the initial predictions.
-        ``init`` has to provide :meth:`fit` and :meth:`predict_proba`. If
+        ``init`` has to provide :term:`fit` and :term:`predict_proba`. If
         'zero', the initial raw predictions are set to zero. By default, a
         ``DummyEstimator`` predicting the classes priors is used.
 
@@ -1512,7 +1512,7 @@ class GradientBoostingRegressor(RegressorMixin, BaseGradientBoosting):
 
     init : estimator or 'zero', default=None
         An estimator object that is used to compute the initial predictions.
-        ``init`` has to provide :meth:`fit` and :meth:`predict`. If 'zero', the
+        ``init`` has to provide :term:`fit` and :term:`predict`. If 'zero', the
         initial raw predictions are set to zero. By default a
         ``DummyEstimator`` is used, predicting either the average target value
         (for loss='squared_error'), or a quantile for the other losses.
