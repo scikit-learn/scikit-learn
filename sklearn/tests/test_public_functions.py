523c135064b2506063c4d8314244943b72eee7c5
Yao Xiao <108576690+Charlie-XIAO@users.noreply.github.com>
2023-04-27 08:30:04 -0400

    MAINT Parameters validation for sklearn.metrics.pairwise.manhattan_distances (#26122)

--- sklearn/tests/test_public_functions.py
+++ sklearn/tests/test_public_functions.py
@@ -237,6 +237,7 @@ PARAM_VALIDATION_FUNCTION_LIST = [
     "sklearn.metrics.pairwise.haversine_distances",
     "sklearn.metrics.pairwise.laplacian_kernel",
     "sklearn.metrics.pairwise.linear_kernel",
+    "sklearn.metrics.pairwise.manhattan_distances",
     "sklearn.metrics.pairwise.nan_euclidean_distances",
     "sklearn.metrics.pairwise.paired_cosine_distances",
     "sklearn.metrics.pairwise.paired_euclidean_distances",
