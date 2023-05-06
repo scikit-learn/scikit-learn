56f5a406cf9f330ae3ff05b4ed1899bdf935045e
Gleb Levitski <36483986+glevv@users.noreply.github.com>
2023-05-06 07:58:58 +0000

    lint

--- sklearn/svm/_classes.py
+++ sklearn/svm/_classes.py
@@ -29,14 +29,14 @@ def _validate_dual_parameter(dual, loss, penalty, multi_class, X):
                 return True
     # TODO 1.5
     elif self.dual == "warn":
-            warnings.warn(
-                (
-                    "The default value of `dual` will change from `True` to `'auto'` in"
-                    " 1.5. Set the value of `dual` explicitly to suppress the warning."
-                ),
-                FutureWarning,
-            )
-            return True
+        warnings.warn(
+            (
+                "The default value of `dual` will change from `True` to `'auto'` in"
+                " 1.5. Set the value of `dual` explicitly to suppress the warning."
+            ),
+            FutureWarning,
+        )
+        return True
     else:
         return dual
 
