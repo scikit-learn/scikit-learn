67faaf5e6c5bac3dcbf5e071173b4b72774d4412
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2023-05-07 14:50:52 +0200

    MAINT unpack 0-dim scalar in test

--- sklearn/linear_model/tests/test_sag.py
+++ sklearn/linear_model/tests/test_sag.py
@@ -167,7 +167,7 @@ def sag_sparse(
     for epoch in range(n_iter):
         for k in range(n_samples):
             # idx = k
-            idx = int(rng.rand(1) * n_samples)
+            idx = int(rng.rand(1)[0] * n_samples)
             entry = X[idx]
             seen.add(idx)
 
