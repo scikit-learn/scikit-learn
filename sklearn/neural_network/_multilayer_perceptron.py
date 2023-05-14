1bfee0f5a94e9825240c8f7c1805e49a3da9bf7a
Zach Shu <zhongzheng.shu@ibm.com>
2023-05-14 11:37:19 -0400

    test: add additional tests for code coverage

--- sklearn/neural_network/_multilayer_perceptron.py
+++ sklearn/neural_network/_multilayer_perceptron.py
@@ -841,9 +841,6 @@ class BaseMultilayerPerceptron(BaseEstimator, metaclass=ABCMeta):
             )
         return True
 
-    def _validate_input(self, X, y, incremental, reset, sample_weight):
-        raise NotImplementedError("Subclass must implement _validate_input().")
-
 
 class MLPClassifier(ClassifierMixin, BaseMultilayerPerceptron):
     """Multi-layer Perceptron classifier.
