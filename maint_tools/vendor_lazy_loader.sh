960992b82a2a3d9d0064b67a451a2456303f010a
Thomas J. Fan <thomasjpfan@gmail.com>
2023-05-27 16:01:26 -0400

    CLN Remove vendoring code

--- maint_tools/vendor_lazy_loader.sh
+++ maint_tools/vendor_lazy_loader.sh
@@ -3,13 +3,15 @@
 # Vendors https://github.com/scientific-python/lazy_loader/ into sklearn/externals
 
 SHA="2334bd279d40e0dadd3af48fe4ec494d3ce7f47d"
-URL="https://github.com/scientific-python/lazy_loader/archive/$SHA.tar.gz"
+URL="https://github.com/scientific-python/lazy_loader/archive/2334bd279d40e0dadd3af48fe4ec494d3ce7f47d.tar.gz"
+ROOT_DIR=sklearn/externals/_lazy_loader
 
-rm -rf sklearn/externals/_lazy_loader
+rm -rf $ROOT_DIR/*
 
 curl -s -L $URL |
-    tar xvz --strip-components=1 -C sklearn/externals \
+    tar xvz --strip-components=1 -C sklearn/externals/_lazy_loader \
         lazy_loader-$SHA/lazy_loader/__init__.py \
         lazy_loader-$SHA/LICENSE.md
 
-mv sklearn/externals/lazy_loader sklearn/externals/_lazy_loader
+mv $ROOT_DIR/lazy_loader/__init__.py $ROOT_DIR/__init__.py
+rmdir $ROOT_DIR/lazy_loader
