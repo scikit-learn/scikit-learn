412084ef5134710b59d432b2bcf52e9a1b637594
Thomas J. Fan <thomasjpfan@gmail.com>
2023-05-30 18:10:22 -0400

    FIX Use list

--- sklearn/utils/validation.py
+++ sklearn/utils/validation.py
@@ -1990,9 +1990,7 @@ def _get_feature_names(X):
         feature_names = np.asarray(X.columns, dtype=object)
     elif hasattr(X, "__dataframe__"):
         df_protocol = X.__dataframe__()
-        feature_names = np.fromiter(
-            df_protocol.column_names(), dtype=object, count=df_protocol.num_columns()
-        )
+        feature_names = np.asarray(list(df_protocol.column_names()), dtype=object)
 
     if feature_names is None or len(feature_names) == 0:
         return
