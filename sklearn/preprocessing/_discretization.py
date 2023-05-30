b780c37dcf4029380b0f7875aa37d1a7372740b0
jeremiedbb <jeremiedbb@yahoo.fr>
2023-05-30 15:21:08 +0200

    fix doctest

--- sklearn/preprocessing/_discretization.py
+++ sklearn/preprocessing/_discretization.py
@@ -136,7 +136,9 @@ class KBinsDiscretizer(TransformerMixin, BaseEstimator):
     ...      [-1, 2, -3, -0.5],
     ...      [ 0, 3, -2,  0.5],
     ...      [ 1, 4, -1,    2]]
-    >>> est = KBinsDiscretizer(n_bins=3, encode='ordinal', strategy='uniform')
+    >>> est = KBinsDiscretizer(
+    ...     n_bins=3, encode='ordinal', strategy='uniform', subsample=None
+    ... )
     >>> est.fit(X)
     KBinsDiscretizer(...)
     >>> Xt = est.transform(X)
