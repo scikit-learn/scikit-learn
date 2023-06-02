d008661bdb2c0dd3a994c1e5c5f1901a16a87428
jeremiedbb <jeremiedbb@yahoo.fr>
2023-06-02 17:34:21 +0200

    add comment for partial_fit and test for _is_fitted

--- sklearn/base.py
+++ sklearn/base.py
@@ -1124,9 +1124,12 @@ def _fit_context(*, prefer_skip_nested_validation):
         @functools.wraps(fit_method)
         def wrapper(estimator, *args, **kwargs):
             global_skip_validation = get_config()["skip_parameter_validation"]
+
+            # we don't want to validate again for each call to partial_fit
             partial_fit_and_fitted = (
                 fit_method.__name__ == "partial_fit" and _is_fitted(estimator)
             )
+
             if not global_skip_validation and not partial_fit_and_fitted:
                 estimator._validate_params()
 
