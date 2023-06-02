f907c0fac67cd4d416ce2f294f22f76b45c37ca7
Stefanie Senger <stefanie.senger@posteo.de>
2023-06-02 15:47:55 +0200

    suggested improvements after review

--- examples/ensemble/plot_adaboost_multiclass.py
+++ examples/ensemble/plot_adaboost_multiclass.py
@@ -4,9 +4,13 @@ Multi-class AdaBoosted Decision Trees
 =====================================
 
 This example shows how boosting can improve prediction accuracy on a multi-class
-problem by reproducing Figure 1 of [1].
+problem. It reproduces the experiment depicted by Figure 1 in [1].
 
-- [1] J. Zhu, H. Zou, S. Rosset, T. Hastie, "Multi-class AdaBoost", 2009.
+.. topic:: References:
+    .. [1] J. Zhu, H. Zou, S. Rosset, T. Hastie,
+       `"Multi-class AdaBoost"
+       <https://hastie.su.domains/Papers/samme.pdf>`_,
+       2009.
 """
 
 # Noel Dawe <noel.dawe@gmail.com>
@@ -22,85 +26,105 @@ problem by reproducing Figure 1 of [1].
 from sklearn.datasets import make_gaussian_quantiles
 
 X, y = make_gaussian_quantiles(
-    n_samples=13000, n_features=10, n_classes=3, random_state=1
+    n_samples=13_000, n_features=10, n_classes=3, random_state=1
 )
 
 # %%
-# We're splitting the data in train and test set. 10000 samples are used for
-# training, 3000 for test.
-n_split = 3000
+# We split the dataset into 2 sets: 10,000 samples are used for training and
+# 3,000 for test.
+from sklearn.model_selection import train_test_split
 
-X_train, X_test = X[:n_split], X[n_split:]
-y_train, y_test = y[:n_split], y[n_split:]
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, train_size=10_000, shuffle=False
+)
 
 # %%
-# Training AdaBoostClassifier with two different algorithms
-# ---------------------------------------------------------
+# Training `AdaBoostClassifier` with two different algorithms
+# -----------------------------------------------------------
 # Next, we compare the performance of the `SAMME` and `SAMME.R` algorithms. Note
-# that in :class:`ensemble.AdaBoostClassifier` the default value for the
-# `algorithm` parameter is `SAMME.R`. `SAMME.R` uses the probability estimates
-# to update the additive model, while `SAMME` uses the class labels predicted by
-# the base estimators (:class:`tree.DecisionTreeClassifier`) to update the
-# sample weights. They both combine the estimators' predictions through weighted
-# voting.
-from sklearn.ensemble import AdaBoostClassifier
+# that in :class:`~sklearn.ensemble.AdaBoostClassifier` the default value for
+# the `algorithm` parameter is `SAMME.R`. `SAMME.R` uses the probability
+# estimates to update the additive model, while `SAMME` uses the class labels
+# predicted by the base estimators (i.e.
+# :class:`~sklearn.tree.DecisionTreeClassifier`) to update the sample weights.
+# They both combine the estimators' predictions through weighted voting.
+#
+# Here, we define the weak learner as a
+# :class:`~sklearn.tree.DecisionTreeClassifier` and set the maximum number of
+# leaves to 4. In a real setting, this parameter should be tuned. We set it to a
+# rather low value to limit the runtime of the example.
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import AdaBoostClassifier
+import time
 
-bdt_real = AdaBoostClassifier(
-    DecisionTreeClassifier(max_depth=2),
+weak_learner = DecisionTreeClassifier(max_depth=1, max_leaf_nodes=4)
+
+start = time.time()
+samme_r = AdaBoostClassifier(
+    estimator=weak_learner,
     n_estimators=300,
-    learning_rate=1,
     algorithm="SAMME.R",
-)
+    random_state=42,
+).fit(X_train, y_train)
+end = time.time()
+print(f"Training time of SAMME.R algorithm: {end - start:.3f}s")
 
-bdt_discrete = AdaBoostClassifier(
-    DecisionTreeClassifier(max_depth=2),
+start = time.time()
+samme = AdaBoostClassifier(
+    estimator=weak_learner,
     n_estimators=300,
-    learning_rate=1.5,
     algorithm="SAMME",
-)
-
-bdt_real.fit(X_train, y_train)
-bdt_discrete.fit(X_train, y_train)
-
-print(bdt_real)
-print(bdt_discrete)
-
-# %%
-# We're calculating the accuracy score of the predicted values against the
-# actual ones. As the example will illustrate, the `SAMME.R` algorithm typically
-# converges faster than `SAMME`, achieving a lower test error with fewer
-# boosting iterations.
-from sklearn.metrics import accuracy_score
-
-real_test_errors = []
-discrete_test_errors = []
-
-for real_test_predict, discrete_test_predict in zip(
-    bdt_real.staged_predict(X_test), bdt_discrete.staged_predict(X_test)
-):
-    real_test_errors.append(1.0 - accuracy_score(real_test_predict, y_test))
-    discrete_test_errors.append(1.0 - accuracy_score(discrete_test_predict, y_test))
+    random_state=42,
+).fit(X_train, y_train)
+end = time.time()
+print(f"Training time of SAMME algorithm: {end - start:.3f}s")
 
 # %%
 # Comparing the Results
 # ---------------------
-# We want to extract the `estimator_errors_` and the `estimator_weights_`
-# from the fitted AdaBoostClassifiers in order to plot them. Unlike `SAMME`, the
-# `SAMME.R` algorithm does not calculate the `estimator_weights_`, because the
-# weights are determined based on the probabilities predicted by the base
-# estimators.
+# We calculate the misclassification error of the additive models at each
+# boosting iteration to assess their performance. To obtain the predictions at
+# each iteration, we use the
+# :func:`~sklearn.esamble._weight_boosting.staged_predict` method, which returns
+# a generator containing the predictions of all the weak learners as the
+# training progresses. The final prediction is determined by majority voting
+# among the weak learners. We compare these predictions to the actual values in
+# `y_test` by calculating the difference. This process allows us to generate the
+# data needed for plotting a learning curve."
+
+from sklearn.metrics import accuracy_score
+
+
+def misclassification_error(y_true, y_pred):
+    return 1 - accuracy_score(y_true, y_pred)
+
+
+samme_r_errors = [
+    misclassification_error(y_test, y_pred) for y_pred in samme_r.staged_predict(X_test)
+]
+samme_errors = [
+    misclassification_error(y_test, y_pred) for y_pred in samme.staged_predict(X_test)
+]
+
+# %%
+# For performance review, we also want to extract the `estimator_errors_` and
+# the `estimator_weights_` from the fitted AdaBoostClassifiers.
+# `estimator_errors_` is an array of weighted errors of each individual weak
+# learner immediately after it has been trained at each boosting iteration.
+# `estimator_weights_` are only calculated in the `SAMME` algorithm, because in
+# `SAMME.R` the estimator weights are updated using the predicted class
+# probabilities instead of the misclassification rate
 #
 # Since boosting might terminate early, leaving some of the `n_estimators`
 # estimators untrained, we want to make sure to only keep attribtes from trained
 # Decision Trees. So, we crop those attribute's arrays to the actual number of
 # trained trees:
-n_trees_discrete = len(bdt_discrete)
-n_trees_real = len(bdt_real)
+n_trees_discrete = len(samme)
+n_trees_real = len(samme_r)
 
-discrete_estimator_errors = bdt_discrete.estimator_errors_[:n_trees_discrete]
-real_estimator_errors = bdt_real.estimator_errors_[:n_trees_real]
-discrete_estimator_weights = bdt_discrete.estimator_weights_[:n_trees_discrete]
+samme_estimator_errors = samme.estimator_errors_[:n_trees_discrete]
+samme_r_estimator_errors = samme_r.estimator_errors_[:n_trees_real]
+samme_r_estimator_weights = samme.estimator_weights_[:n_trees_discrete]
 
 
 # %%
@@ -111,11 +135,11 @@ import matplotlib.pyplot as plt
 plt.figure(figsize=(15, 5))
 
 plt.subplot(131)
-plt.title("Errors on the test set")
-plt.plot(range(1, n_trees_discrete + 1), discrete_test_errors, c="black", label="SAMME")
+plt.title("Classification errors on the test set")
+plt.plot(range(1, n_trees_discrete + 1), samme_errors, c="black", label="SAMME")
 plt.plot(
     range(1, n_trees_real + 1),
-    real_test_errors,
+    samme_r_errors,
     c="black",
     linestyle="dashed",
     label="SAMME.R",
@@ -123,33 +147,37 @@ plt.plot(
 plt.legend()
 plt.ylim(0.18, 0.62)
 plt.ylabel("Test Error")
-plt.xlabel("Number of Trees")
+plt.xlabel("Iterations")
 
 plt.subplot(132)
-plt.title("Classification error while boosting")
+plt.title("Classification errors of individual weak learners \n on the training set")
 plt.plot(
     range(1, n_trees_discrete + 1),
-    discrete_estimator_errors,
+    samme_estimator_errors,
     "b",
     label="SAMME",
     alpha=0.5,
 )
 plt.plot(
-    range(1, n_trees_real + 1), real_estimator_errors, "r", label="SAMME.R", alpha=0.5
+    range(1, n_trees_real + 1),
+    samme_r_estimator_errors,
+    "r",
+    label="SAMME.R",
+    alpha=0.5,
 )
 plt.legend()
 plt.ylabel("Error")
-plt.xlabel("Number of Trees")
-plt.ylim((0.2, max(real_estimator_errors.max(), discrete_estimator_errors.max()) * 1.2))
-plt.xlim((-20, len(bdt_discrete) + 20))
+plt.xlabel("Iterations")
+plt.ylim((0.2, max(samme_r_estimator_errors.max(), samme_estimator_errors.max()) * 1.2))
+plt.xlim((-20, len(samme) + 20))
 
 plt.subplot(133)
-plt.title("Estimator weights for SAMME algorithm")
-plt.plot(range(1, n_trees_discrete + 1), discrete_estimator_weights, "b", label="SAMME")
+plt.title("Estimator weights of individual weak learners \n for SAMME algorithm")
+plt.plot(range(1, n_trees_discrete + 1), samme_r_estimator_weights, "b", label="SAMME")
 plt.legend()
 plt.ylabel("Weight")
-plt.xlabel("Number of Trees")
-plt.ylim((0, discrete_estimator_weights.max() * 1.2))
+plt.xlabel("Iterations")
+plt.ylim((0, samme_r_estimator_weights.max() * 1.2))
 plt.xlim((-20, n_trees_discrete + 20))
 
 # prevent overlapping y-axis labels
@@ -157,9 +185,9 @@ plt.subplots_adjust(wspace=0.25)
 plt.show()
 
 # %%
-# The left plot shows the errors (1 - accuracy) of each algorithm on the test
-# set after each boosting iteration. In the middle plot, the classification
-# errors of the individual estimators are shown. The subplot to the right
-# represents the weights assigned to each estimator (Decision Tree) in the
-# `SAMME` algorithm. For the `SAMME.R` algorithm all trees have a weight of 1,
-# which is therefore not plotted.
+# The left plot shows the majoritarian classification errors (1 - accuracy)
+# of each algorithm on the test set after each boosting iteration. In the middle
+# plot, the classification errors of the individual estimators during the
+# training are shown. The subplot to the right represents the weights assigned
+# to each estimator in the `SAMME` algorithm. For the `SAMME.R`
+# algorithm all trees have a weight of 1, which is therefore not plotted.
