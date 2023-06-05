e31ca930f048916835664e815cef506d7322559a
Stefanie Senger <stefanie.senger@posteo.de>
2023-06-05 19:57:16 +0200

    final modifications

--- examples/bicluster/plot_spectral_biclustering.py
+++ examples/bicluster/plot_spectral_biclustering.py
@@ -4,13 +4,14 @@ A demo of the Spectral Biclustering algorithm
 =============================================
 
 This example demonstrates how to generate a checkerboard dataset and bicluster
-it using the `SpectralBiclustering` algorithm. The Spectral Biclustering
-algorithm is specifically designed to cluster data by simultaneously considering
-both the rows (samples) and columns (features) of a matrix. It aims to identify
-patterns not only between samples but also within subsets of samples, allowing
-for the detection of localized structure within the data. This makes spectral
-biclustering particularly well-suited for datasets where the order or
-arrangement of features is fixed, such as in images, time series, or genomes.
+it using the :class:`~sklearn.cluster.SpectralBiclustering` algorithm. The
+spectral biclustering algorithm is specifically designed to cluster data by
+simultaneously considering both the rows (samples) and columns (features) of a
+matrix. It aims to identify patterns not only between samples but also within
+subsets of samples, allowing for the detection of localized structure within the
+data. This makes spectral biclustering particularly well-suited for datasets
+where the order or arrangement of features is fixed, such as in images, time
+series, or genomes.
 
 The data is generated, then shuffled and passed to the Spectral Biclustering
 algorithm. The rows and columns of the shuffled matrix are then rearranged to
@@ -41,7 +42,7 @@ data, rows, columns = make_checkerboard(
 
 plt.matshow(data, cmap=plt.cm.Blues)
 plt.title("Original dataset")
-plt.show()
+_ = plt.show()
 
 # %%
 # We shuffle the data and the goal is to reconstruct it afterwards using
@@ -54,16 +55,17 @@ row_idx_shuffled = rng.permutation(data.shape[0])
 col_idx_shuffled = rng.permutation(data.shape[1])
 
 # %%
-# We will redefine the shuffled data and plot it. We observe that we lost the
+# We redefine the shuffled data and plot it. We observe that we lost the
 # strucuture of original data matrix.
 data = data[row_idx_shuffled][:, col_idx_shuffled]
 
 plt.matshow(data, cmap=plt.cm.Blues)
 plt.title("Shuffled dataset")
+_ = plt.show()
 
 # %%
 # Fitting `SpectralBiclustering`
-# -----------------------------
+# ------------------------------
 # We fit the model and compare the obtained clusters with the ground truth. Note
 # that when creating the model we specify the same number of clusters that we
 # used to create the dataset (`n_clusters = (4, 3)`), which will contribute to
@@ -99,13 +101,14 @@ reordered_data = reordered_rows[:, np.argsort(model.column_labels_)]
 
 plt.matshow(reordered_data, cmap=plt.cm.Blues)
 plt.title("After biclustering; rearranged to show biclusters")
+_ = plt.show()
 
 # %%
 # As a last step, we want to demonstrate the relationships between the row
 # and column labels assigned by the model. Therefore, we create a grid with
-# :func:`numpy.outer`, which takes the sorted `row_labels_` and `column_labels_` and
-# adds 1 to each to ensure that the labels start from 1 instead of 0 for better
-# visualization.
+# :func:`numpy.outer`, which takes the sorted `row_labels_` and `column_labels_`
+# and adds 1 to each to ensure that the labels start from 1 instead of 0 for
+# better visualization.
 plt.matshow(
     np.outer(np.sort(model.row_labels_) + 1, np.sort(model.column_labels_) + 1),
     cmap=plt.cm.Blues,
