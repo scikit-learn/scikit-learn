71f4048cc8054aab9cefde63a4ac6e30cb776c1d
Stefanie Senger <stefanie.senger@posteo.de>
2023-06-05 11:53:05 +0200

    changes according to review

--- examples/bicluster/plot_spectral_biclustering.py
+++ examples/bicluster/plot_spectral_biclustering.py
@@ -4,11 +4,17 @@ A demo of the Spectral Biclustering algorithm
 =============================================
 
 This example demonstrates how to generate a checkerboard dataset and bicluster
-it using the Spectral Biclustering algorithm.
+it using the `SpectralBiclustering` algorithm. The Spectral Biclustering
+algorithm is specifically designed to cluster data by simultaneously considering
+both the rows (samples) and columns (features) of a matrix. It aims to identify
+patterns not only between samples but also within subsets of samples, allowing
+for the detection of localized structure within the data. This makes spectral
+biclustering particularly well-suited for datasets where the order or
+arrangement of features is fixed, such as in images, time series, or genomes.
 
 The data is generated, then shuffled and passed to the Spectral Biclustering
 algorithm. The rows and columns of the shuffled matrix are then rearranged to
-plot the biclusters found by the algorithm.
+plot the biclusters found.
 """
 
 # Author: Kemal Eren <kemal@kemaleren.com>
@@ -25,7 +31,6 @@ plot the biclusters found by the algorithm.
 #
 # As you can see, the data is distributed over 12 cluster cells and is
 # relatively well distinguishable.
-
 from sklearn.datasets import make_checkerboard
 from matplotlib import pyplot as plt
 
@@ -39,45 +44,45 @@ plt.title("Original dataset")
 plt.show()
 
 # %%
-# We're now going to shuffle the data. The goal is to reconstruct it afterwards
-# using `SpectralBiclustering`.
-
+# We shuffle the data and the goal is to reconstruct it afterwards using
+# :class:`~sklearn.bicluster.SpectralBiclustering`.
 import numpy as np
 
 # Creating lists of shuffled row and column indices
 rng = np.random.RandomState(0)
-row_idx = rng.permutation(data.shape[0])
-col_idx = rng.permutation(data.shape[1])
+row_idx_shuffled = rng.permutation(data.shape[0])
+col_idx_shuffled = rng.permutation(data.shape[1])
 
 # %%
-# We will redefine the shuffled data and plot it.
-data = data[row_idx][:, col_idx]
+# We will redefine the shuffled data and plot it. We observe that we lost the
+# strucuture of original data matrix.
+data = data[row_idx_shuffled][:, col_idx_shuffled]
 
 plt.matshow(data, cmap=plt.cm.Blues)
 plt.title("Shuffled dataset")
-plt.show()
 
 # %%
-# Fitting SpectralBiclustering
-# ----------------------------
-# We fit the model and compare the obtained clusters with the ground truth
-# biclusters (our artifical dataset). Note that when instanciating the
-# model we specify the same number of clusters we used to create the dataset
-# (`n_clusters = (4, 3)`), which will contribute to good results.
-
+# Fitting `SpectralBiclustering`
+# -----------------------------
+# We fit the model and compare the obtained clusters with the ground truth. Note
+# that when creating the model we specify the same number of clusters that we
+# used to create the dataset (`n_clusters = (4, 3)`), which will contribute to
+# obtain a good result.
 from sklearn.cluster import SpectralBiclustering
 from sklearn.metrics import consensus_score
 
 model = SpectralBiclustering(n_clusters=n_clusters, method="log", random_state=0)
 model.fit(data)
 
-# Calculating the similarity of two sets of biclusters
-score = consensus_score(model.biclusters_, (rows[:, row_idx], columns[:, col_idx]))
-print("consensus score: {:.1f}".format(score))
+# Compute the similarity of two sets of biclusters
+score = consensus_score(
+    model.biclusters_, (rows[:, row_idx_shuffled], columns[:, col_idx_shuffled])
+)
+print(f"consensus score: {score:.1f}")
 
 # %%
-# The score is between 0 and 1. It shows the quality of the biclustering
-# results.
+# The score is between 0 and 1, where 1 corresponds to a perfect matching. It
+# shows the quality of the biclustering.
 
 # %%
 # Plotting results
@@ -88,28 +93,27 @@ print("consensus score: {:.1f}".format(score))
 # range from 0 to 2, representing a total of 4 clusters per row and 3 clusters
 # per column.
 
-fit_data = data[np.argsort(model.row_labels_)]
-fit_data = fit_data[:, np.argsort(model.column_labels_)]
+# Reordering first the rows and then the columns.
+reordered_rows = data[np.argsort(model.row_labels_)]
+reordered_data = reordered_rows[:, np.argsort(model.column_labels_)]
 
-plt.matshow(fit_data, cmap=plt.cm.Blues)
+plt.matshow(reordered_data, cmap=plt.cm.Blues)
 plt.title("After biclustering; rearranged to show biclusters")
-plt.show()
-
 
 # %%
 # As a last step, we want to demonstrate the relationships between the row
 # and column labels assigned by the model. Therefore, we create a grid with
-# `np.outer()`, which takes the sorted `row_labels_` and `column_labels_` and
+# :func:`numpy.outer`, which takes the sorted `row_labels_` and `column_labels_` and
 # adds 1 to each to ensure that the labels start from 1 instead of 0 for better
 # visualization.
-#
-# The outer product of the row and column label vectors show a representation
-# of the checkerboard structure, where different combinations of row and column
-# labels are represented by different shades of blue.
-
 plt.matshow(
     np.outer(np.sort(model.row_labels_) + 1, np.sort(model.column_labels_) + 1),
     cmap=plt.cm.Blues,
 )
 plt.title("Checkerboard structure of rearranged data")
 plt.show()
+
+# %%
+# The outer product of the row and column label vectors shows a representation
+# of the checkerboard structure, where different combinations of row and column
+# labels are represented by different shades of blue.
