45e650ae53abc25502d21b6adbdace5fa57d5bdd
Stefanie Senger <stefanie.senger@posteo.de>
2023-06-12 14:52:05 +0200

    without samme.r

--- examples/ensemble/plot_adaboost_multiclass.py
+++ examples/ensemble/plot_adaboost_multiclass.py
@@ -3,14 +3,16 @@
 Multi-class AdaBoosted Decision Trees
 =====================================
 
-This example shows how boosting can improve prediction accuracy on a multi-class
-problem. It reproduces the experiment depicted by Figure 1 in [1]_.
+This example shows how boosting can improve the prediction accuracy on a
+multi-class problem. It reproduces a similar experiment as depicted by Figure 1
+in [1]_.
 
 .. topic:: References:
-    .. [1] J. Zhu, H. Zou, S. Rosset, T. Hastie,
-       `"Multi-class AdaBoost"
-       <https://hastie.su.domains/Papers/samme.pdf>`_,
-       2009.
+
+    .. [1] :doi:`J. Zhu, H. Zou, S. Rosset, T. Hastie, "Multi-class adaboost."
+           Statistics and its Interface 2.3 (2009): 349-360.
+           <10.4310/SII.2009.v2.n3.a8>`
+
 """
 
 # Noel Dawe <noel.dawe@gmail.com>
@@ -26,73 +28,54 @@ problem. It reproduces the experiment depicted by Figure 1 in [1]_.
 from sklearn.datasets import make_gaussian_quantiles
 
 X, y = make_gaussian_quantiles(
-    n_samples=13_000, n_features=10, n_classes=3, random_state=1
+    n_samples=2_000, n_features=10, n_classes=3, random_state=1
 )
 
 # %%
-# We split the dataset into 2 sets: 10,000 samples are used for training and
-# 3,000 for test.
+# We split the dataset into 2 sets: 70 percent of the samples are used for
+# training and the remaining 30 percent for testing.
 from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=10_000)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, train_size=0.7, random_state=42
+)
 
 # %%
-# Training `AdaBoostClassifier` with two different algorithms
-# -----------------------------------------------------------
-# Next, we compare the performance of the `SAMME` and `SAMME.R` algorithms. Note
-# that in :class:`~sklearn.ensemble.AdaBoostClassifier` the default value for
-# the `algorithm` parameter is `SAMME.R`. `SAMME.R` uses the probability
-# estimates to update the additive model, while `SAMME` uses the class labels
-# predicted by the base estimators (i.e.
-# :class:`~sklearn.tree.DecisionTreeClassifier`) to update the sample weights.
-# They both combine the estimators' predictions through weighted voting.
+# Training `AdaBoostClassifier`
+# -----------------------------
+# We train the estimator, that utilizes boosting to improve classification
+# accuracy. Boosting is a method designed to train weak learners (base
+# estimators) that learn from their predecessor's mistakes.
 #
 # Here, we define the weak learner as a
 # :class:`~sklearn.tree.DecisionTreeClassifier` and set the maximum number of
 # leaves to 8. In a real setting, this parameter should be tuned. We set it to a
 # rather low value to limit the runtime of the example.
+#
+# The `SAMME` algorithm build into the
+# :class:`~sklearn.ensemble.AdaBoostClassifier` then uses the class labels
+# predicted by the base estimators to update the sample weights. The base
+# estimators' predictions are combined through weighted voting.
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import AdaBoostClassifier
-import time
 
-weak_learner = DecisionTreeClassifier(max_depth=1, max_leaf_nodes=8)
-n_estimators = 600
+weak_learner = DecisionTreeClassifier(max_leaf_nodes=8)
+n_estimators = 300
 
-start = time.time()
-samme_r = AdaBoostClassifier(
-    estimator=weak_learner,
-    n_estimators=n_estimators,
-    algorithm="SAMME.R",
-    random_state=42,
-).fit(X_train, y_train)
-end = time.time()
-print(f"Training time of SAMME.R algorithm: {end - start:.3f}s")
-
-start = time.time()
-samme = AdaBoostClassifier(
+adaboost_clf = AdaBoostClassifier(
     estimator=weak_learner,
     n_estimators=n_estimators,
     algorithm="SAMME",
     random_state=42,
 ).fit(X_train, y_train)
-end = time.time()
-print(f"Training time of SAMME algorithm: {end - start:.3f}s")
-
 # %%
 # Analysis
 # --------
-# Convergence of the additive AdaBoostClassifier
-# **********************************************
-# We calculate the misclassification error of the additive models at each
-# boosting iteration to assess their performance. To obtain the predictions at
-# each iteration, we use the
-# :func:`~sklearn.esamble._weight_boosting.staged_predict` method, which returns
-# a generator containing the predictions of all the weak learners during the
-# training progresses. The final prediction is determined by majority vote
-# among the weak learners. We compare these predictions to the actual values in
-# `y_test` by calculating the difference. This process allows us to generate the
-# data needed for plotting a learning curve.
-
+# Convergence of the AdaBoostClassifier
+# *************************************
+# To demonstrate how boosting can increase the accuracy, we compare the
+# `misclassification_error` of the :class:`~sklearn.tree.DecisionTreeClassifier`,
+# as a baseline score, with the `misclassification_error` of the boosted Trees.
 from sklearn.metrics import accuracy_score
 
 
@@ -100,58 +83,77 @@ def misclassification_error(y_true, y_pred):
     return 1 - accuracy_score(y_true, y_pred)
 
 
+weak_learners_misclassification_error = misclassification_error(
+    y_test, weak_learner.fit(X_train, y_train).predict(X_test)
+)
+
+print(
+    "DecisionTreeClassifier's misclassification_error:"
+    f" {weak_learners_misclassification_error}"
+)
+
+# %%
+# After training the :class:`~sklearn.tree.DecisionTreeClassifier` model, the
+# achieved error, which is less than 0.66, surpasses the expected value that
+# would have been obtained by random guessing alone.
+#
+# Now, we calculate the misclassification error of the additive model at each
+# boosting iteration to assess it's performance. To obtain the predictions at
+# each iteration, we use the
+# :func:`~sklearn.ensemble._weight_boosting.staged_predict` method which returns
+# a generator containing the predictions of all the weak learners during the
+# training progresses. The final prediction is determined by majority vote among
+# the weak learners. We compare these predictions to the actual values in
+# `y_test` by calculating the difference. This process allows us to generate the
+# data needed for plotting a learning curve.
 import pandas as pd
 
 boosting_errors = pd.DataFrame(
     {
         "Number of trees": range(1, n_estimators + 1),
-        "SAMME.R": [
+        "AdaBoost": [
             misclassification_error(y_test, y_pred)
-            for y_pred in samme_r.staged_predict(X_test)
-        ],
-        "SAMME": [
-            misclassification_error(y_test, y_pred)
-            for y_pred in samme.staged_predict(X_test)
+            for y_pred in adaboost_clf.staged_predict(X_test)
         ],
     }
 ).set_index("Number of trees")
 ax = boosting_errors.plot()
 ax.set_ylabel("Misclassification error on test set")
-ax.set_title("Convergence of AdaBoost algorithms")
+ax.set_title("Convergence of AdaBoost algorithm")
 
 # %%
-# The plot shows the majoritarian classification errors (1 - accuracy) of each
-# algorithm on the test set after each boosting iteration.
+# The plot shows the classification error, i.e. (1 - accuracy), on the test
+# set after each boosting iteration. We can see that the error of the boosted
+# trees converges at approximately 0.3 after 50 trees has been trained, thus
+# archiving much higher accuracy than training a single tree.
 #
-# The misclassification error jitters more with the `SAMME` algorithm than with
-# `SAMME.R`, because it uses the discrete outputs of the weak learners to train
-# the boosted model.
+# The misclassification error jitters because it uses the discrete outputs of
+# the weak learners to train the boosted model.
 #
-# The convergence of AdaBoostClassifier is mainly influenced by the
-# `learning_rate`, the number of weak learners used (`n_estimators`), and the
-# expressivity of the weak learners (e.g. `max_leaf_nodes`).
+# The convergence of :class:`~sklearn.ensemble.AdaBoostClassifier` is mainly
+# influenced by the `learning_rate`, the number of weak learners used
+# (`n_estimators`), and the expressivity of the weak learners (e.g.
+# `max_leaf_nodes`).
 
 # %%
 # Errors of the Weak Learners
 # ***************************
-# We also want to extract the `estimator_errors_` and
-# the `estimator_weights_` from the fitted AdaBoostClassifiers.
+# We also want to extract the `estimator_errors_` and the `estimator_weights_`
+# from the fitted :class:`~sklearn.ensemble.AdaBoostClassifier`.
 # `estimator_errors_` is an array of weighted errors of each individual weak
-# learner immediately after it has been trained at each boosting iteration.
-# `estimator_weights_` are only calculated in the `SAMME` algorithm, because in
-# `SAMME.R` the estimator weights are updated using the predicted class
-# probabilities instead of the misclassification rate
+# learner immediately after it has been trained at each boosting iteration,
+# while the `estimator_weights_` represents the corresponding weights.
 #
 # Since boosting might terminate early, leaving some of the `n_estimators`
 # estimators untrained, we want to make sure to only keep attribtes from trained
 # Decision Trees. So, we crop those attribute's arrays to the actual number of
-# trained trees (using `[:len(samme)]`):
+# trained trees (using `[:len(adaboost_clf)]`):
 
 weak_learners_info = pd.DataFrame(
     {
         "Number of trees": range(1, n_estimators + 1),
-        "Errors": samme.estimator_errors_[: len(samme)],
-        "Weights": samme.estimator_weights_[: len(samme)],
+        "Errors": adaboost_clf.estimator_errors_[: len(adaboost_clf)],
+        "Weights": adaboost_clf.estimator_weights_[: len(adaboost_clf)],
     }
 ).set_index("Number of trees")
 
@@ -163,10 +165,10 @@ axs[0, 0].set_title("Weak learner's training error")
 axs[0, 1].set_ylabel("Weight")
 axs[0, 1].set_title("Weak learner's weight")
 fig = axs[0, 0].get_figure()
-fig.suptitle("Weak learner's errors and weights for SAMME")
+fig.suptitle("Weak learner's errors and weights for the AdaBoostClassifier")
 fig.tight_layout()
 
 # %%
 # In the left plot, the classification errors of the individual estimators
 # during the training are shown. The plot to the right represents the weights
-# assigned to each estimator in the `SAMME` algorithm.
+# assigned to each base estimator.
