3f2046929a61f5b00d2308141c3f8e0616e35dc7
Kushan Sharma <50699013+greyisbetter@users.noreply.github.com>
2023-06-14 16:19:51 +0530

    removed little of docstrings

--- examples/miscellaneous/sklearn_is_fitted.py
+++ examples/miscellaneous/sklearn_is_fitted.py
@@ -20,102 +20,75 @@ the presence of the `_is_fitted` attribute. It can be used internally
 @author: Kushan
 """
 
-# %%
-# An example custom estimator implementing a simple classifier
-################################################################
-# This code snippet defines a custom estimator class called `CustomEstimator`
-# that extends the `BaseEstimator` and `ClassifierMixin` classes from 
-# scikit-learn. It showcases the usage of the `__sklearn_is_fitted__` method 
-# and the `check_is_fitted` utility function as internal developer APIs.
-#
-# Example usage
-#
-# The provided code demonstrates the usage of the `CustomEstimator` 
-# class by fitting the estimator to a sample dataset, making predictions, 
-# and calculating the score.
-#
-# X = ...features
-# y = ...labels
-# Create an instance of the CustomEstimator
-# `estimator = CustomEstimator()`
-# try to predict before data was fitted
-# `estimator.predict(X)` Output: NotFittedError
-# check if the estimater is fitted or not
-# `estimator.__sklearn_is_fitted__()`
-# Now fit the estimator to the training data
-# `estimator.fit(X, y)`
-# Perform predictions on the same dataset
-# `predictions = estimator.predict(X)`
-# Calculate the score of the estimator on the training data
-# `score = estimator.score(X, y)`
-# `estimator.__sklearn_is_fitted__()` Output: True
-
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.validation import check_is_fitted
 
 class CustomEstimator(BaseEstimator, ClassifierMixin):
+    """Custom estimator implementing a simple classifier."""
+    
     def __init__(self, parameter=1):
         self.parameter = parameter
 
     def fit(self, X, y):
         """
         Fit the estimator to the training data.
+        
         Parameters:
         - X : array-like of shape (n_samples, n_features)
             The input samples.
         - y : array-like of shape (n_samples,)
             The target values.
+            
         Returns:
         - self : object
             Returns self.
         """
         self.classes_ = sorted(set(y))
-        # Custom attribute to track if the estimator is fitted
-        self._is_fitted = True  
+        self._is_fitted = True  # Custom attribute to track if the estimator is fitted
         return self
 
     def predict(self, X):
         """
         Perform predictions using the fitted estimator.
+        
         Parameters:
         - X : array-like of shape (n_samples, n_features)
             The input samples.
+            
         Returns:
         - predictions : list
             The predicted labels for the input samples.
-            
-        The predict method will first check if the estimator is fitted 
-        or not then it will perform the scoring logic and return the score 
-        of prediction. As like predict method if it isn't fitted then it will
-        return `NotFittedError` msg.
+        
+        Raises:
+        - NotFittedError: If the estimator is not fitted.
         """
         check_is_fitted(self, '_is_fitted')
-        # Perform prediction logic
-        predictions = [self.classes_[0]] * len(X)
+        predictions = [self.classes_[0]] * len(X)  # Perform prediction logic
         return predictions
 
     def score(self, X, y):
         """
         Calculate the score of the estimator.
+        
         Parameters:
         - X : array-like of shape (n_samples, n_features)
             The input samples.
         - y : array-like of shape (n_samples,)
             The target values.
+            
         Returns:
         - score : float
             The calculated score.
-            
-        The score method will also first check if the estimator is fitted 
-        or not then it will perform the scoring logic and return the score 
-        of prediction. As like predict method if it isn't fitted then it will
-        return `NotFittedError` msg.
+        
+        Raises:
+        - NotFittedError: If the estimator is not fitted.
         """
         check_is_fitted(self, '_is_fitted')
         # Perform scoring logic
         return 0.5
-    
+
     def __sklearn_is_fitted__(self):
-        """Internal developer API. Returns True if the estimator is fitted, 
-        False otherwise."""
+        """
+        Internal developer API. Returns True if the estimator is fitted, False otherwise.
+        """
         return hasattr(self, '_is_fitted')
