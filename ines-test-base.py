bad98120a782bf35a86c3d538b638bac765eedcb
ines.ibnukhsein <ines.ibnukhsein@dataiku.com>
2023-06-21 14:49:44 +0200

    add test_repr_html test

--- ines-test-base.py
+++ ines-test-base.py
@@ -1,13 +1,17 @@
 import pytest
 from sklearn.base import BaseEstimator
 from sklearn.naive_bayes import GaussianNB
-from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.cluster import KMeans
-from packaging.version import parse as parse_version
-import sklearn
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import SelectKBest
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.compose import TransformedTargetRegressor
+from sklearn.compose import ColumnTransformer
 
 class CustomEstimator(BaseEstimator):
     _doc_link = "https://custom_link/{major}.{minor}/docs/{estimator_name}.html"
@@ -50,22 +54,53 @@ class CustomEstimator_wrong(BaseEstimator):
         # Implement the prediction logic of your estimator
         # ...
         return X
+    
+    
+# creating a pipeline
+# Sub-pipeline 1
+preprocessing1 = FeatureUnion([
+    ('numeric_pipeline', Pipeline([
+        ('scaler', StandardScaler()),
+        ('pca', PCA(n_components=2))
+    ])),
+    ('categorical_pipeline', Pipeline([
+        ('encoder', OneHotEncoder())
+    ]))
+])
 
+# Sub-pipeline 2
+preprocessing2 = FeatureUnion([
+    ('numeric_pipeline', Pipeline([
+        ('scaler', StandardScaler()),
+        ('pca', PCA(n_components=3))
+    ])),
+    ('categorical_pipeline', Pipeline([
+        ('encoder', OneHotEncoder())
+    ]))
+])
 
-def _local_get_estimator_doc_url(estimator):
-    """Generating a link to the API documentation for a given estimator and scikit-learn version."""
-    version = parse_version(sklearn.__version__)
-    major = version.major
-    minor = version.minor
-    estimator_name = estimator.__class__.__name__
-    estimator_module = ".".join(
-        [_ for _ in estimator.__class__.__module__.split(".") if not _.startswith("_")]
-    )
-    base_url = f"https://scikit-learn.org/{major}.{minor}/modules/generated/"
-    full_url = f"{base_url}{estimator_module}.{estimator_name}.html"
-    return full_url
+# Main pipeline that will contain _VisualBlocks
+pipeline = lambda: Pipeline([
+    ('preprocess1', preprocessing1),
+    ('preprocess2', preprocessing2),
+    ('feature_selection', SelectKBest(k=5)),
+    ('meta_estimator', TransformedTargetRegressor(
+        regressor=GradientBoostingClassifier(),
+        transformer=StandardScaler()
+    )),
+    ('column_transformer', ColumnTransformer([
+        ('numeric_pipeline', Pipeline([
+            ('scaler', StandardScaler()),
+            ('pca', PCA(n_components=2))
+        ]), ['numeric_feature1', 'numeric_feature2']),
+        ('categorical_pipeline', Pipeline([
+            ('encoder', OneHotEncoder())
+        ]), ['categorical_feature1', 'categorical_feature2'])
+    ])),
+    ('estimator', RandomForestClassifier())
+])
 
-@pytest.fixture(params=[BaseEstimator, GaussianNB, StandardScaler, LogisticRegression, SVC, DecisionTreeClassifier, KMeans, CustomEstimator, CustomEstimator_wrong])
+@pytest.fixture(params=[pipeline, GaussianNB, StandardScaler, LogisticRegression, SVC, DecisionTreeClassifier, KMeans, CustomEstimator, CustomEstimator_wrong])
 def estimator(request):
     return request.param()
 
@@ -80,3 +115,13 @@ def test_get_url_link(estimator):
         assert url_link == ""
     else:
         assert url_link.startswith("https://scikit-learn.org/")
+
+def test_repr_html(estimator):
+    html_repr = estimator._repr_html_()
+    # Perform assertions to check the expected behavior
+    if isinstance(estimator, CustomEstimator): 
+        assert "https://custom_link" in html_repr
+    elif isinstance(estimator, CustomEstimator_wrong):
+        assert "<a" not in html_repr
+    else:
+        assert "https://scikit-learn.org/" in html_repr
