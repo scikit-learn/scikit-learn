d8b6bf84ac93ef0b9fbd84cef45a0d12c0819705
Alexandre Landeau <alandeau@MacBook-Pro-de-Alexandre.local>
2023-06-23 18:18:26 +0200

    added test when W is used and not H

--- sklearn/decomposition/tests/test_nmf.py
+++ sklearn/decomposition/tests/test_nmf.py
@@ -974,6 +974,23 @@ def test_nmf_n_components_auto(Estimator):
     assert est._n_components == H.shape[0]
 
 
+@pytest.mark.parametrize("Estimator", [NMF, MiniBatchNMF])
+def test_nmf_w_used_h_not(Estimator):
+    # Tests that non_negative_factorization does not fail
+    # when setting n_components="auto" with W provided but not H
+    # also tests that the inferred n_component value is the right one
+    rng = np.random.RandomState(0)
+    X = rng.random_sample((6, 5))
+    W_init = rng.random_sample((6, 2))
+    est = Estimator(
+        n_components="auto",
+        random_state=0,
+        tol=1e-6,
+    )
+    est.fit_transform(X, W=W_init)
+    assert est._n_components == W_init.shape[1]
+
+
 def test_nmf_non_negative_factorization_n_components_auto():
     # Check that n_components is correctly inferred from
     # the provided custom initialization.
