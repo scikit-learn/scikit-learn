e4088eb2356413f622ca35911f95c59a4e75ffa8
Binesh Bannerjee <binesh_binesh@hotmail.com>
2023-06-23 11:03:22 -0400

    Change to docstring: Simplified initialization cases.

--- sklearn/cluster/_kmeans.py
+++ sklearn/cluster/_kmeans.py
@@ -367,10 +367,8 @@ def k_means(
         n_init consecutive runs in terms of inertia.
 
         When `n_init='auto'`, the number of runs depends on the value of init:
-        10 if using `init='random'`
-        1 if using `init='k-means++'`
-        10 if init is a callable
-        1 if init is array-like.
+        10 if using `init='random'` or `init` is a callable;
+        1 if using `init='k-means++'` or `init` is an array-like.
 
         .. versionadded:: 1.2
            Added 'auto' option for `n_init`.
@@ -1261,10 +1259,8 @@ class KMeans(_BaseKMeans):
         high-dimensional problems (see :ref:`kmeans_sparse_high_dim`).
 
         When `n_init='auto'`, the number of runs depends on the value of init:
-        10 if using `init='random'`
-        1 if using `init='k-means++'`
-        10 if init is a callable
-        1 if init is array-like.
+        10 if using `init='random'` or `init` is a callable;
+        1 if using `init='k-means++'` or `init` is an array-like.
 
         .. versionadded:: 1.2
            Added 'auto' option for `n_init`.
@@ -1802,8 +1798,6 @@ class MiniBatchKMeans(_BaseKMeans):
         When `n_init='auto'`, the number of runs depends on the value of init:
         3 if using `init='random'`
         1 if using `init='k-means++'`
-        3 if init is a callable
-        1 if init is array-like.
 
         .. versionadded:: 1.2
            Added 'auto' option for `n_init`.
