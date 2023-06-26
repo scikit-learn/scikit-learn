0b5807bb09413c4405bc95316f1af40b0c30d7ce
Stefanie Senger <91849487+StefanieSenger@users.noreply.github.com>
2023-06-26 11:47:22 +0200

    Update examples/linear_model/plot_ridge_coeffs.py
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- examples/linear_model/plot_ridge_coeffs.py
+++ examples/linear_model/plot_ridge_coeffs.py
@@ -72,8 +72,7 @@ errors = []
 
 # Train the model with different regularisation strengths
 for a in alphas:
-    clf.set_params(alpha=a)
-    clf.fit(X, y)
+    clf.set_params(alpha=a).fit(X, y)
     coefs.append(clf.coef_)
     errors.append(mean_squared_error(clf.coef_, w))
 # %%
