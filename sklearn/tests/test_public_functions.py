e490da9a718a9916717c141c4a73f1f93a5618a5
Elabonga Atuo <elabongaatuo@gmail.com>
2023-06-27 20:08:26 +0300

    MAINT Parameters validation for sklearn.utils.safe_mask (#26131)
    Co-authored-by: jeremiedbb <jeremiedbb@yahoo.fr>

--- sklearn/tests/test_public_functions.py
+++ sklearn/tests/test_public_functions.py
@@ -285,6 +285,7 @@ PARAM_VALIDATION_FUNCTION_LIST = [
     "sklearn.utils.gen_batches",
     "sklearn.utils.graph.single_source_shortest_path_length",
     "sklearn.utils.resample",
+    "sklearn.utils.safe_mask",
 ]
 
 
