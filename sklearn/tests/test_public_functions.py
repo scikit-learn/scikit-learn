e3590c8d281ef04363831cfdc72b7209e2fbc3b6
Christian Veenhuis <124370897+ChVeen@users.noreply.github.com>
2023-06-27 17:52:59 +0200

    MAINT Parameters validation for graph.single_source_shortest_path_length (#26091)
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>
    Co-authored-by: Jérémie du Boisberranger <34657725+jeremiedbb@users.noreply.github.com>

--- sklearn/tests/test_public_functions.py
+++ sklearn/tests/test_public_functions.py
@@ -282,6 +282,7 @@ PARAM_VALIDATION_FUNCTION_LIST = [
     "sklearn.tree.export_text",
     "sklearn.tree.plot_tree",
     "sklearn.utils.gen_batches",
+    "sklearn.utils.graph.single_source_shortest_path_length",
     "sklearn.utils.resample",
 ]
 
