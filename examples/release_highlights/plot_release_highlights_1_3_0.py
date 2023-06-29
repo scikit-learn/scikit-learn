6efe3a227a31160a24fabd7b52614685089a05f4
jeremiedbb <jeremiedbb@yahoo.fr>
2023-06-29 13:30:48 +0200

    add gamma deviance / infrequent OrdinalEncoder / ValidationCurveDisplay

--- examples/release_highlights/plot_release_highlights_1_3_0.py
+++ examples/release_highlights/plot_release_highlights_1_3_0.py
@@ -45,10 +45,10 @@ or with conda::
 # Originally hosted in the scikit-learn-contrib repository, :class:`cluster.HDBSCAN`
 # has been adpoted into scikit-learn. It's missing a few features from the original
 # implementation which will be added in future releases.
-# By performing :class:`cluster.DBSCAN` over varying epsilon values
-# :class:`cluster.HDBSCAN` finds clusters of varying densities making it more robust to
-# parameter selection than :class:`cluster.DBSCAN`. More details in the
-# :ref:`User Guide <hdbscan>`.
+# By performing a modified version of :class:`cluster.DBSCAN` over multiple epsilon
+# values simultaneously, :class:`cluster.HDBSCAN` finds clusters of varying densities
+# making it more robust to parameter selection than :class:`cluster.DBSCAN`.
+# More details in the :ref:`User Guide <hdbscan>`.
 import numpy as np
 from sklearn.cluster import HDBSCAN
 from sklearn.datasets import load_digits
@@ -97,3 +97,62 @@ y = [0, 0, 1, 1]
 
 tree = DecisionTreeClassifier(random_state=0).fit(X, y)
 tree.predict(X)
+
+# %%
+# New display `model_selection.ValidationCurveDisplay`
+# ----------------------------------------------------
+# :class:`model_selection.ValidationCurveDisplay` is now available to plot results
+# from :func:`model_selection.validation_curve`.
+from sklearn.datasets import make_classification
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import ValidationCurveDisplay
+
+X, y = make_classification(1000, 10, random_state=0)
+
+ValidationCurveDisplay.from_estimator(
+    LogisticRegression(),
+    X,
+    y,
+    param_name="C",
+    param_range=np.geomspace(1e-5, 1e3, num=9),
+    score_type="both",
+    score_name="Accuracy",
+)
+
+# %%
+# Gamma loss for gradient boosting
+# --------------------------------
+# The class :class:`ensemble.HistGradientBoostingRegressor` supports the
+# Gamma deviance loss function via `loss="gamma"`. This loss function is useful for
+# modeling strictly positive targets with a right-skewed distribution.
+import numpy as np
+from sklearn.model_selection import cross_val_score
+from sklearn.datasets import make_low_rank_matrix
+from sklearn.ensemble import HistGradientBoostingRegressor
+
+n_samples, n_features = 500, 10
+rng = np.random.RandomState(0)
+X = make_low_rank_matrix(n_samples, n_features, random_state=rng)
+coef = rng.uniform(low=-10, high=20, size=n_features)
+y = rng.gamma(shape=2, scale=np.exp(X @ coef) / 2)
+gbdt = HistGradientBoostingRegressor(loss="gamma")
+cross_val_score(gbdt, X, y).mean()
+
+# %%
+# Grouping 
+# %%
+# Grouping infrequent categories in :class:`preprocessing.OrdinalEncoder`
+# -----------------------------------------------------------------------
+# Similarly to :class:`preprocessing.OneHotEncoder`, the class
+# :class:`preprocessing.OrdinalEncoder` now supports aggregating infrequent categories
+# into a single output for each feature. The parameters to enable the gathering of
+# infrequent categories are `min_frequency` and `max_categories`.
+# See the :ref:`User Guide <encoder_infrequent_categories>` for more details.
+from sklearn.preprocessing import OrdinalEncoder
+import numpy as np
+
+X = np.array(
+    [["dog"] * 5 + ["cat"] * 20 + ["rabbit"] * 10 + ["snake"] * 3], dtype=object
+).T
+enc = OrdinalEncoder(min_frequency=6).fit(X)
+enc.infrequent_categories_
