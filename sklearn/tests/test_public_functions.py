87941ab497be5834287274b75fae46df5071df52
Yao Xiao <108576690+Charlie-XIAO@users.noreply.github.com>
2023-06-29 18:25:23 +0800

    MAINT Parameters validation for sklearn.utils.gen_even_slices (#26682)
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- sklearn/tests/test_public_functions.py
+++ sklearn/tests/test_public_functions.py
@@ -307,6 +307,8 @@ PARAM_VALIDATION_FUNCTION_LIST = [
     "sklearn.tree.export_text",
     "sklearn.tree.plot_tree",
     "sklearn.utils.gen_batches",
+    "sklearn.utils.gen_even_slices",
+    "sklearn.utils.graph.single_source_shortest_path_length",
     "sklearn.utils.resample",
     "sklearn.utils.safe_mask",
     "sklearn.utils.extmath.randomized_svd",
