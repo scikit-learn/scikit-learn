42524bd42900d8ea5f4a334780387a72c6f9580d
Olivier Grisel <olivier.grisel@ensta.org>
2023-07-03 23:05:47 +0200

    Typo

--- sklearn/utils/extmath.py
+++ sklearn/utils/extmath.py
@@ -228,7 +228,7 @@ def randomized_range_finder(
         (the fastest but numerically unstable when `n_iter` is large, e.g.
         typically 5 or larger), or 'LU' factorization (numerically stable
         but can lose slightly in accuracy). The 'auto' mode applies no
-        normalization if `n_iter` <= 2 and switches to LR otherwise.
+        normalization if `n_iter` <= 2 and switches to LU otherwise.
 
         .. versionadded:: 0.18
 
