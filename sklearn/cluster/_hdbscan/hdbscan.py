17f26794fae78269046574751cc0e9402906c053
shreesha3112 <shreesha3112.com>
2023-07-05 12:58:34 +0530

    added backwards compatibility

--- sklearn/cluster/_hdbscan/hdbscan.py
+++ sklearn/cluster/_hdbscan/hdbscan.py
@@ -475,6 +475,13 @@ class HDBSCAN(ClusterMixin, BaseEstimator):
         :class:`~sklearn.neighbors.BallTree`, then it resolves to use the
         `"brute"` algorithm.
 
+        .. deprecated:: 1.4
+           The `'kdtree'` option was deprecated in version 1.4,
+           and will be renamed to `'kd_tree'` in 1.6.
+
+           The `'balltree'` option was deprecated in version 1.4,
+           and will be renamed to `'ball_tree'` in 1.6.
+
     leaf_size : int, default=40
         Leaf size for trees responsible for fast nearest neighbour queries when
         a KDTree or a BallTree are used as core-distance algorithms. A large
@@ -625,15 +632,12 @@ class HDBSCAN(ClusterMixin, BaseEstimator):
         "metric": [StrOptions(FAST_METRICS | {"precomputed"}), callable],
         "metric_params": [dict, None],
         "alpha": [Interval(Real, left=0, right=None, closed="neither")],
+        # TODO(1.6): Remove "kdtree" and "balltree"  option
         "algorithm": [
             StrOptions(
-                {
-                    "auto",
-                    "brute",
-                    "kd_tree",
-                    "ball_tree",
-                }
-            )
+                {"auto", "brute", "kd_tree", "ball_tree", "kdtree", "balltree"},
+                deprecated={"kdtree", "balltree"},
+            ),
         ],
         "leaf_size": [Interval(Integral, left=1, right=None, closed="left")],
         "n_jobs": [Integral, None],
@@ -759,6 +763,31 @@ class HDBSCAN(ClusterMixin, BaseEstimator):
                 f"min_samples ({self._min_samples}) must be at most the number of"
                 f" samples in X ({X.shape[0]})"
             )
+
+        # TODO(1.6): Remove
+        if self.algorithm == "kdtree":
+            warn(
+                (
+                    "`algorithm='kdtree'`has been deprecated in 1.4 and will be renamed"
+                    " to'kd_tree'`in 1.6. To keep the past behaviour, set"
+                    " `algorithm=kd_tree`."
+                ),
+                FutureWarning,
+            )
+            self.algorithm = "kd_tree"
+
+        # TODO(1.6): Remove
+        if self.algorithm == "balltree":
+            warn(
+                (
+                    "`algorithm='balltree'`has been deprecated in 1.4 and will be"
+                    " renamed to'ball_tree'`in 1.6. To keep the past behaviour, set"
+                    " `algorithm=ball_tree`."
+                ),
+                FutureWarning,
+            )
+            self.algorithm = "ball_tree"
+
         mst_func = None
         kwargs = dict(
             X=X,
--- sklearn/cluster/tests/test_hdbscan.py
+++ sklearn/cluster/tests/test_hdbscan.py
@@ -22,12 +22,27 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.utils import shuffle
 from sklearn.utils._testing import assert_allclose, assert_array_equal
 
+# TODO(1.6): Remove
+filterwarnings_kdtree = pytest.mark.filterwarnings(
+    "ignore:`algorithm='kdtree'`has been deprecated in 1.4 and will be renamed"
+    " to'kd_tree'`in 1.6. To keep the past behaviour, set `algorithm=kd_tree`."
+)
+# TODO(1.6): Remove
+filterwarnings_balltree = pytest.mark.filterwarnings(
+    "ignore:`algorithm='balltree'`has been deprecated in 1.4 and will be renamed"
+    " to'ball_tree'`in 1.6. To keep the past behaviour, set `algorithm=ball_tree`."
+)
+
 n_clusters_true = 3
 X, y = make_blobs(n_samples=200, random_state=10)
 X, y = shuffle(X, y, random_state=7)
 X = StandardScaler().fit_transform(X)
 
 ALGORITHMS = [
+    # TODO(1.6): Remove "kdtree"
+    "kdtree",
+    # TODO(1.6): Remove "balltree"
+    "balltree",
     "kd_tree",
     "ball_tree",
     "brute",
@@ -133,6 +148,10 @@ def test_hdbscan_feature_array():
     assert score >= 0.98
 
 
+# TODO(1.6): Remove "kdtree" warning
+@filterwarnings_kdtree
+# TODO(1.6): Remove "balltree" warning
+@filterwarnings_balltree
 @pytest.mark.parametrize("algo", ALGORITHMS)
 @pytest.mark.parametrize("metric", _VALID_METRICS)
 def test_hdbscan_algorithms(algo, metric):
@@ -149,6 +168,10 @@ def test_hdbscan_algorithms(algo, metric):
         return
 
     ALGOS_TREES = {
+        # TODO(1.6): Remove "kdtree"
+        "kdtree": KDTree,
+        # TODO(1.6): Remove "balltree"
+        "balltree": BallTree,
         "kd_tree": KDTree,
         "ball_tree": BallTree,
     }
@@ -284,7 +307,9 @@ def test_hdbscan_precomputed_non_brute(tree):
         hdb.fit(X)
 
 
-def test_hdbscan_sparse():
+# TODO(1.6): Remove "balltree" from algorithm parameter
+@pytest.mark.parametrize("algorithm", ["balltree", "ball_tree"])
+def test_hdbscan_sparse(algorithm):
     """
     Tests that HDBSCAN works correctly when passing sparse feature data.
     """
@@ -302,9 +327,13 @@ def test_hdbscan_sparse():
 
     msg = "Sparse data matrices only support algorithm `brute`."
     with pytest.raises(ValueError, match=msg):
-        HDBSCAN(metric="euclidean", algorithm="ball_tree").fit(sparse_X)
+        HDBSCAN(metric="euclidean", algorithm=algorithm).fit(sparse_X)
 
 
+# TODO(1.6): Remove "kdtree" warning
+@filterwarnings_kdtree
+# TODO(1.6): Remove "balltree" warning
+@filterwarnings_balltree
 @pytest.mark.parametrize("algorithm", ALGORITHMS)
 def test_hdbscan_centers(algorithm):
     """
@@ -327,7 +356,11 @@ def test_hdbscan_centers(algorithm):
     assert hdb.medoids_.shape[0] == 0
 
 
-def test_hdbscan_allow_single_cluster_with_epsilon():
+# TODO(1.6): Remove "kdtree" warning
+@filterwarnings_kdtree
+# TODO(1.6): Remove "kdtree" from algorithm parameter
+@pytest.mark.parametrize("algorithm", ["kdtree", "kd_tree"])
+def test_hdbscan_allow_single_cluster_with_epsilon(algorithm):
     """
     Tests that HDBSCAN single-cluster selection with epsilon works correctly.
     """
@@ -353,7 +386,7 @@ def test_hdbscan_allow_single_cluster_with_epsilon():
         cluster_selection_epsilon=0.18,
         cluster_selection_method="eom",
         allow_single_cluster=True,
-        algorithm="kd_tree",
+        algorithm=algorithm,
     ).fit_predict(no_structure)
     unique_labels, counts = np.unique(labels, return_counts=True)
     assert len(unique_labels) == 2
@@ -406,7 +439,15 @@ def test_hdbscan_sparse_distances_too_few_nonzero():
         HDBSCAN(metric="precomputed").fit(X)
 
 
-def test_hdbscan_tree_invalid_metric():
+# TODO(1.6): Remove "kdtree" warning
+@filterwarnings_kdtree
+# TODO(1.6): Remove "balltree" warning
+@filterwarnings_balltree
+# TODO(1.6): Remove "kdtree" from kd_tree_algorithm parameter
+@pytest.mark.parametrize("kd_tree_algorithm", ["kdtree", "kd_tree"])
+# TODO(1.6): Remove "balltree" from ball_tree_algorithm parameter
+@pytest.mark.parametrize("ball_tree_algorithm", ["balltree", "ball_tree"])
+def test_hdbscan_tree_invalid_metric(kd_tree_algorithm, ball_tree_algorithm):
     """
     Tests that HDBSCAN correctly raises an error for invalid metric choices.
     """
@@ -418,16 +459,16 @@ def test_hdbscan_tree_invalid_metric():
 
     # Callables are not supported for either
     with pytest.raises(ValueError, match=msg):
-        HDBSCAN(algorithm="kd_tree", metric=metric_callable).fit(X)
+        HDBSCAN(algorithm=kd_tree_algorithm, metric=metric_callable).fit(X)
     with pytest.raises(ValueError, match=msg):
-        HDBSCAN(algorithm="ball_tree", metric=metric_callable).fit(X)
+        HDBSCAN(algorithm=ball_tree_algorithm, metric=metric_callable).fit(X)
 
     # The set of valid metrics for KDTree at the time of writing this test is a
     # strict subset of those supported in BallTree
     metrics_not_kd = list(set(BallTree.valid_metrics()) - set(KDTree.valid_metrics()))
     if len(metrics_not_kd) > 0:
         with pytest.raises(ValueError, match=msg):
-            HDBSCAN(algorithm="kd_tree", metric=metrics_not_kd[0]).fit(X)
+            HDBSCAN(algorithm=kd_tree_algorithm, metric=metrics_not_kd[0]).fit(X)
 
 
 def test_hdbscan_too_many_min_samples():
@@ -531,3 +572,22 @@ def test_labelling_thresholding():
     # and the largest value is exactly MAX_LAMBDA.
     num_noise = condensed_tree["value"] < MAX_LAMBDA
     assert sum(num_noise) == sum(labels == -1)
+
+
+# TODO(1.6): Remove
+def test_hdbscan_warning_on_deprecated_algorithm_name():
+    # Test that warning message is shown when algorithm='kdtree'
+    kdtree_warning_message = (
+        "`algorithm='kdtree'`has been deprecated in 1.4 and will be renamed"
+        " to'kd_tree'`in 1.6. To keep the past behaviour, set `algorithm=kd_tree`."
+    )
+    with pytest.warns(FutureWarning, match=kdtree_warning_message):
+        HDBSCAN(algorithm="kdtree").fit(X)
+
+    # Test that warning message is shown when algorithm='balltree'
+    balltree_warning_message = (
+        "`algorithm='balltree'`has been deprecated in 1.4 and will be renamed"
+        " to'ball_tree'`in 1.6. To keep the past behaviour, set `algorithm=ball_tree`."
+    )
+    with pytest.warns(FutureWarning, match=balltree_warning_message):
+        HDBSCAN(algorithm="balltree").fit(X)
