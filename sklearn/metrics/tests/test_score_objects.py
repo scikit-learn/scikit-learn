1584c5b00a373f30d324a0cfb7d293aec270a3b7
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2023-07-15 13:49:47 +0200

    add non-regression test

--- sklearn/metrics/tests/test_score_objects.py
+++ sklearn/metrics/tests/test_score_objects.py
@@ -52,6 +52,7 @@ from sklearn.metrics._scorer import (
 )
 from sklearn.model_selection import GridSearchCV, cross_val_score, train_test_split
 from sklearn.multiclass import OneVsRestClassifier
+from sklearn.multioutput import ClassifierChain
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import make_pipeline
 from sklearn.svm import LinearSVC
@@ -1349,3 +1350,20 @@ def test_kwargs_without_metadata_routing_error():
     with config_context(enable_metadata_routing=False):
         with pytest.raises(ValueError, match="kwargs is only supported if"):
             scorer(clf, X, y, param="blah")
+
+
+def test_get_scorer_multilabel_indicator():
+    """Check that our scorer deal with multi-label indicator matrices.
+
+    Non-regression test for:
+    https://github.com/scikit-learn/scikit-learn/issues/26817
+    """
+    X, Y = make_multilabel_classification(n_samples=72, n_classes=3, random_state=0)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state=0)
+
+    base_lr = LogisticRegression(solver="lbfgs", random_state=0)
+    chain = ClassifierChain(base_lr, order="random", random_state=0)
+    chain.fit(X_train, Y_train)
+
+    score = get_scorer("average_precision")(chain, X_test, Y_test)
+    assert score > 0.8
