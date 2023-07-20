c61b038d91bc9aef3f332de456ac380994fe4a0f
Stefanie Senger <stefanie.senger@posteo.de>
2023-07-20 11:08:06 +0200

    same for _classes

--- sklearn/svm/_classes.py
+++ sklearn/svm/_classes.py
@@ -114,13 +114,14 @@ class LinearSVC(LinearClassifierMixin, SparseCoefMixin, BaseEstimator):
         vector. The intercept becomes intercept_scaling * synthetic feature
         weight. Note that liblinear internally penalizes the intercept,
         treating it like any other term in the feature vector. To reduce the
-        impact of the regularization on the intercept, the `intercept_scaling`
-        parameter can be set to a value greater than 1. Then, the weights
-        become `[w_x_1, ..., w_x_n, w_intercept*intercept_scaling]`, where
-        `w_x_1, ..., w_x_n` represent the feature weights and the intercept
-        weight is scaled by `intercept_scaling`. This scaling allows the
-        intercept term to have a different regularization behavior compared to
-        the other features.
+        impact of the regularization on the intercept, the
+        ``intercept_scaling`` parameter can be set to a value greater than 1;
+        the higher the value of ``intercept_scaling``, the lower the impact of
+        regularization on it. Then, the weights become `[w_x_1, ..., w_x_n,
+        w_intercept*intercept_scaling]`, where `w_x_1, ..., w_x_n` represent
+        the feature weights and the intercept weight is scaled by
+        `intercept_scaling`. This scaling allows the intercept term to have a
+        different regularization behavior compared to the other features.
 
     class_weight : dict or 'balanced', default=None
         Set the parameter C of class i to ``class_weight[i]*C`` for
@@ -413,13 +414,14 @@ class LinearSVR(RegressorMixin, LinearModel):
         vector. The intercept becomes intercept_scaling * synthetic feature
         weight. Note that liblinear internally penalizes the intercept,
         treating it like any other term in the feature vector. To reduce the
-        impact of the regularization on the intercept, the `intercept_scaling`
-        parameter can be set to a value greater than 1. Then, the weights
-        become `[w_x_1, ..., w_x_n, w_intercept*intercept_scaling]`, where
-        `w_x_1, ..., w_x_n` represent the feature weights and the intercept
-        weight is scaled by `intercept_scaling`. This scaling allows the
-        intercept term to have a different regularization behavior compared to
-        the other features.
+        impact of the regularization on the intercept, the
+        ``intercept_scaling`` parameter can be set to a value greater than 1;
+        the higher the value of ``intercept_scaling``, the lower the impact of
+        regularization on it. Then, the weights become `[w_x_1, ..., w_x_n,
+        w_intercept*intercept_scaling]`, where `w_x_1, ..., w_x_n` represent
+        the feature weights and the intercept weight is scaled by
+        `intercept_scaling`. This scaling allows the intercept term to have a
+        different regularization behavior compared to the other features.
 
     dual : "auto" or bool, default=True
         Select the algorithm to either solve the dual or primal
