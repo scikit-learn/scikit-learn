dcb750def07718621178b531dbcf4f573868190b
Stefanie Senger <stefanie.senger@posteo.de>
2023-07-20 11:06:12 +0200

    changed description intercept_scaling

--- sklearn/svm/_base.py
+++ sklearn/svm/_base.py
@@ -1105,12 +1105,14 @@ def _fit_liblinear(
         used in calculations (i.e. data is expected to be already centered).
 
     intercept_scaling : float
-        Liblinear internally penalizes the intercept, treating it like any other
-        term in the feature vector. To reduce the impact of the regularization
-        on the intercept, the `intercept_scaling` parameter can be set to a
-        value greater than 1. Then, the weights become `[w_x_1, ..., w_x_n,
-        w_intercept*intercept_scaling]`, where `w_x_1, ..., w_x_n` represent the
-        feature weights and the intercept weight is scaled by
+        Liblinear internally penalizes the intercept, treating it like any
+        other term in the feature vector. To reduce the impact of the
+        regularization on the intercept, the ``intercept_scaling`` parameter
+        can be set to a value greater than 1; the higher the value of
+        ``intercept_scaling``, the lower the impact of regularization on it.
+        Then, the weights become `[w_x_1, ..., w_x_n,
+        w_intercept*intercept_scaling]`, where `w_x_1, ..., w_x_n` represent
+        the feature weights and the intercept weight is scaled by
         `intercept_scaling`. This scaling allows the intercept term to have a
         different regularization behavior compared to the other features.
 
