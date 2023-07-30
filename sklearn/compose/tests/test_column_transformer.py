e4e82493e3fbdd7dcff4b19b66f3a02827f55fe3
Thomas J. Fan <thomasjpfan@gmail.com>
2023-07-29 21:00:34 -0400

    TST Adds test about fitting and transforming with different dataframes objects

--- sklearn/compose/tests/test_column_transformer.py
+++ sklearn/compose/tests/test_column_transformer.py
@@ -2271,3 +2271,29 @@ def test_dataframe_interchange_errors():
     msg = "Using the dataframe protocol requires fitting on dataframes."
     with pytest.raises(ValueError, match=msg):
         ct.transform(X_test)
+
+
+def test_dataframe_different_dataframe_libraries():
+    """Check fitting and transforming on pandas and polars dataframes."""
+    pd = pytest.importorskip("pandas")
+    pl = pytest.importorskip("polars")
+    X_train_np = np.array([[0, 1], [2, 4], [4, 5]])
+    X_test_np = np.array([[1, 2], [1, 3], [2, 3]])
+
+    # Fit on pandas and transform on polars
+    X_train_pd = pd.DataFrame(X_train_np, columns=["a", "b"])
+    X_test_pl = pl.DataFrame(X_test_np, schema=["a", "b"])
+
+    ct = make_column_transformer((Trans(), [0, 1]))
+    ct.fit(X_train_pd)
+
+    out_pl_in = ct.transform(X_test_pl)
+    assert_array_equal(out_pl_in, X_test_np)
+
+    # Fit on polars and transform on pandas
+    X_train_pl = pl.DataFrame(X_train_np, schema=["a", "b"])
+    X_test_pd = pd.DataFrame(X_test_np, columns=["a", "b"])
+    ct.fit(X_train_pl)
+
+    out_pd_in = ct.transform(X_test_pd)
+    assert_array_equal(out_pd_in, X_test_np)
