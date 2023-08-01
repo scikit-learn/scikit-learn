64aaab95dc83dd88e1f693823eaf56162caba4c2
cynthias13w <mcynsiew@gmail.com>
2023-08-01 18:07:19 +0200

    Added a link to documentation

--- sklearn/cluster/_kmeans.py
+++ sklearn/cluster/_kmeans.py
@@ -1240,6 +1240,10 @@ class KMeans(_BaseKMeans):
         If a callable is passed, it should take arguments X, n_clusters and a
         random state and return an initialization.
 
+        For an example of how to use init with PCA, see
+        :ref:`sphx_glr_auto_examples_cluster_plot_kmeans_digits.py`.
+
+
     n_init : 'auto' or int, default=10
         Number of times the k-means algorithm is run with different centroid
         seeds. The final results is the best output of `n_init` consecutive runs
