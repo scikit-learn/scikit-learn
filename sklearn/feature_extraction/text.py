991e575acfdb7175cc8a45cb2f3919adb45ea52d
Tialo <tialox@yandex.ru>
2023-08-02 17:19:42 +0300

    reference of classes

--- sklearn/feature_extraction/text.py
+++ sklearn/feature_extraction/text.py
@@ -602,8 +602,9 @@ class HashingVectorizer(
 
     The hash function employed is the signed 32-bit version of Murmurhash3.
 
-    For an example of document clustering and comparision with ``TfidfVectorizer``,
-    see :ref:`sphx_glr_auto_examples_text_plot_document_clustering.py`.
+    For an example of document clustering and comparision with
+    :class:`~sklearn.feature_extraction.text.TfidfVectorizer`, see
+    :ref:`sphx_glr_auto_examples_text_plot_document_clustering.py`.
 
     Read more in the :ref:`User Guide <text_feature_extraction>`.
 
@@ -1758,8 +1759,9 @@ class TfidfVectorizer(CountVectorizer):
     Equivalent to :class:`CountVectorizer` followed by
     :class:`TfidfTransformer`.
 
-    For an example of document clustering and comparision with ``HashingVectorizer``,
-    see :ref:`sphx_glr_auto_examples_text_plot_document_clustering.py`.
+    For an example of document clustering and comparision with
+    :class:`~sklearn.feature_extraction.text.HashingVectorizer`, see
+    :ref:`sphx_glr_auto_examples_text_plot_document_clustering.py`.
 
     Read more in the :ref:`User Guide <text_feature_extraction>`.
 
