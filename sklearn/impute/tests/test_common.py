cc34e6e388b58d04537c7b1bf36cd360dbc96956
Shreesha Kumar Bhat <shreesha3112@gmail.com>
2023-08-08 22:57:06 +0530

    Update sklearn/impute/tests/test_common.py

--- sklearn/impute/tests/test_common.py
+++ sklearn/impute/tests/test_common.py
@@ -194,7 +194,7 @@ def test_imputation_adds_missing_indicator_if_add_indicator_is_true(
     """
     X_train = np.array([[0, np.NaN], [1, 2]])
 
-    # Test data without missing values that can be set to np.NaN or 1.
+    # Test data where missing_value_test variable can be set to np.NaN or 1.
     X_test = np.array([[0, missing_value_test], [1, 2]])
 
     imputer.set_params(add_indicator=True)
