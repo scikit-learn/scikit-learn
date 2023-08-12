4b241538d654f58c7cf929a678c3d77464c64a47
cynthias13w <89746065+cynthias13w@users.noreply.github.com>
2023-08-12 10:35:50 +0200

    Update sklearn/cluster/_kmeans.py
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- sklearn/cluster/_kmeans.py
+++ sklearn/cluster/_kmeans.py
@@ -1240,9 +1240,6 @@ class KMeans(_BaseKMeans):
         * If a callable is passed, it should take arguments X, n_clusters and a\
         random state and return an initialization.
 
-        For an example of how to use init with PCA, see
-        :ref:`sphx_glr_auto_examples_cluster_plot_kmeans_digits.py`.
-
         For an example of how to use the different `init` strategy, see the example
         entitled :ref:`sphx_glr_auto_examples_cluster_plot_kmeans_digits.py`.
 
