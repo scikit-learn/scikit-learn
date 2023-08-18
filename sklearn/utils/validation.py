c9472a7c861ff0a7c4a7ca59dfdcbd18e8bc0543
Loïc Estève <loic.esteve@ymail.com>
2023-08-18 15:11:25 +0200

    [scipy-dev] clean-up

--- sklearn/utils/validation.py
+++ sklearn/utils/validation.py
@@ -2271,9 +2271,9 @@ def _check_pos_label_consistency(pos_label, y_true):
             or np.array_equal(classes, [1])
         )
     ):
-        classes_repr = str(classes.tolist()).replace("[", "{").replace("]", "}")
+        classes_repr = ", ".join([repr(c) for c in classes.tolist()])
         raise ValueError(
-            f"y_true takes value in {classes_repr} and pos_label is not "
+            f"y_true takes value in {{{classes_repr}}} and pos_label is not "
             "specified: either make y_true take value in {0, 1} or "
             "{-1, 1} or pass pos_label explicitly."
         )
