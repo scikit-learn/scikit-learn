af3448e1f82451c1a295e5ee7fb7390034425bda
Mohit <joshimj143@gmail.com>
2023-08-26 17:19:27 +0530

    extended the test cases of test_base.py

--- sklearn/feature_selection/tests/test_base.py
+++ sklearn/feature_selection/tests/test_base.py
@@ -4,7 +4,7 @@ from numpy.testing import assert_array_equal
 
 from sklearn.base import BaseEstimator
 from sklearn.feature_selection._base import SelectorMixin
-from sklearn.utils.fixes import CSR_CONTAINERS
+from sklearn.utils.fixes import CSC_CONTAINERS
 
 
 class StepSelector(SelectorMixin, BaseEstimator):
@@ -60,9 +60,9 @@ def test_transform_dense():
         sel.transform(np.array([[1], [2]]))
 
 
-@pytest.mark.parametrize("csr_container", CSR_CONTAINERS)
-def test_transform_sparse(csr_container):
-    X_sp = csr_container(X)
+@pytest.mark.parametrize("csc_container", CSC_CONTAINERS)
+def test_transform_sparse(csc_container):
+    X_sp = csc_container(X)
     sel = StepSelector()
     Xt_actual = sel.fit(X_sp).transform(X_sp)
     Xt_actual2 = sel.fit_transform(X_sp)
@@ -96,11 +96,12 @@ def test_inverse_transform_dense():
         sel.inverse_transform(np.array([[1], [2]]))
 
 
-@pytest.mark.parametrize("csr_container", CSR_CONTAINERS)
-def test_inverse_transform_sparse(csr_container):
-    Xt_sp = csr_container(Xt)
+@pytest.mark.parametrize("csc_container", CSC_CONTAINERS)
+def test_inverse_transform_sparse(csc_container):
+    X_sp = csc_container(X)
+    Xt_sp = csc_container(Xt)
     sel = StepSelector()
-    Xinv_actual = sel.fit(Xt_sp).inverse_transform(Xt_sp)
+    Xinv_actual = sel.fit(X_sp).inverse_transform(Xt_sp)
     assert_array_equal(Xinv, Xinv_actual.toarray())
 
     # Check dtype matches
