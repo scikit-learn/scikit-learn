40c45960e9fb0d2dad13951d506ec9260835c500
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2023-09-01 15:16:26 +0200

    FIX make sure the decision function of weak learner is symmetric (#26521)
    Co-authored-by: Jérémie du Boisberranger <34657725+jeremiedbb@users.noreply.github.com>
    Co-authored-by: Olivier Grisel <olivier.grisel@ensta.org>

--- sklearn/ensemble/_weight_boosting.py
+++ sklearn/ensemble/_weight_boosting.py
@@ -780,7 +780,11 @@ class AdaBoostClassifier(ClassifierMixin, BaseWeightBoosting):
             )
         else:  # self.algorithm == "SAMME"
             pred = sum(
-                (estimator.predict(X) == classes).T * w
+                np.where(
+                    (estimator.predict(X) == classes).T,
+                    w,
+                    -1 / (n_classes - 1) * w,
+                )
                 for estimator, w in zip(self.estimators_, self.estimator_weights_)
             )
 
@@ -827,8 +831,11 @@ class AdaBoostClassifier(ClassifierMixin, BaseWeightBoosting):
                 # The weights are all 1. for SAMME.R
                 current_pred = _samme_proba(estimator, n_classes, X)
             else:  # elif self.algorithm == "SAMME":
-                current_pred = estimator.predict(X)
-                current_pred = (current_pred == classes).T * weight
+                current_pred = np.where(
+                    (estimator.predict(X) == classes).T,
+                    weight,
+                    -1 / (n_classes - 1) * weight,
+                )
 
             if pred is None:
                 pred = current_pred
