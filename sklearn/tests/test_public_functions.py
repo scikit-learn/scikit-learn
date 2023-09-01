18d57d74dbc4ffbf7aa76e304a46ec0874a20bd3
Jérémie du Boisberranger <34657725+jeremiedbb@users.noreply.github.com>
2023-09-01 15:24:50 +0200

    MAINT Param validation for dbscan (#27234)

--- sklearn/tests/test_public_functions.py
+++ sklearn/tests/test_public_functions.py
@@ -344,6 +344,7 @@ def test_function_param_validation(func_module):
 
 PARAM_VALIDATION_CLASS_WRAPPER_LIST = [
     ("sklearn.cluster.affinity_propagation", "sklearn.cluster.AffinityPropagation"),
+    ("sklearn.cluster.dbscan", "sklearn.cluster.DBSCAN"),
     ("sklearn.cluster.k_means", "sklearn.cluster.KMeans"),
     ("sklearn.cluster.mean_shift", "sklearn.cluster.MeanShift"),
     ("sklearn.cluster.spectral_clustering", "sklearn.cluster.SpectralClustering"),
