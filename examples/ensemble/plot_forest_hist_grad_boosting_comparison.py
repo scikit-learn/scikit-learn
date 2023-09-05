f1f6d9c3f1b625171b2303d2e3303fc6af11bf63
Loïc Estève <loic.esteve@ymail.com>
2023-09-05 16:43:42 +0200

    Trigger running an example

--- examples/ensemble/plot_forest_hist_grad_boosting_comparison.py
+++ examples/ensemble/plot_forest_hist_grad_boosting_comparison.py
@@ -32,6 +32,7 @@ For more information on ensemble models, see the :ref:`User Guide <ensemble>`.
 # Load dataset
 # ------------
 
+# random comment to trigger running the example
 from sklearn.datasets import fetch_california_housing
 
 X, y = fetch_california_housing(return_X_y=True, as_frame=True)
