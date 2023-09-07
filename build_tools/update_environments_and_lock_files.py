748234c51c92bd9b0d4c6ca9d4524fc6b006c686
Julien Jerphanion <git@jjerphan.xyz>
2023-09-07 10:19:55 +0200

    CI Pin `Cython<3` in some builds due to observed regressions (#27266)
    Co-authored-by: Loïc Estève <loic.esteve@ymail.com>

--- build_tools/update_environments_and_lock_files.py
+++ build_tools/update_environments_and_lock_files.py
@@ -155,6 +155,9 @@ conda_build_metadata_list = [
             "scipy": "min",
             "matplotlib": "min",
             "threadpoolctl": "2.2.0",
+            # Regression have been observed with Cython>=3.0.0.
+            # See: https://github.com/scikit-learn/scikit-learn/issues/27086
+            "cython": "<3.0.0",
         },
     },
     {
@@ -163,7 +166,13 @@ conda_build_metadata_list = [
         "platform": "linux-64",
         "channel": "conda-forge",
         "conda_dependencies": common_dependencies_without_coverage + ["ccache"],
-        "package_constraints": {"python": "3.8", "blas": "[build=openblas]"},
+        "package_constraints": {
+            "python": "3.8",
+            "blas": "[build=openblas]",
+            # Regression have been observed with Cython>=3.0.0.
+            # See: https://github.com/scikit-learn/scikit-learn/issues/27086
+            "cython": "<3.0.0",
+        },
     },
     {
         "build_name": "pylatest_pip_openblas_pandas",
@@ -227,6 +236,9 @@ conda_build_metadata_list = [
         "package_constraints": {
             "blas": "[build=openblas]",
             "python": "3.9",
+            # Regression have been observed with Cython>=3.0.0.
+            # See: https://github.com/scikit-learn/scikit-learn/issues/27086
+            "cython": "<3.0.0",
         },
     },
     {
@@ -241,6 +253,9 @@ conda_build_metadata_list = [
         "package_constraints": {
             "python": "3.8",
             "blas": "[build=mkl]",
+            # Regression have been observed with Cython>=3.0.0.
+            # See: https://github.com/scikit-learn/scikit-learn/issues/27086
+            "cython": "<3.0.0",
         },
     },
     {
@@ -303,6 +318,9 @@ conda_build_metadata_list = [
             "python": "3.9",
             # XXX: sphinx > 6.0 does not correctly generate searchindex.js
             "sphinx": "6.0.0",
+            # Regression have been observed with Cython>=3.0.0.
+            # See: https://github.com/scikit-learn/scikit-learn/issues/27086
+            "cython": "<3.0.0",
         },
     },
     {
@@ -315,6 +333,9 @@ conda_build_metadata_list = [
         ) + ["pip", "ccache"],
         "package_constraints": {
             "python": "3.9",
+            # Regression have been observed with Cython>=3.0.0.
+            # See: https://github.com/scikit-learn/scikit-learn/issues/27086
+            "cython": "<3.0.0",
         },
     },
 ]
@@ -337,6 +358,9 @@ pip_build_metadata_list = [
             "pytest": "min",
             "pytest-cov": "min",
             # no pytest-xdist because it causes issue on 32bit
+            # Regression have been observed with Cython>=3.0.0.
+            # See: https://github.com/scikit-learn/scikit-learn/issues/27086
+            "cython": "<3.0.0",
         },
         # same Python version as in debian-32 build
         "python_version": "3.9.2",
@@ -351,7 +375,13 @@ pip_build_metadata_list = [
             "pytest",
             "pytest-xdist",
         ],
-        "package_constraints": {"joblib": "min", "threadpoolctl": "min"},
+        "package_constraints": {
+            "joblib": "min",
+            "threadpoolctl": "min",
+            # Regression have been observed with Cython>=3.0.0.
+            # See: https://github.com/scikit-learn/scikit-learn/issues/27086
+            "cython": "<3.0.0",
+        },
         "python_version": "3.10.4",
     },
 ]
