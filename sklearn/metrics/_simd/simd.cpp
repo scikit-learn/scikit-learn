70f1cba264f82b43222e7e8be5bd772038384838
Meekail Zain <zainmeekail@gmail.com>
2023-09-08 10:55:41 -0400

    Simplified code

--- sklearn/metrics/_simd/simd.cpp
+++ sklearn/metrics/_simd/simd.cpp
@@ -19,26 +19,33 @@ namespace manhattan {
             const size_t size
         ) {
             const hn::ScalableTag<Type> d;
-            auto simd_sum_1 = hn::Zero(d);
-            auto simd_sum_2 = hn::Zero(d);
+            using batch_type = decltype(hn::Zero(d));
 
-            auto lane_size = hn::Lanes(d);
+            batch_type simd_sum_1 = hn::Zero(d);
+            batch_type simd_x_1;
+            batch_type simd_y_1;
+
+            batch_type simd_sum_2 = hn::Zero(d);
+            batch_type simd_x_2;
+            batch_type simd_y_2;
+
+            size_t lane_size = hn::Lanes(d);
             size_t loop_iter = lane_size * 2;
             size_t vec_size = size - size % loop_iter;
             size_t vec_remainder_size = size - size % lane_size;
 
             for (size_t i = 0; i < vec_size; i += loop_iter) {
-                const auto simd_x_1 = hn::LoadU(d, x + i);
-                const auto simd_y_1 = hn::LoadU(d, y + i);
+                simd_x_1 = hn::LoadU(d, x + i);
+                simd_y_1 = hn::LoadU(d, y + i);
                 simd_sum_1 += hn::AbsDiff(simd_x_1, simd_y_1);
 
-                const auto simd_x_2 = hn::LoadU(d, x + i + lane_size);
-                const auto simd_y_2 = hn::LoadU(d, y + i + lane_size);
+                simd_x_2 = hn::LoadU(d, x + i + lane_size);
+                simd_y_2 = hn::LoadU(d, y + i + lane_size);
                 simd_sum_2 += hn::AbsDiff(simd_x_2, simd_y_2);
             }
             for (size_t i = vec_size; i < vec_remainder_size; i += loop_iter) {
-                const auto simd_x_1 = hn::LoadU(d, x + i);
-                const auto simd_y_1 = hn::LoadU(d, y + i);
+                simd_x_1 = hn::LoadU(d, x + i);
+                simd_y_1 = hn::LoadU(d, y + i);
                 simd_sum_1 += hn::AbsDiff(simd_x_1, simd_y_1);
             }
             simd_sum_1 += simd_sum_2;
