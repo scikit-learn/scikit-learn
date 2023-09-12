29207f759cad5b79a0e1c3d94bf04b0abd4fc1e6
Loïc Estève <loic.esteve@ymail.com>
2023-09-12 11:57:48 +0200

    [cd build] cleanup

--- build_tools/github/build_minimal_windows_image.sh
+++ build_tools/github/build_minimal_windows_image.sh
@@ -14,7 +14,6 @@ cp $WHEEL_PATH $WHEEL_NAME
 # Dot the Python version for identyfing the base Docker image
 PYTHON_VERSION=$(echo ${PYTHON_VERSION:0:1}.${PYTHON_VERSION:1:2})
 
-# TODO: remove this when Python 3.12 is released
 if [[ "$CIBW_PRERELEASE_PYTHONS" == "True" ]]; then
     PYTHON_VERSION="$PYTHON_VERSION-rc"
 fi
