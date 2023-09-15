fb1d27052fb8851c2db8db80a43c739ee46cc74c
Loïc Estève <loic.esteve@ymail.com>
2023-09-15 15:11:44 +0200

    [pyodide] [azure parallel] fix

--- build_tools/azure/install_pyodide.sh
+++ build_tools/azure/install_pyodide.sh
@@ -13,7 +13,9 @@ pip install pyodide-build==$PYODIDE_VERSION pyodide-cli
 
 # TODO: temporary fix for pywasmcross.py taken from https://github.com/pyodide/pyodide/pull/4136
 wget https://github.com/lesteve/pyodide/raw/pywasmcross-include/pyodide-build/pyodide_build/pywasmcross.py
-PYODIDE_BUILD_DIR=$(python -c 'import os; import pyodide_build; print(os.path.dirname(pyodide_build.__file__)))
+PYODIDE_BUILD_DIR=$(python -c 'import os; import pyodide_build; print(os.path.dirname(pyodide_build.__file__))')
+echo ${PYODIDE_BUILD_DIR}
+ls ${PYODIDE_BUILD_DIR}
 diff -u pywasmcross.py ${PYODIDE_BUILD_DIR}/pywasmcross.py
 cp -f pywasmcross.py ${PYODIDE_BUILD_DIR}/pywasmcross.py
 diff -u pywasmcross.py ${PYODIDE_BUILD_DIR}/pywasmcross.py
