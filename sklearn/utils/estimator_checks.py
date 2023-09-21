fc8ea06fa6fb0cfd1793ac1006f13d70bbbe4d79
Stefanie Senger <91849487+StefanieSenger@users.noreply.github.com>
2023-09-21 09:14:23 +0200

    Update sklearn/utils/estimator_checks.py
    Co-authored-by: Olivier Grisel <olivier.grisel@ensta.org>

--- sklearn/utils/estimator_checks.py
+++ sklearn/utils/estimator_checks.py
@@ -4560,7 +4560,7 @@ def check_set_output_transform_pandas(name, transformer_orig):
         error_message = str(e)
         assert (
             "Pandas output does not support sparse data." in error_message
-            or "The output of the transformer used is a scipy sparse matrix."
+            or "The transformer outputs a scipy sparse matrix."
             in error_message
         ), e
         return
