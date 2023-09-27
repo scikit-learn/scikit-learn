2c27f4aac7f56bbc16591d367c8effa0998ee775
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2023-09-27 17:05:29 +0200

    add what's new entry

--- sklearn/utils/tests/test_param_validation.py
+++ sklearn/utils/tests/test_param_validation.py
@@ -92,6 +92,10 @@ def test_interval_range(interval_type):
 
 @pytest.mark.parametrize("interval_type", [Integral, Real])
 def test_interval_large_integers(interval_type):
+    """Check that Interval constraint work with large integers.
+
+    non-regression test for #26648.
+    """
     interval = Interval(interval_type, 0, 2, closed="neither")
     assert 2**65 not in interval
     assert 2**128 not in interval
