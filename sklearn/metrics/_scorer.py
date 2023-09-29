fc4e6d7d63a3a3ec31787f4a323441102a05095f
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2023-09-29 15:03:02 +0200

    fix docstring

--- sklearn/metrics/_scorer.py
+++ sklearn/metrics/_scorer.py
@@ -643,7 +643,7 @@ def make_scorer(
     >>> from sklearn.metrics import fbeta_score, make_scorer
     >>> ftwo_scorer = make_scorer(fbeta_score, beta=2)
     >>> ftwo_scorer
-    make_scorer(fbeta_score, beta=2)
+    make_scorer(fbeta_score, response_method='predict', beta=2)
     >>> from sklearn.model_selection import GridSearchCV
     >>> from sklearn.svm import LinearSVC
     >>> grid = GridSearchCV(LinearSVC(), param_grid={'C': [1, 10]},
