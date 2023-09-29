cd39b3b42337e7ac0e8a16f42ca4fa0b4112f8d8
Puneeth K <puneethk.2899@gmail.com>
2023-09-29 23:01:21 +0530

    fix: lint issue

--- sklearn/cross_decomposition/_pls.py
+++ sklearn/cross_decomposition/_pls.py
@@ -789,8 +789,8 @@ class PLSCanonical(_PLS):
 class CCA(_PLS):
     """Canonical Correlation Analysis, also known as "Mode B" PLS.
 
-    For a comparison between other cross decomposition algorithms,
-    see :ref:`sphx_glr_auto_examples_cross_decomposition_plot_compare_cross_decomposition.py`.
+    For a comparison between other cross decomposition algorithms, see
+    :ref:`sphx_glr_auto_examples_cross_decomposition_plot_compare_cross_decomposition.py`.
 
     Read more in the :ref:`User Guide <cross_decomposition>`.
 
