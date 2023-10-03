29372a16ae7ccbec18a42af3e283b81de1624162
Olivier Grisel <olivier.grisel@ensta.org>
2023-10-03 18:39:01 +0200

    Test solver equivalence with float32 data

--- sklearn/decomposition/tests/test_pca.py
+++ sklearn/decomposition/tests/test_pca.py
@@ -122,7 +122,12 @@ def test_whitening(solver, copy):
 @pytest.mark.parametrize("rank_deficient", [False, True])
 @pytest.mark.parametrize("whiten", [False, True])
 def test_pca_solver_equivalence(
-    other_svd_solver, data_shape, rank_deficient, whiten, global_random_seed
+    other_svd_solver,
+    data_shape,
+    rank_deficient,
+    whiten,
+    global_random_seed,
+    global_dtype,
 ):
     if data_shape == "tall":
         n_samples, n_features = 100, 30
@@ -142,9 +147,15 @@ def test_pca_solver_equivalence(
             n_features=n_features,
             random_state=global_random_seed,
         )
+    X = X.astype(global_dtype, copy=False)
     X_train, X_test = X[:n_samples], X[n_samples:]
 
-    tols = dict(atol=1e-10, rtol=1e-12)
+    if global_dtype == np.float32:
+        tols = dict(atol=5e-3, rtol=1e-5)
+        variance_threshold = 1e-5
+    else:
+        tols = dict(atol=1e-10, rtol=1e-12)
+        variance_threshold = 1e-12
 
     extra_other_kwargs = {}
     if other_svd_solver == "randomized":
@@ -170,8 +181,10 @@ def test_pca_solver_equivalence(
     )
     X_trans_full_train = pca_full.fit_transform(X_train)
     assert np.isfinite(X_trans_full_train).all()
+    assert X_trans_full_train.dtype == global_dtype
     X_trans_other_train = pca_other.fit_transform(X_train)
     assert np.isfinite(X_trans_other_train).all()
+    assert X_trans_other_train.dtype == global_dtype
 
     assert (pca_full.explained_variance_ >= 0).all()
     assert_allclose(pca_full.explained_variance_, pca_other.explained_variance_, **tols)
@@ -187,7 +200,7 @@ def test_pca_solver_equivalence(
 
     # For some choice of n_components and data distribution, some components
     # might be pure noise, let's ignore them in the comparison:
-    stable = pca_full.explained_variance_ > 1e-12
+    stable = pca_full.explained_variance_ > variance_threshold
     assert stable.sum() > 1
     assert_allclose(reference_components[stable], other_components[stable], **tols)
 
@@ -200,16 +213,20 @@ def test_pca_solver_equivalence(
     # last component that can be underdetermined):
     X_trans_full_test = pca_full.transform(X_test)
     assert np.isfinite(X_trans_full_test).all()
+    assert X_trans_full_test.dtype == global_dtype
     X_trans_other_test = pca_other.transform(X_test)
     assert np.isfinite(X_trans_other_test).all()
+    assert X_trans_other_test.dtype == global_dtype
     assert_allclose(X_trans_other_test[:, stable], X_trans_full_test[:, stable], **tols)
 
     # Check that inverse transform reconstructions for both solvers are
     # compatible.
     X_recons_full_test = pca_full.inverse_transform(X_trans_full_test)
     assert np.isfinite(X_recons_full_test).all()
+    assert X_recons_full_test.dtype == global_dtype
     X_recons_other_test = pca_other.inverse_transform(X_trans_other_test)
     assert np.isfinite(X_recons_other_test).all()
+    assert X_recons_other_test.dtype == global_dtype
 
     if n_components is None and X_train.shape[0] > X_train.shape[1]:
         # In this case, both models should be able to reconstruct the data,
