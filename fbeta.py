c26e22457502106c52fe17f8d98da5ee36caf157
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2023-10-12 14:30:12 +0200

    remove useless file

--- fbeta.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-from sklearn.metrics import fbeta_score
-
-y_true = [0, 1, 2, 0, 1, 2]
-y_pred_empty = [0, 0, 0, 0, 0, 0]
-fbeta_score(y_true, y_pred_empty, average="macro", zero_division=np.nan, beta=0.5)
