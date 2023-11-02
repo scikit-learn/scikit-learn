d2eb86731c56a4b2fbfa8f0a9553c236b372bc5f
Tialo <tialox@yandex.ru>
2023-11-02 14:19:25 +0300

    resolve conversation

--- examples/multioutput/plot_classifier_chain_yeast.py
+++ examples/multioutput/plot_classifier_chain_yeast.py
@@ -1,16 +1,21 @@
 """
-==================================
-Using ensemble of Classifier Chain
-==================================
-This example shows how using ensemble of :class:`~sklearn.multioutput.ClassifierChain`
-can help to achieve higher quality on a multilabel dataset,
-compared to using a single instance of a chain.
+==================================================
+Multilabel classification using a classifier chain
+==================================================
+This example shows how to use :class:`~sklearn.multioutput.ClassifierChain` to solve
+a multilabel classification problem.
 
-:class:`~sklearn.multioutput.ClassifierChain` is the metamodel used for multilabel
-classification tasks. It fits a base model for each label. The models in chain are
-ordered randomly. In addition to input features in the dataset, each model gets the
-predictions of the preceding models in the chain as features. These additional features
-allow each chain to exploit correlations among the classes.
+The most naive strategy to solve such a task is to independently train a binary
+classifier on each label (i.e. each column of the target variable). At prediction
+time, the ensemble of binary classifiers is used to assemble multitask prediction.
+
+This strategy does not allow to model relationship between different tasks. The
+:class:`~sklearn.multioutput.ClassifierChain` is the meta-estimator (i.e. an estimator
+taking an inner estimator) that implements a more advanced strategy. The ensemble
+of binary classifiers are used as a chain where the prediction of a classifier in the
+chain is used as a feature for training the next classifier on a new label. Therefore,
+these additional features allow each chain to exploit correlations among labels.
+
 The :ref:`Jaccard similarity <jaccard_similarity_score>` score for chain tends to be
 greater than that of the set independent base models.
 """
@@ -21,9 +26,9 @@ greater than that of the set independent base models.
 # %%
 # Loading a dataset
 # -----------------
-# For this example we will use the `yeast
+# For this example, we use the `yeast
 # <https://www.openml.org/d/40597>`_ dataset which contains
-# 2417 datapoints each with 103 features and 14 possible labels. Each
+# 2,417 datapoints each with 103 features and 14 possible labels. Each
 # data point has at least one label. As a baseline we first train a logistic
 # regression classifier for each of the 14 labels. To evaluate the performance of
 # these classifiers we predict on a held-out test set and calculate the
@@ -43,7 +48,7 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_
 # %%
 # Fit models
 # ----------
-# We will fit :class:`~sklearn.linear_model.LogisticRegression` wrapped by
+# We fit :class:`~sklearn.linear_model.LogisticRegression` wrapped by
 # :class:`~sklearn.multiclass.OneVsRestClassifier` and ensemble of multiple
 # :class:`~sklearn.multioutput.ClassifierChain`.
 #
@@ -58,19 +63,19 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import jaccard_score
 from sklearn.multiclass import OneVsRestClassifier
 
-base_lr = LogisticRegression(random_state=0)
+base_lr = LogisticRegression()
 ovr = OneVsRestClassifier(base_lr)
 ovr.fit(X_train, Y_train)
 Y_pred_ovr = ovr.predict(X_test)
 ovr_jaccard_score = jaccard_score(Y_test, Y_pred_ovr, average="samples")
 
 # %%
-# Ensemble of ClassifierChain
+# Chain of binary classifiers
 # ***************************
 # Because the models in each chain are arranged randomly there is significant
 # variation in performance among the chains. Presumably there is an optimal
 # ordering of the classes in a chain that will yield the best performance.
-# However, we do not know that ordering a priori. Instead, we can construct a
+# However, we do not know that ordering a priori. Instead, we can build a
 # voting ensemble of classifier chains by averaging the binary predictions of
 # the chains and apply a threshold of 0.5. The Jaccard similarity score of the
 # ensemble is greater than that of the independent models and tends to exceed
@@ -83,7 +88,7 @@ chains = [ClassifierChain(base_lr, order="random", random_state=i) for i in rang
 for chain in chains:
     chain.fit(X_train, Y_train)
 
-Y_pred_chains = np.array([chain.predict(X_test) for chain in chains])
+Y_pred_chains = np.array([chain.predict_proba(X_test) for chain in chains])
 chain_jaccard_scores = [
     jaccard_score(Y_test, Y_pred_chain >= 0.5, average="samples")
     for Y_pred_chain in Y_pred_chains
@@ -131,3 +136,18 @@ colors = ["r"] + ["b"] * len(chain_jaccard_scores) + ["g"]
 ax.bar(x_pos, model_scores, alpha=0.5, color=colors)
 plt.tight_layout()
 plt.show()
+
+# %%
+# Results interpretation
+# ----------------------
+# There are three main takeaways from this plot:
+#
+# - Independent model wrapped by :class:`~sklearn.multiclass.OneVsRestClassifier`
+#   performs worse than the ensemble of classifier chains and some of individual chains.
+#   This is caused by the fact that the logistic regression doesn't model relationship
+#   between the labels.
+# - :class:`~sklearn.multioutput.ClassifierChain` takes advantage of correlation
+#   among labels but due to random nature of labels ordering, it could yield worse
+#   result than an independent model.
+# - An ensemble of chains performs better because it not only captures relationship
+#   between labels but also does not make strong assumptions about their correct order.
