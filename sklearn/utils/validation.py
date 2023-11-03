f2ce5e686c33a3363566a5380fcc0023255f81f0
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2023-11-03 22:27:14 +0100

    ENH _get_response_values handles predict_log_proba

--- sklearn/utils/validation.py
+++ sklearn/utils/validation.py
@@ -1923,11 +1923,11 @@ def _check_response_method(estimator, response_method):
     estimator : estimator instance
         Classifier or regressor to check.
 
-    response_method : {"predict_proba", "decision_function", "predict"} or \
-            list of such str
+    response_method : {"predict_proba", "predict_log_proba", "decision_function",
+            "predict"} or list of such str
         Specifies the response method to use get prediction from an estimator
-        (i.e. :term:`predict_proba`, :term:`decision_function` or
-        :term:`predict`). Possible choices are:
+        (i.e. :term:`predict_proba`, :term:`predict_log_proba`,
+        :term:`decision_function` or :term:`predict`). Possible choices are:
         - if `str`, it corresponds to the name to the method to return;
         - if a list of `str`, it provides the method names in order of
           preference. The method returned corresponds to the first method in
