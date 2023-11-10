620a2c3ab7a55680ad540c2205ca69109dc30533
Lucy Liu <jliu176@gmail.com>
2023-11-10 14:51:03 +1100

    review

--- sklearn/utils/tests/test_validation.py
+++ sklearn/utils/tests/test_validation.py
@@ -600,7 +600,7 @@ def test_check_array_accept_sparse_type_exception():
 
     msg = (
         "A sparse matrix was passed, but dense data is required. "
-        r"Use X.toarray\(\) to convert to a dense numpy array."
+        r"Use '.toarray\(\)' to convert to a dense numpy array."
     )
     with pytest.raises(TypeError, match=msg):
         check_array(X_csr, accept_sparse=False)
