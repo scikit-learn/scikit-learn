4f6d2f0f153b81c7812033fc0c386d63270492f7
Stefanie Senger <91849487+StefanieSenger@users.noreply.github.com>
2023-11-11 22:27:08 +0100

    Update sklearn/tests/test_random_projection.py
    Co-authored-by: Julien Jerphanion <git@jjerphan.xyz>

--- sklearn/tests/test_random_projection.py
+++ sklearn/tests/test_random_projection.py
@@ -421,19 +421,21 @@ def test_warning_n_components_greater_than_n_features(
 @pytest.mark.parametrize("coo_container", COO_CONTAINERS)
 def test_works_with_sparse_data(coo_container, global_random_seed):
     n_features = 20
+    n_samples = 5
+    n_nonzeros = int(n_features / 4)
     dense_data = make_sparse_random_data(
         coo_container,
-        5,
+        n_samples,
         n_features,
-        int(n_features / 4),
+        n_nonzeros,
         random_state=global_random_seed,
         sparse_format=None,
     )
     sparse_data = make_sparse_random_data(
         coo_container,
-        5,
+        n_samples,
         n_features,
-        int(n_features / 4),
+        n_nonzeros,
         random_state=global_random_seed,
         sparse_format="csr",
     )
