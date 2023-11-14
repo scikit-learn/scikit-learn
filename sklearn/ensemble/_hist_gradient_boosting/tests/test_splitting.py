f00f4430cfbb0e60178cf9a30e81f418109b9fd9
Olivier Grisel <olivier.grisel@ensta.org>
2023-11-14 18:17:36 +0100

    Typo

--- sklearn/ensemble/_hist_gradient_boosting/tests/test_splitting.py
+++ sklearn/ensemble/_hist_gradient_boosting/tests/test_splitting.py
@@ -966,7 +966,7 @@ def test_split_feature_fraction_per_split(forbidden_features):
     """Check that feature_fraction_per_split is respected.
 
     Because we set `n_features = 4` and `feature_fraction_per_split = 0.25`, it means
-    that caling `splitter.find_node_split` will be allowed to select a split for a
+    that calling `splitter.find_node_split` will be allowed to select a split for a
     single completely random feature at each call. So if we iterate enough, we should
     cover all the allowed features, irrespective of the values of the gradients and
     Hessians of the objective.
