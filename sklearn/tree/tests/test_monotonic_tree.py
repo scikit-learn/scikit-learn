b52776a50ed25756af3783748dd28ef166fa060e
Samuel Ronsin <samuel.ronsin@dataiku.com>
2023-11-14 17:17:54 +0100

    Use assert_allclose

--- sklearn/tree/tests/test_monotonic_tree.py
+++ sklearn/tree/tests/test_monotonic_tree.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pytest
-from numpy.testing import assert_array_almost_equal
 
 from sklearn.datasets import make_classification, make_regression
 from sklearn.ensemble import (
@@ -16,6 +15,7 @@ from sklearn.tree import (
     ExtraTreeRegressor,
 )
 from sklearn.utils.fixes import CSC_CONTAINERS
+from sklearn.utils._testing import assert_allclose
 
 TREE_CLASSIFIER_CLASSES = [DecisionTreeClassifier, ExtraTreeClassifier]
 TREE_REGRESSOR_CLASSES = [DecisionTreeRegressor, ExtraTreeRegressor]
@@ -80,9 +80,10 @@ def test_monotonic_constraints_classifications(
     est.fit(X_train, y_train)
     proba_test = est.predict_proba(X_test)
 
-    assert np.all(proba_test >= 0.0)
-    assert np.all(proba_test <= 1.0)
-    assert_array_almost_equal(np.sum(proba_test, axis=1), np.ones(proba_test.shape[0]))
+    assert np.logical_and(proba_test >= 0.0, proba_test <= 1.0).all(), (
+        "Probability should always be in [0, 1] range."
+    )
+    assert_allclose(proba_test.sum(axis=1), 1.)
 
     # Monotonic increase constraint, it applies to the positive class
     assert np.all(est.predict_proba(X_test_0incr)[:, 1] >= proba_test[:, 1])
