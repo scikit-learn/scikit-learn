41414511514fadb95e6816546371cf4ff2edc00d
Lucy Liu <jliu176@gmail.com>
2023-12-04 21:18:31 +1100

    lint

--- sklearn/multioutput.py
+++ sklearn/multioutput.py
@@ -34,7 +34,6 @@ from .model_selection import cross_val_predict
 from .utils import Bunch, _print_elapsed_time, check_random_state
 from .utils._param_validation import HasMethods, StrOptions
 from .utils._response import _get_response_values
-from .utils.validation import _check_response_method
 from .utils.metadata_routing import (
     MetadataRouter,
     MethodMapping,
@@ -45,7 +44,12 @@ from .utils.metadata_routing import (
 from .utils.metaestimators import available_if
 from .utils.multiclass import check_classification_targets
 from .utils.parallel import Parallel, delayed
-from .utils.validation import _check_method_params, check_is_fitted, has_fit_parameter
+from .utils.validation import (
+    _check_method_params,
+    _check_response_method,
+    check_is_fitted,
+    has_fit_parameter,
+)
 
 __all__ = [
     "MultiOutputRegressor",
