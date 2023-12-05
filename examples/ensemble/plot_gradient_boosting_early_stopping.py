6e4eda60806ad8f1908beb7109d482455f924198
greyisbetter <kushansharma1@gmail.com>
2023-12-06 00:38:44 +0530

    try to fix linting

--- examples/ensemble/plot_gradient_boosting_early_stopping.py
+++ examples/ensemble/plot_gradient_boosting_early_stopping.py
@@ -52,9 +52,7 @@ from sklearn.model_selection import train_test_split
 data = fetch_california_housing()
 X, y = data.data[:600], data.target[:600]
 
-X_train, X_val, y_train, y_val = (
-    train_test_split(X, y, test_size=0.2, random_state=42)
-)
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # %%
 # Model Training and Comparison
