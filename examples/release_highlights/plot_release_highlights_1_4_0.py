c4ace131216a9f419b27666db8a9f71209d558b1
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2023-12-13 18:05:23 +0100

    add monotonic constraint

--- examples/release_highlights/plot_release_highlights_1_4_0.py
+++ examples/release_highlights/plot_release_highlights_1_4_0.py
@@ -93,6 +93,46 @@ forest = RandomForestClassifier(random_state=0).fit(X, y)
 forest.predict(X)
 
 # %%
+# Add support for monotonic constraints in tree-based models
+# ----------------------------------------------------------
+# While we added support for monotonic constraints in histogram-based gradient boosting
+# in scikit-learn 0.23, we now support this feature for all other tree-based models as
+# trees, random forests, extra-trees, and exact gradient boosting. Here, we show this
+# feature for random forest on a regression problem.
+from sklearn.inspection import PartialDependenceDisplay
+from sklearn.ensemble import RandomForestRegressor
+
+n_samples = 500
+rng = np.random.RandomState(0)
+X = rng.randn(n_samples, 2)
+noise = rng.normal(loc=0.0, scale=0.01, size=n_samples)
+y = 5 * X[:, 0] + np.sin(10 * np.pi * X[:, 0]) - noise
+
+rf_no_cst = RandomForestRegressor().fit(X, y)
+rf_cst = RandomForestRegressor(monotonic_cst=[1, 0]).fit(X, y)
+
+disp = PartialDependenceDisplay.from_estimator(
+    rf_no_cst,
+    X,
+    features=[0],
+    feature_names=["feature 0"],
+    line_kw={"linewidth": 4, "label": "unconstrained", "color": "tab:blue"},
+)
+PartialDependenceDisplay.from_estimator(
+    rf_cst,
+    X,
+    features=[0],
+    line_kw={"linewidth": 4, "label": "constrained", "color": "tab:orange"},
+    ax=disp.axes_,
+)
+disp.axes_[0, 0].plot(
+    X[:, 0], y, "o", alpha=0.5, zorder=-1, label="samples", color="tab:green"
+)
+disp.axes_[0, 0].set_ylim(-3, 3)
+disp.axes_[0, 0].set_xlim(-1, 1)
+disp.axes_[0, 0].legend()
+
+# %%
 # Enriched estimator displays
 # ---------------------------
 # Estimators displays have been enriched: if we look at `hist`, defined above:
