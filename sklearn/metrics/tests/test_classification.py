201ce3e6bc0c406b9763a777952d15d72f09c3b3
Tenga <tenga.cortal@insa-rouen.fr>
2023-12-31 17:13:19 +0100

    reformat with black

--- sklearn/metrics/tests/test_classification.py
+++ sklearn/metrics/tests/test_classification.py
@@ -2779,16 +2779,14 @@ def test_balanced_accuracy_score_unseen():
     ],
 )
 def test_balanced_accuracy_score(y_true, y_pred):
-    macro_recall = recall_score(
-        y_true, y_pred, average="macro"
-    )
+    macro_recall = recall_score(y_true, y_pred, average="macro")
     with ignore_warnings():
         # Warnings are tested in test_balanced_accuracy_score_unseen
         balanced = balanced_accuracy_score(y_true, y_pred)
     assert balanced == pytest.approx(macro_recall)
     adjusted = balanced_accuracy_score(y_true, y_pred, adjusted=True)
     max_unique_classes = max(len(np.unique(y_true)), len(np.unique(y_pred)))
-    chance = 1/max_unique_classes
+    chance = 1 / max_unique_classes
     assert adjusted == (balanced - chance) / (1 - chance)
 
 
