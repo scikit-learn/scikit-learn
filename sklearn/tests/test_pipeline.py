fb3f45cc9da44824b1790611fb8c42077fe9f62d
Stefanie Senger <stefanie.senger@posteo.de>
2024-01-04 14:46:19 +0100

    adjusted error message in tests

--- sklearn/tests/test_pipeline.py
+++ sklearn/tests/test_pipeline.py
@@ -1815,7 +1815,7 @@ def test_metadata_routing_error_for_pipeline(method):
     pipeline = Pipeline([("estimator", est)])
     error_message = (
         "[sample_weight, prop] are passed but are not explicitly set as requested"
-        f" or not for SimpleEstimator.{method}"
+        f" or not requested for SimpleEstimator.{method}"
     )
     with pytest.raises(ValueError, match=re.escape(error_message)):
         try:
