041f6a609417577ef1731c43d94e53f88e2ce7ac
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2024-01-07 12:32:29 +0100

    Add doc link to SVC reference

--- sklearn/svm/_classes.py
+++ sklearn/svm/_classes.py
@@ -639,6 +639,9 @@ class SVC(BaseSVC):
     kernel functions and how `gamma`, `coef0` and `degree` affect each
     other, see the corresponding section in the narrative documentation:
     :ref:`svm_kernels`.
+    
+    To learn how use SVC in a real example see the following example:
+    :ref:`sphx_glr_auto_examples_exercises_plot_cv_digits.py`
 
     Read more in the :ref:`User Guide <svm_classification>`.
 
