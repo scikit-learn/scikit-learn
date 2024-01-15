5030835a6fef08e16d07e894230a1fa81e37ee34
Yao Xiao <108576690+Charlie-XIAO@users.noreply.github.com>
2024-01-16 01:26:05 +0800

    make shlex split windows-style path correctly

--- build_tools/update_environments_and_lock_files.py
+++ build_tools/update_environments_and_lock_files.py
@@ -483,7 +483,7 @@ def write_all_conda_environments(build_metadata_list):
 def conda_lock(environment_path, lock_file_path, platform):
     command = (
         f"conda-lock lock --mamba --kind explicit --platform {platform} "
-        f"--file {environment_path} --filename-template {lock_file_path}"
+        f"--file '{environment_path}' --filename-template '{lock_file_path}'"
     )
     execute_command(shlex.split(command))
 
@@ -533,7 +533,9 @@ def write_all_pip_requirements(build_metadata_list):
 
 
 def pip_compile(pip_compile_path, requirements_path, lock_file_path):
-    command = f"{pip_compile_path} --upgrade {requirements_path} -o {lock_file_path}"
+    command = (
+        f"'{pip_compile_path}' --upgrade '{requirements_path}' -o '{lock_file_path}'"
+    )
     execute_command(shlex.split(command))
 
 
