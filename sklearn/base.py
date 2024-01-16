01b8ff8f738c0b2e97c301748f593170da0a0ec5
Salim Dohri <dohri.salim@gmail.com>
2024-01-16 22:43:06 +0100

    Add documentation and example to MetaEstimatorMixin

--- sklearn/base.py
+++ sklearn/base.py
@@ -1287,7 +1287,31 @@ class OutlierMixin:
 
 
 class MetaEstimatorMixin:
-    """Mixin class for all meta estimators in scikit-learn."""
+    """Mixin class for all meta estimators in scikit-learn.
+    
+    This mixin defines the following functionality:
+
+    - `_required_parameters` class attribute that lists the required parameters
+        of the estimator;
+           
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from sklearn.base import BaseEstimator, MetaEstimatorMixin
+    >>> class MyEstimator(MetaEstimatorMixin, BaseEstimator):
+    ...     def __init__(self, *, param=1):
+    ...         self.param = param
+    ...     def fit(self, X, y=None):
+    ...         self.is_fitted_ = True
+    ...         return self
+    ...     def predict(self, X):
+    ...         return np.full(shape=X.shape[0], fill_value=self.param)
+    >>> estimator = MyEstimator(param=1)
+    >>> X = np.array([[1, 2], [2, 3], [3, 4]])
+    >>> y = np.array([1, 0, 1])
+    >>> print(estimator.fit(X, y).predict(X))
+    [1, 1, 1]
+    """
 
     _required_parameters = ["estimator"]
 
