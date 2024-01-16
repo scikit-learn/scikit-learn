91d7371eb087ca78cbf5fb57e5724bb0941e0bd3
Yao Xiao <108576690+Charlie-XIAO@users.noreply.github.com>
2024-01-16 16:46:18 +0800

    format scss and js using prettier

--- doc/js/scripts/details-permalink.js
+++ doc/js/scripts/details-permalink.js
@@ -14,34 +14,38 @@
 // summary text.
 // This syntax is defined in `doc/conf.py` in the `rst_prolog` variable.
 function updateIdAndHrefBasedOnSummaryText() {
-    var allDetailsElements = document.querySelectorAll('details');
-    // Counter to store the duplicated summary text to add it as a suffix in the
-    // anchor ID
-    var anchorIDCounters = {};
+  var allDetailsElements = document.querySelectorAll("details");
+  // Counter to store the duplicated summary text to add it as a suffix in the
+  // anchor ID
+  var anchorIDCounters = {};
 
-    allDetailsElements.forEach(function (detailsElement) {
-        // Get the <summary> element within the current <details>
-        var summaryElement = detailsElement.querySelector('summary');
+  allDetailsElements.forEach(function (detailsElement) {
+    // Get the <summary> element within the current <details>
+    var summaryElement = detailsElement.querySelector("summary");
 
-        // The ID uses the first line, lowercased, and spaces replaced with dashes
-        var anchorID = summaryElement.textContent.trim().split("\n")[0].replace(/\s+/g, '-').toLowerCase();
+    // The ID uses the first line, lowercased, and spaces replaced with dashes
+    var anchorID = summaryElement.textContent
+      .trim()
+      .split("\n")[0]
+      .replace(/\s+/g, "-")
+      .toLowerCase();
 
-        // Suffix the anchor ID with a counter if it already exists
-        if (anchorIDCounters[anchorID]) {
-            anchorIDCounters[anchorID] += 1;
-            anchorID = anchorID + '-' + anchorIDCounters[anchorID];
-        } else {
-            anchorIDCounters[anchorID] = 1;
-        }
+    // Suffix the anchor ID with a counter if it already exists
+    if (anchorIDCounters[anchorID]) {
+      anchorIDCounters[anchorID] += 1;
+      anchorID = anchorID + "-" + anchorIDCounters[anchorID];
+    } else {
+      anchorIDCounters[anchorID] = 1;
+    }
 
-        detailsElement.setAttribute('id', anchorID);
+    detailsElement.setAttribute("id", anchorID);
 
-        var anchorElement = summaryElement.querySelector('a.headerlink');
-        anchorElement.setAttribute('href', '#' + anchorID);
-    });
+    var anchorElement = summaryElement.querySelector("a.headerlink");
+    anchorElement.setAttribute("href", "#" + anchorID);
+  });
 }
 
 // Add an event listener to execute the function when the page is loaded
-document.addEventListener('DOMContentLoaded', function () {
-    updateIdAndHrefBasedOnSummaryText();
+document.addEventListener("DOMContentLoaded", function () {
+  updateIdAndHrefBasedOnSummaryText();
 });
