2a9364e7bc1af51d350469de410287a2bc132f34
Yao Xiao <108576690+Charlie-XIAO@users.noreply.github.com>
2024-01-17 11:43:20 +0800

    remove experimental stuff that are unnecessary

--- doc/conf.py
+++ doc/conf.py
@@ -128,7 +128,7 @@ source_suffix = ".rst"
 source_encoding = "utf-8"
 
 # The main toctree document.
-root_doc = master_doc = "index"
+root_doc = "index"
 
 # General information about the project.
 project = "scikit-learn"
