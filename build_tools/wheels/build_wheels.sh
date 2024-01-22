58dc890eab753aaf714c33bd5abe183a3d46056f
Olivier Grisel <olivier.grisel@ensta.org>
2024-01-22 15:58:03 +0100

    MAINT: set `SOURCE_DATE_EPOCH` in `build_wheels.sh` (#28152)

--- build_tools/wheels/build_wheels.sh
+++ build_tools/wheels/build_wheels.sh
@@ -3,6 +3,18 @@
 set -e
 set -x
 
+# Set environment variables to make our wheel build easier to reproduce byte
+# for byte from source. See https://reproducible-builds.org/. The long term
+# motivation would be to be able to detect supply chain attacks.
+#
+# In particular we set SOURCE_DATE_EPOCH to the commit date of the last commit.
+#
+# XXX: setting those environment variables is not enough. See the following
+# issue for more details on what remains to do:
+# https://github.com/scikit-learn/scikit-learn/issues/28151
+export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
+export PYTHONHASHSEED=0
+
 # OpenMP is not present on macOS by default
 if [[ $(uname) == "Darwin" ]]; then
     # Make sure to use a libomp version binary compatible with the oldest
