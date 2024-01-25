c1378ec6fd27248ef123a6a7ff0dd2d10a98a498
Michael Higgins <55243596+Higgs32584@users.noreply.github.com>
2024-01-24 20:21:21 -0500

    Update _bounds.py

--- sklearn/svm/_bounds.py
+++ sklearn/svm/_bounds.py
@@ -68,7 +68,7 @@ def l1_min_c(X, y, *, loss="squared_hinge", fit_intercept=True, intercept_scalin
     >>> from sklearn.datasets import make_classification
     >>> X, y = make_classification(n_samples=100, n_features=20, random_state=42)
     >>> l1_min_c(X, y, loss='squared_hinge', fit_intercept=True, intercept_scaling=1.0)
-    0.004389021067521428
+    0.004389021067521427
     """
 
     X = check_array(X, accept_sparse="csc")
