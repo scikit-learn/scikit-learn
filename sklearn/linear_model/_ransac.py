eaaab3989ba50d0610b6bf1c8dd3bb1de3f71256
Stefanie Senger <stefanie.senger@posteo.de>
2024-01-25 17:26:36 +0100

    fix for legacy build of routed_params

--- sklearn/linear_model/_ransac.py
+++ sklearn/linear_model/_ransac.py
@@ -431,15 +431,12 @@ class RANSACRegressor(
             routed_params = process_routing(self, "fit", **fit_params)
         else:
             routed_params = Bunch()
-            # routed_params.estimator = {Bunch(fit={}), Bunch(predict={}),
-            # Bunch(score={})}
-            routed_params.estimator = {"fit": {}, "score": {}, "predict": {}}
-            # results in {'estimator': {'fit': {}, 'score': {}, 'predict': {}}}
+            routed_params.estimator = Bunch(fit={}, predict={}, score={})
             if sample_weight is not None:
                 sample_weight = _check_sample_weight(sample_weight, X)
-                routed_params.estimator.fit = sample_weight
-                routed_params.estimator.predict = sample_weight
-                routed_params.estimator.score = sample_weight
+                routed_params.estimator.fit = {"sample_weight": sample_weight}
+                routed_params.estimator.predict = {"sample_weight": sample_weight}
+                routed_params.estimator.score = {"sample_weight": sample_weight}
 
         n_inliers_best = 1
         score_best = -np.inf
