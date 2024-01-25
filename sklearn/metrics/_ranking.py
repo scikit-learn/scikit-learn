3ded4e6e64c15c146510e208735e3ca9ce806ba4
Adam Li <adam2392@gmail.com>
2024-01-25 11:50:11 -0500

    Allow correct comparison
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- sklearn/metrics/_ranking.py
+++ sklearn/metrics/_ranking.py
@@ -982,7 +982,7 @@ def precision_recall_curve(
     array([0.1 , 0.35, 0.4 , 0.8 ])
     """
     # TODO(1.7): remove in 1.7 and reset y_score to be required
-    if y_score is not None and probas_pred != "deprecated":
+    if y_score is not None and str(probas_pred) != "deprecated":
         raise ValueError(
             "`probas_pred` and `y_score` cannot be both specified. Please use `y_score`"
             " only as `probas_pred` is deprecated in v1.5 and will be removed in v1.7."
