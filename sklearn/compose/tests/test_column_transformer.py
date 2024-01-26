d804cd8c7e5a26418e4c58468087615e85363bf1
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2024-01-26 20:01:54 +0100

    case of empty list

--- sklearn/compose/tests/test_column_transformer.py
+++ sklearn/compose/tests/test_column_transformer.py
@@ -2371,6 +2371,7 @@ def test_column_transformer_column_renaming(dataframe_lib):
             ("A", "passthrough", ["x1", "x2", "x3"]),
             ("B", FunctionTransformer(), ["x1", "x2"]),
             ("C", StandardScaler(), ["x1", "x3"]),
+            ("D", "passthrough", []),  # case of empty transformer
         ],
         verbose_feature_names_out=True,
     ).set_output(transform=dataframe_lib)
@@ -2399,6 +2400,7 @@ def test_column_transformer_error_with_duplicated_columns(dataframe_lib):
             ("A", "passthrough", ["x1", "x2", "x3"]),
             ("B", FunctionTransformer(), ["x1", "x2"]),
             ("C", StandardScaler(), ["x1", "x3"]),
+            ("D", "passthrough", []),  # case of empty transformer
         ],
         verbose_feature_names_out=False,
     ).set_output(transform=dataframe_lib)
