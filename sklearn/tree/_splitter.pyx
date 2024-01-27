1c1e34df3ba73b2fb03e8f91640c171d7939612b
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2024-01-28 00:29:46 +0100

    FIX make sure to reinitialize criterion even when n_missing=0

--- sklearn/tree/_splitter.pyx
+++ sklearn/tree/_splitter.pyx
@@ -415,8 +415,8 @@ cdef inline int node_split_best(
         f_i -= 1
         features[f_i], features[f_j] = features[f_j], features[f_i]
         has_missing = n_missing != 0
-        if has_missing:
-            criterion.init_missing(n_missing)
+        criterion.init_missing(n_missing)  # initialize even when n_missing == 0
+
         # Evaluate all splits
 
         # If there are missing values, then we search twice for the most optimal split.
