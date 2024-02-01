02fee0a23cffc61dde3fafbce0bba12330065988
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2024-02-01 19:09:50 +0100

    FIX FunctionTransformer overwrite column names if not consistent (#28241)

--- sklearn/compose/tests/test_column_transformer.py
+++ sklearn/compose/tests/test_column_transformer.py
@@ -1,6 +1,7 @@
 """
 Test the ColumnTransformer.
 """
+
 import pickle
 import re
 import warnings
@@ -2352,6 +2353,35 @@ def test_column_transformer__getitem__():
     msg = "'does_not_exist' is not a valid transformer name"
     with pytest.raises(KeyError, match=msg):
         ct["does_not_exist"]
+
+
+@pytest.mark.parametrize("transform_output", ["default", "pandas"])
+def test_column_transformer_remainder_passthrough_naming_consistency(transform_output):
+    """Check that when `remainder="passthrough"`, inconsistent naming is handled
+    correctly by the underlying `FunctionTransformer`.
+
+    Non-regression test for:
+    https://github.com/scikit-learn/scikit-learn/issues/28232
+    """
+    pd = pytest.importorskip("pandas")
+    X = pd.DataFrame(np.random.randn(10, 4))
+
+    preprocessor = ColumnTransformer(
+        transformers=[("scaler", StandardScaler(), [0, 1])],
+        remainder="passthrough",
+    ).set_output(transform=transform_output)
+    X_trans = preprocessor.fit_transform(X)
+    assert X_trans.shape == X.shape
+
+    expected_column_names = [
+        "scaler__x0",
+        "scaler__x1",
+        "remainder__x2",
+        "remainder__x3",
+    ]
+    if hasattr(X_trans, "columns"):
+        assert X_trans.columns.tolist() == expected_column_names
+    assert preprocessor.get_feature_names_out().tolist() == expected_column_names
 
 
 @pytest.mark.parametrize("dataframe_lib", ["pandas", "polars"])
