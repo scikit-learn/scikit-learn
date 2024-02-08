04c10f6e08b9ba30f35f786004c115adfc1c2cb7
Christian Veenhuis <124370897+ChVeen@users.noreply.github.com>
2024-02-08 19:40:18 +0000

    added example for sklearn.feature_extraction.image.grid_to_graph

--- sklearn/feature_extraction/image.py
+++ sklearn/feature_extraction/image.py
@@ -229,6 +229,63 @@ def grid_to_graph(
     graph : np.ndarray or a sparse matrix class
         The computed adjacency matrix.
 
+    Examples
+    --------
+    >>> from sklearn.feature_extraction.image import grid_to_graph
+    >>> 
+    >>> # example with a 2x2 matrix (4 cells between 0 to 3)
+    >>> graph = grid_to_graph(2, 2, 1)
+    >>> print(graph)
+      (0, 1)        1
+      (2, 3)        1
+      (0, 2)        1
+      (1, 3)        1
+      (1, 0)        1
+      (3, 2)        1
+      (2, 0)        1
+      (3, 1)        1
+      (0, 0)        1
+      (1, 1)        1
+      (2, 2)        1
+      (3, 3)        1
+    >>> 
+    >>> # example with a 2x2x2 matrix (8 cells between 0 to 7)
+    >>> graph = grid_to_graph(2, 2, 2)
+    >>> print(graph)
+      (0, 1)        1
+      (2, 3)        1
+      (4, 5)        1
+      (6, 7)        1
+      (0, 2)        1
+      (1, 3)        1
+      (4, 6)        1
+      (5, 7)        1
+      (0, 4)        1
+      (1, 5)        1
+      (2, 6)        1
+      (3, 7)        1
+      (1, 0)        1
+      (3, 2)        1
+      (5, 4)        1
+      (7, 6)        1
+      (2, 0)        1
+      (3, 1)        1
+      (6, 4)        1
+      (7, 5)        1
+      (4, 0)        1
+      (5, 1)        1
+      (6, 2)        1
+      (7, 3)        1
+      (0, 0)        1
+      (1, 1)        1
+      (2, 2)        1
+      (3, 3)        1
+      (4, 4)        1
+      (5, 5)        1
+      (6, 6)        1
+      (7, 7)        1
+    >>> 
+
     Notes
     -----
     For scikit-learn versions 0.14.1 and prior, return_as=np.ndarray was
