6c277362cadf0c9a81bcaac13e935f98b5df1640
Loïc Estève <loic.esteve@ymail.com>
2024-02-22 13:51:30 +0100

    [azure parallel] Tweak comment

--- build_tools/azure/install.sh
+++ build_tools/azure/install.sh
@@ -140,7 +140,7 @@ scikit_learn_install() {
         python setup.py develop
     fi
 
-    ccache -s || echo ""
+    ccache -s || echo "ccache not installed, skipping ccache statistics"
 }
 
 main() {
