12c2ae182cf3c1e21346ff57b6f0f277c23127eb
Luis <72552948+luis261@users.noreply.github.com>
2024-02-23 11:19:33 -0800

    Remove unused import for now

--- sklearn/cluster/_hierarchical_fast.pyx
+++ sklearn/cluster/_hierarchical_fast.pyx
@@ -3,7 +3,6 @@
 import numpy as np
 cimport cython
 
-from ..metrics._dist_metrics cimport DistanceMetric64
 from ..utils._fast_dict cimport IntFloatDict
 from ..utils._typedefs cimport float64_t, intp_t, uint8_t
 
