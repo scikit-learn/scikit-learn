cc8fa2c40e518dbe107c3a857b5b69b0fb9e4fd1
Stefanie Senger <stefanie.senger@posteo.de>
2024-02-24 12:31:39 +0100

    remove accidentally pushed change

--- sklearn/multioutput.py
+++ sklearn/multioutput.py
@@ -1100,7 +1100,7 @@ class ClassifierChain(MetaEstimatorMixin, ClassifierMixin, _BaseChain):
         return router
 
     def _more_tags(self):
-        return {"_skip_test": True, "multioutput_only": True, "multilabel": True}
+        return {"_skip_test": True, "multioutput_only": True}
 
 
 class RegressorChain(MetaEstimatorMixin, RegressorMixin, _BaseChain):
