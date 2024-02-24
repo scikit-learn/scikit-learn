dec2ee55c7d58226260bfca86e4f8fac42acb5d5
Luis <72552948+luis261@users.noreply.github.com>
2024-02-23 23:15:49 -0800

    Try to resolve circular import

--- sklearn/metrics/cluster/_dbcv_internals.py
+++ sklearn/metrics/cluster/_dbcv_internals.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.spatial.distance import cdist
 
-from ...cluster._hierarchical_fast import mst_linkage_core
+import ...cluster
 from ..pairwise import pairwise_distances
 
 
@@ -128,7 +128,7 @@ def internal_minimum_spanning_tree(mr_distances):
     Moulavi, D., Jaskowiak, P.A., Campello, R.J., Zimek, A. and Sander, J.,
     2014. Density-Based Clustering Validation. In SDM (pp. 839-847).
     """
-    single_linkage_data = mst_linkage_core(mr_distances, None, precomputed=True)
+    single_linkage_data = cluster._hierarchical_fast.mst_linkage_core(mr_distances, None, precomputed=True)
     min_span_tree = single_linkage_data.copy()
     for index, row in enumerate(min_span_tree[1:], 1):
         candidates = np.where(np.isclose(mr_distances[int(row[1])], row[2]))[0]
