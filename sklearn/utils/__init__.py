411b6595068e2f41e1fdc126352b386154b0f9f6
Yao Xiao <108576690+Charlie-XIAO@users.noreply.github.com>
2024-03-02 10:15:45 +0800

    rewording changelog and some comments

--- sklearn/utils/__init__.py
+++ sklearn/utils/__init__.py
@@ -145,7 +145,8 @@ def _polars_indexing(X, key, key_dtype, axis):
     # DataFrame (axis=0)
     X_indexed = X[key]
     if np.isscalar(key) and len(X.shape) == 2:
-        # Return a Series to be consistent with pandas
+        # `X_indexed` is a DataFrame with a single row; we return a Series to be
+        # consistent with pandas
         pl = sys.modules["polars"]
         return pl.Series(X_indexed.row(0))
     return X_indexed
