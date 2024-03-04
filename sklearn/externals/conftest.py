30c68994e1dcd05517aa2b52a365d7b925dcf7b0
adrinjalali <adrin.jalali@gmail.com>
2024-03-04 09:48:30 +0100

    simplify code for pytest>7 dep

--- sklearn/externals/conftest.py
+++ sklearn/externals/conftest.py
@@ -2,19 +2,5 @@
 # --ignore because --ignore needs a path and it is not convenient to pass in
 # the externals path (very long install-dependent path in site-packages) when
 # using --pyargs
-import pytest
-
-PYTEST_GTE_7 = hasattr(pytest, "version_tuple") and pytest.version_tuple >= (
-    7,
-    0,
-)  # type: ignore[attr-defined]
-
-if PYTEST_GTE_7:
-
-    def pytest_ignore_collect(collection_path, config):
-        return True
-
-else:
-
-    def pytest_ignore_collect(path, config):
-        return True
+def pytest_ignore_collect(collection_path, config):
+    return True
