e7c49a7ca957f0b34c6bb8be79b63392685d7b83
adrinjalali <adrin.jalali@gmail.com>
2024-03-04 09:41:17 +0100

    FIX pytest=8.1.0 issue

--- sklearn/externals/conftest.py
+++ sklearn/externals/conftest.py
@@ -2,5 +2,19 @@
 # --ignore because --ignore needs a path and it is not convenient to pass in
 # the externals path (very long install-dependent path in site-packages) when
 # using --pyargs
-def pytest_ignore_collect(path, config):
-    return True
+import pytest
+
+PYTEST_GTE_7 = hasattr(pytest, "version_tuple") and pytest.version_tuple >= (
+    7,
+    0,
+)  # type: ignore[attr-defined]
+
+if PYTEST_GTE_7:
+
+    def pytest_ignore_collect(collection_path, config):
+        return True
+
+else:
+
+    def pytest_ignore_collect(path, config):
+        return True
