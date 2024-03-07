6a728ac4f5a9eedfa12b526f0a7d838c97120ba6
Olivier Grisel <olivier.grisel@ensta.org>
2024-03-07 18:03:54 +0100

    Fix formatting

--- sklearn/utils/tests/test_array_api.py
+++ sklearn/utils/tests/test_array_api.py
@@ -310,9 +310,7 @@ def test_device_inspection():
 # the following upstream issue has been fixed:
 # https://github.com/cupy/cupy/issues/8180
 @skip_if_array_api_compat_not_configured
-@pytest.mark.parametrize(
-    "library", ["numpy", "numpy.array_api", "torch"]
-)
+@pytest.mark.parametrize("library", ["numpy", "numpy.array_api", "torch"])
 @pytest.mark.parametrize(
     "X,reduction,expected",
     [
