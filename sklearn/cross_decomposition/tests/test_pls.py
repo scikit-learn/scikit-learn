FIX PLSRegression.coef_ takes X and Y variance into account when scale=True

sklearn/cross_decomposition/tests/test_pls.py
@@ -634,7 +634,7 @@ def test_pls_regression_fit_1d_y():
     y = np.array([2, 6, 12, 20, 30, 42])
     expected = y.copy()
 
-    plsr = PLSRegression().fit(X, y)
+    plsr = PLSRegression(scale=False).fit(X, y)
     y_pred = plsr.predict(X)
     assert y_pred.shape == expected.shape
 
@@ -644,3 +644,25 @@ def test_pls_regression_fit_1d_y():
     y_pred = vr.fit(X, y).predict(X)
     assert y_pred.shape == expected.shape
     assert_allclose(y_pred, expected)
+
+
+def test_pls_regression_scaling_coef():
+    """Check that when using `scale=True`, the coefficients are using the std. dev. from
+    both `X` and `Y`.
+
+    Non-regression test for:
+    https://github.com/scikit-learn/scikit-learn/issues/27964
+    """
+    # handcrafted data where we can predict Y from X with an additional scaling factor
+    rng = np.random.RandomState(0)
+    coef = rng.random_integers(0, 3, size=(3, 5)).astype(np.float64)
+    X = rng.randn(30, 5) * 10  # add a variance of 10
+    Y = X @ coef.T
+
+    # we need to make sure that the dimension of the latent space is large enough to
+    # perfectly predict `Y` from `X` (no information loss)
+    pls = PLSRegression(n_components=5, scale=True).fit(X, Y)
+    assert_allclose(pls.coef_, coef, atol=1e-7)
+
+    # we therefore should be able to predict `Y` from `X`
+    assert_allclose(pls.predict(X), Y, atol=1e-7)