7bda6d441451dd70aa6b4d148895e89f07ad38af
Yao Xiao <108576690+Charlie-XIAO@users.noreply.github.com>
2024-03-12 19:42:40 +0800

    fix wrong path in version switcher

--- doc/conf.py
+++ doc/conf.py
@@ -236,9 +236,9 @@ html_theme_options = {
     # to be displayed, one adds "version-switcher" somewhere in the page layout. See
     # https://pydata-sphinx-theme.readthedocs.io/en/stable/user_guide/version-dropdown.html
     "switcher": {
-        # TODO: change to https://scikit-learn.org/dev/_static/versions.json before
-        # merging the new_web_theme branch into main
-        "json_url": "https://scikit-learn.org/_pst_preview/_static/versions.json",
+        # TODO: change to https://scikit-learn.org/dev/versions.json before merging the
+        # new_web_theme branch into main
+        "json_url": "https://scikit-learn.org/_pst_preview/versions.json",
         "version_match": release,
     },
     # check_switcher may be set to False if docbuild pipeline fails. See
