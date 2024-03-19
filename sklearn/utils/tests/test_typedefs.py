3aa22df421a3163194c3eaf3015f5bef80692899
Olivier Grisel <olivier.grisel@ensta.org>
2024-03-19 11:24:19 +0100

    TST: missing test for unsigned integers types in _typedefs.pxd

--- sklearn/utils/tests/test_typedefs.py
+++ sklearn/utils/tests/test_typedefs.py
@@ -8,11 +8,13 @@ from sklearn.utils._typedefs import testing_make_array_from_typed_val
     "type_t, value, expected_dtype",
     [
         ("uint8_t", 1, np.uint8),
+        ("uint32_t", 1, np.uint32),
+        ("uint64_t", 1, np.uint64),
         ("intp_t", 1, np.intp),
-        ("float64_t", 1.0, np.float64),
-        ("float32_t", 1.0, np.float32),
         ("int32_t", 1, np.int32),
         ("int64_t", 1, np.int64),
+        ("float64_t", 1.0, np.float64),
+        ("float32_t", 1.0, np.float32),
     ],
 )
 def test_types(type_t, value, expected_dtype):
