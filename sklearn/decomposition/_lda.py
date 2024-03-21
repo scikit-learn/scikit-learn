2d1df87bdd51158b09d8f1be385a6bc1cc654b05
keyber <keyvan.beroukhim@gmail.com>
2024-03-21 16:38:11 +0100

    Update _lda.py perp_tol documentation
    perp_tol is also used in online learning

--- sklearn/decomposition/_lda.py
+++ sklearn/decomposition/_lda.py
@@ -240,7 +240,7 @@ class LatentDirichletAllocation(
         Total number of documents. Only used in the :meth:`partial_fit` method.
 
     perp_tol : float, default=1e-1
-        Perplexity tolerance in batch learning. Only used when
+        Perplexity tolerance. Only used when
         ``evaluate_every`` is greater than 0.
 
     mean_change_tol : float, default=1e-3
