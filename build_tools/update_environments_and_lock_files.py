3c427efc0a56964d9b2dbcd15c6fc7db5cce6296
Jérémie du Boisberranger <jeremie@probabl.ai>
2024-03-25 17:30:09 +0100

    Update build_tools/update_environments_and_lock_files.py
    Co-authored-by: Olivier Grisel <olivier.grisel@ensta.org>

--- build_tools/update_environments_and_lock_files.py
+++ build_tools/update_environments_and_lock_files.py
@@ -141,7 +141,8 @@ build_metadata_list = [
         "package_constraints": {
             "blas": "[build=mkl]",
             # scipy 1.12.x crashes on this platform (https://github.com/scipy/scipy/pull/20086)
-            # TODO: release scipy constraint when 1.13 is available.
+            # TODO: release scipy constraint when 1.13 is available in the "default"
+            # channel.
             "scipy": "<1.12",
         },
         # TODO: put cython back to conda dependencies when required version is
