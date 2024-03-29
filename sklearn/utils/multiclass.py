Fix #27973: Adjust sum_of_confidences calculation for probability confidences in _ovr_decision_function
Previously, the _ovr_decision_function didn't handle well cases where confidences were probabilities. The parameter confidences could either be decision functions or predicted probabilities for the positive class for each binary classifier. While decision functions can have negative values, probabilities are always positive. Consequently, the expression 'sum_of_confidences[:, i] -= confidences[:, k]' always decreased 'sum_of_confidences[:, i]', even when the negative class i is more probable to happen then j (probability of j < 0.5). This fix ensures that sum_of_confidences is adjusted correctly based on whether, confidences are probabilities or decision functions. In case confidences are probabilities, we will increase sum_of_confidences of j for probabilities >= 0.5,
and decrease the sum_of_confidences of i. Similarly, for probabilities < 0.5, we decrease sum_of_confidences of j and increase the sum_of_confidences of i.

Note: Also added a test to verify the function's behavior when confidences are provided as probabilities.

sklearn/utils/multiclass.py
@@ -534,8 +534,27 @@ def _ovr_decision_function(predictions, confidences, n_classes):
     k = 0
     for i in range(n_classes):
         for j in range(i + 1, n_classes):
-            sum_of_confidences[:, i] -= confidences[:, k]
-            sum_of_confidences[:, j] += confidences[:, k]
+            # Handle confidences as probabilities
+            # if probabilities are >= 0.5, (j is more probable) we should increase
+            # the sum of confidences of j and decrease the sum of confidences of i
+            if np.max(confidences) <= 1.0 and np.min(confidences) >= 0:
+                sum_of_confidences[confidences[:, k] >= 0.5, i] -= confidences[
+                    confidences[:, k] >= 0.5, k
+                ]
+                sum_of_confidences[confidences[:, k] < 0.5, i] += confidences[
+                    confidences[:, k] < 0.5, k
+                ]
+
+                sum_of_confidences[confidences[:, k] >= 0.5, j] += confidences[
+                    confidences[:, k] >= 0.5, k
+                ]
+                sum_of_confidences[confidences[:, k] < 0.5, j] -= confidences[
+                    confidences[:, k] < 0.5, k
+                ]
+            else:
+                sum_of_confidences[:, i] -= confidences[:, k]
+                sum_of_confidences[:, j] += confidences[:, k]
+
             votes[predictions[:, k] == 0, i] += 1
             votes[predictions[:, k] == 1, j] += 1
             k += 1