619bf9fe5f6eddc34ec588a126fa29181e3e5722
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2024-04-03 16:19:14 +0200

    cln base

--- sklearn/callback/_base.py
+++ sklearn/callback/_base.py
@@ -17,19 +17,14 @@ class BaseCallback(ABC):
             The estimator the callback is set on.
 
         data : dict
-            Dictionary containing the training and validation data. The keys are
-            "X_train", "y_train", "sample_weight_train", "X_val", "y_val",
-            "sample_weight_val". The values are the corresponding data. If a key is
-            missing, the corresponding value is None.
+            Dictionary containing the training and validation data. The possible
+            keys are "X_train", "y_train", "sample_weight_train", "X_val", "y_val"
+            and "sample_weight_val".
         """
 
     @abstractmethod
-    def on_fit_end(self):
-        """Method called at the end of the fit method of the estimator."""
-
-    @abstractmethod
-    def on_fit_iter_end(self, estimator, node, **kwargs):
-        """Method called at the end of each computation node of the estimator.
+    def on_fit_iter_end(self, estimator, task_node, **kwargs):
+        """Method called at the end of each task of the estimator.
 
         Parameters
         ----------
@@ -37,8 +32,8 @@ class BaseCallback(ABC):
             The caller estimator. It might differ from the estimator passed to the
             `on_fit_begin` method for auto-propagated callbacks.
 
-        node : ComputationNode instance
-            The caller computation node.
+        task_node : TaskNode instance
+            The caller task node.
 
         **kwargs : dict
             arguments passed to the callback. Possible keys are
@@ -74,6 +69,18 @@ class BaseCallback(ABC):
             Whether or not to stop the current level of iterations at this node.
         """
 
+    @abstractmethod
+    def on_fit_end(self, task_node):
+        """Method called at the end of the fit method of the estimator.
+        
+        Parameters
+        ----------
+        task_node : TaskNode instance
+            The task node corresponding to the whole `fit` task. This is usually the
+            root of the task tree of the estimator but it can be an intermediate node
+            if the estimator is a sub-estimator of a meta-estimator.
+        """
+
     @property
     def auto_propagate(self):
         """Whether or not this callback should be propagated to sub-estimators.
@@ -85,13 +92,3 @@ class BaseCallback(ABC):
         the meta-estimator and its sub-estimators.
         """
         return False
-
-    # TODO: This is not used yet but will be necessary for next callbacks
-    #       Uncomment when needed
-    # @property
-    # def request_stopping_criterion(self):
-    #     return False
-
-    # @property
-    # def request_from_reconstruction_attributes(self):
-    #     return False
