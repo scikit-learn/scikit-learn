f963d656ca75b23fc4b08b0ae3ced8623c4e0278
Adrin Jalali <adrin.jalali@gmail.com>
2024-04-16 12:50:08 +0200

    Update sklearn/discriminant_analysis.py
    Co-authored-by: Guillaume Lemaitre <guillaume@probabl.ai>

--- sklearn/discriminant_analysis.py
+++ sklearn/discriminant_analysis.py
@@ -734,7 +734,7 @@ class QuadraticDiscriminantAnalysis(ClassifierMixin, BaseEstimator):
     tol : float, default=1.0e-4
         Absolute threshold for the covariance matrix to be considered rank
         deficient after applying some regularization (see `reg_param`) to each `Sk` where `Sk`
-        represents covariance matrix for kth class. This parameter does not
+        represents covariance matrix for k-th class. This parameter does not
         affect the predictions. It controls when a warning is raised when the
         covariance matrix is not full rank.
 
