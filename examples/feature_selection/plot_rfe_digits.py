4ad18bbdff7067c11e2558f9961869f3ffe82b46
plon-Susk7 <priyashshah4@gmail.com>
2024-04-19 20:40:36 +0530

    made changes as advised

--- examples/feature_selection/plot_rfe_digits.py
+++ examples/feature_selection/plot_rfe_digits.py
@@ -3,8 +3,14 @@
 Recursive feature elimination
 =============================
 
-A recursive feature elimination example showing the relevance of pixels in
-a digit classification task.
+
+This example demonstrates how :class:~sklearn.feature_selection.RFE can be used
+to determine the importance of individual pixels when classifying handwritten digits.
+RFE is a method that recursively removes the least significant features and retrains
+the model, allowing us to rank features by their importance. the most important features
+are assigned rank 1 and the higher ranking_, the less important. This ranking is also
+encoded by the shades of blue. Ranking map is annotated with pixel numbers for clarity.
+As expected, pixels in the center of the image are more predictive than pixels close to the edges.
 
 .. note::
 
@@ -12,16 +18,13 @@ a digit classification task.
 
 """  # noqa: E501
 
-import warnings
-
 import matplotlib.pyplot as plt
 
 from sklearn.datasets import load_digits
 from sklearn.feature_selection import RFE
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
-
-warnings.filterwarnings("ignore")
+from sklearn.preprocessing import StandardScaler
 
 # Load the digits dataset
 digits = load_digits()
@@ -29,7 +32,10 @@ X = digits.images.reshape((len(digits.images), -1))
 y = digits.target
 
 pipe = Pipeline(
-    [("rfe", RFE(estimator=LogisticRegression(), n_features_to_select=1, step=1))]
+    [
+        ("scaler", StandardScaler()),
+        ("rfe", RFE(estimator=LogisticRegression(), n_features_to_select=1, step=1)),
+    ]
 )
 
 pipe.fit(X, y)
@@ -44,5 +50,5 @@ for i in range(ranking.shape[0]):
         plt.text(j, i, str(ranking[i, j]), ha="center", va="center", color="black")
 
 plt.colorbar()
-plt.title("Ranking of pixels with RFE (Logistic Regression)")
+plt.title("Ranking of pixels with RFE\n(Logistic Regression)")
 plt.show()
