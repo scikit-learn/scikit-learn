dc5c891eb249d8fd86738ca0044ee07ea7249b8a
plon-Susk7 <priyashshah4@gmail.com>
2024-04-19 18:14:19 +0530

    fixed formatting

--- examples/feature_selection/plot_rfe_digits.py
+++ examples/feature_selection/plot_rfe_digits.py
@@ -18,6 +18,7 @@ from sklearn.feature_selection import RFE
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 import warnings
+
 warnings.filterwarnings("ignore")
 
 # Load the digits dataset
@@ -25,12 +26,12 @@ digits = load_digits()
 X = digits.images.reshape((len(digits.images), -1))
 y = digits.target
 
-pipe = Pipeline([
-    ('rfe', RFE(estimator=LogisticRegression(), n_features_to_select=1, step=1))
-])
+pipe = Pipeline(
+    [("rfe", RFE(estimator=LogisticRegression(), n_features_to_select=1, step=1))]
+)
 
 pipe.fit(X, y)
-ranking = pipe.named_steps['rfe'].ranking_.reshape(digits.images[0].shape)
+ranking = pipe.named_steps["rfe"].ranking_.reshape(digits.images[0].shape)
 
 # Plot pixel ranking
 plt.matshow(ranking, cmap=plt.cm.Blues)
@@ -38,10 +39,8 @@ plt.matshow(ranking, cmap=plt.cm.Blues)
 # Add annotations for pixel numbers
 for i in range(ranking.shape[0]):
     for j in range(ranking.shape[1]):
-        plt.text(j, i, str(ranking[i, j]), ha='center', va='center', color='black')
+        plt.text(j, i, str(ranking[i, j]), ha="center", va="center", color="black")
 
 plt.colorbar()
 plt.title("Ranking of pixels with RFE (Logistic Regression)")
 plt.show()
-
-
