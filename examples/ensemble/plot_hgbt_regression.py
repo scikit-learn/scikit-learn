06f06003e30ff5e0e9e375dec78bdceb24dfcef2
Christian Lorentzen <lorentzen.ch@gmail.com>
2024-04-19 11:53:41 +0200

    CLN reinsert categorical_features=None to avoid FutureWarning

--- examples/ensemble/plot_hgbt_regression.py
+++ examples/ensemble/plot_hgbt_regression.py
@@ -116,7 +116,9 @@ fig, ax = plt.subplots(figsize=(10, 5))
 average_week_demand.plot(color=colors[0], label="recorded average", linewidth=2, ax=ax)
 
 for idx, max_iter in enumerate(max_iter_list):
-    hgbt = HistGradientBoostingRegressor(max_iter=max_iter, random_state=42)
+    hgbt = HistGradientBoostingRegressor(
+        max_iter=max_iter, categorical_features=None, random_state=42
+    )
     hgbt.fit(X_train, y_train)
 
     y_pred = hgbt.predict(X_test)
@@ -169,6 +171,7 @@ common_params = {
     "learning_rate": 0.3,
     "validation_fraction": 0.2,
     "random_state": 42,
+    "categorical_features": None,
     "scoring": "neg_root_mean_squared_error",
 }
 
