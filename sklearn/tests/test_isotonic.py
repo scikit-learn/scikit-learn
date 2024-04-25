3b50e8d59ae8b1a700a6051720b6ec94bd49e8f1
Christian Lorentzen <lorentzen.ch@gmail.com>
2024-04-25 20:04:51 +0200

    ENH use scipy isotonic_regression

--- sklearn/tests/test_isotonic.py
+++ sklearn/tests/test_isotonic.py
@@ -502,25 +502,26 @@ def test_isotonic_copy_before_fit():
     copy.copy(ir)
 
 
-def test_isotonic_dtype():
+@pytest.mark.parametrize("dtype", [np.int32, np.int64, np.float32, np.float64])
+def test_isotonic_dtype(dtype):
     y = [2, 1, 4, 3, 5]
     weights = np.array([0.9, 0.9, 0.9, 0.9, 0.9], dtype=np.float64)
     reg = IsotonicRegression()
 
-    for dtype in (np.int32, np.int64, np.float32, np.float64):
-        for sample_weight in (None, weights.astype(np.float32), weights):
-            y_np = np.array(y, dtype=dtype)
-            expected_dtype = check_array(
-                y_np, dtype=[np.float64, np.float32], ensure_2d=False
-            ).dtype
+    for sample_weight in (None, weights.astype(np.float32), weights):
+        y_np = np.array(y, dtype=dtype)
+        expected_dtype = check_array(
+            y_np, dtype=[np.float64, np.float32], ensure_2d=False
+        ).dtype
 
-            res = isotonic_regression(y_np, sample_weight=sample_weight)
-            assert res.dtype == expected_dtype
+        res = isotonic_regression(y_np, sample_weight=sample_weight)
+        print(f"{res.dtype=}")
+        assert res.dtype == expected_dtype
 
-            X = np.arange(len(y)).astype(dtype)
-            reg.fit(X, y_np, sample_weight=sample_weight)
-            res = reg.predict(X)
-            assert res.dtype == expected_dtype
+        X = np.arange(len(y)).astype(dtype)
+        reg.fit(X, y_np, sample_weight=sample_weight)
+        res = reg.predict(X)
+        assert res.dtype == expected_dtype
 
 
 @pytest.mark.parametrize("y_dtype", [np.int32, np.int64, np.float32, np.float64])
