4d25bc1916775ff64bf4b028e61e1c7e714210bd
Jérémie du Boisberranger <jeremie@probabl.ai>
2024-05-13 12:52:07 +0200

    try using pip wheel [cd build]

--- build_tools/github/repair_windows_wheels.sh
+++ build_tools/github/repair_windows_wheels.sh
@@ -8,8 +8,8 @@ DEST_DIR=$2
 
 # By default, the Windows wheels are not repaired.
 # In this case, we need to vendor VCRUNTIME140.dll
-wheel unpack "$WHEEL"
+python -m pip wheel unpack "$WHEEL"
 WHEEL_DIRNAME=$(ls -d scikit_learn-*)
 python build_tools/github/vendor.py "$WHEEL_DIRNAME"
-wheel pack "$WHEEL_DIRNAME" -d "$DEST_DIR"
+python -m pip wheel pack "$WHEEL_DIRNAME" -d "$DEST_DIR"
 rm -rf "$WHEEL_DIRNAME"
