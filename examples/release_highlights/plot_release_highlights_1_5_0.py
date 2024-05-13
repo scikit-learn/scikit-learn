5f7d6ef6268bc0f3b84917ab4d80581672d29b0e
Jérémie du Boisberranger <jeremie@probabl.ai>
2024-05-13 14:07:06 +0200

    init

---
+++ examples/release_highlights/plot_release_highlights_1_5_0.py
@@ -0,0 +1,30 @@
+# ruff: noqa
+"""
+=======================================
+Release Highlights for scikit-learn 1.5
+=======================================
+
+.. currentmodule:: sklearn
+
+We are pleased to announce the release of scikit-learn 1.5! Many bug fixes
+and improvements were added, as well as some new key features. We detail
+below a few of the major features of this release. **For an exhaustive list of
+all the changes**, please refer to the :ref:`release notes <release_notes_1_5>`.
+
+To install the latest version (with pip)::
+
+    pip install --upgrade scikit-learn
+
+or with conda::
+
+    conda install -c conda-forge scikit-learn
+
+"""
+
+# %%
+# Placeholder
+# -----------
+
+# %%
+# Placeholder
+# -----------
