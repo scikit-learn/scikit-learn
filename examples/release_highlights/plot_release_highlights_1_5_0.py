524204bcbb6fd017968a7d86d7439c481bfe1105
Jérémie du Boisberranger <jeremie@probabl.ai>
2024-05-17 17:10:12 +0200

    Update examples/release_highlights/plot_release_highlights_1_5_0.py

--- examples/release_highlights/plot_release_highlights_1_5_0.py
+++ examples/release_highlights/plot_release_highlights_1_5_0.py
@@ -7,8 +7,8 @@ Release Highlights for scikit-learn 1.5
 .. currentmodule:: sklearn
 
 We are pleased to announce the release of scikit-learn 1.5! Many bug fixes
-and improvements were added, as well as some key new features. We detail
-below a few of the major features of this release. **For an exhaustive list of
+and improvements were added, as well as some key new features. Below we
+detail a few of the major features of this release. **For an exhaustive list of
 all the changes**, please refer to the :ref:`release notes <release_notes_1_5>`.
 
 To install the latest version (with pip)::
