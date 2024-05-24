3034d2e570f956d234c1ceeda515f45394b5fc6b
Tialo <tialox@yandex.ru>
2024-05-25 02:04:52 +0300

    array-api for entropy

--- sklearn/metrics/cluster/_supervised.py
+++ sklearn/metrics/cluster/_supervised.py
@@ -23,6 +23,7 @@ from numbers import Real
 import numpy as np
 from scipy import sparse as sp
 
+from ...utils._array_api import get_namespace
 from ...utils._param_validation import Interval, StrOptions, validate_params
 from ...utils.multiclass import type_of_target
 from ...utils.validation import check_array, check_consistent_length
@@ -1282,17 +1283,27 @@ def entropy(labels):
     -----
     The logarithm used is the natural logarithm (base-e).
     """
-    if len(labels) == 0:
-        return 1.0
-    label_idx = np.unique(labels, return_inverse=True)[1]
-    pi = np.bincount(label_idx).astype(np.float64)
-    pi = pi[pi > 0]
+    if isinstance(labels, list):
+        xp, is_array_api_compliant = np, False
+    else:
+        xp, is_array_api_compliant = get_namespace(labels)
+
+    shape_0 = labels.shape[0] if is_array_api_compliant else len(labels)
+    if shape_0 == 0:
+        return 0.0
+
+    if is_array_api_compliant:
+        unique_counts = xp.unique_counts(labels)
+    else:
+        unique_counts = np.unique(labels, return_counts=True)
+
+    pi = xp.asarray(unique_counts[1], dtype=xp.float64)
 
     # single cluster => zero entropy
     if pi.size == 1:
         return 0.0
 
-    pi_sum = np.sum(pi)
+    pi_sum = xp.sum(pi)
     # log(a / b) should be calculated as log(a) - log(b) for
     # possible loss of precision
-    return -np.sum((pi / pi_sum) * (np.log(pi) - log(pi_sum)))
+    return -xp.sum((pi / pi_sum) * (xp.log(pi) - log(pi_sum)))
