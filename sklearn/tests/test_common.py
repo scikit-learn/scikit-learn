3c3a569a9905ea8abee101261142132d70c42b3f
Olivier Grisel <olivier.grisel@ensta.org>
2024-05-27 11:05:26 +0200

    Make test_search_cv easier to debug by setting error_score='raise'

--- sklearn/tests/test_common.py
+++ sklearn/tests/test_common.py
@@ -332,7 +332,9 @@ def _generate_search_cv_instances():
         extra_params = (
             {"min_resources": "smallest"} if "min_resources" in init_params else {}
         )
-        search_cv = SearchCV(Estimator(), param_grid, cv=2, **extra_params)
+        search_cv = SearchCV(
+            Estimator(), param_grid, cv=2, error_score="raise", **extra_params
+        )
         set_random_state(search_cv)
         yield search_cv
 
