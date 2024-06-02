3ab02b2da52ace506b1122df6959dd2fa0cf5848
Loïc Estève <loic.esteve@ymail.com>
2024-06-02 07:09:09 +0200

    MAINT Use Pyodide venv rather than js wrapper script

--- build_tools/azure/install_pyodide.sh
+++ build_tools/azure/install_pyodide.sh
@@ -17,4 +17,7 @@ ls -ltrh dist
 
 # The Pyodide js library is needed by build_tools/azure/test_script_pyodide.sh
 # to run tests inside Pyodide
-npm install pyodide@$PYODIDE_VERSION
+# npm install pyodide@$PYODIDE_VERSION
+pyodide venv .pyodide-venv
+source .pyodide-venv/bin/activate
+pip install dist/*.whl
