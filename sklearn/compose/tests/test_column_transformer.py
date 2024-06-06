d7f95524da95df0624e62216c7f0fac9bacb586f
Guillaume Lemaitre <guillaume@probabl.ai>
2024-06-06 14:03:42 +0200

    better test

--- sklearn/compose/tests/test_column_transformer.py
+++ sklearn/compose/tests/test_column_transformer.py
@@ -33,6 +33,7 @@ from sklearn.tests.metadata_routing_common import (
     _Registry,
     check_recorded_metadata,
 )
+from sklearn.utils._indexing import _safe_indexing
 from sklearn.utils._testing import (
     _convert_container,
     assert_allclose_dense_sparse,
@@ -2516,25 +2517,17 @@ def test_column_transformer_column_renaming(dataframe_lib):
 
     df = lib.DataFrame({"x1": [1, 2, 3], "x2": [10, 20, 30], "x3": [100, 200, 300]})
 
-    # We simulate a transformer that for some reason will return no columns.
-    # We need different function for polars and pandas to achieve this behaviour.
-    if dataframe_lib == "polars":
-
-        def _no_column_selector(X):
-            return X[:, []]
-
-    else:  # pandas
-
-        def _no_column_selector(X):
-            return X.loc[:, []]
-
     transformer = ColumnTransformer(
         transformers=[
             ("A", "passthrough", ["x1", "x2", "x3"]),
             ("B", FunctionTransformer(), ["x1", "x2"]),
             ("C", StandardScaler(), ["x1", "x3"]),
-            # special case of empty transformer
-            ("D", FunctionTransformer(_no_column_selector), ["x1", "x2", "x3"]),
+            # special case of a transformer returning 0-columns, e.g feature selector
+            (
+                "D",
+                FunctionTransformer(lambda x: _safe_indexing(x, [], axis=1)),
+                ["x1", "x2", "x3"],
+            ),
         ],
         verbose_feature_names_out=True,
     ).set_output(transform=dataframe_lib)
@@ -2558,25 +2551,17 @@ def test_column_transformer_error_with_duplicated_columns(dataframe_lib):
 
     df = lib.DataFrame({"x1": [1, 2, 3], "x2": [10, 20, 30], "x3": [100, 200, 300]})
 
-    # We simulate a transformer that for some reason will return no columns.
-    # We need different function for polars and pandas to achieve this behaviour.
-    if dataframe_lib == "polars":
-
-        def _no_column_selector(X):
-            return X[:, []]
-
-    else:  # pandas
-
-        def _no_column_selector(X):
-            return X.loc[:, []]
-
     transformer = ColumnTransformer(
         transformers=[
             ("A", "passthrough", ["x1", "x2", "x3"]),
             ("B", FunctionTransformer(), ["x1", "x2"]),
             ("C", StandardScaler(), ["x1", "x3"]),
-            # special case of empty transformer
-            ("D", FunctionTransformer(_no_column_selector), ["x1", "x2", "x3"]),
+            # special case of a transformer returning 0-columns, e.g feature selector
+            (
+                "D",
+                FunctionTransformer(lambda x: _safe_indexing(x, [], axis=1)),
+                ["x1", "x2", "x3"],
+            ),
         ],
         verbose_feature_names_out=False,
     ).set_output(transform=dataframe_lib)
