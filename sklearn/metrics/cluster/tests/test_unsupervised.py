47b9d738a367cc641696aaabc1d4c431cc2ac59f
Luis <72552948+luis261@users.noreply.github.com>
2024-06-10 03:06:05 +0200

    Adjust new test to new behavior (dedicated err)

--- sklearn/metrics/cluster/tests/test_unsupervised.py
+++ sklearn/metrics/cluster/tests/test_unsupervised.py
@@ -457,7 +457,9 @@ def test_dbcv_score_zero_distance():
 
 def test_dbcv_score_tiny_cluster():
     dbcv_score(np.random.rand(5, 2), np.array([-1, 1, 1, 0, 0]))
-    dbcv_score(np.random.rand(3, 2), np.array([1, 1, 0]))
+    expected_msg = "DBCV is not defined for clusters of size 1"
+    with pytest.raises(ValueError, match=expected_msg):
+        dbcv_score(np.random.rand(3, 2), np.array([1, 1, 0]))
 
 
 def test_dbcv_score_verbose(density_sample):
