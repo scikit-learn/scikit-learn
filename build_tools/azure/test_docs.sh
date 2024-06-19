4b246f8679a43fa85e1542ebcf025a708fd201b4
Loïc Estève <loic.esteve@ymail.com>
2024-06-19 13:54:16 +0200

    [azure parallel] debug

--- build_tools/azure/test_docs.sh
+++ build_tools/azure/test_docs.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 source build_tools/shared.sh
 activate_environment
 
+pwd
+which python
+which pytest
+
+pytest $(find doc -name '*.rst' | sort)
 python -m pytest $(find doc -name '*.rst' | sort)
