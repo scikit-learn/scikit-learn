988f0ebf8062cff1a6a2e60e110f31d8dc52cbbf
Loïc Estève <loic.esteve@ymail.com>
2024-06-19 15:17:20 +0200

    [azure parallel] clean-up

--- build_tools/azure/test_docs.sh
+++ build_tools/azure/test_docs.sh
@@ -5,10 +5,6 @@ set -ex
 source build_tools/shared.sh
 activate_environment
 
-pwd
-which python
-which pytest
-
 # XXX: for some unknown reason python -m pytest fails here in the CI, can't
 # reproduce locally and not worth spending time on this
 pytest $(find doc -name '*.rst' | sort)
