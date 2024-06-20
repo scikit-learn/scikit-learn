186bc72969e2f6e63f5336e7b908efc0b1923f39
Aditi Juneja <91629733+Schefflera-Arboricola@users.noreply.github.com>
2024-06-20 17:21:14 +0530

    indentation fix

--- sklearn/cluster/_bicluster.py
+++ sklearn/cluster/_bicluster.py
@@ -488,7 +488,7 @@ class SpectralBiclustering(BaseSpectral):
     SpectralBiclustering(n_clusters=2, random_state=0)
 
     For a more detailed example, see
-	    :ref:`sphx_glr_auto_examples_bicluster_plot_spectral_biclustering.py`
+    :ref:`sphx_glr_auto_examples_bicluster_plot_spectral_biclustering.py`
     """
 
     _parameter_constraints: dict = {
