985e48418e7e175345d459fce637bebc3210d6bb
Ian Faust <icfaust@gmail.com>
2024-06-21 15:05:05 +0200

    Update _base.py

--- sklearn/decomposition/_base.py
+++ sklearn/decomposition/_base.py
@@ -187,6 +187,8 @@ class _BasePCA(
         check_is_fitted(self)
 
         # run validate_data steps but for n_components_ instead of n_features_in_
+        X_transformed = check_array(X, input_name="X", dtype=[xp.float64, xp.float32])
+
         try:
             n_components = _num_features(X)
         except TypeError as e:
@@ -201,15 +203,14 @@ class _BasePCA(
                 f"X has {n_components} components, but {self.__class__.__name__} "
                 f"is expecting {self.n_components_} components as input."
             )
-        X = check_array(X, input_name="X", dtype=[xp.float64, xp.float32])
 
         if self.whiten:
             scaled_components = (
                 xp.sqrt(self.explained_variance_[:, np.newaxis]) * self.components_
             )
-            return X @ scaled_components + self.mean_
+            return X_transformed @ scaled_components + self.mean_
         else:
-            return X @ self.components_ + self.mean_
+            return X_transformed @ self.components_ + self.mean_
 
     @property
     def _n_features_out(self):
