b1a4f257f9b24929fb9c0dc0e29bfef1b8d6e6b5
Jérémie du Boisberranger <jeremie@probabl.ai>
2024-06-28 13:52:15 +0200

    Fix performance regression in ColumnTransformer (#29330)

--- sklearn/compose/_column_transformer.py
+++ sklearn/compose/_column_transformer.py
@@ -20,7 +20,7 @@ from ..pipeline import _fit_transform_one, _name_estimators, _transform_one
 from ..preprocessing import FunctionTransformer
 from ..utils import Bunch
 from ..utils._estimator_html_repr import _VisualBlock
-from ..utils._indexing import _determine_key_type, _get_column_indices
+from ..utils._indexing import _determine_key_type, _get_column_indices, _safe_indexing
 from ..utils._metadata_requests import METHODS
 from ..utils._param_validation import HasMethods, Hidden, Interval, StrOptions
 from ..utils._set_output import (
@@ -874,10 +874,9 @@ class ColumnTransformer(TransformerMixin, _BaseComposition):
                 jobs.append(
                     delayed(func)(
                         transformer=clone(trans) if not fitted else trans,
-                        X=X,
+                        X=_safe_indexing(X, columns, axis=1),
                         y=y,
                         weight=weight,
-                        columns=columns,
                         **extra_args,
                         params=routed_params[name],
                     )
