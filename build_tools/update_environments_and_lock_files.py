71ec23fc6666d74a9a9148da2feff90523ca8aba
Loïc Estève <loic.esteve@ymail.com>
2024-07-03 07:09:43 +0200

    [doc build] pin matplotlib<3.9

--- build_tools/update_environments_and_lock_files.py
+++ build_tools/update_environments_and_lock_files.py
@@ -374,6 +374,13 @@ build_metadata_list = [
             # and numpy 2 that cause a segmentation fault in some examples. For
             # more details, see https://github.com/pola-rs/polars/issues/16998
             "numpy": "<2",
+            # TODO: this needs to be adapted when matplotlib 3.11 is out. In
+            # the meantime, this avoids a warning in matplotlib 3.9 boxplot
+            # labels has been renamed to tick_labels. Possible options:
+            # - bump minimum matplotlib supported versions to 3.9 at one point
+            # - complicate the example code to do the right thing depending on
+            #   maplotlib version
+            "matplotlib": "<3.9",
         },
     },
     {
