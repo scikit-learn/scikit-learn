628a9e3d12d1222f30c95b3dc97c2542b816da55
Drew Craeton <102687650+craetona@users.noreply.github.com>
2024-07-08 05:21:39 -0700

    DOC Add link to plot_regression.py (#29232)
    Co-authored-by: adrinjalali <adrin.jalali@gmail.com>

--- sklearn/neighbors/_regression.py
+++ sklearn/neighbors/_regression.py
@@ -49,6 +49,10 @@ class KNeighborsRegressor(KNeighborsMixin, RegressorMixin, NeighborsBase):
 
         Uniform weights are used by default.
 
+        See the following example for a demonstration of the impact of
+        different weighting schemes on predictions:
+        :ref:`sphx_glr_auto_examples_neighbors_plot_regression.py`.
+
     algorithm : {'auto', 'ball_tree', 'kd_tree', 'brute'}, default='auto'
         Algorithm used to compute the nearest neighbors:
 
