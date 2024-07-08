c43f2560c92cb25dd335e2f70b36a67163038924
Emily Chen <emilyxinyi.chen@mail.utoronto.ca>
2024-07-08 12:40:52 +0200

    WIP on array API support for _weighted_percentile

--- sklearn/utils/stats.py
+++ sklearn/utils/stats.py
@@ -1,5 +1,4 @@
-import numpy as np
-
+from ..utils._array_api import get_namespace
 from .extmath import stable_cumsum
 
 
@@ -29,16 +28,17 @@ def _weighted_percentile(array, sample_weight, percentile=50):
     percentile : int if `array` 1D, ndarray if `array` 2D
         Weighted percentile.
     """
+    xp, _ = get_namespace(array)
     n_dim = array.ndim
     if n_dim == 0:
         return array[()]
     if array.ndim == 1:
-        array = array.reshape((-1, 1))
+        array = xp.reshape(array, (-1, 1))
     # When sample_weight 1D, repeat for each array.shape[1]
     if array.shape != sample_weight.shape and array.shape[0] == sample_weight.shape[0]:
-        sample_weight = np.tile(sample_weight, (array.shape[1], 1)).T
-    sorted_idx = np.argsort(array, axis=0)
-    sorted_weights = np.take_along_axis(sample_weight, sorted_idx, axis=0)
+        sample_weight = xp.tile(sample_weight, (array.shape[1], 1)).T
+    sorted_idx = xp.argsort(array, axis=0)
+    sorted_weights = _take_along_axis(sample_weight, sorted_idx, xp)
 
     # Find index of median prediction for each sample
     weight_cdf = stable_cumsum(sorted_weights, axis=0)
@@ -46,24 +46,39 @@ def _weighted_percentile(array, sample_weight, percentile=50):
 
     # For percentile=0, ignore leading observations with sample_weight=0. GH20528
     mask = adjusted_percentile == 0
-    adjusted_percentile[mask] = np.nextafter(
+    adjusted_percentile[mask] = xp.nextafter(
         adjusted_percentile[mask], adjusted_percentile[mask] + 1
     )
 
-    percentile_idx = np.array(
+    percentile_idx = xp.array(
         [
-            np.searchsorted(weight_cdf[:, i], adjusted_percentile[i])
+            xp.searchsorted(weight_cdf[:, i], adjusted_percentile[i])
             for i in range(weight_cdf.shape[1])
         ]
     )
-    percentile_idx = np.array(percentile_idx)
+    percentile_idx = xp.array(percentile_idx)
     # In rare cases, percentile_idx equals to sorted_idx.shape[0]
     max_idx = sorted_idx.shape[0] - 1
-    percentile_idx = np.apply_along_axis(
-        lambda x: np.clip(x, 0, max_idx), axis=0, arr=percentile_idx
+    percentile_idx = xp.apply_along_axis(
+        lambda x: xp.clip(x, 0, max_idx), axis=0, arr=percentile_idx
     )
 
-    col_index = np.arange(array.shape[1])
+    col_index = xp.arange(array.shape[1])
     percentile_in_sorted = sorted_idx[percentile_idx, col_index]
     percentile = array[percentile_in_sorted, col_index]
     return percentile[0] if n_dim == 1 else percentile
+
+
+def _take_along_axis(sample_weight, sorted_idx, xp=None):
+    sorted_weights = xp.empty_like(sorted_idx, dtype=sample_weight.dtype)
+    if sample_weight.ndim == 1:
+        for i in range(sorted_idx.shape[0]):
+            sorted_weights[i] = sample_weight[sorted_idx[i]]
+        return sorted_weights
+    elif sample_weight.ndim == 2:
+        for j in range(sorted_idx.shape[1]):
+            for i in range(sorted_idx.shape[0]):
+                sorted_weights[i, j] = sample_weight[sorted_idx[i, j], j]
+        return sorted_weights
+    else:
+        raise ValueError("Only 1D and 2D arrays are allowed")
--- sklearn/utils/tests/test_stats.py
+++ sklearn/utils/tests/test_stats.py
@@ -1,43 +1,61 @@
 import numpy as np
+import pytest
 from numpy.testing import assert_allclose
 from pytest import approx
 
+from sklearn.utils._array_api import (
+    yield_namespace_device_dtype_combinations,
+    yield_namespaces,
+)
+from sklearn.utils.estimator_checks import _array_api_for_tests
 from sklearn.utils.stats import _weighted_percentile
 
 
-def test_weighted_percentile():
-    y = np.empty(102, dtype=np.float64)
+@pytest.mark.parametrize(
+    "array_namespace, device, dtype_name", yield_namespace_device_dtype_combinations()
+)
+def test_weighted_percentile(array_namespace, device, dtype_name):
+    xp = _array_api_for_tests(array_namespace, device)
+
+    y = xp.empty(102, dtype=xp.float64, device=device)
     y[:50] = 0
     y[-51:] = 2
     y[-1] = 100000
     y[50] = 1
-    sw = np.ones(102, dtype=np.float64)
+    sw = xp.ones(102, dtype=xp.float64, device=device)
     sw[-1] = 0.0
     score = _weighted_percentile(y, sw, 50)
     assert approx(score) == 1
 
 
-def test_weighted_percentile_equal():
-    y = np.empty(102, dtype=np.float64)
-    y.fill(0.0)
-    sw = np.ones(102, dtype=np.float64)
+@pytest.mark.parametrize("array_namespace", yield_namespaces())
+def test_weighted_percentile_equal(array_namespace):
+    xp = _array_api_for_tests(array_namespace, device=None)
+    y = xp.full(102, 0.0, dtype=xp.float64)
+    sw = xp.ones(102, dtype=xp.float64)
     sw[-1] = 0.0
     score = _weighted_percentile(y, sw, 50)
     assert score == 0
 
 
-def test_weighted_percentile_zero_weight():
-    y = np.empty(102, dtype=np.float64)
-    y.fill(1.0)
-    sw = np.ones(102, dtype=np.float64)
-    sw.fill(0.0)
+@pytest.mark.parametrize("array_namespace", yield_namespaces())
+def test_weighted_percentile_zero_weight(array_namespace):
+    xp = _array_api_for_tests(array_namespace, device=None)
+    y = xp.full(102, 1.0, dtype=xp.float64)
+    sw = xp.full(102, 0.0, dtype=xp.float64)
     score = _weighted_percentile(y, sw, 50)
     assert approx(score) == 1.0
 
 
-def test_weighted_percentile_zero_weight_zero_percentile():
-    y = np.array([0, 1, 2, 3, 4, 5])
-    sw = np.array([0, 0, 1, 1, 1, 0])
+@pytest.mark.parametrize(
+    "array_namespace, device, dtype_name", yield_namespace_device_dtype_combinations()
+)
+def test_weighted_percentile_zero_weight_zero_percentile(
+    array_namespace, device, dtype_name
+):
+    xp = _array_api_for_tests(array_namespace, device)
+    y = xp.asarray([0, 1, 2, 3, 4, 5], device=device)
+    sw = xp.asarray([0, 0, 1, 1, 1, 0], device=device)
     score = _weighted_percentile(y, sw, 0)
     assert approx(score) == 2
 
@@ -48,23 +66,32 @@ def test_weighted_percentile_zero_weight_zero_percentile():
     assert approx(score) == 4
 
 
-def test_weighted_median_equal_weights():
+@pytest.mark.parametrize(
+    "array_namespace, device, dtype_name", yield_namespace_device_dtype_combinations()
+)
+def test_weighted_median_equal_weights(array_namespace, device, dtype_name):
+    xp = _array_api_for_tests(array_namespace, device)
     # Checks weighted percentile=0.5 is same as median when weights equal
     rng = np.random.RandomState(0)
     # Odd size as _weighted_percentile takes lower weighted percentile
     x = rng.randint(10, size=11)
-    weights = np.ones(x.shape)
+    x = x.astype(dtype_name, copy=False)
+    weights = xp.ones(x.shape)
 
     median = np.median(x)
     w_median = _weighted_percentile(x, weights)
     assert median == approx(w_median)
 
 
-def test_weighted_median_integer_weights():
+@pytest.mark.parametrize(
+    "array_namespace, device, dtype_name", yield_namespace_device_dtype_combinations()
+)
+def test_weighted_median_integer_weights(array_namespace, device, dtype_name):
     # Checks weighted percentile=0.5 is same as median when manually weight
     # data
     rng = np.random.RandomState(0)
     x = rng.randint(20, size=10)
+    x = x.astype(dtype_name, copy=False)
     weights = rng.choice(5, size=10)
     x_manual = np.repeat(x, weights)
 
