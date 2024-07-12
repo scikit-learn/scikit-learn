634aeb134286318fbc12657987d76b436b980016
Tingshan Liu <112791813+tingshanL@users.noreply.github.com>
2024-07-12 02:36:01 -0400

    increase codecov

--- sklearn/mixture/tests/test_gaussian_mixture_ic.py
+++ sklearn/mixture/tests/test_gaussian_mixture_ic.py
@@ -7,53 +7,65 @@ from numpy.testing import assert_allclose, assert_equal
 from sklearn.exceptions import NotFittedError
 from sklearn.metrics import adjusted_rand_score
 from sklearn.mixture import GaussianMixtureIC
+from sklearn.utils._param_validation import InvalidParameterError
 
 
-def _test_inputs(X, error_type, **kws):
+def _test_wrong_inputs(X, error_type, **kws):
     with pytest.raises(error_type):
         gmIC = GaussianMixtureIC(**kws)
         gmIC.fit(X)
+
+
+def _test_right_inputs(X, **kws):
+    gmIC = GaussianMixtureIC(**kws)
+    gmIC.fit(X)
 
 
 def test_n_components():
     X = np.random.normal(0, 1, size=(100, 3))
 
     # min_components must be less than 1
-    _test_inputs(X, ValueError, min_components=0)
+    _test_wrong_inputs(X, ValueError, min_components=0)
 
     # min_components must be an integer
-    _test_inputs(X, TypeError, min_components="1")
+    _test_wrong_inputs(X, TypeError, min_components="1")
 
     # max_components must be at least min_components
-    _test_inputs(X, ValueError, max_components=0)
+    _test_wrong_inputs(X, ValueError, max_components=0)
 
     # max_components must be an integer
-    _test_inputs(X, TypeError, max_components="1")
+    _test_wrong_inputs(X, TypeError, max_components="1")
 
     # max_components must be at most n_samples
-    _test_inputs(X, ValueError, max_components=101)
+    _test_wrong_inputs(X, ValueError, max_components=101)
 
     # min_components must be at most n_samples
-    _test_inputs(X, ValueError, **{"min_components": 101, "max_components": 102})
+    _test_wrong_inputs(X, ValueError, **{"min_components": 101, "max_components": 102})
 
 
 def test_input_param():
     X = np.random.normal(0, 1, size=(100, 3))
 
     # covariance type is not an array, string or list
-    _test_inputs(X, TypeError, covariance_type=1)
+    _test_wrong_inputs(X, InvalidParameterError, covariance_type=1)
 
     # covariance type is not in ['spherical', 'diag', 'tied', 'full', 'all']
-    _test_inputs(X, ValueError, covariance_type="1")
+    _test_wrong_inputs(X, InvalidParameterError, covariance_type="1")
+
+    # several but not all covariance types in ['spherical', 'diag', 'tied', 'full']
+    _test_right_inputs(X, covariance_type=["spherical", "diag"])
+
+    # covariance type is 'all'
+    _test_right_inputs(X, covariance_type="all")
 
     # criterion is not "aic" or "bic"
-    _test_inputs(X, ValueError, criterion="cic")
+    _test_wrong_inputs(X, ValueError, criterion="cic")
 
     # n_init is not an integer
-    _test_inputs(X, TypeError, n_init="1")
+    _test_wrong_inputs(X, TypeError, n_init="1")
 
     # n_init must be at least 1
-    _test_inputs(X, ValueError, n_init=0)
+    _test_wrong_inputs(X, ValueError, n_init=0)
 
 
 def test_predict_without_fit():
