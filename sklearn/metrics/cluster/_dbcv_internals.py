294f6ff307593447fa7b4ae4e44775a571a55d30
Luis <72552948+luis261@users.noreply.github.com>
2024-07-13 00:51:21 -0700

    Avoid user warnings in max ratio calc
    Co-authored-by: Meekail Zain <34613774+Micky774@users.noreply.github.com>

--- sklearn/metrics/cluster/_dbcv_internals.py
+++ sklearn/metrics/cluster/_dbcv_internals.py
@@ -308,5 +308,5 @@ def _all_points_core_distance(distance_matrix, d=2):
 
 def _max_ratio(stacked_distances):
     raw_dist, core_dist, _ = np.moveaxis(stacked_distances, -1, 0)
-    ratios = np.where(raw_dist != 0, core_dist / raw_dist, 0)
-    return ratios.max()
+    ratios = np.zeros_like(raw_dist) 
+    np.divide(core_dist, raw_dist, out=ratios, where=(raw_dist!=0))
