07bcde3dc130a1e84a073499a4f0a505d95de3c8
VinayBhagavath <123284895+VinayBhagavath@users.noreply.github.com>
2024-07-30 13:06:12 -0700

    removed original example link

--- sklearn/neighbors/_base.py
+++ sklearn/neighbors/_base.py
@@ -786,9 +786,6 @@ class KNeighborsMixin:
         >>> neigh.kneighbors(X, return_distance=False)
         array([[1],
                [2]]...)
-
-        For a more comprehensive example see:
-        :ref:`sphx_glr_auto_examples_neighbors_approximate_nearest_neighbors.py`
         """
         check_is_fitted(self)
 
