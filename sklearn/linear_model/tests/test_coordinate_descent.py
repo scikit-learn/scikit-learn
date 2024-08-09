f774c6acce08c80bde962cbf5b01117b22c84025
Shruti Nath <shrutinath@yahoo.com>
2024-08-08 20:13:27 -0400

    updated comments and changelog

--- sklearn/linear_model/tests/test_coordinate_descent.py
+++ sklearn/linear_model/tests/test_coordinate_descent.py
@@ -265,7 +265,9 @@ def test_lasso_cv():
     )
     # check that they also give a similar MSE
     mse_lars = interpolate.interp1d(lars.cv_alphas_, lars.mse_path_.T)
-    assert_allclose(mse_lars(clf.alphas_[5]).mean(), clf.mse_path_[5].mean(), rtol=1e-2)
+    np.testing.assert_approx_equal(
+        mse_lars(clf.alphas_[5]).mean(), clf.mse_path_[5].mean(), significant=2
+    )
 
     # test set
     assert clf.score(X_test, y_test) > 0.99
@@ -1308,23 +1310,37 @@ def test_enet_sample_weight_consistency(
 def test_enet_cv_sample_weight_correctness(
     fit_intercept, sparse_container, global_random_seed
 ):
-    """Test that ElasticNetCV with sample weights gives correct results."""
+    """Test that ElasticNetCV with sample weights gives correct results.
+
+    We fit the same model twice, once with weighted training data, once with repeated
+    data points in the training data and check that both models converge to the
+    same solution.
+    Since this model uses an internal cross-validation scheme to tune the alpha
+    regularization parameter, we make sure that the repetitions only occur within
+    a specific CV group. Data points belonging to other CV groups stay
+    unit-weighted / "unrepeated".
+    """
     rng = np.random.RandomState(global_random_seed)
-    n_splits, n_samples, n_features = 3, 10, 5
-    X_with_weights = rng.rand(n_splits * n_samples, n_features)
+    n_splits, n_samples_per_cv, n_features = 3, 10, 5
+    X_with_weights = rng.rand(n_splits * n_samples_per_cv, n_features)
     beta = rng.rand(n_features)
     beta[0:2] = 0
-    y_with_weights = X_with_weights @ beta + rng.rand(n_splits * n_samples)
-    sw = np.ones_like(y_with_weights)
+    y_with_weights = X_with_weights @ beta + rng.rand(n_splits * n_samples_per_cv)
+
     if sparse_container is not None:
         X_with_weights = sparse_container(X_with_weights)
     params = dict(tol=1e-6)
 
-    # Inspect the total number of random repetitions so as to adjust the size of
-    # the first cross-validation group accordingly.
-    sw[:n_samples] = rng.randint(0, 5, size=n_samples)
+    # Assign random integer weights only to the first cross-validation group.
+    # The samples in the other cross-validation groups are left with unit
+    # weights.
+
+    sw = np.ones_like(y_with_weights)
+    sw[:n_samples_per_cv] = rng.randint(0, 5, size=n_samples_per_cv)
     groups_with_weights = np.r_[
-        np.full(n_samples, 0), np.full(n_samples, 1), np.full(n_samples, 2)
+        np.full(n_samples_per_cv, 0),
+        np.full(n_samples_per_cv, 1),
+        np.full(n_samples_per_cv, 2),
     ]
     splits_with_weights = list(
         LeaveOneGroupOut().split(X_with_weights, groups=groups_with_weights)
@@ -1332,13 +1348,12 @@ def test_enet_cv_sample_weight_correctness(
     reg_with_weights = ElasticNetCV(
         cv=splits_with_weights, fit_intercept=fit_intercept, **params
     )
+
     reg_with_weights.fit(X_with_weights, y_with_weights, sample_weight=sw)
 
-    # We repeat the first fold 2 times and provide splits ourselves
     if sparse_container is not None:
         X_with_weights = X_with_weights.toarray()
     X_with_repetitions = np.repeat(X_with_weights, sw.astype(int), axis=0)
-
     if sparse_container is not None:
         X_with_repetitions = sparse_container(X_with_repetitions)
 
@@ -1356,7 +1371,7 @@ def test_enet_cv_sample_weight_correctness(
     # ensure that we chose meaningful alphas, i.e. not boundaries
     assert_allclose(reg_with_weights.mse_path_, reg_with_repetitions.mse_path_)
     assert_allclose(reg_with_weights.alphas_, reg_with_repetitions.alphas_)
-    assert reg_with_weights.alpha_ == reg_with_repetitions.alpha_
+    assert reg_with_weights.alpha_ == pytest.approx(reg_with_repetitions.alpha_)
     assert_allclose(reg_with_weights.coef_, reg_with_repetitions.coef_)
     assert reg_with_weights.intercept_ == pytest.approx(reg_with_repetitions.intercept_)
 
@@ -1394,26 +1409,6 @@ def test_enet_cv_grid_search(sample_weight):
 
     assert reg.l1_ratio_ == pytest.approx(gs.best_params_["l1_ratio"])
     assert reg.alpha_ == pytest.approx(gs.best_params_["alpha"])
-
-
-@pytest.mark.parametrize("sparseX", [False, True])
-@pytest.mark.parametrize("fit_intercept", [False, True])
-@pytest.mark.parametrize("sample_weight", [np.array([10, 1, 10, 1]), None])
-def test_enet_alpha_max_sample_weight(sparseX, fit_intercept, sample_weight):
-    X = np.array([[3.0, 1.0], [2.0, 5.0], [5.0, 3.0], [1.0, 4.0]])
-    beta = np.array([1, 1])
-    y = X @ beta
-    if sparseX:
-        X = sparse.csc_matrix(X)
-    # Test alpha_max makes coefs zero.
-    reg = ElasticNetCV(n_alphas=1, cv=2, eps=1, fit_intercept=fit_intercept)
-    reg.fit(X, y, sample_weight=sample_weight)
-    assert_almost_equal(reg.coef_, 0)
-    alpha_max = reg.alpha_
-    # Test smaller alpha makes coefs nonzero.
-    reg = ElasticNet(alpha=0.99 * alpha_max, fit_intercept=fit_intercept)
-    reg.fit(X, y, sample_weight=sample_weight)
-    assert_array_less(1e-3, np.max(np.abs(reg.coef_)))
 
 
 @pytest.mark.parametrize("fit_intercept", [True, False])
@@ -1470,9 +1465,29 @@ def test_enet_cv_sample_weight_consistency(
         assert_allclose(reg.intercept_, intercept)
 
 
+@pytest.mark.parametrize("X_is_sparse", [False, True])
+@pytest.mark.parametrize("fit_intercept", [False, True])
+@pytest.mark.parametrize("sample_weight", [np.array([10, 1, 10, 1]), None])
+def test_enet_alpha_max_sample_weight(X_is_sparse, fit_intercept, sample_weight):
+    X = np.array([[3.0, 1.0], [2.0, 5.0], [5.0, 3.0], [1.0, 4.0]])
+    beta = np.array([1, 1])
+    y = X @ beta
+    if X_is_sparse:
+        X = sparse.csc_matrix(X)
+    # Test alpha_max makes coefs zero.
+    reg = ElasticNetCV(n_alphas=1, cv=2, eps=1, fit_intercept=fit_intercept)
+    reg.fit(X, y, sample_weight=sample_weight)
+    assert_almost_equal(reg.coef_, 0)
+    alpha_max = reg.alpha_
+    # Test smaller alpha makes coefs nonzero.
+    reg = ElasticNet(alpha=0.99 * alpha_max, fit_intercept=fit_intercept)
+    reg.fit(X, y, sample_weight=sample_weight)
+    assert_array_less(1e-3, np.max(np.abs(reg.coef_)))
+
+
 @pytest.mark.parametrize("estimator", [ElasticNetCV, LassoCV])
 def test_linear_models_cv_fit_with_loky(estimator):
-    # LinearModelsCV.fit performs inplace operations on fancy-indexed memmapped
+    # LinearModelsCV.fit performs operations on fancy-indexed memmapped
     # data when using the loky backend, causing an error due to unexpected
     # behavior of fancy indexing of read-only memmaps (cf. numpy#14132).
 
