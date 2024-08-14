96e30abdd5e68d2c65ca479c1ea7626985d20658
Charlie-XIAO <yx2436@nyu.edu>
2024-08-14 22:16:40 +0800

    navigation when there are no hits

--- doc/js/scripts/algolia-searchbox.js
+++ doc/js/scripts/algolia-searchbox.js
@@ -14,6 +14,27 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   searchPageHref += "algolia-search.html";
 
+  // Function to navigate to the all results page
+  const navigateToResultsPage = () => {
+    const link = document.getElementById("sk-search-all-results-link");
+    if (link !== null) {
+      // If there is the "see all results" link, just click it
+      link.click();
+      return;
+    }
+
+    const inputBox = document.getElementById("docsearch-input");
+    if (inputBox === null || inputBox.value === "") {
+      // If we cannot get the input box or the input box is empty, navigate to the
+      // all results page with no query
+      window.location.assign(searchPageHref);
+      return;
+    }
+    // Navigate to the all results page with query constructed from the input
+    const query = new URLSearchParams({ q: inputBox.value });
+    window.location.assign(`${searchPageHref}?${query}`);
+  };
+
   // Initialize the Algolia DocSearch widget
   docsearch({
     container: "#docsearch",
@@ -38,14 +59,30 @@ document.addEventListener("DOMContentLoaded", () => {
       // Hack implementation to navigate to the search page instead of navigating to the
       // corresponding search result page; `navigateNewTab` and `navigateNewWindow` are
       // still left as the default behavior
-      navigate: () => {
-        const link = document.getElementById("sk-search-all-results-link");
-        if (link) {
-          link.click();
-        } else {
-          window.location.assign(searchPageHref);
-        }
-      },
+      navigate: navigateToResultsPage,
     },
+  });
+
+  // The navigator API only works when there are search results; there are cases where
+  // there are no hits, e.g. empty query + no history, in which case we need to manually
+  // listen to the Enter key
+  document.addEventListener("keydown", (e) => {
+    if (
+      e.key === "Enter" &&
+      !e.shiftKey &&
+      !e.ctrlKey &&
+      !e.metaKey &&
+      !e.altKey
+    ) {
+      const container = document.querySelector(
+        ".DocSearch.DocSearch-Container"
+      );
+      if (container === null) {
+        return;
+      }
+      e.preventDefault();
+      e.stopPropagation();
+      navigateToResultsPage();
+    }
   });
 });
