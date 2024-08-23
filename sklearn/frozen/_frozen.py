532c965253914e58e89025d1223fe2996df44481
adrinjalali <adrin.jalali@gmail.com>
2024-08-23 13:15:36 +0200

    skip tests

--- sklearn/frozen/_frozen.py
+++ sklearn/frozen/_frozen.py
@@ -2,7 +2,9 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 from ..base import BaseEstimator
+from ..exceptions import NotFittedError
 from ..utils._available_if import available_if
+from ..utils.validation import check_is_fitted
 
 
 def _estimator_has(attr):
@@ -15,6 +17,23 @@ def _estimator_has(attr):
         # raise original `AttributeError` if `attr` does not exist
         getattr(self.estimator, attr)
         return True
+
+    return check
+
+
+def _estimator_fitted():
+    """Check that final_estimator has `attr`.
+
+    Used together with `available_if`.
+    """
+
+    def check(self):
+        # raise original `AttributeError` if `attr` does not exist
+        try:
+            check_is_fitted(self)
+            return True
+        except NotFittedError:
+            return False
 
     return check
 
@@ -92,9 +111,10 @@ class FrozenEstimator(BaseEstimator):
         self : object
             Returns the instance itself.
         """
+        check_is_fitted(self.estimator)
         return self
 
-    @available_if(_estimator_has("fit_transform"))
+    @available_if(_estimator_has("fit_transform") and _estimator_fitted())
     def fit_transform(self, X, y=None, *args, **kwargs):
         """Call `estimator.transform`.
 
@@ -124,7 +144,7 @@ class FrozenEstimator(BaseEstimator):
         # fit_transform only transforms the data
         return self.estimator.transform(X, *args, **kwargs)
 
-    @available_if(_estimator_has("fit_predict"))
+    @available_if(_estimator_has("fit_predict") and _estimator_fitted())
     def fit_predict(self, X, y=None, *args, **kwargs):
         """Call `estimator.predict`.
 
@@ -153,3 +173,8 @@ class FrozenEstimator(BaseEstimator):
         """
         # fit_transform only transforms the data
         return self.estimator.predict(X, *args, **kwargs)
+
+    def _more_tags(self):
+        return {
+            "_skip_test": True,
+        }
