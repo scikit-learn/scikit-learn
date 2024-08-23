c55ff81860585fa75a9ab9f182651a55daed92af
Štěpán Sršeň <srsen.stepan@gmail.com>
2024-08-23 14:48:26 +0200

    DOC clarify that n_jobs argument uses threading in pairwise_distances and pairwise_kernels (#29693)
    Co-authored-by: Stepan Srsen <srsen@manganese.itc.univie.ac.at>
    Co-authored-by: Thomas J. Fan <thomasjpfan@gmail.com>

--- sklearn/metrics/pairwise.py
+++ sklearn/metrics/pairwise.py
@@ -1883,7 +1883,7 @@ def _dist_wrapper(dist_func, dist_matrix, slice_, *args, **kwargs):
 
 def _parallel_pairwise(X, Y, func, n_jobs, **kwds):
     """Break the pairwise matrix in n_jobs even slices
-    and compute them in parallel."""
+    and compute them using multithreading."""
 
     if Y is None:
         Y = X
@@ -2272,8 +2272,8 @@ def pairwise_distances(
 
     n_jobs : int, default=None
         The number of jobs to use for the computation. This works by breaking
-        down the pairwise matrix into n_jobs even slices and computing them in
-        parallel.
+        down the pairwise matrix into n_jobs even slices and computing them
+        using multithreading.
 
         ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
         ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
@@ -2517,8 +2517,8 @@ def pairwise_kernels(
 
     n_jobs : int, default=None
         The number of jobs to use for the computation. This works by breaking
-        down the pairwise matrix into n_jobs even slices and computing them in
-        parallel.
+        down the pairwise matrix into n_jobs even slices and computing them
+        using multithreading.
 
         ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
         ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
