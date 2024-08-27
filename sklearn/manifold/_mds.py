7234c0081f61769d7f277376417653c6417e41c7
David Matthew Cherney <103840964+davidcherney@users.noreply.github.com>
2024-08-27 10:11:33 -0600

    added link to examples_manifold_plot_compare_methods.py to manifold.MDS doc string (#29714)

--- sklearn/manifold/_mds.py
+++ sklearn/manifold/_mds.py
@@ -524,8 +524,11 @@ class MDS(BaseEstimator):
     >>> X_transformed.shape
     (100, 2)
 
-    For a more detailed example of usage, see:
-    :ref:`sphx_glr_auto_examples_manifold_plot_mds.py`
+    For a more detailed example of usage, see
+    :ref:`sphx_glr_auto_examples_manifold_plot_mds.py`.
+
+    For a comparison of manifold learning techniques, see
+    :ref:`sphx_glr_auto_examples_manifold_plot_compare_methods.py`.
     """
 
     _parameter_constraints: dict = {
