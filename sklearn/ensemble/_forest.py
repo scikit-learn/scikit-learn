44b14ecab0d92e20f4a6a473fc4e21c94f9171f0
Michael Mayer <mayermichael79@gmail.com>
2024-08-31 14:11:40 +0200

    Fix-formatting-rf

--- sklearn/ensemble/_forest.py
+++ sklearn/ensemble/_forest.py
@@ -1825,7 +1825,6 @@ class RandomForestRegressor(ForestRegressor):
     References
     ----------
     .. [1] L. Breiman, "Random Forests", Machine Learning, 45(1), 5-32, 2001.
-
     .. [2] P. Geurts, D. Ernst., and L. Wehenkel, "Extremely randomized
            trees", Machine Learning, 63(1), 3-42, 2006.
 
