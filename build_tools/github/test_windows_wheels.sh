409883204828a266c692354ee81ebef7b42f592b
Loïc Estève <loic.esteve@ymail.com>
2024-09-06 16:04:47 +0200

    [cd build gh] Attempt fix on Windows with debug

--- build_tools/github/test_windows_wheels.sh
+++ build_tools/github/test_windows_wheels.sh
@@ -3,7 +3,33 @@
 set -e
 set -x
 
+echo number of args: $#
 PYTHON_VERSION=$1
+shift
+TEST_REQUIRES=$*
+
+echo CIBW_TEST_REQUIRES: $CIBW_TEST_REQUIRES
+
+
+if [[ "$PYTHON_VERSION" == "313" ]]; then
+    # TODO: remove when pandas has a release with python 3.13 wheels
+    # First install numpy release
+    docker container run \
+        --rm scikit-learn/minimal-windows \
+        powershell -Command "python -m pip install numpy"
+    # Then install pandas-dev
+    docker container run \
+        --rm scikit-learn/minimal-windows \
+        powershell -Command "python -m pip install --pre --extra-index https://pypi.anaconda.org/scientific-python-nightly-wheels/simple pandas --only-binary :all:"
+fi
+
+docker container run \
+        --rm scikit-learn/minimal-windows \
+        powershell -Command "python -m pip install $TEST_REQUIRES"
+
+docker container run \
+    --rm scikit-learn/minimal-windows \
+    powershell -Command "python -m pip install  -c 'import sklearn; sklearn.show_versions()'"
 
 docker container run \
     --rm scikit-learn/minimal-windows \
