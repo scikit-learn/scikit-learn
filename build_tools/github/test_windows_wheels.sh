0beded47374f611af435f2481a5d0958e79e4d65
Loïc Estève <loic.esteve@ymail.com>
2024-09-09 11:45:49 +0200

    [cd build gh] Windows attempt

--- build_tools/github/test_windows_wheels.sh
+++ build_tools/github/test_windows_wheels.sh
@@ -5,27 +5,24 @@ set -x
 
 PYTHON_VERSION=$1
 
+container_id=$(docker run -it -d scikit-learn/minimal-windows)
+
 if [[ "$PYTHON_VERSION" == "313" ]]; then
     # TODO: remove when pandas has a release with python 3.13 wheels
     # First install numpy release
-    docker container run \
-        --rm scikit-learn/minimal-windows \
+    docker exec $container_id \
         powershell -Command "python -m pip install numpy"
     # Then install pandas-dev
-    docker container run \
-        --rm scikit-learn/minimal-windows \
+    docker exec $container_id \
         powershell -Command "python -m pip install --pre --extra-index https://pypi.anaconda.org/scientific-python-nightly-wheels/simple pandas --only-binary :all:"
 fi
 
-docker container run \
-    --rm scikit-learn/minimal-windows \
+docker exec $container_id \
     powershell -Command "python -m pip install $CIBW_TEST_REQUIRES"
 
-docker container run \
-    --rm scikit-learn/minimal-windows \
+docker exec $container_id \
     powershell -Command "python -c 'import sklearn; sklearn.show_versions()'"
 
-docker container run \
+docker exec $container_id \
     -e SKLEARN_SKIP_NETWORK_TESTS=1 \
-    --rm scikit-learn/minimal-windows \
     powershell -Command "pytest --pyargs sklearn"
