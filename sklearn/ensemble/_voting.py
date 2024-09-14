db7149a7e05e81b3fea4fb4a1bc6a802327a5ede
Kevin Doshi <kevin.doshi2@spit.ac.in>
2024-09-14 10:48:57 +0530

    changes link position

--- sklearn/ensemble/_voting.py
+++ sklearn/ensemble/_voting.py
@@ -555,6 +555,9 @@ class VotingRegressor(RegressorMixin, _BaseVoting):
     regressors, each on the whole dataset. Then it averages the individual
     predictions to form a final prediction.
 
+    For a more detailed example, refer to
+    :ref:`sphx_glr_auto_examples_ensemble_plot_voting_regressor.py`.
+
     Read more in the :ref:`User Guide <voting_regressor>`.
 
     .. versionadded:: 0.21
@@ -637,10 +640,6 @@ class VotingRegressor(RegressorMixin, _BaseVoting):
     >>> er = er.fit(X, y)
     >>> len(er.estimators_)
     2
-
-    For a detailed example of utilizing
-    :class:`~sklearn.ensemble.VotingRegressor`
-    For a more detailed example, refer to :ref:sphx_glr_auto_examples_ensemble_plot_voting_regressor.py.
     """
 
     def __init__(self, estimators, *, weights=None, n_jobs=None, verbose=False):
