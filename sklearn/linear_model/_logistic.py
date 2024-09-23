5a7d0f02ad16d655cf84a8358b344a53d49c17d5
Sean Moiselle <91853278+Sean-Jay-M@users.noreply.github.com>
2024-09-23 13:03:59 +0100

    DOC added link to plot_classification_probability in LogisticRegression

--- sklearn/linear_model/_logistic.py
+++ sklearn/linear_model/_logistic.py
@@ -829,6 +829,11 @@ class LogisticRegression(LinearClassifierMixin, SparseCoefMixin, BaseEstimator):
 
     Read more in the :ref:`User Guide <logistic_regression>`.
 
+    For a visual example comparing SVM, L1 and L2
+    penalized logistic regression (multinomial multiclass), 
+    a One-Vs-Rest with logistic regression, and GPC see:
+    :ref:`sphx_glr_auto_examples_classification_plot_classification_probability.py`
+
     Parameters
     ----------
     penalty : {'l1', 'l2', 'elasticnet', None}, default='l2'
