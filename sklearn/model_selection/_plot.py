1c93fb81a8f4257cbd57c7bc8a3feeae2cb518de
bharat reddy <bharatreddy761@gmail.com>
2024-10-02 14:35:11 +0530

    add link to learning curve example

--- sklearn/model_selection/_plot.py
+++ sklearn/model_selection/_plot.py
@@ -200,6 +200,8 @@ class LearningCurveDisplay(_BaseCurveDisplay):
     >>> display.plot()
     <...>
     >>> plt.show()
+    For a detailed example on how to use LearningCurveDisplay see:
+    ref:`sphx_glr_auto_examples_model_selection_plot_learning_curve.py`
     """
 
     def __init__(self, *, train_sizes, train_scores, test_scores, score_name=None):
