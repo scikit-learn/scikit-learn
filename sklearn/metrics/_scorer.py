24041f6f56d26160e6053136f7ee8098c927d901
Jérémie du Boisberranger <jeremie@probabl.ai>
2024-10-03 15:29:59 +0200

    cln deprecations

--- sklearn/metrics/_scorer.py
+++ sklearn/metrics/_scorer.py
@@ -28,7 +28,7 @@ import numpy as np
 
 from ..base import is_regressor
 from ..utils import Bunch
-from ..utils._param_validation import HasMethods, Hidden, StrOptions, validate_params
+from ..utils._param_validation import HasMethods, StrOptions, validate_params
 from ..utils._response import _get_response_values
 from ..utils.metadata_routing import (
     MetadataRequest,
@@ -605,55 +605,6 @@ def _check_multimetric_scoring(estimator, scoring):
     return scorers
 
 
-def _get_response_method(response_method, needs_threshold, needs_proba):
-    """Handles deprecation of `needs_threshold` and `needs_proba` parameters in
-    favor of `response_method`.
-    """
-    needs_threshold_provided = needs_threshold != "deprecated"
-    needs_proba_provided = needs_proba != "deprecated"
-    response_method_provided = response_method is not None
-
-    needs_threshold = False if needs_threshold == "deprecated" else needs_threshold
-    needs_proba = False if needs_proba == "deprecated" else needs_proba
-
-    if response_method_provided and (needs_proba_provided or needs_threshold_provided):
-        raise ValueError(
-            "You cannot set both `response_method` and `needs_proba` or "
-            "`needs_threshold` at the same time. Only use `response_method` since "
-            "the other two are deprecated in version 1.4 and will be removed in 1.6."
-        )
-
-    if needs_proba_provided or needs_threshold_provided:
-        warnings.warn(
-            (
-                "The `needs_threshold` and `needs_proba` parameter are deprecated in "
-                "version 1.4 and will be removed in 1.6. You can either let "
-                "`response_method` be `None` or set it to `predict` to preserve the "
-                "same behaviour."
-            ),
-            FutureWarning,
-        )
-
-    if response_method_provided:
-        return response_method
-
-    if needs_proba is True and needs_threshold is True:
-        raise ValueError(
-            "You cannot set both `needs_proba` and `needs_threshold` at the same "
-            "time. Use `response_method` instead since the other two are deprecated "
-            "in version 1.4 and will be removed in 1.6."
-        )
-
-    if needs_proba is True:
-        response_method = "predict_proba"
-    elif needs_threshold is True:
-        response_method = ("decision_function", "predict_proba")
-    else:
-        response_method = "predict"
-
-    return response_method
-
-
 def _get_response_method_name(response_method):
     try:
         return response_method.__name__
@@ -671,20 +622,10 @@ def _get_response_method_name(response_method):
             StrOptions({"predict", "predict_proba", "decision_function"}),
         ],
         "greater_is_better": ["boolean"],
-        "needs_proba": ["boolean", Hidden(StrOptions({"deprecated"}))],
-        "needs_threshold": ["boolean", Hidden(StrOptions({"deprecated"}))],
     },
     prefer_skip_nested_validation=True,
 )
-def make_scorer(
-    score_func,
-    *,
-    response_method=None,
-    greater_is_better=True,
-    needs_proba="deprecated",
-    needs_threshold="deprecated",
-    **kwargs,
-):
+def make_scorer(score_func, *, response_method=None, greater_is_better=True, **kwargs):
     """Make a scorer from a performance metric or loss function.
 
     A scorer is a wrapper around an arbitrary metric or loss function that is called
@@ -719,39 +660,14 @@ def make_scorer(
 
         .. versionadded:: 1.4
 
+        .. deprecated:: 1.6
+            None is equivalent to 'predict' and is deprecated. It will be removed in
+            version 1.8.
+
     greater_is_better : bool, default=True
         Whether `score_func` is a score function (default), meaning high is
         good, or a loss function, meaning low is good. In the latter case, the
         scorer object will sign-flip the outcome of the `score_func`.
-
-    needs_proba : bool, default=False
-        Whether `score_func` requires `predict_proba` to get probability
-        estimates out of a classifier.
-
-        If True, for binary `y_true`, the score function is supposed to accept
-        a 1D `y_pred` (i.e., probability of the positive class, shape
-        `(n_samples,)`).
-
-        .. deprecated:: 1.4
-           `needs_proba` is deprecated in version 1.4 and will be removed in
-           1.6. Use `response_method="predict_proba"` instead.
-
-    needs_threshold : bool, default=False
-        Whether `score_func` takes a continuous decision certainty.
-        This only works for binary classification using estimators that
-        have either a `decision_function` or `predict_proba` method.
-
-        If True, for binary `y_true`, the score function is supposed to accept
-        a 1D `y_pred` (i.e., probability of the positive class or the decision
-        function, shape `(n_samples,)`).
-
-        For example `average_precision` or the area under the roc curve
-        can not be computed using discrete predictions alone.
-
-        .. deprecated:: 1.4
-           `needs_threshold` is deprecated in version 1.4 and will be removed
-           in 1.6. Use `response_method=("decision_function", "predict_proba")`
-           instead to preserve the same behaviour.
 
     **kwargs : additional arguments
         Additional parameters to be passed to `score_func`.
@@ -772,10 +688,17 @@ def make_scorer(
     >>> grid = GridSearchCV(LinearSVC(), param_grid={'C': [1, 10]},
     ...                     scoring=ftwo_scorer)
     """
-    response_method = _get_response_method(
-        response_method, needs_threshold, needs_proba
-    )
     sign = 1 if greater_is_better else -1
+
+    if response_method is None:
+        warnings.warn(
+            "The default value of response_method will change from None to 'predict' "
+            "in version 1.8 and None wont be a valid option anymore. Set "
+            "response_method='predict' to avoid this warning.",
+            FutureWarning,
+        )
+        response_method = "predict"
+
     return _Scorer(score_func, sign, kwargs, response_method)
 
 
