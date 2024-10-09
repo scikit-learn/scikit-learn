b0049d38ddcf9b97f39a6ea232de14c95f8740d0
Pablo Salinas Rodriguez <34652443+Pablitosalinero@users.noreply.github.com>
2024-10-09 17:39:22 +0000

    Fix doctests

--- sklearn/model_selection/_split.py
+++ sklearn/model_selection/_split.py
@@ -2493,10 +2493,10 @@ class StratifiedShuffleSplitRegression(StratifiedShuffleSplit):
     >>> X = np.array([[1, 2], [3, 4], [1, 2], [3, 4], [1, 2], [3, 4]])
     >>> y = np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6])
     >>> sss = StratifiedShuffleSplitRegression(
-    >>>         n_splits=5,
-    >>>         test_size=0.5,
-    >>>         n_bins=2,
-    >>>         random_state=0
+    ...     n_splits=5,
+    ...     test_size=0.5,
+    ...     n_bins=2,
+    ...     random_state=0
     >>> )
     >>> y_binned = np.floor(y / y.max() * 10).astype(int)
     >>> sss.get_n_splits(X, y_binned)
