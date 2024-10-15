ba3833fd077fd0a0f3c4fbfb2d080bbd4209275b
Antoine Baker <antoine.baker59@gmail.com>
2024-10-15 13:46:42 +0200

    numpy default for cond

--- sklearn/linear_model/_base.py
+++ sklearn/linear_model/_base.py
@@ -674,7 +674,7 @@ class LinearRegression(MultiOutputMixin, RegressorMixin, LinearModel):
                 self.coef_ = np.vstack([out[0] for out in outs])
         else:
             # cut-off ratio for small singular values
-            cond = np.finfo(X.dtype).resolution
+            cond = max(X.shape) * np.finfo(X.dtype).eps
             self.coef_, _, self.rank_, self.singular_ = linalg.lstsq(X, y, cond=cond)
             self.coef_ = self.coef_.T
 
