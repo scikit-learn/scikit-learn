b16e2910a0ce3c462d98c149d0c815026a2b67dc
Austin <504977925@qq.com>
2024-10-29 17:26:16 +0800

    Update sklearn/linear_model/_least_angle.py
    Co-authored-by: Guillaume Lemaitre <guillaume@probabl.ai>

--- sklearn/linear_model/_least_angle.py
+++ sklearn/linear_model/_least_angle.py
@@ -91,8 +91,8 @@ def lars_path(
     Parameters
     ----------
     X : None or ndarray of shape (n_samples, n_features)
-        Input data. If X is `None`, Gram must also be `None` to avoid a `ValueError`.
-        In such cases, use `lars_path_gram` instead.
+        Input data. If X is `None`, Gram must also be `None`.
+        If only the Gram matrix is available, use `lars_path_gram` instead.
 
     y : None or ndarray of shape (n_samples,)
         Input targets.
