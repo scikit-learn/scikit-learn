21504379435aa4f77b68106723a856f9e4644502
Guillaume Lemaitre <guillaume@probabl.ai>
2024-10-31 14:37:59 +0100

    [azure parallel] debug

--- build_tools/azure/install_setup_conda.sh
+++ build_tools/azure/install_setup_conda.sh
@@ -7,12 +7,13 @@ if [[ -z "${CONDA}" ]]; then
     "${SHELL}" <(curl -L micro.mamba.pm/install.sh)
 
     export CONDA=$MAMBA_ROOT_PREFIX
+    set +x
     echo "##vso[task.setvariable variable=CONDA]$CONDA"
+    set -x
 else
     sudo chown -R $USER $CONDA
 fi
 
-echo "##vso[task.prependpath]$CONDA/bin"
-
-set +e
 set +x
+echo "##vso[task.prependpath]$CONDA/bin"
+set -x
