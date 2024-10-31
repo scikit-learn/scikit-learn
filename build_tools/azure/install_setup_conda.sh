8d0ccd9f99d4c78731e2ddba9285eac3b0d9c8b7
Guillaume Lemaitre <guillaume@probabl.ai>
2024-10-31 13:33:12 +0100

    [azure parallel] simplify with micromamba

--- build_tools/azure/install_setup_conda.sh
+++ build_tools/azure/install_setup_conda.sh
@@ -4,31 +4,11 @@ set -e
 set -x
 
 if [[ -z "${CONDA}" ]]; then
-    # Determine architecture and OS
-    ARCH=$(uname -m)
-    OS=$(uname -s)
+    # By default, it uses conda-forge channels and init conda
+    "${SHELL}" <(curl -L micro.mamba.pm/install.sh)
 
-    if [[ "$ARCH" == "arm64" ]]; then
-        MINIFORGE_ARCH="arm64"
-    else
-        MINIFORGE_ARCH="x86_64"
-    fi
-
-    if [[ "$OS" == "Darwin" ]]; then
-        MINIFORGE_OS="MacOSX"
-    else
-        MINIFORGE_OS="Linux"
-    fi
-
-    # Download and install miniforge
-    MINIFORGE_URL="https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-${MINIFORGE_OS}-${MINIFORGE_ARCH}.sh"
-    wget ${MINIFORGE_URL} -O miniforge.sh
-    chmod +x miniforge.sh
-    ./miniforge.sh -b -u -p $HOME/miniforge3
-
-    # Set CONDA environment variable to point to miniforge installation
-    echo "##vso[task.setvariable variable=CONDA]$HOME/miniforge3"
-
+    # Set CONDA environment variable to point to micromamba installation
+    echo "##vso[task.setvariable variable=CONDA]$HOME/micromamba"
 fi
 
 # Add conda to PATH
