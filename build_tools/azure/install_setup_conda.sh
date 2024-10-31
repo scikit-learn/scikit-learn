fabc9e2e1bac672eefb0201724b39e71080199b6
Guillaume Lemaitre <guillaume@probabl.ai>
2024-10-31 14:46:57 +0100

    [azure parallel] debug

--- build_tools/azure/install_setup_conda.sh
+++ build_tools/azure/install_setup_conda.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
 set -e
-set -x
+set +x
 
 if [[ -z "${CONDA}" ]]; then
-    "${SHELL}" <(curl -L micro.mamba.pm/install.sh)
+    # Determine architecture and OS
+    ARCH=$(uname -m)
+    OS=$(uname -s)
 
-    export CONDA=$MAMBA_ROOT_PREFIX
-    set +x
-    echo "##vso[task.setvariable variable=CONDA]$CONDA"
-    set -x
+    if [[ "$ARCH" == "arm64" ]]; then
+        MINIFORGE_ARCH="arm64"
+    else
+        MINIFORGE_ARCH="x86_64"
+    fi
+
+    if [[ "$OS" == "Darwin" ]]; then
+        MINIFORGE_OS="MacOSX"
+    else
+        MINIFORGE_OS="Linux"
+    fi
+
+    # Download and install miniforge
+    MINIFORGE_URL="https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-${MINIFORGE_OS}-${MINIFORGE_ARCH}.sh"
+    wget ${MINIFORGE_URL} -O miniforge.sh
+    chmod +x miniforge.sh
+    ./miniforge.sh -b -u -p $HOME/miniforge3
+
+    # Set CONDA environment variable to point to miniforge installation
+    echo "##vso[task.setvariable variable=CONDA]$HOME/miniforge3"
 else
     sudo chown -R $USER $CONDA
 fi
 
-set +x
 echo "##vso[task.prependpath]$CONDA/bin"
-set -x
