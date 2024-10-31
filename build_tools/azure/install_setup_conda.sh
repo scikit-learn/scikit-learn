26859ce746624465f3c178f406d13a570b8b1607
Loïc Estève <loic.esteve@ymail.com>
2024-10-31 17:50:33 +0100

    tweak

--- build_tools/azure/install_setup_conda.sh
+++ build_tools/azure/install_setup_conda.sh
@@ -1,40 +1,23 @@
 #!/bin/bash
 
 set -e
-# make sure to not use `set -x` together with `##vso` because it will append
-# single quotes to the output
-set +x
+set -x
 
 if [[ -z "${CONDA}" ]]; then
-    # Determine architecture and OS
-    ARCH=$(uname -m)
-    OS=$(uname -s)
-
-    if [[ "$ARCH" == "arm64" ]]; then
-        MINIFORGE_ARCH="arm64"
-    else
-        MINIFORGE_ARCH="x86_64"
-    fi
-
-    if [[ "$OS" == "Darwin" ]]; then
-        MINIFORGE_OS="MacOSX"
-    else
-        MINIFORGE_OS="Linux"
-    fi
-
-    # Download and install miniforge
-    MINIFORGE_URL="https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-${MINIFORGE_OS}-${MINIFORGE_ARCH}.sh"
+    # In some runners (macOS-13 and macOS-14 in October 2024) conda is not
+    # installed so we install it ourselves
+    MINIFORGE_URL="https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
     wget ${MINIFORGE_URL} -O miniforge.sh
-    chmod +x miniforge.sh
-    ./miniforge.sh -b -u -p $HOME/miniforge3
-
-    # Set CONDA environment variable to point to miniforge installation
-    # We need to export because `##vso` does not create the variable in the
-    # current shell
-    export CONDA="$HOME/miniforge3"
-    echo "##vso[task.setvariable variable=CONDA]$CONDA"
+    bash miniforge.sh -b -u -p $HOME/miniforge3
+    CONDA="$HOME/miniforge3"
 else
+    # In most runners (in October 2024) conda is installed,
+    # but in a system folder and we want it user writable
     sudo chown -R $USER $CONDA
 fi
 
+# Need set +x for ##vso Azure magic otherwise it may add a quote in the PATH.
+# For more details, see https://github.com/microsoft/azure-pipelines-tasks/issues/10331
+set +x
 echo "##vso[task.prependpath]$CONDA/bin"
+set -x
