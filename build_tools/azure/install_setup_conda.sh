5f2593cd1ca5b53294c1a3469206e76a3c91d541
Guillaume Lemaitre <guillaume@probabl.ai>
2024-10-31 11:51:16 +0100

    [azure parallel] clean up

--- build_tools/azure/install_setup_conda.sh
+++ build_tools/azure/install_setup_conda.sh
@@ -3,27 +3,36 @@
 set -e
 set -x
 
-# Determine architecture and OS
-ARCH=$(uname -m)
-OS=$(uname -s)
+if [[ -z "${CONDA}" ]]; then
+    # Determine architecture and OS
+    ARCH=$(uname -m)
+    OS=$(uname -s)
 
-if [[ "$ARCH" == "arm64" ]]; then
-    MINIFORGE_ARCH="arm64"
-else
-    MINIFORGE_ARCH="x86_64"
+    if [[ "$ARCH" == "arm64" ]]; then
+        MINIFORGE_ARCH="arm64"
+    else
+        MINIFORGE_ARCH="x86_64"
+    fi
+
+    if [[ "$OS" == "Darwin" ]]; then
+        MINIFORGE_OS="MacOSX"
+    else
+        MINIFORGE_OS="Linux"
+    fi
+
+    # Download and install miniforge
+    MINIFORGE_URL="https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-${MINIFORGE_OS}-${MINIFORGE_ARCH}.sh"
+    wget ${MINIFORGE_URL} -O miniforge.sh
+    chmod +x miniforge.sh
+    ./miniforge.sh -b -u -p $HOME/miniforge3
+
+    # Set CONDA environment variable to point to miniforge installation
+    export CONDA=$HOME/miniforge3
+
 fi
 
-if [[ "$OS" == "Darwin" ]]; then
-    MINIFORGE_OS="MacOSX"
-else
-    MINIFORGE_OS="Linux"
-fi
+# Add conda to PATH
+export PATH=$CONDA/bin:$PATH
 
-# Download and install miniforge
-MINIFORGE_URL="https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-${MINIFORGE_OS}-${MINIFORGE_ARCH}.sh"
-wget ${MINIFORGE_URL} -O miniforge.sh
-chmod +x miniforge.sh
-./miniforge.sh -b -u -p $HOME/miniforge3
-
-# Set CONDA environment variable
-echo "##vso[task.setvariable variable=CONDA]$HOME/miniforge3"
+# Take ownership of conda installation
+sudo chown -R $USER $CONDA
