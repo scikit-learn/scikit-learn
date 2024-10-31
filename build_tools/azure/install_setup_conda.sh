0d57cda0616648424d063c1ae70ad1ba49c278c1
Guillaume Lemaitre <guillaume@probabl.ai>
2024-10-31 14:13:34 +0100

    [azure parallel] debug

--- build_tools/azure/install_setup_conda.sh
+++ build_tools/azure/install_setup_conda.sh
@@ -4,16 +4,13 @@ set -e
 set -x
 
 if [[ -z "${CONDA}" ]]; then
-    # By default, it uses conda-forge channels and init conda
     "${SHELL}" <(curl -L micro.mamba.pm/install.sh)
 
-    # Set CONDA environment variable to point to micromamba installation
     export CONDA="$HOME/micromamba"
     echo "##vso[task.setvariable variable=CONDA]$CONDA"
+    micromamba shell init
 else
-    # Take ownership of conda installation if CONDA is already set
     sudo chown -R $USER $CONDA
 fi
 
-# Add conda to PATH
 echo "##vso[task.prependpath]$CONDA/bin"
