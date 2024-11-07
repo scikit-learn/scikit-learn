48b24ce6ec31022297ba70dbe00a9ea8561431be
Omar Salman <omar.salman2007@gmail.com>
2024-11-07 11:30:56 +0500

    Use _convert_to_numpy is _tolist

--- sklearn/utils/_array_api.py
+++ sklearn/utils/_array_api.py
@@ -1095,4 +1095,5 @@ def _tolist(array, xp=None):
     xp, _ = get_namespace(array, xp=xp)
     if _is_numpy_namespace(xp):
         return array.tolist()
-    return [element.item() for element in array]
+    array_np = _convert_to_numpy(array, xp=xp)
+    return [element.item() for element in array_np]
