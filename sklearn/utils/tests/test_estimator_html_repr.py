67bd522b6dbf691dfa7b7c68996c27b4ff9d93d8
Guillaume Lemaitre <guillaume@probabl.ai>
2024-11-08 10:34:40 +0100

    linter

--- sklearn/utils/tests/test_estimator_html_repr.py
+++ sklearn/utils/tests/test_estimator_html_repr.py
@@ -142,7 +142,7 @@ def test_get_visual_block_column_transformer():
 
 def test_estimator_html_repr_an_empty_pipeline():
     """Check that the representation of an empty Pipeline does not fail.
-    
+
     Non-regression test for:
     https://github.com/scikit-learn/scikit-learn/issues/30197
     """
