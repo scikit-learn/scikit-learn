d1b7a623f7b262549158a3dad1f09a7abfba2d8d
Nathan Tessadri <nathan.tessadri@insa-rouen.fr>
2024-11-14 17:07:47 +0100

    feat(#26530): add parameter to preserve dimensions

--- sklearn/compose/_target.py
+++ sklearn/compose/_target.py
@@ -151,12 +151,14 @@ class TransformedTargetRegressor(RegressorMixin, BaseEstimator):
         func=None,
         inverse_func=None,
         check_inverse=True,
+        preserve_y_dim=False,
     ):
         self.regressor = regressor
         self.transformer = transformer
         self.func = func
         self.inverse_func = inverse_func
         self.check_inverse = check_inverse
+        self.preserve_y_dim = preserve_y_dim
 
     def _fit_transformer(self, y):
         """Check transformer and fit transformer.
@@ -263,7 +265,6 @@ class TransformedTargetRegressor(RegressorMixin, BaseEstimator):
             dtype="numeric",
             allow_nd=True,
         )
-
         # store the number of dimension of the target to predict an array of
         # similar shape at predict
         self._training_dim = y.ndim
@@ -278,10 +279,9 @@ class TransformedTargetRegressor(RegressorMixin, BaseEstimator):
 
         # transform y and convert back to 1d array if needed
         y_trans = self.transformer_.transform(y_2d)
-        # FIXME: a FunctionTransformer can return a 1D array even when validate
-        # is set to True. Therefore, we need to check the number of dimension
-        # first.
-        if y_trans.ndim == 2 and y_trans.shape[1] == 1:
+
+        # only squeeze to 1D if y was initially 1D and preserve_y_dim is False
+        if y_trans.ndim == 2 and y_trans.shape[1] == 1 and not self.preserve_y_dim:
             y_trans = y_trans.squeeze(axis=1)
 
         self.regressor_ = self._get_regressor(get_clone=True)
