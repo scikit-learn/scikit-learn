FIX gaussian mixture diagonal covariance must result from float64 operands
Squaring float32 numbers leads to a significant loss of precision the regularization term reg_covar might not mitigate.

Moreover, as avgX_means is equal to avg_means2, the returned expression can be simplified

sklearn/mixture/_gaussian_mixture.py
@@ -226,10 +226,11 @@ def _estimate_gaussian_covariances_diag(resp, X, nk, means, reg_covar):
     covariances : array, shape (n_components, n_features)
         The covariance vector of the current components.
     """
-    avg_X2 = np.dot(resp.T, X * X) / nk[:, np.newaxis]
+    X2 = X.astype(np.float64)
+    X2 **= 2
+    avg_X2 = np.dot(resp.T, X2) / nk[:, np.newaxis]
     avg_means2 = means**2
-    avg_X_means = means * np.dot(resp.T, X) / nk[:, np.newaxis]
-    return avg_X2 - 2 * avg_X_means + avg_means2 + reg_covar
+    return avg_X2 - avg_means2 + reg_covar
 
 
 def _estimate_gaussian_covariances_spherical(resp, X, nk, means, reg_covar):