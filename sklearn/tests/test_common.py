95f44d75d29671f97540c51d4939f90299f2b01b
sean moiselle <91853278+Sean-Jay-M@users.noreply.github.com>
2024-12-05 22:53:32 +0000

    ruff formatting

--- sklearn/tests/test_common.py
+++ sklearn/tests/test_common.py
@@ -328,9 +328,7 @@ def test_check_param_validation(estimator):
         pytest.skip("FeatureUnion is not tested here")
     name = estimator.__class__.__name__
     if "check_param_validation" in _get_expected_failed_checks(estimator):
-        pytest.skip(
-            "Estimator does not support check_param_validation"
-        )
+        pytest.skip("Estimator does not support check_param_validation")
     check_param_validation(name, estimator)
 
 
