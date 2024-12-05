324e9f1570565f175ec3a405c6aced7469a67a91
mekleo <36504477+mekleo@users.noreply.github.com>
2024-12-05 09:37:15 +0100

    Align input array precision with the other operands in diagonal gaussian covariance calculation

--- sklearn/mixture/_gaussian_mixture.py
+++ sklearn/mixture/_gaussian_mixture.py
@@ -226,11 +226,17 @@ def _estimate_gaussian_covariances_diag(resp, X, nk, means, reg_covar):
     covariances : array, shape (n_components, n_features)
         The covariance vector of the current components.
     """
-    X2 = X.astype(float)
+    # Align X to the other operands' floating-point precision
+    # as squaring X immediately would increase its precision error and the
+    # mismatch with operands of different precision
+    X2 = np.zeros(X.shape) + X
+    
+    # In-place squaring of X2
     X2 **= 2
+    
     avg_X2 = np.dot(resp.T, X2) / nk[:, np.newaxis]
     avg_means2 = means**2
-    return np.clip(avg_X2 - avg_means2, 0, None) + reg_covar
+    return avg_X2 - avg_means2 + reg_covar
 
 
 def _estimate_gaussian_covariances_spherical(resp, X, nk, means, reg_covar):
