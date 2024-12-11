6177f9f8a0b0f676a5cbc36a3721eb7d53f7f75c
Ahmed Thahir <ahmedthahir2002@gmail.com>
2024-12-11 22:52:53 +0400

    Merge

--- sklearn/linear_model/_coordinate_descent.py
+++ sklearn/linear_model/_coordinate_descent.py
@@ -1689,6 +1689,7 @@ class LinearModelCV(MultiOutputMixin, LinearModel, ABC):
             l1_ratios = [
                 1,
             ]
+        path_params.pop("refit", None)
         path_params.pop("cv", None)
         path_params.pop("n_jobs", None)
 
@@ -1815,10 +1816,10 @@ class LinearModelCV(MultiOutputMixin, LinearModel, ABC):
         else:
             self.alphas_ = np.asarray(alphas[0])
 
-        # Refit the model with the parameters selected
         if not self.refit:
             return self
 
+        # Refit the model with the parameters selected
         common_params = {
             name: value
             for name, value in self.get_params().items()
@@ -1919,6 +1920,10 @@ class LassoCV(RegressorMixin, LinearModelCV):
 
     copy_X : bool, default=True
         If ``True``, X will be copied; else, it may be overwritten.
+
+    refit : bool, default=True
+        If ``True``, refit an estimator using the best found parameters
+        on the whole dataset.
 
     cv : int, cross-validation generator or iterable, default=None
         Determines the cross-validation splitting strategy.
@@ -2181,6 +2186,10 @@ class ElasticNetCV(RegressorMixin, LinearModelCV):
         smaller than ``tol``, the optimization code checks the
         dual gap for optimality and continues until it is smaller
         than ``tol``.
+
+    refit : bool, default=True
+        If ``True``, refit an estimator using the best found parameters
+        on the whole dataset.
 
     cv : int, cross-validation generator or iterable, default=None
         Determines the cross-validation splitting strategy.
@@ -2872,6 +2881,10 @@ class MultiTaskElasticNetCV(RegressorMixin, LinearModelCV):
         dual gap for optimality and continues until it is smaller
         than ``tol``.
 
+    refit : bool, default=True
+        If ``True``, refit an estimator using the best found parameters
+        on the whole dataset.
+
     cv : int, cross-validation generator or iterable, default=None
         Determines the cross-validation splitting strategy.
         Possible inputs for cv are:
@@ -3120,6 +3133,10 @@ class MultiTaskLassoCV(RegressorMixin, LinearModelCV):
 
     copy_X : bool, default=True
         If ``True``, X will be copied; else, it may be overwritten.
+
+    refit : bool, default=True
+        If ``True``, refit an estimator using the best found parameters
+        on the whole dataset.
 
     cv : int, cross-validation generator or iterable, default=None
         Determines the cross-validation splitting strategy.
