1270e8f73622de673c68f7e9ad8b39118b69d08c
Loïc Estève <loic.esteve@ymail.com>
2024-12-17 09:59:04 +0100

    debug [azure parallel]

--- build_tools/azure/test_docs.sh
+++ build_tools/azure/test_docs.sh
@@ -4,10 +4,15 @@ set -ex
 
 source build_tools/shared.sh
 activate_environment
+show_installed_libraries
+
+conda info -e
+which python
+which pytest
 
 scipy_doctest_installed=$(python -c 'import scipy_doctest' && echo "True" || echo "False")
 if [[ "$scipy_doctest_installed" == "True" ]]; then
     # With scipy-doctests --doctest-modules only run doctests (in contrary to what happens with vanilla pytest)
-    python -m pytest --doctest-modules --pyargs sklearn
-    python -m pytest $(find doc -name '*.rst' | sort)
+    pytest --doctest-modules --pyargs sklearn
+    pytest $(find doc -name '*.rst' | sort)
 fi
