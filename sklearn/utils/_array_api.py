beeb3b47c1eec8c0d09cc6ebc5188cf28455ff5c
Loïc Estève <loic.esteve@ymail.com>
2024-12-18 07:05:01 +0100

    FIX Fix device detection when array API dispatch is disabled (#30454)
    Co-authored-by: Olivier Grisel <olivier.grisel@ensta.org>
    Co-authored-by: Omar Salman <omar.salman2007@gmail.com>

--- sklearn/utils/_array_api.py
+++ sklearn/utils/_array_api.py
@@ -130,10 +130,17 @@ def _check_array_api_dispatch(array_api_dispatch):
 
 def _single_array_device(array):
     """Hardware device where the array data resides on."""
-    if isinstance(array, (numpy.ndarray, numpy.generic)) or not hasattr(
-        array, "device"
+    if (
+        isinstance(array, (numpy.ndarray, numpy.generic))
+        or not hasattr(array, "device")
+        # When array API dispatch is disabled, we expect the scikit-learn code
+        # to use np.asarray so that the resulting NumPy array will implicitly use the
+        # CPU. In this case, scikit-learn should stay as device neutral as possible,
+        # hence the use of `device=None` which is accepted by all libraries, before
+        # and after the expected conversion to NumPy via np.asarray.
+        or not get_config()["array_api_dispatch"]
     ):
-        return "cpu"
+        return None
     else:
         return array.device
 
