5e9bb4b79a25e969b9f682bfd0a0a2272e0f93cf
Olivier Grisel <olivier.grisel@ensta.org>
2024-12-27 18:18:40 +0100

    DOC add comment about quantile_method='warn' in tests

--- sklearn/preprocessing/tests/test_discretization.py
+++ sklearn/preprocessing/tests/test_discretization.py
@@ -23,13 +23,13 @@ X = [[-2, 1.5, -4, -1], [-1, 2.5, -3, -0.5], [0, 3.5, -2, 0.5], [1, 4.5, -1, 2]]
     [
         (
             "uniform",
-            "warn",
+            "warn",  # default, will not warn when strategy != "quantile"
             [[0, 0, 0, 0], [1, 1, 1, 0], [2, 2, 2, 1], [2, 2, 2, 2]],
             None,
         ),
         (
             "kmeans",
-            "warn",
+            "warn",  # default, will not warn when strategy != "quantile"
             [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 1], [2, 2, 2, 2]],
             None,
         ),
@@ -41,13 +41,13 @@ X = [[-2, 1.5, -4, -1], [-1, 2.5, -3, -0.5], [0, 3.5, -2, 0.5], [1, 4.5, -1, 2]]
         ),
         (
             "uniform",
-            "warn",
+            "warn",  # default, will not warn when strategy != "quantile"
             [[0, 0, 0, 0], [1, 1, 1, 0], [2, 2, 2, 1], [2, 2, 2, 2]],
             [1, 1, 2, 1],
         ),
         (
             "uniform",
-            "warn",
+            "warn",  # default, will not warn when strategy != "quantile"
             [[0, 0, 0, 0], [1, 1, 1, 0], [2, 2, 2, 1], [2, 2, 2, 2]],
             [1, 1, 1, 1],
         ),
@@ -71,13 +71,13 @@ X = [[-2, 1.5, -4, -1], [-1, 2.5, -3, -0.5], [0, 3.5, -2, 0.5], [1, 4.5, -1, 2]]
         ),
         (
             "kmeans",
-            "warn",
+            "warn",  # default, will not warn when strategy != "quantile"
             [[0, 0, 0, 0], [1, 1, 1, 0], [1, 1, 1, 1], [2, 2, 2, 2]],
             [1, 0, 3, 1],
         ),
         (
             "kmeans",
-            "warn",
+            "warn",  # default, will not warn when strategy != "quantile"
             [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 1], [2, 2, 2, 2]],
             [1, 1, 1, 1],
         ),
@@ -146,13 +146,13 @@ def test_invalid_n_bins_array():
     [
         (
             "uniform",
-            "warn",
+            "warn",  # default, will not warn when strategy != "quantile"
             [[0, 0, 0, 0], [0, 1, 1, 0], [1, 2, 2, 1], [1, 2, 2, 2]],
             None,
         ),
         (
             "kmeans",
-            "warn",
+            "warn",  # default, will not warn when strategy != "quantile"
             [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 1], [1, 2, 2, 2]],
             None,
         ),
@@ -188,7 +188,7 @@ def test_invalid_n_bins_array():
         ),
         (
             "kmeans",
-            "warn",
+            "warn",  # default, will not warn when strategy != "quantile"
             [[0, 0, 0, 0], [0, 1, 1, 0], [1, 1, 1, 1], [1, 2, 2, 2]],
             [1, 0, 3, 1],
         ),
@@ -378,7 +378,7 @@ def test_nonuniform_strategies(
                 [0.5, 4.0, -1.5, 0.5],
                 [0.5, 4.0, -1.5, 1.5],
             ],
-            "warn",
+            "warn",  # default, will not warn when strategy != "quantile"
         ),
         (
             "kmeans",
@@ -388,7 +388,7 @@ def test_nonuniform_strategies(
                 [-0.125, 3.375, -2.125, 0.5625],
                 [0.75, 4.25, -1.25, 1.625],
             ],
-            "warn",
+            "warn",  # default, will not warn when strategy != "quantile"
         ),
         (
             "quantile",
