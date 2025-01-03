9ea6acefe4151e12bd7c586d8926c81e5ec73fd9
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2025-01-03 10:41:57 +0100

    Update sklearn/utils/_tags.py
    Co-authored-by: Adrin Jalali <adrin.jalali@gmail.com>

--- sklearn/utils/_tags.py
+++ sklearn/utils/_tags.py
@@ -404,7 +404,7 @@ def get_tags(estimator) -> Tags:
                 # method in the base class.
                 warnings.warn(
                     f"The following error was raised: {str(exc)}. It seems that "
-                    "some there is no classes that implement `__sklearn_tags__` "
+                    "there are no classes that implement `__sklearn_tags__` "
                     "in the MRO and/or all classes in the MRO call "
                     "`super().__sklearn_tags__()`. Make sure to inherit from "
                     "`BaseEstimator` that implements `__sklearn_tags__`. The default "
