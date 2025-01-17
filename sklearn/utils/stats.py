5f68c2f87c740f8146502e3230d19142df416122
Stefanie Senger <91849487+StefanieSenger@users.noreply.github.com>
2025-01-17 10:16:39 +0100

    Apply suggestions from code review
    Co-authored-by: Christian Lorentzen <lorentzen.ch@gmail.com>

--- sklearn/utils/stats.py
+++ sklearn/utils/stats.py
@@ -55,10 +55,10 @@ def _weighted_percentile(array, sample_weight, percentile_rank=50):
     sorted_idx = np.argsort(array, axis=0)
     sorted_weights = np.take_along_axis(sample_weight, sorted_idx, axis=0)
 
-    # Set `sample_weight` for NaN input to 0, if present. We do this under an if
-    # condition to avoid temporary allocations of size `(n_samples, n_features)` when
-    # there are no NaN values in the input data array. If NaN values are present, they
-    # would sort to the end (which we can observe from `sorted_idx`).
+    # Set NaN values in `sample_weight` to 0. We only perform this operation if NaN values
+    # are present at all to avoid temporary allocations of size `(n_samples, n_features)`. 
+    # If NaN values were present, they would sort to the end (which we can observe from
+    # `sorted_idx`).
     n_features = array.shape[1]
     largest_value_per_column = array[sorted_idx[-1, ...], np.arange(n_features)]
     if np.isnan(largest_value_per_column).any():
@@ -66,12 +66,12 @@ def _weighted_percentile(array, sample_weight, percentile_rank=50):
         sorted_weights[sorted_nan_mask] = 0
 
     # Compute the weighted cumulative distribution function (CDF) based on
-    # `sample_weight` and scale `percentile_rank` along it:
+    # sample_weight and scale percentile_rank along it:
     weight_cdf = stable_cumsum(sorted_weights, axis=0)
     adjusted_percentile_rank = percentile_rank / 100 * weight_cdf[-1]
 
-    # For percentile_rank=0, ignore leading observations with sample_weight=0; see PR
-    # #20528:
+    # For percentile_rank=0, ignore leading observations with sample_weight=0; see
+    # PR #20528:
     mask = adjusted_percentile_rank == 0
     adjusted_percentile_rank[mask] = np.nextafter(
         adjusted_percentile_rank[mask], adjusted_percentile_rank[mask] + 1
