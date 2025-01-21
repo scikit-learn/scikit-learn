56c8cade0ea4e692d8b006355f2a7065fd703a33
Thomas J. Fan <thomasjpfan@gmail.com>
2025-01-20 22:30:10 -0500

    Silently allow lists

--- sklearn/ensemble/_base.py
+++ sklearn/ensemble/_base.py
@@ -212,7 +212,7 @@ class _BaseHeterogeneousEnsemble(
 
     def _validate_estimators(self):
         if len(self.estimators) == 0 or not all(
-            isinstance(item, tuple) and isinstance(item[0], str)
+            isinstance(item, (tuple, list)) and isinstance(item[0], str)
             for item in self.estimators
         ):
             raise ValueError(
