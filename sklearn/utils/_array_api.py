e7fa3fd8b039fe5b4d1197c6dd7cf93c6d7801df
Jerome Dockes <jerome@dockes.org>
2025-01-30 14:02:58 +0100

    do not check for sparse matrices in get_namespace

--- sklearn/utils/_array_api.py
+++ sklearn/utils/_array_api.py
@@ -13,7 +13,6 @@ import numpy
 import scipy
 import scipy.sparse as sp
 import scipy.special as special
-from scipy import sparse
 
 from .._config import get_config
 from .fixes import parse_version
@@ -568,7 +567,7 @@ def get_namespace(*arrays, remove_none=True, remove_types=(str,), xp=None):
         remove_types=remove_types,
     )
 
-    if not arrays or any(map(sparse.issparse, arrays)):
+    if not arrays:
         return _NUMPY_API_WRAPPER_INSTANCE, False
 
     _check_array_api_dispatch(array_api_dispatch)
