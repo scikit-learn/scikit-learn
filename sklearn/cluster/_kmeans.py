2e89d72603708972dc4794fed4ff0f0cd9a9ccec
snath-xoc <shrutinath@yahoo.com>
2025-02-08 13:23:16 +0300

    add n_effective_samples

--- sklearn/cluster/_kmeans.py
+++ sklearn/cluster/_kmeans.py
@@ -2145,6 +2145,7 @@ class MiniBatchKMeans(_BaseKMeans):
 
         n_steps = int(self.max_iter * sample_weight.sum()) // self._batch_size
         normalized_sample_weight = sample_weight / np.sum(sample_weight)
+        n_effective_samples = np.sum(sample_weight)
         unit_sample_weight = np.ones_like(sample_weight, shape=(self._batch_size,))
         with _get_threadpool_controller().limit(limits=1, user_api="blas"):
             # Perform the iterative optimization until convergence
@@ -2186,7 +2187,7 @@ class MiniBatchKMeans(_BaseKMeans):
 
                 # Monitor convergence and do early stopping if necessary
                 if self._mini_batch_convergence(
-                    i, n_steps, n_samples, centers_squared_diff, batch_inertia
+                    i, n_steps, n_effective_samples, centers_squared_diff, batch_inertia
                 ):
                     break
 
@@ -2194,7 +2195,7 @@ class MiniBatchKMeans(_BaseKMeans):
         self._n_features_out = self.cluster_centers_.shape[0]
 
         self.n_steps_ = i + 1
-        self.n_iter_ = int(np.ceil(((i + 1) * self._batch_size) / n_samples))
+        self.n_iter_ = int(np.ceil(((i + 1) * self._batch_size) / n_effective_samples))
 
         if self.compute_labels:
             self.labels_, self.inertia_ = _labels_inertia_threadpool_limit(
@@ -2204,7 +2205,11 @@ class MiniBatchKMeans(_BaseKMeans):
                 n_threads=self._n_threads,
             )
         else:
-            self.inertia_ = self._ewa_inertia * n_samples
+            # In essence since the mini batch step is considering a
+            # weighted subselection of the data we need to pass on
+            # effective sampled into all computations within, feel
+            # free to correct me however if wrong.
+            self.inertia_ = self._ewa_inertia * n_effective_samples
 
         return self
 
