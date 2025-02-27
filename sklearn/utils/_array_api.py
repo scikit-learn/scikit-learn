3216ebcfa9da1c275cb7e6ff3cf8d204f652c7b9
Tim Head <betatim@gmail.com>
2025-02-27 13:08:30 +0100

    Add xp= keyword argument to get_namespace_and_device
    Using this in functions that support the xp short circuiting, so I think
    it makes sense to make this function look similar to get_namespace

--- sklearn/utils/_array_api.py
+++ sklearn/utils/_array_api.py
@@ -600,7 +600,9 @@ def get_namespace(*arrays, remove_none=True, remove_types=(str,), xp=None):
     return namespace, is_array_api_compliant
 
 
-def get_namespace_and_device(*array_list, remove_none=True, remove_types=(str,)):
+def get_namespace_and_device(
+    *array_list, remove_none=True, remove_types=(str,), xp=None
+):
     """Combination into one single function of `get_namespace` and `device`.
 
     Parameters
@@ -611,6 +613,10 @@ def get_namespace_and_device(*array_list, remove_none=True, remove_types=(str,))
         Whether to ignore None objects passed in arrays.
     remove_types : tuple or list, default=(str,)
         Types to ignore in the arrays.
+    xp : module, default=None
+        Precomputed array namespace module. When passed, typically from a caller
+        that has already performed inspection of its own inputs, skips array
+        namespace inspection.
 
     Returns
     -------
@@ -623,16 +629,22 @@ def get_namespace_and_device(*array_list, remove_none=True, remove_types=(str,))
     device : device
         `device` object (see the "Device Support" section of the array API spec).
     """
-    array_list = _remove_non_arrays(
-        *array_list,
-        remove_none=remove_none,
-        remove_types=remove_types,
-    )
+    if xp is None:
+        array_list = _remove_non_arrays(
+            *array_list,
+            remove_none=remove_none,
+            remove_types=remove_types,
+        )
 
-    skip_remove_kwargs = dict(remove_none=False, remove_types=[])
+        skip_remove_kwargs = dict(remove_none=False, remove_types=[])
 
-    xp, is_array_api = get_namespace(*array_list, **skip_remove_kwargs)
+        xp, is_array_api = get_namespace(*array_list, **skip_remove_kwargs)
+    else:
+        xp = xp
+        is_array_api = True
+
     arrays_device = device(*array_list, **skip_remove_kwargs)
+
     if is_array_api:
         return xp, is_array_api, arrays_device
     else:
