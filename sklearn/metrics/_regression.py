4076de890ac27b4d7d4601f75e21a415bbbbd67e
Olivier Grisel <olivier.grisel@ensta.org>
2025-03-06 13:30:04 +0100

    Revert "Remove xp.asarray(..., device=device_) idioms in _mean_tweedie_deviance"
    This reverts commit 920932faacae6a96789a73c6b824d15d31969c07.

--- sklearn/metrics/_regression.py
+++ sklearn/metrics/_regression.py
@@ -1353,16 +1353,17 @@ def _mean_tweedie_deviance(y_true, y_pred, sample_weight, power):
     """Mean Tweedie deviance regression loss."""
     xp, _, device_ = get_namespace_and_device(y_true, y_pred)
     p = power
+    zero = xp.asarray(0, dtype=y_true.dtype, device=device_)
     if p < 0:
         # 'Extreme stable', y any real number, y_pred > 0
         dev = 2 * (
             xp.pow(
-                xp.where(y_true > 0, y_true, 0),
-                (2 - p),
+                xp.where(y_true > 0, y_true, zero),
+                xp.asarray(2 - p, device=device_),
             )
             / ((1 - p) * (2 - p))
-            - y_true * xp.pow(y_pred, 1 - p) / (1 - p)
-            + xp.pow(y_pred, 2 - p) / (2 - p)
+            - y_true * xp.pow(y_pred, xp.asarray(1 - p, device=device_)) / (1 - p)
+            + xp.pow(y_pred, xp.asarray(2 - p, device=device_)) / (2 - p)
         )
     elif p == 0:
         # Normal distribution, y and y_pred any real number
@@ -1375,9 +1376,9 @@ def _mean_tweedie_deviance(y_true, y_pred, sample_weight, power):
         dev = 2 * (xp.log(y_pred / y_true) + y_true / y_pred - 1)
     else:
         dev = 2 * (
-            xp.pow(y_true, 2 - p) / ((1 - p) * (2 - p))
-            - y_true * xp.pow(y_pred, 1 - p) / (1 - p)
-            + xp.pow(y_pred, 2 - p) / (2 - p)
+            xp.pow(y_true, xp.asarray(2 - p, device=device_)) / ((1 - p) * (2 - p))
+            - y_true * xp.pow(y_pred, xp.asarray(1 - p, device=device_)) / (1 - p)
+            + xp.pow(y_pred, xp.asarray(2 - p, device=device_)) / (2 - p)
         )
     return float(_average(dev, weights=sample_weight))
 
