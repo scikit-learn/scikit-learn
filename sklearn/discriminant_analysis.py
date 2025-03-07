368a200ca2c08021f49c1126e5431042c2b5238f
Olivier Grisel <olivier.grisel@ensta.org>
2025-03-07 10:47:54 +0100

    TST enable non-CPU device testing via array-api-strict (#30090)
    Co-authored-by: Tim Head <betatim@gmail.com>

--- sklearn/discriminant_analysis.py
+++ sklearn/discriminant_analysis.py
@@ -596,7 +596,7 @@ class LinearDiscriminantAnalysis(
         std = xp.std(Xc, axis=0)
         # avoid division by zero in normalization
         std[std == 0] = 1.0
-        fac = xp.asarray(1.0 / (n_samples - n_classes), dtype=X.dtype)
+        fac = xp.asarray(1.0 / (n_samples - n_classes), dtype=X.dtype, device=device(X))
 
         # 2) Within variance scaling
         X = xp.sqrt(fac) * (Xc / std)
