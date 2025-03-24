72ad237c3a03f7f0ec88548d28a3c91fec2b10fa
Lucy Liu <jliu176@gmail.com>
2025-03-24 13:44:43 +1100

    fix specifying xfail

--- sklearn/utils/tests/test_stats.py
+++ sklearn/utils/tests/test_stats.py
@@ -175,14 +175,17 @@ def test_weighted_percentile_array_api_consistency(
 ):
     """Check `_weighted_percentile` gives consistent results with array API."""
     if array_namespace == "array_api_strict":
-        import array_api_strict
-
-        if device == array_api_strict.Device("device1"):
-            # See https://github.com/data-apis/array-api-strict/issues/134
-            pytest.xfail(
-                "array_api_strict bug with indexing with tuple of arrays "
-                "on non-'CPU_DEVICE' devices."
-            )
+        try:
+            import array_api_strict
+        except ImportError:
+            pass
+        else:
+            if device == array_api_strict.Device("device1"):
+                # See https://github.com/data-apis/array-api-strict/issues/134
+                pytest.xfail(
+                    "array_api_strict bug with indexing with tuple of arrays "
+                    "on non-'CPU_DEVICE' devices."
+                )
 
     xp = _array_api_for_tests(array_namespace, device)
 
