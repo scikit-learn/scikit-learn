87d1e1948f9c610e632a5bb0b0620606ede0e5b8
Dimitri Papadopoulos Orfanos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2025-03-24 09:54:35 +0100

    MNT Update ruff to 0.11.1 (#30976)

--- build_tools/get_comment.py
+++ build_tools/get_comment.py
@@ -116,7 +116,7 @@ def get_message(log_file, repo, pr_number, sha, run_id, details, versions):
         title="`ruff`",
         message=(
             "`ruff` detected issues. Please run "
-            "`ruff check --fix --output-format=full .` locally, fix the remaining "
+            "`ruff check --fix --output-format=full` locally, fix the remaining "
             "issues, and push the changes. Here you can see the detected issues. Note "
             f"that the installed `ruff` version is `ruff={versions['ruff']}`."
         ),
