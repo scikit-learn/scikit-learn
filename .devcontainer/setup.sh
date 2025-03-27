4b4390a6d9d403a417018a55b2cc58d2152294fd
Loïc Estève <loic.esteve@ymail.com>
2025-03-27 06:21:31 +0100

    debug

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,11 +6,10 @@ set -e
 
 conda init --all
 micromamba shell init -s bash
-micromamba env create -f build_tools/circle/doc_environment.yml --yes
+
+# micromamba env create -f build_tools/circle/doc_environment.yml --yes
 # Note that `micromamba activate sklearn-dev` doesn't work, it must be run by the
 # user (same applies to `conda activate`)
-
-git submodule update --init
 
 # Enables users to activate environment without having to specify the full path
 echo "envs_dirs:
