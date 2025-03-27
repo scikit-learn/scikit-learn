ce121e09f7c7c55058319cbd9b4502a863556da7
Dmitry Kobak <dmitry.kobak@uni-tuebingen.de>
2025-03-27 12:46:57 +0100

    Update examples/manifold/plot_mds.py
    Co-authored-by: antoinebaker <antoinebaker@users.noreply.github.com>

--- examples/manifold/plot_mds.py
+++ examples/manifold/plot_mds.py
@@ -24,8 +24,8 @@ from sklearn.metrics import euclidean_distances
 # Generate the data
 EPSILON = np.finfo(np.float32).eps
 n_samples = 20
-seed = np.random.RandomState(seed=3)
-X_true = seed.randint(0, 20, 2 * n_samples).astype(float)
+rng = np.random.RandomState(seed=3)
+X_true = rng.randint(0, 20, 2 * n_samples).astype(float)
 X_true = X_true.reshape((n_samples, 2))
 
 # Center the data
