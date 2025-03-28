beebdf153717ad7196838152b0f0c6f7e6c1f48e
Loïc Estève <loic.esteve@ymail.com>
2025-03-28 05:42:51 +0100

    tweak

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -8,10 +8,8 @@ conda init --all
 micromamba shell init -s bash
 
 micromamba env create -f build_tools/circle/doc_environment.yml -n sklearn-dev --yes
+# Auto-activate sklearn-dev in terminal
 echo "micromamba activate sklearn-dev" >> $HOME/.bashrc
-
-# Note that `micromamba activate sklearn-dev` doesn't work, it must be run by the
-# user (same applies to `conda activate`)
 
 # Enables users to activate environment without having to specify the full path
 echo "envs_dirs:
