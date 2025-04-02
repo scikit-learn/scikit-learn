95c99bb8aad53afa2a15400649e758e5a6f1e566
Dmitry Kobak <dmitry.kobak@uni-tuebingen.de>
2025-04-02 10:12:36 +0200

    Fix docstring params

--- sklearn/manifold/_mds.py
+++ sklearn/manifold/_mds.py
@@ -572,7 +572,7 @@ class MDS(BaseEstimator):
     >>> X, _ = load_digits(return_X_y=True)
     >>> X.shape
     (1797, 64)
-    >>> embedding = MDS(n_components=2)
+    >>> embedding = MDS(n_components=2, n_init=1, eps=1e-6)
     >>> X_transformed = embedding.fit_transform(X[:100])
     >>> X_transformed.shape
     (100, 2)
