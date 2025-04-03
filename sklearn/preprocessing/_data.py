841ed260c1fa48d84affbbd21418dc700ae7f914
Karassay <karassay.raushanbek@gmail.com>
2025-04-03 12:53:38 +0500

    add sample_weight parameter in fit function

--- sklearn/preprocessing/_data.py
+++ sklearn/preprocessing/_data.py
@@ -2802,7 +2802,7 @@ class QuantileTransformer(OneToOneFeatureMixin, TransformerMixin, BaseEstimator)
         self.quantiles_ = np.maximum.accumulate(self.quantiles_)
 
     @_fit_context(prefer_skip_nested_validation=True)
-    def fit(self, X, y=None):
+    def fit(self, X, y=None, sample_weight=None):
         """Compute the quantiles used for transforming.
 
         Parameters
