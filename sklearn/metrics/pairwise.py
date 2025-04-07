ea99f317c5db9e143ddfe2c4e4164ed256e7c4be
Lucy Liu <jliu176@gmail.com>
2025-04-07 14:34:44 +1000

    transpose

--- sklearn/metrics/pairwise.py
+++ sklearn/metrics/pairwise.py
@@ -1982,6 +1982,11 @@ def _parallel_pairwise(X, Y, func, n_jobs, **kwds):
 
     # enforce a threading backend to prevent data communication overhead
     fd = delayed(_dist_wrapper)
+    # Transpose `ret` such that a given thread writes its ouput to a contiguous chunk.
+    # Note `order` (i.e. F/C-contiguous) is not included in array API standard, see
+    # https://github.com/data-apis/array-api/issues/571 for details.
+    # We assume that currently (April 2025) all array API compatible namespaces
+    # allocate # 2D arrays using the C-contiguity convention by default.
     ret = xp.empty((X.shape[0], Y.shape[0]), dtype=dtype_float).T
     Parallel(backend="threading", n_jobs=n_jobs)(
         fd(func, ret, s, X, Y[s], transpose=True, **kwds)
@@ -1989,7 +1994,7 @@ def _parallel_pairwise(X, Y, func, n_jobs, **kwds):
     )
 
     if (X is Y or Y is None) and func is euclidean_distances:
-        # zeroing diagonal for euclidean norm.
+        # zeroing diagonal for square matrix and euclidean norm.
         # TODO: do it also for other norms.
         _fill_diagonal(ret, 0, xp=xp)
 
