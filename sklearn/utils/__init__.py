bb4ecc46213b22dd42f7493a223f1a2d9d5330b3
Dan Schult <dschult@colgate.edu>
2025-04-10 20:32:25 -0400

    set up config sparse_interface and function _as_sparse() with tests and 3 example modules

--- sklearn/utils/__init__.py
+++ sklearn/utils/__init__.py
@@ -20,6 +20,7 @@ from ._indexing import (
     shuffle,
 )
 from ._mask import safe_mask
+from ._sparse import _as_sparse, _select_interface_if_sparse
 from ._tags import (
     ClassifierTags,
     InputTags,
@@ -56,6 +57,8 @@ __all__ = [
     "Tags",
     "TargetTags",
     "TransformerTags",
+    "_as_sparse",
+    "_select_interface_if_sparse",
     "all_estimators",
     "as_float_array",
     "assert_all_finite",
