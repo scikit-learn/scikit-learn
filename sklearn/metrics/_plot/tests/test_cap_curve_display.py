3d0e722abc3152b0abcdc30619eefce760e81e86
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2025-04-12 09:38:35 +0200

    fix missing definition in test

--- sklearn/metrics/_plot/tests/test_cap_curve_display.py
+++ sklearn/metrics/_plot/tests/test_cap_curve_display.py
@@ -85,8 +85,8 @@ def test_cumulative_accuracy_display_from_predictions(
 
     y_true_cumulative = cap_display.y_true_cumulative
     cumulative_total = cap_display.cumulative_total
-    assert y_true_cumulative.dtype == y_pred.dtype
-    assert cumulative_total.dtype == y_pred.dtype
+    assert y_true_cumulative.dtype == y_scores.dtype
+    assert cumulative_total.dtype == y_scores.dtype
 
 
 @pytest.mark.parametrize(
