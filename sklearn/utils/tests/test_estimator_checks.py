86d099ec1b5c157adf841b9c51560e3c65546f11
Christian Veenhuis <124370897+ChVeen@users.noreply.github.com>
2025-04-18 00:27:51 +0200

    MNT remove unused local var in `sklearn.utils.estimator_checks.py` (#31221)

--- sklearn/utils/tests/test_estimator_checks.py
+++ sklearn/utils/tests/test_estimator_checks.py
@@ -1373,8 +1373,8 @@ def test_check_estimator_callback():
     expected_failed_checks = _get_expected_failed_checks(est)
     # This is to make sure we test a class that has some expected failures
     assert len(expected_failed_checks) > 0
-    with warnings.catch_warnings(record=True) as records:
-        logs = check_estimator(
+    with warnings.catch_warnings(record=True):
+        check_estimator(
             est,
             expected_failed_checks=expected_failed_checks,
             on_fail=None,
