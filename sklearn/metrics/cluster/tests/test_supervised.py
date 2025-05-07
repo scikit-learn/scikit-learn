160ea9f444518786e3de15427afa88f87fa2c556
Lucy Liu <jliu176@gmail.com>
2025-05-07 21:35:36 +1000

    fix import

--- sklearn/metrics/cluster/tests/test_supervised.py
+++ sklearn/metrics/cluster/tests/test_supervised.py
@@ -6,7 +6,6 @@ from numpy.testing import assert_allclose, assert_array_almost_equal, assert_arr
 
 from sklearn.base import config_context
 from sklearn.metrics.cluster import (
-    _entropy,
     adjusted_mutual_info_score,
     adjusted_rand_score,
     completeness_score,
@@ -21,7 +20,11 @@ from sklearn.metrics.cluster import (
     rand_score,
     v_measure_score,
 )
-from sklearn.metrics.cluster._supervised import _generalized_average, check_clusterings
+from sklearn.metrics.cluster._supervised import (
+    _entropy,
+    _generalized_average,
+    check_clusterings,
+)
 from sklearn.utils import assert_all_finite
 from sklearn.utils._array_api import (
     _get_namespace_device_dtype_ids,
