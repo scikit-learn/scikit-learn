caa6fbc5298a4a3d9b9fcb2f332050178d65d082
Jérémie du Boisberranger <jeremie@probabl.ai>
2025-06-02 12:53:11 +0200

    wip

---
+++ examples/release_highlights/plot_release_highlights_1_7_0.py
@@ -0,0 +1,46 @@
+# ruff: noqa: CPY001, E501
+"""
+=======================================
+Release Highlights for scikit-learn 1.7
+=======================================
+
+.. currentmodule:: sklearn
+
+We are pleased to announce the release of scikit-learn 1.7! Many bug fixes
+and improvements were added, as well as some key new features. Below we
+detail the highlights of this release. **For an exhaustive list of
+all the changes**, please refer to the :ref:`release notes <release_notes_1_7>`.
+
+To install the latest version (with pip)::
+
+    pip install --upgrade scikit-learn
+
+or with conda::
+
+    conda install -c conda-forge scikit-learn
+
+"""
+
+# %%
+# parameters added to HTML repr
+# -------------------------------------------
+
+# %%
+# X_val in HistGradientBoosting
+# --------------------------------------
+
+# %%
+# from_cv_results in RocCurveDisplay
+# --------------------------------------------
+
+# %%
+# Progress on array-api support
+# --------------------------------------------
+
+# %%
+# sample_weight support + new losses in MLP ?
+# -------------------------------------------
+
+# %%
+# Support for sparse arrays ?
+# -------------------------------------------
