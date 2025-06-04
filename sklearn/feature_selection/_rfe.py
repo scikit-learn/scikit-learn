a684ef720f889bcb353d1b8cb13b5afe5c8971f5
VirenPassi <virenpassi79@gmail.com>
2025-06-04 16:08:52 +0530

    Fix docstring formatting for RFECV: add safe example ref

--- sklearn/feature_selection/_rfe.py
+++ sklearn/feature_selection/_rfe.py
@@ -563,10 +563,12 @@ class RFECV(RFE):
     The performance of each :class:`RFE` selector is evaluated using `scoring` for
     different numbers of selected features and aggregated together. Finally, the scores
     are averaged across folds and the number of features selected is set to the number
-    of features that maximize the cross-validation score,  as illustrated in the
-    following example:
-    :ref:`example using RFE with cross-validation
-    <sphx_glr_auto_examples_feature_selection_plot_rfe_with_cross_validation.py>`.
+    of features that maximize the cross-validation score.
+
+    For a detailed example of using RFECV to select features when training a
+    :class:`~sklearn.linear_model.LogisticRegression`, see
+    :ref:`sphx_glr_auto_examples_feature_selection_plot_rfe_with_cross_validation.py`.
+
 
     See glossary entry for :term:`cross-validation estimator`.
 
