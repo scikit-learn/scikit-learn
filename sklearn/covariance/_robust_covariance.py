594ec9611a6a32e5390de68f0d4440ec9ac85286
Natalia Mokeeva <91160475+natmokval@users.noreply.github.com>
2025-06-16 21:14:19 +0200

    Update sklearn/covariance/_robust_covariance.py
    Co-authored-by: Stefanie Senger <91849487+StefanieSenger@users.noreply.github.com>

--- sklearn/covariance/_robust_covariance.py
+++ sklearn/covariance/_robust_covariance.py
@@ -635,7 +635,7 @@ class MinCovDet(EmpiricalCovariance):
 
         For an example of comparing raw robust estimates with
         the true location and covariance, refer to
-        :ref:`sphx_glr_auto_examples_covariance_plot_robust_vs_empirical_covariance.py`
+        :ref:`sphx_glr_auto_examples_covariance_plot_robust_vs_empirical_covariance.py`.
 
     covariance_ : ndarray of shape (n_features, n_features)
         Estimated robust covariance matrix.
