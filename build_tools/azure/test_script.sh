405c7b5129aba22203c11d12b70ec4f46c41e6ed
Guillaume Lemaitre <guillaume@probabl.ai>
2025-06-24 08:54:09 +0200

    [azure parallel] debug

--- build_tools/azure/test_script.sh
+++ build_tools/azure/test_script.sh
@@ -39,9 +39,7 @@ python -c "import sklearn; sklearn.show_versions()"
 
 show_installed_libraries
 
-export OMP_NUM_THREADS=1
-
-TEST_CMD="python -m pytest -vsl --maxfail=20 --durations=20 --junitxml=$JUNITXML -o junit_family=legacy"
+TEST_CMD="python -m pytest -vsl --maxfail=2 --durations=20 --junitxml=$JUNITXML -o junit_family=legacy"
 
 if [[ "$COVERAGE" == "true" ]]; then
     # Note: --cov-report= is used to disable to long text output report in the
