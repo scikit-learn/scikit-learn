f14671158a72618969509114dc8bc8f1b413863d
NishanthPrakash <nishanth.prakash@oracle.com>
2025-06-28 14:02:40 -0700

    format file

--- sklearn/tests/test_hash_requires_fit.py
+++ sklearn/tests/test_hash_requires_fit.py
@@ -1,10 +1,13 @@
+import pytest
 from sklearn.feature_extraction import FeatureHasher
 from sklearn.feature_extraction.text import HashingVectorizer
 
+def test_feature_hasher_requires_fit_tag():
+    """Test that FeatureHasher has requires_fit=False tag."""
+    hasher = FeatureHasher()
+    assert hasher._more_tags()['requires_fit'] is False
 
-def test_feature_hasher_requires_fit():
-    assert not FeatureHasher()._get_tags()["requires_fit"]
-
-
-def test_hashing_vectorizer_requires_fit():
-    assert not HashingVectorizer()._get_tags()["requires_fit"]
+def test_hashing_vectorizer_requires_fit_tag():
+    """Test that HashingVectorizer has requires_fit=False tag."""
+    vectorizer = HashingVectorizer()
+    assert vectorizer._more_tags()['requires_fit'] is False
