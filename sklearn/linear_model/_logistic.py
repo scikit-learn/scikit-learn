a9d3605338fa49c3ae7176d3d52833ef05975ef0
SHOURYAHANDA <shouryahanda06@gmail.com>
2025-06-29 22:38:20 +0530

    Fix: remove trailing whitespace and clean up See Also section

--- sklearn/linear_model/_logistic.py
+++ sklearn/linear_model/_logistic.py
@@ -1776,7 +1776,7 @@ class LogisticRegressionCV(LogisticRegression, LinearClassifierMixin, BaseEstima
         Names of features seen during :term:`fit`. Defined only when `X`
         has feature names that are all strings.
 
-        .. versionadded:: 1.0
+     .. versionadded:: 1.0
     See Also
     --------
     LogisticRegression : Logistic regression without tuning the
