e26bf58e837bc01f7b81de3f1957f86f8b350f76
Michael Šimáček <el.simacek@gmail.com>
2025-07-03 16:18:59 +0200

    Use append instead of resize in svmlight format
    Co-authored-by: Jérémie du Boisberranger <jeremie@probabl.ai>

--- sklearn/datasets/_svmlight_format_fast.pyx
+++ sklearn/datasets/_svmlight_format_fast.pyx
@@ -78,10 +78,7 @@ def _load_svmlight_file(f, dtype, bint multilabel, bint zero_based,
         if n_features and features[0].startswith(qid_prefix):
             _, value = features[0].split(COLON, 1)
             if query_id:
-                # Disable refcheck because it doesn't work on all Python
-                # implementations. We know we are the sole owner at this point
-                query.resize(len(query) + 1, refcheck=False)
-                query[len(query) - 1] = np.int64(value)
+                query = np.append(query, np.int64(value))
             features.pop(0)
             n_features -= 1
 
