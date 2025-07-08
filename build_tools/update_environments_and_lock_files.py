e97113477e475afa83c61f066818f68e1863373b
Stefanie Senger <91849487+StefanieSenger@users.noreply.github.com>
2025-07-08 15:03:13 +0200

    CI Move some pip_dependencies to conda_dependencies (#31623)

--- build_tools/update_environments_and_lock_files.py
+++ build_tools/update_environments_and_lock_files.py
@@ -157,10 +157,7 @@ build_metadata_list = [
         "folder": "build_tools/azure",
         "platform": "osx-64",
         "channels": ["defaults"],
-        "conda_dependencies": remove_from(
-            common_dependencies, ["cython", "threadpoolctl", "meson-python"]
-        )
-        + ["ccache"],
+        "conda_dependencies": common_dependencies + ["ccache"],
         "package_constraints": {
             "blas": "[build=mkl]",
             # scipy 1.12.x crashes on this platform (https://github.com/scipy/scipy/pull/20086)
@@ -168,9 +165,6 @@ build_metadata_list = [
             # channel.
             "scipy": "<1.12",
         },
-        # TODO: put cython, threadpoolctl and meson-python back to conda
-        # dependencies when required version is available on the main channel
-        "pip_dependencies": ["cython", "threadpoolctl", "meson-python", "meson"],
     },
     {
         "name": "pymin_conda_forge_openblas_min_dependencies",
@@ -326,13 +320,13 @@ build_metadata_list = [
             "plotly",
             "polars",
             "pooch",
+            "sphinxext-opengraph",
             "sphinx-remove-toctrees",
             "sphinx-design",
             "pydata-sphinx-theme",
             "towncrier",
         ],
         "pip_dependencies": [
-            "sphinxext-opengraph",
             "sphinxcontrib-sass",
         ],
         "package_constraints": {
@@ -386,10 +380,10 @@ build_metadata_list = [
             "sphinx-design",
             "pydata-sphinx-theme",
             "towncrier",
-        ],
-        "pip_dependencies": [
             "jupyterlite-sphinx",
             "jupyterlite-pyodide-kernel",
+        ],
+        "pip_dependencies": [
             "sphinxcontrib-sass",
         ],
         "package_constraints": {
