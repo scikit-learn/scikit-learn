25c0eaf0d0a78f15f371ad614643eb3e4c620289
Loïc Estève <loic.esteve@ymail.com>
2025-07-16 15:27:41 +0200

    attempt to use Windows installer in non-interactive mode [azure parallel]

--- build_tools/azure/install_setup_conda.sh
+++ build_tools/azure/install_setup_conda.sh
@@ -12,19 +12,25 @@ if [[ "$PLATFORM" == "Windows" ]]; then
 else
     EXTENSION="sh"
 fi
+INSTALLER="miniforge.$EXTENSION"
 MINIFORGE_URL="https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$PLATFORM-$(uname -m).$EXTENSION"
-curl -L ${MINIFORGE_URL} -o miniforge
-chmod +x miniforge
-./miniforge -b -u -p $HOME/miniforge3
-CONDA="$HOME/miniforge3"
+curl -L ${MINIFORGE_URL} -o "$INSTALLER"
+
+MINICONDA_DIR="$HOME/miniforge3"
+if [[ "$PLATFORM" != "Windows" ]]; then
+    bash "$INSTALLER" -b -u -p $MINICONDA_DIR
+else
+    WIN_MINICONDA_DIR=$(cygpath -w "$MINICONDA_DIR")
+    cmd "/C $INSTALLER /InstallationType=JustMe /RegisterPython=0 /S /D=$WIN_MINICONDA_DIR"
+fi
 
 # Add conda to the PATH so that it can be used in further Azure CI steps.
 # Need set +x for ##vso Azure magic otherwise it may add a quote in the PATH.
 # For more details, see https://github.com/microsoft/azure-pipelines-tasks/issues/10331
 set +x
 if [[ "$PLATFORM" == "Windows" ]]; then
-   echo "##vso[task.prependpath]$CONDA/Scripts"
+   echo "##vso[task.prependpath]$MINICONDA_DIR/Scripts"
 else
-   echo "##vso[task.prependpath]$CONDA/bin"
+   echo "##vso[task.prependpath]$MINICONDA_DIR/bin"
 fi
 set -x
