76f6861bc870dbebb80397aab619e477d8272fcd
Loïc Estève <loic.esteve@ymail.com>
2025-07-16 14:25:08 +0200

    fix curl argument [azure parallel]

--- build_tools/azure/install_setup_conda.sh
+++ build_tools/azure/install_setup_conda.sh
@@ -8,7 +8,7 @@ if [[ "$PLATFORM" =~ MINGW|MSYS ]]; then
     PLATFORM=Windows
 fi
 MINIFORGE_URL="https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$PLATFORM-$(uname -m).sh"
-curl ${MINIFORGE_URL} -o miniforge.sh
+curl -L ${MINIFORGE_URL} -o miniforge.sh
 bash miniforge.sh -b -u -p $HOME/miniforge3
 CONDA="$HOME/miniforge3"
 
