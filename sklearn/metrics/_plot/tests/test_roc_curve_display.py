68d74ba9af2458a3302280bcbe613966c4d3fce6
Jérémie du Boisberranger <jeremie@probabl.ai>
2025-07-22 15:05:40 +0200

    fix last one

--- sklearn/metrics/_plot/tests/test_roc_curve_display.py
+++ sklearn/metrics/_plot/tests/test_roc_curve_display.py
@@ -924,7 +924,7 @@ def test_plot_roc_curve_pos_label(pyplot, response_method, constructor_name):
 
 
 # TODO(1.9): remove
-def test_y_score_and_y_pred_specified_error():
+def test_y_score_and_y_pred_specified_error(pyplot):
     """1. Check that an error is raised when both y_score and y_pred are specified.
     2. Check that a warning is raised when y_pred is specified.
     """
