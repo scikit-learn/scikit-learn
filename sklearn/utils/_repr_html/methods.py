ccf16dc0ecbd4cdc7054faa69713a4ad1e6659f1
Dea María Léon <deamarialeon@gmail.com>
2025-07-31 14:22:21 +0200

    wip css

--- sklearn/utils/_repr_html/methods.py
+++ sklearn/utils/_repr_html/methods.py
@@ -85,7 +85,7 @@ def _methods_html_repr(methods):
     """
 
     HTML_TEMPLATE = """
-       <div class="estimator-table">
+       <div class="method-table">
            <details>
                <summary>Methods</summary>
                <table class="methods-table">
@@ -98,7 +98,8 @@ def _methods_html_repr(methods):
     """
     ROW_TEMPLATE = """
        <tr class="methods">
-           <td >{name}&nbsp; {signature}</td>
+           <td class="methods-name">{name}&nbsp; </td>
+           <td class="methods-signature">{signature}</td>
            <td><a class="sk-estimator-doc-link
                                  {doc_link}
                                 </a>
