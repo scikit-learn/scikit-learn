ebb528b93107ee5911c5c6a3647bb5b4461d5109
Olivier Grisel <olivier.grisel@ensta.org>
2025-08-07 09:59:34 +0200

    Workaround probably already identified thread-safety bug

--- sklearn/linear_model/tests/test_logistic.py
+++ sklearn/linear_model/tests/test_logistic.py
@@ -438,6 +438,9 @@ def test_logistic_regression_path_convergence_fail():
     assert "linear_model.html#logistic-regression" in warn_msg
 
 
+# XXX: investigate thread-safety bug that might be related to:
+# https://github.com/scikit-learn/scikit-learn/issues/31883
+@pytest.mark.thread_unsafe
 def test_liblinear_dual_random_state(global_random_seed):
     # random_state is relevant for liblinear solver only if dual=True
     X, y = make_classification(n_samples=20, random_state=global_random_seed)
