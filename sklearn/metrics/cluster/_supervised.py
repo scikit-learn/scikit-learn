2bbfeb4c73337b0f0f98da0a2399072004c0c5eb
y.korobko <y.korobko@tbank.ru>
2025-08-07 19:32:01 +0300

    remove xp.asarray

--- sklearn/metrics/cluster/_supervised.py
+++ sklearn/metrics/cluster/_supervised.py
@@ -161,9 +161,8 @@ def contingency_matrix(
 
     xp, _, device = get_namespace_and_device(labels_pred, labels_true)
 
-    # todo: try remove xp.asarray
-    classes, class_idx = xp.unique_inverse(xp.asarray(labels_true))
-    clusters, cluster_idx = xp.unique_inverse(xp.asarray(labels_pred))
+    classes, class_idx = xp.unique_inverse(labels_true)
+    clusters, cluster_idx = xp.unique_inverse(labels_pred)
     class_idx = _convert_to_numpy(class_idx, xp)
     cluster_idx = _convert_to_numpy(cluster_idx, xp)
     n_classes = classes.shape[0]
