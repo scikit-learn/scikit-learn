1fa5d99548dcaf803f223ef97f029695707cae4e
Dea María Léon <deamarialeon@gmail.com>
2025-08-08 09:55:22 +0200

    wip

--- sklearn/utils/_repr_html/params.py
+++ sklearn/utils/_repr_html/params.py
@@ -9,7 +9,6 @@ from collections import UserDict
 from functools import lru_cache
 from urllib.parse import quote
 
-from sklearn.externals._numpydoc import docscrape
 from sklearn.utils._repr_html.base import ReprHTMLMixin
 
 
@@ -53,7 +52,8 @@ def _read_params(name, value, non_default_params):
 
 @lru_cache
 def _get_estimator_docstring(estimator_class_docs):
-    return docscrape.NumpyDocString(estimator_class_docs)
+    # return docscrape.NumpyDocString(estimator_class_docs)
+    return
 
 
 def _params_html_repr(params):
@@ -98,10 +98,11 @@ def _params_html_repr(params):
     docstring = None
     if estimator_class_docs:
         docstring = _get_estimator_docstring(estimator_class_docs)
-        param_map = {
-            param_docstring.name: param_docstring
-            for param_docstring in docstring["Parameters"]
-        }
+        if docstring:
+            param_map = {
+                param_docstring.name: param_docstring
+                for param_docstring in docstring["Parameters"]
+            }
 
     rows = []
     for row in params:
