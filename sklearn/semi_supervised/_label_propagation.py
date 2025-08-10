fix normalization of affinity_matrix in _build_graph

sklearn/semi_supervised/_label_propagation.py
@@ -453,18 +453,26 @@ class LabelPropagation(BaseLabelPropagation):
         )
 
     def _build_graph(self):
-        """Matrix representing a fully connected graph between each sample
-
-        This basic implementation creates a non-stochastic affinity matrix, so
-        class distributions will exceed 1 (normalization may be desired).
-        """
+        """Matrix representing a fully connected graph between each sample."""
         if self.kernel == "knn":
             self.nn_fit = None
         affinity_matrix = self._get_kernel(self.X_)
-        normalizer = affinity_matrix.sum(axis=0)
+        normalizer = affinity_matrix.sum(axis=1)
         if sparse.issparse(affinity_matrix):
-            affinity_matrix.data /= np.diag(np.array(normalizer))
-        else:
+            # handle spmatrix (make 1D)
+            if sparse.isspmatrix(affinity_matrix):
+                normalizer = np.ravel(normalizer)
+            # common case: knn method gives row sum k for all rows
+            if np.all(normalizer == normalizer[0]):
+                affinity_matrix.data /= normalizer[0]
+            else:  # row sums not the same
+                if affinity_matrix.format == "csr":
+                    repeats = np.diff(affinity_matrix.indptr)
+                    rows = np.repeat(np.arange(affinity_matrix.shape[0]), repeats)
+                else:  # CSC format
+                    rows = affinity_matrix.indices
+                affinity_matrix.data /= normalizer[rows]
+        else:  # Dense affinity_matrix
             affinity_matrix /= normalizer[:, np.newaxis]
         return affinity_matrix
 