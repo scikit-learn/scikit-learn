a5c41cc1bd7bd969f7fc6756298fc529212cced9
Guillaume Lemaitre <guillaume@probabl.ai>
2025-08-19 16:17:05 +0200

    FIX align properly ? symbol in HTML diagram

--- sklearn/utils/_repr_html/estimator.py
+++ sklearn/utils/_repr_html/estimator.py
@@ -190,16 +190,22 @@ def _write_label_html(
             if name_caption is None
             else f'<div class="caption">{html.escape(name_caption)}</div>'
         )
-        name_caption_div = f"<div><div>{name}</div>{name_caption_div}</div>"
-        links_div = (
-            f"<div>{doc_link}{is_fitted_icon}</div>"
-            if doc_link or is_fitted_icon
-            else ""
-        )
+
+        # Create the name with icons using unbreakable spaces for proper alignment
+        icons_html = ""
+        if doc_link or is_fitted_icon:
+            icons_html = (
+                f"&nbsp;{doc_link}{is_fitted_icon}"
+                if doc_link or is_fitted_icon
+                else ""
+            )
+
+        name_with_icons = f'<div style="white-space: nowrap;">{name}{icons_html}</div>'
+        name_caption_div = f"<div>{name_with_icons}{name_caption_div}</div>"
 
         label_html = (
             f'<label for="{est_id}" class="sk-toggleable__label {is_fitted_css_class} '
-            f'sk-toggleable__label-arrow">{name_caption_div}{links_div}</label>'
+            f'sk-toggleable__label-arrow">{name_caption_div}</label>'
         )
 
         fmt_str = (
