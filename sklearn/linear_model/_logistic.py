a525ca9bc39595c4d3cfa10dcab37d9d33a10a8d
Rishi Prasad <rishiprasad@Rishis-MacBook-Air.local>
2025-08-20 14:07:28 +0530

    Fix LogisticRegression docstring formatting by removing indentation from line 1126

--- sklearn/linear_model/_logistic.py
+++ sklearn/linear_model/_logistic.py
@@ -1123,7 +1123,7 @@ class LogisticRegression(LinearClassifierMixin, SparseCoefMixin, BaseEstimator):
     >>> clf.score(X, y)
     0.97
 
-     For a comparison of the LogisticRegression with other classifiers see:
+    For a comparison of the LogisticRegression with other classifiers see:
     :ref:`sphx_glr_auto_examples_classification_plot_classification_probability.py`.
     You can find a visual example here:
     :ref:`sphx_glr_auto_examples_linear_model_plot_logistic_path.py`.
