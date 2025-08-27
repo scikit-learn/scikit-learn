28d33a57d2c6866c709f00ef8f8745634eb66d98
ArturoAmorQ <arturo.amor-quiroz@polytechnique.edu>
2025-08-27 18:18:35 +0200

    Notation tweaks

--- examples/semi_supervised/plot_semi_supervised_versus_svm_iris.py
+++ examples/semi_supervised/plot_semi_supervised_versus_svm_iris.py
@@ -38,17 +38,17 @@ y_10[y_rand > 0.1] = -1  # set random samples to be unlabeled
 y_30 = np.copy(y)
 y_30[y_rand > 0.3] = -1
 
-ls30 = (LabelSpreading().fit(X, y_10), y_10, "LabelSpreading with 10% labeled data")
-ls50 = (LabelSpreading().fit(X, y_30), y_30, "LabelSpreading with 30% labeled data")
+ls10 = (LabelSpreading().fit(X, y_10), y_10, "LabelSpreading with 10% labeled data")
+ls30 = (LabelSpreading().fit(X, y_30), y_30, "LabelSpreading with 30% labeled data")
 ls100 = (LabelSpreading().fit(X, y), y, "LabelSpreading with 100% labeled data")
 
-base_classifier = SVC(gamma=0.5, probability=True, random_state=0)
-st30 = (
+base_classifier = SVC(gamma=0.5, probability=True, random_state=42)
+st10 = (
     SelfTrainingClassifier(base_classifier).fit(X, y_10),
     y_10,
     "Self-training with 10% labeled data",
 )
-st50 = (
+st30 = (
     SelfTrainingClassifier(base_classifier).fit(X, y_30),
     y_30,
     "Self-training with 30% labeled data",
@@ -58,7 +58,7 @@ rbf_svc = (base_classifier.fit(X, y), y, "SVC with rbf kernel (100% labeled data
 tab10 = plt.get_cmap("tab10")
 color_map = {cls: tab10(cls) for cls in np.unique(y)}
 color_map[-1] = (1, 1, 1)
-classifiers = (ls30, st30, ls50, st50, ls100, rbf_svc)
+classifiers = (ls10, st10, ls30, st30, ls100, rbf_svc)
 
 fig, axes = plt.subplots(nrows=3, ncols=2, sharex="col", sharey="row", figsize=(10, 12))
 axes = axes.ravel()
