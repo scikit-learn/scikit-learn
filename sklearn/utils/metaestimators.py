bef64ed02cb7d561eebce12265732080ef5a7de4
Sota Goto <sotaackeemgv@gmail.com>
2025-08-30 09:58:28 +0900

    DOC improve _BaseComposition comments

--- sklearn/utils/metaestimators.py
+++ sklearn/utils/metaestimators.py
@@ -56,10 +56,10 @@ class _BaseComposition(BaseEstimator, metaclass=ABCMeta):
 
     def _set_params(self, attr, **params):
         # Ensure strict ordering of parameter setting:
-        # 1. All steps
+        # 1. Replace the entire estimators collection
         if attr in params:
             setattr(self, attr, params.pop(attr))
-        # 2. Replace items with estimators in params
+        # 2. Replace individual estimators by name
         items = getattr(self, attr)
         if isinstance(items, list) and items:
             # Get item names used to identify valid names in params
@@ -71,7 +71,7 @@ class _BaseComposition(BaseEstimator, metaclass=ABCMeta):
                     if "__" not in name and name in item_names:
                         self._replace_estimator(attr, name, params.pop(name))
 
-        # 3. Step parameters and other initialisation arguments
+        # 3. Individual estimator parameters and other initialisation arguments
         super().set_params(**params)
         return self
 
