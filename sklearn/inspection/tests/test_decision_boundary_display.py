bdf5bb5f1f86db2393028502de991cba2c9af810
SrushtikShetty <shettysrushtik@gmail.com>
2025-08-31 10:34:50 +0530

    TST add test for DecisionBoundaryDisplay.plot with sample_weight (gh-27462)

--- sklearn/inspection/tests/test_decision_boundary_display.py
+++ sklearn/inspection/tests/test_decision_boundary_display.py
@@ -1,19 +1,30 @@
-import pytest
 import numpy as np
+import pytest
+
 from sklearn.datasets import make_classification
-from sklearn.linear_model import LogisticRegression
 from sklearn.inspection import DecisionBoundaryDisplay
+from sklearn.linear_model import LogisticRegression
 
 
-@pytest.mark.parametrize("response_method", ["predict", "predict_proba", "decision_function"])
+@pytest.mark.parametrize(
+    "response_method",
+    ["predict", "predict_proba", "decision_function"],
+)
 def test_decision_boundary_display_plotting_with_sample_weight(response_method):
     """Check that DecisionBoundaryDisplay.plot works with sample_weight (gh-27462)."""
-    X, y = make_classification(n_samples=50, n_features=2, n_classes=2, random_state=42)
+    X, y = make_classification(
+        n_samples=50,
+        n_features=2,
+        n_classes=2,
+        random_state=42,
+    )
     sample_weight = np.random.RandomState(0).rand(y.shape[0])
 
     clf = LogisticRegression().fit(X, y, sample_weight=sample_weight)
 
     disp = DecisionBoundaryDisplay.from_estimator(
-        clf, X, response_method=response_method
+        clf,
+        X,
+        response_method=response_method,
     )
-    disp.plot(X, y, sample_weight=sample_weight) 
+    disp.plot(X, y, sample_weight=sample_weight)
