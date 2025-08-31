92e2890182e7684235e023bdf4efb601c4343cf9
SrushtikShetty <shettysrushtik@gmail.com>
2025-08-31 10:41:41 +0530

    TST add test for DecisionBoundaryDisplay.plot with sample_weight (gh-27462)

--- sklearn/inspection/tests/test_decision_boundary_display.py
+++ sklearn/inspection/tests/test_decision_boundary_display.py
@@ -12,9 +12,7 @@ from sklearn.linear_model import LogisticRegression
 )
 def test_decision_boundary_display_plotting_with_sample_weight(response_method):
     """Check that DecisionBoundaryDisplay.plot works with sample_weight (gh-27462)."""
-    X, y = make_classification(
-        n_samples=50, n_features=2, n_classes=2, random_state=42
-    )
+    X, y = make_classification(n_samples=50, n_features=2, n_classes=2, random_state=42)
     sample_weight = np.random.RandomState(0).rand(y.shape[0])
 
     clf = LogisticRegression().fit(X, y, sample_weight=sample_weight)
