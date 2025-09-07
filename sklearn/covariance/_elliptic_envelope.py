39bea6bc5c2709a68656c41a566551a207c46ed3
dherresp-adm <dherrera1911@gmail.com>
2025-09-07 12:28:23 -0400

    Change docstring Example output to new values

--- sklearn/covariance/_elliptic_envelope.py
+++ sklearn/covariance/_elliptic_envelope.py
@@ -135,8 +135,8 @@ class EllipticEnvelope(OutlierMixin, MinCovDet):
     ...              [3, 3]])
     array([ 1, -1])
     >>> cov.covariance_
-    array([[0.7411, 0.2535],
-           [0.2535, 0.3053]])
+    array([[0.8102, 0.2736],
+           [0.2736, 0.3330]])
     >>> cov.location_
     array([0.0813 , 0.0427])
     """
--- sklearn/covariance/_robust_covariance.py
+++ sklearn/covariance/_robust_covariance.py
@@ -733,8 +733,8 @@ class MinCovDet(EmpiricalCovariance):
     ...                                   size=500)
     >>> cov = MinCovDet(random_state=0).fit(X)
     >>> cov.covariance_
-    array([[0.7411, 0.2535],
-           [0.2535, 0.3053]])
+    array([[0.8102, 0.2736],
+           [0.2736, 0.3330]])
     >>> cov.location_
     array([0.0813 , 0.0427])
     """
