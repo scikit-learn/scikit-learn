81e210d60278634d7468851be61e7e43c0789507
Pieterjan Robbe <pieterjanrobbe@gmail.com>
2025-09-09 10:04:31 -0700

    Fixed some more spelling mistakes

--- sklearn/polynomial_chaos/__init__.py
+++ sklearn/polynomial_chaos/__init__.py
@@ -5,6 +5,6 @@ The :mod:`sklearn.pce` module implements Polynomial Chaos regression
 # Authors: The scikit-learn developers
 # SPDX-License-Identifier: BSD-3-Clause
 
-from ._pcr import PolynomialChaosRegressor
+from sklearn.polynomial_chaos._pcr import PolynomialChaosRegressor
 
 __all__ = ["PolynomialChaosRegressor"]
