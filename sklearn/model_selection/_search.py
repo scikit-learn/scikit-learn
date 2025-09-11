9cd8be8d783b313a627cd9f8856a7337db9090ec
Maren Westermann <maren.westermann@gmail.com>
2025-09-11 18:19:29 +0200

    address comments

--- sklearn/model_selection/_search.py
+++ sklearn/model_selection/_search.py
@@ -1837,6 +1837,9 @@ class RandomizedSearchCV(BaseSearchCV):
             'params'             : [{'kernel' : 'rbf', 'gamma' : 0.1}, ...],
             }
 
+        For an example of comparing models statistically using cv_results_,
+        see :ref:`sphx_glr_auto_examples_model_selection_plot_grid_search_stats.py`.
+
         NOTE
 
         The key ``'params'`` is used to store a list of parameter
