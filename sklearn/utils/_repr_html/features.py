c89b3677038073b057c3aeb8c15e9bd11d4eff02
Dea María Léon <deamarialeon@gmail.com>
2025-09-16 19:58:07 +0200

    adding a list of features

---
+++ sklearn/utils/_repr_html/features.py
@@ -0,0 +1,24 @@
+# Authors: The scikit-learn developers
+# SPDX-License-Identifier: BSD-3-Clause
+
+
+def _features_html(features):
+    FEATURES_TABLE_TEMPLATE = """
+        <div class="features">
+          <details>
+            <summary>Output features </summary>
+              <ul>
+               {rows}
+              </ul>
+          </details>
+        </div>
+    """
+
+    FEATURES_ROW_TEMPLATE = """
+        <li>{feature}</li>
+    """
+
+    rows = []
+    for feature in features:
+        rows.append(FEATURES_ROW_TEMPLATE.format(feature=feature))
+    return FEATURES_TABLE_TEMPLATE.format(rows="".join(rows))
