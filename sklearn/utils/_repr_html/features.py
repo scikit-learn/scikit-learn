3d2a9ad62d7f75fb1280f7af8762710d34fbceae
Dea María Léon <deamarialeon@gmail.com>
2025-09-25 14:23:20 +0200

    class for table

--- sklearn/utils/_repr_html/features.py
+++ sklearn/utils/_repr_html/features.py
@@ -11,7 +11,7 @@ def _features_html(features):
             <summary>{total_features} features
             </summary>
             <div class="features-container">
-              <table>
+              <table = "features-table">
                 <tbody>
                   {rows}
                 </tbody>
