b21040e714ee91b994e4752d1842c0ada6bd7716
Arthur Lacote <arthur.lcte@gmail.com>
2025-09-26 22:41:29 +0200

    Apply suggestion from @cakedev0

--- sklearn/tree/tests/test_tree.py
+++ sklearn/tree/tests/test_tree.py
@@ -2922,7 +2922,7 @@ def test_absolute_errors_precomputation_function(global_random_seed):
             abs_errors_ = abs_errors_[::-1]
             medians_ = medians_[::-1]
         assert_allclose(abs_errors, abs_errors_, atol=1e-12)
-        assert_allclose(medians, medians_)
+        assert_allclose(medians, medians_, atol=1e-12)
 
     rng = np.random.default_rng(global_random_seed)
 
