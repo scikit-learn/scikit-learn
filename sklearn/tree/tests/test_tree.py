1e9c74ff9565920aa297480808614382f0f99696
Arthur Lacote <arthur.lcte@gmail.com>
2025-09-26 22:41:06 +0200

    Apply suggestion from @ogrisel
    Co-authored-by: Olivier Grisel <olivier.grisel@ensta.org>

--- sklearn/tree/tests/test_tree.py
+++ sklearn/tree/tests/test_tree.py
@@ -2921,7 +2921,7 @@ def test_absolute_errors_precomputation_function(global_random_seed):
         if reverse:
             abs_errors_ = abs_errors_[::-1]
             medians_ = medians_[::-1]
-        assert_allclose(abs_errors, abs_errors_)
+        assert_allclose(abs_errors, abs_errors_, atol=1e-12)
         assert_allclose(medians, medians_)
 
     rng = np.random.default_rng(global_random_seed)
