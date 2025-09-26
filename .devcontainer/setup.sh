173e8f9988c87c456b1c09718b2858697aa4bb52
Loïc Estève <loic.esteve@ymail.com>
2025-09-26 15:52:04 +0200

    iter

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,13 +4,13 @@ set -e
 
 "${SHELL}" <(curl -Ls micro.mamba.pm/install.sh) < /dev/null
 
-conda init --all
-micromamba shell init -s bash
+export MAMBA_EXE='/root/.local/bin/micromamba';
+$MAMBA_EXE shell init -s bash
 
 micromamba env create -f build_tools/circle/doc_environment.yml -n sklearn-dev --yes
 # Auto-activate sklearn-dev in terminal
 echo "micromamba activate sklearn-dev" >> $HOME/.bashrc
 
 # Enables users to activate environment without having to specify the full path
-echo "envs_dirs:
-  - /home/codespace/micromamba/envs" > /opt/conda/.condarc
+# echo "envs_dirs:
+#  - /home/codespace/micromamba/envs" > /opt/conda/.condarc
