d66ea4378dbbd41669eb7ba60beacc40c8bb8816
Loïc Estève <loic.esteve@ymail.com>
2025-09-26 17:12:05 +0200

    minor tweak

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,8 +4,8 @@ set -e
 
 "${SHELL}" <(curl -Ls micro.mamba.pm/install.sh) < /dev/null
 # .bashrc has been updated by the mamba install one-liner above.
-# source ~/.bashrc sets up micromamba for later use
-source ~/.bashrc
+# 'source $HOME/.bashrc' sets up micromamba for later use
+source $HOME/.bashrc
 
 micromamba env create -f build_tools/circle/doc_environment.yml -n sklearn-dev --yes
 # Auto-activate sklearn-dev in terminal
