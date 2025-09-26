9e95e1af53086c8f663a208d1384e06a1e5bac4e
Loïc Estève <loic.esteve@ymail.com>
2025-09-26 16:46:12 +0200

    try with full thing

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,8 +4,20 @@ set -e
 
 "${SHELL}" <(curl -Ls micro.mamba.pm/install.sh) < /dev/null
 
-export MAMBA_EXE='/root/.local/bin/micromamba';
-$MAMBA_EXE shell init -s bash
+# >>> mamba initialize >>>
+# !! Contents within this block are managed by 'micromamba shell init' !!
+export MAMBA_EXE='/home/vscode/.local/bin/micromamba';
+export MAMBA_ROOT_PREFIX='/home/vscode/micromamba';
+__mamba_setup="$("$MAMBA_EXE" shell hook --shell bash --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__mamba_setup"
+else
+    alias micromamba="$MAMBA_EXE"  # Fallback on help from micromamba activate
+fi
+unset __mamba_setup
+# <<< mamba initialize <<<
+
+micromamba shell init --shell bash
 
 micromamba env create -f build_tools/circle/doc_environment.yml -n sklearn-dev --yes
 # Auto-activate sklearn-dev in terminal
