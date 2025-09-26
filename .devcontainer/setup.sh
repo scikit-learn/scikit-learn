bedae79e128458f5cd043decd30456f7ef093843
Loïc Estève <loic.esteve@ymail.com>
2025-09-26 16:53:40 +0200

    simplify

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,26 +3,10 @@
 set -e
 
 "${SHELL}" <(curl -Ls micro.mamba.pm/install.sh) < /dev/null
-
-# >>> mamba initialize >>>
-# !! Contents within this block are managed by 'micromamba shell init' !!
-export MAMBA_EXE='/home/vscode/.local/bin/micromamba';
-export MAMBA_ROOT_PREFIX='/home/vscode/micromamba';
-__mamba_setup="$("$MAMBA_EXE" shell hook --shell bash --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__mamba_setup"
-else
-    alias micromamba="$MAMBA_EXE"  # Fallback on help from micromamba activate
-fi
-unset __mamba_setup
-# <<< mamba initialize <<<
-
-micromamba shell init --shell bash
+# .bashrc has been updated by the mamba install one-liner above.
+# source ~/.bashrc sets up micromamba for later use
+source ~/.bashrc
 
 micromamba env create -f build_tools/circle/doc_environment.yml -n sklearn-dev --yes
 # Auto-activate sklearn-dev in terminal
 echo "micromamba activate sklearn-dev" >> $HOME/.bashrc
-
-# Enables users to activate environment without having to specify the full path
-# echo "envs_dirs:
-#  - /home/codespace/micromamba/envs" > /opt/conda/.condarc
