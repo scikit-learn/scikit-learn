78226738c9b44efb20854d2cbbe3be913dce03ad
Arthur <arthur.lcte@gmail.com>
2025-09-28 14:52:15 +0200

    comments; docstring; cleanups

--- sklearn/utils/stats.py
+++ sklearn/utils/stats.py
@@ -7,7 +7,43 @@ from sklearn.utils._array_api import (
 )
 
 
-def _old_weighted_percentile(
+def _weighted_percentile_inner(x, w, target_sums, out, average, xp):
+    if x.size == 1:
+        out[:] = x
+        return
+    i = x.size // 2
+    partitioner = xp.argpartition(x, i)
+    w_left = w[partitioner[:i]]
+    sum_left = xp.sum(w_left)
+    j = xp.searchsorted(target_sums, sum_left)
+    target_sums[j:] -= sum_left
+    x_left = None
+    if j > 0:
+        # some quantiles are to be found on the left side of the partition
+        x_left = x[partitioner[:i]]
+        _weighted_percentile_inner(
+            x_left, w_left, target_sums[:j], out[:j], average, xp
+        )
+    if j >= target_sums.size:
+        return
+    idx_0 = xp.searchsorted(target_sums[j:], 0, side="right")
+    if idx_0 > 0:
+        # some quantiles are precisely at the index of the partition
+        x_left = x[partitioner[:i]] if x_left is None else x_left
+        out[j : j + idx_0] = (
+            (x_left.max() + x[partitioner[i:]].min()) / 2 if average else x_left.max()
+        )
+        j += idx_0
+    if j < target_sums.size:
+        # some quantiles are to be found on the right side of the partition
+        x_right = x[partitioner[i:]]
+        w_right = w[partitioner[i:]]
+        _weighted_percentile_inner(
+            x_right, w_right, target_sums[j:], out[j:], average, xp
+        )
+
+
+def _weighted_percentile(
     array, sample_weight, percentile_rank=50, average=False, xp=None
 ):
     """Compute the weighted percentile.
@@ -83,157 +119,13 @@ def _old_weighted_percentile(
         Weighted percentile at the requested probability level.
     """
     xp, _, device = get_namespace_and_device(array)
-    # `sample_weight` should follow `array` for dtypes
-    floating_dtype = _find_matching_floating_dtype(array, xp=xp)
-    array = xp.asarray(array, dtype=floating_dtype, device=device)
-    sample_weight = xp.asarray(sample_weight, dtype=floating_dtype, device=device)
-
-    n_dim = array.ndim
-    if n_dim == 0:
-        return array
-    if array.ndim == 1:
-        array = xp.reshape(array, (-1, 1))
-    # When sample_weight 1D, repeat for each array.shape[1]
-    if array.shape != sample_weight.shape and array.shape[0] == sample_weight.shape[0]:
-        sample_weight = xp.tile(sample_weight, (array.shape[1], 1)).T
-    # Sort `array` and `sample_weight` along axis=0:
-    sorted_idx = xp.argsort(array, axis=0)
-    sorted_weights = xp.take_along_axis(sample_weight, sorted_idx, axis=0)
-
-    # Set NaN values in `sample_weight` to 0. Only perform this operation if NaN
-    # values present to avoid temporary allocations of size `(n_samples, n_features)`.
-    n_features = array.shape[1]
-    largest_value_per_column = array[
-        sorted_idx[-1, ...], xp.arange(n_features, device=device)
-    ]
-    # NaN values get sorted to end (largest value)
-    if xp.any(xp.isnan(largest_value_per_column)):
-        sorted_nan_mask = xp.take_along_axis(xp.isnan(array), sorted_idx, axis=0)
-        sorted_weights[sorted_nan_mask] = 0
-
-    # Compute the weighted cumulative distribution function (CDF) based on
-    # `sample_weight` and scale `percentile_rank` along it.
-    #
-    # Note: we call `xp.cumulative_sum` on the transposed `sorted_weights` to
-    # ensure that the result is of shape `(n_features, n_samples)` so
-    # `xp.searchsorted` calls take contiguous inputs as a result (for
-    # performance reasons).
-    weight_cdf = xp.cumulative_sum(sorted_weights.T, axis=1)
-    adjusted_percentile_rank = percentile_rank / 100 * weight_cdf[..., -1]
-
-    # Ignore leading `sample_weight=0` observations when `percentile_rank=0` (#20528)
-    mask = adjusted_percentile_rank == 0
-    adjusted_percentile_rank[mask] = xp.nextafter(
-        adjusted_percentile_rank[mask], adjusted_percentile_rank[mask] + 1
-    )
-    # For each feature with index j, find sample index i of the scalar value
-    # `adjusted_percentile_rank[j]` in 1D array `weight_cdf[j]`, such that:
-    # weight_cdf[j, i-1] < adjusted_percentile_rank[j] <= weight_cdf[j, i].
-    # Note `searchsorted` defaults to equality on the right, whereas Hyndman and Fan
-    # reference equation has equality on the left.
-    percentile_indices = xp.stack(
-        [
-            xp.searchsorted(
-                weight_cdf[feature_idx, ...], adjusted_percentile_rank[feature_idx]
-            )
-            for feature_idx in range(weight_cdf.shape[0])
-        ],
-    )
-    # `percentile_indices` may be equal to `sorted_idx.shape[0]` due to floating
-    # point error (see #11813)
-    max_idx = sorted_idx.shape[0] - 1
-    percentile_indices = xp.clip(percentile_indices, 0, max_idx)
-
-    col_indices = xp.arange(array.shape[1], device=device)
-    percentile_in_sorted = sorted_idx[percentile_indices, col_indices]
-
-    if average:
-        # From Hyndman and Fan (1996), `fraction_above` is `g`
-        fraction_above = (
-            weight_cdf[col_indices, percentile_indices] - adjusted_percentile_rank
-        )
-        is_fraction_above = fraction_above > xp.finfo(floating_dtype).eps
-        percentile_plus_one_indices = xp.clip(percentile_indices + 1, 0, max_idx)
-        percentile_plus_one_in_sorted = sorted_idx[
-            percentile_plus_one_indices, col_indices
-        ]
-        # Handle case when next index ('plus one') has sample weight of 0
-        zero_weight_cols = col_indices[
-            sample_weight[percentile_plus_one_in_sorted, col_indices] == 0
-        ]
-        for col_idx in zero_weight_cols:
-            cdf_val = weight_cdf[col_idx, percentile_indices[col_idx]]
-            # Search for next index where `weighted_cdf` is greater
-            next_index = xp.searchsorted(
-                weight_cdf[col_idx, ...], cdf_val, side="right"
-            )
-            # Handle case where there are trailing 0 sample weight samples
-            # and `percentile_indices` is already max index
-            if next_index >= max_idx:
-                # use original `percentile_indices` again
-                next_index = percentile_indices[col_idx]
-
-            percentile_plus_one_in_sorted[col_idx] = sorted_idx[next_index, col_idx]
-
-        result = xp.where(
-            is_fraction_above,
-            array[percentile_in_sorted, col_indices],
-            (
-                array[percentile_in_sorted, col_indices]
-                + array[percentile_plus_one_in_sorted, col_indices]
-            )
-            / 2,
-        )
-    else:
-        result = array[percentile_in_sorted, col_indices]
-
-    return result[0] if n_dim == 1 else result
-
-
-def _weighted_percentile_inner(x, w, target_sums, out, average, xp):
-    if x.size == 1:
-        out[:] = x
-        return
-    i = x.size // 2
-    partitioner = xp.argpartition(x, x.size // 2)
-    w_left = w[partitioner[:i]]
-    sum_left = xp.sum(w_left)
-    j = xp.searchsorted(target_sums, sum_left)
-    target_sums[j:] -= sum_left
-    x_left = None
-    if j > 0:
-        x_left = x[partitioner[:i]]
-        _weighted_percentile_inner(
-            x_left, w_left, target_sums[:j], out[:j], average, xp
-        )
-    if j < target_sums.size:
-        idx_0 = xp.searchsorted(target_sums[j:], 0, side="right")
-        if idx_0 > 0:
-            x_left = x[partitioner[:i]] if x_left is None else x_left
-            out[j : j + idx_0] = (
-                (x_left.max() + x[partitioner[i:]].min()) / 2
-                if average
-                else x_left.max()
-            )
-            j += idx_0
-    if j < target_sums.size:
-        x_right = x[partitioner[i:]]
-        w_right = w[partitioner[i:]]
-        _weighted_percentile_inner(
-            x_right, w_right, target_sums[j:], out[j:], average, xp
-        )
-
-
-def _weighted_percentile(
-    array, sample_weight, percentile_rank=50, average=False, xp=None
-):
-    xp, _, device = get_namespace_and_device(array)
 
     # `sample_weight` should follow `array` for dtypes
     # XXX: ^ why? Also: why floating dtype? (is this really floating, I don't think so)
     floating_dtype = _find_matching_floating_dtype(array, xp=xp)
     array = xp.asarray(array, dtype=floating_dtype, device=device)
     sample_weight = xp.asarray(sample_weight, dtype=floating_dtype, device=device)
+    percentile_rank = xp.asarray(percentile_rank, dtype=floating_dtype, device=device)
 
     n_dim = array.ndim
     if n_dim == 0:
@@ -242,33 +134,46 @@ def _weighted_percentile(
         array = xp.reshape(array, (-1, 1))
     n_features = array.shape[1]
 
-    n_dim_percentile = 0
-    q = xp.asarray([percentile_rank / 100])
+    n_dim_percentile = percentile_rank.ndim
+    if n_dim_percentile == 0:
+        percentile_rank = xp.reshape(percentile_rank, (1,))
+    q = percentile_rank / 100
+
+    # Sort quantiles for efficient processing in __weighted_percentile_inner
     sorter = xp.argsort(q, stable=False)
     result = xp.empty((n_features, q.size), dtype=floating_dtype)
     sorted_q = q[sorter]
     result_sorted = xp.empty(q.size, dtype=floating_dtype)
 
+    # Compute weighted percentiles for each feature (column)
     for feature_idx in range(n_features):
         x = array[..., feature_idx]
+        # Ignore NaN values by masking them out
         mask_nnan = ~xp.isnan(x)
         x = x[mask_nnan]
         if x.size == 0:
+            # If all values are NaN, return NaN for this feature
             result[feature_idx, ...] = xp.nan
             continue
+        # Select weights for non-NaN values
         w = (
             sample_weight[mask_nnan, feature_idx]
             if sample_weight.ndim == 2
             else sample_weight[mask_nnan]
         )
+        # Ignore zero weights
         mask_nz = w != 0
-        if not mask_nz.all():
+        has_zero = not mask_nz.all()
+        if has_zero:
             w = w[mask_nz]
-            x[mask_nz]
         weights_sum = xp.sum(w)
         if weights_sum == 0:
+            # If all weights are zero, return max value (consistent with NaN handling)
             result[feature_idx, ...] = xp.max(x)
             continue
+        if has_zero:
+            x = x[mask_nz]
+        # Recursively compute weighted percentiles using partitioning
         _weighted_percentile_inner(
             x,
             w,
@@ -277,6 +182,7 @@ def _weighted_percentile(
             average=average,
             xp=xp,
         )
+        # Store results in original quantile order
         result[feature_idx, sorter] = result_sorted
 
     if n_dim_percentile == 0:
