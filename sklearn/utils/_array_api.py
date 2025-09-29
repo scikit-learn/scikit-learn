4e4537c864e76fdc1e4c1ac613f8485ca7902646
Virgil Chan <virchan.math@gmail.com>
2025-09-28 19:24:59 -0700

    Remove unrelated changes from other PRs.

--- sklearn/utils/_array_api.py
+++ sklearn/utils/_array_api.py
@@ -1105,14 +1105,3 @@ def _linalg_solve(cov_chol, eye_matrix, xp):
         return scipy.linalg.solve_triangular(cov_chol, eye_matrix, lower=True)
     else:
         return xp.linalg.solve(cov_chol, eye_matrix)
-
-
-def _half_multinomial_loss(y, pred, sample_weight=None, xp=None):
-    """A version of the multinomial loss that is compatible with the array API"""
-    xp, _ = get_namespace(y, pred, sample_weight)
-    device_ = device(y, pred, sample_weight)
-    log_sum_exp = special.logsumexp(pred, axis=1)
-    y = xp.asarray(y, dtype=xp.int64, device=device_)
-    class_margins = xp.asarray(xp.arange(y.shape[0], device=device_)) * pred.shape[1]
-    label_predictions = xp.take(_ravel(pred), y + class_margins)
-    return float(_average(log_sum_exp - label_predictions, weights=sample_weight))
