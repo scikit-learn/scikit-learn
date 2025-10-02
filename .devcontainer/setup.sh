f6102048dd61e131805d3ebcc2c00fb274d8879b
Guillaume Lemaitre <guillaume@probabl.ai>
2025-10-02 15:43:06 +0200

    MAINT add jupyter extension and pre-commit in devcontainer

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -8,5 +8,9 @@ set -e
 source $HOME/.bashrc
 
 micromamba env create -f build_tools/circle/doc_environment.yml -n sklearn-dev --yes
+# install ipykernel because it is handy to execute the Jupyter notebook VS Code
+micromamba install ipykernel -n sklearn-dev --yes
+# install pre-commit since it will avoid potential failures with the CI
+micromamba install pre-commit -n sklearn-dev --yes
 # Auto-activate sklearn-dev in terminal
 echo "micromamba activate sklearn-dev" >> $HOME/.bashrc
