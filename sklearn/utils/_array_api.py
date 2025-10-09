dd1576cdc88600dfaefcf1073036c84fac6a5143
Olivier Grisel <olivier.grisel@ensta.org>
2025-10-09 16:10:06 +0200

    numpy conversion for dpnp

--- sklearn/utils/_array_api.py
+++ sklearn/utils/_array_api.py
@@ -845,6 +845,8 @@ def _convert_to_numpy(array, xp):
         return array.get()
     elif _is_xp_namespace(xp, "array_api_strict"):
         return numpy.asarray(xp.asarray(array, device=xp.Device("CPU_DEVICE")))
+    elif _is_xp_namespace(xp, "dpnp"):  # pragma: nocover
+        return array.asnumpy()
 
     return numpy.asarray(array)
 
