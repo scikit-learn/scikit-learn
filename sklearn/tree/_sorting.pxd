acadfb7e5ec7d9a56a6c26e29dcc64dd636b6701
Arthur <arthur.lcte@gmail.com>
2025-10-09 11:40:50 +0200

    attempt at fixing sorting

--- sklearn/tree/_sorting.pxd
+++ sklearn/tree/_sorting.pxd
@@ -1,9 +1,7 @@
 from sklearn.utils._typedefs cimport float32_t, float64_t, intp_t, uint8_t, int32_t, uint32_t
 
 
-ctypedef fused floating_t:
-    float32_t
-    float64_t
+from cython cimport floating
 
 
-cdef inline void sort(floating_t* feature_values, intp_t* samples, intp_t n) noexcept nogil
+cdef void sort(floating* feature_values, intp_t* samples, intp_t n) noexcept nogil
--- sklearn/tree/_sorting.pyx
+++ sklearn/tree/_sorting.pyx
@@ -8,24 +8,24 @@ def _py_sort(float32_t[::1] feature_values, intp_t[::1] samples, intp_t n):
 
 # Sort n-element arrays pointed to by feature_values and samples, simultaneously,
 # by the values in feature_values. Algorithm: Introsort (Musser, SP&E, 1997).
-cdef inline void sort(floating_t* feature_values, intp_t* samples, intp_t n) noexcept nogil:
+cdef void sort(floating* feature_values, intp_t* samples, intp_t n) noexcept nogil:
     if n == 0:
         return
     cdef intp_t maxd = 2 * <intp_t>log2(n)
     introsort(feature_values, samples, n, maxd)
 
 
-cdef inline void swap(floating_t* feature_values, intp_t* samples,
+cdef inline void swap(floating* feature_values, intp_t* samples,
                       intp_t i, intp_t j) noexcept nogil:
     # Helper for sort
     feature_values[i], feature_values[j] = feature_values[j], feature_values[i]
     samples[i], samples[j] = samples[j], samples[i]
 
 
-cdef inline floating_t median3(floating_t* feature_values, intp_t n) noexcept nogil:
+cdef inline floating median3(floating* feature_values, intp_t n) noexcept nogil:
     # Median of three pivot selection, after Bentley and McIlroy (1993).
     # Engineering a sort function. SP&E. Requires 8/3 comparisons on average.
-    cdef floating_t a = feature_values[0], b = feature_values[n / 2], c = feature_values[n - 1]
+    cdef floating a = feature_values[0], b = feature_values[n / 2], c = feature_values[n - 1]
     if a < b:
         if b < c:
             return b
@@ -44,9 +44,9 @@ cdef inline floating_t median3(floating_t* feature_values, intp_t n) noexcept no
 
 # Introsort with median of 3 pivot selection and 3-way partition function
 # (robust to repeated elements, e.g. lots of zero features).
-cdef void introsort(floating_t* feature_values, intp_t *samples,
+cdef void introsort(floating* feature_values, intp_t *samples,
                     intp_t n, intp_t maxd) noexcept nogil:
-    cdef floating_t pivot
+    cdef floating pivot
     cdef intp_t i, l, r
 
     while n > 1:
@@ -77,7 +77,7 @@ cdef void introsort(floating_t* feature_values, intp_t *samples,
         n -= r
 
 
-cdef inline void sift_down(floating_t* feature_values, intp_t* samples,
+cdef inline void sift_down(floating* feature_values, intp_t* samples,
                            intp_t start, intp_t end) noexcept nogil:
     # Restore heap order in feature_values[start:end] by moving the max element to start.
     cdef intp_t child, maxind, root
@@ -100,7 +100,7 @@ cdef inline void sift_down(floating_t* feature_values, intp_t* samples,
             root = maxind
 
 
-cdef void heapsort(floating_t* feature_values, intp_t* samples, intp_t n) noexcept nogil:
+cdef void heapsort(floating* feature_values, intp_t* samples, intp_t n) noexcept nogil:
     cdef intp_t start, end
 
     # heapify
