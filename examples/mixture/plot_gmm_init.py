565346a7416307000fbe2c9aed7cbace6c5d38dd
Christine P. Chai <star1327p@gmail.com>
2025-10-17 13:32:09 -0700

    DOC: Remove blank line in plot_gmm_init.py
    Removed unnecessary blank line in the code.

--- examples/mixture/plot_gmm_init.py
+++ examples/mixture/plot_gmm_init.py
@@ -45,7 +45,6 @@ from sklearn.datasets._samples_generator import make_blobs
 from sklearn.mixture import GaussianMixture
 from sklearn.utils.extmath import row_norms
 
-
 # Generate some data
 
 X, y_true = make_blobs(n_samples=4000, centers=4, cluster_std=0.60, random_state=0)
