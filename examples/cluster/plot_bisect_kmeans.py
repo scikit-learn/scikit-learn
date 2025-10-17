f8be336e87f4527134571ffea94a3b71fef2304f
Christine P. Chai <star1327p@gmail.com>
2025-10-17 13:35:04 -0700

    DOC: Remove another extra blank

--- examples/cluster/plot_bisect_kmeans.py
+++ examples/cluster/plot_bisect_kmeans.py
@@ -22,7 +22,6 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import BisectingKMeans, KMeans
 from sklearn.datasets import make_blobs
 
-
 # Generate sample data
 n_samples = 10000
 random_state = 0
