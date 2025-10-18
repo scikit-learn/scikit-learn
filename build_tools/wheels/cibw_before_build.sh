f529c4aa432849f4a3f0d8307b940d90f688186f
Loïc Estève <loic.esteve@ymail.com>
2025-10-18 08:56:51 +0200

    [cd build] not use limited-api by default and enable it for wheels in some cases

--- build_tools/wheels/cibw_before_build.sh
+++ build_tools/wheels/cibw_before_build.sh
@@ -20,7 +20,7 @@ fi
 PYTHON_VERSION=$(python -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")')
 IS_PYTHON_FREE_THREADED=$(python -c 'import sysconfig; print(sysconfig.get_config_var("Py_GIL_DISABLED"))')
 
-# Change pyproject.toml to not use limited_api
-if [[ $PYTHON_VERSION != "3.11" || IS_PYTHON_FREE_THREADED == "1" ]]; then
-    export CIBW_CONFIG_SETTINGS='setup-args=-Dpython.allow_limited_api=false'
+# Use abi3 wheels for non vanilla Python >= 3.12
+if [[ $PYTHON_VERSION != "3.11" && IS_PYTHON_FREE_THREADED == "0" ]]; then
+    echo "CIBW_CONFIG_SETTINGS='setup-args=-Dpython.allow_limited_api=true" >> "$GITHUB_ENV"
 fi
