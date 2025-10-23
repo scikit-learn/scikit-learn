779c77b79e513471d473a6fb40a4a34ebad7a999
Thomas S <thomas@probabl.ai>
2025-10-23 10:48:33 +0200

    Fix Azure workflows

--- build_tools/azure/test_script.sh
+++ build_tools/azure/test_script.sh
@@ -83,7 +83,7 @@ if [[ -z "${SELECTED_TESTS+x}" ]]; then
 fi
 
 if [[ -n "${SELECTED_TESTS}" ]]; then
-    TEST_CMD="${TEST_CMD} -k '${SELECTED_TESTS}'"
+    TEST_CMD="${TEST_CMD} -k \"${SELECTED_TESTS}\""
 
     # Override to make selected tests run on all random seeds
     export SKLEARN_TESTS_GLOBAL_RANDOM_SEED="all"
