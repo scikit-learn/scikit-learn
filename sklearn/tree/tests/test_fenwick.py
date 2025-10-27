70277d367fab6eccf49b43088db7717fbdf5fb7f
Arthur <arthur.lcte@gmail.com>
2025-10-27 21:50:21 +0100

    adressed comments on test fenwick

--- sklearn/tree/tests/test_fenwick.py
+++ sklearn/tree/tests/test_fenwick.py
@@ -12,9 +12,9 @@ def test_cython_weighted_fenwick_tree(global_random_seed):
     n = 100
     indices = rng.permutation(n)
     y = rng.normal(size=n)
-    w = rng.integers(1, 4, size=n)
-    y_sorted = np.zeros_like(y)
-    w_sorted = np.zeros_like(w)
+    w = rng.integers(0, 4, size=n)
+    y_included_so_far = np.zeros_like(y)
+    w_included_so_far = np.zeros_like(w)
 
     tree = PytestWeightedFenwickTree(n)
     tree.py_reset(n)
@@ -22,15 +22,17 @@ def test_cython_weighted_fenwick_tree(global_random_seed):
     for i in range(n):
         idx = indices[i]
         tree.py_add(idx, y[idx], w[idx])
-        y_sorted[idx] = y[idx]
-        w_sorted[idx] = w[idx]
+        y_included_so_far[idx] = y[idx]
+        w_included_so_far[idx] = w[idx]
 
-        target = rng.uniform(0, w_sorted.sum())
+        target = rng.uniform(0, w_included_so_far.sum())
         t_idx_low, t_idx, cw, cwy = tree.py_search(target)
 
         # check the aggregates are consistent with the returned idx
-        assert np.isclose(cw, w_sorted[:t_idx].sum())
-        assert np.isclose(cwy, (w_sorted[:t_idx] * y_sorted[:t_idx]).sum())
+        assert np.isclose(cw, np.sum(w_included_so_far[:t_idx]))
+        assert np.isclose(
+            cwy, np.sum(w_included_so_far[:t_idx] * y_included_so_far[:t_idx])
+        )
 
         # check if the cumulative weight is less than or equal to the target
         # depending on t_idx_low and t_idx
@@ -40,10 +42,10 @@ def test_cython_weighted_fenwick_tree(global_random_seed):
             assert cw == target
 
         # check that if we add the next weight, we are above the target:
-        next_weights = w_sorted[t_idx:][w_sorted[t_idx:] > 0]
+        next_weights = w_included_so_far[t_idx:][w_included_so_far[t_idx:] > 0]
         if next_weights.size > 0:
             assert cw + next_weights[0] > target
         # and not below the target for `t_idx_low`:
-        next_weights = w_sorted[t_idx_low:][w_sorted[t_idx_low:] > 0]
+        next_weights = w_included_so_far[t_idx_low:][w_included_so_far[t_idx_low:] > 0]
         if next_weights.size > 0:
             assert cw + next_weights[0] >= target
