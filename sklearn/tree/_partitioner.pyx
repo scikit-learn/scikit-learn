b4e81a1030fda63c00221e2fbd89c5c9e5b3ed74
Arthur <arthur.lcte@gmail.com>
2025-10-28 11:25:14 +0100

    fix linting

--- sklearn/tree/_partitioner.pyx
+++ sklearn/tree/_partitioner.pyx
@@ -15,7 +15,7 @@ from libc.math cimport isnan, log2
 from libc.stdlib cimport qsort
 from libc.string cimport memcpy
 
-from ._utils cimport swap_array_slices
+from sklearn.tree._utils cimport swap_array_slices
 
 import numpy as np
 from scipy.sparse import issparse
