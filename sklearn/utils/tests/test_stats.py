e62a54110a0cfb36e90fce996d40958510178769
Lucy Liu <jliu176@gmail.com>
2025-10-31 15:59:48 +1100

    add 1d percentile rank to test_weighted_percentile_2d

--- sklearn/utils/tests/test_stats.py
+++ sklearn/utils/tests/test_stats.py
@@ -178,8 +178,9 @@ def test_weighted_percentile_constant_multiplier(
     assert percentile == approx(percentile_multiplier)
 
 
+@pytest.mark.parametrize("percentile_rank", [50, [20, 35, 50]])
 @pytest.mark.parametrize("average", [True, False])
-def test_weighted_percentile_2d(global_random_seed, average):
+def test_weighted_percentile_2d(global_random_seed, percentile_rank, average):
     """Check `_weighted_percentile` behaviour is correct when `array` is 2D."""
     # Check for when array 2D and sample_weight 1D
     rng = np.random.RandomState(global_random_seed)
@@ -189,23 +190,67 @@ def test_weighted_percentile_2d(global_random_seed, average):
     x2 = rng.randint(20, size=10)
     x_2d = np.vstack((x1, x2)).T
 
-    w_median = _weighted_percentile(x_2d, w1, average=average)
-    p_axis_0 = [
-        _weighted_percentile(x_2d[:, i], w1, average=average)
-        for i in range(x_2d.shape[1])
-    ]
-    assert_allclose(w_median, p_axis_0)
+    wp = _weighted_percentile(
+        x_2d, w1, percentile_rank=percentile_rank, average=average
+    )
+
+    if isinstance(percentile_rank, list):
+        p_list = []
+        for pr in percentile_rank:
+            p_list.append(
+                [
+                    _weighted_percentile(
+                        x_2d[:, i], w1, percentile_rank=pr, average=average
+                    )
+                    for i in range(x_2d.shape[1])
+                ]
+            )
+        p_axis_0 = np.stack(p_list, axis=-1)
+        assert wp.shape == (x_2d.shape[1], len(percentile_rank))
+    else:
+        # percentile_rank is scalar
+        p_axis_0 = [
+            _weighted_percentile(
+                x_2d[:, i], w1, percentile_rank=percentile_rank, average=average
+            )
+            for i in range(x_2d.shape[1])
+        ]
+        assert wp.shape == (x_2d.shape[1],)
+
+    assert_allclose(wp, p_axis_0)
 
     # Check when array and sample_weight both 2D
     w2 = rng.choice(5, size=10)
     w_2d = np.vstack((w1, w2)).T
 
-    w_median = _weighted_percentile(x_2d, w_2d, average=average)
-    p_axis_0 = [
-        _weighted_percentile(x_2d[:, i], w_2d[:, i], average=average)
-        for i in range(x_2d.shape[1])
-    ]
-    assert_allclose(w_median, p_axis_0)
+    wp = _weighted_percentile(
+        x_2d, w_2d, percentile_rank=percentile_rank, average=average
+    )
+
+    if isinstance(percentile_rank, list):
+        p_list = []
+        for pr in percentile_rank:
+            p_list.append(
+                [
+                    _weighted_percentile(
+                        x_2d[:, i], w_2d[:, i], percentile_rank=pr, average=average
+                    )
+                    for i in range(x_2d.shape[1])
+                ]
+            )
+        p_axis_0 = np.stack(p_list, axis=-1)
+        assert wp.shape == (x_2d.shape[1], len(percentile_rank))
+    else:
+        # percentile_rank is scalar
+        p_axis_0 = [
+            _weighted_percentile(
+                x_2d[:, i], w_2d[:, i], percentile_rank=percentile_rank, average=average
+            )
+            for i in range(x_2d.shape[1])
+        ]
+        assert wp.shape == (x_2d.shape[1],)
+
+    assert_allclose(wp, p_axis_0)
 
 
 @pytest.mark.parametrize(
