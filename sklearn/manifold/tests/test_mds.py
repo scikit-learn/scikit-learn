1360d2fe4c0e0f020ca32383792e9c0d04b4117d
Dmitry Kobak <dmitry.kobak@uni-tuebingen.de>
2025-11-05 15:41:01 +0100

    Remove regexp from match

--- sklearn/manifold/tests/test_mds.py
+++ sklearn/manifold/tests/test_mds.py
@@ -261,15 +261,15 @@ def test_future_warning_init_and_metric():
     sim = np.array([[0, 5, 3, 4], [5, 0, 2, 2], [3, 2, 0, 1], [4, 2, 1, 0]])
 
     # dissimilarity argument deprecated
-    with pytest.warns(FutureWarning, match=r"`dissimilarity` parameter is"):
+    with pytest.warns(FutureWarning, match="`dissimilarity` parameter is"):
         mds.MDS(dissimilarity="precomputed", init="random", n_init=1).fit(sim)
 
     # metric=True deprecated
-    with pytest.warns(FutureWarning, match=r"Use metric_mds"):
+    with pytest.warns(FutureWarning, match="Use metric_mds"):
         mds.MDS(metric=True, init="random", n_init=1).fit(X)
 
     # metric=False deprecated
-    with pytest.warns(FutureWarning, match=r"Use metric_mds"):
+    with pytest.warns(FutureWarning, match="Use metric_mds"):
         mds.MDS(metric=False, init="random", n_init=1).fit(X)
 
     # default init will become classical_mds in the future
@@ -282,7 +282,7 @@ def test_future_warning_init_and_metric():
         mds.MDS(metric="euclidean", init="random").fit(X)
 
     # providing both metric and dissimilarity raises an error
-    with pytest.raises(ValueError, match=r"provided both `dissimilarity`"):
+    with pytest.raises(ValueError, match="provided both `dissimilarity`"):
         mds.MDS(
             metric="cosine", dissimilarity="euclidean", init="random", n_init=1
         ).fit(X)
