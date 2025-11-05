090d78b86f35b3a71a7a12890bfb71c084c001d9
Josef Affourtit <josef.affourtit@gmail.com>
2025-11-05 07:29:03 -0500

    Remove duplicate code

--- sklearn/metrics/cluster/tests/test_common.py
+++ sklearn/metrics/cluster/tests/test_common.py
@@ -4,7 +4,6 @@ from itertools import chain
 import numpy as np
 import pytest
 
-from sklearn._config import config_context
 from sklearn.metrics.cluster import (
     adjusted_mutual_info_score,
     adjusted_rand_score,
@@ -19,13 +18,12 @@ from sklearn.metrics.cluster import (
     silhouette_score,
     v_measure_score,
 )
+from sklearn.metrics.tests.test_common import check_array_api_metric
 from sklearn.utils._array_api import (
-    _atol_for_type,
-    _convert_to_numpy,
     _get_namespace_device_dtype_ids,
     yield_namespace_device_dtype_combinations,
 )
-from sklearn.utils._testing import _array_api_for_tests, assert_allclose
+from sklearn.utils._testing import assert_allclose
 
 # Dictionaries of metrics
 # ------------------------
@@ -239,54 +237,6 @@ def test_returned_value_consistency(name):
 
     assert isinstance(score, float)
     assert not isinstance(score, (np.float64, np.float32))
-
-
-def check_array_api_metric(
-    metric, array_namespace, device, dtype_name, a_np, b_np, **metric_kwargs
-):
-    xp = _array_api_for_tests(array_namespace, device)
-
-    a_xp = xp.asarray(a_np, device=device)
-    b_xp = xp.asarray(b_np, device=device)
-
-    metric_np = metric(a_np, b_np, **metric_kwargs)
-
-    # When array API dispatch is disabled, and np.asarray works (for example PyTorch
-    # with CPU device), calling the metric function with such numpy compatible inputs
-    # should work (albeit by implicitly converting to numpy arrays instead of
-    # dispatching to the array library).
-    try:
-        np.asarray(a_xp)
-        np.asarray(b_xp)
-        numpy_as_array_works = True
-    except (TypeError, RuntimeError, ValueError):
-        # PyTorch with CUDA device and CuPy raise TypeError consistently.
-        # array-api-strict chose to raise RuntimeError instead. NumPy raises
-        # a ValueError if the `__array__` dunder does not return an array.
-        # Exception type may need to be updated in the future for other libraries.
-        numpy_as_array_works = False
-
-    def _check_metric_matches(metric_a, metric_b, convert_a=False):
-        if convert_a:
-            metric_a = _convert_to_numpy(xp.asarray(metric_a), xp)
-        assert_allclose(metric_a, metric_b, atol=_atol_for_type(dtype_name))
-
-    if numpy_as_array_works:
-        metric_xp = metric(a_xp, b_xp, **metric_kwargs)
-
-        # Handle cases where multiple return values are not of the same shape,
-        # e.g. precision_recall_curve:
-        _check_metric_matches(metric_xp, metric_np)
-
-        metric_xp_mixed_1 = metric(a_np, b_xp, **metric_kwargs)
-        _check_metric_matches(metric_xp_mixed_1, metric_np)
-
-        metric_xp_mixed_2 = metric(a_xp, b_np, **metric_kwargs)
-        _check_metric_matches(metric_xp_mixed_2, metric_np)
-
-    with config_context(array_api_dispatch=True):
-        metric_xp = metric(a_xp, b_xp, **metric_kwargs)
-        _check_metric_matches(metric_xp, metric_np, convert_a=True)
 
 
 def check_array_api_unsupervised_metric(metric, array_namespace, device, dtype_name):
