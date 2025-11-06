5f91ccfa2e752449d68363376c068bf03f10192f
Olivier Grisel <olivier.grisel@ensta.org>
2025-11-06 10:08:19 +0100

    Adjust rtol to account for the new float32 handling in the gradient estimation code

--- sklearn/linear_model/tests/test_logistic.py
+++ sklearn/linear_model/tests/test_logistic.py
@@ -2599,11 +2599,7 @@ def test_logistic_regression_array_api_compliance(
     preditct_log_proba_np = lr_np.predict_log_proba(X_np)
     prediction_np = lr_np.predict(X_np)
     atol = _atol_for_type(dtype_name)
-    rtol = 1e-5
-    if device_ == "mps":
-        # MPS backed does not allow for float64 computations, leading to
-        # reduced precision.
-        rtol = 5e-2
+    rtol = 5e-2 if dtype_name == "float32" else 1e-5
 
     with config_context(array_api_dispatch=True):
         with warnings.catch_warnings():
