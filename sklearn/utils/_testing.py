d4473bd66decfc86623a678a2c9b493ddbaafbad
Christian Lorentzen <lorentzen.ch@gmail.com>
2025-11-06 17:43:59 +0100

    CLN remove filter warnings in conf.py and _testing.py

--- sklearn/utils/_testing.py
+++ sklearn/utils/_testing.py
@@ -1446,12 +1446,6 @@ def _get_warnings_filters_info_list():
             message="Class PassiveAggressive.+is deprecated",
             category=FutureWarning,
         ),
-        # TODO(1.10): remove use_legacy_attributes from LogisticRegressionCV
-        WarningInfo(
-            "ignore",
-            message="The default value of use_legacy_attributes.+",
-            category=FutureWarning,
-        ),
     ]
 
 
