384e4bd451d1b6627201c6241c6808fcb79c4e7f
Olivier Grisel <olivier.grisel@ensta.org>
2025-11-14 11:29:40 +0100

    Do not expect CPU device in test for older numpy versions

--- sklearn/utils/tests/test_array_api.py
+++ sklearn/utils/tests/test_array_api.py
@@ -687,15 +687,17 @@ def test_add_to_diagonal(array_namespace, device_, dtype_name):
 @pytest.mark.parametrize("csr_container", CSR_CONTAINERS)
 @pytest.mark.parametrize("dispatch", [True, False])
 def test_sparse_device(csr_container, dispatch):
+    # Before numpy 2, the device attribute is not available on numpy arrays.
+    expect_cpu_device = hasattr(numpy.array([[1]]), "device") and dispatch
     a, b = csr_container(numpy.array([[1]])), csr_container(numpy.array([[2]]))
     if dispatch and os.environ.get("SCIPY_ARRAY_API") is None:
         raise SkipTest("SCIPY_ARRAY_API is not set: not checking array_api input")
     with config_context(array_api_dispatch=dispatch):
         assert device(a, b) is None
-        assert device(a, numpy.array([1])) == ("cpu" if dispatch else None)
+        assert device(a, numpy.array([1])) == ("cpu" if expect_cpu_device else None)
         assert get_namespace_and_device(a, b)[2] is None
         assert get_namespace_and_device(a, numpy.array([1]))[2] == (
-            "cpu" if dispatch else None
+            "cpu" if expect_cpu_device else None
         )
 
 
