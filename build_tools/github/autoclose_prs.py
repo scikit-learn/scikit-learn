9523f5f1552a2d8dc5edc56d28990749fcd9cfba
Loïc Estève <loic.esteve@ymail.com>
2025-11-19 16:34:26 +0100

    CI Tweak autoclose script to add more debug info

--- build_tools/github/autoclose_prs.py
+++ build_tools/github/autoclose_prs.py
@@ -3,21 +3,23 @@
 Called from .github/workflows/autoclose-schedule.yml."""
 
 import os
-from datetime import datetime, timezone
+from datetime import datetime, timedelta, timezone
+from pprint import pprint
 
 from github import Github
 
-CUTOFF_DAYS = 14
-
 
 def get_labeled_last_time(pr, label):
-    labeled_time = None
+    labeled_time = datetime.max
     for event in pr.get_events():
         if event.event == "labeled" and event.label.name == label:
             labeled_time = event.created_at
 
     return labeled_time
 
+
+dry_run = False
+cutoff_days = 14
 
 gh_repo = "scikit-learn/scikit-learn"
 github_token = os.getenv("GITHUB_TOKEN")
@@ -29,13 +31,20 @@ repo = gh.get_repo(gh_repo)
 now = datetime.now(timezone.utc)
 label = "autoclose"
 prs = [
-    each
-    for each in repo.get_issues(labels=[label])
-    if each.pull_request is not None
-    and (now - get_labeled_last_time(each, label)).days > CUTOFF_DAYS
+    each for each in repo.get_issues(labels=[label]) if each.pull_request is not None
 ]
-pr_numbers = [pr.number for pr in prs]
-print(f"Found {len(prs)} PRs to autoclose: {pr_numbers}")
+prs_info = [f"{pr.title}: {pr.html_url}" for pr in prs]
+print(f"Found {len(prs)} opened PRs with label {label}")
+pprint(prs_info)
+
+prs = [
+    pr
+    for pr in prs
+    if (now - get_labeled_last_time(pr, label)) > timedelta(days=cutoff_days)
+]
+prs_info = [f"{pr.title} {pr.html_url}" for pr in prs]
+print(f"Found {len(prs)} PRs to autoclose")
+pprint(prs_info)
 
 message = (
     "Thank you for your interest in contributing to scikit-learn, but we cannot "
@@ -49,5 +58,6 @@ message = (
 
 for pr in prs:
     print(f"Closing PR #{pr.number} with comment")
-    pr.create_comment(message)
-    pr.edit(state="closed")
+    if not dry_run:
+        pr.create_comment(message)
+        pr.edit(state="closed")
