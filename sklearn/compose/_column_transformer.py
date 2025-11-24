160312420c33e9365966fe8d3cda60b39191dbb7
Dea María Léon <deamarialeon@gmail.com>
2025-11-24 11:30:28 +0100

    wip

--- sklearn/compose/_column_transformer.py
+++ sklearn/compose/_column_transformer.py
@@ -1223,29 +1223,37 @@ class ColumnTransformer(TransformerMixin, _BaseComposition):
             return np.hstack(Xs)
 
     def _sk_visual_block_(self):
-        breakpoint()
-        transformers = [
-            transformer
-            for transformer in getattr(self, "transformers_", self.transformers)
-            if transformer[1] != "drop"
-        ]
-        # We can find remainder and its column only when it's fitted
-        if hasattr(self, "_remainder"):
-            remainder_columns = self._remainder[2]
-            if (
-                hasattr(self, "feature_names_in_")
-                and remainder_columns
-                and not all(isinstance(col, str) for col in remainder_columns)
-            ):
-                remainder_columns = self.feature_names_in_[remainder_columns].tolist()
-            transformers = chain(
-                self.transformers, [("remainder", self.remainder, remainder_columns)]
-            )
+        if hasattr(self, "transformers_"):
+            transformers = [
+                transformer
+                for transformer in getattr(self, "transformers_", self.transformers)
+                if transformer[1] != "drop"
+            ]
+            # We can find remainder columns only when columntransformer is fitted
+            if hasattr(self, "_remainder"):
+                remainder_columns = self._remainder[2]
+                if (
+                    hasattr(self, "feature_names_in_")
+                    and remainder_columns
+                    and not all(isinstance(col, str) for col in remainder_columns)
+                ):
+                    remainder_columns = self.feature_names_in_[
+                        remainder_columns
+                    ].tolist()
+                transformers = chain(
+                    self.transformers,
+                    [("remainder", self.remainder, remainder_columns)],
+                )
         else:
-            transformers = chain(self.transformers, [("remainder", self.remainder, "")])
-            # remainder_columns = ""
+            if self.remainder != "drop":
+                transformers = chain(
+                    self.transformers, [("remainder", self.remainder, [])]
+                )
+            else:
+                transformers = self.transformers
 
         names, transformers, name_details = zip(*transformers)
+
         return _VisualBlock(
             "parallel", transformers, names=names, name_details=name_details
         )
