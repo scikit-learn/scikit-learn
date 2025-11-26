e3e3e527b633687a8d0115ad20c5d7110e16890b
Loïc Estève <loic.esteve@ymail.com>
2025-11-26 04:04:07 +0100

    Tackle Lucy's comment [azure parallel]

--- sklearn/tests/test_min_dependencies_readme.py
+++ sklearn/tests/test_min_dependencies_readme.py
@@ -96,9 +96,9 @@ def check_pyproject_section(
         info = info[key]
 
     pyproject_build_min_versions = {}
-    # Assuming pyproject.toml build section has something like my-package>=<min_version>
+    # Assuming pyproject.toml build section has something like "my-package>=min_version"
     # Warning: if you try to modify this regex, bear in mind that there can be upper
-    # bounds in release branches so my-package>=<min_version>,<max_version>
+    # bounds in release branches so "my-package>=min_version,<max_version"
     pattern = r"([\w-]+)\s*[>=]=\s*([\d\w.]+)"
     for requirement in info:
         match = re.search(pattern, requirement)
