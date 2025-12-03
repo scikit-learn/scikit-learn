7180b5e7a171c20f3b692221a52a0d80f9a69fd6
Loïc Estève <loic.esteve@ymail.com>
2025-12-03 12:30:49 +0100

    Apply suggestions from code review
    Co-authored-by: Olivier Grisel <olivier.grisel@ensta.org>
    Co-authored-by: Virgil Chan <virchan.math@gmail.com>

--- examples/release_highlights/plot_release_highlights_1_8_0.py
+++ examples/release_highlights/plot_release_highlights_1_8_0.py
@@ -24,8 +24,15 @@ or with conda::
 # %%
 # Array API support
 # -----------------
+# The progressive adoption of the Python array API standard in SciPy and
+# scikit-learn allows the user to pass input arrays from conforming
+# libraries to scikit-learn estimators and functions and let them
+# use those libraries and possibly non-CPU devices such as GPUs to perform
+# the computation instead of attempting to convert all inputs to NumPy.
+# 
 # In scikit-learn 1.8, several estimators and functions have been updated to
-# support array API compatible inputs, for example PyTorch tensors and CuPy arrays.
+# support array API compatible inputs, for example PyTorch tensors and CuPy
+# arrays.
 #
 # Array API support was added to the following estimators:
 # :class:`preprocessing.StandardScaler`, :class:`preprocessing.PolynomialFeatures`,
@@ -37,6 +44,8 @@ or with conda::
 #
 # Please refer the :ref:`array API support<array_api>` page for instructions
 # to use scikit-learn with array API compatible libraries such as PyTorch or CuPy.
+# Note that array API support is still experimental and must be 
+# explicitly be enabled both in SciPy and scikit-learn to work properly.
 #
 # TODO do we want to write a snippet?
 # - which estimators would we feature?
@@ -65,11 +74,49 @@ or with conda::
 # For more details about free-threaded CPython see `py-free-threading doc <https://py-free-threading.github.io>`_,
 # in particular `how to install a free-threaded CPython <https://py-free-threading.github.io/installing_cpython/>`_
 # and `Ecosystem compatibility tracking <https://py-free-threading.github.io/tracking/>`_.
+#
+# The long term goal of free-threaded Python is to more efficiently leverage
+# multi-core CPUs by using thread workers instead of subprocess workers for parallel
+# computation when passing `n_jobs>1` in functions or estimators.
+# Efficiency gains are expected by removing the need for inter-process communication.
+# Note however that process-based parallelism is still the default joblib backend at
+# the time of writing. You can call `joblib.parallel_config(backend="threading")` to
+# change the default backend to "threading". Be aware that properly testing that
+# everything is running smoothly when doing so is still an ongoing effort and that
+# there are open issues to fix before considering making this the default.
 
 # %%
 # Temperature scaling in `CalibratedClassifierCV`
 # -----------------------------------------------
-# TODO
+# Probability calibration with temperature scaling is available in
+# :class:`calibration.CalibratedClassifierCV` by setting `method = "temperature"`.
+# This method offers a natural way to obtain (better-)calibrated multi-class
+# probabilities with just one free parameter, in contrast to using a
+# "One-vs-Rest" scheme that adds more parameters for each single class.
+
+from sklearn.calibration import CalibratedClassifierCV
+from sklearn.datasets import make_classification
+from sklearn.frozen import FrozenEstimator
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
+
+X, y = make_classification(
+    n_samples=1000,
+    n_features=10,
+    n_informative=10,
+    n_redundant=0,
+    n_classes=5,
+    n_clusters_per_class=1,
+    class_sep=2.0,
+    random_state=42,
+)
+X_train, X_calib, y_train, y_calib = train_test_split(X, y, random_state=42)
+clf = LinearSVC(random_state=42)
+clf.fit(X_train, y_train)
+ts = CalibratedClassifierCV(FrozenEstimator(clf), method="temperature", ensemble=False)
+ts.fit(X_calib, y_calib)
+beta = ts.calibrated_classifiers_[0].calibrators[0].beta_
+print(f"Optimal temperature = {1 / beta:.3}")
 
 # %%
 # Linear models improvements
