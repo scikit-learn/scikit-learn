41b0fc912cc67ec5553fe9fc8d716dc5cbc1a722
Lucy Liu <jliu176@gmail.com>
2025-12-10 21:06:11 +1100

    allow string y true in confusion matrix thresholds

--- sklearn/metrics/_ranking.py
+++ sklearn/metrics/_ranking.py
@@ -30,6 +30,7 @@ from sklearn.utils import (
 )
 from sklearn.utils._array_api import (
     _max_precision_float_dtype,
+    get_namespace,
     get_namespace_and_device,
     move_to,
     size,
@@ -927,6 +928,9 @@ def confusion_matrix_at_thresholds(y_true, y_score, pos_label=None, sample_weigh
         raise ValueError("{0} format is not supported".format(y_type))
 
     xp, _, device = get_namespace_and_device(y_score)
+    pos_label = _check_pos_label_consistency(pos_label, y_true)
+    xp_y_true, _ = get_namespace(y_true)
+    y_true = xp_y_true.asarray(y_true == pos_label, dtype=xp_y_true.int64)
     y_true, sample_weight = move_to(y_true, sample_weight, xp=xp, device=device)
 
     check_consistent_length(y_true, y_score, sample_weight)
@@ -943,8 +947,6 @@ def confusion_matrix_at_thresholds(y_true, y_score, pos_label=None, sample_weigh
         y_true = y_true[nonzero_weight_mask]
         y_score = y_score[nonzero_weight_mask]
         sample_weight = sample_weight[nonzero_weight_mask]
-
-    pos_label = _check_pos_label_consistency(pos_label, y_true)
 
     # make y_true a boolean vector
     y_true = y_true == pos_label
