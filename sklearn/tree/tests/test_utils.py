8e57ad29c186c4a93ed7b5d646f41de9b9f898e5
Arthur <arthur.lcte@gmail.com>
2025-12-23 19:30:23 +0100

    improved test_py_swap_array_slices_random

--- sklearn/tree/tests/test_utils.py
+++ sklearn/tree/tests/test_utils.py
@@ -5,25 +5,25 @@ from sklearn.tree._utils import _py_swap_array_slices
 
 
 @pytest.mark.parametrize("dtype", [np.float32, np.intp])
-def test_py_swap_array_slices_random(dtype):
+def test_py_swap_array_slices_random(dtype, global_random_seed):
     def swap_slices_np(arr, start, end, n):
         """
-        Swaps the order of the slices array[start:start + n]
-        and array[start + n:end] while preserving the order
-        in the slices. Works for any itemsize.
+        Swaps the order of the slices array[start:start + n] and
+        array[start + n:end] while preserving the order in the slices.
         """
+        arr = arr.copy()
         arr[start:end] = np.concatenate([arr[start + n : end], arr[start : start + n]])
+        return arr
 
-    rng = np.random.default_rng(0)
+    rng = np.random.default_rng(global_random_seed)
 
-    for _ in range(10):
+    for _ in range(20):
         arr = rng.permutation(100).astype(dtype)
         start = rng.integers(40)
         end = rng.integers(60, 100)
         split = rng.integers(end - start)
-
-        expected = arr.copy()
-        swap_slices_np(expected, start, end, split)
+        # test the swap of arr[start:start + split] with arr[start + split:end]
+        expected = swap_slices_np(arr, start, end, split)
 
         _py_swap_array_slices(arr, start, end, split)
         np.testing.assert_array_equal(arr, expected)
