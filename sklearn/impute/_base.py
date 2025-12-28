962860306aaafa19fc85e176b5b160fbadadb6f3
aliallouf <ali1allouf@gmail.com>
2025-12-28 11:46:06 +0100

    DOC: clarify SimpleImputer docstring example

--- sklearn/impute/_base.py
+++ sklearn/impute/_base.py
@@ -285,14 +285,14 @@ class SimpleImputer(_BaseImputer):
     --------
     >>> import numpy as np
     >>> from sklearn.impute import SimpleImputer
-    >>> imp_mean = SimpleImputer(missing_values=np.nan, strategy='mean')
-    >>> imp_mean.fit([[7, 2, 3], [4, np.nan, 6], [10, 5, 9]])
+    >>> imp = SimpleImputer(missing_values=np.nan, strategy='mean')
+    >>> X = [[1, 24], [2, np.nan], [4, 16]]
+    >>> imp.fit(X)
     SimpleImputer()
-    >>> X = [[np.nan, 2, 3], [4, np.nan, 6], [10, np.nan, 9]]
-    >>> print(imp_mean.transform(X))
-    [[ 7.   2.   3. ]
-     [ 4.   3.5  6. ]
-     [10.   3.5  9. ]]
+    >>> print(imp.transform(X))
+    [[ 1. 24.]
+     [ 2. 20.]
+     [ 4. 16.]]
 
     For a more detailed example see
     :ref:`sphx_glr_auto_examples_impute_plot_missing_values.py`.
