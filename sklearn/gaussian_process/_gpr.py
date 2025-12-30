f5a0975fc4829a9b2382bf68f9b4d5c84a50433d
Matthias De Lozzo <matthias.delozzo@irt-saintexupery.com>
2025-12-30 11:04:35 +0100

    DOC: docstrings (semantic linefeeds, rewording, ReST, etc.)

--- sklearn/gaussian_process/_gpr.py
+++ sklearn/gaussian_process/_gpr.py
@@ -36,15 +36,16 @@ class GaussianProcessRegressor(MultiOutputMixin, RegressorMixin, BaseEstimator):
     In addition to standard scikit-learn estimator API,
     :class:`GaussianProcessRegressor`:
 
-    * allows prediction without prior fitting (based on the GP prior)
-    * provides an additional method `sample_y(X)`, which evaluates samples
-      drawn from the GPR (prior or posterior) at given inputs
-    * exposes a method `log_marginal_likelihood(theta)`, which can be used
-      externally for other ways of selecting hyperparameters, e.g., via
-      Markov chain Monte Carlo.
+    * allows prediction without prior fitting, based on the Gaussian process (GP) prior,
+    * provides an additional method :meth:`sample_y`,
+      which evaluates samples drawn from the GP (prior or posterior) at given inputs,
+    * exposes a method :meth:`log_marginal_likelihood`,
+      which can be used externally for other ways of selecting hyperparameters,
+      e.g., via Markov chain Monte Carlo.
 
-    To learn the difference between a point-estimate approach vs. a more
-    Bayesian modelling approach, refer to the example entitled
+    To learn the difference
+    between a point-estimate approach vs. a more Bayesian modelling approach,
+    refer to the example entitled
     :ref:`sphx_glr_auto_examples_gaussian_process_plot_compare_gpr_krr.py`.
 
     Read more in the :ref:`User Guide <gaussian_process>`.
@@ -54,99 +55,107 @@ class GaussianProcessRegressor(MultiOutputMixin, RegressorMixin, BaseEstimator):
     Parameters
     ----------
     kernel : kernel instance, default=None
-        The kernel specifying the covariance function of the GP. If None is
-        passed, the kernel ``ConstantKernel(1.0, constant_value_bounds="fixed")
-        * RBF(1.0, length_scale_bounds="fixed")`` is used as default. Note that
-        the kernel hyperparameters are optimized during fitting unless the
-        bounds are marked as "fixed".
+        The kernel specifying the covariance function of the GP.
+        If `None` is passed,
+        :class:`~sklearn.gaussian_process.kernels.RBF` with fixed parameters is used.
+        Note that the kernel hyperparameters are optimized during fitting
+        unless the bounds are marked as "fixed".
 
     alpha : float or ndarray of shape (n_samples,), default=1e-10
         Value added to the diagonal of the kernel matrix during fitting.
-        This can prevent a potential numerical issue during fitting, by
-        ensuring that the calculated values form a positive definite matrix.
-        It can also be interpreted as the variance of additional Gaussian
-        measurement noise on the training observations. Note that this is
-        different from using a `WhiteKernel`. If an array is passed, it must
-        have the same number of entries as the data used for fitting and is
-        used as datapoint-dependent noise level. Allowing to specify the
-        noise level directly as a parameter is mainly for convenience and
-        for consistency with :class:`~sklearn.linear_model.Ridge`.
-        For an example illustrating how the alpha parameter controls
-        the noise variance in Gaussian Process Regression, see
-        :ref:`sphx_glr_auto_examples_gaussian_process_plot_gpr_noisy_targets.py`.
+        This can prevent a potential numerical issue during fitting,
+        by ensuring that the calculated values form a positive definite matrix.
+        It can also be interpreted as
+        the variance of a Gaussian measurement noise on the training target values.
+        Note that this is different
+        from using a :class:`~sklearn.gaussian_process.kernels.WhiteKernel`.
+        If an array is passed,
+        it must have the same number of entries as the number of training samples
+        and is used as datapoint-dependent noise level.
+        Allowing to specify the noise level directly as a parameter is mainly
+        for convenience and for consistency with :class:`~sklearn.linear_model.Ridge`.
+        For an example
+        illustrating how the `alpha` parameter controls the noise variance in GPR,
+        see :ref:`sphx_glr_auto_examples_gaussian_process_plot_gpr_noisy_targets.py`.
 
     optimizer : "fmin_l_bfgs_b", callable or None, default="fmin_l_bfgs_b"
-        Can either be one of the internally supported optimizers for optimizing
-        the kernel's parameters, specified by a string, or an externally
-        defined optimizer passed as a callable. If a callable is passed, it
-        must have the signature::
+        Used for optimizing the kernel hyperparameters.
+        Can either be one of the internally supported optimizers specified by a string,
+        or an externally defined optimizer passed as a callable.
+        If a callable is passed,
+        it must have the signature:
 
             def optimizer(obj_func, initial_theta, bounds):
-                # * 'obj_func': the objective function to be minimized, which
-                #   takes the hyperparameters theta as a parameter and an
-                #   optional flag eval_gradient, which determines if the
-                #   gradient is returned additionally to the function value
-                # * 'initial_theta': the initial value for theta, which can be
-                #   used by local optimizers
-                # * 'bounds': the bounds on the values of theta
+                # * 'obj_func': the objective function to be minimized,
+                #   which takes the hyperparameters `theta` as an argument
+                #   and an optional argument `eval_gradient`,
+                #   which determines if the gradient is returned
+                #   additionally to the function value,
+                # * 'initial_theta': the initial value for theta,
+                #   which can be used by local optimizers,
+                # * 'bounds': the bounds on the values of theta.
                 ....
-                # Returned are the best found hyperparameters theta and
-                # the corresponding value of the target function.
+                # Returned are the best found hyperparameters theta
+                # and the corresponding value of the target function.
                 return theta_opt, func_min
 
-        Per default, the L-BFGS-B algorithm from `scipy.optimize.minimize`
-        is used. If None is passed, the kernel's parameters are kept fixed.
+        Per default,
+        the L-BFGS-B algorithm from `scipy.optimize.minimize` is used.
+        If `None` is passed,
+        the kernel hyperparameters are kept fixed.
         Available internal optimizers are: `{'fmin_l_bfgs_b'}`.
 
     n_restarts_optimizer : int, default=0
-        The number of restarts of the optimizer for finding the kernel's
-        parameters which maximize the log-marginal likelihood. The first run
-        of the optimizer is performed from the kernel's initial parameters,
-        the remaining ones (if any) from thetas sampled log-uniform randomly
-        from the space of allowed theta-values. If greater than 0, all bounds
-        must be finite. Note that `n_restarts_optimizer == 0` implies that one
-        run is performed.
+        The number of restarts of the optimizer
+        for finding the kernel hyperparameters maximizing the log-marginal likelihood,
+        in addition to its first run from the initial hyperparameters.
+        These repetititions starts from new initial hyperparameters
+        uniformly sampled over the bounded hyperparameter space.
 
     normalize_y : bool, default=False
-        Whether or not to normalize the target values `y` by removing the mean
-        and scaling to unit-variance. This is recommended for cases where
-        zero-mean, unit-variance priors are used. Note that, in this
-        implementation, the normalisation is reversed before the GP predictions
-        are reported.
+        Whether to normalize the target values `y`
+        by removing the mean and scaling to unit-variance.
+        This is recommended for cases where zero-mean, unit-variance priors are used.
+        Note that,
+        in this implementation,
+        the normalisation is reversed before the GP predictions are reported.
 
         .. versionchanged:: 0.23
 
     copy_X_train : bool, default=True
-        If True, a persistent copy of the training data is stored in the
-        object. Otherwise, just a reference to the training data is stored,
-        which might cause predictions to change if the data is modified
-        externally.
+        If `True`,
+        a persistent copy of the training data is stored in the object.
+        Otherwise,
+        just a reference to the training data is stored,
+        which might cause predictions to change if the data is modified externally.
 
     n_targets : int, default=None
-        The number of dimensions of the target values. Used to decide the number
-        of outputs when sampling from the prior distributions (i.e. calling
-        :meth:`sample_y` before :meth:`fit`). This parameter is ignored once
-        :meth:`fit` has been called.
+        The number of dimensions of the target values.
+        Used to decide the number of outputs when sampling from the prior distributions
+        (i.e. calling :meth:`sample_y` before :meth:`fit`).
+        This argument is ignored once :meth:`fit` has been called.
 
         .. versionadded:: 1.3
 
     random_state : int, RandomState instance or None, default=None
         Determines random number generation used to initialize the centers.
-        Pass an int for reproducible results across multiple function calls.
+        Pass an integer for reproducible results across multiple function calls.
         See :term:`Glossary <random_state>`.
 
     Attributes
     ----------
     X_train_ : array-like of shape (n_samples, n_features) or list of object
-        Feature vectors or other representations of training data (also
-        required for prediction).
+        Feature vectors or other representations of training data
+        (also required for prediction).
 
     y_train_ : array-like of shape (n_samples,) or (n_samples, n_targets)
-        Target values in training data (also required for prediction).
+        Target values in training data (also required for prediction);
+        these target values are normalized when ``normalize_y`` is ``True``.
 
     kernel_ : kernel instance
-        The kernel used for prediction. The structure of the kernel is the
-        same as the one passed as parameter but with optimized hyperparameters.
+        The kernel used for prediction.
+        The structure of the kernel is the same as the one passed as argument
+        but with optimized hyperparameters.
 
     L_ : array-like of shape (n_samples, n_samples)
         Lower-triangular Cholesky decomposition of the kernel in ``X_train_``.
@@ -175,8 +184,8 @@ class GaussianProcessRegressor(MultiOutputMixin, RegressorMixin, BaseEstimator):
     >>> from sklearn.gaussian_process.kernels import DotProduct, WhiteKernel
     >>> X, y = make_friedman2(n_samples=500, noise=0, random_state=0)
     >>> kernel = DotProduct() + WhiteKernel()
-    >>> gpr = GaussianProcessRegressor(kernel=kernel,
-    ...         random_state=0).fit(X, y)
+    >>> gpr = GaussianProcessRegressor(kernel=kernel, random_state=0)
+    >>> gpr.fit(X, y)
     >>> gpr.score(X, y)
     0.3680...
     >>> gpr.predict(X[:2,:], return_std=True)
@@ -311,8 +320,8 @@ class GaussianProcessRegressor(MultiOutputMixin, RegressorMixin, BaseEstimator):
                 self.kernel_.theta, clone_kernel=False
             )
 
-        # Precompute quantities required for predictions which are independent
-        # of actual query points
+        # Precompute quantities required for predictions
+        # which are independent of actual query points
         # Alg. 2.1, page 19, line 2 -> L = cholesky(K + sigma^2 I)
         K = self.kernel_(self.X_train_)
         K[np.diag_indices_from(K)] += self.alpha
@@ -321,9 +330,10 @@ class GaussianProcessRegressor(MultiOutputMixin, RegressorMixin, BaseEstimator):
         except np.linalg.LinAlgError as exc:
             exc.args = (
                 (
-                    f"The kernel, {self.kernel_}, is not returning a positive "
-                    "definite matrix. Try gradually increasing the 'alpha' "
-                    "parameter of your GaussianProcessRegressor estimator."
+                    f"The kernel, {self.kernel_}, "
+                    "is not returning a positive definite matrix. "
+                    "Try gradually increasing the 'alpha' parameter "
+                    "of your GaussianProcessRegressor estimator."
                 ),
             ) + exc.args
             raise
@@ -339,9 +349,10 @@ class GaussianProcessRegressor(MultiOutputMixin, RegressorMixin, BaseEstimator):
         """Predict using the Gaussian process regression model.
 
         We can also predict based on an unfitted model by using the GP prior.
-        In addition to the mean of the predictive distribution, optionally also
-        returns its standard deviation (`return_std=True`) or covariance
-        (`return_cov=True`). Note that at most one of the two can be requested.
+        In addition to the mean of the predictive distribution,
+        optionally also returns its standard deviation (`return_std=True`)
+        or covariance (`return_cov=True`).
+        Note that at most one of the two can be requested.
 
         Parameters
         ----------
@@ -349,25 +360,25 @@ class GaussianProcessRegressor(MultiOutputMixin, RegressorMixin, BaseEstimator):
             Query points where the GP is evaluated.
 
         return_std : bool, default=False
-            If True, the standard-deviation of the predictive distribution at
-            the query points is returned along with the mean.
+            If `True`,
+            the standard-deviation at the query points is returned along with the mean.
 
         return_cov : bool, default=False
-            If True, the covariance of the joint predictive distribution at
-            the query points is returned along with the mean.
+            If `True`,
+            the covariance between the query points is returned along with the mean.
 
         Returns
         -------
         y_mean : ndarray of shape (n_samples,) or (n_samples, n_targets)
-            Mean of predictive distribution at query points.
+            Mean at query points.
 
         y_std : ndarray of shape (n_samples,) or (n_samples, n_targets), optional
-            Standard deviation of predictive distribution at query points.
+            Standard deviation at query points.
             Only returned when `return_std` is True.
 
         y_cov : ndarray of shape (n_samples, n_samples) or \
                 (n_samples, n_samples, n_targets), optional
-            Covariance of joint predictive distribution at query points.
+            Covariance between the query points.
             Only returned when `return_cov` is True.
         """
         if return_std and return_cov:
@@ -463,28 +474,26 @@ class GaussianProcessRegressor(MultiOutputMixin, RegressorMixin, BaseEstimator):
         return dtype, ensure_2d
 
     def sample_y(self, X, n_samples=1, random_state=0):
-        """Draw samples from Gaussian process and evaluate at X.
+        """Draw GP samples and evaluate them at query points.
 
         Parameters
         ----------
         X : array-like of shape (n_samples_X, n_features) or list of object
-            Query points where the GP is evaluated.
+            Query points.
 
         n_samples : int, default=1
-            Number of samples drawn from the Gaussian process per query point.
+            Number of GP samples.
 
         random_state : int, RandomState instance or None, default=0
-            Determines random number generation to randomly draw samples.
-            Pass an int for reproducible results across multiple function
-            calls.
+            Determines random number generation to randomly draw GP samples.
+            Pass an integer for reproducible results across multiple function calls.
             See :term:`Glossary <random_state>`.
 
         Returns
         -------
         y_samples : ndarray of shape (n_samples_X, n_samples), or \
             (n_samples_X, n_targets, n_samples)
-            Values of n_samples samples drawn from Gaussian process and
-            evaluated at query points.
+            GP samples evaluated at query points.
         """
         rng = check_random_state(random_state)
 
@@ -504,23 +513,28 @@ class GaussianProcessRegressor(MultiOutputMixin, RegressorMixin, BaseEstimator):
     def log_marginal_likelihood(
         self, theta=None, eval_gradient=False, clone_kernel=True
     ):
-        """Return log-marginal likelihood of theta for training data.
+        """Return log-marginal likelihood of kernel hyperparameters for training data.
 
         Parameters
         ----------
         theta : array-like of shape (n_kernel_params,) default=None
-            Kernel hyperparameters for which the log-marginal likelihood is
-            evaluated. If None, the precomputed log_marginal_likelihood
-            of ``self.kernel_.theta`` is returned.
+            Kernel hyperparameters value
+            at which the log-marginal likelihood is evaluated.
+            If ``None``,
+            the value ``self.kernel_.theta`` is considered.
 
         eval_gradient : bool, default=False
-            If True, the gradient of the log-marginal likelihood with respect
-            to the kernel hyperparameters at position theta is returned
-            additionally. If True, theta must not be None.
+            If ``True``,
+            the gradient of the log-marginal likelihood
+            with respect to the kernel hyperparameters is evaluated at ``theta``,
+            which must not be ``None``.
 
         clone_kernel : bool, default=True
-            If True, the kernel attribute is copied. If False, the kernel
-            attribute is modified, but may result in a performance improvement.
+            If ``True``,
+            the kernel attribute is copied.
+            If ``False``,
+            the kernel attribute is modified,
+            but may result in a performance improvement.
 
         Returns
         -------
@@ -528,9 +542,9 @@ class GaussianProcessRegressor(MultiOutputMixin, RegressorMixin, BaseEstimator):
             Log-marginal likelihood of theta for training data.
 
         log_likelihood_gradient : ndarray of shape (n_kernel_params,), optional
-            Gradient of the log-marginal likelihood with respect to the kernel
-            hyperparameters at position theta.
-            Only returned when eval_gradient is True.
+            Gradient of the log-marginal likelihood
+            with respect to the kernel hyperparameters at ``theta``.
+            Only returned when ``eval_gradient`` is ``True``.
         """
         if theta is None:
             if eval_gradient:
