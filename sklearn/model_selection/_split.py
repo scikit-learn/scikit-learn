1578321b5ed40f07b404bab36190b9bf5cdff220
Stefanie Senger <stefanie.senger@posteo.de>
2026-01-17 15:44:34 +0100

    fiy typo

--- sklearn/model_selection/_split.py
+++ sklearn/model_selection/_split.py
@@ -2698,7 +2698,7 @@ def check_cv(cv=5, y=None, *, classifier=False):
         - None, to use the default 5-fold cross validation,
         - integer, to specify the number of folds,
         - :term:`CV splitter`,
-        - An iterable that generates (train, test) splits as arrays of indices.
+        - an iterable that generates (train, test) splits as arrays of indices.
 
         For integer/None inputs, if classifier is True and ``y`` is either
         binary or multiclass, :class:`StratifiedKFold` is used. In all other
