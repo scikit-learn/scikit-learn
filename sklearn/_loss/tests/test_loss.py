89347085e50ce98162122847da5b7049cd37431f
Omar Salman <omar.salman2007@gmail.com>
2026-01-22 19:48:15 +0500

    Update sklearn/_loss/tests/test_loss.py
    Co-authored-by: Olivier Grisel <olivier.grisel@ensta.org>

--- sklearn/_loss/tests/test_loss.py
+++ sklearn/_loss/tests/test_loss.py
@@ -1491,4 +1491,7 @@ def test_log1pexp(namespace, device_, dtype_name):
             )
         )
         result_mpmath = float(mpmath.log(1 + mpmath.exp(value)))
-        assert result_xp == pytest.approx(result_mpmath)
+        # Check that the relative error is within float32 or float64 precision.
+        assert result_xp == pytest.approx(
+            result_mpmath, rel=1e-7 if dtype_name == "float32" else 1e-15, abs=0.
+        )
