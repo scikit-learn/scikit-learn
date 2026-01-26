fcd27ed77c41dbd9c8f17d21c5593c5941f5f0ba
ShiffiH <a548487473@gmail.com>
2026-01-26 15:53:42 +0200

    Cleanup: remove leftover local change

--- sklearn/inspection/_plot/decision_boundary.py
+++ sklearn/inspection/_plot/decision_boundary.py
@@ -20,38 +20,6 @@ from sklearn.utils.validation import (
 )
 
 
-# def _check_boundary_response_method(estimator, response_method):
-#     """Validate the response methods to be used with the fitted estimator.
-
-#     Parameters
-#     ----------
-#     estimator : object
-#         Fitted estimator to check.
-
-#     response_method : {'auto', 'decision_function', 'predict_proba', 'predict'}
-#         Specifies whether to use :term:`decision_function`, :term:`predict_proba`,
-#         :term:`predict` as the target response. If set to 'auto', the response method is
-#         tried in the before mentioned order.
-
-#     Returns
-#     -------
-#     prediction_method : list of str or str
-#         The name or list of names of the response methods to use.
-#     """
-#     has_classes = hasattr(estimator, "classes_")
-#     if has_classes and _is_arraylike_not_scalar(estimator.classes_[0]):
-#         msg = "Multi-label and multi-output multi-class classifiers are not supported"
-#         raise ValueError(msg)
-
-#     if response_method == "auto":
-#         if is_regressor(estimator):
-#             prediction_method = "predict"
-#         else:
-#             prediction_method = ["decision_function", "predict_proba", "predict"]
-#     else:
-#         prediction_method = response_method
-
-#     return prediction_method
 
 def _check_boundary_response_method(estimator, response_method):
     """Validate the response methods to be used with the fitted estimator.
