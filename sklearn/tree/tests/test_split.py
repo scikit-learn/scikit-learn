remove w > 0 masking after wp fix was merged

sklearn/tree/tests/test_split.py
@@ -53,13 +53,7 @@ class NaiveSplitter:
             pred = np.average(y, weights=w)
             loss = np.average((y - pred) ** 2, weights=w)
         elif self.criterion == "absolute_error":
-            pred = _weighted_percentile(
-                # TODO: remove w > 0 masking when #33127 is merged
-                y[w > 0],
-                w[w > 0],
-                percentile_rank=50,
-                average=True,
-            )
+            pred = _weighted_percentile(y, w, percentile_rank=50, average=True)
             loss = np.average(np.abs(y - pred), weights=w)
         elif self.criterion == "poisson":
             pred = np.average(y, weights=w)