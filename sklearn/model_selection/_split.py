56e3ec6c623b689a0825c65dd06e185812db8a25
gitCHANI2005 <chani20058664@gmail.com>
2026-02-04 11:51:39 +0200

    STYLE: fix whitespace in _split.py

--- sklearn/model_selection/_split.py
+++ sklearn/model_selection/_split.py
@@ -1046,13 +1046,13 @@ class StratifiedGroupKFold(GroupsConsumerMixin, _BaseKFold):
             groups, return_inverse=True, return_counts=True
         )
         n_groups = len(groups_cnt)
-        
+
         if self.n_splits > n_groups:
             raise ValueError(
                 "Cannot have number of splits n_splits=%d greater"
                 " than the number of groups: %d." % (self.n_splits, n_groups)
             )
-        
+
         y_counts_per_group = np.zeros((len(groups_cnt), n_classes))
         for class_idx, group_idx in zip(y_inv, groups_inv):
             y_counts_per_group[group_idx, class_idx] += 1
