2f6afb9ecbbdaec00efdbcde3edeb52b5070188c
Arthur <arthur.lcte@gmail.com>
2026-02-06 11:57:55 +0100

    numpy-style partitioning

--- sklearn/utils/_sorting.pyx
+++ sklearn/utils/_sorting.pyx
@@ -42,7 +42,7 @@ cdef int simultaneous_sort(
     # an Array of Structures (AoS) instead of the Structure of Arrays (SoA)
     # currently used.
     cdef:
-        intp_t pivot_idx, i, store_idx
+        intp_t pivot_idx, i, j, store_idx
         floating pivot_val
 
     # in the small-array case, do things efficiently
@@ -75,13 +75,23 @@ cdef int simultaneous_sort(
         # Partition indices about pivot.  At the end of this operation,
         # pivot_idx will contain the pivot value, everything to the left
         # will be smaller, and everything to the right will be larger.
-        store_idx = 0
-        for i in range(size - 1):
-            if values[i] < pivot_val:
-                dual_swap(values, indices, i, store_idx)
-                store_idx += 1
+        i = 0
+        j = size - 1
+        while True:
+            # Find element >= pivot from left
+            while i <= j and values[i] < pivot_val:
+                i += 1
+            # Find element <= pivot from right
+            while i <= j and values[j] > pivot_val:
+                j -= 1
+            if i >= j:
+                break
+            dual_swap(values, indices, i, j)
+            i += 1
+            j -= 1
+
         dual_swap(values, indices, store_idx, size - 1)
-        pivot_idx = store_idx
+        pivot_idx = i
 
         # Recursively sort each side of the pivot
         if pivot_idx > 1:
