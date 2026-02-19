bbb829cde0651b57324dd71bccebb21859b5c4e9
pshringi11 <paragshringi54@gmail.com>
2026-02-20 01:46:53 +0530

    docs: clarify redundant validation comment in _encoders.py
    Hi, I noticed that the comment on line 244 regarding check_unknown=False could be more descriptive to help developers understand that we are avoiding redundant validation. I've updated it for better clarity.

--- sklearn/preprocessing/_encoders.py
+++ sklearn/preprocessing/_encoders.py
@@ -241,8 +241,8 @@ class _BaseEncoder(TransformerMixin, BaseEstimator):
                         Xi = Xi.copy()
 
                     Xi[~valid_mask] = self.categories_[i][0]
-            # We use check_unknown=False, since _check_unknown was
-            # already called above.
+            # check_unknown is set to False to prevent duplicate validation, 
+            # as the input has already been verified by the previous call to _check_unknown.
             X_int[:, i] = _encode(Xi, uniques=self.categories_[i], check_unknown=False)
         if columns_with_unknown:
             if handle_unknown == "infrequent_if_exist":
