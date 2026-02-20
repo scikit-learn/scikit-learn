f1c3cb39ecee25ae6803ea8ebe65751fc4374d23
Christian Lorentzen <lorentzen.ch@gmail.com>
2026-02-20 23:27:44 +0100

    MNT remove leftover comment in loss module

--- sklearn/_loss/loss.py
+++ sklearn/_loss/loss.py
@@ -538,10 +538,6 @@ class BaseLoss:
         return gradient, hessian
 
 
-# Note: Naturally, we would inherit in the following order
-#         class HalfSquaredError(IdentityLink, CyHalfSquaredError, BaseLoss)
-#       But because of https://github.com/cython/cython/issues/4350 we
-#       set BaseLoss as the last one. This, of course, changes the MRO.
 class HalfSquaredError(BaseLoss):
     """Half squared error with identity link, for regression.
 
