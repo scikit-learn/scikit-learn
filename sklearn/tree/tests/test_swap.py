2a3c58bb61b07b8f0128d6786c2e9be41958b250
Arthur <arthur.lcte@gmail.com>
2026-03-04 15:58:00 +0100

    added edge cases tests for swap slices

--- sklearn/tree/tests/test_swap.py
+++ sklearn/tree/tests/test_swap.py
@@ -20,11 +20,24 @@ def test_py_swap_array_slices_random(dtype, global_random_seed):
     for _ in range(20):
         size = rng.integers(1, 101)
         arr = rng.permutation(size).astype(dtype)
-        split = rng.integers(0, size)
-        start = rng.integers(0, size - split)
-        end = rng.integers(start + split, size)
-        # test the swap of arr[start:start + split] with arr[start + split:end]
-        expected = swap_slices_np(arr, start, end, split)
+        n = rng.integers(0, size)
+        start = rng.integers(0, size - n)
+        end = rng.integers(start + n, size)
+        # test the swap of arr[start:start + n] with arr[start + n:end]
+        expected = swap_slices_np(arr, start, end, n)
 
-        _py_swap_array_slices(arr, start, end, split)
+        _py_swap_array_slices(arr, start, end, n)
         np.testing.assert_array_equal(arr, expected)
+
+    # test some edge cases:
+    size = 30
+    n = 10
+    start = rng.integers(0, size - n)
+    arr = np.arange(size, dtype=dtype)
+    expected = arr.copy()
+    # n == end - start should be no-op:
+    _py_swap_array_slices(arr, start, start + n, n)
+    np.testing.assert_array_equal(arr, expected)
+    # n == 0 should be no-op:
+    _py_swap_array_slices(arr, start, size, 0)
+    np.testing.assert_array_equal(arr, expected)
