feb34592be4a2d733d1a1663965759ac320d25de
Christian Lorentzen <lorentzen.ch@gmail.com>
2026-03-05 08:37:32 +0100

    MNT cleaner tests
    * test_link_inverse_identity only for numpy
    * new test_link_inverse_array_api

--- sklearn/_loss/tests/test_link.py
+++ sklearn/_loss/tests/test_link.py
@@ -64,15 +64,9 @@ def test_is_in_range(interval):
     )
 
 
-@pytest.mark.parametrize(
-    "namespace, device, dtype_name",
-    yield_namespace_device_dtype_combinations(),
-    ids=_get_namespace_device_dtype_ids,
-)
 @pytest.mark.parametrize("link", LINK_FUNCTIONS)
-def test_link_inverse_identity(namespace, device, dtype_name, link, global_random_seed):
+def test_link_inverse_identity(link, global_random_seed):
     """Test that link of inverse gives identity."""
-    xp = _array_api_for_tests(namespace, device)
     rng = np.random.RandomState(global_random_seed)
     link = link()
     n_samples, n_classes = 100, None
@@ -82,31 +76,63 @@ def test_link_inverse_identity(namespace, device, dtype_name, link, global_rando
     if link.is_multiclass:
         n_classes = 10
         raw_prediction = rng.uniform(low=-20, high=20, size=(n_samples, n_classes))
+        if isinstance(link, MultinomialLogit):
+            raw_prediction = link.symmetrize_raw_prediction(raw_prediction)
     elif isinstance(link, HalfLogitLink):
         raw_prediction = rng.uniform(low=-10, high=10, size=(n_samples))
     else:
         raw_prediction = rng.uniform(low=-20, high=20, size=(n_samples))
 
-    if dtype_name == "float32":
+    assert_allclose(link.link(link.inverse(raw_prediction)), raw_prediction)
+    y_pred = link.inverse(raw_prediction)
+    assert_allclose(link.inverse(link.link(y_pred)), y_pred)
+
+
+@pytest.mark.parametrize(
+    "namespace, device, dtype_name",
+    yield_namespace_device_dtype_combinations(),
+    ids=_get_namespace_device_dtype_ids,
+)
+@pytest.mark.parametrize("link", LINK_FUNCTIONS)
+def test_link_inverse_array_api(
+    namespace, device, dtype_name, link, global_random_seed
+):
+    """Test that link and inverse link give same result for array API inputs."""
+    rng = np.random.RandomState(global_random_seed)
+    link = link()
+    n_samples, n_classes = 100, None
+    # The values for `raw_prediction` are limited from -20 to 20 because in the
+    # class `LogitLink` the term `expit(x)` comes very close to 1 for large
+    # positive x and therefore loses precision.
+    if link.is_multiclass:
+        n_classes = 10
+        raw_prediction = rng.uniform(low=-20, high=20, size=(n_samples, n_classes))
+        if isinstance(link, MultinomialLogit):
+            raw_prediction = link.symmetrize_raw_prediction(raw_prediction)
+    elif isinstance(link, HalfLogitLink):
+        raw_prediction = rng.uniform(low=-10, high=10, size=(n_samples))
+    else:
+        raw_prediction = rng.uniform(low=-20, high=20, size=(n_samples))
+
+    xp = _array_api_for_tests(namespace, device)
+    if dtype_name != "float64":
         raw_prediction *= 0.5  # avoid overflow
         rtol = 1e-3 if n_classes else 1e-4
     else:
         rtol = 1e-8
 
     with config_context(array_api_dispatch=True):
-        raw_prediction = xp.asarray(raw_prediction.astype(dtype_name), device=device)
-
-        if isinstance(link, MultinomialLogit):
-            raw_prediction = link.symmetrize_raw_prediction(raw_prediction)
-
+        raw_prediction_xp = xp.asarray(raw_prediction.astype(dtype_name), device=device)
         assert_allclose(
-            _convert_to_numpy(link.link(link.inverse(raw_prediction)), xp=xp),
-            _convert_to_numpy(raw_prediction, xp=xp),
+            _convert_to_numpy(link.inverse(raw_prediction_xp), xp=xp),
+            link.inverse(raw_prediction),
             rtol=rtol,
         )
+
         y_pred = link.inverse(raw_prediction)
+        y_pred_xp = xp.asarray(y_pred.astype(dtype_name), device=device)
         assert_allclose(
-            _convert_to_numpy(link.inverse(link.link(y_pred)), xp=xp),
-            _convert_to_numpy(y_pred, xp=xp),
+            _convert_to_numpy(link.link(y_pred_xp), xp=xp),
+            link.link(y_pred),
             rtol=rtol,
         )
