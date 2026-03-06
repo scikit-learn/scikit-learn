f200ee6846bf96e6edb577eb8e070337fa5e4692
Anne Beyer <anne.beyer@mailbox.org>
2026-03-06 14:21:24 +0100

    reinsert simplification

--- sklearn/inspection/_plot/decision_boundary.py
+++ sklearn/inspection/_plot/decision_boundary.py
@@ -314,7 +314,6 @@ class DecisionBoundaryDisplay:
         """
         check_matplotlib_support("DecisionBoundaryDisplay.plot")
         import matplotlib as mpl
-        import matplotlib.pyplot as plt
 
         if plot_method not in ("contourf", "contour", "pcolormesh"):
             raise ValueError(
@@ -323,7 +322,7 @@ class DecisionBoundaryDisplay:
             )
 
         if ax is None:
-            _, ax = plt.subplots()
+            _, ax = mpl.pyplot.subplots()
 
         plot_func = getattr(ax, plot_method)
         if self.n_classes == 2:
