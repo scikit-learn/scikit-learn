c8376c302524db45d1c5feac9b94fcb535610d33
Dea María Léon <deamarialeon@gmail.com>
2026-03-18 18:41:05 +0100

    testing

--- sklearn/pipeline.py
+++ sklearn/pipeline.py
@@ -2055,8 +2055,8 @@ class FeatureUnion(TransformerMixin, _BaseComposition):
     def _sk_visual_block_(self):
         names, transformers = zip(*self.transformer_list)
         detailed_names = [
-            f"{str(transformer).split('(')[0]} : {name}"
-            for name, transformer in enumerate(names)
+            f"{str(transformers[idx]).split('(')[0]} : {name}"
+            for idx, name in enumerate(names)
         ]
         return _VisualBlock("parallel", transformers, names=detailed_names)
 
