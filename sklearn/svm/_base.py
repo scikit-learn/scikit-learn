41553302a9ea334aa3eebc6d310d77b575dbd66d
Shruti Nath <51656807+snath-xoc@users.noreply.github.com>
2026-04-02 21:23:40 +0100

    Update sklearn/svm/_base.py
    Co-authored-by: Jérémie du Boisberranger <jeremie@probabl.ai>

--- sklearn/svm/_base.py
+++ sklearn/svm/_base.py
@@ -1037,8 +1037,7 @@ class BaseSVC(ClassifierMixin, BaseLibSVM, metaclass=ABCMeta):
         "Attribute `probB_` was deprecated in version 1.9 and will be removed in "
         "1.11 as the `probability=True` option for SVC and NuSVC was deprecated "
         "and will be removed in 1.11."
-    )
-    # type: ignore[prop-decorator]
+    )  # type: ignore[prop-decorator]
     @property
     def probB_(self):
         """Parameter learned in Platt scaling when `probability=True`.
