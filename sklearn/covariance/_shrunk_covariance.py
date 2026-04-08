1255b62ef07df82e15fb48f077f6c32a9d86ffc7
Tim Head <betatim@gmail.com>
2026-04-08 10:21:44 +0200

    Prefer X@Y over np.dot(X, Y)

--- sklearn/covariance/_shrunk_covariance.py
+++ sklearn/covariance/_shrunk_covariance.py
@@ -384,22 +384,20 @@ def ledoit_wolf_shrinkage(X, assume_centered=False, block_size=1000):
             for j in range(n_splits):
                 rows = slice(block_size * i, block_size * (i + 1))
                 cols = slice(block_size * j, block_size * (j + 1))
-                beta_ += np.sum(np.dot(X2.T[rows], X2[:, cols]))
-                delta_ += np.sum(np.dot(X.T[rows], X[:, cols]) ** 2)
+                beta_ += np.sum(X2.T[rows] @ X2[:, cols])
+                delta_ += np.sum((X.T[rows] @ X[:, cols]) ** 2)
             rows = slice(block_size * i, block_size * (i + 1))
-            beta_ += np.sum(np.dot(X2.T[rows], X2[:, block_size * n_splits :]))
-            delta_ += np.sum(np.dot(X.T[rows], X[:, block_size * n_splits :]) ** 2)
+            beta_ += np.sum(X2.T[rows] @ X2[:, block_size * n_splits :])
+            delta_ += np.sum((X.T[rows] @ X[:, block_size * n_splits :]) ** 2)
         for j in range(n_splits):
             cols = slice(block_size * j, block_size * (j + 1))
-            beta_ += np.sum(np.dot(X2.T[block_size * n_splits :], X2[:, cols]))
-            delta_ += np.sum(np.dot(X.T[block_size * n_splits :], X[:, cols]) ** 2)
+            beta_ += np.sum(X2.T[block_size * n_splits :] @ X2[:, cols])
+            delta_ += np.sum((X.T[block_size * n_splits :] @ X[:, cols]) ** 2)
         delta_ += np.sum(
-            np.dot(X.T[block_size * n_splits :], X[:, block_size * n_splits :]) ** 2
+            (X.T[block_size * n_splits :] @ X[:, block_size * n_splits :]) ** 2
         )
         delta_ /= n_samples**2
-        beta_ += np.sum(
-            np.dot(X2.T[block_size * n_splits :], X2[:, block_size * n_splits :])
-        )
+        beta_ += np.sum(X2.T[block_size * n_splits :] @ X2[:, block_size * n_splits :])
     else:
         # Non-blocked computation: GPU-friendly (single large matmul)
         beta_ = float(xp.sum(X2.T @ X2))
