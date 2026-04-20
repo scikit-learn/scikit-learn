90e1230527d88486689ef48f8f6e2f637990ca10
Bru <b.aristimunha@gmail.com>
2026-04-20 16:32:29 +0200

    Update array_api_extra to use correction kwarg for OAS PR
    Vendor array_api_extra from bruAristimunha/array-api-extra#690 which
    renames the cov() kwarg from 'bias' to 'correction'. Rename the
    changelog entry to 33600 (Bru's PR number) and update its content to
    mention only OAS (since EmpiricalCovariance support was reverted in
    Tim's branch).

--- sklearn/externals/array_api_extra/__init__.py
+++ sklearn/externals/array_api_extra/__init__.py
@@ -1,28 +1,39 @@
 """Extra array functions built on top of the array API standard."""
 
-from ._delegation import isclose, nan_to_num, one_hot, pad
+from ._delegation import (
+    argpartition,
+    atleast_nd,
+    cov,
+    create_diagonal,
+    expand_dims,
+    isclose,
+    isin,
+    nan_to_num,
+    one_hot,
+    pad,
+    partition,
+    searchsorted,
+    setdiff1d,
+    sinc,
+    union1d,
+)
 from ._lib._at import at
 from ._lib._funcs import (
     apply_where,
-    atleast_nd,
     broadcast_shapes,
-    cov,
-    create_diagonal,
     default_dtype,
-    expand_dims,
     kron,
     nunique,
-    setdiff1d,
-    sinc,
 )
 from ._lib._lazy import lazy_apply
 
-__version__ = "0.8.2"
+__version__ = "0.10.2.dev0"
 
 # pylint: disable=duplicate-code
 __all__ = [
     "__version__",
     "apply_where",
+    "argpartition",
     "at",
     "atleast_nd",
     "broadcast_shapes",
@@ -31,12 +42,16 @@ __all__ = [
     "default_dtype",
     "expand_dims",
     "isclose",
+    "isin",
     "kron",
     "lazy_apply",
     "nan_to_num",
     "nunique",
     "one_hot",
     "pad",
+    "partition",
+    "searchsorted",
     "setdiff1d",
     "sinc",
+    "union1d",
 ]
