fc52dfb01aaf387225ce2296c5b492ee1b21509a
Dea María Léon <deamarialeon@gmail.com>
2026-04-21 18:48:42 +0200

    [doc build] removed border - made it wider

--- sklearn/utils/_repr_html/estimator.py
+++ sklearn/utils/_repr_html/estimator.py
@@ -540,6 +540,7 @@ def estimator_html_repr(estimator):
         html_template = (
             f"<style>{_CSS_STYLE}</style>"
             f"<body>"
+            # we need tabindex="0" to make it 'focusable'
             f'<div id="{container_id}" tabindex="0" class="sk-top-container sk-global">'
             '<div class="sk-text-repr-fallback">'
             f"<pre>{html.escape(estimator_str)}</pre><b>{fallback_msg}</b>"
