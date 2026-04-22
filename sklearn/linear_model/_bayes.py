ad94a906954a60db0a0231782ef883cc19f93305
armorbreak001 <contact@agentvote.cc>
2026-04-22 12:09:48 +0800

    fix(linear_model): center test features in BayesianRidge/ARDRegression predict variance
    When fit_intercept=True (default), fit() centers features before computing
    the posterior covariance sigma_. But predict(return_std=True) was using
    raw (uncentered) X for variance calculation, causing incorrect uncertainty
    estimation -- lowest variance at origin instead of near training data center.
    
    Center X by X_offset_ before computing (X @ sigma_ * X).sum() for both
    BayesianRidge.predict and ARDRegression.predict.

--- sklearn/linear_model/_bayes.py
+++ sklearn/linear_model/_bayes.py
@@ -397,6 +397,8 @@ class BayesianRidge(RegressorMixin, LinearModel):
         if not return_std:
             return y_mean
         else:
+            if self.fit_intercept:
+                X = X - self.X_offset_
             sigmas_squared_data = (np.dot(X, self.sigma_) * X).sum(axis=1)
             y_std = np.sqrt(sigmas_squared_data + (1.0 / self.alpha_))
             return y_mean, y_std
@@ -817,6 +819,8 @@ class ARDRegression(RegressorMixin, LinearModel):
         if return_std is False:
             return y_mean
         else:
+            if self.fit_intercept:
+                X = X - self.X_offset_
             col_index = self.lambda_ < self.threshold_lambda
             X = _safe_indexing(X, indices=col_index, axis=1)
             sigmas_squared_data = (np.dot(X, self.sigma_) * X).sum(axis=1)
