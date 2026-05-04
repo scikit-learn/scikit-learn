f14e5cdb3e1b8b1290f66bee90190298916135e5
Lucy Liu <jliu176@gmail.com>
2026-05-04 16:35:18 +1000

    add msg to common test, test multioutput

--- sklearn/metrics/tests/test_common.py
+++ sklearn/metrics/tests/test_common.py
@@ -2511,16 +2511,18 @@ def test_array_api_compliance(
     check_func(metric, array_namespace, device_name, dtype_name)
 
 
-def _check_output(out_np, out_xp, xp_to, y2_xp):
+def _check_output(out_np, out_xp, xp_to, y2_xp, msg=""):
     if isinstance(out_np, float):
-        assert isinstance(out_xp, float)
+        assert isinstance(out_xp, float), f"{msg} Type mismatch - expected float."
     elif hasattr(out_np, "shape"):
-        assert hasattr(out_xp, "shape")
-        assert get_namespace(out_xp)[0] == xp_to
-        assert array_api_device(out_xp) == array_api_device(y2_xp)
+        assert hasattr(out_xp, "shape"), f"{msg} Type mismatch - expected array-like."
+        assert get_namespace(out_xp)[0] == xp_to, f"{msg} Namespace mismatch."
+        assert array_api_device(out_xp) == array_api_device(y2_xp), (
+            f"{msg} Device mismatch."
+        )
     # `classification_report` returns str (with default `output_dict=False`)
     elif isinstance(out_np, str):
-        assert isinstance(out_xp, str)
+        assert isinstance(out_xp, str), f"{msg} Type mismatch - expected string."
 
 
 @pytest.mark.parametrize(
@@ -2559,8 +2561,13 @@ def test_mixed_array_api_namespace_input_compliance(
         "label_indicator_continuous": ([[1, 0, 1, 0]], [[0.5, 0.2, 0.7, 0.6]]),
         "regression_integer": ([2, 1, 3, 4], [2, 1, 2, 2]),
         "regression_continuous": ([2.1, 1.0, 3.0, 4.0], [2.2, 1.1, 2.0, 2.0]),
+        "regression_multioutput": (
+            [[1, 0, 0, 1], [0, 1, 1, 1], [1, 1, 0, 1]],
+            [[0, 0, 0, 1], [1, 0, 1, 1], [0, 0, 0, 1]],
+        ),
     }
     sample_weight = [1, 1, 2, 2]
+    multioutput = [0.2, 0.5, 0.1, 0.2]
 
     # Deal with max mps float precision being float32
     def _get_dtype(data, xp, device):
@@ -2582,7 +2589,11 @@ def test_mixed_array_api_namespace_input_compliance(
         else:
             data_cases = ["label_indicator_continuous"]
     elif metric_name in REGRESSION_METRICS:
-        data_cases = ["regression_integer", "regression_continuous"]
+        data_cases = [
+            "regression_integer",
+            "regression_continuous",
+            "regression_multioutput",
+        ]
 
     with config_context(array_api_dispatch=True):
         for data_case in data_cases:
@@ -2600,6 +2611,15 @@ def test_mixed_array_api_namespace_input_compliance(
                     sample_weight_np, device=device_other
                 )
                 metric_kwargs_xp = {"sample_weight": sample_weight_xp}
+            if (
+                data_case == "regression_multioutput"
+                and metric_name in MULTIOUTPUT_METRICS
+            ):
+                # use `other_ns_and_device` for `multioutput` as well
+                multioutput_np = np.array(multioutput)
+                metric_kwargs_np = {"multioutput": multioutput_np}
+                multioutput_xp = xp_other.asarray(multioutput_np, device=device_other)
+                metric_kwargs_xp = {"multioutput": multioutput_xp}
 
             dtype = _get_dtype(y2, xp_y_pred, device_y_pred)
             y2_xp = xp_y_pred.asarray(y2, device=device_y_pred, dtype=dtype)
@@ -2607,11 +2627,15 @@ def test_mixed_array_api_namespace_input_compliance(
             metric_xp = metric(y1_xp, y2_xp, **metric_kwargs_xp)
             metric_np = metric(y1, y2, **metric_kwargs_np)
 
+            msg = (
+                f"Output incorrect for mixed array input to metric, for {data_case} "
+                "case."
+            )
             if isinstance(metric_np, Tuple):
                 for out_np, out_xp in zip(metric_np, metric_xp):
-                    _check_output(out_np, out_xp, xp_y_pred, y2_xp)
+                    _check_output(out_np, out_xp, xp_y_pred, y2_xp, msg=msg)
             else:
-                _check_output(metric_np, metric_xp, xp_y_pred, y2_xp)
+                _check_output(metric_np, metric_xp, xp_y_pred, y2_xp, msg=msg)
 
 
 # Check thresholded classification metrics, minus multilabel ranking metrics
@@ -2693,11 +2717,15 @@ def test_array_api_classification_mixed_string_numeric_input(
         metric_np = metric(y_true, y_prob_np, **kwargs)
         metric_xp = metric(y_true, y_prob_xp, **kwargs)
 
+        msg = (
+            "Output incorrect for string y_true and numeric y_prob input to "
+            "classification metric, for binary case."
+        )
         if isinstance(metric_np, Tuple):
             for out_np, out_xp in zip(metric_np, metric_xp):
-                _check_output(out_np, out_xp, xp, y_prob_xp)
+                _check_output(out_np, out_xp, xp, y_prob_xp, msg=msg)
         else:
-            _check_output(metric_np, metric_xp, xp, y_prob_xp)
+            _check_output(metric_np, metric_xp, xp, y_prob_xp, msg=msg)
 
     # Multiclass
     if metric_name not in METRIC_UNDEFINED_MULTICLASS:
@@ -2717,11 +2745,15 @@ def test_array_api_classification_mixed_string_numeric_input(
             metric_np = metric(y_true, y_prob_np)
             metric_xp = metric(y_true, y_prob_xp)
 
+            msg = (
+                "Output incorrect for string y_true and numeric y_prob input to "
+                "classification metric, for multiclass case."
+            )
             if isinstance(metric_np, Tuple):
                 for out_np, out_xp in zip(metric_np, metric_xp):
-                    _check_output(out_np, out_xp, xp, y_prob_xp)
+                    _check_output(out_np, out_xp, xp, y_prob_xp, msg=msg)
             else:
-                _check_output(metric_np, metric_xp, xp, y_prob_xp)
+                _check_output(metric_np, metric_xp, xp, y_prob_xp, msg=msg)
 
 
 @pytest.mark.parametrize("df_lib_name", ["pandas", "polars"])
