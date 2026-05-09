90f123c72e6bdd83ed088262e91e50ad30b26246
Christian Lorentzen <lorentzen.ch@gmail.com>
2026-05-09 13:57:24 +0200

    MNT move bitset from HGBT to utils

--- sklearn/utils/validation.py
+++ sklearn/utils/validation.py
@@ -42,6 +42,10 @@ from sklearn.utils.fixes import (
 FLOAT_DTYPES = (np.float64, np.float32, np.float16)
 
 
+def _nw_is_into_df_or_series(X):
+    return nw.dependencies.is_into_dataframe(X) or nw.dependencies.is_into_series(X)
+
+
 # This function is not used anymore at this moment in the code base but we keep it in
 # case that we merge a new public function without kwarg only by mistake, which would
 # require a deprecation cycle to fix.
@@ -377,7 +381,7 @@ def _num_samples(x):
         if isinstance(x.shape[0], numbers.Integral):
             return x.shape[0]
 
-    if nw.dependencies.is_into_dataframe(x) or nw.dependencies.is_into_series(x):
+    if _nw_is_into_df_or_series(x):
         return nw.from_native(x).shape[0]
 
     if not hasattr(x, "__len__") and not hasattr(x, "shape"):
@@ -471,7 +475,7 @@ def _make_indexable(iterable):
     """
     if sp.issparse(iterable):
         return iterable.tocsr()
-    elif hasattr(iterable, "__getitem__") or hasattr(iterable, "iloc"):
+    elif hasattr(iterable, "__getitem__") or _nw_is_into_df_or_series(iterable):
         return iterable
     elif iterable is None:
         return iterable
