9ce28616ef5aca5aa97b420eee6659adee287ca3
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-05-11 21:23:51 +0200

    CausalFeatureImportance class

--- sklearn/causal/_causal_importance.py
+++ sklearn/causal/_causal_importance.py
@@ -1,2 +1,9 @@
 import numpy as np
 from utils import clone_model
+
+class CausalFeatureImportance:
+    def __init__(self,model,treatment,control,label):
+        self.model=model
+        self.treatment=treatment
+        self.control=control
+        self.label=label
