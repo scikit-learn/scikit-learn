6137d6bec8d8b450cc62f146e0c0a7805a55686d
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-05-11 21:22:29 +0200

    created the Causal Feature Importance class for regression models
    ->inherits all the methods and properties from the CausalFeatureImportance class

--- sklearn/causal/_causal_importance_regression.py
+++ sklearn/causal/_causal_importance_regression.py
@@ -1,1 +1,6 @@
+from ._causal_importance import CausalFeatureImportance
 
+class CausalFeatureImportanceRegression(CausalFeatureImportance):
+    def __init__(self,model,treatment,control,label):
+        super().__init__(model,treatment,control,label)
+        
