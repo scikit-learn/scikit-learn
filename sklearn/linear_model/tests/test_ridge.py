96e10d0cccbc05a1076c49dd6da1bd64b53fd338
tommoral <thomas.moreau.2010@gmail.com>
2026-05-12 18:27:16 +0200

    FIX also work when passing numpy array for alphas

--- sklearn/linear_model/tests/test_ridge.py
+++ sklearn/linear_model/tests/test_ridge.py
@@ -1511,17 +1511,28 @@ def test_ridge_per_target_alpha_array_api(
     array_namespace, device_name, dtype_name
 ):
     xp, device = _array_api_for_tests(array_namespace, device_name, dtype_name)
-    X, y = make_regression(n_targets=2, random_state=0)
+    X, y = make_regression(
+        n_targets=3, n_features=10, random_state=0
+    )
     X_np = X.astype(dtype_name)
     y_np = y.astype(dtype_name)
-    alphas = np.asarray([0.1, 1.0], dtype=dtype_name)
+    alphas = np.asarray([1e-2, 0.1, 1.0], dtype=dtype_name)
     estimator = Ridge(alpha=alphas)
 
     ridge_np = estimator.fit(X_np, y_np)
     pred_np = ridge_np.predict(X_np)
     with config_context(array_api_dispatch=True):
-        estimator = Ridge(alpha=xp.asarray(alphas))
         X_xp, y_xp = xp.asarray(X_np, device=device), xp.asarray(y_np, device=device)
+
+        # check that passing a numpy array for alpha works as expected
+        estimator = Ridge(alpha=alphas)
+        ridge_xp = estimator.fit(X_xp, y_xp)
+        pred_xp = ridge_xp.predict(X_xp)
+        assert pred_xp.shape == pred_np.shape == y.shape
+        assert_allclose(move_to(pred_xp, xp=np, device="cpu"), pred_np)
+
+        # check that passing the same array type for alpha also works as expected
+        estimator = Ridge(alpha=xp.asarray(alphas))
         ridge_xp = estimator.fit(X_xp, y_xp)
         pred_xp = ridge_xp.predict(X_xp)
         assert pred_xp.shape == pred_np.shape == y.shape
