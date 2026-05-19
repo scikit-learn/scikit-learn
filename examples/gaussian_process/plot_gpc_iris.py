a5cca240ab9350cbf79b40015b8356ff3724e6f2
Matt Van Horn <455140+mvanhorn@users.noreply.github.com>
2026-05-19 15:46:20 -0700

    DOC Use DecisionBoundaryDisplay in plot_gpc_iris example
    Fixes #33980

--- examples/gaussian_process/plot_gpc_iris.py
+++ examples/gaussian_process/plot_gpc_iris.py
@@ -3,8 +3,9 @@
 Gaussian process classification (GPC) on iris dataset
 =====================================================
 
-This example illustrates the predicted probability of GPC for an isotropic
-and anisotropic RBF kernel on a two-dimensional version for the iris-dataset.
+This example illustrates the maximum predicted class probability of GPC for an
+isotropic and anisotropic RBF kernel on a two-dimensional version for the
+iris-dataset.
 The anisotropic RBF kernel obtains slightly higher log-marginal-likelihood by
 assigning different length-scales to the two feature dimensions.
 
@@ -19,47 +20,45 @@ import numpy as np
 from sklearn import datasets
 from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.gaussian_process.kernels import RBF
+from sklearn.inspection import DecisionBoundaryDisplay
 
 # import some data to play with
 iris = datasets.load_iris()
 X = iris.data[:, :2]  # we only take the first two features.
 y = np.array(iris.target, dtype=int)
 
-h = 0.02  # step size in the mesh
-
 kernel = 1.0 * RBF([1.0])
 gpc_rbf_isotropic = GaussianProcessClassifier(kernel=kernel).fit(X, y)
 kernel = 1.0 * RBF([1.0, 1.0])
 gpc_rbf_anisotropic = GaussianProcessClassifier(kernel=kernel).fit(X, y)
 
-# create a mesh to plot in
 x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
 y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
 
 titles = ["Isotropic RBF", "Anisotropic RBF"]
 plt.figure(figsize=(10, 5))
 for i, clf in enumerate((gpc_rbf_isotropic, gpc_rbf_anisotropic)):
-    # Plot the predicted probabilities. For that, we will assign a color to
-    # each point in the mesh [x_min, m_max]x[y_min, y_max].
-    plt.subplot(1, 2, i + 1)
-
-    Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])
-
-    # Put the result into a color plot
-    Z = Z.reshape((xx.shape[0], xx.shape[1], 3))
-    plt.imshow(Z, extent=(x_min, x_max, y_min, y_max), origin="lower")
+    ax = plt.subplot(1, 2, i + 1)
+    DecisionBoundaryDisplay.from_estimator(
+        clf,
+        X,
+        response_method="predict_proba",
+        plot_method="contourf",
+        multiclass_colors="viridis",
+        alpha=0.7,
+        ax=ax,
+        xlabel="Sepal length",
+        ylabel="Sepal width",
+    )
 
     # Plot also the training points
-    plt.scatter(X[:, 0], X[:, 1], c=np.array(["r", "g", "b"])[y], edgecolors=(0, 0, 0))
-    plt.xlabel("Sepal length")
-    plt.ylabel("Sepal width")
-    plt.xlim(xx.min(), xx.max())
-    plt.ylim(yy.min(), yy.max())
-    plt.xticks(())
-    plt.yticks(())
-    plt.title(
-        "%s, LML: %.3f" % (titles[i], clf.log_marginal_likelihood(clf.kernel_.theta))
+    ax.scatter(X[:, 0], X[:, 1], c=np.array(["r", "g", "b"])[y], edgecolors=(0, 0, 0))
+    ax.set_xlim(x_min, x_max)
+    ax.set_ylim(y_min, y_max)
+    ax.set_xticks(())
+    ax.set_yticks(())
+    ax.set_title(
+        f"{titles[i]}, LML: {clf.log_marginal_likelihood(clf.kernel_.theta):.3f}"
     )
 
 plt.tight_layout()
