4f9eac047c1b2ae48de8762bf330b62f26e23999
Shruti Nath <51656807+snath-xoc@users.noreply.github.com>
2026-05-22 12:43:51 +0200

    Update sklearn/metrics/pairwise.py
    Co-authored-by: Olivier Grisel <olivier.grisel@ensta.org>

--- sklearn/metrics/pairwise.py
+++ sklearn/metrics/pairwise.py
@@ -1996,7 +1996,6 @@ def _parallel_pairwise(X, Y, func, n_jobs, **kwds):
         for s in gen_even_slices(_num_samples(Y), effective_n_jobs(n_jobs))
     )
     for slice_, chunk in chunk_generator:
-        print(f"Writing chunk for slice {slice_} to output array.")
         ret[slice_, ...] = chunk
 
     if (X is Y or Y is None) and func is euclidean_distances:
