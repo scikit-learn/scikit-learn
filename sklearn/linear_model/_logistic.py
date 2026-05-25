852d1a3966b4ba9808c13da3fb287ec8e32199a3
Jérémie du Boisberranger <jeremie@probabl.ai>
2026-05-25 12:08:11 +0200

    consistency

--- sklearn/linear_model/_logistic.py
+++ sklearn/linear_model/_logistic.py
@@ -1178,8 +1178,7 @@ class LogisticRegression(
     classes_ : ndarray of shape (n_classes, )
         A list of class labels known to the classifier.
 
-    coef_ : ndarray or CSR array/matrix of shape \
-            (1, n_features) or (n_classes, n_features)
+    coef_ : ndarray or CSR matrix of shape (1, n_features) or (n_classes, n_features)
         Coefficients of the features in the decision function.
 
         `coef_` is of shape (1, n_features) when the given problem is binary.
