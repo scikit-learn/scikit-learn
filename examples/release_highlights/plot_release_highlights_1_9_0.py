0347ae0bc6ca55cfb3f75d07b53360c3b5ac3ac0
Jérémie du Boisberranger <jeremie@probabl.ai>
2026-05-28 17:03:10 +0200

    wip

---
+++ examples/release_highlights/plot_release_highlights_1_9_0.py
@@ -0,0 +1,34 @@
+# ruff: noqa: CPY001
+"""
+=======================================
+Release Highlights for scikit-learn 1.9
+=======================================
+
+.. currentmodule:: sklearn
+
+We are pleased to announce the release of scikit-learn 1.9! Many bug fixes
+and improvements were added, as well as some key new features. Below we
+detail the highlights of this release. **For an exhaustive list of
+all the changes**, please refer to the :ref:`release notes <release_notes_1_9>`.
+
+To install the latest version (with pip)::
+
+    pip install --upgrade scikit-learn
+
+or with conda::
+
+    conda install -c conda-forge scikit-learn
+
+"""
+
+# %%
+# Callbacks
+# ---------
+
+# %%
+# metric_at_thresholds
+# --------------------
+#
+# %%
+# Output features and fitted attributes in HTML diagrams
+# ------------------------------------------------------
