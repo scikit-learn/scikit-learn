aef8aba02e47e72cf59fbae0a65773a1c9d6a7be
Itamar Turner-Trauring <itamar@itamarst.org>
2026-06-02 11:12:33 -0400

    Windows support

--- sklearn/neighbors/binarytree.h
+++ sklearn/neighbors/binarytree.h
@@ -1,2 +1,7 @@
-// TODO need variants for Windows etc
+#if defined(_MSC_VER)
+// MS compiler doesn't do standardized _Thread_local until very recent versions:
+#define tls_int __declspec(thread) int
+#else
+// _Thread_local is available in C11 and later:
 #define tls_int _Thread_local int
+#endif
