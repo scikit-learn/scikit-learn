98f71e0ab318cbd1a87038b9f80419db0332f9dd
Itamar Turner-Trauring <itamar@itamarst.org>
2026-06-04 15:38:14 -0400

    Inline the .h file.

--- sklearn/neighbors/binarytree.h
+++
@@ -1,7 +0,0 @@
-#if defined(_MSC_VER)
-// MS compiler doesn't do standardized _Thread_local until very recent versions:
-#define tls_int __declspec(thread) int
-#else
-// _Thread_local is available in C11 and later:
-#define tls_int _Thread_local int
-#endif
