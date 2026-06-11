ebb32433c435ac1fc3852ee72ea126bc4b310fc7
Guillaume Lemaitre <guillaume@probabl.ai>
2026-06-11 16:21:38 +0200

    API Move IterativeImputer outside from experimental (#34214)
    Co-authored-by: Stefanie Senger <91849487+StefanieSenger@users.noreply.github.com>
    Co-authored-by: Gael Varoquaux <gael.varoquaux@normalesup.org>

--- sklearn/tests/test_common.py
+++ sklearn/tests/test_common.py
@@ -21,10 +21,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.exceptions import ConvergenceWarning
 
 # make it possible to discover experimental estimators when calling `all_estimators`
-from sklearn.experimental import (
-    enable_halving_search_cv,  # noqa: F401
-    enable_iterative_imputer,  # noqa: F401
-)
+from sklearn.experimental import enable_halving_search_cv  # noqa: F401
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import FeatureUnion, make_pipeline
 from sklearn.preprocessing import (
@@ -129,6 +126,10 @@ def test_estimators(estimator, check, request):
 @pytest.mark.filterwarnings(
     "ignore:The sklearn.experimental.enable_hist_gradient_boosting module is "
     "deprecated:FutureWarning"
+)
+@pytest.mark.filterwarnings(
+    "ignore:Since version 1.10, it is not needed to import "
+    "enable_iterative_imputer anymore"
 )
 @pytest.mark.thread_unsafe  # import side-effects
 def test_import_all_consistency():
