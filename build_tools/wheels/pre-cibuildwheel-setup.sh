7e54b7ac61d54bd5fb7f789cbb8af01066764974
Loïc Estève <loic.esteve@ymail.com>
2026-06-11 14:56:40 +0200

    [cd build] Setting MACOSX_DEPLOYMENT_TARGET may just work since on macOS cibuildwheel does not use a container

--- build_tools/wheels/pre-cibuildwheel-setup.sh
+++ build_tools/wheels/pre-cibuildwheel-setup.sh
@@ -5,10 +5,15 @@ if [[ $(uname) == "Darwin" ]]; then
     # Make sure to use a libomp version binary compatible with the oldest
     # supported version of the macos SDK as libomp will be vendored into the
     # scikit-learn wheels for macos.
-    # MACOSX_DEPLOYMENT_TARGET is defined in pyproject.toml
+
     if [[ $(uname -m) == "arm64" ]]; then
+        # SciPy requires 12.0 on arm to prevent kernel panics
+        # https://github.com/scipy/scipy/issues/14688
+        # We use the same deployment target to match SciPy.
+        echo MACOSX_DEPLOYMENT_TARGET=12.0 >> "$GITHUB_ENV"
         OPENMP_URL="https://anaconda.org/conda-forge/llvm-openmp/11.1.0/download/osx-arm64/llvm-openmp-11.1.0-hf3c4609_1.tar.bz2"
     else
+        echo MACOSX_DEPLOYMENT_TARGET=10.9 >> "$GITHUB_ENV"
         OPENMP_URL="https://anaconda.org/conda-forge/llvm-openmp/11.1.0/download/osx-64/llvm-openmp-11.1.0-hda6cdc1_1.tar.bz2"
     fi
 
