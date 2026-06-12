ab5d33c27650c0344282e4c8a4b10f9c2d4c9ec6
Jérémie du Boisberranger <jeremie@probabl.ai>
2026-06-12 14:14:39 +0200

    fix

--- sklearn/utils/tests/test_pprint.py
+++ sklearn/utils/tests/test_pprint.py
@@ -304,7 +304,7 @@ Pipeline(memory=None,
                                     multi_class='warn', n_jobs=None,
                                     random_state=None, solver='warn',
                                     tol=0.0001, verbose=0, warm_start=False))],
-         transform_input=("X_val",), verbose=False)"""
+         transform_input=('X_val',), verbose=False)"""
 
     expected = expected[1:]  # remove first \n
     assert pipeline.__repr__() == expected
