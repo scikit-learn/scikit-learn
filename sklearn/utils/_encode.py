ce43c3f8271322f7fae0face7519f2106872db64
Arthur <arthur.lcte@gmail.com>
2026-07-08 21:02:11 +0200

    started refactoring

--- sklearn/utils/_encode.py
+++ sklearn/utils/_encode.py
@@ -8,7 +8,7 @@ from typing import NamedTuple
 
 import numpy as np
 
-from sklearn.utils._array_api import _isin, device, get_namespace, xpx
+from sklearn.utils._array_api import device, get_namespace
 from sklearn.utils._missing import is_scalar_nan
 
 
@@ -167,7 +167,7 @@ class _nandict(dict):
     def __missing__(self, key):
         if hasattr(self, "nan_value") and is_scalar_nan(key):
             return self.nan_value
-        raise KeyError(key)
+        return -1
 
 
 def _map_to_integer(values, uniques):
@@ -203,7 +203,7 @@ def _unique_python(values, *, return_inverse, return_counts):
     return ret[0] if len(ret) == 1 else ret
 
 
-def _encode(values, *, uniques, check_unknown=True):
+def _encode_labels(values, *, uniques):
     """Helper function to encode values into [0, n_uniques - 1].
 
     Uses pure python method for object dtype, and numpy method for
@@ -220,12 +220,35 @@ def _encode(values, *, uniques, check_unknown=True):
     uniques : ndarray
         The unique values in `values`. If the dtype is not object, then
         `uniques` needs to be sorted.
-    check_unknown : bool, default=True
-        If True, check for values in `values` that are not in `unique`
-        and raise an error. This is ignored for object dtype, and treated as
-        True in this case. This parameter is useful for
-        _BaseEncoder._transform() to avoid calling _check_unknown()
-        twice.
+
+    Returns
+    -------
+    encoded : ndarray
+        Encoded values
+    """
+    encoded, diff = _encode(values, uniques=uniques, return_diff=True)
+    if diff.size:
+        raise ValueError(f"y contains previously unseen labels: {diff}")
+    return encoded
+
+
+def _encode(values, *, uniques, return_diff=True):
+    """Helper function to encode values into [0, n_uniques - 1].
+
+    Uses pure python method for object dtype, and numpy method for
+    all other dtypes.
+    The numpy method has the limitation that the `uniques` need to
+    be sorted. Importantly, this is not checked but assumed to already be
+    the case. The calling method needs to ensure this for all non-object
+    values.
+
+    Parameters
+    ----------
+    values : ndarray
+        Values to encode.
+    uniques : ndarray
+        The unique values in `values`. If the dtype is not object, then
+        `uniques` needs to be sorted.
 
     Returns
     -------
@@ -234,100 +257,18 @@ def _encode(values, *, uniques, check_unknown=True):
     """
     xp, _ = get_namespace(values, uniques)
     if not xp.isdtype(values.dtype, "numeric"):
-        try:
-            return _map_to_integer(values, uniques)
-        except KeyError as e:
-            raise ValueError(f"y contains previously unseen labels: {e}")
+        encoded = _map_to_integer(values, uniques)
     else:
-        if check_unknown:
-            diff = _check_unknown(values, uniques)
-            if diff:
-                raise ValueError(f"y contains previously unseen labels: {diff}")
-        return xp.searchsorted(uniques, values)
+        encoded = xp.searchsorted(uniques, values)
+        # TODO: handle edge cases (probably)
+        # borders, NaN, ...
+        encoded[uniques[encoded] != values] = -1
 
+    if return_diff:
+        diff = _unique(values[encoded == -1])
+        return encoded, diff
 
-def _check_unknown(values, known_values, return_mask=False):
-    """
-    Helper function to check for unknowns in values to be encoded.
-
-    Uses pure python method for object dtype, and numpy method for
-    all other dtypes.
-
-    Parameters
-    ----------
-    values : array
-        Values to check for unknowns.
-    known_values : array
-        Known values. Must be unique.
-    return_mask : bool, default=False
-        If True, return a mask of the same shape as `values` indicating
-        the valid values.
-
-    Returns
-    -------
-    diff : list
-        The unique values present in `values` and not in `know_values`.
-    valid_mask : boolean array
-        Additionally returned if ``return_mask=True``.
-
-    """
-    xp, _ = get_namespace(values, known_values)
-    valid_mask = None
-
-    if not xp.isdtype(values.dtype, "numeric"):
-        values_set = set(values)
-        values_set, missing_in_values = _extract_missing(values_set)
-
-        uniques_set = set(known_values)
-        uniques_set, missing_in_uniques = _extract_missing(uniques_set)
-        diff = values_set - uniques_set
-
-        nan_in_diff = missing_in_values.nan and not missing_in_uniques.nan
-        none_in_diff = missing_in_values.none and not missing_in_uniques.none
-
-        def is_valid(value):
-            return (
-                value in uniques_set
-                or (missing_in_uniques.none and value is None)
-                or (missing_in_uniques.nan and is_scalar_nan(value))
-            )
-
-        if return_mask:
-            if diff or nan_in_diff or none_in_diff:
-                valid_mask = xp.array([is_valid(value) for value in values])
-            else:
-                valid_mask = xp.ones(len(values), dtype=xp.bool)
-
-        diff = list(diff)
-        if none_in_diff:
-            diff.append(None)
-        if nan_in_diff:
-            diff.append(np.nan)
-    else:
-        unique_values = xp.unique_values(values)
-        diff = xpx.setdiff1d(unique_values, known_values, assume_unique=True, xp=xp)
-        if return_mask:
-            if diff.size:
-                valid_mask = _isin(values, known_values, xp)
-            else:
-                valid_mask = xp.ones(len(values), dtype=xp.bool)
-
-        # check for nans in the known_values
-        if xp.any(xp.isnan(known_values)):
-            diff_is_nan = xp.isnan(diff)
-            if xp.any(diff_is_nan):
-                # removes nan from valid_mask
-                if diff.size and return_mask:
-                    is_nan = xp.isnan(values)
-                    valid_mask[is_nan] = 1
-
-                # remove nan from diff
-                diff = diff[~diff_is_nan]
-        diff = list(diff)
-
-    if return_mask:
-        return diff, valid_mask
-    return diff
+    return encoded
 
 
 def _get_counts(values, uniques, nan_values=(np.nan,)):
