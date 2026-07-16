6099369f4435f8ba26ba58cf56048ff24aed6eea
Kiyarash  Fazeli <caci96@gmail.com>
2026-07-16 09:48:22 +0330

    FEA Add array API support for matthews_corrcoef (#34423)
    Co-authored-by: Olivier Grisel <olivier.grisel@ensta.org>

--- sklearn/metrics/tests/test_common.py
+++ sklearn/metrics/tests/test_common.py
@@ -2532,6 +2532,10 @@ array_api_metric_checkers = {
         check_array_api_multiclass_classification_metric,
         check_array_api_multilabel_classification_metric,
     ],
+    matthews_corrcoef: [
+        check_array_api_binary_classification_metric,
+        check_array_api_multiclass_classification_metric,
+    ],
     multilabel_confusion_matrix: [
         check_array_api_binary_classification_metric,
         check_array_api_multiclass_classification_metric,
