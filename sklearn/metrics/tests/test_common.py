fc839f78fe1635c3de8cbafdaea36bf95b147ecb
Lucy Liu <jliu176@gmail.com>
2026-07-17 14:55:04 +1000

    fix max dtype padded pr

--- sklearn/metrics/tests/test_common.py
+++ sklearn/metrics/tests/test_common.py
@@ -251,10 +251,11 @@ def precision_recall_curve_padded_thresholds(*args, **kwargs):
     precision, recall, thresholds = precision_recall_curve(*args, **kwargs)
 
     xp, _ = get_namespace(precision, recall, thresholds)
+    dtype_ = _max_precision_float_dtype(thresholds)
 
     pad_thresholds = precision.shape[0] - thresholds.shape[0]
     thresholds_padded = xp.concat(
-        [xp.astype(thresholds, xp.float64), xp.full(pad_thresholds, xp.nan)]
+        [xp.astype(thresholds, dtype_), xp.full(pad_thresholds, xp.nan)]
     )
     return xp.asarray([precision, recall, thresholds_padded])
 
