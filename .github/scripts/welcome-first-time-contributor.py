8040e96a7cc349ad61fedeb330792abff9da13df
Virgil Chan <virchan.math@gmail.com>
2026-07-18 16:42:00 -0700

    Move close immature pr to first-time contributor workflow

--- .github/scripts/welcome-first-time-contributor.py
+++ .github/scripts/welcome-first-time-contributor.py
@@ -1,6 +1,6 @@
-"""Close PRs from first-time contributors that link to a not-ready issue.
+"""First-time Contributor Workflow
 
-Called from .github/workflows/close-first-time-non-ready-prs.yml.
+Called from .github/workflows/welcome-first-time-contributor.yml.
 """
 
 import json
@@ -9,11 +9,16 @@ import urllib.request
 
 from github import Github
 
-GITHUB_REPO = "scikit-learn/scikit-learn"
+GITHUB_REPO = os.getenv("GITHUB_REPO")
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 PR_NUMBER = int(os.getenv("PR_NUMBER"))
 
-CLOSING_ISSUE_QUERY = """
+
+def get_linked_issue(gh_repo, pr_number):
+    """Get the linked issue of the PR."""
+    owner, name = gh_repo.split("/")
+
+    CLOSING_ISSUE_QUERY = """
 query($owner: String!, $name: String!, $pr_number: Int!) {
   repository(owner: $owner, name: $name) {
     pullRequest(number: $pr_number) {
@@ -31,10 +36,6 @@ query($owner: String!, $name: String!, $pr_number: Int!) {
   }
 }
 """
-
-
-def get_linked_issue(gh_repo, pr_number):
-    owner, name = gh_repo.split("/")
     request = urllib.request.Request(
         "https://api.github.com/graphql",
         method="POST",
@@ -58,26 +59,28 @@ def get_linked_issue(gh_repo, pr_number):
 
 
 def is_not_ready(issue):
+    """Determine if an issue is still not ready for a PR."""
     return any(
         label["name"].startswith("Needs") or label["name"] == "RFC"
         for label in issue["labels"]["nodes"]
     )
 
 
+gh = Github(GITHUB_TOKEN)
+repo = gh.get_repo(GITHUB_REPO)
+pr = repo.get_pull(PR_NUMBER)
+
 linked_issue = get_linked_issue(GITHUB_REPO, PR_NUMBER)
 
 if linked_issue and is_not_ready(linked_issue):
-    gh = Github(GITHUB_TOKEN)
-    repo = gh.get_repo(GITHUB_REPO)
-    pr = repo.get_pull(PR_NUMBER)
-
+    # Close the PR if the linked issue is not ready
     MESSAGE = (
         "Thank you for your interest in contributing to scikit-learn.\n\n"
         "The linked issue is still under discussion, and the maintainers have not "
         "yet reached consensus on how it should be resolved. Before opening a pull "
         "request, please review the \"Issues tagged 'Needs Triage'\" section of the "
         "Contributing Guide:\n"
-        "https://scikit-learn.org/dev/developers/contributing.html"
+        "https://scikit-learn.org/stable/developers/contributing.html"
         "#issues-tagged-needs-triage\n\n"
         "For now, we are closing this pull request.\n\n"
         "If you believe your proposed change addresses the linked issue, please "
@@ -90,3 +93,28 @@ if linked_issue and is_not_ready(linked_issue):
     pr.create_issue_comment(MESSAGE)
     pr.edit(state="closed")
     pr.add_to_labels("autoclose")
+
+if pr.state == "open":
+    # Post welcome comment
+    MESSAGE = (
+        "Thank you for opening your first pull request to scikit-learn! 🎉"
+        "\n\n"
+        "To help get your contribution reviewed, please make sure that:"
+        "\n\n"
+        "* You have filled out the "
+        "[pull request template]"
+        "(https://github.com/scikit-learn/scikit-learn/blob/main/.github/PULL_REQUEST_TEMPLATE.md)."
+        "\n\n"
+        "* The pull request addresses an existing issue that is ready for contribution "
+        "(e.g. not tagged as 'Needs Triage', 'Needs Decision', ...). "
+        "If you are proposing a new feature, please open an issue to discuss it first."
+        "\n\n"
+        "* There are no other open pull requests already targeting the same issue."
+        "\n\n"
+        "* You have followed the "
+        "[pull request checklist]"
+        "(https://scikit-learn.org/stable/developers/contributing.html#pull-request-checklist)."
+        " In particular, linting and tests should pass."
+    )
+
+    pr.create_issue_comment(MESSAGE)
