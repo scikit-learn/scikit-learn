60239fceba35d0b65f9de899b149f4e4495447f7
Dea María Léon <deamarialeon@gmail.com>
2026-07-21 11:41:56 +0200

    Added TODO note

--- build_tools/github/install.sh
+++ build_tools/github/install.sh
@@ -77,6 +77,8 @@ python_environment_install_and_activate() {
 
         check_packages_dev_version $dev_packages
 
+        # TODO Remove use cython dev once
+        # https://github.com/cython/cython/issues/7846 is fixed
         # Temporary work-around to upload development wheels
         echo "Installing cython 3.2.5 as temporary work-around for cython dev issues"
         pip install cython==3.2.5
