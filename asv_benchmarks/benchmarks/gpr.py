35e9ab54021b37d48c8c6f45fc85dda77d802063
Michelle Laurenti <michelle.laurenti@spacearthnav.it>
2026-07-27 15:11:05 +0200

    refactor GPR benchmark with asv

---
+++ asv_benchmarks/benchmarks/gpr.py
@@ -0,0 +1,48 @@
+from joblib import parallel_config
+
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.gaussian_process.kernels import ConstantKernel as C
+
+from .common import Benchmark, Estimator
+from .datasets import _synth_regression_dataset
+from .utils import make_gen_reg_scorers
+
+
+class GaussianProcessRegressorBenchmark(Estimator, Benchmark):
+    """
+    Benchmarks for GaussianProcessRegressor.
+    """
+
+    param_names = ["n_restarts", "n_jobs"]
+    params = ([0, 1, 2, 4, 8], Benchmark.n_jobs_vals)
+
+    def make_data(self, params):
+        return _synth_regression_dataset(n_samples=500, n_features=5, n_informative=5)
+
+    def make_estimator(self, params):
+        n_restarts, _ = params
+
+        kernel = C(0.1, (1e-2, 1e2)) * RBF(
+            length_scale=1.0, length_scale_bounds=(1e-3, 1e3)
+        ) + C(1e-5, (1e-5, 1e2))
+
+        return GaussianProcessRegressor(
+            kernel=kernel,
+            n_restarts_optimizer=n_restarts,
+            normalize_y=True,
+            random_state=0,
+        )
+
+    def make_scorers(self):
+        make_gen_reg_scorers(self)
+
+    def time_fit(self, *args):
+        _, n_jobs = args
+        with parallel_config(n_jobs=n_jobs):
+            super().time_fit(*args)
+
+    def peakmem_fit(self, *args):
+        _, n_jobs = args
+        with parallel_config(n_jobs=n_jobs):
+            super().peakmem_fit(*args)
