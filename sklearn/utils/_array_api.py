a7ef8c6b21515016d38e1e5449ebe9e1b2808785
Tim Head <betatim@gmail.com>
2026-08-04 14:41:35 +0200

    Always enable array API support when using `move_to`
    Without array API support enabled the namespace determination does not
    work properly. Which means we incorrectly determine that the namespace
    of an array is numpy, even if it is not.

--- sklearn/utils/_array_api.py
+++ sklearn/utils/_array_api.py
@@ -14,7 +14,7 @@ import numpy
 import scipy
 import scipy.sparse as sp
 
-from sklearn._config import get_config
+from sklearn import config_context, get_config
 from sklearn.externals import array_api_compat
 from sklearn.externals import array_api_extra as xpx
 from sklearn.externals.array_api_compat import numpy as np_compat
@@ -558,76 +558,82 @@ def move_to(*arrays, xp, device):
             "namespace is Numpy"
         )
 
-    arrays_ = arrays
-    # Down cast float64 `arrays` when highest precision of `xp`/`device` is float32
-    if _max_precision_float_dtype(xp, device) == xp.float32:
-        arrays_ = []
-        for array in arrays:
-            xp_array, _ = get_namespace(array)
-            if getattr(array, "dtype", None) == xp_array.float64:
-                arrays_.append(xp_array.astype(array, xp_array.float32))
-            else:
-                arrays_.append(array)
+    with config_context(array_api_dispatch=True):
+        arrays_ = arrays
+        # Down cast float64 `arrays` when highest precision of `xp`/`device` is float32
+        if _max_precision_float_dtype(xp, device) == xp.float32:
+            arrays_ = []
+            for array in arrays:
+                xp_array, _ = get_namespace(array)
+                if getattr(array, "dtype", None) == xp_array.float64:
+                    arrays_.append(xp_array.astype(array, xp_array.float32))
+                else:
+                    arrays_.append(array)
 
-    converted_arrays = []
-    for array, is_sparse, is_none in zip(arrays_, sparse_mask, none_mask):
-        if is_none:
-            converted_arrays.append(None)
-        elif is_sparse:
-            converted_arrays.append(array)
-        else:
-            xp_array, _, device_array = get_namespace_and_device(array)
-            if xp == xp_array and device == device_array:
+        converted_arrays = []
+        for array, is_sparse, is_none in zip(arrays_, sparse_mask, none_mask):
+            if is_none:
+                converted_arrays.append(None)
+            elif is_sparse:
                 converted_arrays.append(array)
             else:
-                if _is_xp_namespace(xp, "torch") and _is_numpy_namespace(xp_array):
-                    if any(stride < 0 for stride in array.strides):
-                        # Work around PyTorch aborting the process when importing
-                        # negative-strided NumPy arrays with DLPack. Remove this once
-                        # https://github.com/pytorch/pytorch/issues/188023 is fixed.
-                        # See also https://github.com/scikit-learn/scikit-learn/issues/34307
-                        array = numpy.ascontiguousarray(array)
-                try:
-                    # The dlpack protocol is the future proof and library agnostic
-                    # method to transfer arrays across namespace and device boundaries
-                    # hence this method is attempted first and going through NumPy is
-                    # only used as fallback in case of failure.
-                    # Note: copy=None is the default since array-api 2023.12. Namespace
-                    # libraries should only trigger a copy automatically if needed.
-                    array_converted = xp.from_dlpack(array, device=device)
-                    # `AttributeError` occurs when `__dlpack__` and `__dlpack_device__`
-                    # methods are not present on the input array
-                    # `TypeError` and `NotImplementedError` for packages that do not
-                    # yet support dlpack 1.0
-                    # (i.e. the `device`/`copy` kwargs, e.g., torch <= 2.8.0)
-                    # See https://github.com/data-apis/array-api/pull/741 for
-                    # more details about the introduction of the `copy` and `device`
-                    # kwargs in the from_dlpack method and their expected
-                    # meaning by namespaces implementing the array API spec.
-                    # TODO: try removing this once DLPack v1 more widely supported
-                    # TODO: ValueError not needed once min NumPy >=2.4.0:
-                    # https://github.com/numpy/numpy/issues/30341
-                except (
-                    AttributeError,
-                    TypeError,
-                    NotImplementedError,
-                    BufferError,
-                    ValueError,
-                ):
-                    # Converting to numpy is tricky, handle this via dedicated function
-                    if _is_numpy_namespace(xp):
-                        array_converted = _convert_to_numpy(array, xp_array)
-                    # Convert from numpy, all array libraries can do this
-                    elif _is_numpy_namespace(xp_array):
-                        array_converted = xp.asarray(array, device=device)
-                    else:
-                        # There is no generic way to convert from namespace A to B
-                        # So we first convert from A to numpy and then from numpy to B
-                        # The way to avoid this round trip is to lobby for DLpack
-                        # support in libraries A and B
-                        array_np = _convert_to_numpy(array, xp_array)
-                        array_converted = xp.asarray(array_np, device=device)
-                converted_arrays.append(array_converted)
+                xp_array, _, device_array = get_namespace_and_device(array)
+                if xp == xp_array and device == device_array:
+                    converted_arrays.append(array)
+                else:
+                    if _is_xp_namespace(xp, "torch") and _is_numpy_namespace(xp_array):
+                        if any(stride < 0 for stride in array.strides):
+                            # Work around PyTorch aborting the process when importing
+                            # negative-strided NumPy arrays with DLPack. Remove this
+                            # once https://github.com/pytorch/pytorch/issues/188023 is
+                            # fixed. See also
+                            # https://github.com/scikit-learn/scikit-learn/issues/34307
+                            array = numpy.ascontiguousarray(array)
+                    try:
+                        # The dlpack protocol is the future proof and library agnostic
+                        # method to transfer arrays across namespace and device
+                        # boundaries hence this method is attempted first and going
+                        # through NumPy is only used as fallback in case of failure.
+                        # Note: copy=None is the default since array-api 2023.12.
+                        # Namespace libraries should only trigger a copy automatically
+                        # if needed.
+                        array_converted = xp.from_dlpack(array, device=device)
+                        # `AttributeError` occurs when `__dlpack__` and
+                        # `__dlpack_device__` methods are not present on the input
+                        # array `TypeError` and `NotImplementedError` for packages
+                        # that do not yet support dlpack 1.0
+                        # (i.e. the `device`/`copy` kwargs, e.g., torch <= 2.8.0)
+                        # See https://github.com/data-apis/array-api/pull/741 for
+                        # more details about the introduction of the `copy` and
+                        # `device` kwargs in the from_dlpack method and their expected
+                        # meaning by namespaces implementing the array API spec.
+                        # TODO: try removing this once DLPack v1 more widely
+                        # TODO: supported ValueError not needed once min
+                        # TODO: NumPy >=2.4.0:
+                        # TODO: https://github.com/numpy/numpy/issues/30341
+                    except (
+                        AttributeError,
+                        TypeError,
+                        NotImplementedError,
+                        BufferError,
+                        ValueError,
+                    ):
+                        # Converting to numpy is tricky, handle this via dedicated
+                        # function
+                        if _is_numpy_namespace(xp):
+                            array_converted = _convert_to_numpy(array, xp_array)
+                        # Convert from numpy, all array libraries can do this
+                        elif _is_numpy_namespace(xp_array):
+                            array_converted = xp.asarray(array, device=device)
+                        else:
+                            # There is no generic way to convert from namespace A to B
+                            # So we first convert from A to numpy and then from
+                            # numpy to B.
+                            # The way to avoid this round trip is to lobby for DLpack
+                            # support in libraries A and B
+                            array_np = _convert_to_numpy(array, xp_array)
+                            array_converted = xp.asarray(array_np, device=device)
+                    converted_arrays.append(array_converted)
 
     return (
         converted_arrays[0] if len(converted_arrays) == 1 else tuple(converted_arrays)
@@ -1057,14 +1063,15 @@ def _convert_to_numpy(array, xp):
     This function is not meant to be called directly and
     `move_to(array, xp=np, device="cpu")` should be used instead.
     """
-    if _is_xp_namespace(xp, "torch"):
-        return array.cpu().numpy()
-    elif _is_xp_namespace(xp, "cupy"):  # pragma: nocover
-        return array.get()
-    elif _is_xp_namespace(xp, "array_api_strict"):
-        return numpy.asarray(xp.asarray(array, device=xp.Device("CPU_DEVICE")))
-    elif _is_xp_namespace(xp, "dpnp"):  # pragma: nocover
-        return array.asnumpy()
+    with config_context(array_api_dispatch=True):
+        if _is_xp_namespace(xp, "torch"):
+            return array.cpu().numpy()
+        elif _is_xp_namespace(xp, "cupy"):  # pragma: nocover
+            return array.get()
+        elif _is_xp_namespace(xp, "array_api_strict"):
+            return numpy.asarray(xp.asarray(array, device=xp.Device("CPU_DEVICE")))
+        elif _is_xp_namespace(xp, "dpnp"):  # pragma: nocover
+            return array.asnumpy()
 
     return numpy.asarray(array)
 
