b8acd7c53611f9617e0b8bb9b9599008f4a82724
Itamar Turner-Trauring <itamar@itamarst.org>
2026-08-05 16:46:04 -0400

    Add returns to docstring.

--- sklearn/utils/parallel.py
+++ sklearn/utils/parallel.py
@@ -216,6 +216,14 @@ def _threadpool_controller_decorator(limits=1, user_api="blas"):
 
 
 def is_free_threaded() -> bool:
-    """Return whether the current Python has no GIL."""
+    """Return whether the current Python has no GIL.
+
+    .. versionadded:: 1.10
+
+    Returns
+    -------
+    bool
+        Whether the Python interpreter is free-threaded, i.e. has no GIL.
+    """
     is_gil_enabled = getattr(sys, "_is_gil_enabled", lambda: True)
     return not is_gil_enabled()
