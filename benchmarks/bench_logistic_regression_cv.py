ee35fa2a056dde2dd06a0ef5e80641d5dc1b373c
Itamar Turner-Trauring <itamar@itamarst.org>
2026-08-06 08:00:36 -0400

    Bigger training set.

--- benchmarks/bench_logistic_regression_cv.py
+++ benchmarks/bench_logistic_regression_cv.py
@@ -15,7 +15,7 @@ X, y = make_classification(
     n_samples=100_000, n_features=20, n_informative=2, n_redundant=2, random_state=42
 )
 
-train_samples = 1000  # Samples used for training the models
+train_samples = 10_000  # Samples used for training the models
 X_train, X_test, y_train, y_test = train_test_split(
     X,
     y,
