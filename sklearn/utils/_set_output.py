055997eee8e76475905e4c17faa757384f615591
Anton Karpov <30812217+karpovantonme@users.noreply.github.com>
2026-08-13 11:34:14 +0300

    DOC Fix parameter names in private function docstrings (#34741)

--- sklearn/utils/_set_output.py
+++ sklearn/utils/_set_output.py
@@ -73,8 +73,8 @@ class ContainerAdapterProtocol(Protocol):
 
         Parameters
         ----------
-        Xs: container
-            Containers to be checked.
+        X : container
+            Container to be checked.
 
         Returns
         -------
