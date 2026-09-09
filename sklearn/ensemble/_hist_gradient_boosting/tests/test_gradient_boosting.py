0d4c4b9e277116212e320338034259a9b80f6fde
Arthur Lacote <arthur.lcte@gmail.com>
2026-09-09 11:44:43 +0200

    fix test

--- sklearn/ensemble/_hist_gradient_boosting/tests/test_gradient_boosting.py
+++ sklearn/ensemble/_hist_gradient_boosting/tests/test_gradient_boosting.py
@@ -1837,7 +1837,7 @@ def test_get_heuristic_optimal_n_threads_monotonic_in_n_features():
     # there is at least one feature per thread.
     max_n_threads = 8
     n_threads_by_n_features = [
-        _get_heuristic_n_threads(max_n_threads, n_samples=10, n_features=n_features)
+        _get_heuristic_n_threads(max_n_threads, n_samples=10**5, n_features=n_features)
         for n_features in [1, 2, 4, 8, 16, 100]
     ]
     assert n_threads_by_n_features == sorted(n_threads_by_n_features)
